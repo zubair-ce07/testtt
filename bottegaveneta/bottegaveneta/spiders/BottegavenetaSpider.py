f8e0fc179a0a89b88298733bce2d96f6ca42f947
m-usman-dar <usman.ravian819@gmail.com>
2015-01-22 15:51:46 +0500

    remove unnessary_things

--- bottegaveneta/bottegaveneta/spiders/BottegavenetaSpider.py
+++ bottegaveneta/bottegaveneta/spiders/BottegavenetaSpider.py
@@ -15,14 +15,15 @@ class BottegavenetaSpider(CrawlSpider):
     start_urls = [
         "http://www.bottegaveneta.com/us"
     ]
-    first_page = './/*[@id="menuContainer"]//ul[1]//ul//li/a'
-    article_page = './/*[@id="grid_container"]/article[position()<4]/div[@class="overlay"]/a'
-    items_page = './/*[@id="results"]/div[position()<3]/a'
+    first_page_xpath = './/*[@id="menuContainer"]//ul[1]//ul//li/a'
+    article_page_xpath = './/*[@id="grid_container"]/article[position()<4]/div[@class="overlay"]/a'
+    items_page_xpath = './/*[@id="results"]/div[position()<3]/a'
     product_response = ''
     rules = [
-        Rule(SgmlLinkExtractor(deny=(r'home_section', r'furniture_section'), restrict_xpaths=first_page)),
+        Rule(SgmlLinkExtractor(deny=(r'home_section', r'furniture_section'), restrict_xpaths=first_page_xpath)),
+        Rule(SgmlLinkExtractor(deny=(r'home_section', r'furniture_section'), restrict_xpaths=article_page_xpath)),
         #rule which is used to extract links from first page main menu
-        Rule(SgmlLinkExtractor(restrict_xpaths=items_page, deny=[r'home_section']), callback='parse_get_detail'),
+        Rule(SgmlLinkExtractor(restrict_xpaths=items_page_xpath, deny=[r'home_section']), callback='parse_get_detail'),
         #rule is used to extract product's details
 
     ]
@@ -77,16 +78,12 @@ class BottegavenetaSpider(CrawlSpider):
             description.append(values.xpath('./text()').extract())
         return description
 
-    def get_price(self, res):
-        if res.xpath('.//*[@class="price"]/text()'):
-            price = res.xpath('.//*[@class="price"]/text()').extract()[0]
-        else:
-            price=res.xpath('.//*[@class="itemBoxPrice"]//text()').extract()[0]
+    def get_price(self, response):
+        price = response.xpath('.//*[@class="price" or @class="itemBoxPrice"]/text()').extract()[0]
         return price
 
     def get_skus(self, res):
         skus = {}
-        title = self.get_title(res)
         for sizes in res.xpath('.//*[@id="sizesUl"]/li'):
             arr = {}
             arr['currency'] = 'USD'
@@ -96,22 +93,3 @@ class BottegavenetaSpider(CrawlSpider):
             skus[arr['size'] + '_' + arr['colour']] = arr
         return skus
 
-    def parse_get_detail(self, response):
-        self.product_response = response
-        item = BottegavenetaItem()
-        for res in self.product_response.xpath('.//*[@id="itemInfoBox"]'):
-            item['retailer'] = 'bottegaveneta'
-            if ('gb' in self.product_response.url):
-                item['currency'] = 'Pound'
-            else:
-                item['currency'] = 'USD'
-            item['spider_name'] = self.name
-            item['category'] = self.get_cat()
-            item['link'] = self.product_response.url
-            item['title'] = self.get_title(res)
-            item['retailer_sk'] = self.get_retailer_sk()
-            item['color'] = self.get_color(res)
-            item['image_urls'] = self.get_images(res)
-            item['description'] = self.get_description(res)
-            item['skus'] = self.get_skus(res)
-            yield item
