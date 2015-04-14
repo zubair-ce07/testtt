4c12f13f5ad9696d0ef59f46d1060e0a6fbaf6f4
muhammad-irfan-arbisoft <muhammad.irfan@arbisoft.com>
2015-04-14 18:35:47 +0500

    cors added for restfull services

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,8 @@ var powersuiteApp = angular.module('powersuite', [
   'ngRoute',
   'ngResource',
   'powersuiteControllers',
-  'powersuiteServices'
+  'powersuiteServices',
+  'ui.bootstrap'
 ]);
 
 powersuiteApp.config(['$routeProvider',
@@ -22,3 +23,12 @@ powersuiteApp.config(['$routeProvider',
         redirectTo: 'partials/search.html'
       });
   }]);
+
+powersuiteApp.config(['$httpProvider', function($httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  $httpProvider.defaults.withCredentials = true;
+  delete $httpProvider.defaults.headers.common["X-Requested-With"];
+  $httpProvider.defaults.headers.common["Accept"] = "application/json";
+  $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+}
+]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,12 +4,16 @@
 
 var powersuiteControllers = angular.module('powersuiteControllers', []);
 
-powersuiteControllers.controller('SearchCtrl',['$scope','Dockets',
-  function($scope, Dockets) {
-    $scope.school = Dockets.getSchool;
+powersuiteControllers.controller('SearchCtrl',['$scope', '$http','apiUrl','User','Dockets',
+  function($scope, $http, apiUrl,User, Dockets) {
+
     $scope.searchDockets = function(dockets){
       console.log(dockets);
-      $scope.dockets = {states:10, keyword: "SEARCH", to:'2015-04-10', from:"2015-04-14"};
+      $scope.school = Dockets.getSchool;
+      //$scope.dockets = Dockets.getDockets;
+        //$scope.school = $http({method: 'GET', url: apiUrl + '/schools/1', params: {access_token: User.access_token, email: User.email}});
+        console.log($scope.school);
+      $scope.dockets = {states:10, word: "SEARCH", to: new Date(2015, 3, 12), from: new Date(2015,3,15)};
     }
   }]
 );
@@ -19,3 +23,41 @@ powersuiteControllers.controller('FavouritesCtrl',
 
   }
 );
+
+powersuiteControllers.controller('DatePickerCtrl', function ($scope) {
+  $scope.today = function() {
+    $scope.to_date = new Date();
+    $scope.from_date = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function () {
+    $scope.to_date = null;
+    $scope.from_date = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+});
