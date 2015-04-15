61c03ffbe197613b3787d3092056d87433f35e21
muhammad-irfan-arbisoft <muhammad.irfan@arbisoft.com>
2015-04-15 19:03:22 +0500

    tabs added.

--- app/js/app.js
+++ app/js/app.js
@@ -1,55 +1,53 @@
 'use strict';
 
 var powersuiteApp = angular.module('powersuite', [
-  'ngRoute',
-  'ngResource',
-  'powersuiteControllers',
-  'powersuiteServices',
-  'ui.bootstrap',
+    'ngRoute',
+    'ngResource',
+    'powersuiteControllers',
+    'powersuiteServices',
+    'ui.bootstrap',
     'ui.router',
     'ui.bootstrap',
     'ui.bootstrap.tpls'
 ]);
+//
+//powersuiteApp.config(['$routeProvider',
+//  function($routeProvider) {
+//    $routeProvider.
+//      when('/search', {
+//        templateUrl: 'partials/search.html',
+//        controller: 'SearchCtrl'
+//      }).
+//      when('/favorites', {
+//        templateUrl: 'partials/favourites.html',
+//        controller: 'FavouritesCtrl'
+//      }).
+//      otherwise({
+//        templateUrl: 'partials/home.html'
+//      });
+//  }]);
 
-powersuiteApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/search', {
-        templateUrl: 'partials/search.html',
-        controller: 'SearchCtrl'
-      }).
-      when('/favorites', {
-        templateUrl: 'partials/favourites.html',
-        controller: 'FavouritesCtrl'
-      }).
-      otherwise({
-        templateUrl: 'partials/home.html'
-      });
-  }]);
+powersuiteApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('search', {
+            url: '/search',
+            templateUrl: 'partials/search.html',
+            controller: 'SearchCtrl'
+        }).state('favorites', {
+            url: '/favorites',
+            templateUrl: 'partials/favourites.html',
+            controller: 'FavouritesCtrl'
+        }).state('home',{
+            url:'/home',
+            templateUrl: 'partials/home.html'
+        })
+}]);
 
-powersuiteApp.config(function($stateProvider, $urlRouterProvider){
-  $urlRouterProvider.otherwise('/home');
-  $stateProvider
-      .state("home",{
-        url:'/home',
-        templateUrl:'partials/home.html'
-      })
-       .state('search',{
-        url: '/search',
-        templateUrl:'partials/search.html',
-        controller: 'SearchCtrl'
-      }).state('favorites',{
-        url: '/favorites',
-        templateUrl:'partials/favourites.html',
-        controller: 'FavouritesCtrl'
-      })
-});
-
-powersuiteApp.config(['$httpProvider', function($httpProvider) {
-  delete $httpProvider.defaults.headers.common["X-Requested-With"];
-  $httpProvider.defaults.useXDomain = true;
-  $httpProvider.defaults.withCredentials = true;
-  $httpProvider.defaults.headers.common["Accept"] = "application/json";
-  $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+powersuiteApp.config(['$httpProvider', function ($httpProvider) {
+    delete $httpProvider.defaults.headers.common["X-Requested-With"];
+    $httpProvider.defaults.useXDomain = true;
+    $httpProvider.defaults.withCredentials = true;
+    $httpProvider.defaults.headers.common["Accept"] = "application/json";
+    $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
 }
 ]);
