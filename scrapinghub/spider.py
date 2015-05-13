256155b972023b4366e9a43b9b56527192a11302
Muhammad Usman <usman.ravian819@gmail.com>
2015-05-13 21:01:23 +0500

    rewritten parse_store_hours method

--- scrapinghub/spider.py
+++ scrapinghub/spider.py
@@ -3,13 +3,17 @@ from scrapy.contrib.spiders import CrawlSpider
 
 class BaseSpider(CrawlSpider):
 
-    list_of_days = [u'mon', u'tue', u'wed', u'thu', u'fri', u'sat', u'sun',
-                    u'monday', u'tuesday', u'wednesday', u'thursday', u'friday', u'saturday', u'sunday']
+    full_names_of_days = [u'monday', u'tuesday', u'wednesday', u'thursday', u'friday', u'saturday', u'sunday']
+    abbreviation_of_days = [u'mon', u'tue', u'wed', u'thu', u'fri', u'sat', u'sun']
 
-    def parse_store_hours(self, days, hour_timings, hours):
-        i, j = [self.list_of_days.index(s.strip()) for s in days.strip(':').lower().split('-')]
-        for day in self.list_of_days[i:j + 1]:
-            hours[day.title()] = hour_timings
+    def parse_store_hours(self, days, hour_timings, hours, abbreviation_flag=False):
+        start_index, end_index = [self.abbreviation_of_days.index(s.strip()[:3]) for s in days.lower().split('-')]
+        if abbreviation_flag:
+            for day in self.abbreviation_of_days[start_index:end_index + 1]:
+                hours[day.title()] = hour_timings
+        else:
+            for day in self.full_names_of_days[start_index:end_index + 1]:
+                hours[day.title()] = hour_timings
 
     def __init__(self, *a, **kw):
         super(BaseSpider, self).__init__(*a, **kw)
