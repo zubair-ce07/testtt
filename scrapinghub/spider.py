74e7c8d8cd2101d2ef449b465233367bd645b073
Muhammad Usman <usman.ravian819@gmail.com>
2015-05-13 12:11:07 +0500

    added parse_store_hours method and list of days

--- scrapinghub/spider.py
+++ scrapinghub/spider.py
@@ -2,6 +2,15 @@ from scrapy.contrib.spiders import CrawlSpider
 
 
 class BaseSpider(CrawlSpider):
+
+    list_of_days = [u'mon', u'tue', u'wed', u'thu', u'fri', u'sat', u'sun',
+                    u'monday', u'tuesday', u'wednesday', u'thursday', u'friday', u'saturday', u'sunday']
+
+    def parse_store_hours(self, days, hour_timings, hours):
+        i, j = [self.list_of_days.index(s.strip()) for s in days.strip(':').lower().split('-')]
+        for day in self.list_of_days[i:j + 1]:
+            hours[day.title()] = hour_timings
+
     def __init__(self, *a, **kw):
         super(BaseSpider, self).__init__(*a, **kw)
 
