1cf11f745cbf6d1a484202227feeb92284ff67fe
Muhammad Usman <usman.ravian819@gmail.com>
2015-05-19 12:00:01 +0500

    setting updated
    add download delay, CONCURRENT_REQUESTS

--- Project 1/stores_location_product_spiders/stores_location_spiders/settings.py
+++ Project 1/stores_location_product_spiders/stores_location_spiders/settings.py
@@ -18,3 +18,13 @@ FEED_URI = "jsons/%(name)s-%(time)s.json"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'stores_location_spiders (+http://www.yourdomain.com)'
+from datetime import datetime
+datetime.now().strftime("%Y%m%d_%H%M%S")
+HTTPCACHE_ENABLED = False
+
+DEFAULT_REQUEST_HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.104 Safari/537.36'
+}
+
+DOWNLOAD_DELAY = 5
+CONCURRENT_REQUESTS=5
