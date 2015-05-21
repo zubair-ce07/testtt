1afade828d6273a8de45b8eaafed4006e167e362
Muhammad Usman <usman.ravian819@gmail.com>
2015-05-21 11:45:40 +0500

    made pipeline to filter empty items

--- Project 1/stores_location_product_spiders/stores_location_and_product_spiders/pipelines.py
+++ Project 1/stores_location_product_spiders/stores_location_and_product_spiders/pipelines.py
@@ -6,6 +6,9 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class StoresLocationSpidersPipeline(object):
+class StoresLocationProductPipeline(object):
     def process_item(self, item, spider):
+        for key_name in list(item.keys()):
+            if not(item[key_name]):
+                del item[key_name]
         return item
