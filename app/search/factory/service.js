d0cbc34f6fbf8838fe83e0233a9697b08a6ba535
Rehan Munir <rehan.munir@arbisoft.com>
2015-05-29 15:53:50 +0500

    fix merge branch

--- app/search/factory/service.js
+++ app/search/factory/service.js
@@ -12,32 +12,6 @@ ngdocket.factory('Docket', ['$resource', function($resource) {
     return new Docket;
 }]);
 
-ngdocket.factory('Reddit', function ($http) {
-    var Reddit = {
-        dockets: [],
-        busy: false,
-        cursor: '',
-        get: function () {
-            /*if (this.busy) return;
-             this.busy = true;*/
-
-            var url = 'http://aeedocketapi-staging.appspot.com/dockets?api_key=d4dc4045dd431d43b317190a41b982aa&cursor=' + this.cursor + '&json=JSON_CALLBACK"';
-            return $http.get(url).success(function (data) {
-                /*var items = data.dockets;
-                 for (var i = 0; i < items.length; i++) {
-                 this.dockets.push(items[i]);
-                 }
-                 this.cursor = data.metadata.cursor;
-                 this.busy = false;*/
-            });
-    }
-    };
-<<<<<<< HEAD
-
-    return Reddit;
-=======
-    return new Filing;
-}]);
 
 
 ngdocket.factory('Reddit', function($http) {
@@ -46,21 +20,14 @@ ngdocket.factory('Reddit', function($http) {
     busy : false,
     cursor : '',
     get : function() {
-        /*if (this.busy) return;
-        this.busy = true;*/
-
+        
         var url = 'http://aeedocketapi-staging.appspot.com/dockets?api_key=d4dc4045dd431d43b317190a41b982aa&cursor=' + this.cursor + '&json=JSON_CALLBACK"';
         return $http.get(url).success(function(data) {
-          /*var items = data.dockets;
-          for (var i = 0; i < items.length; i++) {
-            this.dockets.push(items[i]);
-          }
-          this.cursor = data.metadata.cursor;
-          this.busy = false;*/
+
         });
     }
   };
 
   return Reddit;
->>>>>>> 6dea22f805082b9f28f435b83e6ba924a8287958
+
 });
