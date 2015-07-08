513c2dc1899800f2b9896face0169e61b84fea0d
mateen91 <mateen.ahmedd@gmail.com>
2015-07-08 16:49:33 +0500

    assignment1 version -03

--- weatherApp.py
+++ weatherApp.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+import csv
 import os
 import sys
+import argparse
 
 
 # class that will contain the data entries for organized functioning
-class DetailInfo:
-
+class WeatherRecord(object):
     def __init__(self, date, maxtemp, mintemp, maxhumid, minhumid):
         self.date = date
         self.maxTemp = maxtemp
@@ -16,120 +17,74 @@ class DetailInfo:
 
 # contains the logic if user selects option one
 # that is generation of Annual report
-def option1():
-    print("Year     MaxTemp     MinTemp     MaxHumid    MinHumid")
-    print("-----------------------------------------------------")
+def yearly_weather_report(yearly_data, yearlist):
+    heads=["Year","MaxTemp","MinTemp"," MaxHumid","MinHumid"]
+    print('\t\t'.join(heads))
+    print("---------------------------------------------------------------------")
     # going through every year
     for year in yearlist:
-
         # getting list against year from dictionary
-        list_cur = my_dict[year]
+        list_cur = yearly_data[year]
+        list_display= []
+        list_display.append(year)
 
         # Sorting list on Max Temp
-        list_cur.sort(key = lambda x: x.maxTemp, reverse = True)
-        # this loop will find the true value
-        # because there are some dummy values
-        # this wont be inefficient
-        #  because value will be found in less than 10 tries
-        pos = 0
-        while True:
-            if (list_cur[pos].maxTemp == "Max TemperatureC") or \
-                    (list_cur[pos].maxTemp == ""):
-                pos = pos + 1
-            else:
-                maxT = list_cur[pos].maxTemp
-                break
+        list_cur.sort(key=lambda x: x.maxTemp, reverse=True)
+        list_display.append(str(list_cur[0].maxTemp))
 
         # Sorting list on Min Temp
-        list_cur.sort(key = lambda x: x.minTemp, reverse = False)
-        pos = 0
-        # same upper Loop
-        while True:
-            if (list_cur[pos].minTemp == "Min TemperatureC") or \
-                    (list_cur[pos].minTemp == ""):
-                pos = pos + 1
-            else:
-                minT = list_cur[pos].minTemp
-                break
-
-
-        # Sorting list on Min humidity
-        list_cur.sort(key = lambda x: x.minHumid, reverse = False)
-        pos = 0
-        # same loop
-        while True:
-            if (list_cur[pos].minHumid == " Min Humidity") or \
-                    (list_cur[pos].minHumid == ""):
-                pos = pos + 1
-            else:
-                minH = list_cur[pos].minHumid
-                break
+        list_cur.sort(key=lambda x: x.minTemp, reverse=False)
+        list_display.append(str(list_cur[0].minTemp))
 
         # Sorting list on Max humidity
-        list_cur.sort(key = lambda x: x.maxHumid, reverse = True)
-        pos = 0
-        # same loop
-        while True:
-            if (list_cur[pos].maxHumid == "Max Humidity") or \
-                    (list_cur[pos].maxHumid == ""):
-                pos = pos + 1
-            else:
-                maxH = list_cur[pos].maxHumid
-                break
+        list_cur.sort(key=lambda x: x.maxHumid, reverse=True)
+        list_display.append(str(list_cur[0].maxHumid))
 
-        print(year + "       " + maxT + "          " + minT +
-              "          " + maxH + "           " + minH)
+        # Sorting list on Min humidity
+        list_cur.sort(key=lambda x: x.minHumid, reverse=False)
+        list_display.append(str(list_cur[0].minHumid))
 
+        print('\t\t'.join(list_display))
 
 # contains the logic if user selects option two
 # that is generation of Yearly Max temp report
-def option2():
-    print("Year          Date        Max temp")
-    print("-------------------------------")
+def max_temp_report(yearly_data, yearlist):
+    heads=["Year","Date","MaxTemp"]
+    print('\t\t'.join(heads))
+    print("-----------------------------------------------")
     # going through every year
     for year in yearlist:
         # getting list against year from dictionary
-        list_cur = my_dict[year]
+        list_cur = yearly_data[year]
 
         # Sorting list on Max Temp
-        list_cur.sort(key = lambda x: x.maxTemp, reverse = True)
+        list_cur.sort(key=lambda x: x.maxTemp, reverse=True)
 
-        # this loop will find the true value
-        pos = 0
-        while True:
-            if (list_cur[pos].maxTemp == "Max TemperatureC") or \
-                    (list_cur[pos].maxTemp == ""):
-                pos = pos + 1
-            else:
-                print(year + "       " + list_cur[pos].date +
-                      "          " + list_cur[pos].maxTemp)
-                break
+        list_display= []
+        list_display.append(year)
+        list_display.append(str(list_cur[0].date ))
+        list_display.append(str(list_cur[0].maxTemp))
+        print('\t\t'.join(list_display))
 
 
 # contains the logic if user selects option two
 # that is generation of Yearly Min temp report
-def option3():
-    print("Year          Date        Min Temp")
-    print("-----------------------------------")
+def min_temp_report(yearly_data, yearlist):
+    heads=["Year","Date","Min Temp"]
+    print('\t\t'.join(heads))
+    print("--------------------------------------------")
 
     # going through every year
     for year in yearlist:
         # getting list against year from dictionary
-        list_cur = my_dict[year]
+        list_cur = yearly_data[year]
         # Sorting list on Max Temp
-        list_cur.sort(key = lambda x: x.minTemp, reverse = False)
-
-        # this loop will find the true value
-        pos = 0
-        while True:
-            if (list_cur[pos].minTemp == "Min TemperatureC") or \
-                    (list_cur[pos].minTemp == ""):
-                pos = pos + 1
-            else:
-                minT = list_cur[pos].minTemp
-                print(year + "       " + list_cur[pos].date +
-                      "          " + list_cur[pos].minTemp)
-                break
+        list_cur.sort(key=lambda x: x.minTemp, reverse=False)
+        list_display= []
+        list_display.append(year)
+        list_display.append(str(list_cur[0].date ))
+        list_display.append(str(list_cur[0].minTemp))
+        print('\t\t'.join(list_display))
 
 
 # instructions for running the code file
@@ -146,88 +101,103 @@ def instructions():
     print "Path of Directory containing weather data files"
     print " "
 
-# dictionary data struct for having list of data against year
-# i-e [key:value]-> [year: listofusrecords]
-my_dict = {}
-# simple list of years to avoid hard coding of years
-yearlist = []
 
-# argument list check
-if len(sys.argv) != 3:
-    instructions()
-    sys.exit()
-try:
-    option = int(sys.argv[1])
-except:
-    instructions()
-    sys.exit()
-path = sys.argv[2]
-# path = '/home/mateenahmeed/Downloads/weatherdata'
+def main():
+    # dictionary data struct for having list of data against year
+    # i-e [key:value]-> [year: listofusrecords]
+    yearly_data = {}
 
-# checking of Directory path exists or not
-if not os.path.exists(path):
-    print("Directory path not found : " + path)
-    instructions()
-    sys.exit()
+    # simple list of years to avoid hard coding of years
+    yearlist = []
 
-# iterating through every file
-for filename in os.listdir(path):
-    # splitting the filename to get the year
-    tokens = filename.split("_")
-    year = tokens[2]
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument("report")
+        parser.add_argument("data_dir_path")
+        args = parser.parse_args()
+    except:
+        print instructions()
+        sys.exit()
+    try:
+        option = int(args.report)
+    except:
+        print instructions()
+        sys.exit()
+    path = args.data_dir_path
+    # path = '/home/mateenahmeed/Downloads/weatherdata'
 
-    # test list of records for local calculation
-    list1 = []
+    # checking of Directory path exists or not
+    if not os.path.exists(path):
+        print("Directory path not found : " + path)
+        instructions()
+        sys.exit()
+    try:
+        # iterating through every file
+        for filename in os.listdir(path):
+            # splitting the filename to get the year
+            tokens = filename.split("_")
+            year = tokens[2]
 
-    # if entry exists against that year in dictionary
-    # retrieve the list
-    if year in my_dict:
-        list1 = my_dict[year]
-    # otherwise add new list to dictionary
+            # test list of records for local calculation
+            temp_list = []
+
+            # if entry exists against that year in dictionary
+            # retrieve the list
+            if year in yearly_data:
+                temp_list = yearly_data[year]
+            # otherwise add new list to dictionary
+            else:
+                yearly_data[year] = temp_list
+                yearlist.append(year)
+
+            # initializing to dummy values
+            date = "no date specified"
+            minT = 1000
+            maxT = (-1000)
+            minH = 1000
+            maxH = (-1000)
+            # opening file
+            file = open(path + "/" + filename, 'r')
+
+            # using csv to read records
+            lines = file.readlines()[1:-1]
+            records = csv.DictReader(lines)
+            for row in records:
+                try:
+                    date = row['PKT']
+                except KeyError:
+                    date = row['PKST']
+                if row['Max TemperatureC'] is not '':
+                    maxT = int(row['Max TemperatureC'])
+                if row['Min TemperatureC'] is not '':
+                    minT = int(row['Min TemperatureC'])
+                if row['Max Humidity'] is not '':
+                    maxH = int(row['Max Humidity'])
+                if row[' Min Humidity'] is not '':
+                    minH = int(row[' Min Humidity'])
+
+                # Adding each record to list
+                temp_list.append(WeatherRecord(date, maxT, minT, maxH, minH))
+
+            file.close()
+            # updating the dictionary with new updated list of records
+            yearly_data[year] = temp_list
+    except:
+        instructions()
+        sys.exit()
+    # sorting year records to get organized results
+    yearlist.sort()
+
+    # function calls against user choice
+    if option == 1:
+        yearly_weather_report(yearly_data, yearlist)  # annual weather report
+    elif option == 2:
+        max_temp_report(yearly_data, yearlist)  # max temp report
+    elif option == 3:
+        min_temp_report(yearly_data, yearlist)  # min temp report
     else:
-        my_dict[year] = list1
-        yearlist.append(year)
+        instructions()
+        sys.exit()
 
-    # opening file
-    file = open(path + "/" + filename, 'r')
-    # going through line by line
-    for line in file:
-        # splitting data on teh basis of comma
-        details = line.split(",")
-
-        # length check is to avoid last lines that are extra in each file
-        if len(details) > 1:
-
-            date = details[0]
-            maxT = details[1]
-            # zero is padded to single digits to get correct
-            # sorting results as everything is in string format
-            if len(maxT) == 1:
-                maxT = "0" + maxT
-            minT = details[3]
-            if len(minT) == 1:
-                minT = "0" + minT
-            maxH = details[7]
-            if len(maxH) == 1:
-                maxH = "0" + maxT
-            minH = details[9]
-            if len(minH) == 1:
-                minH = "0" + minH
-            # Adding each record to list
-            list1.append(DetailInfo(date, maxT, minT, maxH, minH))
-
-    file.close()
-
-    # updating the dictionary with new updated list of records
-    my_dict[year] = list1
-
-# sorting year records to get organized results
-yearlist.sort()
-
-# function calls against user choice
-if option == 1:
-    option1()       # annual report
-if option == 2:
-    option2()       # max temp report
-if option == 3:
-    option3()       # min temp report
+if __name__ == "__main__":
+    main()
