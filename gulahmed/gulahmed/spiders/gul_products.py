0e542ee85812ba0c347662bcfa0a26faed15c6fb
mateen91 <mateen.ahmedd@gmail.com>
2015-08-06 16:32:28 +0500

    v2 optimized Xpaths

--- gulahmed/gulahmed/spiders/gul_products.py
+++ gulahmed/gulahmed/spiders/gul_products.py
@@ -5,24 +5,22 @@ from scrapy.linkextractors.sgml import SgmlLinkExtractor
 from gulahmed.items import gulItem
 
 
-class MySpider(CrawlSpider):
+class GulahmedSpider(CrawlSpider):
     name = "gulahmed"
     allowed_domains = ["gulahmedshop.com"]
     start_urls = ['http://www.gulahmedshop.com/']
 
     rules = (
-        Rule(SgmlLinkExtractor(restrict_xpaths=("//*[@id='site-menu']/ul/li//li",
-                                                ))),
-        Rule(SgmlLinkExtractor(restrict_xpaths=("//*[@id='main-content']//section/section[1]//*[@id='brands-list']",
-                                               )),
-            callback="parse_items")
+        Rule(SgmlLinkExtractor(restrict_xpaths=("//*[@id='site-menu']//ul[contains(@class,'mega-sub')]",))),
+        Rule(SgmlLinkExtractor(deny=("PriceCondition",), restrict_xpaths=("//*[@id='brands-list']",)),
+             callback="parse_items")
     )
 
     # this will parse all the products on current page
     def parse_items(self, response):
 
         sel = response.xpath("/html")
-        nextpage = sel.xpath("//*[@id='frm']//a[@data-title='Next Page']/@href")
+        nextpage = sel.xpath("//a[@data-title='Next Page']/@href")
         if nextpage:
             next_page_url = response.urljoin(nextpage[0].extract())
             yield scrapy.Request(next_page_url, callback=self.parse_items)
@@ -32,17 +30,16 @@ class MySpider(CrawlSpider):
         for href in product_links:
             plink = href.xpath(".//div[@class='entry-media']/a/@href").extract()
             full_url = response.urljoin(plink[0])
-            #availability of item
-            availabity = self.get_availability(href)
-            yield scrapy.Request(full_url, callback=self.parse_product, meta={'availabilty': availabity})
+            # availability of item
+            availability = self.get_availability(href)
+            yield scrapy.Request(full_url, callback=self.parse_product, meta={'availability': availability})
 
-     # for each product
+    # for each product
     def parse_product(self, response):
         sel = response.xpath("/html")
-        # item = response.meta["item"]
-        item = gulItem()
-        item['item_is_avaiable'] = response.meta["availabilty"]
 
+        item = gulItem()
+        item['item_is_avaiable'] = response.meta["availability"]
         item['item_url'] = response.url
         item['item_category_name'] = self.get_category(sel)
         item['item_brand_id'] = self.get_brand_id(sel)
@@ -63,10 +60,8 @@ class MySpider(CrawlSpider):
 
     # for product description
     def get_description(self, sel):
-        des = sel.xpath("//*[@id='product-description']/p[2]/text()").extract()
-        if des:
-            return des[0]
-        return ""
+        des = sel.xpath("//*[@id='product-description']//p//text()").extract()
+        return des[0] if des else ""
 
     # category
     def get_category(self, sel):
@@ -79,12 +74,12 @@ class MySpider(CrawlSpider):
 
     # price for product
     def get_price(self, sel):
-        price = sel.xpath("//article [@class='entry-content']//li[3]/span[@class= 'value']/text()").extract()[0].strip()
+        price = sel.xpath("//article[@class='entry-content']"
+                          "//span[contains(., 'Price')]/following::span[1]//text()").extract()[0].strip()
         return price.split(" ")[-1]
 
     def get_availability(self, sel):
-        availblilty = sel.xpath(".//div[@class='entry-main']/div/a/span/text()").extract()[0]
+        availblilty = sel.xpath(".//div[@class='entry-main']//span/text()").extract()[0]
         if "Not" in availblilty:
             return False
         return True
-
