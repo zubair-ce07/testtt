f1d178a4d16852149f08dc6dd541b50fa0aea76d
rosheen33 <rosheen33@gmail.com>
2015-08-17 17:36:23 +0500

    Corrections

--- ass1.py
+++ ass1.py
@@ -1,36 +1,35 @@
 import sys
-import re
-import os
 import glob
 
 
 def generate_report(reportNumber, directory):
 		
     year=1996;
-    if reportNumber == 1 :
+    print reportNumber
+    if reportNumber == '1' :
         # Min/Max temperature
-        print("Year        MAX Temp         MIN Temp         MAX Humidity         MIN Humidity")
-        print ("---------------------------------------------------------------------------------------")
+        print "Year        MAX Temp         MIN Temp         MAX Humidity         MIN Humidity"
+        print "--------------------------------------------------------------------------------"
 		
-    elif reportNumber== 2 :
-        print("Year                Date                Temp")
-        print("-----------------------------------------------")
+    elif reportNumber == '2' :
+        print "Year                Date                Temp"
+        print "-----------------------------------------------"
 
-    elif reportNumber== 3 :
-        print("Year                Date                Temp")
-        print("-----------------------------------------------")
+    elif reportNumber == '3' :
+        print "Year                Date                Temp"
+        print "-----------------------------------------------"
 
     # Traverse all files of all years
-    while (year<2012):
+    while (year < 2012):
         # list=glob.glob("/home/rosheen/Assignemnts/weatherdata/*"+str(year)+"*.txt");
-        list=glob.glob(directory+"/*"+str(year)+"*.txt");
+        list = glob.glob(directory+ "/*"+str(year)+ "*.txt");
 			
-        maximunTemp=0
-        minimunTemp=10000
-        maximumHumidity=0
-        minimumHumidity=10000
-        hottet_day_date=""
-        coolest_day_date=""
+        maximunTemp = 0
+        minimunTemp = 10000
+        maximumHumidity = 0
+        minimumHumidity = 10000
+        hottet_day_date = ""
+        coolest_day_date = ""
 		
         # Traverse all the files of a specific year
         for i in range(len(list)):
@@ -62,54 +61,53 @@ def generate_report(reportNumber, directory):
                 if((len(currentline) > 1) and currentline[7] != '' and (int(currentline[7]) >= maximumHumidity)):
                     maximumHumidity=int(currentline[7])
 
-        if reportNumber == 1 :			
+        if reportNumber == '1' :			
             # Print the maximun temperature in specified format
-            # print(str(year)+"             "+str(maximunTemp)+"               "+str(minimunTemp)+"               "+str(maximumHumidity)+"               "+str(minimumHumidity));
             print('{:4}'.format(year)+"             "+'{:5}'.format(maximunTemp)+"             "+'{:5}'.format(minimunTemp)+"            "+'{:5}'.format(maximumHumidity)+"            "+'{:5}'.format(minimumHumidity));			
 			
-        elif reportNumber == 2 :
-            # print(str(year)+"             "+hottet_day_date+"             "+str(maximunTemp))
+        elif reportNumber == '2' :
             print('{:4}'.format(year)+"             "+'{:10}'.format(hottet_day_date)+"             "+'{:5}'.format(maximunTemp))
 			
-        elif reportNumber == 3 :
-            # print(str(year)+"             "+coolest_day_date+"             "+str(minimunTemp))
+        elif reportNumber == '3' :
             print('{:4}'.format(year)+"             "+'{:10}'.format(coolest_day_date)+"             "+'{:5}'.format(minimunTemp))
 			
         year = year + 1;
 		
-    # Close opend file
-    file.close();
 		
     return None
 		
 	
 def main(*argv):
 	
-    total=0
+    total = 0
 	
     for i in argv:
         total = total + 1
 	
-    if (total != 2):
+    if (total < 2):
 		
-        print("No arguments have been passed")
-        print("Usage: weatherman <report Number> <data_dir>")
+        print "No arguments have been passed" 
+        print "Usage: weatherman <report Number> <data_dir>"
 
-        print("[Report Number]")
-        print("1 for Annual Max/Min Temperature")
-        print("2 for Hottest day of each year")
-        print("3 for coldest day of each year")
+        print "[Report Number]"
+        print "1 for Annual Max/Min Temperature" 
+        print "2 for Hottest day of each year"
+        print "3 for coldest day of each year" 
 
-        print("[data_dir]")
-        print("Directory containing weather data files")
+        print "[data_dir]" 
+        print "Directory containing weather data files"
         sys.exit();
 		
-    elif(total == 2):
-        generate_report(argv[0], argv[1])
+    elif(total >= 2):
+        generate_report(argv[1], argv[2])
 		
     return None
 		
 		
 # Call main function
+if __name__ == "__main__":
+    main(*sys.argv)
+	
+	
 # main();		
-main(1,"/home/rosheen/Assignemnts/weatherdata");
+# main(1,"/home/rosheen/Assignemnts/weatherdata");
