f049813ceefe7b9472a1e7966614f20b8257d9a9
rosheen33 <rosheen33@gmail.com>
2015-09-07 12:32:52 +0500

    version 4

--- tu_spider/tu_spider/spiders/tu_crawler.py
+++ tu_spider/tu_spider/spiders/tu_crawler.py
@@ -1,4 +1,5 @@
 import re
+from scrapy import log
 import json
 from scrapy.spiders import BaseSpider
 from scrapy.selector import HtmlXPathSelector
@@ -13,13 +14,16 @@ import logging
 import requests
 import urllib2
 from urlparse import urljoin
+from scrapy.shell import inspect_response
+
 
 
 class TuCrawler(CrawlSpider):
     name = "tu"
     allowed_domians = ["tuclothing.sainsburys.co.uk"]
     start_urls = ["http://tuclothing.sainsburys.co.uk"]
-
+	
+	
     # Set the rules for scraping all the available products of a website
     rules = (
         Rule(
@@ -36,45 +40,13 @@ class TuCrawler(CrawlSpider):
 
     )
 
-    def parse_skus(self, response):
-
-        hxs = HtmlXPathSelector(text=response)
-        item = SkuItem()
-
-        p = (hxs.select("//p[@class='big-price']/span[1]/text()").extract()[0]).strip()
-        item["price"] = re.sub(ur'\D', u'', p, flags=re.UNICODE)
-        previous_prices = hxs.select("//p[@class='big-price']/span/strike/text()").extract()
-
-        for i, s in enumerate(previous_prices):
-            previous_prices[i] = re.sub(ur'\D', u'', s, flags=re.UNICODE)
-
-        item["previous_prices"] = previous_prices
-        item["out_of_stock"] = bool(hxs.select("//p[@class='outOfStockMessage']"))
-        item["color"] = hxs.select("//p[@class='swatchDesc']/text()").extract()
-        item["currency"] = hxs.select("//span[@itemprop='priceCurrency']/@content").extract()
-        size = (hxs.select("//select[@id='Size']/option[@selected]/text()")[0]).extract()
-        size = re.sub(ur'\D', u'', size, flags=re.UNICODE) + " years"
-        item["size"] = size
-        return item
-
-    def get_skus(self, skus, response):
-
-        # Make a dictionary
-        skus_collection = {}
-        skus.pop(0)
-        url_domain = response.url
-
-        for s in skus:
-            # Making full URL
-            url_of_sku = urljoin(url_domain, s)
-            data = urllib2.urlopen(url_of_sku).read()
-            skus_collection[(s).split('=')[-1]] = self.parse_skus(data)
-
-        return skus_collection
 
     def parse_product(self, response):
 
         hxs = HtmlXPathSelector(response)
+
+        # Defining an object for storing the product data
+        item = TuSpiderItem() # that will always create a new item
 
         # Extracting data from the page of one product
         url_orignal = response.url
@@ -87,13 +59,12 @@ class TuCrawler(CrawlSpider):
         category = hxs.select("//ul[@class='clearfix']/li/a/text()").extract()
         category.pop(0)
         care_instructions = hxs.select("//div[@class='productDetails_materials']//li/text()").extract()
-        care_instructions = care_instructions + hxs.select("//div[@class='productDetails_care_instructions']/text()[2]").extract()
+        care_instructions = care_instructions + hxs.select(
+            "//div[@class='productDetails_care_instructions']/text()[2]").extract()
         care_instructions[1] = care_instructions[1].strip()
         gender = category[0]
-        skus = self.get_skus(hxs.select("//select[@id='Size']/option/@value").extract(), response)
 
-        # Defining an object for storing the product data
-        item = TuSpiderItem()
+        
         item["product_id"] = product_id
         item["care"] = care_instructions
         item["category"] = category
@@ -103,5 +74,49 @@ class TuCrawler(CrawlSpider):
         item["url_orignal"] = url_orignal
         item["brand"] = brand
         item["gender"] = gender
-        item["skus"] = skus
+        item['skus'] = {}
+
+        for i in self.get_skus(hxs.select("//select[@id='Size']/option/@value").extract(), item, response.url):
+            yield i
+
+
+    def get_skus(self, skus, item, url):
+
+        # Make a dictionary
+        skus_collection = {}
+        url_domain = url
+        for s in skus[1:]:
+            # Making full URL
+            url_of_sku = urljoin(url_domain, s)
+            yield Request(url_of_sku, callback=self.parse_skus, meta={'item':item }, dont_filter=True)
+            #logging.info( url_of_sku ) #open this url in browser and verify its working correctly.
+	    	   
+	
+    def parse_skus(self, response):
+
+        hxs = HtmlXPathSelector(response)
+        item = response.meta['item']
+        skus_collection = item['skus']
+        logging.info(skus_collection)
+
+
+        item1 = SkuItem()
+
+        p = (hxs.select("//p[@class='big-price']/span[1]/text()").extract()[0]).strip()
+        item1["price"] = re.sub(ur'\D', u'', p, flags=re.UNICODE)
+        previous_prices = hxs.select("//p[@class='big-price']/span/strike/text()").extract()
+
+        for i, s in enumerate(previous_prices):
+            previous_prices[i] = re.sub(ur'\D', u'', s, flags=re.UNICODE)
+
+        item1["previous_prices"] = previous_prices
+        item1["out_of_stock"] = bool(hxs.select("//p[@class='outOfStockMessage']"))
+        item1["color"] = hxs.select("//p[@class='swatchDesc']/text()").extract()
+        item1["currency"] = hxs.select("//span[@itemprop='priceCurrency']/@content").extract()
+        size = (hxs.select("//select[@id='Size']/option[@selected]/text()")[0]).extract()
+        size = re.sub(ur'\D', u'', size, flags=re.UNICODE) + " years"
+        item1["size"] = size
+        skus_collection[(response.url).split('=')[-1]] = item1
+        item['skus'] = skus_collection
         yield item
+
