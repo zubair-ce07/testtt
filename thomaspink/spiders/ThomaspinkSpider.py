3aa30d019abc4620f5fe247add6270c54b8cc8dc
adeelehsan <adeel.ehsan@arbisoft.com>
2016-01-27 19:13:28 +0500

    ThomasPink V8

--- thomaspink/spiders/ThomaspinkSpider.py
+++ thomaspink/spiders/ThomaspinkSpider.py
@@ -1,7 +1,7 @@
+# -*- coding: utf-8 -*-
 from thomaspink.items import ThomaspinkItem
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-import locale
 import re
 import copy
 from scrapy.contrib.loader.processor import TakeFirst
@@ -45,6 +45,7 @@ class ThomaspinkSpider(CrawlSpider):
     def get_sku(self, response):
         colors = self.get_colors(response)
         price = self.get_price(response)
+        print price
         sku = {}
         sizes = self.clean_list(self.get_size(response))
         if sizes[1] in sizes[2:]:
@@ -99,23 +100,16 @@ class ThomaspinkSpider(CrawlSpider):
                 text.append(s)
         return text
 
-    def currency(self, curr_sign):
-        locales = ('en_AU.utf8', 'en_BW.utf8', 'en_CA.utf8',
-                   'en_DK.utf8', 'en_GB.utf8', 'en_HK.utf8', 'en_IE.utf8', 'en_IN', 'en_NG',
-                   'en_PH.utf8', 'en_US.utf8', 'en_ZA.utf8',
-                   'en_ZW.utf8', 'ja_JP.utf8')
-        for l in locales:
-            locale.setlocale(locale.LC_ALL, l)
-            conv = locale.localeconv()
-            if conv['currency_symbol'] == curr_sign:
-                return conv['int_curr_symbol']
+    def currency(self, price):
+        if u'£' in price:
+            return 'GBP'
 
     def sku(self, price, color, sku, sizes, availability, long_availability):
-        currency = self.currency(price[0][0].encode('utf-8'))
+        currency = self.currency(price[0])
         price_list = []
         for pr in price:
             price_list.append(re.sub(r'[^0-9.]+', '', pr))
-        item = self.put_item(color, price_list, currency)
+        item = self.common_sku(color, price_list, currency)
         if not long_availability:
             for size, avail in zip(sizes, availability[1:]):
                 self.check_stock(self.is_available(avail), item, "")
@@ -134,7 +128,7 @@ class ThomaspinkSpider(CrawlSpider):
                 self.del_stock_info('long_out_of_stock', item)
             return sku
 
-    def put_item(self, color, price_list, currency):
+    def common_sku(self, color, price_list, currency):
         if len(price_list) > 1:
             item = {'currency': currency, 'price': price_list[1], 'previous_price': price_list[0],
                     'color': color}
