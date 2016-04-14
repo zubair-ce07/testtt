57361b4ad1c16625a4f9aee5d5306d3147f216e6
irfan_arshad_arbisoft <irfan.arshad@arbisoft.com>
2016-04-14 11:18:13 +0500

    code review changes

--- bluefly_spider.py
+++ bluefly_spider.py
@@ -1,4 +1,6 @@
 import scrapy
+from scrapy import Request
+
 from bluefly.items import BlueflyItem
 
 
@@ -12,26 +14,25 @@ class BlueflySpider(scrapy.Spider):
     def parse(self, response):
         for href in response.xpath('//*[@class="sitenav-sub-column"]//li//@href'):
             url = response.urljoin(href.extract())
-            # print url
-            yield scrapy.Request(url, callback=self.parse_product_list)
+            yield Request(url, callback=self.parse_product_list)
 
     def parse_product_list(self, response):
         for href in response.xpath('//li[@class="mz-productlist-item"]//@href'):
             url = response.urljoin(href.extract())
-            yield scrapy.Request(url, callback=self.parse_product_details)
-        next_page = response.xpath('(//*[@class="mz-pagenumbers-next"])[1]/@href').extract()
+            yield Request(url, callback=self.parse_product_details)
+        next_page = response.xpath('//*[@class="mz-pagenumbers-next"]/@href').extract()
         if next_page:
             next_page_url = response.urljoin(next_page[0])
-            yield scrapy.Request(next_page_url, callback=self.parse_product_list)
+            yield Request(next_page_url, callback=self.parse_product_list)
 
     def parse_product_details(self, response):
         item = BlueflyItem()
-        item['brand'] = response.xpath('//*[@itemprop="brand"]/a/text()').extract()
-        item['name'] = response.xpath('//*[@class="mz-breadcrumb-current"]/text()').extract()
-        item['description'] = response.xpath('//*[@itemprop="description"]/text()').extract()
-        item['details'] = response.xpath('//*[@class="mz-productdetail-props"]/li/text()').extract()
-        item['merch_info'] = response.xpath('//*[@class="mz-price-message"]/text()').extract()
-        item['image_urls'] = response.xpath('//*[@class="mz-productimages-thumbs"]//*//*//@src').extract()
+        item['brand'] = self.get_brand(response)
+        item['name'] = self.get_name(response)
+        item['details'] = self.get_details(response)
+        item['merch_info'] = self.get_merch_info(response)
+        item['retailer_sku'] = self.get_retailer_sku(response)
+        item['image_urls'] = response.xpath('//*[@class="mz-productimages-thumbs"]//@src').extract()
         item['category'] = response.xpath('//*[@class="mz-breadcrumb-link"]/text()').extract()
         item['url'] = response.url
         item['url_original'] = response.url
@@ -39,21 +40,62 @@ class BlueflySpider(scrapy.Spider):
         item['skus'] = self.skus(response)
         return item
 
+    def get_brand(self, response):
+        brand = response.xpath('//*[@itemprop="brand"]/a/text()').extract()
+        if brand:
+            brand = brand[0]
+            return brand
+        return ''
+
+    def get_name(self, response):
+        name = response.xpath('//*[@class="mz-breadcrumb-current"]/text()').extract()[0]
+        name = name.replace(self.get_brand(response), '')
+        return name
+
+    def get_retailer_sku(self, response):
+        retailer_sku = response.xpath('//*[@itemprop="productID"]/text()').extract()[0]
+        retailer_sku = retailer_sku.replace('Style # ', '')
+        return retailer_sku
+
+    def get_merch_info(self, response):
+        merch_info = response.xpath('//*[@class="mz-price-message"]/text()').extract()
+        if merch_info:
+            merch_info[0] = merch_info[0].strip()
+            return merch_info
+        return ''
+
+    def get_details(self, response):
+        description = response.xpath('//*[@itemprop="description"]/text()').extract()
+        details = response.xpath('//*[@class="mz-productdetail-props"]/li/text()').extract()
+        return description + details
+
     def skus(self, response):
-        skus = dict()
-        color = response.xpath('//*[@itemprop="color"]/text()').extract()
-        previous_prices = response.xpath('//*[@class="mz-price is-crossedout"]//text()').extract()
-        if previous_prices:
-            previous_prices = previous_prices[0].strip()
-        size = response.xpath('//*[@class="mz-productoptions-valuecontainer"]/*/text()').extract()
-        prices = response.xpath('//*[@itemprop="priceSpecification"]/div[1]/text()').extract()
-        if prices:
-            prices = prices[0].strip()
-        for s in size:
-            sku = dict()
-            sku['color'] = color
-            sku['previous_prices'] = previous_prices
-            sku['prices'] = prices
-            sku['size'] = s
-            skus[s] = sku
+        skus = {}
+        colour = response.xpath('//*[@itemprop="color"]/text()').extract()
+        previous_prices = self.get_previous_prices(response)
+        sizes = response.xpath('//*[@class="mz-productoptions-valuecontainer"]/*/text()').extract()
+        price = self.get_price(response)
+        for size in sizes:
+            sku = {}
+            sku['colour'] = colour[0]
+            if previous_prices:
+                sku['previous_prices'] = [previous_prices]
+            sku['price'] = price
+            sku['currency'] = 'USD'
+            sku['size'] = size
+            skus[size] = sku
         return skus
+
+    def get_price(self, response):
+        price = response.xpath('//*[@itemprop="priceSpecification"]/div[1]/text()').extract()
+        if price:
+            price = price[0].strip().replace('$', '')
+            return price
+        return ''
+
+    def get_previous_prices(self, response):
+        p_prices = response.xpath('//*[@class="mz-price is-crossedout"]//text()').extract()
+        if p_prices:
+            p_prices = p_prices[0].strip().replace('Retail: $', '')
+            return p_prices
+        return ''
--- items.py
+++ items.py
@@ -14,7 +14,6 @@ class BlueflyItem(scrapy.Item):
     care = scrapy.Field()
     name = scrapy.Field()
     category = scrapy.Field()
-    description = scrapy.Field()
     details = scrapy.Field()
     gender = scrapy.Field()
     image_urls = scrapy.Field()
@@ -26,51 +25,3 @@ class BlueflyItem(scrapy.Item):
     retailer_sku = scrapy.Field()
     url = scrapy.Field()
     url_original = scrapy.Field()
-
-    pass
-
-# code {'brand': u'Hayden',
-# 'care': [u'Hand Wash or Dry Clean', u'100% Cashmere'],
-# 'category': [u'Women', u'Clothing', u'Sweaters', u'Cashmere'],
-# 'description': [u'All you need for the fall is a cozy cashmere sweater to keep you warm on those cool nights courtesy of Hayden.',
-# u'Hayden',
-# u'Lightweight 2-ply 12gg cashmere knit',
-# u'Foldover ribbed turtleneck',
-# u'Long sleeve',
-# u'Ribbed banded cuffs and hem',
-# u'Contemporary fit',
-# u"27'' from shoulder seam to hem&
-# quot;,
-#                   u'Approximate measurements taken from a size S, and may vary by size',
-#                   u'China',
-#                   u'Style # 351480403'],
-#   'gender': 'women',
-#   'image_urls': [u'//cdn-tp4.mozu.com/12106-m2/cms/files/351480403?maxWidth=1800&maxHeight=2160&v=89';,
-#                  u'//cdn-tp4.mozu.com/12106-m2/cms/files/351480403_alt01?maxWidth=&maxHeight=537&v=89';],
-#   'market': 'US',
-#   'merch_info': [u'Includes An Extra 60% Off'],
-#   'name': u'Purple Cashmere Turtleneck Sweater',
-#   'retailer': 'bluefly',
-#   'retailer_sku': u'351480403',
-#   'skus': {u'560': {'colour': u'Purple',
-#                     'currency': 'USD',
-#                     'previous_prices': [28000],
-#                     'price': 3600,
-#                     'size': u'XS'},
-#            u'562': {'colour': u'Purple',
-#                     'currency': 'USD',
-#                     'previous_prices': [28000],
-#                     'price': 3600,
-#                     'size': u'S'},
-#            u'564': {'colour': u'Purple',
-#                     'currency': 'USD',
-#                     'previous_prices': [28000],
-#                     'price': 3600,
-#                     'size': u'M'},
-#            u'566': {'colour': u'Purple',
-#                     'currency': 'USD',
-#                     'previous_prices': [28000],
-#                     'price': 3600,
-#                     'size': u'L'}},
-#   'url': u'http://www.bluefly.com/hayden-purple-cashmere-turtleneck-sweater/p/351480403';,
-#   'url_original': 'http://www.bluefly.com/hayden-purple-cashmere-turtleneck-sweater/p/351480403';}
