e67692447fc36d3a75c8b5571233b3b0fde02880
Ramshah1 <ramshah.jahangir@arbisoft.com>
2016-04-15 12:41:49 +0500

    bluefly spider- code optimization & arrangement

--- bluefly/bluefly/spiders/bluefly_spider.py
+++ bluefly/bluefly/spiders/bluefly_spider.py
@@ -11,50 +11,66 @@ class BlueflySpiderSpider(scrapy.Spider):
 
     def parse(self, response):
         item = BlueflyItem()
-        item['brand'] = response.xpath("//*[contains(@class, 'mz-productbrand')]/a/text()").extract()
-        item['category'] = response.xpath("//*[contains(@class, 'mz-breadcrumbs')]/a[position()>1]/text()").extract()
-        item['description'] = self.get_item_description(response)
+        item['brand'] = self.product_brand(response)
+        item['category'] = self.product_category(response)
+        item['description'] = self.product_description(response)
         item['gender'] = 'women'
-        item['image_urls'] = response.xpath('//*[contains(@class, "mz-productimages-thumbimage")]//@src').extract()
+        item['image_urls'] = self.product_image_urls(response)
         item['market'] = 'US'
-        item['merch_info'] = map(unicode.strip,
-                                 response.xpath('//*[contains(@class, "mz-price-message")]//text()').extract())
-        item['name'] = \
-            response.xpath("//*[contains(@class, 'mz-breadcrumb-current')]//text()").extract()[0].split(' ', 1)[1]
-        item['retailer'] = map(unicode.strip,
-                               response.xpath('//*[contains(@class, "site-toggler bluefly active")]//text()').extract())
-        item['retailer_sku'] = self.extract_retailer_sku(response)
-        item['skus'] = self.get_retailer_sku(response)
+        item['merch_info'] = self.product_merch_info(response)
+        item['name'] = self.product_name(response)
+        item['retailer'] = 'bluefly'
+        item['retailer_sku'] = self.product_retailer_sku(response)
+        item['skus'] = self.product_sku(response)
         item['url'] = response.url
         item['url_original'] = response.url
         yield item
 
-    def extract_retailer_sku(self, response):
-        retailer_sku = response.xpath("//*[contains(@class, 'mz-productdetail-props')]/li[last()]/text()").extract()
+    def product_brand(self, response):
+        return response.xpath("//*[contains(@class,'mz-productbrand')]/a/text()").extract()
+
+    def product_category(self, response):
+        return response.xpath("//*[contains(@class,'mz-breadcrumbs')]/a[position()>1]/text()").extract()
+
+    def product_retailer_sku(self, response):
+        retailer_sku = response.xpath("//*[contains(@class,'mz-productdetail-props')]/li[last()]/text()").extract()
         return (retailer_sku[0].split())[-1]
 
-    def get_item_description(self, response):
-        description = response.xpath("//*[contains(@class, 'mz-productdetail-description')]//text()").extract()
+    def product_description(self, response):
+        description = response.xpath("//*[contains(@class,'mz-productdetail-description')]//text()").extract()
         detail = response.xpath(
-            "//*[contains(@class, 'mz-productdetail-props')]/li[position()<last()]/text()").extract()
+            "//*[contains(@class,'mz-productdetail-props')]/li[position()<last()]/text()").extract()
         return description + detail
 
-    def get_retailer_sku(self, response):
+    def product_image_urls(self, response):
+        return response.xpath('//*[contains(@class,"mz-productimages-thumbimage")]//@src').extract()
+
+    def product_merch_info(self, response):
+        return map(unicode.strip, response.xpath('//*[contains(@class,"mz-price-message")]//text()').extract())
+
+    def product_name(self, response):
+        name = response.xpath("//*[contains(@class,'mz-breadcrumb-current')]//text()").extract()
+        return name[0].split(' ', 1)[1]
+
+    def product_retailer(self, response):
+        return map(unicode.strip,
+                   response.xpath('//*[contains(@class,"site-toggler bluefly active")]//text()').extract())
+
+    def product_sku(self, response):
         skus = {}
-        sizes = response.xpath("//*[contains(@class, 'mz-productoptions-valuecontainer')]//span/text()").extract()
-        index = 0
-        for size in sizes:
-            product = {}
-            product['color'] = response.xpath(
-                "//*[contains(@class, 'mz-productoptions-optionvalue')]//text()").extract()
-            product['currency'] = 'USD'
-            prev_price = \
-            map(unicode.strip, response.xpath("//*[contains(@class, 'mz-price is-crossedout')]//text()").extract())[-1]
-            product['previous_prices'] = prev_price[1:]
-            price = map(unicode.strip,
-                        response.xpath("//*[contains(@class, 'mz-price is-saleprice')]//text()").extract())[0]
-            product['price'] = price[1:]
-            product['size'] = size
-            skus[index] = product
-            index += 1
+        sizes = response.xpath("//*[contains(@class,'mz-productoptions-valuecontainer')]//span/text()").extract()
+        price = map(unicode.strip, response.xpath("//*[contains(@class,'mz-price is-saleprice')]//text()").extract())[0]
+        price = price[1:]
+        prev_price = map(unicode.strip,
+                         response.xpath("//*[contains(@class,'mz-price is-crossedout')]//text()").extract())[-1]
+        prev_price = prev_price[1:]
+        keys = response.xpath("//*[contains(@class,'mz-productoptions-sizebox')]//@data-value").extract()
+        for key, size in zip(keys, sizes):
+            sku = {}
+            sku['color'] = response.xpath("//*[contains(@class,'mz-productoptions-optionvalue')]//text()").extract()
+            sku['currency'] = 'USD'
+            sku['previous_prices'] = prev_price
+            sku['price'] = price
+            sku['size'] = size
+            skus[key] = sku
         return skus
