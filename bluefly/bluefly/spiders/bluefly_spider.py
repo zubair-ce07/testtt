dfbd224e3f4605761911919e0fb70757792a9549
Ramshah1 <ramshah.jahangir@arbisoft.com>
2016-04-18 16:35:36 +0500

    bluefly spider - Complete Site Crawler

--- bluefly/bluefly/spiders/bluefly_spider.py
+++ bluefly/bluefly/spiders/bluefly_spider.py
@@ -6,10 +6,25 @@ class BlueflySpiderSpider(scrapy.Spider):
     name = "bluefly_spider"
     allowed_domains = ["bluefly.com"]
     start_urls = [
-        'http://www.bluefly.com/hayden-purple-cashmere-turtleneck-sweater/p/351480403',
+        'http://www.bluefly.com',
     ]
 
     def parse(self, response):
+        for href in response.css("ul.sitenav-sub-column > li > a::attr('href')"):
+            url = response.urljoin(href.extract())
+            yield scrapy.Request(url, callback=self.parse_product_pages)
+
+    def parse_product_pages(self, response):
+        for product_href in response.xpath("//*[contains(@class,'mz-productlist-list mz-l-tiles')]//li//@href"):
+            url = response.urljoin(product_href.extract())
+            yield scrapy.Request(url, callback=self.parse_product_contents)
+
+        next_page = response.xpath("//*[contains(@class,'mz-pagenumbers-next')]//@href")
+        if next_page:
+            url = response.urljoin(next_page[0].extract())
+            yield scrapy.Request(url, self.parse_product_pages)
+
+    def parse_product_contents(self, response):
         item = BlueflyItem()
         item['brand'] = self.product_brand(response)
         item['category'] = self.product_category(response)
@@ -59,19 +74,36 @@ class BlueflySpiderSpider(scrapy.Spider):
 
     def product_sku(self, response):
         skus = {}
-        sizes = response.xpath("//*[contains(@class,'mz-productoptions-sizebox')]")
-        price = map(unicode.strip, response.xpath("//*[contains(@class,'mz-price is-saleprice')]//text()").extract())[0]
-        price = price[1:]
+        price = map(unicode.strip, response.xpath("//*[contains(@class,'mz-price is-saleprice')]//text()").extract())
+        if price:
+            price = price[0]
+        else:
+            price = map(unicode.strip, response.xpath("//*[contains(@class,'mz-price')]//text()").extract())[1]
+        price = price.split('$', 1)
+        price = price[-1]
         prev_price = map(unicode.strip,
-                         response.xpath("//*[contains(@class,'mz-price is-crossedout')]//text()").extract())[-1]
-        prev_price = prev_price[1:]
-        for size in sizes:
+                         response.xpath("//*[contains(@class,'mz-price is-crossedout')]//text()").extract())
+        if prev_price:
+            prev_price = prev_price[-1]
+            prev_price = prev_price.split('$', 1)
+            prev_price = prev_price[-1]
+        sizes = response.xpath("//*[contains(@class,'mz-productoptions-sizebox')]")
+        if sizes:
+            for size in sizes:
+                sku = {}
+                sku['color'] = response.xpath("//span[contains(@class,'mz-productoptions-optionvalue')]//text()").extract()
+                sku['currency'] = 'USD'
+                sku['previous_prices'] = prev_price
+                sku['price'] = price
+                sku['size'] = size.xpath('.//text()').extract()
+                key = size.xpath('@data-value').extract()
+                skus[key[0]] = sku
+        else:
             sku = {}
-            sku['color'] = response.xpath("//*[contains(@class,'mz-productoptions-optionvalue')]//text()").extract()
+            sku['color'] = response.xpath("//span[contains(@class,'mz-productoptions-optionvalue')]//text()").extract()
             sku['currency'] = 'USD'
             sku['previous_prices'] = prev_price
             sku['price'] = price
-            sku['size'] = size.xpath('.//text()').extract()
-            key = size.xpath('@data-value').extract()
-            skus[key[0]] = sku
+            sku['size'] = ''
+            skus[0] = sku
         return skus
