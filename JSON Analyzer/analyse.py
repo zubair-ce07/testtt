bcc6905b51943af58deacde168a8c929ff1e0ddf
asadazam93 <asadazam93@gmail.com>
2016-07-14 15:30:10 +0500

    Code improved

--- JSON Analyzer/analyse.py
+++ JSON Analyzer/analyse.py
@@ -1,12 +1,16 @@
 import json
 import sys
 import argparse
+
+_keyoccurencestats = dict()
+_valuesofkeys = dict()
 parser = argparse.ArgumentParser()
 parser.add_argument("--f",help="input the JSON key whose values need to be listed")
 parser.add_argument("filepath",help="input the JSON file path")
 args = parser.parse_args()
-keyoccstats = dict()
-valuesofkeys = dict()
+
+def main():
+    "The main function of the program"
 with open(args.filepath) as f:
     Jsons = f.readlines()
 Count = len(Jsons)
@@ -14,25 +18,26 @@ if args.f:
     for index in range(0, len(Jsons) - 1):
         parsed = json.loads(Jsons[index])
         value = parsed[args.f]
-        if value in valuesofkeys:
-            valuesofkeys[value] += 1
+        if value in _valuesofkeys:
+            _valuesofkeys[value] += 1
         else:
-            valuesofkeys[value] = 1
-    print("Total Rows " + str(Count))
-    allstats = valuesofkeys.keys()
+            _valuesofkeys[value] = 1
+    print '{0} {1}'.format("Total Rows ",str(Count))
+    allstats = _valuesofkeys.keys()
     for everystat in allstats:
-        print(everystat + ", " + str(valuesofkeys[everystat]))
+        print '{0} {1} {2}'.format(everystat,", ", str(_valuesofkeys[everystat]))
 else:
     for index in range(0,len(Jsons)-1):
         parsed = json.loads(Jsons[index])
         for key in parsed:
-            if key in keyoccstats:
-                keyoccstats[key] +=1
+            if key in _keyoccurencestats:
+                _keyoccurencestats[key] +=1
             else:
-                keyoccstats[key] =1
+                _keyoccurencestats[key] =1
     print '{0} {1}'.format("Total Rows",str(Count))
-    allkeys = keyoccstats.keys()
+    allkeys = _keyoccurencestats.keys()
     for everykey in allkeys:
-        perc = float((int(keyoccstats[everykey])) / 0.65)
-        print(everykey+": ["+str(perc)+"%]")
+        perc = float((int(_keyoccurencestats[everykey])) / 0.65)
+        print '{0} {1} {2} {3}'.format(everykey,": [",str(perc),"%]")
 
+if __name__ == "__main__": main()
