3617ced162f4d6b665361b17f264d88a78d5e04a
asadazam93 <asadazam93@gmail.com>
2016-07-14 12:16:12 +0500

    updated the solution
    Improved the code according to the suggestions

--- JSON Analyzer/analyse.py
+++ JSON Analyzer/analyse.py
@@ -1,21 +1,19 @@
 import json
 import sys
-if (len(sys.argv)>2):
-    keyvalue = sys.argv[2]
-    filename = sys.argv[3]
-else:
-    filename = sys.argv[1]
-JsonData = open(filename)
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--f",help="input the JSON key whose values need to be listed")
+parser.add_argument("filepath",help="input the JSON file path")
+args = parser.parse_args()
 keyoccstats = dict()
 valuesofkeys = dict()
-
-JsonString = JsonData.read()
-Jsons = JsonString.split('\n')
+with open(args.filepath) as f:
+    Jsons = f.readlines()
 Count = len(Jsons)
-if (len(sys.argv)>2):
+if args.f:
     for index in range(0, len(Jsons) - 1):
         parsed = json.loads(Jsons[index])
-        value = parsed[keyvalue]
+        value = parsed[args.f]
         if value in valuesofkeys:
             valuesofkeys[value] += 1
         else:
@@ -27,15 +25,14 @@ if (len(sys.argv)>2):
 else:
     for index in range(0,len(Jsons)-1):
         parsed = json.loads(Jsons[index])
-        array = parsed.keys()
         for key in parsed:
             if key in keyoccstats:
                 keyoccstats[key] +=1
             else:
                 keyoccstats[key] =1
-    print("Total Rows "+str(Count))
+    print '{0} {1}'.format("Total Rows",str(Count))
     allkeys = keyoccstats.keys()
     for everykey in allkeys:
-        perc = float((int(keyoccstats[everykey])) / 0.66)
+        perc = float((int(keyoccstats[everykey])) / 0.65)
         print(everykey+": ["+str(perc)+"%]")
 
