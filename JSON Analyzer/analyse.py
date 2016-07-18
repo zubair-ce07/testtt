92e360b8f59bcc4e4a0afa5a4da22b7e1c728a32
asadazam93 <asadazam93@gmail.com>
2016-07-18 16:10:55 +0500

    Updated the code

--- JSON Analyzer/analyse.py
+++ JSON Analyzer/analyse.py
@@ -2,21 +2,17 @@ import json
 import itertools
 import argparse
 
-_keyoccurencestats = dict()
-_valuesofkeys = dict()
-parser = argparse.ArgumentParser()
-parser.add_argument("--f",help="input the JSON key whose values need to be listed")
-parser.add_argument("filepath",help="input the JSON file path")
-args = parser.parse_args()
+_key_occurence_stats = dict()
+_values_of_keys = dict()
 
 
 def extract_all_keys(dict_):
     "Function that extracts all the keys of a dictionary at all levels"
     for key in dict_:
-        if key in _keyoccurencestats:
-            _keyoccurencestats[key] += 1
+        if key in _key_occurence_stats:
+            _key_occurence_stats[key] += 1
         else:
-            _keyoccurencestats[key] = 1
+            _key_occurence_stats[key] = 1
         if type(dict_[key]) is dict:
             extract_all_keys(dict_[key])
     return
@@ -29,21 +25,21 @@ def extract_all_values(dict_,key):
             value = dict_[key]
             if type(value) is list:
                 myString = " ".join(value)
-                if myString in _valuesofkeys:
-                    _valuesofkeys[myString] += 1
+                if myString in _values_of_keys:
+                    _values_of_keys[myString] += 1
                 else:
-                    _valuesofkeys[myString] = 1
+                    _values_of_keys[myString] = 1
             elif type(value) is dict:
                 myString = ''.join('{}{}'.format(key, val) for key, val in value.items())
-                if myString in _valuesofkeys:
-                    _valuesofkeys[myString] += 1
+                if myString in _values_of_keys:
+                    _values_of_keys[myString] += 1
                 else:
-                    _valuesofkeys[myString] = 1
+                    _values_of_keys[myString] = 1
             else:
-                if value in _valuesofkeys:
-                    _valuesofkeys[value] += 1
+                if value in _values_of_keys:
+                    _values_of_keys[value] += 1
                 else:
-                    _valuesofkeys[value] = 1
+                    _values_of_keys[value] = 1
         else:
             if type(dict_[each_key]) is dict:
                 extract_all_values(dict_[each_key],key)
@@ -52,26 +48,30 @@ def extract_all_values(dict_,key):
 
 def main():
     "The main function of the program"
-with open(args.filepath) as f:
-    Jsons = f.readlines()
-Count = len(Jsons)
-if args.f:
-    for index in range(0, len(Jsons) - 1):
-        parsed_dict  = json.loads(Jsons[index])
-        # extract all the values and their count corresponding to the key args.f
-        extract_all_values(parsed_dict, args.f)
-    print '{0} {1}'.format("Total Rows ",str(Count))
-    allstats = _valuesofkeys.keys()
-    for everystat in allstats:
-        print '{0} {1} {2}'.format(everystat,", ", str(_valuesofkeys[everystat]))
-else:
-    for index in range(0, len(Jsons) - 1):
-        parsed_dict = json.loads(Jsons[index])
-        extract_all_keys(parsed_dict)
-    print '{0} {1}'.format("Total Rows",str(Count))
-    allkeys = _keyoccurencestats.keys()
-    for everykey in allkeys:
-        perc = float((int(_keyoccurencestats[everykey])) / 0.65)
-        print '{0} {1} {2} {3}'.format(everykey,": [",str(perc),"%]")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--f", help="input the JSON key whose values need to be listed")
+    parser.add_argument("filepath", help="input the JSON file path")
+    args = parser.parse_args()
+    with open(args.filepath) as f:
+        jsons = f.readlines()
+        Count = len(jsons)
+        if args.f:
+            for each_json in jsons:
+                parsed_dict  = json.loads(each_json)
+                # extract all the values and their count corresponding to the key args.f
+                extract_all_values(parsed_dict, args.f)
+            print '{0} {1}'.format("Total Rows ",str(Count))
+            allstats = _values_of_keys.keys()
+            for everystat in allstats:
+                print '{0} {1} {2}'.format(everystat,", ", str(_values_of_keys[everystat]))
+        else:
+            for each_json in jsons:
+                parsed_dict = json.loads(each_json)
+                extract_all_keys(parsed_dict)
+            print '{0} {1}'.format("Total Rows",str(Count))
+            allkeys = _key_occurence_stats.keys()
+            for everykey in allkeys:
+                perc = float((int(_key_occurence_stats[everykey])) / 0.65)
+                print '{0} {1} {2} {3}'.format(everykey,": [",str(perc),"%]")
 
 if __name__ == "__main__": main()
