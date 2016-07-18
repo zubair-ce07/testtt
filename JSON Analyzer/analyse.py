87330f778fea182d7d5f62d5f1e3aec910d81c79
asadazam93 <asadazam93@gmail.com>
2016-07-18 15:31:58 +0500

    upgraded for multiple levels of JSON

--- JSON Analyzer/analyse.py
+++ JSON Analyzer/analyse.py
@@ -1,5 +1,5 @@
 import json
-import sys
+import itertools
 import argparse
 
 _keyoccurencestats = dict()
@@ -9,6 +9,47 @@ parser.add_argument("--f",help="input the JSON key whose values need to be liste
 parser.add_argument("filepath",help="input the JSON file path")
 args = parser.parse_args()
 
+
+def extract_all_keys(dict_):
+    "Function that extracts all the keys of a dictionary at all levels"
+    for key in dict_:
+        if key in _keyoccurencestats:
+            _keyoccurencestats[key] += 1
+        else:
+            _keyoccurencestats[key] = 1
+        if type(dict_[key]) is dict:
+            extract_all_keys(dict_[key])
+    return
+
+
+def extract_all_values(dict_,key):
+    "Function that extracts all the values of a dictionary at all levels"
+    for each_key in dict_:
+        if (each_key == key):
+            value = dict_[key]
+            if type(value) is list:
+                myString = " ".join(value)
+                if myString in _valuesofkeys:
+                    _valuesofkeys[myString] += 1
+                else:
+                    _valuesofkeys[myString] = 1
+            elif type(value) is dict:
+                myString = ''.join('{}{}'.format(key, val) for key, val in value.items())
+                if myString in _valuesofkeys:
+                    _valuesofkeys[myString] += 1
+                else:
+                    _valuesofkeys[myString] = 1
+            else:
+                if value in _valuesofkeys:
+                    _valuesofkeys[value] += 1
+                else:
+                    _valuesofkeys[value] = 1
+        else:
+            if type(dict_[each_key]) is dict:
+                extract_all_values(dict_[each_key],key)
+    return
+
+
 def main():
     "The main function of the program"
 with open(args.filepath) as f:
@@ -16,24 +57,17 @@ with open(args.filepath) as f:
 Count = len(Jsons)
 if args.f:
     for index in range(0, len(Jsons) - 1):
-        parsed = json.loads(Jsons[index])
-        value = parsed[args.f]
-        if value in _valuesofkeys:
-            _valuesofkeys[value] += 1
-        else:
-            _valuesofkeys[value] = 1
+        parsed_dict  = json.loads(Jsons[index])
+        # extract all the values and their count corresponding to the key args.f
+        extract_all_values(parsed_dict, args.f)
     print '{0} {1}'.format("Total Rows ",str(Count))
     allstats = _valuesofkeys.keys()
     for everystat in allstats:
         print '{0} {1} {2}'.format(everystat,", ", str(_valuesofkeys[everystat]))
 else:
-    for index in range(0,len(Jsons)-1):
-        parsed = json.loads(Jsons[index])
-        for key in parsed:
-            if key in _keyoccurencestats:
-                _keyoccurencestats[key] +=1
-            else:
-                _keyoccurencestats[key] =1
+    for index in range(0, len(Jsons) - 1):
+        parsed_dict = json.loads(Jsons[index])
+        extract_all_keys(parsed_dict)
     print '{0} {1}'.format("Total Rows",str(Count))
     allkeys = _keyoccurencestats.keys()
     for everykey in allkeys:
