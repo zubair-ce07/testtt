9ec200fb59dc2c6c70ba6545a2d532adafa4e952
asadazam93 <asadazam93@gmail.com>
2016-07-26 14:21:13 +0500

    Updated output and cleaned the code

--- JSON Analyzer/analyse.py
+++ JSON Analyzer/analyse.py
@@ -4,49 +4,83 @@ import argparse
 _key_occurence_stats = dict()
 _values_of_keys = dict()
 
-def extract_all_keys(dict_):
+
+def extract_all_keys(given_dictionary, previous_key):
     "Function that extracts all the keys of a dictionary at all levels"
-    for key in dict_:
+    for each_key in given_dictionary:
+        key = previous_key+each_key
         if key in _key_occurence_stats:
             _key_occurence_stats[key] += 1
         else:
             _key_occurence_stats[key] = 1
-        if type(dict_[key]) is dict:
-            extract_all_keys(dict_[key])
+        if type(given_dictionary[each_key]) is dict:
+            extract_all_keys(given_dictionary[each_key], key + '.')
     return
 
 
-def extract_all_values(dict_, key, key_flag):
+def extract_all_values(given_dictionary, given_key, parent_key, valid_keys, non_valid_keys):
     "Function that extracts all the values of a dictionary at all levels"
-    for each_key in dict_:
-        if (each_key == key or key_flag):
-            value = dict_[each_key]
-            if type(value) is list:
-                for my_string in value:
-                    if my_string in _values_of_keys:
-                        _values_of_keys[my_string] += 1
+    for key in given_dictionary:
+        key_heirarchy = parent_key + key
+        if (key == given_key and key in valid_keys):
+            value_of_key = given_dictionary[key]
+            if ((isinstance(value_of_key,list)) and (key not in non_valid_keys)):
+                for each_item in value_of_key:
+                    key_heirarchy_string = parent_key + key + ':' + each_item
+                    if key_heirarchy_string in _values_of_keys:
+                        _values_of_keys[key_heirarchy_string] += 1
                     else:
-                        _values_of_keys[my_string] = 1
-            elif type(value) is dict:
-                for my_string in value:
-                    if type(value[my_string]) is dict:
-                        extract_all_values(value,my_string,1)
-                    elif type(value[my_string]) is list:
-                        extract_all_values(value,my_string,1)
+                        _values_of_keys[key_heirarchy_string] = 1
+            elif isinstance(value_of_key,dict):
+                for each_sub_key in value_of_key:
+                    if isinstance(value_of_key[each_sub_key],dict):
+                        value_of_subkey = str(each_sub_key)
+                        my_parent_key = (key_heirarchy.split('.'))[-1]
+                        if (each_sub_key in valid_keys or my_parent_key not in non_valid_keys):
+                            valid_keys.append(value_of_subkey)
+                            extract_all_values(value_of_key, each_sub_key, key_heirarchy +'.', valid_keys, non_valid_keys)
+                    elif type(value_of_key[each_sub_key]) is list:
+                        extract_all_values(value_of_key, each_sub_key, key_heirarchy, valid_keys, non_valid_keys)
                     else:
-                        my_key=my_string+":"+str(value[my_string])
-                        if my_key in _values_of_keys:
-                            _values_of_keys[my_key] += 1
-                        else:
-                            _values_of_keys[my_key] = 1
-            else:
-                if value in _values_of_keys:
-                    _values_of_keys[value] += 1
+                        top_level_key = (key_heirarchy.split('.'))[-1]
+                        if (each_sub_key in valid_keys or top_level_key not in non_valid_keys):
+                            top_level_key=key_heirarchy+'.'+each_sub_key+":"+str(value_of_key[each_sub_key])
+                            if top_level_key in _values_of_keys:
+                                _values_of_keys[top_level_key] += 1
+                            else:
+                                _values_of_keys[top_level_key] = 1
+            elif (key not in non_valid_keys):
+                if value_of_key in _values_of_keys:
+                    _values_of_keys[value_of_key] += 1
                 else:
-                    _values_of_keys[value] = 1
-        else:
-            if type(dict_[each_key]) is dict:
-                extract_all_values(dict_[each_key],key,0)
+                    _values_of_keys[value_of_key] = 1
+    return
+
+
+def extract_all_keys_occurences(jsons_list,total_rows):
+    for each_json in jsons_list:
+        json_dict = json.loads(each_json)
+        extract_all_keys(json_dict, '')
+    print('{0} {1}'.format('Total Rows', total_rows))
+    allkeys = _key_occurence_stats.keys()
+    for everykey in allkeys:
+        percentage_occurence = float((int(_key_occurence_stats[everykey])) / 0.66)
+        print('{0} {1} {2} {3}'.format(everykey, ": [", round(percentage_occurence, 2), "%]"))
+    return
+
+
+def extract_specific_key_occurences(jsons_list,total_rows,input_key):
+    non_valid_keys = ''
+    valid_keys = input_key.split(".")
+    given_key = valid_keys[0]
+    if len(valid_keys) > 1:
+        non_valid_keys = valid_keys[0:-1]
+    for each_json in jsons_list:
+        json_dict = json.loads(each_json)
+        extract_all_values(json_dict, given_key, '', valid_keys, non_valid_keys)
+    print('{0} {1}'.format('Total Rows ', total_rows))
+    for everystat in _values_of_keys:
+        print('{0} {1} {2}'.format(everystat, ", ", _values_of_keys[everystat]))
     return
 
 
@@ -57,29 +91,12 @@ def main():
     parser.add_argument("filepath", help="input the JSON file path")
     args = parser.parse_args()
     with open(args.filepath) as f:
-        jsons = f.readlines()
-        Count = len(jsons)
+        jsons_list = f.readlines()
+        total_rows = len(jsons_list)
         if args.f:
-            key_ = args.f
-            splitted = key_.split(".")
-            given_key = splitted[-1]
-            for each_json in jsons:
-                parsed_dict  = json.loads(each_json)
-                # extract all the values and their count corresponding to the key args.f
-                extract_all_values(parsed_dict, given_key,0)
-            print('{0} {1}'.format("Total Rows ",str(Count)))
-            print(key_)
-            allstats = _values_of_keys.keys()
-            for everystat in allstats:
-                print('{0} {1} {2}'.format(everystat,", ", str(_values_of_keys[everystat])))
+            extract_specific_key_occurences(jsons_list, total_rows, args.f)
         else:
-            for each_json in jsons:
-                parsed_dict = json.loads(each_json)
-                extract_all_keys(parsed_dict)
-            print('{0} {1}'.format("Total Rows",str(Count)))
-            allkeys = _key_occurence_stats.keys()
-            for everykey in allkeys:
-                perc = float((int(_key_occurence_stats[everykey])) / 0.66)
-                print('{0} {1} {2} {3}'.format(everykey,": [",str(perc),"%]"))
+            extract_all_keys_occurences(jsons_list, total_rows)
 
+            
 if __name__ == "__main__": main()
