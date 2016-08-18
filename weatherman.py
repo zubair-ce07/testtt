0eb137c2ae5028e0cb1b4fd950d120cde31e918e
Danish Zahid <danish.zahid@arbisoft.com>
2016-08-18 12:41:40 +0500

    parsing commandline arguments Flollowing PEP 8 Guidelines

--- weatherman.py
+++ weatherman.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import glob
+import argparse
 
 import sys
 
@@ -19,6 +20,7 @@ def prepare_data_for_report(data_dir):
     data_dict = {}
     for weather_file in glob.glob('*.txt'):
         with open(weather_file, "r") as csvfile:
+            # to ignore header line
             csvfile.readline()
             reader = csv.DictReader(csvfile)
 
@@ -39,11 +41,14 @@ def prepare_data_for_report(data_dir):
                         if year not in data_dict:
                             data_dict[year] = {}
                         if key_header not in data_dict[year]:
-                            data_dict[year][key_header] = {value_key: temp, date_key: date}
+                            data_dict[year][key_header] = {value_key: temp,
+                                                           date_key: date}
                             continue
 
-                        if operation(temp, data_dict[year][key_header][value_key]) or not data_dict[year][key_header][value_key]:
-                            data_dict[year][key_header] = {value_key: temp, date_key: date}
+                        if operation(temp, data_dict[year][key_header][value_key]) \
+                                or not data_dict[year][key_header][value_key]:
+                            data_dict[year][key_header] = {value_key: temp,
+                                                           date_key: date}
 
                 except ValueError:
                     # need to find a good solution
@@ -61,11 +66,11 @@ def get_evaluation_header():
     min_humid = [" Min Humidity", operator.lt]
 
     evaluations = [
-                   max_temp,
-                   min_temp,
-                   max_humid,
-                   min_humid
-                   ]
+        max_temp,
+        min_temp,
+        max_humid,
+        min_humid
+    ]
 
     return evaluations
 
@@ -73,33 +78,45 @@ def get_evaluation_header():
 def print_report(data_dict, report_no):
     evaluations = get_evaluation_header()
     if report_no == 1:
-        print_annual_min_max(evaluations, data_dict)
+        print("Annual Max/Min Temperature")
+        print_annual_evaluations(evaluations, data_dict)
     elif report_no == 2:
+        print("Hottest day of each year")
         annual_evaluation_day(data_dict, evaluations[0][0])
     elif report_no == 3:
+        print("Coldest day of each year")
         annual_evaluation_day(data_dict, evaluations[1][0])
 
 
-def print_annual_min_max(evaluations, data_dict):
+def print_annual_evaluations(evaluations, data_dict):
     print ('%s\t%s\t%s\t%s\t%s' % ("Year",
-                                  evaluations[0][0],
-                                  evaluations[1][0],
-                                  evaluations[2][0],
-                                  evaluations[3][0]))
-    print ('------------------------------------------------------------------------------------------')
+                                   evaluations[0][0],
+                                   evaluations[1][0],
+                                   evaluations[2][0],
+                                   evaluations[3][0]))
+    print (
+    '------------------------------------------------------------------------------------------')
     for key_year, eval_dict in data_dict.items():
-        print ('%s\t\t%s\t\t\t%s\t\t\t%s\t\t%s' % (str(key_year),eval_dict[evaluations[0][0]]["value"],eval_dict[evaluations[1][0]]["value"],eval_dict[evaluations[2][0]]["value"],eval_dict[evaluations[3][0]]["value"]))
+        print ('%s\t\t%s\t\t\t%s\t\t\t%s\t\t%s' % (
+            str(key_year),
+            eval_dict[evaluations[0][0]]["value"],
+            eval_dict[evaluations[1][0]]["value"],
+            eval_dict[evaluations[2][0]]["value"],
+            eval_dict[evaluations[3][0]]["value"]))
 
 
 def annual_evaluation_day(data_dict, evaluate):
-    print ('%s\t\t%s\t\t\t\t%s' % ("Year","Date","Temp"))
+    print ('%s\t\t%s\t\t\t\t%s' % ("Year", "Date", "Temp"))
 
     print ('------------------------------------------------------------')
     for key_year, eval_dict in data_dict.items():
-        print ('%s\t\t%s\t\t\t%s' % (str(key_year),eval_dict[evaluate]["date"],eval_dict[evaluate]["value"]))
+        print ('%s\t\t%s\t\t\t%s' % (
+            str(key_year),
+            eval_dict[evaluate]["date"],
+            eval_dict[evaluate]["value"]))
 
 
-def validate(report_no=0, data_dir=''):
+def valid(report_no=0, data_dir=''):
     try:
         val = int(report_no)
         if not val in range(1, 4):
@@ -115,15 +132,23 @@ def validate(report_no=0, data_dir=''):
 
     return True
 
-def main():
-    # report_no = input("Enter Report # : ")
-    # data_dir = input("Enter Data Directory : ")
 
-    report_no = 3
-    data_dir = '/Users/danish/Documents/Python/Assignment_1/weatherdata/'
-    if validate(report_no, data_dir):
-        data_dict = prepare_data_for_report(data_dir)
-        print_report(data_dict, report_no)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "report_no",
+        help="1 for Annual Max/Min Temperature "
+             + "\n2 for Hottest day of each year "
+             + "\n3 for coldest day of each year",
+        type=int)
+    parser.add_argument(
+        "data_dir",
+        help="Directory containing weather data files")
+    args = parser.parse_args()
+
+    if valid(args.report_no, args.data_dir):
+        data_dict = prepare_data_for_report(args.data_dir)
+        print_report(data_dict, args.report_no)
 
 
 main()
