a14141dbbab51ccb6a840d650ae347ea22739724
Danish Zahid <danish.zahid@arbisoft.com>
2016-08-24 15:57:21 +0500

    improvements - adding data structure approach - use of Enum - use of Dunder method

--- weatherman.py
+++ weatherman.py
@@ -3,134 +3,126 @@ import csv
 import glob
 import argparse
 
-import sys
-
+from enum import Enum
 from datetime import datetime
 
-import operator
+
+class WeatherReport(Enum):
+    annual_min_max_temperature = "1"
+    annual_max_temperature = "2"
+    annual_min_temperature = "3"
 
 
-def prepare_data_for_report(data_dir):
+class WeatherData:
+    def __init__(self, date, max_temperature, min_temperature,
+                 max_humidity, min_humidity):
+        self.date = date
+        self.max_temperature = max_temperature
+        self.min_temperature = min_temperature
+        self.max_humidity = max_humidity
+        self.min_humidity = min_humidity
+
+
+def import_data_from_files(data_dir):
     os.chdir(data_dir)
-    evaluations = get_evaluation_header()
 
-    value_key = "value"
-    date_key = "date"
+    weather_data_dict = {}
 
-    data_dict = {}
     for weather_file in glob.glob('*.txt'):
         with open(weather_file, "r") as csvfile:
-            # to ignore header line
-            csvfile.readline()
-            reader = csv.DictReader(csvfile)
+            readable_lines = csvfile.readlines()[1:-1]
+            reader = csv.DictReader(readable_lines)
+
+            max_temp = -100
+            min_temp = 100
+            max_humid = -1
+            min_humid = 101
 
             for row in reader:
-                try:
-                    try:
-                        date = row["PKT"]
-                    except KeyError:
-                        date = row["PKST"]
-
-                    year = datetime.strptime(str(date), "%Y-%m-%d").year
-
-                    for evaluate in evaluations:
-                        key_header = evaluate[0]
-                        operation = evaluate[1]
-                        temp = int(row[key_header])
-
-                        if year not in data_dict:
-                            data_dict[year] = {}
-                        if key_header not in data_dict[year]:
-                            data_dict[year][key_header] = {value_key: temp,
-                                                           date_key: date}
-                            continue
-
-                        if operation(temp, data_dict[year][key_header][value_key]) \
-                                or not data_dict[year][key_header][value_key]:
-                            data_dict[year][key_header] = {value_key: temp,
-                                                           date_key: date}
-
-                except ValueError:
-                    # need to find a good solution
-                    # This occurs because of last line in file
-                    continue
-
-    # print(data_dict)
-    return data_dict
+                date = row.get('PKT') or row.get('PKST')
+                year = datetime.strptime(str(date), "%Y-%m-%d").year
+                if row['Max TemperatureC']:
+                    max_temp = int(row.get('Max TemperatureC'))
+                if row['Max TemperatureC']:
+                    min_temp = int(row.get('Min TemperatureC'))
+                if row['Max TemperatureC']:
+                    max_humid = int(row.get('Max Humidity'))
+                if row['Max TemperatureC']:
+                    min_humid = int(row.get(' Min Humidity'))
+                if year not in weather_data_dict:
+                    weather_data_dict[year] = []
+                weather_data_dict[year].append(
+                    WeatherData(date, max_temp, min_temp, max_humid, min_humid))
+    return weather_data_dict
 
 
-def get_evaluation_header():
-    max_temp = ["Max TemperatureC", operator.gt]
-    min_temp = ["Min TemperatureC", operator.lt]
-    max_humid = ["Max Humidity", operator.gt]
-    min_humid = [" Min Humidity", operator.lt]
-
-    evaluations = [
-        max_temp,
-        min_temp,
-        max_humid,
-        min_humid
-    ]
-
-    return evaluations
-
-
-def print_report(data_dict, report_no):
-    evaluations = get_evaluation_header()
-    if report_no == 1:
-        print("Annual Max/Min Temperature")
-        print_annual_evaluations(evaluations, data_dict)
-    elif report_no == 2:
-        print("Hottest day of each year")
-        annual_evaluation_day(data_dict, evaluations[0][0])
-    elif report_no == 3:
-        print("Coldest day of each year")
-        annual_evaluation_day(data_dict, evaluations[1][0])
-
-
-def print_annual_evaluations(evaluations, data_dict):
-    print ('%s\t%s\t%s\t%s\t%s' % ("Year",
-                                   evaluations[0][0],
-                                   evaluations[1][0],
-                                   evaluations[2][0],
-                                   evaluations[3][0]))
-    print (
-    '------------------------------------------------------------------------------------------')
-    for key_year, eval_dict in data_dict.items():
-        print ('%s\t\t%s\t\t\t%s\t\t\t%s\t\t%s' % (
-            str(key_year),
-            eval_dict[evaluations[0][0]]["value"],
-            eval_dict[evaluations[1][0]]["value"],
-            eval_dict[evaluations[2][0]]["value"],
-            eval_dict[evaluations[3][0]]["value"]))
-
-
-def annual_evaluation_day(data_dict, evaluate):
-    print ('%s\t\t%s\t\t\t\t%s' % ("Year", "Date", "Temp"))
-
-    print ('------------------------------------------------------------')
-    for key_year, eval_dict in data_dict.items():
-        print ('%s\t\t%s\t\t\t%s' % (
-            str(key_year),
-            eval_dict[evaluate]["date"],
-            eval_dict[evaluate]["value"]))
-
-
-def valid(report_no=0, data_dir=''):
-    try:
-        val = int(report_no)
-        if not val in range(1, 4):
-            print("Enter integer between 1 - 3")
-            return False
-    except ValueError:
-        print("Enter integer between 1 - 3")
-        return False
-
+def valid(data_dir=''):
     if not os.path.isdir(data_dir):
         print("Weather data directory does not exist")
         return False
 
     return True
+
+
+def print_yearly_coldest_day(weather_data_dict):
+    print("Coldest day of each year")
+    print ('%s\t\t%s\t\t\t\t%s' % ("Year", "Date", "Temp"))
+    print ('------------------------------------------------------------')
+    for year, year_weather in weather_data_dict.items():
+        year_weather.sort(key=lambda x: x.min_temperature, reverse=False)
+        print ('%s\t\t%s\t\t\t%s' % (
+            str(year),
+            str(year_weather[0].date),
+            year_weather[0].min_temperature))
+
+
+def print_yearly_hottest_day(weather_data_dict):
+    print("Hottest day of each year")
+    print ('%s\t\t%s\t\t\t\t%s' % ("Year", "Date", "Temp"))
+    print ('------------------------------------------------------------')
+    for year, year_weather in weather_data_dict.items():
+        year_weather.sort(key=lambda x: x.max_temperature, reverse=True)
+        print ('%s\t\t%s\t\t\t%s' % (
+            str(year),
+            str(year_weather[0].date),
+            year_weather[0].max_temperature))
+
+
+def print_yearly_report(weather_data_dict):
+    print("Annual Max/Min Temperature")
+    print ('%s\t%s\t%s\t%s\t%s' % (
+        "Year",
+        "Max TemperatureC",
+        "Min TemperatureC",
+        "Max Humidity",
+        "Min Humidity"))
+    print (
+        '------------------------------------------------------------------------------------------')
+    for year, year_weather in weather_data_dict.items():
+        year_weather.sort(key=lambda x: x.max_temperature, reverse=True)
+        max_temp = str(year_weather[0].max_temperature)
+        year_weather.sort(key=lambda x: x.min_temperature, reverse=False)
+        min_temp = str(year_weather[0].min_temperature)
+        year_weather.sort(key=lambda x: x.max_humidity, reverse=True)
+        max_humid = str(year_weather[0].max_humidity)
+        year_weather.sort(key=lambda x: x.min_humidity, reverse=False)
+        min_humid = str(year_weather[0].min_humidity)
+
+        print ('%s\t\t%s\t\t\t%s\t\t\t%s\t\t%s' % (
+            str(year),
+            max_temp,
+            min_temp,
+            max_humid,
+            min_humid))
+
+
+def print_weather_report(weather_data_dict, report_no):
+    if report_no == WeatherReport.annual_min_max_temperature.value:
+        print_yearly_report(weather_data_dict)
+    elif report_no == WeatherReport.annual_max_temperature.value:
+        print_yearly_hottest_day(weather_data_dict)
+    elif report_no == WeatherReport.annual_min_temperature.value:
+        print_yearly_coldest_day(weather_data_dict)
 
 
 def main():
@@ -140,15 +132,16 @@ def main():
         help="1 for Annual Max/Min Temperature "
              + "\n2 for Hottest day of each year "
              + "\n3 for coldest day of each year",
-        type=int)
+        choices=["1", "2", "3"])
     parser.add_argument(
         "data_dir",
         help="Directory containing weather data files")
     args = parser.parse_args()
 
-    if valid(args.report_no, args.data_dir):
-        data_dict = prepare_data_for_report(args.data_dir)
-        print_report(data_dict, args.report_no)
+    if valid(args.data_dir):
+        weather_data_dict = import_data_from_files(args.data_dir)
+        print_weather_report(weather_data_dict, args.report_no)
 
 
-main()
+if __name__ == "__main__":
+    main()
