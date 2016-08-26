d3d4448f60460b8e8814d1946555a8e4e9e68e9d
Danish Zahid <danish.zahid@arbisoft.com>
2016-08-26 18:07:36 +0500

    more improvements seperation to ensure unit testing variable names synced

--- weatherman.py
+++ weatherman.py
@@ -8,7 +8,7 @@ from datetime import datetime
 
 
 class WeatherReport(IntEnum):
-    annual_min_max_temperature = 1
+    annual_min_max_reading = 1
     annual_max_temperature = 2
     annual_min_temperature = 3
 
@@ -26,7 +26,7 @@ class WeatherReading:
 def import_data_from_files(data_dir):
     os.chdir(data_dir)
 
-    weather_data = {}
+    weather_readings = {}
 
     for weather_file in glob.glob('*.txt'):
         with open(weather_file, "r") as csvfile:
@@ -40,38 +40,68 @@ def import_data_from_files(data_dir):
                 min_temp = int(row.get('Min TemperatureC') if row['Max TemperatureC'] else 100)
                 max_humid = int(row.get('Max Humidity') if row['Max TemperatureC'] else -1)
                 min_humid = int(row.get(' Min Humidity') if row['Max TemperatureC'] else 101)
-                if year not in weather_data:
-                    weather_data[year] = []
-                weather_data[year].append(
+                if year not in weather_readings:
+                    weather_readings[year] = []
+                weather_readings[year].append(
                     WeatherReading(date, max_temp, min_temp, max_humid, min_humid))
-    return weather_data
+    return weather_readings
 
 
-def print_yearly_coldest_day(weather_data):
+def get_yearly_coldest_days(weather_readings):
+    coldest_days = {}
+    for year, year_weather in weather_readings.items():
+        min_temp = min(year_weather, key=lambda x: x.min_temperature)
+        coldest_days[year] = min_temp
+    return coldest_days
+
+
+def get_yearly_hottest_days(weather_readings):
+    hottest_days = {}
+    for year, year_weather in weather_readings.items():
+        max_temp = max(year_weather, key=lambda x: x.max_temperature)
+        hottest_days[year] = max_temp
+    return hottest_days
+
+
+def get_yearly_min_max_readings(weather_readings):
+    yearly_min_max_readings = {}
+    for year, year_weather_reading in weather_readings.items():
+        max_temp = max(year_weather_reading, key=lambda x: x.max_temperature)
+        min_temp = min(year_weather_reading, key=lambda x: x.min_temperature)
+        max_humid = max(year_weather_reading, key=lambda x: x.max_humidity)
+        min_humid = min(year_weather_reading, key=lambda x: x.min_humidity)
+
+        yearly_min_max_readings[year] = WeatherReading(year,
+                                                       max_temp.max_temperature,
+                                                       min_temp.min_temperature,
+                                                       max_humid.max_humidity,
+                                                       min_humid.min_humidity)
+    return yearly_min_max_readings
+
+
+def print_yearly_coldest_day(coldest_days):
     print("Coldest day of each year")
     print('%s\t\t%s\t\t\t\t%s' % ("Year", "Date", "Temp"))
     print('------------------------------------------------------------')
-    for year, year_weather in weather_data.items():
-        min_temp = min(year_weather, key=lambda x: x.min_temperature)
+    for year, coldest_day in coldest_days.items():
         print('%s\t\t%s\t\t\t%s' % (
-            str(year),
-            str(min_temp.date),
-            min_temp.min_temperature))
+            year,
+            coldest_day.date,
+            coldest_day.min_temperature))
 
 
-def print_yearly_hottest_day(weather_data):
+def print_yearly_hottest_day(hottest_days):
     print("Hottest day of each year")
     print('%s\t\t%s\t\t\t\t%s' % ("Year", "Date", "Temp"))
     print('------------------------------------------------------------')
-    for year, year_weather in weather_data.items():
-        max_temp = max(year_weather, key=lambda x: x.max_temperature)
+    for year, hottest_day in hottest_days.items():
         print('%s\t\t%s\t\t\t%s' % (
-            str(year),
-            str(max_temp.date),
-            max_temp.max_temperature))
+            year,
+            hottest_day.date,
+            hottest_day.max_temperature))
 
 
-def print_yearly_report(weather_data):
+def print_yearly_report(yearly_min_max_readings):
     print("Annual Max/Min Temperature")
     print('%s\t%s\t%s\t%s\t%s' % (
         "Year",
@@ -81,27 +111,25 @@ def print_yearly_report(weather_data):
         "Min Humidity"))
     print(
         '------------------------------------------------------------------------------------------')
-    for year, year_weather in weather_data.items():
-        max_temp = max(year_weather, key=lambda x: x.max_temperature)
-        min_temp = min(year_weather, key=lambda x: x.min_temperature)
-        max_humid = max(year_weather, key=lambda x: x.max_humidity)
-        min_humid = min(year_weather, key=lambda x: x.min_humidity)
-
-        print('%s\t\t%s\t\t\t%s\t\t\t%s\t\t%s' % (
-            str(year),
-            max_temp.max_temperature,
-            min_temp.min_temperature,
-            max_humid.max_humidity,
-            min_humid.min_humidity))
+    for year, yearly_reading in yearly_min_max_readings.items():
+        print('%d\t\t%s\t\t\t%s\t\t\t%s\t\t%s' % (
+            year,
+            yearly_reading.max_temperature,
+            yearly_reading.min_temperature,
+            yearly_reading.max_humidity,
+            yearly_reading.min_humidity))
 
 
-def print_weather_report(weather_data, report_no):
-    if report_no == WeatherReport.annual_min_max_temperature.value:
-        print_yearly_report(weather_data)
+def print_weather_report(weather_readings, report_no):
+    if report_no == WeatherReport.annual_min_max_reading.value:
+        yearly_min_max_readings = get_yearly_min_max_readings(weather_readings)
+        print_yearly_report(yearly_min_max_readings)
     elif report_no == WeatherReport.annual_max_temperature.value:
-        print_yearly_hottest_day(weather_data)
+        hottest_days = get_yearly_hottest_days(weather_readings)
+        print_yearly_hottest_day(hottest_days)
     elif report_no == WeatherReport.annual_min_temperature.value:
-        print_yearly_coldest_day(weather_data)
+        coldest_days = get_yearly_coldest_days(weather_readings)
+        print_yearly_coldest_day(coldest_days)
 
 
 def main():
@@ -119,8 +147,8 @@ def main():
     args = parser.parse_args()
 
     if os.path.isdir(args.data_dir):
-        weather_data = import_data_from_files(args.data_dir)
-        print_weather_report(weather_data, args.report_no)
+        weather_readings = import_data_from_files(args.data_dir)
+        print_weather_report(weather_readings, args.report_no)
     else:
         print("Weather data directory does not exist")
 
