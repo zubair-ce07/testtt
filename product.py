742d455134e7bdc6eb08bd18a5af40a38efafd8d
SayyedaRijab <sayyeda.rijab@arbisoft.com>
2016-08-26 16:51:35 -0400

    Renamed variables & functions

--- product.py
+++ product.py
@@ -13,21 +13,19 @@ class AsicsSpider(CrawlSpider):
     start_urls = ["http://www.asics.com/us/en-us/"]
 
     listings_xpath = [
-        ".//*[@id='main-menu']//ul[contains(@class,'childLeafNode')]//li[contains(@class,'yCmsComponent')]//a",
-        ".//*[contains(@class,'nm center')]/a"]
+        ".//*[@id='main-menu']//ul[contains(@class,'childLeafNode')]//li[contains(@class,'yCmsComponent')]",
+        ".//*[contains(@class,'nm center')]"]
 
-    product_listings = [".//*[contains(@class,'product-list')]/div/div/a"]
+    product_listings = [".//*[contains(@class,'product-list')]/div/div"]
 
     # this is to get all the product listing including pagination.
     rules = [
         Rule(LinkExtractor(restrict_xpaths=listings_xpath), ),
-        Rule(LinkExtractor(deny=["http://www.onitsukatiger.com/us/en-us/"],
-                           restrict_xpaths=product_listings, ),
-             callback="parse_product_details")
-    ]
+        Rule(LinkExtractor(restrict_xpaths=product_listings, ), callback="parse_product_details")
+            ]
 
     def parse_product_details(self, response):  # Retrieving required product details.
-        print("hello")
+
         garment = ProductSpiderItem()
 
         garment['spider_name'] = 'asics-us-crawl'
@@ -39,8 +37,8 @@ class AsicsSpider(CrawlSpider):
 
         garment['market'] = 'US'
 
-        garment['category'] = response.xpath(
-            "//*[contains(@id, 'breadcrumb')]/ul/li[not (@class='active')]/a[not(@href='/us/en-us/')]/span/text()").extract()
+        category_path = "//*[contains(@id, 'breadcrumb')]/ul/li[not (@class='active')]/a[not(@href='/us/en-us/')]/span/text()"
+        garment['category'] = response.xpath(category_path).extract()
 
         garment['description'] = self.get_description(response)
 
@@ -48,9 +46,9 @@ class AsicsSpider(CrawlSpider):
 
         garment['brand'] = response.xpath(".//*[contains(@class,'singleProduct')]/meta[1]/@content").extract()[-1]
 
-        garment['img_urls'] = self.get_img_urls(response)
+        garment['img_urls'] = self.product_img_urls(response)
 
-        garment['skus'] = self.get_skus(response)
+        garment['skus'] = self.product_skus(response)
 
         name = response.xpath(".//*[contains(@class,'single-prod-title')]/text()").extract()[-1]
         if name:
@@ -58,12 +56,12 @@ class AsicsSpider(CrawlSpider):
 
         garment['url'] = response.url
 
-        garment['gender'] = self.get_gender(garment['category'])
+        garment['gender'] = self.product_gender(garment['category'])
 
         yield garment
 
 
-    def get_skus(self, response):
+    def product_skus(self, response):
         skus = {}
         sel = response.xpath(".//*[contains(@id, 'SelectSizeDropDown')]/li[@class='SizeOption inStock']")
 
@@ -89,7 +87,7 @@ class AsicsSpider(CrawlSpider):
         return skus
 
 
-    def get_img_urls(self, response):  # getting image-URLS
+    def product_img_urls(self, response):  # getting image-URLS
         url_list = []
 
         sel = response.xpath(".//*[contains(@id,'product-image-0')]")
@@ -101,32 +99,23 @@ class AsicsSpider(CrawlSpider):
             url_list = img_urls
         return url_list
 
-
-    def get_gender(self, category_list):
+    def product_gender(self, category_list):
         gender_ = ''
         for item in category_list:
             if 'men' in item:
                 gender_ = 'Male'
-
             if 'women' in item:
                 gender_ = 'Female'
-
             if not ('men' in item and 'women' in item):
                 gender_ = 'Unisex'
-
             if 'kids' in item:
                 gender_ = 'Children'
         return gender_
 
-
     def get_description(self, response):    # Retrieving given description of product.
-        desc = response.xpath(".//*[contains(@class,'tabInfoChildContent')]/text()").extract()
-        desc_ = []
-        for item in desc:
-            if not item.isspace():
-                desc_.append(item.strip())
+        description = response.xpath(".//*[contains(@class,'tabInfoChildContent')]/text()").extract()
+        return [item.strip() for item in description if not item.isspace()]
 
-        return desc_
 
 
 
