4dfc47a738907e11823461cd2ec302ad35412b36
ahmedsuffianjaved <ahmed_suffian_javed@live.com>
2016-09-07 12:52:51 +0500

    Code Refactored

--- the-lab/the-lab/asgn1.py
+++ the-lab/the-lab/asgn1.py
@@ -4,154 +4,217 @@ import argparse
 import csv
 
 
+"""	This WeatherDataParser class reads a file, and stores the results """
 class WeatherDataParser(object):
 
-	def __init__(self, fileName,dataFolderPath):
-		self.fileName = fileName;
-		self.dataFolderPath = dataFolderPath;
+	""" Class constructor that takes file path as an argument """
+	def __init__(self, filePath):
+		self.filePath = filePath
 
 	@staticmethod
 	def __extractYear(str):
-		return str.split ('-') [0];
+		return str.split ('-') [0]
 	
 	@staticmethod
-	def __toInt(str):
-		if str is not None and str != '':
-			return int (str);
+	def __toInt(string):
+		if string:
+			return int (string)
 
+	""" Reads the file row by row and records min/max Temperatures, HUmidities and hottest days """
 	def parseFile(self):
-		with open(self.dataFolderPath+self.fileName) as csvfile:
-			firstTime = 1
+		with open(self.filePath) as csvfile:
+			readingFirstRow = 1
 			csvfile.seek (0)
 			next (csvfile)
 			reader = csv.DictReader(csvfile)
 
 			for row in reader:
 
-				if firstTime == 1:
-					firstTime = 0;
+				if readingFirstRow == 1:
 
-					minTemp = self.__toInt(row ['Min TemperatureC']);
-					maxTemp = self.__toInt(row ['Max TemperatureC']);
+					readingFirstRow = 0
+					minTemp = self.__toInt(row ['Min TemperatureC'])
+					maxTemp = self.__toInt(row ['Max TemperatureC'])
 	
 					if 'PKT' in row.keys():
+
 						dateKey = 'PKT'
 					else:
+
 						dateKey = 'PKST'
 
-					hottestDay = row [dateKey];
-					year = self.__extractYear (row[dateKey]);
+					hottestDay = row [dateKey]
+					year = self.__extractYear (row[dateKey])
 
-					minHumidity = self.__toInt(row [' Min Humidity']);
-					maxHumidity = self.__toInt(row ['Max Humidity']);
+					minHumidity = self.__toInt(row [' Min Humidity'])
+					maxHumidity = self.__toInt(row ['Max Humidity'])
 
+			
 				val = self.__toInt(row ['Max TemperatureC'])
 				if val is not None and (maxTemp is None or val > maxTemp):
-					maxTemp = val;
-					hottestDay = row [dateKey];
-					year = self.__extractYear (row[dateKey]);
+					maxTemp = val
+					hottestDay = row [dateKey]
+					year = self.__extractYear (row[dateKey])
 
 				val = self.__toInt(row ['Max Humidity'])
 				if val is not None and (maxHumidity is None or val > maxTemp):
-					maxHumidity = val;
+					maxHumidity = val
 
 				val = self.__toInt(row [' Min Humidity'])
 				if val is not None and (minHumidity is None or val < minHumidity):
-					minHumidity = val;
+					minHumidity = val
 
-				val = self.__toInt(row ['Min TemperatureC']);
+				val = self.__toInt(row ['Min TemperatureC'])
 				if val is not None and (minTemp is None or val < minTemp):
-					minTemp = val;
+					minTemp = val
 
-			self.minTemp = minTemp;
-			self.maxTemp = maxTemp;
-			self.minHumidity = minHumidity;
-			self.maxHumidity = maxHumidity;
+			self.minTemp = minTemp
+			self.maxTemp = maxTemp
+			self.minHumidity = minHumidity
+			self.maxHumidity = maxHumidity
 			self.hottestDay = hottestDay
 			self.year = year
+# <------------- END OF CLASS ---------------->
 
+
+""" Returns 1 if the input is a valid integer and returns 0 otherwise"""
+def isValidInt (value):
+	if value is not None:
+
+		return 1
+	else:
+
+		return 0
+
+""" Finds annual min/max Temperatures, min/max Humidities and hottest days """
 def generateAnnualReport (parsedDataList):
-	firstTime = 1
 
 	annualDictionary = {}
 
 	for parsedData in parsedDataList:
 		
 		if not parsedData.year in annualDictionary.keys():
+			# Set starting values for min/max Temperatures and min/max Humidity
 			annualDictionary [parsedData.year] = {}
 			annualDictionary [parsedData.year]['minTemp'] = parsedData.minTemp
 			annualDictionary [parsedData.year]['maxTemp'] = parsedData.maxTemp 
 			annualDictionary [parsedData.year]['minHumidity'] = parsedData.minHumidity
 			annualDictionary [parsedData.year]['maxHumidity'] = parsedData.maxHumidity
 			annualDictionary [parsedData.year]['hottestDay'] = parsedData.hottestDay
-			annualDictionary [parsedData.year]['year'] = parsedData.year;
+			annualDictionary [parsedData.year]['year'] = parsedData.year
 		else:
+			# Update the min/max temperatures and Humidities if required
 
 			val = parsedData.maxTemp
-			if val is not None and (annualDictionary [parsedData.year]['maxTemp'] is None or val > annualDictionary [parsedData.year]['maxTemp']):
-				annualDictionary [parsedData.year]['maxTemp'] = val;
-				annualDictionary [parsedData.year]['hottestDay'] = parsedData.hottestDay;
-				annualDictionary [parsedData.year]['year'] = parsedData.year;
+			if isValidInt(val) and (
+					annualDictionary [parsedData.year]['maxTemp'] is None or 
+					val > annualDictionary [parsedData.year]['maxTemp']):
+				# Update max temperature, hottest day and the current year
+
+				annualDictionary [parsedData.year]['maxTemp'] = val
+				annualDictionary [parsedData.year]['hottestDay'] = parsedData.hottestDay
+				annualDictionary [parsedData.year]['year'] = parsedData.year
 
 			val = parsedData.maxHumidity
-			if val is not None and (annualDictionary [parsedData.year]['maxHumidity'] is None or val > annualDictionary [parsedData.year]['maxHumidity']):
-				annualDictionary [parsedData.year]['maxHumidity'] = val;
+			if isValidInt(val) and (
+					annualDictionary [parsedData.year]['maxHumidity'] is None or 
+					val > annualDictionary [parsedData.year]['maxHumidity']):
+				# Update the max Humidity
 
-			val = parsedData.minTemp;
-			if val is not None and (annualDictionary [parsedData.year]['minTemp'] is None or val < annualDictionary [parsedData.year]['minTemp']):
-				annualDictionary [parsedData.year]['minTemp'] = val;
+				annualDictionary [parsedData.year]['maxHumidity'] = val
+
+			val = parsedData.minTemp
+			if isValidInt(val) and (
+					annualDictionary [parsedData.year]['minTemp'] is None or 
+					val < annualDictionary [parsedData.year]['minTemp']):
+				# Update the minimum Temperature
+
+				annualDictionary [parsedData.year]['minTemp'] = val
 
 			val = parsedData.minHumidity
-			if val is not None and (annualDictionary [parsedData.year]['minHumidity'] is None or val < annualDictionary [parsedData.year]['minHumidity']):
-				annualDictionary [parsedData.year]['minHumidity'] = val;
-	
-	return annualDictionary;
+			if isValidInt(val) and (
+					annualDictionary [parsedData.year]['minHumidity'] is None  or 
+					val < annualDictionary [parsedData.year]['minHumidity']):
+				# Update min humidity
 
+				annualDictionary [parsedData.year]['minHumidity'] = val
+	
+	return annualDictionary
+
+
+""" Prints annual report with Year, MAX Temp, MIN Temp, MAX Humidity and Min Humidity """
 def printAnnualWeatherReport (parsedDataList): 
 
 	print ('Year\t\tMAX Temp\tMIN Temp\tMAX Humidity\t  Min Humidity')
 	print ('-----------------------------------------------------------------------------')
 	count = 0
-	annualDictionary = generateAnnualReport (parsedDataList);
+	annualDictionary = generateAnnualReport (parsedDataList)
 	for year,yearData in annualDictionary.items ():
-		print (yearData['year'], '\t\t', yearData['maxTemp'], '\t\t', yearData['minTemp'], '\t\t',  yearData['maxHumidity'], '\t\t', yearData['minHumidity']);
 
+		print (yearData['year'], '\t\t', yearData['maxTemp'], '\t\t', yearData['minTemp'], '\t\t',  yearData['maxHumidity'], '\t\t', yearData['minHumidity'])
+
+
+""" Prints yearly hottest days and the corresponding maximum temperatures """
 def printHottestDays(parsedDataList):
 
-	print ('Hotest days of each year');
-	print ('Year\tDate\t\tTemp');
-	annualDictionary = generateAnnualReport (parsedDataList);
+	print ('Hotest days of each year')
+	print ('Year\tDate\t\tTemp')
+	annualDictionary = generateAnnualReport (parsedDataList)
 	for year,yearData in annualDictionary.items ():
-		print (yearData['year'], '\t', yearData['hottestDay'].replace ('-', '/'), '\t', yearData['maxTemp']);	
 
+		print (yearData['year'], '\t', yearData['hottestDay'].replace ('-', '/'), '\t', yearData['maxTemp'])	
+
+""" Format the path of the file """
+def formatFilePath(directoryPath, fileName):
+	formattedFilePath = ''
+
+	if directoryPath[len (directoryPath) - 1] != '/':
+
+		formattedFilePath = directoryPath+'/'+fileName
+	else:
+
+		formattedFilePath = directoryPath+fileName
+
+	return formattedFilePath
+
+""" Main Function """
 def main():
+
+	# Read Command Line arguments and proceed if the arguments are valid. 
 
 	parser = argparse.ArgumentParser(description='Example with non-optional arguments')
 
-	parser.add_argument('reportID', help='Use 1 for Annual Max/Min Temperature and 2 for Hottest day of each year',action="store", type=int)
-	parser.add_argument('data_dir', action="store", help='Path of directory containing weather data files')
+	parser.add_argument('reportID', help='Use 1 for Annual Max/Min Temperature and 2 for Hottest day of each year',
+							action="store", type=int)
+	parser.add_argument('data_dir', action="store", 
+							help='Path of directory containing weather data files')
 
 	args = parser.parse_args()
 
-	dataFolderPath = args.data_dir;
+	dataFolderPath = args.data_dir
 
 	try:
 		allFileNames = [f for f in listdir(dataFolderPath) if isfile(join(dataFolderPath, f))]
+
 	except OSError as err:
+
 		print("OS error: {0}".format(err))
-		exit ();
+		exit ()
 	
-	parsedFileDataList= [];
+	parsedFileDataList= []
 
 	for fileName in allFileNames:
-		weatherDataParser = WeatherDataParser(fileName, dataFolderPath);
-		weatherDataParser.parseFile();
-		parsedFileDataList.append (weatherDataParser);
+
+		weatherDataParser = WeatherDataParser(formatFilePath(dataFolderPath,fileName))
+		weatherDataParser.parseFile()
+		parsedFileDataList.append (weatherDataParser)
 
 	if args.reportID == 1:
-		printAnnualWeatherReport (parsedFileDataList);
+
+		printAnnualWeatherReport (parsedFileDataList)
 	else:
-		printHottestDays (parsedFileDataList);
+
+		printHottestDays (parsedFileDataList)
 
 if __name__ == "__main__":
 	main()
