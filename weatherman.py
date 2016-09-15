22c8937f2124bf4774f77a0d1bdde2cb9edf5e4e
umair333 <umair333@github.com>
2016-09-15 15:04:23 +0500

    leading double underscores removed, constructed tuned, attempted to shorten long methods - still working on it

--- weatherman.py
+++ weatherman.py
@@ -12,26 +12,24 @@ def skip_last_line(it):
 
 
 class Weather:
-    __dated_weather_data = []
+    dated_weather_data = []
     # date, maxTemp, minTemp, maxHumid
-    __month = ""
-    __year = ""
-    __specified_month_temp = []
+    month = ""
+    year = ""
+    specified_month_temp = []
     # day, maxT, minT
-    __monthly_avg_temp = []
+    monthly_avg_temp = []
     # year, month, avg_maxT, avg_minT, avg_maxH
-    __yearly_max_temp = []
+    yearly_max_temp = []
     # year, month, day, maxTemp
-    __yearly_min_temp = []
+    yearly_min_temp = []
     # year, month, day, minTemp
-    __yearly_max_humidity = []
+    yearly_max_humidity = []
     # year, month, day, maxHumidity
 
-    def __init__(self, path_to_dir, _year="", _month=""):
-        self.__year = _year
-        self.__month = _month
-        Weather.read_data(self, path_to_dir)
-        Weather.process_data(self, _year, _month)
+    def __init__(self):
+        self.year = ""
+        self.month = ""
 
     def read_data(self, path_to_dir):
         for root, dirs, files in os.walk(path_to_dir):
@@ -47,25 +45,21 @@ class Weather:
                             # Do not read this particular day if its -
                             # - missing values of Min or Max temp or humidity
                             continue
-                        self.__dated_weather_data.append((str(line.get("PKT") or
-                                                              line.get("PKST")),
-                                                          int(line.get("Max TemperatureC")),
-                                                          int(line.get("Min TemperatureC")),
-                                                          int(line.get("Max Humidity"))))
+                        self.dated_weather_data.append((str(line.get("PKT") or
+                                                            line.get("PKST")),
+                                                        int(line.get("Max TemperatureC")),
+                                                        int(line.get("Min TemperatureC")),
+                                                        int(line.get("Max Humidity"))))
 
-    def process_data(self, _year, _month):
-        days_in_month = 0
-        max_temp_sum = 0
-        min_temp_sum = 0
-        humidity_sum = 0
-        highest_temp = 0
-        highest_temp_date = ""
+    def process_data(self, _year="", _month=""):
+        self.month = _month
+        self.year = _year
+        days_in_month = max_temp_sum = min_temp_sum = humidity_sum = 0
+        highest_temp = most_humidity = 0
         lowest_temp = 100
-        lowest_temp_date = ""
-        most_humidity = 0
-        most_humid_day = ""
-        prev_month = (str(self.__dated_weather_data[0]).split("-"))[1]
-        for row in self.__dated_weather_data:
+        highest_temp_date = lowest_temp_date = most_humid_day = ""
+        prev_month = (str(self.dated_weather_data[0]).split("-"))[1]
+        for row in self.dated_weather_data:
             date = row[0]
             max_temp = row[1]
             min_temp = row[2]
@@ -74,7 +68,7 @@ class Weather:
             curr_year = d[0]
             curr_month = d[1]
             if curr_year == _year and curr_month == _month:
-                self.__specified_month_temp.append((d[2], max_temp, min_temp))
+                self.specified_month_temp.append((d[2], max_temp, min_temp))
             max_temp_sum += max_temp
             min_temp_sum += min_temp
             humidity_sum += humidity
@@ -90,66 +84,57 @@ class Weather:
                 most_humid_day = d
             if days_in_month > 0 and prev_month != curr_month:
                 prev_month = curr_month
-                max_temp_avg = max_temp_sum / days_in_month
-                min_temp_avg = min_temp_sum / days_in_month
-                humidity_avg = humidity_sum / days_in_month
-                days_in_month = 0
-                max_temp_sum = 0
-                min_temp_sum = 0
-                humidity_sum = 0
-                self.__monthly_avg_temp.append((d[0], d[1], round(max_temp_avg),
-                                                round(min_temp_avg), round(humidity_avg)))
-                highest_temp_tuple = (highest_temp_date[0],
-                                      highest_temp_date[1],
-                                      highest_temp_date[2], highest_temp)
-                for year in self.__yearly_max_temp:
-                    if year[0] == highest_temp_date[0] and \
-                                    int(year[3]) < highest_temp:
-                        self.__yearly_max_temp.remove(year)
-                self.__yearly_max_temp.append(highest_temp_tuple)
-                lowest_temp_tuple = (lowest_temp_date[0],
-                                     lowest_temp_date[1],
-                                     lowest_temp_date[2], lowest_temp)
-                for year in self.__yearly_min_temp:
-                    if year[0] == lowest_temp_date[0] and \
-                                    int(year[3]) > lowest_temp:
-                        self.__yearly_min_temp.remove(year)
-                self.__yearly_min_temp.append(lowest_temp_tuple)
-                most_humid_tuple = (most_humid_day[0],
-                                    most_humid_day[1],
-                                    most_humid_day[2], most_humidity)
-                for year in self.__yearly_max_humidity:
-                    if year[0] == most_humid_day[0] and \
-                            int(year[3] < most_humidity):
-                        self.__yearly_max_humidity.remove(year)
-                self.__yearly_max_humidity.append(most_humid_tuple)
-                highest_temp = 0
-                highest_temp_date = ""
+                self.monthly_avg_temp.append((d[0], d[1], round(max_temp_sum / days_in_month),
+                                              round(min_temp_sum / days_in_month),
+                                              round(humidity_sum / days_in_month)))
+                days_in_month = max_temp_sum = min_temp_sum = humidity_sum = 0
+                Weather.append_yearly_data(self, "max", highest_temp_date, highest_temp)
+                Weather.append_yearly_data(self, "min", lowest_temp_date, lowest_temp)
+                Weather.append_yearly_data(self, "humid", most_humid_day, most_humidity)
+                highest_temp = most_humidity = 0
                 lowest_temp = 100
-                lowest_temp_date = ""
-                most_humidity = 0
-                most_humid_day = ""
+                highest_temp_date = lowest_temp_date = most_humid_day = ""
+        Weather.sort_data(self)
 
-        self.__yearly_max_temp.sort(key=lambda tup: tup[0])
-        self.__yearly_min_temp.sort(key=lambda tup: tup[0])
-        self.__yearly_max_humidity.sort(key=lambda tup: tup[0])
+    def append_yearly_data(self, option_str, date_str, value):
+        temp_tuple = (date_str[0], date_str[1], date_str[2], value)
+        if option_str == "max":
+            for year in self.yearly_max_temp:
+                if year[0] == date_str[0] and int(year[3]) < value:
+                    self.yearly_max_temp.remove(year)
+            self.yearly_max_temp.append(temp_tuple)
+        elif option_str == "min":
+            for year in self.yearly_min_temp:
+                if year[0] == date_str[0] and int(year[3]) > value:
+                    self.yearly_min_temp.remove(year)
+            self.yearly_min_temp.append(temp_tuple)
+        elif option_str == "humid":
+            for year in self.yearly_max_humidity:
+                if year[0] == date_str[0] and int(year[3]) < value:
+                    self.yearly_max_humidity.remove(year)
+            self.yearly_max_humidity.append(temp_tuple)
+
+    def sort_data(self):
+        self.yearly_max_temp.sort(key=lambda tup: tup[0])
+        self.yearly_min_temp.sort(key=lambda tup: tup[0])
+        self.yearly_max_humidity.sort(key=lambda tup: tup[0])
 
     def annual_report(self, year_str):
-        for year in self.__yearly_max_temp:
+        for year in self.yearly_max_temp:
             if year[0] == year_str:
                 print("Highest:", year[3], "\bC on", calendar.month_name[int(year[1])], year[2])
                 break
-        for year in self.__yearly_min_temp:
+        for year in self.yearly_min_temp:
             if year[0] == year_str:
                 print("Lowest:", year[3], "\bC on", calendar.month_name[int(year[1])], year[2])
                 break
-        for year in self.__yearly_max_humidity:
+        for year in self.yearly_max_humidity:
             if year[0] == year_str:
                 print("Humid:", year[3], "\b% on", calendar.month_name[int(year[1])], year[2])
                 break
 
     def monthly_avg_report(self, y, m):
-        for month in self.__monthly_avg_temp:
+        for month in self.monthly_avg_temp:
             if month[0] == y and month[1] == m:
                 print("Highest Average:", month[2], "\bC")
                 print("Lowest Average:", month[3], "\bC")
@@ -157,8 +142,8 @@ class Weather:
                 break
 
     def month_chart_dual(self):
-        print(calendar.month_name[int(self.__month)], self.__year, end="")
-        for day in self.__specified_month_temp:
+        print(calendar.month_name[int(self.month)], self.year, end="")
+        for day in self.specified_month_temp:
             print("\033[1;31;47m")
             print(day[0], "+" * int(day[1]), day[1], "\bC", end="")
             print("\033[1;34;47m")
@@ -166,8 +151,8 @@ class Weather:
         print("\n")
 
     def month_chart_bonus(self):
-        print(calendar.month_name[int(self.__month)], self.__year)
-        for day in self.__specified_month_temp:
+        print(calendar.month_name[int(self.month)], self.year)
+        for day in self.specified_month_temp:
             print("\033[1;30;47m", "{:2}".format(day[0]), end=" ")
             print("\033[1;34;47m", "+" * int(day[2]), sep="", end="")
             print("\033[1;31;47m", "+" * (int(day[1]) - int(day[2])), sep="", end="")
@@ -182,20 +167,25 @@ def main():
     parser.add_argument("-c", help="Monthly Bar Chart", nargs=2)
     parser.add_argument("-b", help="Bonus Chart", nargs=2)
     arg = parser.parse_args()
+    weather = Weather()
     if arg.e:
-        weather = Weather(arg.e[1])
+        weather.read_data(arg.e[1])
+        weather.process_data()
         weather.annual_report(arg.e[0])
     elif arg.a:
-        weather = Weather(arg.a[1])
+        weather.read_data(arg.a[1])
+        weather.process_data()
         term = str(arg.a[0]).split("/")
         weather.monthly_avg_report(term[0], term[1])
     elif arg.c:
         term = str(arg.c[0]).split("/")
-        weather = Weather(arg.c[1], term[0], term[1])
+        weather.read_data(arg.c[1])
+        weather.process_data(term[0], term[1])
         weather.month_chart_dual()
     elif arg.b:
         term = str(arg.b[0]).split("/")
-        weather = Weather(arg.b[1], term[0], term[1])
+        weather.read_data(arg.b[1])
+        weather.process_data(term[0], term[1])
         weather.month_chart_bonus()
 
 if __name__ == '__main__':
