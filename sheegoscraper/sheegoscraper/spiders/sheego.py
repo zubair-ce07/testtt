1f6b9dbc0105ad7c8528c1557b6e0051b03d649e
Nasir Hussain <Nasir.hussain@arbisoft.com>
2016-09-27 15:13:09 +0500

    Better CSS,Renamed some variables and mehtods

--- sheegoscraper/sheegoscraper/spiders/sheego.py
+++ sheegoscraper/sheegoscraper/spiders/sheego.py
@@ -4,7 +4,6 @@ from ..items import SheegoItem
 from xml.etree.ElementTree import Element, SubElement, fromstring, tostring
 import re
 from scrapy.http import Request
-from itertools import product
 
 
 class SheegoSpider(CrawlSpider):
@@ -14,13 +13,12 @@ class SheegoSpider(CrawlSpider):
     rules = [
         Rule(LinkExtractor(allow="sheego.de/", deny=["sheego.de/\?", "\?", "html"], restrict_css="#content"),
              follow=True),
-        Rule(LinkExtractor(restrict_css=".product__item > div > div > .cj-active"),
-             callback='parse_prodcut', follow=True),
+        Rule(LinkExtractor(restrict_css=".cj-active"), callback='parse_prodcut', follow=True),
         Rule(LinkExtractor(allow="sheego.de/", deny=["sheego.de/\?", "\.html"],
                            restrict_css=".next.js-next.btn.btn-next"))
     ]
 
-    def parse_articles(self, response):
+    def articles(self, response):
         articles = response.css("script:contains('articlesString')::text").extract()[0]
         articles_filtered = re.findall("([0-9A-Z]{3,8})\;([0-9]+)", articles)
         return articles_filtered
@@ -33,7 +31,7 @@ class SheegoSpider(CrawlSpider):
                                 "http://www.schwab.de/KAL/KALAvailabilityRequestSchema.xsd"
                                })
         articles = SubElement(root, 'Articles')
-        for article_id, size in self.parse_articles(response):
+        for article_id, size in self.articles(response):
             article = SubElement(articles, 'Article')
             SubElement(article, "CompleteCatalogItemNo").text = article_id
             SubElement(article, "SizeAlphaText").text = size
@@ -41,25 +39,25 @@ class SheegoSpider(CrawlSpider):
             SubElement(article, "CustomerCompanyID").text = '0'
         return tostring(root).decode("utf-8")
 
-    def check_oos(self, response):
+    def request_kal(self, response):
         request = Request(url='https://www.sheego.de/request/kal.php',
                           method='POST',
                           headers={'Content-Type': 'application/xml'},
-                          callback=self.parse_oos,
+                          callback=self.parse_kal,
                           body=self.create_xml(response))
         request.meta['item'] = response.meta['item']
         return request
 
-    def parse_base_url(self, response):
+    def base_url(self, response):
         item = response.meta['item']
         return item['url_original'].split("_")[0] + "_" + item['product_id'] + "-{}-{}-{}.html"
 
-    def parse_oos(self, response):
+    def parse_kal(self, response):
         item = response.meta['item']
         root = fromstring(response.body)
         urls = []
         skus = {}
-        base_url = self.parse_base_url(response)
+        base_url = self.base_url(response)
         for article in root.findall('.//Article'):
             size = article.find(".//SizeAlphaText").text
             stock = article.find(".//Stock").text
@@ -69,8 +67,7 @@ class SheegoSpider(CrawlSpider):
                 url = base_url.format(catalog_id[:6], size, catalog_id[6:])
                 urls.append({"url": url, "sku_key": sku_key})
             else:
-                skus[sku_key] = {}
-                skus[sku_key]['oos'] = True
+                skus[sku_key] = {'oos': True}
         if urls:
             request = Request(urls[0]['url'], callback=self.parse_sku)
             item['skus'] = skus
@@ -86,11 +83,11 @@ class SheegoSpider(CrawlSpider):
         urls = response.meta['urls']
         sku_key = urls.pop(0)['sku_key']
         sku = {}
-        sku['color'] = self.parse_color(response)
-        sku['price'] = self.parse_price(response)
-        sku['previous_prices'] = self.parse_prev_price(response)
-        sku['size'] = self.parse_size(response)
-        sku['image_urls'] = self.parse_image_urls(response)
+        sku['color'] = self.color(response)
+        sku['price'] = self.price(response)
+        sku['previous_prices'] = self.prev_price(response)
+        sku['size'] = self.size(response)
+        sku['image_urls'] = self.image_urls(response)
         sku['currency'] = 'EUR'
         item['skus'][sku_key] = sku
         if urls:
@@ -104,54 +101,53 @@ class SheegoSpider(CrawlSpider):
     def parse_prodcut(self, response):
         item = SheegoItem()
         item['gender'] = 'Women'
-        item['category'] = self.parse_category(response)
+        item['category'] = self.category(response)
         item['url_original'] = response.url
-        item['product_id'] = self.parse_product_id(response)
-        item['name'] = self.parse_name(response)
-        item['brand'] = self.parse_brand(response)
-        item['care'] = self.parse_care(response)
-        item['description'] = self.parse_description(response)
+        item['product_id'] = self.product_id(response)
+        item['name'] = self.product_name(response)
+        item['brand'] = self.brand(response)
+        item['care'] = self.care(response)
+        item['description'] = self.description(response)
         item['skus'] = {}
         response.meta['item'] = item
-        return self.check_oos(response)
+        return self.request_kal(response)
 
-    def parse_prev_price(self, response):
+    def prev_price(self, response):
         prev_price = "".join(response.css(".at-wrongprice::text").extract())
         return ".".join(re.findall(r'\d+', prev_price))
 
-    def parse_price(self, response):
+    def price(self, response):
         price = "".join(response.css(".at-lastprice::text").extract())
         return ".".join(re.findall(r'\d+', price))
 
-    def parse_brand(self, response):
+    def brand(self, response):
         brand = response.css(".brand > a::text").extract() or response.css(".brand::text").extract()
         return brand[0].strip()
 
-    def parse_care(self, response):
-        care = response.css(".articlecare > dd > div > template > b::text").extract()
-        return care[0].strip() if care else ""
+    def care(self, response):
+        return [s for s in self.description(response) if "%" in s]
 
-    def parse_description(self, response):
+    def description(self, response):
         descriptions = []
-        for line in response.css(".at-dv-itemDetails > *::text").extract():
+        for line in response.css(".at-dv-itemDetails > [itemprop='description'] *::text").extract():
             if line.strip():
                 descriptions.append(line.strip())
         return descriptions
 
-    def parse_name(self, response):
+    def product_name(self, response):
         return response.css(".at-dv-itemName::text").extract()[0].strip()
 
-    def parse_product_id(self, response):
+    def product_id(self, response):
         return re.findall("_([0-9A-Za-z]+)", response.url)[0]
 
-    def parse_size(self, response):
+    def size(self, response):
         return response.css(".active::text").extract()[-1].replace('– ', '')
 
-    def parse_color(self, response):
+    def color(self, response):
         return response.css(".at-dv-color::text").extract()[0].replace('— ', '')
 
-    def parse_category(self, response):
+    def category(self, response):
         return response.css("meta[name*='z_breadcrumb']::attr(content)").extract()[0].split(">")[:-1]
 
-    def parse_image_urls(self, response):
+    def image_urls(self, response):
         return response.css(".imageThumb::attr(data-zoom-image)").extract()
