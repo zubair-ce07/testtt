054be4f3249a9f5ac7ac01537849e927b2ac5671
ahmedsuffianjaved <ahmed_suffian_javed@live.com>
2016-10-08 15:46:28 +0500

    Export item data to json feature added, code refactored

--- wittweiden-spider/wittweiden/pipelines.py
+++ wittweiden-spider/wittweiden/pipelines.py
@@ -6,6 +6,18 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class WittweidenPipeline(object):
+import json
+import os
+
+
+class JsonWriterPipeline(object):
+    def __init__(self):
+        self.dir_path = '../witt-weiden-data'
+        if not os.path.exists(self.dir_path):
+            os.makedirs(self.dir_path)
+
     def process_item(self, item, spider):
+        with open(os.path.join(self.dir_path, item['retailer_sku'] +
+                '.json'), 'w') as jsonfile:
+            jsonfile.write(json.dumps(dict(item), indent=3, sort_keys=True))
         return item
