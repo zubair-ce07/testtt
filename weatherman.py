d90db9492188e41ae348fd33492b428a7a5f45ac
Ayaz Ahmad Tarar <Ayaz@UserXs-MacBook-Pro.local>
2016-10-10 19:49:31 +0500

    Code refactored.Files are read onlt once in application.use of valriable minimized using functional programming

--- weatherman.py
+++ weatherman.py
@@ -1,14 +1,10 @@
 import sys
-
 import os
-
 import csv
-
 import datetime
-
 import argparse
-
 import calendar
+from operator import itemgetter, attrgetter
 
 
 class WeatherMan:
@@ -16,140 +12,115 @@ class WeatherMan:
         return
 
     @staticmethod
-    def generate_annual_report(filename_list):
+    def generate_annual_report(temp_data_dict):
+        column_to_be_calculated = [1, 3, 7]
+        value_to_be_printed = ["Highest : ", "Lowest : ", "Max Humidity : "]
+        units = ["C", "C", "%"]
+        calculated_value = 0
+        for iteration, header_value in enumerate(column_to_be_calculated):
+            calculated_date = ""
+            daily_date_record = \
+                [x[file_reader.fieldnames[0]] for x in temp_data_dict]
+            daily_record = \
+                [x[file_reader.fieldnames[header_value]] for x in temp_data_dict]
 
-        high_temp = 0
-        high_temp_date = ""
-
-        low_temp = ''
-        low_temp_date = ""
-
-        max_humidity = 0
-        most_humid_day = ""
-
-        for temperature_file in filename_list:
-            if os.path.isfile(temperature_file):
-                header = WeatherMan(). \
-                    read_csv_file(temperature_file).fieldnames
-
-                for row in WeatherMan().read_csv_file(temperature_file):
-                    if row['Max TemperatureC'] != '' \
-                            and int(row['Max TemperatureC']) >= high_temp:
-                        high_temp = int(row['Max TemperatureC'])
-                        high_temp_date = str(row[header[0]])
-
-                for row in WeatherMan().read_csv_file(temperature_file):
-                    if row['Min TemperatureC'] != '' \
-                            and int(row['Min TemperatureC']) <= low_temp:
-                        low_temp = int(row['Min TemperatureC'])
-                        low_temp_date = str(row[header[0]])
-
-                for row in WeatherMan().read_csv_file(temperature_file):
-                    if row['Max Humidity'] != '' \
-                            and int(row['Max Humidity']) >= max_humidity:
-                        max_humidity = int(row['Max Humidity'])
-                        most_humid_day = str(row[header[0]])
-
-        date_to_month = high_temp_date.split('-')
-        month_of_highest = int(date_to_month[1])
-        day_of_highest = date_to_month[2]
-        print("Highest: " + str(high_temp) +
-              "C on " + year_month[month_of_highest - 1] +
-              " " + str(day_of_highest))
-
-        date_to_month = low_temp_date.split('-')
-        month_of_lowest = int(date_to_month[1])
-        day_of_lowest = date_to_month[2]
-        print("Lowest: " + str(low_temp) + "C on " +
-              year_month[month_of_lowest - 1] + " " +
-              str(day_of_lowest))
-
-        date_to_month = most_humid_day.split('-')
-        month_of_humidity = int(date_to_month[1])
-        day_of_humidity = date_to_month[2]
-
-        print("Humidity: " + str(max_humidity) +
-              "% on " + year_month[month_of_humidity - 1] +
-              " " + str(day_of_humidity))
+            if iteration == 1:
+                daily_formated_record = \
+                    [calculated_value if x == '' else x for x in daily_record]
+                temp_date_dict = dict(
+                    zip(daily_date_record, map(int, daily_formated_record)))
+                calculated_value = min(temp_date_dict.values())
+                calculated_date = min(temp_date_dict, key=temp_date_dict.get)
+                calculation_date = parse_date(calculated_date)
+            else:
+                daily_formated_record = \
+                    [0 if x == '' else x for x in daily_record]
+                temp_date_dict = dict(
+                    zip(daily_date_record, map(int, daily_formated_record))
+                )
+                calculated_value = max(temp_date_dict.values())
+                calculated_date = \
+                    [k for k, v in temp_date_dict.items()
+                     if v == calculated_value][-1]
+                calculation_date = parse_date(calculated_date)
+            print (value_to_be_printed[iteration] +
+                   str(calculated_value) +
+                   units[iteration] +
+                   " on " +
+                   str(year_month[calculation_date.month - 1]) +
+                   " " + str(calculation_date.day))
 
     @staticmethod
-    def generate_monthly_report(filename):
-        daily_maximum_temperature = []
-        daily_minimum_temperature = []
-        daily_mean_humidity = []
-        if os.path.isfile(filename):
-            file_reader = WeatherMan().read_csv_file(filename)
-            header = file_reader.fieldnames
-            for row in file_reader:
-                if row['Max TemperatureC'] != '':
-                    daily_maximum_temperature.append(int(row[header[1]]))
-                if row['Min TemperatureC'] != '':
-                    daily_minimum_temperature.append(int(row[header[3]]))
-                if row[' Mean Humidity'] != '':
-                    daily_mean_humidity.append(int(row[header[8]]))
-            highest_average = int(sum(daily_maximum_temperature) /
-                                  len(daily_maximum_temperature))
-            lowest_average = int(sum(daily_minimum_temperature) /
-                                 len(daily_minimum_temperature))
-            daily_mean_humidity = int(sum(daily_mean_humidity) /
-                                      len(daily_mean_humidity))
-
-            print("Highest Average: " + str(highest_average) + "C")
-            print("Lowest Average : " + str(lowest_average) + "C")
-            print ("Average Mean Humidity: " +
-                   str(daily_mean_humidity) + "%")
+    def generate_monthly_report(temp_data_dict):
+        column_to_be_calculated = [1, 3, 8]
+        value_to_be_printed = ["Highest Average : ",
+                               "Lowest Average : ",
+                               "Average Mean Humidity : "]
+        units = ["C", "C", "%"]
+        for iteration, header_value in enumerate(column_to_be_calculated):
+            daily_calculation = \
+                [x[file_reader.fieldnames[header_value]] for x in temp_data_dict]
+            daily_formated_record = \
+                [0 if x == '' else x for x in daily_calculation]
+            mean_value = reduce(
+                lambda x, y: x + y,
+                map(int, daily_formated_record)) / len(daily_formated_record)
+            print (value_to_be_printed[iteration] +
+                   str(mean_value) + units[iteration])
 
     @staticmethod
-    def generate_monthly_bar_chart(filename):
-        if os.path.isfile(filename):
-            day_counter = 1
-            file_reader = WeatherMan().read_csv_file(filename)
-            for row in file_reader:
-                if row['Max TemperatureC'] != '':
-                    highest_temp = int(row['Max TemperatureC'])
-                    red_text = "+" * highest_temp
-                    red_color_bar = "\033[1;31m" + \
-                                    red_text + \
-                                    "\033[1;m"
-                    print(str(day_counter) +
-                          red_color_bar +
-                          str(highest_temp))
-                if row['Min TemperatureC'] != '':
-                    lowest_temp = int(row['Min TemperatureC'])
-                    blue_text = "+" * lowest_temp
-                    blue_color_bar = "\033[1;34m" + \
-                                     blue_text + \
-                                     "\033[1;m"
-                    print(str(day_counter) +
-                          blue_color_bar +
-                          str(lowest_temp))
-                day_counter += 1
-        return
+    def generate_monthly_bar_chart(temp_data_dict):
+        column_to_be_read = [1, 3]
+        color_start = ['\033[1;31m', '\033[1;34m']
+        color_end = ['\033[1;m', '\033[1;m']
+        daily_calculation = []
+        daily_calculation.append(
+            [x[file_reader.fieldnames[1]] for x in temp_data_dict]
+        )
+        daily_calculation.append(
+            [x[file_reader.fieldnames[3]] for x in temp_data_dict]
+        )
+
+        for day in range(len(temp_data_dict)):
+            for iteration, column_number in enumerate(column_to_be_read):
+                if daily_calculation[iteration][day]:
+                    printline = '+' * int(daily_calculation[iteration][day])
+                else:
+                    printline = ""
+                print (str(day + 1) +
+                       color_start[iteration] +
+                       printline +
+                       color_end[iteration] +
+                       daily_calculation[iteration][day]
+                       )
 
     @staticmethod
-    def generate_monthly_oneline_chart(filename):
-        if os.path.isfile(filename):
-            day_counter = 1
-            file_reader = WeatherMan().read_csv_file(filename)
-            for row in file_reader:
+    def generate_monthly_oneline_chart(temp_data_dict):
+        column_to_be_read = [1, 3]
+        red_color = ['\033[1;31m', '\033[1;m']
+        blue_color = ['\033[1;34m', '\033[1;m']
+        daily_calculation = []
+        daily_maximum = [x[file_reader.fieldnames[1]] for x in temp_data_dict]
+        daily_minimum = [x[file_reader.fieldnames[3]] for x in temp_data_dict]
 
-                if row['Max TemperatureC'] != '':
-                    highest_temp = row['Max TemperatureC']
-                    red_text = "+" * int(highest_temp)
-                    red_color_bar = "\033[1;31m" + \
-                                    red_text + "\033[1;m"
-                if row['Min TemperatureC'] != '':
-                    lowest_temp = row['Min TemperatureC']
-                    blue_text = "+" * int(lowest_temp)
-                    blue_color_bar = "\033[1;34m" + \
-                                     blue_text + "\033[1;m"
-                    print(str(day_counter) + blue_color_bar +
-                          red_color_bar + lowest_temp + "-" +
-                          highest_temp)
-                    day_counter += 1
-        else:
-            print ("No such file found")
-        return
+        for day in range(len(temp_data_dict)):
+            if (daily_minimum[day]):
+                bluebar = blue_color[0] + \
+                          '+' * int(daily_minimum[day]) + \
+                          blue_color[1]
+            else:
+                bluebar = ""
+            if (daily_maximum[day]):
+                redbar = red_color[0] + \
+                         '+' * int(daily_maximum[day]) \
+                         + red_color[1]
+            else:
+                redbar = ""
+            print (str(day + 1) +
+                   bluebar + redbar +
+                   daily_minimum[day] +
+                   "-" +
+                   daily_maximum[day])
 
     @staticmethod
     def read_csv_file(filename):
@@ -162,57 +133,62 @@ class WeatherMan:
                 return file_reader
 
 
+def parse_date(calculated_date):
+    date = calculated_date.split("-")
+    formated_date = datetime.date(int(date[0]), int(date[1]), int(date[2]))
+    return formated_date
+
+
 if __name__ == '__main__':
 
     year_month = [month for month in calendar.month_abbr]
     year_month.pop(0)
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group()
-
     group.add_argument("-e", "--annual", action="store_true")
     group.add_argument("-a", "--monthly", action="store_true")
     group.add_argument("-c", "--bar_chart", action="store_true")
     group.add_argument("-c4", "--oneline_chart", action="store_true")
-    parser.add_argument("report_date", help="Path of folder containing Temperature record")
-    parser.add_argument("path_to_file", help="Path of folder containing Temperature record")
+    parser.add_argument("report_date", help="Date")
+    parser.add_argument("path_to_file", help="Path of folder")
     args = parser.parse_args()
-
     files_in_dir = [file for file in os.listdir(args.path_to_file)]
-    file_dir_name = []
-
-    if (args.annual):
-        year = args.report_date
-        if len(str(args.report_date).split('/')) > 1:
-            print("invalid date .Please enter in YYYY format")
-            sys.exit()
-        file_list = []
+    file_list = []
+    __Date = args.report_date.split("/")
+    year = __Date[0]
+    if len(__Date) < 2:
         for filename in files_in_dir:
-            if (filename.__contains__(args.report_date)):
+            if (filename.__contains__(year) and filename):
                 file_prefix = args.path_to_file + "/" + filename
                 file_list.append(file_prefix)
-        WeatherMan().generate_annual_report(file_list)
 
     else:
-        yearPlusMonth = str(args.report_date).split('/')
-        if len(yearPlusMonth) < 2:
-            print("invalid month or date .Please enter in YYYY/MM format")
-            sys.exit()
-        year = yearPlusMonth[0]
-        month = int(yearPlusMonth[1])
-        if month > 12 or month < 1:
-            print("invalid month")
-            sys.exit()
-        fileprefix = ""
-        for filename in files_in_dir:
-            if (filename.__contains__(year_month[month - 1])) and filename.__contains__(year):
-                file_prefix = args.path_to_file + "/" + filename
+        if len(__Date) > 1:
+            month = __Date[1]
+            for filename in files_in_dir:
+                if (filename.__contains__(year) and
+                        filename.__contains__(year_month[int(month) - 1])):
+                    file_prefix = args.path_to_file + "/" + filename
+                    file_list.append(file_prefix)
+
+    temp_data_dict = []
+    file_reader = ""
+    for list in file_list:
+        file_reader = WeatherMan().read_csv_file(list)
+        file_reader.fieldnames[0] = "PKT"
+        for line in file_reader:
+            temp_data_dict.append(line)
+
+    if (args.annual):
+        a = 5
+        WeatherMan().generate_annual_report(temp_data_dict)
+    else:
 
         if (args.monthly):
-            WeatherMan().generate_monthly_report(file_prefix)
+            WeatherMan().generate_monthly_report(temp_data_dict)
         else:
-            if (args.bar_chart):
-                WeatherMan().generate_monthly_bar_chart(file_prefix)
+            if args.bar_chart:
+                WeatherMan().generate_monthly_bar_chart(temp_data_dict)
             else:
-                if (args.oneline_chart):
-                    WeatherMan().generate_monthly_oneline_chart(file_prefix)
-
+                if args.oneline_chart:
+                    WeatherMan().generate_monthly_oneline_chart(temp_data_dict)
