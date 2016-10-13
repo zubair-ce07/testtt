685f8d599738926ea3f10418147bc37bb9e4ca98
Ahmed Suffian Javed <ahmed_suffian_javed@live.com>
2016-10-13 11:06:19 +0500

    Removed Json Pipeline

--- wittweiden-spider/wittweiden/pipelines.py
+++ wittweiden-spider/wittweiden/pipelines.py
@@ -4,20 +4,3 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-import json
-import os
-
-
-class JsonWriterPipeline(object):
-    def __init__(self):
-        self.dir_path = '../witt-weiden-data'
-        if not os.path.exists(self.dir_path):
-            os.makedirs(self.dir_path)
-
-    def process_item(self, item, spider):
-        with open(os.path.join(self.dir_path, item['retailer_sku'] +
-                '.json'), 'w') as jsonfile:
-            jsonfile.write(json.dumps(dict(item), indent=3, sort_keys=True))
-        return item
