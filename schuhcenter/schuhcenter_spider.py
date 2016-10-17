99a720d739542f01546d8112850f47d268a17389
ahmedsuffianjaved <ahmed_suffian_javed@live.com>
2016-10-17 18:36:15 +0500

    Code Refactored

--- schuhcenter/schuhcenter_spider.py
+++ schuhcenter/schuhcenter_spider.py
@@ -17,7 +17,6 @@ class Mixin(object):
 class SchuhcenterParseSpider(BaseParseSpider, Mixin):
     name = Mixin.retailer + '-parse'
 
-    UNWANTED_TOKENS = {'toy', 'toys'}
     price_x = "//span[@class='price']//text()|//span[" \
               "@class='oldPrice']//text()"
 
@@ -26,24 +25,18 @@ class SchuhcenterParseSpider(BaseParseSpider, Mixin):
     def parse(self, response):
         if '/error_general' in response.url:
             return
-        hxs = response
-        product_components = self.product_title(hxs)
+        product_components = self.product_title(response)
         product_brand, product_name = product_components[0], product_components[1]
         if not product_name:
             self.logger.info('Not a product page %s' % response.url)
             return
-        description = self.product_description(hxs)
-        categories = self.product_category(hxs)
+        categories = self.product_category(response)
         tokens = tokenize(categories)
-        if tokens & self.UNWANTED_TOKENS:
-            self.logger.info('Drop unwanted item %s' % response.url)
-            return
-
         common = {
             'brand': self.retailer,
-            'category': categories,
+            'category': self.product_category(response),
             'care': '',
-            'description': description,
+            'description': self.product_description(response),
             'gender': self.product_gender(tokens),
             'name': product_name,
             'brand': product_brand,
@@ -51,43 +44,44 @@ class SchuhcenterParseSpider(BaseParseSpider, Mixin):
         }
         currency_path = 'div.prod_block meta[itemprop=priceCurrency]::attr(' \
                         'content)'
-        hxs.meta['currency'] = hxs.css(currency_path).extract_first()
-        hxs.meta['common'] = common
-        hxs.meta['remaining_clr_req'] = hxs.css('div.col_sel li>'
+        response.meta['currency'] = response.css(currency_path).extract_first()
+        response.meta['common'] = common
+        response.meta['remaining_clr_req'] = response.css('div.col_sel li>'
                                                 'a::attr(href)').extract()
-        return self.parse_color_variant(hxs)
+        return self.parse_color_variant(response)
 
-    def parse_color_variant(self, hxs):
-        product_id = self.product_id(hxs)
+    def parse_color_variant(self, response):
+        product_id = self.product_id(response)
         garment = self.new_unique_garment(product_id)
-        self.boilerplate_minimal(garment, hxs)
-        garment.update(hxs.meta['common'])
-        garment['image_urls'] = self.image_urls(hxs)
-        skus = self.skus(hxs, product_id)
+        self.boilerplate_minimal(garment, response)
+        garment.update(response.meta['common'])
+        garment['image_urls'] = self.image_urls(response)
+        skus = self.skus(response, product_id)
         if skus:
             garment['skus'] = skus
         else:
             garment['out_of_stock'] = True
-        if hxs.meta['remaining_clr_req']:
-            next_req = hxs.meta['remaining_clr_req'].pop()
-            yield Request(next_req, meta=hxs.meta,
+        if response.meta['remaining_clr_req']:
+            # Request for the remaining colors of the product
+            next_req = response.meta['remaining_clr_req'].pop()
+            yield Request(next_req, meta=response.meta,
                           callback=self.parse_color_variant)
         yield garment
 
-    def product_id(self, hxs):
-        return hxs.css('div.prod_block div.visible-lg>p::text').extract_first(
+    def product_id(self, response):
+        return response.css('div.prod_block div.visible-lg>p::text').extract_first(
         ).split('.:')[1]
 
-    def product_title(self, hxs):
-        return hxs.css('h1.visible-lg[itemprop]::text')\
+    def product_title(self, response):
+        return response.css('h1.visible-lg[itemprop]::text')\
             .extract_first().split('-')
 
-    def product_description(self, hxs):
-        return hxs.css('div.prod_block div.visible-lg ul '
+    def product_description(self, response):
+        return response.css('div.prod_block div.visible-lg ul '
                        'label::text').extract()
 
-    def product_category(self, hxs):
-        return hxs.css('.detail_bread2>li>a>font::text').extract()
+    def product_category(self, response):
+        return response.css('.detail_bread2>li>a>font::text').extract()
 
     def product_gender(self, tokens):
         for token, gender in self.GENDER_MAP:
@@ -99,17 +93,17 @@ class SchuhcenterParseSpider(BaseParseSpider, Mixin):
         title_components = product_title.split('-')
         return title_components[len(title_components)-1]
 
-    def skus(self, hxs, product_id):
+    def skus(self, response, product_id):
         skus = {}
         common = {
-            'colour': self.product_color(hxs.meta['common']['name']),
-            'currency': hxs.meta['currency'],
+            'colour': self.product_color(response.meta['common']['name']),
+            'currency': response.meta['currency'],
         }
-        for size_var in hxs.css('div.size_info li>a'):
+        for size_var in response.css('div.size_info li>a'):
             size_id = size_var.css('::attr(data-selection-id)').extract_first()
             skus[str(product_id+size_id)] = sku = common.copy()
             sku['size'] = size_var.css('span::text').extract_first().strip()
-            previous_price, price, currency = self.product_pricing(hxs)
+            previous_price, price, currency = self.product_pricing(response)
             sku['price'] = price
             if previous_price:
                 sku['previous_prices'] = previous_price
@@ -119,9 +113,9 @@ class SchuhcenterParseSpider(BaseParseSpider, Mixin):
                 sku['out_of_stock'] = True
         return skus
 
-    def image_urls(self, hxs):
+    def image_urls(self, response):
         # Replace 87x87 image size with 380x340 for a full size image
-        return [img.replace('87_87', '380_340') for img in hxs.css(
+        return [img.replace('87_87', '380_340') for img in response.css(
             'div.detailsInfo div.otherPictures img::attr(src)').extract()]
 
 
