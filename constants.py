74c70eeae75551f0e447dc5467cabf757e13a20d
Shahbaz Nazir <shahbaz.nazir@arbisoft.com>
2016-10-19 15:59:00 +0500

    [WIP] weatherman app
     - need code refactoring
     - move helper functions to a seperate
       helper file
     - add exception classes for proper exception
       handling
    
    Signed-off-by: Shahbaz Nazir <shahbaz.nazir@arbisoft.com>

---
+++ constants.py
@@ -0,0 +1,19 @@
+#  This file contains all the constants
+#  reference legends to represent column
+#  number for specific datapoints in data
+#  files
+
+
+# This is an ordered list please do not change
+# order of elements
+MONTHS=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', \
+        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+# Key legend for data files
+# contains the column index for
+# each data point in the data 
+# set
+HI_TEMP = 1
+LO_TEMP = 3
+HUMID = 7
+DATE = 0
--- legend.py
+++
@@ -1,8 +0,0 @@
-#  Maintains key legend for data files
-#  contains the column index for each
-#  Data point in the data set
-
-HI_TEMP = 1
-LO_TEMP = 3
-HUMID = 7
-DATE = 0
