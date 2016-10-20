f13b3576f6efe0dfff3635f401ce548e7dd2772d
ahmedsuffianjaved <ahmed_suffian_javed@live.com>
2016-10-20 10:23:38 +0500

    CSS Selectors and SKU fetch logic updated

--- schuhcenter/schuhcenter_spider.py
+++ schuhcenter/schuhcenter_spider.py
@@ -30,7 +30,6 @@ class SchuhcenterParseSpider(BaseParseSpider, Mixin):
         self.boilerplate_normal(garment, response, response)
         garment['gender'] = self.product_gender(garment)
         garment['image_urls'] = self.image_urls(response)
-        response.meta['currency'] = self.currency(response)
         garment['skus'] = self.skus(response)
 
         if not garment['skus']:
@@ -50,11 +49,12 @@ class SchuhcenterParseSpider(BaseParseSpider, Mixin):
         return response.css(currency_css).extract_first()
 
     def color_requests(self, response):
-        request_urls = response.css('div.col_sel li>a::attr(href)').extract()
+        request_urls = response.css('.col_sel a::attr(href)').extract()
         return [Request(url, callback=self.parse) for url in request_urls]
 
     def product_id(self, response):
-        return response.css('div.visible-lg>p::text').extract_first().split('.:')[1]
+        return response.css('.visible-lg>p'
+                            '::text').extract_first().split('.:')[1]
 
     def product_title(self, response):
         return response.css('h1[itemprop=name]::text').extract_first()
@@ -74,32 +74,35 @@ class SchuhcenterParseSpider(BaseParseSpider, Mixin):
 
     def product_color(self, response):
         # product color is present after right most '-' character
-        prod_title = self.product_title(response).split('-')
-        return ''.join(prod_title[-1])
+        title = self.product_title(response).split('-')
+        return ''.join(title[-1])
 
     def skus(self, response):
         skus = {}
         common = {
             'colour': self.product_color(response),
-            'currency': response.meta['currency'],
+            'currency': self.currency(response),
         }
         for size_var in response.css('div.size_info li>a'):
-            size_id = size_var.css('::attr(data-selection-id)').extract_first()
-            skus[common['colour']+'_'+size_id] = sku = common.copy()
-            sku['size'] = size_var.css('span::text').extract_first().strip()
+            sku = common.copy()
             previous_price, price, currency = self.product_pricing(response)
             sku['price'] = price
+            sku['size'] = size_var.css('span::text').extract_first().strip()
 
             if previous_price:
-                sku['previous_prices'] = previous_price
+                sku['previous_prices'] = [previous_price]
             if size_var.css('.no_stock'):
                 sku['out_of_stock'] = True
+
+            size_id = size_var.css('::attr(data-selection-id)').extract_first()
+            skus[common['colour'] + '_' + size_id] = sku
 
         return skus
 
     def image_urls(self, response):
-        return [img.replace('87_87', '380_340') for img in response.css(
-            'div.otherPictures img::attr(src)').extract()]
+        css = '.otherPictures img::attr(src)'
+        return [i.replace('87_87', '380_340') for i in response.css(
+            css).extract()]
 
     def product_name(self, response):
         # product name is present between left-most and right-most '-' of
