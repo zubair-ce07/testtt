14c695bc0950290aec0796bc54bfa81b1ee0cfe8
arslan-khan <muhammad.arslan@arbisoft.com>
2016-10-26 15:32:33 +0500

    task 1 section 3 (removed ++ from index)

--- 02.py
+++ 02.py
@@ -40,7 +40,7 @@ for index, line in enumerate(f, start=1):
     lowest_temperature = int(columns[3]) if columns[3] != "" else None
     if highest_temperature is not None:
         print("\033[95m{}\033[0m \033[91m{}\033[0m \033[95m{}C\033[0m"
-              .format(++index, "+" * highest_temperature, highest_temperature))
+              .format(index, "+" * highest_temperature, highest_temperature))
     else:
         print("\033[95m{}\033[0m \033[91mDon't have any data to show on\033[0m \033[95m{}".format(index, columns[0]))
     if lowest_temperature is not None:
