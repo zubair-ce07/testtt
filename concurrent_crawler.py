d3c08a1816ad8c3a9c49e16f28d416c281ed20d4
rabia23 <rabia.iftikhar@arbisoft.com>
2016-10-26 16:41:08 +0500

    done with PR comments changes.

--- concurrent_crawler.py
+++ concurrent_crawler.py
@@ -2,55 +2,55 @@ __author__ = 'rabia'
 import requests
 from lxml import html
 import asyncio
+import argparse
 
 
-def generate_report(urls_data_list, total_bytes, total_requests):
+def generate_report(urls, total_bytes):
     print("\n\n---------------Report---------------------")
-    for item in urls_data_list:
-        print("url: " + item['url'])
-        print("page bytes: " + str(item['bytes']))
+    for item in urls:
+        print('{} page is {} bytes'.format(item['url'], item['bytes']))
 
-    print("\n\nTotal Requests made: " + str(total_requests))
-    print("Total Bytes downloaded: " + str(total_bytes))
+    total_requests = len(urls)
     average_size_of_page = total_bytes / total_requests
-    print("Average size of Page: " + str(average_size_of_page))
+
+    return '\n\n Total Requests made: {} \n Total Bytes downloaded: {} \n Average size of Page: {} '.\
+        format(total_requests, total_bytes, average_size_of_page)
 
 
-@asyncio.coroutine
-def get_url_data(url):
+async def get_url_data(url, download_delay):
     response = requests.get(url)
-    yield from asyncio.sleep(5.0)
+    await asyncio.sleep(download_delay)
     return response
 
 
-@asyncio.coroutine
-def concurrent_crawler():
-    urls_data_list = []
+
+async def concurrent_crawler(download_delay):
+    site_urls = []
     total_bytes = 0
-    total_requests = 0
 
     base_url = "https://en.wikipedia.org/wiki/Data_science"
     response = requests.get(base_url)
-    raw_string = html.fromstring(response.text)
-    urls = raw_string.xpath("/html/body//a[starts-with(@href,'/wiki/')]/@href")
+    raw_response = html.fromstring(response.text)
+    urls = raw_response.xpath("/html/body//a[starts-with(@href,'/wiki/')]/@href")
     urls_list = ['https://en.wikipedia.org' + url for url in urls]
 
     for url in urls_list[:20]:
-        print(url)
-        response = yield from get_url_data(url)
+        response = await get_url_data(url, download_delay)
         response_size = len(response.text)
-        total_requests += 1
         total_bytes += response_size
-        urls_data_list.append({'url': url, 'bytes': response_size})
+        site_urls.append({'url': url, 'bytes': response_size})
 
-    generate_report(urls_data_list, total_bytes, total_requests)
+    print(generate_report(site_urls, total_bytes))
 
 
-def main():
+def main(args):
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(concurrent_crawler())
+    loop.run_until_complete(concurrent_crawler(args.download_delay))
     loop.close()
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-download_delay', action="store", default=5, type=int, dest='download_delay',
+                        help='Download delay between each request')
+    main(parser.parse_args())
--- parallel_crawler.py
+++ parallel_crawler.py
@@ -3,58 +3,59 @@ import concurrent.futures
 import requests
 import time
 from lxml import html
+import argparse
 
-
-def generate_report(urls_data_list, total_bytes, total_requests, no_of_workers):
+def generate_report(urls, total_bytes, no_of_workers):
     print("\n---------------Report---------------------")
-    for item in urls_data_list:
-        print('%r page is %d bytes' % (item['url'], item['bytes']))
+    for item in urls:
+        print('{} page is {} bytes'.format(item['url'], item['bytes']))
 
-    print("\n\nTotal Requests made: " + str(total_requests))
-    print("Total Bytes downloaded: " + str(total_bytes))
+    total_requests = len(urls)
     average_size_of_page = total_bytes / total_requests
-    print("Average size of Page: " + str(average_size_of_page))
-    print("Max Workers: " + str(no_of_workers))
+
+    return '\n\n Total Requests made: {} \n Total Bytes downloaded: {} \n Average size of Page: {} \n Max Workers: {}'.\
+        format(total_requests, total_bytes, average_size_of_page, no_of_workers)
 
 
-def get_url_data(url):
-    response = requests.get(url)
-    time.sleep(2)
+def get_url_data(url, download_delay):
+    response = requests.get(url, download_delay)
+    time.sleep(int(download_delay))
     return response
 
 
-def parallel_crawler(urls_list, no_of_workers):
+def parallel_crawler(urls, no_of_workers, download_delay):
 
-    urls_data_list = []
+    site_urls = []
     total_bytes = 0
-    total_requests = 0
 
     with concurrent.futures.ProcessPoolExecutor(max_workers=no_of_workers) as executor:
-        future_to_url = {executor.submit(get_url_data, url): url for url in urls_list}
+        future_to_url = {executor.submit(get_url_data, url, download_delay): url for url in urls}
         for future in concurrent.futures.as_completed(future_to_url):
             url = future_to_url[future]
             response = future.result()
             response_size = len(response.text)
-            total_requests += 1
             total_bytes += response_size
-            urls_data_list.append({'url': url, 'bytes': response_size})
+            site_urls.append({'url': url, 'bytes': response_size})
 
-        generate_report(urls_data_list, total_bytes, total_requests, no_of_workers)
+        print(generate_report(site_urls, total_bytes, no_of_workers))
 
 
-def main():
-
-    no_of_workers = input('Enter the number of processes: ')
+def main(args):
 
     base_url = "https://en.wikipedia.org/wiki/Data_science"
     response = requests.get(base_url)
-    raw_string = html.fromstring(response.text)
-    urls = raw_string.xpath("/html/body//a[starts-with(@href,'/wiki/')]/@href")
-    urls_list = ['https://en.wikipedia.org' + url for url in urls]
-    urls_list = urls_list[:100]
+    raw_response = html.fromstring(response.text)
+    response_urls = raw_response.xpath("/html/body//a[starts-with(@href,'/wiki/')]/@href")
+    urls = ['https://en.wikipedia.org' + url for url in response_urls]
+    urls = urls[:100]
 
-    parallel_crawler(urls_list, int(no_of_workers))
+    parallel_crawler(urls, args.no_of_workers, args.download_delay)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-download_delay', action="store", default=5, type=str, dest='download_delay',
+                        help='Download delay between each request')
+    parser.add_argument('-no_of_workers', action="store", default=5, type=int, dest='no_of_workers',
+                        help='no. of workers for parallel requests')
+    main(parser.parse_args())
