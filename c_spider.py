29ae0137a8b57a96c9a09a5aab6b8cbdb4e45e53
Shahbaz Nazir <shahbaz.nazir@arbisoft.com>
2016-10-26 16:53:55 +0500

    [WIP] concurrent crawler  - do not reivew
    Signed-off-by: Shahbaz Nazir <shahbaz.nazir@arbisoft.com>

--- c_spider.py
+++ c_spider.py
@@ -7,18 +7,23 @@ from time import sleep
 
 class CSpider:
 
-    def __init__(self, domain, max_urls=50, delay=1 ):
+    def __init__(self, domain, workers=5, max_urls=50, delay=1 ):
         self.domain = domain.rstrip("/")
+        self.workers = workers
         self.max_urls = max_urls
         self.delay = delay
         self.size = 0
+        self.visited = set()
+        self.urls = urls = async.Queue()
+        self.tasks = []
+
+        self.init_workers()
 
     def crawl(self):
-        urls = Queue()
-        visited = set()
+        loop = asyncio.get_event_loop()
 
         urls.put(self.domain)
-        while not urls.empty() and len(visited) < self.max_urls:
+        while len(visited) < self.max_urls:
             url = urls.get()
             if url not in visited:
                 size, new_urls = self.visit(url)
@@ -30,17 +35,23 @@ class CSpider:
                         urls.put(new_url)
             sleep(self.delay)
 
+    def init_workers(self):
+        for i in range(self.workers):
+            self.task.append(visit('worker-' + str(i)))
+
+    @asyncio.coroutine
     def visit(self, url):
+        
         print(url)
         try:
             conn = requests.get(url, timeout=5)
             size = len(conn.text)
             urls = self.get_urls(conn.text)
-            #return (size, urls)
+            yield (size, urls)
         except Exception as exc:
             print("Warning: (%s) url skipped due to error" % url)
             print(str(exc))
-            #return (0, [])
+            yield (0, [])
 
     def get_urls(self, body):
         sel = Selector(text=body)
