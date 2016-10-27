aeb70a4e82e834a10275d4bc91ae14ad77ac632e
Shahbaz Nazir <shahbaz.nazir@arbisoft.com>
2016-10-27 17:17:51 +0500

    Adding concurrent spider
     - makes concurrent requests
     - external link handling needs to be
       improved
     - multiple workers need to be added
    
    Signed-off-by: Shahbaz Nazir <shahbaz.nazir@arbisoft.com>

--- c_spider.py
+++ c_spider.py
@@ -1,58 +1,52 @@
 import asyncio
 from parsel import Selector
 from queue import Queue
-import requests
-from time import sleep
+import aiohttp
 
 
-class CSpider:
+async def crawl(domain, max_urls=20, delay=1):
+    urls_q = asyncio.Queue()
+    visited = set()
+    size = 0
 
-    def __init__(self, domain, workers=5, max_urls=50, delay=1 ):
-        self.domain = domain.rstrip("/")
-        self.workers = workers
-        self.max_urls = max_urls
-        self.delay = delay
-        self.size = 0
-        self.visited = set()
-        self.urls = urls = async.Queue()
-        self.tasks = []
+    urls_q.put_nowait(domain)
+    while not urls_q.empty() or len(visited) > max_urls:
+        url = await urls_q.get()
+        if url not in visited:
+            visited.add(url)
+            url_size, new_urls = await visit(url)
 
-        self.init_workers()
+            size = size + url_size
+            for new_url in new_urls:
+                new_url = domain + new_url
+                if new_url not in visited:
+                    urls_q.put_nowait(new_url)
+        asyncio.sleep(delay)
 
-    def crawl(self):
-        loop = asyncio.get_event_loop()
 
-        urls.put(self.domain)
-        while len(visited) < self.max_urls:
-            url = urls.get()
-            if url not in visited:
-                size, new_urls = self.visit(url)
-                self.size = self.size + size                
-                visited.add(url)
-                for new_url in new_urls:
-                    new_url = self.domain + new_url
-                    if new_url not in visited:
-                        urls.put(new_url)
-            sleep(self.delay)
+async def visit(url):
+    print(url)
+    try:
+        resp = await aiohttp.request('GET', url)
+        body = await resp.read()
+        size = len(body)
+        urls = get_urls(body.decode('utf-8'))
+        return (size, urls)
+    except Exception as exc:
+        print("Warning: (%s) url skipped due to error" % url)
+        print(str(exc))
+        return (0, [])
 
-    def init_workers(self):
-        for i in range(self.workers):
-            self.task.append(visit('worker-' + str(i)))
 
-    @asyncio.coroutine
-    def visit(self, url):
-        
-        print(url)
-        try:
-            conn = requests.get(url, timeout=5)
-            size = len(conn.text)
-            urls = self.get_urls(conn.text)
-            yield (size, urls)
-        except Exception as exc:
-            print("Warning: (%s) url skipped due to error" % url)
-            print(str(exc))
-            yield (0, [])
+def get_urls(body):
+    sel = Selector(text=body)
+    return sel.xpath('//a//@href').extract()
 
-    def get_urls(self, body):
-        sel = Selector(text=body)
-        return sel.xpath('//a//@href').extract()
+if __name__ == "__main__":
+
+    domain = "https://www.arbisoft.com"
+
+    loop = asyncio.get_event_loop()
+    tasks = [crawl(domain)]
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
