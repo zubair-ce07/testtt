97d70ada22be562d1cc61cdf88f249e0d165f7a9
Sheraz Ishaq <sheraz_1991@yahoo.com>
2016-11-17 17:22:06 +0500

    calculations displayed

--- scrapper.py
+++ scrapper.py
@@ -7,7 +7,7 @@ def filter_invalid_urls(urls):
     return urls
 
 
-def recursively_extract_html(urls, origin, hits_limit, total_hits=0):
+def recursively_extract_html(urls, origin, hits_limit, page_lengths, total_hits=0):
     if urls and total_hits < hits_limit:
         target_link = urls.pop(0)
 
@@ -17,6 +17,7 @@ def recursively_extract_html(urls, origin, hits_limit, total_hits=0):
             target_link = origin + target_link
         response = requests.get(target_link)
 
+        page_lengths.append(len(response.content))
         total_hits += 1
         print("Total Hits: %s | %s" % (total_hits, target_link))
 
@@ -25,7 +26,7 @@ def recursively_extract_html(urls, origin, hits_limit, total_hits=0):
         page_links = parser.xpath("//a/@href").extract()
         urls += filter_invalid_urls(page_links)
 
-        recursively_extract_html(urls, origin, hits_limit, total_hits)
+        recursively_extract_html(urls, origin, hits_limit, page_lengths, total_hits)
 
 
 def main():
@@ -33,7 +34,12 @@ def main():
     url = "http://sfbay.craigslist.org/search/eby/jjj"
     hits_limit = 100
 
-    recursively_extract_html([url], origin, hits_limit)
+    page_lengths = []
+
+    recursively_extract_html([url], origin, hits_limit, page_lengths)
+
+    print("Total Requests: %s\nTotal Data Bytes: %s\nAverage Page Size: %s" % (len(page_lengths),sum(page_lengths),
+                                                                               sum(page_lengths) / len(page_lengths)))
 
 if __name__ == '__main__':
     main()
