27f444ae0295f80cfcdba99b138cec56989f1728
Sheraz Ishaq <sheraz_1991@yahoo.com>
2016-11-17 16:47:11 +0500

    simple recursive scrapping

--- scrapper.py
+++ scrapper.py
@@ -1,25 +1,39 @@
 import requests
 from parsel import Selector
-import pdb
 
 
-def recursively_extract_html(urls, hits_limit, total_hits = 0):
-    if urls and total_hits <= hits_limit:
-        target_link = urls[0]
+def filter_invalid_urls(urls):
+    urls = [url for url in urls if len(url) > 5]
+    return urls
+
+
+def recursively_extract_html(urls, origin, hits_limit, total_hits=0):
+    if urls and total_hits < hits_limit:
+        target_link = urls.pop(0)
+
+        if target_link.startswith("//"):
+            target_link = "http:" + target_link
+        if target_link.startswith("/"):
+            target_link = origin + target_link
         response = requests.get(target_link)
+
         total_hits += 1
+        print("Total Hits: %s | %s" % (total_hits, target_link))
 
         parser = Selector(response.text)
-        page_links = parser.css("body a")
 
-        recursively_extract_html(page_links, hits_limit, total_hits)
+        page_links = parser.xpath("//a/@href").extract()
+        urls += filter_invalid_urls(page_links)
+
+        recursively_extract_html(urls, origin, hits_limit, total_hits)
 
 
 def main():
-    url = "http://stackoverflow.com/"
+    origin = "http://sfbay.craigslist.org"
+    url = "http://sfbay.craigslist.org/search/eby/jjj"
     hits_limit = 100
 
-    recursively_extract_html([url], hits_limit)
+    recursively_extract_html([url], origin, hits_limit)
 
 if __name__ == '__main__':
     main()
