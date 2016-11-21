8c49ea210b17f4604d178cf8048711306e745427
Sheraz Ishaq <sheraz_1991@yahoo.com>
2016-11-21 13:02:14 +0500

    task-2, concurrent part

--- concurrent_scrapper.py
+++ concurrent_scrapper.py
@@ -1,7 +1,8 @@
 from parsel import Selector
-from time import sleep
 from collections import namedtuple
 import aiohttp
+import asyncio
+import time
 
 
 def filter_invalid_urls(urls):
@@ -19,23 +20,33 @@ def fix_url_path(url, origin):
 
 
 async def hit_target_link(url, config):
-    sleep(config.download_delay)
+    await asyncio.sleep(config.download_delay)
 
     print("Hit : %s" % url)
-    response = await aiohttp.ClientSession.get(url)
+    response = await aiohttp.get(url=url)
+    return await response.text()
 
-    return await response
+
+def extract_pages_link(html):
+    parser = Selector(text=html)
+    page_links = parser.xpath("//a/@href").extract()
+    urls_collection = filter_invalid_urls(page_links)
+
+    return urls_collection
 
 
-def recursively_extract_html(urls_collection, origin, results_collections, config):
+async def recursively_extract_html(urls_collection, origin, results_collections, config):
     for target_link in urls_collection:
         if results_collections.number_of_hits >= config.hits_limit:
             break
 
         target_link = fix_url_path(target_link, origin)
-        response = hit_target_link(target_link, config)
-
+        html = await hit_target_link(target_link, config)
         results_collections.number_of_hits += 1
+
+        new_urls_collection = extract_pages_link(html)
+        await results_collections.event_loop.create_task(recursively_extract_html(new_urls_collection, origin,
+                                                                                  results_collections, config))
 
 
 def main():
@@ -46,20 +57,14 @@ def main():
     config.download_delay = 2
     config.hits_limit = 5
 
-    results_collections = namedtuple("Results", "responses_collection number_of_hits futures_collection processed_urls")
+    results_collections = namedtuple("Results", "responses_collection number_of_hits event_loop processed_urls")
     results_collections.responses_collection = []
+    results_collections.event_loop = asyncio.get_event_loop()
     results_collections.number_of_hits = 0
 
     urls_collection = [url]
 
-    recursively_extract_html(urls_collection, origin, results_collections, config)
+    results_collections.event_loop.run_until_complete(recursively_extract_html(urls_collection, origin,
+                                                                               results_collections, config))
 
-    # total_requests = len(results_collections.responses_collection)
-    # content_size_collection = sum([len(response.content) for response in results_collections.responses_collection])
-    #
-    # print("Total Requests: %s\nTotal Data Bytes: %s\nAverage Page Size: %s" %
-    #       (total_requests, content_size_collection, content_size_collection / total_requests))
-
-
-if __name__ == '__main__':
-    main()
+main()
