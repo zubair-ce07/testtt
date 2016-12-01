a20f6a5cf49297b290bb99f8620ed292b02b98b2
Sheraz Ishaq <sheraz_1991@yahoo.com>
2016-12-01 20:03:03 +0500

    fix one size and out of stock issue

--- jennyfer/jennyfer_spider.py
+++ jennyfer/jennyfer_spider.py
@@ -4,7 +4,8 @@ from scrapy.http import Request
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Rule
 
-from .base import BaseParseSpider, BaseCrawlSpider, Garment, clean
+from .base import BaseParseSpider, BaseCrawlSpider, clean
+from titlecase import titlecase
 
 
 class Mixin(object):
@@ -28,9 +29,7 @@ class JennyferParseSpider(BaseParseSpider, Mixin):
             return
 
         self.boilerplate_normal(garment, response, response)
-        merch_info = self.product_merch(response)
-        if merch_info:
-            garment['merch_info'] = merch_info
+        garment['merch_info'] = self.merch_info(response)
 
         garment['skus'] = {}
         garment['image_urls'] = []
@@ -53,14 +52,14 @@ class JennyferParseSpider(BaseParseSpider, Mixin):
         colour = self.sku_colour(response)
         sku_common['colour'] = colour
 
-        size_variations = self.sku_sizes(response)
-
         skus = {}
-        for size in size_variations:
+        size_variations = self.sku_sizes(response)
+        for size_data in size_variations:
             sku = sku_common.copy()
-            sku['size'] = size
+            sku['size'] = size_data['size']
+            sku['out_of_stock'] = size_data['out_of_stock']
 
-            skus[colour + '_' + size] = sku
+            skus[colour + '_' + size_data['size']] = sku
 
         return skus
 
@@ -82,11 +81,11 @@ class JennyferParseSpider(BaseParseSpider, Mixin):
 
     def product_name(self, response):
         css = '#product-content .product-name::text'
-        return clean(response.css(css))[0]
+        return titlecase(clean(response.css(css))[0])
 
     def product_category(self, response):
         css = '.breadcrumb a span::text'
-        return clean(response.css(css))
+        return clean(response.css(css)[1:])
 
     def product_description(self, response):
         css = '.pdpForm div[itemprop="description"]::text'
@@ -97,14 +96,26 @@ class JennyferParseSpider(BaseParseSpider, Mixin):
         return clean(response.css(css))
 
     def sku_sizes(self, response):
-        css = '.variation-size .emptyswatch div:not(.unselected)::text'
-        return clean(response.css(css))
+        sizes = []
+        css = '.variation-size .emptyswatch div'
+        size_options = response.css(css)
+
+        for size in size_options:
+            data = {}
+            size_text = clean(size.xpath('text()'))[0]
+            out_of_stock_text = clean(size.xpath('@class'))[0]
+
+            data['size'] = self.one_size if size_text == 'TU' else size_text
+            data['out_of_stock'] = True if 'unselected' in out_of_stock_text else False
+            sizes.append(data)
+
+        return sizes
 
     def sku_colour(self, response):
         xpath = '//div[contains(@class, "variation-color")]//a[child::img[@class="selected"]]//@title'
         return clean(response.xpath(xpath))[0]
 
-    def product_merch(self, response):
+    def merch_info(self, response):
         css = '.product-image-container .flag .tag::text'
         return clean(response.css(css))
 
@@ -129,6 +140,6 @@ class JennyferCrawlSpider(BaseCrawlSpider, Mixin):
     ]
 
     rules = (
-        Rule(LinkExtractor(restrict_css=products_css), callback='parse_item'),
+        Rule(LinkExtractor(restrict_css=products_css), callback='parse'),
         Rule(LinkExtractor(restrict_css=listings_css, allow=allow_r)),
     )
