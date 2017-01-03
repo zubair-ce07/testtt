65b3d73bc0915a9445a4f05f18dbe0902c24ff7e
= <=>
2017-01-03 21:23:29 +0500

    initial commit of whitestuff crawler

---
+++ whitestuff/whitestuff/items.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class WhitestuffItem(scrapy.Item):
+    category = scrapy.Field()
+    lang = scrapy.Field()
+    trail = scrapy.Field()
+    gender = scrapy.Field()
+    brand = scrapy.Field()
+    url = scrapy.Field()
+    industry = scrapy.Field()
+    crawl_start_time = scrapy.Field()
+    name = scrapy.Field()
+    description = scrapy.Field()
+    care = scrapy.Field()
+    retailer_sku = scrapy.Field()
+    currency = scrapy.Field()
+    date = scrapy.Field()
+    skus = scrapy.Field()
+    image_urls = scrapy.Field()
+    spider_name = scrapy.Field()
+    market = scrapy.Field()
+    crawl_id = scrapy.Field()
+    retailer = scrapy.Field()
+    price = scrapy.Field()
+    url_original = scrapy.Field()
+    uuid = scrapy.Field()
---
+++ whitestuff/whitestuff/spiders/whitestuff_spider.py
@@ -0,0 +1,142 @@
+import json
+import re
+from scrapy.spiders.crawl import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.http.request import Request
+from whitestuff.items import WhitestuffItem
+from urllib.parse import urlencode
+
+
+class Spider(CrawlSpider):
+    name = "whitestuff2"
+    allowed_domains = ["whitestuff.com", 'fsm.attraqt.com']
+    start_urls = [
+        "http://whitestuff.com/",
+    ]
+
+    rules = (
+        Rule(LinkExtractor(allow=()), callback='parse_items'),
+
+    )
+
+    def parse_items(self, response):
+        ajax_params = self.parse_ajax_params(response)
+
+        if ajax_params['config_page'] == 'category listing':
+            return self.process_category_listing(ajax_params)
+
+        if ajax_params['config_page'] == 'product detail':
+            return self.parse_item(response)
+
+    def process_category_listing(self, params):
+        params['zone0'] = 'category'
+        url = 'http://fsm.attraqt.com/zones-js.aspx?'
+        url += urlencode(params)
+        yield Request(url=url, callback=self.parse_category_listing)
+
+    def parse_category_listing(self, response):
+        product_links = response.css('ul[id*=prodListing] > li > span[id*=link]::text').extract()
+        links = [Request(url='http://www.whitestuff.com' + link, callback=self.parse_item) for link in product_links]
+        for link in links:
+            yield link
+
+    def parse_ajax_params(self, response):
+        params = {}
+        script = response.css('script:contains("LM.config")')
+        params['version'] = '16.6.15'
+        params['config_region'] = script.re_first(r'LM.config\("region",\s*"(.*)"\)')
+        params['config_currency'] = script.re_first(r'LM.config\("currency",\s*"(.*)"\)')
+        params['config_page'] = script.re_first(r'LM.config\("page",\s*"(.*)"\)')
+        params['config_categorytree'] = script.re_first(r'LM.config\("categorytree",\s*"(.*)"\)')
+        params['config_category'] = script.re_first(r'LM.config\("category",\s*"(.*)"\)')
+        params['useragent'] = script.re_first(r'LM.config\("useragent",\s*"(.*)"\)')
+        params['siteid'] = response.css('script[src ^= "//fsm.attraqt.com/zones/"]::attr(src)').re_first(
+            '.*/zones/(.*).js')
+        params['sku'] = script.re_first(r'LM.Sku\s*=\s*\'([^\']*)\';?')
+        params['pageurl'] = response.url
+        return params
+
+    def parse_item(self, response):
+        item = WhitestuffItem()
+        item['retailer'] = 'whitestuff'
+        item['market'] = 'UK'
+        item['lang'] = 'en'
+        item['gender'] = self.product_gender(response)
+        item['name'] = self.product_name(response)
+        item['brand'] = "whitestuff"
+        item['description'] = self.product_description(response)
+        item['category'] = self.product_category(response)
+        item['url'] = response.url
+        item['industry'] = None
+        item['currency'] = self.product_currency(response)
+        item['image_urls'] = self.product_images(response)
+        item['spider_name'] = self.name
+        item['price'] = self.product_price(response)
+        item['url_original'] = response.url
+        item['care'] = self.product_care(response)
+        item['skus'] = self.get_skus(response)
+        item['retailer_sku'] = self.retailer_sku(response)
+
+        return item
+
+    def product_gender(self, response):
+        return response.css('meta[property="product:gender"]::attr(content)') \
+            .extract_first()
+
+    def product_description(self, response):
+        return response.css('div.content > div:first-child::text').extract()
+
+    def product_currency(self, response):
+        return response.css('meta[property="product:price:currency"]::attr(content)') \
+            .extract_first()
+
+    def product_price(self, response):
+        return int(response
+                   .css('meta[property="product:price:amount"]::attr(content)')
+                   .extract_first().replace('.', ''))
+
+    def product_care(self, response):
+        return response.css('div.content > div:first-child::text') \
+            .re_first('^Care:[\s]*(.*)$')
+
+    def product_color(self, response):
+        return response.css('meta[itemprop="color"]::attr(content)').re_first('.+')
+
+    def get_skus(self, response):
+        script = response.css('script:contains("var variants")::text').extract_first()
+        script = re.sub(r'[\s]', ' ', script)
+        variants = re.search(r'var variants = (\{.+\})\s*var img', script).group(1)
+        variants = json.loads(variants)
+        currency = self.product_currency(response)
+        skus = {}
+        for variant in variants:
+            sku = {}
+            sku['color'] = variants[variant]['option1']
+            sku['size'] = variants[variant]['option2']
+            sku['price'] = int(variants[variant]['line_price'].replace('.', ''))
+            sku['currency'] = currency
+
+            skus[variants[variant]['sku']] = sku
+        return skus
+
+    def retailer_sku(self, response):
+        return response.css('script:contains("productId:")').re_first("productId:[\s]*'(.*)'")
+
+    def product_images(self, response):
+        script_elem = response.css('script:contains("var params")::text')
+        script_text = re.sub('[\s]', ' ', script_elem.extract_first())
+        json_obj = re.search(r'var imgItems = (\{[\s\d\w_:#\.\/\-,"]*\})\s*\/\/REM', script_text).group(1)
+        image_prefix = re.search(r'\'large_img\'\),\s*\"prefix\":\"([\w.:\/-]*)\"', script_text).group(1)
+        json_obj = json.loads(json_obj)
+        image_names = []
+        for key in json_obj:
+            image_names += json_obj[key].rstrip('#').lstrip('#').split('#')
+
+        return [image_prefix + name for name in image_names]
+
+    def product_name(self, response):
+        return response.css('meta[itemprop="name"]::attr(content)').re_first('.+')
+
+    def product_category(self, response):
+        return response.css('div#crumb > span:last-child meta[itemprop="title"]::attr(content)') \
+            .extract()
