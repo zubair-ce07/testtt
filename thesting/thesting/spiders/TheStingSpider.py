e87b98bab56d68025beb52bb88b75f7a04311a4f
= <=>
2017-01-09 17:36:11 +0500

    picks old price in case of sale, changed male/female to men/women

--- thesting/thesting/spiders/TheStingSpider.py
+++ thesting/thesting/spiders/TheStingSpider.py
@@ -10,7 +10,7 @@ class ProductLinkExtractor(LinkExtractor):
     def extract_links(self, response):
         script = response.css('div.listings > script')
         links = script.re('\"urlProductDetailPage\":\s*\"(.*)\"')
-        return [Link(url='http://www.thesting.com/'+link) for link in links]
+        return [Link(url='http://www.thesting.com/' + link) for link in links]
 
 
 class TheStingSpider(CrawlSpider):
@@ -19,7 +19,7 @@ class TheStingSpider(CrawlSpider):
         'http://www.thesting.com/en-gb/'
     ]
 
-    allowed_domains =[
+    allowed_domains = [
         'www.thesting.com'
     ]
 
@@ -28,7 +28,7 @@ class TheStingSpider(CrawlSpider):
     ]
 
     def parse_list(self, response):
-        gender = response.css('script:contains("pageAffinity")')\
+        gender = response.css('script:contains("pageAffinity")') \
             .re_first('\"pageAffinity\": \"(.+)\"')
 
         for link in ProductLinkExtractor().extract_links(response):
@@ -40,16 +40,17 @@ class TheStingSpider(CrawlSpider):
         item['retailer_sku'] = script.re_first('(?<="genericCode":)\s*\"(.*)\"')
         item['url'] = response.url
         item['name'] = script.re_first('(?<="productName":)\s*\"(.*)\"')
-        item['category'] = script.re_first('(?<="productCategory":)\s*\"(.*)\"')
+        item['category'] = script.re('(?<="productCategory":)\s*\"(.*)\"')
         item['brand'] = script.re_first('(?<="brandName":)\s\"(.*)\"')
         item['description'] = response.css('meta[name="description"]::attr(content)').extract_first()
         item['currency'] = self.product_currency(script)
-        item['gender'] = response.meta['gender']
+        item['gender'] = self.product_gender(response)
         item['image_urls'] = self.get_image_urls(response)
         item['spider_name'] = self.name
         item['retailer'] = 'thesting'
-        item['price'] = self.product_price(script)
+        item['price'] = self.product_price(response)
         item['url_original'] = response.url
+        item['care'] = self.product_care(response)
         color_info = self.get_color_info(response)
         item['skus'] = {}
         return self.get_skus(item, color_info)
@@ -63,9 +64,9 @@ class TheStingSpider(CrawlSpider):
     def get_skus(self, item, color_info):
         if color_info:
             name, url = color_info.pop()
-            return Request(url='http://www.thesting.com/'+url,
+            return Request(url='http://www.thesting.com/' + url,
                            callback=self.parse_color_sku,
-                           meta = {
+                           meta={
                                'item': item,
                                'color_name': name,
                                'color_info': color_info,
@@ -75,16 +76,14 @@ class TheStingSpider(CrawlSpider):
     def parse_color_sku(self, response):
         item = response.meta['item']
         color_info = response.meta['color_info']
-
         script = response.css('div#overlay-mask + script')
         color = response.meta['color_name']
-        price = self.product_price(script)
+        price = self.product_price(response)
         currency = self.product_currency(script)
-
         sizes = self.product_sizes(response)
         for size in sizes:
-            variantCode = size[0]
-            item['skus'][variantCode] = {
+            variant_code = size[0]
+            item['skus'][variant_code] = {
                 'color': color,
                 'size': size[1],
                 'price': price,
@@ -95,21 +94,40 @@ class TheStingSpider(CrawlSpider):
 
     def get_color_info(self, response):
         script_elem = response.css('div#overlay-mask + script::text')
-        allColors_raw = script_elem.re_first('(?<="allColors":)\s*(\[[^\]]*\])')
-        allColors_json = json.loads(allColors_raw)
+        all_colors_raw = script_elem.re_first('(?<="allColors":)\s*(\[[^\]]*\])')
+        all_colors_json = json.loads(all_colors_raw)
         return [(color_obj['name'], color_obj['productPageColorUrl'])
-                for color_obj in allColors_json]
+                for color_obj in all_colors_json]
 
-    def product_price(self, script):
-        return int(script.re_first('(?<="price":)[\s"]*([\d\.]*)').replace('.',''))
+    def product_price(self, response):
+        script = response.css('script:contains("saleStatus")::text')
+        sale_status = int(script.re_first('(?<=\"saleStatus\":) (\d+)'))
+        if sale_status:
+            return int(script.re_first('(?<="fromPrice":)[\s"]*([\d\.]*)').replace('.', ''))
+        else:
+            return int(script.re_first('(?<="price":)[\s"]*([\d\.]*)').replace('.', ''))
 
     def product_currency(self, script):
         return script.re_first('(?<="currency":)[\s"]*([^:ascii:])')
 
+    def product_care(self, response):
+        script= response.css('div#overlay-mask + script::text')
+        care_raw = script.re_first('(?<="productFabrics":)\s*(\[[^\]]*\])')
+        care_json = json.loads(care_raw)
+        return [care['value'] for care in care_json]
+
+    def product_gender(self, response):
+        script = response.css('script:contains("pageAffinity")')
+        affinity = script.re_first('(?<="pageAffinity":) "(.*)"')
+        if affinity.lower() == 'male':
+            return 'men'
+        elif affinity.lower() == 'female':
+            return 'women'
+
     def product_sizes(self, response):
         script_elem = response.css('div#overlay-mask + script::text')
-        availableSizes_raw = script_elem.re_first('(?<="availableSizes":)\s*(\[[^\]]*\])')
-        availableSizes_json = json.loads(availableSizes_raw)
+        available_sizes_raw = script_elem.re_first('(?<="availableSizes":)\s*(\[[^\]]*\])')
+        available_sizes_json = json.loads(available_sizes_raw)
         return [(size['variantCode'], size['name'])
-                for size in availableSizes_json
+                for size in available_sizes_json
                 if size['available']]
