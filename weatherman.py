e13a19797f1682b5af1ccf3e24f20c9faf01a78b
AdnanAsif <adnan.asif@arbisoft.com>
2017-02-08 19:22:07 +0500

    issues resolved.

--- weatherman.py
+++ weatherman.py
@@ -5,21 +5,46 @@ in text files
 
 import csv
 import argparse
+from pathlib import Path
 from functools import reduce
 from datetime import datetime
-from os import path as os_path
+from re import compile as re_compile
+from os import path as os_path, listdir as os_listdir
 from sys import argv as sys_argv, exit as sys_exit
 
 
-class Bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
+class TerminalColors:
+    PURPLE = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    RESET = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+
+
+class ReadCSVFile:
+    def __init__(self, file_name, file_dir='', field_names=None, skip_first_line=True):
+        self.file_path = os_path.join(file_dir, file_name)
+        self.file_data = []
+        self.field_names = field_names
+        self.skip_first_line = skip_first_line
+        if not Path(self.file_path).is_file():
+            raise FileNotFoundError('No such file: {}'.format(self.file_path))
+
+    def read_file(self):
+        f = open(self.file_path, 'r')
+        reader = csv.DictReader(f, fieldnames=self.field_names)
+
+        if self.skip_first_line:
+            next(reader)
+
+        for line in reader:
+            self.file_data.append(line)
+        f.close()
+
+        return self.file_data
 
 
 def compare_numbers(a, b):
@@ -40,6 +65,34 @@ def print_yearly_report(results, months):
     print('Humidity: {:.2f}% on {}'.format(value, get_day_name(date, months)))
 
 
+def yearly_computation(result, row):
+    date = row['date']
+    max_temperature = row['max_temperature']
+    min_temperature = row['min_temperature']
+    max_humidity = row['max_humidity']
+
+    if max_temperature:
+        max_temperature = int(max_temperature)
+        if result['max_temp_value'] is None or \
+                compare_numbers(max_temperature, result['max_temp_value']) > 0:
+            result['max_temp_value'] = max_temperature
+            result['max_temp_date'] = date
+    if min_temperature:
+        min_temperature = int(min_temperature)
+        if result['min_temp_value'] is None or \
+                compare_numbers(result['min_temp_value'], min_temperature) > 0:
+            result['min_temp_value'] = min_temperature
+            result['min_temp_date'] = date
+    if max_humidity:
+        max_humidity = int(max_humidity)
+        if result['max_humidity_value'] is None or \
+                compare_numbers(max_humidity, result['max_humidity_value']) > 0:
+            result['max_humidity_value'] = max_humidity
+            result['max_humidity_date'] = date
+
+    return result
+
+
 def calculate_yearly_report(files_data):
     results = {
         'max_temp_value': None,
@@ -50,65 +103,46 @@ def calculate_yearly_report(files_data):
         'max_humidity_date': None
     }
 
-    def computation(result, row):
-        date = row['date']
-        max_temperature = row['max_temperature']
-        min_temperature = row['min_temperature']
-        max_humidity = row['max_humidity']
-
-        if max_temperature:
-            max_temperature = int(max_temperature)
-            if result['max_temp_value'] is None or \
-                    compare_numbers(max_temperature, result['max_temp_value']) > 0:
-                result['max_temp_value'] = max_temperature
-                result['max_temp_date'] = date
-        if min_temperature:
-            min_temperature = int(min_temperature)
-            if result['min_temp_value'] is None or \
-                    compare_numbers(result['min_temp_value'], min_temperature) > 0:
-                result['min_temp_value'] = min_temperature
-                result['min_temp_date'] = date
-        if max_humidity:
-            max_humidity = int(max_humidity)
-            if result['max_humidity_value'] is None or \
-                    compare_numbers(max_humidity, result['max_humidity_value']) > 0:
-                result['max_humidity_value'] = max_humidity
-                result['max_humidity_date'] = date
-
-        return result
-
     for data in files_data:
-        results = reduce(computation, data, results)
+        results = reduce(yearly_computation, data, results)
 
     return results
 
 
-def generate_yearly_report(dir_path, query_str, static_values):
+def categorise_location_files_of_year(year):
+    def categorise_location_files(result, file_name):
+        location = file_name.split(year)[0]
+        if location not in result:
+            result[location] = []
+
+        result[location].append(file_name)
+        return result
+
+    return categorise_location_files
+
+
+def generate_yearly_report(dir_path, year, static_values):
     file_columns = static_values['file_columns']
     months = static_values['months']
-    file_list = generate_file_names(query_str, static_values)
+    file_names = select_file_names(year, static_values)
+    categorise_location_files = categorise_location_files_of_year(year)
+    location_files = reduce(categorise_location_files, file_names, {})
 
-    files_data = []
-    for file_name in file_list:
-        file_path = os_path.join(dir_path, file_name)
+    for location in location_files:
+        print(location.rstrip('_'))
+        files_data = []
+        file_list = location_files[location]
+        for file_name in file_list:
+            try:
+                csv_file = ReadCSVFile(file_name, dir_path, file_columns)
+                file_data = csv_file.read_file()
+            except (FileNotFoundError, IOError):
+                continue
 
-        try:
-            f = open(file_path, 'r')
-        except FileNotFoundError:
-            continue
+            files_data.append(file_data)
 
-        reader = csv.DictReader(f, fieldnames=file_columns)
-        next(reader)
-
-        file_data_rows = []
-        for line in reader:
-            file_data_rows.append(line)
-
-        f.close()
-        files_data.append(file_data_rows)
-
-    results = calculate_yearly_report(files_data)
-    print_yearly_report(results, months)
+        results = calculate_yearly_report(files_data)
+        print_yearly_report(results, months)
 
 
 def print_monthly_report(results):
@@ -121,6 +155,24 @@ def print_monthly_report(results):
     print('Average Mean Humidity: {:.2f}%'.format(avg_mean_humidity))
 
 
+def monthly_computation(result, row):
+    max_temperature = row['max_temperature']
+    min_temperature = row['min_temperature']
+    mean_humidity = row['mean_humidity']
+
+    if max_temperature:
+        result['max_temp_sum'] += int(max_temperature)
+        result['max_temp_count'] += 1
+    if min_temperature:
+        result['min_temp_sum'] += int(min_temperature)
+        result['min_temp_count'] += 1
+    if mean_humidity:
+        result['mean_humidity_sum'] += int(mean_humidity)
+        result['mean_humidity_count'] += 1
+
+    return result
+
+
 def calculate_monthly_report(data_rows):
     initial_values = {
         'max_temp_sum': 0,
@@ -131,71 +183,45 @@ def calculate_monthly_report(data_rows):
         'mean_humidity_count': 0
     }
 
-    def computation(result, row):
-        max_temperature = row['max_temperature']
-        min_temperature = row['min_temperature']
-        mean_humidity = row['mean_humidity']
-
-        if max_temperature:
-            result['max_temp_sum'] += int(max_temperature)
-            result['max_temp_count'] += 1
-        if min_temperature:
-            result['min_temp_sum'] += int(min_temperature)
-            result['min_temp_count'] += 1
-        if mean_humidity:
-            result['mean_humidity_sum'] += int(mean_humidity)
-            result['mean_humidity_count'] += 1
-
-        return result
-
-    return reduce(computation, data_rows, initial_values)
+    return reduce(monthly_computation, data_rows, initial_values)
 
 
 def generate_monthly_report(dir_path, query_str, static_values):
     file_columns = static_values['file_columns']
+    file_names = select_file_names(query_str, static_values)
+    if not file_names:
+        sys_exit('Data files not available for: %s' % query_str)
 
-    file_name = generate_file_names(query_str, static_values)[0]
-    file_path = os_path.join(dir_path, file_name)
+    for file_name in file_names:
+        print(os_path.splitext(file_name)[0])
+        try:
+            csv_file = ReadCSVFile(file_name, dir_path, file_columns)
+            file_data = csv_file.read_file()
+        except (FileNotFoundError, IOError) as e:
+            print(str(e))
+            return
 
-    try:
-        f = open(file_path, 'r')
-    except FileNotFoundError:
-        error_msg = 'file not available: {}'.format(file_path)
-        print(error_msg)
-        return
-
-    file_data_rows = []
-    reader = csv.DictReader(f, fieldnames=file_columns)
-    next(reader)
-    for line in reader:
-        file_data_rows.append(line)
-    f.close()
-
-    results = calculate_monthly_report(file_data_rows)
-    print_monthly_report(results)
+        results = calculate_monthly_report(file_data)
+        print_monthly_report(results)
 
 
-def generate_file_names(query_str, static_values):
+def select_file_names(query_str, static_values):
     months = static_values['months']
-    file_prefix = static_values['file_prefix']
-    file_ext = static_values['data_file_ext']
+    file_names = static_values['file_names']
 
     lst = query_str.split('/')
     size = len(lst)
-    file_list = []
+    regex = r'\w+'
 
-    if size == 1:
-        year = lst[0]
-        for m in months:
-            file_list.append(
-                file_prefix + year + '_' + m[:3] + file_ext)
-    elif size == 2:
-        year = lst[0]
+    year = lst[0]
+    regex += year
+
+    if size == 2:
         month = int(lst[1])
-        file_list.append(
-            file_prefix + year + '_' + months[month-1][:3] + file_ext)
+        regex += '_' + months[month-1][:3]
 
-    return file_list
+    r = re_compile(regex)
+    return list(filter(r.match, file_names))
 
 
 def get_day_name(date, months):
@@ -210,14 +236,14 @@ def get_day_name(date, months):
 
 def draw_chart(value, color):
     if color == 'red':
-        use_color = Bcolors.FAIL
+        use_color = TerminalColors.RED
     elif color == 'blue':
-        use_color = Bcolors.OKBLUE
+        use_color = TerminalColors.BLUE
     else:
-        use_color = Bcolors.ENDC
+        use_color = TerminalColors.RESET
 
     text = '{}'.format('+' * value)
-    print(use_color + text + Bcolors.ENDC, end='')
+    print(use_color + text + TerminalColors.RESET, end='')
 
 
 def two_row_chart(day_no, max_t, min_t):
@@ -256,31 +282,32 @@ def print_monthly_bar_chart(data_rows, chart_type):
 
 def monthly_bar_chart(dir_path, query_str, chart_type, static_values):
     file_columns = static_values['file_columns']
-    months = static_values['months']
+    file_names = select_file_names(query_str, static_values)
+    if not file_names:
+        sys_exit('Data files not available for: %s' % query_str)
 
-    file_name = generate_file_names(query_str, static_values)[0]
-    file_path = os_path.join(dir_path, file_name)
+    for file_name in file_names:
+        print(os_path.splitext(file_name)[0])
+        try:
+            csv_file = ReadCSVFile(file_name, dir_path, file_columns)
+            file_data = csv_file.read_file()
+        except (FileNotFoundError, IOError) as e:
+            print(str(e))
+            continue
+
+        print_monthly_bar_chart(file_data, chart_type)
+
+
+def get_files_in_dir(dir_path):
+    def check_is_file(file_name):
+        return os_path.isfile(os_path.join(dir_path, file_name))
 
     try:
-        f = open(file_path, 'r')
+        files = os_listdir(dir_path)
     except FileNotFoundError:
-        error_msg = 'file not available: {}'.format(file_path)
-        print(error_msg)
-        return
-
-    reader = csv.DictReader(f, fieldnames=file_columns)
-    next(reader)
-
-    lst = query_str.split('/')
-    m = int(lst[1])-1
-    print('{} {}'.format(months[m], lst[0]))
-
-    file_data_rows = []
-    for line in reader:
-        file_data_rows.append(line)
-
-    f.close()
-    print_monthly_bar_chart(file_data_rows, chart_type)
+        print('No such directory: %s' % dir_path)
+    else:
+        return list(filter(check_is_file, files))
 
 
 def generate_report(dir_path, key, value, static_values):
@@ -294,18 +321,23 @@ def generate_report(dir_path, key, value, static_values):
         monthly_bar_chart(dir_path, value, 'single', static_values)
 
 
-def check_valid_year(year):
+def valid_text(text, pattern, error_msg):
     try:
-        datetime.strptime(year, '%Y')
+        datetime.strptime(text, pattern)
     except ValueError:
-        sys_exit('Invalid or out of range year(usage: yyyy): %s' % year)
+        raise argparse.ArgumentTypeError(error_msg)
+    else:
+        return text
+
+
+def valid_year(year):
+    msg = 'Invalid or out of range year(usage: yyyy): %s' % year
+    return valid_text(year, '%Y', msg)
 
 
 def valid_year_month(text):
-    try:
-        datetime.strptime(text, '%Y/%m')
-    except ValueError:
-        sys_exit('Invalid of out of range year/month(usage: yyyy/mm): %s' % text)
+    msg = 'Invalid of out of range year/month(usage: yyyy/mm): %s' % text
+    return valid_text(text, '%Y/%m', msg)
 
 
 def argument_order(args):
@@ -318,32 +350,19 @@ def argument_order(args):
     return lst
 
 
-def validate_arguments(args):
-    if args.e:
-        check_valid_year(args.e)
-    if args.a:
-        valid_year_month(args.a)
-    if args.c:
-        valid_year_month(args.c)
-    if args.c1:
-        valid_year_month(args.c1)
-
-
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('dir_path', type=str, help='data files directory path')
-    parser.add_argument('-e', type=str, help='usage: -e yyyy')
-    parser.add_argument('-a', type=str, help='usage: -a yyyy/mm')
-    parser.add_argument('-c', type=str, help='usage: -c yyyy/mm')
-    parser.add_argument('-c1', type=str, help='usage: -c1 yyyy/mm')
+def parse_arguments(usage_msg):
+    parser = argparse.ArgumentParser(usage=usage_msg)
+    parser.add_argument('directory_path', type=str, help='data files directory path')
+    parser.add_argument('-e', type=valid_year, help='usage: -e yyyy')
+    parser.add_argument('-a', type=valid_year_month, help='usage: -a yyyy/mm')
+    parser.add_argument('-c', type=valid_year_month, help='usage: -c yyyy/mm')
+    parser.add_argument('-c1', type=valid_year_month, help='usage: -c1 yyyy/mm')
     return parser.parse_args()
 
 
 def main():
     static_values = {
-        'usage_error': 'usage: path_to_files [-e yyyy] [-a yyyy/mm] [-c yyyy/mm]',
-        'file_prefix': 'Murree_weather_',
-        'data_file_ext': '.txt',
+        'usage_msg': 'directory_path [-e yyyy] [-a yyyy/mm] [-c yyyy/mm]',
         'switches': [
             '-e', '-a', '-c', '-c1'
         ],
@@ -364,15 +383,21 @@ def main():
 
     }
 
-    if len(sys_argv) < 4:
-        sys_exit(static_values.usage_error)
-
-    args = parse_arguments()
-    validate_arguments(args)
+    args = parse_arguments(static_values['usage_msg'])
     args = vars(args)
+    if len(list(filter(lambda x: x, args.values()))) < 2:
+        sys_exit('usage: ' + static_values['usage_msg'])
+
+    files_directory = args['directory_path']
+    file_names = get_files_in_dir(files_directory)
+    if not file_names:
+        sys_exit('No files found in directory %s' % files_directory)
+    else:
+        static_values['file_names'] = file_names
+
     arg_order = argument_order(sys_argv[2:])
     for arg in arg_order:
-        generate_report(sys_argv[1], arg, args[arg], static_values)
+        generate_report(files_directory, arg, args[arg], static_values)
 
 
 if __name__ == '__main__':
