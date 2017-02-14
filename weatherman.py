a4473528badf050514c46a4ae014006751046cf6
AdnanAsif <adnan.asif@arbisoft.com>
2017-02-14 19:26:20 +0500

    created classes WeatherReading and WeatherReader

--- weatherman.py
+++ weatherman.py
@@ -19,70 +19,89 @@ class TerminalColors:
     RESET = '\033[0m'
 
 
-class WeatherReadings:
-    def __init__(self):
-        self.all_weather_readings = {}
+class WeatherReading:
+    def __init__(self, kwargs):
+        self.reading_date = kwargs.get('reading_date')
+        self.max_temperature = kwargs.get('max_temperature')
+        self.min_temperature = kwargs.get('min_temperature')
+        self.max_humidity = kwargs.get('max_humidity')
+        self.mean_humidity = kwargs.get('mean_humidity')
+
+
+class WeatherReader:
+    file_columns = [
+        'date', 'max_temperature', 'mean_temperature', 'min_temperature',
+        'dew_point', 'mean_dew_point', 'min_dew_point', 'max_humidity',
+        'mean_humidity', 'min_humidity', 'max_sea_level_pressure',
+        'mean_sea_level_pressure', 'min_sea_level_pressure', 'max_visibility',
+        'mean_visibility', 'min_visibility', 'max_wind_speed', 'mean_wind_speed',
+        'max_guest_speed', 'precipitation', 'cloud_cover', 'events', 'wind_dir'
+    ]
 
     @staticmethod
-    def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+    def create_reading(row):
+        kwargs = {
+            'reading_date': datetime.strptime(row.get('date'), '%Y-%m-%d')
+        }
+        integer_columns = [
+            'max_temperature', 'min_temperature', 'max_humidity', 'mean_humidity'
+        ]
+        for column in integer_columns:
+            value = row.get(column)
+            kwargs[column] = int(value) if value else None
 
-    def read_file(self, file_name, file_dir='', field_names=None, skip_first_line=True):
-        month_readings_key = self.get_file_name_without_extension(file_name)
-        if month_readings_key in self.all_weather_readings:
-            return self.all_weather_readings[month_readings_key]
+        return WeatherReading(kwargs)
 
-        file_path = os.path.join(file_dir, file_name)
-        if not Path(file_path).is_file():
-            raise FileNotFoundError('No such file: {}'.format(file_path))
-
+    def read_file(self, file_name, file_dir='', skip_first_line=True):
         month_readings = []
+        file_path = os.path.join(file_dir, file_name)
         with open(file_path, 'r') as f:
-            reader = csv.DictReader(f, fieldnames=field_names)
+            reader = csv.DictReader(f, fieldnames=WeatherReader.file_columns)
             if skip_first_line:
                 next(reader)
-            for day_readings in reader:
-                month_readings.append(day_readings)
+            for row in reader:
+                month_readings.append(self.create_reading(row))
 
-        self.all_weather_readings[month_readings_key] = month_readings
         return month_readings
 
-    def get_month_readings(self, file_name):
-        month_readings_key = self.get_file_name_without_extension(file_name)
-        return self.all_weather_readings.get(month_readings_key)
+
+def remove_file_extension(file_name):
+    return os.path.splitext(file_name)[0]
 
 
 def apply_func(func, key_column, readings):
-    return func([row for row in readings if row[key_column]], key=lambda row: int(row[key_column]))
+    return func([row for row in readings if row.__getattribute__(key_column) is not None],
+                key=lambda obj: obj.__getattribute__(key_column))
 
 
 def calculate_average(key_column, readings):
-    reading_points = [int(row[key_column]) for row in readings if row[key_column]]
+    reading_points = [row.__getattribute__(key_column)
+                      for row in readings if row.__getattribute__(key_column) is not None]
     return sum(reading_points) / len(reading_points)
 
 
 def yearly_report(file_names, weather_readings):
-    from_format, to_format = '%Y-%m-%d', '%B %d'
-    months_readings = [weather_readings.get_month_readings(file_name) for file_name in file_names]
+    to_format = '%B %d'
+    months_readings = [weather_readings[remove_file_extension(file_name)] for file_name in file_names]
     full_year_readings = list(chain(*months_readings))
 
     max_temp_row = apply_func(max, 'max_temperature', full_year_readings)
     min_temp_row = apply_func(min, 'min_temperature', full_year_readings)
     max_humid_row = apply_func(max, 'max_humidity', full_year_readings)
 
-    date_strs = [create_date_str(date_str, from_format, to_format)
-                 for date_str in [max_temp_row['date'], min_temp_row['date'], max_humid_row['date']]]
+    date_strs = [date.strftime(to_format) for date in [
+        max_temp_row.reading_date, min_temp_row.reading_date, max_humid_row.reading_date]]
     date_iter = iter(date_strs)
 
-    print('Highest: {:.2f}C on {}'.format(int(max_temp_row['max_temperature']), next(date_iter)))
-    print('Lowest: {:.2f}C on {}'.format(int(min_temp_row['min_temperature']), next(date_iter)))
-    print('Humidity: {:.2f}% on {}'.format(int(max_humid_row['max_humidity']), next(date_iter)))
+    print('Highest: {:.2f}C on {}'.format(int(max_temp_row.max_temperature), next(date_iter)))
+    print('Lowest: {:.2f}C on {}'.format(int(min_temp_row.min_temperature), next(date_iter)))
+    print('Humidity: {:.2f}% on {}'.format(int(max_humid_row.max_humidity), next(date_iter)))
 
 
 def monthly_report(file_name, weather_readings):
-    avg_highest = calculate_average('max_temperature', weather_readings.get_month_readings(file_name))
-    avg_lowest = calculate_average('min_temperature', weather_readings.get_month_readings(file_name))
-    avg_mean_humidity = calculate_average('mean_humidity', weather_readings.get_month_readings(file_name))
+    avg_highest = calculate_average('max_temperature', weather_readings[remove_file_extension(file_name)])
+    avg_lowest = calculate_average('min_temperature', weather_readings[remove_file_extension(file_name)])
+    avg_mean_humidity = calculate_average('mean_humidity', weather_readings[remove_file_extension(file_name)])
 
     print('Highest Average: {:.2f}C'.format(avg_highest))
     print('Lowest Average: {:.2f}C'.format(avg_lowest))
@@ -100,15 +119,14 @@ def two_row_chart(day_no, max_t, min_t):
         print_symbol('+', value, color)
         print(' {}C'.format(value))
 
-    if max_t:
-        helper(int(max_t), TerminalColors.RED)
-    if min_t:
-        helper(int(min_t), TerminalColors.BLUE)
+    if max_t is not None:
+        helper(max_t, TerminalColors.RED)
+    if min_t is not None:
+        helper(min_t, TerminalColors.BLUE)
 
 
 def one_row_chart(day_no, max_t, min_t):
-    if max_t and min_t:
-        max_t, min_t = int(max_t), int(min_t)
+    if max_t is not None and min_t is not None:
         print(day_no, end=' ')
         print_symbol('+', min_t, TerminalColors.BLUE)
         print_symbol('+', max_t, TerminalColors.RED)
@@ -116,11 +134,10 @@ def one_row_chart(day_no, max_t, min_t):
 
 
 def monthly_bar_chart(file_name, weather_readings, chart_type):
-    month_readings = weather_readings.get_month_readings(file_name)
+    month_readings = weather_readings[remove_file_extension(file_name)]
     for day_reading in month_readings:
-        max_temperature, min_temperature = day_reading['max_temperature'], day_reading['min_temperature']
-        day_no = create_date_str(day_reading['date'], '%Y-%m-%d', '%d')
-        day_no = '{0:02d}'.format(int(day_no))
+        max_temperature, min_temperature = day_reading.max_temperature, day_reading.min_temperature
+        day_no = '{0:02d}'.format(int(day_reading.reading_date.strftime('%d')))
 
         if chart_type == 'double':
             two_row_chart(day_no, max_temperature, min_temperature)
@@ -145,21 +162,18 @@ def generate_reports(switch_order, switch_wise_file_names, weather_readings):
             monthly_bar_chart(file_names[0], weather_readings, 'single')
 
 
-def create_date_str(date_str, current_format, target_format):
-    try:
-        date = datetime.strptime(date_str, current_format)
-    except TypeError:
-        return None
-    else:
-        return date.strftime(target_format)
-
-
-def read_files(switch_wise_file_names, files_dir, field_names):
-    weather_readings = WeatherReadings()
+def read_files(switch_wise_file_names, files_dir):
+    file_names = set()
+    all_weather_readings = {}
+    weather_reader = WeatherReader()
     for switch in switch_wise_file_names:
-        [weather_readings.read_file(file_name, files_dir, field_names)
-         for file_name in switch_wise_file_names[switch]]
-    return weather_readings
+        file_names.update(switch_wise_file_names[switch])
+
+    file_names = list(file_names)
+    for file_name in file_names:
+        all_weather_readings[remove_file_extension(file_name)] = weather_reader.read_file(file_name, files_dir)
+
+    return all_weather_readings
 
 
 def get_file_names_to_read(switches, args, available_files):
@@ -210,10 +224,10 @@ def valid_year_month(text):
 
 
 def parse_arguments():
-    usage_msg = 'PROG directory_path [-e yyyy] [-a yyyy/mm] [-c yyyy/mm] [-c1 yyyy/mm]'
+    usage_msg = 'PROG files_directory [-e yyyy] [-a yyyy/mm] [-c yyyy/mm] [-c1 yyyy/mm]'
 
     parser = argparse.ArgumentParser(usage=usage_msg)
-    parser.add_argument('directory_path', type=str, help='data files directory path')
+    parser.add_argument('files_directory', type=str, help='data files directory path')
     parser.add_argument('-e', type=valid_year, help='usage: -e yyyy')
     parser.add_argument('-a', type=valid_year_month, help='usage: -a yyyy/mm')
     parser.add_argument('-c', type=valid_year_month, help='usage: -c yyyy/mm')
@@ -226,20 +240,12 @@ def parse_arguments():
 
 
 def main():
-    file_columns = [
-        'date', 'max_temperature', 'mean_temperature', 'min_temperature',
-        'dew_point', 'mean_dew_point', 'min_dew_point', 'max_humidity',
-        'mean_humidity', 'min_humidity', 'max_sea_level_pressure',
-        'mean_sea_level_pressure', 'min_sea_level_pressure', 'max_visibility',
-        'mean_visibility', 'min_visibility', 'max_wind_speed', 'mean_wind_speed',
-        'max_guest_speed', 'precipitation', 'cloud_cover', 'events', 'wind_dir'
-    ]
     args = parse_arguments()
-    dir_path = args['directory_path']
-    file_names_in_dir = get_file_names_in_dir(dir_path)
+    files_dir = args['files_directory']
+    file_names_in_dir = get_file_names_in_dir(files_dir)
     switch_order = [arg.lstrip('-') for arg in sys.argv[2::2]]
     switch_wise_file_names = get_file_names_to_read(switch_order, args, file_names_in_dir)
-    weather_readings = read_files(switch_wise_file_names, dir_path, file_columns)
+    weather_readings = read_files(switch_wise_file_names, files_dir)
     generate_reports(switch_order, switch_wise_file_names, weather_readings)
 
 
