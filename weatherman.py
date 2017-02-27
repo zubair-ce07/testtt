297eb7c4628d24c7092fb5d5ebd3782353323e6d
SarahRiaz8 <sarah.riaz@arbisoft.com>
2017-02-27 13:47:36 +0500

    "added smaller functions, replaced hard-coded file name with regular expression, made variables/conditions self-explanatory"

--- weatherman.py
+++ weatherman.py
@@ -1,155 +1,203 @@
 import argparse
 import calendar
+from dateutil import parser
 from termcolor import colored
 import csv
+import re
+import os
 
 
-class WeatherMan(object):
-    
+class WeatherMan:
+
     def __init__(self):
         self.months = calendar.month_name
 
+        self.field_date = 'PKT'
+        self.field_temp_max = 'Max TemperatureC'
+        self.field_temp_min = 'Min TemperatureC'
+        self.field_humid_max = 'Max Humidity'
+        self.field_temp_mean = 'Mean TemperatureC'
+        self.field_humid_mean = 'Mean Humidity'
+
     @staticmethod
     def read_file(file_path, year, month, data_fields):
-        filename = "{}/lahore_weather_{}_{}.txt".format(file_path, year, month[:3])
+        filename_pattern = '(?=.*{year})(?=.*{month}).*\.txt$'.format(year=year, month=month[:3])
 
-        with open(filename) as csvFile:
-            return list(csv.DictReader(csvFile, fieldnames=data_fields))[1:]
+        for filename in os.listdir(file_path):
+            if re.search(filename_pattern, filename, flags=re.IGNORECASE):
+                with open('{path}/{filename}'.format(filename=filename, path=file_path), ) as csvFile:
+                    dict_reader = csv.DictReader(csvFile, fieldnames=data_fields)
+                    next(dict_reader)  # Skip header
+                    return list(dict_reader)
 
-    def display_highest_lowest(self, params):
-        if not params:
+    @staticmethod
+    def compare(value1, value2, compare_func):
+        return value1 and (not value2 or compare_func(int(value1), int(value2)))
+
+    def display_highest_lowest_temperature(self, file_path, year):
+        if not file_path or not year:
+            print('Please pass correct arguments.')
             return
 
         highest_data = {'highest_temp': None, 'highest_day': None, 'highest_month': None}
         lowest_data = {'lowest_temp': None, 'lowest_day': None, 'lowest_month': None}
-        humidity_data = {'highest_humidity': None, 'humidity_day': None, 'humidity_month': None}
-
-        data_fields = ['PKT', 'Max TemperatureC',  'Min TemperatureC',  'Max Humidity']
+        humidity_data = {'highest_humid': None, 'humid_day': None, 'humid_month': None}
 
         for month in self.months[1:]:
-            file_data = self.read_file(params[1], params[0], month, data_fields)
-            for row in file_data:
-                date = row[data_fields[0]].split('-')
+            weather_records = self.read_file(file_path, year, month,
+                                             [self.field_date, self.field_temp_max,
+                                              self.field_temp_min, self.field_humid_max])
+            if not weather_records:
+                print('{month},{year} file not found.'.format(year=year, month=month))
+                continue
 
-                if(row[data_fields[1]] and (not highest_data['highest_temp']
-                                            or highest_data['highest_temp'] < int(row[data_fields[1]]))):
-                    highest_data['highest_temp'] = int(row[data_fields[1]])
-                    highest_data['highest_day'] = date[2]
+            for weather_record in weather_records:
+                if self.compare(weather_record[self.field_temp_max], highest_data['highest_temp'],
+                                lambda op1, op2: (op2 < op1)):
+                    cur_date = parser.parse(weather_record[self.field_date])
+                    highest_data['highest_temp'] = int(weather_record[self.field_temp_max])
+                    highest_data['highest_day'] = cur_date.day
                     highest_data['highest_month'] = month
 
-                if(row[data_fields[2]] and (not lowest_data['lowest_temp']
-                                            or lowest_data['lowest_temp'] > int(row[data_fields[2]]))):
-
-                    lowest_data['lowest_temp'] = int(row[data_fields[2]])
-                    lowest_data['lowest_day'] = date[2]
+                if self.compare(weather_record[self.field_temp_min], lowest_data['lowest_temp'],
+                                lambda op1, op2: (op2 > op1)):
+                    cur_date = parser.parse(weather_record[self.field_date])
+                    lowest_data['lowest_temp'] = int(weather_record[self.field_temp_min])
+                    lowest_data['lowest_day'] = cur_date.day
                     lowest_data['lowest_month'] = month
 
-                if(row[data_fields[3]] and (not humidity_data['highest_humidity']
-                                            or humidity_data['highest_humidity'] < int(row[data_fields[3]]))):
-                    humidity_data['highest_humidity'] = int(row[data_fields[3]])
-                    humidity_data['humidity_day'] = date[2]
-                    humidity_data['humidity_month'] = month
+                if self.compare(weather_record[self.field_humid_max], humidity_data['highest_humid'],
+                                lambda op1, op2: (op2 < op1)):
+                    cur_date = parser.parse(weather_record[self.field_date])
+                    humidity_data['highest_humid'] = int(weather_record[self.field_humid_max])
+                    humidity_data['humid_day'] = cur_date.day
+                    humidity_data['humid_month'] = month
 
         print('Highest: {}C on {} {} \nLowest: {}C on {} {} \nHumid: {}C on {} {}'.format(
-            str(highest_data['highest_temp']), highest_data['highest_month'], highest_data['highest_day'],
-            str(lowest_data['lowest_temp']), lowest_data['lowest_month'], lowest_data['lowest_day'],
-            str(humidity_data['highest_humidity']), humidity_data['humidity_month'], humidity_data['humidity_day']))
+            highest_data['highest_temp'], highest_data['highest_month'], highest_data['highest_day'],
+            lowest_data['lowest_temp'], lowest_data['lowest_month'], lowest_data['lowest_day'],
+            humidity_data['highest_humid'], humidity_data['humid_month'], humidity_data['humid_day']))
 
-    def display_average(self, params):
-        if not params:
+    def display_average(self, file_path, year, month):
+        if not file_path or not year or not month:
+            print('Please pass correct arguments.')
             return
 
         highest_temp = None
         lowest_temp = None
-        highest_humidity = None
+        highest_humid = None
 
-        data_fields = ['PKT', 'Mean TemperatureC', 'Mean Humidity']
-
-        date = params[0].split('/')
-
-        file_data = self.read_file(params[1], date[0], self.months[int(date[1])], data_fields)
-        for row in file_data:
-            if (row[data_fields[1]] and (not highest_temp
-                                         or highest_temp < int(row[data_fields[1]]))):
-                highest_temp = int(row[data_fields[1]])
-
-            if (row[data_fields[1]] and (not lowest_temp
-                                         or lowest_temp > int(row[data_fields[1]]))):
-                lowest_temp = int(row[data_fields[1]])
-
-            if (row[data_fields[2]] and (not highest_humidity
-                                         or highest_humidity < int(row[data_fields[2]]))):
-                highest_humidity = int(row[data_fields[2]])
-
-        print('Highest Average: {}C \nLowest Average: {}C \nAverage Humidity: {}%'.format(
-            str(highest_temp), str(lowest_temp), str(highest_humidity)))
-
-    def display_bar_charts(self, params):
-        if not params:
+        weather_records = self.read_file(file_path, year, self.months[month],
+                                         [self.field_date, self.field_temp_mean, self.field_humid_mean])
+        if not weather_records:
+            print('{month},{year} file not found.'.format(year=year, month=month))
             return
 
-        data_fields = ['PKT', 'Max TemperatureC', 'Min TemperatureC']
+        for weather_record in weather_records:
+            if self.compare(weather_record[self.field_temp_mean], highest_temp, lambda op1, op2: (op2 < op1)):
+                highest_temp = int(weather_record[self.field_temp_mean])
 
-        date = params[0].split('/')
-        print('{} {}'.format(self.months[int(date[1])], date[0]))
+            if self.compare(weather_record[self.field_temp_mean], lowest_temp, lambda op1, op2: (op2 > op1)):
+                lowest_temp = int(weather_record[self.field_temp_mean])
 
-        file_data = self.read_file(params[1], date[0], self.months[int(date[1])], data_fields)
+            if self.compare(weather_record[self.field_humid_mean], highest_humid, lambda op1, op2: (op2 < op1)):
+                highest_humid = int(weather_record[self.field_humid_mean])
 
-        for row in file_data:
-            cur_date = row[data_fields[0]].split('-')
+        print('Highest Average: {}C \nLowest Average: {}C \nAverage Humidity: {}%'.format(
+            highest_temp, lowest_temp, highest_humid))
 
-            if row[data_fields[1]]:
-                highest_temp = int(row[data_fields[1]])
+    def print_two_bar_charts_record(self, day, temp, symbol, color):
+        print('{} '.format(day), end='')
+        self.colored_print(temp, symbol, color)
+        print(' {}C'.format(temp))
 
-                print('{} '.format(cur_date[2]), end='')
+    @staticmethod
+    def colored_print(count, symbol, color):
+        for index in range(int(count)):
+            print(colored(symbol, color), end='')
 
-                for index in range(highest_temp):
-                    print(colored('+', 'red'), end='')
-                print(' {}C'.format(str(highest_temp)))
+    def display_two_bar_charts(self, file_path, year, month):
+        if not file_path or not year or not month:
+            print('Please pass correct arguments.')
+            return
 
-            if row[data_fields[2]]:
-                lowest_temp = int(row[data_fields[2]])
+        print('{} {}'.format(self.months[month], year))
 
-                print('{} '.format(cur_date[2]), end='')
+        weather_records = self.read_file(file_path, year, self.months[month],
+                                         [self.field_date, self.field_temp_max, self.field_temp_min])
+        if not weather_records:
+            print('{month},{year} file not found.'.format(year=year, month=month))
+            return
 
-                for index in range(lowest_temp):
-                    print(colored('+', 'blue'), end='')
-                print(' {}C'.format(str(lowest_temp)))
+        for weather_record in weather_records:
+            try:
+                day = parser.parse(weather_record[self.field_date]).day
+            except ValueError:
+                continue
 
-        print('\n')
+            if weather_record[self.field_temp_max]:
+                self.print_two_bar_charts_record(day, weather_record[self.field_temp_max], '+', 'red')
 
-        for row in file_data:
-            highest_temp = None
-            lowest_temp = None
-            if row[data_fields[2]]:
-                lowest_temp = int(row[data_fields[2]])
+            if weather_record[self.field_temp_min]:
+                self.print_two_bar_charts_record(day, weather_record[self.field_temp_min], '+', 'blue')
 
-                print('{} '.format(row[data_fields[0]].split('-')[2]), end='')
+    def display_one_bar_chart(self, file_path, year, month):
+        if not file_path or not year or not month:
+            print('Please pass correct arguments.')
+            return
 
-                for index in range(lowest_temp):
-                    print(colored('+', 'blue'), end='')
+        print('{} {}'.format(self.months[month], year))
 
-            if row[data_fields[1]]:
-                highest_temp = int(row[data_fields[1]])
+        weather_records = self.read_file(file_path, year, self.months[month],
+                                         [self.field_date, self.field_temp_max, self.field_temp_min])
+        if not weather_records:
+            print('{month},{year} file not found.'.format(year=year, month=month))
+            return
 
-                for index in range(highest_temp):
-                    print(colored('+', 'red'), end='')
+        for weather_record in weather_records:
+            highest_temp = weather_record[self.field_temp_max]
+            lowest_temp = weather_record[self.field_temp_min]
 
-            if lowest_temp and highest_temp:
-                print(' {}C-{}C'.format(str(lowest_temp), str(highest_temp)))
+            if lowest_temp or highest_temp:
+                print('{} '.format(parser.parse(weather_record[self.field_date]).day), end='')
+
+                if weather_record[self.field_temp_min]:
+                    self.colored_print(lowest_temp, '+', 'blue')
+
+                if weather_record[self.field_temp_max]:
+                    self.colored_print(highest_temp, '+', 'red')
+
+                print(' {}C-{}C'.format(lowest_temp, highest_temp))
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-e', nargs=2)
-    parser.add_argument('-a', nargs=2)
-    parser.add_argument('-c', nargs=2)
-    args = parser.parse_args()
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-e', nargs=2, dest='highest_lowest_temp')
+    arg_parser.add_argument('-a', nargs=2, dest='average')
+    arg_parser.add_argument('-c', nargs=2, dest='bar_chart')
+    args = arg_parser.parse_args()
 
     weatherman = WeatherMan()
-    weatherman.display_highest_lowest(args.e)
-    weatherman.display_average(args.a)
-    weatherman.display_bar_charts(args.c)
+
+    if args.highest_lowest_temp:
+        try:
+            date = parser.parse(args.highest_lowest_temp[0])
+            weatherman.display_highest_lowest_temperature(args.highest_lowest_temp[1], date.year)
+        except ValueError:
+            print('Please pass date in correct format.')
+    if args.average:
+        try:
+            date = parser.parse(args.average[0])
+            weatherman.display_average(args.average[1], date.year, date.month)
+        except ValueError:
+            print('Please pass date in correct format.')
+    if args.bar_chart:
+        try:
+            date = parser.parse(args.bar_chart[0])
+            weatherman.display_two_bar_charts(args.bar_chart[1], date.year, date.month)
+            weatherman.display_one_bar_chart(args.bar_chart[1], date.year, date.month)
+        except ValueError:
+            print('Please pass date in correct format.')
 
 
 if __name__ == "__main__":
