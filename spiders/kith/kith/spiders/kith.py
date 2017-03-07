3a5674d6b3ab174e7463cf14a3d6dc3a31f4d8a0
MehvishSaleem <mehvsaleem@gmail.com>
2017-03-07 16:39:24 +0500

    css restricted, methods renamed

--- spiders/kith/kith/spiders/kith.py
+++ spiders/kith/kith/spiders/kith.py
@@ -11,7 +11,9 @@ class KithSpider(CrawlSpider):
     start_urls = ['https://kith.com/']
 
     rules = (
-        Rule(LinkExtractor(allow=('/collections/.*/products/',)),
+
+        Rule(LinkExtractor(allow=('/collections/.*/products/',),
+                           restrict_css=("div.product-card.-content-overlay",)),
              callback='parse_item', follow=True),
         Rule(LinkExtractor(allow=('/collections/',)), follow=True),
     )
@@ -20,67 +22,72 @@ class KithSpider(CrawlSpider):
         product = KithItem()
 
         product['url'] = response.url
-        product['brand'] = self.get_product_brand(response)
-        product['category'] = self.get_product_category(response)
+        product['brand'] = self.product_brand(response)
+        product['category'] = self.product_category(response)
 
-        self.get_product_details(response, product)
+        self.product_description_and_care(response, product)
 
-        product['image_urls'] = self.get_image_urls(response)
+        product['image_urls'] = self.image_urls(response)
         product['industry'] = ''
         product['market'] = 'US'
         product['merch_info'] = []
 
-        sku_content = self.get_sku_content(response).split('variants', 1)
-        product['retailer_sku'] = self.get_retailer_sku(sku_content[0])
-        product['skus'] = self.get_skus(sku_content[1], response)
-        product['gender'] = self.get_gender(sku_content[0])
+        sku_content = re.split(r"variants\":", self.get_sku_content(response))
+        product['retailer_sku'] = self.retailer_sku(response)
+        product['skus'] = self.skus(sku_content[1], response)
+        product['gender'] = self.gender(sku_content[0])
 
-        product['name'] = product['category']
+        product['name'] = product['category'][0]
         product['retailer'] = 'kith-us'
 
         return product
 
     def get_sku_content(self, response):
-        pattern = re.compile(r"product:(.*?)}]", re.MULTILINE | re.DOTALL)
+        pattern = re.compile(r"product:(.*?),\"imag", re.MULTILINE | re.DOTALL)
         return response.xpath("//script[contains(.,'product:')]/text()").re(pattern)[0]
 
-    def get_product_brand(self, response):
+    def product_brand(self, response):
         return response.css('h1.product-header-title span::text').extract()
 
-    def get_product_category(self, response):
+    def product_category(self, response):
         return response.css('.breadcrumb.text-center a[href^="/c"]::text').extract()
 
-    def get_product_details(self, response, product):
+    def product_description_and_care(self, response, product):
         parent_class = response.css('.product-single-details-rte')
         product['description'] = parent_class.css('p::text').extract()[:-2]
         product['care'] = parent_class.css('p:last-child::text').extract()
 
-    def get_image_urls(self, response):
-        return response.css('img.full-width::attr(src)').extract()
+    def image_urls(self, response):
+        image_urls = response.xpath("//meta[@property='og:image:secure_url']"
+                                    "/@content").extract()
+        return [url.replace('_grande', "") for url in image_urls]
 
-    def get_gender(self, script):
+    def gender(self, script):
         if "wmns" in script:
             return "women"
         else:
             return "men"
 
-    def get_retailer_sku(self, response):
-        return re.findall(r"\"id\":(.*?),", response)[0]
+    def retailer_sku(self, response):
+        return response.css('input#product_id::attr(value)').extract_first()
 
-    def get_skus(self, script, response):
-        sku_fields = json.loads((script + '}]')[2:])
+    def skus(self, script, response):
+        variants = json.loads(script)
 
         skus = {}
 
-        for field in sku_fields:
+        for variant in variants:
             sku = {}
-            sku['price'] = field['price']
+            sku['price'] = variant['price']
             sku['currency'] = 'USD'
-            sku['out_of_stock'] = field['available']
-            sku['previous_prices'] = [field['compare_at_price']]
-            sku['size'] = field['title']
-            sku['color'] = response.css('.product-single-header-upper .product-'
-                                        'header-title.-variant::text').extract_first().strip()
-            skus[field['id']] = sku
+            sku['out_of_stock'] = variant['available']
+
+            if variant['compare_at_price']:
+                sku['previous_prices'] = [variant['compare_at_price']]
+
+            sku['size'] = variant['title']
+            sku['colour'] = response.css('.product-single-header-upper .product-'
+                                         'header-title.-variant::text').extract_first().strip()
+            skus[variant['id']] = sku
 
         return skus
