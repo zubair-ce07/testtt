a2ced3cb7b514eb85f2cb1fb62bb9357d1432e8e
MehvishSaleem <mehvsaleem@gmail.com>
2017-03-07 11:23:23 +0500

    seperate methods made, variables renamed

--- spiders/kith/kith/spiders/kith.py
+++ spiders/kith/kith/spiders/kith.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from kith.items import KithItem
@@ -13,60 +12,75 @@ class KithSpider(CrawlSpider):
 
     rules = (
         Rule(LinkExtractor(allow=('/collections/.*/products/',)),
-                           callback='parse_item', follow=True),
+             callback='parse_item', follow=True),
         Rule(LinkExtractor(allow=('/collections/',)), follow=True),
     )
 
     def parse_item(self, response):
         product = KithItem()
 
-        product['brand'] = response.css('h1.product-header-title span::text').extract()
         product['url'] = response.url
-        main_class = response.css('nav.breadcrumb.text-center')
-        product['category'] = main_class.css('a[href^="/c"]::text').extract()
-        product['gender'] = self.get_gender(product['category'])
-        product['description'] = response.css('div.product-single-details-rte '
-                                              'p::text').extract()
-        product['care'] = [item for item in product['description']
-                           if item.startswith('Material:')]
-        product['image_urls'] = response.css('img.full-width::attr(src)').extract()
+        product['brand'] = self.get_product_brand(response)
+        product['category'] = self.get_product_category(response)
+
+        self.get_product_details(response, product)
+
+        product['image_urls'] = self.get_image_urls(response)
         product['industry'] = ''
         product['market'] = 'US'
         product['merch_info'] = []
 
-        pattern = re.compile(r"product:(.*?)}]", re.MULTILINE | re.DOTALL)
-        script = response.xpath("//script[contains(.,'product:')]/text()").re(pattern)[0]
-        script = script.split('variants', 1)
-        product['retailer_sku'] = self.get_retailer_sku(script[0])
-        product['skus'] = self.get_skus(script[1])
+        sku_content = self.get_sku_content(response).split('variants', 1)
+        product['retailer_sku'] = self.get_retailer_sku(sku_content[0])
+        product['skus'] = self.get_skus(sku_content[1], response)
+        product['gender'] = self.get_gender(sku_content[0])
 
-        product['name'] = product['brand'][0].split(' ')[-1]
+        product['name'] = product['category']
         product['retailer'] = 'kith-us'
 
         return product
 
-    def get_gender(self, category):
-        if "Women" in category:
+    def get_sku_content(self, response):
+        pattern = re.compile(r"product:(.*?)}]", re.MULTILINE | re.DOTALL)
+        return response.xpath("//script[contains(.,'product:')]/text()").re(pattern)[0]
+
+    def get_product_brand(self, response):
+        return response.css('h1.product-header-title span::text').extract()
+
+    def get_product_category(self, response):
+        return response.css('.breadcrumb.text-center a[href^="/c"]::text').extract()
+
+    def get_product_details(self, response, product):
+        parent_class = response.css('.product-single-details-rte')
+        product['description'] = parent_class.css('p::text').extract()[:-2]
+        product['care'] = parent_class.css('p:last-child::text').extract()
+
+    def get_image_urls(self, response):
+        return response.css('img.full-width::attr(src)').extract()
+
+    def get_gender(self, script):
+        if "wmns" in script:
             return "women"
+        else:
+            return "men"
 
     def get_retailer_sku(self, response):
-        return response.split(',')[0][1:]
+        return re.findall(r"\"id\":(.*?),", response)[0]
 
-    def get_skus(self, response):
-        response = (response + '}]')[2:]
-        json_obj = json.loads(response)
+    def get_skus(self, script, response):
+        sku_fields = json.loads((script + '}]')[2:])
 
-        skus = []
+        skus = {}
 
-        for each in json_obj:
+        for field in sku_fields:
             sku = {}
-            sku['ID'] = each['id']
-            sku['price'] = each['price']
+            sku['price'] = field['price']
             sku['currency'] = 'USD'
-            sku['out_of_stock'] = each['available']
-            sku['previous_price'] = each['compare_at_price']
-            sku['size'] = each['title']
-
-            skus.append(sku)
+            sku['out_of_stock'] = field['available']
+            sku['previous_prices'] = [field['compare_at_price']]
+            sku['size'] = field['title']
+            sku['color'] = response.css('.product-single-header-upper .product-'
+                                        'header-title.-variant::text').extract_first().strip()
+            skus[field['id']] = sku
 
         return skus
