d8117218fd78e932a125af48a6a89cd047a85650
ahmad ashraf <ahmad.ashraf@arbisoft.com>
2017-03-08 13:04:56 +0500

    changes made to weatherman

--- Weatherman.py
+++ Weatherman.py
@@ -8,20 +8,18 @@ import os
 import re
 
 
-class Weather:
+class WeatherRecord:
     """weather object to store a days weather"""
 
     def __init__(self, date, month, year, max_temp, min_temp, max_humidity, mean_humidity):
-        self.Date = date
-        self.Month = month
-        self.Year = year
-        self.Max_Temp = max_temp
-        self.Min_Temp = min_temp
-        self.Max_Humidity = max_humidity
-        self.Mean_Humidity = mean_humidity
+        self.date = date
+        self.month = month
+        self.year = year
+        self.max_temp = max_temp
+        self.min_temp = min_temp
+        self.max_humidity = max_humidity
+        self.mean_humidity = mean_humidity
 
-
-weather_records = []
 
 # dividing date into separate values of day month and year
 def dividing_date(date):
@@ -31,57 +29,53 @@ def dividing_date(date):
 
 # reading the files needed for the user requirement and maintaining a record
 def read_files(file_path):
+    weather_records =[]
     files = glob.glob(file_path)
     for file in files:
         with open(file) as csv_file:
             reader = csv.DictReader(csv_file)
             for row in reader:
-                if 'PKST' in row.keys():
-                    row['PKT'] = row['PKST']
-                    del row['PKST']
-                day, month, year = dividing_date(row['PKT'])
+                day, month, year = dividing_date(row.get('PKT') or row.get('PKST'))
 
                 # catering for empty values
-                if row['Min TemperatureC'] == "":
+                if not row['Min TemperatureC']:
                     row['Min TemperatureC'] = "0.000001"
-                if row['Max TemperatureC'] == "":
+                if not row['Max TemperatureC']:
                     row['Max TemperatureC'] = "0.000001"
-                if row['Max Humidity'] == "":
+                if not row['Max Humidity']:
                     row['Max Humidity'] = "0.000001"
-                if row[' Mean Humidity'] == "":
+                if not row[' Mean Humidity']:
                     row[' Mean Humidity'] = "0.000001"
 
                 weather_records.append(
-                    Weather(int(day), month, int(year), float(row['Max TemperatureC']), float(row['Min TemperatureC']),
-                            float(row['Max Humidity']), float(row[' Mean Humidity'])))
+                    WeatherRecord(int(day), month, int(year), float(row['Max TemperatureC']),
+                                  float(row['Min TemperatureC']), float(row['Max Humidity']), float(row[' Mean Humidity'])))
 
     return weather_records
 
 
 # calculating yearly summary of max temp, min temp and max humidiity
-def year_info(year):
+def year_info(weather_records, year):
     year_records = []
     for record in weather_records:
-        if record.Year == year:
+        if record.year == year:
             year_records.append(record)
 
     # calculating min max mean
-    max_temp_day = max(year_records, key=operator.attrgetter('Max_Temp'))
-    max_humid_day = max(year_records, key=operator.attrgetter('Max_Humidity'))
-    min_temp_day = min(year_records, key=operator.attrgetter('Min_Temp'))
+
+    max_temp_day = max(filter(lambda x: x.max_temp != 0.000001, year_records), key=operator.attrgetter('max_temp'))
+    max_humid_day = max(filter(lambda x: x.max_humidity != 0.000001, year_records), key=operator.attrgetter('max_humidity'))
+    min_temp_day = min(filter(lambda x: x.min_temp != 0.000001, year_records), key=operator.attrgetter('min_temp'))
 
     # printing as required
-    print("Highest : " + str(int(max_temp_day.Max_Temp)) + "C on " + str(max_temp_day.Month) + " " + str(max_temp_day.Date))
-
-    print("Lowest : " + str(int(min_temp_day.Min_Temp)) + "C on " + str(min_temp_day.Month) + " " + str(min_temp_day.Date))
-
-    print("Humidity : " + str(int(max_humid_day.Max_Humidity)) + "C on " + str(max_humid_day.Month) + " " + str(max_humid_day.Date))
-
+    print("Highest : {0:.0f}C on {1} {2}".format(max_temp_day.max_temp, max_temp_day.month, max_temp_day.date))
+    print("Lowest : {0:.0f}C on {1} {2}".format(min_temp_day.min_temp, min_temp_day.month, min_temp_day.date))
+    print("Humidity : {0:.0f}% on {1} {2}".format(max_humid_day.max_humidity, max_humid_day.month, max_humid_day.date))
     print("")
 
 
 # calculating monthly summary
-def month_info(month, year):
+def month_info(weather_records, month, year):
 
     month_records = []
     zero_added_in_min_temp = 0
@@ -89,38 +83,34 @@ def month_info(month, year):
     zero_added_in_mean_humid = 0
 
     for record in weather_records:
-        if record.Year == year and record.Month == month:
-            if record.Min_Temp == 0.000001:
+        if record.year == year and record.month == month:
+            if record.min_temp == 0.000001:
                 zero_added_in_min_temp += 1
-            if record.Max_Temp == 0.000001:
+            if record.max_temp == 0.000001:
                 zero_added_in_max_temp += 1
-            if record.Mean_Humidity == 0.000001:
+            if record.mean_humidity == 0.000001:
                 zero_added_in_mean_humid += 1
             month_records.append(record)
 
     # calculating averages
-    max_temp_day = sum(record.Max_Temp for record in month_records) / (len(month_records) - zero_added_in_max_temp)
-    mean_humid_day = sum(record.Mean_Humidity for record in month_records) / (len(month_records) - zero_added_in_mean_humid)
-    min_temp_day = sum(record.Min_Temp for record in month_records) / (len(month_records) - zero_added_in_min_temp)
+    max_temp_day = sum(record.max_temp for record in month_records) / (len(month_records) - zero_added_in_max_temp)
+    mean_humid_day = sum(record.mean_humidity for record in month_records) / (len(month_records) - zero_added_in_mean_humid)
+    min_temp_day = sum(record.min_temp for record in month_records) / (len(month_records) - zero_added_in_min_temp)
 
     # printing as required
-    print("Highest Temperature Average : " + str(int(max_temp_day)) + "C")
-    print("Lowest Temperature Average : " + str(int(min_temp_day)) + "C")
-    print("Average Mean humidity : " + str(int(mean_humid_day)) + "%")
+    print("Highest Temperature Average : {0:.0f}C".format(max_temp_day))
+    print("Lowest Temperature Average : {0:.0f}C".format(min_temp_day))
+    print("Average Mean humidity : {0:.0f}%".format(mean_humid_day))
     print("")
 
 
 # displaying monthly graph
-def month_graph(month, year):
+def month_graph(weather_records, month, year):
 
     month_records = []
     for record in weather_records:
-        if record.Year == year and record.Month == month:
+        if record.year == year and record.month == month:
             month_records.append(record)
-
-    i = 0
-    # string to be printed
-    symbol_str = "+"
 
     #  colors for graphs
     red = '\033[91m'
@@ -129,61 +119,64 @@ def month_graph(month, year):
 
     # printing bar for each day in the month
     for d in month_records:
-        if d.Min_Temp == 0 and d.Max_Temp == 0:
+        if d.min_temp == 0.000001 and d.max_temp == 0.000001:
             print("no record found")
         else:
-            print(str(d.Date) + " " + blue + symbol_str * int(d.Min_Temp) + red + symbol_str * (
-                int(d.Max_Temp) - int(d.Min_Temp)) + grey + "  " +
-                  str(int(d.Min_Temp)) + "-" + str(int(d.Max_Temp)) + "C")
+            blue_str = "+" * int(d.min_temp)
+            red_str = "+" * (int(d.max_temp) - int(d.min_temp))
+
+            print("{} {}{}{}{} {}{:.0f}-{:.0f}C".format(d.date, blue, blue_str, red, red_str, grey, d.min_temp, d.max_temp ))
+
     print("")
 
 
 def main(argv):
+
 
     # regex to check input
     regex = r"(\d{4})/(\d{1,2})"
 
     # creating and parsing arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("Directory", help="Path to record Directory e.g weatherfiles/weatherfiles/")
-    parser.add_argument("-e", "--yearInfo", action='append',
+    parser.add_argument("directory", help="Path to record Directory e.g weatherfiles/weatherfiles/")
+    parser.add_argument("-e", "--year_info", action='append',
                         help="give the year you want the record of e.g. 2008", type=int)
-    parser.add_argument("-a", "--monthInfo", action='append',
+    parser.add_argument("-a", "--month_info", action='append',
                         help="give the month and year you want record of e.g. 2008/4")
-    parser.add_argument("-c", "--monthGraph", action='append',
+    parser.add_argument("-c", "--month_graph", action='append',
                         help="give the month and year you want graph of e.g. 2008/4")
     args = parser.parse_args()
 
 
-    if args.Directory:
-        if os.path.isdir(args.Directory):
-            file_path = args.Directory + "*.txt"
-            read_files(file_path)
+    if args.directory:
+        if os.path.isdir(args.directory):
+            file_path = args.directory + "*.txt"
+            weather_records = read_files(file_path)
         else:
-            print("Illegal Path")
+            print("Invalid Path")
 
-    if args.yearInfo is not None:
-        for x in args.yearInfo:
-            if 1900 > x > 3000:
+    if args.year_info is not None:
+        for y in args.year_info:
+            if 1900 > y > 3000:
                 print("please enter a viable year")
             else:
-                year_info(x)
+                year_info(weather_records, y)
 
-    if args.monthInfo is not None:
-        for x in args.monthInfo:
+    if args.month_info is not None:
+        for x in args.month_info:
             match = re.match(regex, x)
             if match is not None:
-                month_info(calendar.month_abbr[int(match.group(2))], int(match.group(1)))
+                month_info(weather_records, calendar.month_abbr[int(match.group(2))], int(match.group(1)))
             else:
-                print ("please enter correct month format")
+                print ("please enter correct month format e.g. 2008/4")
 
-    if args.monthGraph is not None:
-        for x in args.monthGraph:
+    if args.month_graph is not None:
+        for x in args.month_graph:
             match = re.match(regex, x)
             if match is not None:
-                month_graph(calendar.month_abbr[int(match.group(2))], int(match.group(1)))
+                month_graph(weather_records, calendar.month_abbr[int(match.group(2))], int(match.group(1)))
             else:
-                print("please enter correct month format")
+                print("please enter correct month format e.g. 2008/4")
 
 
 if __name__ == "__main__":
