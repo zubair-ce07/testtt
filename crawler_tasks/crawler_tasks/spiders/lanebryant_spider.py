156213c20d71a6d48ff03eff20d81e451da0a9df
AdnanAsif <adnan.asif@arbisoft.com>
2017-03-08 15:22:02 +0500

    issues resolved.

--- crawler_tasks/crawler_tasks/spiders/lanebryant_spider.py
+++ crawler_tasks/crawler_tasks/spiders/lanebryant_spider.py
@@ -7,7 +7,7 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.http import Request, FormRequest
 from scrapy.http import HtmlResponse
 
-from crawler_tasks.items import HypedcProduct
+from crawler_tasks.items import GenericProduct
 
 
 class LanebryantSpider(Spider):
@@ -16,17 +16,17 @@ class LanebryantSpider(Spider):
     start_urls = ['http://www.lanebryant.com/']
 
     base_url = 'http://www.lanebryant.com'
-    pagination_url = 'http://www.lanebryant.com/lanebryant/plp/includes/plp-filters.jsp?N={}&No={}'
+    pagination_url_t = 'http://www.lanebryant.com/lanebryant/plp/includes/plp-filters.jsp?N={}&No={}'
     product_skus_url = 'http://www.lanebryant.com/lanebryant/baseAjaxServlet?pageId=PDP_getProductSKU'
-    product_imgs_url = 'http://lanebryant.scene7.com/is/image/lanebryantProdATG/{}?req=set,json'
-    product_category_url = 'http://www.lanebryant.com/lanebryant/baseAjaxServlet?pageId=UserState'\
-                           '&Action=Header.userState&userState_id=pId%3D{}&fetchFavorites=false'
+    product_imgs_url_t = 'http://lanebryant.scene7.com/is/image/lanebryantProdATG/{}?req=set,json'
+    product_category_url_t = 'http://www.lanebryant.com/lanebryant/baseAjaxServlet?pageId=UserState'\
+                             '&Action=Header.userState&userState_id=pId%3D{}&fetchFavorites=false'
 
     def parse(self, response):
         links = LinkExtractor(allow=('\/P-(\d+)$',)).extract_links(response)
         for link in links:
             page_id = re.search('P-(\d+)', link.url).group(1)
-            page_url = LanebryantSpider.pagination_url.format(page_id, 0)
+            page_url = self.pagination_url_t.format(page_id, 0)
             yield Request(page_url, callback=self.parse_product_list)
 
     def parse_product_list(self, response):
@@ -36,21 +36,21 @@ class LanebryantSpider(Spider):
 
         product_links = products_html.css('a.mar-prd-item-image-container::attr(href)').extract()
         for link in product_links:
-            yield Request(url='{}{}'.format(LanebryantSpider.base_url, link), callback=self.parse_product)
+            yield Request(url='{}{}'.format(self.base_url, link), callback=self.parse_product)
 
         next_page_url = json_response.get('nextPageUrl')
         if not next_page_url:
             return
 
         match = re.search('N=(\d+)&No=(\d+)', next_page_url)
-        next_url = LanebryantSpider.pagination_url.format(match.group(1), match.group(2))
+        next_url = self.pagination_url_t.format(match.group(1), match.group(2))
         yield Request(next_url, callback=self.parse_product_list)
 
     def parse_product(self, response):
         desc = response.css('div#tab1 p::text').extract()
         desc1 = response.css('div#tab1 li::text').extract()
 
-        product = HypedcProduct()
+        product = GenericProduct()
         product['brand'] = ''
         product['market'] = ''
         product['merch_info'] = ''
@@ -64,19 +64,19 @@ class LanebryantSpider(Spider):
             'Action': 'PDP.getProduct'
         }
         yield FormRequest(
-            LanebryantSpider.product_skus_url, method='POST', formdata=formdata,
+            self.product_skus_url, method='POST', formdata=formdata,
             callback=self.parse_product_skus, meta={'product': product}
         )
 
     def parse_product_skus(self, response):
         product_detail = json.loads(response.text)['product'][0]
-        all_colors = {}
+        colors_map = {}
         for color in product_detail['all_available_colors'][0]['values']:
-            all_colors[color['id']] = color['name']
+            colors_map[color['id']] = color['name']
 
-        all_sizes = {}
+        sizes_map = {}
         for size in product_detail['all_available_sizes'][0]['values']:
-            all_sizes[size['id']] = size['value']
+            sizes_map[size['id']] = size['value']
 
         skus = {}
         for item in product_detail['skus']:
@@ -85,8 +85,8 @@ class LanebryantSpider(Spider):
             skus[item['sku_id']] = {
                 'currency': currency,
                 'price': price,
-                'size': all_sizes[item['size']],
-                'colour': all_colors[item['color']]
+                'size': sizes_map[item['size']],
+                'colour': colors_map[item['color']]
             }
 
         product = response.meta['product']
@@ -96,8 +96,14 @@ class LanebryantSpider(Spider):
 
         img_server_url = 'http:{}'.format(product_detail['scene7_params']['server_url'])
         imageset = product_detail['default_imageset']
-        yield Request(LanebryantSpider.product_imgs_url.format(imageset), callback=self.parse_product_img_urls,
-                      meta={'product': product, 'other': {'img_server_url': img_server_url}})
+        meta = {
+            'product': product,
+            'other': {
+                'img_server_url': img_server_url
+            }
+        }
+        yield Request(self.product_imgs_url_t.format(imageset),
+                      callback=self.parse_product_img_urls, meta=meta)
 
     def parse_product_img_urls(self, response):
         img_server_url = response.meta['other']['img_server_url']
@@ -112,7 +118,7 @@ class LanebryantSpider(Spider):
                 image_urls.append(img_server_url + item['i']['n'])
 
         product['image_urls'] = image_urls
-        yield Request(LanebryantSpider.product_category_url.format(product['product_id']),
+        yield Request(self.product_category_url_t.format(product['product_id']),
                       callback=self.parse_product_category, meta={'product': product})
 
     def parse_product_category(self, response):
