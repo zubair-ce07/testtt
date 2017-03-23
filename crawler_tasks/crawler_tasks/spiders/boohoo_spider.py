27bdb87c7ea984163fd63b1d92c6842217e8b3b1
AdnanAsif <adnan.asif@arbisoft.com>
2017-03-23 16:03:47 +0500

    refactored methods.

--- crawler_tasks/crawler_tasks/spiders/boohoo_spider.py
+++ crawler_tasks/crawler_tasks/spiders/boohoo_spider.py
@@ -2,9 +2,10 @@ import re
 import codecs
 from itertools import product
 
+from scrapy import Selector
 from scrapy.spiders import Spider
 from scrapy.linkextractors import LinkExtractor
-from scrapy.http import Request, TextResponse
+from scrapy.http import Request
 
 from crawler_tasks.items import GenericProduct
 
@@ -19,9 +20,14 @@ class BoohooSpider(Spider):
         '&zone0=prod_list_prods&currency=EUR' \
         '&pageurl={}' \
         '&config_categorytree={}'
-    html_tag_removal_regex = '<.*?>'
-    description_tab_index = 0
-    care_tab_index = 1
+
+    description_tab_name = 'Beschreibung'
+    care_tab_name = 'Produktdetails & Pflege'
+    gender_mapping = {
+        'herren': 'mens',
+        'girls': 'girls',
+        'boys': 'boys'
+    }
 
     def parse(self, response):
         links = LinkExtractor(
@@ -31,15 +37,17 @@ class BoohooSpider(Spider):
 
     def parse_category_url(self, category_url):
         pre_known_product_values = {}
-        if 'herren' in category_url:
-            pre_known_product_values['gender'] = 'mens'
-        elif 'girls' in category_url:
-            pre_known_product_values['gender'] = 'girls'
-        elif 'boys' in category_url:
-            pre_known_product_values['gender'] = 'boys'
-        elif 'looks' not in category_url:
-            pre_known_product_values['gender'] = 'womens'
+        for gender in self.gender_mapping:
+            if gender in category_url:
+                pre_known_product_values['gender'] = self.gender_mapping[gender]
+                break
+        else:
+            if 'looks' not in category_url:
+                pre_known_product_values['gender'] = 'womens'
 
+        return self.make_category_request(category_url, pre_known_product_values)
+
+    def make_category_request(self, category_url, pre_known_product_values):
         category_tree = category_url.split('.com/')[1]
         url = self.category_pagination_url_t.format(category_url, category_tree)
         meta = {
@@ -48,54 +56,34 @@ class BoohooSpider(Spider):
         yield Request(url, callback=self.parse_category, meta=meta)
 
     def parse_category(self, response):
-        text_response = self.response_containing_products(response)
-        yield next(self.parse_category_products(text_response))
+        html_selector = self.response_html_selector(response)
+        yield next(self.parse_category_products(html_selector, response.meta))
 
         if not response.meta.get('follow_pagination_urls', True):
             return
 
-        next_page_urls = text_response.css('#pagnTop .pagnnum a::attr(href)').extract()
+        next_page_urls = html_selector.css('#pagnTop .pagnnum a::attr(href)').extract()
         for url in next_page_urls:
             page_url = self.base_url + url
             category_tree = url.split('?')[0].lstrip('/')
             complete_url = self.category_pagination_url_t.format(
                 page_url, category_tree
             )
-            meta = {
-                'follow_pagination_urls': False,
-                'pre_known_product_values': text_response.meta['pre_known_product_values']
-            }
+            meta = response.meta.copy()
+            meta['follow_pagination_urls'] = False
             yield Request(complete_url, callback=self.parse_category, meta=meta)
 
-    def response_containing_products(self, response):
+    def response_html_selector(self, response):
         escaped_html = re.search('"html":"(.*)",', response.text).group(1)
-        unescaped_html = codecs.escape_decode(escaped_html.encode())[0].decode()
-        dummy_request = Request(self.base_url, meta=response.meta)
-        return TextResponse(url='', body=unescaped_html.encode(), request=dummy_request)
+        unescaped_html = codecs.escape_decode(escaped_html.encode())[0]
+        return Selector(text=unescaped_html, type='html')
 
-    def parse_category_products(self, response):
-        products_html = response.css('.prod')
+    def parse_category_products(self, html_selector, meta):
+        products_html = html_selector.css('.prod')
         for item in products_html:
-            meta = self.parse_product_html(item)
-            meta['pre_known_product_values'] = response.meta['pre_known_product_values']
-            complete_url = self.base_url + meta['product_url']
+            product_url = item.css('.prod-name ::attr(href)').extract_first()
+            complete_url = self.base_url + product_url
             yield Request(complete_url, callback=self.parse_product, meta=meta)
-
-    def parse_product_html(self, selector):
-        price, currency = selector.css('#price::text').extract_first().split(' ')
-        try:
-            previous_price = selector.css('#atrwas::text').extract_first().split(' ')[0]
-        except AttributeError:
-            previous_price = ''
-
-        return {
-            'price': price,
-            'previous_price': previous_price,
-            'currency': currency,
-            'product_url': selector.css('.prod-name a::attr(href)').extract_first(),
-            'product_id': selector.css('::attr(data-id)').extract_first(),
-            'name': selector.css('::attr(data-name)').extract_first()
-        }
 
     def parse_product(self, response):
         boohoo_product = GenericProduct()
@@ -104,33 +92,38 @@ class BoohooSpider(Spider):
         boohoo_product['merch_info'] = []
         boohoo_product['category'] = []
         boohoo_product['url'] = response.url
-        boohoo_product['product_id'] = response.meta['product_id']
-        boohoo_product['name'] = response.meta['name']
+        boohoo_product['product_id'] = response.css('#prodSKU::text').extract_first()
+        boohoo_product['name'] = response.css('#pageName::text').extract_first()
         boohoo_product['image_urls'] = self.parse_image_urls(response)
         boohoo_product['skus'] = self.parse_skus(response)
         boohoo_product.update(response.meta['pre_known_product_values'])
 
         tab_selector = response.css('#collateral-accordion')
-        boohoo_product['care'] =\
-            self.parse_description_tab(tab_selector, self.care_tab_index)
-        boohoo_product['description'] =\
-            self.parse_description_tab(tab_selector, self.description_tab_index)
+        if tab_selector:
+            boohoo_product['care'] =\
+                self.parse_description_tab(tab_selector, self.care_tab_name)
+            boohoo_product['description'] =\
+                self.parse_description_tab(tab_selector, self.description_tab_name)
+        else:
+            boohoo_product['care'] = []
+            boohoo_product['description'] = []
 
         return boohoo_product
 
-    def parse_description_tab(self, selector, tab_index):
-        try:
-            text = selector.css('dd')[tab_index].extract()
-        except (AttributeError, IndexError):
-            return []
-        else:
-            return [re.sub(self.html_tag_removal_regex, '', text)]
+    def parse_description_tab(self, selector, tab_name):
+        tab_names = selector.css('dt').css('::text').extract()
+        if tab_name in tab_names:
+            index = tab_names.index(tab_name)
+            return selector.css('dd')[index].css('::text').extract()
+        return []
 
     def parse_image_urls(self, response):
         image_urls = response.css('.product-image-thumbs .thumbnail img::attr(src)').extract()
         return [url.split('?')[0] for url in image_urls]
 
     def parse_skus(self, response):
+        price, currency = response.css('.special-price .price::text')[0].re('\S+')
+        prev_price = response.css('.old-price .price::text')[0].re('\S+')[0]
         sizes = response.css(
             '#configurable_swatch_international_size a::attr(title)').extract()
         colors = response.css('#configurable_swatch_color a::attr(name)').extract()
@@ -140,8 +133,8 @@ class BoohooSpider(Spider):
             skus['{}_{}'.format(colour, size)] = {
                 'size': size,
                 'colour': colour,
-                'price': response.meta['price'],
-                'previous_price': response.meta['previous_price'],
-                'currency': response.meta['currency']
+                'price': price,
+                'previous_price': prev_price,
+                'currency': currency
             }
         return skus
