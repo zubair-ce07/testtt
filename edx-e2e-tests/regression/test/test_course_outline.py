c832572f28177d5a39d12717174b2cdbe4f5e6c6
Aisha Sajid <aishasajid@AishaSajid-MacMini-08-Wifi.arbisoft.com>
2017-04-13 15:02:02 +0500

    added setup() to handle common test steps for multiple tests in the same test Class

--- edx-e2e-tests/regression/test/test_course_outline.py
+++ edx-e2e-tests/regression/test/test_course_outline.py
@@ -22,16 +22,15 @@ DEMO_COURSE_PASSWORD = os.environ.get('USER_LOGIN_PASSWORD')
 class StudioCourseOutlineTest(WebAppTest):
     """Tests of the Course Outline in Studio."""
 
-    def test_course_outline(self):
+    def setup(self):
         """
-        Verifies that user can click Edit Start Date button and is navigated
-        to Schedule and Details page, and that the Help link for
-        'Learn more about content visibility settings' is working.
+        Handles the common instantiations and function calls needed for
+        all the tests in this Class
+        :return:none
         """
+
         studio_login_page = StudioLogin(self.browser)
         studio_home_page = DashboardPageExtended(self.browser)
-        studio_login_page.visit()
-        studio_login_page.login(DEMO_COURSE_USER, DEMO_COURSE_PASSWORD)
 
         course_info = get_course_info()
 
@@ -43,25 +42,22 @@ class StudioCourseOutlineTest(WebAppTest):
             self.browser, course_info['org'], course_info['number'],
             course_info['run'])
 
+        studio_login_page.visit()
+        studio_login_page.login(DEMO_COURSE_USER, DEMO_COURSE_PASSWORD)
+
         # Verification only, should be on this page after login.
-        studio_home_page.wait_for_page()
+        self.studio_home_page.wait_for_page()
 
         # Navigate to the course's outline page
-        studio_home_page.select_course(get_course_display_name())
+        self.studio_home_page.select_course(get_course_display_name())
         studio_course_outline.wait_for_page()
 
-        # First verify the Help link
-        expected_href = 'https://edx.readthedocs.io/projects/' \
-                        'edx-partner-course-staff/en/latest/' \
-                        'developing_course/controlling_content_visibility.html'
-        # Assert that help link is correct.
-        assert_side_bar_help_link(
-            test=self,
-            page=studio_course_outline,
-            href=expected_href,
-            help_text='Learn more about content visibility settings',
-            as_list_item=False
-        )
+    def test_course_outline(self):
+        """
+        Verifies that user can click Edit Start Date button and is navigated
+        to Schedule and Details page, and that the Help link for
+        'Learn more about content visibility settings' is working.
+        """
 
         # If the help page is still up (see LT-53), then close it.
         if self.browser.current_url.startswith('https://edx.readthedocs.io'):
@@ -72,10 +68,10 @@ class StudioCourseOutlineTest(WebAppTest):
             self.browser.switch_to_window(self.browser.window_handles[0])
 
         # Now do the verification for the edit start date button.
-        studio_course_outline.click_edit_start_date_button()
+        self.studio_course_outline.click_edit_start_date_button()
 
         # This wait_for_page will also assert that we are on the correct page.
-        schedule_page.wait_for_page()
+        self.schedule_page.wait_for_page()
 
     def test_verify_helplinks(self):
         """
@@ -83,3 +79,17 @@ class StudioCourseOutlineTest(WebAppTest):
         'Learn more about content visibility settings' is working.
         :return:
         """
+
+        # First verify the Help link
+        expected_href = 'https://edx.readthedocs.io/projects/' \
+                        'edx-partner-course-staff/en/latest/' \
+                        'developing_course/controlling_content_visibility.html'
+
+        # Assert that help link is correct.
+        assert_side_bar_help_link(
+            test=self,
+            page=self.studio_course_outline,
+            href=expected_href,
+            help_text='Learn more about content visibility settings',
+            as_list_item=False
+        )
