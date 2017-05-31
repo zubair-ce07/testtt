0c1b1623acd82dbdff4378a8c3b67a928e7f63d1
ubair-hamid <ubair.hamid@arbisoft.com>
2017-05-31 17:52:22 +0500

    minor improvements
    minor improvements

--- pakwheel_concurrent.py
+++ pakwheel_concurrent.py
@@ -11,38 +11,38 @@ from parsel import Selector
 
 parse_url = 'https://www.pakwheels.com/used-cars/search/-/mk_toyota/md_corolla/ct_lahore/'
 
-default_concurrency = 99999
-default_concurent_requests = 10
-default_download_delay = 0.001
+default_max_urls = 99999
+default_concurrent_requests = 10
+default_download_delay = 0.0001
 
 
-def get_all_urls(url_link):
-    fetched_html = requests.get(url_link)
+def get_all_urls(url_to_parse):
+    fetched_html = requests.get(url_to_parse)
     sel = Selector(text=fetched_html.text)
-    partial_urls = sel.css('div.col-md-9.grid-style a::attr(href)')
-    complete_urls = [urljoin(url_link, url.extract()) for url in partial_urls]
+    partial_urls = sel.css('div.col-md-9.grid-style a::attr(href)').extract()
+    complete_urls = [urljoin(url_to_parse, url) for url in partial_urls]
     return complete_urls
 
 
-async def fetch_child_urls(urls):
+async def fetch_child_urls(urls_to_fetch):
     connector = aiohttp.TCPConnector(verify_ssl=False)
     total_bytes = 0
-    for url in urls:
+    for url in urls_to_fetch:
         time.sleep(download_delay)
         try:
-            r = await aiohttp.request('get', url, connector=connector)
+            response = await aiohttp.request('get', url, connector=connector)
         except Exception as e:
             print('bad link %s: %s' % (url, e))
         else:
-            total_bytes += len(await r.text())
+            total_bytes += len(await response.text())
     connector.close()
     return total_bytes
 
 
-def validate_args(concurrent_requests, download_delay, maxurls):
-    return (concurrent_requests and concurrent_requests >= 0) \
-            or (download_delay and download_delay >= 0) \
-            or (maxurls and maxurls >= 0)
+def validate_args(concurrent_requests, download_delay, max_urls):
+    return (concurrent_requests >= 0) \
+            and (download_delay >= 0) \
+            and (max_urls >= 0)
 
 
 def parse_arguments():
@@ -50,12 +50,15 @@ def parse_arguments():
 
     parser.add_argument('-n', '--concurrent_requests',
                         help='number of threads',
+                        default=default_concurrent_requests,
                         type=int)
     parser.add_argument('-t', '--download_delay',
                         help='timeout for web request',
+                        default=default_download_delay,
                         type=float)
-    parser.add_argument('-m', '--maxurls',
-                        help='number of maxurls',
+    parser.add_argument('-m', '--max_urls',
+                        help='number of max_urls',
+                        default=default_max_urls,
                         type=int)
     return parser.parse_args()
 
@@ -69,17 +72,17 @@ def display_report(downloaded_bytes, num_urls):
 if __name__ == '__main__':
 
     args = parse_arguments()
-    if validate_args(args.concurrent_requests, args.download_delay, args.maxurls) is False:
+    if not validate_args(args.concurrent_requests, args.download_delay, args.max_urls):
         print("Please provide positive inputs")
         sys.exit()
 
-    workers = args.concurrent_requests or default_concurent_requests
-    download_delay = args.download_delay or default_download_delay
-    max_urls = args.maxurls or default_concurrency
+    workers = args.concurrent_requests
+    download_delay = args.download_delay
+    max_urls = args.max_urls
 
     urls = get_all_urls(parse_url)
-    if len(urls) > max_urls:
-        urls = urls[0:max_urls]
+    if len(urls) > args.max_urls:
+        urls = urls[0:args.max_urls]
 
     total_bytes_downloaded = asyncio.get_event_loop().run_until_complete(fetch_child_urls(urls))
     display_report(total_bytes_downloaded, len(urls))
--- pakwheel_parallel.py
+++ pakwheel_parallel.py
@@ -11,41 +11,40 @@ parse_url = 'https://www.pakwheels.com/used-cars/search/-/mk_toyota/md_corolla/c
 workers = 10
 download_delay = 0.01
 
-default_concurrency = 99999
-default_concurent_requests = 10
-default_download_delay = 0.001
+default_max_urls = 99999
+default_concurrent_requests = 10
+default_download_delay = 0.0001
 
 
-def get_all_urls(url_link):
-    fetched_html = requests.get(url_link)
+def get_all_urls(url_to_parse):
+    fetched_html = requests.get(url_to_parse)
     sel = Selector(text=fetched_html.text)
-    partial_urls = sel.css('div.col-md-9.grid-style a::attr(href)')
-    complete_urls = [urljoin(url_link, url.extract()) for url in partial_urls]
+    partial_urls = sel.css('div.col-md-9.grid-style a::attr(href)').extract()
+    complete_urls = [urljoin(url_to_parse, url) for url in partial_urls]
     return complete_urls
 
 
-def fetch_url_data(url_link):
+def fetch_url_data(fetch_url):
     time.sleep(download_delay)
     try:
-        fetched_html = requests.get(url_link)
+        fetched_html = requests.get(fetch_url)
         return len(fetched_html.text)
     except:
         return 0
 
 
-def fetch_child_urls(urls):
+def fetch_child_urls(urls_to_fetch):
     fetched_bytes = 0
     with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as executor:
-        for bytes_length in executor.map(fetch_url_data, urls):
+        for bytes_length in executor.map(fetch_url_data, urls_to_fetch):
             fetched_bytes += bytes_length
-
     return fetched_bytes
 
 
-def validate_args(concurrent_requests, download_delay, maxurls):
-    return (concurrent_requests and concurrent_requests >= 0) \
-           or (download_delay and download_delay >= 0) \
-           or (maxurls and maxurls >= 0)
+def validate_args(concurrent_requests, download_delay, max_urls):
+    return (concurrent_requests >= 0) \
+            and (download_delay >= 0) \
+            and (max_urls >= 0)
 
 
 def parse_arguments():
@@ -53,12 +52,15 @@ def parse_arguments():
 
     parser.add_argument('-n', '--concurrent_requests',
                         help='number of threads',
+                        default=default_concurrent_requests,
                         type=int)
     parser.add_argument('-t', '--download_delay',
                         help='timeout for web request',
+                        default=default_download_delay,
                         type=float)
-    parser.add_argument('-m', '--maxurls',
-                        help='number of maxurls',
+    parser.add_argument('-m', '--max_urls',
+                        help='number of max_urls',
+                        default=default_max_urls,
                         type=int)
     return parser.parse_args()
 
@@ -72,13 +74,13 @@ def display_report(downloaded_bytes, num_urls):
 if __name__ == '__main__':
 
     args = parse_arguments()
-    if validate_args(args.concurrent_requests, args.download_delay, args.maxurls) is False:
+    if not validate_args(args.concurrent_requests, args.download_delay, args.max_urls):
         print("Please provide positive inputs")
         sys.exit()
 
-    workers = args.concurrent_requests or default_concurent_requests
-    download_delay = args.download_delay or default_download_delay
-    max_urls = args.maxurls or default_concurrency
+    workers = args.concurrent_requests
+    download_delay = args.download_delay
+    max_urls = args.max_urls
 
     urls = get_all_urls(parse_url)
     if len(urls) > max_urls:
