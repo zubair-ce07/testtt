a28fd839263eb1041df1a4d990aeaa2794d8f818
Faizan-Shiraz <faizan.shiraz@arbisoft.com>
2017-06-09 18:39:35 +0500

    Weatherman Updated

--- weatherman.py
+++ weatherman.py
@@ -2,24 +2,30 @@ import csv
 import os
 import collections
 import argparse
-
-Years = ('2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016')
-YearsRecord = dict((key, [0, 100, 0, 1000, "Date"]) for key in Years)
+from _datetime import datetime
 
 
-def update_record(m_max_t, m_min_t, m_max_h, m_min_h, rec):
+class Record:
+    max_temperature = 0
+    min_temperature = 100
+    max_humidity = 0
+    min_humidity = 500
+    date = "00/00/00"
+
+
+def update_record(m_max_t, m_min_t, m_max_h, m_min_h, record):
     """This function Updates the Yearly Record"""
-    index = -1
-    if rec[0] < int(max(m_max_t)):
-        rec[0] = int(max(m_max_t))
-        index = m_max_t.index(rec[0])
-    if rec[1] > int(min(m_min_t)):
-        rec[1] = int(min(m_min_t))
-    if rec[2] < int(max(m_max_h)):
-        rec[2] = int(max(m_max_h))
-    if rec[3] > int(min(m_min_h)):
-        rec[3] = int(min(m_min_h))
-    return rec, index
+    index_for_max_temp_date = -1
+    if record.max_temperature < int(max(m_max_t)):
+        record.max_temperature = int(max(m_max_t))
+        index_for_max_temp_date = m_max_t.index(record.max_temperature)
+    if record.min_temperature > int(min(m_min_t)):
+        record.min_temperature = int(min(m_min_t))
+    if record.max_humidity < int(max(m_max_h)):
+        record.max_humidity = int(max(m_max_h))
+    if record.min_humidity > int(min(m_min_h)):
+        record.min_humidity = int(min(m_min_h))
+    return record, index_for_max_temp_date
 
 
 def default_display():
@@ -28,7 +34,7 @@ def default_display():
     print("[Data_dir]\nDirectory containing weather data files")
 
 
-def process_data(month_data):
+def process_month_data(month_data, yearly_record):
     """This function processes the available data"""
     next(month_data)
     monthly_max_temp = []
@@ -36,46 +42,78 @@ def process_data(month_data):
     monthly_max_hum = []
     monthly_min_hum = []
     date_max_temp = []
-    for row in month_data:
-        date = row[0]
-        if row[1] != '':
-            monthly_max_temp.append(int(row[1]))
+    for each_day in month_data:
+        date = each_day[0]
+        if each_day[1] != '':
+            monthly_max_temp.append(int(each_day[1]))
             date_max_temp.append(date)
         else:
             monthly_max_temp.append(0)
-        if row[3] != '':
-            monthly_min_temp.append(int(row[3]))
-        if row[7] != '':
-            monthly_max_hum.append(int(row[7]))
-        if row[9] != '':
-            monthly_min_hum.append(int(row[9]))
-    year = date[0:4]
-    record = YearsRecord[year]
-    record, index = update_record(monthly_max_temp, monthly_min_temp, monthly_max_hum, monthly_min_hum, record)
-    if not index == -1:
-        record[4] = date_max_temp[index]
-    YearsRecord[year] = record
+        if each_day[3] != '':
+            monthly_min_temp.append(int(each_day[3]))
+        if each_day[7] != '':
+            monthly_max_hum.append(int(each_day[7]))
+        if each_day[9] != '':
+            monthly_min_hum.append(int(each_day[9]))
+    data_year = date[0:4]
+    current_record = yearly_record[data_year]
+    #  Storing Updated Record
+    current_record, max_temp_date_index = update_record(
+                                monthly_max_temp, monthly_min_temp,
+                                monthly_max_hum, monthly_min_hum,
+                                current_record)
+    #   Updating Date
+    if not max_temp_date_index == -1:
+        current_record.date = date_max_temp[max_temp_date_index]
+        yearly_record[data_year] = current_record
 
 
 def display_yearly_records(years_record):
     """Function to display record of each year"""
-    od = collections.OrderedDict(sorted(years_record.items()))
+    ordered_data_over_years = collections.OrderedDict(sorted(years_record.items()))
     print("Year\tMAX Temp\tMIN Temp\tMAX Humidity\tMIN "
           "Humidity\n--------------------------------------------------------------\n")
-    for key in od:
-        print("%s\t%d\t\t%d\t\t%d\t\t%d\n" % (key, od[key][0], od[key][1], od[key][2], od[key][3]))
+    for year in ordered_data_over_years:
+        print("%s\t%d\t\t%d\t\t%d\t\t%d\n" % (year, ordered_data_over_years[year].max_temperature,
+                                              ordered_data_over_years[year].min_temperature,
+                                              ordered_data_over_years[year].max_humidity,
+                                              ordered_data_over_years[year].min_humidity))
 
 
 def display_max_temp_yearly(years_record):
     """Function to display max temperatures of each year"""
-    od = collections.OrderedDict(sorted(years_record.items()))
+    ordered_data_over_years = collections.OrderedDict(sorted(years_record.items()))
     print(
         "Year\t\tDate\t\tTemp\n--------------------------------------------\n"
     )
-    for key in od:
-        date = od[key][4].split("-")
-        date = date[2] + "/" + date[1] + "/" + date[0]
-        print("%s\t\t%s\t%d\n" % (key, date, od[key][0]))
+    for year in ordered_data_over_years:
+        date = ordered_data_over_years[year].date.split("-")
+        date = "{:%d/%m/%Y}".format(datetime(int(date[0]), int(date[1]), int(date[2])))
+        print("%s\t\t%s\t%d\n" % (year, str(date), ordered_data_over_years[year].max_temperature))
+
+
+def get_years_list_from_files(files):
+    years_list = []
+    for name in files:
+        if name.endswith('.txt'):
+            year_value = name.split('_')
+            if year_value[2] not in years_list:
+                years_list.append(year_value[2])
+    return years_list
+
+
+def process(data_files, report_type):
+    years = get_years_list_from_files(data_files)
+    years_record = dict((key, Record()) for key in years)
+    for name in data_files:
+        if name.endswith('.txt'):
+            with open(report_type.dir + name, 'r') as csvfile:
+                line = csv.reader(csvfile, delimiter=',')
+                process_month_data(line, years_record)
+    if report_type.report == "1":
+        display_yearly_records(years_record)
+    elif report_type.report == "2":
+        display_max_temp_yearly(years_record)
 
 
 def main():
@@ -84,19 +122,9 @@ def main():
     parser.add_argument('report', nargs='?', const=1)
     parser.add_argument('dir', nargs='?', const=1)
     args = parser.parse_args()
-    files = os.listdir(args.dir)
+    weather_files = os.listdir(args.dir)
     if (args.report is not None) and args.dir is not None:
-        for name in files:
-            if name == '.DS_Store':
-                continue
-            else:
-                with open(args.dir + name, 'r') as csvfile:
-                    line = csv.reader(csvfile, delimiter=',')
-                    process_data(line)
-        if args.report == "1":
-            display_yearly_records(YearsRecord)
-        elif args.report == "2":
-            display_max_temp_yearly(YearsRecord)
+        process(weather_files, args)
     else:
         default_display()
 
