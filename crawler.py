81f9cdf859ad8b12c6d519ae421439c2e018c243
ChWasiUllah <wasi.ullah@arbisoft.com>
2017-06-13 15:45:25 +0500

    desired behaviour is acheived and all other mistakes are corrected like naming convention

--- crawler.py
+++ crawler.py
@@ -1,68 +1,94 @@
 import argparse
-import aiohttp
 import asyncio
+import threading
+from asyncio.tasks import FIRST_COMPLETED
+from concurrent.futures.thread import ThreadPoolExecutor
+from queue import Queue
+from urllib import parse
+
+import aiohttp
 import requests
-from concurrent import futures
+import time
 from parsel import Selector
 
 
 class Crawler:
-
-    def __init__(self, base_url, concurrent_requests, delay, visiting_urls):
+    def __init__(self, base_url, delay, concurrent_requests, max_urls):
         self.base_url = base_url
-        self.concurrent_requests = concurrent_requests
-        self.delay = delay
-        self.visiting_urls = visiting_urls   # number of urls to be visited
+        self.download_delay = delay
+        self.concurrent_requests_count = concurrent_requests
+        self.max_urls_to_visit = max_urls
+        self.active_requests = []
+        self.extracted_urls = []
+        self.visited_urls = []
         self.downloaded_bytes = 0
-        self.urls_reading = []
-        self.visited_url = []
+        self.extracted_urls.append(base_url)
+        self.visited_urls.append(self.base_url)
 
-    def downloaded_bytes_counter(self, response):
-        self.downloaded_bytes += len(response)
+    def count_downloaded_bytes(self, html):
+        self.downloaded_bytes += len(html)
 
-    def url_reader(self, html):
+    def extract_urls_from_html(self, html):
         selector = Selector(text=html)
-        url_in_page = selector.xpath('//a/@href').extract()
-        for url in url_in_page:
-            if len(self.urls_reading) == self.visiting_urls:
+        for relative_url in selector.xpath('//a/@href').extract():
+            if len(self.extracted_urls) == self.max_urls_to_visit:
                 break
-            if url.startswith('/'):
-                url = url[1:]
-            if url.find('http') == -1:
-                url = self.base_url + url
-                if url not in self.visited_url:
-                    self.urls_reading.append(url)
+            if relative_url.startswith('/'):
+                relative_url = relative_url[1:]
+            if 'http' not in relative_url:
+                absolute_url = parse.urljoin(self.base_url, relative_url)
+                if self.is_absolute_url(absolute_url) and absolute_url not in self.visited_urls:
+                    self.extracted_urls.append(absolute_url)
 
-    def show_performance(self, method):
-        print('Crawling through {}'.format(method))
-        print('Visited pages: {}'.format(len(self.visited_url)))
+    def is_absolute_url(self, url):
+        return bool(parse.urlparse(url).netloc)
+
+    def show_performance(self, crawling_approach):
+        print('Crawling through {}'.format(crawling_approach))
+        print('Visited pages: {}'.format(len(self.visited_urls)))
         print('Downloaded Bytes: {}'.format(self.downloaded_bytes))
-        print('Average size of a page {}\n\n'.format(self.downloaded_bytes / len(self.visited_url)))
+        print('Average size of a page {0:.2f}\n\n'.format(self.downloaded_bytes / len(self.visited_urls)))
 
 
 class AsyncCrawler(Crawler):
-
-    async def crawling(self, url):
-        self.visited_url.append(url)
+    async def crawling(self, url, request_count):
+        print('request {}'.format(request_count))
         async with aiohttp.request('Get', url) as response:
             html = await response.text()
-            self.downloaded_bytes_counter(html)
-            self.url_reader(html)
+            self.count_downloaded_bytes(html)
+            self.extract_urls_from_html(html)
 
     async def ensure_concurrency(self):
-        # It ensures number of concurrent request by creating specified number of tasks named
-        # async_requesting_urls and pass it to the asyncio.wait()
-        await self.crawling(self.base_url)
-        while len(self.visited_url) < len(self.urls_reading):
-            async_crawling_urls = []
-            for url_index in range(len(self.visited_url),
-                                   len(self.visited_url) + self.concurrent_requests):
-                if url_index < len(self.urls_reading):
-                    url = self.urls_reading[url_index]
-                    crawling_url = asyncio.ensure_future(self.crawling(url))
-                    async_crawling_urls.append(crawling_url)
-                    await asyncio.sleep(self.delay)
-            await asyncio.wait(async_crawling_urls)
+        await self.crawling(self.base_url, 1)
+        requesting_url_index = 1
+        while True:
+            active_task_count = len([task for task in asyncio.Task.all_tasks() if not task.done()])
+            requests_count = self.concurrent_requests_count - active_task_count
+            print('Active tasks count: {} New requests will be {}'.format(active_task_count,
+                                                                          requests_count))
+            if len(self.visited_urls) == self.max_urls_to_visit:
+                break
+            if requests_count:
+                for _ in range(requests_count):
+                    await self.schedule_new_request(requesting_url_index)
+                    requesting_url_index += 1
+                await asyncio.wait(self.active_requests, return_when=FIRST_COMPLETED)
+                print('visited_urls: {}\n'.format(len(self.visited_urls)))
+            else:
+                print('wait for proceeding previous requests\n')
+                await asyncio.sleep(self.download_delay)
+        while active_task_count > 1:
+            print('{} tasks are pending. Wait to complete these tasks\n'.format(active_task_count))
+            await asyncio.sleep(self.download_delay)
+            active_task_count = len([task for task in asyncio.Task.all_tasks() if not task.done()])
+
+    async def schedule_new_request(self, requesting_url_index):
+        if requesting_url_index < len(self.extracted_urls):
+            url = self.extracted_urls[requesting_url_index]
+            self.visited_urls.append(url)
+            task = asyncio.ensure_future(self.crawling(url, requesting_url_index + 1))
+            await asyncio.sleep(self.download_delay)
+            self.active_requests.append(task)
 
     def start(self):
         loop = asyncio.get_event_loop()
@@ -71,58 +97,111 @@ class AsyncCrawler(Crawler):
 
 
 class ParallelCrawler(Crawler):
-
-    def crawling(self, url):
-        self.visited_url.append(url)
+    def crawling(self, url, request_count=1):
+        lock.acquire()
+        print('request {}, active requests {}'.format(request_count, threading.active_count()))
         html = requests.get(url).text
-        self.downloaded_bytes_counter(html)
-        self.url_reader(html)
+        self.count_downloaded_bytes(html)
+        self.extract_urls_from_html(html)
+        lock.release()
 
     def ensure_parallelism(self):
         # It ensures number of parallel request by collecting specified number of urls and pass these
         # urls to ThreadPoolExecutor for crawling
-        while len(self.visited_url) < len(self.urls_reading):
-            parallel_crawling_urls = []
-            for url_index in range(len(self.visited_url),
-                                   len(self.visited_url) + self.concurrent_requests):
-                if url_index < len(self.urls_reading):
-                    url = self.urls_reading[url_index]
-                    parallel_crawling_urls.append(url)
-            with futures.ThreadPoolExecutor(max_workers=self.concurrent_requests) as executor:
-                executor.map(self.crawling, parallel_crawling_urls)
+        requesting_url_index = 1
+        active_request_count = Queue(maxsize=self.concurrent_requests_count)
+        while requesting_url_index <= self.concurrent_requests_count:
+            active_request_count.put(self.extracted_urls[requesting_url_index])
+            self.visited_urls.append(self.extracted_urls[requesting_url_index])
+            requesting_url_index += 1
+        with ThreadPoolExecutor(max_workers=self.concurrent_requests_count) as executor:
+            request_count = 1
+            while active_request_count.empty() is False:
+                url = active_request_count.get()
+                request_count += 1
+                self.active_requests.append(executor.submit(self.crawling, url, request_count))
+                time.sleep(self.download_delay)
+                if requesting_url_index < len(self.extracted_urls):
+                    active_request_count.put(self.extracted_urls[requesting_url_index])
+                    self.visited_urls.append(self.extracted_urls[requesting_url_index])
+                    requesting_url_index += 1
+            executor.map(self.active_requests)
 
     def start(self):
         self.crawling(self.base_url)
         self.ensure_parallelism()
 
 
-if __name__ == '__main__':
+def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u', help='Enter the website url')
-    parser.add_argument('-d', help='Specify download delay')
-    parser.add_argument('-r', help='Specify number of concurrent requests')
-    parser.add_argument('-m', help='Specify the maximum number of urls should be visited')
+    parser.add_argument('-c', '--crawling_approach', type=validate_crawling_approach,
+                        help='Enter c for concurrent crawling or p for parallel crawling')
+    parser.add_argument('-u', '--url', type=validate_url, help='Enter any website url')
+    parser.add_argument('-d', '--download_delay', type=validate_float_value,
+                        help='Specify download delay')
+    parser.add_argument('-r', '--concurrent_request_count', type=validate_int_value,
+                        help='Specify number of concurrent requests')
+    parser.add_argument('-m', '--max_urls_to_visit', type=validate_int_value,
+                        help='Specify the maximum number of urls should be visited')
     args = parser.parse_args()
-    if args.u:
-        if not args.u.endswith('/'):
-            args.u += '/'
+    if not args.crawling_approach:
+        args.crawling_approach = 'c'
+    if not args.url:
+        args.url = 'https://www.tutorialspoint.com/'
+    if not args.concurrent_request_count:
+        args.concurrent_request_count = 5
+    if not args.download_delay:
+        args.download_delay = 1
+    if not args.max_urls_to_visit:
+        args.max_urls_to_visit = 20
+    return args
+
+
+def validate_crawling_approach(craw_approach):
+    if len(craw_approach) == 1:
+        if 'c' in craw_approach or 'p' in craw_approach:
+            return craw_approach
+        else:
+            raise argparse.ArgumentTypeError(
+                'Please enter c for concurrent crawling or p for parallel crawling')
     else:
-        args.u = 'https://www.tutorialspoint.com/'
-    if args.r:
-        args.r = int(args.r)
+        raise argparse.ArgumentTypeError(
+            'Please enter c for concurrent crawling or p for parallel crawling')
+
+
+def validate_url(url):
+    if bool(parse.urlparse(url).netloc):
+        if not url.endswith('/'):
+            url += '/'
+        return url
     else:
-        args.r = 8
-    if args.d:
-        args.d = float(args.d)
+        raise argparse.ArgumentTypeError('Please enter a valid url')
+
+
+def validate_float_value(number):
+    try:
+        return float(number)
+    except:
+        raise argparse.ArgumentTypeError('Please enter a valid floating point value')
+
+
+def validate_int_value(number):
+    try:
+        return int(number)
+    except:
+        raise argparse.ArgumentTypeError('Please enter a valid integer value')
+
+
+if __name__ == '__main__':
+    args = get_arguments()
+    if 'c' in args.crawling_approach:
+        async_crawler = AsyncCrawler(
+            args.url, args.download_delay, args.concurrent_request_count, args.max_urls_to_visit)
+        async_crawler.start()
+        async_crawler.show_performance('Asynchronous')
     else:
-        args.d = 0.5
-    if args.m:
-        args.m = int(args.m)
-    else:
-        args.m = 30
-    async_crawler = AsyncCrawler(args.u, args.r, args.d, args.m)
-    async_crawler.start()
-    async_crawler.show_performance('Asynchronous')
-    parallel_crawler = ParallelCrawler(args.u, args.r, args.d, args.m)
-    parallel_crawler.start()
-    parallel_crawler.show_performance('Parallel')
+        lock = threading.Lock()
+        parallel_crawler = ParallelCrawler(
+            args.url, args.download_delay, args.concurrent_request_count, args.max_urls_to_visit)
+        parallel_crawler.start()
+        parallel_crawler.show_performance('Parallel')
