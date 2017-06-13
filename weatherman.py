33228ae9ff7d26e4af5f225db7230224af932f24
Palwisha-18 <palwisha.akhtar@arbisoft.com>
2017-06-13 13:09:15 +0500

    updated file with all changes

--- weatherman.py
+++ weatherman.py
@@ -8,7 +8,7 @@ import csv
 import argparse
 from termcolor import colored, cprint
 
-LABELS = ['-e', '-a', '-c', '-b']
+
 
 class ExtremeWeatherReport(object):
     def __init__(self):
@@ -25,87 +25,93 @@ class DisplayReports(object):
         pass
 
     def display_extreme_report(self, myreport):
-        print('Highest: ' + str(myreport.highest_temp_val) + 'C on '
-            + myreport.highest_temp_day)
-        print('Lowest: ' + str(myreport.lowest_temp_val) + 'C on '
-            + myreport.lowest_temp_day)
-        print('Humid: ' + str(myreport.humidity_val) + '% on '
-            + myreport.humidity_day)
+        print('Highest: {0}C on {1}'.format(str(myreport.highest_temp_val), myreport.highest_temp_day))
+        print('Lowest: {0}C on {1}'.format(str(myreport.lowest_temp_val), myreport.lowest_temp_day))
+        print('Humid: {0}% on {1}'.format(str(myreport.humidity_val), myreport.humidity_day))
 
     def display_average_report(self, myreport):
-        print('Highest Average: ' + str(myreport.avg_highest_temp) + 'C')
-        print('Lowest Average: ' + str(myreport.avg_lowest_temp) + 'C')
-        print('Average Humidity: ' + str(myreport.avg_humidity) + '%')
+        print('Highest Average: {}C'.format(str(myreport.avg_highest_temp)))
+        print('Lowest Average: {}C'.format(str(myreport.avg_lowest_temp)))
+        print('Average Humidity: {}%'.format(str(myreport.avg_humidity)))
 
     def display_chart(self, myreport):
-        print(myreport.month + ' ' + str(myreport.year))
+        print('{0} {1}'.format(myreport.month, str(myreport.year)))
         days = len(myreport.highest_temp)
         num = 0
         while num < days:
-            max_temp_val = myreport.highest_temp[num]
-            plus_str_max_temp = get_plus_str(max_temp_val)
-            min_temp_val = myreport.lowest_temp[num]
-            plus_str_min_temp = get_plus_str(min_temp_val)
-            max_temp_red_str = colored(plus_str_max_temp, 'red')
-            min_temp_blue_str = colored(plus_str_min_temp, 'blue')
+            (max_temp_val, min_temp_val,
+             plus_str_max_temp, plus_str_min_temp,
+             max_temp_red_str, min_temp_blue_str) = get_temp_vals_and_str(myreport, num)
             num = num + 1
             date = num
-            date, max_temp_val, min_temp_val = change_date_temp_format(date, num, max_temp_val, min_temp_val)
+            date, max_temp_val, min_temp_val = change_date_temp_format(date, num,
+                                                                    max_temp_val,
+                                                                    min_temp_val)
             if plus_str_max_temp == '--':
-                x = 'Data not available'
-                print(date + ' ' + max_temp_val)
+                max_temp_val = 'Data not available'
+                print('{0} {1}'.format(date, max_temp_val))
             else:
-                print(date + ' ' + max_temp_red_str + ' ' + str(max_temp_val) + 'C')
+                print('{0} {1} {2}C'.format(date, max_temp_red_str, str(max_temp_val)))
 
             if plus_str_min_temp == '--':
                 min_temp_val = 'Data not available'
-                print(date + ' ' + min_temp_val)
+                print('{0} {1}'.format(date, min_temp_val))
             else:
-                print(date + ' ' + min_temp_blue_str + ' ' + str(min_temp_val) + 'C')
+                print('{0} {1} {2}C'.format(date, min_temp_blue_str, str(min_temp_val)))
 
     def display_bonus_chart(self, myreport):
-        print(myreport.month + ' ' + str(myreport.year))
+        print('{0} {1}'.format(myreport.month, str(myreport.year)))
         days = len(myreport.highest_temp)
         num = 0
         while num < days:
-            max_temp_val = myreport.highest_temp[num]
-            plus_str_max_temp = get_plus_str(max_temp_val)
-            min_temp_val = myreport.lowest_temp[num]
-            plus_str_min_temp = get_plus_str(min_temp_val)
-            max_temp_red_str = colored(plus_str_max_temp, 'red')
-            min_temp_blue_str = colored(plus_str_min_temp, 'blue')
+            (max_temp_val, min_temp_val,
+             plus_str_max_temp, plus_str_min_temp,
+             max_temp_red_str, min_temp_blue_str) = get_temp_vals_and_str(myreport, num)
             num = num + 1
             date = num
-            date, max_temp_val, min_temp_val = change_date_temp_format(date, num, max_temp_val, min_temp_val)
-            max_temp_val = str(max_temp_val) + 'C'
-            min_temp_val = str(min_temp_val) + 'C'
+            date, max_temp_val, min_temp_val = change_date_temp_format(date, num,
+                                                                    max_temp_val,
+                                                                    min_temp_val)
+            max_temp_val = '{}C'.format(str(max_temp_val))
+            min_temp_val = '{}C'.format(str(min_temp_val))
             if plus_str_max_temp == '--':
                 max_temp_val = 'Data not available'
             if plus_str_min_temp == '--':
                 min_temp_val = 'Data not available'
-            print(date + ' ' + min_temp_blue_str + max_temp_red_str + ' ' + min_temp_val + ' - ' + max_temp_val)
+            print('{0} {1}{2} {3}-{4}'.format(date, min_temp_blue_str, max_temp_red_str, min_temp_val, max_temp_val))
+
+
+def get_temp_vals_and_str(myreport, num):
+    max_temp_val = myreport.highest_temp[num]
+    plus_str_max_temp = get_plus_str(max_temp_val)
+    min_temp_val = myreport.lowest_temp[num]
+    plus_str_min_temp = get_plus_str(min_temp_val)
+    max_temp_red_str = colored(plus_str_max_temp, 'red')
+    min_temp_blue_str = colored(plus_str_min_temp, 'blue')
+    return (max_temp_val, min_temp_val, plus_str_max_temp,
+        plus_str_min_temp, max_temp_red_str, min_temp_blue_str)
 
 
 def get_plus_str(num):
     plus_str  = ''
     if num != 'no data':
         for count in range(0, num):
-            plus_str = plus_str + '+'
+            plus_str = '{}+'.format(plus_str)
     else:
         plus_str = '--'
     return plus_str
 
+
 def change_date_temp_format(date, num, max_temp_val, min_temp_val):
     if date < 10:
-        date = '0' + str(num)
+        date = '0{}'.format(str(num))
     else:
         date = str(date)
     if max_temp_val < 10:
-        max_temp_val = '0' + str(max_temp_val)
+        max_temp_val = '0{}'.format(str(max_temp_val))
     if min_temp_val < 10:
-        min_temp_val = '0' + str(min_temp_val)
+        min_temp_val = '0{}'.format(str(min_temp_val))
     return date, max_temp_val, min_temp_val
-
 
 
 class MakeReports(object):
@@ -117,24 +123,22 @@ class MakeReports(object):
             for filename in files:
                 with open(os.path.join(os.sep, dir_path, filename), 'r') as csvfile:
                     reader = csv.DictReader(csvfile)
-                    month_name = get_month_name(filename)
                     for rows in reader:
                         elements = rows[None]
                         count = count + 1
                         if count >= 2  and len(elements) > 1:
-                            date = str(count - 1)
                             maxtemp = elements[1]
                             if maxtemp != '' and int(maxtemp) > myreport.highest_temp_val:
                                 myreport.highest_temp_val = int(maxtemp)
-                                myreport.highest_temp_day = month_name + ' ' + date
+                                myreport.highest_temp_day = get_date(elements[0])
                             mintemp = elements[3]
                             if mintemp != '' and int(mintemp) < myreport.lowest_temp_val:
                                 myreport.lowest_temp_val = int(mintemp)
-                                myreport.lowest_temp_day = month_name + ' ' + date
+                                myreport.lowest_temp_day = get_date(elements[0])
                             maxhumid = elements[7]
                             if maxhumid != '' and int(maxhumid) > myreport.humidity_val:
                                 myreport.humidity_val = int(maxhumid)
-                                myreport.humidity_day = month_name + ' ' + date
+                                myreport.humidity_day = get_date(elements[0])
                 count = 0
             return myreport
 
@@ -161,15 +165,13 @@ class MakeReports(object):
             myreport.avg_humidity = compute_average(myreport.avg_humidity)
             return myreport
 
-
         def make_chart_report(self, filename, dir_path, year):
             myreport = WeatherChartReport()
             count = 0
             with open(os.path.join(os.sep, dir_path, filename), 'r') as csvfile:
                 reader = csv.DictReader(csvfile)
-                month_name = get_month_name(filename)
                 myreport.year = year
-                myreport.month = month_name
+                myreport.month = get_month_name(filename)
                 for rows in reader:
                     elements = rows[None]
                     count = count + 1
@@ -210,11 +212,17 @@ class AverageWeatherReport(object):
 def compute_average(vals):
     return sum(vals)/len(vals)
 
+
 def get_month_name(filename):
     splited_file_name = filename.split('_')
     month_name = splited_file_name[3][0:3]
     month_name = calendar.month_name[time.strptime(month_name, '%b')[1]]
     return month_name
+
+
+def get_date(date):
+    return time.strftime('%B %d',time.strptime(date, '%Y-%m-%d'))
+
 
 def check_year(year):
     match_obj = re.match(r'\d{4}', year, re.M|re.I)
@@ -222,8 +230,13 @@ def check_year(year):
         print('invalid year')
         exit()
 
+
 def check_year_month(year, month):
     check_year(year)
+    match_obj = re.match(r'\d{1}', month, re.M|re.I)
+    if match_obj is None:
+        print('invalid month')
+        exit()
     if len(month) > 2:
         print('invalid month format')
         exit()
@@ -248,6 +261,7 @@ class WeatherChartReport(object):
         self.highest_temp = []
         self.lowest_temp = []
 
+
 def get_files(dir_path, year, english_month):
     all_files = os.listdir(dir_path)
     required_files = []
@@ -260,6 +274,7 @@ def get_files(dir_path, year, english_month):
         print('Data for this month is not available')
         exit()
     return required_files
+
 
 def get_year_and_month(year_month):
     year_and_month = year_month.split('/')
@@ -290,43 +305,31 @@ def check_dirpath_exists(dir_path):
         exit()
 
 
-def check_report_type(report_label):
-    if report_label not in LABELS:
-        print('invalid report label')
-        exit()
-
-
-def check_args(list_args):
-    if len(list_args) != 4:
-        print('invalid number of args')
-        exit()
-
-
 def main():
     myparser = argparse.ArgumentParser()
-    myparser.add_argument('report-type')
-    myparser.add_argument('year-month')
-    myparser.add_argument('dir-path')
+    myparser.add_argument('-e')
+    myparser.add_argument('-a')
+    myparser.add_argument('-c')
+    myparser.add_argument('-b')
+    myparser.add_argument('dirpath')
     args = myparser.parse_args()
-    argsdict = vars(args)
-    print argsdict
 
-    #list_args = sys.argv
-    #check_args(list_args)
-
-    report_label = argsdict['report_type']
-    year_month = argsdict['year_month']
-    dir_path = argsdict['dir_path']
-
-    check_report_type(report_label)
+    dir_path = args.dirpath
     check_dirpath_exists(dir_path)
 
-    if report_label == '-e':
-        extreme_weathers(year_month, dir_path)
-    elif report_label == '-a':
-        average_weathers(year_month, dir_path)
-    elif report_label == '-c' or report_label == '-b':
-        weather_charts(year_month, dir_path, report_label)
+    if args.e:
+        extreme_weathers(args.e, dir_path)
+    elif args.a:
+        average_weathers(args.a, dir_path)
+    elif args.c:
+        report_label = '-c'
+        weather_charts(args.c, dir_path, report_label)
+    elif args.b:
+        report_label = '-b'
+        weather_charts(args.b, dir_path, report_label)
+    else:
+        print('No Report Label Matched')
+        exit()
 
 
 if __name__ == '__main__':
