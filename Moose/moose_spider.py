497d4c508132626a410ae6031b47ccb0de39e928
ahmad ashraf <ahmad.ashraf@arbisoft.com>
2017-06-14 16:01:15 +0500

    using magento product data function

--- Moose/moose_spider.py
+++ Moose/moose_spider.py
@@ -1,4 +1,5 @@
 import json
+import re
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Rule
 from .base import BaseParseSpider, BaseCrawlSpider, clean
@@ -45,12 +46,8 @@ class MooseParseSpider(BaseParseSpider):
 
         self.boilerplate_normal(garment, response)
         garment['image_urls'] = self.image_urls(response)
-        garment['skus'] = self.skus(response, sku_id)
+        garment['skus'] = self.skus(response)
         garment['gender'] = self.detect_gender(self.product_gender(response))
-
-        availability = clean(self.product_availability(response))
-        if availability != 'In stock':
-            garment['out_of_stock'] = True
 
         return garment
 
@@ -59,50 +56,69 @@ class MooseParseSpider(BaseParseSpider):
         return []
 
     def product_id(self, response):
-        return clean(response.css('meta[itemprop=sku]::attr(content)').extract_first())
+        return clean(response.css('meta[itemprop=sku]::attr(content)'))[0]
 
     def product_name(self, response):
-        return clean(response.css('meta[itemprop=name]::attr(content)').extract_first())
+        return clean(response.css('meta[itemprop=name]::attr(content)'))[0]
 
     def product_description(self, response):
-        first_tab = clean(response.css('div.std').css('ul>li::text').extract()[1:])
-        second_tab = clean(response.css('div.std>p::text').extract()[1:])
+        first_tab = clean(response.css('div.std ul>li::text'))[1:]
+        second_tab = clean(response.css('div.std>p::text'))[1:]
         return first_tab+second_tab
 
     def image_urls(self, response):
-        return clean(response.css('div.cust-view a::attr(href)').extract())
+        return clean(response.css('div.cust-view a::attr(href)'))
 
     def product_color_and_size(self, response):
         colors = []
         sizes = []
-        script_json = clean(response.css('div[id=product-options-wrapper]>script::text').extract_first())
-        script_json = script_json[script_json.find('{'):]
-        script_json = script_json[:-2]
-        parsed_json_data = json.loads(script_json)
-        color_dict = parsed_json_data['attributes']['141']['options']
-        size_dict = parsed_json_data['attributes']['142']['options']
-        for col in color_dict:
-            colors.append(col['label'])
-        for siz in size_dict:
-            sizes.append(siz['label'])
-        return colors, sizes
+        color_codes = []
+        size_codes = []
 
-    def skus(self, response, sku):
-        colors, sizes = self.product_color_and_size(response)
+        script_json = self.magento_product_data(response)
+        colors_dict = script_json['attributes']['141']['options']
+        sizes_dict = script_json['attributes']['142']['options']
+        for color in colors_dict:
+            colors.append(color['label'])
+            color_codes.append(color['products'])
+        for size in sizes_dict:
+            sizes.append(size['label'])
+            size_codes.append(size['products'])
+        return self.create_sku_keys(colors, sizes, color_codes, size_codes)
+
+    def create_sku_keys(self, colors, sizes, color_codes, size_codes):
+        sku_keys = []
+        itera = 0
+        for color in colors:
+            next = 0
+            for size in sizes:
+                if not set(color_codes[itera]).isdisjoint(size_codes[next]):
+                    sku_keys.append(color+"_"+size)
+                next += 1
+        return sku_keys
+
+    def skus(self, response):
+        sku_keys = self.product_color_and_size(response)
         price = self.product_pricing_new(response)
         skus = {}
+        availability = self.product_availability(response)
 
-        for color in colors:
-            for size in sizes:
-                skus[color+"_"+size] = {"color": color, "size": size, "price": price}
+        for keys in sku_keys:
+            color_size = keys.split('_')
+            skus[keys] = {"color": color_size[0], "size": color_size[1], "price": price[1], "currency": price[2],
+                          "out_of_stock": availability}
 
         return skus
 
     def product_availability(self, response):
-        return clean(response.css('p.availability>span::text').extract_first())
+        availability = clean(response.css('p.availability>span::text'))[0]
+        flag = False
+        if availability != 'In stock':
+            flag = True
+        return flag
 
     def product_gender(self, response):
-        return clean(response.css('tbody>tr>td::text').extract_first())
+        return clean(response.css('tbody>tr>td::text'))[0]
 
 
 class MooseCrawlSpider(BaseCrawlSpider):
