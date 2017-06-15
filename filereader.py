d70324bf4fad81df5d356ca03250d8028d82607d
Wasiq Noor <wasiqnoorahmad@gmail.com>
2017-06-16 01:45:57 +0500

    Adding getfiles function

--- filereader.py
+++ filereader.py
@@ -1,3 +1,5 @@
+import os
+import fnmatch
 
 
 class WeatherFilesReader(object):
@@ -7,9 +9,19 @@ class WeatherFilesReader(object):
     def __init__(self, dir):
         self.dir = dir
 
+    def get_filenames_by_year(self, year):
+        all_files = os.listdir(self.dir)
+        output_files = []
+        for file in all_files:
+           if fnmatch.fnmatch(file, '*_' + year + '*'):
+               output_files.append(file)
+        return output_files
+
     def read_by_year(self, year):
         if not any(key.startswith(year) for key in self.retrieved_records):
-            pass
+            filenames = self.get_filenames_by_year(year)
+            with open(os.path.join(self.dir, filenames)) as csvfile:
+                pass
 
     def read_by_years(self, years):
         pass
