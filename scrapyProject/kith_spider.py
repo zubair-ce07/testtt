6d665a7820d6510838670af433e6b018b78a0ba8
rayyan17 <rayyan.khalid@arbisoft.com>
2017-06-19 16:07:49 +0500

    kith-crawler v1.2

--- scrapyProject/kith_spider.py
+++ scrapyProject/kith_spider.py
@@ -4,12 +4,11 @@ from scrapy.spiders import CrawlSpider, Rule
 
 class KithSpider(CrawlSpider):
     name = "kith"
-    start_urls = ['https://kith.com/',
-                  'https://kith.com/pages/women/',
-                  'https://kith.com/pages/kids/']
+    start_urls = ['https://kith.com/']
     allowed_domains = ['kith.com']
     DOWNLOAD_DELAY = 0.5
     rules = (
+        Rule(LinkExtractor(restrict_css='ul.ksplash-header-upper-items')),
         Rule(LinkExtractor(restrict_css='ul li.main-nav-list-item')),
         Rule(LinkExtractor(restrict_css='a.product-card-info'), callback="parse_products"),
     )
@@ -34,16 +33,17 @@ class KithSpider(CrawlSpider):
         return response.css(css).extract_first().strip()
 
     def get_retailer_sku(self, response):
-        css = 'div#notify-wrapper form input#product_id::attr(value)'
+        css = 'input#product_id::attr(value)'
         return response.css(css).extract_first()
 
     def get_url(self, response):
         return response.url
 
     def get_gender(self, response):
-        product_header = self.get_name(response)
-        if "Kidset" in product_header:
-            return "unisex"
+        product_name = self.get_name(response)
+        if "Kidset" in product_name:
+            return "unisex - kids"
+
         css = 'nav.breadcrumb a::attr(href)'
         product_header = response.css(css).extract_first().strip()
         if "women" in product_header:
@@ -51,8 +51,8 @@ class KithSpider(CrawlSpider):
         return "men"
 
     def get_description(self, response):
-        css1 = 'div.product-single-details-dropdown div p::text'
-        css2 = 'div.product-single-details-dropdown div ul li::text'
+        css1 = '.product-single-details-dropdown div p::text'
+        css2 = '.product-single-details-dropdown div ul li::text'
         description1 = response.css(css1).extract()
         description2 = response.css(css2).extract()
         description = description1 + description2
@@ -61,16 +61,21 @@ class KithSpider(CrawlSpider):
 
     def get_skus(self, response):
         skus = {}
-        sizes_css = 'div.product-single-form-wrapper form div select option::text'
+        sizes_css = '.product-single-form-wrapper form div select option::text'
         sizes = response.css(sizes_css).extract()
-        products_css = 'div.product-single-form-wrapper form div select option::attr(value)'
-        product_ids = response.css(products_css).extract()
+
+        products_id_css = '.product-single-form-wrapper form div select option::attr(value)'
+        product_ids = response.css(products_id_css).extract()
+
         currency_css = 'div.product-single-header meta::attr(content)'
         currency = response.css(currency_css).extract_first()
+
         color_css = 'div.product-single-header div span::text'
         color = response.css(color_css).extract_first().strip()
+
         price_css = 'span.product-header-title -price::attr(content)'
         price = response.css(price_css).extract_first()
+
         for product_id, size in zip(product_ids, sizes):
             skus[product_id] = {}
             skus[product_id]["colour"] = color
