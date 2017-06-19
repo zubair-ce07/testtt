8f684625d3dcf8faef78993e0ecb30c3f48411d1
Faizan-Shiraz <faizan.shiraz@arbisoft.com>
2017-06-19 18:42:25 +0500

    Hype DC spider V2.2

--- hype.py
+++ hype.py
@@ -3,41 +3,43 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 
-class HypeDC(scrapy.Spider):
-    name = "hypedc"
+class HypeDCProducts(scrapy.Item):
+    brand = scrapy.Field()
+    name = scrapy.Field()
+    category = scrapy.Field()
+    description = scrapy.Field()
+    other_colors = scrapy.Field()
+    breadcrumb = scrapy.Field()
+    images = scrapy.Field()
+    url = scrapy.Field()
+    skus = scrapy.Field()
+
+
+class HypeDCSpider(CrawlSpider):
+    name = "hypedc2"
     start_urls = [
         'https://www.hypedc.com/',
     ]
     products_name_repository = []
-
-    def parse(self, response):
-        extractor = LinkExtractor(allow=(), allow_domains='hypedc.com')
-        links = extractor.extract_links(response)
-        for link in links:
-            yield scrapy.Request(link.url, callback=self.parse_item)
-
-    def parse_item(self, response):
-        for product_url in response.css('div.item > a::attr(href)').extract():
-            yield scrapy.Request(product_url, callback=self.parse_product)
+    extractor = LinkExtractor(restrict_css='header.header-primary div.navbar div.container nav.navbar-collapse ul > '
+                                           'li.dropdown a', allow_domains='hypedc.com')
+    rules = (Rule(extractor), Rule(LinkExtractor(restrict_css='div.item'), callback="parse_product"))
 
     def parse_product(self, response):
         identifier = self.get_identifier(response)
         if identifier not in self.products_name_repository:
             self.products_name_repository.append(identifier)
-            yield {
-                'Brand': self.get_brand(response),
-                'Name': self.get_name(response),
-                'Gender': self.get_gender(response),
-                'Description': self.get_description(response),
-                'Other Colors': self.get_other_colors(response),
-                'Breadcrumb': self.get_breadcrumb(response),
-                'Img-Src': self.get_img_src(response),
-                'URL': self.get_url(response),
-                'Skus': self.get_skus(response),
-            }
-        next_page = response.css('a.next::attr(href)').extract_first()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse_product)
+            product = HypeDCProducts()
+            product['brand'] = self.get_brand(response)
+            product['name'] = self.get_name(response)
+            product['category'] = self.get_gender(response)
+            product['other_colors'] = self.get_other_colors(response)
+            product['breadcrumb'] = self.get_breadcrumb(response)
+            product['images'] = self.get_images(response)
+            product['url'] = response.url
+            product['skus'] = self.get_skus(response)
+            print(product['brand'])
+            yield product
 
     def get_identifier(self, response):
         name = response.css('h1.product-name::text').extract_first()
@@ -60,11 +62,8 @@ class HypeDC(scrapy.Spider):
     def get_breadcrumb(self, response):
         return response.css('ul.breadcrumb li a span::text').extract()
 
-    def get_img_src(self, response):
+    def get_images(self, response):
         return response.css('div.slider-inner div.unveil-container noscript img::attr(src)').extract()
-
-    def get_url(self, response):
-        return response.url
 
     def get_gender(self, response):
         return response.css('ul.breadcrumb li a span::text').extract()[1]
@@ -73,18 +72,18 @@ class HypeDC(scrapy.Spider):
         skus = {}
         currency = response.css('meta[itemprop="priceCurrency"]::attr(content)').extract_first()
         price = response.css('div.price-div meta::attr(content)').extract_first()
-        color = (response.css('h3.product-colour::text').extract_first()).strip()
+        color = response.css('h3.product-colour::text').extract_first().strip()
         size_groups = response.css('div#product-options-wrapper > ul#size-selector-desktop-tabs li a::text').extract()
         size_selectors = response.css('div.tab-content div.tab-pane::attr(id)').extract()
         sizes = []
-        for s in size_selectors:
-            sizes.append(response.css('div.tab-content div#' + s + ' ul li[data-stock="in"] a::text').extract())
-        for g in size_groups:
+        for selector in size_selectors:
+            sizes.append(response.css('div.tab-content div#' + selector + ' ul li[data-stock="in"] a::text').extract())
+        for group in size_groups:
             for size in sizes:
-                for s in size:
-                    skus[color + g + s] = {}
-                    skus[color + g + s]["colour"] = color
-                    skus[color + g + s]["currency"] = currency
-                    skus[color + g + s]["price"] = price
-                    skus[color + g + s]["size"] = s
+                for size_individual in size:
+                    skus[color + group + size_individual] = {}
+                    skus[color + group + size_individual]["colour"] = color
+                    skus[color + group + size_individual]["currency"] = currency
+                    skus[color + group + size_individual]["price"] = price
+                    skus[color + group + size_individual]["size"] = size_individual
         return skus
