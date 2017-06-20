6c55091b05fb78500215b1ae827827281502f7ca
ahmad ashraf <ahmad.ashraf@arbisoft.com>
2017-06-20 17:16:34 +0500

    changes made as suggested in review

--- Richards/richards_Spider.py
+++ Richards/richards_Spider.py
@@ -11,7 +11,7 @@ class Mixin:
     lang = 'pt'
     market = 'BR'
     start_urls = ['http://www.richards.com.br']
-    category_IDs = [560931917, 3056766704, 43564721, 1005777739]
+    category_ids = [560931917, 3056766704, 43564721, 1005777739]
     gender_map = [
         ('1005777739', 'men'),
         ('43564721', 'women'),
@@ -24,40 +24,33 @@ class RichardsParseSpider(BaseParseSpider, Mixin):
 
     def parse(self, response):
 
-        product = self.clean_json(response)
-        sku_id = product['skuId']
+        raw_product = self.get_json(response)
+        sku_id = raw_product['skuId']
         garment = self.new_unique_garment(sku_id)
         if not garment:
             return
         self.boilerplate_minimal(garment, response)
-        garment['gender'] = self.detect_gender_from_tokens(str(response.meta['id']), gender_map=Mixin.gender_map)
-        garment['name'] = product['name']
+        garment['gender'] = self.detect_gender_from_tokens(str(response.meta['id']), gender_map=self.gender_map)
+        garment['name'] = raw_product['name']
         garment['brand'] = self.brand_name(garment['name'])
-        garment['url'] = product['url']
-        garment['description'] = self.product_description(product['details'])
-        garment['care'] = self.product_care(product['details'])
+        garment['url'] = raw_product['url']
+        garment['description'] = self.product_description(raw_product['details'])
+        garment['care'] = self.product_care(raw_product['details'])
         garment['skus'] = {}
         garment['image_urls'] = []
 
-        color_skus = self.get_color_skus(product['colorList'])
-        currency = self.get_currency(response)
-        garment['meta'] = {'requests_queue': self.get_request(product['id'], color_skus, currency)}
+        currency = self.currency(response)
+        garment['meta'] = {'requests_queue': self.color_request(raw_product['id'], raw_product['colorList'], currency)}
 
         return self.next_request_or_garment(garment)
-
-    def get_color_skus(self, color_list):
-        color_skus = []
-        for color in color_list:
-            color_skus.append(color['skuId'])
-        return color_skus
 
     def request_params(self, sku, prod_id):
         return {'imageProperties': 'thumb,large,zoom', 'productId': prod_id, 'selectedSkuId': sku}
 
-    def get_request(self, prod_id, color_skus, currency):
+    def color_request(self, prod_id, color_list, currency):
         requests = []
-        for skus in color_skus:
-            params = self.request_params(skus, prod_id)
+        for color in color_list:
+            params = self.request_params(color['skuId'], prod_id)
             url_color = self.create_url(params)
             request = Request(url=url_color, callback=self.parse_color)
             request.meta['currency'] = currency
@@ -65,11 +58,10 @@ class RichardsParseSpider(BaseParseSpider, Mixin):
 
         return requests
 
-    def get_currency(self, response):
+    def currency(self, response):
         return CurrencyParser.currency(clean(response.css('meta[property="og:price:currency"]::attr(content)'))[0])
 
     def brand_name(self, name):
-        name = name.split(' ')
         if "BIARRITZ" in name:
             return "BIARRITZ"
         elif "BIRKENSTOCK" in name:
@@ -79,46 +71,46 @@ class RichardsParseSpider(BaseParseSpider, Mixin):
     def parse_color(self, response):
 
         garment = response.meta['garment']
-        json_data = json.loads(response.text)
+        raw_product = json.loads(response.text)
         currency = response.meta['currency']
 
-        garment['skus'] = self.skus(garment['skus'], json_data, currency)
-        garment['image_urls'] = garment['image_urls'] + self.image_urls(json_data['mediaSets'])
+        garment['skus'] = self.skus(garment['skus'], raw_product, currency, response)
+        garment['image_urls'] = garment['image_urls'] + self.image_urls(raw_product['mediaSets'])
 
         return self.next_request_or_garment(garment)
 
-    def product_description(self, product):
-        return [rd for rd in self.raw_description(product) if not self.care_criteria(rd)]
+    def product_description(self, raw_product):
+        return [rd for rd in self.raw_description(raw_product) if not self.care_criteria(rd)]
 
-    def product_care(self, product):
-        return [rd for rd in self.raw_description(product) if self.care_criteria(rd)]
+    def product_care(self, raw_product):
+        return [rd for rd in self.raw_description(raw_product) if self.care_criteria(rd)]
 
-    def raw_description(self, product):
-        description_tab, details_tab = [], []
-        if 'description' in product.keys():
-            description_tab = [product['description']['value']]
-        if 'details' in product.keys():
-            details_tab = [product['details']['value']]
-        return description_tab+details_tab
+    def raw_description(self, raw_product):
+        description, details = [], []
+        if 'description' in raw_product.keys():
+            description = [raw_product['description']['value']]
+        if 'details' in raw_product.keys():
+            details = [raw_product['details']['value']]
+        return description+details
 
-    def image_urls(self, json_data):
-        img_url = []
-        for image in json_data:
-            img_url.append(image['zoom'])
-        return img_url
+    def image_urls(self, images):
+        return [img['zoom'] for img in images]
 
-    def skus(self, skus, json_data, currency):
+    def skus(self, skus, raw_product, currency, response):
 
-        colors = json_data['colorList']
+        colors = raw_product['colorList']
         sizes = []
-        if 'sizeList' in json_data.keys():
-            sizes = json_data['sizeList']
-        color_sku = json_data['skuId']
+        price = [raw_product['atualPrice'], currency]
+        price = self.product_pricing_common_new(response, money_strs=price)
+
+        if 'sizeList' in raw_product.keys():
+            sizes = raw_product['sizeList']
+        color_sku = raw_product['skuId']
         color_name = self.color_name(colors, color_sku)
 
         for size in sizes:
-            skus[color_sku + "_" + size['skuId']] = {"size": size['name'], 'price': int(json_data['atualPrice'])*100,
-                                                     'currency': currency, 'out_of_stock': size['hasStock'],
+            skus[color_sku + "_" + size['skuId']] = {"size": size['name'], 'price': price['price'],
+                                                     'currency': price['currency'], 'out_of_stock': size['hasStock'],
                                                      'color': color_name}
         return skus
 
@@ -130,15 +122,11 @@ class RichardsParseSpider(BaseParseSpider, Mixin):
     def create_url(self, params):
         return 'http://www.richards.com.br/services/get-complete-product.jsp?' + urllib.parse.urlencode(params)
 
-    def clean_json(self, response):
-        json_string = self.get_json(response)
-        return json.loads(json_string)
-
     def get_json(self, response):
         script = response.xpath('//script[contains(., "var globalProduct =")]/text()').extract()
         if script:
             raw_data = re.findall('var globalProduct = (.*);', script[0])[0]
-            return raw_data
+            return json.loads(raw_data)
 
 
 class RichardsCrawlSpider(BaseCrawlSpider, Mixin):
@@ -148,18 +136,18 @@ class RichardsCrawlSpider(BaseCrawlSpider, Mixin):
 
     def parse(self, response):
         params = {'recsPerPage': 1000}
-        for ids in Mixin.category_IDs:
+        for ids in self.category_ids:
             params.update({'N': int(ids)})
             url_params = urllib.parse.urlencode(params)
-            url = Mixin.start_urls[0] + '/services/records.jsp?' + url_params
+            url = self.start_urls[0] + '/services/records.jsp?' + url_params
             request = Request(url=url, callback=self.parse_urls)
             request.meta["id"] = int(ids)
             yield request
 
     def parse_urls(self, response):
-        json_dict = json.loads(response.text)
+        product_links = json.loads(response.text)
         ids = response.meta["id"]
-        for record in json_dict['records']:
+        for record in product_links['records']:
             request = Request(url=record['url'], callback=self.parse_item)
             request.meta["id"] = ids
             yield request
