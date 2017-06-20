855d2e043efae0a945f4570438301672daa15ec2
ahmedfaraz <ahmed.faraz0046@gmail.com>
2017-06-20 09:20:58 +0700

    1st code review handeled except link extractor

--- ernstingsfamily/ernstingsfamily/spiders/ernstings_family.py
+++ ernstingsfamily/ernstingsfamily/spiders/ernstings_family.py
@@ -1,48 +1,56 @@
 import scrapy
+from scrapy.spiders import Rule
+
 from ernstingsfamily.items import Product, StoreKeepingUnits
+from scrapy.linkextractors import LinkExtractor
 
 class ErnstingsFamilySpider(scrapy.Spider):
     name = 'ernstings_family'
     allowed_domains = ['www.ernstings-family.de']
     start_urls = ['http://www.ernstings-family.de']
 
+    rules = [
+        Rule(
+            LinkExtractor(
+                restrict_xpaths=".//ul[@id='navi_main']",
+                canonicalize=True,
+                unique=True
+            ),
+            follow=True,
+            callback="parse"
+        )
+    ]
 
     def parse(self, response):
-
         for page in response.xpath(".//ul[@id='navi_main']/li/a"):
             url = page.xpath("./@href").extract_first()
-            yield scrapy.Request(url, callback=self.parse_a_tab)
+            #for l in response.xpath('//@href').extract():
+            #yield {'u': l}
+            yield scrapy.Request(url, callback=self.get_urls_of_given_category)
 
-
-    def parse_a_tab(self, response):
-
+    def get_urls_of_given_category(self, response):
         for item_page in response.xpath(".//li[contains(@id,'catList')]//a/@href").extract():
-            yield scrapy.Request(item_page, callback=self.parse_list_of_product)
+            yield scrapy.Request(item_page, callback=self.parse_products)
 
-
-    def parse_list_of_product(self, response):
-
+    def parse_products(self, response):
         for product in response.xpath('.//li[@class="list_product"]'):
-            url = 'http://www.ernstings-family.de' + product.xpath('.//div[@class=\'product_basic_content\']/a[1]//@href').extract_first()
+            url = response.urljoin(product.xpath('.//div[@class=\'product_basic_content\']/a//@href').extract_first())
+            yield scrapy.Request(url, callback=self.get_complete_details_of_single_product)
 
-            yield scrapy.Request(url, callback=self.image_urls_parser)
-
-    def image_urls_parser(self, response):
-
-        p = Product()
-        p['url'] = response.url
-        p['actual_price'] = response.xpath(".//strike/text()").extract_first()
-        p['discount_price'] = response.xpath("//*[@class='prd_price prd_new_price']/text()").extract_first()
-        p['description'] = response.xpath(".//p[@class='infotext']/text()").extract()
-        p['name'] = response.xpath(".//span[@class='prd_name']/text()[1]").extract_first()
-        p['description'] = p['description'][2]
+    def get_complete_details_of_single_product(self, response):
+        product_item = Product()
+        product_item['url'] = response.url
+        product_item['actual_price'] = response.xpath(".//strike/text()").extract_first()
+        product_item['discount_price'] = response.xpath("//*[@class='prd_price prd_new_price']/text()").extract_first()
+        product_item['description'] = response.xpath(".//p[@class='infotext']/text()").extract()
+        product_item['name'] = response.xpath(".//span[@class='prd_name']/text()[1]").extract_first()
+        product_item['description'] = product_item['description'][2]
         imgs = []
         for img in response.xpath(".//div[@id='prd_thumbs']/a"):
             imgs.append(img.xpath("./img/@src").extract_first())
 
-
-        p['image_urls'] = imgs
-        p['skus'] = []
+        product_item['image_urls'] = imgs
+        product_item['skus'] = []
         color = response.xpath("//script[@type='text/javascript']/text()").re(r'"Farbe":\["(.*)"]')
         color = color[0]
         sizes =  response.xpath("//script[@type='text/javascript']/text()").re(r'"Größe":\["(.*)"],')
@@ -50,11 +58,11 @@ class ErnstingsFamilySpider(scrapy.Spider):
         sizes = sizes.split(',')
         for s in sizes:
             sku = StoreKeepingUnits()
-            sku['actual_price'] = p['actual_price']
-            sku['discount_price'] = p['discount_price']
+            sku['actual_price'] = product_item['actual_price']
+            sku['discount_price'] = product_item['discount_price']
             sku['colour'] = color
-            sku['size'] = s #s.xpath("./span/text()") escaped for the time being because these things are rendering via javascript
-            sku['sku_id'] = p['name'] + '_' + sku['size']
-            p['skus'].append(sku)
+            sku['size'] = s
+            sku['sku_id'] = product_item['name'] + '_' + sku['size']
+            product_item['skus'].append(sku)
 
-        return p
+        return product_item
