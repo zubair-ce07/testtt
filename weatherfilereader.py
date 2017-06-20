8cf65af5eac43e499f83034681ab60f8916c772c
Wasiq Noor <wasiqnoorahmad@gmail.com>
2017-06-20 13:44:22 +0500

    fixing if conditions

--- weatherfilereader.py
+++ weatherfilereader.py
@@ -8,12 +8,12 @@ class WeatherFilesReader(object):
 
     retrieved_records = None
 
-    def __init__(self, dir=None):
-        self.dir = dir
+    def __init__(self, directory=None):
+        self.dir = directory
         self.retrieved_records = {}
 
-    def set_directory(self, dir):
-        self.dir = dir
+    def set_directory(self, directory):
+        self.dir = directory
 
     def get_filenames_by_month(self, year, month):
         return fnmatch.filter(os.listdir(self.dir), '*' + year + '_' + calendar.month_abbr[int(month)] + '.txt')
@@ -30,21 +30,21 @@ class WeatherFilesReader(object):
                 with open(os.path.join(self.dir, weather_file)) as csvfile:
                     reader = csv.DictReader(csvfile)
                     for row in reader:
-                        if not (row['Max TemperatureC'] == '' or row['Min TemperatureC'] == ''
-                                or row['Max Humidity'] == ''):
-                            records[row['PKT']] = {'max_temp':row['Max TemperatureC'],
-                                                   'min_temp':row['Min TemperatureC'],
-                                                   'max_humid':row['Max Humidity']}
+                        if (row['Max TemperatureC'] or row['Min TemperatureC']
+                                or row['Max Humidity']):
+                            records[row['PKT']] = {'max_temp': row['Max TemperatureC'],
+                                                   'min_temp': row['Min TemperatureC'],
+                                                   'max_humid': row['Max Humidity']}
             self.update_retrieved_records(records)
         else:
-            records = {key:value for key, value in self.retrieved_records.iteritems()
+            records = {key: value for key, value in self.retrieved_records.iteritems()
                        if key.startswith(year) and '-' + month + '-' in key}
 
         return records
 
     def read_by_year(self, year):
         record = {}
-        for month in range(1,13):
+        for month in range(1, 13):
             record.update(self.read_by_month(year, str(month)))
         return record
 
