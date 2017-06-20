35203f21873e62a1e38b8ded1580c7028fe74bba
ahmedfaraz <ahmed.faraz0046@gmail.com>
2017-06-21 04:59:33 +0700

    css selectors + link extracto

--- ernstingsfamily/ernstingsfamily/spiders/ernstings_family.py
+++ ernstingsfamily/ernstingsfamily/spiders/ernstings_family.py
@@ -1,33 +1,32 @@
 import scrapy
 from scrapy.spiders import Rule, CrawlSpider
-from scrapy.utils.response import open_in_browser
 from ernstingsfamily.items import Product, StoreKeepingUnits
 from scrapy.linkextractors import LinkExtractor
+
 
 class ErnstingsFamilySpider(CrawlSpider):
     name = 'ernstings_family'
     allowed_domains = ['ernstings-family.de']
     start_urls = ['http://www.ernstings-family.de/']
 
+
     rules = (
-        Rule(LinkExtractor(restrict_xpaths=".//ul[@id='navi_main']/li/a",)),
-        Rule(LinkExtractor(restrict_xpaths=".//li[contains(@id,'catList')]//a" )),
-        Rule(LinkExtractor(restrict_xpaths="..//div[@class='product_basic_content']/a"),
-             callback='get_complete_details_of_single_product')
+        Rule(LinkExtractor(restrict_css="ul[id='navi_main'] > li > a")),
+        Rule(LinkExtractor(restrict_css="li[id*='catList'] > a")),
+        Rule(LinkExtractor(restrict_css="div[class='product_basic_content'] > a"),
+             callback='get_complete_details_of_product')
     )
 
-
-    def get_complete_details_of_single_product(self, response):
+    def get_complete_details_of_product(self, response):
         product_item = Product()
         product_item['url'] = response.url
-        product_item['actual_price'] = response.xpath(".//strike/text()").extract_first()
-        product_item['discount_price'] = response.xpath("//*[@class='prd_price prd_new_price']/text()").extract_first()
-        product_item['description'] = response.xpath(".//p[@class='infotext']/text()").extract()
-        product_item['name'] = response.xpath(".//span[@class='prd_name']/text()[1]").extract_first()
-        product_item['description'] = product_item['description'][2]
+        product_item['actual_price'] = response.css("strike::text").extract_first()
+        product_item['discount_price'] = response.css("span#prd_price::text").extract_first()
+        product_item['description'] = response.css("p[class=infotext]::text").extract()[2]
+        product_item['name'] = response.css("span[class=prd_name]::text").extract_first()
         imgs = []
-        for img in response.xpath(".//div[@id='prd_thumbs']/a"):
-            imgs.append(img.xpath("./img/@src").extract_first())
+        for img in response.css("div[id=prd_thumbs] > a"):
+            imgs.append(img.css("img::attr(src)").extract_first())
 
         product_item['image_urls'] = imgs
         product_item['skus'] = []
