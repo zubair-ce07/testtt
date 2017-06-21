0f748ac45c229e9e0a168eb42b08d7122aa9a315
hammadahmadwaqas <hammadahmadwaqas@github.com>
2017-06-21 13:34:23 +0500

    scraping menatwork.nl shoping site's "DAMES" (Ladies) portion products. every product contains product name and a dictionary contains of avaible sizes list with respect to their availble color

--- menatwork_scraper/menatwork_scraper/spiders/menatwork_spider.py
+++ menatwork_scraper/menatwork_scraper/spiders/menatwork_spider.py
@@ -1,3 +1,5 @@
+import scrapy
+import w3lib.url
 from scrapy.contrib.linkextractors import LinkExtractor
 from scrapy.contrib.spiders.crawl import CrawlSpider, Rule
 
@@ -9,7 +11,7 @@ class MySpider(CrawlSpider):
     start_urls = ['https://www.menatwork.nl']
     rules = (
         Rule(
-            LinkExtractor(allow='.*nl_NL/dames/nieuwe-collectie.*')
+            LinkExtractor(allow='.*nl_NL/dames/.*', process_value=w3lib.url.url_query_cleaner)
         ),
         Rule(
             LinkExtractor(allow='.+\d{18}.html.+'),
@@ -18,7 +20,37 @@ class MySpider(CrawlSpider):
     )
 
     def parse_product_page(self, response):
-        # yield product item here
-        a_prodect = Product()
-        a_prodect['name'] = response.xpath('//h1[@class="product-name"]/text()').extract_first()
-        yield a_prodect
+
+        a_product = Product()
+        a_product['name'] = response.xpath('//h1[@class="product-name"]/text()').extract_first()
+        a_product['size'] = {}
+
+        product_color = response.css('li.selected-value::text').extract_first()
+        size_variants = response.xpath('//select[@class="variation-select "]/option/text()').extract()
+        size_variants = list(map(str.strip, size_variants))
+
+        a_product['size'][product_color] = size_variants
+
+        more_size_urls = response.xpath('//ul[contains(@class,"color")]/li[@class="selectable"]/a/@href').extract()
+        if more_size_urls:
+            size_url = more_size_urls.pop()
+            yield scrapy.Request(url=size_url, callback=self.get_product_size,
+                                 meta={'product': a_product, 'urls': more_size_urls})
+        else:
+            yield a_product
+
+    def get_product_size(self, response):
+        a_product = response.meta['product']
+        more_size_urls = response.meta['urls']
+
+        product_color = response.css('li.selected-value::text').extract_first()
+        size_variants = response.xpath('//select[@class="variation-select "]/option/text()').extract()
+        size_variants = list(map(str.strip, size_variants))
+        a_product['size'][product_color] = size_variants
+
+        if more_size_urls:
+            size_url = more_size_urls.pop()
+            yield scrapy.Request(url=size_url, callback=self.get_product_size,
+                                 meta={'product': a_product, 'urls': more_size_urls})
+        else:
+            yield a_product
