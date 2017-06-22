0cc76641fe8bd7bbd236385100c1b473d8d3ac8b
Safi.Ullah <safi.ullah@arbisoft.com>
2017-06-22 12:16:33 +0500

    mysite completed

--- mysite/training/urls.py
+++ mysite/training/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.training_index),
     url(r'^trainee/(?P<trainee_id>[0-9]+)/$', views.trainee_details),
     url(r'^trainer/(?P<trainer_id>[0-9]+)/$', views.trainer_details),
-    url(r'^assignment/(?P<assignment_id>[0-9]+)/$', views.assignment_details)
+    url(r'^assignment/(?P<assignment_id>[0-9]+)/$', views.assignment_details),
+    url(r'^technology/(?P<technology_id>[0-9]+)/$', views.technology_details)
 ]
