4b7bcf91113f020027ff9f46ebddd53e28068239
Wasiq Noor <wasiqnoorahmad@gmail.com>
2017-06-23 12:11:01 +0500

    Omitting commented out

--- items.py
+++ items.py
@@ -1,16 +1,9 @@
-# -*- coding: utf-8 -*-
 
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
 
 
 class ProductItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
     description = scrapy.Field()
     retailer_sku = scrapy.Field()
     image_urls = scrapy.Field()
