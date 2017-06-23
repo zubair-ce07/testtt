9b025cf1301db191dc0df03ade2d64fe4b8d7a14
Safi.Ullah <safi.ullah@arbisoft.com>
2017-06-23 17:54:28 +0500

    Login and signup functionality implemented

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '4_&%u85qya9t*^luqcr5s*ucz3xlz*5ouq_h8x-4xp4ez40t(x'
-
+# STATIC_URL = '/home/safi/Python Projs/the-lab/mysite/'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
--- mysite/training/views.py
+++ mysite/training/views.py
@@ -1,23 +1,36 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import Trainee, Trainer, Technology, Assignment
-from forms import LoginForm
+from forms import LoginForm, SignUpForm
+from django.contrib.auth import authenticate, login as django_login, logout as django_logout
 from django.contrib.auth.models import User
+from django.urls import reverse, reverse_lazy
 
 
 def training_index(request):
+    if not request.user.is_authenticated:
+        return redirect(reverse("login"))
     trainers_list = Trainer.objects.all()
     template = loader.get_template('training/training_index.html')
+    name = ""
+    if request.user.first_name:
+        name = request.user.first_name
+    else:
+        name = request.user.username
+
     context = {
+        'user_name': name,
         'trainers_list': trainers_list,
     }
     return HttpResponse(template.render(context, request))
 
 
 def trainee_details(request, trainee_id):
+    if not request.user.is_authenticated:
+        return redirect(reverse("login"))
     trainee = Trainee.objects.get(id=trainee_id)
     template = loader.get_template('training/trainee_details.html')
     context = {
@@ -28,6 +41,8 @@ def trainee_details(request, trainee_id):
 
 
 def trainer_details(request, trainer_id):
+    if not request.user.is_authenticated:
+        return redirect(reverse("login"))
     trainer = Trainer.objects.get(id=trainer_id)
     template = loader.get_template('training/trainer_details.html')
     context = {
@@ -39,6 +54,8 @@ def trainer_details(request, trainer_id):
 
 
 def assignment_details(request, assignment_id):
+    if not request.user.is_authenticated:
+        return redirect(reverse("login"))
     assignment = Assignment.objects.get(id=assignment_id)
     template = loader.get_template('training/assignment_details.html')
     context = {
@@ -48,6 +65,8 @@ def assignment_details(request, assignment_id):
 
 
 def technology_details(request, technology_id):
+    if not request.user.is_authenticated:
+        return redirect(reverse("login"))
     technology = Technology.objects.get(id=technology_id)
     template = loader.get_template('training/technology_details.html')
     context = {
@@ -57,6 +76,8 @@ def technology_details(request, technology_id):
 
 
 def search(request):
+    if not request.user.is_authenticated:
+        return redirect(reverse("login"))
     error = False
     if 'q' in request.GET:
         q = request.GET['q']
@@ -71,18 +92,53 @@ def search(request):
 
 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect(reverse('training_index'))
+
+    template_name = 'training/login.html'
+    if not request.POST:
+        form = LoginForm(None)
+        return render(request, template_name, {'form': form})
+
     form = LoginForm(request.POST)
     message = ''
+    username = ''
+    password = ''
     if form.is_valid():
         username = form.cleaned_data['username']
         password = form.cleaned_data['password']
     user = authenticate(username=username, password=password)
-    if user:
-        if user.is_active:
-            login(self.request, user)
-            # Redirect to a success page.
-            return render(request, 'training/training_index.html')
-    # if request.method == 'POST':
-    #     form = LoginForm(request.POST)
-    #     if form.is_valid():
-    #         cd = form.cleaned_data
+
+    if user and user.is_active:
+        django_login(request, user)
+        # Redirect to a success page.
+        return redirect(reverse(training_index))
+    else:
+        message = "login failed"
+    return render(request, template_name, {'error': message,
+                                           'form': form})
+
+
+def signup(request):
+    template_name = 'training/signup.html'
+    if request.user.is_authenticated:
+        return redirect(reverse('training_index'))
+    if not request.POST:
+        form = SignUpForm(None)
+        # ?return HttpResponse("GET Req")
+        return render(request, template_name, {'form': form})
+    form = SignUpForm(request.POST)
+
+    if form.is_valid():
+        user = form.save(commit=False)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user.set_password(password)
+        user.save()
+        return redirect(reverse('login'))
+    return render(request, template_name, {'form': form})
+
+
+def logout(request):
+    django_logout(request)
+    return redirect(reverse('login'))
