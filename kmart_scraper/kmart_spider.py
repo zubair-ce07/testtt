f16fc55c872c4a0088e4712539d709747a88d42c
ubair-hamid <ubair.hamid@arbisoft.com>
2017-06-29 23:19:12 +0500

    code improvements
    code improvements

--- kmart_scraper/kmart_spider.py
+++ kmart_scraper/kmart_spider.py
@@ -14,7 +14,7 @@ class KmartSpider(CrawlSpider):
     start_urls = ["http://www.kmart.com.au"]
     pagination_regex = re.compile(r"\'http.*?\'")
     categories_selector = '.mega-menu li>a'
-    item_card_selector = '.product_image>a'
+    item_card_path = '.product_image>a'
     allowed_categories = ['/category/men/mens-clothing/',
                           '/category/women/womens-clothing/',
                           '/kids-clothing/',
@@ -24,7 +24,7 @@ class KmartSpider(CrawlSpider):
     rules = (
         Rule(LinkExtractor(allow=allowed_categories, restrict_css=[categories_selector]), callback="handle_pagination",
              follow=True),
-        Rule(LinkExtractor(restrict_css=[item_card_selector]), callback="parse_item"),
+        Rule(LinkExtractor(restrict_css=item_card_path), callback="parse_item"),
     )
 
     def handle_pagination(self, response):
@@ -52,23 +52,50 @@ class KmartSpider(CrawlSpider):
 
     def parse_item(self, response):
         item = KmartItem()
+
+        retailer_sku = self.get_retailer_sku(response)
+        if not retailer_sku:
+            return
+
+        item['retailer_sku'] = retailer_sku
         item['name'] = self.get_name(response)
+        item['price'] = self.get_price(response)
+        item['category'] = self.get_category(response)
         item['image_urls'] = self.get_image_urls(response)
         item['description'] = self.get_description(response)
-        item['price'] = self.get_price(response)
+        item['gender'] = self.get_gender(item['category'])
         item['skus'] = self.get_skus(response) or self.get_default_sku(item['price'])
         item['url'] = response.url
+        item['brand'] = 'Kmart'
+
         return item
 
     def get_name(self, response):
-        name_selector = '.h2[itemprop = "name"] ::text'
-        return response.css(name_selector).extract_first()
+        name_path = '.h2[itemprop = "name"] ::text'
+        return response.css(name_path).extract_first()
+
+    def get_retailer_sku(self, response):
+        sku = response.css('span.sku ::text').extract_first() or ''
+        return sku.replace('SKU: ', '')
+
+    def get_gender(self, category):
+        if "Women" in category:
+            return 'women'
+        elif "Men" in category:
+            return 'men'
+        else:
+            return 'kids'
+
+    def get_category(self, response):
+        category_path = '.columns h1[clas="h2"] ::text'
+        return response.css(category_path).extract_first() or 'N/A'
 
     def get_image_urls(self, response):
-        images_selector = '.multipleimages + input ::attr(value)'
-        default_image_selector = '#productMainImage ::attr(src)'
-        image_urls = response.css(images_selector).extract() or \
-                     response.css(default_image_selector).extract()
+        images_path = '.multipleimages + input ::attr(value)'
+        default_image_path = '#productMainImage ::attr(src)'
+        image_urls = \
+            response.css(images_path).extract() or \
+            response.css(default_image_path).extract()
         return [self.start_urls[0] + s for s in image_urls]
 
     def get_description(self, response):
@@ -81,23 +108,24 @@ class KmartSpider(CrawlSpider):
 
     def get_skus(self, response):
         skus_json_selector = '#catEntryParams ::attr(value)'
-        json_wo_quotes = response.css(skus_json_selector).extract_first()
+        json_wo_quotes = response.css(skus_json_selector).extract_first() or {}
 
         skus_json = yaml.load(json_wo_quotes.replace("'", '"'))
-        skus_data = skus_json['skus']
+        skus_data = skus_json.get('skus', [])
         price = self.get_price(response)
 
         skus = []
         for sku in skus_data:
-            colour = sku['attributes'].get('Colour', 'N/A')
-            size = sku['attributes'].get('Size', 'N/A')
+            size = sku['attributes'].get('Size', 'one-size')
             curr_sku = {
-                "colour": colour,
+                "size": size,
                 "currency": "AUD",
                 "price": price,
                 "sku_id": sku['id'],
-                "size": size
             }
+            colour = sku['attributes'].get('Colour')
+            if colour:
+                curr_sku['colour'] = colour
             skus.append(curr_sku)
 
         return skus
