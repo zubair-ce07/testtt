51180aaedaacbd312d434e28c54930772dd6c090
Safi.Ullah <safi.ullah@arbisoft.com>
2017-06-29 19:09:46 +0500

    review changes made

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, url, static
+from django.conf import settings
 from django.contrib import admin
 from mysite.views import hello
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^training/', include('training.urls'))
-    # url(r'^hello/$', hello)
-]
+] + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mysite/training/urls.py
+++ mysite/training/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
-    url(r'^trainee/(?P<trainee_id>[0-9]+)/$', views.trainee_details),
-    url(r'^trainer/(?P<trainer_id>[0-9]+)/$', views.trainer_details),
-    url(r'^assignment/(?P<assignment_id>[0-9]+)/$', views.assignment_details),
-    url(r'^technology/(?P<technology_id>[0-9]+)/$', views.technology_details),
-    url(r'^search/$', views.search),
+    url(r'^trainee/(?P<trainee_id>[0-9]+)/$', views.trainee_details,
+        name="trainee_details"),
+    url(r'^trainer/(?P<trainer_id>[0-9]+)/$', views.trainer_details,
+        name="trainer_details"),
+    url(r'^assignment/(?P<assignment_id>[0-9]+)/$', views.assignment_details,
+        name="assignment_details"),
+    url(r'^technology/(?P<technology_id>[0-9]+)/$', views.technology_details,
+        name="technology_details"),
+    url(r'^search/$', views.search, name="search"),
     url(r'^signup/$', views.signup, name="signup"),
     url(r'^training_index/$', views.training_index, name="training_index"),
     url(r'^logout/$', views.logout, name="logout"),
