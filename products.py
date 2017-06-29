595829f43df4cac6552ce0d0ebe048c15b4bbbb2
Wasiq Noor <wasiqnoorahmad@gmail.com>
2017-06-29 17:42:13 +0500

    Incoperating suggestions

--- products.py
+++ products.py
@@ -9,48 +9,41 @@ class AlexaChungSpider(CrawlSpider):
     name = 'alexachung'
     allowed_domains = ['alexachung.com']
     start_urls = [
-        'https://www.alexachung.com/uk/',
+        'https://www.alexachung.com/uk',
     ]
 
     rules = [Rule(LinkExtractor(restrict_css='.sub-menu')),
-             Rule(LinkExtractor(allow=['.*[a-z]+$'], restrict_css='.products.wrapper.grid.products-grid')),
-             Rule(LinkExtractor(allow=['.*\/[a-z-0-9]+$']), callback='parse_products')
+             Rule(LinkExtractor(restrict_css='.product-items'), callback='parse_products')
              ]
 
     def get_colour(self, response):
-        _, colour, _ = self.get_url(response).rsplit('-', 2)
+        _, colour, _ = response.url.rsplit('-', 2)
         return colour
 
     def get_skus(self, response):
-        item_details = {}
         try:
-            _, item_details, _ = response.css('.fieldset script::text').extract()
-        except ValueError:
-            pass
-        if item_details:
-            item_details = json.loads(item_details)['[data-role=swatch-options]']['Magento_Swatches/js/swatch-renderer']
-            item_product_ids = item_details['jsonConfig']['index']
-            product_labels = item_details['jsonSwatchConfig']
-            price = self.get_price(response)
-            for product_key, product_details in item_product_ids.iteritems():
-                item_product_ids[product_key].update({'size':
-                                                        product_labels[next(iter(product_labels.keys()))][next(iter(
-                                                        product_details.values()))]['value']})
-                item_product_ids[product_key].update({'price': price})
-                item_product_ids[product_key].update({'colour': self.get_colour(response)})
-                item_product_ids[product_key].update({'currency': 'GBP'})
-                return item_product_ids
-        return {}
+            item_details = json.loads(response.css('.swatch-opt + script::text').
+                                      extract_first())['[data-role=swatch-options]']\
+                                                        ['Magento_Swatches/js/swatch-renderer']
+        except TypeError:
+            return {}
+
+        skus = item_details['jsonConfig']['index']
+        size_per_sku = item_details['jsonSwatchConfig']
+        sizes_per_type = {}
+        for sku_id in size_per_sku:
+            for sku_type, sku_info in size_per_sku[sku_id].iteritems():
+                sizes_per_type.update({sku_info['value']: {sku_id: sku_type}})
+        for sku, size in zip(skus, sizes_per_type):
+            skus[sku] = {'size': size, 'price': self.get_price(response),
+                         'colour': self.get_colour(response), 'currency': 'GBP'}
+        return {'skus': skus}
 
     def get_retailer_sku(self, response):
-        product_id = response.css('.price-box.price-final_price')
-        return product_id.css('::attr(data-product-id)').extract_first()
+        return response.css('.price-final_price::attr(data-product-id)').extract_first()
 
     def get_price(self, response):
         return response.css('.price-wrapper .price::text').extract_first()
-
-    def get_url(self, response):
-        return response.url
 
     def get_name(self, response):
         return response.css('.page-title::text').extract_first()
@@ -68,9 +61,9 @@ class AlexaChungSpider(CrawlSpider):
         output['description'] = self.get_description(response)
         output['image_urls'] = self.get_image_urls(response)
         output['retailer_sku'] = self.get_retailer_sku(response)
-        output['name'] = self.get_name(response)
-        output['url'] = self.get_url(response)
         output['skus'] = self.get_skus(response)
+        output['name'] = self.get_name(response)
+        output['url'] = response.url
         output['gender'] = 'female'
         output['brand'] = 'Alexa Chung'
         output['date'] = time.strftime("%H:%M:%S")
