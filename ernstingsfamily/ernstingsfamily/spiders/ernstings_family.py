0d01779c20d0ab22fb5329ec1707d0cb6d2ad536
ahmedfaraz <ahmed.faraz0046@gmail.com>
2017-06-30 08:07:24 +0700

    Complete all reviews handled

--- ernstingsfamily/ernstingsfamily/spiders/ernstings_family.py
+++ ernstingsfamily/ernstingsfamily/spiders/ernstings_family.py
@@ -2,7 +2,7 @@ import scrapy
 from scrapy.spiders import Rule, CrawlSpider
 from ernstingsfamily.items import Product, StoreKeepingUnits
 from scrapy.linkextractors import LinkExtractor
-
+import urllib
 
 class ErnstingsFamilySpider(CrawlSpider):
     name = 'ernstings_family'
@@ -10,74 +10,70 @@ class ErnstingsFamilySpider(CrawlSpider):
     start_urls = ['http://www.ernstings-family.de/']
     rules = (
         Rule(LinkExtractor(restrict_css="ul[id='navi_main'] > li > a")),
-        Rule(LinkExtractor(restrict_css="li[id*='catList'] > a"), callback='lazy_parser'),
-
+        Rule(LinkExtractor(restrict_css="li[id*='catList'] > a"), callback='parse_pagination', follow=True),
+        Rule(LinkExtractor(restrict_css="div[class='product_basic_content'] > a"), callback='parse_detail'),
     )
 
-    def lazy_parser(self, response):
+    def parse_pagination(self, response):
         url = response.xpath("//script[@type='text/javascript']").re("endlessScrollingUrl': '(.*)'")
         limit = response.xpath(".//ul[@class='category_product_list']/@data-max-page").extract_first()
-        url = self.start_urls[0] + url[0][:-1]
+        url = urllib.parse.urljoin(self.start_urls[0], url[0][:-1])
         urls = []
-        for l in range(int(limit)):
-            urls.append(url+str(l+1))
+        for index in range(int(limit)):
+            urls.append(url+str(index+1))
 
-        for product in response.xpath('.//li[@class="list_product"]'):
-            url = response.urljoin(product.xpath('.//div[@class=\'product_basic_content\']/a//@href').extract_first())
-            urls.append(url)
-
-        for u in urls:
-            yield scrapy.Request(u, callback=self.parse_detail)
+        for url in urls:
+            yield scrapy.Request(url, callback=self.parse_detail)
 
     def parse_detail(self, response):
         product_item = Product()
-        product_item['url'] = self._get_url(response)
-        product_item['actual_price'] = self._get_actual_price(response)
-        product_item['discount_price'] = self._get_discount_price(response)
-        product_item['description'] = self._get_description(response)
-        product_item['name'] = self._get_name(response)
-        product_item['image_urls'] = self._get_images(response)
+        product_item['url'] = self.get_url(response)
+        product_item['actual_price'] = self.get_actual_price(response)
+        product_item['discount_price'] = self.get_discount_price(response)
+        product_item['description'] = self.get_description(response)
+        product_item['name'] = self.get_name(response)
+        product_item['image_urls'] = self.get_images(response)
 
         color = response.xpath("//script[@type='text/javascript']/text()").re(r'"Farbe":\["(.*)"]')
         color = color[0]
         sizes =  response.xpath("//script[@type='text/javascript']/text()").re(r'"Größe":\["(.*)"],')
         sizes = sizes[0]
         sizes = sizes.split(',')
-        product_item['skus'] = self._get_skus(sizes, product_item, color)
+        product_item['skus'] = self.get_skus(sizes, product_item, color)
 
         return product_item
 
-    def _get_url(self,response):
+    def get_url(self,response):
         return response.url
 
-    def _get_actual_price(self,response):
+    def get_actual_price(self,response):
         return response.css("strike::text").extract_first()
 
-    def _get_discount_price(self, response):
+    def get_discount_price(self, response):
         return response.css("span#prd_price::text").extract_first()
 
-    def _get_name(self, response):
+    def get_name(self, response):
         return response.css("span[class=prd_name]::text").extract_first()
 
-    def _get_description(self, response):
+    def get_description(self, response):
         return response.css("p[class=infotext]::text").extract()[2]
 
-    def _get_images(self, response):
+    def get_images(self, response):
         imgs = []
         for img in response.css("div[id=prd_thumbs] > a"):
             imgs.append(img.css("img::attr(src)").extract_first())
 
         return imgs
 
-    def _get_color(self, response):
+    def get_color(self, response):
         color= response.xpath("//script[@type='text/javascript']/text()").re(r'"Farbe":\["(.*)"]')
         return color[0]
 
-    def _get_sizes(self, response):
+    def get_sizes(self, response):
         sizes= response.xpath("//script[@type='text/javascript']/text()").re(r'"Größe":\["(.*)"],')
         return sizes[0]
 
-    def _get_skus(self, sizes, product_item,color):
+    def get_skus(self, sizes, product_item,color):
         skus = []
         for s in sizes:
             sku = StoreKeepingUnits()
