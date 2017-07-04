5760bcba73be982c9947b1a11c643b6a599eb4a5
Humdah Qamar <hqmannan@gmail.com>
2017-07-04 18:58:32 +0500

    Admin Site for Posts

--- insta/instagram/admin.py
+++ insta/instagram/admin.py
@@ -6,7 +6,13 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('name', 'username', 'email')
     search_fields = ('name', 'username')
 
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'author', 'created_at')
+    search_fields = ('text', 'author__username')
+    list_filter = ('created_at',)
+
 admin.site.register(User, UserAdmin)
 admin.site.register(Comment)
 admin.site.register(Like)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
