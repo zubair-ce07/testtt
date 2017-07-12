5687f6032f8fe1002b7cb4a3e5abfbc6af4bcb1a
ruhaib <ruhaibh@gmail.com>
2017-07-12 15:10:49 +0500

    For SEO, both id and name of poroducts appear in url split with dash(-)

--- mysite/super_store/templatetags/super_filters.py
+++ mysite/super_store/templatetags/super_filters.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter(name='sub')
 def sub(value, arg):
     return value - arg
+
+
+@register.filter(name='id_name')
+def id_name(value, arg):
+    return str(value) + '-' + arg
