40bffc47e6021ee5d24f590576ce4daa5d59f85f
Fakhar <fakhar.muhammad@arbisoft.com>
2017-07-12 15:37:29 +0500

    fixed intendation + request method issue

--- Django_Poll_Site/polls/models.py
+++ Django_Poll_Site/polls/models.py
@@ -11,6 +11,7 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published Recently?'
--- Django_Poll_Site/polls/views.py
+++ Django_Poll_Site/polls/views.py
@@ -31,7 +31,9 @@ class ResultsView(generic.DetailView):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        if request.method == 'POST':
+            selected_choice = question.choice_set.get(
+                pk=request.data['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html',
                       {'question': question, 'error_message': "You didn't select a choice."})
