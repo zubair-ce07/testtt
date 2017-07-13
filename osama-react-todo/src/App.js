6d2ec92e26bc329430608d2066850187af57cbd7
Muhammad Osama Arshad <osama.arshad@arbisoft.com>
2017-07-13 15:59:18 +0500

    added filters (incomplete)

--- osama-react-todo/src/App.js
+++ osama-react-todo/src/App.js
@@ -7,9 +7,8 @@ class Task extends Component {
     super();
     this.handleClick = this.handleClick.bind(this);
     this.state = {
-      id: props.value.id,
-      description: props.value.description,
-      status: props.value.status
+      id: props.id,
+      description: props.value
     };
   }
 
@@ -18,7 +17,9 @@ class Task extends Component {
     if (
       window.confirm('Complete this task? \n"' + evt.target.innerHTML + '"')
     ) {
-      this.props.value.status = "complete";
+      var temp = JSON.parse(localStorage.getItem(this.state.id));
+      temp.status = "complete";
+      localStorage.setItem(this.state.id, JSON.stringify(temp));
     }
   }
 
@@ -36,23 +37,31 @@ class Task extends Component {
 class App extends Component {
   constructor() {
     super();
+    this.getTask = this.getTask.bind(this);
     this.getTaskList = this.getTaskList.bind(this);
     this.setTaskList = this.setTask.bind(this);
-    this.handleChange = this.handleChange.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
     this.addTask = this.addTask.bind(this);
-    this.state = {
-      count: Boolean(localStorage.getItem("count"))
+    this.count = Boolean(localStorage.getItem("count"))
       ? localStorage.getItem("count")
-      : 1,
+      : 0;
+    this.state = {
+      count: this.count,
       tasks: this.getTaskList(),
       inputValue: ""
     };
   }
 
+  // gets a task object from localStorage
+  getTask(index) {
+    return JSON.parse(localStorage.getItem(+index));
+  }
+
   getTaskList() {
-    var tasks = []
-    for (let i = 1; i < this.state.count; i++) {
-      tasks.push(JSON.parse(localStorage.getItem(i)));
+    var tasks = [];
+    for (let i = 0; i < this.count; i++) {
+      var temp = this.getTask(i);
+      tasks[i] = temp;
     }
     return tasks;
   }
@@ -64,18 +73,35 @@ class App extends Component {
       status: "pending"
     };
     this.setState({
-      count: this.state.count + 1 
+      count: this.state.count + 1
     });
+    this.count += 1;
     localStorage.setItem(this.state.count, JSON.stringify(task));
+    localStorage.setItem("count", ++this.state.count);
   }
 
-  handleChange(evt) {
+  handleInputChange(evt) {
     this.setState({
       inputValue: evt.target.value
     });
   }
 
+  handleRadioChange(evt) {
+    var filterStatus = evt.target.value;
+    var tasks = this.getTaskList();
+    if (filterStatus !== "all") {
+      tasks = tasks.filter(element => {
+        return element.status === filterStatus;
+      });
+    };    
+    
+    this.setState({
+      tasks: tasks
+    });
+  }
+
   addTask() {
+    console.log(this);
     this.setTask();
     this.setState({
       tasks: this.getTaskList()
@@ -84,8 +110,10 @@ class App extends Component {
   }
 
   render() {
-    const taskList = this.state.tasks.forEach(function(task) {
-      return <Task value={task.description} />;
+    const taskList = this.state.tasks.map(task => {
+      if (task) {
+        return <Task id={task.id} value={task.description} />;
+      }
     });
     return (
       <div className="App">
@@ -93,16 +121,32 @@ class App extends Component {
           <h2>The 10001st Todo App</h2>
         </div>
         <p className="inputField">
-          <input id="todoInput" type="text" onChange={this.handleChange} />
+          <input id="todoInput" type="text" onChange={this.handleInputChange} />
           <button onClick={this.addTask}>Add Task</button>
         </p>
         <form id="filterForm">
-          <input type="radio" name="status" value="all" />
-          <span>All</span>
-          <input type="radio" name="status" value="complete" />
-          <span>Completed</span>
-          <input type="radio" name="status" value="pending" />
+          <input
+            type="radio"
+            onClick={this.handleRadioChange.bind(this)}
+            name="status"
+            value="pending"
+            defaultChecked
+          />
           <span>Pending</span>
+          <input
+            type="radio"
+            onClick={this.handleRadioChange.bind(this)}
+            name="status"
+            value="complete"
+          />
+          <span>Completed</span>
+          <input
+            type="radio"
+            onClick={this.handleRadioChange.bind(this)}
+            name="status"
+            value="all"
+          />
+          <span>All</span>
         </form>
         <ul>
           {taskList}
@@ -113,9 +157,9 @@ class App extends Component {
 }
 
 // HELPER FUNCTIONS
-function search(nameKey, myArray) {
+function search(nameid, myArray) {
   for (var i = 0; i < myArray.length; i++) {
-    if (myArray[i].name === nameKey) {
+    if (myArray[i].name === nameid) {
       return myArray[i];
     }
   }
