c3bac6ec457d68badc3ab021029745c48caadfda
Fakhar <fakhar.muhammad@arbisoft.com>
2017-07-17 16:01:26 +0500

    Indentation fixes

--- Django_Poll_Site/polls/views.py
+++ Django_Poll_Site/polls/views.py
@@ -31,15 +31,11 @@ class ResultsView(generic.DetailView):
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
-        # if request.method == 'POST':
-        selected_choice = question.choice_set.get(
-            pk=request.POST['choice'])
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html',
                       {'question': question, 'error_message': "You didn't select a choice."})
     else:
         selected_choice.votes += 1
         selected_choice.save()
-
     return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
-    # return HttpResponse(request.data)
