f5744a6b217589ccbe8cba054206cdfb48a4a198
Kuldeep <kuldeep.kumar@arbisoft.com>
2017-07-21 13:14:47 +0500

    Added rules for links

--- daraz/daraz/items.py
+++ daraz/daraz/items.py
@@ -10,5 +10,20 @@ import scrapy
 
 class DarazItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    rating = scrapy.Field()
+    price = scrapy.Field()
+    SKU = scrapy.Field()
+    Colour = scrapy.Field()
+    Resolution = scrapy.Field()
+    ProcessorType = scrapy.Field()
+    OS = scrapy.Field()
+    Connectivity = scrapy.Field()
+    RAM = scrapy.Field()
+    Brand = scrapy.Field()
+    Speed = scrapy.Field()
+    Ratings = scrapy.Field()
+    DisplaySize = scrapy.Field()
+    Storage = scrapy.Field()
+    ShippingWeight = scrapy.Field()
+    Title = scrapy.Field()
---
+++ daraz/daraz/spiders/Fetch_Mobiles.py
@@ -0,0 +1,36 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from daraz.items import DarazItem
+
+class rules(CrawlSpider):
+    name = 'rules'
+    start_urls = [
+                    'https://www.daraz.pk/phones-tablets/samsung/'
+                 ]
+
+    def parse_details(self, response):
+        title = response.css('h1.title::text').extract_first()
+        rating =  response.css('.stars::attr(style)').extract_first()
+        if rating:
+            rating = rating.split()[-1]
+        price = response.css('.price span::text').extract()[1]
+        specs_heading = response.css('.osh-row .-head::text').extract()
+        specs = response.css('#product-details .osh-row .osh-col+div::text').extract()
+        specs_dic = { 'Title': title,
+                      'Ratings': rating,
+                      'Price' : price
+                    }
+        for heading,spec in zip(specs_heading, specs):
+            specs_dic[heading] = spec
+        yield specs_dic
+        return
+
+
+    rules = [
+
+            Rule(LinkExtractor(restrict_css=('.link')), callback='parse_details'),
+            Rule(LinkExtractor(restrict_css=('.item')), callback='parse'),
+            Rule(LinkExtractor(restrict_css=('.-gallery','.pagination .-selected + li')), follow=True)
+
+    ]
