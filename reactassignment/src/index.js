94244329f31384d5d65991b5eb2199352a9a3f9b
ruhaib <ruhaibh@gmail.com>
2017-07-25 11:57:44 +0500

    node_modules in gitignore

--- reactassignment/src/index.js
+++ reactassignment/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import {BrowserRouter} from 'react-router-dom'
 
+
 ReactDOM.render(
     <BrowserRouter>
-        <App/>
+        <App />
     </BrowserRouter>,
     document.getElementById('root'))
