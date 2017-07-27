07f1d5fc4fb429efa32608d4ebe6b159717ef207
ahmedfaraz <ahmed.faraz0046@gmail.com>
2017-07-28 03:46:14 +0700

    with FK errors removed

--- mems_api/memsapi_app/serializers.py
+++ mems_api/memsapi_app/serializers.py
@@ -6,27 +6,27 @@ from rest_framework import serializers
 class MemorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Memory
-        fields = ['id', 'title', 'text', 'url', 'tags', 'is_public', 'image']
+        fields = ['id', 'title', 'text', 'url', 'tags', 'is_public', 'image', 'user', 'category']
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name', 'description']
+        fields = ['id', 'name', 'description', 'user']
 
 
 class ActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Activity
-        fields = ['id', 'memory_title', 'datetime', 'activity']
+        fields = ['id', 'memory_title', 'datetime', 'activity', 'user']
 
 
 class UserSerializer(serializers.ModelSerializer):
-    mems = MemorySerializer(many=True)
-    categories = CategorySerializer(many=True)
-    activities = ActivitySerializer(many=True)
+    mems = MemorySerializer(many=True, read_only=True)
+    categories = CategorySerializer(many=True, read_only=True)
+    activities = ActivitySerializer(many=True, read_only=True)
     class Meta:
         model = User
-        fields = ['id','first_name', 'last_name', 'email', 'username',
+        fields = ['id','first_name', 'last_name', 'email', 'username', 'image',
                   'password', 'categories', 'activities', 'mems']
 
--- mems_api/memsapi_app/urls.py
+++ mems_api/memsapi_app/urls.py
@@ -1,9 +1,16 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from rest_framework import routers
 from memsapi_app.views import UserListView, UserView, MemoryListView, MemoryView, \
                               ActivityListView, ActivityView, CategoryListView, CategoryView
 
+
+router = routers.DefaultRouter()
+
+
+
 app_name = 'memoapp'
 urlpatterns = [
+    url(r'^$', include(router.urls)),
     url(r'^users/$', UserListView.as_view()),
     url(r'^user/(?P<pk>[0-9]+)/$', UserView.as_view()),
     url(r'^mems/$', MemoryListView.as_view()),
