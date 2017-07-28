e9fae76a445f6299f5390758873d8fc490e98a55
hammadahmadwaqas <hammadahmadwaqas@github.com>
2017-07-28 12:12:51 +0500

    fix url pattern for news api
    now it will work for /api/news/
    instead of => /news/api/

--- twitter/news/urls.py
+++ twitter/news/urls.py
@@ -4,7 +4,6 @@ from django.contrib.auth.decorators import login_required
 from news import views
 
 urlpatterns = [
-    url(r'^api/',include('news.api.urls')),
     url(r'^$', login_required()(views.NewsView.as_view()), name='news'),
     url(r'^(?P<pk>[\d]+)/$', login_required()(views.NewsDetailedView.as_view()), name='news_detailed'),
 ]
