be2e39865de2a9d4cd42b7edf1d43656b0fba7c4
Muhammad Osama Arshad <osama.arshad@arbisoft.com>
2017-07-28 12:01:01 +0500

    added employee model (incomplete)

--- osama-emp/emp/settings.py
+++ osama-emp/emp/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
 ]
+
+REST_FRAMEWORK = [
+    'PAGE_SIZE': 10
+]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- osama-emp/employee/models.py
+++ osama-emp/employee/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Employee(models.Model):
+    '''
+    Extension of the User model to include employee data
+    '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    job_title = models.CharField(max_length=32)
+    supervisor = models.ForeignKey("self", blank=True, null=True)
+    
