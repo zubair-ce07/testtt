c5f5529c53708a7a8c78d80d32da587ed51a93b0
usman-abbasi <usman.abbasi@ymail.com>
2017-08-01 16:56:45 +0500

    2nd iteration

--- UsmanAbbasi-Weatherman.py
+++ UsmanAbbasi-Weatherman.py
@@ -1,170 +1,158 @@
 import sys
 import fnmatch
+import argparse
 import os
 import calendar
+import re
+import csv
 
-
-
-
-def calcFileName(argList):
+def generateFileNames(arg, operation):
     fileNames = []
-    pattern = 'Murree_weather_' + argList[argList.index("-e")+1] + '_' + '???' + '.txt'
-    for file in os.listdir(argList[1]):
-        if fnmatch.fnmatch(file, pattern):
-            fileNames.append(file)
-    return fileNames
+    for file in os.listdir(args.f):
+        fileNames.append(file)
 
+    if operation == '-a' or operation == '-c':
+        yymm = arg.split("/")
+        year = yymm[0]
+        month = calendar.month_abbr[int(yymm[1])]
+        pattern = r'[A-Za-z]*_weather_' + year + '_' + month + '.txt'    
+        name = []
+        for file in fileNames:
+            if fnmatch.fnmatch(file, pattern):
+                name.append(file)
+        namestring = str(name[0])
+        return namestring
 
-def calcFileName2(argList):
-    months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec"]
-
-    if "-c" in argList:
-        index = int(argList.index("-c")) + 1
-        
-    if "-a" in argList:
-        index = int(argList.index("-a")) + 1
-    
-    yymm = argList[index].split("/")
-    pattern = 'Murree_weather_' + yymm[0] + '_' + months[int(yymm[1])-1] + '.txt'
-    return pattern
-
-
-def calcMax(lines, attribute):    
-    i = 1
-    maxVal= -100
-    while i != (len(lines)):
-        line = lines[i].split(",")      
-        i += 1
-        
-        if not line[attribute]:
-            continue
-            
-        maxVal = max(maxVal, int(line[attribute]))        
+    if operation == '-e':
+        year = arg
+        pattern = r'[A-Za-z]*_weather_' + year + '_' + r'???' + '.txt'    
+        name = []
+        for file in fileNames:
+            if fnmatch.fnmatch(file, pattern):
+                name.append(file)
+        return name
+ 
+def calcMax(result, attribute):    
+    maxVal = None 
+    maxList = filter(None, result.get(attribute))
+    maxList = [int(i) for i in maxList]
+    maxVal = max(maxList)
     return maxVal
-
-
-def calcMin(lines, attribute):    
-    i = 1
-    minVal = 100
-    while i != (len(lines)):
-        line = lines[i].split(",")      
-        i += 1
-        if not line[attribute]:
-            continue
-        minVal = min(minVal, int(line[attribute]))   
+    
+def calcMin(result, attribute):    
+    minVal = None 
+    minList = filter(None, result.get(attribute))
+    minList = [int(i) for i in minList]
+    minVal = min(minList)
     return minVal
     
-
-def calcAvg(lines, attribute):    
-    i = 1
-    l1 = []
-    while i != (len(lines)):
-        line = lines[i].split(",")      
-        i += 1
-        if not line[attribute]:
-            continue
-        l1.append(int(line[attribute]))    
+def calcAvg(result, attribute):    
+    avg = None
+    l1 = list(filter(None, result.get(attribute)))
+    l1 = [int(i) for i in l1]
     avg = sum(l1)/len(l1)
     return int(avg)
-    
-    
-def calcDates(fileNames, highest, lowest, humid):    
-    
-    lines = []
+
+def calcDates(fileNames, yearMaxTemp, yearMinTemp, yearMaxHumid, args):    
+    yearMaxTempDate, yearMinTempDate, yearMaxHumidDate  = "", "", ""
     for file in fileNames:
-        i = 1
-        lines = open('weatherfiles/'+file, "r").readlines()
-        
-        while i != (len(lines)):
-            line = lines[i].split(",")      
-            i+=1
-            if line[3] and lowest == int(line[3]):
-                ldate = line[0].split("-")
-                lldate = str(calendar.month_name[int(ldate[1])])
-                lldate = lldate + " " + ldate[2]
-        
-            if line[7] and humid == int(line[7]):
-                hudate = line[0].split("-")
-                hhudate = str(calendar.month_name[int(hudate[1])])
-                hhudate = hhudate + " " + hudate[2]
+        iteration = 1
+        result = readFile(args, file)
 
-
+        for maxT, minT, maxH, date in zip(result.get("Max TemperatureC"), result.get("Min TemperatureC"), result.get("Max Humidity"), result.get("PKT")):
+            if not maxT:
+                continue
                 
-            if line[1] and highest == int(line[1]):
-                hdate = line[0].split("-")
-                hhdate = str(calendar.month_name[int(hdate[1])])
-                hhdate = hhdate + " " + hdate[2]
-    
-    
-    
-    return hhdate,lldate, hhudate   
-    
+            if yearMaxTemp == int(maxT):
+                hdate = date.split("-")
+                yearMaxTempDate = str(calendar.month_name[int(hdate[1])])
+                yearMaxTempDate = yearMaxTempDate + " " + hdate[2] 
 
-def barChart(lines):    
-    i, index = 1, 1
-    while i != (len(lines)):
-        line = lines[i].split(",")
-        i += 1    
-        if not line[1] and not line[3]:
-            continue
-        
-        print('\033[35m', index, '\033[31m' , "+" * int(line[1]), '\033[35m', line[1],"C")
-        print('\033[35m', index, '\033[36m' ,"+" * int(line[3]), '\033[35m', line[3],"C", '\033[0m')
+            if yearMinTemp == int(minT):
+                ldate = date.split("-")
+                yearMinTempDate = str(calendar.month_name[int(ldate[1])])
+                yearMinTempDate = yearMinTempDate + " " + ldate[2] 
+                
+            if not maxH:
+                continue
+                
+            if yearMaxHumid == int(maxH):
+                hudate = date.split("-")
+                yearMaxHumidDate = str(calendar.month_name[int(hudate[1])])
+                yearMaxHumidDate = yearMaxHumidDate + " " + hudate[2]
+    return yearMaxTempDate,yearMinTempDate, yearMaxHumidDate 
+
+def barChart(result):    
+    index = 1
+    maxList = list(filter(None, result.get("Max TemperatureC")))
+    maxList = [int(i) for i in maxList]
+    minList = list(filter(None, result.get("Min TemperatureC")))
+    minList = [int(i) for i in minList]
+    for maxTemp, minTemp in zip(maxList, minList):
+        print('\033[35m', index, '\033[31m' , "+" * maxTemp, '\033[35m', maxTemp,"C")
+        print('\033[35m', index, '\033[36m' ,"+" * minTemp, '\033[35m', minTemp,"C", '\033[0m')
         index += 1
         
-def barChartBonus(lines):
-    i, index = 1, 1
-    while i != (len(lines)):
-        line = lines[i].split(",")
-        i += 1    
-        if not line[1] and not line[3]:
-            continue    
-        print('\033[35m', index, '\033[36m' ,"+" * int(line[3]) +  '\033[31m', "+" * int(line[1]), '\033[35m', line[3],"C  -", '\033[35m', line[1],"C", '\033[0m')        
+def barChartBonus(result):
+    index = 1
+    maxList = list(filter(None, result.get("Max TemperatureC")))
+    maxList = [int(i) for i in maxList]
+    minList = list(filter(None, result.get("Min TemperatureC")))
+    minList = [int(i) for i in minList]
+    for maxTemp, minTemp in zip(maxList, minList):
+        print('\033[35m', index, '\033[36m' ,"+" * minTemp +  '\033[31m', "+" * maxTemp, '\033[35m', minTemp,"C  -", '\033[35m', maxTemp,"C", '\033[0m')        
         index += 1
 
-
+def calcValues(fileNames, args):
+    monthMaxTemp, monthMinTemp, monthMaxHumid = [],[],[]
+    yearMaxTemp, yearMinTemp, yearMaxHumid = 0, 0, 0
+    
+    for file in fileNames:
+        result = readFile(args, file)
+        monthMaxTemp.append(calcMax(result, "Max TemperatureC"))
+        monthMinTemp.append(calcMin(result, "Min TemperatureC"))
+        monthMaxHumid.append(calcMax(result, "Max Humidity"))
+    yearMaxTemp = max(monthMaxTemp)
+    yearMinTemp = min(monthMinTemp)
+    yearMaxHumid = max(monthMaxHumid)
+    return yearMaxTemp, yearMinTemp, yearMaxHumid        
+ 
+def readFile(args, fileName):
+    with open(args.f + fileName) as read:
+        oneFile = csv.DictReader(read)
+        result = {}
+        for row in oneFile:
+            for column, value in row.items():
+                result.setdefault(column, []).append(value)
+    return result
+    
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', type=str, help='File directory')
+    parser.add_argument('-c', type=str, help='-c')
+    parser.add_argument('-a', type=str, help='-a')
+    parser.add_argument('-e', type=str, help='-e')
+    args = parser.parse_args()
+    
+    if args.e:
+        fileNames = generateFileNames(args.e, '-e')
+        yearMaxTemp, yearMinTemp, yearMaxHumid = calcValues(fileNames, args)
+        yearMaxTempDate, yearMinTempDate, yearMaxHumidDate = calcDates(fileNames, yearMaxTemp, yearMinTemp, yearMaxHumid, args)
+        print("Highest: %dC on %s" % (yearMaxTemp, yearMaxTempDate))
+        print("Lowest: %dC on %s" % (yearMinTemp, yearMinTempDate))
+        print("Humidity:", yearMaxHumid, "% on", yearMaxHumidDate)
+        
+    if args.a:
+        fileName = generateFileNames(args.a, '-a')
+        result = readFile(args, fileName)           
+        print("Highest Average:" , calcMax(result, "Mean TemperatureC"), "C")
+        print("Lowest Average:" , calcMin(result, "Mean TemperatureC"), "C")
+        print("Average mean humidity:", calcAvg(result, " Mean Humidity"), "%")
 
-    if '-e' in sys.argv:
-        fileNames = calcFileName(sys.argv)
-        monthMaxTemp, monthMinTemp, monthMaxHumid = [],[],[]
-        highest, lowest, humid = 0, 0, 0
-        for file in fileNames:
-            lines = open(sys.argv[1] + file, "r").readlines()
-            monthMaxTemp.append(calcMax(lines, 1))
-            monthMinTemp.append(calcMin(lines, 3))
-            monthMaxHumid.append(calcMax(lines, 7))
-        highest = max(monthMaxTemp)
-        lowest = min(monthMinTemp)
-        humid = max(monthMaxHumid)
-        hdate, ldate, hudate = calcDates(fileNames, highest, lowest, humid)
-        print("Highest: %dC on %s" % (highest, hdate))
-        print("Lowest: %dC on %s" % (lowest, ldate))
-        print("Humidity:", humid, "% on", hudate)
-        print("\n")
         
-    if '-a' in sys.argv:
-        fileName = calcFileName2(sys.argv)
-        lines = open(sys.argv[1] + fileName, "r").readlines()
-        print("Highest Average:" , calcMax(lines, 2), "C")
-        print("Lowest Average:" , calcMin(lines, 2), "C")
-        print("Average mean humidity:", calcAvg(lines, 8), "%")
-        print("\n")
-        
-    if '-c' in sys.argv:
-        fileName = calcFileName2(sys.argv)
-        lines = open(sys.argv[1] + fileName, "r").readlines()
-        monthMaxTemp, monthMinTemp = 0,0
-        highest, lowest = 0, 0
-        monthMaxTemp = calcMax(lines, 1)
-        monthMinTemp = calcMin(lines, 3)
-        barChart(lines)
-        print("\n")
-        barChartBonus(lines)
-        print("\n")
-        
-    else:
-        print("Invalid arguments.")
-        sys.exit()
-        
+    if args.c:
+        fileName = generateFileNames(args.c, '-c')
+        result = readFile(args, fileName)
+        barChart(result)
+        barChartBonus(result)
     
