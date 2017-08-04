c8c02f1aff29b0de234c8607c46aea5fbf54ab72
Kuldeep <kuldeep.kumar@arbisoft.com>
2017-08-04 10:38:48 +0500

    Adding extra lines

--- BookInventory/books/models.py
+++ BookInventory/books/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from datetime import datetime
 
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
