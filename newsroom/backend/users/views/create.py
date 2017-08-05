2a336b532e9e30ac2710ed008c017e14c0d5030b
Luqman-Ud-Din <luqman.muhammad@arbisoft.com>
2017-08-05 12:26:15 +0500

    refactored User Create API

--- newsroom/backend/users/views/create.py
+++ newsroom/backend/users/views/create.py
@@ -1,28 +1,15 @@
-from django.db import DatabaseError
-from django.core.validators import validate_email
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from backend.users.models import User
+from backend.users.serializers.user import UserSerializer
 
 
 class UserCreateAPIView(APIView):
+    serializer_class = UserSerializer
+
     def post(self, request, *args, **kwargs):
-        try:
-            email = request.data['email']
-            password = request.data['password']
-            first_name = request.data['first_name']
-            last_name = request.data['last_name']
-            validate_email(email)
-            user = User(username=email, email=email, first_name=first_name, last_name=last_name)
-            user.set_password(password)
-            user.save()
-            return Response(request.data, status=status.HTTP_200_OK)
-        except validate_email.ValidationError:
-            return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
-        except KeyError:
-            return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
-        except DatabaseError:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        except:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
