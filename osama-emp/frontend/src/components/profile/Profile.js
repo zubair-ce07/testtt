e1ec2c57cd738d679e76c8b8cfb0b0061fb64cbe
Muhammad Osama Arshad <osama.arshad@arbisoft.com>
2017-08-08 18:14:18 +0500

    added profile stuff

--- osama-emp/frontend/src/components/profile/Profile.js
+++ osama-emp/frontend/src/components/profile/Profile.js
@@ -8,10 +8,30 @@ class Profile extends React.Component {
 
   render() {
     return (
-      <p>
-        Name:{" "}
-        {this.props.profile.first_name + " " + this.props.profile.last_name}
-      </p>
+      <div className="profile">
+        <p>
+          Username: {this.props.profile.username}
+        </p>
+        <p>
+          Name:{" "}
+          {this.props.profile.first_name + " " + this.props.profile.last_name}
+        </p>
+        <p>
+          Job Title: {this.props.profile.job_title}
+        </p>
+        <p>
+          Gender: {this.props.profile.gender === "M" ? "Male" : "Female"}
+        </p>
+        <p>
+          Date of Birth: {this.props.profile.date_of_birth}
+        </p>
+        <p>
+          Date of Joining: {this.props.profile.date_of_joining}
+        </p>
+        <p>
+          Nationality: {this.props.profile.nationality}
+        </p>
+      </div>
     );
   }
 }
