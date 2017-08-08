1ca56ba530997b612d10ada1a3a2087ef814a29b
Muhammad Osama Arshad <osama.arshad@arbisoft.com>
2017-08-08 18:58:29 +0500

    removed unnecessary files

--- osama-emp/frontend/src/components/profile/Profile.js
+++ osama-emp/frontend/src/components/profile/Profile.js
@@ -2,10 +2,6 @@ import React from "react";
 import "./Profile";
 
 class Profile extends React.Component {
-  constructor(props) {
-    super();
-  }
-
   render() {
     return (
       <div className="profile">
--- osama-emp/frontend/src/index.js
+++ osama-emp/frontend/src/index.js
@@ -1,18 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 import "./index.css";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
 import Login from "./Login.js";
 import { loggedIn } from "./auth";
-import configureStore from "./configureStore";
-import { getProfileStart } from "./actions";
-
-let store = configureStore();
-
-store.dispatch(getProfileStart(localStorage.username));
 
 ReactDOM.render(
   <BrowserRouter history={BrowserRouter.browserHistory}>
