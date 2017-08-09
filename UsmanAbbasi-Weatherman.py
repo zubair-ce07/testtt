8302eff54031de817cf905d637f3e3752df759ce
usman-abbasi <usman.abbasi@ymail.com>
2017-08-09 17:32:14 +0500

    Weatherman 3rd iteration

--- UsmanAbbasi-Weatherman.py
+++ UsmanAbbasi-Weatherman.py
@@ -1,158 +1,186 @@
-import sys
 import fnmatch
 import argparse
 import os
 import calendar
-import re
 import csv
+from datetime import datetime
+#import itertools
 
-def generateFileNames(arg, operation):
-    fileNames = []
-    for file in os.listdir(args.f):
-        fileNames.append(file)
+
+class ColorText:
+    red = '\033[31m'
+    purple = '\033[35m'
+    blue = '\033[36m'
+    original = '\033[0m'
+
+
+def select_files(new_args):
+    directory_files = os.listdir(new_args.f)
+    selected_files = fnmatch.filter(directory_files, '*_weather_*.txt')
+    return selected_files
+
+
+def read_files(new_args, selected_files):
+    data = []
+    for file_name in selected_files:
+        with open(new_args.f + file_name) as read:
+            reader = csv.DictReader(read)
+            for row in reader:
+                data.append(row)
+
+    for i in data:
+
+        if i.get('PKST'):
+            i['PKT'] = i['PKST']
+            del i['PKST']
+
+        if i['PKT']:
+            i['PKT'] = datetime.strptime(i['PKT'], '%Y-%m-%d').date()
+
+        if i['Max TemperatureC']:
+            i['Max TemperatureC'] = int(i['Max TemperatureC'])
+
+        if i['Mean TemperatureC']:
+            i['Mean TemperatureC'] = int(i['Mean TemperatureC'])
+
+        if i['Min TemperatureC']:
+            i['Min TemperatureC'] = int(i['Min TemperatureC'])
+
+        if i['Max Humidity']:
+            i['Max Humidity'] = int(i['Max Humidity'])
+
+        if i[' Mean Humidity']:
+            i[' Mean Humidity'] = int(i[' Mean Humidity'])
+
+    return data
+
+
+def convert_args_type(arg):
+    if '/' in arg:
+        return datetime.strptime(arg, '%Y/%m').date()
+
+    else:
+        return datetime.strptime(arg, '%Y').date()
+
+
+def extract_data(arg, operation, files_data):
+    extracted_data = []
+    if operation == "-e":
+        for data in files_data:
+            if arg.year == data['PKT'].year:
+                extracted_data.append(data)
+        return extracted_data
 
     if operation == '-a' or operation == '-c':
-        yymm = arg.split("/")
-        year = yymm[0]
-        month = calendar.month_abbr[int(yymm[1])]
-        pattern = r'[A-Za-z]*_weather_' + year + '_' + month + '.txt'    
-        name = []
-        for file in fileNames:
-            if fnmatch.fnmatch(file, pattern):
-                name.append(file)
-        namestring = str(name[0])
-        return namestring
+        for data in files_data:
+            if arg.year == data['PKT'].year and arg.month == data['PKT'].month:
+                extracted_data.append(data)
+        return extracted_data
 
-    if operation == '-e':
-        year = arg
-        pattern = r'[A-Za-z]*_weather_' + year + '_' + r'???' + '.txt'    
-        name = []
-        for file in fileNames:
-            if fnmatch.fnmatch(file, pattern):
-                name.append(file)
-        return name
- 
-def calcMax(result, attribute):    
-    maxVal = None 
-    maxList = filter(None, result.get(attribute))
-    maxList = [int(i) for i in maxList]
-    maxVal = max(maxList)
-    return maxVal
-    
-def calcMin(result, attribute):    
-    minVal = None 
-    minList = filter(None, result.get(attribute))
-    minList = [int(i) for i in minList]
-    minVal = min(minList)
-    return minVal
-    
-def calcAvg(result, attribute):    
-    avg = None
-    l1 = list(filter(None, result.get(attribute)))
-    l1 = [int(i) for i in l1]
-    avg = sum(l1)/len(l1)
-    return int(avg)
 
-def calcDates(fileNames, yearMaxTemp, yearMinTemp, yearMaxHumid, args):    
-    yearMaxTempDate, yearMinTempDate, yearMaxHumidDate  = "", "", ""
-    for file in fileNames:
-        iteration = 1
-        result = readFile(args, file)
+def calc_max(extracted_data, attribute):
+    value = []
+    for data in extracted_data:
+        if data[attribute]:
+            value.append(data[attribute])
+    return max(value)
 
-        for maxT, minT, maxH, date in zip(result.get("Max TemperatureC"), result.get("Min TemperatureC"), result.get("Max Humidity"), result.get("PKT")):
-            if not maxT:
-                continue
-                
-            if yearMaxTemp == int(maxT):
-                hdate = date.split("-")
-                yearMaxTempDate = str(calendar.month_name[int(hdate[1])])
-                yearMaxTempDate = yearMaxTempDate + " " + hdate[2] 
 
-            if yearMinTemp == int(minT):
-                ldate = date.split("-")
-                yearMinTempDate = str(calendar.month_name[int(ldate[1])])
-                yearMinTempDate = yearMinTempDate + " " + ldate[2] 
-                
-            if not maxH:
-                continue
-                
-            if yearMaxHumid == int(maxH):
-                hudate = date.split("-")
-                yearMaxHumidDate = str(calendar.month_name[int(hudate[1])])
-                yearMaxHumidDate = yearMaxHumidDate + " " + hudate[2]
-    return yearMaxTempDate,yearMinTempDate, yearMaxHumidDate 
+def calc_date(max_temp, min_temp, max_humid, extracted_data):
+    max_temp_date = None
+    max_humid_date = None
+    min_temp_date = None
+    for data in extracted_data:
+        if max_temp == data['Max TemperatureC']:
+            max_temp_date = calendar.month_name[data['PKT'].month] + " " + str(data['PKT'].day)
 
-def barChart(result):    
+        if max_humid == data['Max Humidity']:
+            max_humid_date = calendar.month_name[data['PKT'].month] + " " + str(data['PKT'].day)
+
+        if min_temp == data['Min TemperatureC']:
+            min_temp_date = calendar.month_name[data['PKT'].month] + " " + str(data['PKT'].day)
+
+    return max_temp_date, max_humid_date, min_temp_date
+
+
+def calc_min(extracted_data, attribute):
+    value = []
+    for data in extracted_data:
+        if data[attribute]:
+            value.append(data[attribute])
+    return min(value)
+
+
+def calc_avg(extracted_data, attribute):
+    value = []
+    for data in extracted_data:
+        if data[attribute]:
+            value.append(data[attribute])
+    avg = (int(sum(value) / len(value)))
+    return avg
+
+def bar_chart_bonus(extracted_data):
+    max_temp = []
+    min_temp = []
     index = 1
-    maxList = list(filter(None, result.get("Max TemperatureC")))
-    maxList = [int(i) for i in maxList]
-    minList = list(filter(None, result.get("Min TemperatureC")))
-    minList = [int(i) for i in minList]
-    for maxTemp, minTemp in zip(maxList, minList):
-        print('\033[35m', index, '\033[31m' , "+" * maxTemp, '\033[35m', maxTemp,"C")
-        print('\033[35m', index, '\033[36m' ,"+" * minTemp, '\033[35m', minTemp,"C", '\033[0m')
-        index += 1
-        
-def barChartBonus(result):
-    index = 1
-    maxList = list(filter(None, result.get("Max TemperatureC")))
-    maxList = [int(i) for i in maxList]
-    minList = list(filter(None, result.get("Min TemperatureC")))
-    minList = [int(i) for i in minList]
-    for maxTemp, minTemp in zip(maxList, minList):
-        print('\033[35m', index, '\033[36m' ,"+" * minTemp +  '\033[31m', "+" * maxTemp, '\033[35m', minTemp,"C  -", '\033[35m', maxTemp,"C", '\033[0m')        
+    color = ColorText()
+    for data in extracted_data:
+        if data['Max TemperatureC'] and data['Min TemperatureC']:
+            max_temp.append(data['Max TemperatureC'])
+            min_temp.append(data['Min TemperatureC'])
+
+    for maxTemp, minTemp in zip(max_temp, min_temp):    #or itertools.zip_longest
+        print(color.purple, index, color.blue, "+" * minTemp + color.red, "+" * maxTemp, color.purple, minTemp, "C  -",
+              color.purple, maxTemp, "C", color.original)
         index += 1
 
-def calcValues(fileNames, args):
-    monthMaxTemp, monthMinTemp, monthMaxHumid = [],[],[]
-    yearMaxTemp, yearMinTemp, yearMaxHumid = 0, 0, 0
-    
-    for file in fileNames:
-        result = readFile(args, file)
-        monthMaxTemp.append(calcMax(result, "Max TemperatureC"))
-        monthMinTemp.append(calcMin(result, "Min TemperatureC"))
-        monthMaxHumid.append(calcMax(result, "Max Humidity"))
-    yearMaxTemp = max(monthMaxTemp)
-    yearMinTemp = min(monthMinTemp)
-    yearMaxHumid = max(monthMaxHumid)
-    return yearMaxTemp, yearMinTemp, yearMaxHumid        
- 
-def readFile(args, fileName):
-    with open(args.f + fileName) as read:
-        oneFile = csv.DictReader(read)
-        result = {}
-        for row in oneFile:
-            for column, value in row.items():
-                result.setdefault(column, []).append(value)
-    return result
-    
+def bar_chart(extracted_data):
+    max_temp = []
+    min_temp = []
+    index = 1
+    color = ColorText()
+    for data in extracted_data:
+        if data['Max TemperatureC'] and data['Min TemperatureC']:
+            max_temp.append(data['Max TemperatureC'])
+            min_temp.append(data['Min TemperatureC'])
+
+    for maxTemp, minTemp in zip(max_temp, min_temp):
+        print(color.purple, index, color.red, "+" * maxTemp, color.purple, maxTemp, "C")
+        print(color.purple, index, color.blue, "+" * minTemp, color.purple, minTemp, "C", color.original)
+        index += 1
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', type=str, help='File directory')
-    parser.add_argument('-c', type=str, help='-c')
-    parser.add_argument('-a', type=str, help='-a')
-    parser.add_argument('-e', type=str, help='-e')
+    parser.add_argument('-c', type=convert_args_type, help='-c')
+    parser.add_argument('-a', type=convert_args_type, help='-a')
+    parser.add_argument('-e', type=convert_args_type, help='-e')
     args = parser.parse_args()
-    
-    if args.e:
-        fileNames = generateFileNames(args.e, '-e')
-        yearMaxTemp, yearMinTemp, yearMaxHumid = calcValues(fileNames, args)
-        yearMaxTempDate, yearMinTempDate, yearMaxHumidDate = calcDates(fileNames, yearMaxTemp, yearMinTemp, yearMaxHumid, args)
-        print("Highest: %dC on %s" % (yearMaxTemp, yearMaxTempDate))
-        print("Lowest: %dC on %s" % (yearMinTemp, yearMinTempDate))
-        print("Humidity:", yearMaxHumid, "% on", yearMaxHumidDate)
-        
-    if args.a:
-        fileName = generateFileNames(args.a, '-a')
-        result = readFile(args, fileName)           
-        print("Highest Average:" , calcMax(result, "Mean TemperatureC"), "C")
-        print("Lowest Average:" , calcMin(result, "Mean TemperatureC"), "C")
-        print("Average mean humidity:", calcAvg(result, " Mean Humidity"), "%")
+    selected_files = select_files(args)
+    files_data = read_files(args, selected_files)
 
-        
+    if args.e:
+        extracted_data = extract_data(args.e, "-e", files_data)
+        max_temp = calc_max(extracted_data, "Max TemperatureC")
+        min_temp = calc_min(extracted_data, "Min TemperatureC")
+        max_humid = calc_max(extracted_data, "Max Humidity")
+        max_temp_date, max_humid_date, min_temp_date = calc_date(max_temp, min_temp, max_humid, extracted_data)
+        print("Highest:", max_temp, "C on", max_temp_date)
+        print("Lowest:", min_temp, "C on", min_temp_date)
+        print("Humidity:", max_humid, "% on", max_humid_date)
+
+    if args.a:
+        extracted_data = extract_data(args.a, "-a", files_data)
+        avg_high_temp = calc_max(extracted_data, "Mean TemperatureC")
+        avg_low_temp = calc_min(extracted_data, "Mean TemperatureC")
+        avg_mean_humid = calc_avg(extracted_data, " Mean Humidity")
+        print("Highest Average:", avg_high_temp, "C")
+        print("Lowest Average:", avg_low_temp, "C")
+        print("Average Mean Humidity:", avg_mean_humid, "%")
+
     if args.c:
-        fileName = generateFileNames(args.c, '-c')
-        result = readFile(args, fileName)
-        barChart(result)
-        barChartBonus(result)
-    
+        extracted_data = extract_data(args.c, "-c", files_data)
+        print(calendar.month_name[args.c.month], args.c.year)
+        bar_chart(extracted_data)
+        bar_chart_bonus(extracted_data)
+
