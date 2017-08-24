5bb5609e7a2aed7e3b3db6deed6479d65e90d775
ruhaib <ruhaibh@gmail.com>
2017-08-24 15:56:43 +0500

    reviewed changes applied

--- Woolrich/woolrich_spider.py
+++ Woolrich/woolrich_spider.py
@@ -1,17 +1,14 @@
-import json
 import re
-from termcolor import colored
 
 from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
-from scrapy import Request, FormRequest, Selector
+from scrapy import FormRequest
 
 from .base import BaseCrawlSpider, BaseParseSpider, clean
 
 
 class Mixin:
     retailer = 'woolrich-us'
-    lang = 'en'
     market = 'US'
 
     allowed_domains = ['woolrich.com']
@@ -21,10 +18,9 @@ class Mixin:
 class WoolrichParseSpider(BaseParseSpider, Mixin):
     name = Mixin.retailer + '-parse'
     product_api_url = 'http://www.woolrich.com/woolrich/prod/fragments/productDetails.jsp'
-    price_css = '#productDetails .dividers .price .price_sale span::text, .price_reg.strikethrough::text, .price'
+    price_css = '.price ::text'
 
     brands = [
-
         'Frost River X Woolrich',
         'Topo X Woolrich',
         'Westerlind X Woolrich',
@@ -34,8 +30,8 @@ class WoolrichParseSpider(BaseParseSpider, Mixin):
         'Woolrich'
     ]
 
-    gender_map = [
-        'Men', 'Women'
+    genders = [
+        'men', 'women'
     ]
 
     def parse(self, response):
@@ -48,12 +44,11 @@ class WoolrichParseSpider(BaseParseSpider, Mixin):
         self.boilerplate_normal(garment, response)
 
         garment['skus'] = {}
-        garment['image_urls'] = response.css('img[itemprop="image"]::attr(src)').extract()
-        garment['image_urls'].extend(response.css('#prod-detail__slider-nav img::attr(src)').extract())
+        garment['image_urls'] = self.image_urls(response)
         garment['gender'] = self.product_gender(response)
 
         garment['meta'] = {
-            'requests_queue': self.garment_requests(response, product_id, garment),
+            'requests_queue': self.color_requests(response, garment),
         }
 
         return self.next_request_or_garment(garment)
@@ -61,229 +56,157 @@ class WoolrichParseSpider(BaseParseSpider, Mixin):
     def parse_color_variants(self, response):
         garment = response.meta['garment']
         garment['image_urls'].extend(self.image_urls(response))
-        color = response.meta['color_id']
-        product_id = response.meta['product_id']
-        requests = []
-        for indx, size in enumerate(self.product_sizes(response)):
-            if not size['out_of_stock']:
-                if not self.is_size_id(response):
-                    requests.append(
-                        FormRequest(
-                            url=self.product_api_url,
-                            formdata={
-                                'productId': product_id,
-                                'colorId': color,
-                                'selectedSize': size['size'],
-                            },
-                            callback=self.parse_size_variants,
-                            dont_filter=True,
-                            meta={
-                                'garment': garment,
-                                'product_id': product_id,
-                                'color_id': color,
-                                'size': size['size']
-                            }
-                        )
-                    )
-                else:
-                    id_from_size = self.id_from_size(response)
-                    requests.append(
-                        FormRequest(
-                            url=self.product_api_url,
-                            formdata={
-                                'productId': product_id,
-                                'colorId': color,
-                                'selectedSize': size['size'],
-                                'skuId': id_from_size[indx]
-                            },
-                            callback=self.parse_product_final_variants,
-                            dont_filter=True,
-                            meta={
-                                'garment': garment,
-                                'product_id': product_id,
-                                'color_id': color,
-                                'sku_id': id_from_size[indx],
-                                'size': size['size']
-                            }
-                        )
-                    )
-            else:
-                pricing = self.product_pricing_common_new(response)
-                garment['skus'].update({
-                    color + '_' + size['size'] + '_' + product_id: {
-                        'color': color,
-                        'size': size['size'],
-                        'out_of_stock': size['out_of_stock'],
-                        'price': pricing['price'],
-                        'currency': pricing['currency']
-                    }
-                })
-        garment['meta']['requests_queue'].extend(requests)
+        garment['meta']['requests_queue'].extend(self.size_requests(response))
         return self.next_request_or_garment(garment)
 
     def parse_size_variants(self, response):
         garment = response.meta['garment']
-        size = response.meta['size']
-        color = response.meta['color_id']
-        product_id = response.meta['product_id']
-
-        requests = []
-        for sku in self.product_skuID(response):
-            if not sku['out_of_stock']:
-                requests.append(
-                    FormRequest(
-                        url=self.product_api_url,
-                        formdata={
-                            'productId': product_id,
-                            'colorId': color,
-                            'selectedSize': size,
-                            'skuId': sku['sku_id']
-                        },
-                        callback=self.parse_product_final_variants,
-                        dont_filter=True,
-                        meta={
-                            'garment': garment,
-                            'product_id': product_id,
-                            'color_id': color,
-                            'size': size,
-                            'sku_id': sku['sku_id']
-                        }
-                    )
-                )
-            else:
-                pricing = self.product_pricing_common_new(response)
-                garment['skus'].update({
-                    color + '_' + size + '_' + product_id: {
-                        'color': color,
-                        'size': size,
-                        'out_of_stock': sku['out_of_stock'],
-                        'sku_id': sku['sku_id'],
-                        'price': pricing['price'],
-                        'currency': pricing['currency']
-                    }
-                })
-        garment['meta']['requests_queue'].extend(requests)
+        garment['meta']['requests_queue'].extend(self.fitting_requests(response))
         return self.next_request_or_garment(garment)
 
-    def parse_product_final_variants(self, response):
+    def parse_fitting_variants(self, response):
         garment = response.meta['garment']
-        color_id = response.meta['color_id']
-        size = response.meta['size']
-        sku_id = response.meta['sku_id']
-        product_id = response.meta['product_id']
-        pricing = self.product_pricing_common_new(response)
-        garment['skus'].update({
-            color_id + '_' + size + '_' + product_id: {
-                'color': color_id,
-                'size': size,
-                'out_of_stock': False,
-                'sku_id': sku_id,
-                'price': pricing['price'],
-                'currency': pricing['currency']
-            }
-        })
+        garment['skus'].update(self.skus(response))
         return self.next_request_or_garment(garment)
 
-    def product_skuID(self, response):
-        r = re.compile('id="([\d]+)"')
-        temp = []
-        try:
-            temp = [{
-                'sku_id': r.search(x).group(1) or None,
-                'out_of_stock': 'stocklevel="0"' in x
-            } for x in response.css('.dimensionslist li a').extract(
-            )]
-        except Exception:
-            pass
-
-        return temp
-
-    def garment_requests(self, response, prodId, garment):
-        color_ids = self.product_color_ids(response)
+    def fitting_requests(self, response):
         requests = []
-        for colorid in color_ids:
+        for fitting_sel in response.css('.dimensionslist a'):
+            if fitting_sel.css('[stocklevel="0"]'):
+                continue
+            sku_id = clean(fitting_sel.css('::attr(id)'))[0]
+            form_data = {
+                'productId': response.meta['garment']['retailer_sku'],
+                'colorId': response.meta['color_id'],
+                'selectedSize': response.meta['size'],
+                'skuId': sku_id
+            }
+            meta = {
+                'garment': response.meta['garment'],
+                'color': response.meta['color'],
+                'size': response.meta['size'],
+                'sku_id': sku_id,
+                'fit': clean(fitting_sel.css('::text'))[0]
+            }
             requests.append(
-                FormRequest(
-                    url=self.product_api_url,
-                    formdata={
-                        'productId': prodId,
-                        'colorId': colorid['color'],
-                    },
-                    callback=self.parse_color_variants,
-                    dont_filter=True,
-                    meta={
-                        'garment': garment,
-                        'product_id': prodId,
-                        'color_id': colorid['color']
-                    }
+                self.form_request(
+                    meta=meta,
+                    callback=self.parse_fitting_variants,
+                    form_data=form_data
                 )
             )
         return requests
 
-    def id_from_size(self, response):
-        aq = response.css('.sizelist li a').extract()
-        arr = [re.compile('id="([\d]+)"').search(val).group(1) for val in response.css('.sizelist li a').extract()]
-        return arr
+    def form_request(self, callback, form_data, meta):
+        return FormRequest(url=self.product_api_url, callback=callback, dont_filter=True, formdata=form_data, meta=meta)
 
-    def is_size_id(self, response):
-        if 'id' not in response.css('.sizelist li a').extract_first():
-            return False
-        r = re.compile('id="([\d]+)"')
-        if r.search(response.css('.sizelist li a').extract_first()) is None:
-            return False
-        return True
+    def color_requests(self, response, garment):
+        requests = []
+        for color_sel in response.css('#productDetails .colorlist .link'):
+            if color_sel.css('.disabled'):
+                continue
+            color_id = clean(color_sel.css('img::attr(colorid)'))[0]
+            form_data = {
+                'productId': garment['retailer_sku'],
+                'colorId': color_id
+            }
+            meta = {
+                'garment': garment,
+                'color_id': color_id,
+                'color': clean(color_sel.css('::attr(title)'))[0]
+            }
+            requests.append(
+                self.form_request(callback=self.parse_color_variants, form_data=form_data, meta=meta)
+            )
+        return requests
+
+    def size_requests(self, response):
+        requests = []
+        for size_selector in response.css('.sizelist a'):
+            if size_selector.css('[stocklevel="0"]'):
+                continue
+            size_id = clean(size_selector.css('::attr(id)'))[0]
+            size = clean(size_selector.css('::text'))[0]
+            form_data = {
+                'productId': response.meta['garment']['retailer_sku'],
+                'colorId': response.meta['color_id'],
+                'selectedSize': size,
+            }
+            meta = {
+                'garment': response.meta['garment'],
+                'color_id': response.meta['color_id'],
+                'color': response.meta['color'],
+                'size': size
+            }
+            callback = self.parse_size_variants
+            if size_id.isdigit() and not size_id == size:
+                form_data.update({'skuId': size_id})
+                meta.update({'sku_id': size_id})
+                callback = self.parse_fitting_variants
+            requests.append(
+                self.form_request(form_data=form_data, meta=meta,
+                                  callback=callback,)
+            )
+        return requests
+
+    def skus(self, response):
+        sku = {}
+        color = response.meta['color']
+        size = response.meta['size']
+        sku_id = response.meta['sku_id']
+        sku[sku_id] = {
+                'color': color,
+                'size': size,
+        }
+
+        if 'fit' in response.meta:
+            sku[sku_id].update({'fit': response.meta['fit']})
+        pricing = self.product_pricing_common_new(response)
+        sku[sku_id].update(pricing)
+        return sku
 
     def image_urls(self, response):
-        return response.css('#prod-detail__slider-nav img::attr(src)').extract()
+        main_image = clean(response.css('[itemprop="image"]::attr(src)'))
+        main_image.extend(clean(response.css('#prod-detail__slider-nav img::attr(src)')))
+        return main_image
 
     def product_category(self, response):
-        return response.css('.wrap.breadcrumb .container a::text').extract()[1:]
+        return clean(response.css('.wrap.breadcrumb a::text'))[1:]
 
     def product_care(self, response):
-        features = response.css('.row.pdp_specs .span4 li::text').extract()
-        care = []
-        care.extend([x for x in features if self.care_criteria_simplified(x)])
-        return care
+        features = clean(response.css('.row .span4 li::text'))
+        return [x for x in features if self.care_criteria_simplified(x)]
 
     def product_brand(self, response):
-        name = response.css('h1[itemprop="name"]::text').extract_first()
+        name = self.raw_name(response)
         for brand in self.brands:
             if brand in name:
                 return brand
         return self.brands[-1]
 
-    def product_sizes(self, response):
-        sizes = response.css(".sizelist li a::text").extract()
-        out_of_stock = ['stocklevel="0"' in x for x in response.css(".sizelist li a").extract()]
-        return [{'size': val, 'out_of_stock': out_of_stock[indx]} for indx, val in enumerate(sizes)]
-
-    def product_color_ids(self, response):
-        colors = response.css(".colorlist li a img::attr(colorid)").extract()
-        color_links = response.css(".colorlist li a").extract()
-        in_stock = [ 'disabled' not in x  for x in color_links]
-        return [{'color': val, 'out_of_stock': in_stock[indx]} for indx, val in enumerate(colors)]
-
     def product_name(self, response):
-        name =  response.css('h1[itemprop="name"]::text').extract_first()
-        for brand in self.brands:
-            if brand in name:
-                name = name.replace(brand, '').strip()
+        name = self.raw_name(response)
+        name = clean(name.replace(self.product_brand(response), ''))
         return name
 
     def product_description(self, response):
-        description = response.css('span[itemprop="description"]::text').extract_first()
-        description += '. '.join([x for x in response.css('.row.pdp_specs .span4 li::text').extract() if 'wash' not in x and 'Care' not in x and 'clean' not in x])
+        description = clean(response.css('[itemprop="description"]::text'))
+        description.extend([x for x in clean(response.css('.row .span4 li::text')) if not self.care_criteria_simplified(x)])
         return description
 
     def product_id(self, response):
-        return response.css('span[itemprop="productID"]::text').extract_first()
+        return clean(response.css('[itemprop="productID"]::text'))[0]
 
     def product_gender(self, response):
-        name = response.css('h1[itemprop="name"]::text').extract_first()
-        for gender in self.gender_map:
-            if gender in name:
+        name = self.raw_name(response)
+        for gender in self.genders:
+            if gender in name.lower():
                 return gender
         return 'unisex-adults'
+
+    def raw_name(self, response):
+        return clean(response.css('[itemprop="name"]::text'))[0]
+
 
 class WoolrichCrawlSpider(BaseCrawlSpider, Mixin):
     name = Mixin.retailer + '-crawl'
@@ -292,21 +215,13 @@ class WoolrichCrawlSpider(BaseCrawlSpider, Mixin):
     listing_css = [
         '.dropdown.yamm-fw a.upper',
         '.nav.nav-list.nav-',
-    ]
-    pagination_css = [
+        # pagination-css
         '.clear.addMore'
     ]
     product_css = '.span3.element.productCard a'
 
     rules = (
-        Rule(LinkExtractor(restrict_css=listing_css,), follow=True,
-             callback='paging_requests'),
-        Rule(LinkExtractor(restrict_css=pagination_css, tags=('div',),
-                           attrs=('nextpage',)),
-             follow=True, callback='paging_requests'),
-        Rule(LinkExtractor(restrict_css=product_css,), callback='parse_item')
+        Rule(LinkExtractor(restrict_css=listing_css, tags=('div', 'a'), attrs=('nextpage', 'href')),
+             follow=True, callback='parse'),
+        Rule(LinkExtractor(restrict_css=product_css,), follow=True, callback='parse_item')
     )
-
-    def paging_requests(self, response):
-        # print(colored(response, 'red'))
-        pass
