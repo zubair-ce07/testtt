ffa108e84cd72ba82d0ffd9f653a3ce56fad947c
hammadahmadwaqas <hammadahmadwaqas@github.com>
2017-09-07 12:11:24 +0500

    edit settings file to heroku

--- twitter/twitter/settings.py
+++ twitter/twitter/settings.py
@@ -152,10 +152,10 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# # Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
