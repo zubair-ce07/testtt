bf823ba80433a7bc772269af81a18f31138cd9b1
Awaish kumar <awaish.kumar@arbisoft.com>
2017-09-07 12:43:21 +0500

    changes made

--- figleaves_spider.py
+++ figleaves_spider.py
@@ -19,14 +19,17 @@ class FigLeavesParseSpider(BaseParseSpider, Mixin):
     def parse(self, response):
         product_id = self.product_id(response)
         garment = self.new_unique_garment(product_id)
+
         if not garment:
             return
 
         self.boilerplate_normal(garment, response)
         garment['gender'] = self.product_gender(garment)
         garment['image_urls'] = self.image_urls(response)
+
         garment['skus'] = {}
         garment['meta'] = {'requests_queue': self.size_requests(response) + self.colour_requests(response)}
+
         return self.next_request_or_garment(garment)
 
     def product_gender(self, garment):
@@ -65,21 +68,22 @@ class FigLeavesParseSpider(BaseParseSpider, Mixin):
         sku = {'colour': response.css('.selected-value ::text').extract_first()}
         sku['size'] = \
             clean(response.css('.product-content-bottom  .variation-select option[selected][data-lgimg]::text'))[0]
+
+        sku.update(self.product_pricing_common_new(response))
+        if 'Out' in clean(response.css('.product-content-bottom .availability-msg ::text')):
+            sku['out_of_stock'] = True
+
         skus[sku['colour'] + "_" + sku['size']] = sku
         return skus
 
     def parse_size(self, response):
         garment = response.meta['garment']
         garment['skus'].update(self.skus(response))
-        for sku in garment['skus'].values():
-            if sku['size'] in response.url:
-                sku.update(self.product_pricing_common_new(response))
-                if 'Out' in clean(response.css('.product-content-bottom .availability-msg ::text')):
-                    sku['out_of_stock'] = True
         return self.next_request_or_garment(garment)
 
     def parse_colour(self, response):
         garment = response.meta['garment']
+        garment['image_urls'] += self.image_urls(response)
         garment['meta']['requests_queue'] += self.size_requests(response)
         return self.next_request_or_garment(garment)
 
@@ -96,11 +100,6 @@ class FigLeavesCrawlSpider(BaseCrawlSpider, Mixin):
     name = Mixin.retailer + "-crawl"
     parse_spider = FigLeavesParseSpider()
 
-    pagination_css = [
-        '.infinite-scroll-button',
-        '.infinite-scroll-placeholder'
-    ]
-
     products_css = '.search-result-items'
 
     deny = [
@@ -110,11 +109,17 @@ class FigLeavesCrawlSpider(BaseCrawlSpider, Mixin):
 
     listing_css = [
         '.header-nav-strip',
+        '.infinite-scroll-button',
+        '.infinite-scroll-placeholder'
     ]
 
+    tags = ['div', 'button', 'a']
+
+    attributes = ['data-grid-url', 'href']
+
     rules = (
-        Rule(LinkExtractor(restrict_css=pagination_css, tags=['div', 'button'], attrs=['data-grid-url']),
+        Rule(LinkExtractor(restrict_css=listing_css, deny=deny, tags=tags, attrs=attributes),
              callback='parse'),
-        Rule(LinkExtractor(restrict_css=listing_css, deny=deny), callback='parse'),
+
         Rule(LinkExtractor(restrict_css=products_css), callback='parse_item'),
     )
