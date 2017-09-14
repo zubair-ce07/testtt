8d63df305938947baa6f9eeb30e5a8f9e5aabd4b
Luqman-Ud-Din <luqman.muhammad@arbisoft.com>
2017-09-14 16:22:50 +0500

    refactored arezzo spider

--- training_scrapy/training/spiders/arezzo.py
+++ training_scrapy/training/spiders/arezzo.py
@@ -1,9 +1,10 @@
-import scrapy
-from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+# from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+from scrapy.linkextractor import LinkExtractor
 from scrapy.spiders import Rule
 from scrapy.spiders import CrawlSpider
 import datetime
 import re
+
 
 class ArezzoSpider(CrawlSpider):
     name = 'arrezo-br'
@@ -11,32 +12,13 @@ class ArezzoSpider(CrawlSpider):
     language = 'pt'
     currency = 'BRL'
 
-    price_regex = '\s*R\s*\$\s*'
     brand_regex = '(\s*Disney x Arezzo\s*\|\s*)'
-    color_regex = '\A\s*Cor\s*:\s*'
     merch_info_regex = '(\s*\[PRE VENDA\]\s*\-?\s*)'
-    retailer_sku_regex = '\A\s*Referência\s*:\s*'
-
-    title_css = '.arz-product-detail .arz-product-title span::text'
-    price_css = '.arz-product-detail .arz-product-price::text'
-    category_css = '#breadcrumb a::text'
-    images_url_css = '.arz-product-gallery-thumb img::attr(data-original)'
-    description_css = '.arz-description p:nth-child(2)::text'
-    size_available_css = '.arz-btn-secondary.arz-sm:not(.arz-disabled) span::text'
-    size_not_available_css = '.arz-btn-secondary.arz-sm.arz-disabled span::text'
-    information_css = '.arz-product-description-fix ::text'
 
     rules = (
-
+        Rule(LinkExtractor(restrict_css=['.arz-nav'],),),
         Rule(
-            LxmlLinkExtractor(
-                restrict_css=['.arz-navbar', '.pagination'],
-            ),
-        ),
-        Rule(
-            LxmlLinkExtractor(
-                restrict_css=['.arz-home-sliders .arz-cover-link'],
-            ),
+            LinkExtractor(restrict_css=['.arz-home-sliders .arz-cover-link'],),
             callback='parse_product',
         ),
     )
@@ -49,40 +31,35 @@ class ArezzoSpider(CrawlSpider):
         product = {}
         product['crawl_start_time'] = datetime.datetime.now()
 
-        raw_product_name = response.css(self.title_css).extract()
+        raw_product_name = self.product_raw_name(response)
+        raw_information = self.product_raw_information(response)
+        size_list = self.product_size(response)
+        price = self.product_price(response)
 
-        product['name'] = self.get_product_name(raw_product_name)
-        product['brand'] = self.get_product_brand(raw_product_name)
-        product['merch_info'] = self.get_merch_info(raw_product_name)
-        product['category'] = self.get_product_category(response.css(self.category_css).extract())
-        product['image_urls'] = response.css(self.images_url_css).extract()
-        product['description'] = response.css(self.description_css).extract()
+        color = self.product_color(raw_information)
+
+        product['name'] = self.product_name(raw_product_name)
+        product['brand'] = self.product_brand(raw_product_name)
+        product['merch_info'] = self.merch_info(raw_product_name)
+        product['retailer_sku'] = self.product_retailer_sku(raw_information)
+        product['category'] = self.product_category(response)
+        product['image_urls'] = self.product_image_urls(response)
+        product['description'] = self.product_description(response)
+        product['sku'] = self.product_skus(color, size_list, price)
+        product['out_of_stock'] = self.product_out_of_stock(product['sku'])
         product['gender'] = self.gender
         product['spider_name'] = self.name
         product['lang'] = self.language
         product['url'] = response.url
         product['url_original'] = response.url
 
-        raw_information = response.css(self.information_css).extract()
-
-        product['retailer_sku'] = self.get_product_information(self.retailer_sku_regex, raw_information)
-
-        color = self.get_product_information(self.color_regex, raw_information)
-        size_list = self.get_product_size_list(response)
-        raw_price = response.css(self.price_css).extract()
-        price = self.get_product_information(self.price_regex, raw_price)
-
-        product['sku'] = self.get_product_sku_list(color, size_list, price)
-        product['out_of_stock'] = False if product['sku'] else True
-
         return product
 
-    def get_product_sku_list(self, color, size_list, price):
+    def product_skus(self, color, size_list, price):
         sku_list = []
         for size, available in size_list:
             sku = {}
             sku['out_of_stock'] = not available
-            sku['gender'] = self.gender
             sku['sku_id'] = '{color}_{size}'.format(color=color, size=size)
             sku['size'] = size
             sku['currency'] = self.currency
@@ -90,39 +67,80 @@ class ArezzoSpider(CrawlSpider):
             sku_list.append(sku)
         return sku_list
 
-    def get_product_name(self, values):
+    def product_name(self, values):
         name = ' '.join(values)
         name = re.sub(self.merch_info_regex, '', name)
         return re.sub(self.brand_regex, '', name)
 
-    def get_product_category(self, categories):
+    def product_category(self, response):
+        category_css = '#breadcrumb a::text'
+        categories = response.css(category_css).extract()
         if categories:
             categories.pop(0)
             categories.pop()
         return categories
 
-    def get_product_brand(self, name):
+    def product_brand(self, name):
         name = ' '.join(name)
         brand = re.search(self.brand_regex, name)
         if brand:
             return 'Disney x Arezzo'
         return 'Arezzo'
 
-    def get_product_information(self, regex, raw_information_list):
+    def product_information(self, regex, raw_information_list):
         for raw_information in raw_information_list:
             status = re.search(regex, raw_information)
             if status:
                 return re.sub(regex, '', raw_information)
         return None
 
-    def get_product_size_list(self, response):
-        available_size = response.css(self.size_available_css).extract()
-        not_available_size = response.css(self.size_not_available_css).extract()
+    def product_size(self, response):
+        size_available_css = '.arz-btn-secondary.arz-sm:not(.arz-disabled) span::text'
+        size_not_available_css = '.arz-btn-secondary.arz-sm.arz-disabled span::text'
+
+        available_size = response.css(size_available_css).extract()
+        not_available_size = response.css(size_not_available_css).extract()
         return [(size, True) for size in available_size] + [(size, False) for size in not_available_size]
 
-    def get_merch_info(self, name):
+    def merch_info(self, name):
         name = ' '.join(name)
         status = re.search(self.merch_info_regex, name)
         if status:
             return ['PRE-SALE']
         return []
+
+    def product_image_urls(self, response):
+        images_url_css = '.arz-product-gallery-thumb img::attr(data-original)'
+        return response.css(images_url_css).extract()
+
+    def product_description(self, response):
+        description_css = '.arz-description p:nth-child(2)::text'
+        return response.css(description_css).extract()
+
+    def product_price(self, response):
+        price_regex = '\s*R\s*\$\s*'
+        price_css = '.arz-product-detail .arz-product-price::text'
+        raw_price = response.css(price_css).extract()
+        return self.product_information(price_regex, raw_price)
+
+    def product_color(self, raw_information):
+        color_regex = '\A\s*Cor\s*:\s*'
+        return self.product_information(color_regex, raw_information)
+
+    def product_retailer_sku(self, raw_information):
+        retailer_sku_regex = '\A\s*Referência\s*:\s*'
+        return self.product_information(retailer_sku_regex, raw_information)
+
+    def product_raw_name(self, response):
+        title_css = '.arz-product-detail .arz-product-title span::text'
+        return response.css(title_css).extract()
+
+    def product_raw_information(self, response):
+        information_css = '.arz-product-description-fix ::text'
+        return response.css(information_css).extract()
+
+    def product_out_of_stock(self, skus):
+        for sku in skus:
+            if not sku.get('out_of_stock', False):
+                return False
+        return True
