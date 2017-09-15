3deff74e7eebd95f52937b5502e025bf1546f5dd
Luqman-Ud-Din <luqman.muhammad@arbisoft.com>
2017-09-15 21:58:31 +0500

    fixed comments arezzo spider

--- training_scrapy/training/spiders/arezzo.py
+++ training_scrapy/training/spiders/arezzo.py
@@ -3,7 +3,8 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Rule
 from scrapy.spiders import CrawlSpider
 import datetime
-from training.utils import clean_price, currency_name
+import re
+from training.utils import clean_and_convert_price, currency_information
 
 
 class ArezzoSpider(CrawlSpider):
@@ -13,117 +14,105 @@ class ArezzoSpider(CrawlSpider):
 
     visited_references = dict()
 
-    rules = (
-        Rule(LinkExtractor(restrict_css=['.arz-nav'], ), callback='request_next_page'),
-        Rule(LinkExtractor(restrict_css=['.arz-product-wrapper .arz-cover-link'], ), callback='parse_product'),
-    )
+    # rules = (
+    #     Rule(LinkExtractor(restrict_css=['.arz-nav'], ), callback='request_next_page'),
+    #     Rule(LinkExtractor(restrict_css=['.arz-product-wrapper .arz-cover-link'], ), callback='parse_product'),
+    # )
 
     start_urls = [
-        'https://www.arezzo.com.br',
+        'https://www.arezzo.com.br/bolsas/bolsa-satchel-persona-me-vinho/p/5001301710004U',
+        # 'https://www.arezzo.com.br',
     ]
 
     def request_next_page(self, response):
         yield from self.parse(response)
 
         url = self.next_page_url(response)
-        yield Request(url=url, callback=self.request_next_page) if url else None
+        if url:
+            yield Request(url=url, callback=self.request_next_page)
 
-    def parse_product(self, response):
-        if self.visited(response):
+    def parse(self, response):
+        retailer_sku = self.product_retailer_sku(response)
+        if retailer_sku in self.visited_references:
             return
 
+        self.visited_references[retailer_sku] = True
         product = {
             'crawl_start_time': datetime.datetime.now(),
             'name': self.product_name(response),
             'brand': self.product_brand(response),
             'merch_info': self.merch_info(response),
-            'retailer_sku': self.product_retailer_sku(response),
+            'retailer_sku': retailer_sku,
             'category': self.product_category(response),
             'image_urls': self.product_image_urls(response),
             'description': self.product_description(response),
             'sku': self.product_skus(response),
+            'care': self.product_care(response),
             'gender': self.gender,
             'spider_name': self.name,
             'lang': self.language,
             'url': response.url,
             'url_original': response.url
         }
-        product['out_of_stock'] = self.product_out_of_stock(product['sku'])
+
+        if self.product_out_of_stock(response):
+            product['out_of_stock'] = True
 
         return product
 
-    def visited(self, response):
-        if self.visited_references.get(self.product_retailer_sku(response), False):
-            return True
-        self.visited_references[self.product_retailer_sku(response)] = True
-        return False
-
     def product_skus(self, response):
-        product_size = self.product_size(response)
+        size_css = '.arz-btn-secondary.arz-sm'
+        size_unavailable_css = '.arz-disabled span::text'
+        size_available_css = ':not(.arz-disabled) span::text'
+        skus = []
         price, currency = self.product_price_and_currency(response)
         color = self.product_color(response)
-
-        skus = []
-        for size, available in product_size.items():
+        for selector in response.css(size_css):
+            out_of_stock = False
+            if selector.css(size_unavailable_css):
+                out_of_stock = True
+                size = selector.css(size_unavailable_css).extract_first().strip()
+            else:
+                size = selector.css(size_available_css).extract_first().strip()
             sku = {
-                'out_of_stock': not available,
                 'sku_id': '{color}_{size}'.format(color=color, size=size),
                 'size': size,
                 'currency': currency,
                 'price': price,
                 'color': color,
-                'care': self.product_care(response)
             }
+
+            if out_of_stock:
+                sku['out_of_stock'] = out_of_stock
+
             skus.append(sku)
 
         return skus
 
     def product_name(self, response):
-        remove_string = ['Disney x Arezzo', '[PRE VENDA]', '-']
+        remove_string_regex = '(\s*Disney x Arezzo\s*|\s*\[PRE VENDA\]\s*-\s*|\|)+'
         name = self.product_raw_name(response)
-        for string in remove_string:
-            if string in name:
-                name = name.replace(string, '')
-        return name.strip()
+        return re.sub(remove_string_regex,'',name).strip()
 
     def product_category(self, response):
         category_css = '#breadcrumb a::text'
         categories = response.css(category_css).extract()
-        return categories[1:-1] if categories else None
+        return categories[1:-1] if categories else []
 
     def product_brand(self, response):
         brands = ['Disney x Arezzo', 'Arezzo']
         name = self.product_raw_name(response)
-        for brand in brands:
-            if brand in name:
-                return brand
-        return 'Arezzo'
+        return brands[0] if brands[0] in name else brands[1]
 
     def merch_info(self, response):
-        tags = {
-            '[PRE VENDA]': 'PRE-SALE'
-        }
-        merch_info_tags = list()
         name = self.product_raw_name(response)
-        for key, value in tags.items():
-            if key in name:
-                merch_info_tags.append(value)
-        return merch_info_tags
+        return ['PRE-SALE'] if '[PRE VENDA]' in name else []
 
     def product_information(self, key, raw_information):
         for information in raw_information:
             if key in information:
                 return information.replace(key, '').strip()
         return None
-
-    def product_size(self, response):
-        size_available_css = '.arz-btn-secondary.arz-sm:not(.arz-disabled) span::text'
-        size_not_available_css = '.arz-btn-secondary.arz-sm.arz-disabled span::text'
-
-        available_size = response.css(size_available_css).extract()
-        not_available_size = response.css(size_not_available_css).extract()
-
-        return {**{size: True for size in available_size}, **{size: False for size in not_available_size}}
 
     def product_image_urls(self, response):
         images_url_css = '.arz-product-gallery-thumb img::attr(data-original)'
@@ -139,18 +128,21 @@ class ArezzoSpider(CrawlSpider):
         for css in price_css:
             price = response.css(css).extract_first()
             if price:
-                currency =  currency_name(price)
-                return (clean_price(price), currency)
+                currency = currency_information(price)
+                return (clean_and_convert_price(price, currency[1]), currency[0])
         return (None, None)
 
     def product_color(self, response):
-        raw_information = self.product_raw_information(response)
-        return self.product_information('Cor:', raw_information)
+        characteristics = self.product_characteristics(response)
+        for characteristic in characteristics:
+            if 'Cor:' in characteristic:
+                return characteristic.replace('Cor:', '').strip()
+        return None
 
     def product_care(self, response):
-        care = ['Material']
-        raw_information = self.product_raw_information(response)
-        return [information.strip() for information in raw_information for key in care if key in information]
+        care = 'Material'
+        characteristics = self.product_characteristics(response)
+        return [characteristic.strip() for characteristic in characteristics if care in characteristic]
 
     def product_retailer_sku(self, response):
         return response.url.split('/')[-1]
@@ -159,15 +151,13 @@ class ArezzoSpider(CrawlSpider):
         title_css = '.arz-product-detail .arz-product-title span::text'
         return response.css(title_css).extract_first()
 
-    def product_raw_information(self, response):
+    def product_characteristics(self, response):
         information_css = '.arz-product-description-fix :not([class])::text'
         return [value.strip() for value  in response.css(information_css).extract() if value.strip()]
 
-    def product_out_of_stock(self, skus):
-        for sku in skus:
-            if not sku.get('out_of_stock', False):
-                return False
-        return True
+    def product_out_of_stock(self, response):
+        out_of_stock_css = '.arz-product-available-info .arz-product-line span::text'
+        return False if response.css(out_of_stock_css).extract_first() == 'Comprar' else True
 
     def page_category(self, response):
         category_css = '#breadcrumb a::text'
@@ -185,4 +175,3 @@ class ArezzoSpider(CrawlSpider):
 
         if next_page and page_category:
             return url.format(category=page_category.lower(), number=next_page)
-        return None
--- training_scrapy/training/utils.py
+++ training_scrapy/training/utils.py
@@ -1,16 +1,20 @@
-def clean_price(raw_price):
-    price = ''
-    for digit in raw_price:
-        if digit.isnumeric():
-            price += digit
-    return price
+import re
 
-def currency_name(amount):
+
+def clean_and_convert_price(raw_price, currency_conversion_rate):
+    if currency_conversion_rate:
+        price_regex = '\d+\.?\d*|$'
+        price = re.findall(price_regex,raw_price)[0]
+        return str(float(price) * currency_conversion_rate) if price else '0'
+    return '0'
+
+def currency_information(amount):
     currency = {
-        'R$': 'BRL'
+        'R$': ('BRL', 0.32 * 100),
+        '€': ('EURO', 1.2 * 100)
     }
 
-    for symbol, name in currency.items():
+    for symbol, information in currency.items():
         if symbol in amount:
-            return name
-    return None
+            return information
+    return (None, None)
