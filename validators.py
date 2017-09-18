88e01038606be758eed5514367ae95ea82555204
Hamza Ali <31878888+imhamzaa@users.noreply.github.com>
2017-09-18 11:52:04 +0500

    Update validators.py

--- validators.py
+++ validators.py
@@ -4,26 +4,18 @@ import datetime
 
 
 # validate_year takes the value and validate for year
-def validate_year(year):
+def validate_date(year=1970, month=1):
     """
     :param year:
+    :param month:
     :return:
     """
     try:
-        if year:
-            if year.isdigit():
-                year = int(year)
-                current_year = datetime.datetime.now().year
-                if 1900 <= year <= current_year:
-                    return year
-                else:
-                    return False
-            else:
-                return False
-        else:
-            return None
-    except ValueError:
-        return "Format Error"
+        year = int(year)
+        datetime.datetime(year=year, month=month, day=1)
+        return year
+    except:
+        return False
 
 
 # validate_year takes the value and validate for year
@@ -33,25 +25,11 @@ def validate_year_and_month(year_month):
     :return:
     """
     try:
-        if year_month:
-            year = year_month.split('/')[0]
-            is_year_valid= validate_year(year)
-            if is_year_valid:
-                month = year_month.split('/')[1]
-                if month.isdigit():
-                    month = int(month)
-                    if 1 <= month <= 12:
-                        return year_month
-                    else:
-                        return False
-                else:
-                    return False
-            else:
-                return False
-        else:
-            return None
-    except ValueError:
-        return "Format Error"
+        year_month_value = year_month.split('/')
+        year = validate_date(year_month_value[0],year_month_value[1])
+        return year_month
+    except:
+        return False
 
 
 def validate_path(path):
@@ -59,26 +37,22 @@ def validate_path(path):
     :param path:
     :return:
     """
-    if path:
-        is_directory_exist= os.path.isdir(path)
-        if is_directory_exist:
-            return path
-        else:
-            return False
+    is_directory_exist = os.path.isdir(path)
+    if is_directory_exist:
+        return path
     else:
-        return None
+        return False
 
 
-def verify_year_month_weather_readings_exist_and_read_file_paths(path,year,month="*"):
+def verify_year_month_weather_readings_exist_and_read_file_paths(path, year, month="*"):
     """
     :param path:
     :param year:
     :param month:
     :return:
     """
-    file_paths = []
-    file_path = path + "/Murree_weather_" + str(year) + "_" + month + ".txt"
-    for f_path in glob.glob(file_path):
-        file_paths.append(f_path)
+    file_paths = list()
+    file_path = path + "/Murree_weather_" + year + "_" + month + ".txt"
+    for file_path_ in glob.glob(file_path):
+        file_paths.append(file_path_)
     return file_paths
-
