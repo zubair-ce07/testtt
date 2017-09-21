385bd26ce64c4130e11db5baa8a41c445c90adf9
Luqman-Ud-Din <luqman.muhammad@arbisoft.com>
2017-09-21 17:58:09 +0500

    refactored clean_price in utils.py

--- training_scrapy/training/utils.py
+++ training_scrapy/training/utils.py
@@ -8,13 +8,10 @@ currency = {
 
 
 def clean_price(raw_price):
-    regex = '\d+[\,\.]?[\d+]*[[\,\.]\d\d]?|$'
+    regex = '(\d[\d\.\,]*)|$'
     raw_price = re.findall(regex, raw_price)[0]
-    if re.search('[.]\d\d$', raw_price):
-        return raw_price.replace(',', '')
-    elif re.search('[,]\d\d$', raw_price):
-        return raw_price.replace('.', '').replace(',', '.')
-    return re.sub('[,.]', '', raw_price)
+    return '{decimal}{fraction}'.format(decimal=re.sub('[,.]', '', re.split('[,.]\d\d$',raw_price)[0]),
+                                        fraction=re.findall('[,.]?\d\d$|$',raw_price)[0].replace(',','.'))
 
 
 def currency_unit(price):
@@ -32,7 +29,7 @@ def pricing(response, css):
     if prices:
         currency = currency_information(prices[0])
         prices = [clean_price(price) for price in prices if price.strip()]
-        price_in_min_unit = sorted([currency_unit(price) for price in prices if price.strip()])
+        price_in_min_unit = sorted([currency_unit(price) for price in prices if price and price.strip()])
         return {
             'price': price_in_min_unit[0],
             'previous_price': price_in_min_unit[1:],
