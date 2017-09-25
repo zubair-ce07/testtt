414d07a1d741fe1740318d1f2ee1fd5a68f292ea
Hamza Ali <31878888+imhamzaa@users.noreply.github.com>
2017-09-25 18:59:59 +0500

    puket spider to crawl garment products information

--- puket_spider.py
+++ puket_spider.py
@@ -1,62 +1,81 @@
+import re
+import json
 import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 
 
-class Skus:
-    def __int__(self):
-        self.sku_id = ""
-        self.size = ""
-        self.price = ""
-        self.currency = ""
-        self.previous_prices = []
+class PuketSpider(CrawlSpider):
 
-
-class Garment:
-    def __int__(self):
-        self.price = ""
-        self.name = ""
-        self.lang = ""
-        self.gender = ""
-        self.currency = ""
-        self.brand = ""
-        self.image_urls = []
-        self.category = []
-        self.care = []
-        self.skus = []
-        self.trail = []
-        self.retailer_sku = ""
-
-
-class PuketSpider(scrapy.Spider):
     name = "puket"
-    start_urls = [
-        'http://www.puket.com.br/mulher/pijamas/pijamas-curtos/pijama-curto-manga-curta-multibichos-adulto_1707544?skuId=0306016043902',
-    ]
+    allowed_domains = ['puket.com.br']
+    rules = (
+        Rule(LinkExtractor(allow='a.next'), callback='parse_product'),
+    )
 
-    def parse(self, response):
+    def start_requests(self):
+        yield scrapy.Request('http://www.puket.com.br/', callback=self.parse_menu)
 
-        garment = Garment()
-        garment.lang = response.css('html::attr(lang)').extract_first()
-        garment.name = response.css('article.infoProduct > h1.nameProduct::text').extract_first()
+    def parse_menu(self, response):
+        menu_urls = response.css('div#topMenu a::attr(href)').extract()[:-3]
+        for menu_url in menu_urls:
+            yield scrapy.Request(url=menu_url, callback=self.parse_product_category)
 
-        garment.category = response.css('div.product-breadcrumb > a::text').extract()[1:]
-        garment.gender = garment.category[0]
-        garment.trail = [response.css('div.product-breadcrumb > a::attr(href)').extract()[1]]
-        garment.care = response.css('div.descProduct::text').extract()[1:]
-        garment.image_urls = response.css('ul.list-thumbs > li > a::attr(href)').extract()
+    def parse_product_category(self, response):
+        category_urls = response.css('div.shelf-category-list a::attr(href)').extract()
+        for category_url in category_urls:
+            yield scrapy.Request(url=category_url, callback=self.parse_product)
 
-        garment.brand = response.css('h1.logo > a > img::attr(alt)').extract_first()
-        garment.price = response.css('article.infoProduct > div.priceProduct > p.bestPrice > span.val::text').extract_first()
-        garment.currency = response.css('article.infoProduct > div.priceProduct > span[itemprop="priceCurrency"]::attr(content)').extract_first()
-        garment.retailer_sku = response.css('span#product-showcase-image::attr(data-product-id)').extract_first()
+    def parse_product(self, response):
+        product_url = response.css('div.box-product a.shelf-url::attr(href)').extract_first()
+        if product_url:
+            yield response.follow(product_url, callback=self.parse_product_info)
 
-        skus = response.css('div.wrap-sku-selection > fieldset > div[data-dimension="Tamanho"] > ul > li').extract()
-        for sku in skus:
-            sku_ = Skus()
-            sku_.sku_id = sku.css('input::attr(data-skurel)').extract_first()
-            sku_.size = sku.css('input::attr(value)').extract_first()
-            sku_.price = garment.price
-            sku_.currency = garment.currency
-            sku_.previous_prices = response.css('article.infoProduct > div.priceProduct > p.oldPrice > span.val::text').extract()
-            garment.skus.append(sku_.__dict__)
+    def parse_product_info(self, response):
 
-        return garment.__dict__
+        garment_skus = []
+        trail = []
+
+        def extract_with_css(selector):
+            return response.css(selector).extract_first()
+
+        def extract_all_with_css(selector):
+            return response.css(selector).extract()
+
+        visited_urls = response.css('.product-breadcrumb a::attr(href)').extract()
+        for url in visited_urls:
+            trail.append(response.urljoin(url))
+
+        care = response.css('.descProduct::text').extract()[2:]
+        if not care:
+            care = response.css('.descProduct p::text').extract()[2:]
+
+        puket_skus = response.css('script[xml\:space="preserve"]::text').extract_first()
+        puket_skus = re.search('(skuJson = )(.*\n)*', puket_skus).group(0).split('skuJson = ')[1].rsplit(';', 1)[0]
+        puket_skus = json.loads(puket_skus)
+
+        for sku in puket_skus["skus"]:
+            garment_skus.append({
+                'sku_id': sku['sku'],
+                'price': sku['bestPrice'],
+                'previous_prices': sku['listPrice'],
+                'currency': sku['currency'],
+                'colour': sku['udas']['COR'],
+                'size': sku['udas']['TAMANHO']
+            })
+
+        yield {
+            'url': response.urljoin(puket_skus["product_url"]),
+            'gender': puket_skus['udasProd']['google_gender'],
+            'retailer_sku': puket_skus['productId'],
+            'brand': puket_skus['udasProd']['marca'],
+            'lang': extract_with_css('html::attr(lang)'),
+            'name': extract_with_css('article.infoProduct .nameProduct::text'),
+            'price': extract_with_css('.bestPrice .val::attr(content)'),
+            'currency': extract_with_css('[itemprop="priceCurrency"]::attr(content)'),
+            'care': care,
+            'category': extract_all_with_css('div.product-breadcrumb > a::text')[1:],
+            'trail': trail,
+            'image_urls': extract_all_with_css('ul.list-thumbs a::attr(href)'),
+            'skus': garment_skus
+        }
