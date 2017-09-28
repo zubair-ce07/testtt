fc27aaf3aeaad221ec1e12402623ae1c2c113447
ali-ashraf-karimi <ali.ashraf@arbisoft.com>
2017-09-28 18:25:25 +0500

    Used exact path for routing

--- reactnd-project-myreads-starter/src/App.js
+++ reactnd-project-myreads-starter/src/App.js
@@ -3,21 +3,14 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookShelf from './components/BookShelf.js'
 import Book from './components/Book.js'
-import {HashRouter, Route, Link, Redirect} from 'react-router-dom'
+import {HashRouter, Route, Link} from 'react-router-dom'
 
 class BooksApp extends React.Component {
     constructor()
     {
         super();
         this.state = {
-            /**
-             * TODO: Instead of using this state variable to keep track of which page
-             * we're on, use the URL in the browser's address bar. This will ensure that
-             * users can use the browser's back and forward buttons to navigate between
-             * pages, as well as provide a good URL they can bookmark and share.
-             */
             books: [],
-            showSearchPage: false,
             searchResults: []
 
         };
@@ -102,8 +95,7 @@ class BooksApp extends React.Component {
         return (
             <HashRouter>
                 <div>
-                    <Redirect from="/" to="/list" />
-                    <Route path='/list' component={this.listBooks}/>
+                    <Route exact path='/' component={this.listBooks}/>
                     <Route path='/search' component={this.searchBooks}/>
                 </div>
             </HashRouter>
