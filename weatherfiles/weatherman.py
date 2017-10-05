b301511039fc6bb11b89047c567ba510f965f8d3
MZohaibAsaad <zohaib.asaad@gmail.com>
2017-10-05 16:15:50 +0500

    Completed part 4

--- weatherfiles/weatherman.py
+++ weatherfiles/weatherman.py
@@ -4,6 +4,7 @@ from FileReader import FileReader
 from MonthlyWeatherInfo import MonthlyWeatherInfo
 from ReportGeneratorFactory import ReportGeneratorFactory
 
+
 def main():
     # This command-line parsing code is provided.
     # Make a list of command line arguments, omitting the [0] element
@@ -15,10 +16,16 @@ def main():
         options: -a, -e, -c''')
         sys.exit(1)
 
-    files_data = FileReader().read_files_from_path(args[0], args[2])
-    weathers_info = [MonthlyWeatherInfo(filedata) for filedata in files_data.values()]
-    report_generator = ReportGeneratorFactory().get_report_generator(args[1])
-    report_generator.generate_report(weathers_info)
+    dir = args[0]
+
+    options = args[1:]
+    while len(options)>1:
+        files_data = FileReader().read_files_from_path(dir, options[1])
+        weathers_info = [MonthlyWeatherInfo(filedata) for filedata in files_data.values()]
+        report_generator = ReportGeneratorFactory().get_report_generator(options[0])
+        report_generator.generate_report(weathers_info)
+        options = options[2:]
+        print("-"*20)
 
 
 if __name__ == '__main__':
