6977efdc58ba119b3cd447c6c3696f242bae56c8
MZohaibAsaad <zohaib.asaad@gmail.com>
2017-10-05 11:26:07 +0500

    Setup basic inheritance structure for generating report

--- weatherfiles/weatherman.py
+++ weatherfiles/weatherman.py
@@ -2,8 +2,38 @@ import sys
 import os
 import fnmatch
 
-def weatherman():
-    return
+class MonthlyWeatherInfo:
+    def __init__(self, weather_info_list):
+        del(weather_info_list[0])
+        self.month = weather_info_list[0].split(',')[0]
+        self.daily_weathers_info = [DailyWeatherInfo(weather_info) for weather_info in weather_info_list]
+
+
+class DailyWeatherInfo:
+    def __init__(self, weather_info_string):
+        weather_info = weather_info_string.split(',')
+        self.date = weather_info[0]
+        self.max_temp = weather_info[1]
+        self.mean_temp = weather_info[2]
+        self.min_temp = weather_info[3]
+        self.max_humidity = weather_info[7]
+        self.mean_humidity = weather_info[8]
+        self.min_humidity = weather_info[9]
+
+class ReportGeneratorFactory:
+    def get_report_generator(self, option):
+        if option == '-e':
+            return EReportGenerator()
+
+class ReportGenerator:
+    def generate_report(self, data):
+        return
+
+class EReportGenerator(ReportGenerator):
+    def generate_report(self, data):
+        print(data[0].month)
+        print(data[0].daily_weathers_info[0].max_temp)
+        return
 
 
 def get_filenames_from_dir(dir, year):
@@ -26,8 +56,8 @@ def read_files(filenames):
 def read_files_from_path(dir, year):
     filenames = get_filenames_from_dir(dir, year)
     filesdata = read_files(filenames)
-    print(filesdata)
-    return
+    weathers_info = [MonthlyWeatherInfo(filedata) for filedata in filesdata.values()]
+    return weathers_info
 
 
 def main():
@@ -41,7 +71,9 @@ def main():
         options: -a, -e, -c''')
         sys.exit(1)
 
-    read_files_from_path(args[0], args[2])
+    weathers_info = read_files_from_path(args[0], args[2])
+    report_generator = ReportGeneratorFactory().get_report_generator(args[1])
+    report_generator.generate_report(weathers_info)
 
 if __name__ == '__main__':
     main()
