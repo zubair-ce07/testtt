5a6faf4c2706a881e4ef1b6bbe72c6c183da8441
marslan02 <m.arslan@arbisoft.com>
2017-10-06 11:31:44 +0500

    Completed part4 and bonus

--- dailyweather.py
+++ dailyweather.py
@@ -1,17 +1,20 @@
 class DailyWeather:
-
-    weather = {}
+    PKT = ''
+    max_temperature = 0
+    min_temperature = 0
+    max_humidity = 0
+    mean_humidity = 0
 
     def __init__(self):
-        self.weather = {}
+        self.PKT = ''
+        self.max_temperature = 0
+        self.min_temperature = 0
+        self.max_humidity = 0
+        self.mean_humidity = 0
 
     def set_weather(self, data):
-        if 'PKT' in data:
-            self.weather['PKT'] = data['PKT']
-        elif 'PKST' in data.keys():
-            self.weather['PKT'] = data['PKST']
-        self.weather['Max TemperatureC'] = data['Max TemperatureC']
-        self.weather['Min TemperatureC'] = data['Min TemperatureC']
-        self.weather['Max Humidity'] = data['Max Humidity']
-        self.weather['Mean Humidity'] = data[' Mean Humidity']
-
+        self.PKT = data['PKT'] if 'PKT' in data else data['PKST']
+        self.max_temperature = data['Max TemperatureC']
+        self.min_temperature = data['Min TemperatureC']
+        self.max_humidity = data['Max Humidity']
+        self.mean_humidity = data[' Mean Humidity']
--- weather_man.py
+++ weather_man.py
@@ -1,27 +1,19 @@
 import csv
+
 from termcolor import colored
+
 from dailyweather import DailyWeather
 
 
 class WeatherReport:
     files_names = []
 
-    months = {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Jul", 8: "Aug", 9: "Sep",
-              10: "Oct", 11: "Nov", 12: "Dec"}
+    MONTHS_KEY_MAP = {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Jul", 8: "Aug", 9: "Sep",
+                      10: "Oct", 11: "Nov", 12: "Dec"}
 
-    months_complete_name = {"Jan": "January", "Feb": "February", "Mar": "March", "Apr": "April", "May": "May",
-                            "Jun": "June", "Jul": "July", "Aug": "August", "Sep": "September", "Oct": "October",
-                            "Nov": "November", "Dec": "December"}
-
-    WEATHER_KEY_MAP = {1: 'PKT', 2: 'Max TemperatureC', 3: 'Mean TemperatureC', 4: 'Min TemperatureC',
-                       5: 'Dew PointC', 6: 'MeanDew PointC', 7: 'Min DewpointC', 8: 'Max Humidity',
-                       9: ' Mean Humidity', 10: ' Min Humidity', 11: ' Max Sea Level PressurehPa',
-                       12: ' Mean Sea Level PressurehPa',
-                       13: ' Min Sea Level PressurehPa', 14: ' Max VisibilityKm', 15: ' Mean VisibilityKm',
-                       16: ' Min VisibilitykM',
-                       17: ' Max Wind SpeedKm/h', 18: ' Mean Wind SpeedKm/h', 19: ' Max Gust SpeedKm/h',
-                       20: 'Precipitationmm',
-                       21: ' CloudCover', 22: ' Events', 23: 'WindDirDegrees'}
+    MONTHS_NAMES = {"Jan": "January", "Feb": "February", "Mar": "March", "Apr": "April", "May": "May",
+                    "Jun": "June", "Jul": "July", "Aug": "August", "Sep": "September", "Oct": "October",
+                    "Nov": "November", "Dec": "December"}
 
     def __init__(self):
         file = open('files_names', 'rU')
@@ -37,7 +29,7 @@ class WeatherReport:
         return month_data
 
     def __get_month_name(self, month_number):
-        return self.months_complete_name[self.months[int(month_number)]]
+        return self.MONTHS_NAMES[self.MONTHS_KEY_MAP[int(month_number)]]
 
     def __get_day_weather(self, day_data):
         day_weather = DailyWeather()
@@ -45,60 +37,59 @@ class WeatherReport:
         return day_weather
 
     def __compute_average(self, data):
-        """
-        This is not completed yet, Print is to be implemented
-        """
-        average_max_temperature = sum(int(line.weather['Max TemperatureC']) for line in data)/len(data)
-        average_min_temperature = sum(int(line.weather['Min TemperatureC']) for line in data)/len(data)
-        average_mean_humidity = sum(int(line.weather['Mean Humidity']) for line in data)/len(data)
-        print(average_mean_humidity)
+        average_max_temperature = sum(int(line.max_temperature) for line in data) // len(data)
+        average_min_temperature = sum(int(line.min_temperature) for line in data) // len(data)
+        average_mean_humidity = sum(int(line.mean_humidity) for line in data) // len(data)
+        print('Highest Average: ' + str(average_max_temperature) + 'C')
+        print('Lowest Average: ' + str(average_min_temperature) + 'C')
+        print('Average Mean Humidity: ' + str(average_mean_humidity) + '%')
         return None
 
     def get_required_day(self, data, option):
         if option == 1:
-            return max(data, key=lambda x: int(x.weather['Max TemperatureC']))
+            return max(data, key=lambda x: int(x.max_temperature))
         if option == 2:
-            return min(data, key=lambda x: int(x.weather['Min TemperatureC']))
+            return min(data, key=lambda x: int(x.min_temperature))
         if option == 3:
-            return max(data, key=lambda x: int(x.weather['Max Humidity']))
+            return max(data, key=lambda x: int(x.max_humidity))
         return data[0]
 
-    def print_yearly_report(self,highest_temp_day, lowest_temp_day, highest_humidity_day):
-        print('Highest: ' + highest_temp_day.weather['Max TemperatureC'] + 'c on '
-              + self.__get_month_name(highest_temp_day.weather['PKT'].split('-')[1])
-              + ' ' + highest_temp_day.weather['PKT'].split('-')[2])
+    def print_yearly_report(self, highest_temp_day, lowest_temp_day, highest_humidity_day):
 
-        print('Highest: ' + lowest_temp_day.weather['Min TemperatureC'] + 'c on '
-              + self.__get_month_name(lowest_temp_day.weather['PKT'].split('-')[1])
-              + ' ' + lowest_temp_day.weather['PKT'].split('-')[2])
+        print('Highest: ' + highest_temp_day.max_temperature + 'c on '
+              + self.__get_month_name(highest_temp_day.PKT.split('-')[1])
+              + ' ' + highest_temp_day.PKT.split('-')[2])
 
-        print('Highest: ' + highest_humidity_day.weather['Max Humidity'] + '% on '
-              + self.__get_month_name(highest_humidity_day.weather['PKT'].split('-')[1])
-              + ' ' + highest_humidity_day.weather['PKT'].split('-')[2])
+        print('Lowest: ' + lowest_temp_day.min_temperature + 'c on '
+              + self.__get_month_name(lowest_temp_day.PKT.split('-')[1])
+              + ' ' + lowest_temp_day.PKT.split('-')[2])
+
+        print('Humidity: ' + highest_humidity_day.max_humidity + '% on '
+              + self.__get_month_name(highest_humidity_day.PKT.split('-')[1])
+              + ' ' + highest_humidity_day.PKT.split('-')[2])
 
     def get_month_data(self, year_and_month, files_path):
         month_data = []
         year = year_and_month.split('/')[0]
         month = year_and_month.split('/')[1]
-        months_key = {v: k for k, v in self.months.items()}
+        months_key = {v: k for k, v in self.MONTHS_KEY_MAP.items()}
         for file_name in self.files_names:
             if year == file_name.split('_')[2] and month == str(months_key[file_name.split('_')[3].split('.')[0]]):
                 file_data = self.__read_month_weather(files_path + '/' + file_name)
                 for data in file_data:
-                    if data['Max TemperatureC'] and data['Min TemperatureC'] and data['Max Humidity']:
+                    if data['Max TemperatureC'] and data['Min TemperatureC'] and data[' Mean Humidity']:
                         month_data.append(self.__get_day_weather(data))
-        month_name = self.months_complete_name[self.months[int(month)]]
+        month_name = self.MONTHS_NAMES[self.MONTHS_KEY_MAP[int(month)]]
         return year, month_name, month_data
 
     def print_dayily_data(self, year, month_name, month_data):
         print(month_name + ' ' + year)
         for i in range(len(month_data)):
-            max_temp = month_data[i].weather['Max TemperatureC']
-            min_temp = month_data[i].weather['Min TemperatureC']
-            print(str(i+1) + ' ' +
-                  colored('+'*int(max_temp), 'red') + colored('+' * int(min_temp), 'blue')
+            max_temp = month_data[i].max_temperature
+            min_temp = month_data[i].min_temperature
+            print(str(i + 1) + ' ' +
+                  colored('+' * int(max_temp), 'red') + colored('+' * int(min_temp), 'blue')
                   + ' ' + max_temp + 'C - ' + min_temp + 'C')
-        return None
 
     def get_yearly_insights(self, year, files_path):
         year_data = []
@@ -106,13 +97,12 @@ class WeatherReport:
             if year == file_name.split('_')[2]:
                 month_data = self.__read_month_weather(files_path + '/' + file_name)
                 for data in month_data:
-                    if data['Max TemperatureC'] and data['Min TemperatureC']and data['Max Humidity']:
+                    if data['Max TemperatureC'] and data['Min TemperatureC'] and data['Max Humidity']:
                         year_data.append(self.__get_day_weather(data))
         highest_temperature_day = self.get_required_day(year_data, 1)
         lowest_temperature_day = self.get_required_day(year_data, 2)
         highest_humidity_day = self.get_required_day(year_data, 3)
         self.print_yearly_report(highest_temperature_day, lowest_temperature_day, highest_humidity_day)
-        return None
 
     def get_monthly_insights(self, year_and_month, files_path):
         year, month_name, month_data = self.get_month_data(year_and_month, files_path)
@@ -121,4 +111,3 @@ class WeatherReport:
     def get_days_insights(self, year_and_month, files_path):
         year, month_name, month_data = self.get_month_data(year_and_month, files_path)
         self.print_dayily_data(year, month_name, month_data)
-        return None
