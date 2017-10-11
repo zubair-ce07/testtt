a43a919e646a44c45f93ea4fe3d50b275241744c
marslan02 <m.arslan@arbisoft.com>
2017-10-11 11:52:44 +0500

    code refactoring and calculated date

--- scrapy_spiders/carrier_reviews.py
+++ scrapy_spiders/carrier_reviews.py
@@ -1,5 +1,6 @@
 import json
 import re
+from datetime import datetime
 
 import scrapy
 from w3lib.url import add_or_replace_parameter
@@ -9,64 +10,55 @@ class CarrierReviews(scrapy.Spider):
     name = "carriers"
     json_api_start = "https://api.bazaarvoice.com/data/batch.json?passkey=u22jmac9b0y5a4g20u67tooka&" \
                      "apiversion=5.5"
-    json_api_key = "p_PRO_FAMILY"
+    json_company_key = ""
     json_api_ratings = "&filteredstats.q0=reviews&resource.q0=products&filter.q0=id%3Aeq%3A"
     json_api_reviewer = '&filteredstats.q1=reviews&resource.q1=reviews&limit.q1=100' \
-                        '&offset.q1={0}&filter.q1=productid%3Aeq%3A'
+                        '&offset.q1=0&filter.q1=productid%3Aeq%3A'
     start_urls = ['https://www.answerfinancial.com/AboutUs/CarrierPartner/1?a=RETAIL']
-
-    required_data = {}
 
     def parse(self, response):
         for link in response.css('div.reviews-link a::attr(href)'):
-            yield response.follow(link, self.parse_carrier)
+            yield response.follow(url=link, callback=self.parse_carrier)
             # for link in response.css('div.pager-block a::attr(href)').extract()[-1]:
             #    if link:
-            #        yield response.follow(link, self.parse)
+            #        yield response.follow(url=link, callback=self.parse)
 
     def parse_carrier(self, response):
         carrier_name = response.css('div.main-title h1::text').extract_first()
+        overall_ratings = response.css('span.bvseo-ratingValue::text').extract_first()
         script = response.css('script[type="text/javascript"]::text').extract_first()
-        self.json_api_key = re.search(r'p_[A-Z]{3}_FAMILY', script).group(0)
+        self.json_company_key = re.search(r'p_[A-Z]{3}_FAMILY', script).group(0)
         company = {
             'Carrier Name': carrier_name,
-            'Overall Ratings': response.css('span.bvseo-ratingValue::text').extract_first(),
+            'Overall Ratings': overall_ratings,
             'reviews': []
         }
         json_api_url = '{}{}{}'.format(self.json_api_start,
-                                       self.json_api_reviewer.format(0),
-                                       self.json_api_key)
-        yield scrapy.http.Request(url=json_api_url, callback=self.parse_json, meta={'offset': 0, 'company': company})
+                                       self.json_api_reviewer,
+                                       self.json_company_key)
+        yield scrapy.http.Request(url=json_api_url, callback=self.parse_reviews, meta={'offset': 0, 'company': company})
 
-        # for review in response.css('div.bvseo-review'):
-        #    yield{
-        #        'Author': review.css('span[itemprop="author"]::text').extract_first(),
-        #        'Date Published': review.css('meta[itemprop="datePublished"]::attr(content)').extract_first(),
-        #        'Review Text': review.css('span[itemprop="name"]::text').extract_first()
-        #    }
-        # link = response.css('a.bvseo-paginationLink::attr(href)').extract_first()
-        # if link:
-        #    yield response.follow(link, self.parse_carrier)
-
-    def parse_json(self, response):
+    def parse_reviews(self, response):
         company = response.meta['company']
         json_data = json.loads(response.text)
-        filtered_reviews = json_data['BatchedResults']['q1'].get('Results', [])
-        # implement a for loop for all reviews in results and increment offset..
-        for raw_review in filtered_reviews:
+        company_reviews = json_data['BatchedResults']['q1'].get('Results', [])
+        date_time_format = "%Y-%m-%dT%H:%M:%S.%f+00:00"
+
+        for raw_review in company_reviews:
             review = {
-                'name': raw_review.get('UserNickname', 'anonymous'),
-                'city/state': raw_review.get('UserLocation', 'N/A'),
-                'quality_of_service': raw_review['SecondaryRatings'].get('Quality', {'Value': 'N/A'})['Value'],
-                'date': raw_review.get('SubmissionTime', 'N/A'),
-                'description': raw_review.get('Title', 'N/A'),
+                'name': raw_review.get('UserNickname'),
+                'city/state': raw_review.get('UserLocation'),
+                'date': datetime.strptime(raw_review.get('SubmissionTime'), date_time_format).date(),
+                'title': raw_review.get('Title'),
+                'description': raw_review.get('ReviewText'),
                 'competitiveness': raw_review['SecondaryRatings'].get('Competitiveness', {'Value': 'N/A'})['Value'],
                 'discounts': raw_review['SecondaryRatings'].get('Comfort', {'Value': 'N/A'})['Value'],
+                'quality_of_service': raw_review['SecondaryRatings'].get('Quality', {'Value': 'N/A'})['Value'],
             }
             company['reviews'].append(review)
-        if filtered_reviews:
+        if company_reviews:
             offset = int(response.meta['offset']) + 100
             url = add_or_replace_parameter(response.url, 'offset.q1', offset)
-            yield scrapy.Request(url=url, callback=self.parse_json, meta={'offset': offset, 'company': company})
+            yield scrapy.Request(url=url, callback=self.parse_reviews, meta={'offset': offset, 'company': company})
         else:
             yield company
