ab0b5cf2f0b6ad3363389578013488d6737e5a21
Faraz Mirza <faraz.mirza@arbisoft.com>
2017-10-12 18:02:13 +0500

    Implemented the required changes

---
+++ list.py
@@ -0,0 +1,131 @@
+from record import Record
+from termcolor import colored
+
+
+class RecordList:
+    def __init__(self):
+        self.record_list = []
+        self.record = Record()
+
+    def add_record(self, row):
+        self.record = row
+        self.record_list.append(row)
+
+    def print_data(self):
+        for row in self.record_list:
+            print('Date: {}'.format(row.get_date()))
+            print('Max Temperature: {}'.format(row.get_max_temp()))
+            print('Max Humidity: {}'.format(row.get_max_humidity()))
+            print('Min Temperature: {}'.format(row.get_min_temp()))
+            print('Min Humidity: {}\n'.format(row.get_min_humidity()))
+
+    def get_query_data(self, date):
+        new_record_list = RecordList()
+        date = date.replace('/', '-')  # in case the input date is of format yyyy/mm/dd instead of yyyy-mm-dd
+
+        for record in self.record_list:
+            if date in record.get_date():
+                new_record_list.add_record(record)
+                # print(record.get_date())
+        return new_record_list
+
+    def calculate_min_max(self):
+        max = 0
+        max_date = ''
+        min_date = ''
+        humid_date = ''
+        min = 10000
+        humid = 0
+
+        result = []
+        for record in self.record_list:
+            if record.get_max_temp():
+                if record.get_max_temp() > max:
+                    max = record.get_max_temp()
+                    max_date = record.get_date()
+
+            if record.get_min_temp():
+                if record.get_min_temp() < min:
+                    min = record.get_min_temp()
+                    min_date = record.get_date()
+
+            if record.get_max_humidity():
+                if record.get_max_humidity() > humid:
+                    humid = record.get_max_humidity()
+                    humid_date = record.get_date()
+
+        result.append(max)
+        result.append(max_date)
+        result.append(min)
+        result.append(min_date)
+        result.append(humid)
+        result.append(humid_date)
+
+        return result
+
+    # Function to calculate mean averages and store the results in a list
+    def calculate_mean(self):
+        mean_result = []
+        max_avg_temp = 0
+        min_avg_temp = 10000
+        avg_humid = 0
+        count = 0
+        for record in self.record_list:
+            if record.get_mean_humidity() > max_avg_temp:
+                max_avg_temp = record.get_mean_humidity()
+
+            if record.get_mean_temp() < min_avg_temp:
+                min_avg_temp = record.get_mean_temp()
+
+            if record.get_mean_humidity():
+                if avg_humid == 0:
+                    avg_humid = record.get_mean_humidity()
+                    count += 1
+                else:
+                    avg_humid = avg_humid + record.get_mean_humidity()
+                    count += 1
+
+        avg_humid = avg_humid / count
+
+        mean_result.append(max_avg_temp)
+        mean_result.append(min_avg_temp)
+        mean_result.append(avg_humid)
+
+        return mean_result
+
+    # Function to print horizontal bar charts of a given month
+    def generate_bar_chart(self):
+        for record in self.record_list:
+            max_temp = record.get_max_temp()
+            min_temp = record.get_min_temp()
+
+            day = record.get_date().split(sep='-')[2]
+
+            #     Sub-part 4
+            #     print(day + ' ', end='')
+            #     for i in range(max_temp):
+            #         print(colored('+', 'red'), end='')
+            #     print(' ' + str(max_temp) + 'C')
+            #
+            #     print(day + ' ', end='')
+            #     for i in range(min_temp):
+            #         print(colored('+', 'blue'), end='')
+            #     print(' ' + str(min_temp) + 'C')
+
+            # BONUS TASK
+            print('{} '.format(day), end='')
+            for i in range(min_temp):
+                print(colored('+', 'blue'), end='')
+
+            for i in range(max_temp):
+                print(colored('+', 'red'), end='')
+            print(' {}C -'.format(min_temp), end='')
+            print(' {}C'.format(max_temp))
+        print()
+
+
+
+
+
+
+
---
+++ record.py
@@ -0,0 +1,96 @@
+class Record:
+    """Common base class for all records"""
+
+    def __init__(self):
+        self.date = " "
+        self.max_temp = 0
+        self.mean_temp = 0
+        self.min_temp = 0
+        self.dew_point = 0
+        self.mean_dew_point = 0
+        self.min_dew_point = 0
+        self.max_humidity = 0
+        self.mean_humidity = 0
+        self.min_humidity = 0
+        self.max_sea_level_pressure = 0
+        self.mean_sea_level_pressure = 0
+        self.min_sea_level_pressure = 0
+        self.max_sea_level_pressure = 0
+        self.max_visibility = 0.0
+        self.mean_visibility = 0.0
+        self.min_visibility = 0.0
+        self.max_wind_speed = 0
+        self.mean_wind_speed = 0
+        self.max_gust_speed = 0
+        self.precipitation = 0
+        self.cloud_cover = " "
+        self.events = 0
+        self.wind_direction_degree = 0
+
+    def add(self, dict):
+        for key in dict:
+            if key == 'PKT':
+                self.date = dict['PKT']
+            elif key == 'PKST':
+                self.date = dict['PKST']
+
+        if dict['Max TemperatureC']:
+            self.max_temp = int(dict['Max TemperatureC'])
+        if dict['Min TemperatureC']:
+            self.min_temp = int(dict['Min TemperatureC'])
+        if dict['Mean TemperatureC']:
+            self.mean_temp = int(dict['Mean TemperatureC'])
+
+        self.dew_point = dict['Dew PointC']
+        self.mean_dew_point = dict['MeanDew PointC']
+        self.min_dew_point = dict['Min DewpointC']
+
+        if dict['Max Humidity']:
+            self.max_humidity = int(dict['Max Humidity'])
+        if dict[' Min Humidity']:
+            self.min_humidity = int(dict[' Min Humidity'])
+        if dict[' Mean Humidity']:
+            self.mean_humidity = int(dict[' Mean Humidity'])
+
+        self.max_sea_level_pressure = dict[' Max Sea Level PressurehPa']
+        self.mean_sea_level_pressure = dict[' Mean Sea Level PressurehPa']
+        self.min_sea_level_pressure = dict[' Min Sea Level PressurehPa']
+        self.max_visibility = dict[' Max VisibilityKm']
+        self.mean_visibility = dict[' Mean VisibilityKm']
+        self.min_visibility = dict[' Min VisibilitykM']
+        self.max_wind_speed = dict[' Max Wind SpeedKm/h']
+        self.mean_wind_speed = dict[' Mean Wind SpeedKm/h']
+        self.max_gust_speed = dict[' Max Gust SpeedKm/h']
+        self.precipitation = dict['Precipitationmm']
+        self.cloud_cover = dict[' CloudCover']
+        self.events = dict[' Events']
+        self.wind_direction_degree = dict['WindDirDegrees']
+
+    def get_date(self):
+        return self.date
+
+    def get_max_temp(self):
+        return self.max_temp
+
+    def get_mean_temp(self):
+        return self.mean_temp
+
+    def get_min_temp(self):
+        return self.min_temp
+
+    def get_max_humidity(self):
+        return self.max_humidity
+
+    def get_mean_humidity(self):
+        return self.mean_humidity
+
+    def get_min_humidity(self):
+        return self.min_humidity
+
+    def displayDate(self):
+        print("Date: {}".format(self.date))
+
+    def displayTemp(self):
+        print("Temperature : {}".format(self.max_temp))
+
+
