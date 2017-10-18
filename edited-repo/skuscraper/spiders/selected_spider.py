4a1d862d5ce81209b0f717d26a77fd35a0f4ec43
mubtadaali <mubtada.ali@arbisoft.com>
2017-10-18 15:58:34 +0500

    made changes according to review

--- edited-repo/skuscraper/spiders/selected_spider.py
+++ edited-repo/skuscraper/spiders/selected_spider.py
@@ -4,7 +4,7 @@ from scrapy import Request
 from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
 
-from .base import BaseCrawlSpider, BaseParseSpider, CurrencyParser, clean
+from .base import BaseCrawlSpider, BaseParseSpider, clean
 
 
 class Mixin:
@@ -12,11 +12,10 @@ class Mixin:
     allowed_domains = ['selected.com.cn']
     lang = 'zh'
     market = 'CN'
-    brand_map = {'男士': 'Selected Homme', '女': 'Selected Femme'}
     page_size = 40
-    pagination_base_url = ("http://www.selected.com.cn/webapp/wcs/stores/servlet/CategoryNavigationResultsView?manufac"
-                           "turer=&searchType=&resultCatEntryType=&searchTerm=+&catalogId=10001&categoryId=-{gender_id}"
-                           "&storeId=10151&metaData==&pageSize=40&beginIndex={starting_index}&orderBy=5&categoryId=")
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    pagination_base_url = "=&pageSize=40&beginIndex={starting_index}&orderBy=5&categoryId="
+    images_base_url = "http://img1.selected.com.cn/slt/product/"
     start_urls_with_meta = [
         ('http://www.selected.com.cn/cn/sltstore/quanbunanzhuang.html', {'gender': 'men'}),
         ('http://www.selected.com.cn/cn/sltstore/quanbunvzhuang.html', {'gender': 'women'}),
@@ -25,97 +24,80 @@ class Mixin:
 
 class SelectedParseSpider(BaseParseSpider, Mixin):
     name = Mixin.retailer + '-parse'
+    price_css = '.pRightPice[id="div_price"] ::text'
 
     def parse(self, response):
-        sku_id = self.product_id(response)
-        garment = self.new_unique_garment(sku_id)
+        p_id = self.product_id(response)
+        garment = self.new_unique_garment(p_id)
         if not garment:
             return
 
         self.boilerplate_normal(garment, response)
-        garment['skus'] = self.product_skus(response)
+        garment['skus'] = self.skus(response)
         garment['image_urls'] = self.product_image_urls(response)
         return garment
 
     def product_image_urls(self, response):
-        img_urls = clean(response.css('.smallImg img::attr(src)'))
-        if not img_urls:
-            return
-        urls = []
+        img_names = clean(response.css('.smallImg img::attr(name)'))
+        if not img_names:
+            return []
+        image_urls = []
         color_codes = self.color_codes(response)
+        p_id = self.product_id(response)
         for code in color_codes:
-            if code in img_urls[0]:
-                active_color_code = code
-        for code in color_codes:
-            if code is active_color_code:
-                continue
-            copies_urls = img_urls.copy()
-            copies_urls = [u.replace(active_color_code, code) for u in copies_urls]
-            urls.extend(copies_urls)
-        return img_urls + urls
+            for name in img_names:
+                image_urls.append("{b_url}{p_id}/{c_code}/{c_code}{i_name}".format(b_url=self.images_base_url,
+                                                                                   p_id=p_id, c_code=code, i_name=name))
+        return image_urls
 
-    def product_skus(self, response):
+    def skus(self, response):
         skus = {}
-        curreny = self.curreny(response)
-        for color, c_code in zip(self.product_colors(response), self.color_codes(response)):
-            for size in self.product_sizes(response, c_code):
-                sku = {}
-                sku['color'] = color
-                sku['currency'] = curreny
-                sku['price'] = self.product_price(response, c_code)
+        common = self.product_pricing_common_new(response)
+        for colour_s in response.css('.pPropColor li'):
+            colour = clean(colour_s.css('img ::attr(title)'))[0]
+            colour_code = clean(colour_s.css('a ::attr(cpartnumber)'))[0]
+            common['colour'] = colour
+            for size_s in response.css(".popPageSel div[id*='{}'] a ".format(colour_code)):
+                sku = common.copy()
+                size = clean(size_s.css(' a::text'))[0]
                 sku['size'] = size
-                skus[color + size] = sku
+                if size_s.css('input[value="0.0"]'):
+                    sku['out_of_stock'] = True
+                skus['{}_{}'.format(colour, size)] = sku
         return skus
 
     def color_codes(self, response):
         return clean(response.css('.pPropColor a::attr("cpartnumber")'))
 
     def product_care(self, response):
-        description, care, delivery_note = clean(response.css('.menu_body::text'))
-        return care
-
-    def product_colors(self, response):
-        return clean(response.css('.pPropColor img::attr("title")'))
-
-    def product_price(self, response, c_code):
-        price = clean(response.css('.pRightPice div[id*="{}"] strong::text'.format(c_code)))[0]
-        return int(''.join(re.findall('\d+', price)))
-
-    def product_sizes(self, response, c_code):
-        size_quantity = clean(response.css('div[id*="{}"] input::attr(value)'.format(c_code)))
-        sizes = clean(response.css('div[id*="{}"] a::text'.format(c_code)))
-        return [size for size, quantity in zip(sizes, size_quantity) if quantity != "0.0"]
+        return clean(response.xpath('//div[@class="menu_list"]/div[@class="menu_body"][2]/text()'))
 
     def product_category(self, response):
         return clean(response.css('.productListGroup a::text'))[1:]
 
     def product_id(self, response):
-        id = clean(response.css('.c-gray6:not([id])::text'))[0]
-        return ''.join(re.findall('\d+', id))
+        p_id = clean(response.css('.c-gray6:not([id])::text'))[0]
+        return p_id.split(": ")[1]
 
     def product_name(self, response):
         return clean(response.css('.popProDelRight h3::text'))[0]
 
     def product_description(self, response):
-        description, care, delivery_note = clean(response.css('.menu_body::text'))
-        return description
+        return clean(response.xpath('//div[@class="menu_list"]/div[@class="menu_body"][1]/text()'))
 
     def product_brand(self, response):
-        product_name = self.product_name(response)
-        for brand_str, brand in self.brand_map.items():
-            if brand_str in product_name:
-                return brand
-        return 'Selected'
-
-    def curreny(self, response):
-        return CurrencyParser.currency(clean(response.css('.pRightPice div[id="div_price"] strong::text'))[0])
+        gender = response.meta['gender']
+        return 'Selected Homme' if gender is 'men' else 'Selected Femme'
 
 
 class SelectedCrawlSpider(BaseCrawlSpider, Mixin):
     name = Mixin.retailer + '-crawl'
     parse_spider = SelectedParseSpider()
-
     products_css = '.pageContainerList'
+
+    rules = (
+        Rule(LinkExtractor(restrict_css=products_css), callback='parse_item'),
+    )
 
     def parse(self, response):
         yield from super(SelectedCrawlSpider, self).parse(response)
@@ -124,11 +106,7 @@ class SelectedCrawlSpider(BaseCrawlSpider, Mixin):
         last_page = int(clean(response.css('.listFbfeet li:nth-last-child(2) a::text'))[0])
         if current_page == last_page:
             return
-        gender = response.meta['gender']
-        g_id = 40 if gender == 'men' else 41
-        url = self.pagination_base_url.format(starting_index=current_page * self.page_size, gender_id=g_id)
-        yield Request(url, meta=response.meta, callback=self.parse)
-
-    rules = (
-        Rule(LinkExtractor(restrict_css=products_css), callback='parse_item'),
-    )
+        base_url = response.xpath('//body//script[contains(text(),"pageSize = 40")]').re(self.url_regex)[0]
+        pagination_url = self.pagination_base_url.format(starting_index=current_page * self.page_size)
+        response.meta['trail'] = self.add_trail(response)
+        yield Request(base_url + pagination_url, meta=response.meta, callback=self.parse)
