405cae7900f98b7adaf0af5bbc895d1e5d719480
mubtadaali <mubtada.ali@arbisoft.com>
2017-10-23 16:16:54 +0500

    made changes according to review

--- edited-repo/skuscraper/spiders/americaneagle_spider.py
+++ edited-repo/skuscraper/spiders/americaneagle_spider.py
@@ -1,10 +1,10 @@
 import re
 import json
-import math
 
 from scrapy import Request
 from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
+from w3lib.url import url_query_parameter
 
 from .base import BaseCrawlSpider, BaseParseSpider, clean
 
@@ -14,7 +14,6 @@ class Mixin:
     allowed_domains = ['aeo.jp']
     lang = 'ja'
     market = 'JP'
-    page_size = 150
     gender_map = [
         ('women', 'women'),
         ('ウィメンズ', 'women'),
@@ -25,7 +24,7 @@ class Mixin:
         ('AEO', 'American Eagle Outfitters'),
         ('AE', 'aerie'),
     ]
-    url_regex = 'catMainLarge":"\/\/(.+?)"}'
+    image_url_re = 'catMainLarge":"\/\/(.+?)"}'
     pagination_url = "{b_url}&pp={page_no}&sort=00"
     start_urls = ['http://www.aeo.jp/top/CSfTop.jsp?cd=1']
 
@@ -43,24 +42,25 @@ class AmericanEagleParseSpider(BaseParseSpider, Mixin):
         self.boilerplate_normal(garment, response)
         garment['gender'] = self.product_gender(response)
         garment['skus'] = self.skus(response)
-        garment['image_urls'] = self.product_image_urls(response)
+        garment['image_urls'] = self.image_urls(response)
         garment['merch_info'] = self.merch_info(response)
         return garment
 
     def skus(self, response):
         skus = {}
         common = self.product_pricing_common_new(response)
-        raw_skus = response.xpath('//script[contains(text(),"catMainLarge")]').re('gpObj.matrix(.+)')[0]
-        for raw_sku in re.findall("({.+?})", raw_skus):
-            sku = common.copy()
-            sku_json = json.loads(raw_sku)
-            if not sku_json.get('goodsNo'):
-                continue
-            sku['colour'] = sku_json.get('colorNm')
-            sku['size'] = sku_json.get('dispSizeNm')
-            if sku_json.get('size1OutOfStock') == "0":
-                sku['out_of_stock'] = True
-            skus['{}_{}'.format(sku['colour'], sku['size'])] = sku
+        raw_skus = response.xpath('//script[contains(text(),"catMainLarge")]/text()').extract_first()
+        skus_json = json.loads(re.findall('gpObj.matrix = (.+);', raw_skus)[0])
+        for colour_sku in skus_json:
+            for size_sku in colour_sku:
+                sku = common.copy()
+                if not size_sku.get('goodsNo'):
+                    continue
+                sku['colour'] = size_sku.get('colorNm')
+                sku['size'] = size_sku.get('dispSizeNm')
+                if size_sku.get('size1OutOfStock') == "0":
+                    sku['out_of_stock'] = True
+                skus['{}_{}'.format(sku['colour'], sku['size'])] = sku
         return skus
 
     def product_care(self, response):
@@ -78,14 +78,14 @@ class AmericanEagleParseSpider(BaseParseSpider, Mixin):
     def product_description(self, response):
         return clean(response.css('.pdp-about-info-txt .pdp-about-copy-specifics::text'))
 
-    def product_image_urls(self, response):
-        return response.xpath('//script[contains(text(),"catMainLarge")]').re(self.url_regex)
+    def image_urls(self, response):
+        return response.xpath('//script[contains(text(),"catMainLarge")]').re(self.image_url_re)
 
     def merch_info(self, response):
         return clean(response.css('.psp-product-yousave ::text'))
 
     def product_gender(self, response):
-        category = "".join(self.product_category(response)).lower()
+        category = " ".join(self.product_category(response)).lower()
         for gender_string, gender in self.gender_map:
             if gender_string in category:
                 return gender
@@ -102,19 +102,21 @@ class AmericanEagleParseSpider(BaseParseSpider, Mixin):
 class AmericanEagleCrawlSpider(BaseCrawlSpider, Mixin):
     name = Mixin.retailer + '-crawl'
     parse_spider = AmericanEagleParseSpider()
+    page_size = 150
     products_css = '.ProductItem'
+    listing_css = ['.WOMEN', '.MEN']
 
     rules = (
-        Rule(LinkExtractor(restrict_css=['.WOMEN', '.MEN']), callback='parse'),
+        Rule(LinkExtractor(restrict_css=listing_css), callback='parse'),
         Rule(LinkExtractor(restrict_css=products_css), callback='parse_item'),
     )
 
     def parse(self, response):
         yield from super(AmericanEagleCrawlSpider, self).parse(response)
-        if "&pp=" in response.url:
+        if url_query_parameter(response.url, "pp"):
             return
-        total_pages = int(clean(response.css('.CategoryContentsWrap span[id="totalCount"]::text'))[0]) / self.page_size
-        total_pages = math.ceil(total_pages)
+        total_items = clean(response.css('.CategoryContentsWrap span[id="totalCount"]::text'))[0]
+        total_pages = int(total_items) // self.page_size + 1
         for p_no in range(2, total_pages + 1):
             url = self.pagination_url.format(b_url=response.url, page_no=p_no)
             response.meta['trail'] = self.add_trail(response)
