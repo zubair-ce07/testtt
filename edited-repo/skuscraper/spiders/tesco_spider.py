4e430316da68b1c461bd9b2923a520671bc5de64
mubtadaali <mubtada.ali@arbisoft.com>
2017-10-31 16:12:32 +0500

    made some changes for crawler and prices

--- edited-repo/skuscraper/spiders/tesco_spider.py
+++ edited-repo/skuscraper/spiders/tesco_spider.py
@@ -1,7 +1,7 @@
 import re
 import json
 
-from scrapy import Request
+from scrapy import Request, Selector
 from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
 
@@ -35,16 +35,16 @@ class TescoParseSpider(BaseParseSpider, Mixin):
         if not garment:
             return
 
-        raw_sku = response.xpath('//script[contains(text(), "window.Data.PDP")]').extract_first()
+        raw_skus = response.xpath('//script[contains(text(), "window.Data.PDP")]').extract_first()
         self.boilerplate(garment, response)
         garment['care'] = []
         garment['skus'] = {}
-        garment['name'] = self.product_name(response, raw_sku)
-        garment['brand'] = self.product_brand(raw_sku)
+        garment['name'] = self.product_name(response, raw_skus)
+        garment['brand'] = self.product_brand(raw_skus)
         garment['description'] = self.product_description(response)
         garment['category'] = self.product_category(response)
-        garment['image_urls'] = self.image_urls(response, raw_sku)
-        garment['meta'] = {'requests_queue': self.skus_requests(response, raw_sku)}
+        garment['image_urls'] = self.image_urls(response, raw_skus)
+        garment['meta'] = {'requests_queue': self.skus_requests(response, raw_skus)}
         return self.next_request_or_garment(garment)
 
     def parse_colour(self, response):
@@ -73,11 +73,9 @@ class TescoParseSpider(BaseParseSpider, Mixin):
 
     def sku_prices(self, res_json):
         price_json = res_json.get('prices')
-        price_string = price_json.get('price') + self.currency
-        if price_json.get('was'):
-            p_price_string = price_json['was'] + self.currency
-            return price_string, p_price_string
-        return price_string
+        price_string = price_json.get('price', price_json.get('fromPrice')) + self.currency
+        p_price_string = price_json.get('was', price_json.get('toPrice'))
+        return price_string, p_price_string
 
     def skus_stock(self, response):
         skus = response.meta['garment']['skus']
@@ -89,20 +87,20 @@ class TescoParseSpider(BaseParseSpider, Mixin):
             skus[sku_json['id']] = sku
         return skus
 
-    def colours_map(self, response, raw_sku):
+    def colours_map(self, response, raw_skus):
         c_map = {}
-        for c_sku_json in json.loads(re.findall('links" : (?s)(.+)},\nsku ', raw_sku)[0]):
+        for c_sku_json in json.loads(re.findall('links" : (?s)(.+)},\nsku ', raw_skus)[0]):
             if c_sku_json.get("rel") not in ["colourAssociation", "self"]:
                 continue
             if c_sku_json.get('options'):
                 c_map[c_sku_json["id"]] = c_sku_json['options']['primary']
             else:
-                c_map[c_sku_json["id"]] = self.detect_colour(self.product_name(response, raw_sku))
+                c_map[c_sku_json["id"]] = self.detect_colour(self.product_name(response, raw_skus))
         return c_map
 
-    def skus_requests(self, response, raw_sku):
+    def skus_requests(self, response, raw_skus):
         requests = []
-        for c_code, c_name in self.colours_map(response, raw_sku).items():
+        for c_code, c_name in self.colours_map(response, raw_skus).items():
             response.meta['colour'] = c_name
             requests.append(Request(self.stock_url_t.format(c_code=c_code), meta=response.meta,
                                     callback=self.parse_stock))
@@ -118,22 +116,22 @@ class TescoParseSpider(BaseParseSpider, Mixin):
             return clean(response.css('.product-description-block .style-ref::text'))[0].split('Ref: ')[1][:-2]
         return clean(response.css('.catalogue-id::text'))[0]
 
-    def product_name(self, response, raw_sku):
-        brand = self.product_brand(raw_sku)
+    def product_name(self, response, raw_skus):
+        brand = self.product_brand(raw_skus)
         return clean(response.css('.product-title-text::text'))[0].replace(brand, "")
 
     def product_description(self, response):
         return clean(response.css('div[itemprop="description"] ::text'))
 
-    def image_urls(self, response, raw_sku):
+    def image_urls(self, response, raw_skus):
         urls = []
-        img_urls = set(re.findall('src" : "(.+)\?', raw_sku))
-        for c_code in self.colours_map(response, raw_sku).keys():
+        img_urls = set(re.findall('src" : "(.+)\?', raw_skus))
+        for c_code in self.colours_map(response, raw_skus).keys():
             urls += [re.sub('(\d+-\d+)', c_code, url) for url in img_urls]
         return urls
 
-    def product_brand(self, raw_sku):
-        return re.findall('brand" : "(.+)",', raw_sku)[0]
+    def product_brand(self, raw_skus):
+        return re.findall('brand" : "(.+)",', raw_skus)[0]
 
 
 class TescoCrawlSpider(BaseCrawlSpider, Mixin):
@@ -154,14 +152,15 @@ class TescoCrawlSpider(BaseCrawlSpider, Mixin):
         if not total_items:
             return
         total_pages = int(total_items[0]) // self.page_size + 1
-        cat_id = response.css('.products-wrapper').re('data-endecaid="(.+?)"')[0]
+        cat_id = clean(response.css('.products-wrapper::attr(data-endecaid)'))[0]
         for pg_no in range(2, total_pages + 1):
             url = self.pagination_url_t.format(cat_id=cat_id, offset=(pg_no - 1) * self.page_size)
             response.meta['trail'] = self.add_trail(response)
             yield Request(url, meta=response.meta, callback=self.parse_pagination)
 
     def parse_pagination(self, response):
-        urls = re.findall('nail\\\\" href=\\\\"(.+?)\\\\"', response.text)
-        for url in urls:
+        products_urls_css = '.image-container .thumbnail::attr(href)'
+        products_html = Selector(text=json.loads(response.text)['products'])
+        for url in clean(products_html.css(products_urls_css)):
             response.meta['trail'] = self.add_trail(response)
             yield Request(response.urljoin(url), meta=response.meta, callback=self.parse_item)
