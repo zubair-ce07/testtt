ec88f7fe6aa189cec5ca45e3aa8d3816d9a09b78
rabbiyaalamgir <rabia.alam@arbisoft.com>
2017-11-01 16:36:24 +0500

    Basic folder complete.

---
+++ basic/list1.py
@@ -0,0 +1,60 @@
+def match_ends(words):
+  count = 0
+  for str in words:
+    if len(str)>1:
+      if str[0] == str[-1]:
+        count += 1
+
+  return count
+
+def front_x(words):
+  a = []; b = []
+  for str in words:
+    if str[0] == "x":
+      a.append(str)
+    else:
+      b.append(str)
+
+  return sorted(a)+sorted(b)
+
+def lastElement(tuple):
+  return tuple[-1]
+
+def sort_last(tuples):
+  return sorted(tuples, key = lastElement)
+
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+
+def main():
+  print 'match_ends'
+  test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
+  test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
+  test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
+
+  print
+  print 'front_x'
+  test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
+       ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+  test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
+       ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+  test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
+       ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+
+       
+  print
+  print 'sort_last'
+  test(sort_last([(1, 3), (3, 2), (2, 1)]),
+       [(2, 1), (3, 2), (1, 3)])
+  test(sort_last([(2, 3), (1, 2), (3, 1)]),
+       [(3, 1), (1, 2), (2, 3)])
+  test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+       [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+
+
+if __name__ == '__main__':
+  main()
---
+++ basic/string1.py
@@ -0,0 +1,56 @@
+def donuts(count):
+  if count>9:
+    return "Number of donuts: many"
+  else:
+    return "Number of donuts: "+str(count)
+
+def both_ends(s):
+  if len(s)<2:
+    return ""
+  else:
+    return s[0:2]+s[-2:]
+
+def fix_start(s):
+  return s[0]+s[1:].replace(s[0],"*")
+
+def mix_up(a, b):
+  return b[0:2]+a[2:]+" "+a[0:2]+b[2:]
+
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+
+def main():
+  print 'donuts'
+  test(donuts(4), 'Number of donuts: 4')
+  test(donuts(9), 'Number of donuts: 9')
+  test(donuts(10), 'Number of donuts: many')
+  test(donuts(99), 'Number of donuts: many')
+
+  print
+  print 'both_ends'
+  test(both_ends('spring'), 'spng')
+  test(both_ends('Hello'), 'Helo')
+  test(both_ends('a'), '')
+  test(both_ends('xyz'), 'xyyz')
+
+  
+  print
+  print 'fix_start'
+  test(fix_start('babble'), 'ba**le')
+  test(fix_start('aardvark'), 'a*rdv*rk')
+  test(fix_start('google'), 'goo*le')
+  test(fix_start('donut'), 'donut')
+
+  print
+  print 'mix_up'
+  test(mix_up('mix', 'pod'), 'pox mid')
+  test(mix_up('dog', 'dinner'), 'dig donner')
+  test(mix_up('gnash', 'sport'), 'spash gnort')
+  test(mix_up('pezzy', 'firm'), 'fizzy perm')
+
+if __name__ == '__main__':
+  main()
