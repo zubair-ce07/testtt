a58aaa06d5eb99f85299affe2b31c71012a30f92
rabbiyaalamgir <rabia.alam@arbisoft.com>
2017-11-01 16:54:11 +0500

    All exercises completed.

--- copyspecial/copyspecial.py
+++ copyspecial/copyspecial.py
@@ -1,22 +1,9 @@
-#!/usr/bin/python
-# Copyright 2010 Google Inc.
-# Licensed under the Apache License, Version 2.0
-# http://www.apache.org/licenses/LICENSE-2.0
-
-# Google's Python Class
-# http://code.google.com/edu/languages/google-python-class/
-
 import sys
 import re
 import os
 import shutil
 import commands
 
-"""Copy Special exercise
-"""
-
-# +++your code here+++
-# Write functions and modify main() to call them
 def get_special_paths(dir):
   filenames = os.listdir(dir)
   matches = re.findall(r'\w+_+\w+_+\.\w+', ' '.join(filenames))
@@ -33,32 +20,19 @@ def zip_to(paths, zippath):
   allpaths = ' '.join(paths)
   cmd = 'zip -j '+zippath+' '+allpaths
   (status, output) = commands.getstatusoutput(cmd)
-  # print cmd
-
-
   return
 
 def main():
-  # This basic command line argument parsing code is provided.
-  # Add code to call your functions below.
-
-  # Make a list of command line arguments, omitting the [0] element
-  # which is the script itself.
   args = sys.argv[1:]
   if not args:
     print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
     sys.exit(1)
 
-  # todir and tozip are either set from command line
-  # or left as the empty string.
-  # The args array is left just containing the dirs.
-  todir = ''
   if args[0] == '--todir':
     todir = args[1]
     dir = args[2]
     paths = get_special_paths(dir)
     copy_to(paths, todir)
-    # print paths
     del args[0:2]
 
   tozip = ''
@@ -68,13 +42,8 @@ def main():
     paths = get_special_paths(dir)
     zip_to(paths,tozip)
     del args[0:2]
-  #
-  # if len(args) == 0:
-  #   print "error: must specify one or more dirs"
+
     sys.exit(1)
 
-  # +++your code here+++
-  # Call your functions
-  
 if __name__ == "__main__":
   main()
--- logpuzzle/logpuzzle.py
+++ logpuzzle/logpuzzle.py
@@ -1,23 +1,7 @@
-#!/usr/bin/python
-# Copyright 2010 Google Inc.
-# Licensed under the Apache License, Version 2.0
-# http://www.apache.org/licenses/LICENSE-2.0
-
-# Google's Python Class
-# http://code.google.com/edu/languages/google-python-class/
-
 import os
 import re
 import sys
 import urllib
-
-"""Logpuzzle exercise
-Given an apache logfile, find the puzzle urls and download the images.
-
-Here's what a puzzle url looks like:
-10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
-"""
-
 
 def read_urls(filename):
   server = re.search(r'_\w+\.\w+\.\w+', filename).group()[1:]
@@ -26,26 +10,11 @@ def read_urls(filename):
   matches = re.findall(r'GET\s(\S+puzzle\S+)\sHTTP', text)
   fullmatches = ["http://"+server+match for match in matches]
   return fullmatches
-  """Returns a list of the puzzle urls from the given log file,
-  extracting the hostname from the filename itself.
-  Screens out duplicate urls and returns the urls sorted into
-  increasing order."""
-  # +++your code here+++
-  
 
 def download_images(img_urls, dest_dir):
-  """Given the urls already in the correct order, downloads
-  each image into the given directory.
-  Gives the images local filenames img0, img1, and so on.
-  Creates an index.html in the directory
-  with an img tag to show each local image file.
-  Creates the directory if necessary.
-  """
   for img in img_urls:
     urllib.urlretrieve(img, dest_dir)
   return
-  # +++your code here+++
-  
 
 def main():
   args = sys.argv[1:]
@@ -59,7 +28,6 @@ def main():
     todir = args[1]
     del args[0:2]
 
-  # print args[0]
   img_urls = read_urls(args[0])
 
   if todir:
