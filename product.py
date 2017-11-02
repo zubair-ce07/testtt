c4d6f6003d8a372b6a5107a73af4304161d9a0ea
Faraz Mirza <faraz.mirza@arbisoft.com>
2017-11-02 19:14:03 +0500

    Farnell Parser

---
+++ product.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class Product(scrapy.Item):
+    url = scrapy.Field()
+    title = scrapy.Field()
+    unit_price = scrapy.Field()
+    overview = scrapy.Field()
+    manufacturer = scrapy.Field()
+    manufacturer_part = scrapy.Field()
+    tariff_number = scrapy.Field()
+    origin_country = scrapy.Field()
+    files = scrapy.Field()
+    file_urls = scrapy.Field()
+    image_urls = scrapy.Field()
+    primary_image_url = scrapy.Field()
+    trail = scrapy.Field()
+    information = scrapy.Field()
+
+
