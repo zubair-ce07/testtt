184bf185fe7742d292e231387ac723c224a692e1
umer-nadeem <umer.nadeem@arbisoft.com>
2017-11-15 12:08:16 +0500

    Changed items.py used style_ , Removed the slicing n looping in get_json method , Concatination of strings is done by using string.format method

--- Scraper/FirstScrapyTask/spiders/skechers_spider.py
+++ Scraper/FirstScrapyTask/spiders/skechers_spider.py
@@ -30,12 +30,12 @@ class SkechersSpider(scrapy.Spider):
         bookmark = self.get_bookmarks(response)
 
         for row in product_urls:
-            url = heading + row
+            url = '{}{}'.format(heading , row)
             request = scrapy.Request(url=url, callback=self.parse_three)
             yield request
 
         if bookmark:
-            url = api + bookmark
+            url = '{}{}'.format(api , bookmark)
             request = scrapy.Request(url=url, callback=self.parse_two , meta={'api' : api})
             yield request
 
@@ -47,12 +47,12 @@ class SkechersSpider(scrapy.Spider):
         product_urls = re.findall('\/en-us.*\s' ,response_result['stylesHtml'])
 
         for row in product_urls:
-            url = heading + row.split('"')[0]
+            url = '{}{}'.format(heading , row.split('"')[0])
             request = scrapy.Request(url=url, callback=self.parse_three)
             yield request
 
         if response_result['bookmark']:
-            url = api + response_result['bookmark']
+            url = '{}{}'.format(api , response_result['bookmark'])
             request = scrapy.Request(url=url, callback=self.parse_two , meta={'api' : api})
             yield request
 
@@ -60,29 +60,29 @@ class SkechersSpider(scrapy.Spider):
 
         product = SkechersItem()
 
-        product['Name'] = response.css('h1.product-title::text').extract_first()
-        product['URL'] = response.url
-        product['Price_Final'] = response.css('ins.price-final::text').extract_first()
-        product['Currency'] = product['Price_Final'][0]
-        product['Color'] = response.css('div.product-label.js-color::text').extract_first()
-        product['Brand'] = 'Skechers'
-        product['Desc'] = response.css('div.toggle-expand.toggle-description div::text').extract_first()
-        product['More_Details'] = response.css('div.toggle-expand.toggle-description li::text').extract()
+        product['Style_Name'] = response.css('h1.product-title::text').extract_first()
+        product['Style_URL'] = response.url
+        product['Style_Price_Final'] = response.css('ins.price-final::text').extract_first()
+        product['Style_Currency'] = product['Style_Price_Final'][0]
+        product['Style_Color'] = response.css('div.product-label.js-color::text').extract_first()
+        product['Style_Brand'] = 'Skechers'
+        product['Style_Desc'] = response.css('div.toggle-expand.toggle-description div::text').extract_first()
+        product['Style_More_Details'] = response.css('div.toggle-expand.toggle-description li::text').extract()
         product_style_one = response.css('span.pull-right.product-label::text').extract_first()
         product_style_two = response.css('span.js-color-code::text').extract_first()
-        product['Product_Code'] = product_style_one + product_style_two
+        product['Style_Product_Code'] = '{}{}'.format(product_style_one , product_style_two)
 
-        product['Category'] = self.get_category(response)
-        product['Image_Urls'] = self.get_image_urls(response)
-        product['Sizes_Info'] = self.get_size(response)
+        product['Style_Category'] = self.get_category(response)
+        product['Style_Image_Urls'] = self.get_image_urls(response)
+        product['Style_Sizes_Info'] = self.get_size(response)
 
         yield product
 
     def get_bookmarks(self , response):
 
-        script_tag = response.css('div.js-listing.listing.droplet.styles-droplet.clearfix script').extract()
+        script_tag = response.css('div.js-listing.listing.droplet.styles-droplet.clearfix script').extract_first()
 
-        bookmark_tag = re.findall('bookmark.*\s', str(script_tag[0]))
+        bookmark_tag = re.findall('bookmark.*\s', str(script_tag))
 
         bookmark_tag = bookmark_tag[0]
 
@@ -97,7 +97,7 @@ class SkechersSpider(scrapy.Spider):
     def get_image_urls(self, response):
 
         image_urls = response.css('img.responsiveImg::attr(src)').extract()
-        image_path = response.css('img.responsiveImg::attr(src)').extract_first()[:-4]
+        image_path = image_urls[0].replace('.jpg' , '')
         product_info = self.get_json(response)
 
         img_count = int(product_info['products'][0]['numimages'])
@@ -106,8 +106,7 @@ class SkechersSpider(scrapy.Spider):
 
         char_value = 66
         while img_count != 1:
-            filename = '_' + chr(char_value) + '.jpg'
-            image_urls.append(image_path + filename)
+            image_urls.append('{}{}{}{}'.format(image_path , '_' , chr(char_value) , '.jpg'))
             img_count = img_count - 1
             char_value = char_value + 1
 
@@ -135,7 +134,7 @@ class SkechersSpider(scrapy.Spider):
         sizes = []
 
         for row in product_info['products'][0]['sizes']:
-            sizes.append('Size: ' + row['size'] + ' In_Stock: ' + str(row['instock']) )
+            sizes.append('{}{}{}{}'.format('Size: ' , row['size'] , ' In_Stock: ' , str(row['instock'])))
 
         if sizes:
             return sizes
@@ -144,11 +143,8 @@ class SkechersSpider(scrapy.Spider):
 
     def get_json(self , response):
 
-        product_info = re.findall('\Skx.style.*\;', response.text)
-        product_info = product_info[0][12:]
-        size = len(product_info)
-        product_info = json.loads(product_info[0:size - 1])
-        return product_info
+        product_info = re.findall('{"stylecode.*\;', response.text)
+        return json.loads(product_info[0].replace(';',''))
 
     def get_api(self , response):
 
