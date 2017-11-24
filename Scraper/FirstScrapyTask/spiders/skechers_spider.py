5cbb92b7db820f3a31d77437980c22d225be02c4
umer-nadeem <umer.nadeem@arbisoft.com>
2017-11-24 15:15:57 +0500

    Removed one liner functions , Removed get_size function , made seperate function for skus and prices

--- Scraper/FirstScrapyTask/spiders/skechers_spider.py
+++ Scraper/FirstScrapyTask/spiders/skechers_spider.py
@@ -21,130 +21,110 @@ class SkechersSpider(CrawlSpider):
                       'G': 'Girls',
                       'U': 'Men/Women/Accessories'}
     base_api = 'https://www.skechers.com/en-us/api/html/products'
-
     api_accessories_t = '{}{}?category={}&bookmark='
     api_apparel_t = '{}{}?bookmark='
     api_gender_t = '{}{}?genders={}&bookmark='
-
-
     name = 'skechers'
     start_urls = ['https://www.skechers.com/en-us/']
     rules = (
-
-        Rule(LinkExtractor(allow=('all$', 'apparel$','accessories')) , callback='parse_start_page_urls'),
+        Rule(LinkExtractor(allow=('all$', 'apparel$', 'accessories')), callback='parse_start_page_urls'),
     )
-
     custom_settings = {
         'DOWNLOAD_DELAY': 1
     }
 
     def parse_start_page_urls(self, response):
+        category = response.css('.js-page-title::text').extract()
+        sub_category = response.css('.selected::text').extract_first()
+        if sub_category:
+            category.append(sub_category)
         product_urls = response.css('div::attr(data-clicked-style-url)').extract()
         api = self.get_api(response)
-        bookmark = self.get_bookmarks(response)
+        bookmark = response.css('script').re_first('bookmark =  \'(.+)\'')
+        meta={'category' : category}
         for url in product_urls:
             url = urllib.parse.urljoin(self.base_url , url)
-            request = Request(url=url, callback=self.parse_style_info)
+            request = Request(url=url, callback=self.parse_style_info , meta=meta)
             yield request
 
         if bookmark:
-            yield self.request_for_next_page(api=api , bookmark= bookmark)
+            yield self.pagination_request(api=api, bookmark=bookmark, category=category)
 
     def parse_style_urls(self, response):
+        category = response.meta['category']
         api = response.meta['api']
         response_result = json.loads(response.text)
         product_urls = re.findall('\/en-us.*\s',response_result['stylesHtml'])
-
+        meta = {'category': category}
         for url in product_urls:
             url = urllib.parse.urljoin(self.base_url , re.match('(.*?)\"', url).group(1))
-            request = Request(url=url, callback=self.parse_style_info)
+            request = Request(url=url, callback=self.parse_style_info , meta=meta)
             yield request
 
         if response_result['bookmark']:
-            yield self.request_for_next_page(api=api, bookmark=response_result['bookmark'])
+            yield self.pagination_request(api=api, bookmark=response_result['bookmark'], category=category)
 
     def parse_style_info(self, response):
-
         product = SkechersItem()
         product_style_id = response.css('.pull-right.product-label::text').extract_first()
         product_style_color = response.css('.js-color-code::text').extract_first()
         product_id_number = int(re.search('\d.*(?= )', product_style_id).group(0))
-
         product['name'] = response.css('.product-title::text').extract_first()
         product['url'] = response.url
         product['brand'] = 'Skechers'
         product['description'] = response.css('.toggle-expand.toggle-description div::text').extract_first()
         product['product_id'] = '{}{}'.format(product_style_id, product_style_color)
-        product_info = self.get_json(response)
+        product_info = json.loads(response.css('script').re_first('Skx.style = ({.+})'))
         image_path = response.css('.responsiveImg::attr(src)').extract_first().replace('.jpg' , '')
-
-        product['gender'] = self.get_gender(product_info['gender'])
+        product['gender'] = self.gender_mapping[product_info['gender']]
         product['image_urls'] = self.get_image_urls(image_path , product_info['products'][0]['numimages'])
-        product['skus'] = {}
-        product['category'] = product['name'].split('-')
-        product['category'].append(product['gender'])
-
+        product['category'] = response.meta['category']
         price_final = response.css('.price-final::text').extract_first()
         currency = response.css('script').re_first('skx_currency_code: \'(.+)\'')
         color = response.css('.product-label.js-color::text').extract_first()
+        skus_info = {'product': product, 'price': price_final, 'currency': currency, 'product_code': product_id_number,
+                     'color': color , 'product_sizes_info':product_info['products'][0]['sizes']}
+        yield self.create_skus(skus_info)
 
-        sizes_info = self.get_size(product_info['products'][0]['sizes'])
-        for size in sizes_info:
-            product['skus'][str(product_id_number)] = {'currency': currency,
-                                                       'size' : size,
-                                                       'price' : price_final ,
-                                                       'color':color}
+    def create_skus(self, skus_info):
+        sizes = set ()
+        currency = skus_info['currency']
+        price = skus_info['price']
+        color = skus_info['color']
+        product_sizes_info = skus_info['product_sizes_info']
+        product_id_number = skus_info['product_code']
+        product = skus_info['product']
+        product['skus'] = {}
+
+        for row in product_sizes_info:
+            sizes.add(row['size'])
+        for size in sizes:
+            product['skus'][str(product_id_number)] = {'currency': currency,'size' : size,
+                                                       'price' : price ,'color':color}
             product_id_number = product_id_number + 1
-
-        yield product
-
-    def get_bookmarks(self , response):
-
-        return response.css('script').re_first('bookmark =  \'(.+)\'')
+        return product
 
     def get_image_urls(self, image_path , img_count):
-
         image_urls = []
         img_count = min(int(img_count), self.max_image_limit)
         for character in string.ascii_uppercase[:img_count]:
             image_urls.append(self.image_url_t.format(image_path, character))
         return image_urls
 
-    def get_gender(self , gender):
-
-        return self.gender_mapping[gender]
-
-    def get_size(self, product_sizes):
-
-        sizes = []
-        for size in product_sizes:
-            sizes.append(size['size'])
-
-        if sizes:
-            return sizes
-        else:
-            return 'N/A'
-
-    def get_json(self , response):
-
-        return json.loads(response.css('script').re_first('Skx.style = ({.+})'))
-
     def get_api(self , response):
-
         base_api_attributes = json.loads(response.css('script').re_first('Skx.refinement.values = ({.+})'))
         if base_api_attributes['genders']:
             gender = ','.join(base_api_attributes['genders'])
             return self.api_gender_t.format(self.base_api , base_api_attributes['page'] , gender)
-
         else:
             if not base_api_attributes['category']:
                 return self.api_apparel_t.format(self.base_api , base_api_attributes['page'])
             else:
                 return self.api_accessories_t.format(self.base_api, base_api_attributes['page'] , base_api_attributes['category'])
 
-    def request_for_next_page(self , api , bookmark):
-
+    def pagination_request(self , api , bookmark, category):
         parse_result = urllib.parse.urlparse(api)
         url = urllib.parse.urlunparse((parse_result.scheme, parse_result.netloc, parse_result.path, '', parse_result.query + bookmark, ''))
-        pagination_request = Request(url=url, callback=self.parse_style_urls, meta={'api': api})
+        pagination_request = Request(url=url, callback=self.parse_style_urls, meta={'api': api , 'category':category})
         return pagination_request
+
