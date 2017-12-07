6a0b54c66ea8ecc4bf872539d5de05f008a5d2e3
Ahsan Shafiq <ahsanshafiq742@gmail.com>
2017-12-07 14:36:33 +0500

    Feedback incorporated.

--- weatherman.py
+++ weatherman.py
@@ -6,248 +6,219 @@ import calendar
 from datetime import datetime
 from termcolor import colored
 
-class WeatherMan():
-    def print_Yearly_results(self, filePath, year):
 
-        fileNames = []
-        os.chdir(filePath)
+class WeatherMan():
+
+    def print_Yearly_results(self, file_path, year):
+        file_names=[]
+        os.chdir(file_path)
         for file in glob.glob("*.txt"):
             if file.find(year) != -1:
-                fileNames.append(file)
+                file_names.append(file)
 
-        if len(fileNames) == 0:
+        if len(file_names) == 0:
             print('No File Found!')
         else:
-            for fileName in fileNames:
+            high_temp = {}
+            low_temp = {}
+            high_humidity = {}
 
-                f = open(fileName, 'r')
-                next(f)
-                highTemp = {}
-                lowTemp = {}
-                highHumidity = {}
-
-                for line in f:
-                    splittedWords = line.split(',')
-                    highTemp[splittedWords[0]] = splittedWords[1]
-                    lowTemp[splittedWords[0]] = splittedWords[3]
-                    highHumidity[splittedWords[0]] = splittedWords[7]
+            for file_name in file_names:
+                with open(file_name, 'r') as f:
+                    next(f)
+                    for line in f:
+                        splitted_words = line.split(',')
+                        high_temp[splitted_words[0]] = splitted_words[1]
+                        low_temp[splitted_words[0]] = splitted_words[3]
+                        high_humidity[splitted_words[0]] = splitted_words[7]
 
             def key_search(x):
                 return x[1]
 
-            highTempRes = sorted(
-                highTemp.items(),
+            high_temp_res = sorted(
+                high_temp.items(),
                 key=key_search,
                 reverse=True)
-            lowTempRes = sorted(lowTemp.items(), key=key_search, reverse=True)
-            highHumidityRes = sorted(
-                highHumidity.items(),
+            low_temp_res = sorted(
+                low_temp.items(),
                 key=key_search,
                 reverse=True)
-            del highTempRes[1:]
-            del lowTempRes[1:]
-            del highHumidityRes[1:]
-            highDate = datetime.strptime(highTempRes[0][0], '%Y-%m-%d')
-            lowDate = datetime.strptime(lowTempRes[0][0], '%Y-%m-%d')
-            humDate = datetime.strptime(highHumidityRes[0][0], '%Y-%m-%d')
+            high_humidity_res = sorted(
+                high_humidity.items(),
+                key=key_search,
+                reverse=True)
+
+            del high_temp_res[1:]
+            del low_temp_res[1:]
+            del high_humidity_res[1:]
+            high_date = datetime.strptime(high_temp_res[0][0], '%Y-%m-%d')
+            low_date = datetime.strptime(low_temp_res[0][0], '%Y-%m-%d')
+            hum_date = datetime.strptime(high_humidity_res[0][0], '%Y-%m-%d')
 
             print ('Highest: ' + \
-                  highTempRes[0][1] + \
+                  high_temp_res[0][1] + \
                   'C on ' + \
-                  highDate.strftime("%B") \
-                  + ' ' + str(highDate.strftime("%d")))
-            print ('Lowest: ' + lowTempRes[0][1] +\
-                  'C on ' + lowDate.strftime("%B") +\
-                  ' ' + str(lowDate.strftime("%d")))
+                  high_date.strftime("%B") \
+                  + ' ' + str(high_date.strftime("%d")))
+            print ('Lowest: ' + low_temp_res[0][1] +\
+                  'C on ' + low_date.strftime("%B") +\
+                  ' ' + str(low_date.strftime("%d")))
             print ('Humidity: ' +\
-                  highHumidityRes[0][1] +\
-                  '% on ' + humDate.strftime("%B") +\
-                  ' ' + str(humDate.strftime("%d")))
+                  high_humidity_res[0][1] +\
+                  '% on ' + hum_date.strftime("%B") +\
+                  ' ' + str(hum_date.strftime("%d")))
 
-        return
-
-    def verify_directory(self, directoryPath):
-        if os.path.isdir(directoryPath):
-            return True
-        else:
-            return False
-        return
+    def verify_directory(self, directory_path):
+        return os.path.isdir(directory_path)
 
     def verify_year(self, year):
-        if year.isdigit() and len(year) == 4:
-            return True
-        else:
-            return False
+        return year.isdigit() and len(year) == 4
 
-        return
-
-    def verify_year_month(self, yearMonth):
-        match = re.search(r'^\d\d\d\d/\d+$', yearMonth)
+    def verify_year_month(self, year_month):
+        match = re.search(r'^\d\d\d\d/\d+$', year_month)
         if match:
-            splittedWords = yearMonth.split('/')
-            month = int(splittedWords[1])
-            if month >= 1 and month <= 12:
-                return True
-            else:
-                return False
+            splitted_words = year_month.split('/')
+            month = int(splitted_words[1])
+            return month >= 1 and month <= 12
         else:
             return False
 
-        return
-
-    def print_month_average(self, filePath, year, month):
-
-        fileNames = []
-        os.chdir(filePath)
+    def print_month_average(self, file_path, year, month):
+        file_names = []
+        os.chdir(file_path)
         for file in glob.glob("*.txt"):
             if file.find(year) != -1 and file.find(month) != -1:
-                fileNames.append(file)
+                file_names.append(file)
 
-        if len(fileNames) == 0:
+        if len(file_names) == 0:
             print('No Data Found!')
         else:
-            for fileName in fileNames:
+            high_temp = {}
+            low_temp = {}
+            high_humidity = {}
 
-                f = open(fileName, 'r')
-                next(f)
-                highTemp = {}
-                lowTemp = {}
-                highHumidity = {}
+            for file_name in file_names:
+                with open(file_name, 'r') as f:
+                    next(f)
+                    for line in f:
+                        splitted_words = line.split(',')
+                        high_temp[splitted_words[0]] = splitted_words[1]
+                        low_temp[splitted_words[0]] = splitted_words[3]
+                        high_humidity[splitted_words[0]] = splitted_words[9]
 
-                for line in f:
-                    splittedWords = line.split(',')
-                    highTemp[splittedWords[0]] = splittedWords[1]
-                    lowTemp[splittedWords[0]] = splittedWords[3]
-                    highHumidity[splittedWords[0]] = splittedWords[9]
+            high_temp_avg = self.find_average(high_temp.values())
+            low_temp_avg = self.find_average(low_temp.values())
+            high_hum_avg = self.find_average(high_humidity.values())
 
-                    highTempAvg = self.find_average(highTemp.values())
-                    lowTempAvg = self.find_average(lowTemp.values())
-                    highHumAvg = self.find_average(highHumidity.values())
-
-            print('Highest Average: ' + str(round(highTempAvg, 2)) + 'C')
-            print('Lowest average: ' + str(round(lowTempAvg, 2)) + 'C')
-            print('average Mean Humidity: ' + str(round(highHumAvg, 2)) + '%')
-        return
+            print('Highest Average: ' + str(round(high_temp_avg, 2)) + 'C')
+            print('Lowest average: ' + str(round(low_temp_avg, 2)) + 'C')
+            print('average Mean Humidity: ' + str(round(high_hum_avg, 2)) + '%')
 
     def find_average(self, values):
-        sumVal = 0.0
-        for val in values:
-            if len(val) != 0:
-                sumVal += float(val)
-        return sumVal / len(values)
+        if len(values) != 0:
+            sum_val = 0.0
+            for val in values:
+                if len(val) != 0:
+                    sum_val += float(val)
+            return sum_val / len(values)
+        else:
+            return 0
 
-    def print_day_bars(self, filePath, year, month):
-
-        fileNames = []
-        os.chdir(filePath)
+    def print_day_bars(self, file_path, year, month):
+        file_names = []
+        os.chdir(file_path)
         for file in glob.glob("*.txt"):
             if file.find(year) != -1 and file.find(month) != -1:
-                fileNames.append(file)
+                file_names.append(file)
 
-        if len(fileNames) == 0:
+        if len(file_names) == 0:
             print('No Data Found!')
         else:
-            for fileName in fileNames:
+            for file_name in file_names:
+                with open(file_name, 'r') as f:
+                    next(f)
+                    for line in f:
+                        splitted_words = line.split(',')
+                        day = datetime.strptime(splitted_words[0], '%Y-%m-%d')
+                        high = self.get_Integer(splitted_words[1])
+                        low = self.get_Integer(splitted_words[3])
 
-                f = open(fileName, 'r')
-                next(f)
-
-                for line in f:
-                    splittedWords = line.split(',')
-                    day = datetime.strptime(splittedWords[0], '%Y-%m-%d')
-
-                    high = self.get_Integer(splittedWords[1])
-                    low = self.get_Integer(splittedWords[3])
-
-                    """print(day.strftime("%d"),
-                    self.print_plus(high,'red'))
-                    print(str(high) + 'C')
-                    print(day.strftime("%d"),
-                    self.print_plus(low,'blue'))
-                    print(str(low) + 'C')
-                    """
-                    #print(day.strftime("%d"), end=' ')
-                    print(day.strftime("%d"),end="")
-                    self.print_plus(low, 'blue')
-                    self.print_plus(high - low, 'red')
-                    print(str(low) + 'C - ' + str(high) + 'C')
-        return
+                        """print(day.strftime("%d"),
+                        self.print_plus(high,'red'))
+                        print(str(high) + 'C')
+                        print(day.strftime("%d"),
+                        self.print_plus(low,'blue'))
+                        print(str(low) + 'C')
+                        """
+                        print(day.strftime("%d"),end="")
+                        self.print_plus(low, 'blue')
+                        self.print_plus(high - low, 'red')
+                        print(str(low) + 'C - ' + str(high) + 'C')
 
     def get_Integer(self, val):
         if len(val) == 0:
             result = 0
         else:
             result = int(val)
-
         return result
 
     def print_plus(self, val, color):
-        i = 0
+        i=0
         while i < val:
             print(colored('+', color),end="")
             i += 1
-        return
 
 
-def sub_main(directoryPath, year, reportType):
+def sub_main(directory_path, year, report_type):
     weather = WeatherMan()
-    if reportType == '-e':
-        if weather.verify_directory(directoryPath) and weather.verify_year(year):
-            weather.print_Yearly_results(directoryPath, year)
+    if report_type == '-e':
+        if weather.verify_directory(directory_path) and weather.verify_year(year):
+            weather.print_Yearly_results(directory_path, year)
         else:
             print('Incorrect Arguments!')
 
-    elif reportType == '-a':
-
-        if weather.verify_directory(
-                directoryPath) and weather.verify_year_month(year):
-            splittedWords = year.split('/')
-            year = splittedWords[0]
-            temp = calendar.month_name[int(splittedWords[1])]
+    elif report_type == '-a':
+        if weather.verify_directory(directory_path) and weather.verify_year_month(year):
+            splitted_words = year.split('/')
+            year = splitted_words[0]
+            temp = calendar.month_name[int(splitted_words[1])]
             month = temp[0:3]
-            weather.print_month_average(directoryPath, year, month)
+            weather.print_month_average(directory_path, year, month)
         else:
             print("Incorrect Arguments!")
 
-    elif reportType == '-c':
-
-        if weather.verify_directory(
-                directoryPath) and weather.verify_year_month(year):
-            splittedWords = year.split('/')
-            year = splittedWords[0]
-            temp = calendar.month_name[int(splittedWords[1])]
+    elif report_type == '-c':
+        if weather.verify_directory(directory_path) and weather.verify_year_month(year):
+            splitted_words = year.split('/')
+            year = splitted_words[0]
+            temp = calendar.month_name[int(splitted_words[1])]
             month = temp[0:3]
-            weather.print_day_bars(directoryPath, year, month)
+            weather.print_day_bars(directory_path, year, month)
         else:
             print("Incorrect Arguments!")
-    return
 
 
 def main():
     if len(sys.argv) == 4:
-        directoryPath = sys.argv[1]
+        directory_path = sys.argv[1]
         year = sys.argv[3]
-        reportType = sys.argv[2]
-        sub_main(directoryPath, year, reportType)
+        report_type = sys.argv[2]
+        sub_main(directory_path, year, report_type)
     elif len(sys.argv) == 8:
-        directoryPath = sys.argv[1]
+        directory_path = sys.argv[1]
         year1 = sys.argv[3]
         year2 = sys.argv[5]
         year3 = sys.argv[7]
-        reportType1 = sys.argv[2]
-        reportType2 = sys.argv[4]
-        reportType3 = sys.argv[6]
-        sub_main(directoryPath, year1, reportType1)
+        report_type1 = sys.argv[2]
+        report_type2 = sys.argv[4]
+        report_type3 = sys.argv[6]
+        sub_main(directory_path, year1, report_type1)
         print('#########################################')
-        sub_main(directoryPath, year2, reportType2)
+        sub_main(directory_path, year2, report_type2)
         print('#########################################')
-        sub_main(directoryPath, year3, reportType3)
-
+        sub_main(directory_path, year3, report_type3)
     else:
         print('Incorrect arguments!')
-
     sys.exit(1)
-
-
 if __name__ == '__main__':
     main()
