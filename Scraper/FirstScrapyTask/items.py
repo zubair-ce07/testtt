f23d994b75eea0fa4169a80df97b912abc70a081
umer-nadeem <umer.nadeem@arbisoft.com>
2017-12-08 09:27:07 +0500

    Removing Conflicts

---
+++ Scraper/FirstScrapyTask/items.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class FirstscrapytaskItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+
+class HypedcItem(scrapy.Item):
+
+    name = scrapy.Field()
+    brand = scrapy.Field()
+    category = scrapy.Field()
+    url = scrapy.Field()
+    product_code = scrapy.Field()
+    price_final = scrapy.Field()
+    desc = scrapy.Field()
+    color = scrapy.Field()
+    currency = scrapy.Field()
+    image_urls = scrapy.Field()
+    sizes_info = scrapy.Field()
+
+
+class MarcJacobsItem(scrapy.Item):
+
+    product_url = scrapy.Field()
+    product_id = scrapy.Field()
+    title = scrapy.Field()
+    category = scrapy.Field()
+    description = scrapy.Field()
+    locale = scrapy.Field()
+    currency = scrapy.Field()
+    variations = scrapy.Field()
+
+
+class SkechersItem(scrapy.Item):
+
+    name = scrapy.Field()
+    brand = scrapy.Field()
+    category = scrapy.Field()
+    gender = scrapy.Field()
+    url = scrapy.Field()
+    product_id = scrapy.Field()
+    image_urls = scrapy.Field()
+    description = scrapy.Field()
+    skus = scrapy.Field()
+
