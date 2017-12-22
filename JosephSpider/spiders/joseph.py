6d4509358e3be382f864aace7f4ba9823cfa461f
larebsyed <lareb_khalil_syed@hotmail.com>
2017-12-22 18:20:19 +0500

    ✔ Let's add the urls from which we scraped the item to trail while crawling. and no need use b". Use/install scrapy with python 3.x @joseph:43 @done (17-12-22 18:17)  ✔ return response.xpath("//div[./label[@for='tab-2']]/div[@class='tab-content']/text()").extract() @joseph:45 @done (17-12-22 17:17)  ✔ details["out_of_stock"] = "False" @joseph:85 @done (17-12-22 16:47)  ✔ Let's set out_of_stock only and remove parse_in_stock function and move out_of_stock detection to skus function. @joseph:90 @done (17-12-22 16:47)  ✔ product or item. Let's move item parsing to a separate class. @joseph:22 @done (17-12-22 16:03)  ✔ Only use parse_ with callback function names. Let's rename rest to the item_ or product_ prefix. @joseph:41 @done (17-12-22 16:03)  ✔ We don't need this in python 3.x @joseph:1 @done (17-12-22 15:45)  ✔ Only import what's required. @joseph:4 @done (17-12-22 15:45)  ✔ Let's remove unwanted spaces @joseph:6 @done (17-12-22 15:45)

--- JosephSpider/spiders/joseph.py
+++ JosephSpider/spiders/joseph.py
@@ -1,9 +1,10 @@
-# -*- coding: utf-8 -*-
-import sys
+import copy
 from urllib.parse import urlparse
-import scrapy
+
 from scrapy.linkextractors import LinkExtractor
-from scrapy.    spiders import CrawlSpider, Rule
+from scrapy.spiders import CrawlSpider, Rule
+
+from .item import ItemSpider
 
 
 class JosephSpider(CrawlSpider):
@@ -13,101 +14,53 @@ class JosephSpider(CrawlSpider):
 
     rules = (
         Rule(LinkExtractor(
-            restrict_css='a[class*="navigation__link"]')),
+            restrict_css='a[class*="navigation__link"]'), callback='parse'),
         Rule(LinkExtractor(restrict_css='.search-result-content .thumb-link'),
              callback='parse_product'),
     )
 
-    def parse_product(self, response):
-        data = {}
+    def parse(self, response):
 
-        brand = response.xpath("//meta[@itemprop='brand']/@content")
-        data["brand"] = brand.extract_first()
+        for request in super().parse(response):
+            data = response.meta.get('data') or dict()
+            data = copy.deepcopy(data)
+            data['trail'] = data.get('trail') or list()
+            data['trail'].append(request.url)
+            request.meta['data'] = data
+            yield request
+
+    def parse_product(self, response):
+        data = response.meta.get('data') or dict()
+        data['trail'] = data.get('trail') or list()
+        data["trail"].append(response.url)
+
+        brand_xpath = "//meta[@itemprop='brand']/@content"
+        name_xpath = "//h1[@itemprop='name']/text()"
+        images_xpath = "//div[@id='thumbnails']//img/@src"
+        retailer_sku = "//meta[@itemprop='SKU']/@content"
+        care_xpath = "//div[./label[@for='tab-2']]/div[@class='tab-content']/text()"
+        descriptions_xpath = "//div[./label[@for='tab-1']]/div[@class='tab-content']/text()"
+
         data["market"] = "US"
         data["retailer"] = "joseph-us"
-        name = response.xpath("//h1[@itemprop='name']/text()")
-        data["name"] = name.extract_first()
-        data["care"] = self.parse_care(response)
-        data["description"] = self.parse_description(response)
-        data["image_urls"] = self.parse_images(response)
-        retailer_sku = response.xpath("//meta[@itemprop='SKU']/@content")
-        data["retailer_sku"] = retailer_sku.extract_first()
-        data["url"] = response.url
-        referrer = str(response.request.headers.get('Referer'))
-        data["gender"] = self.parse_gender(response)
+        referrer = data['trail'][0]
         data["category"] = urlparse(referrer).path.split('/')[-2]
-        return self.parse_skus(response, data)
+        data["brand"] = response.xpath(brand_xpath).extract_first()
+        data["name"] = response.xpath(name_xpath).extract_first()
+        data["care"] = response.xpath(care_xpath).extract()
+        data["description"] = response.xpath(descriptions_xpath).extract()
+        data["image_urls"] = response.xpath(images_xpath).extract()
+        data["retailer_sku"] = response.xpath(retailer_sku).extract_first()
+        data["gender"] = "women" if "women" in referrer else "men"
 
-    def parse_gender(self, response):
-        referer_link = response.request.headers.get("Referer")
-        return "women" if b"women" in referer_link else "men"
+        return self.item_skus(response, data)
 
-    def parse_care(self, response):
-        cares = response.xpath(
-            "//div[./label[@for='tab-2']]/div[@class='tab-content']/text()")
-        return [care.extract() for care in cares]
-
-    def parse_images(self, response):
-        images = response.xpath("//div[@id='thumbnails']//img/@src")
-        return [image_url.extract() for image_url in images]
-
-    def parse_description(self, response):
-        descriptions = response.xpath(
-            "//div[./label[@for='tab-1']]/div[@class='tab-content']/text()")
-        return [description.extract() for description in descriptions]
-
-    def parse_skus(self, response, data):
-
+    def item_skus(self, response, data):
+        item_spider = ItemSpider()
         data["skus"] = {}
         colors = response.xpath("//ul[contains(@class, 'color')]//a")
         for color in colors:
             request = response.follow(
-                color, self.parse_skus_callback)
+                color, item_spider.parse)
             request.meta["product"] = data
             yield request
-        return data
-
-    def parse_skus_callback(self, response):
-
-        data = response.meta["product"]
-        data_catalogue = data["skus"]
-        data_catalogue.update(self.parse_in_stock(response))
-        data_catalogue.update(self.parse_out_of_stock(response))
-        return data
-
-    def parse_in_stock(self, response):
-        items = dict()
-        data = response.meta["product"]
-        selectable = response.xpath(
-            "//ul[contains(@class, 'size')]//li[contains(@class, 'selectable')]/a/@title")
-        for selectable_size in selectable:
-            details = self.parse_size(response, selectable_size)
-            details["out_of_stock"] = "False"
-            item_key = details["colour"]+"_"+details["size"]
-            items[item_key] = details
-        return items
-
-    def parse_out_of_stock(self, response):
-        items = dict()
-        data = response.meta["product"]
-        out_stock = response.xpath(
-            "//ul[contains(@class, 'size')]//li[contains(@class, 'unselectable')]/a/@title")
-        for out_stock_size in out_stock:
-            details = self.parse_size(response, out_stock_size)
-            details["out_of_stock"] = "False"
-            item_key = details["colour"]+"_"+details["size"]
-            items[item_key] = details
-        return items
-
-    def parse_size(self, response, item):
-        details = dict()
-        price = response.xpath("//meta[@itemprop='price']/@content")
-        currency = response.xpath("//meta[@itemprop='priceCurrency']/@content")
-        color = response.xpath(
-            "//ul[contains(@class, 'color')]//li[contains(@class, 'selected')]//img/@alt")
-        size = item.extract().strip("Select Size: ")
-        details["colour"] = color.extract_first()
-        details["currency"] = currency.extract_first()
-        details["price"] = price.extract_first()
-        details["size"] = size
-        return details
