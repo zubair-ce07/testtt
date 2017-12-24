3f76eef8aa92d669120b21b186e5be3a2d1f3632
ahmadashraf2 <ahmad.ashraf@arbisoft.com>
2017-12-24 14:38:26 +0500

    adding filters in admin view

--- blogSite/blog/admin.py
+++ blogSite/blog/admin.py
@@ -5,9 +5,42 @@ from .models import Comment
 from .models import LikeComment
 from .models import LikePost
 
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(Comment)
-admin.site.register(LikePost)
-admin.site.register(LikeComment)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category_name')
+    list_filter = ('author', 'category__category')
+
+    def category_name(self, post):
+        return post.category.category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('body', 'user', 'post_name')
+    list_filter = ('user', 'post__title')
+
+    def post_name(self, comment):
+        return comment.post.title
+
+
+class LikePostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post_name', 'vote')
+    list_filter = ('user', 'post__title',)
+
+    def post_name(self, like):
+        return like.post.title
+
+class LikeCommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'comment_id', 'vote')
+    list_filter = ('user', 'comment__id')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(LikePost, LikePostAdmin)
+admin.site.register(LikeComment, LikeCommentAdmin)
 
