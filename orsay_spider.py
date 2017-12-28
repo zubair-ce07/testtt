73982a403253ea6df8675c645b44fb87ecb78021
shafqatfarhan <shafqat.farhan@arbisoft.com>
2017-12-28 19:22:52 +0500

    modified orsay spider file

--- orsay_spider.py
+++ orsay_spider.py
@@ -12,7 +12,7 @@ class OrsaySpiderSpider(scrapy.Spider):
     def parse(self, response):
         product_categories = response.css('#nav > .level0')
         for category in product_categories:
-            category_links = category.css('.level0 > .level1 > a[href]::attr(href)').getall()
+            category_links = category.css('.level0 > .level1 > a[href]::attr(href)').get()
             if category_links:
                 for link in category_links:
                     yield response.follow(link, self.parse_category)
@@ -87,7 +87,7 @@ class OrsaySpiderSpider(scrapy.Spider):
             yield product
 
     def product_color_info(self, response):
-        color_info = {}
+        product_color_info = {}
         product_id = response.css('#product_addtocart_form > .no-display > input[name="product"]::attr(value)').get()
         currency = response.css('#product-options-wrapper .sizebox-wrapper::attr(data-currency)').get()
 
@@ -103,17 +103,13 @@ class OrsaySpiderSpider(scrapy.Spider):
                 out_of_stock = "True"
 
             product_size = size_detail.css('li::text').get().strip()
-
-            if product_size:
-
-                color_info = {
-                    'price': response.css('#product_main .regular-price > .price::text').get(),
-                    'colour': response.css('#product_addtocart_form > .no-display > input[name="color"]::attr(value)').get(),
-                    'out_of_stock': out_of_stock,
-                    'size': product_size,
-                    'currency': currency
-                }
-
-                color_info_key = '{}_{}'.format(product_id, product_size)
-                color_info[color_info_key] = color_info
-        return color_info
+            color_info = {
+                'price': response.css('#product_main .regular-price > .price::text').get(),
+                'colour': response.css('#product_addtocart_form > .no-display > input[name="color"]::attr(value)').get(),
+                'out_of_stock': out_of_stock,
+                'size': product_size,
+                'currency': currency
+            }
+            color_info_key = '{}_{}'.format(product_id, product_size)
+            product_color_info[color_info_key] = color_info
+        return product_color_info
