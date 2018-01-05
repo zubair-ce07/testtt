64f7b828954b1681b366cc227771df0554d5c0d0
ahmadashraf2 <ahmad.ashraf@arbisoft.com>
2018-01-05 15:21:50 +0500

    adding tellme

--- blogSite/blogSite/urls.py
+++ blogSite/blogSite/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^tellme/', include("tellme.urls")),
 
 ]
