03be8888f048b08e7c8bb86786b1252914d232e7
Zulqarnain <zulqarnain.qctech@gmail.com>
2018-01-09 18:45:18 +0500

    add files for commit

---
+++ constants.py
@@ -0,0 +1,11 @@
+NULLS = (None, '')
+RED_COLOR_ANSI = '\033[91m'
+BLUE_COLOR_ANSI = '\033[94m'
+END_COLOR = '\033[0m'
+DATE_LABEL = 'PKT'
+DATE_LABEL_2 = 'PKST'
+MAX_TEMP_LABEL = 'Max TemperatureC'
+MIN_TEMP_LABEL = 'Min TemperatureC'
+MAX_HUMIDITY_LABEL = 'Max Humidity'
+NO_ARG_ERROR_MSG = 'Kindly pass an argument to the script to process further'
+NO_DATA_ABAILABLE_MSG = 'No Data Available For This Year'
