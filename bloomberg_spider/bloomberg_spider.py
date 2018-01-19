a60043f8c8ce456f1be2abfe30d0b261340a4e7f
rabbiyaalamgir <rabia.alam@arbisoft.com>
2018-01-19 17:05:50 +0500

    Revised spider.

--- bloomberg_spider/bloomberg_spider.py
+++ bloomberg_spider/bloomberg_spider.py
@@ -6,12 +6,6 @@ import scrapy
 import time
 
 
-def concatenate_strings(a, b):
-    if b == None:
-        return None
-    else:
-        return '{}{}'.format(a, b)
-
 
 class BloombergSpider(scrapy.Spider):
     name = 'bloomberg_spider'
@@ -26,13 +20,13 @@ class BloombergSpider(scrapy.Spider):
     def parse(self, response):
         data = json.loads(response.body)
         yield scrapy.Request(
-            url = concatenate_strings('https://careers.bloomberg.com/job_search'
-                                   '/search_query?jobStartIndex=0&jobBatchSize'
-                                   '=', data['totalJobCount']),
+            url='{}{}'.format('https://careers.bloomberg.com/job_search/search'
+                              '_query?jobStartIndex=0&jobBatchSize=',
+                              data['totalJobCount']),
             headers={'X-Requested-With': 'XMLHttpRequest'},
-            callback=self.all_jobs)
+            callback=self.get_all_jobs)
 
-    def all_jobs(self, response):
+    def get_all_jobs(self, response):
         date_format = "%Y-%m-%d"
         date_today = datetime.datetime.strptime(str(datetime.datetime.now()
                                                     .date()), date_format)
@@ -44,36 +38,28 @@ class BloombergSpider(scrapy.Spider):
             if days_since_posted <= datetime.timedelta(days = int(self.n)):
                 url = urljoin('https://careers.bloomberg.com/job/'
                                        'detail/', job['JobReqNbr'])
+
+                job_loader = JobLoader(selector=response)
+                job_loader.add_value('business_area', job['Function']['Value'])
+                job_loader.add_value('experience_level', job['ExperienceLevel'])
+                job_loader.add_value('job_date', job_date)
+                job_loader.add_value('job_function', job['Specialty']['Value'])
+                job_loader.add_value('location', job['Office']['City'])
+                job_loader.add_value('requisition_no', job['JobReqNbr'])
+                job_loader.add_value('title', job['JobTitle'])
+                job_loader.add_value('url', url)
                 yield scrapy.Request(
-                    url = url,
-                    meta = {
-                        'business_area' : job['Function']['Value'],
-                        'experience_level' : job['ExperienceLevel'],
-                        'job_date': job_date,
-                        'job_function' : job['Specialty']['Value'],
-                        'location' : job['Office']['City'],
-                        'requisition_no' : job['JobReqNbr'],
-                        'title' : job['JobTitle'],
-                        'url' : url,
-                    },
-                    callback = self.parse_job
+                    url=url, meta={'item_loader':job_loader},
+                    callback=self.parse_job
                 )
 
     def parse_job(self, response):
-        job = JobLoader(selector=response)
-        job.add_value('business_area', response.meta['business_area'])
+        job = response.meta['item_loader']
         time_now = datetime.datetime.now().fromtimestamp(time.time()) \
             .strftime('%Y-%m-%d %H:%M:%S.%f')
         job.add_value('crawled_at', time_now)
         job.add_xpath('description', '//div[@class="col-xs-12"]/div[@class='
                                      '"row"]/div[@class="col-xs-12"]//text()')
-        job.add_value('experience_level', response.meta['experience_level'])
-        job.add_value('job_date', response.meta['job_date'])
-        job.add_value('job_function', response.meta['job_function'])
-        job.add_value('location', response.meta['location'])
         job.add_value('provider', 'bloomberg')
         job.add_value('provider_url', 'https://careers.bloomberg.com/')
-        job.add_value('requisition_no', response.meta['requisition_no'])
-        job.add_value('title', response.meta['title'])
-        job.add_value('url', response.meta['url'])
         return job.load_item()
