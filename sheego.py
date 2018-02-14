c642f4843dc3eef05d0bd3a233c49b547dddd509
HusnainAslam <husnain.aslam@arbisoft.com>
2018-02-14 18:46:03 +0500

    changes parser are at top and dependents are at bottom

--- sheego.py
+++ sheego.py
@@ -19,7 +19,7 @@ class SheegoSpider(CrawlSpider):
     start_urls = ['https://www.sheego.de/']
     variant_url = 'https://www.sheego.de/index.php?'
 
-    rules = (Rule(LinkExtractor(restrict_css=(['.mainnav--top', 'a.js-next']))),
+    rules = (Rule(LinkExtractor(restrict_css=(['.mainnav--top', '.js-next']))),
              Rule(LinkExtractor(restrict_css=('.product__top')), callback="parse_product_detail"),)
 
     def parse_product_detail(self, response):
@@ -38,107 +38,21 @@ class SheegoSpider(CrawlSpider):
         item['meta']['requests'] = self.colour_requests(item, response)
         yield self.next_request(item)
 
-    def colour_requests(self, item, response):
-        requests = []
-        param_cl = self.param_cl(response)
-        colours_varselids = self.colours_varselid(response)
-        for colour_varselid in colours_varselids:
-            query_parameters = {
-                'anid': item['retailor_sku'],
-                'cl': param_cl,
-                'varselid[0]': colour_varselid,
-            }
-            colour_url = self.colour_url(query_parameters)
-            request = Request(url=colour_url, callback=self.parse_colour)
-            requests.append(request)
-        return requests
-
     def parse_colour(self, response):
         item = response.meta['item']
         item['image_urls'] += self.images_url(response)
         item['meta']['requests'] = self.size_requests(response)
         yield self.next_request(item)
 
-    def size_requests(self, response):
-        length_type_varselids = self.length_type_varselids(response)
-        size_varselids = self.sizes_varselid(response)
-        size2_varselids = self.size2_verselids(response)
-        sizes_type = SizeType.First
-        if length_type_varselids:
-            size_varselids = length_type_varselids
-            sizes_type = SizeType.Third
-        elif size2_varselids:
-            sizes_type = SizeType.Second
-        requests = response.meta['item']['meta']['requests']
-        for size_varselid in size_varselids:
-            size_url = add_or_replace_parameter(response.url, 'varselid[1]', size_varselid)
-            request = Request(url=size_url,
-                              meta={
-                                  'sizes_type': sizes_type
-                              })
-
-            if sizes_type is SizeType.First:
-                request.callback = self.parse_skus
-            else:
-                request.callback = self.parse_size
-            requests.append(request)
-        return requests
-
     def parse_size(self, response):
         item = response.meta['item']
         item['meta']['requests'] = self.size2_request(response)
         yield self.next_request(item)
 
-    def size2_request(self, response):
-        requests = response.meta['item']['meta']['requests']
-        sizes_type = response.meta['sizes_type']
-        size2_varselids = []
-        if sizes_type is SizeType.Second:
-            size2_varselids = self.size2_verselids(response)
-        elif sizes_type is SizeType.Third:
-            size2_varselids = self.sizes_varselid(response)
-        for size2_varselid in size2_varselids:
-            size_url = add_or_replace_parameter(response.url, 'varselid[2]', size2_varselid)
-            request = Request(url=size_url,
-                              callback=self.parse_skus,
-                              meta={
-                                  'sizes_type': response.meta['sizes_type']
-                              })
-            requests.append(request)
-        return requests
-
     def parse_skus(self, response):
         item = response.meta['item']
-        sizes_type = response.meta['sizes_type']
-        size = self.size(response)
-        if sizes_type is SizeType.Second:
-            size2 = self.size2(response)
-            size = f'{size}_{size2}'
-        elif sizes_type is SizeType.Third:
-            size2 = self.length_type(response)
-            size = f'{size}_{size2}'
-        sku = self.sku(response, size)
-        item['skus'].update(sku)
+        item['skus'].update(self.sku(response))
         yield self.next_request(item)
-
-    def sku(self, response, size):
-        privious_price, price, currency = self.product_pricing(response)
-        colour = self.colour(response)
-        stock = self.is_instock(response)
-        sku_key = f'{colour}_{size}'.replace(' ', '_')
-        sku_value = {
-            'price': price,
-            'currency': currency,
-            'size': size,
-            'colour': colour,
-            'privious_price': privious_price
-        }
-        if not stock:
-            sku_value['Out of Stock'] = True
-        sku = {
-            sku_key: sku_value
-        }
-        return sku
 
     def next_request(self, item):
         if item['meta']['requests']:
@@ -147,14 +61,6 @@ class SheegoSpider(CrawlSpider):
             return request
         del item['meta']
         return item
-
-    def product_pricing(self, response):
-        price = response.css('meta[itemprop="price"]::attr(content)').extract_first()
-        currency = response.css('meta[itemprop="priceCurrency"]::attr(content)').extract_first()
-        privious_price = self.privious_price(response)
-        price = self.int_price(price)
-        privious_price = self.int_price(privious_price)
-        return privious_price, price, currency
 
     def int_price(self, price):
         return int(price.replace('.', '')) if price else ''
@@ -226,4 +132,97 @@ class SheegoSpider(CrawlSpider):
 
     def categories(self, response):
         return response.css('.breadcrumb__item ::text').extract()
+
+    def product_pricing(self, response):
+        price = response.css('meta[itemprop="price"]::attr(content)').extract_first()
+        currency = response.css('meta[itemprop="priceCurrency"]::attr(content)').extract_first()
+        privious_price = self.privious_price(response)
+        price = self.int_price(price)
+        privious_price = self.int_price(privious_price)
+        return privious_price, price, currency
+
+    def colour_requests(self, item, response):
+        requests = []
+        param_cl = self.param_cl(response)
+        colours_varselids = self.colours_varselid(response)
+        for colour_varselid in colours_varselids:
+            query_parameters = {
+                'anid': item['retailor_sku'],
+                'cl': param_cl,
+                'varselid[0]': colour_varselid,
+            }
+            colour_url = self.colour_url(query_parameters)
+            request = Request(url=colour_url, callback=self.parse_colour)
+            requests.append(request)
+        return requests
+
+    def size_requests(self, response):
+        length_type_varselids = self.length_type_varselids(response)
+        size_varselids = self.sizes_varselid(response)
+        size2_varselids = self.size2_verselids(response)
+        sizes_type = SizeType.First
+        if length_type_varselids:
+            size_varselids = length_type_varselids
+            sizes_type = SizeType.Third
+        elif size2_varselids:
+            sizes_type = SizeType.Second
+        requests = response.meta['item']['meta']['requests']
+        for size_varselid in size_varselids:
+            size_url = add_or_replace_parameter(response.url, 'varselid[1]', size_varselid)
+            request = Request(url=size_url,
+                              meta={
+                                  'sizes_type': sizes_type
+                              })
+
+            if sizes_type is SizeType.First:
+                request.callback = self.parse_skus
+            else:
+                request.callback = self.parse_size
+            requests.append(request)
+        return requests
+
+    def size2_request(self, response):
+        requests = response.meta['item']['meta']['requests']
+        sizes_type = response.meta['sizes_type']
+        size2_varselids = []
+        if sizes_type is SizeType.Second:
+            size2_varselids = self.size2_verselids(response)
+        elif sizes_type is SizeType.Third:
+            size2_varselids = self.sizes_varselid(response)
+        for size2_varselid in size2_varselids:
+            size_url = add_or_replace_parameter(response.url, 'varselid[2]', size2_varselid)
+            request = Request(url=size_url,
+                              callback=self.parse_skus,
+                              meta={
+                                  'sizes_type': response.meta['sizes_type']
+                              })
+            requests.append(request)
+        return requests
+
+    def sku(self, response):
+        privious_price, price, currency = self.product_pricing(response)
+        colour = self.colour(response)
+        size = self.size(response)
+        stock = self.is_instock(response)
+        sizes_type = response.meta['sizes_type']
+        if sizes_type is SizeType.Second:
+            size2 = self.size2(response)
+            size = f'{size}_{size2}'
+        elif sizes_type is SizeType.Third:
+            length_type = self.length_type(response)
+            size = f'{size}_{length_type}'
+        sku_key = f'{colour}_{size}'.replace(' ', '_')
+        sku_value = {
+            'price': price,
+            'currency': currency,
+            'size': size,
+            'colour': colour,
+            'privious_price': privious_price
+        }
+        if not stock:
+            sku_value['Out of Stock'] = True
+        sku = {
+            sku_key: sku_value
+        }
+        return sku
 
