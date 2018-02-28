cb703d22f05e313a5db960e1ee2669374ebc4727
Abdullah <abdullah.baig@arbisoft.com>
2018-02-28 18:43:15 +0500

    unused files & default comments removed!

--- scrapylab/scrapylab/items.py
+++ scrapylab/scrapylab/items.py
@@ -1,15 +1,7 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class SweatBettyItem(scrapy.Item):
-    # define the fields for your item here like:
     category = scrapy.Field()
     name = scrapy.Field()
     care = scrapy.Field()
---
+++ scrapylab/scrapylab/pipelines.py
@@ -0,0 +1,4 @@
+
+class ScrapylabPipeline(object):
+    def process_item(self, item, spider):
+        return item
