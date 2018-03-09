80eca4b8d6dabe02c705d9aa2e054aabfd7ae4a6
Muhammad Ahmed Sharif <ahmad.sharif@arbisoft.com>
2018-03-09 19:00:40 +0500

    Custom filters, custom list display has been added.

--- django-polls/polls/admin.py
+++ django-polls/polls/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
 from .models import Choice, Question
 
 # Register your models here.
+@admin.register(Choice)
+class AuthorAdmin(admin.ModelAdmin):
+    fields = ('question', 'choice_text', 'votes')
+    
+    list_display = ('question','choice_text','votes')
+    list_filter = ['choice_text']
+    
 
 
 class ChoiceInLine(admin.TabularInline):
@@ -19,4 +27,23 @@ class QuestionAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
 
 
+# class OptionFilter(SimpleListFilter):
+#     title = 'choice'
+#     parameter_name = 'votes'
+
+#     def lookup(self, request, ModelAdmin):
+#         votes = set([Choice.votes for model in model.objects.all()])
+#         # return [(model.question, model.votes for c in votes)] 
+#         return votes
+
+#     def queryset(self, request, queryset):
+#         if self.value():
+#             return queryset.filter(self.value())
+#         else:
+#             return queryset
+       
+admin.site.unregister(Choice)
+admin.site.register(Choice, AuthorAdmin)
 admin.site.register(Question, QuestionAdmin)
+
+
