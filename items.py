d99f2be56f26d01c85c6704faf51be81bad59ccf
Abdullah <abdullah.baig@arbisoft.com>
2018-03-09 16:25:36 +0500

    items file updated!

--- items.py
+++ items.py
@@ -1,19 +1,6 @@
 import scrapy
 
 
-class SweatBettyItem(scrapy.Item):
-    category = scrapy.Field()
-    name = scrapy.Field()
-    care = scrapy.Field()
-    description = scrapy.Field()
-    skus = scrapy.Field()
-    retailer_sku = scrapy.Field()
-    image_urls = scrapy.Field()
-    brand = scrapy.Field()
-    url = scrapy.Field()
-    currency = scrapy.Field()
-
-
 class UrbanLockerItem(scrapy.Item):
     category = scrapy.Field()
     name = scrapy.Field()
