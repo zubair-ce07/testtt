3d5f1f4a6b2f9296173e1965ff22f683fb279f02
Hassaan Ahmad Farooqi <hassaan.ahmad@arbisoft.com>
2018-04-02 18:03:54 +0500

    Completed ernstings_spider task
    No repetitons in products now, and products coming from the whole website, even from hovering navigation bar and more products pages.
    Handled both of the above by going through the json files, extracting data and making links manually.
    Wohnen (Living items) are excluded from following by 'deny' and not considering some links having text 'My Home' only (because they are also living items)

--- ernstings_spider.py
+++ ernstings_spider.py
@@ -4,7 +4,9 @@ Example command from terminal: $ scrapy crawl ernstings -o ernstings.json
 Pylint score: 10.00
 """
 
+import json
 import re
+import scrapy                                       # pylint: disable=import-error
 from scrapy.spiders import Rule, CrawlSpider        # pylint: disable=import-error
 from scrapy.linkextractors import LinkExtractor     # pylint: disable=import-error
 from scrapy_task.items import Product, StoreKeepingUnits
@@ -12,32 +14,97 @@ from scrapy_task.items import Product, StoreKeepingUnits
 
 class ErnstingsSpider(CrawlSpider):
     """Spider to crawl all products in ernstings-family.de website"""
+
     name = "ernstings"
     allowed_domains = ['ernstings-family.de']
-    start_urls = ['https://www.ernstings-family.de']
     ids = []
 
     rules = (
         Rule(LinkExtractor(
-            restrict_css="div#main-navigation-wrapper a:not(:contains('Wohnen'))"
-            )),
-        Rule(LinkExtractor(
             restrict_css=("main#content-wrapper > div.container-fluid "
                           "a:not([class*='product-list-tile-holder']):not(:contains('My Home')), "
-                          "a:not([class*='product-list-tile-holder']):contains('My Home ')")
-            )),
+                          "a:not([class*='product-list-tile-holder']):contains('My Home ')"),
+            deny=('(.*?)wohnen(.*?)', )),
+             callback='more_products',
+             follow=True
+            ),
         Rule(LinkExtractor(
-            restrict_css="div.teaser-highlight-product-wrapper a.link-blank"),
+            restrict_css="div.teaser-highlight-product-wrapper a.link-blank",
+            deny=('(.*?)wohnen(.*?)', )),
              callback='parse_product'
             ),
         Rule(LinkExtractor(
-            restrict_css="a[class*='product-list-tile-holder']"),
+            restrict_css="a[class*='product-list-tile-holder']",
+            deny=('(.*?)wohnen(.*?)', )),
              callback='parse_product'
             ),
     )
 
+    def start_requests(self):
+        """Manually defining it, because the links in hover of navigation bar are
+        not available in homepage"""
+        yield scrapy.Request('https://www.ernstings-family.de')
+        yield scrapy.Request('https://www.ernstings-family.de//navigation.json?storeId=10151',
+                             self.parse_json_links)
+
+    @staticmethod
+    def parse_json_links(response):
+        """Extract links from navigation json page"""
+        navbar_dict = json.loads(response.text)
+        navbar_texts = []
+
+        for navbar in navbar_dict['navigation']:
+            if navbar['name'] != 'Wohnen' and navbar['name'] != 'NEU':
+                navbar_texts.append(str(navbar))
+
+        for dict_text in navbar_texts:
+            nav_links = re.findall("'href': u'(.+?)'", dict_text)
+            for nav_link in nav_links:
+                yield scrapy.Request(nav_link)
+
+    def more_products(self, response):
+        """Method to get more products from json page, if more products button is there"""
+        more = response.css(
+            "main#content-wrapper > div.container-fluid span#product-list-load-more-products"
+        )
+
+        if more:
+            store_id = self.get_js_element(response, 'storeId')
+            catalog_id = self.get_js_element(response, 'catalogId')
+            products_per_page = self.get_js_element(response, 'productListPageSizeOfPage')
+            products_list_pages = self.get_js_element(response, 'productListPageReloadBoundary')
+            category_id = self.get_js_element(response, 'categoryId')
+            search_type = self.get_search_type(response)
+
+            for num in range(2, int(products_list_pages) + 1):
+                load_products_url = (
+                    "https://www.ernstings-family.de/wcs/resources/store/{}/productview"
+                    "/bySearchTermDetails/*?pageNumber={}&pageSize={}&pageSizeReloadBoundary={}"
+                    "&searchType={}&categoryId={}&profileName=EF_findCatalogEntryByNameAndShort"
+                    "Description_Details"
+                ).format(
+                    store_id, num, products_per_page, products_list_pages, search_type, category_id
+                )
+                yield scrapy.Request(load_products_url, self.parse_json_products,
+                                     meta={'catalog_id': catalog_id, 'store_id': store_id})
+
+    def parse_json_products(self, response):
+        """Method to make the links for products having data in json file"""
+        catalog_id = response.meta.get('catalog_id')
+        store_id = response.meta.get('store_id')
+        products_dict = json.loads(response.text)
+
+        for product in products_dict['CatalogEntryView']:
+            product_id = product['uniqueID']
+            product_url = (
+                "https://www.ernstings-family.de/ProductDisplay?urlRequestType=Base"
+                "&catalogId={}&productId={}&storeId={}"
+            ).format(catalog_id, product_id, store_id)
+
+            yield scrapy.Request(product_url, self.parse_product)
+
     def parse_product(self, response):
-        """Parsing product and getting required details"""
+        """Method to parse a product and get required details"""
 
         product_id = self.get_retailer_sku(response)
 
@@ -141,17 +208,6 @@ class ErnstingsSpider(CrawlSpider):
         return price, previous
 
     @staticmethod
-    def get_currency(response):
-        """Method to get currency"""
-        javascripts = response.css('script ::text').extract()
-        currency = None
-        for each in javascripts:
-            text = re.search(r'"commandContextCurrency": "(\w+)"', each)
-            if text:
-                currency = text.group(1)
-        return currency
-
-    @staticmethod
     def get_colour(response):
         """Method to get color"""
         colour = response.css('p.product-detail-product-color::text').extract()
@@ -164,7 +220,7 @@ class ErnstingsSpider(CrawlSpider):
         for option in options:
             sku = StoreKeepingUnits()
             sku['price'], sku['previous_prices'] = self.get_prices(response)
-            sku['currency'] = self.get_currency(response)
+            sku['currency'] = self.get_js_element(response, 'commandContextCurrency')
             sku['colour'] = self.get_colour(response)
             size = option.css('option::text').extract_first()
             sku['size'] = None
@@ -175,6 +231,28 @@ class ErnstingsSpider(CrawlSpider):
             sku['sku_id'] = option.css('option::attr(data-catentry-id)').extract_first()
             skus.append(sku)
         return skus
+
+    @staticmethod
+    def get_search_type(response):
+        """Method to get searchtype"""
+        javascripts = response.css('script ::text').extract()
+        search_type = None
+        for each in javascripts:
+            text = re.search(r"var searchType = '(\d+)'", each)
+            if text:
+                search_type = text.group(1)
+        return search_type
+
+    @staticmethod
+    def get_js_element(response, element):
+        """Method to get JavaScript Elements"""
+        javascripts = response.css('script ::text').extract()
+        value = None
+        for each in javascripts:
+            text = re.search(r'"{}": "(\w+)"'.format(element), each)
+            if text:
+                value = text.group(1)
+        return value
 
 
 def get_float_price(price):
