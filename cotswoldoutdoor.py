86ceb6aa8d450eb0054b488517d052c86db26424
Hassaan Ahmad Farooqi <hassaan.ahmad@arbisoft.com>
2018-05-08 20:36:29 +0500

    Improved the code on the basis of reviews 2.0

--- cotswoldoutdoor.py
+++ cotswoldoutdoor.py
@@ -1,8 +1,9 @@
 import json
 
 from scrapy.spiders import Request
-from skuscraper.parsers.jsparser import JSParser
+from w3lib.url import add_or_replace_parameter, url_query_parameter
 
+from skuscraper.parsers.jsparser import JSParser
 from .base import BaseParseSpider, BaseCrawlSpider, clean, soupify
 
 
@@ -12,8 +13,8 @@ class Mixin:
 
 
 class MixinUK(Mixin):
-    retailer = Mixin.retailer + "-uk"
-    market = "UK"
+    retailer = Mixin.retailer + '-uk'
+    market = 'UK'
     start_urls = [
         'https://www.cotswoldoutdoor.com/c/mens.html',
         'https://www.cotswoldoutdoor.com/c/womens.html',
@@ -62,70 +63,44 @@ class CotswoldParseSpider(BaseParseSpider):
         soup = soupify([self.product_name(response)] + category)
         return self.gender_lookup(soup) or 'unisex-adults'
 
-    def image_urls(self, response):
+    def raw_product(self, response):
         raw_product_css = 'script:contains(productInfo)::text'
-        raw_product = JSParser(clean(response.css(raw_product_css))[0])['productInfo']
+        return JSParser(clean(response.css(raw_product_css))[0])['productInfo']
 
-        images = [image_urls['bigImageUrl']
-                  for image_urls in raw_product['selectedProductColorVariation']['images']]
-
-        images += [image_urls['bigImageUrl']
-                   for variation in raw_product['otherProductColorVariation']
-                   for image_urls in variation['images']]
-
+    def image_urls(self, response):
+        raw_product = self.raw_product(response)
+        raw_images = [raw_product['selectedProductColorVariation']] \
+                     + raw_product['otherProductColorVariation']
+        images = [img['bigImageUrl'] for raw_img in raw_images for img in raw_img['images']]
         return images
 
     def skus(self, response):
         skus = {}
 
-        raw_product_css = 'script:contains(productInfo)::text'
-        raw_product = JSParser(clean(response.css(raw_product_css))[0])['productInfo']
-
+        raw_product = self.raw_product(response)
         product_id = raw_product['productId']
 
         raw_prices_re = f'SITE.data.productPrices\["{product_id}"] = SITE.dataImporter\((.*?)\);'
         raw_prices = json.loads(response.css('script').re_first(raw_prices_re))
-
         currency = raw_prices['currencyCode']
 
-        colors_map = {
-            clean(raw_product['selectedProductColorVariation']['description']):
-                raw_product['selectedProductColorVariation']['colorId']
-        }
-        for each in raw_product['otherProductColorVariation']:
-            colors_map[clean(each['description'])] = each['colorId']
+        color_price_map = {}
+        for color in raw_prices['colours']:
+            color_price_map[str(color['colourId'])] = [
+                color['rrpPrice'], color['standardPrice'], color['sellPrice'], currency]
 
-        for raw_size in raw_product['selectedProductColorVariation']['sizes']:
-            sku_id = raw_size['sku']
-            sku = {}
-            sku['size'] = raw_size['code']
-            sku['colour'] = clean(raw_product['selectedProductColorVariation']['description'])
-
-            if not raw_size['active']:
-                sku['out_of_stock'] = True
-
-            skus[sku_id] = sku
-
-        for variation in raw_product['otherProductColorVariation']:
-            for raw_size in variation['sizes']:
+        for raw_sku in [raw_product['selectedProductColorVariation']] \
+                       + raw_product['otherProductColorVariation']:
+            for raw_size in raw_sku['sizes']:
                 sku_id = raw_size['sku']
-                sku = {}
+                sku = self.product_pricing_common(response, color_price_map[raw_sku['colorId']])
                 sku['size'] = raw_size['code']
-                sku['colour'] = clean(variation['description'])
+                sku['colour'] = clean(raw_sku['description'])
 
                 if not raw_size['active']:
                     sku['out_of_stock'] = True
 
                 skus[sku_id] = sku
-
-        for _, sku in skus.items():
-            for color in raw_prices['colours']:
-                if str(color['colourId']) == colors_map[sku['colour']]:
-                    money_str = [
-                        color['rrpPrice'], color['standardPrice'], color['sellPrice'], currency
-                    ]
-                    sku.update(self.product_pricing_common(response, money_str))
-                    break
 
         return skus
 
@@ -137,32 +112,28 @@ class CotswoldCrawlSpider(BaseCrawlSpider):
                            'page={}&size=48&platform=public_site&filter={}'
 
     def parse(self, response):
-        yield from super().parse(response)
-
         category_filter = response.css('script').re_first('"defaultSearchFilter":"(.*?)",')
         listing_url = self.listing_url_template.format(0, category_filter)
 
         meta = response.meta.copy()
         meta['trail'] = self.add_trail(response)
-        meta['filter'] = category_filter
         yield Request(url=listing_url, callback=self.parse_listings, meta=meta)
 
     def parse_listings(self, response):
         yield from self.product_requests(response)
 
+        if url_query_parameter(response.url, 'page') != '0':
+            return
+
         listing = json.loads(response.text)
         total_pages = listing['totalPages']
-        category_filter = response.meta['filter']
 
         meta = response.meta.copy()
         meta['trail'] = self.add_trail(response)
 
         for page_index in range(1, total_pages):
-            pagination_url = self.listing_url_template.format(page_index, category_filter)
-            yield Request(url=pagination_url, callback=self.parse_products, meta=meta)
-
-    def parse_products(self, response):
-        yield from self.product_requests(response)
+            pagination_url = add_or_replace_parameter(response.url, 'page', page_index)
+            yield Request(url=pagination_url, callback=self.parse_listings, meta=meta.copy())
 
     def product_requests(self, response):
         listing = json.loads(response.text)
@@ -172,10 +143,11 @@ class CotswoldCrawlSpider(BaseCrawlSpider):
         meta['trail'] = self.add_trail(response)
 
         for item in listing['items']:
-            if item['impression']['category'].lower() not in ['toys']:
-                product_url = f"/p{item['seoUrl']}.html"
-                request = response.follow(product_url, callback=self.parse_item, meta=meta)
-                requests.append(request)
+            if item['impression']['category'].lower() in ['toys']:
+                continue
+            product_url = f"/p{item['seoUrl']}.html"
+            request = response.follow(product_url, callback=self.parse_item, meta=meta.copy())
+            requests.append(request)
 
         return requests
 
