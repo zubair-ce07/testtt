7e98c04f149998736e916c44a3b0699c8639f5b8
Ahmed Buksh <ahmed.buksh@arbisoft.com>
2018-05-17 15:58:33 +0500

    Modifications with argument validation and passing, removal of unnecessary code and some optimizations

--- crawler.py
+++ crawler.py
@@ -8,8 +8,7 @@ from request_parser import RequestParser
 def valid_url_count(url_count):
     if 1 <= int(url_count) <= 100:
         return url_count
-    print ('Invalid URL Count')
-    sys.exit()
+    raise ValueError()
 
 
 def crawler_report(pages, start_time, end_time):
@@ -21,31 +20,41 @@ def crawler_report(pages, start_time, end_time):
     print(f'Time Taken: {end_time - start_time}s \n')
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', type=float, nargs='?', default=0.0, help="Time(ms) for download delay")
-    parser.add_argument('-r', type=int, nargs='?', default=1, help="Number of requests")
-    parser.add_argument('-l', type=valid_url_count, nargs='?', default=5, help="Max URL to crawl")
-    args = parser.parse_args()
-    request_parser = RequestParser('https://www.jacklemkus.com/', args.l, args.r, args.d)
-
+def recursive_concurent_crawler(request_parser):
     start_time = time.time()
     print('Recursive Concurrent Crawling')
     request_parser.recursive_concurent_crawler()
     end_time = time.time()
     crawler_report(request_parser.traversed_pages, start_time, end_time)
 
+
+def recursive_parallel_crawler(request_parser):
     start_time = time.time()
     print('Recursive Parallel Crawling')
     request_parser.recursive_parallel_crawler()
     end_time = time.time()
     crawler_report(request_parser.traversed_pages, start_time, end_time)
 
+
+def iterative_concurrent_crawler(request_parser):
     start_time = time.time()
     print('Ietrative Concurent Crawling')
     request_parser.iterative_concurrent_crawl()
     end_time = time.time()
     crawler_report(request_parser.traversed_pages, start_time, end_time)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url')
+    parser.add_argument('-d', type=float, nargs='?', default=0.0, help="Time(ms) for download delay")
+    parser.add_argument('-r', type=int, nargs='?', default=1, help="Number of requests")
+    parser.add_argument('-l', type=valid_url_count, nargs='?', default=5, help="Max URL to crawl")
+    args = parser.parse_args()
+    request_parser = RequestParser(args.url, int(args.l), int(args.r), int(args.d))
+    recursive_concurent_crawler(request_parser)
+    recursive_parallel_crawler(request_parser)
+    iterative_concurrent_crawler(request_parser)
 
 
 if __name__ == '__main__':
--- request_parser.py
+++ request_parser.py
@@ -11,7 +11,7 @@ from multiprocessing.pool import ThreadPool
 from parsel import Selector
 
 
-PageInfo = namedtuple('Page', ('page_url', 'page_size'))
+PageDetails = namedtuple('Page', ('page_url', 'page_size'))
 
 
 class RequestParser():
@@ -28,7 +28,7 @@ class RequestParser():
         request = requests.get(url)
         selector = Selector(request.text)
         page_urls = list(set(selector.css(f'a[href^="{self.base_url}"]::attr(href)').extract()))
-        self.traversed_pages += [PageInfo(url, len(request.content))]
+        self.traversed_pages += [PageDetails(url, len(request.content))]
         for value in page_urls:
             self.url_queue.put(value)
         await asyncio.sleep(self.download_delay)
@@ -38,9 +38,9 @@ class RequestParser():
         self.url_queue.put(self.base_url)
         loop = asyncio.get_event_loop()
 
-        while (not self.url_queue.empty() and len(self.traversed_pages) < int(self.url_limit)):
+        while (not self.url_queue.empty() and len(self.traversed_pages) < self.url_limit):
             url = self.url_queue.get()
-            if url in [value.page_url for value in self.traversed_pages]:
+            if any(value == url for value in [value.page_url for value in self.traversed_pages]):
                 continue
             loop.run_until_complete(self.read_url(url))
         loop.close()
@@ -55,33 +55,29 @@ class RequestParser():
         self.traversed_pages = []
         traversed_url = self.crawl_parallel(self.base_url)
 
-    async def crawl_conccurently(self, base_url, traversed_url_count=1):
-        if traversed_url_count > int(self.url_limit):
-            return traversed_url_count
+    async def crawl_conccurently(self, base_url):
+        if len(self.traversed_pages) >= self.url_limit:
+            return
         request = requests.get(base_url)
         selector = Selector(request.text)
         page_urls = selector.css(f'a[href^="{self.base_url}"]::attr(href)').extract()
-        self.traversed_pages += [PageInfo(base_url, len(request.content))]
+        self.traversed_pages += [PageDetails(base_url, len(request.content))]
         for url in page_urls:
-            if url in [value.page_url for value in self.traversed_pages]:
+            if any(value == url for value in [value.page_url for value in self.traversed_pages]):
                 continue
-            traversed_url_count += 1
-            traversed_url_count = await self.crawl_conccurently(url, traversed_url_count)
+            await self.crawl_conccurently(url)
         await asyncio.sleep(self.download_delay)
-        return traversed_url_count
 
-    def crawl_parallel(self, base_url, traversed_url_count=1):
-        if traversed_url_count > int(self.url_limit):
-            return traversed_url_count
+    def crawl_parallel(self, base_url):
+        if len(self.traversed_pages) >= self.url_limit:
+            return
         request = requests.get(base_url)
-        self.traversed_pages += [PageInfo(base_url, len(request.content))]
+        self.traversed_pages += [PageDetails(base_url, len(request.content))]
         executor = ThreadPoolExecutor(max_workers=self.number_of_requests)
         selector = Selector(request.text)
         page_urls = selector.css(f'a[href^="{self.base_url}"]::attr(href)').extract()
         for url in page_urls:
-            if url in [value.page_url for value in self.traversed_pages]:
+            if any(value == url for value in [value.page_url for value in self.traversed_pages]):
                 continue
-            traversed_url_count += 1
-            future = executor.submit(self.crawl_parallel, url, traversed_url_count)
-            traversed_url_count = future.result()
-        return traversed_url_count
+            future = executor.submit(self.crawl_parallel, url)
+        return future.result()
