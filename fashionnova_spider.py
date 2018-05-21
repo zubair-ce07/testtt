b8b2cecb1c440f247b65d984a7f7033f79d9256b
Hassaan Ahmad Farooqi <hassaan.ahmad@arbisoft.com>
2018-05-21 13:13:04 +0500

    QA issues resolved 2.0

--- fashionnova_spider.py
+++ fashionnova_spider.py
@@ -3,7 +3,7 @@ import re
 
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Request, Rule
-from w3lib.url import urlsplit, urljoin, url_query_parameter, add_or_replace_parameter
+from w3lib.url import url_query_parameter, url_query_cleaner, add_or_replace_parameter
 from math import ceil
 
 from skuscraper.parsers.jsparser import JSParser
@@ -12,10 +12,7 @@ from .base import BaseParseSpider, BaseCrawlSpider, clean
 
 class Mixin:
     retailer = 'fashionnova'
-    allowed_domains = [
-        'fashionnova.com',
-        'ultimate-dot-acp-magento.appspot.com'
-    ]
+    allowed_domains = ['fashionnova.com']
     start_urls = ['https://www.fashionnova.com/']
     default_brand = 'Fashion Nova'
     gender = 'women'
@@ -38,6 +35,7 @@ class FashionNovaParseSpider(BaseParseSpider):
             return
 
         self.boilerplate_normal(garment, response)
+        self.boilerplate_minimal(garment, response, response.url)
 
         garment['merch_info'] = self.merch_info([garment['name']] + garment['description'])
         garment['image_urls'] = self.image_urls(response)
@@ -82,12 +80,16 @@ class FashionNovaParseSpider(BaseParseSpider):
         raw_product = JSParser(clean(response.css(css))[0])['json_product']
         base_sku = self.product_pricing_common(response)
 
+        raw_color = [tag for tag in raw_product['tags'] if tag.find('Color-') > -1]
+        color = re.sub(r'(\w)([A-Z])', r"\1 \2", raw_color[0].strip('Color-')) if raw_color else ''
+
         for raw_sku in raw_product['variants']:
             sku_id = raw_sku['id']
             sku = base_sku.copy()
             size = raw_sku['title']
             sku['size'] = self.one_size if size == 'OS' else size
-            sku['colour'] = re.search('- (.*?) -', raw_sku['name']).group(1)
+            colour = re.search('- (.*?) -', raw_sku['name'])
+            sku['colour'] = colour.group(1) if colour else color
 
             if not raw_sku['available']:
                 sku['out_of_stock'] = True
@@ -99,58 +101,35 @@ class FashionNovaParseSpider(BaseParseSpider):
 
 class FashionNovaCrawlSpider(BaseCrawlSpider):
     listings_css = '.main-menu'
-    listings_url = 'https://ultimate-dot-acp-magento.appspot.com/categories_navigation?q='
-    page_size = 32
+    products_css = '.product-item .info'
+
+    page_size = 50
 
     deny_r = ['pages']
 
-    rules = [Rule(LinkExtractor(restrict_css=listings_css, deny=deny_r), callback='parse')]
+    rules = [
+        Rule(LinkExtractor(restrict_css=listings_css, deny=deny_r), callback='parse'),
+        Rule(LinkExtractor(restrict_css=products_css, process_value=url_query_cleaner),
+             callback='parse_item'),
+    ]
 
     def parse(self, response):
         yield from super().parse(response)
 
-        category_path = urlsplit(response.url).path
-        link_for_uuid = clean(response.xpath('//script[contains(@src, "UUID")]/@src'))[0]
-        uuid = url_query_parameter(link_for_uuid, 'UUID')
-
-        url = add_or_replace_parameter(self.listings_url, 'UUID', uuid)
-        url = add_or_replace_parameter(url, 'page_num', 1)
-        listing_url = add_or_replace_parameter(url, 'category_url', category_path)
-
-        meta = response.meta.copy()
-        meta['trail'] = self.add_trail(response)
-        yield Request(url=listing_url, callback=self.parse_listings, meta=meta)
-
-    def parse_listings(self, response):
-        yield from self.product_requests(response)
-
-        if url_query_parameter(response.url, 'page_num') != '1':
+        if url_query_parameter(response.url, 'page'):
             return
 
-        listing = json.loads(response.text)
-        total_products = listing['total_results']
+        total_products = response.css('.total-items::text').re_first('(\d+) styles') or '0'
+        total_products = int(total_products)
         total_pages = ceil(total_products / self.page_size)
 
         meta = response.meta.copy()
         meta['trail'] = self.add_trail(response)
 
         for page_index in range(2, total_pages + 1):
-            pagination_url = add_or_replace_parameter(response.url, 'page_num', page_index)
-            yield Request(url=pagination_url, callback=self.parse_listings, meta=meta.copy())
-
-    def product_requests(self, response):
-        listing = json.loads(response.text)
-        requests = []
-
-        meta = response.meta.copy()
-        meta['trail'] = self.add_trail(response)
-
-        for item in listing['items']:
-            product_url = urljoin(self.start_urls[0], item['u'])
-            request = Request(url=product_url, callback=self.parse_item, meta=meta.copy())
-            requests.append(request)
-
-        return requests
+            pagination_url = add_or_replace_parameter(response.url, 'page', page_index)
+            request = Request(url=pagination_url, callback=self.parse, meta=meta.copy())
+            yield request
 
 
 class FashionNovaUSParseSpider(MixinUS, FashionNovaParseSpider):
