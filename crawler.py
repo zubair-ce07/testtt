8cc134ca4902f091ab3b91f54c922e7094bd8577
Farwa Shahid <farwa.shahid@arbisoft.com>
2018-05-21 15:28:24 +0500

    Code Modifications
    Divide complex loops into sub functions
    user Selector from parsel instead of BeautifulSoup
    Move main into a class
    use 'not' operator instead of len()

--- crawler.py
+++ crawler.py
@@ -3,54 +3,65 @@ from bs4 import BeautifulSoup
 import datetime
 from file_handler import FileHandler
 from url_generator import UrlGenerator
+import parsel
 
-if __name__ == '__main__':
+class WeatherCrawler:
 
-    # City code, change this code to get data from other cities
-    city_code = 'OPLA'
-    
-    url_list = UrlGenerator.get_dynamic_url_list(city_code)
-    fields_name = ['PKT','High TempC','Average TempC','Low TempC','High Dew Point','Average Dew Point','Low Dew Point','High Humidity','Average Humidity','Low Humidity','High Sea Level Pressure','Average Sea Level Pressure','Low Sea Level Pressure','High VisibilityKM','Average VisibilityKM','Low VisibilityKM','High WindKMH','Average WindKMH','Low WindKMH','Precipitaion mm','Events']
+    #This function formats data in proper dictionary format
+    def data_formatter(year, month, row_list, fields_name):
+        #first index will have date, format the date properly.
+        date_string = f"{year}/{month}/{row_list[0]}"
+        date = datetime.datetime.strptime(date_string, "%Y/%B/%d").strftime("%Y-%m-%d")
+        row_list[0] = date
+        
+        #Convert the list to dictionary to use with csv DictWriter
+        dictionary = {}
+        for index in range(0, len(fields_name)):
+            dictionary[fields_name[index]] = row_list[index]
+        return dictionary
 
-    for url in url_list:
-        code = requests.get(url)
-        html_data = BeautifulSoup(code.text, "html.parser")
-        table_data = html_data.find('table', {'id':'obsTable'})
+    # This function accepts month and year
+    # and returns file name for that month in a specified format
+    def file_name_formatter(year, month):
+        file_month = datetime.datetime.strptime(f"{year}/{month}", "%Y/%B").strftime("%Y_%b")
+        file_name = f"WeatherFiles/Lahore_Weather_{file_month}.txt"
+        return file_name
 
-        # Month and year for file name and PKT format
-        month = html_data.find('select', {'class':'month form-select'}).find('option', {'selected':'selected'}).getText()
-        year = html_data.find('select', {'class':'year form-select'}).find('option', {'selected':'selected'}).getText()
 
-        if table_data:
-            table_body = table_data.findAll('tbody')
-            data_list = []
+    if __name__ == '__main__':
+        # City code, change this code to get data for other cities
+        city_code = 'OPLA'
+        
+        url_list = UrlGenerator.get_dynamic_url_list(city_code)
+        fields_name = ['PKT','High TempC','Average TempC','Low TempC','High Dew Point','Average Dew Point','Low Dew Point','High Humidity','Average Humidity','Low Humidity','High Sea Level Pressure','Average Sea Level Pressure','Low Sea Level Pressure','High VisibilityKM','Average VisibilityKM','Low VisibilityKM','High WindKMH','Average WindKMH','Low WindKMH','Precipitaion mm','Events']
 
-            for body in table_body[1:]:
-                for row in body.select('tr'):
-                    row_list = []
-                    for td in row.findAll('td'):
-                        if (td.find('span' , {'class':'wx-value'})):
-                            row_list.append(td.find('span' , {'class':'wx-value'}).getText())
-                        else:
-                            row_list.append(td.getText().strip().replace('\n','').replace('\t', ''))
-
-                    #first index will have date, format the date properly.
-                    date_string = f"{year}/{month}/{row_list[0]}"
-                    date = datetime.datetime.strptime(date_string, "%Y/%B/%d").strftime("%Y-%m-%d")
-                    row_list[0] = date
-
-                    # Convert the appended list to dictionary to use DictWriter
-                    dictionary = {}
-                    if len(row_list) != 0:
-                        for index in range(0, len(fields_name)):
-                            dictionary[fields_name[index]] = row_list[index]
-                
-                    data_list.append(dictionary)
+        for url in url_list:
+            code = requests.get(url)
+            parser = parsel.Selector(code.text)
             
-            # File name in specified format
-            file_month = datetime.datetime.strptime(f"{year}/{month}", "%Y/%B").strftime("%Y_%b")
-            file_name = f"WeatherFiles/Lahore_Weather_{file_month}.txt"
+            # Month and year for file name and PKT format
+            month = parser.css('select.month').css('option[selected=selected]::text').extract_first()
+            year = parser.css('select.year').css('option[selected=selected]::text').extract_first()
+            
+            table = parser.css('table.obs-table')
 
-            FileHandler.file_writer(file_name, fields_name, data_list)
-        else:
-            print(f"Data not found for {month} {year}")
+            if table:
+                tableBody = table.css('tbody')
+                data_list = []
+            
+                for tbody in tableBody[1:]:
+                    row_list = []
+                    for value in tbody.css('tr').css('td'):
+                        if (value.css('span.wx-value::text').extract_first()):
+                            row_list.append(value.css('span.wx-value::text').extract_first())
+                        else:
+                            row_list.append(value.xpath('.//text()').extract_first().strip().replace('\n','').replace('\t', ''))
+                    
+                    if row_list:
+                        dictionary = data_formatter(year, month, row_list, fields_name)
+                    
+                    data_list.append(dictionary)
+                    file_name = file_name_formatter(year, month)
+                    FileHandler.file_writer(file_name, fields_name, data_list)
+            else:
+                print(f"Data not found for {month} {year}")
