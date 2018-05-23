793a3adeb83b2021947c926c8836957ac3fb8e3b
Farwa Shahid <farwa.shahid@arbisoft.com>
2018-05-23 10:11:24 +0500

    Use yield, rename variables and remove extra code
    Define constants at class level
    Give meaningful names to variables
    Remove extra code
    Append _s with selectors
    Use yeild statement

--- crawler.py
+++ crawler.py
@@ -1,64 +1,61 @@
 import requests
 import datetime
+import parsel
 from file_handler import FileHandler
 from url_generator import UrlGenerator
-import parsel
+
 
 class WeatherCrawler:
+    # City code, change this code to get data for other cities
+    CITY_CODE = "OPLA"
+    FIELD_NAMES = ['PKT','High TempC','Average TempC','Low TempC','High Dew Point','Average Dew Point','Low Dew Point','High Humidity','Average Humidity','Low Humidity','High Sea Level Pressure','Average Sea Level Pressure','Low Sea Level Pressure','High VisibilityKM','Average VisibilityKM','Low VisibilityKM','High WindKMH','Average WindKMH','Low WindKMH','Precipitaion mm','Events']
 
-    #This function formats data in proper dictionary format
-    @staticmethod
-    def data_formatter(year, month, row_list, fields_name):
+    #This function formats data in proper dictionary format    
+    def data_formatter(year, month, row_list):
         #first index will have date, format the date properly.
         date_string = f"{year}/{month}/{row_list[0]}"
         date = datetime.datetime.strptime(date_string, "%Y/%B/%d").strftime("%Y-%m-%d")
         row_list[0] = date
         
         #Convert the list to dictionary to use with csv DictWriter
-        return dict(zip(fields_name, row_list))
+        return dict(zip(WeatherCrawler.FIELD_NAMES, row_list))
 
     # This function accepts month and year
     # and returns file name for that month in a specified format
-    @staticmethod
     def file_name_formatter(year, month):
         file_month = datetime.datetime.strptime(f"{year}/{month}", "%Y/%B").strftime("%Y_%b")
         file_name = f"WeatherFiles/Lahore_Weather_{file_month}.txt"
         return file_name
 
-
-    @staticmethod
     def weather_crawler() :
-        # City code, change this code to get data for other cities
-        city_code = 'OPLA'
+        url_list = UrlGenerator.get_dynamic_url_list(WeatherCrawler.CITY_CODE)
         
-        url_list = UrlGenerator.get_dynamic_url_list(city_code)
-        fields_name = ['PKT','High TempC','Average TempC','Low TempC','High Dew Point','Average Dew Point','Low Dew Point','High Humidity','Average Humidity','Low Humidity','High Sea Level Pressure','Average Sea Level Pressure','Low Sea Level Pressure','High VisibilityKM','Average VisibilityKM','Low VisibilityKM','High WindKMH','Average WindKMH','Low WindKMH','Precipitaion mm','Events']
-
         for url in url_list:
-            code = requests.get(url)
-            parser = parsel.Selector(code.text)
+            response = requests.get(url)
+            parser_s = parsel.Selector(response.text)
             # Month and year for file name and PKT format
-            month = parser.css('select.month > option[selected=selected]::text').extract_first()
-            year = parser.css('select.year > option[selected=selected]::text').extract_first()
+            month = parser_s.css('select.month > option[selected=selected]::text').extract_first()
+            year = parser_s.css('select.year > option[selected=selected]::text').extract_first()
             
-            table = parser.css('table.obs-table > tbody')[1:]
+            tablebody_s = parser_s.css('table.obs-table > tbody')[1:]
             data_list = []
             
-            for row in table.css('tr'):
+            for row_s in tablebody_s:
                 row_list = []
-                for value in row.css('td'):
-                    span_text = value.css('span.wx-value::text').extract_first()
+                for td_s in row_s.css('tr > td'):
+                    span_text = td_s.css('span.wx-value::text').extract_first()
                     if (span_text):
                         row_list.append(span_text)
                     else:
-                        row_list.append(value.xpath('.//text()').extract_first().strip().replace('\n','').replace('\t', ''))
+                        row_list.append(td_s.xpath('.//text()').extract_first()
+                                .strip().replace('\n','').replace('\t', ''))
                         
                 if row_list:
-                    dictionary = WeatherCrawler.data_formatter(year, month, row_list, fields_name)
+                    dictionary = WeatherCrawler.data_formatter(year, month, row_list)
                     data_list.append(dictionary)
 
             file_name = WeatherCrawler.file_name_formatter(year, month)
-            FileHandler.file_writer(file_name, fields_name, data_list)
+            FileHandler.file_writer(file_name, WeatherCrawler.FIELD_NAMES, data_list)
         
 if __name__ == '__main__':
     WeatherCrawler.weather_crawler()
--- url_generator.py
+++ url_generator.py
@@ -8,22 +8,15 @@ class UrlGenerator:
     @staticmethod
     def get_year_range_from_dropdown(city_code):
         url = f"https://www.wunderground.com/history/airport/{city_code}/1996/1/1/MonthlyHistory.html?req_city=&req_state=&req_statename=&reqdb.zip=&reqdb.magic=&reqdb.wmo="
-        code = requests.get(url)
-        parser = parsel.Selector(code.text)
-        year_list = []
-        for option in parser.css('select.year > option::text').extract():
-            year_list.append(option)
-
-        print(year_list)
-        return year_list
+        response = requests.get(url)
+        parser_s = parsel.Selector(response.text)
+        for option in parser_s.css('select.year > option::text').extract():
+            yield option
 
     # The function accepts list of years with city code and returns a list of all URLs
     @staticmethod
     def get_dynamic_url_list(city_code):
         years_range = UrlGenerator.get_year_range_from_dropdown(city_code)
-        url_list = []
         for year in years_range:
             for month in range(1, 13):
-                url = f"https://www.wunderground.com/history/airport/{city_code}/{year}/{month}/1/MonthlyHistory.html?req_city=&req_state=&req_statename=&reqdb.zip=&reqdb.magic=&reqdb.wmo="
-                url_list.append(url)
-        return url_list
+                yield f"https://www.wunderground.com/history/airport/{city_code}/{year}/{month}/1/MonthlyHistory.html?req_city=&req_state=&req_statename=&reqdb.zip=&reqdb.magic=&reqdb.wmo="
