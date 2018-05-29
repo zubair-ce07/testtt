812c6a8e152239ca1a4ae22687d06bde89ff2c35
Farwa Shahid <farwa.shahid@arbisoft.com>
2018-05-29 15:37:20 +0500

    PEP-8 issues, proper functions naming conventions
    Pass respnse instead of selectors
    Make single function for price and skus
    Use proper naming conventions
    Solve PEP-8 issues
    Remove checks and iterations on length
    Use regex to clean price list

--- schutz.py
+++ schutz.py
@@ -4,62 +4,133 @@ import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.selector import Selector
-from utils import Utils
+import re
+
+
 class SchutzSpider(CrawlSpider):
-    name = 'SchutzSpider'
+    name = 'schutzspider'
     allowed_domains = ['schutz.com.br']
-    start_urls = ['https://schutz.com.br/store']
+    start_urls = ['https://schutz.com.br/store/']
+    xpaths = ['//div[@class="sch-main-menu-sub-links-left"]',
+                 '//ul[@class="pagination"]/li[@class="next"]']
 
     # Follow any link scrapy finds (that is allowed and matches the patterns).
     rules = [Rule(LinkExtractor(
-                restrict_xpaths=['//div[@class="sch-main-menu-sub-links-left"]', '//ul[@class="pagination"]/li[@class="next"]']
+                restrict_xpaths=xpaths
                 ), callback='parse'),
                 Rule(LinkExtractor(
                 restrict_xpaths='//a[@class="sch-category-products-item-link"]'
                 ), callback='parse_product', follow=True)]
-     
-    def parse (self, response):
+ 
+    def parse(self, response):
         requests = super(SchutzSpider, self).parse(response)
         for request in requests:
             request.meta['trail'] = ['https://schutz.com/br']
             request.meta['trail'].append(response.url)
             yield request
 
-    def parse_product(self, response):
-        price = Utils.get_price_info(response.css('span.sch-price ::text').extract())
-       
-        size_list = response.xpath('//div[@class="sch-sizes"]/ul/li/label/@class | //div[@class="sch-sizes"]/ul/li/label/text()').extract()
-        if size_list:
-            sku_info = Utils.get_sku_info(size_list, price,
-                                            Utils.get_color_info(response.css('ul.sch-description-list > li')))
-            out_of_stock = Utils.is_out_of_stock(size_list)
-        else:
-            size_list = response.css('div.sch-form-group-select > select > option::text').extract()
-            sku_info = Utils.get_sku_info_from_drop_down(size_list, price,
-                                                        Utils.get_color_info(response.css('ul.sch-description-list > li')))
-            out_of_stock = True
-        
-        category_list = response.css('ul.clearfix > li > a::text').extract()
-        category_list = category_list[1:len(category_list)-1]
+    @staticmethod
+    def clean_price_list(price_list):
+        cleaned_price_list = []
+        for prices in price_list:
+            prices = re.findall('\d+', prices)
+            if prices:
+                cleaned_price_list.append(int(prices[0]) * 100)
+        return cleaned_price_list
 
+    @staticmethod
+    def description(response):
+        description_text = '.sch-description-content p::text'
+        description_list = '.sch-description-list li'
+
+        description = [response.css(description_text).extract_first()]
+        for list_item in response.css(description_list):
+            span_text = list_item.css('span::text').extract_first()
+            strong_text = list_item.css('strong::text').extract_first()
+            description.append(f"{span_text}: {strong_text}")  
+        return description
+
+    @staticmethod
+    def color(description):
+        for list_item in description[1:]:
+            if 'Cor' in list_item:
+                return list_item.split(':')[1]
+        return None
+
+    @staticmethod
+    def care(description):
+        care = []
+        for list_item in description[1:]:
+            if 'Material' in list_item:
+                care.append(list_item)
+        return care
+
+    @staticmethod
+    def prices(response):
+        price_list = response.css('.sch-price ::text').extract()
+        price_list = set(SchutzSpider.clean_price_list(price_list))
+        return sorted(price_list)
+
+    @staticmethod
+    def category(response):
+        category_list = response.css('.clearfix a::text').extract()
+        return category_list[1:-1]
+
+    @staticmethod
+    def sku(response):
+        color_dictionary = {}
+        price = SchutzSpider.prices(response)[0]
+        color = SchutzSpider.color(SchutzSpider.description(response))
+        drop_down_sel = '.sch-notify-form .sch-form-group-select select option'
+        list_sel = '.sch-sizes label'
+        sizes = response.css(list_sel) or response.css(drop_down_sel)
+        for size in sizes:
+            dictionary = {'color': color, 'currencey': 'BRL', 'price': price}
+            size_value = size.css(' ::text').extract_first()
+            dictionary['size'] = size_value
+            if 'sch-avaiable' in size.xpath("@class").extract_first():
+                dictionary['out_of_stock'] = False
+            else:
+                dictionary['out_of_stock'] = True
+            color_dictionary[f"{color}{size_value}"] = dictionary
+        return color_dictionary
+
+    def is_out_of_stock(sku):
+        for key, value in sku.items():
+            if not value['out_of_stock']:
+                return False
+        return True
+
+    def retailer_sku(response):
+        retailer_sku_sel = '.sch-pdp::attr(data-product-code)'
+        return response.css(retailer_sku_sel).extract_first()
+
+    def name(response):
+        name_sel = '.sch-sidebar-product-title::text'
+        return response.css(name_sel).extract_first()
+
+    def trail(response):
         trail = response.meta['trail']
         trail.append(response.url)
-        
+        return trail
+
+    def parse_product(self, response):
+        price = SchutzSpider.prices(response)
+        description = SchutzSpider.description(response)
+        sku = SchutzSpider.sku(response)
+
         yield {
             'brand': 'Schutz',
-            'care': Utils.get_care_info(response.css('ul.sch-description-list > li')),
-            'category': category_list,
+            'care': SchutzSpider.care(description),
+            'category': SchutzSpider.category(response),
             'currency': 'BRL',
-            'description': Utils.get_decription_info(
-                                        response.css('div.sch-description-content > p::text').extract_first(),
-                                        response.css('ul.sch-description-list > li')),
-            'image_urls': response.css('div.is-slider-item > img::attr(src)').extract(),
-            'name': response.css('h1.sch-sidebar-product-title::text').extract_first(),
-            'price': price,
-            'previous-prices': Utils.get_previous_prices(response.css('span.sch-price ::text').extract()),
-            'retailer_sku': response.css('div.sch-pdp::attr(data-product-code)').extract(),
-            'sku':sku_info,
-            'trail':trail,
-            'url':response.url,
-            'out-of-stock':out_of_stock,
+            'description': description,
+            'name': SchutzSpider.name(response),
+            'price': price[0],
+            'previous-prices': price[1:],
+            'retailer_sku': SchutzSpider.retailer_sku(response),
+            'sku': sku,
+            'trail': SchutzSpider.trail(response),
+            'url': response.url,
+            'out-of-stock': SchutzSpider.is_out_of_stock(sku),
         }
