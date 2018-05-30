22b8af06639d595f32027c09bad9ad7a6a091fb5
Ahmed Buksh <ahmed.buksh@arbisoft.com>
2018-05-30 14:34:16 +0500

    Data with json is aquired and removal of unnecessary imports

--- woolrich_spider.py
+++ woolrich_spider.py
@@ -29,28 +29,30 @@ class WoolrichSpider(CrawlSpider):
 
     def parse_product(self, response):
         product = {}
-        product['retailer_sku'] = self.retailer_sku(response)
+        attributes = self.attributes(response)
+        product['retailer_sku'] = self.retailer_sku(attributes)
         product['uuid'] = "null"
         product['language'] = "en"
         product['trail'] = response.meta.get('trail')
         product['url'] = response.url
         product['url_original'] = response.url
-        product['name'] = self.product_name(response)
-        product['price'] = self.product_price(response)
-        product['brand'] = "woolrich"
-        product['market'] = "EU"
-        product['retailer'] = "woolrich-eu"
-        product['category'] = self.product_category(response)
+        product['name'] = self.product_name(attributes)
+        product['price'] = self.price(attributes)
+        product['brand'] = self.brand(attributes)
+        product['language'] = self.language(attributes)
+        product['market'] = self.market(attributes)
+        product['retailer'] = product['brand'] + '-' + product['language']
+        product['category'] = self.category(attributes)
         product['care'] = []
         product['description'] = []
-        self.product_description(response, product['care'], product['description'])
+        self.description(response, product['care'], product['description'])
         product['images'] = self.images(response)
         product['date'] = int(time.time())
-        product['crawl_start_time'] = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
         product['skus'] = []
         product['currency'] = 'POUND'
         product['request_queue'] = queue.Queue()
         product['spider_name'] = "woolrich_spider"
+        product['crawl_start_time'] = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
         colors_name = response.css('.swatch::text').extract()
         colors_link = response.css('.color>.selectable>a::attr(href)').extract()
         for index, value in enumerate(colors_link):
@@ -60,7 +62,11 @@ class WoolrichSpider(CrawlSpider):
 
         yield product['request_queue'].get(request)
 
-    def product_description(self, response, care_list, descp_list):
+    def attributes(self, response):
+        product_description = re.findall("dataLayer.push.apply(.+?);", response.text, re.S)[0][12:-1]
+        return json.loads(product_description)
+
+    def description(self, response, care_list, descp_list):
         description = response.css('.description::text').extract_first()
         if not description:
             return
@@ -70,17 +76,26 @@ class WoolrichSpider(CrawlSpider):
             if value not in care_list:
                 descp_list.append(value)
 
-    def retailer_sku(self, response):
-        return response.css('.sku::attr(skuid)').extract_first()
+    def market(self, attributes):
+        return attributes[1]['country'].upper()
 
-    def product_name(self, response):
-        return response.css('.product-name::text').extract_first()
+    def language(self, attributes):
+        return attributes[1]['language']
 
-    def product_price(self, response):
-        return response.css('.product-sales-price::text').extract_first()[1:]
+    def brand(self, attributes):
+        return attributes[0]['ecommerce']['detail']['products'][0]['brand']
 
-    def product_category(self, response):
-        return response.css('.breadcrumb>a::text').extract()
+    def retailer_sku(self, attributes):
+        return attributes[0]['ecommerce']['detail']['products'][0]['id']
+
+    def product_name(self, attributes):
+        return attributes[0]['ecommerce']['detail']['products'][0]['name']
+
+    def price(self, attributes):
+        return attributes[0]['ecommerce']['detail']['products'][0]['price']
+
+    def category(self, attributes):
+        return attributes[0]['ecommerce']['detail']['products'][0]['category']
 
     def images(self, response):
         return response.css('.productthumbnail>picture>img::attr(src)').extract()
@@ -97,11 +112,13 @@ class WoolrichSpider(CrawlSpider):
     def skus_maker(self, response):
         size_info = response.css('.size>.selected>a::text').extract_first().strip()
         stock_info = response.css('.in-stock-msg::text').extract_first() or 'Out of Stock'
+        skus_id = response.meta.get('color').replace(" ", "-").lower() + '_' + size_info.replace(" ","-").lower()
         response.meta['product']['skus'].append({'size': size_info,
                                                  'color': response.meta.get('color'),
                                                  'price': response.meta.get('product')['price'],
                                                  'currency': response.meta.get('product')['currency'],
-                                                 'stock_status': stock_info}) 
+                                                 'stock_status': stock_info,
+                                                 'skus_id': skus_id}) 
         if response.meta.get('product')['request_queue'].qsize() > 0:
             yield response.meta.get('product')['request_queue'].get()
         else:
