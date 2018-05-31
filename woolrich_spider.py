7fd8f1dde70bb1c9a1c949fbb26a3c5f511d203e
Ahmed Buksh <ahmed.buksh@arbisoft.com>
2018-05-31 11:30:59 +0500

    Removal of queue, rules for pagination and other conventions changes

--- woolrich_spider.py
+++ woolrich_spider.py
@@ -1,6 +1,5 @@
 import datetime
 import json
-import queue
 import re
 import scrapy
 import time
@@ -13,114 +12,121 @@ class WoolrichSpider(CrawlSpider):
     name = 'woolrich_spider'
     allowed_domains = ['woolrich.eu']
     start_urls = ['http://www.woolrich.eu/en/gb/home']
-    rules = [Rule(LinkExtractor(allow=(r'.*/en/.*'), restrict_css='.menu-category'),
+    allowed_url = r'.*/en/.*'
+    rules = [Rule(LinkExtractor(allow=(allowed_url), restrict_css=['.menu-category', '.ajax-loader']),
                                 callback='parse', follow=True),
-             Rule(LinkExtractor(allow=(r'.*/en/.*'), restrict_css='.product-name'),
+             Rule(LinkExtractor(allow=(allowed_url), restrict_css='.product-name'),
                                 callback='parse_product')]
     care = ['polyster', 'cotton', 'silk', 'fabric', 'wash']
 
     def parse(self, response):
+        page_title = response.css('title::text').extract_first()
         trail = response.meta.get('trail') or []
-        for request in super().parse(response):
-            page_title = response.css('title::text').extract_first()
+        trail.append([page_title, response.url])
+        for request in super().parse(response):    
             request.meta['trail'] = trail.copy()
-            request.meta['trail'].append([page_title, response.url])
             yield request
 
     def parse_product(self, response):
         product = {}
-        attributes = self.attributes(response)
-        product['retailer_sku'] = self.retailer_sku(attributes)
+        raw_product = self.raw_products(response)
+        product['retailer_sku'] = self.retailer_sku(raw_product)
         product['uuid'] = "null"
         product['language'] = "en"
         product['trail'] = response.meta.get('trail')
         product['url'] = response.url
         product['url_original'] = response.url
-        product['name'] = self.product_name(attributes)
-        product['price'] = self.price(attributes)
-        product['brand'] = self.brand(attributes)
-        product['language'] = self.language(attributes)
-        product['market'] = self.market(attributes)
+        product['name'] = self.product_name(raw_product)
+        product['price'] = self.price(raw_product)
+        product['brand'] = self.brand(raw_product)
+        product['market'] = "GB"
         product['retailer'] = product['brand'] + '-' + product['language']
-        product['category'] = self.category(attributes)
-        product['care'] = []
-        product['description'] = []
-        self.description(response, product['care'], product['description'])
+        product['category'] = self.category(raw_product)
+        product['care'], product['description'] = self.description(response)
         product['images'] = self.images(response)
         product['date'] = int(time.time())
         product['skus'] = []
-        product['currency'] = 'POUND'
-        product['request_queue'] = queue.Queue()
-        product['spider_name'] = "woolrich_spider"
+        product['currency'] = "POUND"
+        product['spider_name'] = self.name
         product['crawl_start_time'] = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-        colors_name = response.css('.swatch::text').extract()
-        colors_link = response.css('.color>.selectable>a::attr(href)').extract()
-        for index, value in enumerate(colors_link):
-            request = scrapy.Request(value, meta={'color': colors_name[index].strip(), 'product': product},
-                                     callback=self.sizes)
-            product['request_queue'].put(request)
+        product['request_queue'] = self.color_requests(response, product)
+        
+        if product['request_queue']:
+            yield product['request_queue'].pop()
+        else:
+            del response.meta.get('product')['request_queue']
+            yield product
 
-        yield product['request_queue'].get(request)
+    def brand(self, attributes):
+        return attributes['brand']
 
-    def attributes(self, response):
+    def retailer_sku(self, attributes):
+        return attributes['id']
+
+    def product_name(self, attributes):
+        return attributes['name']
+
+    def price(self, attributes):
+        return attributes['price']
+
+    def category(self, attributes):
+        return attributes['category']
+
+    def images(self, response):
+        return response.css('.productthumbnail>picture>img::attr(src)').extract()
+    
+    def raw_products(self, response):
         product_description = re.findall("dataLayer.push.apply(.+?);", response.text, re.S)[0][12:-1]
-        return json.loads(product_description)
+        product_description = json.loads(product_description)
+        return product_description[0]['ecommerce']['detail']['products'][0]
 
-    def description(self, response, care_list, descp_list):
+    def description(self, response):
+        care_list = []
+        descp_list = []
         description = response.css('.description::text').extract_first()
         if not description:
-            return
+            return [], []
         description = list(filter(str.strip, description.strip().split('.')))
         for value in description:
             care_list.extend([value for care in self.care if care in value])
             if value not in care_list:
                 descp_list.append(value)
+        return care_list, descp_list
 
-    def market(self, attributes):
-        return attributes[1]['country'].upper()
+    def color_requests(self, response, product):
+        colors_link = response.css('.color>.selectable>a::attr(href)').extract()
+        requests = [scrapy.Request(value, meta={'product': product}, callback=self.parse_color)
+                    for value in colors_link]
+        return requests
 
-    def language(self, attributes):
-        return attributes[1]['language']
+    def parse_color(self, response):
+        response.meta['product']['request_queue'] = self.size_requests(response, response.meta.get('product'))
+        if response.meta.get('product')['request_queue']:
+            yield response.meta['product']['request_queue'].pop()
+        else:
+            del response.meta.get('product')['request_queue']
+            yield response.meta.get('product')
 
-    def brand(self, attributes):
-        return attributes[0]['ecommerce']['detail']['products'][0]['brand']
-
-    def retailer_sku(self, attributes):
-        return attributes[0]['ecommerce']['detail']['products'][0]['id']
-
-    def product_name(self, attributes):
-        return attributes[0]['ecommerce']['detail']['products'][0]['name']
-
-    def price(self, attributes):
-        return attributes[0]['ecommerce']['detail']['products'][0]['price']
-
-    def category(self, attributes):
-        return attributes[0]['ecommerce']['detail']['products'][0]['category']
-
-    def images(self, response):
-        return response.css('.productthumbnail>picture>img::attr(src)').extract()
-
-    def sizes(self, response):
+    def size_requests(self, response, product):
+        request_queue = product['request_queue']
         sizes_links = response.css('.size>.selectable>a::attr(href)').extract()
-        for link in sizes_links:
-            request = scrapy.Request(link, meta={'color': response.meta.get('color'),
-                                                 'product': response.meta.get('product')},
-                                                  callback=self.skus_maker)
-            response.meta['product']['request_queue'].put(request)
-        yield response.meta.get('product')['request_queue'].get()
+        request_queue.extend([scrapy.Request(link, meta={'product': product}, callback=self.parse_size)
+                              for link in sizes_links])
+        return request_queue
 
-    def skus_maker(self, response):
+    def parse_size(self, response):
         size_info = response.css('.size>.selected>a::text').extract_first().strip()
         stock_info = response.css('.in-stock-msg::text').extract_first() or 'Out of Stock'
-        skus_id = response.meta.get('color').replace(" ", "-").lower() + '_' + size_info.replace(" ","-").lower()
+        color_name = response.css('.color>.selected>a>div::text').extract_first().strip()
+        skus_id = color_name.replace(" ", "-").lower() + '_' + size_info.replace(" ", "-").lower()
         response.meta['product']['skus'].append({'size': size_info,
-                                                 'color': response.meta.get('color'),
+                                                 'color': color_name,
                                                  'price': response.meta.get('product')['price'],
                                                  'currency': response.meta.get('product')['currency'],
                                                  'stock_status': stock_info,
                                                  'skus_id': skus_id}) 
-        if response.meta.get('product')['request_queue'].qsize() > 0:
-            yield response.meta.get('product')['request_queue'].get()
+        if response.meta.get('product')['request_queue']:
+            yield response.meta.get('product')['request_queue'].pop()
         else:
             del response.meta.get('product')['request_queue']
             yield response.meta.get('product') 
