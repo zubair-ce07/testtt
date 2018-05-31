269c92c46c701bd5a9bd6a55e4b5aa855fdd04dc
Ahsan Malik <ahsan.muhammad@arbisoft.com>
2018-05-31 18:11:46 +0500

    limited the lines under 80 characters length

--- weatherman.py
+++ weatherman.py
@@ -1,7 +1,7 @@
 import sys
 import os
 from itertools import islice
-import populate_weather_readings
+import populate_weather_readings as parser
 
 def main():
     given_path=sys.argv[1]
@@ -11,16 +11,18 @@ def main():
             given_date=sys.argv[i].split("/")
             given_year=given_date[0]
             given_month=int(given_date[1])
-            weather_file_parser = populate_weather_readings.WeatherReadingsPopulator()
-            weather_file_parser.list_files(given_path, sys.argv[i-1], given_year, given_month)
+            weather_file_parser = parser.WeatherReadingsPopulator()
+            weather_file_parser.list_files(given_path, sys.argv[i-1],
+                                           given_year, given_month)
             weather_file_parser.populate_weather_readings(given_path)
             print(weather_file_parser.weather_readings)
             print ("*******************************\n")
         elif sys.argv[i] == "-e":
             i+=1
             given_year=sys.argv[i]
-            weather_file_parser = populate_weather_readings.WeatherReadingsPopulator()
-            weather_file_parser.list_files(given_path, sys.argv[i-1], given_year)
+            weather_file_parser = parser.WeatherReadingsPopulator()
+            weather_file_parser.list_files(given_path,
+                                           sys.argv[i-1], given_year)
             weather_file_parser.populate_weather_readings(given_path)
             print(weather_file_parser.weather_readings)
             print ("*******************************\n")
