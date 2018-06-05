d47db45a60ec0bc30a4672f1b0a871d42819314b
Ahmed Buksh <ahmed.buksh@arbisoft.com>
2018-06-05 15:47:29 +0500

    Use of regex modified, functions of base are used and some convention's revision

--- y3_spider.py
+++ y3_spider.py
@@ -2,11 +2,13 @@ import scrapy
 import re
 import json
 
+from ast import literal_eval
 from scrapy.http import Response
 from scrapy.spiders import Rule, Request
 from w3lib.url import add_or_replace_parameter, url_query_cleaner
 
 from .base import BaseParseSpider, BaseCrawlSpider, LinkExtractor, clean
+from skuscraper.parsers.jsparser import JSParser
 
 
 class Mixin:
@@ -16,13 +18,13 @@ class Mixin:
 
 class MixinUS(Mixin):
     retailer = Mixin.retailer + '-us'
-    market = 'us'
-    language = 'ENG'
-    currency = 'POUND'
+    market = 'US'
+    currency = 'GBP'
     start_urls = ['http://store.y-3.com/us']
     
 
-class y3ParseSpider(BaseParseSpider):
+class Y3ParseSpider(BaseParseSpider):
+    price_css = '.prices'
 
     def parse(self, response):
         sku_id = self.sku_id(response)
@@ -34,66 +36,63 @@ class y3ParseSpider(BaseParseSpider):
         self.boilerplate_normal(garment, response)
         garment['gender'] = self.product_gender(response)
         garment['image_urls'] = self.image_urls(response)
-        garment['skus'] = self.skus_extractor(response)
+        garment['skus'] = self.skus(response)
 
         yield garment
 
     def sku_id(self, response):
-        sku_id = re.findall("var jsinit_doubleclick_config=(.+?);", response.text, re.S)[0]
-        sku_id = json.loads(sku_id.replace("'", '"'))
-        return sku_id['sku']
+        sku_id = clean(response.xpath('//script//text()').re("'sku'\s:\s(.*)"))
+        return sku_id[0]
 
     def product_gender(self, response):
-        gender = re.findall("jsinit_commander=(.+?);", response.text, re.S)[0]
-        gender = json.loads(gender)
-        return self.detect_gender(gender['GENDER'])
+        gender = clean(response.xpath('//script//text()').re('"GENDER":\s"(\w+)"'))
+        return self.gender_lookup(gender[1])
 
-    def skus_extractor(self, response):
+    def skus(self, response):
         skus = {}
         sizes = self.sizes(response)
         color = self.color(response)
 
+        previous_price, price, currency = self.product_pricing(response)
+
+        common_sku = {'color': color,
+                      'out_of_stock': self.out_of_stock(response),
+                      'price': price,
+                      'previous_price': previous_price[0],
+                      'currency': currency}
+
         if sizes:
             for size in sizes:
-                skus[color.lower() + '-' + size.lower() if color else size] = self.skus(response, color, size)
+                if self.out_of_stock(response):
+                    common_sku['out_of_stock'] = True
+                common_sku['size'] = size
+                skus[color.lower() + '-' + size.lower() if color else size] = common_sku 
         else:
-            skus[color.lower()] = self.skus(response, color)
+            skus[color.lower()] = common_sku
 
         return skus
 
-    def skus(self, response, color=None, size=None):
-        stock_status = self.stock_status(response)
-        price = self.price(response)
-        previous_price = self.previous_prices(response)
-
-        return {'color': color,
-                'size': size,
-                'out_of_stock': stock_status,
-                'price': price,
-                'previous_prices': previous_price}
-
-    def stock_status(self, response):
-        stock_status = re.findall("var jsinit_item=(.+?);", response.text, re.S)[0]
-        stock_status = json.loads(stock_status)
-        return not stock_status['CURRENTITEM']['isAvailable']
+    def out_of_stock(self, response):
+        out_of_stock = clean(response.xpath('//script//text()').re('"isAvailable":\s(\w+)'))
+        return not out_of_stock[0]
 
     def color(self, response):
-        return response.css('.miniThumbsColor>span::text').extract_first()
+        return clean(response.css('.miniThumbsColor>span::text').extract_first())
 
     def product_name(self, response):
-        return response.css('h1::text').extract_first()
+        return clean(response.css('h1::text').extract_first())
 
     def product_category(self, response):
-        return response.css('.microName::text').extract()
+        return clean(response.css('.microName::text').extract())
 
     def image_urls(self, response):
-        return response.css('#alternateList>div>img::attr(src)').extract()
+        return clean(response.css('#alternateList>div>img::attr(src)').extract())
 
     def sizes(self, response):
-        return response.css('.sizeBoxIn::attr(data-sizewid)').extract() 
+        return clean(response.css('.sizeBoxIn::attr(data-sizewid)').extract()) 
 
     def raw_description(self, response):
-        return response.css('.techDescription>li::text').extract()
+        return clean(response.css('.techDescription>li::text').extract())
 
     def product_care(self, response):
         return [pc for pc in self.raw_description(response) if self.care_criteria(pc)]
@@ -102,26 +101,24 @@ class y3ParseSpider(BaseParseSpider):
         return [pd for pd in self.raw_description(response) if not self.care_criteria(pd)]
 
     def price(self, response):
-        return response.css('.newprice>.priceValue::text').extract_first()
+        return clean(response.css('.newprice>.priceValue::text').extract_first())
 
     def previous_prices(self, response):
-        return response.css('.oldprice>.priceValue::text').extract_first()
+        return clean(response.css('.oldprice>.priceValue::text').extract_first())
 
 
-class y3CrawlSpider(BaseCrawlSpider):
-    allowed_url = r'.*/us/.*'
+class Y3CrawlSpider(BaseCrawlSpider):
     listing = '.colonna'
     product = ['.productInfo', '.slick-track']
     
-    rules = (Rule(LinkExtractor(allow=allowed_url, restrict_css=listing), callback='parse'),      
-             Rule(LinkExtractor(allow=allowed_url, restrict_css=product), callback='parse_item'))
+    rules = (Rule(LinkExtractor(restrict_css=listing), callback='parse'),      
+             Rule(LinkExtractor(restrict_css=product), callback='parse_item'))
 
 
-class y3USParseSpider(y3ParseSpider, MixinUS):
+class Y3USParseSpider(Y3ParseSpider, MixinUS):
     name = MixinUS.retailer + '-parse'
 
 
-class y3USCrawlSpider(y3CrawlSpider, MixinUS):
+class Y3USCrawlSpider(Y3CrawlSpider, MixinUS):
     name = MixinUS.retailer + '-crawl'
-    parse_spider = y3USParseSpider()
-
+    parse_spider = Y3USParseSpider()
