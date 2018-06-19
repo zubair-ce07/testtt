3fb184a5955328200c29d1d4c005cff1d93e5fe0
AbdulMaroof <abdul.maroof@arbisoft.com>
2018-06-19 11:29:49 +0500

    csv reader, header updated, arguments

---
+++ Desktop/abdulmaroofgit/main.py
@@ -0,0 +1,30 @@
+from PopulateWeatherData import WeatherFilesData
+from AnnualResultCalculator import AnnualResultCalculator
+from MonthlyResultCalculator import MonthlyResutCalculator
+import argparse
+from DrawMonthlyPlot import DrawMonthlyPlot
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Processing weather report ')
+    parser.add_argument('-a')
+    parser.add_argument('-c')
+    parser.add_argument('-e')
+    parser.add_argument('path')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = get_args()
+    weatherData = WeatherFilesData()
+    wdata = weatherData.populate_data_structures(args.path)
+    if args.a != None:
+        resultCalc = AnnualResultCalculator()
+        resultCalc.do_year_wise_calculation(wdata, args.a)
+    if args.e != None:
+        resultCalc = MonthlyResutCalculator()
+        resultCalc.do_month_wise_calculation(wdata, args.e)
+    if args.c != None:
+        drawPlot = DrawMonthlyPlot()
+        drawPlot.draw(wdata, args.c)
