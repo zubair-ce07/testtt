f52811056e93e49b6ffb038a5a1379f6f53fb4ea
Ahsan Malik <ahsan.muhammad@arbisoft.com>
2018-06-21 11:28:44 +0500

    corrected names, used iterator, Crawler is not abstract now

--- crawler.py
+++ crawler.py
@@ -5,31 +5,14 @@ import asyncio
 from itertools import islice
 from concurrent import futures
 from parsel import Selector
-from abc import ABC
 
 
-# Input:
-# Number of concurrent request that can be made
-# maximum number of urls to visit
-# download delay each worker will observe
-
-# Output:
-# number of requests made
-# total bytes downloaded
-# average size of a page
-
-
-def iterate_links(urls):
-    for url_ in urls:
-        yield url_
-
-
-class Crawler(ABC):
-    def __init__(self, max_url_hits, download_delay, x_path):
+class Crawler:
+    def __init__(self, max_url_hits, download_delay):
         self.requests_made = 0
         self.max_url_hits = max_url_hits
         self.download_delay = download_delay
-        self.x_path = x_path
+        self.xpath = "//a[starts-with(@href,'https')]/@href"
         self.responses = []
 
     def get_stats(self):
@@ -41,58 +24,55 @@ class Crawler(ABC):
 
 
 class ConcurrentWebCrawler(Crawler):
-    def __init__(self, concurrency, max_url_hits, download_delay, x_path):
-        super().__init__(max_url_hits, download_delay, x_path)
+    def __init__(self, concurrency, max_url_hits, download_delay):
+        super().__init__(max_url_hits, download_delay)
         self.concurrency = concurrency
 
-    async def crawl_page(self, link):
-        if self.requests_made >= self.max_url_hits:
+    async def crawl_page(self, url):
+        if self.requests_made >= self.max_url_hits or not url:
             return
         else:
             self.requests_made += 1
             await asyncio.sleep(self.download_delay)
-            http_response = requests.get(link)
-            print(http_response.content)
-            self.responses.append(http_response)
-            urls = Selector(http_response.text).xpath(self.x_path).extract()
-            if urls:
-                urls = iterate_links(urls)
-                while self.requests_made < self.max_url_hits:
-                    await asyncio.gather(*map(
-                        self.crawl_page, list(islice(urls, self.concurrency))))
+            response = requests.get(url)
+            print(response.content)
+            self.responses.append(response)
+            urls = iter(Selector(response.text).xpath(self.xpath).extract())
+            while self.requests_made < self.max_url_hits:
+                sublist = list(islice(urls, self.concurrency))
+                if sublist:
+                    await asyncio.gather(*map(self.crawl_page, sublist))
+                else:
+                    break
 
 
 class ParallelWebCrawler(Crawler):
-    def __init__(self, concurrency, max_url_hits, download_delay, x_path):
-        super().__init__(max_url_hits, download_delay, x_path)
-        self.executor = futures.ProcessPoolExecutor(
-            max_workers=concurrency)
+    def __init__(self, concurrency, max_url_hits, download_delay):
+        super().__init__(max_url_hits, download_delay)
+        self.executor = futures.ProcessPoolExecutor(max_workers=concurrency)
 
     def crawl(self, urls):
-        future_requests = (self.executor.submit(requests.get, url)
-                           for url in urls)
-        for future in futures.as_completed(future_requests):
+        future_reqs = (self.executor.submit(requests.get, url) for url in urls)
+        for future in futures.as_completed(future_reqs):
             if self.requests_made >= self.max_url_hits:
                 future.cancel()
                 continue
             self.requests_made += 1
             time.sleep(self.download_delay)
-            http_response = future.result()
-            print(http_response.content)
-            self.responses.append(http_response)
-            urls = Selector(http_response.text).xpath(self.x_path).extract()
+            response = future.result()
+            print(response.content)
+            self.responses.append(response)
+            urls = Selector(response.text).xpath(self.xpath).extract()
             if urls and self.requests_made < self.max_url_hits:
-                urls = iterate_links(urls)
                 self.crawl(urls)
 
 
 def get_input():
-    delay = int(input("Download Dela1y for every request: "))
+    download_delay = int(input("Download Delay for every request: "))
     max_hits = int(input("Max URLS to be hit: "))
     max_concurrency = int(input("Max Concurrent requests: "))
     url = input("URL to begin crawling: ")
-    x_path = "//a[starts-with(@href,'https')]/@href"
-    return max_concurrency, max_hits, delay, x_path, url
+    return max_concurrency, max_hits, download_delay, url
 
 
 def crawler_type():
@@ -101,17 +81,17 @@ def crawler_type():
     return parser.parse_args().p
 
 
-def init_concurrent(max_concurrency, max_hits, delay, x_path, link):
-    crawler = ConcurrentWebCrawler(max_concurrency, max_hits, delay, x_path)
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(crawler.crawl_page(link))
-    loop.close()
+def init_concurrent(max_concurrency, max_hits, download_delay, url):
+    crawler = ConcurrentWebCrawler(max_concurrency, max_hits, download_delay)
+    event_loop = asyncio.get_event_loop()
+    event_loop.run_until_complete(crawler.crawl_page(url))
+    event_loop.close()
     crawler.get_stats()
 
 
-def init_parallel(max_concurrency, max_hits, delay, x_path, link):
-    crawler = ParallelWebCrawler(max_concurrency, max_hits, delay, x_path)
-    crawler.crawl([link])
+def init_parallel(max_concurrency, max_hits, download_delay, url):
+    crawler = ParallelWebCrawler(max_concurrency, max_hits, download_delay)
+    crawler.crawl([url])
     crawler.get_stats()
 
 
@@ -119,5 +99,5 @@ crawl = {True: init_parallel, False: init_concurrent}
 
 
 if __name__ == "__main__":
-    max_concurrency, max_hits, delay, x_path, url = get_input()
-    crawl[crawler_type()](max_concurrency, max_hits, delay, x_path, url)
+    max_concurrency, max_hits, download_delay, url = get_input()
+    crawl[crawler_type()](max_concurrency, max_hits, download_delay, url)
