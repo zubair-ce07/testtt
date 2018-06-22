ab6b6f7e76d5267a357e4f0ed28053926bfa0efe
Ahmed Buksh <ahmed.buksh@arbisoft.com>
2018-06-22 16:01:23 +0500

    Working crawler with 330703 products in five categories

--- hktvmall.py
+++ hktvmall.py
@@ -2,69 +2,111 @@ import scrapy
 import json
 
 from scrapy.spiders import CrawlSpider, Request
-from scrapy.linkextractors import LinkExtractor
 from scrapy.loader import ItemLoader
 
 from HKTV.items import Product
+
 
 class HktvmallSpider(CrawlSpider):
     name = 'hktvmall'
 
     allowed_domains = ['www.hktvmall.com']
     start_urls = ['https://www.hktvmall.com/hktv/en/']
-    api_url = 'https://www.hktvmall.com/hktv/en/ajax/search_products?query="":relevance:zone:{}:street:main:&currentPage={}&pageSize=60'
+    api_url = 'https://www.hktvmall.com/hktv/en/ajax/search_products?query="":'\
+              'relevance:zone:{}:street:main:&currentPage={}&pageSize=60'
     zones = ['supermarket', 'beautynhealth', 'housewares', 'homenfamily', 'fashion']
 
     def parse(self, response):
         for zone in self.zones:
-            yield Request(self.api_url.format('fashion', 0), callback=self.parse_json, meta={'zone': 'fashion'})
+            yield Request(self.api_url.format(zone, 0), callback=self.parse_json, meta={zone: zone})
 
     def parse_json(self, response):
-        products = response.meta.get('product') or []
         zone = response.meta.get('zone')
-        data = json.loads(response.text)
-        products = products.copy() + data['products']
-        number_of_pages = int(data['pagination']['numberOfPages'])
-        current_page = int(data['pagination']['currentPage'])
+        raw_data = json.loads(response.text)
+        products = raw_data['products']
+        number_of_pages = int(raw_data['pagination']['numberOfPages'])
 
-        if current_page < number_of_pages:
-            for page in range(number_of_pages):
-                yield Request(self.api_url.format(zone, page), meta={'product': products}, callback=self.parse_json)
-        else:
-            yield from self.parse_product(products)
+        yield from self.parse_product(products)
+        
+        for page in range(number_of_pages):
+            yield Request(self.api_url.format(zone, page), callback=self.parse_json)
 
     def parse_product(self, products):
         for product in products:    
             product_loader = ItemLoader(item=Product())
-            product_loader.add_value('name', product['name'])
-            product_loader.add_value('url', product['url'])
-            product_loader.add_value('brand', product['brandName'])
-
-            if 'categories' in product:
-                product_loader.add_value('code', product['categories'][0]['code'])
-                product_loader.add_value('categories', product['categories'][0]['name'])
-
-            if 'price' in product:
-                product_loader.add_value('price', product['price']['value'])
-                product_loader.add_value('currency', product['price']['currencyIso'])
-                product_loader.add_value('previous_price', product['price']['value'])
-
-            if 'promotionPrice' in product:
-                product_loader.add_value('price', product['promotionPrice']['value'])
-
-            if 'description' in product:
-                product_loader.add_value('description', product['description'])     
             
-            product_loader.add_value('availability', product['stock']['stockLevelStatus']['code'])
-            product_loader.add_value('packaging', product['packingSpec'])
-            product_loader.add_value('image_urls', self.get_images(product['images']))
-            product_loader.add_value('reviews_count', product['numberOfReviews'])
-            product_loader.add_value('reviews_score', product['score'])
-            product_loader.add_value('barcode', product['code'])
-            product_loader.add_value('store_name', product['store']['name'])
+            product_loader.add_value('name', self.get_product_name(product))
+            product_loader.add_value('url', self.get_product_url(product))
+            product_loader.add_value('brand', self.get_product_brand(product))
+            product_loader.add_value('code', self.get_product_code(product))
+            product_loader.add_value('categories', self.get_product_categories(product))
+            product_loader.add_value('price', self.get_product_price(product))
+            product_loader.add_value('currency', self.get_product_currency(product))
+            product_loader.add_value('previous_price', self.get_product_previous_price(product))
+            product_loader.add_value('description', self.get_product_description(product))          
+            product_loader.add_value('availability', self.get_product_stock(product))
+            product_loader.add_value('packaging', self.get_product_packaging(product))
+            product_loader.add_value('image_urls', self.get_product_images(product))
+            product_loader.add_value('reviews_count', self.get_product_reviews_count(product))
+            product_loader.add_value('reviews_score', self.get_product_reviews_score(product))
+            product_loader.add_value('barcode', self.get_product_barcode(product))
+            product_loader.add_value('store_name', self.get_product_store_name(product))
 
-            yield product_loader
+            yield product_loader.load_item()
 
+    def get_product_name(self, product):
+        return product['name']
 
-    def get_images(self, images):
+    def get_product_url(self, product):
+        return product['url']
+
+    def get_product_brand(self, product):
+        return product['brandName']
+
+    def get_product_code(self, product):
+        if 'categories' in product:
+            return product['categories'][0]['code']
+
+    def get_product_categories(self, product):
+        if 'categories' in product:
+            return product['categories'][0]['name']
+
+    def get_product_price(self, product):
+        if 'promotionPrice' in product:
+            return product['promotionPrice']['value']
+        return product['price']['value']
+
+    def get_product_currency(self, product):
+        if 'price' in product:
+            return product['price']['currencyIso']
+
+    def get_product_previous_price(self, product):
+        if 'promotionPrice' in product:
+            return product['price']['value']
+
+    def get_product_description(self, product):
+        return product['summary']
+
+    def get_product_stock(self, product):
+        return product['stock']['stockLevelStatus']['code']
+
+    def get_product_packaging(self, product):
+        return product['packingSpec']
+
+    def get_product_images(self, product):
+        return self.parse_images(product['images'])
+
+    def get_product_reviews_count(self, product):
+        return product['numberOfReviews']
+
+    def get_product_reviews_score(self, product):
+        return product['score']
+
+    def get_product_barcode(self, product):
+        return product['code']
+
+    def get_product_store_name(self, product):
+        return product['store']['name']
+
+    def parse_images(self, images):
         return [image['url'] for image in images]
--- items.py
+++ items.py
@@ -1,28 +1,73 @@
 import scrapy
 
 from scrapy.item import Item, Field
-from scrapy.loader.processors import TakeFirst, MapCompose
+from scrapy.loader.processors import TakeFirst, MapCompose, Join
+
+from w3lib.html import remove_tags
+
+
+def format_price(value):
+    return float(value)
+
+
+def format_description(value):
+    return remove_tags(value)
 
 
 class Product(Item):
-    url = Field()
-    code = Field()
-    brand = Field()
-    name = Field()
-    price = Field()
-    currency = Field()
-    previous_price = Field()
-    description = Field()
-    categories = Field()
-    availability = Field()
-    packaging = Field()
+    url = Field(
+        output_processor=TakeFirst()
+    )
+    code = Field(
+        output_processor=TakeFirst()
+    )
+    brand = Field(
+        output_processor=TakeFirst()
+    )
+    name = Field(
+        output_processor=TakeFirst()
+    )
+    price = Field(
+        output_processor=TakeFirst()
+    )
+    currency = Field(
+        output_processor=TakeFirst()
+    )
+    previous_price = Field(
+        output_processor=TakeFirst()
+    )
+    description = Field(
+        input_processor=MapCompose(format_description),
+        output_processor=TakeFirst()
+    )
+    categories = Field(
+        output_processor=TakeFirst()
+    )
+    availability = Field(
+        output_processor=TakeFirst()
+    )
+    packaging = Field(
+        output_processor=TakeFirst()
+    )
     image_urls = Field()
-    reviews_count = Field()
-    reviews_score = Field()
-    barcode = Field()
-    store_name = Field()
-    website_name = Field()
-    product_type = Field()
-    price_per_unit = Field()
-
-
+    reviews_count = Field(
+        output_processor=TakeFirst()
+    )
+    reviews_score = Field(
+        output_processor=TakeFirst()
+    )
+    barcode = Field(
+        output_processor=TakeFirst()
+    )
+    store_name = Field(
+        output_processor=TakeFirst()
+    )
+    website_name = Field(
+        output_processor=TakeFirst()
+    )
+    product_type = Field(
+        output_processor=TakeFirst()
+    )
+    price_per_unit = Field(
+        output_processor=TakeFirst()
+    )
