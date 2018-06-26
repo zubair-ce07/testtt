968c95e60a753958828f98be3d128ab14fbb8d4b
razibaig <raziullah.baig@arbisoft.com>
2018-06-26 18:43:06 +0500

    Initial Commit: Created basic data structure

---
+++ task1.py
@@ -0,0 +1,28 @@
+class WeatherReading:
+	def __init__(self, PKT, MaxTemp, MeanTemp, MinTemp, Dew, MeanDew, MinDew, MaxHumidity, MeanHumidity, MinHumidity, MaxSeaPressure, MeanSeaPressure, 				MinSeaPressure, MaxVisibility, MeanVisibility, Minvisibility, MaxWindSpeed, MeanWindSpeed, MinWindSpeed, MaxGustSpeed, Precipitation, CloudCover, 				Events, WinDirDegrees):
+
+				self.PKT = PKT
+				self.MaxTemp = MaxTemp
+				self.MeanTemp = MeanTemp
+				self.MinTemp = MinTemp
+				self.Dew = Dew
+				self.MeanDew = MeanDew
+				self.MinDew = MinDew
+				self.MaxHumidity = MaxHumidity
+				self.MeanHumidity = MeanHumidity
+				self.MinHumidity = MinHumidity
+				self.MaxSeaPressure = MaxSeaPressure
+				self.MeanSeaPressure = MeanSeaPressure
+				self.MinSeaPressure = MinSeaPressure
+				self.MaxVisibility = MaxVisibility
+				self.MeanVisibility = MeanVisibility
+				self.Minvisibility = Minvisibility
+				self.MaxWindSpeed = MaxWindSpeed
+				self.MeanWindSpeed = MeanWindSpeed
+				self.MinWindSpeed = MinWindSpeed
+				self.MaxGustSpeed = MaxGustSpeed
+				self.Precipitation = Precipitation
+				self.CloudCover = CloudCover
+				self.Events = Events
+				self.WinDirDegrees = WinDirDegrees
+
