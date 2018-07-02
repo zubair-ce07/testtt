95d6379924bce1f8838e9863360237cc83b13aec
Farwa Shahid <farwa.shahid@arbisoft.com>
2018-07-02 11:59:14 +0500

    Changed date format, website address and solved QA issues

--- propertyscraper/propertyscraper/spiders/rightmove.py
+++ propertyscraper/propertyscraper/spiders/rightmove.py
@@ -42,7 +42,10 @@ class RightmoveParseSpider(Spider, Mixin):
     name = f"{Mixin.name}-parse"
 
     amenities = ['friendly', 'pets', 'smoking', 'storage', 'spacious', 'window', 'sun']
-    property_types = ['Studio', 'Flat', 'House', 'Apartment', 'Maisonette', 'Room']
+    property_types = ['Studio', 'Flat', 'House', 'Apartment', 'Maisonette', 'Room', 'Hall']
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+              'November', 'December', 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov',
+              'Dec']
 
     def parse(self, response):
         property_item = PropertyscraperItem()
@@ -50,11 +53,11 @@ class RightmoveParseSpider(Spider, Mixin):
         property_item['property_name'] = self.property_name(response)
         property_item['street_address_1'] = self.street_address(response)
         property_item['street_address_2'] = ''
-        property_item['city'] = ''
-        property_item['postcode'] = ''
+        property_item['city'] = 'London'
+        property_item['postcode'] = self.postcode(response)
         property_item['area'] = self.area(response)
         property_item['area_info'] = ''
-        property_item['website'] = ''
+        property_item['website'] = self.website(response)
         property_item['furnished'] = self.furnished_info(response)
         property_item['bills_included'] = self.bills_included(response)
         property_item['property_type'] = self.property_type(response)
@@ -68,7 +71,7 @@ class RightmoveParseSpider(Spider, Mixin):
         property_item['room_name'] = self.room_name(response)
         property_item['room_availability'] = ''
         property_item['property_contact_name'] = ''
-        property_item['property_contact_email'] = ''
+        property_item['property_contact_email'] = self.property_contact_email(response)
         property_item['property_contact_number'] = self.property_contact_number(response)
         property_item['discounts'] = ''
         property_item['room_price'] = self.room_price(response)
@@ -82,6 +85,23 @@ class RightmoveParseSpider(Spider, Mixin):
         property_item['agent_name'] = self.agent_name(response)
 
         yield property_item
+
+    def property_contact_email(self, response):
+        description = self.description(response)
+        contact_email = [''.join(re.findall('[a-zA-Z-0-9_]+@[a-z]+.[a-z]{3,5}', d)) for d in description if
+                         ''.join(re.findall('[a-zA-Z-0-9_]+@[a-z]+.[a-z]{3,5}', d))]
+        return contact_email[0] if contact_email else ''
+
+    def postcode(self, response):
+        page = response.xpath(
+            '//script[contains(text(), "RIGHTMOVE.ANALYTICS.DataLayer.pushKV")]/text()').extract_first()
+        for s in page.split(','):
+            if 'post' in s:
+                return s.split('"')[-2]
+
+    def website(self, response):
+        website_x = '//div[@class="agent-details-display"]/p[contains(@class, "overflow-hidden")]/a/@href'
+        return response.xpath(website_x).extract()
 
     def property_contact_number(self, response):
         contact_number_css = '.request-property-details strong::text'
@@ -105,29 +125,65 @@ class RightmoveParseSpider(Spider, Mixin):
 
     def move_in_date(self, response):
         letting_info = self.letting_info(response)
+        letting_info.extend(self.property_amenities(response))
+        letting_info.extend(self.combine_description_and_amenities(response))
+
         move_in_info = [r for r in letting_info if 'available' in r.lower()]
-        move_in_date_info = ''
-        if len(move_in_info) > 0:
-            move_in_date_info = move_in_info[0].split(':')[1]
-        try:
-            date = parser.parse(move_in_date_info)
-            return date.strftime("%d/%m/%Y")
-        except ValueError:
-            if 'Now' in move_in_date_info:
-                return datetime.datetime.now().strftime("%d/%m/%Y")
+
+        if move_in_info:
+            try:
+                move_in_date_info = move_in_info[0].split(':')[1]
+                date = parser.parse(move_in_date_info)
+                return date.strftime("%d/%m/%Y")
+            except ValueError:
+                if 'Now' in move_in_date_info:
+                    return datetime.datetime.now().strftime("%d/%m/%Y")
+            except IndexError:
+                date = re.findall('(?:on|from|:)\s*([a-zA-Z0-9,\s]{1,17}20[\d]{2})', move_in_info[0])
+                try:
+                    date = parser.parse(date[0])
+                    return date.strftime("%d/%m/%Y")
+                except IndexError:
+                    if re.findall('immediately|now', ''.join(move_in_info[0]).lower()):
+                        return datetime.datetime.now().strftime("%d/%m/%Y")
+                    if re.findall('mid|end|beginning', ''.join(move_in_info[0]).lower()):
+                        month = re.findall('(?:mid|end|beginning)\s*([a-zA-Z0-9]+)', move_in_info[0])
+                        month = parser.parse(month[0])
+                        return month.strftime("%d/%m/%Y")
+                    month = [m for m in self.months if m in move_in_info[0]]
+                    if month:
+                        regex_mm_yy = '{0}\s20[\d]{{2}}'.format(month[0])
+                        regex_ddth_mm_yy = '[\d]{{2}}th\s{0}\s20[\d]{{2}}'.format(month[0])
+                        regex_dd_mm_yy = '[\d]{{2}}\s{0}\s20[\d]{{2}}'.format(month[0])
+                        date = re.findall(regex_dd_mm_yy, move_in_info[0]) \
+                               or re.findall(regex_ddth_mm_yy, move_in_info[0]) \
+                               or re.findall(regex_mm_yy, move_in_info[0])
+                        date = parser.parse(date[0])
+                        return date.strftime("%d/%m/%Y")
 
     def deposit_amount(self, response):
         property_info = self.combine_description_and_amenities(response)
         deposit_info = [l for l in property_info if 'deposit' in l.lower()]
-        try:
-            return deposit_info[0].split(':')[1] if deposit_info else ''
-        except IndexError:
-            return ''
+        if deposit_info:
+            try:
+                return deposit_info[0].split(':')[1] if deposit_info else ''
+            except IndexError:
+                regex = '(?:deposit.*?)(?:(£[\d,]+|\s[\d,]+\spounds)|(?:agency|agent|fee))'
+                deposit_amount = [' '.join(list(re.findall(regex, d.lower()))) for d in deposit_info if
+                                  ' '.join(list(re.findall(regex, d.lower())))]
+                if deposit_amount:
+                    return deposit_amount[0]
+        return ''
 
     def deposit_name(self, response):
         property_info = self.combine_description_and_amenities(response)
+        deposit_name = ""
         deposit_info = [l for l in property_info if 'deposit' in l.lower()]
-        return deposit_info[0].split(':')[0] if deposit_info else ''
+        if deposit_info:
+            regex = 'Deposit|Bond|Security|Holding'
+            deposit_name = [' '.join(re.findall(regex, d)) for d in deposit_info if
+                            re.findall(regex, d) and re.findall('£|pounds', d)]
+        return deposit_name[0] if deposit_name else ''
 
     def combine_description_and_amenities(self, response):
         property_info = self.letting_info(response)
@@ -173,8 +229,28 @@ class RightmoveParseSpider(Spider, Mixin):
         return property_heading.split("to")[0]
 
     def room_name(self, response):
-        property_heading = response.css('.left h1::text').extract_first()
-        return property_heading.split("to")[0]
+        property_info = self.combine_description_and_amenities(response)
+        property_info.append(self.property_name(response))
+        room_name = ""
+
+        bedroom_count = [''.join(re.findall('[\d]+\sbedroom', i.lower())) for i in property_info if
+                         ''.join(re.findall('[\d]+\sbedroom', i.lower()))]
+        if bedroom_count:
+            print(bedroom_count)
+            room_name = bedroom_count[0]
+
+        bathroom_count = [''.join(re.findall('[\d]+\sbathroom', i.lower())) for i in property_info if
+                          ''.join(re.findall('[\d]+\sbathroom', i.lower()))]
+        if bathroom_count:
+            room_name = f"{room_name}, {bathroom_count[0]}"
+
+        reception_room_count = [''.join(re.findall('[\d]+\sreception\sroom', i.lower())) for i in property_info if
+                                ''.join(re.findall('[\d]+\sreception\sroom', i.lower()))]
+
+        if reception_room_count:
+            room_name = f"{room_name}, {reception_room_count[0]}"
+
+        return room_name
 
     def raw_images(self, response):
         images = response.css('.gallery-thumbs-list li meta::attr(content)').extract()
@@ -260,7 +336,7 @@ class RightmoveCrawlSpider(CrawlSpider, Mixin):
 
         for pair in list(itertools.product(bedding_range_chunks, price_range_chunks)):
             next_chunk = (f"{response.url}&maxPrice={pair[1][-1]}&minPrice={pair[1][0]}&"
-                                    f"maxBedrooms={pair[0][1]}&minBedrooms={pair[0][0]}")
+                          f"maxBedrooms={pair[0][1]}&minBedrooms={pair[0][0]}")
             requests.append(scrapy.Request(url=next_chunk, callback=self.parse))
 
         return requests
