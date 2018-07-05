8ad0b747b5db217177e802be2bee6d4965dc2a52
sohaibaslam <sohaib.sameja@gmail.com>
2018-07-05 14:26:20 +0500

    Changed the user argument date validation to add the validate method in the argparse.add_argument call.

--- main.py
+++ main.py
@@ -13,31 +13,6 @@ def valid_year(year):
     return year
 
 
-user_command_parser = argparse.ArgumentParser()
-
-user_command_parser.add_argument("file_path", help="This arg stores the path to all weather data files", type=str)
-
-user_command_parser.add_argument("-e", help="This command will give you the highest and "
-                                 "lowest temperature and highest humidity with "
-                                 "respective days for given year", type=valid_year)
-
-user_command_parser.add_argument("-a", help="This command will give you the highest and "
-                                 "lowest avg temperature and Mean avg Humidity"
-                                 "for a given month", type=str)
-
-user_command_parser.add_argument("-c", help="For a given month this command will draw two "
-                                 "horizontal bar charts on the console for the "
-                                 "highest and lowest temperature on each day. "
-                                 "Highest in red and lowest in blue.", type=str)
-
-user_command_parser.add_argument("-b", help="For a given month this command will draw one "
-                                 "horizontal bar charts on the console for the "
-                                 "highest and lowest temperature on each day. "
-                                 "Highest in red and lowest in blue.", type=str)
-
-user_cli_args = user_command_parser.parse_args()
-
-
 def get_year_month(weather_date):
     date_year_month = weather_date.split("/")
 
@@ -49,7 +24,32 @@ def get_year_month(weather_date):
                 and valid_year(year) and int(month) in range(1, 13):
             return int(year), int(month)
 
-    print("The date given is not valid!")
+    raise argparse.ArgumentTypeError(f"The Date is not valid")
+
+
+user_command_parser = argparse.ArgumentParser()
+
+user_command_parser.add_argument("file_path", help="This arg stores the path to all weather data files", type=str)
+
+user_command_parser.add_argument("-e", help="This command will give you the highest and "
+                                 "lowest temperature and highest humidity with "
+                                 "respective days for given year", type=valid_year)
+
+user_command_parser.add_argument("-a", help="This command will give you the highest and "
+                                 "lowest avg temperature and Mean avg Humidity"
+                                 "for a given month", type=get_year_month)
+
+user_command_parser.add_argument("-c", help="For a given month this command will draw two "
+                                 "horizontal bar charts on the console for the "
+                                 "highest and lowest temperature on each day. "
+                                 "Highest in red and lowest in blue.", type=get_year_month)
+
+user_command_parser.add_argument("-b", help="For a given month this command will draw one "
+                                 "horizontal bar charts on the console for the "
+                                 "highest and lowest temperature on each day. "
+                                 "Highest in red and lowest in blue.", type=get_year_month)
+
+user_cli_args = user_command_parser.parse_args()
 
 
 if __name__ == "__main__":
@@ -62,31 +62,22 @@ if __name__ == "__main__":
         report = report_maker.WeatherReportMaker.print_report_for_e(result)
 
     if user_cli_args.a:
-        year_month = get_year_month(user_cli_args.a)
-
-        if year_month:
-            data_parser = parsed_weather_reading.ParsedWeatherReading()
-            filtered_data = data_parser.get_weather_records(file_path, year_month[0],
-                                                            calendar.month_abbr[year_month[1]])
-            result = ws.WeatherResultComputer.get_result_for_a(filtered_data)
-            report_maker.WeatherReportMaker.print_report_for_a(result)
+        year, month = user_cli_args.a
+        data_parser = parsed_weather_reading.ParsedWeatherReading()
+        filtered_data = data_parser.get_weather_records(file_path, year, calendar.month_abbr[month])
+        result = ws.WeatherResultComputer.get_result_for_a(filtered_data)
+        report_maker.WeatherReportMaker.print_report_for_a(result)
 
     if user_cli_args.c:
-        year_month = get_year_month(user_cli_args.c)
-
-        if year_month:
-            data_parser = parsed_weather_reading.ParsedWeatherReading()
-            filtered_data = data_parser.get_weather_records(file_path, year_month[0],
-                                                            calendar.month_abbr[year_month[1]])
-            result = ws.WeatherResultComputer.get_result_for_c(filtered_data)
-            report_maker.WeatherReportMaker.print_report_for_c(result)
+        year, month = user_cli_args.c
+        data_parser = parsed_weather_reading.ParsedWeatherReading()
+        filtered_data = data_parser.get_weather_records(file_path, year, calendar.month_abbr[month])
+        result = ws.WeatherResultComputer.get_result_for_c(filtered_data)
+        report_maker.WeatherReportMaker.print_report_for_c(result)
 
     if user_cli_args.b:
-        year_month = get_year_month(user_cli_args.c)
-
-        if year_month:
-            data_parser = parsed_weather_reading.ParsedWeatherReading()
-            filtered_data = data_parser.get_weather_records(file_path, year_month[0],
-                                                            calendar.month_abbr[year_month[1]])
-            result = ws.WeatherResultComputer.get_result_for_c(filtered_data)
-            report_maker.WeatherReportMaker.print_report_for_c_bonus(result)
+        year, month = user_cli_args.b
+        data_parser = parsed_weather_reading.ParsedWeatherReading()
+        filtered_data = data_parser.get_weather_records(file_path, year, calendar.month_abbr[month])
+        result = ws.WeatherResultComputer.get_result_for_c(filtered_data)
+        report_maker.WeatherReportMaker.print_report_for_c_bonus(result)
