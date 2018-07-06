ecd5092cf42dbb694b29b5c476121ce0d213c1cd
AbdulMaroof <abdul.maroof@arbisoft.com>
2018-07-06 21:34:46 +0500

    Initial Release LornaJaneSpider

--- LornaJaneSpider.py
+++ LornaJaneSpider.py
@@ -2,6 +2,7 @@
 import scrapy
 import math
 
+
 class LornajaneSpider(scrapy.Spider):
 
     name = 'LornaJaneSpider'
@@ -9,42 +10,43 @@ class LornajaneSpider(scrapy.Spider):
     start_urls = ['https://www.lornajane.sg/']
     download_delay = 3
 
-    def __init__(self):
-        self.flag = True
-
     def parse(self, response):
-        list_product_link = {'product_links': self.main_product_links(response)[0]}
-        yield scrapy.http.Request(list_product_link['product_links'], callback=self.parse_main_products, method='GET')
+        cat_urls = self.main_product_links(response)[0]
+        yield scrapy.http.Request(cat_urls, callback=self.parse_main_products)
 
     def parse_main_products(self, response):
-        item = {'sub_product_links': self.get_sub_prod_links(response),'next_page': []}
-        item['sub_product_links'] = ["https://www.lornajane.sg{0}".format(link) for link in item['sub_product_links']]
+        product_links = self.get_sub_prod_links(response)
+        product_links = [response.urljoin(href) for href in product_links]
+
+        for product in product_links:
+            yield scrapy.http.Request(product, callback=self.parse_products)
+
         no_of_records = self.records_for_category(response)
-        if no_of_records:
-            self.flag = False
-            no_of_records = int(no_of_records[-1])
-            max_pages = int(math.ceil(no_of_records/20.0))
-            for page_no in range(1, max_pages, 1):
-                item['next_page'].append(('{0}?page={1}').format(response.url, page_no))
+        no_of_records = int(no_of_records[-1])
+        max_pages = int(math.ceil(no_of_records / 20.0))
 
+        for next_page in range(1, max_pages, 1):
+            yield scrapy.http.Request(self.next_page_link(next_page, response), callback=self.parse_main_products)
 
-        for product in item['sub_product_links']:
-            yield scrapy.http.Request(product, callback=self.parse_sub_products_callback, method='GET')
-
-        for next_page in item['next_page']:
-            yield scrapy.http.Request(next_page, callback=self.parse_main_products, method='GET')
-
-    def parse_sub_products_callback(self, response):
+    def parse_products(self, response):
         item = {
             'care': self.care(response),
             'description': self.description(response),
             'type': self.product_type(response),
-            'Product_name': self.product_name(response),
+            'image_url': self.image_url(response),
+            'product_name': self.product_name(response),
             'sku': self.sku_id(response),
             'url': response.url,
             'skus': {}
         }
 
+        yield self.populate_sku_for_all_sizes(item, response)
+
+        color_links = self.color_links(response)
+        for color in color_links:
+            yield scrapy.http.Request(color, callback=self.parse_products)
+
+    def populate_sku_for_all_sizes(self, item, response):
         for x in self.size(response):
             values = {
                 'color': self.color(response),
@@ -52,29 +54,30 @@ class LornajaneSpider(scrapy.Spider):
                 'price': self.price(response),
                 'size': x
             }
-            item['skus'][item['sku'] + "_" + x  + "_"+ self.color(response)] = values
-
-        yield item
-
-        color_links = self.color_links(response)
-        for color in color_links:
-            yield scrapy.http.Request(color, callback=self.parse_sub_products_callback, method='GET')
+            item['skus'][item['sku'] + "_" + x + "_" + self.color(response)] = values
+        return item
 
     def main_product_links(self, response):
         links =  response.css('.main-menu > li:nth-child(1) > a:nth-child(1)::attr(href)').extract()
-        return [link if 'https://' in link else 'https://www.lornajane.sg{0}'.format(link) for link in links ]
+        return [link if 'https://' in link else 'https://www.lornajane.sg{0}'.format(link) for link in links]
+
+    def next_page_link(self, next_page, response):
+        return '{0}?page={1}'.format(response.url.split('?')[0], next_page)
+
+    def image_url(self, response):
+        return response.urljoin(response.css('.product-slider-image > div.item > picture > source::'
+                                             'attr(srcset)')[0].extract())
 
     def records_for_category(self, response):
-        if(self.flag):
             return response.css('.count-text::text').extract()[1].split(' ')
-        else:
-            return 0
 
     def get_sub_prod_links(self, response):
-        return response.css('div.product-item > div.product-grid-item > div:nth-child(1) > a:nth-child(1)::attr(href)').extract()
+        return response.css('div.product-item > div.product-grid-item > div:nth-child(1) > a:nth-child(1)'
+                            '::attr(href)').extract()
 
     def color_links(self, response):
-        cols = response.css('.color-swatch > ul:nth-child(1) > li > a.product-detail-swatch-btn::attr(data-url)').extract()
+        cols = response.css('.color-swatch > ul:nth-child(1) > li > a.product-detail-swatch-btn'
+                            '::attr(data-url)').extract()
         return [('https://www.lornajane.sg{0}').format(col) for col in cols]
 
     def size(self,response):
@@ -88,7 +91,8 @@ class LornajaneSpider(scrapy.Spider):
         return response.css('.selected > span::attr(title)').extract()[0].encode('utf-8')
 
     def product_type(self, response):
-        return response.css('.breadcrumb > ul:nth-child(1) > li:nth-child(3) > a:nth-child(1)::text').extract()[0].strip()
+        return response.css('.breadcrumb > ul:nth-child(1) > li:nth-child(3) > a:nth-child(1)::'
+                            'text').extract()[0].strip()
 
     def gender(self, response):
         return response.css('female').extract()[0]
