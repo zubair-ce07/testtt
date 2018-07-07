631b189b12918f15c2cbfc1f22d13d942e158405
Hamza953 <hamzaakhtar0@gmail.com>
2018-07-07 13:28:17 +0500

    Incorporated all the changes requested

--- hakhtar-weatherman.py
+++ hakhtar-weatherman.py
@@ -1,5 +1,4 @@
 import calendar as cal
-import datetime as dt
 import csv
 import argparse
 import utility as ut
@@ -22,13 +21,15 @@ WEATHER_CITY = "Murree_weather"
 YEAR_LENGTH = 12
 
 
-# def get_month(string):
-#     temp = string.year_month.split("/")
-#     return cal.month_name[int(temp[1])], temp[0]
-#
-
 def get_month(string):
     return string[:3]
+
+
+def get_year(action):
+    year = 0
+    for i in action or []:
+        year = i
+    return year
 
 
 def parse_arguments():
@@ -44,6 +45,7 @@ def parse_files(directory, read_list):
     year = 2016
     year_list = list(range(year, year - 13, -1))
 
+    # Reading all the files in the directory
     for i in range(0, len(year_list)):
         for j in range(0, YEAR_LENGTH):
             file_location = ut.get_file_location(year_list[i], get_month(cal.month_name[j + 1]),
@@ -58,66 +60,105 @@ def parse_files(directory, read_list):
 
             except:
                 print("File at location %s not found" % file_location)
+    print("\n")
 
 
-def calculate_results(readings):
-    print("HERE")
+def calculate_results(args, readings):
+    list_subset = []
+    if args.e:
+        year = get_year(args.e)
+        for i in readings:
+            if year in i.date and not (i.max_temp == ''
+                                       and i.min_temp == ''
+                                       and i.max_humidity == ''):
+                list_subset.append(i)
+
+        node_max = max(list_subset, key=lambda DayForecast: int(DayForecast.max_temp))
+        node_min = min(list_subset, key=lambda DayForecast: int(DayForecast.min_temp))
+        node_max_humidity = max(list_subset, key=lambda DayForecast: int(DayForecast.max_humidity))
+
+        generate_yearly_report(node_max, node_min, node_max_humidity)
+        list_subset.clear()
+
+    if args.a:
+        temp = get_year(args.a).split("/")
+        key = temp[0] + "-" + temp[1]
+
+        for i in readings:
+            if key in i.date and not (i.max_temp == ''
+                                      and i.min_temp == ''
+                                      and i.mean_humidity == ''):
+                list_subset.append(i)
+
+        avg_max_temp = round(sum(int(DayForecast.max_temp) for DayForecast in list_subset) / len(list_subset))
+        avg_min_temp = round(sum(int(DayForecast.min_temp) for DayForecast in list_subset) / len(list_subset))
+        avg_mean_humidity = round(sum(int(DayForecast.mean_humidity) for DayForecast in list_subset) / len(list_subset))
+
+        generate_monthly_report(avg_max_temp, avg_min_temp, avg_mean_humidity)
+        list_subset.clear()
+
+    if args.c:
+        day_index = 1
+        temp = get_year(args.c).split("/")
+        key = temp[0] + "-" + temp[1][1:]
+
+        print(cal.month_name[int(temp[1])], temp[0])
+        for i in readings:
+            if key in i.date and not (i.max_temp == ''
+                                      and i.min_temp == ''
+                                      and i.mean_humidity == ''):
+
+                # UNCOMMENT ONE OF ANY BAR CHART FORMATS
+                display_single_bar_chart(day_index, i.min_temp, i.max_temp)
+                # display_separate_bar_charts(day_index, i.min_temp, i.max_temp)
+
+                day_index += 1
 
 
-def generate_bar_chart(max_temp, min_temp, day):
-    print("0%d " % day, end="")
-    for i in range(0, max_temp + min_temp):
-        if i < min_temp:
-            ut.print_blue("+")
-        else:
-            ut.print_red("+")
-
-    print(" %dC - %dC" % (min_temp, max_temp))
+def display_single_bar_chart(day, min_temp, max_temp):
+    print("%d " % day, end="")
+    ut.print_blue("+", int(min_temp))
+    ut.print_red("+", int(max_temp))
+    print(" %sC - %sC" % (min_temp, max_temp))
 
 
-def high_temp_bar_chart(max_temp, day):
-    print("0%d " % day, end="")
-    for i in range(0, max_temp):
-        print(colored('+', 'red'), end="")
-    print(" %dC" % max_temp)
+def display_separate_bar_charts(day, min_temp, max_temp):
+    print("%d " % day, end="")
+    ut.print_red("+", int(max_temp))
+    print(" %sC" % max_temp)
+
+    print("%d " % day, end="")
+    ut.print_blue("+", int(min_temp))
+    print(" %sC" % min_temp)
 
 
-def low_temp_bar_chart(min_temp, day):
-    print("0%d " % day, end="")
-    for i in range(0, min_temp):
-        print(colored('+', 'blue'), end="")
-    print(" %dC" % min_temp)
+def get_date_params(string):
+    string = string.split("-")
+    return [string[0], string[1], string[2]]
+
+
+def generate_yearly_report(max_node, min_node, humidity_node):
+    year_max, month_max, date_max = get_date_params(max_node.date)
+    year_min, month_min, date_min = get_date_params(min_node.date)
+    year_humid, month_humid, date_humid = get_date_params(humidity_node.date)
+
+    print("Highest: %sC on %s %s" % (max_node.max_temp, cal.month_name[int(month_max)], date_max))
+    print("Lowest: %sC on %s %s" % (min_node.min_temp, cal.month_name[int(month_min)], date_min))
+    print("Humidity: %s%% on %s %s" % (humidity_node.max_humidity, cal.month_name[int(month_humid)], date_humid))
+    print('\n')
 
 
 def generate_monthly_report(avg_max_temp, avg_lowest_temp, avg_mean_humidity):
     print("Highest Average: %dC" % avg_max_temp)
     print("Lowest Average: %dC" % avg_lowest_temp)
     print("Average Mean Humidity: %d%%" % avg_mean_humidity)
+    print('\n')
 
 
-def generate_yearly_report(max_temp, max_temp_month, max_temp_day,
-                           min_temp, min_temp_month, min_temp_day,
-                           max_humidity, max_humidity_month, max_humidity_day):
-    print("Highest: %dC on %s %d" %
-          (max_temp,
-           max_temp_month,
-           max_temp_day))
-
-    print("Lowest: %dC on %s %d" %
-          (min_temp,
-           min_temp_month,
-           min_temp_day))
-
-    print("Humidity: %d%% on %s %d" %
-          (max_humidity,
-           max_humidity_month,
-           max_humidity_day))
-
-
-def main(args):
+def main(arguments):
     readings_list = []
-    parse_files(args, readings_list)
-    calculate_results(readings_list)
+    parse_files(arguments.directory, readings_list)
+    calculate_results(arguments, readings_list)
 
 
 if __name__ == '__main__':
--- utility.py
+++ utility.py
@@ -1,84 +1,11 @@
-def print_blue(text):
-    print("\033[96m{}\033[00m".format(text), end="")
+def print_blue(text, n):
+    print("\033[96m{}\033[00m".format(text) * n, end="")
 
 
-def print_red(text):
-    print("\033[31m{}\033[00m".format(text), end="")
+def print_red(text, n):
+    print("\033[31m{}\033[00m".format(text) * n, end="")
 
 
 def get_file_location(year, month, city, directory):
-    file_name = city + "_" + year + "_" + month + ".txt"
-    return directory + file_name
-
-
-def find_max(read_list):
-    max = int(read_list[0].max_temp)
-    max_index = 0
-
-    for i in range(1, len(read_list)):
-        if read_list[i].max_temp != '':
-            if max < int(read_list[i].max_temp):
-                max = int(read_list[i].max_temp)
-                max_index = i
-
-    return [max, max_index]
-
-
-def find_avg_highest(read_list):
-    sum = 0
-    divisor = 0
-
-    for i in range(0, len(read_list)):
-        if read_list[i].max_temp != '':
-            sum += int(read_list[i].max_temp)
-            divisor += 1
-
-    return sum / divisor
-
-
-def find_avg_lowest(read_list):
-    sum = 0
-    divisor = 0
-    for i in range(0, len(read_list)):
-        if read_list[i].min_temp != '':
-            sum += int(read_list[i].min_temp)
-            divisor += 1
-
-    return sum / divisor
-
-
-def find_avg_mean_humidity(read_list):
-    sum = 0
-    divisor = 0
-    for i in range(0, len(read_list)):
-        if read_list[i].mean_humidity != '':
-            sum += int(read_list[i].mean_humidity)
-            divisor += 1
-
-    return sum / divisor
-
-
-def find_lowest(read_list):
-    min = int(read_list[0].min_temp)
-    min_index = 0
-
-    for i in range(1, len(read_list)):
-        if read_list[i].min_temp != '':
-            if min > int(read_list[i].min_temp):
-                min = int(read_list[i].min_temp)
-                min_index = i
-
-    return [min, min_index]
-
-
-def find_max_humidity(read_list):
-    max = int(read_list[0].max_humidity)
-    max_index = 0
-
-    for i in range(1, len(read_list)):
-        if read_list[i].max_humidity != '':
-            if max < int(read_list[i].max_humidity):
-                max = int(read_list[i].max_humidity)
-                max_index = i
-
-    return [max, max_index]
+    file_name = city + "_" + str(year) + "_" + month + ".txt"
+    return directory + "/" + file_name
