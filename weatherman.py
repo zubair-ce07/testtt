e94cba82a9607d09d1aaa5baae8258b86038fc11
arslanather <arslan.ather@arbisoft.com>
2018-07-10 12:07:51 +0500

    Updated file reading to read all reading and then filter as needed by arguments types

--- weatherman.py
+++ weatherman.py
@@ -1,9 +1,9 @@
 import argparse
 import csv
-import datetime
 import re
 from os import path
 from glob import glob
+from datetime import datetime
 from weatherman_ds import WeatherReading
 from weatherman_calculation import WeatherReport
 
@@ -25,15 +25,15 @@ def main():
     args = parser.parse_args()
 
     reading_file_paths = glob(args.directory+'/*.txt')
-    weather_reading_files = []
+    weather_readings = []
     for file_path in reading_file_paths:
-            opened_file = open(file_path, 'r')
-            weather_reading_files.append(opened_file)
+            with open(file_path, 'r') as opened_reading_file:
+                weather_reading_csv = csv.DictReader(opened_reading_file)
+                for row in weather_reading_csv:
+                    weather_readings.append(WeatherReading(row))
 
     if args.extreme:
-        input_date_extreme = args.extreme
-        weather_readings_extreme = load_readings(weather_reading_files,
-                                                 input_date_extreme)
+        weather_readings_extreme = list(filter(lambda x: x.pkt.startswith(args.extreme), weather_readings))
         if weather_readings_extreme:
             extreme_weather_results = WeatherReport.yearly_results(weather_readings_extreme)
             weather_report_extreme(extreme_weather_results)
@@ -42,9 +42,8 @@ def main():
     if args.average:
         input_date_average = args.average
         input_date_average = input_date_average.split('/')
-        weather_readings_average = load_readings(weather_reading_files,
-                                                 input_date_average[0],
-                                                 input_date_average[1])
+        average_weather_regex = input_date_average[0]+'-'+str(int(input_date_average[1]))+'-'
+        weather_readings_average = list(filter(lambda x: re.match(average_weather_regex, x.pkt), weather_readings))
         if weather_readings_average:
             average_weather_results = WeatherReport.monthly_results(weather_readings_average)
             weather_report_average(average_weather_results)
@@ -54,41 +53,21 @@ def main():
     if args.chart:
         input_date_chart = args.chart
         input_date_chart = input_date_chart.split('/')
-        weather_readings_chart = load_readings(weather_reading_files,
-                                               input_date_chart[0],
-                                               input_date_chart[1])
+        chart_weather_regex = input_date_chart[0]+'-'+str(int(input_date_chart[1]))+'-'
+        weather_readings_chart = list(filter(lambda x: re.match(chart_weather_regex, x.pkt), weather_readings))
         if weather_readings_chart:
             weather_report_chart(weather_readings_chart)
         else:
             print("Readings not available for " + args.chart)
 
 
-def load_readings(reading_files='', year='', month=''):
-        """Loads a given months data in the data structure"""
-        daily_readings = []
-        if not month:
-            expression = r'.*_'+year+'_.*'
-            files_needed = list(filter(lambda x: re.match(expression, x.name), reading_files))
-        else:
-            month_name = datetime.date(2000, int(month), 1).strftime('%b')
-            expression = r'.*_' + year + '_'+month_name+'.*'
-            files_needed = list(filter(lambda x: re.match(expression, x.name), reading_files))
-        for weather_reading_file in files_needed:
-            weather_csv = csv.DictReader(weather_reading_file)
-            weather_reading_file.seek(0)
-            for row in weather_csv:
-                daily_readings.append(WeatherReading(row))
-
-        return daily_readings
-
-
 def weather_report_extreme(result):
     """"Displays the highest lowest temperature and highest humidity report"""
-    high_day = datetime.datetime.strptime(result.highest_day, "%Y-%m-%d").strftime('%B %d')
+    high_day = datetime.strptime(result.highest_day, "%Y-%m-%d").strftime('%B %d')
     print("Highest: " + str(result.highest_reading) + "C on " + high_day)
-    low_day = datetime.datetime.strptime(result.lowest_day, "%Y-%m-%d").strftime('%B %d')
+    low_day = datetime.strptime(result.lowest_day, "%Y-%m-%d").strftime('%B %d')
     print("Lowest: " + str(result.lowest_reading) + "C on " + low_day)
-    humid_day = datetime.datetime.strptime(result.humidity_day, "%Y-%m-%d").strftime('%B %d')
+    humid_day = datetime.strptime(result.humidity_day, "%Y-%m-%d").strftime('%B %d')
     print("Humidity: " + str(result.humidity_reading) + "% on " + humid_day)
     print("")
 
@@ -103,17 +82,15 @@ def weather_report_average(result):
 
 def weather_report_chart(weather_readings):
     """"Displays the bar chart for temperatures through the month"""
-    report_month = datetime.datetime.strptime(weather_readings[0].pkt, "%Y-%m-%d").strftime('%B %Y')
+    report_month = datetime.strptime(weather_readings[0].pkt, "%Y-%m-%d").strftime('%B %Y')
 
     print(report_month)
     for reading in weather_readings:
-        day_number = datetime.datetime.strptime(reading.pkt, "%Y-%m-%d").strftime('%d')
-        max_temp = reading.max_temperature
-        min_temp = reading.min_temperature
-        if max_temp and min_temp:
-            print(day_number + ColorOutput.BLUE + "+" * min_temp + ColorOutput.RED +
-                  "+" * max_temp + ColorOutput.RESET + str(min_temp) +
-                  "C-" + str(max_temp) + "C")
+        day_number = datetime.strptime(reading.pkt, "%Y-%m-%d").strftime('%d')
+        if reading.max_temperature and reading.min_temperature:
+            print(day_number + ColorOutput.BLUE + "+" * reading.min_temperature + ColorOutput.RED +
+                  "+" * reading.max_temperature + ColorOutput.RESET + str(reading.min_temperature) +
+                  "C-" + str(reading.max_temperature) + "C")
     print("")
 
 
