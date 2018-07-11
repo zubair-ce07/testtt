4dc6880020b493fccff1d26eb4cbcede4d7365d6
arslanather <arslan.ather@arbisoft.com>
2018-07-11 17:58:53 +0500

    Seperated calculation function into multiple functions

--- weatherman_calculation.py
+++ weatherman_calculation.py
@@ -6,8 +6,8 @@ class WeatherReport:
 
     @staticmethod
     def calculate_extreme_for_year(weather_records):
-        mean_records = list(filter(lambda reading: reading.mean_temperature, weather_records))
-        humidity_records = list(filter(lambda reading: reading.mean_humidity, weather_records))
+        mean_records = WeatherReport.filter_by_attribute(weather_records, 'mean_temperature')
+        humidity_records = WeatherReport.filter_by_attribute(weather_records, 'mean_humidity')
 
         minimum_reading = min(mean_records, key=attrgetter('mean_temperature'))
         maximum_reading = max(mean_records, key=attrgetter('mean_temperature'))
@@ -19,12 +19,15 @@ class WeatherReport:
 
     @staticmethod
     def calculate_average_for_month(weather_records):
-        high_records = list(filter(lambda reading: reading.max_temperature, weather_records))
-        low_records = list(filter(lambda reading: reading.min_temperature, weather_records))
-        humidity_records = list(filter(lambda reading: reading.mean_humidity, weather_records))
-
+        high_records = WeatherReport.filter_by_attribute(weather_records, 'max_temperature')
+        low_records = WeatherReport.filter_by_attribute(weather_records, 'min_temperature')
+        humidity_records = WeatherReport.filter_by_attribute(weather_records, 'mean_humidity')
         avg_high = sum(reading.max_temperature for reading in high_records) // len(high_records)
         avg_low = sum(reading.min_temperature for reading in low_records) // len(low_records)
         avg_humid = sum(reading.mean_humidity for reading in humidity_records) // len(humidity_records)
 
         return WeatherResult('', avg_low, '', avg_high, '', avg_humid)
+
+    @staticmethod
+    def filter_by_attribute(weather_records, attribute):
+        return list(filter(attrgetter(attribute), weather_records))
