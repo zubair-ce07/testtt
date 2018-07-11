3bb17c5b257db6a15505990ca2aa5e20d68b7ee2
arslanather <arslan.ather@arbisoft.com>
2018-07-11 17:35:11 +0500

    date in datastructure now stores as datetime object

--- weatherman_ds.py
+++ weatherman_ds.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
+
 class WeatherReading:
 
     def __init__(self, readings):
-        self.pkt = readings.get('PKT', readings.get('PKST'))
+        self.pkt = datetime.strptime(readings.get('PKT', readings.get('PKST')), '%Y-%m-%d')
         self.max_temperature = (int(readings.get('Max TemperatureC'))
                                 if readings.get('Max TemperatureC') else None)
         self.min_temperature = (int(readings.get('Min TemperatureC'))
@@ -18,8 +21,8 @@ class WeatherReading:
 
 class WeatherResult:
     """"Data structure to hold the results calculated by calculation module"""
-    def __init__(self, high_day='', lowest=0, low_day='',
-                 highest=0, humid_day='', humidity=0):
+    def __init__(self, high_day, lowest, low_day,
+                 highest, humid_day, humidity):
         self.highest_reading = highest
         self.highest_day = low_day
         self.lowest_reading = lowest
