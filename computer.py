96aa353fa27f3388c687701f4e4cc4706a46876c
WadoodAmer <wadood.islam@arbisoft.com>
2018-07-11 18:45:29 +0500

    1) Made Computer Module Responsible. 2) Used Lamda functions for list comprehension. 3) Refactored.

--- computer.py
+++ computer.py
@@ -1,16 +1,14 @@
 from statistics import mean
 
 
-def result_for_e(data):
-    yearly_max_temperature = max([wr for wr in data if wr.max_temperature], key=lambda wr: wr.max_temperature)
-    yearly_min_temperature = min([wr for wr in data if wr.min_temperature], key=lambda wr: wr.max_temperature)
-    yearly_max_humidity = max([wr for wr in data if wr.max_humidity], key=lambda wr: wr.max_humidity)
-    return [yearly_max_temperature, yearly_min_temperature, yearly_max_humidity]
+def max_value(data, key):
+    return max([wr for wr in data if key(wr)], key=key)
 
 
-def result_for_a(data):
-    max_avg_temperature = max([wr for wr in data if wr.mean_temperature], key=lambda wr: wr.mean_temperature)
-    min_avg_temperature = min([wr for wr in data if wr.mean_temperature], key=lambda wr: wr.mean_temperature)
-    avg_mean_humidity = int(mean(wr.mean_humidity for wr in data if wr.mean_humidity))
-    avg_mean_humidity = next((wr for wr in data if wr.mean_humidity == avg_mean_humidity), None)
-    return [max_avg_temperature, min_avg_temperature, avg_mean_humidity]
+def min_value(data, key):
+    return min([wr for wr in data if key(wr)], key=key)
+
+
+def mean_value(data, function):
+    avg = int(mean(function(wr) for wr in data if function(wr)))
+    return next((wr for wr in data if function(wr) == avg), None)
--- reporter.py
+++ reporter.py
@@ -6,22 +6,26 @@ class Reporter:
         self.__data = data
 
     def report_for_e(self):
-        result_e = result_for_e(self.__data.readings)
+        max_temperature = max_value(self.__data.readings, lambda wr: wr.max_temperature)
+        min_temperature = min_value(self.__data.readings, lambda wr: wr.min_temperature)
+        max_humidity = max_value(self.__data.readings, lambda wr: wr.max_humidity)
         print("Highest: {}C on {}".format(
-            result_e[0].max_temperature,
-            result_e[0].date.strftime("%B %d")))
+            max_temperature.max_temperature,
+            max_temperature.date.strftime("%B %d")))
         print("Lowest: {}C on {}".format(
-            result_e[1].min_temperature,
-            result_e[1].date.strftime("%B %d")))
+            min_temperature.min_temperature,
+            min_temperature.date.strftime("%B %d")))
         print("Humidity: {}% on {}".format(
-            result_e[2].max_humidity,
-            result_e[2].date.strftime("%B %d")))
+            max_humidity.max_humidity,
+            max_humidity.date.strftime("%B %d")))
 
     def report_for_a(self):
-        result_a = result_for_a(self.__data.readings)
-        print("Highest Average: {}C".format(result_a[0].mean_temperature))
-        print("Lowest Average: {}C".format(result_a[1].mean_temperature))
-        print("Average Mean Humidity: {}%".format(result_a[2].mean_humidity))
+        max_mean_temperature = max_value(self.__data.readings, lambda wr: wr.mean_temperature)
+        min_mean_temperature = min_value(self.__data.readings, lambda wr: wr.mean_temperature)
+        avg_mean_humidity = mean_value(self.__data.readings, lambda wr: wr.mean_humidity)
+        print("Highest Average: {}C".format(max_mean_temperature.mean_temperature))
+        print("Lowest Average: {}C".format(min_mean_temperature.mean_temperature))
+        print("Average Mean Humidity: {}%".format(avg_mean_humidity.mean_humidity))
 
     def report_for_c(self):
         print(self.__data.month, self.__data.year)
