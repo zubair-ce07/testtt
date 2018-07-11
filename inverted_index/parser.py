cc5cfebdc801c06fc8f0650af31dcf5c1eb97ca4
Moeez Zahid <moeezzahid1996@gmail.com>
2018-07-11 12:25:42 +0500

    Moeezzahid - Search Engine (Task 5) (#841)
    * Feedback(KK) implemented

---
+++ inverted_index/parser.py
@@ -0,0 +1,29 @@
+__author__ = 'abdul'
+
+from html.parser import HTMLParser
+
+import constants
+
+
+class HtmlParser(HTMLParser):
+
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.isRecording = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'p':
+            self.isRecording = True
+
+    def handle_endtag(self, tag):
+        if tag == 'p':
+            self.isRecording = False
+
+    def handle_data(self, data):
+        """
+        If data belongs to <p> tag,
+        Write data to file.
+        """
+        if self.isRecording and data != '\n':
+            with open(constants.parsed_data_file_name, 'a') as f:
+                    f.write("{}\n".format(data.strip('\n')))
