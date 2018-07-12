904cb92271409df53b72922b920e29fbe0aad30c
zeba ramzan <zeba.ramzan@arbisoft.com>
2018-07-12 15:18:17 +0500

    removed directory

--- WeatherAssignment/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import WeatherResultList from './reducer_weather_list';
-
-const rootReducer = combineReducers({
-    weather: WeatherResultList
-});
-
-export default rootReducer;
