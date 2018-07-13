f9f600183985693a32beef7b684cfd133795884f
Muhammad Ejaz <muhammad.ejaz@arbisoft.com>
2018-07-13 18:05:01 +0500

    final commit on jul 13

--- result_data.py
+++
@@ -1,6 +0,0 @@
-class ResultData:
-    def __init__(self):
-        self.year = {}
-        self.month_average = {}
-        self.month_chart = {}
-        self.bonus = {}
--- weather_data.py
+++
@@ -1,6 +0,0 @@
-class WeatherData:
-    def __init__(self):
-        self.max_temperature = {}
-        self.min_temperature = {}
-        self.max_humidity = {}
-        self.mean_humidity = {}
