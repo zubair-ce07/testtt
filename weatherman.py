27a0be5ff52e22c60bf984a8125e5a4670cd6a06
zeba ramzan <zeba.ramzan@arbisoft.com>
2018-07-13 10:14:25 +0500

    used str_to_date func

--- weatherman.py
+++ weatherman.py
@@ -25,97 +25,90 @@ def parse_files(directory):
     return readings
 
 
-def print_report(report_contents, seperator):
-    print(seperator.join(report_contents))
+def average_report_generator(readings_result, report_date):
+    header = f"{Colors.GREEN}\n{'*' * 12}Average Readings of "\
+             f"{report_date} {'*' * 12}\n{Colors.RESET}"
+
+    highest_avg = f"Highest Average: {readings_result.max_mean_temp}C"
+    lowest_average = f"Lowest Average: {readings_result.min_mean_temp}C"
+    mean_humidity = f"Average Mean Humidity: "\
+        f"{int(readings_result.average_mean_humidity)}%\n"
+
+    results = [
+        header,
+        highest_avg,
+        lowest_average,
+        mean_humidity
+    ]
+
+    print('\n'.join(results))
 
 
-def report_generator(report_type, readings_result, report_date):
-    if report_type == 'a':
+def extreme_report_generator(readings_result, report_date):
+    header = f"{Colors.GREEN}\n{'*' * 12} Extreme Readings of "\
+             f"{report_date} {'*' * 12}\n{Colors.RESET}"
 
-        header = f"{Colors.GREEN}\n{'*' * 12}Average Readings of "\
-                 f"{report_date} {'*' * 12}\n{Colors.RESET}"
+    highest = f"Highest: {readings_result.maximum_temp}"\
+        f"C on {readings_result.maximum_temp_day}"
 
-        highest_avg = f"Highest Average: {readings_result.max_mean_temp}C"
-        lowest_average = f"Lowest Average: {readings_result.min_mean_temp}C"
-        mean_humidity = f"Average Mean Humidity: "\
-            f"{int(readings_result.average_mean_humidity)}%\n"
+    lowest = f"Lowest: {readings_result.minimum_temp}C on "\
+        f"{readings_result.minimum_temp_day}"
 
-        results = [
-            header,
-            highest_avg,
-            lowest_average,
-            mean_humidity
-        ]
+    humidity = f"Humidity: {readings_result.maximum_humidity}% on "\
+        f"{readings_result.maximum_humidity_day}"
 
-        return results
+    results = [
+        header,
+        highest,
+        lowest,
+        humidity
+    ]
 
-    elif report_type == 'e':
+    print('\n'.join(results))
 
-        header = f"{Colors.GREEN}\n{'*' * 12} Extreme Readings of "\
-                 f"{report_date} {'*' * 12}\n{Colors.RESET}"
 
-        highest = f"Highest: {readings_result.maximum_temp}"\
-            f"C on {readings_result.maximum_temp_day}"
+def charts_report_generator(readings_result, report_date):
+    day = f"{Colors.MAGENTA}{int(report_date)}"
+    symbols = f"{Colors.BLUE}{'+' * int(readings_result[1])}"\
+        f"{Colors.RED}{'+' * int(readings_result[0])}"
+    min_temp = f"{Colors.MAGENTA} {int(readings_result[1])}C - "
+    max_temp = f"{int(readings_result[0])}C{Colors.RESET}"
 
-        lowest = f"Lowest: {readings_result.minimum_temp}C on "\
-            f"{readings_result.minimum_temp_day}"
+    results = [
+        day,
+        symbols,
+        min_temp,
+        max_temp
+    ]
 
-        humidity = f"Humidity: {readings_result.maximum_humidity}% on "\
-            f"{readings_result.maximum_humidity_day}"
-
-        results = [
-            header,
-            highest,
-            lowest,
-            humidity
-        ]
-
-        return results
-
-    else:
-        day = f"{Colors.MAGENTA}{int(report_date)}"
-        symbols = f"{Colors.BLUE}{'+' * int(readings_result[1])}"\
-            f"{Colors.RED}{'+' * int(readings_result[0])}"
-        min_temp = f"{Colors.MAGENTA} {int(readings_result[1])}C - "
-        max_temp = f"{int(readings_result[0])}C{Colors.RESET}"
-
-        results = [
-            day,
-            symbols,
-            min_temp,
-            max_temp
-        ]
-
-        return results
+    print(' '.join(results))
 
 
 def readings_of_year(given_date, weather_readings):
-    readings_result = weatherman_computations.calculate_readings(
-                      weather_readings, 'e', given_date)
-    report = report_generator('e', readings_result,
-                              given_date.year)
-    print_report(report, '\n')
+    readings_result = weatherman_computations.calculate_extreme_readings(
+                      weather_readings, given_date)
+    extreme_report_generator(readings_result,
+                             given_date.year)
 
 
 def average_of_date(given_date, weather_readings):
-    readings_result = weatherman_computations.calculate_readings(
-                      weather_readings, 'a', given_date)
-    report = report_generator('a', readings_result, given_date)
-    print_report(report, '\n')
+    readings_result = weatherman_computations.calculate_average_readings(
+                      weather_readings, given_date)
+    average_report_generator(readings_result,
+                             given_date.strftime("%Y/%m"))
 
 
 def charts(given_date, weather_readings):
     print(f"{Colors.GREEN}\n************ Temperature chart for "
           f"{given_date.year}/{given_date.month}"
           f" ************\n{Colors.RESET}")
-    for row in weather_readings:
-        date_from_file = datetime.strptime(row.pkt, '%Y-%m-%d')
+    for reading in weather_readings:
+        date_from_file = weatherman_computations.str_to_date(reading.pkt)
         if(date_from_file.year == given_date.year and
            date_from_file.month == given_date.month):
-            report = report_generator('c', [row.max_temp,
-                                      row.min_temp],
-                                      str(date_from_file.day))
-            print_report(report, ' ')
+            charts_report_generator([reading.max_temp,
+                                     reading.min_temp],
+                                    str(date_from_file.day))
 
 
 def main():
@@ -124,13 +117,13 @@ def main():
     parser.add_argument("directory_path", type=str,
                         help="Path to the directory containing data files")
     parser.add_argument("-a", "--month", type=lambda m:
-                        datetime.strptime(m, '%Y/%m'),
+                        weatherman_computations.str_to_date(m, format='%Y/%m'),
                         action='append', nargs='*', default=[])
     parser.add_argument("-e", "--year", type=lambda y:
-                        datetime.strptime(y, '%Y'),
+                        weatherman_computations.str_to_date(y, format='%Y'),
                         action='append', nargs='*', default=[])
     parser.add_argument("-c", "--chart", type=lambda c:
-                        datetime.strptime(c, '%Y/%m'),
+                        weatherman_computations.str_to_date(c, format='%Y/%m'),
                         action='append', nargs='*', default=[])
     args = parser.parse_args()
 
