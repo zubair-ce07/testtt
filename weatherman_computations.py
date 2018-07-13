dd0fb71bbee693ea8678928fa2ec921dd053c1dd
zeba ramzan <zeba.ramzan@arbisoft.com>
2018-07-13 10:15:03 +0500

    used strftime instead of indexing and removed if else statements

--- weatherman_computations.py
+++ weatherman_computations.py
@@ -5,44 +5,37 @@ from statistics import mean
 from weatherman_data_structure import CalculationHolder
 
 
-def str_to_date(str_date):
-    return datetime.strptime(str_date, '%Y-%m-%d')
+def str_to_date(str_date, format='%Y-%m-%d'):
+    return datetime.strptime(str_date, format)
 
 
-def extreme_values_days(weather_readings, max_temp, min_temp,
-                        max_humid, extreme_type):
-    if extreme_type == 'max':
-        for row in weather_readings:
-            if row.max_temp == max_temp:
-                highest_temp_day = str_to_date(row.pkt).ctime()[:-14]
-        return highest_temp_day
-    elif extreme_type == 'min':
-        for row in weather_readings:
-            if row.min_temp == min_temp:
-                lowest_temp_day = str_to_date(row.pkt).ctime()[:-14]
-        return lowest_temp_day
-    else:
-        for row in weather_readings:
-            if row.max_humidity == max_humid:
-                max_humidity_day = str_to_date(row.pkt).ctime()[:-14]
-        return max_humidity_day
+def extreme_values_days(weather_readings, max_temp, min_temp, max_humid):
+    for reading in weather_readings:
+        if reading.max_temp == max_temp:
+            highest_temp_day = str_to_date(reading.pkt).strftime("%A %B %d")
+        if reading.min_temp == min_temp:
+            lowest_temp_day = str_to_date(reading.pkt).strftime("%A %B %d")
+        if reading.max_humidity == max_humid:
+            max_humidity_day = str_to_date(reading.pkt).strftime("%A %B %d")
+
+    return {'max': highest_temp_day, 'min': lowest_temp_day,
+            'humid': max_humidity_day}
 
 
 def calculate_extreme_readings(weather_readings, given_date):
 
-    ext_max_temp = max(row.max_temp for row in weather_readings
-                       if str_to_date(row.pkt).year == given_date.year)
-    ext_min_temp = min(row.min_temp for row in weather_readings
-                       if str_to_date(row.pkt).year == given_date.year)
-    ext_max_humid = max(row.max_humidity for row in weather_readings
-                        if str_to_date(row.pkt).year == given_date.year)
+    ext_max_temp = max(reading.max_temp for reading in weather_readings
+                       if str_to_date(reading.pkt).year == given_date.year)
+    ext_min_temp = min(reading.min_temp for reading in weather_readings
+                       if str_to_date(reading.pkt).year == given_date.year)
+    ext_max_humid = max(reading.max_humidity for reading in weather_readings
+                        if str_to_date(reading.pkt).year == given_date.year)
 
-    max_temp_day = extreme_values_days(weather_readings, ext_max_temp,
-                                       ext_min_temp, ext_max_humid, 'max')
-    min_temp_day = extreme_values_days(weather_readings, ext_max_temp,
-                                       ext_min_temp, ext_max_humid, 'min')
-    max_humid_day = extreme_values_days(weather_readings, ext_max_temp,
-                                        ext_min_temp, ext_max_humid, 'humid')
+    extreme_days = extreme_values_days(weather_readings, ext_max_temp,
+                                       ext_min_temp, ext_max_humid)
+    max_temp_day = extreme_days.get('max')
+    min_temp_day = extreme_days.get('min')
+    max_humid_day = extreme_days.get('humid')
 
     return CalculationHolder(max_temp=ext_max_temp, min_temp=ext_min_temp,
                              max_humidity=ext_max_humid,
@@ -52,13 +45,13 @@ def calculate_extreme_readings(weather_readings, given_date):
 
 
 def calculate_average_readings(weather_readings, given_date):
-    mean_temps = list(row.mean_temp for row in weather_readings
-                      if str_to_date(row.pkt).year == given_date.year and
-                      str_to_date(row.pkt).month == given_date.month)
+    mean_temps = list(reading.mean_temp for reading in weather_readings
+                      if str_to_date(reading.pkt).year == given_date.year and
+                      str_to_date(reading.pkt).month == given_date.month)
 
-    mean_humid = mean(row.mean_humidity for row in weather_readings
-                      if str_to_date(row.pkt).year == given_date.year and
-                      str_to_date(row.pkt).month == given_date.month)
+    mean_humid = mean(reading.mean_humidity for reading in weather_readings
+                      if str_to_date(reading.pkt).year == given_date.year and
+                      str_to_date(reading.pkt).month == given_date.month)
 
     highest_mean_temp = max(mean_temps)
     lowest_mean_temp = min(mean_temps)
@@ -66,11 +59,3 @@ def calculate_average_readings(weather_readings, given_date):
     return CalculationHolder(max_mean_temp=highest_mean_temp,
                              min_mean_temp=lowest_mean_temp,
                              avg_mean_humidity=mean_humid)
-
-
-def calculate_readings(weather_readings, report_type, given_date):
-    if report_type == 'e':
-        return calculate_extreme_readings(weather_readings, given_date)
-
-    if report_type == 'a':
-        return calculate_average_readings(weather_readings, given_date)
