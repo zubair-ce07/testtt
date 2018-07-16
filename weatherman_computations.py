8ef34795142e33a310e092c94d1b324e62c3ff25
zeba ramzan <zeba.ramzan@arbisoft.com>
2018-07-16 18:35:25 +0500

    used attrgetter

--- weatherman_computations.py
+++ weatherman_computations.py
@@ -1,63 +1,41 @@
 from statistics import mean
 from collections import namedtuple
+from operator import attrgetter
 
 
-def extreme_values_days(weather_readings, max_temp, min_temp, max_humid):
-    for reading in weather_readings:
-        if reading.max_temp == max_temp:
-            highest_temp_day = reading.pkt.strftime("%A %B %d")
-        if reading.min_temp == min_temp:
-            lowest_temp_day = reading.pkt.strftime("%A %B %d")
-        if reading.max_humidity == max_humid:
-            max_humidity_day = reading.pkt.strftime("%A %B %d")
+def calculate_ext_readings(weather_readings):
 
-    days_ = namedtuple('days_', 'max, min, humid')
+    max_temperatures = list(filter(attrgetter('max_temp'), weather_readings))
+    min_temperatures = list(filter(attrgetter('min_temp'), weather_readings))
+    max_humidity = list(filter(attrgetter('max_humidity'), weather_readings))
 
-    return days_(highest_temp_day, lowest_temp_day,
-                 max_humidity_day)
-
-
-def calculate_extreme_readings(weather_readings, given_date):
-
-    max_temperature = max(reading.max_temp for reading in weather_readings
-                          if reading.pkt.year == given_date.year)
-    min_temperature = min(reading.min_temp for reading in weather_readings
-                          if reading.pkt.year == given_date.year)
-    max_humid = max(reading.max_humidity for reading in weather_readings
-                    if reading.pkt.year == given_date.year)
-
-    extreme_days = extreme_values_days(weather_readings, max_temperature,
-                                       min_temperature, max_humid)
-    max_temp_day = extreme_days.max
-    min_temp_day = extreme_days.min
-    max_humid_day = extreme_days.humid
+    max_temperature = max(max_temperatures, key=attrgetter('max_temp'))
+    min_temperature = min(min_temperatures, key=attrgetter('min_temp'))
+    max_humid = max(max_humidity, key=attrgetter('max_humidity'))
 
     calculation_holder = namedtuple('calculation_holder', 'maximum_temp,\
                                     minimum_temp, maximum_humidity,\
                                     maximum_temp_day, minimum_temp_day,\
                                     maximum_humidity_day')
-    return calculation_holder(max_temperature,
-                              min_temperature,
-                              max_humid,
-                              max_temp_day,
-                              min_temp_day,
-                              max_humid_day)
+    return calculation_holder(max_temperature.max_temp,
+                              min_temperature.min_temp,
+                              max_humid.max_humidity,
+                              max_temperature.pkt.strftime("%A %B %d"),
+                              min_temperature.pkt.strftime("%A %B %d"),
+                              max_humid.pkt.strftime("%A %B %d"))
 
 
-def calculate_average_readings(weather_readings, given_date):
-    mean_temps = list(reading.mean_temp for reading in weather_readings
-                      if reading.pkt.year == given_date.year and
-                      reading.pkt.month == given_date.month)
+def calculate_avg_readings(weather_readings):
+    mean_temps = list(filter(attrgetter('mean_temp'), weather_readings))
+    mean_humid = list(filter(attrgetter('mean_humidity'), weather_readings))
 
-    mean_humid = mean(reading.mean_humidity for reading in weather_readings
-                      if reading.pkt.year == given_date.year and
-                      reading.pkt.month == given_date.month)
+    mean_value = mean(m.mean_humidity for m in mean_humid)
 
-    highest_mean_temp = max(mean_temps)
-    lowest_mean_temp = min(mean_temps)
+    highest_mean_temp = max(mean_temps, key=attrgetter('mean_temp'))
+    lowest_mean_temp = min(mean_temps, key=attrgetter('mean_temp'))
 
     calculation_holder = namedtuple('calculation_holder', 'max_mean_temp,\
                                     min_mean_temp, average_mean_humidity')
-    return calculation_holder(highest_mean_temp,
-                              lowest_mean_temp,
-                              mean_humid)
+    return calculation_holder(highest_mean_temp.mean_temp,
+                              lowest_mean_temp.mean_temp,
+                              mean_value)
