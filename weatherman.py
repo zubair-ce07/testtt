89b7c9b3a0e75bc18c11e9d79d572a7543d5ec86
zeba ramzan <zeba.ramzan@arbisoft.com>
2018-07-16 13:14:01 +0500

    handled relative path

--- weatherman.py
+++ weatherman.py
@@ -10,6 +10,9 @@ import weatherman_computations
 
 
 def parse_files(directory):
+    if not os.path.isabs(directory):
+        directory = os.path.dirname(os.path.abspath(__file__)
+                    ) + directory.strip(".")
     readings = []
     required_features = ['Max TemperatureC', 'Min TemperatureC',
                          'Mean TemperatureC', 'Max Humidity',
@@ -23,28 +26,28 @@ def parse_files(directory):
     return readings
 
 
-def average_report_generator(readings_result, report_date):
-    header = f"{Colors.GREEN}\n{'*' * 12}Average Readings of "\
-             f"{report_date} {'*' * 12}\n{Colors.RESET}"
-
-    highest_avg = f"Highest Average: {readings_result.max_mean_temp}C"
-    lowest_average = f"Lowest Average: {readings_result.min_mean_temp}C"
-    mean_humidity = f"Average Mean Humidity: "\
-        f"{int(readings_result.average_mean_humidity)}%\n"
+def charts_report_generator(readings_result, report_date):
+    day = f"{Colors.MAGENTA}{int(report_date)}"
+    symbols = f"{Colors.BLUE}{'+' * int(readings_result[1])}"\
+        f"{Colors.RED}{'+' * int(readings_result[0])}"
+    min_temp = f"{Colors.MAGENTA} {int(readings_result[1])}C - "
+    max_temp = f"{int(readings_result[0])}C{Colors.RESET}"
 
     results = [
-        header,
-        highest_avg,
-        lowest_average,
-        mean_humidity
+        day,
+        symbols,
+        min_temp,
+        max_temp
     ]
 
-    print('\n'.join(results))
+    print(' '.join(results))
 
 
-def extreme_report_generator(readings_result, report_date):
+def display_extreme_readings(given_date, weather_readings):
+    readings_result = weatherman_computations.calculate_extreme_readings(
+                      weather_readings, given_date)
     header = f"{Colors.GREEN}\n{'*' * 12} Extreme Readings of "\
-             f"{report_date} {'*' * 12}\n{Colors.RESET}"
+             f"{given_date} {'*' * 12}\n{Colors.RESET}"
 
     highest = f"Highest: {readings_result.maximum_temp}"\
         f"C on {readings_result.maximum_temp_day}"
@@ -65,38 +68,28 @@ def extreme_report_generator(readings_result, report_date):
     print('\n'.join(results))
 
 
-def charts_report_generator(readings_result, report_date):
-    day = f"{Colors.MAGENTA}{int(report_date)}"
-    symbols = f"{Colors.BLUE}{'+' * int(readings_result[1])}"\
-        f"{Colors.RED}{'+' * int(readings_result[0])}"
-    min_temp = f"{Colors.MAGENTA} {int(readings_result[1])}C - "
-    max_temp = f"{int(readings_result[0])}C{Colors.RESET}"
-
-    results = [
-        day,
-        symbols,
-        min_temp,
-        max_temp
-    ]
-
-    print(' '.join(results))
-
-
-def get_extreme_of_readings(given_date, weather_readings):
-    readings_result = weatherman_computations.calculate_extreme_readings(
-                      weather_readings, given_date)
-    extreme_report_generator(readings_result,
-                             given_date.year)
-
-
-def get_average_of_readings(given_date, weather_readings):
+def display_average_readings(given_date, weather_readings):
     readings_result = weatherman_computations.calculate_average_readings(
                       weather_readings, given_date)
-    average_report_generator(readings_result,
-                             given_date.strftime("%Y/%m"))
+    header = f"{Colors.GREEN}\n{'*' * 12}Average Readings of "\
+             f"{given_date} {'*' * 12}\n{Colors.RESET}"
+
+    highest_avg = f"Highest Average: {readings_result.max_mean_temp}C"
+    lowest_average = f"Lowest Average: {readings_result.min_mean_temp}C"
+    mean_humidity = f"Average Mean Humidity: "\
+        f"{int(readings_result.average_mean_humidity)}%\n"
+
+    results = [
+        header,
+        highest_avg,
+        lowest_average,
+        mean_humidity
+    ]
+
+    print('\n'.join(results))
 
 
-def get_charts_of_readings(given_date, weather_readings):
+def display_charts_of_readings(given_date, weather_readings):
     print(f"{Colors.GREEN}\n************ Temperature chart for "
           f"{given_date.year}/{given_date.month}"
           f" ************\n{Colors.RESET}")
@@ -124,17 +117,13 @@ def main():
                         datetime.strptime(c, '%Y/%m'),
                         action='append', nargs='*', default=[])
     args = parser.parse_args()
-    directory_path = args.directory_path+"/"
-    if directory_path.startswith('.'):
-        abs_path = os.path.abspath(os.path.dirname(__file__))
-        directory_path = abs_path + directory_path.strip(".")
-    readings = parse_files(directory_path)
+    readings = parse_files(args.directory_path)
     for arg in args.month:
-        get_average_of_readings(arg[0], readings)
+        display_average_readings(arg[0], readings)
     for arg in args.year:
-        get_extreme_of_readings(arg[0], readings)
+        display_extreme_readings(arg[0], readings)
     for arg in args.chart:
-        get_charts_of_readings(arg[0], readings)
+        display_charts_of_readings(arg[0], readings)
 
 
 if __name__ == "__main__":
