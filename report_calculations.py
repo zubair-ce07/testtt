8f89968129b59808543b9960a2becf7a08738759
mumerfarooq000 <mfarooq@arbisoft.com>
2018-07-16 14:39:01 +0500

    reduced the complexity and extensive code.

--- report_calculations.py
+++ report_calculations.py
@@ -1,57 +1,33 @@
-def max_temperature_record(days_list):
-    return max(days_list, key=lambda day: day.max_temperature if
-               day.max_temperature is not None else 0)
+def max_temperature_record(readings):
+    readings = [day for day in readings if day.max_temperature]
+    return max(readings, key=lambda day: day.max_temperature)
 
 
-def min_temperature_record(days_list):
-    return min(days_list, key=lambda day: day.min_temperature if
-               day.min_temperature is not None else 1000)
+def min_temperature_record(readings):
+    readings = [day for day in readings if day.min_temperature]
+    return min(readings, key=lambda day: day.min_temperature)
 
 
-def max_humidity_record(days_list):
-    return max(days_list, key=lambda day: day.max_humidity if
-               day.max_humidity is not None else 0)
+def max_humidity_record(readings):
+    readings = [day for day in readings if day.max_humidity]
+    return max(readings, key=lambda day: day.max_humidity)
 
 
-def total_max_temp_recorded_days(days_list):
-    recorded_days = 1
-    for i, day in enumerate(days_list):
-        if day.max_temperature is not None:
-            recorded_days = i
-    return recorded_days
+def get_avg_max_temp(readings):
+    total_days = len([day for day in readings if day.max_temperature])
+    return sum(day.max_temperature for day in readings
+               if day.max_temperature)//total_days
 
 
-def total_min_temp_recorded_days(days_list):
-    recorded_days = 1
-    for i, day in enumerate(days_list):
-        if day.min_temperature is not None:
-            recorded_days = i
-    return recorded_days
+def get_avg_min_temp(readings):
+    total_days = len([day for day in readings if day.min_temperature])
+    return sum(day.min_temperature for day in readings
+               if day.min_temperature )//total_days
 
 
-def total_mean_humidity_recorded_days(days_list):
-    recorded_days = 1
-    for i, day in enumerate(days_list):
-        if day.mean_humidity is not None:
-            recorded_days = i
-    return recorded_days
-
-
-def get_avg_max_temp(days_list):
-    total_days = total_max_temp_recorded_days(days_list)
-    return sum(day.max_temperature for day in days_list
-               if day.max_temperature is not None)/total_days
-
-
-def get_avg_min_temp(days_list):
-    total_days = total_min_temp_recorded_days(days_list)
-    return sum(day.min_temperature for day in days_list
-               if day.min_temperature is not None)/total_days
-
-
-def get_avg_mean_humidity(days_list):
-    total_days = total_mean_humidity_recorded_days(days_list)
-    return sum(day.mean_humidity for day in days_list
-               if day.mean_humidity is not None)/total_days
+def get_avg_mean_humidity(readings):
+    total_days = len([day for day in readings if day.mean_humidity])
+    return sum(day.mean_humidity for day in readings
+               if day.mean_humidity)//total_days
 
 
--- weatherman.py
+++ weatherman.py
@@ -1,51 +1,59 @@
-from argparse import ArgumentParser
+import re
+import argparse
 
 from data_structure import *
 
 
+def validate_date(date):
+    if date:
+        pattern1 = re.compile("200[4-9]/[0-1]*[0-9]$")
+        pattern2 = re.compile("201[0-6]/[0-1]*[0-9]$")
+        if not (pattern1.match(date) or pattern2.match(date)):
+            raise argparse.ArgumentTypeError(
+                f"{date} is an invalid year/month input, '/' must be used as "
+                f"separator between year and month")
+    return date
+
+
 def main():
-    arg_parser = ArgumentParser(description='Process some integer')
+    arg_parser = argparse.ArgumentParser(description='Process some date')
     arg_parser.add_argument('path', type=str, nargs='+',
                             help='Collect the data from Directory')
-    arg_parser.add_argument('-e', type=str, nargs='+',
+    arg_parser.add_argument('-e',  type=validate_date, nargs='+',
                             help='Find the highest temperature and day, '
                                  'lowest temperature and day, most humid day '
                                  '(Single Month)')
-    arg_parser.add_argument('-a', type=str, nargs='+',
+    arg_parser.add_argument('-a', type=validate_date, nargs='+',
                             help='Find the average highest temperature,'
                                  ' average lowest temperature, average mean '
                                  'humidity (Range of Months)')
-    arg_parser.add_argument('-c', type=str, nargs='+',
+    arg_parser.add_argument('-c', type=validate_date, nargs='+',
                             help='Draws two horizontal bar charts for the'
                                  ' highest and lowest temperature on each  '
                                  'day. Highest in  red and lowest in blue. ('
                                  'Range of Months)')
     args = arg_parser.parse_args()
-    try:
-        if args.e:
-            year_report = WeatherReport()
+    if args.e:
+        year_report = WeatherReport()
 
-            year_report.file_read(args.path[0], args.e[0], '*')
-            year_report.yearly_report()
+        year_report.file_read(args.path[0], args.e[0], '*')
+        year_report.yearly_report()
 
-        if args.a:
-            year = datetime.strptime(args.a[0], "%Y/%m").strftime('%Y')
-            month = datetime.strptime(args.a[0], "%Y/%m").strftime('%b')
+    if args.a:
+        year = datetime.strptime(args.a[0], "%Y/%m").strftime('%Y')
+        month = datetime.strptime(args.a[0], "%Y/%m").strftime('%b')
 
-            month_report = WeatherReport()
-            month_report.file_read(args.path[0], year, month)
-            month_report.monthly_report()
+        month_report = WeatherReport()
+        month_report.file_read(args.path[0], year, month)
+        month_report.monthly_report()
 
-        if args.c:
-            year = datetime.strptime(args.c[0], "%Y/%m").strftime('%Y')
-            month = datetime.strptime(args.c[0], "%Y/%m").strftime('%b')
+    if args.c:
+        year = datetime.strptime(args.c[0], "%Y/%m").strftime('%Y')
+        month = datetime.strptime(args.c[0], "%Y/%m").strftime('%b')
 
-            daily_report = WeatherReport()
-            daily_report.file_read(args.path[0], year, month)
-            daily_report.daily_report()
-
-    except ValueError:
-        print("No Such File Exist | Invalid Input")
+        daily_report = WeatherReport()
+        daily_report.file_read(args.path[0], year, month)
+        daily_report.daily_report()
 
 
 if __name__ == "__main__":
