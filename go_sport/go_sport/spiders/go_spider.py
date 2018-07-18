ceacd329d1905b4ac583e608a8d36a2e468122ec
BilalZaib <zaibbilal0@gmail.com>
2018-07-18 13:20:14 +0500

    scrapy.Spider used and functions defined for each attribute to parse

--- go_sport/go_sport/spiders/go_spider.py
+++ go_sport/go_sport/spiders/go_spider.py
@@ -1,48 +1,78 @@
 import scrapy
 import json
 
-from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
-
 from go_sport.items import GoSportItem
 
 
-class GoSpiderSpider(CrawlSpider):
-    name = 'go_spider'
+class GoSpiderSpider(scrapy.Spider):
+    name = 'go_sport'
     allowed_domains = ['go-sport.pl']
     start_urls = ['https://www.go-sport.pl/']
 
-    rules = (
-        Rule(LinkExtractor(restrict_xpaths=("//a[@class='level-top']",)),
-             follow=True),
-        Rule(LinkExtractor(restrict_css=("a.action.next",)),
-             follow=True),
-        Rule(LinkExtractor(restrict_css=("div.product-item h2 > a.product_url",)),
-            callback='parse_item',
-             follow=True),
-    )
+    def parse(self, response):
+        category_links = response.xpath("//a[@class='level-top']/@href").extract()
+        
+        for link in category_links:
+            yield response.follow(link, self.parse_products)
 
+    def parse_products(self, response):
+        item_links =  response.css("div.product-item h2 > a::attr(href)").extract()
+
+        for link in item_links:
+            yield response.follow(link, self.parse_item)
+        
+        next_page = response.css("a.action.next::attr(href)").extract_first()
+        if next_page:
+            yield response.follow(next_page, self.parse_products)
+        
     def parse_item(self, response):
         item = GoSportItem()
 
-        item['retailer_sku'] = response.css('span.product_id::text').extract_first()
-        item['gender'] = response.xpath('//span[@class="tags1"]/span[contains(@class, "tag")\
-                         and contains(., "gender")]/text()').extract_first().split(":")[1]
-        item['category'] = response.css('span.categories span.category::text').extract()[0].split('/')[1:]
-        item['brand'] = response.css('span.nosto_product span.brand::text').extract_first()
-        item['url'] = response.css('span.nosto_product span.url::text').extract_first()
-        item['name'] = response.css('span.nosto_product span.name::text').extract_first()
-        description = response.css('div.product.attribute.description div::text').extract_first()
-
-        if description:
-            item['description'] = description.split(".")
-        else:
-            item['description'] = "No Description Avaliable"
-
-        item['image_urls'] = response.css('span.alternate_image_url::text').extract()
+        item['retailer_sku'] = self._get_retailer_sku(response)
+        item['gender'] = self._get_gender(response)
+        item['category'] = self._get_category(response)
+        item['brand'] = self._get_brand(response)
+        item['url'] = self._get_url(response)
+        item['name'] = self._get_name(response)
+        item["description"] = self._get_description(response)
+        item['image_urls'] = self._get_image_urls(response)
         item['skus'] = self._get_skus(response)
 
-        yield item
+        return item
+
+    def _get_retailer_sku(self, response):
+        return response.css('span.product_id::text').extract_first()
+
+    def _get_gender(self, response):
+        genders = {'Mężczyzna': "Man", 'Kobieta': "Woman",
+                   'Dziewczynka': "Girl", 'Chłopiec': "Boy",
+                   'Dla dzieci': "Children"}
+        gender = response.xpath('//span[@class="tags1"]\
+                                /span[contains(., "gender")]/text()').extract_first().split(":")[-1]
+        
+        return genders.get(gender, "No")
+    
+    def _get_category(self, response):
+        return response.css('span.categories span.category::text').extract()[0].split('/')[1:]
+    
+    def _get_brand(self, response):
+        return response.css('span.nosto_product span.brand::text').extract_first()
+    
+    def _get_url(self, response):
+        return response.css('span.nosto_product span.url::text').extract_first()
+    
+    def _get_name(self, response):
+        return response.css('span.nosto_product span.name::text').extract_first()
+    
+    def _get_description(self, response):
+        description = response.css('div.product.attribute.description div::text').extract_first()
+        if description:
+            return  description.split(".")
+
+        return ["No Description"]
+    
+    def _get_image_urls(self, response):
+        return response.css('span.alternate_image_url::text').extract()
 
     def _get_skus(self, response):
         skus_selector = response.css('span.skus span.nosto_sku')
@@ -64,12 +94,13 @@ class GoSpiderSpider(CrawlSpider):
         skus_in_stock = megento_json["[data-role=swatch-options]"]\
                                     ["Magento_Swatches/js/swatch-renderer-custom"]\
                                     ["jsonConfig"]["optionPrices"]
+        currency = response.css('span.nosto_product span.price_currency_code::text').extract_first()
 
         for sku in skus_selector:
             sku_item = {}
             sku_item['color'] = sku.css('span.color_web::text').extract_first()
             sku_item['price'] = sku.css('span.price::text').extract_first()
-            sku_item['currency'] = response.css('span.nosto_product span.price_currency_code::text').extract_first()
+            sku_item['currency'] = currency
             sku_item['size'] = sku.css('span.size::text').extract_first()
             sku_item['previous_price'] = sku.css('span.list_price::text').extract_first()
             sku_item['id'] = sku.css('span.id::text').extract_first()
