c48ee67d33643e80f5eb679b1e6c57a1413427a9
Aftab Akram <aftabakram>
2018-07-19 20:42:12 +0500

    anchor tags removed,  static xpathexpression removed and variables names improved

--- jacklemkus_spider.py
+++ jacklemkus_spider.py
@@ -10,11 +10,11 @@ from jacklemkus.items import ProductItem
 
 class ProductsSpider(CrawlSpider):
     name = 'jacklemkus'
-    start_urls = ['https://www.jacklemkus.com']
+    start_urls = ['https://www.jacklemkus.com/womens-apparel']
     rules = (
-        Rule(LinkExtractor(restrict_css='#nav li.level0>a', deny='how-to-order')),
-        Rule(LinkExtractor(restrict_css='ol.row a.product-image'), 'parse_product'),
-        Rule(LinkExtractor(restrict_css='ol.pagination.left a.next')),
+        Rule(LinkExtractor(restrict_css='#nav .level0 > .menu-link', deny='how-to-order')),
+        Rule(LinkExtractor(restrict_css='.row .product-image'), 'parse_product'),
+        Rule(LinkExtractor(restrict_css='pagination .next')),
 
     )
 
@@ -23,47 +23,51 @@ class ProductsSpider(CrawlSpider):
     }
 
     def parse_product(self, response):
-        xpath_query = "//body//div[@class='row']" \
-                      "//tbody//th[contains(.,'{}')]/following-sibling::td/text()"
         base_url = urlparse(response.request.headers.get('Referer', 'None').decode("utf-8"))
 
         product_loader = ItemLoader(item=ProductItem(), response=response)
-        product_loader.add_css('retailer_sku', 'div.product-essential span.sku::text')
 
-        product_loader.add_xpath('gender', xpath_query.format('Gender'))
+        product_loader.add_css('retailer_sku', '.prod-sku .sku::text')
+        product_loader.add_css('image_urls', '.product-essential .product-image a::attr(href)')
+        product_loader.add_css('description', '#description-tab .std::text')
+        product_loader.add_css('name', '.product-essential .product-name h1::text')
 
-        product_loader.add_css('image_urls', 'div.product-essential p.product-image a::attr(href)')
-
-        product_loader.add_css('description', '#description-tab div.std::text')
-
-        product_loader.add_xpath('category', xpath_query.format('DEPARTMENT'))
-        product_loader.add_xpath('category', xpath_query.format('Type'))
+        product_loader.add_value('gender', self.extract_attr(response, 'Gender'))
+        product_loader.add_value('category', self.extract_attr(response, 'DEPARTMENT'))
+        product_loader.add_value('category', self.extract_attr(response, 'Type'))
         product_loader.add_value('category', base_url.path[1:])
-
         product_loader.add_value('url', response.url)
+        product_loader.add_value('brand', self.extract_attr(response, 'Brand'))
+        product_loader.add_value('skus', self.extract_skus(response))
 
-        product_loader.add_css('name', 'div.product-essential div.product-name h1::text')
-
-        product_loader.add_xpath('brand', xpath_query.format('Brand'))
-
-        product_loader.add_value('skus', self.get_list_of_skus(response))
         return product_loader.load_item()
 
     @staticmethod
-    def get_list_of_skus(response):
-        lookup_mine = json.loads(response.css('div.product-data-mine::attr(data-lookup)')
-                                 .extract_first().replace("\'", "\""))
-        list_of_sku = []
-        for item in lookup_mine.values():
-            product_sku = {}
-            price = response.css('div.product-essential span.price::text').extract_first()
-            if price[0] is 'R':
-                product_sku["price"] = float(price[1:].replace(",", ""))
-                product_sku["currency"] = 'RAND'
-            product_sku["size"] = item.get("size")
-            product_sku["sku_id"] = "{}_{}".format(item.get("id"),
-                                                   item.get("size").replace(" ", '_'))
-            if not item.get("stock_status"):
-                product_sku["out_of_stock"] = True
-            list_of_sku.append(product_sku)
-        return list_of_sku
+    def extract_attr(response, query):
+        product_details = dict(
+            zip(['Type' if row.endswith('Type') else row and
+                 'Brand' if row.endswith('Brand') else row
+                 for row in response.css('#more-info-tab .data-table .label::text').extract()],
+                 response.css('#more-info-tab .data-table .data::text').extract())
+            )
+        return product_details.get(query)
+
+
+    @staticmethod
+    def extract_skus(response):
+        product_skus = []
+        product_sel = response.css('.product-data-mine::attr(data-lookup)')
+        if any(product_sel):
+            product_details = json.loads(product_sel.extract_first().replace("\'", "\""))
+            for item in product_details.values():
+                sku = {}
+                price = response.css('.product-essential span.price::text').extract_first()
+                if price[0] is 'R':
+                    sku["price"] = float(price[1:].replace(",", ""))
+                    sku["currency"] = 'RAND'
+                sku["size"] = item.get("size")
+                sku["sku_id"] = "{}_{}".format(item.get("id"), item.get("size").replace(" ", '_'))
+                if not item.get("stock_status"):
+                    sku["out_of_stock"] = True
+                product_skus.append(sku)
+        return product_skus
