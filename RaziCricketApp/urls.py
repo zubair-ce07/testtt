e964fe04f16e3abd629c69e1172754270bce7633
razibaig <raziullah.baig@arbisoft.com>
2018-07-19 14:58:20 +0500

    intial commit

--- RaziCricketApp/urls.py
+++ RaziCricketApp/urls.py
@@ -1,4 +1,4 @@
-"""DjangoCricketAppRazi URL Configuration
+"""RaziCricketApp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/dev/topics/http/urls/
--- RaziCricketApp/wsgi.py
+++ RaziCricketApp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DjangoCricketAppRazi project.
+WSGI config for RaziCricketApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoCricketAppRazi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RaziCricketApp.settings')
 
 application = get_wsgi_application()
