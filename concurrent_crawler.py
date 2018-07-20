b56e3e33f0d08596d66e35d22017fc92deaf015e
mumerfarooq000 <mfarooq@arbisoft.com>
2018-07-20 18:27:58 +0500

    run_in_executioner used for putting into future and await for response

--- concurrent_crawler.py
+++ concurrent_crawler.py
@@ -17,11 +17,13 @@ class AsyncSpider:
         self.num_request_made = 0
         self.total_bytes_downloaded = 0
 
-    async def extract_urls(self, semaphore):
+    async def extract_urls(self, semaphore, loop):
         await semaphore.acquire()
-        self.num_request_made += 1
         url = self.pending_urls.pop()
-        response = requests.get(url)
+        future = loop.run_in_executor(None, requests.get, url)
+        response = await asyncio.wait_for(future, 3, loop=loop)
+
+        self.num_request_made += 1
         if response.status_code == 200 \
                 and response.headers.get('content-length'):
             print(f'URL = {url}')
@@ -30,25 +32,27 @@ class AsyncSpider:
                 'content-length'))
             selector = parsel.Selector(text=response.text)
             extracted_urls = selector.css("a::attr(href)").extract()
+
             for link in extracted_urls:
                 link = parse.urljoin(url, link)
                 if not self.visited_urls.get(link):
                     self.pending_urls.add(link)
 
-        await asyncio.sleep(self.delay)
+        await  asyncio.sleep(self.delay)
         semaphore.release()
 
     def crawl(self):
-
-        semaphore = asyncio.BoundedSemaphore(self.concurrent_req)
-        futures = []
-        for i in range(self.url_visit_limit):
-            futures.append(
-                asyncio.ensure_future(
-                    self.extract_urls(semaphore)))
-
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(asyncio.wait(futures))
+        futures = []
+        while self.url_visit_limit > 1:
+            if len(self.pending_urls):
+                print(self.url_visit_limit)
+                semaphore = asyncio.BoundedSemaphore(self.concurrent_req)
+                futures.append(
+                    asyncio.ensure_future(
+                        self.extract_urls(semaphore, loop)))
+                self.url_visit_limit -= 1
+                loop.run_until_complete(asyncio.wait(futures))
 
     def spider_report(self):
         print(f"Total Bytes downloaded: {self.total_bytes_downloaded//1024}KB")
@@ -58,9 +62,15 @@ class AsyncSpider:
               f"KB")
 
 
+def validate_url(url):
+    if parse.urlparse(url).scheme:
+        return url
+    raise argparse.ArgumentTypeError('url is not valid')
+
+
 def main():
     arg_parser = argparse.ArgumentParser(description='Process some date')
-    arg_parser.add_argument('url', type=str,
+    arg_parser.add_argument('url', type=validate_url,
                             help='Domain to crawl')
     arg_parser.add_argument('concurrent_request', type=int,
                             help='Number of Concurrent Request in one go')
