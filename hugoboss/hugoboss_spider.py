72a94e19f817e24127bb7d5178ba0dcd6feac8a4
BilalZaib <zaibbilal0@gmail.com>
2018-07-20 17:10:27 +0500

    Complete Scrapper for website https://www.hugoboss.com/uk

--- hugoboss/hugoboss_spider.py
+++ hugoboss/hugoboss_spider.py
@@ -7,21 +7,19 @@ from scrapy.linkextractors import LinkExtractor
 
 from items import HugoBossItem
 
+
 class MySpider(CrawlSpider):
     name = 'hugoboss'
     allowed_domains = ['hugoboss.com']
-    start_urls = ['https://www.hugoboss.com/uk/slim-fit-graph-check-suit-in-yarn-dyed-virgin-wool/hbeu50384104_461.html?cgid=21100#start=1']
+    start_urls = ['https://www.hugoboss.com/uk/']
+    restricted_css = ['ul.nav-list--third-level', 'li.pagingbar__item--arrow']
 
-    # rules = (
-    #     # Extract links matching 'category.php' (but not matching 'subsection.php')
-    #     # and follow links from them (since no callback means follow=True by default).
-    #     Rule(LinkExtractor(allow=('category\.php', ), deny=('subsection\.php', ))),
+    DOWNLOAD_DELAY = 0.2
 
-    #     # Extract links matching 'item.php' and parse them with the spider's method parse_item
-    #     Rule(LinkExtractor(allow=('item\.php', )), callback='parse_item'),
-    # )
-    def parse_start_url(self, response):
-        self.parse_item(response)
+    rules = (
+        Rule(LinkExtractor(restrict_css=restricted_css, deny=('/inspiration/'))),
+        Rule(LinkExtractor(restrict_css=('div.product-tile__product-name')), callback='parse_item'),
+    )
 
     def parse_item(self, response):
         item = HugoBossItem()
@@ -40,8 +38,29 @@ class MySpider(CrawlSpider):
         item['care'] = self._extract_care(response)
         item['image_urls'] = self._extract_image_urls(response)
         item['skus'] = self._extract_skus(response)
-        
-        print(item) 
+
+        color_links = response.css('div.swatch-list__button--is-empty a::attr(href)').extract()
+
+        if color_links:
+            yield response.follow(color_links.pop(), callback=self._extract_colors,\
+                                 meta={'item': item, 'color_links': color_links})
+        else:
+            yield item     
+
+    def _extract_colors(self, response):
+        print("Extracting color:", response.url)
+        item = response.meta.get('item')
+        color_links = response.meta.get('color_links')
+
+        item['image_urls'] += self._extract_image_urls(response)
+        item['skus'] += self._extract_skus(response)
+
+        if color_links:
+            yield response.follow(color_links.pop(), callback=self._extract_colors,\
+                                 meta={'item': item, 'color_links': color_links})
+        else:
+            yield item
+
     
     def _extract_json(self, json_str):
         return json.loads(json_str)
@@ -53,9 +72,11 @@ class MySpider(CrawlSpider):
 
     def _extract_description(self, response):
         description = response.css('div.product-container__text__description::text').\
-                                    extract_first().strip().split('.')
+                                    extract_first()
+        if description:
+            return list(map(str.strip, description.strip().split(".")))
 
-        return list(map(str.strip, description))
+        return []
     
     def _extract_image_urls(self, response):
         img_urls = response.css('img.slider-item__image::attr(src)').extract()[:-1]
@@ -64,7 +85,7 @@ class MySpider(CrawlSpider):
 
 
     def _extract_skus(self, response):
-        color, currency, price, previous_price = self._extract_sku_static_attr(response)
+        color, currency, price, previous_price = self._extract_sku_static_attrs(response)
         
         skus = []
         size_selectors = response.css('a.product-stage__choose-size__select-size')
@@ -74,19 +95,23 @@ class MySpider(CrawlSpider):
             sku_item['price'] = price
             sku_item['currency'] = currency
             sku_item['size'] = sku.css('a::attr(title)').extract_first()
-            sku_item['id'] = '{}_{}'.format(color, sku_item['size'])
 
             if previous_price:
                 sku_item['previous_price'] = previous_price.strip()[1:]
             
             if sku.css('a::attr(disabled)').extract_first():
                 sku_item['out_of_stock'] = True
+            
+            if not sku_item['size']:
+                sku_item['size'] = 'ONE SIZE'
+
+            sku_item['id'] = '{}_{}'.format(color, sku_item['size'])
 
             skus.append(sku_item)
 
         return skus
 
-    def _extract_sku_static_attr(self, response):
+    def _extract_sku_static_attrs(self, response):
         data_current = response.css('div.product-variations::attr(data-current)').extract_first()
         data_current = self._extract_json(data_current)
 
