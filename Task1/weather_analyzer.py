41eda13e2f1dd767900a4602c57afe9d6d0239de
sohaibaslam <sohaib.sameja@gmail.com>
2018-07-23 16:33:54 +0500

    Created separate method for calculation of each result in WeatherAnalyzer class.

--- Task1/weather_analyzer.py
+++ Task1/weather_analyzer.py
@@ -4,8 +4,16 @@ from weather_result import WeatherResult
 class WeatherAnalyzer:
     @staticmethod
     def get_result(weather_records, year, month=None):
+        weather_result = WeatherResult()
         filtered_records = WeatherAnalyzer.get_filtered_records(weather_records, year, month)
-        weather_result = WeatherAnalyzer.get_weather_result(filtered_records)
+
+        weather_result.max_temperature_record = WeatherAnalyzer.get_max_temperature(filtered_records)
+        weather_result.min_temperature_record = WeatherAnalyzer.get_min_temperature(filtered_records)
+        weather_result.max_humidity_record = WeatherAnalyzer.get_max_humidity(filtered_records)
+        weather_result.max_avg_temperature_record = WeatherAnalyzer.get_max_avg_temperature(filtered_records)
+        weather_result.min_avg_temperature_record = WeatherAnalyzer.get_min_avg_temperature(filtered_records)
+        weather_result.mean_humidity = WeatherAnalyzer.get_mean_humidity(filtered_records)
+        weather_result.daily_temperatures = filtered_records
 
         return weather_result
 
@@ -19,14 +27,25 @@ class WeatherAnalyzer:
         return filtered_records
 
     @staticmethod
-    def get_weather_result(filtered_records):
-        weather_result = WeatherResult()
-        weather_result.max_temperature_record = max(filtered_records, key=lambda p: p.highest_temperature)
-        weather_result.min_temperature_record = min(filtered_records, key=lambda p: p.lowest_temperature)
-        weather_result.max_humidity_record = max(filtered_records, key=lambda p: p.max_humidity)
-        weather_result.max_avg_temperature_record = max(filtered_records, key=lambda p: p.mean_temperature)
-        weather_result.min_avg_temperature_record = min(filtered_records, key=lambda p: p.mean_temperature)
-        weather_result.mean_humidity = sum(record.mean_humidity for record in filtered_records)/len(filtered_records)
-        weather_result.daily_temperatures = filtered_records
+    def get_max_temperature(filtered_records):
+        return max(filtered_records, key=lambda p: p.highest_temperature)
 
-        return weather_result
+    @staticmethod
+    def get_min_temperature(filtered_records):
+        return min(filtered_records, key=lambda p: p.lowest_temperature)
+
+    @staticmethod
+    def get_max_humidity(filtered_records):
+        return max(filtered_records, key=lambda p: p.max_humidity)
+
+    @staticmethod
+    def get_max_avg_temperature(filtered_records):
+        return max(filtered_records, key=lambda p: p.mean_temperature)
+
+    @staticmethod
+    def get_min_avg_temperature(filtered_records):
+        return min(filtered_records, key=lambda p: p.mean_temperature)
+
+    @staticmethod
+    def get_mean_humidity(filtered_records):
+        return sum(record.mean_humidity for record in filtered_records)/len(filtered_records)
