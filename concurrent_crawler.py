7f0d6a9ce525fefaca8ce5ae521dce1d7f9db4ff
zeba ramzan <zeba.ramzan@arbisoft.com>
2018-07-23 12:54:49 +0500

    merged ConcurrentCrawler and CrawlerResult class and added delay between concurrent requests

--- concurrent_crawler.py
+++ concurrent_crawler.py
@@ -2,40 +2,36 @@ import asyncio
 import requests
 
 
-class CrawlerResults:
-    def __init__(self, total_bytes=0, total_requests=0):
-        self.total_bytes = total_bytes
-        self.total_requests = total_requests
-
-
 class ConcurrentCrawler:
     def __init__(self, download_delay, conc_requests):
         self.download_delay = download_delay
-        self.loop_ = asyncio.get_event_loop()
+        self.loop = asyncio.get_event_loop()
         self.semaphore = asyncio.BoundedSemaphore(conc_requests)
-        self.crawler_results = CrawlerResults()
+        self.total_bytes = 0
+        self.total_requests = 0
 
-    async def crawl_url(self, url, semaphore):
-        async with semaphore:
-            responce = await asyncio.ensure_future(self.loop_.run_in_executor(None, requests.get, url))
+    async def crawl_url(self, url):
+        async with self.semaphore:
+            responce = await asyncio.ensure_future(self.loop.run_in_executor(None, requests.get, url))
+            await asyncio.sleep(self.download_delay)
         return responce.text
 
-    async def create_tasks(self, filtered_urls, delay):
+    async def create_tasks(self, filtered_urls):
         tasks = []
         for url in filtered_urls:
-            tasks.append(self.crawl_url(url, self.semaphore))
-            await asyncio.sleep(delay)
+            tasks.append(self.crawl_url(url))
+
         results = await asyncio.gather(*tasks)
         return results
 
     async def process_crawler_results(self, filtered_urls):
-        results = await self.create_tasks(filtered_urls, self.download_delay)
+        results = await self.create_tasks(filtered_urls)
         for result in results:
-            self.crawler_results.total_bytes += len(result)
-            self.crawler_results.total_requests += 1
+            self.total_bytes += len(result)
+            self.total_requests += 1
 
     def start_crawler(self, filtered_urls):
-        self.loop_.run_until_complete(
+        self.loop.run_until_complete(
             self.process_crawler_results(filtered_urls))
 
-        self.loop_.close()
+        self.loop.close()
