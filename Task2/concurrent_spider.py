196cd67564eac345df93bbaea3eda3a334c98f4c
sohaibaslam <sohaib.sameja@gmail.com>
2018-07-23 14:51:22 +0500

    Modified termination logic and used visited_urls for it.

--- Task2/concurrent_spider.py
+++ Task2/concurrent_spider.py
@@ -20,14 +20,17 @@ class RecursiveConcurrentSpider:
         self.__tasks_limiting_semaphore = asyncio.BoundedSemaphore(self.concurrent_requests_limit)
 
     async def start_crawler(self, urls, limit):
-        if limit > 0 and urls:
-            urls, limit = (urls[:limit], 0) if len(urls) > limit else (urls, limit - len(self.visited_urls))
+        if len(self.visited_urls) < limit and urls:
+            if len(urls) > limit - len(self.visited_urls):
+                urls = urls[:limit - len(self.visited_urls)]
 
             completed_futures, pending_futures = await self.visit_urls(urls)
+            self.visited_urls.extend(urls)
+
             while True:
                 for completed in completed_futures:
-                    self.spider_execution_report.total_requests += 1
                     self.spider_execution_report.bytes_downloaded += len(completed.result())
+                    self.spider_execution_report.total_requests += 1
                     await self.start_crawler(self.get_urls(completed.result()), limit)
 
                 if pending_futures:
@@ -41,7 +44,6 @@ class RecursiveConcurrentSpider:
         for url in urls:
             tasks.append(asyncio.ensure_future(self.make_get_request(url, self.__tasks_limiting_semaphore)))
             await asyncio.sleep(self.download_delay)
-            self.visited_urls.append(url.geturl())
 
         completed, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
 
@@ -59,6 +61,10 @@ class RecursiveConcurrentSpider:
         filtered_urls = self.get_filtered_urls(absolute_urls)
         return list(filtered_urls)
 
+    def get_filtered_urls(self, absolute_urls):
+        visited_urls_strings = [url.geturl() for url in self.visited_urls]
+        return [url for url in absolute_urls if url.geturl() not in visited_urls_strings]
+    
     def get_absolute_urls(self, html_text):
         return {self.get_parsed_url(url) for url in self.get_links_from_html(html_text)
                 if urlparse(url).scheme == '' and urlparse(url).path not in ('', '/')}
@@ -71,6 +77,3 @@ class RecursiveConcurrentSpider:
     def get_links_from_html(self, html_text):
         html_selector = Selector(html_text)
         return html_selector.css('a::attr(href)').extract()
-
-    def get_filtered_urls(self, absolute_urls):
-        return [url for url in absolute_urls if url.geturl() not in self.visited_urls]
