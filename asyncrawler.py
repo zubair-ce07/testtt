1ccb02ad17e491a64b53a0ba34942760b263c148
Hamza953 <hamzaakhtar0@gmail.com>
2018-07-23 14:45:20 +0500

    removed useless files and solved naming problem

--- asyncrawler.py
+++ asyncrawler.py
@@ -1,7 +1,6 @@
 import asyncio
 import requests
 import argparse
-import urllib.request as url_checker
 import re
 
 from parsel import Selector
@@ -10,72 +9,59 @@ from urllib.parse import urljoin
 
 class AsyncCrawler:
 
-    def __init__(self, parent_url, concurrent_requests=1,
+    def __init__(self, start_url, concurrent_requests=1,
                  download_delay=0.0, maximum_urls=0):
-        self.parent_url = parent_url
+        self.start_url = start_url
         self.download_delay = download_delay
         self.maximum_urls = maximum_urls
         self.bounded_semaphore = asyncio.BoundedSemaphore(concurrent_requests)
         self.visited_urls = set()
-        self.download_bytes = 0
+        self.downloaded_bytes = 0
 
-    async def get_html(self, url, event_loop):
+    async def parse(self, url, event_loop):                                 # Downloads the page size and returns html
         async with self.bounded_semaphore:
             await asyncio.sleep(self.download_delay)
             future_response = event_loop.run_in_executor(None, requests.get, url)
             response = await future_response
-            self.download_bytes += len(response.content)
-            print(f"Visited url {url}")
+            self.downloaded_bytes += len(response.content)
             return response.text
 
-    async def extract_anchor_urls(self, base_url, event_loop):
-        sel = Selector(text=await self.get_html(base_url, event_loop))
-        return set(map(lambda url: urljoin(base_url, url), sel.css('a::attr(href)').getall()))
+    async def extract_anchor_urls(self, base_url, event_loop):              # Returns a list of urls from the page
+        sel = Selector(text=await self.parse(base_url, event_loop))
+        return list(map(lambda url: urljoin(base_url, url), sel.css('a::attr(href)').getall()))
 
-    async def extract_multiple_anchor_urls(self, to_extract, event_loop):
+    async def extract_multiple_anchor_urls(self, anchor_urls, event_loop):   # Extracts multiple urls asynchronously
         futures = []
-        for web_url in to_extract:
-            if len(self.visited_urls) < self.maximum_urls:
-                if web_url in self.visited_urls:
-                    continue
-
+        for web_url in anchor_urls:
+            if len(self.visited_urls) > self.maximum_urls:
+                break
+            if web_url not in self.visited_urls:
                 self.visited_urls.add(web_url)
-                future = asyncio.ensure_future(self.extract_anchor_urls(web_url, event_loop))
-                futures.append(future)
+                futures.append(asyncio.ensure_future(self.extract_anchor_urls(web_url, event_loop)))
 
-        results = await asyncio.gather(*futures)
-        return results
+        extracted_urls = await asyncio.gather(*futures)
+        return extracted_urls
 
-    async def crawl(self, event_loop):
-        anchor_urls = [self.parent_url]
+    async def crawl(self, event_loop):                                      # Recursively calls the Async crawler
+        anchor_urls = [self.start_url]
         while len(self.visited_urls) < self.maximum_urls:
-            results = await self.extract_multiple_anchor_urls(anchor_urls, event_loop)
-
+            extracted_urls = await self.extract_multiple_anchor_urls(anchor_urls, event_loop)
             anchor_urls = []
-            for extracted_urls in results:
-                anchor_urls.extend(extracted_urls)
-
-    def generate_report(self):
-        print(f'\nTotal requests: {self.maximum_urls}')
-        print(f'Total bytes downloaded: {self.download_bytes}')
-        print(f'Average page size: {int(self.download_bytes / len(self.visited_urls))}\n')
+            anchor_urls += sum(extracted_urls, [])
 
     def execute_crawler(self):
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.crawl(loop))
         loop.close()
-        print("Done crawling")
+
+    def generate_report(self):
+        print(f'\nTotal requests: {self.maximum_urls}')
+        print(f'Total bytes downloaded: {self.downloaded_bytes}')
+        print(f'Average page size: {int(self.downloaded_bytes / len(self.visited_urls))}\n')
 
 
 def validate_web_url(url):
-    regex = re.compile(
-        r'^(?:http|ftp)s?://'
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
-        r'localhost|'
-        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-        r'(?::\d+)?'
-        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
-
+    regex = re.compile(r'(^https?:\/\/)((www)\.?[^\W]+(.\w+)+)(\/[^\W]*)*', re.IGNORECASE)
     if re.match(regex, url):
         return url
     raise argparse.ArgumentTypeError(f"Invalid url {url}")
@@ -83,7 +69,7 @@ def validate_web_url(url):
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("parent_url", help="url for crawling", type=validate_web_url)
+    parser.add_argument("start_url", help="url for crawling", type=validate_web_url)
     parser.add_argument("maximum_urls", help="Total urls to visits", type=int)
     parser.add_argument("download_delay", help="Download delay for each worker", type=float)
     parser.add_argument("number_of_concurrent_requests",
@@ -95,7 +81,7 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    async_crawler = AsyncCrawler(args.parent_url, args.number_of_concurrent_requests,
+    async_crawler = AsyncCrawler(args.start_url, args.number_of_concurrent_requests,
                                  args.download_delay, args.maximum_urls)
     async_crawler.execute_crawler()
     async_crawler.generate_report()
