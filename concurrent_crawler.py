61fbf2e0fad5e72ff1ac4aaaba30d06880b8a348
zeba ramzan <zeba.ramzan@arbisoft.com>
2018-07-23 10:35:22 +0500

    Used BoundedSempahore and ensure_future

--- concurrent_crawler.py
+++ concurrent_crawler.py
@@ -1,72 +1,41 @@
-import argparse
-import time
 import asyncio
 import requests
-from parsel import Selector
-from urllib import parse
-from requests.compat import urljoin
 
 
-def parse_raw_url(start_url, conc_reqs, delay, max_reqs):
-    selector = Selector(requests.get(start_url).text)
-    extracted_urls = selector.css("a::attr(href)").extract()
-    absolute_urls = [urljoin(start_url, url)
-                     for url in extracted_urls if not parse.urlparse(url).scheme == 'mailto']
-
-    q, r = divmod(max_reqs, conc_reqs)
-    if r:
-        url_sets = [absolute_urls[i:i+conc_reqs]
-                    for i in range(0, max_reqs, conc_reqs)] + [absolute_urls[q*r:max_reqs]]
-    else:
-        url_sets = [absolute_urls[i:i+conc_reqs]
-                    for i in range(0, max_reqs, conc_reqs)]
-
-    total_bytes = 0
-    total_requests = 0
-    for url_set in url_sets:
-        loop = asyncio.get_event_loop()
-        future = [loop.run_in_executor(None, requests.get, url)
-                  for url in url_set]
-        results = loop.run_until_complete(asyncio.gather(*future))
-        total_bytes, total_requests = calculate_results(
-            results, total_bytes, total_requests)
-        asyncio.sleep(delay)
-    return total_bytes, total_requests
+class CrawlerResults:
+    def __init__(self, total_bytes=0, total_requests=0):
+        self.total_bytes = total_bytes
+        self.total_requests = total_requests
 
 
-def calculate_results(results, total_bytes, total_requests):
-    for result in results:
-        total_bytes += len(result.text)
-        total_requests += 1
-    return total_bytes, total_requests
+class ConcurrentCrawler:
+    def __init__(self, download_delay, conc_requests):
+        self.download_delay = download_delay
+        self.loop_ = asyncio.get_event_loop()
+        self.semaphore = asyncio.BoundedSemaphore(conc_requests)
+        self.crawler_results = CrawlerResults()
 
+    async def crawl_url(self, url, semaphore):
+        async with semaphore:
+            responce = await asyncio.ensure_future(self.loop_.run_in_executor(None, requests.get, url))
+        return responce.text
 
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("starting_url", type=str,
-                        help="Url to start crawling with")
-    parser.add_argument("-r", "--concurrent_requests", type=int, default=5)
-    parser.add_argument("-d", "--download_delay", type=int, default=2)
-    parser.add_argument(
-        "-n", "--maximum_number_of_requests", type=int, default=50)
-    return parser.parse_args()
+    async def create_tasks(self, filtered_urls, delay):
+        tasks = []
+        for url in filtered_urls:
+            tasks.append(self.crawl_url(url, self.semaphore))
+            await asyncio.sleep(delay)
+        results = await asyncio.gather(*tasks)
+        return results
 
+    async def process_crawler_results(self, filtered_urls):
+        results = await self.create_tasks(filtered_urls, self.download_delay)
+        for result in results:
+            self.crawler_results.total_bytes += len(result)
+            self.crawler_results.total_requests += 1
 
-def print_results(total_time, total_bytes, total_requests):
-    print(f'Total bytes: {total_bytes}\nTotal requests: {total_requests}\nAverage page size:'
-          f'{total_bytes/total_requests} bytes\nTotal time: {total_time}s')
+    def start_crawler(self, filtered_urls):
+        self.loop_.run_until_complete(
+            self.process_crawler_results(filtered_urls))
 
-
-def main():
-
-    args = parse_arguments()
-    start_url = args.starting_url
-    start_time = time.time()
-    total_bytes, total_requests = parse_raw_url(start_url, args.concurrent_requests,
-                                                args.download_delay, args.maximum_number_of_requests)
-    total_time = time.time() - start_time
-    print_results(total_time, total_bytes, total_requests)
-
-
-if __name__ == "__main__":
-    main()
+        self.loop_.close()
