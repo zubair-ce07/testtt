37143b2b2001eacb5d3838e310e90acb27fe71cd
zeba ramzan <zeba.ramzan@arbisoft.com>
2018-07-23 15:59:34 +0500

    added recursive url parsing functionality

--- concurrent_crawler.py
+++ concurrent_crawler.py
@@ -1,14 +1,36 @@
 import asyncio
 import requests
+from parsel import Selector
+from urllib import parse
+from requests.compat import urljoin
+
+
+class UrlProcessor:
+    @staticmethod
+    def extract_urls(start_url):
+        selector = Selector(requests.get(start_url).text)
+        extracted_urls = selector.css("a::attr(href)").extract()
+        return extracted_urls
+
+    @staticmethod
+    def filter_urls(start_url, extracted_urls):
+        domain = parse.urlparse(start_url).netloc
+        filtered_urls = [urljoin(start_url, url)
+                         for url in extracted_urls if parse.urlparse(url).netloc == domain]
+        return filtered_urls
 
 
 class ConcurrentCrawler:
-    def __init__(self, download_delay, conc_requests):
+    def __init__(self, start_url, download_delay, conc_requests, max_reqs):
+        self.start_url = start_url
         self.download_delay = download_delay
         self.loop = asyncio.get_event_loop()
         self.semaphore = asyncio.BoundedSemaphore(conc_requests)
+        self.max_reqs = max_reqs
         self.total_bytes = 0
         self.total_requests = 0
+        self.visited_urls = []
+        self.remaining_requests = max_reqs
 
     async def crawl_url(self, url):
         async with self.semaphore:
@@ -20,15 +42,24 @@ class ConcurrentCrawler:
         tasks = []
         for url in filtered_urls:
             tasks.append(self.crawl_url(url))
+            self.visited_urls.append(url)
 
         results = await asyncio.gather(*tasks)
         return results
 
     async def process_crawler_results(self, filtered_urls):
-        results = await self.create_tasks(filtered_urls)
+        results = await self.create_tasks(filtered_urls[:self.remaining_requests])
         for result in results:
             self.total_bytes += len(result)
             self.total_requests += 1
+            self.remaining_requests = self.max_reqs - self.total_requests
+
+        if self.remaining_requests:
+            starting_url = self.visited_urls.pop()
+            extracted_urls = UrlProcessor.extract_urls(starting_url)
+            filtered_urls = UrlProcessor.filter_urls(
+                starting_url, extracted_urls)
+            await self.process_crawler_results(filtered_urls)
 
     def start_crawler(self, filtered_urls):
         self.loop.run_until_complete(
--- main.py
+++ main.py
@@ -1,23 +1,8 @@
 import argparse
 import requests
-from parsel import Selector
-from urllib import parse
-from requests.compat import urljoin
 
 from concurrent_crawler import ConcurrentCrawler
-
-
-def extract_urls(start_url):
-    selector = Selector(requests.get(start_url).text)
-    extracted_urls = selector.css("a::attr(href)").extract()
-    return extracted_urls
-
-
-def filter_urls(start_url, extracted_urls, max_reqs):
-    domain = parse.urlparse(start_url).netloc
-    filtered_urls = [urljoin(start_url, url)
-                     for url in extracted_urls if parse.urlparse(url).netloc == domain]
-    return filtered_urls[:max_reqs]
+from concurrent_crawler import UrlProcessor
 
 
 def parse_arguments():
@@ -45,10 +30,11 @@ def main():
     download_delay = args.download_delay
     start_url = args.starting_url
 
-    extracted_urls = extract_urls(start_url)
-    filtered_urls = filter_urls(start_url, extracted_urls, max_reqs)
+    extracted_urls = UrlProcessor.extract_urls(start_url)
+    filtered_urls = UrlProcessor.filter_urls(start_url, extracted_urls)
 
-    conc_crawler = ConcurrentCrawler(download_delay, conc_reqs)
+    conc_crawler = ConcurrentCrawler(
+        start_url, download_delay, conc_reqs, max_reqs)
     conc_crawler.start_crawler(filtered_urls)
 
     print_results(conc_crawler.total_bytes, conc_crawler.total_requests)
