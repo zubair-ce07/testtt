131122452378086d37b9e8b8842214f8758788c9
Muhammad Haseeb <haseeb@tplink--KhwajaMahmood.arbisoft.com>
2018-07-26 14:26:45 +0500

    Monthly tempreture & humidity calulator implemented

--- weatherman.py
+++ weatherman.py
@@ -63,21 +63,51 @@ class ResultsCalculator:
         self.calculated_results = None
 
     
-    def calculate_average(self):
+    def monthly_tempreture_and_humitdity_calculator(self, weather_data, year, month):
+        self.calculated_results = {
+                                    "HighestAverage": 0
+                                    ,"LowestAverage": 0
+                                    ,"AverageMeanHumidity": 0
+                                    }
+        total_days = 0
+                                    
+        for data in weather_data:    
+            year_level_data = data.get(year)
+            if year_level_data is not None:
+                month_level_data = year_level_data.get(month)
+                if month_level_data is not None:
+                    for day in month_level_data:
+                        total_days += 1
+                        day_level_data = month_level_data.get(day)
+                        
+                        if not day_level_data.get("Max TemperatureC") == "" :
+                            self.calculated_results["HighestAverage"] = (float(day_level_data.get("Max TemperatureC")) + 
+                                                                        float(self.calculated_results["HighestAverage"]))
 
-        pass
-    
+                        if not day_level_data.get("Min TemperatureC") == "" :
+                            self.calculated_results["LowestAverage"] = (float(day_level_data.get("Min TemperatureC")) + 
+                                                                        float(self.calculated_results["LowestAverage"]))
+                                                                        
+                        if not day_level_data.get(" Mean Humidity") == "":
+                            self.calculated_results["AverageMeanHumidity"] = (float(day_level_data.get(" Mean Humidity")) + 
+                                                                        float(self.calculated_results["AverageMeanHumidity"]))    
+        
+        self.calculated_results["HighestAverage"] = int(self.calculated_results["HighestAverage"] / total_days)
+        self.calculated_results["LowestAverage"] = int(self.calculated_results["LowestAverage"] / total_days)
+        self.calculated_results["AverageMeanHumidity"] = int(self.calculated_results["AverageMeanHumidity"] 
+                                                            / total_days)
 
-    def yearly_temperature_and_humidity_calulator(self,weather_data, year):
+
+    def yearly_temperature_and_humidity_calulator(self, weather_data, year):
         self.calculated_results = {}
-        first_iteration = True
+        first_iteration = True  #Flag to memorize to do initial setup 
         for data in weather_data:
-            month_level_data = data.get(year)
-            if month_level_data is not None:
-                for month in month_level_data:
-                    day_level_data = month_level_data.get(month)
-                    for day in day_level_data:
-                        temp_data  = day_level_data.get(day)
+            year_level_data = data.get(year)
+            if year_level_data is not None:
+                for month in year_level_data:
+                    month_level_data = year_level_data.get(month)
+                    for day in month_level_data:
+                        temp_data  = month_level_data.get(day)
                         if first_iteration:
                             first_iteration = False
                             self.calculated_results = {
@@ -110,8 +140,7 @@ class ResultsCalculator:
                                 self.calculated_results["Humidity"] = temp_data.get(" Mean Humidity")
                                 self.calculated_results["MostHumidDay"]  = day
                                 self.calculated_results["MostHumidMonth"] = month_names[month]
-        print(self.calculated_results)
-
+        
                             
 def usage_printer():
     print("Usage:")
@@ -137,7 +166,11 @@ if __name__ == "__main__":
     ResultsCalculatorInstance = ResultsCalculator()
 
     if sys.argv[2] == '-a':
-        pass
+        splited_year_n_month = sys.argv[3].split("/")  #Parse input
+        ResultsCalculatorInstance.monthly_tempreture_and_humitdity_calculator(
+            WeatherRecordInstance.weather_data, splited_year_n_month[0],
+            splited_year_n_month[1]
+            )
     elif sys.argv[2] == '-c':
         pass
     elif sys.argv[2] == '-e':
