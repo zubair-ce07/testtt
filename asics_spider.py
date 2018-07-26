c1113850764240cb0900d964677a001c48bedd31
Hamza953 <hamzaakhtar0@gmail.com>
2018-07-26 18:21:55 +0500

    removed a bug when making request

--- asics_spider.py
+++ asics_spider.py
@@ -27,11 +27,11 @@ class Product:
 
 
 def get_html(url):
-    with requests.get(url) as response:
-        yield response.text
+    with requests.get(url, allow_redirects=False) as response:
+        yield response.text or None
 
 
-def extract_categories(start_url):  # Returns links of all categories
+def extract_categories(start_url):                          # Returns links of all categories
     sel = Selector(text=next(get_html(start_url)))
     category_links = sel.css("li.nav-item:not(.mobile) a::attr(href)").getall()
     return set(map(lambda url: urljoin(start_url, url), category_links))
@@ -40,13 +40,11 @@ def extract_categories(start_url):  # Returns links of all categories
 def extract_product_urls(url):
     page_no = 0
     page_urls = set()
-    while (True):
-        new_url = f'{url}?page={page_no}'
-        sel = Selector(text=next(get_html(new_url)))
+    while True:
+        sel = Selector(text=next(get_html(f'{url}?page={page_no}')))
         links = sel.css('a.productMainLink::attr(href)').getall()
         if not links:
             break
-        # page_urls = page_urls.union(set((map(lambda link: urljoin(url, link), links))))
         page_urls |= set((map(lambda link: urljoin(url, link), links)))
         page_no += 1
     return page_urls
@@ -59,7 +57,7 @@ def get_product_obj(sel, model_no, product_url):
     product_colors = sel.css("div#colour-label span.color-label::text").get().split('/')
     product_features = sel.css("div#productFeaturesContent h5::text").getall()
     product_shipping = sel.css("div#info-container > div::text").get()
-    product_details = sel.css("div#collapse1 p::text").get() or
+    product_details = sel.css("div#collapse1 p::text").get() or sel.css("div#collapse1 li::text").getall()
     product_image_urls = sel.css("div#pdp-main-image img.product-img::attr(data-url-src)").getall()
     product_status = sel.css("div#stock-info-container::text").get().strip()
     product_status = False if "Out" in product_status else True
@@ -75,39 +73,20 @@ def crawl(category_links, visited_products):
     for category in category_links:
         print(category)
         extracted_product_links = extract_product_urls(
-            "http://www.asics.com/nz/en-nz/mens-training-shoes/c/mens-training-shoes")  # For each category, extract product links to crawl
-        # T838N.9090
+            "https://www.asics.com/nz/en-nz/gel-quantum-collection/c/gel-quantum")  # For each category, extract product links to crawl
+
         for product in extracted_product_links:
             print(f'product=link: {product}')
-            sel = Selector(text=next(get_html(product)))
+            html = next(get_html(product))
+            if not html:
+                continue
+            sel = Selector(text=html)
             product_model = sel.css("span[itemprop='model']::text").get().strip()
 
             if product_model in visited_products:
                 continue
             visited_products.add(product_model)
-            myobj.append(get_product_obj(sel))
-            # print(f"Gender: {product_gender}")
-            # print(f"url: {product_url}")
-            # print(f"product_name: {product_name}")
-            # print(f"product_price: ${product_price}")
-            # print(f"model: {product_model}")
-            # print(f"product_color: {product_colors}")
-            # print(f"Status: {product_status}")
-            # print(f"Shipment: {product_shipping}")
-            # print(f"details: {product_details}")
-            # print(f"Size: {product_size}")
-            # print(f"Images: {product_image_urls}")
-            # print(f"Product features: {product_features}")
-
-            myobj.append(
-                Product(product_name, product_gender, product_model, product_price, product_colors, product_url,
-                        product_features,
-                        product_status, product_shipping, product_details, product_size, product_image_urls))
-
-            # json_string = json.dumps([obj.obj_to_json() for obj in myobj], indent=4)
-            #
-            # print("\n\n", json_string)
-
+            myobj.append(get_product_obj(sel, product_model, product))
         break
     return myobj
 
@@ -120,7 +99,7 @@ def get_json(pods):
 def main():
     url = "http://www.asics.com/nz/en-nz/"
     visited_products = set()
-    category_links = extract_categories(url)  # links from home page
+    category_links = extract_categories(url)                # links from home page
     pods = crawl(category_links, visited_products)
     get_json(pods)
 
