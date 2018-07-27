eb76d22e67e3d5dfcef353eafdbbda0dfcddb6c3
Hamza953 <hamzaakhtar0@gmail.com>
2018-07-27 15:59:30 +0500

    improved css query strings

--- asics_spider.py
+++ asics_spider.py
@@ -31,48 +31,48 @@ def get_html(url):
         yield response.text or ""
 
 
-def extract_categories(start_url):                                          # Returns links of all categories
+def extract_categories(start_url):                                  # Returns links of all categories
     sel = Selector(text=next(get_html(start_url)))
     category_links = sel.css("li.nav-item:not(.mobile) a::attr(href)").getall()
     yield set(map(lambda url: urljoin(start_url, url), category_links))
 
 
-def extract_product_links(category_links):                                  # Return links of all products on site
+def extract_product_links(category_links):                          # Return links of all products on site
     product_links = set()
     for category in category_links:
         product_links |= extract_relative_urls(category)
     yield product_links
 
 
-def extract_relative_urls(url):                                              # Get url of each product for a category
+def extract_relative_urls(url):                                     # Get url of each product for a category
     page_no = 0
     page_urls = set()
     seen_urls = set()
     while True:
         url_sel = Selector(text=next(get_html(f'{url}?page={page_no}')))
         product_links = url_sel.css('a.productMainLink::attr(href)').getall()
-        if not product_links:                                               # if next page exists
+        if not product_links:                                       # if next page exists
             break
         seen_urls |= set(product_links)
-        if not seen_urls - page_urls:                                       # if on the same page
+        if not seen_urls - page_urls:                               # if on the same page
             break
         page_urls |= seen_urls
-        page_no += 1                                                        # Move to next page
+        page_no += 1                                                # Move to next page
     page_urls = set(map(lambda link: urljoin(url, link), filter(lambda url: "//" not in url, page_urls)))
     return set(filter(lambda url: "onitsukatiger" not in url, page_urls))
 
 
-def get_product(sel, product_url):                                          # Extract product information
-    product_name = sel.css('h1.single-prod-title::text').get()
-    product_gender = sel.css("div#unisex-tab::attr(class)").get()
-    product_price = sel.css("div#top-right-info meta[itemprop='price']::attr(content)").get()
+def get_product(sel, product_url):                                  # Extract product information
+    product_name = sel.css('.single-prod-title::text').get()
+    product_gender = sel.css("#unisex-tab::attr(class)").get()
+    product_price = sel.css("meta[itemprop='price']::attr(content)").get()
     product_model = sel.css("span[itemprop='model']::text").get().strip()
-    product_colors = sel.css("div#colour-label span.color-label::text").get().split('/')
-    product_features = sel.css("div#productFeaturesContent h5::text").getall()
-    product_shipping = sel.css("div#info-container > div::text").get()
-    product_details = sel.css("div#collapse1 p::text").get() or sel.css("div#collapse1 li::text").getall()
-    product_image_urls = sel.css("div#pdp-main-image img.product-img::attr(data-url-src)").getall()
-    product_status = sel.css("div#stock-info-container::text").get().strip()
+    product_colors = sel.css("#colour-label span.color-label::text").get().split('/')
+    product_features = sel.css("#productFeaturesContent h5::text").getall()
+    product_shipping = sel.css("#info-container > div::text").get()
+    product_details = sel.css("#collapse1 p::text").get() or sel.css("#collapse1 li::text").getall()
+    product_image_urls = sel.css("#pdp-main-image .product-img::attr(data-url-src)").getall()
+    product_status = sel.css("#stock-info-container::text").get().strip()
     product_status = False if "Out" in product_status else True
     product_size = list(filter(None, set(size.strip() for size in sel.css("a.SizeOption::text").getall())))
 
