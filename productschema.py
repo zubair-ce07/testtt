804248bc26786ef4d5b2f9d28b52951f80b54a8b
WadoodAmer <wadood.islam@arbisoft.com>
2018-07-30 19:22:20 +0500

    Removed unnecessary classes, converted them into dictionaries. Refactored the spaghetti code to responsible functions. conformed to PEP 8 rules.

--- productschema.py
+++ productschema.py
@@ -1,89 +1,74 @@
 import json
 import re
+from parsel import Selector
 
 
-class Sku:
-    def __init__(self, data):
-        self.colour = data.get("color")
-        self.price = float(data.get("offers").get("price")) if "offers" in data else float(data.get("price"))
-        self.currency = data.get("offers").get("priceCurrency") if "offers" in data else None
-        self.size = re.search(r"\w{16}([\.\w\d]*)", data.get("mpn")).group(1) if "mpn" in data else "one size"
-        self.size = self.size if self.size != "T.U." else "one size"
-        self.previous_prices = []
-        self.out_of_stock = "offers"not in data or "InStock"in data.get("offers").get("availability")
-        self.sku_id = data.get("mpn") or data.get("id")
+def parse_price(selector):
+    price = selector.css('.special-price>span[class=price]::text').extract_first() \
+        or selector.css('.regular-price>span::text').extract_first()
+    return re.search(r"\d+\.\d+", price).group()
 
 
-class Product:
-    def __init__(self, url, selector):
-        self.selector = selector
-        json_string = self.selector.css('script[type="application/ld+json"]::text').extract_first()
-        if re.search(r'"@type": *"Product"', json_string):
-            self._process_skus(json_string)
-        else:
-            json_string = self._extract_json()
-            self._process_single_product(json_string)
-        self.gender = "girls"
-        self.image_urls = selector.css(".slide>a>img::attr(data-more-views)").extract()
-        self.care = []
-        self.url = url
-
-    def _process_single_product(self, json_string):
-        price = self.selector.css('span.price[id*="product-price"]::text').extract_first()
-        raw_details = json.loads(json_string)
-        self.skus = {}
-        product = raw_details.get("detail").get("products").pop()
-        sku = Sku(product)
-        sku.currency = raw_details.get("currencyCode")
-        if price:
-            price = self.selector.css('span.price[id*="old-price"]::text').extract_first()
-            print(price)
-            sku.previous_prices.append(float(re.search(r"\d+[\.|,]\d+", price).group().replace(",", ".")))
-        self.skus.update({sku.sku_id: sku})
-        self.retailer_sku = raw_details.get("id")
-        self.category = product.get("category").split("/")
-        self.brand = 'LIUJO'
-        self.name = product.get("name")
-        self.description = self.selector.xpath('//div[@class="short-description-value"]//text()').extract()
-        self.description = ''.join(''.join(self.description).strip().splitlines()).split(". ")
-
-    def _process_skus(self, json_string):
-        price = self.selector.css('span.price[id*="product-price"]::text').extract_first()
-        json_string = self._validate_json(json_string)
-        raw_skus = json.loads(json_string)
-        self.skus = {}
-        for raw_sku in raw_skus:
-            sku = Sku(raw_sku)
-            if price:
-                sku.previous_prices.append(sku.price)
-                sku.price = float(re.search(r"\d+[\.|,]\d+", price).group().replace(",", "."))
-            self.skus.update({sku.sku_id: sku})
-        self.retailer_sku = raw_sku.get("sku")
-        self.category = re.search(r'"category": "+(.+)"', self._extract_json()).group(1).split("/")
-        self.brand = raw_sku.get("brand").get("name")
-        self.name = raw_sku.get("name")
-        self.description = raw_sku.get("description").split(". ")
-
-    def _extract_json(self):
-        for text in self.selector.css('script[type="text/javascript"]::text').extract():
-            if "ecommerce" in text:
-                return self._validate_name(re.search(r"ecommerce': (\{[\n\s\S]+][\n\s]+\})", text).group(1))
-
-    @staticmethod
-    def _validate_name(string):
-        name = re.search(r"[\"|\']name[\"|\']: *\"(.*)", string).group(1)[0:-2]
-        new_name = name.replace('"', '\\"')
-        name = name.replace("'", '"')
-        string = string.replace("'", '"')
-        string = string.replace(name, new_name)
-        return string
-
-    def _validate_json(self, string):
-        description = re.search(r"\"description\": *\"(.*)", string).group(1)[0:-2]
-        new_description = description.replace('"', '\\"')
-        description = description.replace("'", "\"")
-        string = self._validate_name(string)
-        string = string.replace(description, new_description)
-        return string
+def previous_prices(selector):
+    old_price = selector.css('.old-price>span[class=price]::text').extract_first()
+    return [re.search(r"\d+\.\d+", old_price).group()] if old_price else []
 
 
+def parse_description(selector):
+    description = selector.xpath('//div[@class="short-description-value"]//text()').extract()
+    return ''.join(''.join(description).strip().splitlines()).split(". ")
+
+
+def parse_category(selector):
+    for text in selector.css('script[type="text/javascript"]::text').extract():
+        if "category" in text:
+            return re.search(r'\'category\': "+(.+)"', text).group(1).split("/")
+
+
+def single_product(selector):
+    return {
+        "colour":          "one colour",
+        "currency":        re.search(r"\w*", selector.css('.price::text').extract_first()).group(),
+        "out_of_stock":    False,
+        "size":            "one size",
+        "price":           parse_price(selector),
+        "previous-prices": previous_prices(selector),
+        "sku_id":          'one colour_one size'
+    }
+
+
+def yield_sku(selector):
+    json_string = selector.css('script[type="application/ld+json"]::text').extract_first()
+    if re.search(r'"@type": *"Product"', json_string):
+        price = parse_price(selector),
+        previous = previous_prices(selector),
+        for raw_sku in json.loads(json_string):
+            size = re.search(r"\w{16}([.\w\d]*)", raw_sku.get("mpn")).group(1)
+            yield {
+                "colour":          raw_sku.get("color"),
+                "currency":        raw_sku.get("offers").get("priceCurrency"),
+                "out_of_stock":    "InStock" in raw_sku.get("offers").get("availability"),
+                "size":            size if size != "T.U." else "one size",
+                "price":           price,
+                "previous-prices": previous,
+                "sku_id":          f'{raw_sku.get("color")}_{size}'
+            }
+    else:
+        yield single_product(selector)
+
+
+def parse_product(response):
+    selector = Selector(response.text)
+    brand = selector.css('.branding>a::attr(title)').extract_first()
+    return {
+        "name":         selector.css('.product-name>h1::text').extract_first(),
+        "gender":       "girls",
+        "description":  parse_description(selector),
+        "image_urls":   selector.css(".slide>a>img::attr(data-more-views)").extract(),
+        "care":         [],
+        "brand":        selector.css('.branding>strong::text').extract_first(),
+        "url":          response.url,
+        "retailer_sku": selector.css('.product-ids::attr(data-sku)').extract_first()[:11],
+        "category":     parse_category(selector),
+        "skus":         {sku.get("sku_id"): sku for sku in yield_sku(selector)}
+    }
--- scraper.py
+++ scraper.py
@@ -1,21 +1,19 @@
 import requests
 from parsel import Selector
-from urllib.parse import urljoin, urlparse
-from productschema import Product
+from productschema import parse_product
 
 
 class Scraper:
     def __init__(self, url):
         self.products = []
-        self._url = f'{urlparse(url).scheme}://{urlparse(url).netloc}'
+        self._url = url
         self._pending_urls = {url}
         self._seen_urls = set()
 
     def _extract_urls(self, html):
         new_urls = set()
         selector = Selector(html)
-        for anchor in selector.css('a::attr(href)').extract():
-            url = urljoin(self._url, anchor)
+        for url in selector.css('a::attr(href)').extract():
             if url not in self._seen_urls and url.startswith(self._url):
                 new_urls.add(url)
         return new_urls
@@ -23,7 +21,8 @@ class Scraper:
     def _extract_product(self, response):
         selector = Selector(response.text)
         if selector.css('.catalog-product-view').extract_first():
-            product = Product(response.url, selector)
+            product = parse_product(response)
+            print(product)
             self.products.append(product)
 
     def scrap(self):
@@ -38,22 +37,6 @@ class Scraper:
 
 
 if __name__ == "__main__":
-    cloth_reg = "https://www.liujo.com/gb/chino-corto-reg-w-12.html"
-    cloth_dis = "https://www.liujo.com/gb/short-con-applicazioni-12.html"
-    cloth_out = "https://www.liujo.com/gb/shorts-c-cintura-12.html"
-    shoes_reg = "https://www.liujo.com/gb/sandalo-111.html"
-    shoes_dis = "https://www.liujo.com/gb/mocassino-tc-90-tortora-7.html"
-    shoes_out = "https://www.liujo.com/gb/ciabattina-tavolara-12.html"
-    thing_reg = "https://www.liujo.com/gb/xs-crossbody-gioia-4.html"
-    thing_dis = "https://www.liujo.com/gb/s-cross-body-arizona-2.html"
-    thing_out = "https://www.liujo.com/gb/xs-crossbody-baltimora-3.html"
-    onesku_reg = "https://www.liujo.com/gb/minaudiere-magenta.html"
-    onesku_dis = "https://www.liujo.com/gb/xs-cross-body-maryland.html"
-    kidos_mon = "https://www.liujo.com/gb/t-shirt-m-l-stripes-46.html"
-    kidos_age = "https://www.liujo.com/gb/pant-lungo-jersey-fashion-10.html"
-    product_L = "https://www.liujo.com/gb/calzamaglia-shoes-3.html"
-    T_U_case = "https://www.liujo.com/gb/m-satchel-manhattan-11-1.html?color=5955"
-    test = "https://www.liujo.com/gb/minaudiere-magenta-2.html"
-    scraper = Scraper(test)
+    scraper = Scraper('https://www.liujo.com/gb/abito-1037.html')
     products = scraper.scrap()
     print(len(products), products)
