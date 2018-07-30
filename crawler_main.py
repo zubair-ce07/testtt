ce6ff9a6ad78dfbf42765471fa2078a0b33638e1
zeba ramzan <zeba.ramzan@arbisoft.com>
2018-07-30 14:59:08 +0500

    printed product when returned

--- crawler_main.py
+++ crawler_main.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from parsel import Selector
 from Product_details import Product
 
@@ -7,56 +8,43 @@ class UpsideSportCrawler(object):
 
     def __init__(self):
         self.visited_urls = []
-        self.products = []
 
     def parse_category(self, url):
 
         selector = Selector(requests.get(url).text)
-
         products_urls = selector.css(
             '.product-item-link::attr(href)').extract()
 
         for product_url in products_urls:
-
             if(product_url not in self.visited_urls):
-
-                product = Product(product_url).parse_product()
-
-                self.products.append(product)
-
+                product = Product().parse_product(product_url)
+                print(json.dumps(product, indent=4))
                 self.visited_urls.append(product_url)
 
         next_page = selector.css('.action.next::attr(href)').get()
-
         self.parse_category(next_page) if next_page else None
 
+    def parse_url(self, url):
 
-def parse_url(url):
+        selector = Selector(requests.get(url).text)
+        categories_urls = selector.css(
+            '.nav-item.level2 > a::attr(href)').extract()
+        sale_men_url = selector.css(
+            '.nav-item.level1.nav-5-1 > a::attr(href)').extract()
+        sale_women_url = selector.css(
+            '.nav-item.level1.nav-5-2 > a::attr(href)').extract()
 
-    selector = Selector(requests.get(url).text)
+        categories_urls.extend(sale_men_url)
+        categories_urls.extend(sale_women_url)
 
-    categories_urls = selector.css(
-        '.nav-item.level2 > a::attr(href), .nav-item.level1.nav-5-1 > a::attr(href), .nav-item.level1.nav-5-2 > a::attr(href)').extract()
-
-    return categories_urls
-
-
-def print_products(upside_crawler):
-
-    for product in upside_crawler.products:
-        print(product)
+        for url in categories_urls:
+            self.parse_category(url)
 
 
 def main():
 
-    categories_urls = parse_url("https://www.theupsidesport.com/")
-
     us_crawler = UpsideSportCrawler()
-
-    for url in categories_urls:
-        us_crawler.parse_category(url)
-
-    print_products(us_crawler)
+    us_crawler.parse_url("https://www.theupsidesport.com/")
 
 
 if __name__ == '__main__':
