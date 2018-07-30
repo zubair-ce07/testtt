6bdfc1dd8d46b1fb6c7f554c934a27e427f6c852
zeba ramzan <zeba.ramzan@arbisoft.com>
2018-07-30 15:00:31 +0500

    used dictionary for product and sku features, and loaded script as json

--- Product_details.py
+++ Product_details.py
@@ -1,105 +1,85 @@
 import requests
 import re
+import json
 from parsel import Selector
 
 
-class Sku(object):
-    def __init__(self):
-        self.color = ""
-        self.price = ""
-        self.currency = ""
-        self.size = ""
-        self.previous_prices = []
-        self.out_of_stock = "false"
-        self.sku_id = ""
+class Product():
 
     def extract_sku_features(self, selector, size, retailer_sku):
-        self.color = selector.css('.color::text').get()
-
-        self.price = selector.css(
+        sku = {}
+        sku['color'] = selector.css('.color::text').get()
+        sku['price'] = selector.css(
             '[id^=product-price] > .price::text').get()
 
-        self.currency = selector.css(
+        sku['currency'] = selector.css(
             '.inner-heading > .label::text').getall()[1]
 
-        self.size = size
-
-        old_price = selector.css(
+        sku['size'] = size
+        sku['previous_prices'] = selector.css(
             '[id^=old-price] > .price::text').get()
 
-        self.previous_prices.append(old_price)
+        sku['out_of_stock'] = "false"
+        sku['sku_id'] = retailer_sku + "_" + size
+        return sku
 
-        self.sku_id = retailer_sku + "_" + size
+    def generate_skus(self, sizes, selector, retailer_sku):
+        generated_skus = []
+        for size in sizes:
+            sku = self.extract_sku_features(selector, size, retailer_sku)
+            generated_skus.append(sku)
+        return generated_skus
 
-        return self.__dict__
-
-
-class Product(object):
-    def __init__(self, url):
-
-        self.retailer_sku = ""
-        self.gender = 'women' if 'women' in url else 'men'
-        self.category = []
-        self.brand = "The Upside Sport"
-        self.url = url
-        self.name = ""
-        self.description = ""
-        self.care = ""
-        self.image_urls = []
-        self.skus = []
-
-    def extract_category(self):
+    def extract_category(self, url):
 
         category = re.search(
-            r'^https:\/\/[^\/]*\/[^\/]*\/[^\/]*\/([^\/]*).*', self.url)
+            r'^https:\/\/[^\/]*\/[^\/]*\/[^\/]*\/([^\/]*).*', url)
 
         if category:
-            return "sale" if "sale" in self.url else category.group(1)
+            return "sale" if "sale" in url else category.group(1)
         else:
             return ""
 
     def extract_image_urls(self, selector):
+        images_url = []
+        images_x = selector.xpath(
+            '//script[@type="text/x-magento-init" and contains(text(), "mage/gallery/gallery")]/text()')
 
-        images_script = selector.xpath(
-            '//script[@type="text/x-magento-init" and contains(text(), "mage/gallery/gallery")]/text()').get()
+        images_script = images_x.get()
+        images_script_json = json.loads(images_script)
+        images_urls = images_script_json.get(
+            "[data-gallery-role=gallery-placeholder]").get("mage/gallery/gallery").get("data")
 
-        images = re.findall(
-            r'\"full\":\"https:[\\/a-z0-9\.\-_]+\.png', images_script)
-
-        return [img.replace('\\', '').replace(
-            '\"full\":\"', '') for img in images]
+        for image_urls in images_urls:
+            images_url.append(image_urls.get("full"))
+        return images_url
 
     def extract_sizes(self, selector):
 
-        sizes_script = selector.xpath(
-            '//script[contains(text(), "AEC.SUPER")]/text()').get()
-        sizes = re.findall('\"default_label\": \"(.+?)\",', sizes_script)
+        sizes_x = selector.xpath(
+            '//script[contains(text(), "AEC.SUPER")]/text()')
 
+        sizes_script = sizes_x.get()
+        sizes = re.findall('\"default_label\": \"(.+?)\",', sizes_script)
         return sizes
 
-    def parse_product(self):
-        selector = Selector(requests.get(self.url).text)
-
-        self.retailer_sku = selector.css(
+    def parse_product(self, url):
+        product = {}
+        selector = Selector(requests.get(url).text)
+        product['retailer_sku'] = selector.css(
             '.product.attribute.sku.custom > .value::text').get()
 
-        self.category = self.extract_category()
-
-        self.name = selector.css('.page-title > .base::text').get()
-
-        self.description = selector.css(
+        product['gender'] = 'women' if 'women' in url else 'men'
+        product['category'] = self.extract_category(url)
+        product['brand'] = "The Upside Sport"
+        product['url'] = url
+        product['name'] = selector.css('.page-title > .base::text').get()
+        product['description'] = selector.css(
             '.product.attribute.description > .value > .p1::text').getall()
 
-        self.care = selector.css('.fabric-care > ul > li::text').getall()
-
-        self.image_urls = self.extract_image_urls(selector)
-
+        product['care'] = selector.css('.fabric-care > ul > li::text').getall()
+        product['image_urls'] = self.extract_image_urls(selector)
         sizes = self.extract_sizes(selector)
-
-        for size in sizes:
-
-            sku = Sku().extract_sku_features(selector, size, self.retailer_sku)
-
-            self.skus.append(sku)
-
-        return self.__dict__
+        product['skus'] = self.generate_skus(
+            sizes, selector, product['retailer_sku'])
+        return product
