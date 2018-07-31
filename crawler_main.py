684e90901e9696fce0eb5f5601f39f49e8fd6dc8
zeba ramzan <zeba.ramzan@arbisoft.com>
2018-07-31 11:38:13 +0500

    used single css selector to extract category links instead of three

--- crawler_main.py
+++ crawler_main.py
@@ -1,44 +1,45 @@
 import requests
 import json
 from parsel import Selector
-from Product_details import Product
+from Product_details import ProductParser
 
 
 class UpsideSportCrawler(object):
 
     def __init__(self):
-        self.visited_urls = []
+        self.visited_products = []
 
-    def parse_category(self, url):
-
-        selector = Selector(requests.get(url).text)
-        products_urls = selector.css(
-            '.product-item-link::attr(href)').extract()
-
-        for product_url in products_urls:
-            if(product_url not in self.visited_urls):
-                product = Product().parse_product(product_url)
-                print(json.dumps(product, indent=4))
-                self.visited_urls.append(product_url)
-
+    def parse_next_page(selector):
         next_page = selector.css('.action.next::attr(href)').get()
         self.parse_category(next_page) if next_page else None
 
-    def parse_url(self, url):
+    def parse_category(self, product_id):
+        if(product_id not in self.visited_products):
+            product = ProductParser.parse_product(product_url)
+            print(json.dumps(product, indent=4))
+            self.visited_products.append(product_id)
 
+    def parse_product_url(products_urls):
+        for product_url in products_urls:
+            product_id = product_url.split("/")[:-1]
+            self.parse_category(product_id)
+
+    def parse_categories(self, url):
         selector = Selector(requests.get(url).text)
-        categories_urls = selector.css(
-            '.nav-item.level2 > a::attr(href)').extract()
-        sale_men_url = selector.css(
-            '.nav-item.level1.nav-5-1 > a::attr(href)').extract()
-        sale_women_url = selector.css(
-            '.nav-item.level1.nav-5-2 > a::attr(href)').extract()
+        products_urls = selector.css('.product-item-link::attr(href)').extract()
+        self.parse_product_url(products_urls)
+        self.parse_next_page(selector)
 
-        categories_urls.extend(sale_men_url)
-        categories_urls.extend(sale_women_url)
+    def filter_urls(self, categories_urls):
+        return [url for url in categories_urls if url.startswith('https://')]
 
-        for url in categories_urls:
-            self.parse_category(url)
+    def parse_url(self, url):
+        selector = Selector(requests.get(url).text)
+        categories_urls = selector.css('#mainmenu a::attr(href)').extract()
+        valid_urls = self.filter_urls(categories_urls)
+
+        for url in valid_urls:
+            self.parse_categories(url)
 
 
 def main():
