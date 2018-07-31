80881fd221c91a8e1142a3fb901b27e6bc850eff
Ali Nauman <ali60351@gmail.com>
2018-07-31 12:34:51 +0500

    generalized selectors, added get_product_currency method, prices => common_pricing, size => size_sel, color => color_sel refactor, handled multiple prices

--- apc_us/apc_us/spiders/apc_us_spider.py
+++ apc_us/apc_us/spiders/apc_us_spider.py
@@ -25,7 +25,7 @@ class APCUSSpider(scrapy.Spider):
             yield response.follow(url, self.parse_listing)
 
     def parse_listing(self, response):
-        product_css = 'a.product-image::attr(href)'
+        product_css = '.product-image::attr(href)'
 
         for url in response.css(product_css).extract():
             yield response.follow(url, self.parse_product)
@@ -46,26 +46,26 @@ class APCUSSpider(scrapy.Spider):
 
     @staticmethod
     def get_product_categories(response):
-        categories = response.css('div.breadcrumbs a::text').extract()
+        categories = response.css('.breadcrumbs a::text').extract()
         return categories[1:]
 
     @staticmethod
     def get_product_name(response):
-        return response.css('div.product-name h1::text').extract_first()
+        return response.css('.product-name h1::text').extract_first()
 
     @staticmethod
     def get_product_description(response):
-        description = response.css('section#description div > div::text').extract_first()
+        description = response.css('#description div div::text').extract_first()
         return [d.strip() for d in description.split('. ') if '%' not in d]
 
     @staticmethod
     def get_product_care(response):
-        care = response.css('section#description div > div::text').extract_first()
+        care = response.css('#description div div::text').extract_first()
         return [c.strip() for c in care.split('. ') if '%' in c]
 
     def get_product_images(self, response):
         retailer_sku = self.get_product_retailer_sku(response)
-        product_gallery = response.css('div.product-image-gallery img::attr(src)').extract()
+        product_gallery = response.css('.product-image-gallery img::attr(src)').extract()
         retailer_sku_lowercase = retailer_sku.lower()
         return [i.replace('600x', '1800x') for i in product_gallery
                 if retailer_sku_lowercase in i.lower()]
@@ -80,19 +80,24 @@ class APCUSSpider(scrapy.Spider):
         size_label = response.xpath('//dt/label[contains(@id, "size")]/@id').extract_first()
         return gender_map.get(size_label)
 
+    @staticmethod
+    def get_product_currency(response):
+        currency_xpath = '//script[contains(text(), "currency")]'
+        return response.xpath(currency_xpath).re(r'currency: \'([A-Z]+)\'')[0]
+
     def get_product_skus(self, response):
         skus = []
         colors = response.css('#configurable_swatch_color li')
-        sizes = response.css('ul.configurable-swatch-list.configurable-block-list.clearfix li')
-        prices = self.get_product_prices(response)
+        sizes = response.css('.configurable-swatch-list.configurable-block-list.clearfix li')
+        common_pricing = self.get_product_prices(response)
         raw_product = self.get_raw_product(response)
+        currency = self.get_product_currency(response)
 
-        for color in colors:
-            color_code, color_name = self.get_product_color(color)
-            for size in sizes:
-                sku = {}
-                sku.update(prices)
-                size_code, size_name = self.get_product_size(size)
+        for color_sel in colors:
+            color_code, color_name = self.get_product_color(color_sel)
+            for size_sel in sizes:
+                sku = common_pricing.copy()
+                size_code, size_name = self.get_product_size(size_sel)
                 key = f"{color_code},{size_code}"
                 raw_sku = raw_product.get(key)
                 if not raw_sku:
@@ -100,7 +105,7 @@ class APCUSSpider(scrapy.Spider):
 
                 sku['sku_id'] = raw_sku.get('product_id')
                 sku['color'] = color_name
-                sku['currency'] = 'USD'
+                sku['currency'] = currency
                 sku['size'] = size_name
                 sku['out_of_stock'] = not raw_sku.get('is_in_stock')
 
@@ -121,9 +126,9 @@ class APCUSSpider(scrapy.Spider):
 
     @staticmethod
     def get_raw_product(response):
-        script = response.xpath(
-            '//div[@id="product-options-wrapper"]//script[contains(text(), "StockStatus")]/text()'
-        ).extract_first()
+        raw_product_xpath = '//*[@id="product-options-wrapper"]' \
+                            '//script[contains(text(), "StockStatus")]/text()'
+        script = response.xpath(raw_product_xpath).extract_first()
         stock_status_json = script.split('new StockStatus(')[1].split(')')[0]
         return json.loads(stock_status_json)
 
@@ -133,14 +138,14 @@ class APCUSSpider(scrapy.Spider):
         return int(float(price.replace(',', '')) * 100)
 
     def get_product_prices(self, response):
-        prices = response.css('div.product-shop div.price-info span.price::text').extract()
+        prices = response.css('.product-shop .price-info .price::text').extract()
         prices = [self.get_price_from_string(price.strip()) for price in prices]
 
         product_price = {}
 
         if len(prices) > 1:
-            product_price['previous_price'] = prices[0]
-            product_price['price'] = prices[1]
+            product_price['price'] = prices.pop(-1)
+            product_price['previous_price'] = prices
         else:
             product_price['price'] = prices[0]
 
