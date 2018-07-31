3b2e31c86cfb67d1282cc7d304bb44d7e8787e5c
hamza-56 <hamza.shafique@arbiosoft.com>
2018-07-31 18:24:38 +0500

    removed unused imports, renamed is_dir, removed extra lines, removed variable for blank, changed return type to dict, improved the logic of parsing file, improved the logic of min, max, cleaned print statements

--- weatherman.py
+++ weatherman.py
@@ -1,21 +1,20 @@
-import os
 import argparse
-import datetime
 import calendar
 import copy
-import sys
 import csv
+import datetime
+import os
+import re
+import sys
 from time import strptime
-from collections import defaultdict
 
-EMPTY = ''
 COLOR_RED = '\033[91m'
 COLOR_BLUE = '\033[94m'
 COLOR_PURPLE = '\033[35m'
 COLOR_DEFAULT = '\033[0m'
 
 
-def is_dir(dirname):
+def if_dir_exists(dirname):
     if os.path.isdir(dirname):
         return dirname
     else:
@@ -23,11 +22,9 @@ def is_dir(dirname):
 
 
 def get_year_and_month(file_name):
-
     year_and_month = file_name.split('_')
     year = year_and_month[2]
     month = year_and_month[3][:-4]
-
     return year, str(strptime(month, '%b').tm_mon)
 
 
@@ -38,8 +35,7 @@ class FileParser:
         self.month_data = {}
 
     def parse_file(self, dir_path):
-
-        if is_dir(dir_path) is None:
+        if if_dir_exists(dir_path) is None:
             print('Sorry! The directory path is not valid')
             return None
 
@@ -48,12 +44,11 @@ class FileParser:
         for file_name in file_names:
             with open(dir_path + file_name, 'r') as file:
                 year, month = get_year_and_month(file_name)
-                read_csv = csv.DictReader(file, delimiter=',')
-                for i, row in enumerate(read_csv):
-                    row = {key.strip(): value.strip() for key, value in row.items()}
-                    self.month_data[str(i + 1)] = row
+                read_csv = csv.DictReader(file, skipinitialspace=True, delimiter=',')
+                for index, row in enumerate(read_csv):
+                    self.month_data[str(index + 1)] = row
 
-                if year not in self.year_data:
+                if year not in self.year_data.keys():
                     self.year_data[year] = {}
 
                 self.year_data[year][month] = copy.deepcopy(self.month_data)
@@ -65,99 +60,116 @@ class ResultComputer:
     def compute_avg_results(self, month_data):
         sum_highest = sum_lowest = sum_humidity = 0
         highest_temp_count = lowest_temp_count = humidity_count = 0
+        result = {}
 
         for day in month_data.values():
-            if day['Max TemperatureC'] is not EMPTY:
+            if day['Max TemperatureC'] is not '':
                 sum_highest += int(day['Max TemperatureC'])
                 highest_temp_count += 1
 
-            if day['Min TemperatureC'] is not EMPTY:
+            if day['Min TemperatureC'] is not '':
                 sum_lowest += int(day['Min TemperatureC'])
                 lowest_temp_count += 1
 
-            if day['Max Humidity'] is not EMPTY:
+            if day['Max Humidity'] is not '':
                 sum_humidity += int(day['Max Humidity'])
                 humidity_count += 1
 
-        return (
-            sum_highest / highest_temp_count, sum_lowest / lowest_temp_count,
-            sum_humidity / humidity_count
-        )
+        result['Highest Average'] = sum_highest / highest_temp_count
+        result['Lowest Average'] = sum_lowest / lowest_temp_count
+        result['Average Humidity'] = sum_humidity / humidity_count
+
+        return result
 
     def compute_extreme_results(self, year_data):
-        highest_temp = -273
-        lowest_temp = +273
-        highest_humidity = -100
+        min_temp_list = []
+        max_temp_list = []
+        max_humidity_list = []
+        result = {}
 
         for month in year_data.values():
-            for day in month.values():
+            value = max(month.items(), key=lambda x: x[1]['Max TemperatureC'])
+            max_temp_list.append((value[1]['Max TemperatureC'], value[1]['PKT']))
+            value = min(month.items(), key=lambda x: x[1]['Min TemperatureC'])
+            min_temp_list.append((value[1]['Min TemperatureC'], value[1]['PKT']))
+            value = max(month.items(), key=lambda x: x[1]['Max Humidity'])
+            max_humidity_list.append((value[1]['Max Humidity'], value[1]['PKT']))
 
-                if day['Max TemperatureC'] is not EMPTY:
-                    val = int(day['Max TemperatureC'])
-                    if val > highest_temp:
-                        highest_temp = val
-                        highest_temp_date = day['PKT']
+        highest_temp_and_date = max(max_temp_list, key=lambda x: x[0])
+        lowest_temp_and_date = min(min_temp_list, key=lambda x: x[0])
+        highest_humidity_and_date = max(max_humidity_list, key=lambda x: x[0])
 
-                if day['Min TemperatureC'] is not EMPTY:
-                    val = int(day['Min TemperatureC'])
-                    if val < lowest_temp:
-                        lowest_temp = val
-                        lowest_temp_date = day['PKT']
+        result['Highest Temp'] = highest_temp_and_date[0]
+        result['Highest Temp Date'] = highest_temp_and_date[1]
+        result['Lowest Temp'] = lowest_temp_and_date[0]
+        result['Lowest Temp Date'] = lowest_temp_and_date[1]
+        result['Highest Humidity'] = highest_humidity_and_date[0]
+        result['Highest Humidity Date'] = highest_humidity_and_date[1]
 
-                if day['Max Humidity'] is not EMPTY:
-                    val = int(day['Max Humidity'])
-                    if val > highest_humidity:
-                        highest_humidity = val
-                        highest_humidity_date = day['PKT']
-
-        return (
-            highest_temp, highest_temp_date,
-            lowest_temp, lowest_temp_date,
-            highest_humidity, highest_humidity_date
-        )
+        return result
 
     def compute_high_low_day_results(self, month_data):
-
-        temp_list = []
+        result_list = []
 
         for day in month_data.values():
-            if day['Max TemperatureC'] is not EMPTY and day['Min TemperatureC'] is not EMPTY:
-                temp_list.append((int(day['Max TemperatureC']), int(day['Min TemperatureC'])))
+            if day['Max TemperatureC'] is not '' and day['Min TemperatureC'] is not '':
+                result_list.append((int(day['Max TemperatureC']), int(day['Min TemperatureC'])))
 
-        return temp_list
+        return result_list
 
 
 class ResultGenerator:
-    def gen_ext_results(self, data):
-        date = datetime.datetime.strptime(data[1], '%Y-%m-%d')
-        print('Highest: {}C on {} {}'.format(data[0], calendar.month_name[date.month], date.day))
-        date = datetime.datetime.strptime(data[3], '%Y-%m-%d')
-        print('Lowest: {}C on {} {}'.format(data[2], calendar.month_name[date.month], date.day))
-        date = datetime.datetime.strptime(data[5], '%Y-%m-%d')
-        print('Humidity: {}% on {} {}\n'.format(data[4], calendar.month_name[date.month], date.day))
 
-    def gen_avg_results(self, data):
-        print('Highest Average: {}'.format(round(data[0], 2)))
-        print('Lowest Average: {}'.format(round(data[1], 2)))
-        print('Average Mean Humidity: {}\n'.format(round(data[2], 2)))
+    def generate_extreme_results(self, data):
+        date = datetime.datetime.strptime(data['Highest Temp Date'], '%Y-%m-%d')
+        print('Highest: {}C on {} {}'.format(data['Highest Temp'],
+                                             calendar.month_name[date.month], date.day))
+        date = datetime.datetime.strptime(data['Lowest Temp Date'], '%Y-%m-%d')
+        print('Lowest: {}C on {} {}'.format(data['Lowest Temp'],
+                                            calendar.month_name[date.month], date.day))
+        date = datetime.datetime.strptime(data['Highest Humidity Date'], '%Y-%m-%d')
+        print('Humidity: {}% on {} {}\n'.format(data['Highest Humidity'],
+                                                calendar.month_name[date.month], date.day))
 
-    def gen_high_low_day_double_results(self, data):
+    def generate_average_results(self, data):
+        print('Highest Average: {}'.format(round(data['Highest Average'], 2)))
+        print('Lowest Average: {}'.format(round(data['Lowest Average'], 2)))
+        print('Average Mean Humidity: {}\n'.format(round(data['Average Humidity'], 2)))
 
-        for i, val in enumerate(data):
-            print(COLOR_PURPLE + str(i + 1).zfill(2) + ' ' + COLOR_RED + '+' * data[i][0] + ' ' + COLOR_PURPLE + str(
-                data[i][0]) + 'C\n'
-                  + COLOR_PURPLE + str(i + 1).zfill(2) + ' ' + COLOR_BLUE + '+' * data[i][1] + ' ' + COLOR_PURPLE + str(
-                data[i][1]) + 'C')
+    def generate_high_low_day_double_results(self, data):
+        for index, record in enumerate(data):
+            print('{:0>2d} {} {}C\n{:0>2d} {} {}C'.format(index + 1, COLOR_RED + '+' * record[0],
+                                                          COLOR_PURPLE + str(record[0]), index + 1,
+                                                          COLOR_BLUE + '+' * record[1], COLOR_PURPLE + str(record[1])))
+
+        print(COLOR_DEFAULT)
+
+    def generate_high_low_day_single_results(self, data):
+        for index, record in enumerate(data):
+            print('{:0>2d} {}{} {}C - {}C'.format(index + 1, COLOR_BLUE + '+' * record[1],
+                                                  COLOR_RED + '+' * record[0],
+                                                  COLOR_PURPLE + str(record[1]), str(record[0])))
 
         print(COLOR_DEFAULT)
 
-    def gen_high_low_day_single_results(self, data):
 
-        for i, val in enumerate(data):
-            print(COLOR_PURPLE + str(i + 1).zfill(2) + ' ' + COLOR_BLUE + '+' * data[i][1] + COLOR_RED +
-                  '+' * data[i][0] + ' ' + COLOR_PURPLE + str(data[i][1]) + 'C - ' + str(data[i][0]) + 'C')
+def print_data_not_present_message():
+    print('Sorry! data is not available of required year')
 
-        print(COLOR_DEFAULT)
+
+def print_invalid_input_message():
+    print('Invalid input format')
+
+
+def validate_year_and_month(year_and_month):
+    match = re.search('^(20\d{2}|19\d{2}|0(?!0)\d|[1-9]\d)/(1[0-2]|0[1-9]|\d)', year_and_month)
+    year_and_month = {}
+    if match:
+        year_and_month['year'] = match.group(1)
+        year_and_month['month'] = match.group(2)
+        return year_and_month
+    else:
+        return None
 
 
 def main():
@@ -181,47 +193,74 @@ def main():
 
     my_computer = ResultComputer()
 
-    my_result_gen = ResultGenerator()
+    my_result_generator = ResultGenerator()
 
     if args.extreme_report:
-        if args.extreme_report in my_parser.year_data:
-            my_result_gen.gen_ext_results(
-                my_computer.compute_extreme_results(my_parser.year_data[args.extreme_report]))
+        if args.extreme_report in my_parser.year_data.keys():
+            year_data = my_parser.year_data[args.extreme_report]
+            year_result = my_computer.compute_extreme_results(year_data)
+            my_result_generator.generate_extreme_results(year_result)
         else:
-            print('Sorry! Data is not available for report')
+            print_data_not_present_message()
             sys.exit(1)
 
     if args.average_report:
-        tokens = args.average_report.split('/')
-        year = tokens[0]
-        month = tokens[1]
+        year_and_month = validate_year_and_month(args.average_report)
+        if year_and_month:
+            year = year_and_month['year']
+            month = year_and_month['month']
+        else:
+            print_invalid_input_message()
+            sys.exit(2)
 
-        my_result_gen.gen_avg_results(
-            my_computer.compute_avg_results(my_parser.year_data[year][month]))
+        if year in my_parser.year_data.keys():
+            if month in my_parser.year_data[year].keys():
+                month_data = my_parser.year_data[year][month]
+                month_result = my_computer.compute_avg_results(month_data)
+                my_result_generator.generate_average_results(month_result)
+            else:
+                print_data_not_present_message()
+        else:
+            print_data_not_present_message()
 
     if args.chart_report:
-        tokens = args.chart_report.split('/')
-        year = tokens[0]
-        month = tokens[1]
+        year_and_month = validate_year_and_month(args.chart_report)
+        if year_and_month:
+            year = year_and_month['year']
+            month = year_and_month['month']
+        else:
+            print_invalid_input_message()
+            sys.exit(3)
 
-        my_result_gen.gen_high_low_day_double_results(
-            my_computer.compute_high_low_day_results(my_parser.year_data[year][month]))
+        if year in my_parser.year_data.keys():
+            if month in my_parser.year_data[year].keys():
+                month_data = my_parser.year_data[year][month]
+                month_result = my_computer.compute_high_low_day_results(month_data)
+                my_result_generator.generate_high_low_day_double_results(month_result)
+            else:
+                print_data_not_present_message()
+        else:
+            print_data_not_present_message()
 
     if args.bonus:
-        tokens = args.bonus.split('/')
-        year = tokens[0]
-        month = tokens[1]
+        year_and_month = validate_year_and_month(args.bonus)
+        if year_and_month:
+            year = year_and_month['year']
+            month = year_and_month['month']
+        else:
+            print_invalid_input_message()
+            sys.exit(4)
 
-        my_result_gen.gen_high_low_day_single_results(
-            my_computer.compute_high_low_day_results(my_parser.year_data[year][month]))
+        if year in my_parser.year_data.keys():
+            if month in my_parser.year_data[year].keys():
+                month_data = my_parser.year_data[year][month]
+                month_result = my_computer.compute_high_low_day_results(month_data)
+                my_result_generator.generate_high_low_day_single_results(month_result)
+            else:
+                print_data_not_present_message()
+        else:
+            print_data_not_present_message()
 
 
 if __name__ == '__main__':
-    try:
-        main()
-    except IndexError:
-        print('Sorry! The input provided is not valid')
-        sys.exit(2)
-    except KeyboardInterrupt:
-        print('Killed by user')
-        sys.exit(4)
+    main()
