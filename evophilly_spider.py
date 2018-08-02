2fda46d0699f44b6964334739dd39d149453afb8
dawoodkhan5 <dawood.khan@arbisoft.com>
2018-08-02 11:33:29 +0500

    New Functionality

--- evophilly_spider.py
+++ evophilly_spider.py
@@ -1,3 +1,4 @@
+import re
 from datetime import datetime
 
 from scrapy import Request, FormRequest
@@ -15,12 +16,15 @@ class Mixin:
     room_data_url = 'https://evoatciracentresouth.prospectportal.com/Apartments/module/application_options/'
     landlord_slug = 'evophilly-property-group-pty-ltd'
     landlord_name = slugify('evo Philly Property Group Pty Ltd')
+    login_user = 'dak@gmail.com'
+    login_password = 'asd123'
 
 
 class EvophillySpider(BaseParseSpider, Mixin):
     name = Mixin.name + '-parse'
 
     def parse(self, response):
+        plan_dates = self.movein_moveout_dates(response.meta['plan_date'])
         loader = RoomLoader(item=Room(), response=response, date_format='%B %d, %Y')
         loader.add_value('property_name', 'evo Philly')
         loader.add_value('landlord_slug', self.landlord_slug)
@@ -29,59 +33,33 @@ class EvophillySpider(BaseParseSpider, Mixin):
         loader.add_value('property_amenities', response.meta['property_amenities'])
         loader.add_value('property_images', response.meta['property_images'])
         loader.add_value('property_description', response.meta['property_description'])
+        loader.add_value('move_in_date', plan_dates[0])
+        loader.add_value('move_out_date', plan_dates[1])
         common_room = loader.load_item()
 
-        for rooms_sel in response.css('.js-select-floorplan'):
-            url = clean(response.css('#application_location ::attr(action)'))[0]
-            form_data = {
-                'current_navigation_url': '',
-                'is_from_navigation_button': '0',
-                'application[space_configuration_id]': clean(response.css('#space_configuration_id ::attr(value)'))[0],
-                'application[desired_rent_min]': clean(rooms_sel.css('::attr(fp_min_rent)'))[0],
-                'application[desired_rent_max]': clean(rooms_sel.css('::attr(fp_max_rent)'))[0],
-                'application[lease_start_window_id]': response.meta['planid'],
-                'application[property_floorplan_id]': clean(response.css('::attr(value)'))
-            }
-            yield FormRequest(url=url, formdata=form_data, callback=self.request_room, meta={'room': common_room})
+        for room_sel in response.css('.list-item'):
+            loader_r = RoomLoader(item=common_room.copy(), selector=room_sel, response=room_sel)
+            self.parse_rooms(loader_r)
+            yield loader_r.load_item()
 
-    def request_room(self, response):
-        return Request(url=self.room_data_url, callback=self.parse_room, meta={'room': response.meta['room']})
-
-    def parse_room(self, response):
-        raw_room = response.meta['room']
-        loader = RoomLoader(item=raw_room, response=response)
-        loader.add_css('room_name', '.selected-unit-name ::text')
-        loader.add_value('room_amenities', self.amenities(response))
+    def parse_rooms(self, loader):
+        loader.add_css('room_name', '.title-block .title span::text')
+        loader.add_css('room_amenities', '.title-block .sub-title ::text')
+        loader.add_css('room_photos', '.layout img::attr(src)')
         loader.add_value('room_availability', 'available')
-        loader.add_value('move_in_date', self.moveindate(response))
-        loader.add_value('move_out_date', self.moveindate(response))
-        loader.add_value('room_price', self.room_price(response))
-        loader.add_value('deposit_amount', self.room_deposit(response))
-        return loader.load_item()
+        loader.add_css('room_price', '.room-type-table .room-row .monthly-col::text')
 
-    def room_deposit(self, response):
-        deposit_sel = response.css('.shopping-cart')
-        return clean(deposit_sel.css('.pay-total.js-pay-total ::text'))
+    def movein_moveout_dates(self, in_out_date):
+        plan_dates = []
+        in_out_dates = in_out_date.split('-')
+        for plan_date in in_out_dates:
+            plan_dates.append(self.date_format(plan_date))
+        return plan_dates
 
-    def room_price(self, response):
-        price_sel = response.css('#rental-options')
-        return clean(price_sel.css('.item-price span::text'))
-
-    def amenities(self, response):
-        amenities = []
-        for aminiti in response.css('.unit-details-item'):
-            title = (clean(aminiti.css('.title ::text')) or [''])[0]
-            value = (clean(aminiti.css('.value ::text')) or [''])[0]
-            amenities.append(f'{title}:{value}')
-        return amenities
-
-    def date_format(self, in_out_date):
-        indate = datetime.strptime(in_out_date, "%b %d, %Y").date()
+    def date_format(self, plan_date):
+        start_end_date = re.search("([0-9]{2}/[0-9]{2}/[0-9]{4})", plan_date)[0]
+        indate = datetime.strptime(start_end_date, "%m/%d/%Y").date()
         return indate.strftime('%B %d, %Y')
-
-    def moveindate(self, response):
-        move_in_date = clean(response.css('#cart-movein-date ::text'))[0]
-        return self.date_format(move_in_date)
 
 
 class EvophillyCrawler(BaseCrawlSpider, Mixin):
@@ -93,8 +71,8 @@ class EvophillyCrawler(BaseCrawlSpider, Mixin):
 
     def start_requests(self):
         yield Request(url=self.url, callback=self.parse_description)
-        yield Request(url=self.url+'amenities', callback=self.parse_amenities)
-        yield Request(url=self.url+'apply', callback=self.parse_login_page)
+        yield Request(url=self.url + 'amenities', callback=self.parse_amenities)
+        yield Request(url=self.url + 'apply', callback=self.login_page_request)
 
     def parse_description(self, response):
         self.decription = clean(response.css('.pane-node-body .field--body p::text'))[0]
@@ -103,19 +81,19 @@ class EvophillyCrawler(BaseCrawlSpider, Mixin):
         self.property_amenities = clean(response.css('.pane-amenities-list p::text'))
         self.property_images = clean(response.css('.photoswipe-gallery img::attr(src)'))
 
-    def parse_login_page(self, response):
+    def login_page_request(self, response):
         url = clean(response.css('#returning_applicants_login ::attr(action)'))[0]
         form_data = {
-            'applicant[username]': 'qwe@gmail.com',
-            'applicant[password]': 'asd123'
+            'applicant[username]': self.login_user,
+            'applicant[password]': self.login_password
         }
         yield FormRequest(url, formdata=form_data, callback=self.parse_login)
 
     def parse_login(self, response):
         url = clean(response.css('.btn.js-app-nav ::attr(href)'))[0]
-        yield Request(url, self.request_plans)
+        yield Request(url, self.plans_request)
 
-    def request_plans(self, response):
+    def plans_request(self, response):
         url = clean(response.css('#lease_start_window_id ::attr(data-url)'))[0]
         plan_sel = response.css('.vertform-item.radio')
         plan_id = clean(plan_sel[1].css('.js-select-lease-start-window ::attr(value)'))[0]
@@ -126,7 +104,6 @@ class EvophillyCrawler(BaseCrawlSpider, Mixin):
             'property_url': self.url,
             'property_contact_info': self.contact_info(response.css('.vcard')),
             'property_description': self.decription,
-            'planid': plan_id,
             'plan_date': plan_date,
         }
         yield Request(url=url + plan_id, callback=self.parse_item, meta=meta)
