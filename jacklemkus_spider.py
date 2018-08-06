3e32ba234227ce40f4faad363a69a3c9b18bdc8c
Aftab Akram <aftabakram>
2018-08-06 15:21:53 +0500

    category css improved and extra spacing removed

--- jacklemkus_spider.py
+++ jacklemkus_spider.py
@@ -27,7 +27,6 @@ class JacklemkusSpider(CrawlSpider):
         product_item['image_urls'] = self.get_image_urls(response)
         product_item['description'] = self.get_description(response)
         product_item['name'] = self.get_product_name(response)
-
         product_item['gender'] = self.get_gender(response)
         product_item['category'] = self.get_categories(response)
         product_item['url'] = self.get_product_url(response)
@@ -37,70 +36,64 @@ class JacklemkusSpider(CrawlSpider):
         yield product_item
 
     def get_product_name(self, response):
-        return response.css('.product-essential .product-name h1::text').extract_first()
+        prod_name_css = '.product-essential .product-name h1::text'
+        return response.css(prod_name_css).extract_first()
 
     def get_description(self, response):
-        return list(map(str.strip, response.css('#description-tab .std::text').extract()))
+        desc_css = '#description-tab .std::text'
+        return list(map(str.strip, response.css(desc_css).extract()))
 
     def get_retailer_sku(self, response):
-        return response.css('.prod-sku .sku::text').extract_first()
+        prod_sku_css = '.prod-sku .sku::text'
+        return response.css(prod_sku_css).extract_first()
 
     def get_image_urls(self, response):
-        return response.css('.product-essential .product-image a::attr(href)').extract()
+        image_urls_css = '.product-essential .product-image a::attr(href)'
+        return response.css(image_urls_css).extract()
 
     def get_gender(self, response):
-        product_details = dict(
-            zip([row for row in response.css('#more-info-tab .data-table .label::text').extract()],
-                response.css('#more-info-tab .data-table .data::text').extract()))
-        return product_details.get('Gender')
+        gender_css = '#product-attribute-specs-table tr:contains(Gender) .data::text'
+        return response.css(gender_css).extract_first()
 
     def get_brand(self, response):
-        product_details = dict(
-            zip(['Brand' if row.endswith('Brand') else row
-                 for row in response.css('#more-info-tab .data-table .label::text').extract()],
-                response.css('#more-info-tab .data-table .data::text').extract()))
-
-        return product_details.get('Brand')
+        brand_css = '#product-attribute-specs-table tr:contains(Brand) .data::text'
+        return response.css(brand_css).extract_first()
 
     def get_product_url(self, response):
         return response.url
 
     def get_categories(self, response):
-        product_details = dict(
-            zip(['Type' if row.endswith('Type') else row and 'Brand' if row.endswith('Brand') else row
-                 for row in response.css('#more-info-tab .data-table .label::text').extract()],
-                response.css('#more-info-tab .data-table .data::text').extract()))
-
-        category_css = '.breadcrumbs li[itemprop="itemListElement"]:not(.home) a::text'
-        categories = response.css(category_css).extract()
-
-        categories.append(product_details.get('DEPARTMENT'))
-        categories.append(product_details.get('Type'))
-
+        category_css = ['.breadcrumbs li[itemprop="itemListElement"]:not(.home) a::text',
+                        '#product-attribute-specs-table tr:contains({}) .data::text']
+        categories = response.css(category_css[0]).extract()
+        categories.append(response.css(category_css[1].format('DEPARTMENT')).extract_first())
+        categories.append(response.css(category_css[1].format('Type')).extract_first())
         return list(filter(None, categories))
 
-    def get_skus(self, response):
-        product_skus = []
+    def get_price(self, response):
+        price_css = '.product-essential span.price::text'
+        return response.css(price_css).extract_first()
 
-        product_sel = response.css('.product-data-mine::attr(data-lookup)')
+    def get_skus(self, response):
+        sku_css = '.product-data-mine::attr(data-lookup)'
+        product_sel = response.css(sku_css)
         if not any(product_sel):
             return []
+
         product_details = json.loads(product_sel.extract_first().replace("\'", "\""))
+        price = self.get_price(response)
 
-        price = response.css('.product-essential span.price::text').extract_first()
-
-        for item in product_details.values():
-            sku = {}
+        product_skus = []
+        for raw_sku in product_details.values():
+            sku = {"size": raw_sku.get("size"),
+                   "sku_id": "{}_{}".format(raw_sku.get("id"),
+                                            raw_sku.get("size").replace(" ", '_'))}
 
             if price[0] is 'R':
                 sku["price"] = float(price[1:].replace(",", ""))
                 sku["currency"] = JacklemkusSpider.currency
 
-            sku["size"] = item.get("size")
-
-            sku["sku_id"] = "{}_{}".format(item.get("id"), item.get("size").replace(" ", '_'))
-
-            if not item.get("stock_status"):
+            if not raw_sku.get("stock_status"):
                 sku["out_of_stock"] = True
 
             product_skus.append(sku)
