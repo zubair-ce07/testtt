037a02e3fd25370054c6e83527912c78872dfa4a
arslanather <arslan.ather@arbisoft.com>
2018-08-08 14:39:48 +0500

    using w3lib for adding pagenumber parameter now

--- Levis/Levis/spiders/levis_br_crawl.py
+++ Levis/Levis/spiders/levis_br_crawl.py
@@ -3,6 +3,7 @@
 from scrapy import Request
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from w3lib.url import add_or_replace_parameter
 
 from Levis.spiders.product_parser import ProductParser
 
@@ -30,10 +31,10 @@ class LevisBrCrawlSpider(CrawlSpider):
         if not pages:
             return
         pages = int(pages)
-
         request_url = response.css(".main script").re_first(".*(/buscapagina?.*=)'")
         for page in range(1, pages + 1):
-            request = Request(response.urljoin(f"{request_url}{page}"))
+            request_url = add_or_replace_parameter(request_url, 'PageNumber', page)
+            request = Request(response.urljoin(request_url), callback=self.parse)
             trail = response.meta['trail'].copy()
             trail.append(response.url)
             request.meta['trail'] = trail
