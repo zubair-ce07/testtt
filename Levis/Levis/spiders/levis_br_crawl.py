899789451fa15f6a94d27fb194ebe06e7b2c12ac
arslanather <arslan.ather@arbisoft.com>
2018-08-08 14:15:19 +0500

    removed crawl_start_time and parse_product and improved Rule for products

--- Levis/Levis/spiders/levis_br_crawl.py
+++ Levis/Levis/spiders/levis_br_crawl.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-import datetime
 
-import scrapy
+from scrapy import Request
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+
 from Levis.spiders.product_parser import ProductParser
 
 
@@ -12,12 +12,10 @@ class LevisBrCrawlSpider(CrawlSpider):
     allowed_domains = ['www.levi.com.br']
     start_urls = ['https://www.levi.com.br/']
 
-    start_time = None
-
     product_parser = ProductParser()
 
     rules = (Rule(LinkExtractor(restrict_css=".menu-departamento"), callback='parse_category'),
-             Rule(LinkExtractor(restrict_css=".product-name a"), callback='parse_product'))
+             Rule(LinkExtractor(restrict_css=".product-name a"), callback=product_parser.parse))
 
     def parse(self, response):
         requests = super().parse(response)
@@ -27,10 +25,6 @@ class LevisBrCrawlSpider(CrawlSpider):
             request.meta['trail'] = trail
             yield request
 
-    def start_requests(self):
-        self.start_time = datetime.datetime.now()
-        return super().start_requests()
-
     def parse_category(self, response):
         pages = response.css(".main script").re_first(".*pagecount_.*=\s(\d+)")
         if not pages:
@@ -39,14 +33,9 @@ class LevisBrCrawlSpider(CrawlSpider):
 
         request_url = response.css(".main script").re_first(".*(/buscapagina?.*=)'")
         for page in range(1, pages + 1):
-            request = scrapy.Request(response.urljoin(f"{request_url}{page}"))
+            request = Request(response.urljoin(f"{request_url}{page}"))
             trail = response.meta['trail'].copy()
             trail.append(response.url)
             request.meta['trail'] = trail
             yield request
 
-    def parse_product(self, response):
-        product = self.product_parser.parse(response)
-        product['crawl_start_time'] = self.start_time
-
-        yield product
