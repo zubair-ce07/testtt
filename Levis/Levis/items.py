f69e8543d5ee966166e4b762a7cfae6e8a94242b
arslanather <arslan.ather@arbisoft.com>
2018-08-08 14:19:46 +0500

    improved imports and removed some fields

--- Levis/Levis/items.py
+++ Levis/Levis/items.py
@@ -5,32 +5,29 @@
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
 
-import scrapy
+from scrapy import Item, Field
 
 
-class ProductItem(scrapy.Item):
-    retailer_sku = scrapy.Field()
-    lang = scrapy.Field()
-    uuid = scrapy.Field()
-    trail = scrapy.Field()
-    gender = scrapy.Field()
-    category = scrapy.Field()
-    industry = scrapy.Field()
-    brand = scrapy.Field()
-    url = scrapy.Field()
-    date = scrapy.Field()
-    market = scrapy.Field()
-    retailer = scrapy.Field()
-    crawl_id = scrapy.Field()
-    url_original = scrapy.Field()
-    name = scrapy.Field()
-    description = scrapy.Field()
-    care = scrapy.Field()
-    image_urls = scrapy.Field()
-    skus = scrapy.Field()
-    out_of_stock = scrapy.Field()
-    price = scrapy.Field()
-    currency = scrapy.Field()
-    spider_name = scrapy.Field()
-    crawl_start_time = scrapy.Field()
+class ProductItem(Item):
+    retailer_sku = Field()
+    lang = Field()
+    trail = Field()
+    gender = Field()
+    category = Field()
+    brand = Field()
+    url = Field()
+    date = Field()
+    market = Field()
+    retailer = Field()
+    crawl_id = Field()
+    url_original = Field()
+    name = Field()
+    description = Field()
+    care = Field()
+    image_urls = Field()
+    skus = Field()
+    out_of_stock = Field()
+    price = Field()
+    currency = Field()
+    spider_name = Field()
 
