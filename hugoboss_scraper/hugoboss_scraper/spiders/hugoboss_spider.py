1b5d5491f3f03b341cb90afa96300907a6899956
WadoodAmer <wadood.islam@arbisoft.com>
2018-08-09 19:05:43 +0500

    For check out draft.

--- hugoboss_scraper/hugoboss_scraper/spiders/hugoboss_spider.py
+++ hugoboss_scraper/hugoboss_scraper/spiders/hugoboss_spider.py
@@ -1,5 +1,7 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+import w3lib.url as w3url
+
 from .product_schema import Parser
 
 
@@ -9,27 +11,23 @@ class HugoBossSpider(CrawlSpider):
     start_urls = [
         'https://www.hugoboss.com/us/'
     ]
-    rules = (Rule(LinkExtractor(restrict_css='.swatch-list'), callback="parse_product"),
-             Rule(LinkExtractor(restrict_css=('.main-nav__scroller', 'pagingbar__item')), callback='parse'),
+
+    rules = (Rule(LinkExtractor(restrict_css='.swatch-list__image'), callback="parse_product"),
+             Rule(LinkExtractor(restrict_css='.nav-list--third-level'), callback='parse', process_links='clean_links'),
+             Rule(LinkExtractor(restrict_css='pagingbar__item'), callback='parse'),
              )
 
     def parse(self, response):
+        trail = response.meta.get("trail", [])
         for request in super(HugoBossSpider, self).parse(response):
-            if response.meta.get("trail"):
-                trail = response.meta["trail"].copy()
-                trail.append(response.url)
-                request.meta["trail"] = trail
-            else:
-                request.meta["trail"] = [response.url]
+            request.meta["trail"] = trail.copy().append(response.url)
             yield request
 
     def parse_product(self, response):
-        if response.css("body.pda"):
-            for url_or_item in self.parser.parse(response):
-                if not isinstance(url_or_item, dict):
-                    request = response.follow(url_or_item, callback=self.parse_product)
-                    request.meta["trail"] = response.meta["trail"].copy()
-                    request.meta["trail"].append(response.url)
-                    yield request
-                else:
-                    yield url_or_item
+        yield from self.parser.parse(response)
+
+    @staticmethod
+    def clean_links(links):
+        for link in links:
+            link.url = w3url.url_query_cleaner(link.url)
+            yield link
--- hugoboss_scraper/hugoboss_scraper/spiders/product_schema.py
+++ hugoboss_scraper/hugoboss_scraper/spiders/product_schema.py
@@ -1,108 +1,110 @@
 import json
+
 import scrapy
-from urllib.parse import urlparse
 import w3lib.url as w3url
 
 
 class Parser(scrapy.Spider):
     name = "Parser"
-    common_sku = {}
-    product = {}
-    response = None
     possible_genders = {"men", "women", "girls", "boys"}
-    Default_gender = "unisex_adults"
+    default_gender = "unisex_adults"
 
     def parse(self, response):
-        self.response = response
-        self.product["retailer_sku"] = self.get_retailer_sku()
-        self.product["lang"] = "de"
-        self.product["gender"] = self.get_gender()
-        self.product["category"] = self.get_category()
-        self.product["industry"] = None
-        self.product["brand"] = "Joop"
-        self.product["url"] = self.response.url
-        self.product["market"] = "DE"
-        self.product["trail"] = self.response.meta["trail"].copy()
-        self.product["retailer"] = "joop-de"
-        self.product["url_original"] = self.response.url
-        self.product["name"] = self.get_name()
-        self.product["description"] = self.get_description()
-        self.product["care"] = self.get_care()
-        self.product["image_urls"] = self.get_image_urls()
-        if not self.is_outofstock():
-            self.product["skus"] = self.get_skus()
-            self.product["price"] = self.get_price()
-            self.product["currency"] = self.get_currency()
-        else:
-            self.product["out_of_stock"] = True
-        product_or_url = [self.product]
-        product_or_url.extend(self.colour_urls())
-        return product_or_url
+        product = {}
+        product["retailer_sku"] = self.get_retailer_sku(response)
+        product["lang"] = "de"
+        product["gender"] = self.get_gender(response)
+        product["category"] = self.get_categories(response)
+        product["industry"] = None
+        product["brand"] = self.get_brand(response)
+        product["url"] = response.url
+        product["market"] = "DE"
+        product["trail"] = response.meta["trail"].copy()
+        product["retailer"] = "joop-de"
+        product["url_original"] = response.url
+        product["name"] = self.get_name(response)
+        product["description"] = self.get_description(response)
+        product["care"] = self.get_care(response)
+        product["image_urls"] = self.get_image_urls(response)
+        product["skus"] = self.get_skus(response)
+        product["price"] = self.get_price(response)
+        product["currency"] = self.get_currency(response)
+        yield product
+        yield from self.colour_requests(response)
 
-    def get_skus(self):
-        self.common_sku["price"] = self.get_price()
-        self.common_sku["currency"] = self.get_currency()
-        self.common_sku["colour"] = self.get_colour()
-        options = self.response.css('option[data-code]')
+    def get_skus(self, response):
+        common_sku = {}
+        common_sku["price"] = self.get_price(response)
+        common_sku["currency"] = self.get_currency(response)
+        common_sku["colour"] = self.get_colour(response)
+        sku_id_prefix = self.get_retailer_sku(response)
         skus = {}
-        for option in options:
-            sku = self.common_sku.copy()
-            if "unavailable" in option.css("::attr(class)").extract_first().split():
+        sizes = response.css('.swatch-list__size::text').extract()
+        unavailable_sizes = response.css('.swatch-list__size[class*="unselectable"]::text').extract()
+        for size in sizes:
+            sku = common_sku.copy()
+            sku["size"] = size.strip()
+            if size in unavailable_sizes:
                 sku["out_of_stock"] = True
-            if self.previous_prices(option):
-                sku["previous_prices"] = self.previous_prices(option)
-            skus[option.css("::attr(data-code)").extract_first()] = sku
+            skus[f"{sku_id_prefix}_{size.strip()}"] = sku
         return skus
 
-    #done
-    def get_price(self):
-        return float(self.response.css('meta[itemprop="price"]::attr(content)').extract_first())
+    @staticmethod
+    def get_price(response):
+        return float(response.css('meta[itemprop="price"]::attr(content)').extract_first())
 
-    #done
-    def get_currency(self):
-        return self.response.css('meta[itemprop="priceCurrency"]::attr(content)').extract_first()
+    @staticmethod
+    def get_currency(response):
+        return response.css('meta[itemprop="priceCurrency"]::attr(content)').extract_first()
 
-    #done
-    def get_colour(self):
-        variation_json = self.response.css('.product-variations::attr(data-current)').extract_first()
+    @staticmethod
+    def get_colour(response):
+        variation_json = response.css('.product-variations::attr(data-current)').extract_first()
         json_object = json.loads(variation_json)
         return json_object.get("color").get("displayValue")
 
-    #done
-    def get_name(self):
-        return self.response.css('h1[itemprop="name"]::text').extract_first()
+    @staticmethod
+    def get_name(response):
+        return response.css('h1[itemprop="name"]::text').extract_first()
 
-    def get_gender(self):
-        category = self.get_category()
-        return category[0] if category[0] in self.possible_genders else self.Default_gender
-
-    def get_category(self):
-        return self.response.css('.breadcrumb__title::text').extract()
-
-    #done
-    def get_image_urls(self):
-        image_paths = self.response.css('img.slider-item__image::attr(src)').extract()
-        return [w3url.url_query_cleaner(path) for path in image_paths]
-
-    #done
-    def get_retailer_sku(self):
-        return self.response.css('div[data-variationgroupid]::attr(data-variationgroupid)').extract_first()
+    def get_gender(self, response):
+        categories = self.get_categories(response)
+        for category in categories:
+            if category.lower() in self.possible_genders:
+                return category.lower()
+        return self.default_gender
 
     @staticmethod
-    def previous_prices(option):
-        previous_price = option.css("::attr(data-listprice)").extract_first()
-        return [100 * float(previous_price[:-2].replace(",", "."))] if previous_price else []
+    def get_categories(response):
+        unique_breadcrumbs = set(response.css('.breadcrumb__link::attr(title)').extract())
+        unique_breadcrumbs.remove("Home")
+        unique_breadcrumbs.remove("Shop")
+        return list(unique_breadcrumbs)
 
-    #done
-    def get_description(self):
-        return self.response.css('div.product-container__text__description::text').extract_first().strip().split('.\n\n')
+    @staticmethod
+    def get_image_urls(response):
+        image_paths = response.css('img.slider-item__image::attr(src)').extract()
+        return [w3url.url_query_cleaner(path) for path in image_paths]
 
-    #done
-    def get_care(self):
-        return self.response.css('.accordion__care-icon__text::text').extract()
+    @staticmethod
+    def get_retailer_sku(response):
+        return response.css('div[data-variationgroupid]::attr(data-variationgroupid)').extract_first()
 
-    def colour_urls(self):
-        return self.response.css('.colors a::attr(href)').extract()
+    @staticmethod
+    def get_description(response):
+        return response.css('div.product-container__text__description::text').extract_first().strip().split('.\n\n')
 
-    def is_outofstock(self):
-        return False
+    @staticmethod
+    def get_care(response):
+        return response.css('.accordion__care-icon__text::text').extract()
+
+    def colour_requests(self, response):
+        colour_urls = response.css('.swatch-list__image::attr(href)').extract()
+        trail = response.meta.get("trail", [])
+        for url in colour_urls:
+            request = response.follow(url, callback=self.parse)
+            request.meta["trail"] = trail.copy().append(response.url)
+            yield request
+
+    def get_brand(self, response):
+        pass
