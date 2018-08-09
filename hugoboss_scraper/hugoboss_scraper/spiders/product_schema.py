ca18722761da0226820b387ec8d4145969070313
WadoodAmer <wadood.islam@arbisoft.com>
2018-08-09 12:17:10 +0500

    Extracted price, currency, color, name,

--- hugoboss_scraper/hugoboss_scraper/spiders/product_schema.py
+++ hugoboss_scraper/hugoboss_scraper/spiders/product_schema.py
@@ -1,3 +1,4 @@
+import json
 import scrapy
 from urllib.parse import urlparse
 import w3lib.url as w3url
@@ -19,7 +20,7 @@ class Parser(scrapy.Spider):
         self.product["category"] = self.get_category()
         self.product["industry"] = None
         self.product["brand"] = "Joop"
-        self.product["url"] = w3url.url_query_cleaner(self.response.url)
+        self.product["url"] = self.response.url
         self.product["market"] = "DE"
         self.product["trail"] = self.response.meta["trail"].copy()
         self.product["retailer"] = "joop-de"
@@ -53,50 +54,55 @@ class Parser(scrapy.Spider):
             skus[option.css("::attr(data-code)").extract_first()] = sku
         return skus
 
+    #done
     def get_price(self):
-        price = self.response.css('meta[itemprop="price"]::attr(content)').extract_first()
-        return 100 * float(price.replace(',', '.'))
+        return float(self.response.css('meta[itemprop="price"]::attr(content)').extract_first())
 
+    #done
     def get_currency(self):
-        return self.response.css('meta[itemprop="currency"]::attr(content)').extract_first()
+        return self.response.css('meta[itemprop="priceCurrency"]::attr(content)').extract_first()
 
+    #done
     def get_colour(self):
-        return self.get_name().split()[-1]
+        variation_json = self.response.css('.product-variations::attr(data-current)').extract_first()
+        json_object = json.loads(variation_json)
+        return json_object.get("color").get("displayValue")
 
+    #done
     def get_name(self):
-        return self.response.css('h2[itemprop="name"]::text').extract_first()
+        return self.response.css('h1[itemprop="name"]::text').extract_first()
 
     def get_gender(self):
         category = self.get_category()
         return category[0] if category[0] in self.possible_genders else self.Default_gender
 
     def get_category(self):
-        return self.response.css('#breadcrumb a::text').extract()
+        return self.response.css('.breadcrumb__title::text').extract()
 
+    #done
     def get_image_urls(self):
-        image_paths = self.response.css('li[data-mimetype="image/jpeg"]::attr(data-detail)').extract()
-        url = urlparse(self.response.url)
-        return [f'{url.scheme}://{url.netloc}{path}' for path in image_paths]
+        image_paths = self.response.css('img.slider-item__image::attr(src)').extract()
+        return [w3url.url_query_cleaner(path) for path in image_paths]
 
+    #done
     def get_retailer_sku(self):
-        return self.response.css('span[data-code]::attr(data-code)').extract_first()
+        return self.response.css('div[data-variationgroupid]::attr(data-variationgroupid)').extract_first()
 
     @staticmethod
     def previous_prices(option):
         previous_price = option.css("::attr(data-listprice)").extract_first()
         return [100 * float(previous_price[:-2].replace(",", "."))] if previous_price else []
 
+    #done
     def get_description(self):
-        return self.response.css('div[itemprop=description]>div:nth-child(2) ::text').extract()
+        return self.response.css('div.product-container__text__description::text').extract_first().strip().split('.\n\n')
 
+    #done
     def get_care(self):
-        care_lines = self.response.css('div[itemprop=description]>div:last-child').css(' ::text').extract()
-        care_lines = [line.strip() for line in care_lines]
-        care_lines = ''.join(care_lines)
-        return care_lines.split(". ")
+        return self.response.css('.accordion__care-icon__text::text').extract()
 
     def colour_urls(self):
         return self.response.css('.colors a::attr(href)').extract()
 
     def is_outofstock(self):
-        return self.response.css('meta[itemprop="availability"][content="out_of_stock"]')
+        return False
