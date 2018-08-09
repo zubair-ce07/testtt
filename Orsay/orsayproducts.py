b8bd1aba87ffb4066e34238b3be31ecc962a3fdf
Muhammad Haseeb <muhammad.haseeb@arbisoft.com>
2018-08-09 14:35:58 +0500

    Selectors combined + Regex corrected + Following products added + code cleaned

--- Orsay/orsayproducts.py
+++ Orsay/orsayproducts.py
@@ -13,32 +13,35 @@ class OrsayproductSpider(CrawlSpider):
     start_urls = ['http://www.orsay.com/de-de/']
     rules = (
                 Rule(LinkExtractor(
-                    allow=(r'([\w \W])*/produkte/\Z',)), follow=True),
+                    allow=(r'/produkte/\Z',)),),
                 Rule(LinkExtractor(
                     allow=(r'([\w \W])*/produkte/([\w \W])*',)),
-                    callback='parse_listings', follow=True),
+                    callback='parse_listings',),
                 Rule(LinkExtractor(
                     allow=(r'([\w \W])*.html\Z',)),
-                    callback='parse_product', follow=True),
+                    callback='parse_product',),
             )
 
     def parse_listings(self, response):
+        products_urls = self.get_products_urls(response)
         next_page = self.get_next_page(response)
         if next_page:
             yield scrapy.Request(
                     url=next_page, callback=self.parse_listings)
+        for url in products_urls:
+            yield scrapy.Request(
+                url=url, callback=self.parse_product)
 
     def parse_product(self, response):
         product = Product()
-        product['Id'] = self.get_product_details(response)["productId"]
+        product_details = self.get_product_details(response)
+        product['Id'] = product_details["productId"]
         product['brand'] = "Orsay"
         product['description'] = self.get_product_description(response),
         product['product_imgs'] = self.get_product_imgs(response),
-        product['category'] = (
-            self.get_product_details(response))["categoryName"],
-        product['name'] = (self.get_product_details(response))["name"],
-        product['skus'] = self.convert_gen_to_dict(
-            self.get_product_skus(response)),
+        product['category'] = product_details["categoryName"],
+        product['name'] = product_details["name"],
+        product['skus'] = self.get_product_skus(response),
         product['urls'] = [response.request.url],
         product['care'] = [self.get_care_text(response)]
         colors_to_follow = self.has_more_colors(response)
@@ -65,12 +68,12 @@ class OrsayproductSpider(CrawlSpider):
         total_products = response.css(
                 ".load-more-progress-label::text").extract()
         for txt in total_products:
-            if self.has_numbers(txt):
+            if any(char.isdigit() for char in txt):
                 return self.get_number(txt)
     
     def get_next_page(self, response):
         product_url = response.url
-        if not self.has_numbers(response.url):
+        if not any(char.isdigit() for char in response.url):
             product_url = response.urljoin("?sz=72")    
         total_products = self.get_total_products(response)
         listed_products = self.get_number(product_url)
@@ -111,7 +114,7 @@ class OrsayproductSpider(CrawlSpider):
         for size in sizes:
             for color in colors:
                 skus = {
-                    product_details["productId"]+"_"+size: {
+                    "{}_{}".format(product_details["productId"], size): {
                         "color": color,
                         "currency": product_details["currency_code"],
                         "price": product_details["netPrice"],
@@ -125,7 +128,7 @@ class OrsayproductSpider(CrawlSpider):
             product_item['skus'].update(sub_skus)
             product_item['urls'].append(response.url)
         else:
-            yield sub_skus
+            yield sub_skus.get(0)
         if 'colors_list' in response.meta:
             if len(response.meta['colors_list']) == 0:
                 item = response.meta['item']
@@ -141,10 +144,20 @@ class OrsayproductSpider(CrawlSpider):
             
     def get_care_text(self, response):
         care_text = response.css(
-                    ".product-material div::text,.product-material p::text "
-                    ).extract()
+                    "{},{},{}".format(
+                        ".product-material div::text",
+                        ".product-material p::text",
+                        ".content-asset p::text"
+                    )).extract()
         care_text.extend(response.css(".content-asset p::text").extract())
         return self.clean_text(care_text)
+    
+    def get_products_urls(self, response):
+        product_urls = response.css(
+            ".grid-tile .product-image a::attr(href)").extract()
+        product_urls = [response.urljoin(url)
+                        for url in product_urls]
+        return product_urls
     
     def has_more_colors(self, response):
         colors_to_follow = response.css(
