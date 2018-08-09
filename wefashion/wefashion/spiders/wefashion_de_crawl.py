9438b67f5cdd074a174e8ea5613c00b7245d9eeb
Hamza953 <hamzaakhtar0@gmail.com>
2018-08-09 12:31:50 +0500

    extracted main and sub-categories

--- wefashion/wefashion/spiders/wefashion_de_crawl.py
+++ wefashion/wefashion/spiders/wefashion_de_crawl.py
@@ -1,11 +1,17 @@
-import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 
-class WefashionDeCrawlSpider(scrapy.Spider):
+class WefashionDeCrawlSpider(CrawlSpider):
     name = 'wefashion-de-crawl'
-    start_urls = ['http://https://www.wefashion.de//']
+    allowed_domains = ['example.com']
+    start_urls = [
+        'http://www.wefashion.de/'
+    ]
 
-    def parse(self, response):
-        pass
+    rules = (
+        Rule(LinkExtractor(restrict_css=['.header-top-level-menu', '#category-level-0']), follow=True, callback='parse_category_page'),
+    )
+
+    def parse_category_page(self, response):
+        print(f"Category {response.url}")
