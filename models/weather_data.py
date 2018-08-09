d9f471cf4448234e2a13dc82d24204e348c96ed3
asadshahjahan <m.asad.s94@gmail.com>
2018-08-09 21:04:30 +0500

    added year-set in weather_data

--- models/weather_data.py
+++ models/weather_data.py
@@ -1,5 +1,6 @@
 class WeatherData:
     __data = {}
+    __years_added = set()
 
     def __init__(self, entry):
         WeatherData.__add_weather_data(entry)
@@ -11,10 +12,21 @@ class WeatherData:
             WeatherData.__data[entry] = {}
         elif entry not in WeatherData.__data.keys():
             WeatherData.__data[entry] = {}
+        if entry not in WeatherData.__years_added:
+            WeatherData.__years_added.add(entry)
 
     @staticmethod
-    def get_data():
-        return WeatherData.__data
+    def get_data(key=None):
+        if key is None:
+            return WeatherData.__data
+        else:
+            if key in WeatherData.__data:
+                return WeatherData.__data[key]
+        return None
+
+    @staticmethod
+    def get_years():
+        return list(WeatherData.__years_added)
 
     @staticmethod
     def add_array_to_key(arr, key, entry, weather_entity_data):
@@ -24,10 +36,15 @@ class WeatherData:
             WeatherData.__data[entry] = dict(list(WeatherData.__data[entry].items()) + list(arr.items()))
 
     @staticmethod
+    def print_specific_key(key):
+        if key in WeatherData.__data:
+            print(WeatherData.__data[key])
+
+    @staticmethod
     def print_():
         print(WeatherData.__data)
         for k, v in sorted(WeatherData.__data.items()):
             print(k)
-            for month_k, month_v in sorted(v.items()):
+            for month_k, month_v in v.items():
                 print("\t", end='')
                 print(month_k, month_v)
