dbac8f414c3d74a4a530ab0bcfd8bbf8db14487b
Hamza953 <hamzaakhtar0@gmail.com>
2018-08-10 12:11:57 +0500

    extracted all items

--- wefashion/wefashion/spiders/wefashion_de_crawl.py
+++ wefashion/wefashion/spiders/wefashion_de_crawl.py
@@ -1,17 +1,31 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
+from wefashion.wefashion_de_parse import ProductParser
+
 
 class WefashionDeCrawlSpider(CrawlSpider):
     name = 'wefashion-de-crawl'
-    allowed_domains = ['example.com']
+    parser = ProductParser()
+    allowed_domains = ['www.wefashion.de']
     start_urls = [
-        'http://www.wefashion.de/'
+        # 'http://www.wefashion.de/'
+        'https://www.wefashion.de/de_DE/herren/t-shirts/printed-t-shirts/'
     ]
 
     rules = (
-        Rule(LinkExtractor(restrict_css=['.header-top-level-menu', '#category-level-0']), follow=True, callback='parse_category_page'),
+        # Rule(LinkExtractor(restrict_css=['.header-top-level-menu', '#category-level-0']),
+        #      follow=True,
+        #      callback='parse_category_page'),
+        Rule(LinkExtractor(restrict_css=['.header-top-level-menu']), callback='parse'),
+        Rule(LinkExtractor(restrict_css='.product-image'), callback='parse_item'),
     )
 
-    def parse_category_page(self, response):
-        print(f"Category {response.url}")
+    def parse(self, response):
+
+        for request in super(WefashionDeCrawlSpider, self).parse(response):
+            print(f"HAMZA {response.css('.image-holder::attr(data-color-id)').extract_first()}")
+            yield request
+
+    # def parse_item(self, response):
+    #     return self.parser.parse(response)
--- wefashion/wefashion/wefashion_de_parse.py
+++ wefashion/wefashion/wefashion_de_parse.py
@@ -1,6 +1,80 @@
-from scrapy import Spider
+import re
+
+import scrapy
+
 from wefashion.items import WefashionItem
 
 
-class ProductParser:
-    pass
+class ProductParser(scrapy.Spider):
+    name = 'wefashion-de-parser'
+    brand = "WE"
+
+    def parse(self, response):
+        item = HunkemollerItem()
+        item['retailer_sku'] = self.extract_retailer_sku(response)
+        item['trail'] = self.extract_trails(response)
+        item['gender'] = self.gender
+        item['category'] = self.extract_category(response)
+        item['brand'] = self.brand
+        item['url'] = response.url
+        item['name'] = self.extract_product_name(response)
+        item['description'] = self.extract_product_description(response)
+        item['care'] = self.extract_product_care(response)
+        item['image_urls'] = self.extract_image_urls(response)
+        item['skus'] = self.extract_skus(response)
+        item['price'] = self.extract_price(response)
+        item['currency'] = self.extract_currency(response)
+        return item
+
+    def extract_retailer_sku(self, response):
+        return response.css('.pdp-main::attr(data-product-id)').extract_first()
+
+    def extract_gender(self, response):
+        return response.css("meta[itemprop='name']::attr(content)").extract_first().split('-')[0]
+
+    def extract_trails(self, response):
+        return response.meta.get('trail')
+
+    def extract_product_name(self, response):
+        return response.css(".product-details .product-name h2::text").extract_first().strip()
+
+    def extract_product_description(self, response):
+        description = response.css("div[itemprop='description']  *::text").extract()
+        return description[:description.index('Waschanleitung')]
+
+    def extract_product_care(self, response):
+        description = response.css("div[itemprop='description']  *::text").extract()
+        return description[description.index('Waschanleitung'):]
+
+    def extract_price(self, response):
+        return response.css("meta[itemprop='price']::attr(content)").extract_first()
+
+    def extract_currency(self, response):
+        return response.css("meta[itemprop='priceCurrency']::attr(content)").extract_first()
+
+    def extract_color(self, response):
+        color_id = self.extract_retailer_sku(response).split('_')[1]
+        return response.css(f".color a[data-value='{color_id}']::text").extract_first().strip()
+        # color_urls = response.css('.color a::attr(href)').extract()
+        # for url in color_urls:
+        #     yield scrapy.Request(url=url, callback=self.parse)
+
+    def extract_category(self, response):
+        categories = response.css(".breadcrumb > li *::text").extract()
+        return list(filter(lambda category: category.strip(), categories))
+
+    def extract_image_urls(self, response):
+        return response.css('.productcarouselslides *::attr(data-image-replacement)').extract()
+
+    def extract_skus(self, response):
+        skus = {}
+        sku_info = {
+            'color': self.extract_color(response),
+            'currency': self.extract_currency(response),
+            'price': self.extract_price(response)
+        }
+        for size in response.css('.size li'):
+            sku = sku_info.copy()
+            sku['size'] = size.css('::attr(title)').extract_first()
+            skus[size.css('::attr(data-value)').extract_first()] = sku
+        return skus
