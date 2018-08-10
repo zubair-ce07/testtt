1131afcae46c9446d634f30af684b0e847ce9f11
sohaibaslam <sohaib.sameja@gmail.com>
2018-08-10 19:05:55 +0500

    Changed datamember name

--- Task5/items.py
+++ Task5/items.py
@@ -12,4 +12,4 @@ class Product(scrapy.Item):
     skus = scrapy.Field()
     image_urls = scrapy.Field()
     description = scrapy.Field()
-    image_urls_requests = scrapy.Field()
+    requests_queue = scrapy.Field()
