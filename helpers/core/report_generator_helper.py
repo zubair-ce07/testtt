a56a38b713558054bdf6e4a928e9b60db44a0dac
asadshahjahan <m.asad.s94@gmail.com>
2018-08-10 19:28:21 +0500

    PR :: core, config and helper packages removed

--- helpers/core/report_generator_helper.py
+++
@@ -1,7 +0,0 @@
-from helpers.core.calculator_helper import month_with_num
-
-
-def readable_date(date):
-    # assumed date is given in YYYY-MM-DD method
-    split_date = date.split('-')
-    return month_with_num(split_date[1]) + " " + split_date[2]
