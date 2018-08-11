c7873acc44b6e168f33e7c7fb90d7e66961d9f52
Muhammad Haseeb <muhammad.haseeb@arbisoft.com>
2018-08-11 11:39:03 +0500

    Crawling of prodcuts & listing implemented

--- Sheego/sheegoproducts.py
+++ Sheego/sheegoproducts.py
@@ -15,18 +15,27 @@ class SheegoproductsSpider(CrawlSpider):
                 deny=('/neu/', '/inspiration/',
                  '/damenmode-sale/', '/magazin/'),
                 restrict_css='.cj-mainnav'),
-                callback='parse_listing',),
+                callback='parse_categories',),
             )
-
-    def parse_listing(self, response):
+    
+    def parse_categories(self, response):
         categories = self.get_categories(response)
         for category in categories:
             url = response.urljoin("?filterSHKategorie={}".format(category))
-            yield scrapy.Request(url=url, callback=self.parse_products)
+            yield scrapy.Request(url=url, callback=self.parse_listing)
 
-    def parse_products(self, response):
+    def parse_listing(self, response):
+        product_urls = self.get_products_urls(response)
+        for url in product_urls:
+            yield scrapy.Request(url=url, callback=self.parse_product)
+
+    def parse_product(self, response):
         pass
     
     def get_categories(self, response):
         return response.css(
             ".form-group--checkbox input::attr(value)").extract()
+
+    def get_products_urls(self, response):
+        return response.css(
+            ".js-product__link::attr(href)").extract()
