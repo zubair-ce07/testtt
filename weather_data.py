dc67f09a2dafb446d25a18d1a02d7994a35d2fd9
asadshahjahan <m.asad.s94@gmail.com>
2018-08-11 14:01:14 +0500

    PR Review: code hard wrapped to 100 chars

--- weather_data.py
+++ weather_data.py
@@ -58,16 +58,19 @@ class WeatherData:
                 max_humidity = 0.0
             else:
                 max_humidity = float(arr[7])
-            daily_weather_entry = {'pkt': arr[0], 'max_temperature_c': max_temp, 'mean_temperature_c': arr[2],
-                                   'min_temperature_c': min_temp, 'dew_point_c': arr[4], 'mean_dev_point_c': arr[5],
-                                   'min_dew_point_c': arr[6], 'max_humidity': max_humidity, 'mean_humidity': arr[8],
-                                   'min_humidity': arr[9], 'max_sea_level_pressure_hpa': arr[10],
-                                   'mean_sea_level_pressure_hpa': arr[11], 'min_sea_level_pressure_hpa': arr[12],
+            daily_weather_entry = {'pkt': arr[0], 'max_temperature_c': max_temp,
+                                   'mean_temperature_c': arr[2], 'min_temperature_c': min_temp,
+                                   'dew_point_c': arr[4], 'mean_dev_point_c': arr[5],
+                                   'min_dew_point_c': arr[6], 'max_humidity': max_humidity,
+                                   'mean_humidity': arr[8], 'min_humidity': arr[9],
+                                   'max_sea_level_pressure_hpa': arr[10],
+                                   'mean_sea_level_pressure_hpa': arr[11],
+                                   'min_sea_level_pressure_hpa': arr[12],
                                    'max_visibility_km': arr[13], 'mean_visibility_km': arr[14],
                                    'min_visibility_km': arr[15], 'max_wind_speed_kmh': arr[16],
                                    'mean_wind_speed_kmh': arr[17], 'max_gust_speed_kmh': arr[18],
-                                   'precipitation_mm': arr[19], 'cloud_cover': arr[20], 'events': arr[21],
-                                   'wind_dir_degree': arr[22]}
+                                   'precipitation_mm': arr[19], 'cloud_cover': arr[20],
+                                   'events': arr[21], 'wind_dir_degree': arr[22]}
             WeatherData.single_month_weather_list.append(daily_weather_entry)
 
     @staticmethod
@@ -75,10 +78,12 @@ class WeatherData:
         """
         :param month:
         :param year: year from weather files
-        :param list_of_daily_weather_entry: data structure to be appended in main WeatherData.weather_yearly_data
+        :param list_of_daily_weather_entry: data structure
+        to be appended in main WeatherData.weather_yearly_data
         if month is not added in year then add as dict
         :return:
         """
         if month not in WeatherData.weather_yearly_data[year].keys():
-            WeatherData.weather_yearly_data[year] = dict(list(WeatherData.weather_yearly_data[year].items()) +
-                                                         list({month: list_of_daily_weather_entry}.items()))
+            WeatherData.weather_yearly_data[year] = dict(
+                list(WeatherData.weather_yearly_data[year].items()) +
+                list({month: list_of_daily_weather_entry}.items()))
