4e484df6ea38fa757c59a53568e5a12a305e1da9
danial.malik <danial.malik@arbisoft.com>
2018-08-15 10:39:50 +0500

    Input parsing done

--- weatherman/weatherman.py
+++ weatherman/weatherman.py
@@ -1,26 +1,60 @@
-import csv
-import sys
 import re
+import sys
+from datetime import date
 
-
-"""These constants are used to identify type of the operation
-to be performed.
-"""
-HIGHEST_LOWEST_HUMID = 1
+YEARLY_HIGHEST_LOWEST_HUMID = 1
 AVERAGE_TEMPERATURE = 2
 TWO_LINE_CHART = 3
 ONE_LINE_CHART = 4
 
 
-class Weatherman:
-    """This class is for the weatherman object which is used
-    to read data from a csv file and then print the required
-    formation depending the parameters and date provided
-    """
+def validate_input(args):
+    if len(args) is not 4:
+        return False
+    if not re.match(r'-[eac]$', args[1]):
+        return False
+    if args[1][1] is 'e':
+        if not re.match(r'\d{4}$', args[2]):
+            return False
+    else:
+        if not re.match(r'^(\d{4})/((0?[1-9])|(1[012]))$', args[2]):
+            return False
+    return True
 
-    """Attributes:
-        path_to_files = path to the all csv files for weather information
-    """
 
-    def __init__(self, path_to_files: "path to the directory containing all csv files"):
-        self.path_to_files = path_to_files
+def generate_date(year, month=1, day=1):
+    return date(int(year), int(month), int(day))
+
+
+def parse_input(args):
+    """Parse the input to decide what to do."""
+    operation = None
+    weather_date = None
+    path_to_files = None
+
+    if args[1] == '-e':
+        operation = YEARLY_HIGHEST_LOWEST_HUMID
+    elif args[1] == "-a":
+        operation = AVERAGE_TEMPERATURE
+    elif args[1] == "-c":
+        operation = TWO_LINE_CHART
+    else:
+        operation = ONE_LINE_CHART
+
+    if operation == YEARLY_HIGHEST_LOWEST_HUMID:
+        weather_date = generate_date(args[2][:4])
+    else:
+        weather_date = generate_date(args[2][:4], args[2][5:])
+
+    path_to_files = args[3]
+
+    return operation, weather_date, path_to_files
+
+
+def main():
+    print("{} {}".format(len(sys.argv), validate_input(sys.argv)))
+    operation, weather_date, path_to_files = parse_input(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
