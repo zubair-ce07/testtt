c1b92194c782f6ae41a9800bdf19b1352c48f3bc
danial.malik <danial.malik@arbisoft.com>
2018-08-15 14:39:12 +0500

    first task done.Displays a year's max and min temperatures and max humidity

---
+++ weatherman/constants.py
@@ -0,0 +1,11 @@
+"""These constants define what operation is to be performed"""
+YEARLY_HIGHEST_LOWEST_TEMP_HUMID = 1
+AVERAGE_TEMPERATURE = 2
+TWO_LINE_CHART = 3
+ONE_LINE_CHART = 4
+
+"""These constants are for file column indices"""
+MAX_TEMPERATURE_INDEX = 1
+MIN_TEMPERATURE_INDEX = 3
+MAX_HUMIDITY_INDEX = 7
+PKT_INDEX = 0
--- weatherman/weatherman.py
+++ weatherman/weatherman.py
@@ -1,11 +1,9 @@
+import csv
+import os.path
 import re
 import sys
-from datetime import date
-
-YEARLY_HIGHEST_LOWEST_HUMID = 1
-AVERAGE_TEMPERATURE = 2
-TWO_LINE_CHART = 3
-ONE_LINE_CHART = 4
+from datetime import date, datetime
+import constants
 
 
 def validate_input(args):
@@ -33,15 +31,15 @@ def parse_input(args):
     path_to_files = None
 
     if args[1] == '-e':
-        operation = YEARLY_HIGHEST_LOWEST_HUMID
+        operation = constants.YEARLY_HIGHEST_LOWEST_TEMP_HUMID
     elif args[1] == "-a":
-        operation = AVERAGE_TEMPERATURE
+        operation = constants.AVERAGE_TEMPERATURE
     elif args[1] == "-c":
-        operation = TWO_LINE_CHART
+        operation = constants.TWO_LINE_CHART
     else:
-        operation = ONE_LINE_CHART
+        operation = constants.ONE_LINE_CHART
 
-    if operation == YEARLY_HIGHEST_LOWEST_HUMID:
+    if operation == constants.YEARLY_HIGHEST_LOWEST_TEMP_HUMID:
         weather_date = generate_date(args[2][:4])
     else:
         weather_date = generate_date(args[2][:4], args[2][5:])
@@ -51,9 +49,89 @@ def parse_input(args):
     return operation, weather_date, path_to_files
 
 
+def get_min_max_row_from_file(func: "min or max function",
+                              column_index,
+                              csvfile):
+    csvfile.seek(0)
+    next(csvfile)
+    result_row = func(csv.reader(csvfile),
+                      key=lambda row: int(row[column_index]))
+    return result_row
+
+
+def get_file_path(path_to_files, city_name, year, month):
+    file_path = "{}{}_weather_{}_{}.txt".format(path_to_files,
+                                                city_name,
+                                                year,
+                                                date(year, month, 1).strftime("%b"))
+    return file_path
+
+
+def get_min_max_row(func: "min or max function", row1, row2, key_index):
+    if row1 is None:
+        return row2
+    elif row2 is None:
+        return row1
+    else:
+        return func(row1, row2, key=lambda x: int(x[key_index]))
+
+
+def result_formatter(data_row, key_index):
+    if data_row is None:
+        return "No data found for given date."
+    else:
+        result_date = datetime.strptime(data_row[constants.PKT_INDEX], "%Y-%m-%d").date()
+        return "{}C on {} {}".format(data_row[key_index],
+                                     result_date.strftime("%B"),
+                                     result_date.day)
+
+
+def display_min_max_temp_humid(weather_date, path_to_files):
+    city_name = "Murree"
+    max_temp_row = None
+    min_temp_row = None
+    max_humid_row = None
+    for month in range(1, 12):
+        file_path = get_file_path(path_to_files, city_name, weather_date.year, month)
+        if os.path.isfile(file_path):
+            with open(file_path) as csvfile:
+                current_month_max_temp = get_min_max_row_from_file(
+                    max, constants.MAX_TEMPERATURE_INDEX, csvfile)
+
+                current_month_min_temp = get_min_max_row_from_file(
+                    min, constants.MIN_TEMPERATURE_INDEX, csvfile)
+
+                current_month_max_humid = get_min_max_row_from_file(
+                    max, constants.MAX_HUMIDITY_INDEX, csvfile)
+
+                max_temp_row = get_min_max_row(max,
+                                               current_month_max_temp,
+                                               max_temp_row,
+                                               constants.MAX_TEMPERATURE_INDEX)
+                min_temp_row = get_min_max_row(min,
+                                               current_month_min_temp,
+                                               min_temp_row,
+                                               constants.MIN_TEMPERATURE_INDEX)
+                max_humid_row = get_min_max_row(max,
+                                                current_month_max_humid,
+                                                max_humid_row,
+                                                constants.MAX_HUMIDITY_INDEX)
+
+    result_str = result_formatter(max_temp_row, constants.MAX_TEMPERATURE_INDEX)
+    print("Highest: {}".format(result_str))
+
+    result_str = result_formatter(min_temp_row, constants.MIN_TEMPERATURE_INDEX)
+    print("Lowest: {}".format(result_str))
+
+    result_str = result_formatter(max_humid_row, constants.MAX_HUMIDITY_INDEX)
+    print("Humidity: {}".format(result_str))
+
+
 def main():
-    print("{} {}".format(len(sys.argv), validate_input(sys.argv)))
     operation, weather_date, path_to_files = parse_input(sys.argv)
+
+    if operation == constants.YEARLY_HIGHEST_LOWEST_TEMP_HUMID:
+        display_min_max_temp_humid(weather_date, path_to_files)
 
 
 if __name__ == "__main__":
