17fcfbab4fd9f97cac0fd14b542de99a2cbb41d8
Muhammad Ikram <ikram@IkramKhan-Desktop.arbisoft.com>
2018-08-16 17:29:09 +0500

    repition bug removed

---
+++ boohooMan/items.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class BoohoomanItem(scrapy.Item):
+    name = scrapy.Field()
+    date = scrapy.Field()
+    sku = scrapy.Field()
+    url = scrapy.Field()
+    retailer_sku = scrapy.Field()
+    retailer = scrapy.Field()
+    lang = scrapy.Field()
---
+++ boohooMan/spiders/boohoo_spider.py
@@ -0,0 +1,128 @@
+import scrapy
+import time
+import re
+import queue
+from boohooMan.items import BoohoomanItem
+
+
+class BoohooSpider(scrapy.Spider):
+    name = "boohoo"
+    start_urls = [
+        'https://www.boohooman.com/',
+    ]
+
+    def __init__(self):
+        self.items_set = set()
+
+    def parse(self, response):
+        for item in response.css('li.has-submenu'):
+
+            for subitem in item.css('li a'):
+                next_page = subitem.css('a::attr(href)').extract_first()
+                if next_page is not None:
+                    next_page = response.urljoin(next_page)
+                    yield scrapy.Request(next_page, callback=self.parse_url)
+
+    def parse_url(self, response):
+        for item in response.css('div.product-tile'):
+            product_item = BoohoomanItem()
+            product_item['sku'] = {}
+            product_item['lang'] = response.css(
+                'html::attr(lang)').extract_first()
+            product_item['name'] = item.css(
+                'div.product-name a::text').extract_first().strip("\n")
+            product_item['date'] = time.time()
+            item_url = response.urljoin(item.css(
+                'div.product-image a::attr(href)')
+                                        .extract_first())
+            product_item['url'] = response.urljoin(item.css(
+                'div.product-image a::attr(href)').extract_first())
+            yield scrapy.Request(item_url, callback=self.parse_item_color,
+                                 meta={'item': product_item})
+
+        next_page = response.css(
+            'li.pagination-item a::attr(href)').extract_first()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
+
+    def parse_item_color(self, response):
+        product_item = response.meta['item']
+        product_item['retailer_sku'] = response.css(
+            'div.product-number span::text').extract_first()
+        product_item['retailer'] = 'boohooman-us'
+        if product_item['retailer_sku'] in self.items_set:
+            return
+        else:
+            self.items_set = product_item['retailer_sku']
+        item_colors_list = response.css('div.product-variations ul.color \
+                                         li.selectable:not(.selected) \
+                                         span::attr(data-href) ').extract()
+        items_color_queue = queue.Queue()
+        count_color_contrl = item_colors_list
+        for item_url in item_colors_list:
+            items_color_queue.put(scrapy.Request(item_url + "&format=ajax",
+                                                 callback=self.parse_item_size,
+                                                 meta={'item': product_item,
+                                                       'colors_items_list': count_color_contrl
+                                                       }
+                                                 ))
+        while not items_color_queue.empty():
+            yield items_color_queue.get()
+
+    def parse_item_size(self, response):
+        print(response.url)
+        product_item = response.meta['item']
+        count_color_contrl = response.meta['colors_items_list']
+        item_size_list = response.css('div.product-variations ul.size \
+                                        li.selectable:not(.selected) \
+                                        span::attr(data-href) ').extract()
+        items_size_queue = queue.Queue()
+        for item_url in item_size_list:
+            items_size_queue.put(scrapy.Request(item_url + "&format=ajax",
+                                                callback=self.parse_item_info,
+                                                meta={'item': product_item,
+                                                      'count_color_contrl': count_color_contrl,
+                                                      'count_size_contrl': item_size_list}
+                                                ))
+        while not items_size_queue.empty():
+            yield items_size_queue.get()
+
+    def parse_item_info(self, response):
+        item_color = response.css(
+            'div.product-variations ul.color li.selected span::attr(title) ').extract_first()
+        item_size = response.css(
+            'div.product-variations ul.size li.selected span::attr(title)').extract_first()
+        item_sales_price = response.css(
+            'div.product-price span.price-sales::text ').extract_first()
+        item_std_price = response.css(
+            'div.product-price span.price-standard::text ').extract_first()
+        item_currency = response.css(
+            'div.product-price meta::attr(content) ').extract_first()
+        item_color = re.sub('.*: ', '', item_color)
+        item_size = re.sub('.*: ', '', item_size)
+        product_item = response.meta['item']
+        count_color_contrl = response.meta['count_color_contrl']
+        count_size_contrl = response.meta['count_size_contrl']
+        item = {
+            'colour': item_color,
+            'item_size': item_size,
+            'price': self.strip_chars(item_sales_price),
+            'previous_prices': self.strip_chars(item_std_price),
+            'currency': item_currency,
+        }
+        product_item['sku'].update({(item_color + "_" + item_size): item})
+        count_size_contrl.pop()
+        if len(count_size_contrl) == 0:
+            count_color_contrl.pop()
+        if len(count_color_contrl) == 0:
+            yield product_item
+
+    def strip_chars(self, str_to_strp):
+        if str_to_strp is None:
+            return ""
+        else:
+            if "\u00a3" in str_to_strp:
+                return str_to_strp.strip("\u00a3")
+            else:
+                return str_to_strp
