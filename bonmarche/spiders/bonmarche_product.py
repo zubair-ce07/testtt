5558803bae16ed1e4e750cf23d79321c178eec22
zeba ramzan <zeba.ramzan@arbisoft.com>
2018-08-16 17:50:28 +0500

    bonmarche spider

--- bonmarche/spiders/bonmarche_product.py
+++ bonmarche/spiders/bonmarche_product.py
@@ -1,13 +1,11 @@
 import json
 from scrapy import Spider, Request
-from urllib.parse import urlparse
+from w3lib.url import url_query_cleaner
 from bonmarche.items import ProductItem
 
 
 class ProductParser(Spider):
     name = "bonmarche-parse"
-    image_urls = []
-    skus = {}
 
     def parse(self, response):
         product = ProductItem(brand="bonmarche", market="UK", retailer="bonmarche-uk", currency="GBP", gender="women")
@@ -18,19 +16,20 @@ class ProductParser(Spider):
         product['name'] = self.product_name(response)
         product['description'] = self.product_description(response)
         product['care'] = self.product_care(response)
-        self.generate_skus(response)
-        product['image_urls'] = self.image_urls
-        product['skus'] = self.skus
-
+        product['image_urls'] = []
+        product['skus'] = {}
         product['spider_name'] = self.name
-        return product
+
+        color_urls = self.color_requests(response)
+        request = self.process_request(color_urls, product)
+        yield request
 
     def product_id(self, response):
         return response.css('::attr(data-masterid)').extract_first()
 
     def product_trail(self, response):
-        trail_urls = response.meta.get('trail', ['https://www.bonmarche.co.uk/'])
-        return [[url.split("/")[-1].split(".")[0], url] for url in trail_urls]
+        trail_urls = response.meta.get('trail', [])
+        return [[url.split("/")[-2].split(".")[0], url] for url in trail_urls]
 
     def product_category(self, response):
         categories = response.css('.breadcrumb-element [itemprop=name]::text').extract()
@@ -46,38 +45,59 @@ class ProductParser(Spider):
     def product_care(self, response):
         return response.css('.product-description::text, .feature-value::text').extract()[-1:]
 
-    def image_url(self, response):
+    def color_requests(self, response):
+        requests = []
+        color_urls = response.css('.color .swatchanchor.selectable:not(.selected)::attr(href)').extract()
+        color_urls.append(response.url)
+        for url in color_urls:
+            request = Request(url, callback=self.parse_colors, dont_filter=True)
+            requests.append(request)
+        return requests
+
+    def parse_colors(self, response):
+        product = response.meta["product"]
+        product["image_urls"].extend(self.parse_images(response))
+        product['skus'].update(self.generate_skus(response))
+        request = self.process_request(response.meta["colour_requests"], product)
+        if request:
+            yield request
+        else:
+            yield product
+
+    def parse_images(self, response):
         raw_images = response.css('.productthumbnail::attr(data-lgimg)').extract()
-        image_urls_ = [json.loads(url)["url"] for url in raw_images]
-        return [urlparse(url).geturl() for url in image_urls_]
+        image_urls = [json.loads(url)["url"] for url in raw_images]
+        image_urls = [url_query_cleaner(url) for url in image_urls]
+        return set(image_urls)
 
     def generate_skus(self, response):
-        color_urls = response.css('.color .swatchanchor.selectable::attr(href)').extract()
-        for url in color_urls:
-            print(url)
-            Request(url, callback=self.product_details)
-
-    def product_details(self, response):
-        self.image_urls.append(self.image_url(response))
-        size_urls = response.css('.size .swatchanchor.selectable::attr(href)').extract()
+        common_sku = self.product_sku(response)
+        skus = {}
         sizes = response.css('.size .swatchanchor.selectable::text').extract()
-        request = Request(size_urls[0], callback=self.product_sku)
-        request.meta['sizes'] = sizes
+        colour = common_sku['colour']
+        for size in sizes:
+            sku = common_sku.copy()
+            sku['size'] = size.strip()
+            skus[colour + '_' + size.strip()] = sku
+        return skus
 
     def product_sku(self, response):
-        sizes = response.meta['sizes']
         pricing = {}
-        skus_={}
-        color = response.css('.attribute .label::text').extract_first()
-        color = color.split(":")[-1].strip()
-        pricing['colour'] = color
+        colour = response.css('.attribute .label::text').extract_first()
+        colour = colour.split(":")[-1].strip()
+        pricing['colour'] = colour
         pricing['currency'] = 'GBP'
-        previous_price = response.css('.price-standard::text')
+        previous_price = response.css('.price-standard::text').extract_first()
         if previous_price:
-            pricing['previous_price'] = previous_price.remove('£').strip()
+            pricing['previous_price'] = previous_price.replace('£', '').strip()
         pricing['price'] = response.css('.price-sales::attr(content)').extract_first()
-        for size in sizes:
-            sku = pricing.copy()
-            sku['size'] = size
-            skus_[color+'_'+size] = sku
-        self.skus.update(skus_)
+        return pricing
+
+    def process_request(self, requests, product):
+        if requests:
+            request = requests.pop()
+            request.meta["product"] = product
+            request.meta["colour_requests"] = requests
+            return request
+        else:
+            return
