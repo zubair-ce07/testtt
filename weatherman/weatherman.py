4b8921ea5177e532dba93e33c7b666d9d12cd193
danial.malik <danial.malik@arbisoft.com>
2018-08-16 13:10:59 +0500

    Added documentation and requirements.txt

--- weatherman/weatherman.py
+++ weatherman/weatherman.py
@@ -1,3 +1,6 @@
+"""weaatherman.py script.
+used for manipulating files having daily weather information of a city.
+"""
 import csv
 import os.path
 import re
@@ -6,7 +9,28 @@ from datetime import date, datetime
 import constants
 
 
+def display_usage_help():
+    """Displays help message if wrong input is given."""
+    message = """weatherman.py -[acde] <date> <path/to/files>/
+        Parameters :-
+            -e = Displays minimum and highest temperatures and highest humidity for given year
+            
+            -a = Displays average minimum temperature, maximum temperature and average \
+            humidity for given month
+            
+            -c = Displays one line chart for minimum and highest temperatures of given month
+            
+            -d = Displays two line chart for minimum and highest temperatures of given month
+            """
+    print(message)
+
+
 def validate_input(args):
+    """ validate command line arguments
+
+    :param args: command line args
+    :return: true if input is correct else false
+    """
     if len(args) is not 4:
         return False
     if not re.match(r'-[eacd]$', args[1]):
@@ -21,6 +45,7 @@ def validate_input(args):
 
 
 def generate_date(year, month=1, day=1):
+    """Generate and return datetime.date object"""
     return date(int(year), int(month), int(day))
 
 
@@ -52,17 +77,22 @@ def parse_input(args):
 def get_min_max_row_from_file(func: "min or max function",
                               column_index,
                               csvfile):
+    """Find the row in the csvfile which has optimum value for the given
+    function and column index
+    """
     csvfile.seek(0)
     next(csvfile)
     try:
-        result_row = func(csv.reader(csvfile),
-                      key=lambda row: int(row[column_index]) )
+        result_row = func([row for row in csv.reader(csvfile) if row[column_index] != ""],
+                          key=lambda row: int(row[column_index]))
+
     except ValueError:
         return None
     return result_row
 
 
 def get_file_path(path_to_files, city_name, year, month):
+    """Return path to the required file given the city and date"""
     file_path = "{}{}_weather_{}_{}.txt".format(path_to_files,
                                                 city_name,
                                                 year,
@@ -71,6 +101,7 @@ def get_file_path(path_to_files, city_name, year, month):
 
 
 def get_min_max_row(func: "min or max function", row1, row2, key_index):
+    """Compare two row entries depending upon the function given and key index value"""
     if row1 is None:
         return row2
     elif row2 is None:
@@ -80,6 +111,7 @@ def get_min_max_row(func: "min or max function", row1, row2, key_index):
 
 
 def min_max_result_formatter(data_row, key_index, unit):
+    """Generate result string to be printed provided result elements"""
     if data_row is None:
         return "No data found for given date."
     else:
@@ -91,11 +123,12 @@ def min_max_result_formatter(data_row, key_index, unit):
 
 
 def display_min_max_temp_humid(weather_date, path_to_files):
+    """Find the min and max temperature and humidity rows for the given year"""
     city_name = "Murree"
     max_temp_row = None
     min_temp_row = None
     max_humid_row = None
-    for month in range(1, 12):
+    for month in range(1, 13):
         file_path = get_file_path(path_to_files, city_name, weather_date.year, month)
         if os.path.isfile(file_path):
             with open(file_path) as csvfile:
@@ -131,18 +164,17 @@ def display_min_max_temp_humid(weather_date, path_to_files):
     print("Humidity: {}".format(result_str))
 
 
-"""TASK2"""
-
-
-def count_entries(file):
+def count_entries(file, key_index):
+    """count number of rows in a file"""
     file.seek(0)
     next(file)
-    count = sum([1 for row in file])
+    count = sum([1 for row in file if row[key_index] != ""])
     return count
 
 
 def get_average_value(csvfile, key_index):
-    days_count = count_entries(csvfile)
+    """Get the average of values for a column given the key_index"""
+    days_count = count_entries(csvfile, key_index)
     csvfile.seek(0)
     next(csvfile)
     try:
@@ -156,6 +188,7 @@ def get_average_value(csvfile, key_index):
 
 
 def average_result_formatter(value, unit):
+    """Generate result string to be displayed"""
     if value is None:
         return "No data could be found on the given date."
     else:
@@ -163,6 +196,7 @@ def average_result_formatter(value, unit):
 
 
 def display_average_temperature_humidity(weather_date, path_to_files):
+    """Provided the -a option, Display the average temperatures and humidity for a month"""
     city_name = "Murree"
     average_max_temp = None
     average_min_temp = None
@@ -185,10 +219,9 @@ def display_average_temperature_humidity(weather_date, path_to_files):
         print("Error in opening the file. Such file does not exist.")
 
 
-"""TASK3"""
-
-
 def one_line_chart(index, low, low_color, high, high_color, unit):
+    """displays a one line chart for highest and lowest values provided"""
+
     result_string = "{index:02d} {low_color}{low_marks}{high_color}{high_marks}\
     {color_reset} {low}{unit} - {high}{unit}".format(index=index,
                                                      low_color=low_color,
@@ -203,18 +236,21 @@ def one_line_chart(index, low, low_color, high, high_color, unit):
 
 
 def two_line_chart(index, low, low_color, high, high_color, unit):
+    """displays a one line chart for highest and lowest values provided"""
     result_1 = "{index:02d} {low_color}{low_marks} {color_reset} {low}{unit}".format(
-        index=index, low_color=low_color, low_marks="+" * low, color_reset=constants.COLOR_RESET,
-        low=low, unit=unit)
+        index=index, low_color=low_color, low_marks="+" * low,
+        color_reset=constants.COLOR_RESET, low=low, unit=unit)
     result_2 = "{index:02d} {high_color}{high_marks}{color_reset} {high}{unit}".format(
-        index=index, high_color=high_color, high_marks="+" * high, color_reset=constants.COLOR_RESET,
-        high=high, unit=unit)
+        index=index, high_color=high_color, high_marks="+" * high,
+        color_reset=constants.COLOR_RESET, high=high, unit=unit)
     result_string = "{}\n{}".format(result_1, result_2)
 
     return result_string
 
 
-def display_temperature_chart(weather_date, path_to_files, type):
+def display_temperature_chart(weather_date, path_to_files, chart_type):
+    """Provided the -d option, Display lower and higher temperatures of each day of
+    the given month in line chart form"""
     city_name = "Murree"
     file_path = get_file_path(path_to_files, city_name, weather_date.year, weather_date.month)
     if os.path.isfile(file_path):
@@ -228,7 +264,7 @@ def display_temperature_chart(weather_date, path_to_files, type):
                     max_temperature = int(row[constants.MAX_TEMPERATURE_INDEX])
                     day = datetime.strptime(
                         row[constants.PKT_INDEX], "%Y-%m-%d").date().day
-                    if type == constants.ONE_LINE_CHART:
+                    if chart_type == constants.ONE_LINE_CHART:
                         line_chart = one_line_chart(day,
                                                     min_temperature,
                                                     constants.COLOR_BLUE,
@@ -250,13 +286,18 @@ def display_temperature_chart(weather_date, path_to_files, type):
 
 
 def main():
+    """Driver function for the script."""
+    if validate_input(sys.argv) is not True:
+        display_usage_help()
+        return
+
     operation, weather_date, path_to_files = parse_input(sys.argv)
 
     if operation == constants.YEARLY_HIGHEST_LOWEST_TEMP_HUMID:
         display_min_max_temp_humid(weather_date, path_to_files)
     elif operation == constants.AVERAGE_TEMPERATURE:
         display_average_temperature_humidity(weather_date, path_to_files)
-    elif operation == constants.ONE_LINE_CHART or operation == constants.TWO_LINE_CHART:
+    elif operation in (constants.ONE_LINE_CHART, constants.TWO_LINE_CHART):
         display_temperature_chart(weather_date, path_to_files, operation)
 
 
