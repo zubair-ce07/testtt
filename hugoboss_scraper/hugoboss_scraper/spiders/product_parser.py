8bde2074538e1366d026db3734a6bb08bf384978
WadoodAmer <wadood.islam@arbisoft.com>
2018-08-16 17:01:55 +0500

    added blank lines for saparating code blocks and did some name refactoring.

--- hugoboss_scraper/hugoboss_scraper/spiders/product_parser.py
+++ hugoboss_scraper/hugoboss_scraper/spiders/product_parser.py
@@ -6,14 +6,15 @@ from w3lib.url import url_query_cleaner as w3cleaner
 
 class Parser(Spider):
     name = "parser"
-    scraped_ids = []
+    scraped_ids = set()
 
     def parse(self, response):
         product = {}
         product_data = self.get_product_json(response)
-        if product_data["id"] in self.scraped_ids:
+
+        if self.id_exists(product_data["id"]):
             return
-        self.scraped_ids.append(product_data["id"])
+
         product["retailer_sku"] = product_data["id"]
         product["name"] = product_data["name"] or ""
         product["image_urls"] = self.get_image_urls(response)
@@ -34,46 +35,52 @@ class Parser(Spider):
         response.meta["product"] = product
         return self.item_or_request(response)
 
-    @staticmethod
-    def item_or_request(response):
-        if not response.meta["pending_color_reqs"]:
-            return response.meta["product"].copy()
-        next_color_req = response.meta["pending_color_reqs"].pop()
-        next_color_req.meta["product"] = response.meta["product"].copy()
-        next_color_req.meta["pending_color_reqs"] = response.meta["pending_color_reqs"].copy()
-        return next_color_req
-
     def parse_colour(self, response):
         response.meta["product"]["skus"].update(self.get_skus(response))
         response.meta["product"]["image_urls"] += self.get_image_urls(response)
         return self.item_or_request(response)
 
+    @staticmethod
+    def item_or_request(response):
+
+        if not response.meta["pending_color_reqs"]:
+            return response.meta["product"].copy()
+
+        next_color_req = response.meta["pending_color_reqs"].pop()
+        next_color_req.meta["product"] = response.meta["product"].copy()
+        next_color_req.meta["pending_color_reqs"] = response.meta["pending_color_reqs"].copy()
+        return next_color_req
+
     def get_skus(self, response):
         skus = {}
         product_data = self.get_product_json(response)
         sku_variant = product_data["variant"]
-        common_sku = self.get_common_sku(response, product_data)
         sizes_s = response.css('.swatch-list__size')
+
         if not sizes_s:
-            skus[f"{sku_variant}_One Size"] = common_sku
+            skus[f"{sku_variant}_One Size"] = self.get_common_sku(response, product_data)
+            skus[f"{sku_variant}_One Size"]["size"] = "One Size"
             return skus
+
         for size in sizes_s:
-            sku = common_sku.copy()
+            sku = self.get_common_sku(response, product_data)
             sku["size"] = size.css('::text').extract_first().strip()
             if size.css('[class*="unselectable"]'):
                 sku["out_of_stock"] = True
             skus[f'{sku_variant}_{sku["size"]}'] = sku
+
         return skus
 
     def get_common_sku(self, response, product_data):
         common_sku = {}
+
         discount = product_data["metric3"]
         if discount:
             common_sku["previous_prices"] = [product_data["price"] + discount]
+
         common_sku["price"] = product_data["price"]
         common_sku["currency"] = self.get_currency(response)
         common_sku["colour"] = self.get_colour(response)
-        common_sku["size"] = "One Size"
         return common_sku
 
     @staticmethod
@@ -82,9 +89,8 @@ class Parser(Spider):
 
     @staticmethod
     def get_colour(response):
-        variation_json = response.css('.product-variations::attr(data-current)').extract_first()
-        json_object = json_loads(variation_json)
-        return json_object["color"]["displayValue"]
+        variation = response.css('.product-variations::attr(data-current)').extract_first()
+        return json_loads(variation)["color"]["displayValue"]
 
     @staticmethod
     def get_categories(response):
@@ -95,13 +101,13 @@ class Parser(Spider):
 
     @staticmethod
     def get_image_urls(response):
-        image_paths = response.css('img.slider-item__image::attr(src)').extract()
-        return [w3cleaner(path) for path in image_paths]
+        image_urls = response.css('img.slider-item__image::attr(src)').extract()
+        return [w3cleaner(url) for url in image_urls]
 
     @staticmethod
     def get_description(response):
-        paragraph = response.css('div.product-container__text__description::text').extract_first(default='')
-        return [line.strip() for line in paragraph.strip().split('.\n')]
+        description = response.css('div.product-container__text__description::text').extract_first(default='')
+        return [line.strip() for line in description.strip().split('.\n')]
 
     @staticmethod
     def get_care(response):
@@ -118,7 +124,14 @@ class Parser(Spider):
     def colour_requests(self, response):
         colour_urls = response.css('.swatch-list__button--is-empty>a::attr(href)').extract()
         colour_reqs = []
+
         for url in colour_urls:
-            request = response.follow(w3cleaner(url), callback=self.parse_colour, dont_filter=True)
-            colour_reqs.append(request)
+            colour_reqs.append(response.follow(w3cleaner(url), callback=self.parse_colour, dont_filter=True))
+
         return colour_reqs
+
+    def id_exists(self, product_id):
+        if product_id in self.scraped_ids:
+            return True
+        self.scraped_ids.add(product_id)
+        return False
