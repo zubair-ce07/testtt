c2da26d21d6827e4082259717d1ec44a7a291fe1
Muhammad Haseeb <muhammad.haseeb@arbisoft.com>
2018-08-17 17:25:09 +0500

    Product crawling implemented

--- damart/Damall.py
+++ damart/Damall.py
@@ -17,4 +17,13 @@ class DamallSpider(CrawlSpider):
             )
 
     def parse_listing(self, response):
+        product_urls = get_products_urls(response)
+        for url in product_urls:
+            yield scrapy.Request(url=url callback=self.parse_product)
+
+    def parse_product(self, response):
         pass
+
+    def get_products_urls(self, response):
+        urls = response.css(".ss_menu_bloc a::attr(href)").extract()
+        return [response.urljoin(url) for ulr in urls]
