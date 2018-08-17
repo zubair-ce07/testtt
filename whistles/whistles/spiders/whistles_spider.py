d00578e8315f91fcbd72290d8a0e5d3936452204
arslanhashmi <m.arslanijaz9@gmail.com>
2018-08-17 17:48:57 +0500

    Changed xpath into css selectors.

--- whistles/whistles/spiders/whistles_spider.py
+++ whistles/whistles/spiders/whistles_spider.py
@@ -13,11 +13,11 @@ class WhistlesSpider(CrawlSpider):
     start_urls = ['http://www.whistles.com/']
 
     def parse(self, response):
-        for href in response.xpath("//li[contains(@class, 'meganav-link')]/a/@href"):
+        for href in response.css("li[class*='meganav-link'] a::attr(href)"):
             yield response.follow(href, callback=self.parse)
 
-        next_page_href = response.xpath("//a[contains(@class,'name-link')]/@href")
-        next_page_href.extend(response.xpath("//a[contains(@class,'page-next')]/@href"))
+        next_page_href = response.css("a[class*='name-link']::attr(href)")
+        next_page_href.extend(response.css("a[class*='page-next']::attr(href)"))
         for href in next_page_href:
             yield response.follow(href, callback=self.parse_item)
 
@@ -35,55 +35,57 @@ class WhistlesSpider(CrawlSpider):
 
     @staticmethod
     def get_name(response):
-        return response.xpath("//h1[contains(@class,'product-name')]/text()").extract_first()
+        return response.css("h1[class*='product-name']::text").extract_first()
 
     @staticmethod
     def get_brand(response):
-        return response.xpath("//meta[contains(@property, 'product:brand')]/@content").extract_first()
+        return response.css("meta[property*='product:brand']::attr(content)").extract_first()
 
     @staticmethod
     def get_image_urls(response):
-        return response.xpath("//img[contains(@class, 'productthumbnail')]/@src").extract()
+        return response.css("img[class*='productthumbnail']::attr(src)").extract()
 
     @staticmethod
     def get_product_sku(response):
-        return response.xpath("//p[contains(., 'Product Key:')]/text()").extract_first()
+        return response.css("p:contains('Product Key:')::text").extract_first()
 
     @staticmethod
     def get_care(response):
         return [
-            "material: {}".format(response.xpath("//p[contains(., 'Composition:')]/text()").extract_first()),
-            "wash-care: {}".format(response.xpath("//p[contains(., 'Wash care:')]/text()").extract_first())
+            "material: {}".format(response.css("p:contains('Composition:')::text").extract_first()),
+            "wash-care: {}".format(response.css("p:contains('Wash care:')::text").extract_first())
         ]
 
     @staticmethod
     def get_color(response):
-        return response.xpath("//p[contains(., 'Colour:')]/text()").extract_first()
+        return response.css('p:contains("Colour:")::text').extract_first()
 
     @staticmethod
     def get_price(response):
-        sale_price = response.xpath("//span[contains(@title, 'Sale Price')]/text()").extract_first(),
-        regular_price = response.xpath("//span[contains(@title, 'Regular Price')]/text()").extract_first()
+        sale_price = response.css("span[title*='Sale Price']::text").extract_first(),
+        regular_price = response.css("span[title*='Regular Price']::text").extract_first()
         if isinstance(sale_price, str):
             return {
-                'price': sale_price,
-                'previous_price': regular_price
+                'unit': sale_price[0],
+                'price': sale_price[1:],
+                'previous_price': regular_price[1:]
             }
         return {
-            'price': regular_price
+            'unit': regular_price[0],
+            'price': regular_price[1:]
         }
 
     @staticmethod
     def get_description(response):
-        description = response.xpath("(//div[contains(@class,'product-tabs')]/ul/li)")[0]
-        return list(filter((lambda x: len(x)), map(str.strip, description.xpath("./div/div/p/text()").extract())))
+        description = response.css("div[class*='product-tabs'] ul:first-child")
+        return list(filter((lambda x: len(x)), map(str.strip, description.css("div div p::text").extract())))
 
     @classmethod
     def get_skus(cls, response):
         color = cls.get_color(response)
         price = cls.get_price(response)
         skus = list()
-        sizes_info = response.xpath("//li[contains(@class, 'emptyswatch')]")
+        sizes_info = response.css("li[class*='emptyswatch']")
         if not sizes_info:  # In case on one size only, eg: For `bags`
             sku = {
                 'color': color,
@@ -94,16 +96,15 @@ class WhistlesSpider(CrawlSpider):
             skus.append(sku)
             return skus
         for size_info in sizes_info:
-            size = size_info.xpath("./a/span/text()").extract_first()
+            size = size_info.css("a span::text").extract_first()
             # Status eg: Out Of Stock => out_of_stock
-            status = size_info.xpath("./a/@title").extract_first().replace(' ', '_').lower()
+            status = size_info.css("a::attr(title)").extract_first().replace(' ', '_').lower()
             sku = {
                 'color': color,
                 'size': size,
                 status: True,
-                'sku_id': "{}_{}".format(color, size)
+                'sku_id': "{}_{}".format(color.replace(' ', '_'), size)
             }
             sku.update(price)
             skus.append(sku)
         return skus
-
