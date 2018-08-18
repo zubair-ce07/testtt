d2b41ab36ec4b23e24be7c7d30b858e6a4704fbc
mumerfarooq000 <mfarooq@arbisoft.com>
2018-08-18 12:41:44 +0500

    discussion purpose

--- levi_pt/levi_pt/spiders/levi_parse_spider.py
+++ levi_pt/levi_pt/spiders/levi_parse_spider.py
@@ -14,22 +14,29 @@ class LeviptParseSpider(Spider):
         ('mulher', 'women')
     ]
 
+    visiting_ids = set()
+
     def parse(self, response):
+        retailer_sku = self.item_retailer_sku(response)
+        
+        if retailer_sku in self.visiting_ids:
+            return
+        self.visiting_ids.add(retailer_sku)
+
         item = Product()
-        item['retailer_sku'] = self.item_retailer_sku(response)
+        item['retailer_sku'] = retailer_sku
         item['gender'] = self.item_gender(response)
-        item['trail'] = response.meta['trail']
+        item['trail'] = response.meta.get('trail')
         item['category'] = self.item_category(response)
         item['brand'] = self.item_brand(response)
         item['url'] = response.url
         item['name'] = self.item_name(response)
         item['description'] = self.item_description(response)
         item['care'] = self.item_care(response)
-        item['image_urls'] = self.item_image_urls(response)
 
         response.meta['item'] = item
-        yield self.request_colors(response)
-        yield self.fetch_request_or_item(response)
+        response.meta['requests'] = self.fetch_colors_requests(response)
+        return self.fetch_request_or_item(response)
 
     def parse_item_skus(self, response):
         item = response.meta['item']
@@ -41,10 +48,16 @@ class LeviptParseSpider(Spider):
         return self.fetch_request_or_item(response)
 
     def parse_item_stocks(self, response):
+        item = response.meta['item']
+
+        image_urls = item.get('image_urls', [])
+        image_urls += self.item_image_urls(response)
+        item['image_urls'] = image_urls
+
         item_stock_request = Request(url=f'{response.url}/stocks',
                                      callback=self.parse_item_skus)
         item_stock_request.meta['common'] = self.item_skus_common(response)
-        item_stock_request.meta['item'] = response.meta['item']
+        item_stock_request.meta['item'] = item
         item_stock_request.meta['requests'] = response.meta['requests']
 
         return item_stock_request
@@ -73,7 +86,7 @@ class LeviptParseSpider(Spider):
             if stock.get('stock', 0) == 0:
                 sku['out_of_stock'] = True
 
-            if stock.get('leg_size') != '0':
+            if stock.get('leg_size') == '0':
                 sku['size'] = f"{stock.get('size')}/{stock.get('leg_size')}"
             else:
                 sku['size'] = stock.get('size')
@@ -84,6 +97,7 @@ class LeviptParseSpider(Spider):
     @staticmethod
     def fetch_request_or_item(response):
         requests = response.meta['requests']
+
         if not requests:
             return response.meta['item']
 
@@ -92,9 +106,10 @@ class LeviptParseSpider(Spider):
 
         return request
 
-    def request_colors(self, response):
+    def fetch_colors_requests(self, response):
         item = response.meta['item']
         requests = []
+
         for url in response.css('.color-list a::attr(href)').extract():
             request = response.follow(response.urljoin(url),
                                       callback=self.parse_item_stocks,
@@ -103,12 +118,12 @@ class LeviptParseSpider(Spider):
             request.meta['requests'] = requests
             request.meta['item'] = item
 
-        response.meta['requests'] = requests
+        return requests
 
     @staticmethod
     def item_retailer_sku(response):
         css = '.product-data .btn-add-to-favorites::attr(data-model-id)'
-        return response.css(css).extract_first()
+        return response.css(css).extract_first().split('-')[0]
 
     def item_gender(self, response):
         css = '.size-chart-lightbox .title::text'
