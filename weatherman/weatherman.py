573d58264978a6f5baaf86eccf77f968e2d63e5a
Muhammad Umar <umar@HassanKhan-Desktop.arbisoft.com>
2018-08-20 16:17:49 +0500

    formatting issues resolved

--- weatherman/weatherman.py
+++ weatherman/weatherman.py
@@ -127,10 +127,8 @@ def get_output_for_a_option(file_content, date):
     if monthly_temp_humidity_avg is None:
         print(Constants.IO_EXCEPTION)
         exit(1)
-    print("Highest Average: {:02d}C"
-          .format(monthly_temp_humidity_avg["max_temp_avg"]))
-    print("Lowest Average: {:02d}C"
-          .format(monthly_temp_humidity_avg["min_temp_avg"]))
+    print("Highest Average: {:02d}C".format(monthly_temp_humidity_avg["max_temp_avg"]))
+    print("Lowest Average: {:02d}C".format(monthly_temp_humidity_avg["min_temp_avg"]))
     print("Average Mean Humidity: {:02d}%\n"
           .format(monthly_temp_humidity_avg["mean_humidity_avg"]))
 
