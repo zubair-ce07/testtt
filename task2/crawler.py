03156e53370c58e9716567079568984d301104b7
zaidbamber <zaid@ZainDar-Desktop-118.arbisoft.com>
2018-08-20 14:42:05 +0500

    changes made

--- task2/crawler.py
+++ task2/crawler.py
@@ -7,24 +7,22 @@ from bs4 import BeautifulSoup
 
 class CrawlQuotes:
     """The class hold quotes and authors """
-    def __init__(self):
-        self.quote = []
-        self.author = []
-
-    def crawl_webpage(self):
+    @staticmethod
+    def crawl_webpage():
         """The method get page data and populate useful data"""
         url = 'http://quotes.toscrape.com/'
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'lxml')
-        required_data = soup.find_all('span')
-
-        for index in range(0, 20, 2):
-            self.quote.append(required_data[index].text)
-            self.author.append(required_data[index+1].text.split('\n')[0])
-        print(self.quote)
-        print()
-        print(self.author)
+        page_data = soup.find_all(
+            lambda tag: tag.name == 'div' and tag['class'] == ['quote'])
+        quote_and_author = {'quote': [], 'author': []}
+        for data, data in enumerate(page_data):
+            quote_and_author['quote'].append(data.text.split('\n')[1])
+            quote_and_author['author'].append(data.text.split('\n')[2])
+        for index in range(len(quote_and_author['quote'])):
+            print('Quote: ', quote_and_author['quote'][index])
+            print('Author: ', quote_and_author['author'][index])
+            print()
 
 if __name__ == "__main__":
-    crawling = CrawlQuotes()
-    crawling.crawl_webpage()
+    CrawlQuotes.crawl_webpage()
