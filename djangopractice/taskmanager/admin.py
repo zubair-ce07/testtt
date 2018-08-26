2bd0069472ccd1cfe3178b299b12f1077b5e6eac
Zainab Amir <40633976+zainab-amir@users.noreply.github.com>
2018-08-26 22:18:53 +0500

    zainabamir-admin_customization (#1005)
    * Admin customizations
    
    * Add action button

--- djangopractice/taskmanager/admin.py
+++ djangopractice/taskmanager/admin.py
@@ -1,22 +1,42 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.urls import path, reverse
+from django.utils.html import mark_safe, format_html
 
-from taskmanager.models import CustomUser, Task
-from taskmanager.forms import CustomUserChangeForm
+from taskmanager import views, models, forms
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status', 'mark_as_completed']
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path('<int:pk>/status/', views.change_status, name='status'),
+        ]
+        return custom_urls + urls
+
+    def mark_as_completed(self, obj):
+        return format_html(
+            '<a class="button" href="{}">Change Status</a>',
+            reverse('admin:status', args=[obj.pk]),
+        )
+    mark_as_completed.short_description = "Status"
 
 
 class CustomUserAdmin(UserAdmin):
-    form = CustomUserChangeForm
-    fieldsets = (
-        ('User details', {
-            'fields': ('username', 'first_name', 'last_name', 'email')
-        }),
-        ('Extra', {
-            'fields': ('bio', 'image', 'birth_date')
-        }),
-    )
-    list_display = ['username', 'id', 'full_name', 'total_tasks']
+    list_display = ['username', 'full_name', 'total_tasks']
+    form = forms.CustomUserChangeForm
+    add_form = forms.CustomUserCreationForm
+    fieldsets = (('User details', {'fields': ('username', 'first_name', 'last_name', 'email')}),
+                 ('Extra', {'fields': ('bio', 'image', 'birth_date')}),)
+    add_fieldsets = (('None', {'fields': ('username', 'email', 'password1', 'password2')}),)
 
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super().get_search_results(request, queryset, search_term)
+        queryset |= self.model.objects.filter(task__title__icontains=search_term)
+        use_distinct = True
+        return queryset, use_distinct
 
-admin.site.register(Task)
-admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(models.Task, TaskAdmin)
+admin.site.register(models.CustomUser, CustomUserAdmin)
--- djangopractice/taskmanager/views.py
+++ djangopractice/taskmanager/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
@@ -17,7 +18,7 @@ def change_status(request, pk):
     else:
         task.status = 0
     task.save()
-    return redirect(reverse('taskmanager:details', args=(pk, )))
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
 class SignUp(generic.CreateView):
