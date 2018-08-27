20adba6f778ef6e3aaf13e13be572afac7c77177
BilalZaib <zaibbilal0@gmail.com>
2018-08-27 15:55:49 +0500

    short forms removed in variable names

--- woolrich/woolrich_us.py
+++ woolrich/woolrich_us.py
@@ -45,23 +45,23 @@ class WoolrichSpider(CrawlSpider):
 
     def next_request_or_item(self, item):
         if item['meta']['queued_requests']:
-            req = item['meta']['queued_requests'].pop()
-            req.meta['item'] = item
-            return req
+            request = item['meta']['queued_requests'].pop()
+            request.meta['item'] = item
+            return request
 
         del item['meta']
         return item
 
     def color_requests(self, response, product_id):
-        attributes_map = self.get_product_attrs(response)
+        attributes_map = self.get_product_attributes(response)
 
         color_map = attributes_map.pop('color')
-        attr_value = color_map.get('value')
+        attribute_value = color_map.get('value')
 
         formdata = {'action': 'add', 'product_id': product_id}
-        color_reqs = []
-        for color, color_id in color_map['varients']:
-            formdata[attr_value] = color_id
+        color_requests = []
+        for color, color_id in color_map['variants']:
+            formdata[attribute_value] = color_id
             
             raw_sku = {'color': color, 'currency': attributes_map.get('currency')}
             meta = {'raw_sku': raw_sku, 'attributes_map': attributes_map}
@@ -69,18 +69,18 @@ class WoolrichSpider(CrawlSpider):
 
             request = FormRequest(url=url, meta=meta, formdata=formdata, 
                             callback=self.parse_color)
-            color_reqs += [request]
+            color_requests += [request]
         
-        return color_reqs
+        return color_requests
 
     def parse_color(self, response):
         item = response.meta.get('item')
 
-        size_reqs = self.size_requests(response)
-        if not size_reqs:
+        size_requests = self.size_requests(response)
+        if not size_requests:
             item['skus'].append(self.make_sku(response))
             
-        item['meta']['queued_requests'] += size_reqs
+        item['meta']['queued_requests'] += size_requests
         return self.next_request_or_item(item)
     
     def size_requests(self, response):
@@ -91,23 +91,23 @@ class WoolrichSpider(CrawlSpider):
         in_stock_attributes = raw_item['in_stock_attributes']
 
         size_map = attributes_map.get('size', {})
-        attr_value = size_map.get('value')
-        size_reqs = []
-        for size, size_id in size_map.get('varients', []):
+        attribute_value = size_map.get('value')
+        size_requests = []
+        for size, size_id in size_map.get('variants', []):
             if int(size_id) not in in_stock_attributes:
                 continue
 
             formdata = dict(parse_qsl(response.request.body.decode()))
-            formdata[attr_value] = size_id
+            formdata[attribute_value] = size_id
 
             raw_sku = sku_common.copy()
             raw_sku['size'] = size
             meta = {'raw_sku': raw_sku, 'attributes_map': attributes_map}
             request = FormRequest(url=response.url, meta=meta,
                               formdata=formdata, callback=self.parse_size)
-            size_reqs += [request]
+            size_requests += [request]
         
-        return size_reqs
+        return size_requests
 
     def parse_size(self, response):
         item = response.meta.get('item')
@@ -127,23 +127,23 @@ class WoolrichSpider(CrawlSpider):
         in_stock_attributes = raw_item['in_stock_attributes']
 
         fit_map = attributes_map.get('fit', {})
-        attr_value = fit_map.get('value')
-        fitting_reqs = []
-        for fit, fit_id in fit_map.get('varients', []):
+        attribute_value = fit_map.get('value')
+        fitting_requests = []
+        for fit, fit_id in fit_map.get('variants', []):
             if int(fit_id) not in in_stock_attributes:
                 continue
 
             formdata = dict(parse_qsl(response.request.body.decode()))
-            formdata[attr_value] = fit_id
+            formdata[attribute_value] = fit_id
 
             raw_sku = sku_common.copy()
             raw_sku['size'] = f'{raw_sku["size"]}/{fit}'
             meta = {'raw_sku': raw_sku, 'attributes_map': attributes_map}
             request = FormRequest(url=response.url, meta=meta,
                               formdata=formdata, callback=self.parse_fitting)
-            fitting_reqs += [request]
+            fitting_requests += [request]
         
-        return fitting_reqs
+        return fitting_requests
     
     def parse_fitting(self, response):
         item = response.meta.get('item')
@@ -154,34 +154,33 @@ class WoolrichSpider(CrawlSpider):
     def make_sku(self, response):
         sku = response.meta.get('raw_sku')
         raw_item = json.loads(response.text)['data']
-        prev_price, price = self.sku_pricing(raw_item)
+        previous_price, price = self.sku_pricing(raw_item)
 
         sku['id'] = raw_item['sku']
         sku['price'] = price
 
-        if prev_price:
-            sku['previous_price'] = prev_price
+        if previous_price:
+            sku['previous_price'] = previous_price
         
         sku['size'] = sku.get('size', 'One Size')        
         return sku
 
     def sku_pricing(self, raw_sku):
         price = self.to_cent(raw_sku['price']['without_tax']['value'])
-        prev_price = raw_sku['price'].get('non_sale_price_without_tax', {}).get('value')
-        if prev_price:
-            prev_price = self.to_cent(prev_price)
+        previous_price = raw_sku['price'].get('non_sale_price_without_tax', {}).get('value')
+        if previous_price:
+            previous_price = self.to_cent(previous_price)
 
-        return prev_price, price
-
+        return previous_price, price
 
     def get_retailer_sku(self, response):
         return response.css('[name="product_id"]::attr(value)').extract_first()
 
     def get_gender(self, response):
-        prod_name = self.get_name(response)
+        product_name = self.get_name(response)
 
         for gender in self.genders:
-            if gender in prod_name:
+            if gender in product_name:
                 return gender
 
         return 'unisex-adults'   
@@ -211,22 +210,22 @@ class WoolrichSpider(CrawlSpider):
         
         return [u['data'].replace('{:size}', '1200x1318') for u in image_urls if not "thumbnail" in u['alt']]
 
-    def get_product_attrs(self, response):
-        product_attrs_sel = response.css('.productView-options [data-product-attribute]')
+    def get_product_attributes(self, response):
+        product_attrs_selectors = response.css('.productView-options [data-product-attribute]')
         attr_name_r = re.compile('color|size|fit', flags=re.I)
 
         attributes_map = {}
-        for attr_sel in product_attrs_sel:
-            attr_name = attr_sel.css('.form-label span::text').re_first(attr_name_r).lower()
+        for selector in product_attrs_selectors:
+            attr_name = selector.css('.form-label span::text').re_first(attr_name_r).lower()
 
             attributes_map[attr_name] = {}
-            attributes_map[attr_name]['value'] = attr_sel.css('.form-radio::attr(name)').extract_first()
+            attributes_map[attr_name]['value'] = selector.css('.form-radio::attr(name)').extract_first()
 
             attr_titles_css = '.form-option-variant::attr(title), .form-option-variant::text'
-            attr_titles = attr_sel.css(attr_titles_css).extract()
-            attr_titles_ids = attr_sel.css('.form-radio::attr(value)').extract()
+            attr_titles = selector.css(attr_titles_css).extract()
+            attr_titles_ids = selector.css('.form-radio::attr(value)').extract()
 
-            attributes_map[attr_name]['varients'] = list(zip(attr_titles, attr_titles_ids))
+            attributes_map[attr_name]['variants'] = list(zip(attr_titles, attr_titles_ids))
 
         currency_css = '[itemprop="priceCurrency"]::attr(content)'
         attributes_map['currency'] = response.css(currency_css).extract_first()
