d2787d8975802737ff2716cf4e7acd32ec762b96
Muhammad Haseeb <muhammad.haseeb@arbisoft.com>
2018-08-28 14:15:18 +0500

    Pagination + scrapping for products implemented

--- damart/Damall.py
+++ damart/Damall.py
@@ -11,22 +11,26 @@ from damall.items import Product
 class DamallSpider(CrawlSpider):
     name = 'Damall'
     allowed_domains = ['damart.co.uk']
-    start_urls = ["https://www.damart.co.uk/F-10020-shirts-m"
-                  + "/P-349197-checked-shirt?from=subhomee"]
-    # start_urls = ['http://damart.co.uk/']
-    # rules = (
-    #         Rule(LinkExtractor(
-    #             allow=('\d'),
-    #             restrict_css='.ss_menu_bloc'),
-    #             callback='parse_listing',),
-    #         )
+    start_urls = ['http://damart.co.uk/']
+    rules = (
+            Rule(LinkExtractor(
+                allow=('\d'),
+                restrict_css='.ss_menu_bloc'),
+                callback='parse_listing',),
+            )
 
-    # def parse_listing(self, response):
-    #     product_urls = self.get_products_urls(response)
-    #     for url in product_urls:
-    #         yield Request(url=url, callback=self.parse_product)
+    def parse_listing(self, response):
+        product_urls = self.get_products_urls(response)
+        next_page = self.get_next_page(response)
+        for url in product_urls:
+            yield Request(url=url, callback=self.parse_product)
+        sublisting_urls = self.get_sublisting_urls(response)
+        for url in sublisting_urls:
+            yield Request(url=url, callback=self.parse_listing)
+        if next_page:
+            yield Request(url=next_page, callback=self.parse_listing)
 
-    def parse(self, response):
+    def parse_product(self, response):
         product = Product()
         product["Id"] = self.get_product_id(response)
         product["name"] = self.get_product_name(response)
@@ -35,7 +39,7 @@ class DamallSpider(CrawlSpider):
         product["description"] = self.get_product_description(response)
         product["care"] = self.get_product_care(response)
         product["skus"] = {}
-        product["brand"] = "DaMart"
+        product["brand"] = "DaMall"
         product["price"] = self.get_product_price(response)
         colors_names, colors_urls = self.get_product_colors(response)
         color_url = colors_urls[len(colors_urls)-1]
@@ -49,9 +53,17 @@ class DamallSpider(CrawlSpider):
                           "product": product,
                           "c_names": colors_names,
                           "c_urls": colors_urls})
+    
+    def get_next_page(self, response):
+        url = response.css(".next::attr(href)").extract_first()
+        return response.urljoin(url)
+
+    def get_sublisting_urls(self, response):
+        urls = response.css(".ss_menu_bloc a::attr(href)").extract()
+        return [response.urljoin(url) for url in urls]
 
     def get_products_urls(self, response):
-        urls = response.css(".ss_menu_bloc a::attr(href)").extract()
+        urls = response.css(".k-catalog a::attr(href)").extract()
         return [response.urljoin(url) for url in urls]
 
     def get_product_imgs(self, response):
@@ -86,15 +98,20 @@ class DamallSpider(CrawlSpider):
         raw = json.loads(response.body.decode('utf-8'))
         raw = raw['inits'][2]['initDDdSlickComponent'][0]["ddData"]
         sizes = []
+        is_out_of_stock = []
         for raw_txt in raw:
             sizes.append(raw_txt["text"])
-        return sizes
+            if raw_txt["description"] == "Out of stock":
+                is_out_of_stock.append(False)
+            else:
+                is_out_of_stock.append(True)
+        return sizes, is_out_of_stock
 
     def get_product_variants(self, response):
         colors_names = response.meta["c_names"]
         colors_urls = response.meta["c_urls"]
         product = response.meta["product"]
-        skus = self.get_product_skus(response, colors_names, product)
+        skus = self.get_product_skus(response)
         colors_names.pop()
         product["skus"].update(skus)
         if not colors_urls:
@@ -112,17 +129,20 @@ class DamallSpider(CrawlSpider):
                             "c_names": colors_names,
                             "c_urls": colors_urls})
 
-    def get_product_skus(self, response, colors_names, product):
-        sizes = self.get_product_sizes(response)
+    def get_product_skus(self, response):
+        colors_names = response.meta["c_names"]
+        product = response.meta["product"]
+        sizes, is_out_of_stock = self.get_product_sizes(response)
         skus = {}
-        for size in sizes:
+        for itr, size in enumerate(sizes):
             skus.update({
                 "{}_{}_{}".format(
                     product["Id"], size, colors_names[len(colors_names)-1]): {
                     "color": colors_names[len(colors_names)-1],
                     "currency": "EUR",
                     "price": product["price"],
-                    "size": size
+                    "size": size,
+                    "is_out_of_stock": is_out_of_stock[itr]
                 }
             })
         return skus
