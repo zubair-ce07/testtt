dd657d4dc79de777941461303c382ede0c376929
arslanather <arslan.ather@arbisoft.com>
2018-08-28 18:11:51 +0500

    improved consistency

--- woolrich/woolrich/spiders/woolrich_crawl.py
+++ woolrich/woolrich/spiders/woolrich_crawl.py
@@ -5,15 +5,15 @@ from woolrich.spiders.woolrich_product_parser import WoolrichProductParserSpider
 
 
 class WoolrichCrawlSpider(CrawlSpider):
-    name = 'woolrich-crawl'
-    allowed_domains = ['www.woolrich.com']
-    start_urls = ['http://www.woolrich.com/']
+    name = "woolrich-crawl"
+    allowed_domains = ["www.woolrich.com"]
+    start_urls = ["http://www.woolrich.com/"]
     listing_css = ["#primary ul:not([class])", ".pagination-item--next"]
     product_css = ".card-title"
     product_parser = WoolrichProductParserSpider()
 
     rules = (
-        Rule(LinkExtractor(restrict_css=listing_css), callback='parse'),
+        Rule(LinkExtractor(restrict_css=listing_css), callback="parse"),
         Rule(LinkExtractor(restrict_css=product_css), callback=product_parser.parse)
     )
 
