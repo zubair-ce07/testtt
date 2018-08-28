951d0007364f6e48e156ed7cd6ec97cfa5913772
arslanather <arslan.ather@arbisoft.com>
2018-08-28 18:10:10 +0500

    1.renamed description > product_description 2.removed sku_id 3.improved methods order 4.improved raw_skus method

--- woolrich/woolrich/spiders/woolrich_product_parser.py
+++ woolrich/woolrich/spiders/woolrich_product_parser.py
@@ -1,3 +1,4 @@
+from itertools import product
 from json import loads
 import time
 
@@ -37,7 +38,7 @@ class WoolrichProductParserSpider(Spider):
         product["market"] = "US"
         product["url_original"] = response.url
         product["name"] = self.product_name(response)
-        product["description"] = self.description(response)
+        product["description"] = self.product_description(response)
         product["care"] = self.care(response)
         product["image_urls"] = self.images(response)
         product["skus"] = {}
@@ -60,7 +61,6 @@ class WoolrichProductParserSpider(Spider):
     def sku(self, response):
         raw_sku = loads(response.text)["data"]
         sku_price = raw_sku["price"]
-        sku_id = raw_sku["sku"]
         sku = {
             "colour": response.meta["colour"],
             "size": response.meta['size'],
@@ -74,7 +74,7 @@ class WoolrichProductParserSpider(Spider):
         if not raw_sku["instock"]:
             sku["out_of_stock"] = True
 
-        return {sku_id: sku}
+        return {raw_sku["sku"]: sku}
 
     @staticmethod
     def sku_image(response):
@@ -82,52 +82,10 @@ class WoolrichProductParserSpider(Spider):
 
         if image:
             return image["data"].replace("{:size}", "1200x1318")
-        return None
-
-    def sku_requests(self, response):
-        raw_skus = self.raw_skus(response)
-        product_id = response.css("[name='product_id']::attr('value')").extract_first()
-        sku_url = urljoin(self.sku_base_url, product_id)
-        requests = []
-
-        for raw_sku in raw_skus:
-            request = FormRequest(url=sku_url, formdata=raw_sku["form-data"],
-                                  callback=self.parse_sku)
-            request.meta["colour"] = raw_sku["colour"]
-            request.meta["size"] = raw_sku.get("size", "One size")
-            requests.append(request)
-
-        return requests
 
     @staticmethod
-    def prepare_request(requests, product):
-        if requests:
-            request = requests.pop()
-            request.meta["product"] = product
-            request.meta["requests"] = requests
-            yield request
-        else:
-            yield product
-
-    @staticmethod
-    def images(response):
-        return response.css(".zoom ::attr(src)").extract()
-
-    @staticmethod
-    def price(response):
-        return int(float(response.css("[itemprop='price']::attr(content)").extract_first()) * 100)
-
-    @staticmethod
-    def care(response):
-        return response.css("#features-content >::text").extract()
-
-    @staticmethod
-    def description(response):
-        return response.css("#details-content::text").extract_first()
-
-    @staticmethod
-    def product_name(response):
-        return response.css(".productView-title::text").extract_first()
+    def product_id(response):
+        return response.css("strong:contains(Style)::text").extract_first().split("#: ")[1]
 
     def gender(self, response):
         gender_soup = " ".join(self.category(response)).lower()
@@ -143,35 +101,84 @@ class WoolrichProductParserSpider(Spider):
         return response.css(".breadcrumb-label::text").extract()
 
     @staticmethod
-    def product_id(response):
-        return response.css("strong:contains(Style)::text").extract_first().split("#: ")[1]
+    def product_name(response):
+        return response.css(".productView-title::text").extract_first()
+
+    @staticmethod
+    def product_description(response):
+        return response.css("#details-content::text").extract_first()
+
+    @staticmethod
+    def care(response):
+        return response.css("#features-content >::text").extract()
+
+    @staticmethod
+    def images(response):
+        return response.css(".zoom ::attr(src)").extract()
+
+    @staticmethod
+    def price(response):
+        return int(float(response.css("[itemprop='price']::attr(content)").extract_first()) * 100)
+
+    def sku_requests(self, response):
+        raw_skus = self.raw_skus(response)
+        product_id = response.css("[name='product_id']::attr('value')").extract_first()
+        sku_url = urljoin(self.sku_base_url, product_id)
+        requests = []
+
+        for raw_sku in raw_skus:
+            meta = {
+                "colour": raw_sku["colour"],
+                "size": raw_sku.get("size", "One size")
+            }
+            request = FormRequest(url=sku_url, formdata=raw_sku["form-data"],
+                                  callback=self.parse_sku, meta=meta)
+            requests.append(request)
+
+        return requests
+
+    @staticmethod
+    def prepare_request(requests, product):
+        if requests:
+            request = requests.pop()
+            request.meta["product"] = product
+            request.meta["requests"] = requests
+            yield request
+        else:
+            yield product
 
     @staticmethod
     def raw_skus(response):
-        skus_colour_s = response.css("[data-cart-item-add] [data-product-attribute='swatch']")
-        skus_size_s = response.css("[data-cart-item-add] [data-product-attribute='set-rectangle']")
+        raw_skus_css = "[data-cart-item-add] [data-product-attribute='{}']"
+        colour_title_css = "[for*='{}'] ::attr(title)"
+        size_title_css = "[for*='{}'] >::text"
+        attr_value_css = "::attr(value)"
+        attr_name_css = "::attr(name)"
+        form_input_css = "input[name]"
+
+        raw_colour_s = response.css(raw_skus_css.format("swatch"))
+        raw_size_s = response.css(raw_skus_css.format("set-rectangle"))
         raw_skus = []
 
-        for colour_s in skus_colour_s.css("input[name]"):
-            value = colour_s.css("::attr(value)").extract_first()
+        for colour_s in raw_colour_s.css(form_input_css):
+            value = colour_s.css(attr_value_css).extract_first()
             raw_skus.append({
-                "form-data": {colour_s.css("::attr(name)").extract_first(): value},
-                "colour": skus_colour_s.css(f"[for*='{value}'] ::attr(title)").extract_first()
+                "form-data": {colour_s.css(attr_name_css).extract_first(): value},
+                "colour": raw_colour_s.css(colour_title_css.format(value)).extract_first()
             })
 
-        for size_op_s in skus_size_s:
+        for size_s in raw_size_s:
             skus = raw_skus.copy()
             raw_skus = []
 
-            for raw_sku in skus:
-                for size_s in size_op_s.css("input[name]"):
-                    sku = raw_sku.copy()
-                    form_data = sku["form-data"].copy()
-                    size_value = size_s.css("::attr(value)").extract_first()
-                    form_data[size_s.css("::attr(name)").extract_first()] = size_value
-                    sku["form-data"] = form_data
-                    size_title = skus_size_s.css(f"[for*='{size_value}'] >::text").extract_first()
-                    sku["size"] = f"{sku['size']}/{size_title}" if sku.get("size") else size_title
-                    raw_skus.append(sku)
+            for raw_sku in product(skus, size_s.css(form_input_css)):
+                sku = raw_sku[0].copy()
+                form_data = sku["form-data"].copy()
+                value = raw_sku[1].css(attr_value_css).extract_first()
+                form_data[raw_sku[1].css(attr_name_css).extract_first()] = value
+                sku["form-data"] = form_data
+                title = raw_size_s.css(size_title_css.format(value)).extract_first()
+                sku["size"] = "{}/{}".format(sku["size"], title) if "size" in sku else title
+                raw_skus.append(sku)
 
         return raw_skus
