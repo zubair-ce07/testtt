743d4e65b2e9115cbfebf9fafbdc225f16564fb4
tehreem-sadat <42185078+tehreem-sadat@users.noreply.github.com>
2018-08-29 11:20:57 -0700

    update controller file

--- weatherman/main.py
+++ weatherman/main.py
@@ -1,27 +1,58 @@
 """ Controller file """
 import argparse
+from datetime import datetime
 from year_reports import display_year_report
 from month_reports import display_month_report
 
 
-PARSER = argparse.ArgumentParser(description="Weatherman: to generate different reports")
+TODAY = datetime.today()
+
+
+def validate_year(year):
+    """ function to validate year string """
+    if year and year.isdigit():
+        if int(year) <= int(TODAY.year) and int(year) >= 1990:
+            return year
+    print("Invalid option [Required year in range (1990, {0})]",
+          str(TODAY.year))
+    return False
+
+
+def validate_date(date):
+    """ function to validate year/month string """
+    try:
+        if date != datetime.strptime(date, "%Y/%m").strftime('%Y/%m'):
+            raise ValueError
+        return date
+    except ValueError:
+        print("Invalid option [Required year/month ie 2011/05]")
+        return False
+
+
+PARSER = argparse.ArgumentParser(description=(
+    "Weatherman: to generate different reports"))
+
 PARSER.add_argument("path",
                     help="path to the dir that conatain weatherfiles.")
-PARSER.add_argument("-e", dest="year_false",
+
+PARSER.add_argument("-e", type=validate_year,
                     help="display year report: Max temp, Min temp, Humidity")
-PARSER.add_argument("-d", dest="year_true",
-                    help="display year graph of highest and lowest temperature.")
-PARSER.add_argument("-a", dest="month_false",
+PARSER.add_argument("-d", type=validate_year,
+                    help=("display year graph of highest " +
+                          "and lowest temperature."))
+PARSER.add_argument("-a", type=validate_date,
                     help="display month average report")
-PARSER.add_argument("-c", dest="month_true",
-                    help="display month graph of highest and lowest temperature.")
+PARSER.add_argument("-c", type=validate_date,
+                    help=("display month graph of highest " +
+                          "and lowest temperature."))
 
 ARGS = PARSER.parse_args()
-if ARGS.year_false:
-    display_year_report(ARGS.path, ARGS.year_false, False)
-if ARGS.year_true:
-    display_year_report(ARGS.path, ARGS.year_true, True)
-if ARGS.month_false:
-    display_month_report(ARGS.path, ARGS.month_false, False)
-if ARGS.month_true:
-    display_month_report(ARGS.path, ARGS.month_true, True)
+
+if ARGS.e is not None and ARGS.e is not False:
+    print(display_year_report(ARGS.path, ARGS.e, False))
+if ARGS.d is not None and ARGS.d is not False:
+    print(display_year_report(ARGS.path, ARGS.d, True))
+if ARGS.a is not None and ARGS.a is not False:
+    print(display_month_report(ARGS.path, ARGS.a, False))
+if ARGS.c is not None and ARGS.c is not False:
+    print(display_month_report(ARGS.path, ARGS.c, True))
