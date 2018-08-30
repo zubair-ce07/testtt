811db6b6f4f723a612e40bdc43b02cdfbf07cfb0
BilalZaib <zaibbilal0@gmail.com>
2018-08-30 18:15:19 +0500

    'find_key method refactored'

--- newlook/newlook_spider.py
+++ newlook/newlook_spider.py
@@ -178,13 +178,13 @@ class NewlookCrawlSpider(BaseCrawlSpider, Mixin):
         for k, v in dictionary.items():
             if k == key:
                 yield v
+
             elif isinstance(v, dict):
-                for result in self.find_key(key, v):
-                    yield result
+                yield from (result for result in self.find_key(key, v))
+
             elif isinstance(v, list):
                 for d in v:
-                    for result in self.find_key(key, d):
-                        yield result
+                    yield from (result for result in self.find_key(key, d))
 
 
 class NewlookUKParseSpider(NewlookParseSpider, MixinUK):
