a4a0b9ba34c64c306ade762f940ed7cd3ea13d91
Muhammad Haseeb <muhammad.haseeb@arbisoft.com>
2018-08-31 11:53:39 +0500

    Updated code as requested

--- App/main.py
+++ App/main.py
@@ -4,14 +4,14 @@ from Utils.InputSystem import InputSystem
 from Utils.OutputSystem import OutputSystem
 from Crawler.Crawler import Crawler
 from Utils.TextProcessor import TextProcessor
-from Db.Db import DataAcessLayer
+from Db.Db import DataAccessLayer
 
 
 input_sys = InputSystem()
 output_sys = OutputSystem()
 crawler = Crawler()
 text_processor = TextProcessor()
-db = DataAcessLayer()
+db = DataAccessLayer()
 
 
 def main_controller():
