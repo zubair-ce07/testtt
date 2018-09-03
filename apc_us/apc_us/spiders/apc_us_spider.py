8fed0a10b7e501b96ed8b93b0fa1c7f94abac214
Ali Nauman <ali60351@gmail.com>
2018-09-03 11:39:05 +0500

    Replaced functions with item loaders

--- apc_us/apc_us/spiders/apc_us_spider.py
+++ apc_us/apc_us/spiders/apc_us_spider.py
@@ -1,7 +1,77 @@
-import re
 import json
+import re
 
 import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst, Identity
+
+
+class Sku(scrapy.Item):
+    sku_id = scrapy.Field()
+    color = scrapy.Field()
+    size = scrapy.Field()
+    out_of_stock = scrapy.Field()
+    currency = scrapy.Field()
+    price = scrapy.Field()
+    previous_price = scrapy.Field()
+
+
+class Product(scrapy.Item):
+    retailer_sku = scrapy.Field()
+    name = scrapy.Field()
+    brand = scrapy.Field()
+    gender = scrapy.Field()
+    category = scrapy.Field()
+    image_urls = scrapy.Field()
+    url = scrapy.Field()
+    description = scrapy.Field()
+    care = scrapy.Field()
+    skus = scrapy.Field()
+
+
+class ProductLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+    gender_map = {'women_apparel_size_label': 'female', 'men_apparel_size_label': 'male'}
+
+    @staticmethod
+    def fetch_category(categories):
+        return categories[1:]
+
+    def fetch_gender(self, size_label):
+        return self.gender_map.get(size_label[0])
+
+    @staticmethod
+    def fetch_description(description):
+        description = description[0]
+        return [d.strip() for d in description.split('. ') if '%' not in d]
+    
+    @staticmethod
+    def fetch_care(care):
+        care = care[0]
+        return [d.strip() for d in care.split('. ') if '%' in d]
+
+    @staticmethod
+    def fetch_images(product_gallery, loader_context):
+        retailer_sku = loader_context.get('response').url.split('-')[-1]
+        retailer_sku_lowercase = retailer_sku.lower()
+        return [i.replace('600x', '1800x') for i in product_gallery
+                if retailer_sku_lowercase in i.lower()]
+
+    category_in = fetch_category
+    gender_in = fetch_gender
+    description_in = fetch_description
+    care_in = fetch_care
+    image_urls_in = fetch_images
+
+    category_out = Identity()
+    image_urls_out = Identity()
+    description_out = Identity()
+    care_out = Identity()
+    skus_out = Identity()
+
+
+class SkuLoader(ItemLoader):
+    default_output_processor = TakeFirst()
 
 
 class APCUSSpider(scrapy.Spider):
@@ -31,54 +101,20 @@ class APCUSSpider(scrapy.Spider):
             yield response.follow(url, self.parse_product)
 
     def parse_product(self, response):
-        return {
-            'retailer_sku': self.get_product_retailer_sku(response),
-            'name': self.get_product_name(response),
-            'category': self.get_product_categories(response),
-            'description': self.get_product_description(response),
-            'image_urls': self.get_product_images(response),
-            'url': response.url,
-            'gender': self.get_product_gender(response),
-            'brand': 'A.P.C.',
-            'care': self.get_product_care(response),
-            'skus': self.get_product_skus(response)
-        }
+        product_loader = ProductLoader(item=Product(), response=response)
 
-    @staticmethod
-    def get_product_categories(response):
-        categories = response.css('.breadcrumbs a::text').extract()
-        return categories[1:]
+        product_loader.add_css('name', '.product-name h1::text')
+        product_loader.add_css('category', '.breadcrumbs a::text')
+        product_loader.add_css('description', '#description div div::text')
+        product_loader.add_css('image_urls', '.product-image-gallery img::attr(src)')
+        product_loader.add_css('care', '#description div div::text')
+        product_loader.add_value('brand', 'A.P.C.')
+        product_loader.add_value('retailer_sku', response.url.split('-')[-1])
+        product_loader.add_value('url', response.url)
+        product_loader.add_value('skus', self.get_product_skus(response))
+        product_loader.add_xpath('gender', '//dt/label[contains(@id, "size")]/@id')
 
-    @staticmethod
-    def get_product_name(response):
-        return response.css('.product-name h1::text').extract_first()
-
-    @staticmethod
-    def get_product_description(response):
-        description = response.css('#description div div::text').extract_first()
-        return [d.strip() for d in description.split('. ') if '%' not in d]
-
-    @staticmethod
-    def get_product_care(response):
-        care = response.css('#description div div::text').extract_first()
-        return [c.strip() for c in care.split('. ') if '%' in c]
-
-    def get_product_images(self, response):
-        retailer_sku = self.get_product_retailer_sku(response)
-        product_gallery = response.css('.product-image-gallery img::attr(src)').extract()
-        retailer_sku_lowercase = retailer_sku.lower()
-        return [i.replace('600x', '1800x') for i in product_gallery
-                if retailer_sku_lowercase in i.lower()]
-
-    @staticmethod
-    def get_product_retailer_sku(response):
-        return response.url.split('-')[-1]
-
-    @staticmethod
-    def get_product_gender(response):
-        gender_map = {'women_apparel_size_label': 'female', 'men_apparel_size_label': 'male'}
-        size_label = response.xpath('//dt/label[contains(@id, "size")]/@id').extract_first()
-        return gender_map.get(size_label)
+        return product_loader.load_item()
 
     @staticmethod
     def get_product_currency(response):
@@ -86,7 +122,6 @@ class APCUSSpider(scrapy.Spider):
         return response.xpath(currency_xpath).re(r'currency: \'([A-Z]+)\'')[0]
 
     def get_product_skus(self, response):
-        skus = []
         colors = response.css('#configurable_swatch_color li')
         sizes = response.css('.configurable-swatch-list.configurable-block-list.clearfix li')
         common_pricing = self.get_product_prices(response)
@@ -96,21 +131,23 @@ class APCUSSpider(scrapy.Spider):
         for color_sel in colors:
             color_code, color_name = self.get_product_color(color_sel)
             for size_sel in sizes:
-                sku = common_pricing.copy()
+                sku_loader = SkuLoader(item=Sku(), response=response)
+                for price in common_pricing.keys():
+                    sku_loader.add_value(price, common_pricing[price])
+
                 size_code, size_name = self.get_product_size(size_sel)
                 key = f"{color_code},{size_code}"
                 raw_sku = raw_product.get(key)
                 if not raw_sku:
                     continue
 
-                sku['sku_id'] = raw_sku.get('product_id')
-                sku['color'] = color_name
-                sku['currency'] = currency
-                sku['size'] = size_name
-                sku['out_of_stock'] = not raw_sku.get('is_in_stock')
+                sku_loader.add_value('sku_id', raw_sku.get('product_id'))
+                sku_loader.add_value('color', color_name)
+                sku_loader.add_value('currency', currency)
+                sku_loader.add_value('size', size_name)
+                sku_loader.add_value('out_of_stock', not raw_sku.get('is_in_stock'))
 
-                skus.append(sku)
-        return skus
+                yield sku_loader.load_item()
 
     @staticmethod
     def get_product_size(response):
