ddc38a59ac12136a556d7711afab15cf3a4b73d1
tehreem-sadat <42185078+tehreem-sadat@users.noreply.github.com>
2018-09-04 08:13:11 -0700

    Update items.py

--- orsay/orsay/items.py
+++ orsay/orsay/items.py
@@ -12,23 +12,3 @@ class OrsayItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
-
-class Product(scrapy.Item):
-    brand = 'Orsay'
-    gender = 'women'
-    name = scrapy.Field()
-    description = scrapy.Field()
-    retailer_sku = scrapy.Field()
-    category = scrapy.Field()
-    # care image url
-    skus = scrapy.Field(serializer=dict)
-    url = scrapy.Field()
-    currency = scrapy.Field()
-    price = scrapy.Field()
-
-
-class Sku(scrapy.Item):
-    color = scrapy.Field()
-    currency = scrapy.Field()
-    price = scrapy.Field()
-    size = scrapy.Field()
