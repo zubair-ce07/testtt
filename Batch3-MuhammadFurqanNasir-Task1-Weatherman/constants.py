7df6342e79dbc8b291c3ac0cea5c6c220d97f03b
m-furqan <furqan.nasir@arbisoft.com>
2018-09-04 13:03:14 +0500

    deleted extra files and combined all code files in code_file directory

---
+++ Batch3-MuhammadFurqanNasir-Task1-Weatherman/constants.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3.6
+
+MONTHS_NAME = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+ARGUMENT_ERROR_MESSAGE = "Invalid Arguments"
