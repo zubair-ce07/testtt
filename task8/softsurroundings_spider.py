65e2966cdda4ae8b7677d3d282ed92f3470b5e12
Aftab Akram <aftabakram>
2018-09-04 14:21:36 +0500

    extract_sku function rewriiten, retailer_sku uniqueness mechanisim, pagination-func revisted

--- task8/softsurroundings_spider.py
+++ task8/softsurroundings_spider.py
@@ -1,12 +1,8 @@
-from json import loads
-from re import sub
 from itertools import product
-from six.moves.urllib.parse import unquote_plus, urljoin
 
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-from scrapy import FormRequest
-from scrapy.selector import Selector
+from scrapy import Request
 
 from softsurroundings.items import Product
 
@@ -17,62 +13,56 @@ class SoftSurroundingsSpider(CrawlSpider):
 
     start_urls = ['https://www.softsurroundings.com/']
 
-    categoy_css = '#menuNav'
+    listing_css = '#menuNav'
     product_css = '.product'
-    pagination_css = '.thumbscroll'
+    pagination_css = '.thumbscroll [name="page"]::attr(value)'
+    retailer_skus = set()
 
     rules = (
-        Rule(LinkExtractor(restrict_css=categoy_css), callback='parse_category'),
+        Rule(LinkExtractor(restrict_css=listing_css), callback='parse'),
         Rule(LinkExtractor(restrict_css=product_css), callback='parse_product'),
     )
 
     custom_settings = {
-        'DOWNLOAD_DELAY': 6,
-        'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)'
-                      ' Chrome/68.0.3440.106 Safari/537.36'
+        'DOWNLOAD_DELAY': 2,
     }
 
-    def parse_category(self, response):
-        next_page_css = '[name="page"]::attr(value)'
-        prod_keys_css = '[name="docs"]::attr(value)'
-        params = {'ajax': '1'}
+    def parse(self, response):
+        yield from super().parse(response)
+        yield from self.parse_listing(response)
 
-        next_page_sels = response.css(self.pagination_css)
-
-        for page_sel in next_page_sels:
-            next_page = page_sel.css(next_page_css).extract_first()
-            prod_keys = page_sel.css(prod_keys_css).extract_first()
-
-            params['docKeys'] = prod_keys
-            params['count'] = len(prod_keys.split(','))
-
-            pagination_request_url = urljoin(response.url, f'page-{next_page}/')
-            yield FormRequest(pagination_request_url, formdata=params, callback=self.parse_pagination)
-
-    def parse_pagination(self, response):
-        raw_prod_content = loads(response.text)['content']
-        product_css = f'{self.product_css} a::attr(href)'
-        product_urls = Selector(text=raw_prod_content).css(product_css).extract()
-
-        for url in product_urls:
-            yield response.follow(url, callback=self.parse_product)
+    def parse_listing(self, response):
+        next_pages = response.css(self.pagination_css).extract()
+        for next_page in next_pages:
+            yield Request(f'{response.url}page-{next_page}/', callback=self.parse)
 
     def parse_product(self, response):
-        product_item = Product()
+        if self.extract_retailer_sku(response) in self.retailer_skus:
+            return
+        self.retailer_skus.add(self.extract_retailer_sku(response))
+        product = Product()
 
-        product_item['gender'] = self.extract_gender(response)
-        product_item['name'] = self.extract_product_name(response)
-        product_item['image_urls'] = self.extract_image_urls(response)
-        product_item['retailer_sku'] = self.extract_retailer_sku(response)
-        product_item['description'] = self.extract_description(response)
-        product_item['category'] = self.extract_categories(response)
-        product_item['url'] = self.extract_product_url(response)
-        product_item['brand'] = self.extract_brand(response)
-        product_item['care'] = self.extract_care(response)
+        product['gender'] = self.extract_gender(response)
+        product['name'] = self.extract_product_name(response)
+        product['image_urls'] = self.extract_image_urls(response)
+        product['retailer_sku'] = self.extract_retailer_sku(response)
+        product['description'] = self.extract_description(response)
+        product['category'] = self.extract_categories(response)
+        product['url'] = self.extract_product_url(response)
+        product['brand'] = self.extract_brand(response)
+        product['care'] = self.extract_care(response)
+        product['skus'] = []
+        product['requests_queue'] = self.extract_skus_requests(response)
 
-        product_item['requests_queue'] = self.extract_skus_requests(response)
+        yield self.requests_to_follow(product)
 
-        return self.requests_to_follow(product_item)
+        yield from self.category_request(response)
+
+    def category_request(self, response):
+        prod_category_ids = response.css('#sizecat a:not(.sel)::attr(id)').extract()
+        for prod_category_id in prod_category_ids:
+            prod_category_url = f"https://www.softsurroundings.com/p/{prod_category_id.split('_')[1]}/"
+            yield Request(prod_category_url, callback=self.parse_product)
 
     def requests_to_follow(self, product_item):
         next_requests = product_item.get("requests_queue")
@@ -87,38 +77,17 @@ class SoftSurroundingsSpider(CrawlSpider):
         return product_item
 
     def extract_skus_requests(self, response):
-        unique_id = response.css('[name="uniqid"]::attr(value)').extract_first()
-        parent_id = response.css('[name="parentId"]::attr(value)').extract_first().lower()
-
-        color_id_css = f'#color .swatchlink img::attr(data-value),' \
-                       f'input[name="specOne-{unique_id}"]::attr(value)'
-        color_ids = response.css(color_id_css).extract()
-        color_ids = set(filter(None, color_ids))
-
-        size_ids = response.css('#size a.size::attr(id)').extract() or ['size_000']
-        size_ids = set(filter(None, size_ids))
-
-        params = {'ajax': '1', f'sizecat-{unique_id}': parent_id, 'sizecat_desc': parent_id,
-                  f'quantity-{unique_id}': '1', 'uniqid': unique_id, 'parentId': parent_id}
-
+        color_ids = self.extract_color_ids(response)
+        size_ids = self.extract_size_ids(response)
         requests_to_follow = []
         for color_id, size_id in product(color_ids, size_ids):
-
-            color_css = f'img[data-value="{color_id}"]::attr(alt),#color .sizetbs .basesize::text'
-            size_css = f'a[id="{size_id}"]::text,#size .sizetbs .basesize::text'
-
-            meta = {'color': response.css(color_css).extract_first(default=''),
-                    'size': response.css(size_css).extract_first(default='One Size')}
+            meta = {}
+            meta['color'] = self.extract_color(response, color_id)
+            meta['size'] = self.extract_size(response, size_id)
 
             size_id = size_id.split("_")[1]
-            params = params.copy()
-            params[f'specOne-{unique_id}'] = color_id
-            params[f'specTwo-{unique_id}'] = size_id
-            params[f'sku_{parent_id}'] = f'{parent_id}{color_id}{size_id}'
-
-            sku_request_url = f'https://www.softsurroundings.com/p/{parent_id}/{color_id}{size_id}'
-            request = FormRequest(sku_request_url, meta=meta, formdata=params,
-                                  callback=self.parse_skus)
+            sku_request_url = f'{response.url}{color_id}{size_id}'
+            request = Request(sku_request_url, meta=meta, callback=self.parse_skus)
 
             requests_to_follow.append(request)
 
@@ -126,67 +95,76 @@ class SoftSurroundingsSpider(CrawlSpider):
 
     def parse_skus(self, response):
         product_item = response.meta['item']
-        product_item["skus"] = product_item.get('skus', []) + self.extract_sku(response)
+        product_item["skus"].append(self.extract_sku(response))
         return self.requests_to_follow(product_item)
 
+    def extract_color_ids(self, response):
+        raw_id = response.css('[name="uniqid"]::attr(value)').extract_first()
+
+        color_id_css = f'#color .swatchlink img::attr(data-value),' \
+                       f'input[name="specOne-{raw_id}"]::attr(value)'
+        color_ids = response.css(color_id_css).extract()
+        return set(filter(None, color_ids))
+
+    def extract_size_ids(self, response):
+        size_ids = response.css('#size a.size::attr(id)').extract() or ['size_000']
+        return set(filter(None, size_ids))
+
+    def extract_color(self, response, color_id):
+        color_css = f'img[data-value="{color_id}"]::attr(alt),#color .sizetbs .basesize::text'
+        return response.css(color_css).extract_first(default='')
+
+    def extract_size(self, response, size_id):
+        size_css = f'a[id="{size_id}"]::text,#size .sizetbs .basesize::text'
+        return response.css(size_css).extract_first(default='One Size')
+
     def extract_sku(self, response):
-        raw_product = loads(response.text)
-        raw_prod_header = Selector(text=unquote_plus(raw_product['productHeader']))
-        raw_sku_sel = Selector(text=unquote_plus(raw_product['productBulk']))
-
-        sku = {'currency': raw_prod_header.css('[itemprop="priceCurrency"]::text').extract_first(),
-               'price': raw_prod_header.css('[itemprop="price"]::text').extract_first()}
-
-        prod_color = response.meta['color']
-        prod_size = response.meta['size']
-        sku['color'] = prod_color
-        sku['size'] = prod_size
-        sku['sku_id'] = f'{prod_color}_{prod_size}'
-
-        raw_prev_price = response.css('.ctntPrice::text').re('Was([^;]+)')
+        sku = {}
+        sku['currency'] = response.css('.dtlHeader [itemprop="priceCurrency"]::text').extract_first()
+        sku['price'] = response.css('.dtlHeader [itemprop="price"]::text').extract_first()
+        sku['color'] = response.meta['color']
+        sku['size'] = response.meta['size']
+        sku['sku_id'] = f"{response.meta['color']}_{response.meta['size']}"
+        raw_prev_price = response.css('.dtlHeader .ctntPrice::text').re('Was([^;]+)')
 
         if raw_prev_price:
             sku['previous_price'] = [raw_prev_price[0].strip()[1:]]
 
-        prod_stock_status = raw_sku_sel.css('.stockStatus .basesize::text').extract_first()
-        if prod_stock_status != 'In Stock.':
+        prod_stock_status = response.css('#orderProcessError ::text')
+
+        if prod_stock_status:
             sku['out_of_stock'] = True
 
-        return [sku]
+        return sku
 
     def extract_gender(self, response):
         if 'bedding' in ' '.join(self.extract_categories(response)).lower():
-            return "Unisex"
+            return "unisex-adults"
         return "Women"
 
     def extract_image_urls(self, response):
-        raw_images = response.css('#detailAltImgs ::attr(src)').extract()
-        return [sub('\/(\d*x\d*)\/', '/1200x1802/', raw_image) for raw_image in raw_images]
+        raw_image_urls = response.css('#detailAltImgs ::attr(src)').extract()
+        return [url.replace('/80x120/', '/1200x1802/') for url in raw_image_urls]
 
     def extract_product_name(self, response):
-        return response.css('[itemprop="name"]::text').extract_first()
+        return response.css('.dtlHeader [itemprop="name"]::text').extract_first()
 
     def extract_care(self, response):
-        care_css = '#careAndContentInfo span::text, #directionsInfo ::text'
-        return response.css(care_css).extract()
+        return response.css('#careAndContentInfo span::text, #directionsInfo ::text').extract()
 
     def extract_description(self, response):
-        desc_css = '.productInfo>:not(.productInfoDetails) ::text ,.productInfo ::text'
-        raw_desc = response.css(desc_css).extract()
-        return [desc for desc in raw_desc if desc.strip() != 'Description & Details']
+        return response.css('.productInfo ::text').extract()
 
     def extract_retailer_sku(self, response):
-        return response.css('[itemprop="productID"]::text').extract_first()
+        return response.css('.dtlHeader [itemprop="productID"]::text').extract_first()
 
     def extract_brand(self, response):
         brand_css = '.productInfoDetails li:contains("Designed by")::text,' \
                     '#aboutbrandInfo img::attr(alt)'
-        raw_brand = response.css(brand_css).extract_first(default='Soft Surroundings')
-        return raw_brand.replace('Designed by', '').strip()
+        return response.css(brand_css).extract_first(default='Soft Surroundings')
 
     def extract_product_url(self, response):
         return response.url
 
     def extract_categories(self, response):
-        raw_categories = response.css('.pagingBreadCrumb ::text').extract()
-        return [raw_cat for raw_cat in raw_categories if '/' != raw_cat.strip()]
+        return response.css('.pagingBreadCrumb ::text').extract()
