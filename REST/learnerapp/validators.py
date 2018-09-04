832a415e65820654dca9a6e09f44bb528536a75b
Zainab Amir <40633976+zainab-amir@users.noreply.github.com>
2018-09-04 12:15:56 +0500

    zainabamir-learnerapp_validators (#1070)
    * Add validators
    
    * Remove typo

--- REST/learnerapp/validators.py
+++ REST/learnerapp/validators.py
@@ -17,5 +17,5 @@ def dob_validation(date):
     days = date - datetime.datetime.now().date()
     age = (abs(days)/365).days
     if age < 13:
-        raise serializers.ValidationError('You mush be at least 13 yrs old to join learnerapp')
+        raise serializers.ValidationError('You must be at least 13 yrs old to join learnerapp')
 
