aaa35643c2b08eb3d23bea8b5974c8a49a4c8447
m-furqan <furqan.nasir@arbisoft.com>
2018-09-04 13:15:17 +0500

    deleting extra files

--- Batch3-MuhammadFurqanNasir-Task1-Weatherman/constants.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3.6
-
-MONTHS_NAME = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-
-ARGUMENT_ERROR_MESSAGE = "Invalid Arguments"
