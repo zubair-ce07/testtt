c2503b4200ffb9bab40ede979dfc48e35f4d6ebf
Muhammad Umar <umar@HassanKhan-Desktop.arbisoft.com>
2018-09-05 18:06:24 +0500

    formatting issues has been resolved

--- books_scraper/books_scraper/spiders/book_spider.py
+++ books_scraper/books_scraper/spiders/book_spider.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+"""
+Scraps details of each book available
+on each page following hyperlinks
+"""
 import scrapy
 
 from items import BooksScraperItem
@@ -10,7 +14,13 @@ class BookScraperSpider(scrapy.Spider):
     start_urls = ['http://books.toscrape.com/']
 
     def parse(self, response):
-        urls = response.css('section li[class*="col-xs-6"]>article.product_pod h3>a::attr(href)').extract()
+        """
+        Scraps all the available
+        hyperlinks of the books.
+        """
+        urls = response.css(
+            'section li[class*="col-xs-6"]>article.product_pod h3>a::attr(href)'
+        ).extract()
         for url in urls:
             url = response.urljoin(url)
             yield scrapy.Request(url=url, callback=self.parse_book_details)
@@ -22,16 +32,26 @@ class BookScraperSpider(scrapy.Spider):
             yield scrapy.Request(url=next_page_url, callback=self.parse)
 
     def parse_book_details(self, response):
+        """
+        Scraps the details of book
+        available on the page.
+        """
         book = BooksScraperItem()
         keys = response.css('article.product_page table th::text').extract()
         values = response.css('article.product_page table td::text').extract()
         dictionary = dict(zip(keys, values))
 
-        book["title"] = response.css('div.page_inner > ul.breadcrumb li.active::text').extract_first()
-        book["category"] = response.css('div.page_inner > ul.breadcrumb li>a::text').extract()[2]
-        book["description"] = response.css('div#product_description + p::text').extract()
+        book["title"] = response.css(
+            'div.page_inner > ul.breadcrumb li.active::text').extract_first()
+        book["category"] = response.css(
+            'div.page_inner > ul.breadcrumb li>a::text').extract()[2]
+        book["description"] = response.css(
+            'div#product_description + p::text').extract()
         book["product_information"] = dictionary
-        book["rating"] = response.css('div.product_main > p.star-rating::attr(class)').extract_first().split()[1]
-        book["price"] = response.css('div.product_main p.price_color::text').extract()
+        book["rating"] = response.css(
+            'div.product_main > p.star-rating::attr(class)').extract_first(
+            ).split()[1]
+        book["price"] = response.css(
+            'div.product_main p.price_color::text').extract()
 
         yield book
