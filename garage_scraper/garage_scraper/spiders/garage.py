cf74f041fee838c13fd9dfa210cbaa7bfc14f6ae
Muhammad Umar <umar@HassanKhan-Desktop.arbisoft.com>
2018-09-05 17:54:24 +0500

    formatting issues resolved

--- garage_scraper/garage_scraper/spiders/garage.py
+++ garage_scraper/garage_scraper/spiders/garage.py
@@ -3,7 +3,6 @@
 Extracts the details of product list
 available on garageclothing.com/ca
 """
-
 import scrapy
 
 
@@ -15,7 +14,7 @@ class GarageSpider(scrapy.Spider):
     def start_requests(self):
         yield scrapy.Request(
             url='https://www.dynamiteclothing.com/?postSessionRedirect=https%3A'
-                '//www.garageclothing.com/ca&noRedirectJavaScript=true',
+            '//www.garageclothing.com/ca&noRedirectJavaScript=true',
             callback=self.pre_parse,
         )
 
@@ -28,7 +27,6 @@ class GarageSpider(scrapy.Spider):
         method parse who does the actual
         scraping
         """
-
         self.JSESSIONID = response.url.split('=')[1]
         yield scrapy.Request(
             url='https://www.garageclothing.com/ca',
@@ -43,19 +41,26 @@ class GarageSpider(scrapy.Spider):
         for my_url in urls:
             if my_url:
                 my_url = response.urljoin(my_url)
-            yield scrapy.Request(url=my_url,
-                                 callback=self.parse_cloth,
-                                 cookies={
-                                     'JSESSIONID': self.JSESSIONID,
-                                 }
-                                 )
+            yield scrapy.Request(
+                url=my_url,
+                callback=self.parse_cloth,
+                cookies={
+                    'JSESSIONID': self.JSESSIONID,
+                })
 
     def parse_cloth(self, response):
         yield {
-            "name": response.css('h1.prodName::text').extract_first(),
-            "price": response.css('h2.prodPricePDP::text').extract_first(),
-            "promo_message": response.css('p.promoMessage::text').extract_first(),
-            "color": response.css('div.swatchColor::text').extract_first(),
-            "sizes": response.css('div#productSizes span::text').extract(),
-            "description": response.css('div#descTabDescriptionContent>p::text').extract_first(),
+            "name":
+            response.css('h1.prodName::text').extract_first(),
+            "price":
+            response.css('h2.prodPricePDP::text').extract_first(),
+            "promo_message":
+            response.css('p.promoMessage::text').extract_first(),
+            "color":
+            response.css('div.swatchColor::text').extract_first(),
+            "sizes":
+            response.css('div#productSizes span::text').extract(),
+            "description":
+            response.css('div#descTabDescriptionContent>p::text')
+            .extract_first(),
         }
