46fdb786925f8ee5050d170d071e6a5050c3d614
Muhammad Ejaz <40828187+muhammad-ejazz@users.noreply.github.com>
2018-09-05 18:02:31 +0500

    Ejaz neimanmarcus (#1072)
    * Wrote  neimanmarcus branch

--- scrapy_advance_training/scrapy_advance_training/settings.py
+++ scrapy_advance_training/scrapy_advance_training/settings.py
@@ -16,7 +16,10 @@ NEWSPIDER_MODULE = 'scrapy_advance_training.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'scrapy_advance_training (+http://www.yourdomain.com)'
+#USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+
+# Enabled cookies
+#COOKIES_ENABLED = True
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
