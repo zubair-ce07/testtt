8b42a33d683f37d7109e92149d7101d652a893e7
Syed Rehan <rehan@AwaishKumar-Desktop-106.arbisoft.com>
2018-09-05 15:17:30 +0500

    Iteration one

---
+++ django/users/views.py
@@ -0,0 +1,9 @@
+from .forms import SignUpForm
+from django.urls import reverse_lazy
+from django.views import generic
+
+
+class SignUp(generic.CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
