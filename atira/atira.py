cbdb10acc7f2cb878738105ec85092e1fa96e57b
abbas-maalik <abbas.malik@arbisoft.com>
2018-09-05 12:02:09 +0500

    improved room photos css, used variables for css, better variable names

--- atira/atira.py
+++ atira/atira.py
@@ -63,6 +63,12 @@ class Atira(CrawlSpider):
         ('latrobe', 'La Trobe')
     )
 
+    images_regex = re.compile('\((.+)\)')
+    duration_price_regex = re.compile('(\d+).*?(\$\d+)')
+    contact_info_regex = re.compile(
+        r"subtitle\":\"([^\"]+?)\"[^}]*?\"phone\":\"([^\"]+?)\"[^}]*?email\":\"(.*?)\""
+    )
+
     def parse(self, response):
         self.deals = self.extract_deals(response)
         yield Request(url=self.home_url, callback=self.parse_atira)
@@ -104,7 +110,7 @@ class Atira(CrawlSpider):
         item['listing_type'] = 'flexible_open_end'
         item['available_from'] = self.peel_latrobe_date
 
-        rooms_css = '#rooms .et_pb_row_7 .et_pb_column, #rooms .et_pb_row_8 .et_pb_column, ' \
+        rooms_css = '#rooms .et_pb_row_7 .et_pb_column, #rooms .et_pb_row_8 .et_pb_column :not(.et_pb_column_empty), ' \
                     '#rooms .et_pb_row_9 .et_pb_column'
         for apartment_html in response.css(rooms_css).extract():
             yield from self.extract_apartments_peel_latrobe(apartment_html, item.copy())
@@ -117,7 +123,7 @@ class Atira(CrawlSpider):
 
         yield from self.make_apartment_requests(response.meta['urls'], item)
 
-    def parse_rooms(self, response):
+    def parse_apartments(self, response):
         item = response.meta['item']
 
         item['property_url'] = response.url
@@ -150,7 +156,8 @@ class Atira(CrawlSpider):
             yield copy_item
 
     def make_locations_requests(self, response, item):
-        location_urls = response.css('.location-three-col a::attr(href)').extract()
+        css = '.location-three-col a::attr(href)'
+        location_urls = response.css(css).extract()
 
         location_requests = []
         for url in location_urls:
@@ -162,7 +169,8 @@ class Atira(CrawlSpider):
     def make_facilities_requests(self, response, item):
         apartment_urls = self.extract_apartment_urls(response)
 
-        facilities_url = response.css('.grid-seemore ::attr(href)').extract_first()
+        css = '.grid-seemore ::attr(href)'
+        facilities_url = response.css(css).extract_first()
         facility_request = Request(url=facilities_url, callback=self.parse_facilities)
         facility_request.meta['item'] = item
         facility_request.meta['urls'] = apartment_urls
@@ -172,13 +180,14 @@ class Atira(CrawlSpider):
     def make_apartment_requests(self, apartment_urls, item):
         apartment_requests = []
         for url in apartment_urls:
-            request = Request(url=url, callback=self.parse_rooms)
+            request = Request(url=url, callback=self.parse_apartments)
             request.meta['item'] = item.copy()
             apartment_requests.append(request)
         return apartment_requests
 
     def has_facilities_link(self, response):
-        return response.css('.grid-seemore ::attr(href)').extract_first()
+        css = '.grid-seemore ::attr(href)'
+        return response.css(css).extract_first()
 
     def extract_apartment_urls(self, response):
         css = '.d-1of3 a::attr(href), #rooms .et_pb_module.et_pb_image > a::attr(href)'
@@ -242,16 +251,17 @@ class Atira(CrawlSpider):
 
     def extract_without_table_room_variants(self, response):
         apartment_name = self.extract_apartment_name(response)
-        prices = response.css('.room-type-pricebreakdown ::text').extract()
 
-        regex = '(\d+).*?(\$\d+)'
+        css = '.room-type-pricebreakdown ::text'
+        duration_and_prices = response.css(css).extract()
+
         rooms_variants = []
-        for price in prices:
-            duration, price = re.findall(regex, price)[0]
+        for duration_and_price in duration_and_prices:
+            duration, duration_and_price = self.duration_price_regex.findall(duration_and_price)[0]
             room_info = {
                 "min_duration": self.duration_map.get(duration),
                 "room_name": apartment_name,
-                "room_price": price,
+                "room_price": duration_and_price,
             }
             rooms_variants.append(room_info)
 
@@ -268,40 +278,42 @@ class Atira(CrawlSpider):
 
     def extract_contact_info(self, response):
         reg = re.compile('.*#uber-google-map.*', re.S)
-        script = response.css('script::text').re_first(reg) or ''
+        script = response.css('script::text').re_first(reg, default='')
 
-        contact_info_regex = re.compile(
-            r"subtitle\":\"([^\"]+?)\"[^}]*?\"phone\":\"([^\"]+?)\"[^}]*?email\":\"(.*?)\""
-        )
-        raw_contact_info = contact_info_regex.findall(script)
-
+        raw_contact_info = self.contact_info_regex.findall(script)
         for contact_info in raw_contact_info:
             if contact_info[0] in self.extract_property_name(response):
                 return list(contact_info[1:])
         return []
 
     def extract_room_photos(self, response):
-        photos = response.css('.et_pb_image_0 img::attr(src)').extract()
+        peel_latrobe_css = '.et_pb_image img::attr(src)'
+        photos = response.css(peel_latrobe_css).extract()
 
-        reg = '\((.+)\)'
-        return sum([re.findall(reg, a) for a in response.css('.flexslider-roomtype li::attr(style)').extract()], photos)
+        css = '.flexslider-roomtype li::attr(style)'
+        return sum([self.images_regex.findall(a) for a in response.css(css).extract()], photos)
 
     def extract_property_images(self, response):
-        property_images = response.css('.et_pb_lightbox_image::attr(href)').extract()
+        new_format_css = '.et_pb_lightbox_image::attr(href)'
+        property_images = response.css(new_format_css).extract()
 
-        reg = '\((.+)\)'
-        return sum([re.findall(reg, a) for a in response.css('.slides li::attr(style)').extract()], property_images)
+        legacy_format_css = '.slides li::attr(style)'
+        return sum([self.images_regex.findall(a) for a in response.css(legacy_format_css).extract()], property_images)
 
     def extract_floor_plan(self, response):
-        return response.css('.floorplan-thumbnail a::attr(data-featherlight)').extract()
+        css = '.floorplan-thumbnail a::attr(data-featherlight)'
+        return response.css(css).extract()
 
     def extract_room_amenities(self, response):
         css = '.et_pb_column li ::text, .home-icon-grid-inner-container p::text'
         return response.css(css).extract()
 
     def extract_property_amenities(self, response):
-        amenities = set([d.strip() for d in response.css('#facilities .et_pb_blurb_description ::text').extract()])
-        return response.css('.home-icon-grid-contents p::text').extract() + list(amenities)
+        new_format_css = '#facilities .et_pb_blurb_description ::text'
+        amenities = set([d.strip() for d in response.css(new_format_css).extract()])
+
+        legacy_format_css = '.home-icon-grid-contents p::text'
+        return response.css(legacy_format_css).extract() + list(amenities)
 
     def extract_property_name(self, response):
         for name_str, name in self.name_map:
@@ -310,13 +322,15 @@ class Atira(CrawlSpider):
 
     def extract_property_description(self, response):
         new_format_css = '.et_pb_fullwidth_header_subhead::text, #hero p::text'
-        old_format_css = '[itemprop="articleBody"] ::text'
         description = list(set(response.css(new_format_css).extract()))
-        return response.css(old_format_css).extract() + description
+
+        legacy_format_css = '[itemprop="articleBody"] ::text'
+        return response.css(legacy_format_css).extract() + description
 
     def extract_apartment_name(self, response):
         css = '.page-title ::text, h4::text'
         return response.css(css).extract_first()
 
     def extract_deals(self, response):
-        return response.css('.wrap.cf.two-column-padding p:first-of-type::text').extract()
+        css = '.wrap.cf.two-column-padding p:first-of-type::text'
+        return response.css(css).extract()
