5e508ecd2e7bce09e52a3fc6f0aefef46e4533c8
tehreem-sadat <42185078+tehreem-sadat@users.noreply.github.com>
2018-09-06 16:48:05 +0500

    Update constants.py

--- weatherman/constants.py
+++ weatherman/constants.py
@@ -2,13 +2,3 @@
 CRED = '\033[91m'
 CBLUE = '\033[94m'
 CEND = '\033[0m'
-
-MAX_TEMPERATURE_KEY = "Max TemperatureC"
-MIN_TEMPERATURE_KEY = "Min TemperatureC"
-MAX_HUMIDITY_KEY = "Max Humidity"
-
-MAX_INDEX = -1
-MIN_INDEX = 0
-
-
-
