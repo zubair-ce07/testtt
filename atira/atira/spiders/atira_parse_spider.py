413cb87c03a1f6e98a74db7084f720e8f77d2222
mumerfarooq000 <mfarooq@arbisoft.com>
2018-09-07 20:01:05 +0500

    deals is called from the crawl spider, parse function calling revised, weeks counting dynamically

--- atira/atira/spiders/atira_parse_spider.py
+++ atira/atira/spiders/atira_parse_spider.py
@@ -1,8 +1,6 @@
-import copy
 import json
 import re
 from datetime import datetime
-from urllib import parse
 
 from scrapy.spiders import Spider
 
@@ -13,27 +11,31 @@ class AtiraParseSpider(Spider):
     name = "atira_parser"
     price_raw_map = {"p/week", "Per Week", "Per week", "per week"}
     price_notation = "pw"
+    week_notaion = 'WEEKS'
 
     def parse(self, response):
         item = Product()
-        response.meta['item'] = item
-        return self.request_deals(response)
-
-    def parse_deals(self, response):
-        item = response.meta['item']
-        css = '.content-content p::text'
-        item['deals'] = [response.css(css).extract_first()]
+        item['deals'] = response.meta['deals']
+        item['property_description'] = self.property_description(response)
+        item['property_name'] = self.property_name(response)
+        item['property_contact_info'] = self.property_contact_info(response)
+        room_requests = self.room_requests(response)
         response.meta['item'] = item
 
-        return self.property_details(response.meta['main_response'])
+        if not room_requests:
+            return self.room_details(response)
 
-    def parse_further_detail(self, response):
+        return self.room_further_details(response, room_requests)
+
+    def schedule_room_requests(self, response):
         item = response.meta['item']
         item = self.property_main_details(response, item)
-
         for request in response.meta['room_requests']:
-            request.meta['item'] = copy.copy(item)
+            request.meta['item'] = item.copy()
             yield request
+
+    def parse_further_detail(self, response):
+       self.schedule_room_requests(response)
 
     def parse_room_details(self, response):
         item = response.meta['item']
@@ -42,31 +44,6 @@ class AtiraParseSpider(Spider):
         room_name = self.room_name(response)
         pricing_info = self.pricing_breakdown(response)
         yield from self.room_variant_details(item, pricing_info, room_name)
-
-    def request_deals(self, response):
-        css = 'a:contains("Offer")::attr(href)'
-        url = response.css(css).extract_first()
-
-        if not url:
-            return self.property_details(response)
-
-        request = response.follow(url, dont_filter=True, callback=self.parse_deals)
-        request.meta['main_response'] = response
-        request.meta['item'] = response.meta['item']
-
-        return request
-
-    def property_details(self, response):
-        item = response.meta['item']
-        item['property_description'] = self.property_description(response)
-        item['property_name'] = self.property_name(response)
-        item['property_contact_info'] = self.property_contact_info(response)
-        room_requests = self.room_requests(response)
-
-        if not room_requests:
-            return self.room_details(response)
-
-        return self.room_further_details(response, room_requests, item)
 
     def room_requests(self, response):
         css = '.room-type-container a::attr(href), ' \
@@ -84,23 +61,22 @@ class AtiraParseSpider(Spider):
         parent_item = self.property_main_details(response, parent_item)
 
         for room_sel in response.css('.et_pb_column'):
-            main_item = copy.copy(parent_item)
+            main_item = parent_item.copy()
             main_item = self.room_main_details(response, main_item)
             main_item = self.property_common_details(main_item)
             room_name = self.room_name(room_sel)
             pricing_info = self.pricing_breakdown_same_page(room_sel)
             yield from self.room_variant_details(main_item, pricing_info, room_name)
 
-    def room_further_details(self, response, room_requests, item):
-        css = '.grid-seemore::attr(href)'
-        url = response.css(css).extract_first()
-
-        if not url:
+    def room_further_details(self, response, room_requests):
+        item = response.meta['item']
+        room_url = self.room_url(response)
+        if not room_url:
             response.meta['room_requests'] = room_requests
             response.meta['item'] = item
-            return self.parse_further_detail(response)
+            return self.schedule_room_requests(response)
 
-        request = response.follow(url, callback=self.parse_further_detail)
+        request = response.follow(room_url, callback=self.parse_further_detail)
         request.meta['room_requests'] = room_requests
         request.meta['item'] = item
 
@@ -134,7 +110,7 @@ class AtiraParseSpider(Spider):
     @staticmethod
     def room_variant_details(main_item, pricing_details, room_name):
         for variant in pricing_details:
-            item = copy.copy(main_item)
+            item = main_item.copy()
             item['room_type'] = variant['type']
             item['room_price'] = variant['price']
             item['room_name'] = f"{room_name} {variant['type']}"
@@ -142,7 +118,12 @@ class AtiraParseSpider(Spider):
             item['product_id'] = f"{room_name}_{variant['type']}_{variant['duration']}"
 
             yield item
-
+    
+    @staticmethod
+    def room_url(response):
+        css = '.grid-seemore::attr(href)'
+        return  response.css(css).extract_first()
+        
     @staticmethod
     def property_name(response):
         css = '.flexslider-caption ::text, link ::attr(title)'
@@ -191,11 +172,10 @@ class AtiraParseSpider(Spider):
     @staticmethod
     def property_images(response):
         pattern = re.compile(r"url\((.*?)\)")
-        galary_css = '.et_pb_lightbox_image ::attr(href)'
-        img_css = '.et_pb_image_wrap ::attr(src)'
+        galary_css = '.et_pb_lightbox_image ::attr(href), ' \
+                     '.et_pb_column:first-child ::attr(src)'
         images =  response.xpath('//ul[@class="slides"]').re(pattern)\
-                  or response.css(galary_css).extract()\
-                  or [response.css(img_css).extract_first()]
+                  or response.css(galary_css).extract()
 
         return images
 
@@ -209,17 +189,13 @@ class AtiraParseSpider(Spider):
         return "Not Available"
 
     @staticmethod
-    def floor_plans(response, url=None):
-        if not url:
-            css = '.floorplan-thumbnail ::attr(data-featherlight)'
-            return response.css(css).extract()
-
-        floor_plan = []
+    def floor_plans(response):
         for fp in response.css('img::attr(src)').extract():
             if "floorplan" in fp:
-                floor_plan.append(parse.urljoin(url, fp))
+                return response.urljoin(fp)
 
-        return floor_plan
+        css = '.floorplan-thumbnail ::attr(data-featherlight)'
+        return response.css(css).extract_first()
 
     @staticmethod
     def room_name(response):
@@ -246,9 +222,9 @@ class AtiraParseSpider(Spider):
         if not price:
             return None
 
-        for notation in self.price_raw_map:
-            if notation in price:
-                return price.replace(notation, self.price_notation)
+        for raw_notation in self.price_raw_map:
+            if raw_notation in price:
+                return price.replace(raw_notation, self.price_notation)
 
         return f"{price} {self.price_notation}"
 
@@ -272,12 +248,13 @@ class AtiraParseSpider(Spider):
         high_view_class = self.high_view_class_name(response)
         low_view_class = self.low_view_class_name(response)
         sem_class = self.semester_class_name(response)
+        sem_css = f'.{sem_class}:contains("2 Semester")'
+        avail_css = '.{} :contains("N/A")'
 
         price_details = []
         title = None
         for table_row in response.css('tbody tr'):
             duration = "119"
-            sem_css = f'.{sem_class}:contains("2 Semester")'
 
             if table_row.css(sem_css):
                 duration = "308"
@@ -287,14 +264,12 @@ class AtiraParseSpider(Spider):
             if sem_class != "column-1" and column1_info:
                 title = column1_info
 
-            avail_css = f'.{low_view_class}:contains("N/A")'
-            if not table_row.css(avail_css).extract_first() and low_view_class:
+            if not table_row.css(avail_css.format(low_view_class)) and low_view_class:
                 price = self.room_price(low_view_class, table_row)
                 price_details.append(self.pricing_details(title, price,
                                                           duration, "Low View"))
 
-            avail_css = f'.{high_view_class}:contains("N/A")'
-            if not table_row.css(avail_css).extract_first() and high_view_class:
+            if not table_row.css(avail_css .format(high_view_class)) and high_view_class:
                 price = self.room_price(high_view_class, table_row)
                 price_details.append(self.pricing_details(title, price,
                                                           duration, "High View"))
@@ -302,21 +277,23 @@ class AtiraParseSpider(Spider):
         return price_details
 
     def pricing_breakdown_same_page(self, response):
+        days_package = []
+        weeks_number_css = f'tbody tr:first-child :contains("{self.week_notaion}") ::text'
+
+        for week_value in response.css(weeks_number_css).extract():
+            days_package.append(int(week_value.replace(self.week_notaion, ''))*7)
+
+        name = self.room_name(response)
         price_details = []
-        for row in response.css('tbody tr+tr'):
-            room = row.css('td:nth-child(1) ::text').extract_first()
-            name = self.room_name(response)
+        price_css = 'td:nth-child(n+2) ::text'
 
-            price = self.refine_price(row.css('td:nth-child(2) ::text').extract_first())
-            if price:
-                price_details.append(self.pricing_details(name, price, "357", room))
+        for row in response.css('tbody tr:nth-child(n+2)'):
+            room = row.css('td:first-child ::text').extract_first()
 
-            price = self.refine_price(row.css('td:nth-child(3) ::text').extract_first())
-            if price:
-                price_details.append(self.pricing_details(name, price, "336", room))
-
-            price = self.refine_price(row.css('td:nth-child(4) ::text').extract_first())
-            if price:
-                price_details.append(self.pricing_details(name, price, "168", room))
+            for index, price in enumerate(row.css(price_css).extract()):
+                if not price:
+                    continue
+                price_details.append(self.pricing_details(name, self.refine_price(price),
+                                                          days_package[index], room))
 
         return price_details
