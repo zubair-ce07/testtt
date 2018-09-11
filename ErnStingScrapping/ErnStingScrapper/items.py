e82214b50ce4ce61950746f834c2971566f57463
Usama Sadiq <usama@UsamaSaqib-Desktop-05.arbisoft.com>
2018-09-11 16:34:26 +0500

    Commit September 11 - renamed project file names from Product* to ErnSting*

---
+++ ErnStingScrapping/ErnStingScrapper/items.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from scrapy import Item, Field
+
+
+class ErnStingItem(Item):
+    # define the fields for your item here like:
+    name = Field()
+    url = Field()
+    labels = Field()
+    detail = Field()
+    material = Field()
+    image_urls = Field()
+    colors = Field()
+    skus = Field()
