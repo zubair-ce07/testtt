1cab6aaa498ba04a694567712ee377166b64e73c
tehreem-sadat <42185078+tehreem-sadat@users.noreply.github.com>
2018-09-12 16:27:58 +0500

    Update orsay_spider.py

--- orsay/orsay_spider.py
+++ orsay/orsay_spider.py
@@ -8,127 +8,132 @@ from scrapy.spiders import CrawlSpider, Rule
 import scrapy
 import json
 
-
 MAX_DISPLAY = 72
-
 
 class OrsaySpider(CrawlSpider):
     """ This crawler will extract all products of website """
-    
+
     name = "orsay"
-    start_urls = ['http://www.orsay.com/de-de/produkte']
+    start_urls = ['http://www.orsay.com/de-de/produkte/']
     allowed_domains = ['www.orsay.com']
-    
-    sub_categories_extractor = LinkExtractor(
-        restrict_css="ul.refinement-category-list a")
+
     rules = (
-        Rule(sub_categories_extractor, callback='parse_categories', follow=True),
+        Rule(LinkExtractor(restrict_css="ul.refinement-category-list a"),
+             callback='parse_pages',
+            follow=True),
+        Rule(LinkExtractor(restrict_css=".product-image>a"), callback='parse_product')
     )
 
-    def parse_categories(self, response):
-        """this funbction will parse sub categories and their further pages"""
-        
+    def parse_pages(self, response):
+        """It is dealing with pagination"""
+
         max_items = self.get_max_items(response)
-        # check for more items or further pages and create request
         for items_count in range(MAX_DISPLAY, max_items, MAX_DISPLAY):
-            yield self.get_next_page_request(response, items_count)
-   
-    def parse_products(self, response):
-        """
-        this function will parse sub category page and extract
-        main information of all products
-        """
-        for product_div in response.css('div.product-tile'):
-            product_detail_json = self.get_product_detail_json(product_div)
-            if product_detail_json:
-                product = self.get_product(product_detail_json, product_div)
-                colors_url = self.get_colors_url(product_div)
-                for color in colors_url:
-                    color_url = color.css('a::attr(href)').extract_first()
-                    
-                    #if condition as there are some relative urls
-                    if "//" not in color_url:
-                        color_url = urljoin("http: // www.orsay.com", color_url)
-                    yield scrapy.Request(url=color_url,
-                                         callback=self.parse_color,
-                                         meta={'curr_item': product})
+            next_url = response.url + "?sz=" + str(items_count)
+            yield scrapy.Request(url=next_url, callback=self.parse)
 
-    def parse_color(self, response):
-        """this function is a parser for size and color"""
-        
-        product = response.meta['curr_item']
-        product["urls"].append(response.url)
-        product['images_url'].append(self.get_images_url(response))
-        json_data = self.get_product_detail_json(response)
-        if json_data:
-            self.get_sku(self, json_data, response)
-        yield product
+    def parse_product(self, response):
+        """It is a parser for product"""
+        for function in self.handle_product_variations(response):
+            yield function
 
-    def get_product(self, product_detail_selector, response):
-        """this function will form a product dict from data"""
-        
+    def handle_product_variations(self, response):
+        """This function will attach all sku to the product"""
+
+        product = response.meta.get('curr_item')
+        if not product:
+            product = self.get_product(response)
+            colors_url = self.get_colors_url(response)
+            response.meta["colors_url"] = colors_url
+            response.meta['curr_item'] = product
+        else:
+            json_data = self.get_product_variation_json(response)
+            if json_data:
+                self.get_sku(json_data, response, product)
+        colors_url = response.meta.get('colors_url')
+        if colors_url:
+            color = colors_url.pop(0)
+            yield scrapy.Request(url=color,
+                                 callback=self.handle_product_variations,
+                                 meta={'curr_item': product,
+                                       'colors_url':colors_url},
+                                 dont_filter=True)
+        else:
+            yield product
+
+    def get_product(self, response):
+        """this function will return product information from data"""
+
+        product_detail_json = self.get_product_detail(response)
         product = {}
-        product_detail_json = json.loads(product_detail_selector)
+        product_detail_json = json.loads(product_detail_json)
+        product['brand'] = 'Orsay'
+        product['gender'] = 'women'
         product["name"] = product_detail_json["name"]
         product["description"] = self.get_description(response)
         product["retailer_sku"] = product_detail_json["idListRef6"]
         product["category"] = product_detail_json["categoryName"]
         product["currency"] = product_detail_json["currency_code"]
         product["price"] = product_detail_json["grossPrice"]
-        product["urls"] = []
-        product['images_url']=[]
-        product['colors'] = []
+        product["urls"] = response.url
+        product['care'] = self.get_care(response)
+        product['images_url'] = self.get_images_url(response)
         product['skus'] = {}
         return product
 
     def get_sku(self, json_data, response, product):
+        """this function will return sku from data"""
+
         product_variation = json.loads(json_data)
         color_value = product_variation["color"]["value"]
-        color_name = product_variation["color"]["displayName"]
+        color_name = self.get_color_name(response)
         size_li = response.css('ul.size li')
         for li in size_li:
             sku = {}
-            sku["outOfStock"] = self.is_in_stock(response)
+            sku["outOfStock"] = self.is_in_stock(li)
             size = self.get_size(li)
             sku["color"] = color_name
             sku["price"] = self.get_price(response)
             sku["size"] = size
-            product['skus'][color_value + '_' + sku.size] = dict(sku)
-
-    def get_product_detail_json(self, product_div):
-        return product_div.css(
-            '::attr(data-product-details)').extract_first()
+            product['skus'][color_value + '_' + sku["size"]] = sku
 
     def get_description(self, response):
-        return response.css('div.product-info-title::text').extract()
+        description = response.css('.with-gutter::text').extract()
+        return ' '.join(description) if description else ""
 
     def get_images_url(self, response):
-        return response.css('img.primary-image::attr(src)').extract_first()
+        return response.css('.productthumbnail::attr(src)').extract()
 
-    def get_product_detail_json(self, response):
+    def get_product_variation_json(self, response):
         return response.css(
-            'div.product-variations::attr(data-attributes)').extract_first()
+            '::attr(data-attributes)').extract_first()
 
     def get_price(self, response):
-        response.css('span.price-sales::text').extract_first().strip('\n')
+        return response.css('span.price-sales::text').extract_first().strip('\n')
 
     def get_size(self, li_response):
         return (li_response.css(' a::text').extract_first()).strip('\n')
 
-    def get_colors_url(self, product_div):
-        product_div.css('ul.product-swatch-list li')
+    def get_colors_url(self, response):
+        return response.css('.color a::attr(href)').extract()
 
     def get_max_items(self, response):
         return int(response.css(
-            'div.load-more-progress::attr(data-max)').extract_first())
+            '.load-more-progress::attr(data-max)').extract_first())
 
-    def get_next_page_request(self, response, count):
-        next_url = f"{response.url}?sz={str(count)}"
-        return scrapy.Request(url=next_url, callback=self.parse_products)
+    def get_care(self, response):
+        return response.css('.product-info-block p::text').extract_first()
+
+    def get_product_detail(self, response):
+        return response.css(
+            '::attr(data-product-details)').extract_first()
+
+    def get_color_name(self, response):
+        return response.css('.selected-value::text').extract_first()
 
     def is_in_stock(self, li_response):
-        class_list = li_response.css('::attr(class)').extract()
+        class_list = li_response.css('::attr(class)').extract_first()
         if "unselectable" in class_list:
-            return  True
+            return True
         else:
             return False
