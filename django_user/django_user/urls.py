a11f6d03b68cc6cfe0413acb344e371b8c33b000
Muhammad Umar <umar@HassanKhan-Desktop.arbisoft.com>
2018-09-12 18:15:33 +0500

    Exclude empty urlpattern

--- django_user/django_user/urls.py
+++ django_user/django_user/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('users.urls')),
+    #path('', include('users.urls')),
     path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
