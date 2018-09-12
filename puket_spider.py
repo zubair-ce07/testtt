a81b3366d490c00331127927ca071ed84be4108e
Talqeen-Ahmad <talqeen.ahmad@arbisoft.com>
2018-09-12 19:50:32 +0500

    Changes Accommodated

--- puket_spider.py
+++ puket_spider.py
@@ -7,36 +7,31 @@ import re
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
-
 class PuketSpider(CrawlSpider):
-
-    json_storage = {}
+    raw_data = {}
     name = 'puket'
     allowed_domains = ['puket.com.br']
     start_urls = ['http://www.puket.com.br/']
     rules = (
-
         Rule(LinkExtractor(restrict_css=['.dropLink'])),
         Rule(LinkExtractor(restrict_css=['.shelf-url']), callback='parse_product'),
-
     )
-
     def parse_product(self, response):
         product = {}
-        self.json_storage = self.retrieve_data(response)
+        self.raw_data = self.retrieve_data(response)
         product['name'] = response.css('.nameProduct::text').extract_first()
         product['price'] = response.css('.val::attr(content)').extract_first()
-        product['currency'] = response.css('.priceProduct::attr(data-currency-symbol)')\
-            .extract_first()
-        product['composition'] = response.css('p.product-attribute-body::text')\
-            .extract_first().strip()
+        product['currency'] = response.css(
+            '.priceProduct::attr(data-currency-symbol)').extract_first()
+        product['composition'] = response.css(
+            'p.product-attribute-body::text').extract_first().strip()
         product['description'] = response.css('div.descProduct::text').extract_first().strip()
         product['image_url'] = response.css('.thumb-img::attr(src)').extract()
-        product['bread-crumb'] = '>'.join(response.css('.product-breadcrumb a::text').extract())
-        product['product_url'] = self.json_storage['product_url']
-        product['gender'] = self.json_storage['udasProd']['google_gender']
-        product['brand'] = self.json_storage['udasProd']['marca']
-        product['category'] = self.json_storage['udasProd']['categoria']
+        product['bread-crumb'] = response.css('.product-breadcrumb a::text').extract()
+        product['product_url'] = self.raw_data['product_url']
+        product['gender'] = self.raw_data['udasProd']['google_gender']
+        product['brand'] = self.raw_data['udasProd']['marca']
+        product['category'] = self.raw_data['udasProd']['categoria']
         product['skus'] = self.skus_formation()
         return product
 
@@ -56,12 +51,11 @@ class PuketSpider(CrawlSpider):
 
     def skus_formation(self):
         sku = {}
-        for item in self.json_storage['skus']:
+        for item in self.raw_data['skus']:
             sku_id = item['sku']
             sku[sku_id] = {
-                'currency': item['currency'],
-                'bestPrice': item['bestPrice'],
-                'Tamanho': item['udas']['TAMANHO'],
+                'Price': item['bestPrice'],
+                'size': item['udas']['TAMANHO'],
                 'color': item['udas']['CORES']
             }
         return sku
