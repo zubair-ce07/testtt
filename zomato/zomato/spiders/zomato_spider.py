eccfd5174f7a35c8c4eb1e7abc3c1c523ede0de8
Ali Nauman <ali60351@gmail.com>
2018-09-14 12:18:57 +0500

    Removed duplication of headers and unnecessary conditionals

--- zomato/zomato/spiders/zomato_spider.py
+++ zomato/zomato/spiders/zomato_spider.py
@@ -43,104 +43,40 @@ class Item(scrapy.Item):
 class ProductLoader(ItemLoader):
     default_output_processor = TakeFirst()
 
-    @staticmethod
-    def strip(value):
-        return value[0].strip()
-
 
 class ZomatoParser(scrapy.Spider):
-    name = "zomato"
-
+    name = "zomato_parse"
     special_categories = ['EveryDay Value Offers', 'Bestsellers']
 
     custom_settings = {
         'DOWNLOAD_DELAY': 1,
-        # 'COOKIES_ENABLED': False,
         'COOKIES_DEBUG': True,
         'ROBOTSTXT_OBEY': False,
         'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/63.0'
     }
 
-    def start_requests(self):
-        form_inputs = [
-            # Commercial Street BGL
-            # KFC
-            {
-                'res_id': '51824',
-                'case': 'getdata'
-            },
-            # Domino
-            {
-                'res_id': '51659',
-                'case': 'getdata'
-            },
-            # MAC
-            # Burger King
-            {
-                'res_id': '18088641',
-                'case': 'getdata'
-            },
-            # Velachery, Chennai
-            # KFC
-            {
-                'res_id': '65283',
-                'case': 'getdata'
-            },
-            # Domino
-            {
-                'res_id': '71522',
-                'case': 'getdata'
-            },
-            # MAC
-            {
-                'res_id': '65345',
-                'case': 'getdata'
-            },
-            # Burger King
-            {
-                'res_id': '18225860',
-                'case': 'getdata'
-            },
-            # Mylapore, Chennai
-            # KFC
-            {
-                'res_id': '65287',
-                'case': 'getdata'
-            },
-            # Domino
-            {
-                'res_id': '71594',
-                'case': 'getdata'
-            },
-            # MAC
-            {
-                'res_id': '18387727',
-                'case': 'getdata'
-            },
-            # Burger King
-            {
-                'res_id': '73090',
-                'case': 'getdata'
-            }
-        ]
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0',
+        'Accept': 'application/json',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Referer': 'https://www.zomato.com/',
+        'content-type': 'application/x-www-form-urlencoded',
+        'origin': 'https://www.zomato.com',
+        'Connection': 'keep-alive',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache'
+    }
 
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0',
-            'Accept': 'application/json',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Referer': 'https://www.zomato.com/',
-            'content-type': 'application/x-www-form-urlencoded',
-            'origin': 'https://www.zomato.com',
-            'Connection': 'keep-alive',
-            'Pragma': 'no-cache',
-            'Cache-Control': 'no-cache'
+    def start_requests(self):
+        form_data = {
+            'res_id': '51824',
+            'case': 'getdata'
         }
 
-        for form_data in form_inputs:
-            yield scrapy.FormRequest('https://www.zomato.com/php/o2_handler.php',
-                                     formdata=form_data,
-                                     callback=self.parse, headers=headers)
+        yield scrapy.FormRequest('https://www.zomato.com/php/o2_handler.php',
+                                 formdata=form_data,
+                                 callback=self.parse, headers=self.headers)
 
     def parse(self, response):
         raw_restaurant = json.loads(response.text)
@@ -156,63 +92,73 @@ class ZomatoParser(scrapy.Spider):
             'min_order': raw_restaurant['minOrder'],
             'average_rating': raw_restaurant['restaurant']['userRating']['aggregate_rating'],
             'num_of_ratings': raw_restaurant['restaurant']['userRating']['votes'],
-            'date_of_crawl': self.fetch_date_of_crawl(),
-            'search_pincode': response.meta['location'].pincode
+            'date_of_crawl': self.fetch_date_of_crawl()
         }
+
+        if response.meta.get('location'):
+            common_fields['search_pincode'] = response.meta['location'].pincode
 
         for menu in raw_restaurant['menus']:
             for category in menu['menu']['categories']:
                 for product in category['category']['items']:
                     if category['category']['name'] in self.special_categories:
-                        item_fields = {
-                            'category': menu['menu']['name'],
-                            'subcategory': product['item']['name'],
-                            'description': product['item']['desc'],
-                            'sku': product['item']['id'],
-                            'special_category': 'Y'
-                        }
-
-                        variants = self.get_item_variants(product, '', [])
-
-                        for variant in variants:
-                            item = ProductLoader(item=Item())
-
-                            item.add_value('title', variant['name'])
-                            item.add_value('price', variant['price'])
-                            item.add_value('veg', self.check_veg(product))
-
-                            for field in common_fields.keys():
-                                item.add_value(field, common_fields[field])
-
-                            for field in item_fields.keys():
-                                item.add_value(field, item_fields[field])
-
-                            yield item.load_item()
+                        special_items = self.generate_special_items(menu, product, common_fields)
+                        yield from (item for item in special_items)
                     else:
-                        item_fields = {
-                            'category': menu['menu']['name'],
-                            'subcategory': category['category']['name'],
-                            'title': product['item']['name'],
-                            'description': product['item']['desc'],
-                            'special_category': self.check_special_category(menu['menu']['name'])
-                        }
+                        items = self.generate_items(menu, product, common_fields, category)
+                        yield from (item for item in items)
 
-                        variants = self.get_item_variants(product, '', [])
+    def generate_items(self, menu, product, common_fields, category):
+        item_fields = {
+            'category': menu['menu']['name'],
+            'subcategory': category['category']['name'],
+            'title': product['item']['name'],
+            'description': product['item']['desc'],
+            'special_category': self.check_special_category(menu['menu']['name'])
+        }
 
-                        for variant in variants:
-                            item = ProductLoader(item=Item())
+        variants = self.get_item_variants(product, '', [])
 
-                            item.add_value('size', variant['size'])
-                            item.add_value('price', variant['price'])
-                            item.add_value('veg', self.check_veg(product))
+        for variant in variants:
+            item = ProductLoader(item=Item())
 
-                            for field in common_fields.keys():
-                                item.add_value(field, common_fields[field])
+            item.add_value('size', variant['size'])
+            item.add_value('price', variant['price'])
+            item.add_value('veg', self.check_veg(product))
 
-                            for field in item_fields.keys():
-                                item.add_value(field, item_fields[field])
+            for field in common_fields.keys():
+                item.add_value(field, common_fields[field])
 
-                            yield item.load_item()
+            for field in item_fields.keys():
+                item.add_value(field, item_fields[field])
+
+            yield item.load_item()
+
+    def generate_special_items(self, menu, product, common_fields):
+        item_fields = {
+            'category': menu['menu']['name'],
+            'subcategory': product['item']['name'],
+            'description': product['item']['desc'],
+            'sku': product['item']['id'],
+            'special_category': 'Y'
+        }
+
+        variants = self.get_item_variants(product, '', [])
+
+        for variant in variants:
+            item = ProductLoader(item=Item())
+
+            item.add_value('title', variant['name'])
+            item.add_value('price', variant['price'])
+            item.add_value('veg', self.check_veg(product))
+
+            for field in common_fields.keys():
+                item.add_value(field, common_fields[field])
+
+            for field in item_fields.keys():
+                item.add_value(field, item_fields[field])
+
+            yield item.load_item()
 
     @staticmethod
     def get_restaurant_pincode(raw_restaurant):
@@ -248,81 +194,54 @@ class ZomatoParser(scrapy.Spider):
         for group in product['item']['groups']:
             if group['group']['name'] in ['Sizes', 'Choose a type']:
                 for item in group['group']['items']:
-                    if size:
-                        self.get_item_variants(item, '|'.join([size, item['item']['name']]), items)
-                    else:
-                        self.get_item_variants(item, item['item']['name'], items)
+                    self.get_item_variants(item, item['item']['name'], items)
             else:
                 item = group['group']['items'][0]
-
-                if size:
-                    self.get_item_variants(item, '', items)
-                else:
-                    self.get_item_variants(item, '', items)
+                self.get_item_variants(item, '', items)
 
         return items
 
 
 class ZomatoCrawler(scrapy.Spider):
-    name = "zomato_crawl"
+    name = "zomato"
     parser = ZomatoParser()
     Location = namedtuple('Location', ['city', 'pincode'])
 
     custom_settings = {
         'DOWNLOAD_DELAY': 1.25,
-        # 'COOKIES_ENABLED': False,
-        # 'COOKIES_DEBUG': True,
         'ROBOTSTXT_OBEY': False,
         'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0'
     }
 
-    start_urls = ['https://www.zomato.com/']
-
     def start_requests(self):
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0',
-            'Accept': 'application/json',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Referer': 'https://www.zomato.com/',
-            'content-type': 'application/x-www-form-urlencoded',
-            'origin': 'https://www.zomato.com',
-            'Connection': 'keep-alive',
-            'Pragma': 'no-cache',
-            'Cache-Control': 'no-cache'
-        }
-
         yield scrapy.Request('https://www.zomato.com/php/o2_handler.php', callback=self.parse,
-                             headers=headers, method='POST')
+                             headers=self.parser.headers, method='POST')
 
     def parse(self, response):
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Connection': 'keep-alive',
-            'Upgrade-Insecure-Requests': '1'
-        }
-
         locations = self.read('locations.csv')
 
         for location in locations:
             url = 'https://www.zomato.com/php/liveSuggest.php?q={}&type=locality'.format(
                 location.city)
-            request = scrapy.Request(url, callback=self.parse_location, headers=headers)
+            request = scrapy.Request(url, callback=self.parse_location)
             request.meta['location'] = location
             yield request
 
     def parse_location(self, response):
-        location_type = re.findall(r'data-entity_type=\\"(.+?)\\"', response.text)[0]
-        location_id = re.findall(r'data-entity_id=\\"(\d+)\\"', response.text)[0]
+        location_type = re.findall(r'data-entity_type=\\"(.+?)\\"', response.text)
+        location_id = re.findall(r'data-entity_id=\\"(\d+)\\"', response.text)
 
         restaurants = ['Burger King', 'KFC', "Domino's Pizza", "Mc'Donalds"]
 
+        if not location_type:
+            logging.warning(response.meta['location'].city)
+            logging.warning('NO LOCATION FOUND')
+            logging.warning(response.text)
+            return
+
         for restaurant in restaurants:
             url = 'https://www.zomato.com/php/liveSuggest.php?type=keyword&q={}&entity_id={}' \
-                  '&entity_type={}'.format(restaurant, location_id, location_type)
+                  '&entity_type={}'.format(restaurant, location_id[0], location_type[0])
             request = scrapy.Request(url, callback=self.parse_chains)
             request.meta['location'] = response.meta['location']
             yield request
@@ -340,19 +259,6 @@ class ZomatoCrawler(scrapy.Spider):
         raw_listing = response.xpath('//script[contains(text(), "search_results")]').re(r'{.+}')[0]
         raw_listing = json.loads(raw_listing)
 
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0',
-            'Accept': 'application/json',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Referer': 'https://www.zomato.com/',
-            'content-type': 'application/x-www-form-urlencoded',
-            'origin': 'https://www.zomato.com',
-            'Connection': 'keep-alive',
-            'Pragma': 'no-cache',
-            'Cache-Control': 'no-cache'
-        }
-
         for restaurant_id in raw_listing['results']:
             form_data = {
                 'res_id': restaurant_id,
@@ -361,7 +267,7 @@ class ZomatoCrawler(scrapy.Spider):
 
             request = scrapy.FormRequest('https://www.zomato.com/php/o2_handler.php',
                                          formdata=form_data,
-                                         callback=self.parser.parse, headers=headers)
+                                         callback=self.parser.parse, headers=self.parser.headers)
             request.meta['location'] = response.meta['location']
             yield request
 
