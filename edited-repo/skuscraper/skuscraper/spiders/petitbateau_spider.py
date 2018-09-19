3d8640ff72bcb1466b95e290b56561b3975a0659
mumerfarooq000 <mfarooq@arbisoft.com>
2018-09-19 21:08:36 +0500

    changes done

--- edited-repo/skuscraper/skuscraper/spiders/petitbateau_spider.py
+++ edited-repo/skuscraper/skuscraper/spiders/petitbateau_spider.py
@@ -4,20 +4,23 @@ from urllib.parse import urljoin
 
 from scrapy import Request
 from scrapy.spiders import Rule
-from scrapy.linkextractors import LinkExtractor
 from scrapy.http import FormRequest
+from scrapy.linkextractors import LinkExtractor
 
-from .base import BaseCrawlSpider, BaseParseSpider, clean
+from .base import BaseCrawlSpider, BaseParseSpider, clean, soupify
+
 
 class Mixin:
     market = 'UK'
     retailer = 'petitbateau'
     allowed_domains = ['petit-bateau.co.uk']
-    start_urls = ['https://www.petit-bateau.co.uk/e-shop/bf-24/1/our-basics.html?refiningId=page%3D2%26nbResultsPerPage%3D18%26sorting%3DMY_SELECTION%26constraints%3Dzone%3AUK_%2F_category%3Abf-24']
+    start_urls = [
+        'https://www.petit-bateau.co.uk'
+    ]
 
 
 class PetitBateauParseSpider(BaseParseSpider, Mixin):
-    name = f"{Mixin.retailer}-parse"
+    name = Mixin.retailer + '-parse'
 
     description_css = '[itemprop="description"] ::text'
     care_css = '.maintenance-guide .tooltip ::text'
@@ -27,25 +30,22 @@ class PetitBateauParseSpider(BaseParseSpider, Mixin):
     headers = {'content-type': 'application/json; charset=UTF-8'}
 
     def parse(self, response):
-        pid = self.product_id(response)
-        garment = self.new_unique_garment(pid)
+        product_id = self.product_id(response)
+        garment = self.new_unique_garment(product_id)
 
         if not garment:
             return
-        
+
         self.boilerplate_normal(garment, response)
-        garment['gender'] = self.gender_lookup(self.product_name(response))
+
         garment['skus'] = {}
         garment['image_urls'] = []
-
-        garment['meta'] = {'requests_queue' : self.colour_requests(response)}
-        # garment['meta']['requests_queue'] += self.colour_requests(response)
+        garment['gender'] = self.product_gender(garment)
+        garment['meta'] = {'requests_queue': self.colour_requests(response)}
 
         return self.next_request_or_garment(garment)
 
     def parse_colours(self, response):
-        print("\033[92m-------------=-=-KSYS-=-=---------------------\033[0m")
-
         garment = response.meta['garment']
         garment['image_urls'] += self.image_urls(response)
         garment['meta']['requests_queue'] += self.size_requests(response)
@@ -54,42 +54,27 @@ class PetitBateauParseSpider(BaseParseSpider, Mixin):
 
     def parse_skus(self, response):
         garment = response.meta['garment']
-        skus = self.skus(response)
-        # print(skus)
         garment['skus'].update(self.skus(response))
-        # garment['skus'][raw_product['CurrentVariantIdWithoutCatalogName']] = self.skus(raw_product)
 
         return self.next_request_or_garment(garment)
 
-    def skus(self, response):
-        raw_product = json.loads(response.text)['d']['CurrentVariant']
-        money_strs = [raw_product['DisplayPrice'], raw_product['PrivilegePrice']]
-        skus = {}
-        sku = self.product_pricing_common(response=None, money_strs=money_strs)
-        sku['size'] = raw_product['Size']
+    def product_id(self, response):
+        css = '::attr(data-productid)'
+        return clean(response.css(css))[0]
 
-        if raw_product['OutOfStock']:
-            sku['out_of_stock'] = raw_product['OutOfStock']
-
-        skus[raw_product['CurrentVariantIdWithoutCatalogName']] = sku
-        return skus
-
-    def request_payloads(self, url):
-        pattern = r"\d+.*?\)"
-        ids = re.findall(pattern, url, re.M)[:2]
-        return self.variant_payload_t.format(*ids, "")
+    def product_gender(self, garment):
+        gender_soup = soupify([garment['name']] + garment['description']).lower()
+        return self.gender_lookup(gender_soup)
 
     def colour_requests(self, response):
-        # print("\033[92m-------------=-=-KSYS-=-=---------------------\033[0m")
-
         colour_requests = []
-        image_source = self.image_data_source(response)
+        image_source_url = self.image_source_url(response)
 
         for url in self.colour_urls(response):
-            request_body = self.request_payloads(url)
+            request_body = self.request_colour_body(url)
             request = FormRequest(url=self.variant_url, callback=self.parse_colours,
                                   method="POST", headers=self.headers, body=request_body)
-            request.meta['image_source'] = image_source
+            request.meta['image_source_url'] = image_source_url
             colour_requests.append(request)
 
         return colour_requests
@@ -97,11 +82,14 @@ class PetitBateauParseSpider(BaseParseSpider, Mixin):
     def image_urls(self, response):
         image_urls = []
         raw_product = json.loads(response.text)['d']['CurrentVariant']
+        image_source_url = response.meta['image_source_url']
 
         for image in raw_product:
+
             if "Image" not in image:
                 continue
-            image_url = urljoin(response.meta['image_source'], raw_product[image])
+
+            image_url = urljoin(image_source_url, raw_product[image])
             image_urls.append(image_url)
 
         return image_urls
@@ -116,44 +104,52 @@ class PetitBateauParseSpider(BaseParseSpider, Mixin):
         for size in sizes:
             request_body = self.variant_payload_t.format(product_id, variant_id, size)
             request = FormRequest(url=self.variant_url, callback=self.parse_skus,
-                                  method="POST", headers=self.headers, body=request_body,
-                                  dont_filter=True)
+                                  method="POST", headers=self.headers, body=request_body)
             size_requests.append(request)
 
         return size_requests
 
-    @staticmethod
-    def product_id(response):
-        css = '::attr(data-productid)'
-        return clean(response.css(css))[0]
+    def skus(self, response):
+        raw_product = json.loads(response.text)['d']['CurrentVariant']
+        money_strs = [raw_product['DisplayPrice'], raw_product['PrivilegePrice']]
+        skus = {}
+        sku = self.product_pricing_common(response=None, money_strs=money_strs)
+        sku['size'] = raw_product['Size']
 
-    @staticmethod
-    def product_name(response):
-        css = '.title-product ::text'
-        return clean(response.css(css))[0]
+        if raw_product['OutOfStock']:
+            sku['out_of_stock'] = raw_product['OutOfStock']
+
+        skus[raw_product['CurrentVariantIdWithoutCatalogName']] = sku
+        return skus
+
+    def request_colour_body(self, url):
+        pattern = r"\d+.*?\)"
+        ids = re.findall(pattern, url, re.M)[:2]
+        return self.variant_payload_t.format(*ids, "")
 
     def product_category(self, response):
         css = '.breadcrum a:nth-child(n+2) ::text'
-        return clean(response.css(css))
+        return clean(response.css(css))[1:]
 
-    @staticmethod
-    def colour_urls(response):
+    def colour_urls(self, response):
         css = '.list-colors ::attr(data-actionurl)'
         return clean(response.css(css))
 
-    @staticmethod
-    def image_data_source(response):
+    def image_source_url(self, response):
         css = '.visu-mobile ::attr(data-src)'
+        return clean(response.css(css))[0]
+
+    def product_name(self, response):
+        css = '.title-product ::text'
         return clean(response.css(css))[0]
 
 
 class PetitBateauCrawlSpider(BaseCrawlSpider, Mixin):
-    name = f"{Mixin.retailer}-crawl"
+    name = Mixin.retailer + '-crawl'
     parse_spider = PetitBateauParseSpider()
 
-    main_url_t = 'https://www.petit-bateau.co.uk{}'
     listing_url = 'https://www.petit-bateau.co.uk/WebServices/MerchService.asmx/Listing'
-    listing_payload_t = '{{"refiningId":"{}","category":"{}","allowVp":false,"filters":null}}'
+    request_body_t = '{{"refiningId":"{}","category":"{}","allowVp":false,"filters":null}}'
     headers = {'content-type': 'application/json; charset=UTF-8'}
 
     listing_css = [
@@ -164,40 +160,46 @@ class PetitBateauCrawlSpider(BaseCrawlSpider, Mixin):
         '.product-page-link'
     ]
 
+    deny_r = [
+        'soft-toys'
+    ]
+
     rules = (
-        # Rule(LinkExtractor(restrict_css=listing_css), callback='parse'),
+        Rule(LinkExtractor(restrict_css=listing_css), callback='parse'),
         Rule(LinkExtractor(restrict_css=product_css), callback='parse_item'),
-        )
+    )
 
-    # def parse(self, response):
-    #     yield from super().parse(response)
-    #
-    #     category = clean(response.css('::attr(data-categoryid)'))
-    #     refining_id = clean(response.css('.slider ::attr(data-refiningid)'))
-    #
-    #     if category and refining_id:
-    #         yield self.request_listing(category[0], refining_id[0])
-    #
-    # def parse_listings(self, response):
-    #     links = response.meta['links']
-    #     listing = json.loads(response.text)['d']
-    #
-    #     links += [Request(url=self.main_url_t.format(item['Url']), callback=self.parse_item)
-    #              for item in listing['Items']]
-    #
-    #     next_refining_id = listing['Pager']['NextPage']
-    #
-    #     if next_refining_id:
-    #         category = listing['Infos']['Id']
-    #         return  self.request_listing(category, next_refining_id, links)
-    #
-    #     return self.process_links(links)
-    #
-    # def request_listing(self, category, refining_id, links=[]):
-    #     request_body = self.listing_payload_t.format(refining_id, category)
-    #     request = FormRequest(url=self.listing_url, callback=self.parse_listings,
-    #                           method="POST", headers=self.headers,
-    #                           body=request_body)
-    #     request.meta['links'] = links
-    #     return request
+    def parse(self, response):
+        yield from super().parse(response)
 
+        category = clean(response.css('::attr(data-categoryid)'))
+        refining_id = clean(response.css('.slider ::attr(data-refiningid)'))
+
+        if category and refining_id:
+            yield self.request_listing(category[0], refining_id[0])
+
+    def parse_listings(self, response):
+        listing_requests = response.meta['listing_requests']
+        listing = json.loads(response.text)['d']
+
+        for item in listing['Items']:
+            url = urljoin(self.start_urls[0], item['Url'])
+            request = Request(url=url, callback=self.parse_item)
+            request.meta['trail'] = self.add_trail(response)
+            listing_requests.append(request)
+
+        next_refining_id = listing['Pager']['NextPage']
+
+        if next_refining_id:
+            category = listing['Infos']['Id']
+            return self.request_listing(category, next_refining_id, listing_requests)
+
+        return listing_requests
+
+    def request_listing(self, category, refining_id, listing_requests = []):
+        request_body = self.request_body_t.format(refining_id, category)
+        request = FormRequest(url=self.listing_url, callback=self.parse_listings,
+                              method="POST", headers=self.headers,
+                              body=request_body)
+        request.meta['listing_requests'] = listing_requests
+        return request
