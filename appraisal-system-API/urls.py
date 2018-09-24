21699e3b7299d553fbeaa27079334b65b9a46184
tehreem-sadat <42185078+tehreem-sadat@users.noreply.github.com>
2018-09-24 12:26:30 +0500

    update urls.py handle API with single class

--- appraisal-system-API/urls.py
+++ appraisal-system-API/urls.py
@@ -1,18 +1,17 @@
 from django.urls import path, include
-from django.views.generic import RedirectView
-from system.views import UserRudView, UserAPIView,\
-    AppraisalAPIView, AppraisalRudView
+from system.views import UserAPIView, AppraisalAPIView
 
 
 urlpatterns = [
+
     path('api/rest-auth/',
          include('rest_auth.urls'), name='login'),
     path('api/user/<int:pk>',
-         UserRudView.as_view(), name='user-rud'),
+         UserAPIView.as_view(), name='user-rud'),
     path('api/user/',
          UserAPIView.as_view(), name='user-list'),
     path('api/appraisals/',
          AppraisalAPIView.as_view(), name='appraisal-list'),
     path('api/appraisals/<int:pk>',
-         AppraisalRudView.as_view(), name='aprpaisal-rud')
+         AppraisalAPIView.as_view(), name='aprpaisal-rud'),
 ]
