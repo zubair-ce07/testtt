d751b3e774889e24f66a6b33a61461788055f34f
rehansharifqazi <rehan.sharif@arbisoft.com>
2018-09-24 15:53:24 +0500

    Iteration 5

--- Walmart/walmart.py
+++ Walmart/walmart.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 import scrapy
 from WALMART.items import WalmartLoader
@@ -10,128 +11,34 @@ class WalmartSpider(CrawlSpider):
     start_urls = ["https://www.walmart.com"]
 
     def parse(self, response):
-        data = self.parse_script(response)
-        clothing_link = data['header']['quimbyData']['global_header_ny']['headerZone1']['configs']['departments'][6][
+        data = self.parse_script(response, -3)
+        clothing_link = data['header']['quimbyData']['global_header_ny']['headerZone1']['configs']['departments'][5][
             'departments'][0]['department']['clickThrough']['value']
         clothing_link = response.urljoin(clothing_link)
         yield scrapy.Request(clothing_link, callback=self.parse_category_links)
 
     def parse_category_links(self, response):
-        required_script_xpath = '//script[contains(text(), "window.__WML_REDUX_INITIAL_STATE__")]/text()'
-        script = response.xpath(required_script_xpath).extract_first()
-        script = script.lstrip('var _setReduxState = function() {window.__WML_REDUX_INITIAL_STATE__ =')
-        script = script[:-1]
-        script = "{" + script
-        data = json.loads(script)
-        for index in range(6, 9):
-            yield scrapy.Request(data['header']['quimbyData']['global_header_ny']['headerZone1']['configs'][
-                                     'departments'][1]['departments'][index]['department']['clickThrough']['value'],
-                                 callback=self.parse_products)
+        data = self.parse_script(response, -1)
+        links = []
+        for index in range(2, 4):
+            links.append("https://www.walmart.com" + data['presoData']['modules']['top'][1]['configs']['navHeaders'][
+                index]['subHeaders'][3]['subnavHeader']['clickThrough']['value'])
+        links.append("https://www.walmart.com" + data['presoData']['modules']['top'][1]['configs']['navHeaders'][
+            4]['subHeaders'][0]['subnavHeader']['clickThrough']['value'])
+        for link in links:
+            yield scrapy.Request(link, callback=self.parse_products)
 
     def parse_products(self, response):
         product_links = response.xpath('//a[contains(@class, "product-title-link")]/@href').extract()
         for link in product_links:
-            yield scrapy.Request("https://www.walmart.com"+link, callback=self.parse_item)
-        data = self.parse_script(response)
+            yield scrapy.Request("https://www.walmart.com" + link, callback=self.parse_item)
+        data = self.parse_script(response, -3)
         canonical_next = data['preso']['pageMetadata'].get('canonicalNext')
         if canonical_next:
-            yield scrapy.Request(canonical_next, callback=self.parse_pages)
-
-    def parse_script(self, response):
-        required_script_xpath = '//script[contains(text(), "window.__WML_REDUX_INITIAL_STATE__")]/text()'
-        script = response.xpath(required_script_xpath).extract_first()
-        script = script.lstrip('var _setReduxState = function() {window.__WML_REDUX_INITIAL_STATE__ =')
-        script = script[:-3]
-        script = "{" + script
-        data = json.loads(script)
-        return data
-
-    def parse_product_id(self, data):
-        product_id = data['productBasicInfo']['selectedProductId']
-        return product_id
-
-    def parse_category(self, data):
-        product_id = self.parse_product_id(data)
-        general_info = data['product']['idmlMap'][product_id]['modules'].get('GeneralInfo')
-        if general_info:
-            category = general_info['category_path_name']['values'][0]
-            return category
-        else:
-            return "Unavailable"
-
-    def parse_description(self, data):
-        product_id = self.parse_product_id(data)
-        description = data['product']['idmlMap'][product_id]['modules']['LongDescription']['product_long_description']['values'][0]
-        return description
-
-    def parse_brand(self, data):
-        product_id = self.parse_product_id(data)
-        brand = data['product']['idmlMap'][product_id]['modules']['GeneralInfo']['brand']['displayValue']
-        return brand
-
-    def parse_retailer_sku(self, data):
-        retailer_sku = data['productId']
-        return retailer_sku
-
-    def parse_image_urls(self, data):
-        images = data['product']['images']
-        image_urls = []
-        for image in images:
-            for value in images[image]['assetSizeUrls'].values():
-                image_urls.append(value)
-        return image_urls
-
-    def get_size(self, variants):
-        size = variants.get('size')
-        if size:
-            size_cleaned = size.replace('size-', '')
-        else:
-            size = variants.get('clothing_size')
-            if size:
-                size_cleaned = size.replace('clothing_size-', '')
-            else:
-                size_cleaned = "generic"
-        return size_cleaned
-
-    def get_color(self, variants):
-        color = variants.get('actual_color')
-        if color:
-            color_cleaned = color.replace('actual_color-', '')
-        else:
-            color_cleaned = "multi-color"
-        return color_cleaned
-
-    def get_variants(self, products, product):
-        variants = products[product].get('variants')
-        if variants:
-            size_cleaned = self.get_size(variants)
-            color_cleaned = self.get_color(variants)
-            return size_cleaned, color_cleaned
-        else:
-            size_cleaned = "generic"
-            color_cleaned = "generic"
-            return size_cleaned, color_cleaned
-
-    def parse_skus(self, response, data):
-        products = data['product']['products']
-        price = response.xpath('//span[@class="price-characteristic"]/@content').extract_first()
-        currency = response.xpath('//span[@class="price-currency"]/text()').extract_first()
-        skus = {}
-        for product in products:
-            size_cleaned, color_cleaned = self.get_variants(products, product)
-            offer = products[product]['offers'][0]
-            color_size = "{}_{}".format(color_cleaned, size_cleaned)
-            skus[color_size] = {
-                "color": color_cleaned,
-                "size": size_cleaned,
-                "price": price,
-                "currency": currency,
-                "availability": data['product']['offers'][offer]['productAvailability']['availabilityStatus'],
-            }
-            return skus
+            yield scrapy.Request(canonical_next, callback=self.parse_products)
 
     def parse_item(self, response):
-        data = self.parse_script(response)
+        data = self.parse_script(response, -3)
         category = self.parse_category(data)
         description = self.parse_description(data)
         brand = self.parse_brand(data)
@@ -151,3 +58,81 @@ class WalmartSpider(CrawlSpider):
         loader.add_value("skus", skus)
         return loader.load_item()
 
+    def parse_script(self, response, index_script):
+        required_script_xpath = '//script[contains(text(), "window.__WML_REDUX_INITIAL_STATE__")]/text()'
+        script = response.xpath(required_script_xpath).extract_first()
+        script = script.lstrip('var _setReduxState = function() {window.__WML_REDUX_INITIAL_STATE__ =')
+        script = script[:index_script]
+        script = "{" + script
+        data = json.loads(script)
+        return data
+
+    def parse_product_id(self, data):
+        return data['productBasicInfo']['selectedProductId']
+
+    def parse_category(self, data):
+        product_id = self.parse_product_id(data)
+        general_info = data['product']['idmlMap'][product_id]['modules'].get('GeneralInfo')
+        if general_info:
+            category = general_info['category_path_name']['values'][0]
+            return category
+        return "Unavailable"
+
+    def parse_description(self, data):
+        product_id = self.parse_product_id(data)
+        return data['product']['idmlMap'][product_id]['modules']['LongDescription']['product_long_description']['values'][0]
+
+    def parse_brand(self, data):
+        product_id = self.parse_product_id(data)
+        return data['product']['idmlMap'][product_id]['modules']['GeneralInfo']['brand']['displayValue']
+
+    def parse_retailer_sku(self, data):
+        return data['productId']
+
+    def parse_image_urls(self, data):
+        images = data['product']['images']
+        image_urls = []
+        for image in images:
+            for value in images[image]['assetSizeUrls'].values():
+                image_urls.append(value)
+        return image_urls
+
+    def get_size(self, variants):
+        size = variants.get('size', variants.get('clothing_size', "generic"))
+        if "size" in size:
+            size = re.search('-(.*)', size)
+            size = size.group(1)
+        return size
+
+    def get_color(self, variants):
+        color = variants.get('actual_color', "multi-color")
+        if "actual" in color:
+            color = re.search('-(.*)', color)
+            color = color.group(1)
+        return color
+
+    def get_variants(self, products, product):
+        variants = products[product].get('variants')
+        if variants:
+            size_cleaned = self.get_size(variants)
+            color_cleaned = self.get_color(variants)
+            return size_cleaned, color_cleaned
+        return "generic", "generic"
+
+    def parse_skus(self, response, data):
+        products = data['product']['products']
+        price = response.xpath('//span[@class="price-characteristic"]/@content').extract_first()
+        currency = response.xpath('//span[@class="price-currency"]/text()').extract_first()
+        skus = {}
+        for product in products:
+            size_cleaned, color_cleaned = self.get_variants(products, product)
+            offer = products[product]['offers'][0]
+            color_size = "{}_{}".format(color_cleaned, size_cleaned)
+            skus[color_size] = {
+                "color": color_cleaned,
+                "size": size_cleaned,
+                "price": price,
+                "currency": currency,
+                "availability": data['product']['offers'][offer]['productAvailability']['availabilityStatus'],
+            }
+        return skus
