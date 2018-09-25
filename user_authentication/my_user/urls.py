4249072a7ff4d78add291cfac853bd0a1c879771
Abdullah Waheed <abdullah.waheed@arbisoft.com>
2018-09-25 11:00:47 +0500

    added docstrings and set some code

--- user_authentication/my_user/urls.py
+++ user_authentication/my_user/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path, reverse
-from . import views
+"""
+this module contains all the urls of this app
+"""
+from django.urls import path
 from django.contrib.auth.decorators import login_required
+from . import views
 
 app_name = 'my_user'
 
@@ -19,8 +22,10 @@ urlpatterns = [
     path('rest/login/', views.RestUserLoginFormView.as_view(), name='rest_login'),
     path('rest/register/', views.RestUserFormView.as_view(), name='rest_register'),
     path('rest/logout/', views.rest_logout_view, name='rest_logout'),
-    path('rest/edit-profile/', login_required(views.RestUserEditFormView.as_view(), login_url='/rest/login'),
+    path('rest/edit-profile/',
+         login_required(views.RestUserEditFormView.as_view(), login_url='/rest/login'),
          name='rest_edit'),
-    path('rest/change-password/', login_required(views.RestUserEditPassword.as_view(), login_url='/rest/login'),
+    path('rest/change-password/',
+         login_required(views.RestUserEditPassword.as_view(), login_url='/rest/login'),
          name='rest_change_password'),
 ]
--- user_authentication/my_user/views.py
+++ user_authentication/my_user/views.py
@@ -1,33 +1,46 @@
+"""
+this module contains the views of this django app
+"""
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm, \
-    PasswordChangeForm
+from django.views.generic import View, TemplateView
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .forms import UserEditForm
-from .serializer import UserRegisterSerializer, UserLoginSerializer, UserEditSerializer, \
+from .serializers import UserRegisterSerializer, UserLoginSerializer, UserEditSerializer, \
     UserEditPasswordSerializer
 
 
-class IndexView(View):
+class IndexView(TemplateView):
+    """
+    this is TemplateView of Index page
+    """
     template_name = 'user/index.html'
-
-    def get(self, request):
-        return render(request, self.template_name)
 
 
 class UserFormView(View):
+    """
+     it is user registation page's view
+    """
     form_class = UserCreationForm
     template_name = 'user/registration_form.html'
 
     def get(self, request):
+        """
+        if request is of type get, it show the form
+        """
         form = self.form_class(None)
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
+        """
+        if request is of type post, it gets a form, verify it and save the user
+        :param request:
+        :return:
+        """
         form = self.form_class(request.POST)
 
         if form.is_valid():
@@ -46,21 +59,32 @@ class UserFormView(View):
                 if user.is_active:
                     login(request, user)
                     return redirect('my_user:index')
-            else:
-                return
 
         return render(request, self.template_name, {'form': form})
 
 
 class UserLoginFormView(View):
+    """
+    it is login page's view
+    """
     form_class = AuthenticationForm
     template_name = 'user/login_form.html'
 
     def get(self, request):
+        """
+        if request is of type get, it show the login form
+        :param request:
+        :return:
+        """
         form = self.form_class(None)
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
+        """
+         if request is of type post, it gets form data, verify it and login the user
+        :param request:
+        :return:
+        """
         form = self.form_class(data=request.POST)
 
         if form.is_valid():
@@ -75,27 +99,43 @@ class UserLoginFormView(View):
             else:
                 error = "Please enter a valid username or password"
                 return render(request, self.template_name, {'form': form, 'error_message': error})
-        else:
-            return render(request, self.template_name, {'form': form})
 
         return render(request, self.template_name, {'form': form})
 
 
 def logout_view(request):
+    """
+    it logs out the user
+    :param request:
+    :return:
+    """
     logout(request)
     return redirect('my_user:index')
 
 
 class UserEditFormView(View):
+    """
+    it is user edit profile page's view
+    """
     form_class = UserEditForm
 
     template_name = 'user/edit_form.html'
 
     def get(self, request):
+        """
+        if request is of type get, it show the edit form
+        :param request:
+        :return:
+        """
         form = self.form_class(instance=request.user, )
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
+        """
+        if request is of type post, it gets form data, verify it and update the user
+        :param request:
+        :return:
+        """
         # to save the current instance and not create a new instance
         form = self.form_class(request.POST, instance=request.user)
         if form.is_valid():
@@ -106,14 +146,27 @@ class UserEditFormView(View):
 
 
 class UserEditPassword(View):
+    """
+    it is user edit password page's view
+    """
     form_class = PasswordChangeForm
     template_name = 'user/change_password.html'
 
     def get(self, request):
+        """
+        if request is of type get, it show the password change form
+        :param request:
+        :return:
+        """
         form = self.form_class(user=request.user)
         return render(request, self.template_name, {'form': form})
 
     def post(self, request):
+        """
+        if request is of type post, it gets form data, verify it and update the password
+        :param request:
+        :return:
+        """
         # to save the current instance and not create a new instance
         form = self.form_class(user=request.user, data=request.POST)
         if form.is_valid():
@@ -122,28 +175,51 @@ class UserEditPassword(View):
 
         return render(request, self.template_name, {'form': form})
 
+
 # ##################################################################################################
 # Below are the same views with REST framework
 class RestIndexView(APIView):
+    """
+    this is a View of Index page with DRF
+    """
     template_name = 'user/index.html'
     renderer_classes = (TemplateHTMLRenderer,)
 
     def get(self, request):
-        return Response({'is_rest':True})
+        """
+        it displays index page with DRF
+        :param request:
+        :return:
+        """
+        return Response({'is_rest': True})
 
 
 class RestUserFormView(APIView):
+    """
+    it is user registation page's view with DRF
+    """
     renderer_classes = (TemplateHTMLRenderer,)
     template_name = 'user/registration_form.html'
 
     def get(self, request):
+        """
+        if request is of type get, it shows the registration form but this form is serializer form
+         of DRF
+        :param request:
+        :return:
+        """
         serializer = UserRegisterSerializer(None)
-        return Response({'serializer': serializer, 'is_rest':True})
+        return Response({'serializer': serializer, 'is_rest': True})
 
     def post(self, request):
+        """
+        if request is of type post, it gets the serializer form, verify it and save the user
+        :param request:
+        :return:
+        """
         serializer = UserRegisterSerializer(data=request.data)
         if not serializer.is_valid():
-            return Response({'serializer': serializer, 'is_rest':True})
+            return Response({'serializer': serializer, 'is_rest': True})
 
         serializer.save()
         username = request.data['username']
@@ -157,19 +233,32 @@ class RestUserFormView(APIView):
 
 
 class RestUserLoginFormView(APIView):
-    # form_class = AuthenticationForm
+    """
+    it is login page's view with DRF
+    """
     template_name = 'user/login_form.html'
     renderer_classes = (TemplateHTMLRenderer,)
 
     def get(self, request):
+        """
+        if request is of type get, it show the login form but this form is serializer form
+         of DRF
+        :param request:
+        :return:
+        """
         serializer = UserLoginSerializer(None)
-        return Response({'serializer': serializer, 'is_rest':True})
+        return Response({'serializer': serializer, 'is_rest': True})
 
     def post(self, request):
+        """
+        if request is of type post, it gets form data, verify it and login the user
+        :param request:
+        :return:
+        """
         serializer = UserLoginSerializer(data=request.data)
         if not serializer.is_valid():
             error = "Please enter a valid username or password"
-            return Response({'serializer': serializer, 'error_message': error, 'is_rest':True})
+            return Response({'serializer': serializer, 'error_message': error, 'is_rest': True})
         username = request.data['username']
         password = request.data['password']
         user = authenticate(username=username, password=password)
@@ -180,46 +269,79 @@ class RestUserLoginFormView(APIView):
                 return redirect('my_user:rest_index')
             else:
                 error = "Your account is inactive. Kindly contact the admin"
-                return Response({'serializer': serializer, 'error_message': error, 'is_rest':True})
+                return Response({'serializer': serializer, 'error_message': error, 'is_rest': True})
         else:
             error = "Please enter a valid username or password"
-            return Response({'serializer': serializer, 'error_message': error, 'is_rest':True})
+            return Response({'serializer': serializer, 'error_message': error, 'is_rest': True})
 
 
 def rest_logout_view(request):
+    """
+    it logs out the user and redirect user to RestIndex
+    :param request:
+    :return:
+    """
     logout(request)
     return redirect('my_user:rest_index')
 
 
 class RestUserEditFormView(APIView):
+    """
+    it is user edit profile page's view with DRF
+    """
     renderer_classes = (TemplateHTMLRenderer,)
     template_name = 'user/edit_form.html'
 
     def get(self, request):
+        """
+        if request is of type get, it show the edit form but this form is serializer form
+         of DRF
+        :param request:
+        :return:
+        """
         serializer = UserEditSerializer(request.user)
-        return Response({'serializer': serializer, 'is_rest':True})
+        return Response({'serializer': serializer, 'is_rest': True})
 
     def post(self, request):
+        """
+        if request is of type post, it gets form data, verify it and update the user
+        :param request:
+        :return:
+        """
         serializer = UserEditSerializer(request.user, data=request.data)
         if not serializer.is_valid():
-            return Response({'serializer': serializer, 'is_rest':True})
+            return Response({'serializer': serializer, 'is_rest': True})
 
         serializer.save()
         return redirect('my_user:rest_index')
 
 
 class RestUserEditPassword(APIView):
+    """
+    it is user edit password page's view with DRF
+    """
     renderer_classes = (TemplateHTMLRenderer,)
     template_name = 'user/change_password.html'
 
     def get(self, request):
+        """
+        if request is of type get, it show the password change form but this form is serializer form
+         of DRF
+        :param request:
+        :return:
+        """
         serializer = UserEditPasswordSerializer(None)
-        return Response({'serializer': serializer, 'is_rest':True})
+        return Response({'serializer': serializer, 'is_rest': True})
 
     def post(self, request):
+        """
+        if request is of type post, it gets form data, verify it and update the password
+        :param request:
+        :return:
+        """
         serializer = UserEditPasswordSerializer(request.user, data=request.data)
         if not serializer.is_valid():
-            return Response({'serializer': serializer, 'is_rest':True})
+            return Response({'serializer': serializer, 'is_rest': True})
 
         serializer.save()
         return redirect('my_user:rest_index')
