edbaa074c1b23cb6458f230721052d8f11f54b2d
tehreem-sadat <42185078+tehreem-sadat@users.noreply.github.com>
2018-09-25 13:18:47 +0500

    change (Appraisal -> Competencies)

--- appraisal-system-API/views.py
+++ appraisal-system-API/views.py
@@ -14,20 +14,21 @@ User = get_user_model()
 
 
 class AppraisalAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
     lookup_field = 'pk'
-    serializer_class = CompetenceSerializer
+    serializer_class = AppraisalSerializer
 
     def get(self, request, format=None, pk=None):
         if pk is None:
-            serializer = CompetenceSerializer(self.get_queryset(), many=True)
+            serializer =  AppraisalSerializer(self.get_queryset(), many=True)
             return Response(serializer.data)
         else:
-            competence = self.get_object(pk)
-            serializer = CompetenceSerializer(competence)
+            appraisal = self.get_object(pk)
+            serializer =  AppraisalSerializer(appraisal)
             return Response(serializer.data)
 
     def post(self, request, format=None, pk=None):
-        serializer = CompetenceSerializer(
+        serializer =  AppraisalSerializer(
             data=request.data, context={'request': request})
         if serializer.is_valid(raise_exception=ValueError):
             serializer.save()
@@ -36,36 +37,36 @@ class AppraisalAPIView(APIView):
             serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, pk, format=None):
-        competence = self.get_object(pk)
-        serializer = CompetenceSerializer(
-            competence, data=request.data, context={'request': request})
+        appraisal = self.get_object(pk)
+        serializer =  AppraisalSerializer(appraisal, data=request.data,
+                                          context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk, format=None):
-        competence = self.get_object(pk)
-        competence.appraisal.delete()
+        appraisal = self.get_object(pk)
+        appraisal.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
     def get_queryset(self):
-        qs = Competence.objects.all()
+        qs = Appraisal.objects.all()
         if self.request.user.user_level == "employee":
-            return qs.filter(appraisal__to_user=self.request.user.pk)
+            return qs.filter(to_user=self.request.user.pk)
         if self.request.user.user_level == "manager":
-            return qs.filter(appraisal__from_user=self.request.user.pk)
+            return qs.filter(from_user=self.request.user.pk)
         return qs
 
     def get_object(self, pk):
         try:
             appraisal = Appraisal.objects.get(pk=pk)
-            return appraisal.competence_set.all()[0]
+            return appraisal
         except ObjectDoesNotExist:
             raise serializers.ValidationError("Object does not exist.")
 
-
 class UserAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
     lookup_field = 'pk'
     serializer_class = UserSerializer
 
@@ -80,16 +81,16 @@ class UserAPIView(APIView):
 
     def put(self, request, pk, format=None):
         user = self.get_object(pk)
-        serializer = UserSerializer(
-            user, data=request.data, context={'request': request})
+        serializer = UserSerializer(user, data=request.data,
+                                    context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def post(self, request, format=None, pk=None):
-        serializer = UserSerializer(
-            data=request.data, context={'request': request})
+        serializer = UserSerializer(data=request.data,
+                                    context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -111,3 +112,13 @@ class UserAPIView(APIView):
         if self.request._user.user_level != "admin":
             return qs.filter(report_to=self.request._user.pk)
         return qs
+
+class SignupAPI(APIView):
+    serializer_class = SignUpSerializer
+
+    def post(self, request, format=None, pk=None):
+        serializer = SignUpSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
