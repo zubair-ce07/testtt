8cbf605cb21de9f480f3739d86c9b8229cb3dccc
Hussnain Rehman <31269363+hussnainrehman@users.noreply.github.com>
2018-09-26 15:09:36 +0500

    added mixins and refactored the code as asked

--- icebreaker_spider.py
+++ icebreaker_spider.py
@@ -4,34 +4,125 @@ from scrapy.http import Request
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Rule
 
-from .base import BaseParseSpider, BaseCrawlSpider, clean
+from .base import BaseParseSpider, BaseCrawlSpider, clean, Gender
 
 
-class IceBreakerMixin:
-    retailer = 'ice-breaker'
+class Mixin:
+    retailer = 'icebreaker'
+    allowed_domains = ['icebreaker.com']
+    default_brand = "ICEBREAKER"
+    cookie_value = ""
 
+    def process_request(self, request):
+        request.headers['Cookie'] = self.cookie_value
+        request.meta['dont_merge_cookies'] = True
+        return request
+
+
+class MixinUK(Mixin):
+    retailer = Mixin.retailer + '-uk'
+    market = 'UK'
     allowed_domains = ['uk.icebreaker.com']
     start_urls = [
-        'https://uk.icebreaker.com/en/home',
+        'https://uk.icebreaker.com/',
     ]
 
-    default_brand = "ICEBREAKER"
-    market = "UK"
+
+class MixinUS(Mixin):
+    retailer = Mixin.retailer + '-us'
+    market = 'US'
+    start_urls = [
+        'https://us.icebreaker.com/',
+    ]
 
 
-class CommonParseSpider(BaseParseSpider):
+class MixinDE(Mixin):
+    retailer = Mixin.retailer + '-de'
+    market = 'DE'
+    allowed_domains = ['eu.icebreaker.com']
+    start_urls = [
+        'https://eu.icebreaker.com/de/',
+    ]
+    cookie_value = 'IBPreferred=DE|EU|de|EUR'
+
+
+class MixinCA(Mixin):
+    retailer = Mixin.retailer + '-ca'
+    market = 'CA'
+    allowed_domains = ['ca.icebreaker.com']
+    start_urls = [
+        'https://ca.icebreaker.com/',
+    ]
+
+
+class MixinDK(Mixin):
+    retailer = Mixin.retailer + '-dk'
+    market = 'DK'
+    cookie_value = "IBPreferred=DK|EU|en|DKK"
+    allowed_domains = ['eu.icebreaker.com']
+    start_urls = [
+        'https://eu.icebreaker.com/en/',
+    ]
+
+
+class MixinFR(Mixin):
+    retailer = Mixin.retailer + '-fr'
+    market = 'FR'
+    cookie_value = "IBPreferred=FR|EU|fr_FR|EUR"
+    allowed_domains = ['eu.icebreaker.com']
+    start_urls = [
+        'https://eu.icebreaker.com/fr_FR/',
+    ]
+
+
+class MixinNZ(Mixin):
+    retailer = Mixin.retailer + '-nz'
+    market = 'NZ'
+    allowed_domains = ['nz.icebreaker.com']
+    start_urls = [
+        'https://nz.icebreaker.com/',
+    ]
+
+
+class MixinPL(Mixin):
+    retailer = Mixin.retailer + '-pl'
+    market = 'PL'
+    cookie_value = 'IBPreferred=PL|EU|en|PLN'
+    allowed_domains = ['eu.icebreaker.com']
+    start_urls = [
+        'https://eu.icebreaker.com/en/',
+    ]
+
+
+class MixinSE(Mixin):
+    retailer = Mixin.retailer + '-se'
+    market = 'SE'
+    cookie_value = 'IBPreferred=SE|EU|en|SEK'
+    allowed_domains = ['eu.icebreaker.com']
+    start_urls = [
+        'https://eu.icebreaker.com/en/',
+    ]
+
+
+class MixinEU(Mixin):
+    retailer = Mixin.retailer + '-eu'
+    market = 'EU'
+    allowed_domains = ['eu.icebreaker.com']
+    start_urls = [
+        'https://eu.icebreaker.com/en/',
+    ]
+
+
+class IcebreakerParseSpider(BaseParseSpider):
     price_css = '.product-price ::text'
-    details_css = '.product-box::attr(data-selectedproduct)'
 
     def parse(self, response):
         raw_product = self.raw_product(response)
-        pid = raw_product['id']
-
-        garment = self.new_unique_garment(pid)
+        garment = self.new_unique_garment(self.product_id(raw_product))
 
         if not garment:
             return
-
+        
         self.boilerplate_normal(garment, response)
         garment["gender"] = self.product_gender(raw_product, response)
         garment["skus"] = {}
@@ -42,7 +133,7 @@ class CommonParseSpider(BaseParseSpider):
 
     def color_requests(self, response):
         color_urls = clean(response.css('ul.color a::attr(href)'))
-        return [Request(f"{url}&format=ajax", callback=self.parse_colors) for url in color_urls]
+        return [self.process_request(Request(url=url, callback=self.parse_colors)) for url in color_urls]
 
     def parse_colors(self, response):
         garment = response.meta["garment"]
@@ -58,13 +149,14 @@ class CommonParseSpider(BaseParseSpider):
         return self.next_request_or_garment(garment)
 
     def size_requests(self, response):
-        css = 'ul.size a.swatchanchor ::attr(href)'
-        sizes = response.css(css).extract()
-        return [Request(url=f"{size_url}&format=ajax", callback=self.parse_sizes) for size_url in sizes]
+        sizes = clean(response.css('.size .swatchanchor ::attr(href)'))
+        return [self.process_request(Request(url=size_url, dont_filter=True, callback=self.parse_sizes))
+                for size_url in sizes]
 
     def parse_sizes(self, response):
         garment = response.meta["garment"]
         garment["skus"].update(self.skus(response))
+
         return self.next_request_or_garment(garment)
 
     def skus(self, response):
@@ -80,17 +172,19 @@ class CommonParseSpider(BaseParseSpider):
 
         return {f'{sku["colour"]}_{sku["size"]}': sku}
 
+    def product_id(self, raw_product):
+        return raw_product['id']
+
     def product_images(self, response):
         css = '[class="thumbnail-tile"]::attr(href),.primary-image::attr(src)'
         return clean(response.css(css))
 
     def raw_product(self, response):
-        json_text = clean(response.css(self.details_css))[0]
+        json_text = clean(response.css('.product-box::attr(data-selectedproduct)'))[0]
         return json.loads(json_text)
 
     def product_name(self, response):
-        name = clean(response.css(self.details_css))[0]
-        return json.loads(name).get("name")
+        return self.raw_product(response)["name"]
 
     def product_category(self, response):
         return clean(response.css('.breadcrumb a::text'))
@@ -100,40 +194,121 @@ class CommonParseSpider(BaseParseSpider):
         categories = self.product_category(response)
         soup = f"{' '.join(categories)} {raw_gender}"
 
-        return self.gender_lookup(soup) or "unisex-adults"
+        return self.gender_lookup(soup) or Gender.ADULTS.value
 
-    def product_description(self, response, **kwargs):
-        css_1 = ".description ::text"
-        css_2 = ".product-bullets span ::text,.product-bullets a ::text"
-        raw_description = clean(response.css(css_1))
-        raw_description += [' '.join(clean(response.css(css_2)))]
+    def raw_description(self, response, **kwargs):
+        raw_desc_css = ".description ::text"
+        key_css = '.catlabel::text'
+        value_css = '.bullet-info::text'
+        raw_description = clean(response.css(raw_desc_css))
+
+        raw_description += [f"{clean(d_sel.css(key_css))[0]}{clean(d_sel.css(value_css))[0]}"
+                            for d_sel in response.css('.attr') if d_sel.css(value_css)]
 
         return raw_description
 
-    def product_care(self, response, **kwargs):
-        care_css = '.attributes-box ::text'
-        return [' '.join(clean(response.css(care_css)))]
 
-
-class CommonCrawlSpider(BaseCrawlSpider):
+class IcebreakerCrawlSpider(BaseCrawlSpider):
     listings_css = [
         ".nav-item .sub-nav-column-container",
         ".infinite-scroll-placeholder"
-
     ]
+                
     product_css = '.product-image'
 
     rules = (
         Rule(LinkExtractor(restrict_css=listings_css, attrs=["href", "data-grid-url"]),
-             callback='parse'),
-        Rule(LinkExtractor(restrict_css=product_css), callback='parse_item')
+             process_request='process_request', callback='parse'),
+        Rule(LinkExtractor(restrict_css=product_css), process_request='process_request', callback='parse_item')
     )
 
 
-class IceBreakerParseSpider(CommonParseSpider, IceBreakerMixin):
-    name = IceBreakerMixin.retailer + '-parse'
+class IceBreakerParseSpiderUK(IcebreakerParseSpider, MixinUK):
+    name = MixinUK.retailer + '-parse'
 
 
-class IceBreakerCrawlSpider(CommonCrawlSpider, IceBreakerMixin):
-    name = IceBreakerMixin.retailer + '-crawl'
-    parse_spider = IceBreakerParseSpider()
+class IceBreakerCrawlSpiderUK(IcebreakerCrawlSpider, MixinUK):
+    name = MixinUK.retailer + '-crawl'
+    parse_spider = IceBreakerParseSpiderUK()
+
+
+class IceBreakerParseSpiderUS(IcebreakerParseSpider, MixinUS):
+    name = MixinUS.retailer + '-parse'
+
+
+class IceBreakerCrawlSpiderUS(IcebreakerCrawlSpider, MixinUS):
+    name = MixinUS.retailer + '-crawl'
+    parse_spider = IceBreakerParseSpiderUS()
+
+
+class IceBreakerParseSpiderDE(IcebreakerParseSpider, MixinDE):
+    name = MixinDE.retailer + '-parse'
+
+
+class IceBreakerCrawlSpiderDE(IcebreakerCrawlSpider, MixinDE):
+    name = MixinDE.retailer + '-crawl'
+    parse_spider = IceBreakerParseSpiderDE()
+
+
+class IceBreakerParseSpiderCA(IcebreakerParseSpider, MixinCA):
+    name = MixinCA.retailer + '-parse'
+
+
+class IceBreakerCrawlSpiderCA(IcebreakerCrawlSpider, MixinCA):
+    name = MixinCA.retailer + '-crawl'
+    parse_spider = IceBreakerParseSpiderCA()
+
+
+class IceBreakerParseSpiderDK(IcebreakerParseSpider, MixinDK):
+    name = MixinDK.retailer + '-parse'
+
+
+class IceBreakerCrawlSpiderDK(IcebreakerCrawlSpider, MixinDK):
+    name = MixinDK.retailer + '-crawl'
+    parse_spider = IceBreakerParseSpiderDK()
+
+
+class IceBreakerParseSpiderFR(IcebreakerParseSpider, MixinFR):
+    name = MixinFR.retailer + '-parse'
+
+
+class IceBreakerCrawlSpiderFR(IcebreakerCrawlSpider, MixinFR):
+    name = MixinFR.retailer + '-crawl'
+    parse_spider = IceBreakerParseSpiderFR()
+
+
+class IceBreakerParseSpiderNZ(IcebreakerParseSpider, MixinNZ):
+    name = MixinNZ.retailer + '-parse'
+
+
+class IceBreakerCrawlSpiderNZ(IcebreakerCrawlSpider, MixinNZ):
+    name = MixinNZ.retailer + '-crawl'
+    parse_spider = IceBreakerParseSpiderNZ()
+
+
+class IceBreakerParseSpiderPL(IcebreakerParseSpider, MixinPL):
+    name = MixinPL.retailer + '-parse'
+
+
+class IceBreakerCrawlSpiderPL(IcebreakerCrawlSpider, MixinPL):
+    name = MixinPL.retailer + '-crawl'
+    parse_spider = IceBreakerParseSpiderPL()
+
+
+class IceBreakerParseSpiderSE(IcebreakerParseSpider, MixinSE):
+    name = MixinSE.retailer + '-parse'
+
+
+class IceBreakerCrawlSpiderSE(IcebreakerCrawlSpider, MixinSE):
+    name = MixinSE.retailer + '-crawl'
+    parse_spider = IceBreakerParseSpiderSE()
+
+
+class IceBreakerParseSpiderEU(IcebreakerParseSpider, MixinEU):
+    name = MixinEU.retailer + '-parse'
+
+
+class IceBreakerCrawlSpiderEU(IcebreakerCrawlSpider, MixinEU):
+    name = MixinEU.retailer + '-crawl'
+    parse_spider = IceBreakerParseSpiderEU()
+                
