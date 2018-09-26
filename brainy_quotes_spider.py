740ea1046e35788623ff29ecec33d035decbf94a
abeermahboob1 <abeer.mahboob@arbisoft.com>
2018-09-26 15:08:22 +0500

    2nd iteration of brainyquotes

--- brainy_quotes_spider.py
+++ brainy_quotes_spider.py
@@ -1,41 +1,44 @@
-import scrapy
 import json
+from copy import deepcopy
+
+import scrapy
 
 
 class BrainyQuotesSpider(scrapy.Spider):
     name = "BrainyQuotes"
     start_urls = [
-         'https://www.brainyquote.com/topics/motivational'
-     ]
+        'https://www.brainyquote.com/topics'
+    ]
     request_url = 'https://www.brainyquote.com/api/inf'
-
-    page = 1
     request_params = {
         'ab': 'a',
         'fdd': 'd',
-        'id': 't:132622',
         'langc': 'en',
         'm': 0,
-        'pg': 1,
         'typ': 'topic',
-        'v': '8.4.0b:2951607',
-        'vid': '7b363d749b4c7c684ace871c8a75f8e6'
     }
 
     def parse(self, response):
-        for quotes in response.css('div.m-brick'):
-            yield {
-                    'Quote': quotes.css('div.clearfix a.b-qt::text').extract_first(),
-                    'Author': quotes.css('div.clearfix a.oncl_a::text').extract_first(),
-                    'Tags': quotes.css('div.kw-box a.oncl_list_kc::text').extract(),
-                    'Shareable links': quotes.css('div.sh-box a.fa-stack::attr(href)').extract(),
-                    'img_url': quotes.css('div.qti-listm img.zoomc::attr(data-img-url)').extract_first(),
-            }
+        topics = response.css('div.bqLn')
+        for topic in topics:
+            topic_url = response.urljoin(topic.css('a.topicIndexChicklet::attr(href)').extract_first())
+            yield scrapy.Request(topic_url, callback=self.parse_topics)
 
-        self.page += 1
-        self.request_params['pg'] = self.page
-        yield scrapy.Request(self.request_url, callback=self.parse_scrolled_pages, method='POST', body=json.dumps(self.request_params),
-                             headers={'Content-Type': 'application/json'})
+    def parse_topics(self, response):
+        v = response.xpath("//meta[@property='ver']/@content").extract_first()
+        vid = response.xpath('//script[@type="text/javascript"]').re_first("VID='(.*?)';")
+        id = response.xpath('//script[@type="text/javascript"]').re_first('PG_DM_ID="(.+)";')
+        last_page = response.xpath('//script[@type="text/javascript"]').re_first('LPAGE =(.+);')
+        request_params = deepcopy(self.request_params)
+        request_params.update({
+            'v': v,
+            'vid': vid,
+            'id': id,
+        })
+        for page_no in range(1, int(last_page)):
+            request_params['pg'] = page_no
+            yield scrapy.Request(self.request_url, callback=self.parse_scrolled_pages, method='POST', body=json.dumps(request_params),
+                                 headers={'Content-Type': 'application/json'})
 
     def parse_scrolled_pages(self, response):
         json_response = json.loads(response.body_as_unicode())
@@ -43,16 +46,9 @@ class BrainyQuotesSpider(scrapy.Spider):
         data = scrapy.Selector(text=data_store)
         for quotes in data.css('div.m-brick'):
             yield {
-                    'Quote': quotes.css('div.clearfix a.b-qt::text').extract_first(),
-                    'Author': quotes.css('div.clearfix a.oncl_a::text').extract_first(),
-                    'Tags': quotes.css('div.kw-box a.oncl_list_kc::text').extract(),
-                    'Shareable links': quotes.css('div.sh-box a.fa-stack::attr(href)').extract(),
-                    'img_url': quotes.css('div.qti-listm img.zoomc::attr(data-img-url)').extract_first(),
+                    'Quote': quotes.css('a.b-qt::text').extract_first(),
+                    # 'Author': quotes.css('div.clearfix a.oncl_a::text').extract_first(),
+                    # 'Tags': quotes.css('div.kw-box a.oncl_list_kc::text').extract(),
+                    # 'Shareable links': quotes.css('div.sh-box a.fa-stack::attr(href)').extract(),
+                    # 'img_url': quotes.css('div.qti-listm img.zoomc::attr(data-img-url)').extract_first(),
             }
-        self.page += 1
-        self.request_params['pg'] = self.page
-        yield scrapy.Request(self.request_url, callback=self.parse_scrolled_pages, method='POST',
-                             body=json.dumps(self.request_params),
-                             headers={'Content-Type': 'application/json'})
-
-
