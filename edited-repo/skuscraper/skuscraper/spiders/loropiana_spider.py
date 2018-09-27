ca4cd3b113018ea627daf4ac838b3dc00fb98ace
mumerfarooq000 <mfarooq@arbisoft.com>
2018-09-27 12:07:43 +0500

    first commit

---
+++ edited-repo/skuscraper/skuscraper/spiders/loropiana_spider.py
@@ -0,0 +1,184 @@
+import json
+
+from scrapy import Request
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import Rule
+
+from .base import BaseParseSpider, BaseCrawlSpider, clean, Gender, soupify
+
+
+class Mixin:
+    retailer = 'loropiana'
+    default_brand = 'Loro Piana'
+    allowed_domains = ['loropiana.com']
+
+
+class MixinUS(Mixin):
+    retailer = Mixin.retailer + '-us'
+    market = 'US'
+    start_urls = ['https://us.loropiana.com/en/']
+
+
+class MixinUK(Mixin):
+    retailer = Mixin.retailer + '-uk'
+    market = 'UK'
+    start_urls = ['https://uk.loropiana.com/en/']
+
+
+class MixinIT(Mixin):
+    retailer = Mixin.retailer + '-it'
+    market = 'IT'
+    start_urls = ['https://it.loropiana.com/it/']
+
+
+class LoroPianaParseSpider(BaseParseSpider):
+    image_url_t = "https://us.loropiana.com/en/api/pdp/get-images?articleCode={}&colorCode={}"
+    variant_url_t = "https://us.loropiana.com/en/api/pdp/product-variants?articleCode={}&colorCode={}"
+
+    price_css = '.t-product-cta-price ::text'
+    raw_description_css = '.desktop-details .t-product-copy ::text'
+    description_css = '.product-info .t-caption ::text'
+    care_css = '#accordion_care .content ::text'
+
+    def parse(self, response):
+        product_id = self.product_id(response)
+        garment = self.new_unique_garment(product_id)
+
+        if not garment:
+            return
+
+        self.boilerplate_normal(garment, response)
+
+        garment['skus'] = {}
+        garment['image_urls'] = []
+
+        garment['gender'] = self.product_gender(response)
+        garment['category'] = self.product_category(response)
+
+        garment['meta'] = {'pricing_common': self.product_pricing_common(response=response)}
+        garment['meta']['requests_queue'] = self.request_image_urls(response)
+        garment['meta']['requests_queue'] += self.request_variants(response)
+
+        return self.next_request_or_garment(garment)
+
+    def parse_image_urls(self, response):
+        garment = response.meta['garment']
+
+        for raw_image in json.loads(response.text):
+            for image in raw_image['formats']:
+
+                if image['format'] != 'ZOOM':
+                    continue
+
+                garment['image_urls'].append(image['url'])
+
+        return self.next_request_or_garment(garment)
+
+    def parse_skus(self, response):
+        garment = response.meta['garment']
+        garment['skus'].update(self.skus(response))
+
+        return self.next_request_or_garment(garment)
+
+    def skus(self, response):
+        skus = {}
+        garment = response.meta['garment']
+
+        for variants in json.loads(response.text):
+            colour = variants['description']
+
+            for variant in variants['sizes']:
+                sku = garment['meta']['pricing_common'].copy()
+                sku['size'] = variant['code']
+                sku['colour'] = colour
+
+                if variant['stock']['stockLevelStatus']['code'] == 'outOfStock':
+                    sku['out_of_stock'] = True
+
+                skus[variant['variantCode']] = sku
+
+        return skus
+
+    def product_id(self, response):
+        return clean(response.css('::attr("data-base-product-code")'))[0]
+
+    def product_gender(self, response):
+        trail = [url for _, url in response.meta.get('trail') or []]
+        soup = [response.url] + self.product_category(response) + trail
+
+        return self.gender_lookup(soupify(soup)) or Gender.ADULTS.value
+
+    def product_category(self, response):
+        css = '.t-product-breadcrumps ::text'
+        return [c.replace('>', '') for c in clean(response.css(css))]
+
+    def detect_colour_codes(self, response):
+        css = '#js-pdp-initial-variants ::attr(value)'
+        raw_variants = clean(response.css(css))[0]
+
+        return [variant['code'] for variant in json.loads(raw_variants)]
+
+    def request_image_urls(self, response):
+        product_id = self.product_id(response)
+        colour_requests = []
+
+        for colour_code in self.detect_colour_codes(response):
+            url = self.image_url_t.format(product_id, colour_code)
+            colour_requests.append(Request(url=url, callback=self.parse_image_urls))
+
+        return colour_requests
+
+    def request_variants(self, response):
+        product_id = self.product_id(response)
+        variant_requests = []
+
+        for colour_code in self.detect_colour_codes(response):
+            url = self.variant_url_t.format(product_id, colour_code)
+            variant_requests.append(Request(url=url, callback=self.parse_skus))
+
+        return variant_requests
+
+    def product_name(self, response):
+        return clean(response.css('.product-info .t-h2 ::text'))[0]
+
+
+class LoroPianaCrawlSpider(BaseCrawlSpider):
+    listings_css = [
+        '.safari_only'
+    ]
+
+    products_css = [
+        '.product-result'
+    ]
+
+    rules = [
+        Rule(LinkExtractor(restrict_css=listings_css), callback='parse'),
+        Rule(LinkExtractor(restrict_css=products_css), callback='parse_item'),
+    ]
+
+
+class LoroPianaUSParseSpider(MixinUS, LoroPianaParseSpider):
+    name = MixinUS.retailer + '-parse'
+
+
+class LoroPianaUSCrawlSpider(MixinUS, LoroPianaCrawlSpider):
+    name = MixinUS.retailer + '-crawl'
+    parse_spider = LoroPianaUSParseSpider()
+
+
+class LoroPianaUKParseSpider(MixinUK, LoroPianaParseSpider):
+    name = MixinUK.retailer + '-parse'
+
+
+class LoroPianaUKCrawlSpider(MixinUK, LoroPianaCrawlSpider):
+    name = MixinUK.retailer + '-crawl'
+    parse_spider = LoroPianaUKParseSpider()
+
+
+class LoroPianaITParseSpider(MixinIT, LoroPianaParseSpider):
+    name = MixinIT.retailer + '-parse'
+
+
+class LoroPianaITCrawlSpider(MixinIT, LoroPianaCrawlSpider):
+    name = MixinIT.retailer + '-crawl'
+    parse_spider = LoroPianaITParseSpider()
