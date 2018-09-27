e194eb94d4ba77b88bf509b49af26251bbb6049b
rehansharifqazi <rehan.sharif@arbisoft.com>
2018-09-27 12:15:28 +0500

    Iteration 1

---
+++ Sokamal/items.py
@@ -0,0 +1,96 @@
+"""Script for Item Loading for Sokamal's products"""
+import json
+
+import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst, MapCompose
+from w3lib.html import remove_tags
+
+
+def filter_image_urls(value):
+    """Filters image_urls received as string"""
+    if value and len(value) > 1:
+        img_url_json = json.loads(value)
+        img_urls = []
+        for url in img_url_json.values():
+            img_urls.append("https://fishry-image.azureedge.net/product/" + url['Image'])
+            return img_urls
+    return ""
+
+
+def filter_category(value):
+    """Filters category received as string"""
+    if value:
+        category = json.loads(value)
+        categories = []
+        for category in category.values():
+            categories.append(category['name'])
+        return categories
+    return ""
+
+
+def filter_url(value):
+    """Filters url received as string"""
+    return "https://sokamal.com/product/" + value
+
+
+def filter_skus(value):
+    """Filters sku received as string"""
+    if value:
+        sku_data = json.loads(value)
+        skus = {}
+        for sku in sku_data:
+            inventory_quantity = sku['inventoryQuantity']
+            barcode = sku['barcode']
+            price = sku['price']
+            size, color = filter_color_size(sku)
+            skus[barcode] = {
+                "color": color,
+                "size": size,
+                "price": price,
+                "currency": "Rs",
+                "inventory_quantity": inventory_quantity
+            }
+        return skus
+    return value
+
+
+def filter_color_size(value):
+    """Filters color_size received as list"""
+    piece_size_color = value['name']
+    color = "generic"
+    if len(piece_size_color) > 2:
+        size = piece_size_color[1]
+        color = piece_size_color[2]
+    elif len(piece_size_color) == 2:
+        size = piece_size_color[0]
+        color = piece_size_color[1]
+    else:
+        size = piece_size_color[0]
+    return size, color
+
+
+class SokamalItem(scrapy.Item):
+    """Class for items for Sokamal's products"""
+    brand = scrapy.Field()
+    url = scrapy.Field()
+    name = scrapy.Field()
+    description = scrapy.Field()
+    retailer_sku = scrapy.Field()
+    care = scrapy.Field()
+    category = scrapy.Field()
+    skus = scrapy.Field()
+    image_urls = scrapy.Field()
+    barcode = scrapy.Field()
+
+
+class SokamalLoader(ItemLoader):
+    """Class for ItemLoader for Sokamal's products"""
+    default_output_processor = TakeFirst()
+    default_item_class = SokamalItem
+    description_in = MapCompose(remove_tags)
+    image_urls_in = MapCompose(filter_image_urls)
+    category_in = MapCompose(filter_category)
+    url_in = MapCompose(filter_url)
+    skus_in = MapCompose(filter_skus)
+
---
+++ Sokamal/sokamal.py
@@ -0,0 +1,37 @@
+"""This script parses the site Sokamal's products"""
+import json
+
+import scrapy
+from scrapy import FormRequest
+from SOKAMAL.items import SokamalLoader
+
+
+class SokamalSpider(scrapy.Spider):
+    """Class Spider with two functions"""
+    name = "sokamal"
+    start_urls = ["http://sokamal.com/"]
+
+    def parse(self, response):
+        url = "https://fishry-api-live.azurewebsites.net/collection_request"
+        data_headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+        yield FormRequest(url=url, callback=self.parse_item, method='POST', dont_filter=True,
+                          headers=data_headers,
+                          formdata={'storeID'
+                                    : '480EFD74-078D-4CF2-AC68-270940ED408F', 'take': '5000', })
+
+    @staticmethod
+    def parse_item(response):
+        """Takes the script and parses data"""
+        data = json.loads(response.text)
+        for product in data:
+            loader = SokamalLoader()
+            loader.add_value("url", product['productUrl'])
+            loader.add_value("name", product['productName'])
+            loader.add_value("brand", "Sokamal")
+            loader.add_value("description", product['productDescription'])
+            loader.add_value("retailer_sku", product['productSKU'])
+            loader.add_value("category", product.get('productCollections', ""))
+            loader.add_value("image_urls", product.get('productImage'))
+            loader.add_value("skus", product['productVarients'])
+            loader.add_value("barcode", product['productBarcode'])
+            yield loader.load_item()
