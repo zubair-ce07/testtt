edbc78c913d951de235d6b4c9f21157d51a4d64f
arslanhashmi <m.arslanijaz9@gmail.com>
2018-10-02 16:32:23 +0500

    Change field address max length.

--- juntos/user/migrations/0002_auto_20181001_0954.py
+++ juntos/user/migrations/0002_auto_20181001_0954.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='profile',
             name='address',
-            field=models.CharField(blank=True, max_length=1024, null=True),
+            field=models.CharField(blank=True, max_length=128, null=True),
         ),
     ]
