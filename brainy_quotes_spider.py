dc564ec93c6a378412dd4d6cc31d8f71d5b2e8ae
abeer mahboob <42963220+abeermahboob1@users.noreply.github.com>
2018-10-02 12:53:51 +0500

    renamed variables, uncommented items and optimized imports

--- brainy_quotes_spider.py
+++ brainy_quotes_spider.py
@@ -10,13 +10,7 @@ class BrainyQuotesSpider(scrapy.Spider):
         'https://www.brainyquote.com/topics'
     ]
     request_url = 'https://www.brainyquote.com/api/inf'
-    request_params = {
-        'ab': 'a',
-        'fdd': 'd',
-        'langc': 'en',
-        'm': 0,
-        'typ': 'topic',
-    }
+
 
     def parse(self, response):
         topics = response.css('div.bqLn')
@@ -25,15 +19,22 @@ class BrainyQuotesSpider(scrapy.Spider):
             yield scrapy.Request(topic_url, callback=self.parse_topics)
 
     def parse_topics(self, response):
+        request_params = {
+            'ab': 'a',
+            'fdd': 'd',
+            'langc': 'en',
+            'm': 0,
+            'typ': 'topic',
+        }
         v = response.xpath("//meta[@property='ver']/@content").extract_first()
         vid = response.xpath('//script[@type="text/javascript"]').re_first("VID='(.*?)';")
-        id = response.xpath('//script[@type="text/javascript"]').re_first('PG_DM_ID="(.+)";')
+        _id = response.xpath('//script[@type="text/javascript"]').re_first('PG_DM_ID="(.+)";')
         last_page = response.xpath('//script[@type="text/javascript"]').re_first('LPAGE =(.+);')
-        request_params = deepcopy(self.request_params)
+        request_params = deepcopy(request_params)
         request_params.update({
             'v': v,
             'vid': vid,
-            'id': id,
+            'id': _id,
         })
         for page_no in range(1, int(last_page)):
             request_params['pg'] = page_no
@@ -47,8 +48,8 @@ class BrainyQuotesSpider(scrapy.Spider):
         for quotes in data.css('div.m-brick'):
             yield {
                     'Quote': quotes.css('a.b-qt::text').extract_first(),
-                    # 'Author': quotes.css('div.clearfix a.oncl_a::text').extract_first(),
-                    # 'Tags': quotes.css('div.kw-box a.oncl_list_kc::text').extract(),
-                    # 'Shareable links': quotes.css('div.sh-box a.fa-stack::attr(href)').extract(),
-                    # 'img_url': quotes.css('div.qti-listm img.zoomc::attr(data-img-url)').extract_first(),
+                    'Author': quotes.css('div.clearfix a.oncl_a::text').extract_first(),
+                    'Tags': quotes.css('div.kw-box a.oncl_list_kc::text').extract(),
+                    'Shareable links': quotes.css('div.sh-box a.fa-stack::attr(href)').extract(),
+                    'img_url': response.urljoin(quotes.css('img.zoomc::attr(data-img-url)').extract_first()),
             }
