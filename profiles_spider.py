f60969d2906cfdaa3a2c0aed8bdbffb826889ba0
abeer mahboob <42963220+abeermahboob1@users.noreply.github.com>
2018-10-03 13:58:17 +0500

    removed further unnecessary parameters and cleaned the code

--- profiles_spider.py
+++ profiles_spider.py
@@ -8,23 +8,19 @@ class NwhSpider(scrapy.Spider):
     allowed_domains = ['www.nwh.org']
 
     def get_parameters(self, response):
-        data = {}
         specialty = response.css('#ctl00_cphContent_ctl01_ddlResultsSpecialties > option::attr(value)').extract_first()
         generator = response.css('input[name="__VIEWSTATEGENERATOR"]::attr(value)').extract_first()
         state = response.css('input[name="__VIEWSTATE"]::attr(value)').extract_first()
-        data.update({
+        results_per_page = response.css('style::text').re_first("\d{5}")
+        target = response.css('#ctl00_cphContent_ctl01_lnkSeachResults::attr(href)').extract()
+        target_param = target[0][25:-5]
+        data = {
             'ctl00$cphContent$ctl01$ddlResultsSpecialties': specialty,
-            'ctl00$cphContent$ctl01$ddlPublished': 'False',
             '__VIEWSTATE': state,
             '__VIEWSTATEGENERATOR': generator,
-            '__EVENTTARGET': 'ctl00$cphContent$ctl01$lnkSeachResults',
-            'ctl00$cphContent$ctl01$ddlResultsPerPage': '99999',
-            'ctl00$header$searchRdoBtn': '0',
-            'ctl00$header$hdnHoverLocationId': '1',
-            'ctl00$cphContent$ctl01$ddlPhysicianRefferalRequired': '-1',
-            'ctl00$cphContent$ctl01$ddlAcceptNewPatients': '-1',
-            'ctl00$cphContent$ctl01$ddlConcierge': '-1',
-        })
+            '__EVENTTARGET': target_param,
+            'ctl00$cphContent$ctl01$ddlResultsPerPage': results_per_page,
+        }
         return data
 
     def parse(self, response):
@@ -35,10 +31,10 @@ class NwhSpider(scrapy.Spider):
     def parse_doctors(self, response):
         doc_params = self.get_parameters(response)
         for doctors in response.css('a.link-name-profile'):
-            target = doctors.css('a.link-name-profile::attr(href)').extract()
-            targets = target[0][25:-5]
+            target_doc = doctors.css('a.link-name-profile::attr(href)').extract()
+            target_doc_param = target_doc[0][25:-5]
             doc_params.update({
-                '__EVENTTARGET': targets,
+                '__EVENTTARGET': target_doc_param,
             })
             yield scrapy.FormRequest(url=u'https://www.nwh.org/find-a-doctor/ContentPage.aspx?nd=847',
                                      formdata=doc_params, method='POST', callback=self.parse_profile)
@@ -46,25 +42,28 @@ class NwhSpider(scrapy.Spider):
     def parse_profile(self, response):
         for information in response.css('div.light'):
             item = {
-                'full-name': information.xpath('//h1[@class="header-doctor-name"]/text()').extract_first(),
-                'specialty': information.xpath('//div[@class="pnl-doctor-specialty"]//h2/text()').extract_first().strip().split(','),
-                'year-joined': information.xpath('//div[@id="ctl00_cphContent_ctl01_pnlYearJoined"]//h2/text()').re_first("\d{4}"),
+                'full-name': information.xpath(
+                    '//*[@class="header-doctor-name"]/text()').extract_first(),
+                'specialty': information.xpath(
+                    '//*[@class="pnl-doctor-specialty"]//h2/text()').extract_first().strip().split(','),
+                'year-joined': information.xpath(
+                    '//*[@id="ctl00_cphContent_ctl01_pnlYearJoined"]//h2/text()').re_first("\d{4}"),
                 'med-school': information.xpath(
-                    '//div[@id="ctl00_cphContent_ctl01_pnlMedicalSchool"]//ul//li/text()').extract_first(),
+                    '//*[@id="ctl00_cphContent_ctl01_pnlMedicalSchool"]//ul//li/text()').extract_first(),
                 'affiliation': information.xpath(
-                    '//div[@id="ctl00_cphContent_ctl01_pnlBoardOfCertifications"]//ul//li/text()').extract_first(),
+                    '//*[@id="ctl00_cphContent_ctl01_pnlBoardOfCertifications"]//ul//li/text()').extract_first(),
                 'fellowship': information.xpath(
-                    '//div[@id="ctl00_cphContent_ctl01_pnlFellowship"]//ul//li/text()').extract_first(),
+                    '//*[@id="ctl00_cphContent_ctl01_pnlFellowship"]//ul//li/text()').extract_first(),
                 'internship': information.xpath(
-                    '//div[@id="ctl00_cphContent_ctl01_pnlInternship"]//ul//li/text()').extract_first(),
+                    '//*[@id="ctl00_cphContent_ctl01_pnlInternship"]//ul//li/text()').extract_first(),
                 'residency': information.xpath(
-                    '//div[@id="ctl00_cphContent_ctl01_pnlResidency"]//ul//li/text()').extract_first(),
+                    '//*[@id="ctl00_cphContent_ctl01_pnlResidency"]//ul//li/text()').extract_first(),
                 'address': information.css('.doctor-contact-location-address>a::text').extract(),
-                'img-url': response.urljoin(information.css('div.pnl-doctor-image>img::attr(src)').extract_first()),
+                'img-url': response.urljoin(information.css('.pnl-doctor-image>img::attr(src)').extract_first()),
                 'source-url': response.request.url,
                 'contact': {
-                    'phone': information.css('div.pnl-doctor-contact-phone>a>span::text').extract_first(),
-                    'fax': information.xpath('//span[@id="ctl00_cphContent_ctl01_lblDocContactFax"]/text()').extract_first(),
+                    'phone': information.css('.pnl-doctor-contact-phone>a>span::text').extract_first(),
+                    'fax': information.xpath('//*[@id="ctl00_cphContent_ctl01_lblDocContactFax"]/text()').extract_first(),
                 }
             }
             yield item
