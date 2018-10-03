83178456da7875b222d7248772e3d3deb983fab7
Fahad Shawal <fahad@A00694.arbisoft.com>
2018-10-03 18:50:07 +0500

    made some changes

--- greatfoodhallV1_0.py
+++ greatfoodhallV1_0.py
@@ -4,16 +4,27 @@ import logging as log
 
 
 class Greatfoodhallv10Spider(scrapy.Spider):
-    name = 'greatfoodhallV1.0'
-    allowed_domains = ['greatfoodhall.com']
-    start_urls = ['http://www.greatfoodhall.com/eshop/OnlieShopLanding.do']
-    #start_urls = ['http://www.greatfoodhall.com/eshop/ShowProductPage.do?service=direct&service=1&service=ProductList.%24ProductView.%24DirectLink&sp=l44806']
-    count = 0
-    products_links = []
-    
+    """
+    This is doc String
+    """
+    def __init__(self):
+        """
+        This is doc String
+        """
+        self.name = 'greatfoodhallV1.0'
+        self.allowed_domains = ['greatfoodhall.com']
+        self.start_urls = [
+            'http://www.greatfoodhall.com/eshop/OnlieShopLanding.do'
+            ]
+        self.count = 0
+        self.products_links = []
+        self.online_nav_links = []
+
     def parse(self, response):
-        
-        self.online_nav_links=response.xpath(
+        """
+        This is doc String
+        """
+        self.online_nav_links = response.xpath(
                     '//div[contains(@class, "item")]/a/@href'
                 ).extract()
 
@@ -23,14 +34,16 @@ class Greatfoodhallv10Spider(scrapy.Spider):
             )
              
     def get_products_links(self, response):
-        
+        """
+        This is doc String
+        """
         self.products_links += response.xpath(
                     '//div[contains(@class, "productTmb")]//a/@href'
                 ).extract()
 
         try:
-            curr_items=self.get_curr_items(response)
-            total_items=self.get_total_items(response)
+            curr_items = self.get_curr_items(response)
+            total_items = self.get_total_items(response)
             
             if curr_items < total_items:   
                 self.count += 1
@@ -43,8 +56,7 @@ class Greatfoodhallv10Spider(scrapy.Spider):
             
             elif self.online_nav_links:
 
-                self.count=0
-
+                self.count = 0
                 yield scrapy.Request(
                     url='http://www.greatfoodhall.com/eshop/'
                         +self.online_nav_links.pop(), 
@@ -55,76 +67,113 @@ class Greatfoodhallv10Spider(scrapy.Spider):
                 for url in self.products_links:
                     yield scrapy.Request(url=url,
                             callback = self.get_item_data)
-        except:
+        
+        except AssertionError as error:
+            log.error (error)
             log.warning('\n\nFound Error in --> ' + response.url+ '\n\n')
     
     def get_item_data(self, response):
+        """
+        This is doc String
+        """
         yield {
             'company' : self.get_company_name(response),
             'product Name' : self.get_product_name(response),
             'weight' : self.get_weight(response),
             'price' : self.get_price(response),
             'nuterition info' : self.get_nutrition_info(response),
-            'images' : 'http://www.greatfoodhall.com/eshop'+self.get_product_img_url(response),
+            'images' : 'http://www.greatfoodhall.com/eshop'
+                            +self.get_product_img_url(response),
             'category' : self.get_category(response),
-            'country flag' : 'http://www.greatfoodhall.com/eshop'+self.get_country(response),
+            'country flag' : 'http://www.greatfoodhall.com/eshop'
+                            +self.get_country(response),
             'product url' : response.url
         }
 
 
-######################### Data Getters Defined Below #########################        
+####################### Data Getters Defined Below ######################
 
     def get_curr_items(self, response):
+        """
+        This is doc String
+        """
         return int((response.xpath(
             '//b[contains(@class, "currentPage")]/text()'
         ).extract_first()).split('-')[-1])
     
     def get_total_items(self, response):
+        """
+        This is doc String
+        """
         return int(response.xpath(
             '//b[contains(@class, "totalItem")]/text()'
         ).extract_first())
 
     def get_company_name(self, response):
+        """
+        This is doc String
+        """
         return response.xpath(
             '//h1[contains(@class, "pL6")]/text()'
         ).extract_first(default='N/A')
     
     def get_product_name(self, response):
+        """
+        This is doc String
+        """
         return response.xpath(
             '//p[contains(@class, "description")]/text()'
         ).extract_first(default='N/A')
     
     def get_weight(self, response):
+        """
+        This is doc String
+        """
         return response.xpath(
             '//span[contains(@class, "pL6")]/text()'
         ).extract_first(default='N/A')
     
     def get_price(self, response):
+        """
+        This is doc String
+        """
         return response.xpath(
             '//div[contains(@class, "itemOrgPrice2")]/text()'
         ).extract_first()
     
     def get_country(self, response):
+        """
+        This is doc String
+        """
         return response.xpath(
             '//img[contains(@class, "flag")]/@src'
         ).extract_first(default='N/A')
     
     def get_product_img_url(self, response):
+        """
+        This is doc String
+        """
         return response.xpath(
             '//div[contains(@class, "productPhoto")]/img/@src'
         ).extract_first(default='N/A')
     
     def get_nutrition_info(self, response):
+        """
+        This is doc String
+        """
         return response.xpath(
             '//div[contains(@id, "nutrition")]//td/text()'
         ).extract_first(default='N/A')
     
     def get_category(self, response):
+        """
+        This is doc String
+        """
         try:
             return response.xpath(
                 '//a[contains(@class, "highlight")]/text()'
             )[-1].extract_first(default='N/A')  
-        except:
-            pass
+        except AssertionError as error:
+            log.error(error)
             return 'None'
     
