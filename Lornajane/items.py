1295a2a68ffa5fbe8eca06ab67f1d4a6ddc55586
Bilal Sajid <bilal.sajid@arbisoft.com>
2018-10-05 17:02:55 +0500

    Lornajane products spider

---
+++ Lornajane/items.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class LornajaneItem(scrapy.Item):
+    """Item class which contains all the required
+    fields which has to be crawled from the website
+    """
+
+    _id = scrapy.Field()    # Defined id for checking repetition
+    brand = scrapy.Field()
+    care = scrapy.Field()
+    category = scrapy.Field()
+    description = scrapy.Field()
+    gender = scrapy.Field()
+    image_urls = scrapy.Field()
+    name = scrapy.Field()
+    retailer_sku = scrapy.Field()
+    skus = scrapy.Field()
+    url = scrapy.Field()
+    pass
---
+++ Lornajane/pipelines.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+from scrapy.exceptions import DropItem
+
+
+class LornajanePipeline(object):
+    """Pipeline for removing duplicates"""
+
+    def __init__(self):
+        self.ids = set()
+
+    def process_item(self, item, spider):
+        if item['_id'] in self.ids:
+            print("Duplicate Found: ", item["_id"])
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
+            self.ids.add(item['_id'])
+            return item
