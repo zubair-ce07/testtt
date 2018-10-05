98a14f6689d73791212cddbd48108515ba016631
Bilal Sajid <bilal.sajid@arbisoft.com>
2018-10-05 18:46:04 +0500

    Code cleaned

--- Orsay/products.py
+++ Orsay/products.py
@@ -17,7 +17,6 @@ class ProductsSpider(CrawlSpider):
     start_urls = [
         'http://www.orsay.com/de-de/',
     ]
-
     extra_links = [
         'http://www.orsay.com/de-de/help/faq.html',
         'http://www.orsay.com/de-de/help/privacy-policy.html',
@@ -25,7 +24,6 @@ class ProductsSpider(CrawlSpider):
         'http://www.orsay.com/de-de/help/contact.html',
         'http://www.orsay.com/de-de/help/club-term-conditions.html',
         'http://www.orsay.com/de-de/help/imprimint.html']
-
     rules = (
         Rule(LinkExtractor(
             allow=(r'http://www.orsay.com/de-de/produkte/.*')),
@@ -36,7 +34,6 @@ class ProductsSpider(CrawlSpider):
             callback='get_product',
             follow=True)
     )
-
     # List of css selectors used in the crawler
     selectors = {
         "products": ".grid-tile .product-image a::attr(href)",
@@ -53,9 +50,7 @@ class ProductsSpider(CrawlSpider):
 
     def extract_product_urls(self, response):
         """Extracts all the products url for a particular category"""
-        products = response.css(
-            self.selectors["products"]
-        ).extract()
+        products = response.css(self.selectors["products"]).extract()
         products = [response.urljoin(url) for url in products]
         return products
 
@@ -63,7 +58,6 @@ class ProductsSpider(CrawlSpider):
         """Gets all the products list"""
         product_urls = self.extract_product_urls(response)
         next_page = self.get_next_page(response)
-
         if next_page:
             yield scrapy.Request(
                 url=next_page,
@@ -81,14 +75,11 @@ class ProductsSpider(CrawlSpider):
 
     def get_next_page(self, response):
         """Finds if the page has more items and returns the link"""
-        check = response.css(
-            self.selectors["next_page"]
-        )
+        check = response.css(self.selectors["next_page"])
         if check:
             new_url = response.url
             if not self.has_digits(response.url):
                 new_url = response.urljoin("?sz=72")
-
             total_products = self.get_total_products(response)
             shown_products = re.findall(r'\d+', new_url)[0]
 
@@ -110,7 +101,6 @@ class ProductsSpider(CrawlSpider):
     def get_product(self, response):
         """Getting all the product details and storind it in the item"""
         details = self.get_details(response)
-
         product_details = {
             '_id': details["productId"],
             'brand': "Orsay",
@@ -151,7 +141,6 @@ class ProductsSpider(CrawlSpider):
         details = [txt.strip() for txt in details]
         details = list(filter(lambda txt: txt != '', details))
         details.pop(0)
-
         return details
 
     def get_skus(self, response):
@@ -159,12 +148,10 @@ class ProductsSpider(CrawlSpider):
         details = self.get_details(response)
         sizes = self.get_size(response)
         image_urls = self.get_image_urls(response)
-
         skus = {}
 
         if not sizes:
             sizes.append("<constant>")
-
         for size in sizes:
             skus["{}_{}".format(details["color"], size)] = {
                 "color": details["color"],
@@ -205,73 +192,55 @@ class ProductsSpider(CrawlSpider):
                           'item':  item})
 
     # Getters using css selectors #
-
     def get_details(self, response):
         return json.loads(
             response.css(self.selectors["details"]).extract_first()
         )
 
     def get_description(self, response):
-        description = response.css(
-            self.selectors["description"]
-        ).extract()
-
+        description = response.css(self.selectors["description"]).extract()
         return self.clean_description(description)
 
     def get_care(self, response):
         return self.clean_care_text(
-            response.css(
-                self.selectors["care"]
-            ).extract()
+            response.css(self.selectors["care"]).extract()
         )
 
     def get_image_urls(self, response):
-        images = response.css(
-            self.selectors["images"]
-        ).extract()
+        images = response.css(self.selectors["images"]).extract()
         return {re.sub(r'[?].*fit$', "", image) for image in images}
 
     def get_size(self, response):
-        sizes = set(response.css(
-            self.selectors["size"]
-        ).extract())
+        sizes = set(response.css(self.selectors["size"]).extract())
         sizes = [re.sub('\s+', ' ', size).strip() for size in sizes if size]
         return [size for size in sizes if size]
 
     def get_availability(self, response):
-        return response.css(
-            self.selectors["availability"]
-        ).extract()
+        return response.css(self.selectors["availability"]).extract()
 
     def get_total_products(self, response):
         total_products = response.css(
             self.selectors["total_products"]
         ).extract()
-
         for text in total_products:
             if self.has_digits(text):
                 text = text.replace(".", "")
                 return re.findall(r'\d+', text)[0]
 
     def more_colors(self, response):
-        colors = response.css(
-            self.selectors["more_colors"]).extract()
+        colors = response.css(self.selectors["more_colors"]).extract()
         return colors
 
     def errback_httpbin(self, failure):
         """Error back function for detecting the request errors"""
-
         # log all failures
         self.logger.error(repr(failure))
-
         if failure.check(HttpError):
             response = failure.value.response
             self.logger.error('HttpError on %s', response.url)
-
         elif failure.check(DNSLookupError):
             request = failure.request
             self.logger.error('DNSLookupError on %s', request.url)
-
         elif failure.check(TimeoutError, TCPTimedOutError):
             request = failure.request
             self.logger.error('TimeoutError on %s', request.url)
