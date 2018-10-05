98bb90eacd41eb7c2ff18ed3e75effbc77816254
dawoodkhan5 <dawood.khan@arbisoft.com>
2018-10-05 18:38:02 +0500

    currency function called classes name changed xpath converted to css

--- eloquii_spider.py
+++ eloquii_spider.py
@@ -6,7 +6,7 @@ from scrapy import Request
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Rule
 
-from .base import BaseParseSpider, BaseCrawlSpider, clean
+from .base import BaseParseSpider, BaseCrawlSpider, clean, Gender
 
 
 class Mixin:
@@ -16,15 +16,14 @@ class Mixin:
     start_urls = ['https://www.eloquii.com/']
     allowed_domains = ['eloquii.com']
     api_url = 'https://www.eloquii.com/on/demandware.store/Sites-eloquii-Site/default/Product-GetVariants?'
-
-    currency = 'USD'
-    gender = 'women'
+    gender = Gender.WOMEN.value
 
 
-class EloquiiParser(BaseParseSpider, Mixin):
+class EloquiiParseSpider(BaseParseSpider, Mixin):
     name = Mixin.retailer + '-parse'
     description_css = ".description.pt-3 li:first-child ::text"
     care_css = '.description.pt-3 li:not(:first-child) ::text'
+    currency = ''
 
     def parse(self, response):
         product_id = self.product_id(response)
@@ -32,8 +31,8 @@ class EloquiiParser(BaseParseSpider, Mixin):
         if not garment:
             return
         self.boilerplate_normal(garment, response)
-        garment['gender'] = self.gender
         garment['image_urls'] = self.image_urls(response)
+        self.currency = self.currency(response)
         garment['skus'] = {}
         garment['meta'] = {'requests_queue': self.request_variants(product_id)}
         return self.next_request_or_garment(garment)
@@ -48,21 +47,18 @@ class EloquiiParser(BaseParseSpider, Mixin):
         url = add_or_replace_parameter(url, 'format', 'ajax')
         return [Request(url, self.parse_variants)]
 
-    @staticmethod
-    def product_id(response):
+    def product_id(self, response):
         return clean(response.css('.yotpo.yotpo-pictures-widget::attr(data-product-id)'))[0]
 
-    @staticmethod
-    def product_category(response):
-        return clean(response.css('.breadcrumb.bg-white .text-12::text'))
+    def product_category(self, response):
+        return clean(response.css('.breadcrumb [itemprop="title"] ::text'))
 
-    @staticmethod
-    def product_name(response):
-        return clean(response.xpath('//meta[@name="keywords"]/@content'))[0]
+    def product_name(self, response):
+        return clean(response.css('meta[name="keywords"] ::attr(content)'))[0]
 
     def image_urls(self, response):
         image_urls = []
-        images = clean(response.xpath('//script[contains(text(),"app.execUjs")]/text()'))[0]
+        images = clean(response.css('script:contains("app.execUjs") ::text'))[0]
         image_sets = re.findall('("xlarge"\s*:\s*\[.*?\])', images)
         for image_set in image_sets:
             images = json.loads("{" + image_set + "}")
@@ -81,6 +77,9 @@ class EloquiiParser(BaseParseSpider, Mixin):
 
         return self.next_request_or_garment(garment)
 
+    def currency(self, response):
+        return clean(response.css('meta[property="og:price:currency"]::attr(content)'))[0]
+
     def skus(self, variant):
         money_strs = [variant['pricing']['sale'], variant['pricing']['standard'], self.currency]
         sku = self.product_pricing_common(None, money_strs=money_strs)
@@ -89,14 +88,16 @@ class EloquiiParser(BaseParseSpider, Mixin):
         size = self.one_size if variant['size'] == 'NS' else variant['size']
         sku['size'] = f"{size}_{variant['sizeType']}"
 
-        sku['out_of_stock'] = not variant['inStock']
+        if not sku['inStock']:
+            sku['out_of_stock'] = True
+
         sku_id = f"{sku['colour']}_{sku['size']}"
         return {sku_id: sku}
 
 
-class EloquiiCrawler(BaseCrawlSpider, Mixin):
+class EloquiiCrawlspider(BaseCrawlSpider, Mixin):
     name = Mixin.retailer + '-crawl'
-    parse_spider = EloquiiParser()
+    parse_spider = EloquiiParseSpider()
     listing_css = [
         '.sub-nav-style a',
         '.justify-content-center.mt-5 div:nth-child(3) a'
