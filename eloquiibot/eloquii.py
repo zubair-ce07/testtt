a12ebfef41b45a594a3975b54e40c6e7555cb2df
Rana Aurangzaib <rana.aurangzaib@arbisoft.com>
2018-10-09 12:04:11 +0500

    a/c to pep8

--- eloquiibot/eloquii.py
+++ eloquiibot/eloquii.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
+"""This file contains scrapper of eloquii.com"""
 import re
 import scrapy
 from eloquiibot.items import EloquiiProduct
 
 
 class EloquiiSpider(scrapy.Spider):
+    """This is the class of scrapper
+    that scrape the eloquii.com"""
+
     name = 'eloquii'
     allowed_domains = ['www.eloquii.com/']
     start_urls = ['http://www.eloquii.com/']
@@ -16,23 +20,30 @@ class EloquiiSpider(scrapy.Spider):
 
         for category in cate_links:
             yield scrapy.Request(
-                url= response.urljoin(category),
+                url=response.urljoin(category),
                 callback=self.parse_products,
                 dont_filter=True
             )
 
     def parse_products(self, response):
-        product_links = response.css("div.product-images a::attr(href)").extract()
+        """This method extract all the links of product
+        and send request to all links to get details"""
+
+        product_links = response.css(
+            "div.product-images a::attr(href)"
+            ).extract()
         for product in product_links:
             yield scrapy.Request(
                 url=product,
                 callback=self.parse_product_details,
                 dont_filter=True
             )
-        
+
         # pagination
         #
-        next_page_sel = response.css("div.row.justify-content-center.mt-5 div:nth-child(3)")
+        next_page_sel = response.css(
+            "div.row.justify-content-center.mt-5 div:nth-child(3)"
+            )
         if next_page_sel:
             next_page = next_page_sel.css("a::attr(href)").extract_first()
             if next_page:
@@ -42,25 +53,42 @@ class EloquiiSpider(scrapy.Spider):
                     dont_filter=True
                 )
 
-
     def parse_product_details(self, response):
+        """This method extract all the details of
+        product and fill the item"""
+
         product = EloquiiProduct()
 
-        product["product_id"] = re.search(r'(\d+)\.html', response.url).group(1)
+        product["product_id"] = re.search(
+            r'(\d+)\.html', response.url
+        ).group(1)
+
         product["brand"] = 'Eloquii'
         name = response.css("div.d-none.d-md-block h1::text").extract_first()
         name = name.strip('\r\t\n')
         product["name"] = name
-        product["category"] = response.xpath("//*[@id='bt_pdp_main']/div[1]/div/span[3]/span/text()").extract_first()
-        product["description"] = response.xpath("//*[@id='bt_pdp_main']/div[2]/div/div/div[3]/div/div[3]/div/div/div/div[3]/span/text()").extract_first()
+        product["category"] = response.xpath(
+            "//*[@id='bt_pdp_main']/div[1]/div/span[3]/span/text()"
+            ).extract_first()
+
+        product["description"] = response.xpath(
+            "//*[@id='bt_pdp_main']/div[2]/div/div/div[3]/div/div[3]/div\
+            /div/div/div[3]/span/text()"
+            ).extract_first()
+
         if not product["description"]:
             product["description"] = "description isn't available"
         product["url"] = response.url
         product["image_urls"] = self.get_img_urls(response)
 
-        merch_info = response.css("h3.text-24.font-demi.mb-3.text-center::text").extract_first()
+        merch_info = response.css(
+            "h3.text-24.font-demi.mb-3.text-center::text"
+            ).extract_first()
+
         if not merch_info:
-            merch_info = re.findall(r'\'COMINGSOON\': (true|false)',response.text)
+            merch_info = re.findall(
+                r'\'COMINGSOON\': (true|false)', response.text
+            )
             if merch_info:
                 merch_info = merch_info[0]
                 merch_info = "Coming Soon"
@@ -73,41 +101,55 @@ class EloquiiSpider(scrapy.Spider):
         else:
             product["merch_info"] = merch_info
 
-
         yield product
 
+    def get_img_urls(self, response):
+        """This method extract the all image urls
+        and convert small images to large"""
 
-    def get_img_urls(self,response):
-        raw_img_url = response.css("div.swiper-wrapper.productthumbnails div img::attr(src)").extract()
-        img_url = [i.replace("small","large",1) for i in raw_img_url]
+        raw_img_url = response.css(
+            "div.swiper-wrapper.productthumbnails div img::attr(src)"
+            ).extract()
+        img_url = [i.replace("small", "large", 1) for i in raw_img_url]
         img_url = [response.urljoin(i) for i in img_url]
 
         if not img_url:
-            img_url = response.css("div.productimagearea img::attr(src)").extract_first()
+            img_url = response.css(
+                "div.productimagearea img::attr(src)"
+                ).extract_first()
             img_url = response.urljoin(img_url)
 
         return img_url
 
-    def get_product_skus(self,response):
+    def get_product_skus(self, response):
+        """This method extract the skus of product
+        if product is available"""
+
         skus = {}
-        colors_list = response.css("ul.swatchesdisplay.list-inline li a::attr(title)").extract()
-        sizes_list = response.css("div#product_detail_size_drop_down_expanded a::attr(title)").extract()
+        colors_list = response.css(
+            "ul.swatchesdisplay.list-inline li a::attr(title)"
+            ).extract()
+        sizes_list = response.css(
+            "div#product_detail_size_drop_down_expanded a::attr(title)"
+            ).extract()
         sizes_list = sizes_list[1:]
         price = response.css("span.subprice-retail::text").extract_first()
         if not price:
-            price = response.css("div.priceGroup span.text-pink::text").extract_first()
+            price = response.css(
+                "div.priceGroup span.text-pink::text"
+                ).extract_first()
 
         currency = ''
         if price:
-            currency = re.search(r'([^0-9.])',price).group(1)
-            price = float(re.search(r'(\d+\.\d+)',price).group(1))
+            currency = re.search(r'([^0-9.])', price).group(1)
+            price = float(re.search(r'(\d+\.\d+)', price).group(1))
 
         for color in colors_list:
             for size in sizes_list:
                 skus[color + '_' + size] = {
-                        "color": color,
-                        "currency": currency,
-                        "price": price,
-                        "size": size,
-                        }
+                    "color": color,
+                    "currency": currency,
+                    "price": price,
+                    "size": size,
+                }
         return skus
