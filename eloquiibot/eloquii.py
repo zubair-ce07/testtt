84dc4f08b10b8dd4337e9cfbbbc3ed730af7fa86
Rana Aurangzaib <rana.aurangzaib@arbisoft.com>
2018-10-15 15:29:30 +0500

    Updated according to review 2

--- eloquiibot/eloquii.py
+++ eloquiibot/eloquii.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
+
 from scrapy.spiders import Rule, CrawlSpider
 from scrapy.linkextractors import LinkExtractor
 from eloquiibot.items import EloquiiProduct
@@ -9,92 +10,84 @@ class EloquiiSpider(CrawlSpider):
     name = 'eloquii'
     allowed_domains = ['www.eloquii.com']
     start_urls = ['https://www.eloquii.com']
-
     rules = (
-        Rule(
-            LinkExtractor(
-                allow=(r'https://www.eloquii.com/.*'),
-                deny=(r'.*\.html.*')
-            )
+        Rule(LinkExtractor(
+            allow=(r'https://www.eloquii.com/.*'),
+            deny=(r'.*\.html.*'),
+            restrict_css=["#nav_menu a", ".row.justify-content-center.mt-5"]
+        )),
+        Rule(LinkExtractor(
+            allow=(r'.*\.html.*'),
+            restrict_css=".product-images a"
         ),
-        Rule
-        (
-            LinkExtractor(
-                allow=(r'.*\.html.*')
-            ),
-            callback='parse_product_details'
-        )
-
+             callback='parse_product'),
     )
 
-    def parse_product_details(self, response):
+    def parse_product(self, response):
         product = EloquiiProduct()
-
-        product["product_id"] = response.css(
-            "#yotpo-bottomline-top-div::attr(data-product-id)"
-            ).extract_first()
-
+        product["product_id"] = self.product_id(response)
         product["brand"] = 'Eloquii'
-        product["name"] = response.css(
-            "#yotpo-bottomline-top-div::attr(data-name)"
-            ).extract_first()
-
-        product["category"] = response.css(
-            "#yotpo-bottomline-top-div::attr(data-bread-crumbs)"
-            ).extract_first()
-
-        product["description"] = response.css(
-            "[name=description]::attr(content)"
-            ).extract_first()
-
-        if not product["description"]:
-            product["description"] = "description isn't available"
+        product["name"] = self.product_name(response)
+        product["category"] = self.product_category(response)
+        product["description"] = self.product_description(response)
         product["url"] = response.url
-        product["image_urls"] = self.get_img_urls(response)
+        product["image_urls"] = self.image_urls(response)
         product["skus"] = {}
-
-        merch_info = self.get_merch_info(response)
-
-        if not merch_info:
-            product["merch_info"] = "Available"
-            product["skus"] = self.get_product_skus(response)
-        else:
-            product["merch_info"] = merch_info
-
+        product["merch_info"] = self.merch_info(response)
+        if not product["merch_info"]:
+            product["skus"] = self.skus(response)
         yield product
 
-    def get_img_urls(self, response):
-        raw_img_url = response.css(
-            ".productthumbnails img::attr(src)"
+    def product_id(self, response):
+        pid = response.css(
+            "#yotpo-bottomline-top-div::attr(data-product-id)"
+            ).extract_first()
+        return pid
+
+    def product_name(self, response):
+        pro_name = response.css(
+            "#yotpo-bottomline-top-div::attr(data-name)"
+            ).extract_first()
+        return pro_name
+
+    def product_category(self, response):
+        category = response.css(
+            "#yotpo-bottomline-top-div::attr(data-bread-crumbs)"
+            ).extract_first()
+        return category
+
+    def product_description(self, response):
+        desc = response.css(
+            "[name=description]::attr(content)"
+            ).extract_first()
+        return desc
+
+    def image_urls(self, response):
+        image_url = response.css(
+            ".productthumbnails image::attr(src)"
             ).extract()
-
-        img_url = [i.replace("small", "large", 1) for i in raw_img_url]
-        img_url = [response.urljoin(i) for i in img_url]
-
-        if not img_url:
-            img_url = response.css(
-                "div.productimagearea img::attr(src)"
+        image_url = [
+            response.urljoin(i.replace('small', 'large', 1)) for i in image_url
+        ]
+        if not image_url:
+            image_url = response.css(
+                ".productimagearea image::attr(src)"
                 ).extract_first()
-            img_url = response.urljoin(img_url)
+            image_url = response.urljoin(image_url)
+        return image_url
 
-        return img_url
-
-    def get_merch_info(self, response):
-        merch_info = response.css(
+    def merch_info(self, response):
+        pro_merch_info = response.css(
             "h3.text-24.font-demi.mb-3::text"
             ).extract_first()
+        if not pro_merch_info:
+            pro_merch_info = re.findall(r'\'COMINGSOON\': (true|false)',
+                                        response.text)
+            if pro_merch_info:
+                pro_merch_info = "Coming Soon"
+        return pro_merch_info
 
-        if not merch_info:
-            merch_info = re.findall(
-                r'\'COMINGSOON\': (true|false)', response.text
-            )
-            if merch_info:
-                merch_info = merch_info[0]
-                merch_info = "Coming Soon"
-
-        return merch_info
-
-    def get_product_skus(self, response):
+    def skus(self, response):
         skus = {}
         colours = response.css(
             ".swatchesdisplay a::attr(title)"
@@ -107,14 +100,11 @@ class EloquiiSpider(CrawlSpider):
         previous_price = response.css(
             ".priceGroup strike::text"
             ).extract_first()
-
         price = response.css(".priceGroup span::text").extract_first()
-
         currency = ''
         if price:
             currency = re.search(r'([^0-9.])', price).group(1)
             price = float(re.search(r'(\d+\.\d+)', price).group(1))
-
         for colour in colours:
             for size in sizes:
                 skus[colour + '_' + size] = {
