74eccb36478d2f1b732320a0c695813f6be44aba
usamasadiq <usama.sadiq@arbisoft.com>
2018-10-16 18:37:35 +0500

    Updated code with following improvements: - Extracted methods from large functions - Link Extractors could not be applied because all the   data is extracted from one Json Response. - Added DocStrings in the spider.

--- WoolWorthsSpider/WoolWorthsSpider/spiders/woolworthspider.py
+++ WoolWorthsSpider/WoolWorthsSpider/spiders/woolworthspider.py
@@ -1,3 +1,6 @@
+"""
+Spider to extract products from WoolWorths.com
+"""
 # -*- coding: utf-8 -*-
 import json
 import scrapy
@@ -6,9 +9,16 @@ from WoolWorthsSpider.items import WoolworthsItem
 
 
 class WoolworthspiderSpider(scrapy.Spider):
+    """
+    Spider class to extract items from woolworths.com
+    """
     name = 'woolworthspider'
 
     def start_requests(self):
+        """
+        Initial url to fetch Json Object containing categories
+        :return: Yields Reuqest to fetch categories.
+        """
         cat_url = (
             'https://www.woolworths.com.au/apis/ui/'
             'PiesCategoriesWithSpecials/'
@@ -16,6 +26,12 @@ class WoolworthspiderSpider(scrapy.Spider):
         yield scrapy.Request(url=cat_url, callback=self.parse_categories)
 
     def parse_categories(self, response):
+        """
+        Traverses categories and sub categories in Json response
+        and yields requests to parse items.
+        :param response: response to parse categories from
+        :return: Yield POST requests to fetch items from
+        """
         categories = json.loads(response.body)['Categories']
         for category in categories:
             request_url = (
@@ -55,7 +71,49 @@ class WoolworthspiderSpider(scrapy.Spider):
                         callback=self.parse_items,
                     )
 
+    def parse_nutrition_info(self, attributes):
+        """
+        Extracts nutrition information from given attributes
+        :param attributes: dict containing nutritional information
+        :return: dict containing specific attributes
+        """
+        energy = None
+        carbohydrate = None
+        fat = None
+        protein = None
+        sugar = None
+        sodium = None
+        nutrition_info = attributes['nutritionalinformation']
+        if nutrition_info is not None:
+            values = json.loads(nutrition_info)['Attributes']
+            for index, _ in enumerate(values):
+                if index == 0:
+                    energy = values[index]['Value']
+                elif index == 1:
+                    carbohydrate = values[index]['Value']
+                elif index == 2:
+                    fat = values[index]['Value']
+                elif index == 3:
+                    protein = values[index]['Value']
+                elif index == 4:
+                    sugar = values[index]['Value']
+                elif index == 5:
+                    sodium = values[index]['Value']
+        return {
+            'energy': energy,
+            'carbohydrate': carbohydrate,
+            'fat': fat,
+            'protein': protein,
+            'sugar': sugar,
+            'sodium': sodium,
+        }
+
     def parse_items(self, response):
+        """
+        Parse item attributes and yield final product.
+        :param response: response object to parse
+        :return: Item details
+        """
         data = json.loads(response.body)
         bundles = data['Bundles']
         for bundle in bundles:
@@ -74,36 +132,5 @@ class WoolworthspiderSpider(scrapy.Spider):
             product['detail'] = attributes['description']
             product['ingredients'] = attributes['ingredients']
             product['allergens'] = attributes['allergencontains']
-
-            energy = None
-            carbohydrate = None
-            fat = None
-            protein = None
-            sugar = None
-            sodium = None
-            nutrition_info = attributes['nutritionalinformation']
-            if nutrition_info is not None:
-                values = json.loads(nutrition_info)['Attributes']
-                for index in range(len(values)):
-                    if index == 0:
-                        energy = values[index]['Value']
-                    elif index == 1:
-                        carbohydrate = values[index]['Value']
-                    elif index == 2:
-                        fat = values[index]['Value']
-                    elif index == 3:
-                        protein = values[index]['Value']
-                    elif index == 4:
-                        sugar = values[index]['Value']
-                    elif index == 5:
-                        sodium = values[index]['Value']
-
-            product['nutrition'] = {
-                'energy': energy,
-                'carbohydrate': carbohydrate,
-                'fat': fat,
-                'protein': protein,
-                'sugar': sugar,
-                'sodium': sodium,
-            }
+            product['nutrition'] = self.parse_nutrition_info(attributes)
             yield product
