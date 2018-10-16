0071c8ef913965609d3548e1855a145e11c4ff6f
Bilal Sajid <bilal.sajid@arbisoft.com>
2018-10-16 11:45:46 +0500

    Products url extraction through rules and link extractor

--- Isetan/isetan.py
+++ Isetan/isetan.py
@@ -7,18 +7,20 @@ from isetan.items import IsetanItem, IsetanItemLoader
 
 
 class IsetanSpider(CrawlSpider):
-    name = 'isetan'
-    allowed_domains = ['isetan.com.sg']
-    start_urls = ['http://isetan.com.sg/']
-    urls_css = [
+    name = "isetan"
+    allowed_domains = ["isetan.com.sg"]
+    start_urls = ["http://isetan.com.sg/"]
+    listings_css = [
         ".navPages-list .navPages-item",
-        ".pagination-item--next"]
+        ".pagination-item--next"
+        ]
+    products_css = [".custm-tags"]
+    extra_links = ["/workshops/"]
 
     rules = (
-        Rule(LinkExtractor(
-            restrict_css=url_css,
-            deny=("/workshops/")), callback="parse"),
-        Rule(LinkExtractor(restrict_css=(".custm-tags")),
+        Rule(LinkExtractor(restrict_css=listings_css, deny=extra_links),
+            callback="parse"),
+        Rule(LinkExtractor(restrict_css=products_css),
             callback="parse_product")
     )
 
@@ -26,13 +28,13 @@ class IsetanSpider(CrawlSpider):
         loader = IsetanItemLoader(item=IsetanItem(), response=response)
         loader.add_css("name", ".productView-title::text")
         loader.add_css("brand", ".product-brand::text")
-        loader.add_css("price", ".productView-price div .price::text")
+        loader.add_css("price", ".productView-price .price::text")
         loader.add_css("quantity", ".productView-info-value::text")
         loader.add_css("categories", ".breadcrumb-label::text")
         loader.add_css("description", "#tab-description p::text")
         loader.add_css("image_urls", ".productView-thumbnail a::attr(href)")
         loader.add_css("currency", "meta[property='product:price:currency']::attr(content)")
-        loader.add_css("product_type", "ul.breadcrumbs > li:nth-child(2) a::text")
+        loader.add_css("product_type", ".breadcrumb:nth-child(2) a::text")
         loader.add_value("url", response.url)
-        loader.add_value("website", "https://www.isetan.com.sg/")
+        loader.add_value("website", self.start_urls)
         return loader.load_item()
