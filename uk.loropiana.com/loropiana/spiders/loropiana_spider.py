4a11637073379a6274b05d5f0072015966c4531c
muhammadali286 <muhammad.al@arbisoft.com>
2018-10-17 17:39:55 +0500

    Add color name and outofstock status for skus. Change selectors. Improve code and accomplished other requsted changes

--- uk.loropiana.com/loropiana/spiders/loropiana_spider.py
+++ uk.loropiana.com/loropiana/spiders/loropiana_spider.py
@@ -1,7 +1,6 @@
 import json
 import re
 
-import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
@@ -9,98 +8,88 @@ from loropiana.items import LoropianaItem
 
 
 class LoropianaParse:
-    def parse_product(self, response):
-        item_trail = response.meta.get('trail', [])
-        item_trail.append((response.css('head title::text').extract_first().strip(), response.url))
-
+    def parse(self, response):
         item = LoropianaItem()
-        item['retailer_sku'] = self.retailer_sku(response)
-        item['trail'] = item_trail
+
+        item['retailer_sku'] = self.common_data(response)['id']
+        item['trail'] = response.meta.get('trail')
         item['category'] = self.category(response)
-        item['brand'] = response.css('.top-bar a::attr(title)').extract_first()
-        item['product_name'] = response.css('.product-info .t-h2::text').extract_first()
-        item['gender'] = self.gender(item.get('category'))
+        item['brand'] = self.common_data(response)['brand']
+        item['name'] = self.common_data(response)['name']
         item['care'] = self.care(response)
         item['description'] = self.description(response)
+        item['gender'] = self.gender(item['category'] + item['description'])
         item['image_urls'] = []
         item['url'] = response.url
         item['skus'] = []
 
-        color_codes = self.add_skus(response, item, meta={
-            'currency': self.currency(response),
-            'price': self.price(response)
-        })
+        requests = [response.follow(url=self.color_request_url(item['retailer_sku'], color), callback=self.parse_skus,
+                                    meta={'item': item, 'price': self.common_data(response)['price'],
+                                          'currency': self.currency(response)}) for color in
+                    self.color_codes(response)] + [
+                       response.follow(url=self.image_request_url(item['retailer_sku'], color),
+                                       callback=self.parse_image_urls, meta={'item': item}) for color in
+                       self.color_codes(response)]
 
-        yield scrapy.Request(url=self.image_detail_url(item.get('retailer_sku'), color_codes.pop()),
-                             callback=self.add_image_urls, meta={
-                'product_details': item,
-                'color_codes': color_codes
-            })
+        return self.next_request(item, requests)
 
-    def add_image_urls(self, response):
-        item = response.meta.get('product_details')
-        images_detail = json.loads(response.text)
-        color_codes = response.meta.get('color_codes')
-
-        for images in images_detail:
-            item['image_urls'] += [format.get('url') for format in images.get('formats') if
-                                   'LARGE' in format.get('url', '')]
-
-        if color_codes:
-            yield scrapy.Request(url=self.image_detail_url(item.get('retailer_sku'), color_codes.pop()),
-                                 callback=self.add_image_urls, meta={
-                    'product_details': item,
-                    'color_codes': color_codes
-                })
+    def next_request(self, item, resquests):
+        if resquests:
+            next_request = resquests.pop()
+            next_request.meta['resquests'] = resquests
+            return next_request
         else:
-            yield item
+            return item
 
-    def add_skus(self, response, item, meta):
-        skus = []
-        color_codes = []
-        for color in self.skus(response):
-            color_codes.append(color.get('code'))
-            for size in color['sizes']:
-                skus += [{'sku_id': size['variantCode'],
-                          'size': size['code'],
-                          'currency': meta['currency'],
-                          'price': meta['price']
-                          }]
-        item['skus'] += skus
-        return color_codes
+    def parse_skus(self, response):
+        raw_sku = json.loads(response.text)
+        item = response.meta['item']
+
+        for color in raw_sku:
+            for size in color.get('sizes', []):
+                sku = dict()
+                sku['sku_id'] = size['variantCode']
+                sku['color'] = color['description']
+                sku['price'] = response.meta['price']
+                sku['currency'] = response.meta['currency']
+                sku['size'] = 'One_size' if size['code'] == 'NR' else size['code']
+                if size['stock']['stockLevelStatus']['code'] == "outOfStock":
+                    sku['outOfStock'] = True
+
+                item['skus'] += [sku]
+
+        return self.next_request(item, response.meta['resquests'])
+
+    def parse_image_urls(self, response):
+        item = response.meta['item']
+        raw_images = json.loads(response.text)
+
+        for images in raw_images:
+            item['image_urls'] += [format['url'] for format in images.get('formats') if 'LARGE' in format.get('url')]
+
+        return self.next_request(item, response.meta['resquests'])
+
+    def color_codes(self, response):
+        raw_colors = json.loads(response.css('#js-pdp-initial-variants::attr(value)').extract_first())
+        return [color.get('code') for color in raw_colors]
 
     def gender(self, details):
-        if 'Men' in details:
-            gender = 'men'
-        elif 'Women' in details:
-            gender = 'Women'
-        elif 'girl' in details:
-            gender = 'girl'
-        elif 'boy' in details:
-            gender = 'boy'
-        elif 'girl or boy' in details and 'baby' in details or 'Children' in details:
-            gender = 'unisex-kids'
-        else:
-            gender = 'unisex-adults'
+        gender = 'unisex-adults'
+        genders = {'Men': 'men', 'girl': 'girl', 'Women': 'women', 'boy': 'boy', 'girl or boy': 'unisex-kids',
+                   'Children': 'unisex-kids'}
+
+        for key in genders.keys():
+            if key in details:
+                gender = genders[key]
+
         return gender
 
-    def retailer_sku(self, response):
-        raw_retailer_sku = response.css('.t-product-copy::text').extract_first()
-        return re.findall(r'[A-Z0-9]+', raw_retailer_sku)[0]
-
     def category(self, response):
-        sub_category = response.css('.t-product-breadcrumps a::text').extract_first()
-        main_category = response.css(
-            '.t-product-breadcrumps::text').extract_first().strip().split('>')
-        main_category = [category.strip()
-                         for category in main_category if category != '']
-        if sub_category:
-            main_category.append(sub_category.strip())
-        return main_category
+        raw_categoty = self.common_data(response)['category'].split('/')
+        return [category for category in raw_categoty if category != '']
 
     def description(self, response):
-        sub_description = response.css('.t-pdp-page-section-title.title ~ p::text').extract_first()
-        return f"{response.css('.t-caption::text').extract_first()} {sub_description}"
+        return response.css('.t-caption::text, .t-pdp-page-section-title.title ~ p::text').extract()
 
     def care(self, response):
         raw_care = response.css('button[aria-label="Care & Maintenance"] ~ .content ::text').extract()
@@ -110,17 +99,13 @@ class LoropianaParse:
         raw_currency = response.css('.t-product-cta-price::text').extract_first()
         return re.findall('[A-Z]+', raw_currency)[0]
 
-    def price(self, response):
-        raw_price = response.css('.t-product-cta-price::text').extract_first()
-        return int(''.join(re.findall('[0-9]+', raw_price)))
+    def common_data(self, response):
+        return json.loads(response.css('#js-gtm-pdp-landing::attr(data-gtm-info)').extract_first())
 
-    def color_code(self, response):
-        return response.css('.colourvalue .t-caps.t-grey::text').extract_first()[1:-1]
+    def color_request_url(self, retailer_sku, color_code):
+        return f"https://uk.loropiana.com/en/api/pdp/product-variants?articleCode={retailer_sku}&colorCode={color_code}"
 
-    def skus(self, response):
-        return json.loads(response.css('#js-pdp-initial-variants::attr(value)').extract_first())
-
-    def image_detail_url(self, retailer_sku, color_code):
+    def image_request_url(self, retailer_sku, color_code):
         return f"https://uk.loropiana.com/en/api/pdp/get-images?articleCode={retailer_sku}&colorCode={color_code}"
 
 
@@ -130,11 +115,14 @@ class LoropianaCrawler(CrawlSpider):
     start_urls = ['https://uk.loropiana.com/en']
     loropiana_parse = LoropianaParse()
 
-    rules = [Rule(LinkExtractor(restrict_css='[class=menu-mask]'), callback='parse'),
-             Rule(LinkExtractor(restrict_css='.category-results-grid'), callback=loropiana_parse.parse_product)]
+    listing_css = '[class=menu-mask]'
+    catagorise_css = '.category-results-grid'
+
+    rules = [Rule(LinkExtractor(restrict_css=[listing_css]), callback='parse'),
+             Rule(LinkExtractor(restrict_css=[catagorise_css]), callback=loropiana_parse.parse)]
 
     def parse(self, response):
-        req = super().parse(response)
-        for r in req:
-            r.meta['trail'] = [(response.css('head title::text').extract_first().strip(), response.url)]
-            yield r
+        trail = [(response.css('head title::text').extract_first().strip(), response.url)]
+        for req in super().parse(response):
+            req.meta['trail'] = trail
+            yield req
