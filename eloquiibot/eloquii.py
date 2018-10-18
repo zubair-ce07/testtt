b7df05b15587c4dc52f86cb7b277f852044fef68
Rana Aurangzaib <rana.aurangzaib@arbisoft.com>
2018-10-18 18:04:01 +0500

    Fixed skus and images_urls method added json call

--- eloquiibot/eloquii.py
+++ eloquiibot/eloquii.py
@@ -1,4 +1,6 @@
+import json
 import re
+import scrapy
 
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Rule, CrawlSpider
@@ -10,21 +12,23 @@ class EloquiiSpider(CrawlSpider):
     name = 'eloquii'
     allowed_domains = ['www.eloquii.com']
     start_urls = ['https://www.eloquii.com']
+    merch_map = [
+        ("limited edition", "Limited Edition"),
+        ("special edition", "Special Edition")
+    ]
 
     listings_css = ['#nav_menu', '.row.justify-content-center.mt-5']
-    products_css = ".product-images a"
-    merch_map = [("limited edition", "Limited Edition"),
-                 ("special edition", "Special Edition"),
-                 ("discounted", "Discount"),
-                 ("COMINGSOON", "COMING SOON")]
+    products_css = ".product-images"
 
     rules = (
-        Rule(LinkExtractor(restrict_css=listings_css), callback='parse'),
-        Rule(LinkExtractor(restrict_css=products_css), callback='parse_product')
+        Rule(LinkExtractor(deny=r'.*\.html.*', restrict_css=listings_css), callback='parse'),
+        Rule(LinkExtractor(allow=r'.*\.html.*', restrict_css=products_css), callback="parse_product")
     )
 
     def parse_product(self, response):
         product = EloquiiProduct()
+        if self.is_available(response):
+            return None
         product["product_id"] = self.product_id(response)
         product["brand"] = 'Eloquii'
         product["name"] = self.product_name(response)
@@ -32,12 +36,17 @@ class EloquiiSpider(CrawlSpider):
         product["description"] = self.product_description(response)
         product["url"] = response.url
         product["image_urls"] = self.image_urls(response)
-        product["out_of_stock"] = self.out_of_stock(response)
+        if self.is_out_of_stock(response):
+            product["out_of_stock"] = True
         product["skus"] = {}
         product["merch_info"] = self.merch_info(response, product)
-        if self.is_coming_soon(product["merch_info"]) and not product["out_of_stock"]:
-            product["skus"] = self.skus(response)
-        yield product
+        json_url = "https://www.eloquii.com/on/demandware.store/Sites-eloquii-Site/default/" \
+                   + f"Product-GetVariants?pid={product['product_id']}&format=json"
+        yield scrapy.Request(
+            url=json_url,
+            callback=self.skus,
+            meta={"product": product}
+        )
 
     def product_id(self, response):
         css = "#yotpo-bottomline-top-div::attr(data-product-id)"
@@ -56,42 +65,43 @@ class EloquiiSpider(CrawlSpider):
         return response.css(css).extract_first()
 
     def image_urls(self, response):
-        css = ".productthumbnails img::attr(src),.productimagearea img::attr(src)"
-        image_urls = list(set(response.css(css).extract()))
-        return [response.urljoin(i.replace('small', 'large', 1)) for i in image_urls]
+        images_script = response.css("#bt_pdp_main > script::text").extract_first()
+        images_script = images_script.replace("\n", "").replace("\t", "").replace("\r", "")
+        images_data = re.findall(r'\"large\" :\[.*?\]',images_script)
+        return [re.findall(r'{\"url\" : \"(.*?\.jpg)\",.*?}', images) for
+                images in images_data]
 
     def merch_info(self, response, product):
         soup = ' '.join(product["name"] + product["description"]).lower()
-        merch_info = [i[1] for i in self.merch_map if i[0] in soup]
-        if bool(re.search(r'\'COMINGSOON\': (true)', response.text)):
-            merch_info.append(self.merch_map[3][1])
+        merch_info = [merch for merch_str, merch in self.merch_map if merch_str in soup]
         return merch_info
 
-    def is_coming_soon(self, merch_info):
-        return not "COMING SOON" not in merch_info
+    def is_available(self, response):
+        return bool(re.search(r'\'COMINGSOON\': (true)', response.text))
 
-    def out_of_stock(self, response):
+    def is_out_of_stock(self, response):
         availability = response.css("[property='og:availability']::attr(content)").extract_first()
         return not availability == "IN_STOCK"
 
     def skus(self, response):
+        product = response.meta["product"]
         skus = {}
-        colours = response.css(".swatchesdisplay a::attr(title)").extract()
-        sizes_css = "#product_detail_size_drop_down_expanded a::attr(title)"
-        sizes = response.css(sizes_css).extract()[1:]
-        for colour in colours:
-            for size in sizes:
-                sku = self.product_pricing(response)
-                sku['size'] = size
-                sku['colour'] = colour
-                skus[colour + "_" + size] = sku.copy()
-        return skus
+        json_data = json.loads(response.text)
+        json_data = json_data["variations"]["variants"]
+        for data in json_data:
+            sku = self.product_pricing(data)
+            sku['color'] = data["attributes"]["colorCode"]
+            sku['size'] = data["attributes"]["size"]
+            sku['in_stock'] = data["inStock"]
+            skus['_'.join(data["attributes"].values())] = sku.copy()
+        product["skus"] = skus
+        yield product
 
-    def product_pricing(self, response):
+    def product_pricing(self, json_data):
         prices = {}
-        previous_price = response.css(".priceGroup strike::text").re_first(r'(\d+\.\d+)')
-        prices["price"] = response.css(".priceGroup span::text").re_first(r'(\d+\.\d+)')
-        prices["currency"] = response.css("[property='og:price:currency']::attr(content)").extract_first()
-        if previous_price:
+        previous_price = json_data["pricing"]["standard"]
+        prices["price"] = json_data["pricing"]["sale"]
+        prices["currency"] = '$'
+        if previous_price != prices["price"]:
             prices["previous_price"] = previous_price
         return prices
