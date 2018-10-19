3c910f6d03494ed38f68da50bae68a78ec547c55
m-furqan <furqan.nasir@arbisoft.com>
2018-10-19 15:44:41 +0500

    removed unnecessory anchor tags, changed function names and used string Templates

--- woolrich_spider_code/woolrich_products.py
+++ woolrich_spider_code/woolrich_products.py
@@ -3,66 +3,71 @@ import scrapy
 import json
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from string import Template
 
 
 class WoolrichSpider(CrawlSpider):
     name = 'woolrich_products'
+    product_attribute_url_t = Template('/remote/v1/product-attributes/$sku_id')
     allowed_domains = ['woolrich.com']
     start_urls = ['https://www.woolrich.com/']
 
     rules = (
-        Rule(LinkExtractor(deny=('.php$'), restrict_css=(
-            ['#primary > ul > li > a', '.pagination-item--next > a']))),
-        Rule(LinkExtractor(restrict_css=('#product-listing-container .card')),
-             callback='parse_products'),
+        Rule(LinkExtractor(allow=('.com/([a-z-]+$)|(.*page)'),
+                           restrict_css=(['#primary', '.pagination-item--next']))),
+        Rule(LinkExtractor(restrict_css=('.card')), callback='parse_products'),
     )
 
     def parse_products(self, response):
+        care_css = '#features-content > li::text'
+        category_css = '.breadcrumb > a::text'
+        description_css = '#details-content::text'
+        image_url_css = '.productView-image img::attr(src)'
+        name_css = '.productView-title::text'
+        retailer_sku_css = 'input[name="product_id"]::attr(value)'
         product = {
             'brand': 'woolrich',
-            'care': response.css('#features-content > li::text').extract(),
-            'category': response.css('.breadcrumb > a::text')[-1].extract(),
-            'description': response.css('#details-content::text').extract_first(),
-            'name': response.css('.productView-title::text').extract_first(),
-            'retailer_sku': response.css('input[name="product_id"]::attr(value)').extract_first(),
+            'care': response.css(care_css).extract(),
+            'category': response.css(category_css)[-1].extract(),
+            'description': response.css(description_css).extract_first(),
+            'image_url': response.css(image_url_css).extract(),
+            'name': response.css(name_css).extract_first(),
+            'retailer_sku': response.css(retailer_sku_css).extract_first(),
             'skus': {},
             'url': response.url
         }
         response.meta['product'] = product
-        my_meta = self.extract_attributes_and_values(response)
-        url = '/remote/v1/product-attributes/'
-        url += response.meta.get('product').get('retailer_sku')
+        meta = self.extract_attributes_with_values(response)
+        url = self.product_attribute_url_t.substitute(
+            sku_id=product['retailer_sku'])
         params = {
-            'attribute['+str(my_meta['color_attribute'])+']': list(my_meta['color_values'].keys())[0]
+            'attribute['+str(meta['color_attribute'])+']': list(meta['color_values'].keys())[0]
         }
-        my_meta['curr_color_value'] = list(my_meta['color_values'].keys())[0]
+        meta['curr_color_value'] = list(meta['color_values'].keys())[0]
         yield scrapy.Request(
             url=response.urljoin(url),
             callback=self.parse_colors,
             method='Post',
             body=json.dumps(params),
-            meta=my_meta
+            meta=meta
         )
 
-    def extract_attributes_and_values(self, response):
-        color_attribute = response.css(
-            '.form-option-swatch::attr(data-swatch-id)').extract_first()
-        color_values = self.find_color_values_and_names(response)
-        size_values = self.find_size_values_and_names(response)
-        my_meta = {
+    def extract_attributes_with_values(self, response):
+        color_attribute_css = '.form-option-swatch::attr(data-swatch-id)'
+        color_attribute = response.css(color_attribute_css).extract_first()
+        color_values = self.color_map(response)
+        size_values = self.size_map(response)
+        meta = {
             'product': response.meta['product'],
             'color_attribute': color_attribute,
             'color_values': color_values,
             'size_values': size_values,
         }
-        fit_attribute = response.xpath(
-            '//div[@class="form-field" and @data-product-attribute="set-rectangle"]/input/@name'
-        ).re_first(r'[0-9]+')
-        if fit_attribute is not None:
-            fit_values = self.find_fit_values_and_names(
-                response, fit_attribute)
-            my_meta['fit_values'] = fit_values
-        return my_meta
+        fit_attribute_xpath = '//div[@class="form-field" and @data-product-attribute="set-rectangle"]/input/@name'
+        fit_attribute = response.xpath(fit_attribute_xpath).re_first(r'[0-9]+')
+        if fit_attribute:
+            meta['fit_values'] = self.fit_map(response, fit_attribute)
+        return meta
 
     def parse_colors(self, response):
         color_attribute = response.meta.get('color_attribute')
@@ -73,10 +78,10 @@ class WoolrichSpider(CrawlSpider):
         curr_color = response.meta.get('curr_color_value')
         self.update_skus(response, color_values.get(curr_color))
         del color_values[curr_color]
-        if len(color_values) > 0:
-            url = '/remote/v1/product-attributes/'
-            url += response.meta.get('product').get('retailer_sku')
-            my_meta = {
+        if color_values:
+            url = self.product_attribute_url_t.substitute(
+                sku_id=product['retailer_sku'])
+            meta = {
                 'product': response.meta['product'],
                 'color_attribute': color_attribute,
                 'color_values': color_values,
@@ -86,13 +91,13 @@ class WoolrichSpider(CrawlSpider):
             curr_color = list(color_values.keys())[0]
             params = {
                 'attribute['+str(color_attribute)+']': curr_color}
-            my_meta['curr_color_value'] = curr_color
+            meta['curr_color_value'] = curr_color
             yield scrapy.Request(
                 url=response.urljoin(url),
                 callback=self.parse_colors,
                 method='Post',
                 body=json.dumps(params),
-                meta=my_meta,
+                meta=meta,
                 dont_filter=True
             )
         else:
@@ -103,16 +108,17 @@ class WoolrichSpider(CrawlSpider):
         size_values = response.meta.get('size_values')
         fit_values = response.meta.get('fit_values')
         resp_dict = json.loads(response.body)
-        if size_values is None:
+        if not size_values:
             self.update_without_size(response, color, resp_dict)
-        elif fit_values is None:
+        elif not fit_values:
             self.update_without_fit(response, color, resp_dict, size_values)
         else:
             self.update_with_size_and_fit(
                 response, color, resp_dict, size_values, fit_values)
 
     def update_without_size(self, response, color, resp_dict):
-        response.meta['product']['skus'][color] = {
+        sku_key = color
+        response.meta['product']['skus'][sku_key] = {
             'color': color.replace('-', ' '),
             'currency': 'USD',
             'price': resp_dict['data']['price']['without_tax']['value'],
@@ -122,18 +128,17 @@ class WoolrichSpider(CrawlSpider):
         in_stock_attributes = resp_dict['data'].get('in_stock_attributes')
         for size_value in size_values:
             size = size_values.get(size_value)
-            response.meta['product']['skus'][color+'_'+size] = {
+            sku_key = color + '_' + size
+            response.meta['product']['skus'][sku_key] = {
                 'color': color.replace('-', ' '),
                 'currency': 'USD',
                 'price': resp_dict['data']['price']['without_tax']['value'],
                 'size': size,
             }
             if int(size_value) in in_stock_attributes:
-                response.meta['product']['skus'][color +
-                                                 '_'+size]['in_stock'] = True
+                response.meta['product']['skus'][sku_key]['in_stock'] = True
             else:
-                response.meta['product']['skus'][color +
-                                                 '_'+size]['in_stock'] = False
+                response.meta['product']['skus'][sku_key]['in_stock'] = False
 
     def update_with_size_and_fit(self, response, color,
                                  resp_dict, size_values, fit_values):
@@ -142,7 +147,8 @@ class WoolrichSpider(CrawlSpider):
             size = size_values.get(size_value)
             for fit_value in fit_values:
                 fit = fit_values.get(fit_value)
-                response.meta['product']['skus'][color+'_'+size+'_'+fit] = {
+                sku_key = color + '_' + size + '_' + fit
+                response.meta['product']['skus'][sku_key] = {
                     'color': color.replace('-', ' '),
                     'fit': fit,
                     'currency': 'USD',
@@ -151,15 +157,14 @@ class WoolrichSpider(CrawlSpider):
                 }
                 if int(size_value) in in_stock_attributes and \
                         int(fit_value) in in_stock_attributes:
-                    response.meta['product']['skus'][color +
-                                                     '_'+size+'_'+fit]['in_stock'] = True
+                    response.meta['product']['skus'][sku_key]['in_stock'] = True
                 else:
-                    response.meta['product']['skus'][color +
-                                                     '_'+size+'_'+fit]['in_stock'] = False
+                    response.meta['product']['skus'][sku_key]['in_stock'] = False
 
-    def find_color_values_and_names(self, response):
+    def color_map(self, response):
+        color_attribute_values_css = '.form-option-swatch::attr(data-product-attribute-value)'
         color_attribute_values = response.css(
-            '.form-option-swatch::attr(data-product-attribute-value)').extract()
+            color_attribute_values_css).extract()
         color_attributes = dict()
         for color_value in color_attribute_values:
             color_attributes[str(color_value)] = response.xpath(
@@ -167,9 +172,10 @@ class WoolrichSpider(CrawlSpider):
                 val=color_value).extract_first()
         return color_attributes
 
-    def find_size_values_and_names(self, response):
+    def size_map(self, response):
+        size_attribute_values_css = '.product-size .form-option::attr(data-product-attribute-value)'
         size_attribute_values = response.css(
-            '.product-size .form-option::attr(data-product-attribute-value)').extract()
+            size_attribute_values_css).extract()
         size_attributes = {}
         for size_value in size_attribute_values:
             size_attributes[str(size_value)] = response.xpath(
@@ -177,7 +183,7 @@ class WoolrichSpider(CrawlSpider):
                 val=size_value).extract_first()
         return size_attributes
 
-    def find_fit_values_and_names(self, response, fit_attribute):
+    def fit_map(self, response, fit_attribute):
         fit_attribute_values = response.xpath(
             '//input[@name=$val]/@value', val='attribute['+str(fit_attribute)+']'
         ).extract()
@@ -186,4 +192,3 @@ class WoolrichSpider(CrawlSpider):
             fit_attributes[str(fit_value)] = response.xpath(
                 '//label[@data-product-attribute-value=$val]/span/text()', val=fit_value).extract_first()
         return fit_attributes
-
