113e53dc6c51ec611890471623f0f895e10e0d59
Fahad Shawal <fahad@A00694.arbisoft.com>
2018-10-23 12:45:16 +0500

    Changes incorporated in watherman.py
    -use of csv.DictReader for reading data from file
    -change the file format for reading the data from files

--- weatherman.py
+++ weatherman.py
@@ -4,6 +4,8 @@ import csv
 import glob
 import argparse
 
+from collections import OrderedDict
+
 from datetime import datetime
 
 
@@ -12,84 +14,73 @@ class Reporting:
     def __init__(self):
         pass
     
-    def a_type_high_temp(self,high_temp, head):
-        
+    def a_type_high_temp(self,high_temp):
         print("Highest Average : " , 
-            high_temp[head.index('Max TemperatureC')], "C")
+            high_temp['Mean TemperatureC'], "C")
         
-    def a_type_low_temp(self,low_temp, head):
+    def a_type_low_temp(self,low_temp):
         print("Lowest Average : " , 
-                low_temp[head.index('Min TemperatureC')], "C")
+                low_temp['Mean TemperatureC'], "C")
     
-    def a_type_high_humid(self, mean_humidity, head):
+    def a_type_high_humid(self, mean_humidity):
         print("Average Mean Humidity: " , 
-                mean_humidity[head.index(' Mean Humidity')]+ "%")
+                mean_humidity[' Mean Humidity']+ "%")
 
-    def a_type_report(self, complete_data):
-        head = complete_data['header']
+    def a_type_report(self, data):
         
         high_temp = self.extract_needed_row(
-            complete_data, 'Mean TemperatureC', True)
+            data, 'Mean TemperatureC', True)
         low_temp = self.extract_needed_row(
-            complete_data, 'Mean TemperatureC', False)
+            data, 'Mean TemperatureC', False)
         mean_humidity = self.extract_needed_row(
-            complete_data, ' Mean Humidity', True)
+            data, ' Mean Humidity', True)
         
-        self.a_type_high_temp(mean_humidity, head)
-        self.a_type_low_temp(low_temp, head)
-        self.a_type_high_humid(high_temp, head)
-        
-        
-    def c_type_report(self, complete_data):
-        head = complete_data['header']
-        data = complete_data['data']
-        max_index = head.index('Max TemperatureC')
-        min_index = head.index('Min TemperatureC')
-        day_index = head.index('PKT')
+        self.a_type_high_temp(high_temp)
+        self.a_type_low_temp(low_temp)
+        self.a_type_high_humid(mean_humidity)
+              
+    def c_type_report(self, data):
         
         for row in data:    
-            to_convert = '0' + row[max_index]
+            to_convert = '0' + row['Max TemperatureC']
             file_value = int(to_convert)
-            print(row[day_index], end='')
+            print(row['PKT'], end='')
             for value in range(file_value):
                 print ('\033[1;31m+\033[1;m', end = '')
             print(' ',file_value,"C")  
             
-            to_convert = '0' + row[min_index]
+            to_convert = '0' + row['Min TemperatureC']
             file_value = int(to_convert)
-            print(row[day_index], end='')
+            print(row['PKT'], end='')
             for value in range(file_value):
                 print ('\033[1;34m+\033[1;m', end = '')
             print(' ',file_value,"C")  
             
-    def e_type_report(self, complete_data):
-        head = complete_data['header']
+    def e_type_report(self, data):
         
-        high_temp = self.extract_needed_row(complete_data, 'Max TemperatureC', True)
-        temperature = high_temp[head.index('Max TemperatureC')]
-        date_to_parse = high_temp[head.index('PKT')]
+        high_temp = self.extract_needed_row(data, 'Max TemperatureC', True)
+        temperature = high_temp['Max TemperatureC']
+        date_to_parse = high_temp['PKT']
         date = datetime.strptime(date_to_parse, "%Y-%m-%d")
         print("Highest : " , temperature, "C",  date.strftime("%B") ,  date.day)
         
-        low_temp = self.extract_needed_row(complete_data, 'Min TemperatureC', False)
-        temperature = low_temp[head.index('Min TemperatureC')]
-        date_to_parse = low_temp[head.index('PKT')]
+        low_temp = self.extract_needed_row(data, 'Min TemperatureC', False)
+        temperature = low_temp['Min TemperatureC']
+        date_to_parse = low_temp['PKT']
         date = datetime.strptime(date_to_parse, "%Y-%m-%d")
         print("Lowest : " , temperature, "C",  date.strftime("%B") ,  date.day)
         
-        mean_humidity = self.extract_needed_row(complete_data, ' Mean Humidity', True)
-        temperature = mean_humidity[head.index(' Mean Humidity')]
-        date_to_parse = mean_humidity[head.index('PKT')]
+        mean_humidity = self.extract_needed_row(data, ' Mean Humidity', True)
+        temperature = mean_humidity[' Mean Humidity']
+        date_to_parse = mean_humidity['PKT']
         date = datetime.strptime(date_to_parse, "%Y-%m-%d")
         print("Average Mean Humidity: " , temperature, "C",  date.strftime("%B") ,  date.day)
 
-    def extract_needed_row(self, complete_data, col_name, reverse_flag):
-        head = complete_data['header']
-        data = complete_data['data']
-        temp_index = head.index(col_name)
-        data.sort(key=lambda x: x[temp_index], reverse=reverse_flag)
+    def extract_needed_row(self, data, col_name, reverse_flag):
+        data.sort(key=lambda x: x[col_name], reverse=reverse_flag)
+        
         if not reverse_flag:
-            data = [row for row in data if row[temp_index] != '']
+            data = [row for row in data if row[col_name] != '']
         return data[0]
 
 
@@ -104,41 +95,40 @@ def argument_handler():
     report = Reporting()
     if args.a:
         file_names = files_to_read('a', args.a, args.path)
-        complete_data = reading_file(file_names)
-        report.a_type_report(complete_data)
+        data = reading_file(file_names)
+        report.a_type_report(data)
     
     if args.c:
         file_names = files_to_read('c', args.c, args.path)
-        complete_data = reading_file(file_names)
-        report.c_type_report(complete_data)
+        data = reading_file(file_names)
+        report.c_type_report(data)
     
     if args.e:
         file_names = files_to_read('e', args.e, args.path)
-        complete_data = reading_file(file_names)
-        report.e_type_report(complete_data)
+        data = reading_file(file_names)
+        report.e_type_report(data)
     
 
 def reading_file(file_names):
     data = []
     for file in file_names:
          with open(file, newline='') as csvfile:
-            file_data = csv.reader(csvfile)
-            header = next(file_data)
+            file_data = csv.DictReader(csvfile)
             for row in file_data:
                 data.append(row) 
-    
-    complete_data = {'header': header, 'data':data}
-    return complete_data
+
+    return data
 
 
 def files_to_read(operation, file_name, file_path):
+    pattren = '*{}_{}*.txt'
+    
     if operation == 'a' or operation == 'c':
-        file_name += '/1'
-        pattren = "*" + file_name.split('/')[0] + "_"\
-                + datetime.strptime(file_name, "%Y/%m/%d").strftime('%b')\
-                + "*.txt"
+        file_month = datetime.strptime(file_name, "%Y/%m").strftime('%b')
+        pattren = pattren.format(file_name.split('/')[0], file_month)
     elif operation == 'e':
-        pattren = "*"+ file_name + "*.txt"
+        pattren = pattren.format(file_name, '')
+    
     return glob.glob(file_path + pattren)
 
 
