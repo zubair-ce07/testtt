32a03544f5c4126088999a34ad442f5bff908e84
muhammadali286 <muhammad.al@arbisoft.com>
2018-10-23 14:43:27 +0500

    Accomplished requested changes

--- fredperry_spider.py
+++ fredperry_spider.py
@@ -5,13 +5,19 @@ from fredperry.items import FredperryItem
 
 
 class FredperryParse:
+    visited_products = set()
+
     gender_map = {'women': 'women', 'men': 'men', 'kid': 'unisex-kids'}
-    category_map = {'women': 'women', 'men': 'men', 'kid': 'kids'}
 
     def parse(self, response):
+        product_id = self.retailer_sku(response)
+
+        if self.id_exists(product_id):
+            return
+
         item = FredperryItem()
 
-        item['retailer_sku'] = self.retailer_sku(response)
+        item['retailer_sku'] = product_id
         item['brand'] = self.brand(response)
         item['name'] = self.name(response)
         item['trail'] = response.meta['trail']
@@ -53,22 +59,21 @@ class FredperryParse:
                       'sku_id': self.retailer_sku(response),
                       'currency': self.currency(response)}
 
-        if self.colour(response):
-            common_sku['colour'] = self.colour(response)
-            common_sku['sku_id'] += '_' + common_sku['colour']
+        colour = self.colour(response)
 
-        for size in self.in_stock_sizes(response):
+        if colour:
+            common_sku['colour'] = colour
+            common_sku['sku_id'] += f"_{common_sku['colour']}"
+
+        out_stock_sizes = self.out_stock_sizes(response)
+
+        for size in self.sizes(response):
             sku = common_sku.copy()
             sku['size'] = size
-            sku['sku_id'] += '_' + size
+            sku['sku_id'] += f"_{size}"
 
-            skus += [sku]
-
-        for size in self.out_stock_sizes(response):
-            sku = common_sku.copy()
-            sku['size'] = size
-            sku['out_of_stock'] = True
-            sku['sku_id'] += '_' + size
+            if size in out_stock_sizes:
+                sku['out_of_stock'] = True
 
             skus += [sku]
 
@@ -79,13 +84,13 @@ class FredperryParse:
 
         for url in self.colour_urls(response):
             requests.append(response.follow(
-                url=url, callback=self.parse_colours, dont_filter=True))
+                url, callback=self.parse_colours, dont_filter=True))
 
         return requests
 
     def colour_urls(self, response):
-        css = '.colour-swatches a::attr(href)'
-        return response.css(css).extract()[1:]
+        css = '.colour-swatches :not(.active) > a::attr(href)'
+        return response.css(css).extract()
 
     def brand(self, response):
         css = 'head title::text'
@@ -125,16 +130,11 @@ class FredperryParse:
         return response.css(css).extract_first()
 
     def category(self, response):
-        soup = ' '.join([trail[0] for trail in response.meta['trail']]).lower()
+        return [t for t, _ in response.meta['trail']]
 
-        for category_str, category in self.category_map.items():
-            if category_str.lower() in soup:
-                return [category]
-
-    def in_stock_sizes(self, response):
-        path = "//li[contains(@class, 'option') and \
-        not(contains(@class, 'notify'))]/a/@name"
-        return response.selector.xpath(path).extract()
+    def sizes(self, response):
+        css = '#configurable_swatch_size a::attr(name)'
+        return response.css(css).extract()
 
     def out_stock_sizes(self, response):
         path = "//li[contains(@class, 'notify')]/a/@name"
@@ -145,17 +145,23 @@ class FredperryParse:
         return response.css(css).extract()
 
     def description(self, response):
-        css = '.std ::text'
-        return response.css(css).extract()
+        return response.css('.std ::text').extract()
 
     def gender(self, response):
-        soup = ' '.join([trail[0] for trail in response.meta['trail']]).lower()
+        soup = ' '.join([t for t, _ in response.meta['trail']]).lower()
 
         for gender_str, gender in self.gender_map.items():
             if gender_str.lower() in soup:
                 return gender
 
-        return 'unisex'
+        return 'Unisex Adults'
+
+    def id_exists(self, product_id):
+
+        if product_id in self.visited_products:
+            return True
+
+        self.visited_products.add(product_id)
 
 
 class FredperryCrawler(CrawlSpider):
@@ -170,8 +176,6 @@ class FredperryCrawler(CrawlSpider):
     rules = [Rule(LinkExtractor(restrict_css=listing_css), callback='parse'),
              Rule(LinkExtractor(restrict_css=product_css), callback='parse_item')]
 
-    visited_products = set()
-
     def parse(self, response):
         trail = [(response.css('head title::text').extract_first(), response.url)]
         for req in super().parse(response):
@@ -179,11 +183,4 @@ class FredperryCrawler(CrawlSpider):
             yield req
 
     def parse_item(self, response):
-        retailer_sku = self.fredperry_parse.retailer_sku(response)
-
-        if retailer_sku in self.visited_products:
-            return
-
-        self.visited_products.add(retailer_sku)
-
         return self.fredperry_parse.parse(response)
