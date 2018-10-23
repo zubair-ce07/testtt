fe24ca78dbe5152608660b6a8d40c742e93f7e72
Rana Aurangzaib <rana.aurangzaib@arbisoft.com>
2018-10-23 16:11:04 +0500

    Updated skus method

--- eloquiibot/eloquii.py
+++ eloquiibot/eloquii.py
@@ -107,7 +107,7 @@ class EloquiiSpider(CrawlSpider):
             sku["size"] = size
 
             if not raw_sku["inStock"]:
-                sku["out_of_stock"] = raw_sku["inStock"]
+                sku["out_of_stock"] = True
 
             skus[f"{sku['colour']}_{sku['size']}"] = sku
         return skus
