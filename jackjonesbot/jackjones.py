2c75230345ae9ef0a51f7cb7ba056885fc4aa1fe
Rana Aurangzaib <rana.aurangzaib@arbisoft.com>
2018-10-24 12:46:56 +0500

    Updated and fixed skus

--- jackjonesbot/jackjones.py
+++ jackjonesbot/jackjones.py
@@ -1,144 +1,123 @@
 # -*- coding: utf-8 -*-
-import re
 import json
+import re
+
 import scrapy
 
 from jackjonesbot.items import JackjonesProduct
 
 
 class JackjonesSpider(scrapy.Spider):
-    name = 'jackjones'
-    allowed_domains = ['jackjones.com.cn']
-    start_urls = ['https://www.jackjones.com.cn/assets/pc/JACKJONES/nav.json']
-    token = "eyJqdGkiOiI4Ulg2dDViRHlHIiwiaWF0IjoxNTM5MjUzMzc1LCJjaGFubmVsI" \
-        + "joiNiJ9.WznZGroP1OD5EI8E6UR5mvEOr2K_nTnKgNuGYNQq7H8"
+    name = "jackjones"
+    allowed_domains = ["jackjones.com.cn"]
+    start_urls = ["https://www.jackjones.com.cn/api/service/init?channel=6"]
+
+    token = ""
+    category_urls_req = "https://www.jackjones.com.cn/assets/pc/JACKJONES/nav.json"
+    category_url_t = "https://www.jackjones.com.cn/api/goods/{}&currentpage=1" \
+                     "&sortDirection=desc&sortType=1"
+    product_url_t = "https://www.jackjones.com.cn/detail/JACKJONES/{}.json"
+    category_urls = []
 
     def parse(self, response):
-        category_urls = self.get_all_category_urls(response)
-        processed_cate_urls = self.process_category_url(category_urls)
-        for category in processed_cate_urls:
-            yield scrapy.Request(
-                url=category,
-                headers={"token": str(self.token)},
-                callback=self.parse_category
-            )
+        raw_token = json.loads(response.text)
+        self.token = raw_token["data"]["token"]
+        yield scrapy.Request(url=self.category_urls_req, callback=self.parse_category_urls)
 
-    def get_all_category_urls(self, response):
-        category_links = []
-        json_data = json.loads(response.text)
-        super_nodes = json_data["data"]
-        for super_node in super_nodes:
-            if bool(
-                    re.search(r'.*goodsList.*', super_node["navigationUrl"])
-            ):
-                category_links.append(super_node["navigationUrl"])
-            parent_nodes = super_node["list"]
-            for parent_node in parent_nodes:
-                if bool(
-                        re.search(r'.*goodsList.*',
-                                  parent_node["navigationUrl"])
-                ):
-                    category_links.append(parent_node["navigationUrl"])
-                sub_nodes = parent_node["list"]
-                for sub_node in sub_nodes:
-                    if bool(
-                            re.search(r'.*goodsList.*',
-                                      sub_node["navigationUrl"])
-                    ):
-                        category_links.append(sub_node["navigationUrl"])
-        return category_links
+    def parse_category_urls(self, response):
+        raw_category_urls = json.loads(response.text)
+        raw_category_urls = raw_category_urls["data"]
+        self.process_category_urls(raw_category_urls)
+        for category in self.category_urls:
+            yield scrapy.Request(url=category, headers={"token": self.token},
+                                 callback=self.parse_category)
 
-    def process_category_url(self, categorise):
-        category_ids = [
-            re.search(
-                r'.*(goodsList\.html\?classifyIds=\d+)', i
-            ).group(1) for i in categorise
-        ]
-        category_start_url = "https://www.jackjones.com.cn/api/goods/"
-        category_end_url = "&currentpage=1&sortDirection=desc&sortType=1"
-        urls = [
-            category_start_url + i + category_end_url for i in category_ids
-        ]
-        urls = [i.replace(".html", "") for i in urls]
-        urls = list(set(urls))
-        return urls
+    def process_category_urls(self, raw_urls):
+        for raw_url in raw_urls:
+            category_id = re.findall(r".*(goodsList\.html\?classifyIds=\d+).*",
+                                     raw_url["navigationUrl"])
+
+            if category_id:
+                self.category_urls.append(self.category_url_t.format(
+                    category_id[0].replace(".html", "")))
+
+            sub_list = raw_url.get("list")
+
+            if sub_list:
+                self.process_category_urls(sub_list)
+            else:
+                continue
 
     def parse_category(self, response):
-        json_data = json.loads(response.text)
-        total_pages = int(json_data["totalPage"])
-        current_page = int(json_data["currentpage"])
-        prodcuts = json_data["data"]
-        for prodcut in prodcuts:
-            prodcut_code = prodcut["goodsCode"]
-            prodcut_start_url = "https://www.jackjones.com.cn/detail/JACKJONES"
-            prodcut_url = prodcut_start_url + "/" + prodcut_code + ".json"
-            yield scrapy.Request(
-                url=prodcut_url,
-                callback=self.parse_prodcut
-            )
+        raw_products = json.loads(response.text)
+        total_pages = int(raw_products.get("totalPage"))
+        current_page = int(raw_products.get("currentpage"))
+        raw_products = raw_products.get("data")
+        for raw_product in raw_products:
+            yield scrapy.Request(url=self.product_url_t.format(raw_product["goodsCode"]),
+                                 callback=self.parse_product)
         next_page = current_page + 1
-        if next_page < total_pages:
-            next_page_url = re.sub(
-                r'currentpage=(\d+)', "currentpage=" +
-                str(next_page), response.url
-            )
-            yield scrapy.Request(
-                url=next_page_url,
-                headers={"token": str(self.token)},
-                callback=self.parse_category
-            )
 
-    def parse_prodcut(self, response):
+        if next_page < total_pages:
+            next_page_url = re.sub(r"currentpage=(\d+)", "currentpage=" +
+                                   str(next_page), response.url)
+
+            yield scrapy.Request(url=next_page_url, headers={"token": str(self.token)},
+                                 callback=self.parse_category)
+
+    def parse_product(self, response):
         product = JackjonesProduct()
-        json_data = json.loads(response.text)
-        json_data = json_data["data"]
-        product["product_id"] = self.product_id(json_data)
-        product["brand"] = 'JACKJONES'
-        product["care"] = self.care(json_data)
-        product["category"] = self.category(json_data)
-        product["description"] = self.description(json_data)
-        product["image_urls"] = self.image_urls(json_data)
-        product["name"] = self.product_name(json_data)
-        product["skus"] = self.skus(json_data)
+        raw_product = json.loads(response.text)
+        raw_product = raw_product["data"]
+        product["product_id"] = self.product_id(raw_product)
+        product["brand"] = "JACKJONES"
+        product["care"] = self.product_care(raw_product)
+        product["category"] = self.product_category(raw_product)
+        product["description"] = self.product_description(raw_product)
+        product["image_urls"] = self.image_urls(raw_product)
+        product["name"] = self.product_name(raw_product)
+        product["skus"] = self.skus(raw_product)
         product["url"] = response.url
         yield product
 
     def product_id(self, json_data):
         return json_data["projectCode"]
 
-    def care(self, json_data):
+    def product_care(self, json_data):
         return json_data["goodsInfo"]
 
     def product_name(self, json_data):
         return json_data["goodsName"]
 
-    def category(self, json_data):
+    def product_category(self, json_data):
         return json_data["color"][0]["categoryName"]
 
-    def description(self, json_data):
+    def product_description(self, json_data):
         return json_data["describe"]
 
-    def image_urls(self, json_data):
-        image_url = []
-        img_nodes = json_data["color"]
-        image_url = [node["picurls"] for node in img_nodes]
-        return image_url
+    def image_urls(self, raw_images):
+        return [images.get("picurls") for images in raw_images.get("color")]
 
-    def skus(self, json_data):
+    def skus(self, raw_skus):
         skus = {}
-        colour_nodes = json_data["color"]
-        for colour_node in colour_nodes:
-            colour = colour_node["color"]
-            previouse_price = colour_node["originalPrice"]
-            price = colour_node["price"]
-            size_nodes = colour_node["sizes"]
-            for size_node in size_nodes:
-                size = size_node["size"]
-                skus[colour + '_' + size] = {
-                    "colour": colour,
-                    "currency": "",
-                    "previouse_price": previouse_price,
-                    "price": price,
-                    "size": size,
-                }
+        raw_colours = raw_skus["color"]
+        for raw_colour in raw_colours:
+            common_sku = self.product_pricing(raw_colour)
+            raw_sizes = raw_colour["sizes"]
+            for raw_size in raw_sizes:
+                sku = common_sku.copy()
+                sku["size"] = raw_size["size"]
+                sku["colour"] = raw_colour["color"]
+
+                if raw_colour["status"] != "InShelf":
+                    sku["out_of_stock"] = True
+
+                skus[sku["colour"] + "_" + sku["size"]] = sku
         return skus
+
+    def product_pricing(self, raw_prices):
+        prices = {"currency": "￥", "price": raw_prices["price"]}
+
+        if prices["price"] != raw_prices["originalPrice"]:
+            prices["previous_price"] = raw_prices["originalPrice"]
+        return prices
