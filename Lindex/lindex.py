05f17301fa819f4d24f36e988438e5de88cdb9e7
Bilal Sajid <bilal.sajid@arbisoft.com>
2018-10-24 14:43:09 +0500

    skus generation updated

--- Lindex/lindex.py
+++ Lindex/lindex.py
@@ -14,12 +14,13 @@ class LindexSpider(CrawlSpider):
     start_urls = ["http://www.lindex.com/uk/"]
     listing_css = [".mega_menu_box"]
     products_css = [".gridPage .img_wrapper"]
+    # allow_re = ["/women/tops/tops-t-shirts/"]
     deny_re = [
         "/sale/", "/new-in/", "guide", "giftcard",
         "/children-wear/", "/campaign/", "/Assets/"
         ]
     rules = [
-        Rule(LinkExtractor(restrict_css=listing_css, deny=deny_re), callback="parse_pagination"),
+        Rule(LinkExtractor(restrict_css=listing_css, deny=deny_re),callback="parse_pagination"),
         Rule(LinkExtractor(restrict_css=products_css, deny=deny_re), callback="parse_product")
     ]
 
@@ -55,28 +56,34 @@ class LindexSpider(CrawlSpider):
         item_loader = response.meta["item-loader"]
         item_loader.add_value("skus", self.skus(details))
         item_loader.add_value("image_urls", self.image_urls(details))
+        return self.parse_request(item_loader)
 
-        response.meta["colors"].pop(0)
-        if len(response.meta["colors"]) == 0:
-            yield item_loader.load_item()
-
-    def make_colors_request(self, response, item_loader):
+    def color_requests(self, response, item_loader):
         colors = self.product_colors_id(response)
         headers = {
             "X-Requested-With": "XMLHttpRequest",
             "Content-Type": "application/json; charset=UTF-8"}
         url = "https://www.lindex.com/WebServices/ProductService.asmx/GetProductData"
 
+        colors_request = []
         for color in colors:
-            yield Request(
-                url=url, callback=self.parse_colors, method="POST",
-                headers=headers,
+            colors_request.append(Request(
+                url=url, method="POST", callback=self.parse_colors,
+                headers=headers, priority=10,
                 body=json.dumps({
                     "productIdentifier": self.product_id(response),
                     "colorId": color,
                     "primaryImageType": "1"}),
-                meta={"item-loader": item_loader, "colors": colors},
-                )
+                meta={"item-loader": item_loader}
+                ))
+        return colors_request
+
+    def parse_request(self, item_loader):
+        colors_request = item_loader.get_collected_values("meta")
+        if not colors_request:
+            yield item_loader.load_item()
+        else:
+            yield colors_request.pop(0)
 
     def parse_product(self, response):
         item_loader = LindexItemLoader(item=LindexItem(), response=response)
@@ -90,7 +97,7 @@ class LindexSpider(CrawlSpider):
         item_loader.add_value("gender", "Women")
         item_loader.add_value("categories", self.product_categories(response))
         item_loader.add_value("description", self.product_description(response))
-        item_loader.add_value("care", self.product_care_info(response))
+        item_loader.add_value("care", self.product_care(response))
         item_loader.add_value("url_orignal", response.url)
         item_loader.add_value("url", self.product_url(response))
         item_loader.add_value("date", datetime.now().strftime("%Y-%m-%d"))
@@ -99,8 +106,8 @@ class LindexSpider(CrawlSpider):
         item_loader.add_value("spider_name", "lindex-uk-crawl")
         item_loader.add_value("crawl_id", f"lindex-uk-{datetime.now().strftime('%Y%m%d-%H%M%s')}-axuj")
         item_loader.add_value("crawl_start_time", datetime.now().isoformat())
-
-        return self.make_colors_request(response, item_loader)
+        item_loader.add_value("meta", self.color_requests(response, item_loader))
+        return self.parse_request(item_loader)
 
     def image_urls(self, details):
         return [image["Standard"] for image in details["Images"]]
@@ -153,7 +160,7 @@ class LindexSpider(CrawlSpider):
         css = ".description ::text"
         return response.css(css).extract()
 
-    def product_care_info(self, response):
+    def product_care(self, response):
         css = ".more_info ::text"
         return response.css(css).extract()
 
