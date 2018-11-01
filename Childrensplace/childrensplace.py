9ee9f948d1da6598df15359afe7113fbf0dfe110
Bilal Sajid <bilal.sajid@arbisoft.com>
2018-11-01 17:05:25 +0500

    Product request and pagination modified

--- Childrensplace/childrensplace.py
+++ Childrensplace/childrensplace.py
@@ -17,10 +17,13 @@ class Mixin:
     retailer = "childrensplace-us"
     market = "US"
 
-    category_req_url = "https://search.unbxd.io/8870d5f30d9bebafac29a18cd12b801d"\
-        "/childrensplace-com702771523455856/category?"
-    variant_req_url = "https://search.unbxd.io/8870d5f30d9bebafac29a18cd12b801d"\
-        "/childrensplace-com702771523455856/search?"
+    category_req_url_t = "https://search.unbxd.io/8870d5f30d9bebafac29a18cd12b801d"\
+        "/childrensplace-com702771523455856/category?{0}"
+    variant_req_url_t = "https://search.unbxd.io/8870d5f30d9bebafac29a18cd12b801d"\
+        "/childrensplace-com702771523455856/search?{0}"
+    product_url_t = "http://www.childrensplace.com/us/p/{0}"
+    image_url_t = "https://www.childrensplace.com/wcsstore/GlobalSAS/images/tcp/"\
+        "products/500/{0}"
 
 
 class ChildrensPlaceParser(Mixin):
@@ -38,7 +41,7 @@ class ChildrensPlaceParser(Mixin):
         item["retailer"] = self.retailer
         item["retailer_sku"] = self.retailer_sku(product_details)
         item["categories"] = self.product_categories(product_details)
-        item["gender"] = self.get_gender(product_details)
+        item["gender"] = self.product_gender(product_details)
         item["spider_name"] = self.name
         item["date"] = datetime.now().strftime("%Y-%m-%d")
         item["crawl_start_time"] = datetime.now().isoformat()
@@ -49,7 +52,7 @@ class ChildrensPlaceParser(Mixin):
     def skus(self, color_variant):
         skus = []
         for size_variant in color_variant["variants"]:
-            sku = self.pricing_details(size_variant)
+            sku = self.product_pricing(size_variant)
             sku["color"] = size_variant["auxdescription"]
             sku["size"] = size_variant["v_tcpsize"]
             sku["sku_id"] = f"{size_variant['auxdescription']}_{size_variant['v_tcpsize']}"
@@ -61,7 +64,7 @@ class ChildrensPlaceParser(Mixin):
         item["image_urls"] = []
         for color_variant in product_details:
             item["skus"] += self.skus(color_variant)
-            item["image_urls"].append(self.image_urls(color_variant))
+            item["image_urls"] += self.image_urls(color_variant)
         yield item
 
     def product_name(self, product_details):
@@ -70,7 +73,7 @@ class ChildrensPlaceParser(Mixin):
     def product_price(self, product_details):
         return product_details[0]["min_offer_price"]
 
-    def pricing_details(self, product_details):
+    def product_pricing(self, product_details):
         return {
             "price": int(float(product_details["v_offerprice"]) * 100),
             "previous_price": int(float(product_details["v_listprice"]) * 100),
@@ -83,7 +86,7 @@ class ChildrensPlaceParser(Mixin):
         return product_details[0]["uniqueId"]
 
     def product_url(self, product_details):
-        return f"http://www.childrensplace.com/us/p/{product_details[0]['seo_token']}"
+        return self.product_url_t.format(product_details[0]['seo_token'])
 
     def product_description(self, product_details):
         return [product_details[0]["product_short_description"]]
@@ -91,14 +94,15 @@ class ChildrensPlaceParser(Mixin):
     def product_categories(self, product_details):
         return product_details[0]["categoryPath2"]
 
-    def get_gender(self, product_details):
+    def product_gender(self, product_details):
         return product_details[0]["gender"]
 
     def get_crawl_id(self):
         return f"childrensplace-us-{datetime.now().strftime('%Y%m%d-%H%M%s')}-axuj"
 
     def image_urls(self, product_details):
-        return product_details["imageUrl"][0]
+        return product_details["imageUrl"] + [self.image_url_t.format(
+            img) for img in json.loads(product_details["alt_img"]).values()]
 
 
 class ChildrensPlaceCrawler(CrawlSpider, Mixin):
@@ -107,23 +111,26 @@ class ChildrensPlaceCrawler(CrawlSpider, Mixin):
     PAGE_SIZE = 100
 
     def parse(self, response):
-        main_data = self.get_home_page_data(response)
+        main_data = self.raw_listings(response)
         naviagtion_details = main_data["globalComponents"]["header"][
             "navigationTree"]
-        category_map = self.category_map(naviagtion_details)
+        category_map = self.map_category(naviagtion_details)
 
         for cat_id, sub_categories in category_map.items():
-            for sub_category in sub_categories:
-                    yield response.follow(
-                        sub_category[0], callback=self.parse_listing,
-                        meta={"category": cat_id, "sub_category": sub_category[1]})
+            yield self.category_requests(cat_id, sub_categories, response)
 
-    def get_home_page_data(self, response):
+    def category_requests(self, cat_id, sub_categories, response):
+        for sub_category in sub_categories:
+            return response.follow(
+                sub_category[0], callback=self.parse_listing,
+                meta={"category": cat_id, "sub_category": sub_category[1]})
+
+    def raw_listings(self, response):
         script = "window.__PRELOADED_STATE__ = (.+?);\n"
         site_data = re.findall(script, response.body.decode("utf-8"))[0]
         return json.loads(site_data)
 
-    def category_map(self, naviagtion_details):
+    def map_category(self, naviagtion_details):
         category_map = {}
         for menu in naviagtion_details:
             category_map[menu["categoryId"]] = [
@@ -135,21 +142,24 @@ class ChildrensPlaceCrawler(CrawlSpider, Mixin):
         parameters = {
             "start": 0, "rows": self.PAGE_SIZE, "pagetype": "boolean",
             "p-id": f'categoryPathId:"{meta["category"]}>{meta["sub_category"]}"'}
-        request_url = f"{self.category_req_url}{urllib.parse.urlencode(parameters)}"
+        request_url = self.category_req_url_t.format(urllib.parse.urlencode(parameters))
+        yield Request(request_url, callback=self.parse_pagination)
 
-        yield Request(request_url, callback=self.parse_details, meta={"page": 0})
-
-    def parse_details(self, response):
+    def parse_pagination(self, response):
         parameters = {"variants": True, "variants.count": self.PAGE_SIZE, "pagetype": "boolean"}
-        json_response = json.loads(response.body)["response"]
+        product_list = json.loads(response.body)["response"]
+        product_requests = self.parse_products(parameters, product_list)
 
-        for product_details in json_response["products"]:
+        for page in range(0, product_list["numberOfProducts"], self.PAGE_SIZE):
+            url = add_or_replace_parameter(response.url, "start", page)
+            product_requests.append(Request(url, callback=self.parse_pagination))
+
+        return product_requests
+
+    def parse_products(self, parameters, product_list):
+        product_requests = []
+        for product_details in product_list["products"]:
             parameters["q"] = product_details["style_partno"]
-            request_url = f"{self.variant_req_url}{urllib.parse.urlencode(parameters)}"
-            yield Request(request_url, callback=self.parser.parse_product)
-
-        page = response.meta["page"]
-        start = page + self.PAGE_SIZE
-        if start < int(json_response["numberOfProducts"]):
-            url = add_or_replace_parameter(response.url, "start", start)
-            yield Request(url, callback=self.parse_details, meta={"page": start})
+            request_url = self.variant_req_url_t.format(urllib.parse.urlencode(parameters))
+            product_requests.append(Request(request_url, callback=self.parser.parse_product))
+        return product_requests
