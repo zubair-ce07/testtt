49bee7d3d53d9dc5afa335bf5dbcddfbb10428af
m-furqan <furqan.nasir@arbisoft.com>
2018-11-07 12:39:20 +0500

    add new functions for better readability

--- maurices_spider_code/maurices_parse_product.py
+++ maurices_spider_code/maurices_parse_product.py
@@ -1,12 +1,13 @@
 import json
 import re
 
-import scrapy
+from scrapy import Spider
+from scrapy import Request
 
 from maurices.items import MauricesProduct
 
 
-class MauricesParseProduct(scrapy.Spider):
+class MauricesParseProduct(Spider):
     name = 'maurices_parse_product'
     image_url_t = 'https://mauricesprodatg.scene7.com/is/image/mauricesProdATG/' \
         '{pid}_{color_id}_ms?req=set,json&id={color_id}'
@@ -18,67 +19,97 @@ class MauricesParseProduct(scrapy.Spider):
         product['description'] = self.product_description(response)
         product['retailer_sku'] = self.product_retailer_sku(response)
         product['url'] = response.url
-        product['skus'] = {}
+        product['category'] = self.product_category(response)
+        product['skus'] = self.product_skus(response)
         product['image_urls'] = []
-        product_data = response.css('#pdpInitialData::text').extract_first()
-        raw_product = json.loads(product_data)['pdpDetail']['product'][0]
-        self.add_skus(product, raw_product)
-        product['requests'] = self.create_color_requests(raw_product, product)
+        product['requests'] = self.product_color_requests(response)
         yield self.request_or_item(product)
 
     def parse_color(self, response):
         product = response.meta.get('product')
         image_url_r = 'mauricesProdATG/[^"]*'
         image_url_re = re.compile(image_url_r)
-        product['image_urls'].extend(
-            list(set(image_url_re.findall(response.body.decode()))))
+        product['image_urls'].extend(list(set(image_url_re.findall(response.body.decode()))))
         yield self.request_or_item(product)
 
     def request_or_item(self, product):
         requests = product['requests']
+        
         if requests:
             request = requests.pop()
             request.meta['product'] = product
             return request
+        
         del product['requests']
         return product
 
-    def create_color_requests(self, raw_product, product):
-        product_id = product['retailer_sku']
-        colors = self.attribute_map(raw_product['all_available_colors'])
-        requests = []
-        for color_id in colors:
-            url = self.image_url_t.format(pid=product_id, color_id=color_id)
-            requests.append(scrapy.Request(url, callback=self.parse_color))
-        return requests
-
-    def add_skus(self, product, raw_product):
+    def available_sku_keys(self, raw_product):
         colors = self.attribute_map(raw_product['all_available_colors'])
         sizes = self.attribute_map(raw_product['all_available_sizes'])
-        product['category'] = raw_product['ensightenData'][0]['categoryPath']
         available_sku_keys = []
+
         for raw_sku in raw_product['skus']:
             sku_key = f"{colors.get(raw_sku['color'])}_{sizes.get(raw_sku['size'])}"
             available_sku_keys.append(sku_key)
+        
+        return available_sku_keys
+
+    def product_color_requests(self, response):
+        product_id = self.product_retailer_sku(response)
+        raw_product = self.raw_product(response)
+        colors = self.attribute_map(raw_product['all_available_colors'])
+        requests = []
+        
+        for color_id in colors:
+            url = self.image_url_t.format(pid=product_id, color_id=color_id)
+            requests.append(Request(url, callback=self.parse_color))
+        
+        return requests
+
+    def product_skus(self, response):
+        raw_product = self.raw_product(response)
+        colors = self.attribute_map(raw_product['all_available_colors'])
+        sizes = self.attribute_map(raw_product['all_available_sizes'])
+        available_sku_keys = self.available_sku_keys(raw_product)
+        skus = {}
 
         for color_id in colors:
             for size_id in sizes:
                 sku_key = str(colors[color_id] + '_' + sizes[size_id])
-                product['skus'][sku_key] = {
+                skus[sku_key] = {
                     'color': colors[color_id],
                     'size': sizes[size_id],
                 }
-                self.product_currency_and_price(product, raw_product, sku_key)
+                skus[sku_key].update(self.product_currency_and_price(response))
+                
                 if sku_key not in available_sku_keys:
-                    product['skus'][sku_key]['out_of_stock'] = True
+                    skus[sku_key]['out_of_stock'] = True
+        
+        return skus
 
-    def product_currency_and_price(self, product, raw_product, sku_key):
-        product['skus'][sku_key]['currency'] = 'USD'
-        curr_price = raw_product['all_available_colors'][0]['values'][0]['prices']['sale_price']
-        previous_price = raw_product['all_available_colors'][0]['values'][0]['prices']['list_price']
-        product['skus'][sku_key]['price'] = curr_price
+    def product_currency_and_price(self, response):
+        raw_product = self.raw_product(response)
+        prices = raw_product['all_available_colors'][0]['values'][0]['prices']
+        curr_price = prices['sale_price']
+        previous_price = prices['list_price']
+        currency_and_price = {
+            'currency': 'USD',
+            'price': curr_price,
+            }
+
         if curr_price != previous_price:
-            product['skus'][sku_key]['previous_price'] = previous_price
+            currency_and_price['previous_price'] = previous_price
+        
+        return currency_and_price
+
+    def raw_product(self, response):
+        css = '#pdpInitialData::text'
+        product_data = response.css(css).extract_first()
+        return json.loads(product_data)['pdpDetail']['product'][0]
+
+    def product_category(self, response):
+        raw_product = self.raw_product(response)
+        return raw_product['ensightenData'][0]['categoryPath']
 
     def product_name(self, response):
         name_css = '.mar-product-title::text'
--- maurices_spider_code/maurices_spider.py
+++ maurices_spider_code/maurices_spider.py
@@ -1,24 +1,25 @@
 # -*- coding: utf-8 -*-
 import re
 import json
-import w3lib
 
-import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.selector import Selector
+from scrapy import Request
+from w3lib.url import url_query_parameter
+from w3lib.url import add_or_replace_parameter
 
 from maurices.maurices_parse_product import MauricesParseProduct
 
 
 class MauricesSpider(CrawlSpider):
     name = 'maurices_spider'
-    allowed_domains = ['maurices.com', 'mauricesprodatg.scene7.com']
     start_urls = ['https://www.maurices.com']
-    product_pagination_url_t = 'https://www.maurices.com/maurices/plp/includes' \
-        '/plp-filters.jsp?N={sub_catagory_id}&No=0'
     sub_catagory_allow_r = '/p/'
     product_parser = MauricesParseProduct()
+    allowed_domains = ['maurices.com', 'mauricesprodatg.scene7.com']
+    product_pagination_url_t = 'https://www.maurices.com/maurices/plp/includes' \
+        '/plp-filters.jsp?N={sub_catagory_id}&No=0'
 
     rules = (Rule(LinkExtractor(allow=(sub_catagory_allow_r)),
                   callback='parse_subcatagory'),)
@@ -26,32 +27,29 @@ class MauricesSpider(CrawlSpider):
     def parse_subcatagory(self, response):
         sub_catagory_id_re = re.compile(r'[0-9]+')
         sub_catagory_id = sub_catagory_id_re.findall(response.url)
-        url = self.product_pagination_url_t.format(
-            sub_catagory_id=sub_catagory_id[0])
-        yield scrapy.Request(url, callback=self.parse_pagination)
+        url = self.product_pagination_url_t.format(sub_catagory_id=sub_catagory_id[0])
+        yield Request(url, callback=self.parse_pagination)
 
     def parse_pagination(self, response):
         response_json = json.loads(response.body)
+        yield from self.product_requests(response)
+        next_page_url = response_json.get('nextPageUrl')
+        
+        if next_page_url:
+            product_count = url_query_parameter(next_page_url, 'No')
+            url = add_or_replace_parameter(response.url, 'No', product_count)
+            yield Request(url, callback=self.parse_pagination,)
+
+    def product_requests(self, response):
+        response_json = json.loads(response.body)
         html_response = response_json.get('product_grid')
         html_response = html_response.get('html_content')
-        product_requests = self.product_requests(html_response, response)
-        for product_request in product_requests:
-            yield product_request
-
-        next_page_url = response_json.get('nextPageUrl')
-        if next_page_url:
-            product_count = w3lib.url.url_query_parameter(next_page_url, 'No')
-            url = w3lib.url.add_or_replace_parameter(
-                response.url, 'No', product_count)
-            yield scrapy.Request(url, callback=self.parse_pagination,)
-
-    def product_requests(self, html_response, response):
-        product_url_css = '.mar-prd-item-image-container::attr(href)'
-        product_urls = Selector(text=html_response).css(
-            product_url_css).extract()
+        css = '.mar-prd-item-image-container::attr(href)'
+        product_urls = Selector(text=html_response).css(css).extract()
         requests = []
+        
         for product_url in product_urls:
-            requests.append(scrapy.Request(response.urljoin(product_url),
+            requests.append(Request(response.urljoin(product_url),
                                            callback=self.product_parser.parse_product))
         return requests
 
