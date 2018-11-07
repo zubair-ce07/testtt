7a854bab055dfc711ff50ad1287345f243537752
tazeem <tazeem.imran@arbisoft.com>
2018-11-07 16:08:37 +0500

    Blank Line added

--- sku_scraper/sku_scraper/spiders/mooseknuckles.py
+++ sku_scraper/sku_scraper/spiders/mooseknuckles.py
@@ -18,6 +18,7 @@ class Mixin:
         'USER_AGENT': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0'
     }
 
+
 class MooseknucklesParseSpider(Mixin, Spider):
     name = 'mooseknuckles-parse'
 
