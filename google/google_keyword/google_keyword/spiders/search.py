f75c4306dcde4cc3b8439f9bf0eaca715ec63453
arslanather <arslan.ather@arbisoft.com>
2018-11-13 12:18:12 +0500

    complete spider created

--- google/google_keyword/google_keyword/spiders/search.py
+++ google/google_keyword/google_keyword/spiders/search.py
@@ -15,9 +15,14 @@ class SearchSpider(scrapy.Spider):
 
     # start_urls = ["https://www.google.com/search?q=site:https://arbisoft.com Disco"]
     custom_settings = {
+        "USER_AGENT": "Mozilla/5.0 (X11; Linux x86_64) "
+                      "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
         "DOWNLOAD_DELAY" : 1.5,
-        "ROBOTSTXT_OBEY": False
+        "ROBOTSTXT_OBEY": False,
+        "FEED_EXPORT_ENCODING": "utf-8"
     }
+
+    number_of_results = 5
 
     def start_requests(self):
         search_requests = []
@@ -36,20 +41,21 @@ class SearchSpider(scrapy.Spider):
 
     def parse(self, response):
         item = {"result_stats": response.css("#resultStats::text").extract_first(),
-                "results": [], "keyword": response.meta["keyword"]}
+                "keyword_found": [], "keyword": response.meta["keyword"]}
         results = response.css(".g")
-        if len(results) > 5:
-            for result in results[:5]:
-                item["results"].append({
+        item["website_exists"] = "Y" if item["result_stats"] else "N"
+
+        if len(results) > self.number_of_results:
+            for result in results[:self.number_of_results]:
+                item["keyword_found"].append({
                     "title": "".join(result.css(".r a ::text").extract()),
-                    "url": result.css(".r a::attr(href)").extract()
+                    "url": "https://google.com{}".format(result.css(".r a::attr(href)").extract_first())
                 })
         else:
             for result in results:
-                item["results"].append({
+                item["keyword_found"].append({
                     "title": "".join(result.css(".r a ::text").extract()),
-                    "url": result.css(".r a::attr(href)").extract()
+                    "url": "https://google.com{}".format(result.css(".r a::attr(href)").extract_first())
                 })
 
-        yield item        
-        # inspect_response(response, self)
+        yield item
