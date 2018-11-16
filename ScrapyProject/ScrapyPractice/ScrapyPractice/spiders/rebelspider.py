f951994773e6623fd87ac5f411e323055fa21516
Muhammad Tauseeq <tauseeq@AbuzerRauf-Desktop-51.arbisoft.com>
2018-11-16 17:27:47 +0500

    done with all categories

--- ScrapyProject/ScrapyPractice/ScrapyPractice/spiders/rebelspider.py
+++ ScrapyProject/ScrapyPractice/ScrapyPractice/spiders/rebelspider.py
@@ -1,63 +1,107 @@
-import scrapy
+import copy
 
-from scrapy import Request
-from urllib.parse import urljoin, urlparse
-
-from ScrapyPractice.items import ProductItem
-from ScrapyPractice.items import SizeItem
-from ScrapyPractice.items import VariationItem
+from scrapy import Request, Spider, signals
+from urllib.parse import urljoin
+from ScrapyPractice.items import ProductItem, SizeItem, VariationItem
 
 
-class MangoSpider(scrapy.Spider):
+class RebelSpider(Spider):
     name = "rebelspider"
     domain_url = "https://www.rebelsport.com.au"
+    items = dict()
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super(RebelSpider, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.process_items, signal=signals.spider_idle)
+        return spider
+
+    def process_items(self, spider):
+        if self.items:
+            sku, item = self.items.popitem()
+            req = Request(
+                url=urljoin(self.domain_url, item['product_url']),
+                callback=self.parse_product,
+                meta={'item': item},
+            )
+            self.crawler.engine.crawl(req, self)
 
     def start_requests(self):
-        start_url = 'https://www.rebelsport.com.au/brands'
-        yield Request(start_url, callback=self.parse)
+        start_url = 'https://www.rebelsport.com.au'
+        yield Request(start_url, callback=self.parse_homepage)
 
-    def parse(self, response):
-        brands_list_xpath = "//div[@id='secondary']//ul[@id='category-level-1']/li/a/@href"
-        brand_urls = response.xpath(brands_list_xpath).extract()
+    def parse_homepage(self, response):
+        for category_level1 in response.xpath("//nav[@id='navigation']/ul/li"):
+            category_level1_label = category_level1.xpath("./a/text()").extract_first('').strip()
+            category_level1_url = category_level1.xpath("./a/@href").extract_first()
+            meta = copy.deepcopy(response.meta)
 
-        for url in brand_urls[0:1]:
+            meta['category'] = 'Home', category_level1_label
             yield Request(
-                url=url,
-                callback=self.parse_brand
+                url=category_level1_url,
+                callback=self.parse_categories,
+                meta=meta,
             )
 
-    def parse_brand(self, response):
-        product_xpath = "//ul[@id='search-result-items']/li/div/div[1]/a[1]/@href"
-        next_page_xpath = "//div[@data-loading-state='unloaded']/@data-grid-url"
+            for category_level2 in category_level1.xpath(".//div[@class='level-2-outer']"):
+                category_level2_label = category_level2.xpath("./a/text()").extract_first('').strip()
+                category_level2_url = category_level2.xpath("./a/@href").extract_first()
 
-        product_urls = response.xpath(product_xpath).extract()
-        for url in product_urls:
-            req_url = urljoin(self.domain_url, url)
-            yield Request(
-                url=req_url,
-                callback=self.parse_product,
-            )
+                meta['category'] = 'Home', category_level1_label, category_level2_label
+                yield Request(
+                    url=category_level2_url,
+                    callback=self.parse_categories,
+                    meta=meta,
+                )
+                for category_level3 in category_level2.xpath(".//div/ul/li"):
+                    category_level3_label = category_level3.xpath("./a/text()").extract_first('').strip()
+                    category_level3_url = category_level3.xpath("./a/@href").extract_first()
 
-        next_page_url = response.xpath(next_page_xpath).extract_first(default=None)
-        if next_page_url:
-            yield Request(
-                url=next_page_url,
-                callback=self.parse_brand
-            )
+                    meta['category'] = 'Home', category_level1_label, category_level2_label, category_level3_label
+                    yield Request(
+                        url=category_level3_url,
+                        callback=self.parse_categories,
+                        meta=meta,
+                    )
+
+    def parse_categories(self, response):
+        for product in response.xpath("//ul[@id='search-result-items']/li"):
+            item = ProductItem(breadcrumbs=[])
+            item['product_url'] = product.xpath(".//div[@class='product-image']/a/@href").extract_first()
+            item['store_keeping_unit'] = product.xpath("./div/@data-itemid").extract_first()
+            item['breadcrumbs'].append(response.meta['category'])
+
+            if item['store_keeping_unit'] not in self.items.keys():
+                self.items[item['store_keeping_unit']] = item
+            else:
+                self.items[item['store_keeping_unit']]['breadcrumbs'].append(item['breadcrumbs'])
+
+        next_page_xpath = "//div[@class='search-result-content']/div[@data-loading-state='unloaded']/@data-grid-url"
+        next_page = response.xpath(next_page_xpath).extract_first()
+
+        # if next_page:
+        #     yield Request(
+        #         url=next_page,
+        #         callback=self.parse_category_levels,
+        #         meta=response.meta,
+        #     )
+        pass
 
     def parse_product(self, response):
+        item = response.meta['item']
+
         des_xpath = "//div[@id='product-description']//p/text() | //div[@id='product-description']//ul/li/text()"
         product = ProductItem(
-            product_url=response.xpath("//span[@itemprop='url']/text()").extract_first().strip(),
-            title=response.xpath("//h1[@class='product-name']/text()").extract_first(),  # .strip(),
+            product_url=urljoin(self.domain_url,item['product_url']),
+            title=response.xpath("//h1[@class='product-name']/text()").extract_first(),
             brand=response.xpath("//span[@class='product-brand']/text()").extract_first(),
             locale="en_AU",
             currency="AUD",
             variations=[],
-            breadcrumbs=response.xpath("//*[@class='breadcrumb-element']/text()").extract(),
+            breadcrumbs=item['breadcrumbs'],
             description=response.xpath(des_xpath).extract(),
         )
-        product["store_keeping_unit"] = product['product_url'].split('-')[-1].split('.')[0]
+        product["store_keeping_unit"] = item["store_keeping_unit"]
 
         colors_exist = response.xpath("//div[@class='product-variations']").extract()
         if not colors_exist:
@@ -75,6 +119,14 @@ class MangoSpider(scrapy.Spider):
             return
 
         yield self.parse_no_variation_items(response, product)
+
+        if self.items:
+            sku, item = self.items.popitem()
+            yield Request(
+                url=urljoin(self.domain_url, item['product_url']),
+                callback=self.parse_product,
+                meta={'item': item},
+            )
 
     def color_info(self, response):
         product = response.meta['product']
@@ -196,7 +248,7 @@ class MangoSpider(scrapy.Spider):
                 'discounted_price': '',
             }
 
-        price_sales= price_sales.split('$')[1]
+        price_sales = price_sales.split('$')[1]
 
         if price_standard and "N/A" not in price_standard:
             is_discounted = True
