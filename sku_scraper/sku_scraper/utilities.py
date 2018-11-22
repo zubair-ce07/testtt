ac6fa8a67fee336dca7a9e9b7f587a548fe345a8
Muhammad Tazeem <tazeem@HamzaAkhtar-Desktop-17.arbisoft.com>
2018-11-22 18:46:24 +0500

    restructured utilities.py, extract_gender

--- sku_scraper/sku_scraper/utilities.py
+++ sku_scraper/sku_scraper/utilities.py
@@ -1,11 +1,38 @@
 
+currency_map = {
+    '$': 'USD',
+    '€': 'EUR',
+    '¥': 'YEN',
+    '₨': 'PKR',
+    '£': 'GBP'
+}
+
+
+gender_map = {
+    'women': 'Women',
+    'men': 'Men',
+    'female': 'Women',
+    'male': 'Men',
+    'kids': 'Kids',
+    'boy': 'Kids',
+    'girl': 'Kids',
+    'unisex': 'Unisex'
+}
+
+
+merch_info_map = {
+    'limited': 'Limited Edition',
+    'special': 'Special Edition'
+}
+
+
 def convert_price_to_integer(price):
     if price:
         return int(float(price) * 100)
 
 
-def pricing(raw_prices):
-    prices = remove_non_numerics(raw_prices)
+def pricing(soup):
+    prices = remove_non_numerics(soup)
     converted_prices = [convert_price_to_integer(price) for price in set(prices) if price]
     converted_prices.sort()
 
@@ -16,7 +43,7 @@ def pricing(raw_prices):
     if previous_price:
         price_map['previous_price'] = previous_price
 
-    price_map['currency'] = extract_currency(raw_prices)
+    price_map['currency'] = extract_currency(soup)
     return price_map
 
 
@@ -25,40 +52,18 @@ def remove_non_numerics(raw_prices):
 
 
 def map_currency_code(currency_code):
-    currencies = {
-        '$': 'USD',
-        '€': 'EUR',
-        '¥': 'YEN',
-        '₨': 'PKR',
-        '£': 'GBP'
-    }
-    return currencies.get(currency_code)
+    return currency_map.get(currency_code)
 
 
-def map_merch_info(item_details):
-    merch_info_map = {
-        'limited': 'Limited Edition',
-        'special': 'Special Edition'
-    }
+def map_merch_info(soup):
     for merch_info in merch_info_map.keys():
-        if merch_info in item_details.lower():
+        if merch_info in soup.lower():
             return merch_info_map.get(merch_info)
 
 
-def map_gender(raw_string):
-    gender_map = {
-        'women': 'Women',
-        'men': 'Men',
-        'female': 'Women',
-        'male': 'Men',
-        'kids': 'Kids',
-        'boy': 'Kids',
-        'girl': 'Kids',
-        'unisex': 'Unisex'
-    }
-
+def map_gender(soup):
     for gender in gender_map.keys():
-        if gender in raw_string:
+        if gender in soup:
             return gender_map.get(gender, 'unisex-adults')
 
 
@@ -69,4 +74,3 @@ def extract_currency(money_strings):
         currency = map_currency_code(char)
         if currency:
             return currency
- 
