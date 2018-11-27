7446f40d3967319af88302bb0c03be82a1afb78a
Bilal Sajid <bilal.sajid@arbisoft.com>
2018-11-27 18:17:56 +0500

    price_css added and code cleaned

--- rakuten_spider.py
+++ rakuten_spider.py
@@ -11,23 +11,22 @@ class Mixin:
     lang = "ja"
     market = "JP"
     retailer = "rakuten-jp"
+    default_brand = "rakuten"
 
-    MERCH_MAP = [
+    merch_map = [
         ("期間限定", "Limited Time"),
         ("送料無料", "Free Shipping")]
-
-    COLOUR_KEYS = ["色", "カラー", "色彩", "顔色", "ストリーマ", "ストリーマー", "呈色", "色のついた"]
+    colour_keys = ["色", "カラー", "色彩", "顔色", "ストリーマ", "ストリーマー", "呈色", "色のついた"]
 
     one_colour = "ワンカラー"
     one_size = "ワンサイズ"
-
-    default_brand = ["rakuten"]
 
 
 class RakutenParser(Mixin, BaseParseSpider):
     name = Mixin.retailer + "-parser"
 
     description_css = "meta[name='description']::attr(content), .item_desc::text"
+    price_css = ".price2::text"
 
     def parse(self, response):
         product_id = self.product_id(response)
@@ -50,66 +49,58 @@ class RakutenParser(Mixin, BaseParseSpider):
 
     def product_id(self, response):
         css = "meta[property='apprakuten:item_id']::attr(content)"
-        return response.css(css).extract_first()
+        return clean(response.css(css))[0]
 
     def product_name(self, response):
         css = "meta[property='og:title']::attr(content)"
-        return response.css(css).extract_first()
+        return clean(response.css(css))[0]
 
     def product_category(self, response):
         css = "meta[property='apprakuten:shop_code']::attr(content)"
-        return response.css(css).extract()
+        return clean(response.css(css))
 
     def product_gender(self, garment):
         soup = " ".join(garment["description"] + [garment["name"]] + garment["merch_info"])
         return self.gender_lookup(soup) or Gender.ADULTS.value
 
     def merch_info(self, garment):
-        soup = " ".join([desc.lower() for desc in garment["description"]])
-        return [merch for key, merch in self.MERCH_MAP if key.lower() in soup]
+        soup = " ".join([desc for desc in garment["description"]]).lower()
+        return [merch for key, merch in self.merch_map if key.lower() in soup]
 
     def image_urls(self, response):
         css = ".rakutenLimitedId_ImageMain1-3::attr(href)"
-        return response.css(css).extract()
-
-    def product_price(self, response):
-        css = "meta[property='apprakuten:price']::attr(content)"
-        return response.css(css).extract_first()
-
-    def currency_type(self, response):
-        css = "meta[property='apprakuten:currency']::attr(content)"
-        return response.css(css).extract_first()
+        return clean(response.css(css))
 
     def sku_dimensions(self, response):
         css = "td[valign='bottom'] .inventory_title::text"
-        return clean(response.css(css).extract())
+        return clean(response.css(css))
 
     def skus(self, response):
         skus = {}
         row_attr, col_attr = [], []
-        money_str = [self.product_price(response), self.currency_type(response)]
+        common_sku = self.product_pricing_common(response)
 
         css_row = ".skuDisplayTable table tr:nth-child(1) ::text"
         css_col = ".skuDisplayTable table tr td:nth-child(1) ::text"
 
         skus_dimension = self.sku_dimensions(response)
         if not skus_dimension:
-            sku = self.product_pricing_common(None, money_strs=money_str)
+            sku = common_sku.copy()
             sku["size"] = self.one_size
             skus[self.product_id(response)] = sku
             return skus
 
         row_soup = skus_dimension[0].split()
-        row_check = [key for key in self.COLOUR_KEYS if key in row_soup]
+        row_check = [key for key in self.colour_keys if key in row_soup]
 
-        colours = clean(response.css(css_row).extract()) if row_check else row_attr.extend(
-            clean(response.css(css_row).extract()))
+        colours = clean(response.css(css_row)) if row_check else row_attr.extend(
+            clean(response.css(css_row)))
 
         col_soup = skus_dimension[-1].split()
-        col_check = [key for key in self.COLOUR_KEYS if key in col_soup]
+        col_check = [key for key in self.colour_keys if key in col_soup]
 
-        colours = clean(response.css(css_col).extract()) if col_check else col_attr.extend(
-            clean(response.css(css_col).extract()))
+        colours = clean(response.css(css_col)) if col_check else col_attr.extend(
+            clean(response.css(css_col)))
 
         if not colours:
             colours = [self.one_colour]
@@ -119,7 +110,7 @@ class RakutenParser(Mixin, BaseParseSpider):
 
         for colour in colours:
             for size in sizes:
-                sku = self.product_pricing_common(None, money_strs=money_str)
+                sku = common_sku.copy()
                 sku["colour"] = colour
                 sku["size"] = size
                 skus[f"{colour}_{size}"] = sku
