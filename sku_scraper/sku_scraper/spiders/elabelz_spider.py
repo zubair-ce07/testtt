756573fc4f469e8463ca76cc7ccb3269287e0902
Muhammad Tazeem <tazeem@HamzaAkhtar-Desktop-17.arbisoft.com>
2018-11-28 12:41:38 +0500

    removed currency from Mixin level, cleaned skus method, improved css in some places, used raw_description_css instead of care/description css

--- sku_scraper/sku_scraper/spiders/elabelz_spider.py
+++ sku_scraper/sku_scraper/spiders/elabelz_spider.py
@@ -2,8 +2,7 @@ import re
 import json
 
 from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import Rule
-from scrapy.http import Request
+from scrapy.spiders import Rule, Request
 from scrapy.selector import Selector
 
 from .base import BaseParseSpider, BaseCrawlSpider, clean, soupify, Gender
@@ -12,66 +11,51 @@ from .base import BaseParseSpider, BaseCrawlSpider, clean, soupify, Gender
 class Mixin:
     retailer = 'elabelz'
     allowed_domains = ['elabelz.com']
-    spider_gender_map = [
-        ('النساء', Gender.WOMEN.value),
-        ('الرجال', Gender.MEN.value),
-        ('الاطفال', Gender.KIDS.value),
-        ('أولاد', Gender.BOYS.value),
-        ('بنات', Gender.GIRLS.value),
-    ]
 
 
 class MixinUS(Mixin):
     retailer = Mixin.retailer + '-us'
     market = 'US'
     start_urls = ['https://www.elabelz.com/en_us/']
-    currency = 'USD'
-    lang = 'en'
-    size_api_url = 'https://www.elabelz.com/en_uk/marketplace/product/getproducturl/'
 
 
 class MixinAE(Mixin):
     retailer = Mixin.retailer + '-ae'
     market = 'AE'
     start_urls = ['https://www.elabelz.com/ar_ae']
-    retailer_currency = 'AED'
-    lang = 'arabic'
+    lang = 'ar'
 
 
 class MixinSA(Mixin):
     retailer = Mixin.retailer + '-sa'
     market = 'SA'
     start_urls = ['https://www.elabelz.com/ar_sa']
-    retailer_currency = 'SAR'
-    lang = 'arabic'
+    lang = 'ar'
 
 
 class MixinKW(Mixin):
     retailer = Mixin.retailer + '-kw'
     market = 'KW'
     start_urls = ['https://www.elabelz.com/ar_kw']
-    retailer_currency = 'KWD'
-    lang = 'arabic'
+    lang = 'ar'
 
 
 class MixinEG(Mixin):
     retailer = Mixin.retailer + '-eg'
     market = 'EG'
     start_urls = ['https://www.elabelz.com/ar_qa/']
-    retailer_currency = 'EGP'
-    lang = 'arabic'
+    lang = 'ar'
 
 
 class ElabelzParseSpider(BaseParseSpider):
     default_brand = 'Elabelz'
-    price_css = '.price-info .price::text'
+    price_css = '.price-info .price::text, .special-price::text'
     brand_css = '.product-name a::text'
-    description_css = '.attribute_description_text ul ::text'
-    care_css = '#product-attribute-specs-table ::text'
+    raw_description_css = '.attribute_description_text ul ::text, '\
+                          '#product-attribute-specs-table ::text'
 
     def parse(self, response):
         product_id = self.product_id(response)
-
         garment = self.new_unique_garment(product_id)
 
         if not garment:
@@ -80,20 +64,19 @@ class ElabelzParseSpider(BaseParseSpider):
         self.boilerplate_normal(garment, response)
 
         garment['image_urls'] = self.image_urls(response)
-        garment['skus'] = self.sku(response)
+        garment['skus'] = self.skus(response)
         garment['gender'] = self.product_gender(garment)
 
-        return self.next_request_or_garment(garment)
+        return garment
 
     def product_id(self, response):
-        x = '//th[contains(text(),"SKU ")]/following-sibling::td/text()'
-        return clean(response.xpath(x))[0]
+        return clean(response.css('td::text'))[0]
 
     def product_name(self, response):
-        return clean(response.css('.productnametop::text'))
+        return clean(response.css('.productnametop::text'))[0]
 
     def product_category(self, response):
-        css = '.breadcrumbs a:not([title*="Home"])::text'
+        css = '.breadcrumbs a::text'
         return clean(response.css(css))
 
     def image_urls(self, response):
@@ -101,25 +84,23 @@ class ElabelzParseSpider(BaseParseSpider):
         return clean(response.css(css))
 
     def product_gender(self, garment):
-        soup = soupify(garment.get('care') + garment.get('name', []) +
-                       garment.get('category') + garment.get('description'))
+        soup = soupify([garment.get('name', '')] + garment.get('category') +
+                       garment.get('description'))
         return self.gender_lookup(soup) or Gender.ADULTS.value
 
-    def sku(self, response):
+    def skus(self, response):
         skus = {}
 
         colour = clean(response.css('.Color p::text'))[0]
         sizes = clean(response.css('.Size span::text'))
 
         common_sku = self.product_pricing_common(response)
-        if not common_sku['currency']:
-            common_sku['currency'] = self.retailer_currency
         common_sku['colour'] = colour
 
         for size in sizes:
             sku = common_sku.copy()
-            sku['size'] = 'One Size' if size == 'OS' else size
-            skus[f'{sku["size"]}_{sku["colour"]}'] = sku
+            sku['size'] = size = self.one_size if size == 'OS' else size
+            skus[f'{size}_{colour}'] = sku
 
         return skus
 
@@ -128,10 +109,10 @@ class ElabelzCrawlSpider(BaseCrawlSpider):
     listings_css = ['#navbar-collapse-grid', '.pages']
     products_css = ['.product_listing_link']
 
-    deny_listings = ['stationary']
+    deny_re = ['stationary']
 
     rules = (
-        Rule(LinkExtractor(restrict_css=listings_css, deny=deny_listings), callback='parse'),
+        Rule(LinkExtractor(restrict_css=listings_css, deny=deny_re), callback='parse'),
         Rule(LinkExtractor(restrict_css=products_css), callback='parse_item'),
     )
 
@@ -147,6 +128,7 @@ class ElabelzUSCrawlSpider(ElabelzCrawlSpider, MixinUS):
 
 class ElabelzAEParseSpider(ElabelzParseSpider, MixinAE):
     name = MixinAE.retailer + '-parse'
+    start_urls = ['https://www.elabelz.com/ar_ae/buy-sacoor-brothers-blue-classic-full-suit-for-men-clothing-417681.html']
 
 
 class ElabelzAECrawlSpider(ElabelzCrawlSpider, MixinAE):
