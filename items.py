12e724d912ccf5e881fbd2bae69cc0fc4ec9d27b
yasirrazakhan <yasirrazakhan93@gmail.com>
2018-11-30 11:03:32 +0500

    add watches2u spider to repo

---
+++ items.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class Watches2USpiderItem(scrapy.Item):
+
+    url = scrapy.Field()
+    name = scrapy.Field()
+    price = scrapy.Field()
+    image_url = scrapy.Field()
+    brand = scrapy.Field()
+    stock = scrapy.Field()
+    category = scrapy.Field()
+
