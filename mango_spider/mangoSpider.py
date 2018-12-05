69dbac1228a1346c294799eee10d26fea2d9f868
yasirrazakhan <yasirrazakhan93@gmail.com>
2018-12-05 17:41:36 +0500

    made changes according to review

--- mango_spider/mangoSpider.py
+++ mango_spider/mangoSpider.py
@@ -5,9 +5,8 @@ from scrapy.http import Request
 from mango_spider.items import MangoSpiderItem
 
 
-class MangospiderSpider(scrapy.Spider):
+class MangoSpider(scrapy.Spider):
     name = 'mango_spider'
-    allowed_domains = ['mango.com']
     start_urls = ['https://shop.mango.com/us']
 
     def parse(self, response):
@@ -24,9 +23,8 @@ class MangospiderSpider(scrapy.Spider):
         page = json.loads(response.text)
         for link in page['menus']:
 
-            level2_links = link.get('menus', [])
-            if not level2_links:
-                continue
+            level2_links = link.get('menus', {})
+
 
             for key, links in level2_links.items():
                 for level3_link in links:
@@ -42,13 +40,13 @@ class MangospiderSpider(scrapy.Spider):
 
         page_session = json.loads(script)
 
-        if not page_session.get('catalogParameters'):
+        category_params = page_session.get('catalogParameters')
+        if not category_params:
             return
 
-        url = url.format(page_session['catalogParameters']['idShop'],
-                         page_session['catalogParameters']['idSection'],
-                         page_session['catalogParameters']['optionalParams']['columnsPerRow']
-                         )
+        url = url.format(category_params['idShop'],
+                         category_params['idSection'],
+                         category_params['optionalParams']['columnsPerRow'])
 
         yield Request(url=url, callback=self.parse_items)
 
@@ -56,25 +54,25 @@ class MangospiderSpider(scrapy.Spider):
         page = json.loads(response.text)
         for items in page['products']['groups']:
             garments = items.get('garments', [])
-            for garments_id in garments:
-                mango_items = MangoSpiderItem()
 
-                item_name = garments[garments_id]['name']
+            for garment_id, garment_info in garments.items():
+                item_name = garment_info['name']
+                item_sale_price = garment_info['price']['salePrice']
+                item_crossedout_price = garment_info['price']['crossedOutPrices']
+                item_discount_rate = garment_info['price']['discountRate']
 
-                item_sale_price = garments[garments_id]['price']['salePrice']
-                item_crossedout_price = garments[garments_id]['price']['crossedOutPrices']
-                item_discount_rate = garments[garments_id]['price']['discountRate']
+                item_stock = garment_info['stock']
+                item_category = garment_info['analyticsEventsData']['category']
 
-                item_stock = garments[garments_id]['stock']
-                item_category = garments[garments_id]['analyticsEventsData']['category']
-
-                size = garments[garments_id]['colors'][0]['sizes']
+                size = garment_info['colors'][0]['sizes']
                 all_sizes = [x['label'] for x in size]
 
-                item_page_url = garments[garments_id]['colors'][0]['linkAnchor']
+                item_page_url = garment_info['colors'][0]['linkAnchor']
 
-                images = garments[garments_id]['colors'][0]['images']
+                images = garment_info['colors'][0]['images']
                 hq_image_url = [x['img1HQSrc'] for x in images]
+
+                mango_items = MangoSpiderItem()
 
                 mango_items['title'] = item_name
                 mango_items['category'] = item_category
