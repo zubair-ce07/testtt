ecfc7a708a72d6d15fcf87073c6a7e72362ae0ca
Asad Manzoor <asad33manzoor@gmail.com>
2018-12-18 18:14:52 +0500

    code clean-hash function revamp-resize- feedback implemented

--- dictionary_hash_structure.py
+++ dictionary_hash_structure.py
@@ -1,4 +1,3 @@
-SIZE=100
 
 
 class DictionaryItem(object):
@@ -7,41 +6,30 @@ class DictionaryItem(object):
         self.value = value
 
     def __repr__(self):
-        return "<DictionaryItem: key={0} value={1}>".format(self.key, self.value)
+        return f"<DictionaryItem: key={self.key} value={self.value}>"
 
 
-class DictionaryHash(object):
+class Dictionary(object):
     def __init__(self):
         self.used = 0
-        self.table = []
-        for i in range(SIZE):
-            self.table.append(DictionaryItem())
+        self.min_size = 10
+        self.dictionary_items = []
+        for i in range(self.min_size):
+            self.dictionary_items.append(DictionaryItem())
 
-    def hash_function(self, key, table_size):
-        total_sum = sum([ord(key[pos]) for pos in range(len(key))])
-        return total_sum % table_size
-
-    def rehash(self, oldhash, table_size):
-        return (oldhash + 1) % table_size
+    def hash_function(self, hashed_value):
+        return hashed_value % len(self.dictionary_items)
 
     def __getitem__(self, key):
-        hash_value = self.hash_function(key, len(self.table))
-        value = None
-        end_search = False
-        initial_value = hash_value
-        while self.table[hash_value].key is not None and not end_search:
-            if self.table[hash_value].key == key:
-                value = self.table[hash_value].value
-                end_search = True
-            else:
-                hash_value = self.rehash(hash_value, len(self.table))
-                if hash_value == initial_value:
-                    return value
-
-        return value
+        dict_item = self.get_dict_item(key)
+        if dict_item is not None:
+            return dict_item.value
+        return dict_item
 
     def __setitem__(self, key, value):
         self.insert_dict_item(key, value)
+        if self.used == (self.min_size - 1):
+            self.resize()
 
     def __delitem__(self, key):
         dict_item = self.get_dict_item(key)
@@ -51,11 +39,18 @@ class DictionaryHash(object):
             self.used -= 1
 
     def __repr__(self):
-        return "DictionaryHash({" + ", ".join([f"{key} : {value}" for key, value in self.items()]) + "})"
+        return "Dictionary({" + ", ".join([f"{key} : {value}" for key, value in self.items()]) + "})"
+
+    def resize(self):
+        for i in range(self.min_size):
+            self.dictionary_items.append(DictionaryItem())
+
+        self.min_size = self.min_size * 2
 
     def insert_dict_item(self, key, value):
-        hash_value = self.hash_function(key, len(self.table))
-        dict_item = self.table[hash_value]
+        hash_value = self.hash_function(hash(key))
+        dict_item = self.dictionary_items[hash_value]
+
         if dict_item.key is None:
             dict_item.key = key
             dict_item.value = value
@@ -64,46 +59,41 @@ class DictionaryHash(object):
             if dict_item.key == key:
                 dict_item.value = value
             else:
-                next_slot = self.rehash(hash_value, len(self.table))
-                next_dict_item = self.table[next_slot]
-                while next_dict_item.key is not None and next_dict_item != key:
-                    next_slot = self.rehash(next_slot, len(self.table))
-                    next_dict_item = self.table[next_slot]
+                next_slot = self.hash_function(hash_value + 1)
+                next_dict_item = self.dictionary_items[next_slot]
 
-                if self.table[next_slot].key is None:
-                    self.table[next_slot].key = key
-                    self.table[next_slot].value = value
+                while next_dict_item.key is not None and next_dict_item.key != key:
+                    next_slot = self.hash_function(next_slot + 1)
+                    next_dict_item = self.dictionary_items[next_slot]
+
+                if next_dict_item.key is None:
+                    next_dict_item.key = key
+                    next_dict_item.value = value
                     self.used += 1
                 else:
-                    self.table[next_slot].value = value
-
-    def get_dict_item(self, key):
-        hash_value = self.hash_function(key, len(self.table))
-        if self.table[hash_value].key == key:
-            return self.table[hash_value]
-        return None
+                    next_dict_item.value = value
 
     def fromkeys(self, keys, value=0):
         self.clear()
         for key in keys:
-            self.table.append(DictionaryItem(key, value))
+            self.dictionary_items.append(DictionaryItem(key, value))
         return self
 
     def clear(self):
         self.used = 0
-        self.table = []
-        for i in range(SIZE):
-            self.table.append(DictionaryItem())
+        self.dictionary_items = []
+        for i in range(self.min_size):
+            self.dictionary_items.append(DictionaryItem())
         return self
 
     def keys(self):
-        return [dict_item.key for dict_item in self.table if dict_item.value is not None]
+        return [dict_item.key for dict_item in self.dictionary_items if dict_item.value is not None]
 
     def values(self):
-        return [dict_item.value for dict_item in self.table if dict_item.value is not None]
+        return [dict_item.value for dict_item in self.dictionary_items if dict_item.value is not None]
 
     def items(self):
-        return [(dict_item.key, dict_item.value) for dict_item in self.table if dict_item.value is not None]
+        return [(dict_item.key, dict_item.value) for dict_item in self.dictionary_items if dict_item.value is not None]
 
     def get(self, key, default=0):
         dict_item = self.get_dict_item(key)
@@ -130,10 +120,29 @@ class DictionaryHash(object):
         if self.used == 0:
             return 'Dictionary is empty !'
 
-        dict_items = [dict_item for dict_item in self.table if dict_item.key is not None]
+        dict_items = [dict_item for dict_item in self.dictionary_items if dict_item.key is not None]
         dict_items[self.used - 1].key = None
         dict_items[self.used - 1].value = None
         self.used -= 1
+
+    def get_dict_item(self, key):
+        hash_value = self.hash_function(hash(key))
+        value = None
+        end_search = False
+        initial_value = hash_value
+        dict_item = self.dictionary_items[hash_value]
+
+        while dict_item.key is not None and not end_search:
+            if dict_item.key == key:
+                value = dict_item
+                end_search = True
+            else:
+                hash_value = self.hash_function(hash_value + 1)
+                dict_item = self.dictionary_items[hash_value]
+                if hash_value == initial_value:
+                    return value
+
+        return value
 
     def setdefault(self, key, value):
         dict_item = self.get_dict_item(key)
@@ -145,7 +154,7 @@ class DictionaryHash(object):
             return value
 
     def copy(self):
-        dup_dict = DictionaryHash()
+        dup_dict = Dictionary()
         for dict_item in self.items():
             dup_dict[dict_item[0]] = dict_item[1]
 
--- pythondict.py
+++ pythondict.py
@@ -1,19 +1,26 @@
-from dictionary_hash_structure import DictionaryHash
+from dictionary_hash_structure import Dictionary
 
 
 def main():
 
-    hash_dict = DictionaryHash()
+    hash_dict = Dictionary()
     hash_dict['year'] = 2018
+    hash_dict[23] = 'numeric value'
     hash_dict['month'] = 'Dec'
     hash_dict['day'] = 'Monday'
     hash_dict['brand'] = '_Ford'
-    hash_dict['model'] = '_Mustang'
+    hash_dict['brand1'] = '_Ford1'
+    hash_dict['brand2'] = '_Ford2'
+    hash_dict['brand3'] = '_Ford3'
+    hash_dict['brand4'] = '_Ford4'
+    hash_dict['brand5'] = '_Ford5'
+    hash_dict['brand6'] = '_Ford6'
+    hash_dict['model7'] = '_Mustang'
 
     print(hash_dict.fromkeys(('key1', 'key2', 'key3'), 0))
 
     hash_dict.pop("day")
-    update_dict = DictionaryHash()
+    update_dict = Dictionary()
     update_dict['year'] = 2019
     update_dict['color'] = '_White'
     update_dict['last item'] = 'last item'
