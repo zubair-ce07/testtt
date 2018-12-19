d0ec2ab39dcc81de500e4682676368360a2c9d2c
Asad Manzoor <asad33manzoor@gmail.com>
2018-12-19 11:31:39 +0500

    data structure-code clean-wrapper class added-feedback implemented

--- pythondict.py
+++ pythondict.py
@@ -1,35 +1,30 @@
-from dictionary_hash_structure import Dictionary
+from dictionary_structure import Dictionary
 
 
 def main():
 
-    hash_dict = Dictionary()
-    hash_dict['year'] = 2018
-    hash_dict[23] = 'numeric value'
-    hash_dict['month'] = 'Dec'
-    hash_dict['day'] = 'Monday'
-    hash_dict['brand'] = '_Ford'
-    hash_dict['brand1'] = '_Ford1'
-    hash_dict['brand2'] = '_Ford2'
-    hash_dict['brand3'] = '_Ford3'
-    hash_dict['brand4'] = '_Ford4'
-    hash_dict['brand5'] = '_Ford5'
-    hash_dict['brand6'] = '_Ford6'
-    hash_dict['model7'] = '_Mustang'
+    dictionary_obj = Dictionary()
+    print(dictionary_obj.fromkeys(('key1', 'key2', 'key3'), 0))
+    dictionary_obj.clear()
+    
+    dictionary_obj['year'] = 2018
+    dictionary_obj[23] = 'numeric value'
+    dictionary_obj['month'] = 'Dec'
+    dictionary_obj['day'] = 'Monday'
+    dictionary_obj['brand'] = '_Ford'
+    dictionary_obj['model'] = '_Mustang'
+    dictionary_obj.pop("day")
 
-    print(hash_dict.fromkeys(('key1', 'key2', 'key3'), 0))
-
-    hash_dict.pop("day")
     update_dict = Dictionary()
     update_dict['year'] = 2019
     update_dict['color'] = '_White'
-    update_dict['last item'] = 'last item'
-    hash_dict.update(update_dict)
-    hash_dict.popitem()
-    hash_dict.setdefault('DOB', '1993')
 
-    hash_dict_copy = hash_dict.copy()
-    print(hash_dict_copy)
+    dictionary_obj.update(update_dict)
+    dictionary_obj.popitem()
+    dictionary_obj.setdefault('DOB', '1993')
+
+    dictionary_obj_copy = dictionary_obj.copy()
+    print(dictionary_obj_copy)
 
 
 if __name__ == "__main__":
