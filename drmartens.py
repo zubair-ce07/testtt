c2bb6117ed739c50221684a490184d0e72a00a76
Asad Manzoor <asad33manzoor@gmail.com>
2018-12-24 14:52:53 +0500

    restrict_css-urls trail-extraction rules-feedback implementation

--- drmartens.py
+++ drmartens.py
@@ -1,33 +1,40 @@
-# -*- coding: utf-8 -*-
-import scrapy
 from scrapy.linkextractor import LinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
 import json
 
 
-class DrmartensSpider(scrapy.Spider):
+class DrmartensSpider(CrawlSpider):
     name = 'drmartens_au_spider'
     allowed_domains = ['www.drmartens.com.au']
     start_urls = ['http://www.drmartens.com.au/']
-    visited_urls = []
     products = {}
+
+    custom_settings = {
+        'DOWNLOAD_DELAY': 5,
+        'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 '
+                      'Safari/537.36'
+    }
 
     rules = [
         Rule(
             LinkExtractor(
-                canonicalize=True,
-                unique=True,
+                restrict_css='.main-menu',
             ),
-            follow=True
+            callback='parse',
+        ),
+        Rule(
+            LinkExtractor(
+                restrict_css='.column.main',
+            ),
+            callback='parse_page',
         )
     ]
 
     def parse(self, response):
-        links = LinkExtractor().extract_links(response)
-
-        for link in links:
-            if self.is_allowed_link(link):
-                yield scrapy.Request(link.url, callback=self.parse_page)
+        for request in super().parse(response):
+            trail = response.meta.get('trail', [])
+            request.meta['trail'] = trail + [response.url]
+            yield request
 
     def parse_page(self, response):
         item = {}
@@ -45,25 +52,17 @@ class DrmartensSpider(scrapy.Spider):
             item['description'] = self.get_product_description(response)
             item['image_urls'] = self.extract_image_urls(response)
 
-            self.visited_urls.append(response.request.headers.get('Referer', None))
-            item['trail'] = self.visited_urls
+            item['trail'] = response.request.meta['trail']
 
             price_details = self.get_price_details(response)
             if price_details:
                 item['price'] = float(price_details[0]) * 100
                 item['currency'] = price_details[1]
+                item['skus'] = self.extract_product_size(item, response)
 
             self.products[item['retailer_sku']] = item
 
             yield item
-        else:
-            links = LinkExtractor().extract_links(response)
-
-            for link in links:
-                if self.is_allowed_link(link):
-                    yield scrapy.Request(link.url, callback=self.parse_page)
-
-        self.visited_urls = []
 
     def get_product_id(self, response):
         return response.css('.extra-product::attr(data-sku)').extract_first()
@@ -97,6 +96,36 @@ class DrmartensSpider(scrapy.Spider):
             image_urls = [image['img'] for image in json.loads(images_data[0])]
 
         return image_urls
+
+    def extract_product_size(self, product, response):
+        record = response.xpath("//script[contains(.,'sizeRangesSort')]/text()").re('jsonConfig:{"attributes":({.*?}})')
+        if not record:
+            return record
+
+        product_size = []
+        record = eval(record[0])
+
+        for key in record:
+            if record[key] and record[key]['code'] == 'size':
+                self.get_size_options(record[key]['options'], product, product_size)
+
+        return product_size
+
+    def get_size_options(self, options, product, product_size):
+
+        for option in options:
+            size_available = True if option['products'] else False
+            sk_ui = f"{product['name']}_{option['label']}"
+
+            product_size.append({
+                "price": product['price'],
+                "currency": product['currency'],
+                "size": option['label'],
+                "sku_id": sk_ui,
+                "size_available": size_available,
+            })
+
+        return product_size
 
     def is_allowed_link(self, link):
         is_allowed = False
