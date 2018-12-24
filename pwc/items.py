7589b620246d7204599b7c42b432328e25a5f8ef
Ramsha Bukhari <ramsha.bukhari@arbisoft.com>
2018-12-24 11:18:59 +0500

    -output processor added

--- pwc/items.py
+++ pwc/items.py
@@ -6,18 +6,19 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import TakeFirst
 
 
 class Item(scrapy.Item):
     categories=scrapy.Field()
     company=scrapy.Field()
     crawled_at=scrapy.Field()
-    description=scrapy.Field()
-    external_id=scrapy.Field()
+    description=scrapy.Field(output_processor=TakeFirst())
+    external_id=scrapy.Field(output_processor=TakeFirst())
     job_date=scrapy.Field()
     job_types=scrapy.Field()
-    location=scrapy.Field()
+    location=scrapy.Field(output_processor=TakeFirst())
     logo_urls=scrapy.Field()
-    provider=scrapy.Field()
-    title=scrapy.Field()
-    url=scrapy.Field()
+    provider=scrapy.Field(output_processor=TakeFirst())
+    title=scrapy.Field(output_processor=TakeFirst())
+    url=scrapy.Field(output_processor=TakeFirst())
