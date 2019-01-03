75232a15d23983548419d7404ac778848cd9d8d1
Tanveer Mahmood <tanveer@a002-00027.arbisoft.com>
2019-01-03 12:30:21 +0500

    Removed extra files

--- dictionary_clone/item.py
+++
@@ -1,7 +0,0 @@
-class Item:
-    __slots__ = ("key", "value", "hashed_key")
-
-    def __init__(self, key=None, value=None, hashed_key=None):
-        self.key = key
-        self.value = value
-        self.hashed_key = hashed_key
