1a013c01e32ced0489184c75df70be4f6929a412
Syed.Adnan <adnan@A002-00105.arbisoft.com>
2019-03-13 15:17:22 +0500

    task updated with PR reccomendations

--- task1.py
+++ task1.py
@@ -1,12 +1,11 @@
+import sys
 from sys import stdout
 
-import sys
 
 date = 0
 max = 1
 min = 3
 humidity = 7 
-
 maxTemp = -50
 minTemp = 80
 maxTempD = ''
@@ -14,52 +13,48 @@ minTempD = ''
 maxHumidity = 0
 maxHumidityD = ''
 
+months = [
+	'Jan', 'Feb' , 'Mar', 'Apr', 'May', 'Jun', 
+	'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+]
 
-months = ['Jan', 'Feb' , 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-
-def printBar(n, m, count):
-
+def print_bar(n, m, count):
 	stdout.write(str(count))
 	stdout.write('- ')
+
 	for x in range(0, n):	
-		stdout.write('+')
+		stdout.write("\033[1;31;40m+")
+
 	stdout.write(str(n) + 'C' )
-
 	print(' ')
-
 	stdout.write(str(count))
 	stdout.write('- ')
 
 	for x in range(0, m):	
-		stdout.write('+')	
+		stdout.write("\033[1;34;40m+")	
+
 	stdout.write(str(m) + 'C' )
 	print(' ')	
 
-
-def printBarInOneRow(n, m, count):
-
+def print_bar_single_row(n, m, count):
 	stdout.write(str(count))
 	stdout.write('- ')
+
 	for x in range(0, n):
 		if x < m :	
-			stdout.write('+')
+			stdout.write("\033[1;34;40m+")
 		else :
-			stdout.write('*')
-	stdout.write(str(m) + 'C - ' + str(n) + 'C' )
+			stdout.write("\033[1;31;40m+")
 
+	stdout.write(str(m) + 'C - ' + str(n) + 'C' )
 	print(' ')
 
-
-
-def findMinMax(filename) :
+def find_min_max(filename) :
 	global maxTemp, minTemp, maxTempD, minTempD, maxHumidity, maxHumidityD 
 	try :
 		f = open(filename)
-
-
 		line = f.readline()
 		line = f.readline()
-
 		s = line.split(',')
 
 		if int(s[max]) > maxTemp :	
@@ -67,21 +62,18 @@ def findMinMax(filename) :
 			maxTempD = s[date]
 	
 		if int(s[min]) < minTemp :
-
 			minTemp = int(s[min])		
 			minTempD = s[date]
+
 		if int(s[humidity]) > maxHumidity :	
-			
 			maxHumidity = int(s[humidity])
 			maxHumidityD = s[date]
 	
 		line = f.readline()
 
 		while line :
-		
 			s = line.split(',')
 			if s[max] :
-				
 				if int(s[max]) > maxTemp :	
 					maxTemp = int(s[max])
 					maxTempD = s[date]
@@ -95,41 +87,32 @@ def findMinMax(filename) :
 				if int(s[humidity]) > maxHumidity :	
 					maxHumidity = int(s[humidity])
 					maxHumidityD = s[date]
-	
-				
+
 			line = f.readline()
-	
 		f.close()
-	
+
 	except :
 		print('file ' + filename  + 'does not exist')
 
-
-def minMaxAverage(filename) :
+def min_max_average(filename) :
 	minAvg = 0
 	maxAvg = 0 
 	humAvg = 0
 	count=0
 	try : 
 		f = open(filename)
-
-
 		line = f.readline()
 		line = f.readline()
 		s = line.split(',')
-
-
 		maxAvg  = maxAvg +int(s[max])				
 		minAvg  = minAvg + int(s[min])		
 		humAvg = humAvg + int(s[humidity])
 		count = count + 1
 		
 		line = f.readline()
-
-		while line :
-		
+		while line :		
 			s = line.split(',')
-			
+	
 			if s[max] :
 				maxAvg  = maxAvg +int(s[max])
 			if s[min] :					
@@ -145,48 +128,38 @@ def minMaxAverage(filename) :
 		print('Lowest Average : ' +  str(minAvg/count) + 'C' )
 		print('Average Humidity : ' +  str(humAvg/count) + 'C' )
 
-
 	except :
 		print('file ' + filename  + 'does not exist')
 
-def dailyReport(filename, option) :
+def daily_report(filename, option) :
 	count = 1
 
 	try : 
 		f = open(filename)
-
-
 		line = f.readline()
 		line = f.readline()
 		
-		while line :
-		
-			s = line.split(',')
-			
+		while line :		
+			s = line.split(',')			
 			if s[max] :
 				if option == 3  :
-					printBar(int(s[max]) , int(s[min]), count)
+					print_bar(int(s[max]) , int(s[min]), count)
 				elif option == 4 :
-					printBarInOneRow(int(s[max]) , int(s[min]), count)
+					print_bar_single_row(int(s[max]) , int(s[min]), count)
 				count = count +1
 
-			line = f.readline()
-	
+			line = f.readline()	
 		f.close()
 
 	except :
 		print('file ' + filename  + 'does not exist')
 
 
-
-
-#dailyReport('./weatherfiles/Murree_weather_2004_Aug.txt') 
-
 if int(sys.argv[1]) == 1 :
 	
 	for s in months :
 		s1 = './weatherfiles/Murree_weather_' +  str(sys.argv[2]) + '_' + s + '.txt'
-		findMinMax(s1)
+		find_min_max(s1)
 	print ('Maximum temperature details : ')
 	print(maxTemp) 
 	print(maxTempD)
@@ -199,13 +172,12 @@ if int(sys.argv[1]) == 1 :
 
 elif int(sys.argv[1]) == 2 :
 	s1 = './weatherfiles/Murree_weather_' +  str(sys.argv[2]) + '_' + months[int(sys.argv[3])] + '.txt'
-	minMaxAverage(s1)	
+	min_max_average(s1)	
 
 elif int(sys.argv[1]) == 3 :
 	s1 = './weatherfiles/Murree_weather_' +  str(sys.argv[2]) + '_' + months[int(sys.argv[3])] + '.txt'
-	dailyReport(s1, int(sys.argv[1]) )	
+	daily_report(s1, int(sys.argv[1]) )	
 
 elif int(sys.argv[1]) == 4 :
 	s1 = './weatherfiles/Murree_weather_' +  str(sys.argv[2]) + '_' + months[int(sys.argv[3])] + '.txt'
-	dailyReport(s1, int(sys.argv[1]) )	
-
+	daily_report(s1, int(sys.argv[1]) )	
