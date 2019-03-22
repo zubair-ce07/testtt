f71fe2d159da08f6b2d2b40a0f7b5e13e56d7514
Aiman.Sajid <aiman@unassigned.arbisoft.com>
2019-03-22 13:21:54 +0500

    FashionSpider Second iteration

--- selectfashion_crawler/selectfashion_crawler/spiders/fashion_spider.py
+++ selectfashion_crawler/selectfashion_crawler/spiders/fashion_spider.py
@@ -21,12 +21,11 @@ class FashionSpider(scrapy.spiders.CrawlSpider):
             'gender': 'women',
             'category': self.product_category(response),
             'url': self.current_page_url(response),
-            'date': self.current_date(response),
-            'price': self.product_price(response),
+            'date': self.current_date(),
             'name': self.product_name(response),
             'description': self.product_description(response),
             'care': self.product_care(response),
-            'image_url': self.image_url(response),
+            'image_url': self.image_urls(response),
             'skus': self.parse_product_detail(response),
         }
 
@@ -43,25 +42,23 @@ class FashionSpider(scrapy.spiders.CrawlSpider):
         return response.css('.panel-body::text').get().strip().split('.')[1:]
 
     def product_price(self, response):
-        price = response.css('.product-price > span::text').getall()
-        return f"Old {price[0]}, New {price[1]}" if len(price) > 1 else price[0]
+        return response.css('.product-price > span::text').getall()
 
     def product_category(self, response):
         return response.css('.breadcrumb > li > a::text').getall()
 
-    def image_url(self, response):
+    def image_urls(self, response):
         return response.css('.zoom ::attr(src)').getall()
 
     def current_page_url(self, response):
         return response.css('link[rel = "canonical"]::attr(href)').get()
 
-    def current_date(self, response):
+    def current_date(self):
         now = datetime.datetime.now()
         return str(now)
 
     def parse_product_detail(self, response):
         product_detail = []
-
         price = self.product_price(response)
         currency = response.css('meta[itemprop="priceCurrency"]::attr(content)').get()
         size_options = response.css('select[title="Size"] > option::text').getall()
@@ -70,11 +67,28 @@ class FashionSpider(scrapy.spiders.CrawlSpider):
 
         for sku_id in size_options[1:]:
             size = sku_id.split('-')
-            product_detail.append(f"price: {price}, currency: {currency}, colour: {colour}, size: {size[0]}")
-            if len(size) > 1 and size[1] == ' SOLD OUT':
-                product_detail.append(f"out_of_stock = true,sku_id: {colour}_{size[0]}")
+
+            if len(price) > 1:
+                product = {
+                    'price': price[0],
+                    'previous price': price[1],
+                    'currency': currency,
+                    'colour': colour,
+                    'size': size[0],
+                    'sku_id': colour + "_" + sku_id,
+                }
             else:
-                product_detail.append(f"sku_id: {colour}_{sku_id}")
+                product = {
+                    'price': price[0],
+                    'currency': currency,
+                    'colour': colour,
+                    'size': size[0],
+                    'sku_id': colour + "_" + sku_id,
+                }
+            if len(size) > 1 and size[1] == ' SOLD OUT':
+                product.update({'out_of_stock': 'true', 'sku_id': colour + "_" + size[0]})
+
+            product_detail.append(product)
 
         return product_detail
 
