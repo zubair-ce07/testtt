29acc219733d44ab9f0540300e6744844f30c5cb
Aiman.Sajid <aiman@unassigned.arbisoft.com>
2019-04-02 13:31:32 +0500

    Removal of duplicate code

--- softsurroundings_crawler/softsurroundings_crawler/spiders/soft_surroundings_spider.py
+++ softsurroundings_crawler/softsurroundings_crawler/spiders/soft_surroundings_spider.py
@@ -1,15 +1,11 @@
-import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Rule, CrawlSpider
-import datetime
-from urllib.parse import urljoin
 from softsurroundings_crawler.items import SoftSurroundingsCrawlerItem, ProductLoader
 
 
 class SoftSurroundingSpider(CrawlSpider):
     name = 'soft_surroundings_spider'
     start_urls = ['https://www.softsurroundings.com/']
-
     category_css = '.clMn'
     product_css = '.product'
 
@@ -23,14 +19,7 @@ class SoftSurroundingSpider(CrawlSpider):
         product['skus'] = []
         product['requests'] = self.size_requests(response)
 
-        if product['requests']:
-            request = product['requests'].pop()
-            request.meta['product'] = product
-            return request
-        else:
-            del product['requests']
-
-        return product
+        return self.next_request_or_product(product)
 
     def parse_info(self, response):
         product = ProductLoader(item=SoftSurroundingsCrawlerItem(), response=response)
@@ -48,26 +37,13 @@ class SoftSurroundingSpider(CrawlSpider):
 
     def category_size(self, response):
         size_ids = response.css('#sizecat > a::attr(id)').getall()
-        size = [size.split('_')[1] for size in size_ids]
-        return size
+        return [size.split('_')[1] for size in size_ids]
 
     def size_requests(self, response):
-        request = []
         size_ids = self.category_size(response)
-        for size in size_ids:
-            page_request = scrapy.Request(urljoin(response.url, size),
-                                          callback=self.parse_skus)
-            request.append(page_request)
+        return [response.follow('/p/' + size, callback=self.parse_skus) for size in size_ids]
 
-        return request
-
-    def parse_skus(self, response):
-        product = response.meta['product']
-        product_skus = self.skus(response)
-
-        if product_skus:
-            product['skus'].append(product_skus)
-
+    def next_request_or_product(self, product):
         if product['requests']:
             request = product['requests'].pop()
             request.meta['product'] = product
@@ -76,6 +52,14 @@ class SoftSurroundingSpider(CrawlSpider):
             del product['requests']
 
         return product
+
+    def parse_skus(self, response):
+        product = response.meta['product']
+        product_skus = self.skus(response)
+        if product_skus:
+            product['skus'].append(product_skus)
+
+        return self.next_request_or_product(product)
 
     def skus(self, response):
         product_detail = []
@@ -87,12 +71,12 @@ class SoftSurroundingSpider(CrawlSpider):
         size_options = response.css('#size a::text ,#size >div > .basesize::text').getall()
         colour_options = response.css('#color > .swatchlink > img::attr(alt) ,#color > div > .basesize::text').getall()
 
-        for sku_id in size_options[1:]:
+        for size in size_options[1:]:
             for color in colour_options:
                 product = {
                     'color': color,
-                    'size': sku_id,
-                    'sku_id': color + "_" + sku_id,
+                    'size': size,
+                    'sku_id': color + "_" + size,
                     'currency': currency,
                     'price': price,
                 }
