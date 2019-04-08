12734cf2f243c17c24fa34917255cb84fc814d54
Azhar.Ali <azhar@A002-00037.arbisoft.com>
2019-04-08 19:55:43 +0500

    new implementation

--- weatherman/report_generation.py
+++ weatherman/report_generation.py
@@ -2,12 +2,14 @@ from calculations import Calculator
 import statistics
 import datetime
 
+
 class  Printer:
+    color_blue="\033[1;34;40m+"
+    color_red="\033[1;31;40m+"
+    color_white="\033[1;37;40m"        
 
-
-    def print_averges(self, all_data, input_date):
-        c = Calculator()
-        max_avg, min_avg, mean_humdity = c.calculating_averages(all_data, input_date)
+    def print_averges(self, max_avg, min_avg, mean_humdity):
+        
         print("\n---------------------------")   
         print(f'Highest Average:  {max_avg}C')
         print(f'Lowest Average:   {min_avg}C')
@@ -15,39 +17,25 @@ class  Printer:
         print("\n---------------------------")
             
 
-    def print_graph(self, all_data, input_date):
-        c = Calculator()
-        final = c.getting_min_max(all_data, input_date)
-        date = datetime.datetime.strptime(str(input_date), '%Y-%m-%d').date()
+    def print_graph(self, final_values, input_date):        
+        
         print(f'{input_date:%B }{input_date:%Y}')
-        for key in final:
-            date = datetime.datetime.strptime(key[0], '%Y-%m-%d').day
-            self.draw_graph(date, key[1], key[2])
+        for value in final_values:
+            self.draw_graph(value.date.day, value.max_temp, value.min_temp)
 
-
-    def print_max(self, all_data, inputt):
-        c = Calculator()
-        max_temp, min_temp, max_humidity = c.getting_temperatures(all_data, inputt)
+    def print_max(self, max_temp, min_temp, max_humidity):
         
         print("\n---------------------------")   
-        date = datetime.datetime.strptime(max_temp[-1][0], '%Y-%m-%d').date()
-        print (f"Max temprature {max_temp[-1][1]}C on {date: %B} {date.day}")
-        
-        date = datetime.datetime.strptime(min_temp[0][0], '%Y-%m-%d').date()
-        print (f"Min temprature {min_temp[0][1]}C on {date: %B} {date.day}")
-        
-        date = datetime.datetime.strptime(max_humidity[-1][0], '%Y-%m-%d').date()
-        print (f"Max Humidity   {max_humidity[-1][1]}% on {date: %B} {date.day}")             
+        print (f"Max temprature {max_temp.max_temp}C on {max_temp.date: %B} {max_temp.date.day}")
+        print (f"Min temprature {min_temp.min_temp}C on {min_temp.date: %B} {min_temp.date.day}")
+        print (f"Max Humidity   {max_humidity.max_humidity}% on {max_humidity.date: %B} {max_humidity.date.day}")             
         print("\n---------------------------")
-            
-    
+           
     def draw_graph(self, day, max_temp, min_temp):
-        print(day, end=' ')
-
-        for i in range(int(min_temp)):
-            print("\033[1;34;40m+", end='')     
         
-        for i in range(int(max_temp)):
-            print("\033[1;31;40m+", end='')
-        print(f'\033[1;37;40m {min_temp}C', end=' ')
-        print(f'\033[1;37;40m {max_temp}C')
+        print(day, end=' ')
+        print(self.color_blue*min_temp, end='')
+        print(self.color_red*max_temp, end='')
+        print(f'{self.color_white} {min_temp}C', end=' ')
+        print(f'{self.color_white} {max_temp}C')
+
