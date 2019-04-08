b6a83a926f22dcfea5602907ebf53419f262919f
Azhar.Ali <azhar@A002-00037.arbisoft.com>
2019-04-08 12:42:24 +0500

    new approach wityh loading data at start and calling other functions according to input

--- weatherman/weatherman.py
+++ weatherman/weatherman.py
@@ -1,60 +1,49 @@
 import datetime
-import report_generation as generator
-import calculations as cal
 import argparse
+from report_generation import Printer
+from Files_reading import Reader
+
+p=Printer()
 
 
-def find_max_values(basepath, year):
-    max_temp = {'MaxTemp': 0, 'date': ''}
-    min_temp = {'MinTemp': 200, 'date': ''}
-    max_humd = {'MaxHumd': 0, 'date': ''}
-   
-    files = cal.get_files(basepath)
-    for file in files:
-        (max_temp, min_temp, max_humd) = cal.getting_max(str(file), max_temp, min_temp, max_humd, year)
-   
-    generator.print_max(max_temp, min_temp, max_humd)
-   
-
-def find_avge_values(basepath, year_month):
-    max_temp = []
-    min_temp = []
-    max_humd = []
-
-    files = cal.get_files(basepath) 
-    for file in files:
-        (max_temp, min_temp, max_humd) = cal.calculating_averages(str(file), year_month, max_temp, min_temp, max_humd)
-
-    generator.print_averges(max_temp, min_temp, max_humd) 
+def find_max_values(all_data, inputt):
+    p.print_max(all_data, inputt)
 
 
-def design_graph(basepath, year_month):
-    max_temp = {}
-    min_temp = {}
-    files = cal.get_files(basepath) 
-    for file in files:
-        (max_temp, min_temp) = cal.getting_temperatures(str(file), year_month, max_temp, min_temp)
-    generator.print_graph(max_temp, min_temp,year_month)
+def find_avge_values(all_data, input_date):
+    
+    p.print_averges(all_data, input_date)
 
 
-def main():
+def design_graph(all_data, input_date):
+    
+    p.print_graph(all_data, input_date)
+    
+
+def main():        
     parser = argparse.ArgumentParser()
     parser.add_argument("basepath", help="directory path",
                         type=str)
     parser.add_argument("-e", "--max_temp", help="year to get max teprature",
-                        type=lambda d: datetime.datetime.strptime(d, '%Y').year)
+                        type=lambda d: datetime.datetime.strptime(d, '%Y').date())
     parser.add_argument("-a", "--avg_temp", help="Year and month to get average temprature",
                         type=lambda d: datetime.datetime.strptime(d, '%Y/%m').date())
     parser.add_argument("-c", "--graph", help="Year and month to Draw graphs",
                         type=lambda d: datetime.datetime.strptime(d, '%Y/%m').date())
+    
+    args = parser.parse_args()  
+    
+    r = Reader(args.basepath)
+    all_data = r.read_files(r.basepath)
 
-    args = parser.parse_args()    
     if args.max_temp:
-            find_max_values(args.basepath, args.max_temp)   
+        find_max_values(all_data, args.max_temp)   
+    
     if args.avg_temp:
-            find_avge_values(args.basepath, args.avg_temp)
+        find_avge_values(all_data, args.avg_temp)
     
     if args.graph:
-            design_graph(args.basepath, args.graph)
-main()
+        design_graph(all_data, args.graph)
+    
 
+main()
