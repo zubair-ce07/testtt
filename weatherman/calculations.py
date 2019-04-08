1c6afacdcc55b98b3ae11c2a0c1ff7a201e09fd7
Azhar.Ali <azhar@A002-00037.arbisoft.com>
2019-04-08 12:38:58 +0500

    new approach as per suggested

--- weatherman/calculations.py
+++ weatherman/calculations.py
@@ -3,110 +3,101 @@ import statistics
 import csv
 import datetime
 import glob
+import Files_reading as reader
+
+class Calculator:
+    all_content = None
+    max_temp = []
+    min_temp = []
+    max_humidity = []
+    
+
+    def converting_into_datetime(self, date):
+        if date != '':
+            date2 = datetime.datetime.strptime(date, '%Y-%m-%d').date()
+            return date2
 
 
-def getting_year(date):
-    return str(datetime.datetime.strptime(date, '%Y-%m-%d').year)
-
-
-def getting_day(date):
-    return str(datetime.datetime.strptime(date, '%Y-%m-%d').day)
-
-
-def getting_year_month(date):
-    year = str(datetime.datetime.strptime(date, '%Y-%m-%d').year)
-    month = str(datetime.datetime.strptime(date, '%Y-%m-%d').month)
-    return (year, month)
-
-
-def get_files(basepath):
-    files = glob.glob(f'{basepath}{"/"}{"*.txt"}')
-    return files
-
-
-def getting_max(files, final_max_temp, final_min_temp, final_max_humd, year):
-    temp = None
-    max_temp = None
-    max_date = None
-    min_temp = None
-    min_date = None
-    max_humd = None
-    max_h_date = None
-    input_file = csv.DictReader(open(files))
-    for row in input_file:
+    def calculating_averages(self, all_data, input_date):
+        for row in all_data:
+            if "PKST" in row.keys():
+                date = self.converting_into_datetime(row["PKST"])
+                if date != None:
+                    if date.year == input_date.year and date.month == input_date.month:    
+                        if  row["Max TemperatureC"] != '' :
+                            self.max_temp.append(row["Max TemperatureC"])
+                        if  row["Min TemperatureC"] != '' :
+                            self.min_temp.append(row["Min TemperatureC"])
+                        if  row[" Mean Humidity"] !='' :
+                            self.max_humidity.append(row[" Mean Humidity"])
+                                
             if "PKT" in row.keys():
-                if getting_year(row["PKT"]) == str(year):
-                    if row["Max TemperatureC"] != '':
-                        temp = int(row["Max TemperatureC"])
-                        if  max_temp is None or max_temp < temp:
-                            max_temp = temp
-                            max_date = row["PKT"]
-                        
-                        if row["Min TemperatureC"] != '':
-                            temp2 = int(row["Min TemperatureC"])
-                            if min_temp is None or min_temp > temp2:
-                                min_temp = temp2
-                                min_date = row["PKT"]
-                        
-                        if row["Max Humidity"] != '':
-                            temp3 = int(row["Max Humidity"])
-                            if max_humd is None or max_humd < temp3:
-                                max_humd = temp3
-                                max_h_date = row["PKT"]
-                        
-                        if max_temp > int(final_max_temp['MaxTemp']):
-                            final_max_temp['MaxTemp'] = max_temp
-                            final_max_temp['date'] = max_date
-                        
-                        if min_temp < int(final_min_temp['MinTemp']):
-                            final_min_temp['MinTemp'] = min_temp
-                            final_min_temp['date'] = min_date
-                        
-                        if max_humd > int(final_max_humd['MaxHumd']):
-                            final_max_humd['MaxHumd'] = max_humd
-                            final_max_humd['date'] = max_h_date
+                date = self.converting_into_datetime(row["PKT"])
+                if date != None:
+                    if date.year == input_date.year and date.month == input_date.month:    
+                        if  row["Max TemperatureC"] != '':
+                            self.max_temp.append(row["Max TemperatureC"])
+                        if  row["Min TemperatureC"] != '':
+                            self.min_temp.append(row["Min TemperatureC"])
+                        if  row[" Mean Humidity"] != '':
+                            self.max_humidity.append(row[" Mean Humidity"])
+                            
+        self.max_temp = [int(x) for x in self.max_temp if x]
+        self.min_temp = [int(x) for x in self.min_temp if x]
+        self.max_humidity = [int(x) for x in self.max_humidity if x]
+        
+        return max(self.max_temp), min(self.min_temp), max(self.max_humidity)
+
+
+    def getting_temperatures(self, all_data, input_date):
+        max_temp = []
+        min_temp = []
+        max_humidity = [] 
+        for row in all_data:
+            if "PKST" in row.keys():
+                date=self.converting_into_datetime(row["PKST"])
+                if date != None:
+                    if date.year == input_date.year:    
+                        if  row["Max TemperatureC"] != '':
+                            max_temp.append ((row["PKST"], row["Max TemperatureC"]))
+                        if  row["Min TemperatureC"] != '':    
+                            min_temp.append ((row["PKST"], row["Min TemperatureC"]))
+                        if  row["Max Humidity"] != '':    
+                            max_humidity.append((row["PKST"], row["Max Humidity"]))
             
-    return (final_max_temp, final_min_temp, final_max_humd)
-
-
-def calculating_averages(files, year_month, max_temp, min_temp, max_humd):
-     (year, month) = getting_year_month(str(year_month))
-     input_file = csv.DictReader(open (files))
-     for row in input_file:
             if "PKT" in row.keys():
-                if getting_year_month(row["PKT"]) == (year, month):
-                         for row in input_file:
-                             max_temp.append(row["Max TemperatureC"])
-                             min_temp.append(row["Min TemperatureC"])
-                             max_humd.append(row[" Mean Humidity"])
+                date = self.converting_into_datetime(row["PKT"])
+                if date != None:
+                    if date.year == input_date.year:    
+                        if  row["Max TemperatureC"] != '':
+                            max_temp.append ((row["PKT"], row["Max TemperatureC"]))
+                        if  row["Min TemperatureC"] != '':    
+                            min_temp.append ((row["PKT"], row["Min TemperatureC"]))
+                        if  row["Max Humidity"] !='' :    
+                            max_humidity.append((row["PKT"], row["Max Humidity"]))
+                    
+        max_temp.sort(key = lambda x: int(x[1]))
+        min_temp.sort(key = lambda x: int(x[1]))
+        max_humidity.sort(key = lambda x: int(x[1]))
+            
+        return max_temp, min_temp, max_humidity
+        
 
-     max_temp = [int(t) for t in max_temp if t]
-     min_temp = [int(t) for t in min_temp if t]
-     max_humd = [int(t) for t in max_humd if t]
+    def getting_min_max(self, all_data, input_date):
+        final = []
+        for row in all_data:
+            if "PKST" in row.keys():
+                date = self.converting_into_datetime(row["PKST"])
+                if date != None:
+                    if date.year == input_date.year and date.month == input_date.month:    
+                        if  row["Max TemperatureC"] != '' and row["Min TemperatureC"] != '':
+                            final.append ((row["PKST"], row["Max TemperatureC"],row["Min TemperatureC"]))
 
-     return (max_temp, min_temp, max_humd)
-
-def getting_temperatures(files,year_month,max_temp,min_temp):
-     
-     (year,month) = getting_year_month(str(year_month))
-     input_file = csv.DictReader(open (files))
-     for row in input_file:
             if "PKT" in row.keys():
-                if getting_year_month(row["PKT"]) == (year, month):
-                         for row in input_file:
-                             if row["Max TemperatureC"] != '' and row["Min TemperatureC"] != '':
-                                max_temp[getting_day(row["PKT"])] = row["Max TemperatureC"]
-                                min_temp[getting_day(row["PKT"])] = row["Min TemperatureC"]
-                                                  
-     return (max_temp, min_temp)
-
-
-def draw_graph(day, max_temp, min_temp):
-    print(day, end=' ')                      
-    for i in range(int(min_temp)):
-        print("\033[1;34;40m+", end='')     
-    for i in range(int(max_temp)):
-        print("\033[1;31;40m+", end='')
-    print("\033[1;37;40m" + str(min_temp) + "C", end=' ')
-    print("\033[1;37;40m" + str(max_temp) + "C")
-
+                date = self.converting_into_datetime(row["PKT"])
+                if date != None:
+                    if date.year == input_date.year and date.month == input_date.month:    
+                       if  row["Max TemperatureC"] != '' and row["Min TemperatureC"] != '':
+                            final.append ((row["PKT"], row["Max TemperatureC"],row["Min TemperatureC"]))
+        
+        return final
