651838583bc048dc141c30ce14cdcf18b131a1b5
Azhar.Ali <azhar@A002-00037.arbisoft.com>
2019-04-09 17:18:19 +0500

    minor bug fixed

--- weatherman/files_reading.py
+++ weatherman/files_reading.py
@@ -2,12 +2,10 @@ import glob
 import csv
 from weather_records import Weather_record
 
-
 class file_reader:
 
     def read_files(self, basepath):
         all_content = []
-
         for f in glob.glob(f'{basepath}{"/"}{"*.txt"}'):
             for record in csv.DictReader(open(f)):
                 if (record.get("PKST") or record.get("PKT")) and self.is_valid(record):                    
@@ -15,8 +13,8 @@ class file_reader:
         return all_content
 
     def is_valid(self, record):
-        if record.get("Max TemperatureC") and record.get("Min TemperatureC") and record.get("Max Humidity") and record.get(" Mean Humidity"):
+        if record.get("Max TemperatureC") and record.get("Min TemperatureC") and \
+            record.get("Max Humidity") and record.get(" Mean Humidity"):
             return True
         else:
             False
-    
