9bdcbc1550a1af787af4b4dfa660a4c9c3b4a6eb
Azhar.Ali <azhar@A002-00037.arbisoft.com>
2019-04-10 14:49:03 +0500

    removed one function

--- weatherman/files_reading.py
+++ weatherman/files_reading.py
@@ -8,11 +8,7 @@ class FileReader:
         all_content = []
         for f in glob.glob(f'{basepath}{"/"}{"*.txt"}'):
             for record in csv.DictReader(open(f)):
-                if self.is_valid_record(record):                    
+                if (record.get("PKST") or record.get("PKT")) and record.get("Max TemperatureC") and \
+                    record.get("Min TemperatureC") and record.get("Max Humidity") and record.get(" Mean Humidity"):                    
                     all_content.append(WeatherRecord(record))        
         return all_content
-
-    def is_valid_record(self, record):
-        return (record.get("PKST") or record.get("PKT")) and record.get("Max TemperatureC") and \
-               record.get("Min TemperatureC") and record.get("Max Humidity") and record.get(" Mean Humidity")
-                
