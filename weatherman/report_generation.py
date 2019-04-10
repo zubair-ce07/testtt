787e9a187d61b2844b550ba8a244fed1fc7fad7f
Azhar.Ali <azhar@A002-00037.arbisoft.com>
2019-04-10 13:48:41 +0500

    change all function names

--- weatherman/report_generation.py
+++ weatherman/report_generation.py
@@ -5,32 +5,27 @@ class  ReportGenerator:
     COLOR_RED = "\033[1;31;40m+"
     COLOR_WHITE = "\033[1;37;40m"        
 
-    def print_averges(self, max_avg, min_avg, mean_humdity):
-        
+    def generate_averges(self, max_avg, min_avg, mean_humdity):
         print("\n---------------------------")   
         print(f'Highest Average:  {max_avg}C')
         print(f'Lowest Average:   {min_avg}C')
         print(f'Average Mean Humidity: {mean_humdity}%') 
         print("\n---------------------------")
             
-
-    def print_graph(self, final_values, input_date):
-
+    def generate_graph(self, final_values, input_date):
         print(f'{input_date:%B }{input_date:%Y}')
         
         for value in final_values:
-            self.draw_graph(value.date.day, value.max_temp, value.min_temp)
+            self.formatting_graph(value.date.day, value.max_temp, value.min_temp)
 
-    def print_max(self, max_temp, min_temp, max_humidity):
-        
+    def generate_maximums(self, max_temp, min_temp, max_humidity):
         print("\n---------------------------")   
         print (f"Max temprature {max_temp.max_temp}C on {max_temp.date: %B} {max_temp.date.day}")
         print (f"Min temprature {min_temp.min_temp}C on {min_temp.date: %B} {min_temp.date.day}")
         print (f"Max Humidity {max_humidity.max_humidity}% on {max_humidity.date: %B} {max_humidity.date.day}")             
         print("\n---------------------------")
            
-    def draw_graph(self, day, max_temp, min_temp):
-        
+    def formatting_graph(self, day, max_temp, min_temp):
         print(day, end=' ')
         print(self.COLOR_BLUE*min_temp, end='')
         print(self.COLOR_RED*max_temp, end='')
