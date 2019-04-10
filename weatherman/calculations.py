e20065ee1962568b8194527962f68715c7273cf3
Azhar.Ali <azhar@A002-00037.arbisoft.com>
2019-04-10 14:46:42 +0500

    function name changed

--- weatherman/calculations.py
+++ weatherman/calculations.py
@@ -2,15 +2,16 @@ import datetime
 
 class Calculator: 
     
-    def calculating_averages(self, all_data, input_date):
-        records = [record for record in all_data if self.is_valid_record(record, input_date)]         
+    def calculate_averages(self, all_data, input_date):
+        records = [record for record in all_data if record.date.year == input_date.year and \
+                  record.date.month == input_date.month]         
         avg_max_temp = sum([item.max_temp for item in records]) // len(records)
         avg_min_temp = sum([item.min_temp for item in records]) // len(records)
         avg_mean_humidity = sum([item.max_humidity for item in records]) // len(records)
 
         return avg_max_temp, avg_min_temp, avg_mean_humidity
         
-    def getting_temperatures(self, all_data, input_date):
+    def get_temperatures(self, all_data, input_date):
         records = [record for record in all_data if record.date.year == input_date.year]
         record_max_temp = max(records, key=lambda item: item.max_temp)
         record_min_temp = max(records, key=lambda item: item.min_temp)
@@ -18,9 +19,6 @@ class Calculator:
         
         return record_max_temp, record_min_temp, record_max_humd
         
-    def getting_records(self, all_data, input_date):
-        return [record for record in all_data if self.is_valid_record(record, input_date)]
-
-    def is_valid_record(self, record, input_date):
-        return record.date.year == input_date.year and record.date.month == input_date.month
-    
+    def get_records(self, all_data, input_date):
+        return [record for record in all_data if record.date.year == input_date.year and \
+                record.date.month == input_date.month]
