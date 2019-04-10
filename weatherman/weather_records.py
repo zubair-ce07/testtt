8ce156094d235b411d6c35559b68dea8ff97a18e
Azhar.Ali <azhar@A002-00037.arbisoft.com>
2019-04-10 13:47:42 +0500

    change function name

--- weatherman/weather_records.py
+++ weatherman/weather_records.py
@@ -3,9 +3,10 @@ import datetime
 class WeatherRecord:
 
     def __init__(self, record):   
-        self.date = datetime.datetime.strptime(record.get("PKT") or record.get("PKST"), '%Y-%m-%d').date()
+        date = record.get("PKT") or record.get("PKST")
+        self.date = datetime.datetime.strptime(date, '%Y-%m-%d').date()
         self.max_temp = int(record.get("Max TemperatureC"))
         self.min_temp = int(record.get("Min TemperatureC"))
         self.max_humidity = int(record.get("Max Humidity"))
         self.mean_humidity = int(record.get(" Mean Humidity"))
-
+        
