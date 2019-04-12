52820bcf1f660f98ea726c2f0562056e7bdd51db
Aiman.Sajid <aiman@unassigned.arbisoft.com>
2019-04-12 17:41:12 +0500

    Soft wool Spider - Code Refactor

--- smartwool_crawler/smartwool_crawler/spiders/smart_wool_spider.py
+++ smartwool_crawler/smartwool_crawler/spiders/smart_wool_spider.py
@@ -11,13 +11,11 @@ class SmartWoolSpider(CrawlSpider):
     start_urls = ['https://www.smartwool.com/homepage.html']
     category_css = '.topnav-accordion-l3-dropdown'
     product_css = '.product-block-name-link'
-    currency = ''
+
     gender = ("womens", "mens", "kids")
     allow_links = ('/shop')
-    page_size = ['24', '48', '72']
+    page_size = ['24', '48', '72', '96']
     store_id = ''
-    lang_id = ''
-    catalog_id = ''
     category_id = ''
     product_id = ''
 
@@ -32,8 +30,9 @@ class SmartWoolSpider(CrawlSpider):
         product['color_key'] = response.css('.attr-color-js::attr(data-attribute-id)').get()
         product['prices_detail'] = self.product_price_detail(response)
         product['size'] = self.product_size(response)
-        request_url = f"https://www.smartwool.com/webapp/wcs/stores/servlet/VFAjaxProductAvailabilityView?storeId={self.store_id}&" \
-            f"langId={self.lang_id}&productId={self.product_id}"
+
+        request_url = (f"https://www.smartwool.com/webapp/wcs/stores/servlet/VFAjaxProductAvailabilityView?"
+                       f"storeId={self.store_id}& &productId={self.product_id}")
 
         return scrapy.Request(request_url, meta={"product": product}, callback=self.parse_skus)
 
@@ -53,42 +52,35 @@ class SmartWoolSpider(CrawlSpider):
 
     def parse_product_page(self, response):
         self.store_id = response.css('meta[name="storeId"]::attr(content)').get()
-        self.lang_id = response.css('meta[name="langId"]::attr(content)').get()
-        self.catalog_id = response.css('meta[name="catalogId"]::attr(content)').get()
         self.category_id = response.css('meta[name="categoryId"]::attr(content)').get()
 
         for size in self.page_size:
-            requested_url = self.page_request_url(response, size)
+            requested_url = f"{response.url}/VFAjaxGetFilteredSearchResultsView?categoryId={self.category_id}&beginIndex={size}"
             yield scrapy.Request(url=requested_url, callback=self.parse_product_request)
 
     def parse_product_request(self, response):
-        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
-                          response.text)
+        urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),])+', response.text)
         if urls:
             for url in urls[:-1]:
                 yield scrapy.Request(url=url, callback=self.parse_item)
 
-    def page_request_url(self, response, page_size):
-        url = f"{response.url}/VFAjaxGetFilteredSearchResultsView?categoryId={self.category_id}&searchSource=N&storeId={self.store_id}&" \
-            f"catalogId={self.catalog_id}&langId={self.lang_id}&beginIndex={page_size}&returnProductsOnly=true&requesttype=ajax"
-        return url
-
     def parse_skus(self, response):
         product = response.meta['product']
         product['skus'] = self.skus(response)
+
         del product['prices_detail'], product['color_key'], product['size'], product['size_key']
         return product
 
     def skus(self, response):
         product = response.meta['product']
         product_detail = []
+
         raw_product = json.loads(response.text)
-        stock = self.stock_detail(raw_product, product['size_key'])
+        stock_detail = self.stock_detail(raw_product, product['size_key'])
 
         for color in raw_product['attributes'][product['color_key']]:
             for sku_id in color['catentryId']:
-                sku_id = str(sku_id)
-                stock, size = stock[sku_id]
+                stock, size = stock_detail[str(sku_id)]
 
                 skus = product['prices_detail']
                 skus['color'] = color['display']
@@ -97,22 +89,20 @@ class SmartWoolSpider(CrawlSpider):
                 if not stock:
                     skus['out_of stock'] = True
 
-                product_detail.append(skus)
-
+                product_detail.append(skus.copy())
         return product_detail
 
-    def stock_detail(self, data, size_key):
+    def stock_detail(self, raw_product, size_key):
         stock_detail = {}
 
-        for sku_id, stock in data['stock'].items():
-            size = [size['display'] for size in data['attributes'][size_key] if sku_id in str(size['catentryId'])]
+        for sku_id, stock in raw_product['stock'].items():
+            size = [size['display'] for size in raw_product['attributes'][size_key] if sku_id in str(size['catentryId'])]
             stock_detail[sku_id] = (stock, '/'.join(size))
 
         return stock_detail
 
     def product_category(self, response):
-        category_list = response.css(
-            '.page-breadcrumb-list >li >a::text , .page-breadcrumb-list >li>span::text').getall()
+        category_list = response.css('.page-breadcrumb-list a::text,.page-breadcrumb-list span::text').getall()
         return [categorry.strip() for categorry in category_list]
 
     def product_gender(self, response):
@@ -130,15 +120,13 @@ class SmartWoolSpider(CrawlSpider):
             'currency': response.css('meta[property="og:price:currency"]::attr(content)').get(),
         }
 
-        previous_price = response.css(
-            '.product-content-info-original-price.original-price.original-price-js ::text').get()
+        previous_price = response.css('.original-price-js ::text').get()
         if previous_price:
             product_price['previous_price'] = previous_price
 
         return product_price
 
     def product_image_urls(self, response):
-        image_urls = response.css(
-            '.product-content-form-attr-container.attr-container.attr-container-js.collapsable-container-js.color-swatches-js img::attr(src)').getall()
+        image_urls = response.css('.color-swatches-js img::attr(src)').getall()
         return ["https:" + image for image in image_urls]
 
