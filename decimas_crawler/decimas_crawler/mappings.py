fbcfbd14d023be2d001a500c7f30621fd2abcd33
Azhar.Ali <azhar@A002-00037.arbisoft.com>
2019-04-17 20:31:12 +0500

    minor changes

--- decimas_crawler/decimas_crawler/mappings.py
+++ decimas_crawler/decimas_crawler/mappings.py
@@ -1,3 +1,6 @@
 
+
 class Mapping:
+
     currency_map = {'€':"EUR", '$':"Dollar" }
+    gender_map = {'MAN':'men', 'WOMAN': 'woman', 'BOY':'boy', 'GIRL': 'girl', 'UNISEX-ADULTS': 'unisex-adults', 'UNISEX-KIDS': 'unisex-kids'}
