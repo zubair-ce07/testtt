b6f40452bb2af738b664b3c28f79045e8192fb3c
Azhar.Ali <azhar@A002-00037.arbisoft.com>
2019-04-17 16:33:47 +0500

    include seperate functions to get each attribute seperately

--- decimas_crawler/decimas_crawler/spiders/product_crawler.py
+++ decimas_crawler/decimas_crawler/spiders/product_crawler.py
@@ -1,57 +1,101 @@
 # -*- coding: utf-8 -*-
-import scrapy
 from ..items import DecimasCrawlerItem
+from ..mappings import Mapping
+import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Rule
 import json
-import re
+
 
 class ProductCrawlerSpider(scrapy.spiders.CrawlSpider):
-    name = 'product_crawler'
+    name = 'decimas_crawler'
     allowed_domains = ['decimas.es']
     start_urls = ['https://www.decimas.es']
-    rules = [
-            Rule(
-            LinkExtractor(
-                restrict_css = ".clever-mega-menu",
-            ),
+    rules = (
+        Rule(LinkExtractor(restrict_css=".clever-mega-menu"), callback='parse', follow=True),
+        Rule(LinkExtractor(restrict_css="li.product-item"), callback='parse_item', follow=True),
+    )
     
-            callback = 'parse',
-            follow = True,
-        ),
-
-        Rule(
-            LinkExtractor(
-                restrict_css = "li.product-item",
-            ),
-    
-            callback = 'parse_item',
-            follow=True,
-        ),
-    ]
-
-    def parse_product(self, response):
-        yield scrapy.Request(response.url, callback=self.parse_item)    
-    
-    def parse_item(self, response):
-        previous_price = None
-        skus =[]
+    def parse_item(self, response): 
         item = DecimasCrawlerItem()
+        item['retailer_sku'] = self.extract_retailer_sku(response)
+        item['gender'] = self.extract_gender(response)
+        item['brand'] = self.extract_brand(response)
+        item['url'] = self.extract_url(response)
+        item['retailer'] = self.extract_retailer()
+        item['name'] = self.extract_name(response)
+        item['description'] = self.extract_description(response)
+        item['old_price'] = self.extract_old_price(response)
+        item['img_urls'] = self.extract_img_urls(response)
+        item['skus'] = self.extract_sku(response)
+        item['currency'] = self.extract_currency(response)
+        item['final_price'] = self.extract_final_price(response)
+        yield item
 
-        currency_conversion = {'€':"EUR", '$':"Dollar" }
+    def extract_retailer_sku(self, response):
+        return response.css('div.price-box::attr(data-product-id)').extract_first()
+        
+    def extract_gender(self, response):
         genders = ['MAN', 'WOMAN', 'BOY', 'GIRL', 'MAGAZINE', 'JUNIORA']
+        product_name = response.css('div.attribute.name h1::text').extract_first()
         
-        brands = response.css('a.brand img::attr(title)').extract()
-        objs = response.css("script[type='text/x-magento-init']::text").extract()
+        for gender in genders:
+            if gender in product_name:
+                return gender
+
+    def extract_brand(self, response):
+        brand_names = response.css('a.brand img::attr(title)').extract()
+        product_name = response.css('div.attribute.name h1::text').extract_first()
         
-        json_object = json.loads(objs[10])
+        for brand in brand_names:
+            if brand.lower() in product_name.lower():
+                return brand
+    
+    def extract_url(self, response):
+        return response.url
+    
+    def extract_retailer(self):
+        return "Decimas.es"
+
+    def extract_name(self, response):
+        return response.css('div.attribute.name h1::text').extract_first()    
+        
+    def extract_description(self, response):
+        return response.css('div.value::text').extract_first()
+
+    def extract_old_price(self, response):
+        previous_price = None 
+        current_price = response.css('span.price::text').extract()
+        current_price = [x.replace(u'\xa0€', u' ') for x in current_price if x]
+        current_price = [float(x.replace(u',', u'.')) for x in current_price]
+        if len(current_price) >= 2:
+            previous_price = current_price.remove(min(current_price))         
+        
+        return previous_price
+    
+    def extract_img_urls(self, response):
+        all_images = []
+        script_text = response.css("script[type='text/x-magento-init']::text").extract()
+        json_object = json.loads(script_text[10])
+        image_sources = json_object["[data-role=swatch-options]"]["Magento_Swatches/js/swatch-renderer"]["jsonConfig"]['images']
         data = json_object["[data-role=swatch-options]"]["Magento_Swatches/js/swatch-renderer"]["jsonConfig"]
-        image_sources = data['images']
-        
+
+        for color in data['attributes']['93']['options']:
+            for product in color['products']:
+                product_images = image_sources[product]
+                for product_image in product_images:
+                    all_images.append(product_image['full'])       
+        return all_images    
+    
+    def extract_sku(self, response):
+        skus =[]
+        script_text = response.css("script[type='text/x-magento-init']::text").extract()
+        json_object = json.loads(script_text[10])
+        data = json_object["[data-role=swatch-options]"]["Magento_Swatches/js/swatch-renderer"]["jsonConfig"]
         currency = data['currencyFormat']
         currency = (currency.replace("%s",u' ')).strip()
-        currency = currency_conversion[currency]
-        
+        currency = Mapping.currency_map[currency]
+    
         for color in data['attributes']['93']['options']:
             for size in data['attributes']['154']['options']:
                 for k in color['products']:
@@ -59,41 +103,24 @@ class ProductCrawlerSpider(scrapy.spiders.CrawlSpider):
                         if k==t:
                             skus.append(self.make_sku(data['optionPrices'][t]['finalPrice']['amount'], \
                             currency, color['label'], size['label'], k))
+        return skus
+
+    def extract_currency(self, response):
+        script_text = response.css("script[type='text/x-magento-init']::text").extract()
+        json_object = json.loads(script_text[10])
+        data = json_object["[data-role=swatch-options]"]["Magento_Swatches/js/swatch-renderer"]["jsonConfig"]
+        currency = data['currencyFormat']
+        currency = (currency.replace("%s",u' ')).strip()
+        return Mapping.currency_map[currency]
         
-        all_image = self.img_source_extractor(image_sources, data['attributes']['93']['options'])
-        print(f'***{all_image}')
-        product_titile = response.css('div.attribute.name h1::text').extract_first()
-    
-        brand = self.brand_name_extractor(brands, product_titile)
-        
+
+    def extract_final_price(self, response):
         current_price = response.css('span.price::text').extract()
         current_price = [x.replace(u'\xa0€', u' ') for x in current_price if x]
         current_price = [float(x.replace(u',', u'.')) for x in current_price]
         if len(current_price) >= 2:
-            current_price = min(current_price)
-        product_description = response.css('div.value::text').extract_first()
-        gender = self.gender_extractor(genders, product_titile)
-        item['retailer_sku'] = response.css('div.price-box::attr(data-product-id)').extract_first()
-        item['gender'] = gender
-        item['brand'] = brand
-        item['url'] = response.url
-        item['retailer'] = "Decimas.es"
-        item['name'] = product_titile
-        item['description'] = product_description
-        item['old_price'] = previous_price
-        item['skus']=skus
-        item['currency'] = currency
-        item['final_price'] = current_price
-       
-        yield item
-
-    def img_source_extractor(self, image_sources, products):
-        all_images = []
-        for color in products:
-            for product in color['products']:
-                for x in product:
-                    print(f'****{image_sources[x]}')    
-        return all_images    
+            current_price = min(current_price)         
+        return current_price
     
     def make_sku(self, price, currency, color, size, sku_id):
         skus = {}
@@ -102,16 +129,4 @@ class ProductCrawlerSpider(scrapy.spiders.CrawlSpider):
         skus['size'] = size
         skus['color'] = color
         skus['sku_id'] = sku_id
-        
         return skus
-    
-    def brand_name_extractor(self, brand_names, product_name):
-        for brand in brand_names:
-            if brand.lower() in product_name.lower():
-                return brand
-        
-    def gender_extractor(self, genders, product_name):
-        for gender in genders:
-            if gender in product_name:
-                return gender
-    
