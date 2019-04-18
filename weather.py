242afa23edc9dd9da0de1bfecfe2a2125f01ae38
Syed.Adnan <adnan@A002-00105.arbisoft.com>
2019-04-18 12:18:58 +0500

    final-revisions

--- weather.py
+++ weather.py
@@ -3,15 +3,7 @@ class Weather:
 	def __init__(self, attribute_date, attribute_max, attribute_min, attribute_humidity):
 		self.weather_date = attribute_date
 		
-		if attribute_max:
-			self.max_temperature = int(attribute_max)
-		else :
-			self.max_temperature = 0
-		if attribute_min:
-			self.min_temperature = int(attribute_min)
-		else : 
-			self.min_temperature = 0
-		if attribute_humidity:
-			self.max_humidity = int(attribute_humidity)
-		else :
-			self.max_humidity = 0
+		self.max_temperature = int(attribute_max) if attribute_max else 0	
+		self.min_temperature = int(attribute_min) if attribute_min else 0		
+		self.max_humidity = int(attribute_humidity) if attribute_humidity else 0
+		
