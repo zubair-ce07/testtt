b8b8d842e12002fac9e22b22a2bff3ddf8bf5280
tanveer mahmood <tanveerpucit111@gmail.com>
2019-04-19 15:00:48 +0500

    remove extract and update skus(), product_id()

--- vagabond_spider/vagabond_spider.py
+++ vagabond_spider/vagabond_spider.py
@@ -1,86 +1,84 @@
 from scrapy.spiders import Rule
 
-from .base import BaseParseSpider, BaseCrawlSpider, LinkExtractor, clean, soupify
+from .base import BaseParseSpider, BaseCrawlSpider, LinkExtractor, clean, soupify, Gender
 
 
 class Mixin:
-
     retailer = 'vagabond'
     default_brand = 'Vagabond'
     allowed_domains = ['vagabond.com']
 
 
 class MixinUK(Mixin):
-
     market = 'UK'
     retailer = Mixin.retailer + '-uk'
     start_urls = ['https://www.vagabond.com/']
+    image_url_t = 'https://www.vagabond.com{}'
 
 
 class VagabondParseSpider(BaseParseSpider):
-
     price_css = '.productPrice .price ::text'
     raw_description_css = '#productInformationArea .tab-content ::text'
 
     def parse(self, response):
-        product_id = self.product_id(response)
-        garment = self.new_unique_garment(product_id)
+        garment = self.new_unique_garment(self.product_id(response))
 
         if not garment:
             return
 
         self.boilerplate_normal(garment, response)
 
-        garment['skus'] = self.skus(response)
         garment["gender"] = self.product_gender(response)
-        garment['image_urls'] = self.image_urls(response)
+        garment['image_urls'] = self.product_image_urls(response)
+        garment['skus'] = self.skus(response)
 
         return garment
 
+    def product_category(self, response):
+        return [i[0] for i in response.meta['trail']]
+
+    def product_name(self, response):
+        return soupify(clean(response.css('.product_name ::text')))
+
     def product_id(self, response):
-        css = 'script:contains(\'ecommerce\')::text'
-        return response.css(css).re_first('\'id\':\s*\'(.*)\',')
+        return clean(response.css('#ProductCode ::attr(value)'))[0]
 
     def product_gender(self, response):
         soup = soupify([response.url] + self.product_category(response))
-        return self.gender_lookup(soup)
+        return self.gender_lookup(soup) or Gender.ADULTS.value
 
-    def product_category(self, response):
-        return [i[1] for i in response.meta['trail']][-1].split('/')[4:-1]
-
-    def product_name(self, response):
-        return clean(' '.join(response.css('.product_name ::text').extract()))
-
-    def image_urls(self, response):
-        base_url = 'https://www.vagabond.com{}'
+    def product_image_urls(self, response):
         css = '.product-image-navigation.thumbs img ::attr(data-src)'
-        return [base_url.format(rel_url) for rel_url in response.css(css).extract()]
+        return [self.image_url_t.format(rel_url) for rel_url in clean(response.css(css))]
 
     def skus(self, response):
         skus = {}
-        colour_css = '#tab2 > div > span:nth-child(2) ::text'
-
         common_sku = self.product_pricing_common(response)
-        common_sku['colour'] = self.detect_colour(response.css(colour_css).extract_first())
+        sizes_s = response.css('#productInformationArea .sel__box__options ')
+        colour = self.detect_colour(soupify(clean(response.css('#tab2 ::text'))))
 
-        for size_s in response.css('#productInformationArea .sel__box__options '):
-            sku = common_sku.copy()
-            sku['size'] = clean(size_s.css('::text').extract_first().split(' ')[0])
+        if colour:
+            common_sku['colour'] = colour
 
-            if size_s.css('.disabled'):
-                sku['out_of_stock'] = True
+        if sizes_s:
 
-            skus[f'{common_sku["colour"]}_{sku["size"]}'] = sku
+            for size_s in sizes_s:
+                sku = common_sku.copy()
+                sku['size'] = clean(size_s.css('::text')[0])
+
+                if size_s.css('.disabled'):
+                    sku['out_of_stock'] = True
+
+                skus[f'{colour}_{sku["size"]}' if colour else sku["size"]] = sku
 
         else:
             common_sku['size'] = self.one_size
-            skus[f'{common_sku["colour"]}_{self.one_size}'] = common_sku
+            skus[f'{colour}_{self.one_size}' if colour else self.one_size] = common_sku
 
         return skus
 
 
 class VagabondCrawlSpider(BaseCrawlSpider):
-
     listings_css = ['.header__main--nav .main li']
     products_css = ['#ProductList a']
 
