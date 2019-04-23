e1391d3147b6e2cdc409d20a278f16f6f7699d1c
Aiman.Sajid <aiman@unassigned.arbisoft.com>
2019-04-23 16:37:48 +0500

    Addition of methods , list comprehension & f string

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -20,7 +20,7 @@ class FetchResponse:
 
     def parse_urls(self, raw_response_text):
         response = html.fromstring(raw_response_text)
-        return response.xpath('//a[contains(@href, "/")]/@href')[0]
+        return response.xpath('//a[contains(@href, "/")]/@href')
 
     def fetch_absolute_urls(self, relative_urls):
         return [urljoin(self.start_url, url) for url in relative_urls]
@@ -37,30 +37,35 @@ class Scheduler:
         self.max_urls_limit = max_urls_limit
         self.extracted_urls = Manager().list([start_url])
         self.visited_urls = Manager().list()
-        self.total_bytes_downloaded = Manager().Value('value', 0)
-        self.total_pages_crawled = Manager().Value('value', 0)
-        self.total_errors = Manager().Value('value', 0)
+        self.bytes_downloaded = Manager().Value('value', 0)
+        self.pages_crawled = Manager().Value('value', 0)
+        self.errors = Manager().Value('value', 0)
 
     def crawl_parallel(self):
         while len(self.visited_urls) < self.max_urls_limit:
             pool = Pool(self.threads)
-            pool.apply(self.get_next_urls)
+            pool.apply(self.get_url_response)
             pool.terminate()
             pool.join()
 
-        return self.total_bytes_downloaded.value, self.total_pages_crawled.value, self.total_errors.value
+        return (self.bytes_downloaded.value,
+                self.pages_crawled.value,
+                self.errors.value)
 
-    def get_next_urls(self):
+    def get_url_response(self):
         url = self.extracted_urls.pop()
         self.visited_urls.append(url)
 
         fetch = FetchResponse(self.start_url)
         raw_response_status_code, raw_response_text = fetch.parse_url_response(url)
-        time.sleep(self.delay)
 
+        time.sleep(self.delay)
+        self.fetch_next_urls(fetch, raw_response_status_code, raw_response_text)
+
+    def fetch_next_urls(self, fetch, raw_response_status_code, raw_response_text):
         if raw_response_status_code == 200:
-            self.total_bytes_downloaded.value += len(raw_response_text)
-            self.total_pages_crawled.value += 1
+            self.bytes_downloaded.value += len(raw_response_text)
+            self.pages_crawled.value += 1
 
             relative_urls = fetch.parse_urls(raw_response_text)
             absolute_urls = fetch.fetch_absolute_urls(relative_urls)
@@ -72,59 +77,57 @@ class Scheduler:
             self.add_next_urls(domain_urls)
 
         else:
-            self.total_errors.value += 1
+            self.errors.value += 1
 
     def add_next_urls(self, domain_urls):
-        for url in domain_urls:
-            if url not in self.extracted_urls:
-                self.extracted_urls.append(url)
+        [self.extracted_urls.append(url) for url in domain_urls if url not in self.extracted_urls]
 
 
 class CrawlerReport:
-    def __init__(self, total_bytes_downloaded, total_pages_crawled, total_errors):
-        self.total_bytes_downloaded = total_bytes_downloaded
-        self.total_pages_crawled = total_pages_crawled
-        self.total_errors = total_errors
+    def __init__(self, bytes_downloaded, pages_crawled, errors):
+        self.bytes_downloaded = bytes_downloaded
+        self.pages_crawled = pages_crawled
+        self.errors = errors
 
     def print_crawler_report(self, start_time):
-        total_crawl_time = time.time() - start_time
-        print("Total bytes downloaded: {}".format(self.total_bytes_downloaded))
-        print("Total pages crawled: {}".format(self.total_pages_crawled))
-        print("Average page size: {}".format(self.total_bytes_downloaded / self.total_pages_crawled))
-        print("Total crawel time: {}".format(total_crawl_time))
-        print("Total Errors: {}".format(self.total_errors))
+        print(f"Total bytes downloaded: {self.bytes_downloaded}")
+        print(f"Total pages crawled: {self.pages_crawled}")
+        print(f"Average page size: {self.bytes_downloaded / self.pages_crawled}")
+        print(f"Total crawel time: {time.time() - start_time}")
+        print(f"Total Errors: {self.errors}")
 
 
 def main():
-    try:
-        args = parse_configuration()
-        start_time = time.time()
+    args = parse_arguments()
+    start_time = time.time()
 
-        scheduler = Scheduler(args.url, args.delay, args.threads, args.limit)
-        scheduler.get_next_urls()
-        total_bytes_downloaded, total_pages_crawled, total_errors = scheduler.crawl_parallel()
+    scheduler = Scheduler(args.url, args.delay, args.threads, args.limit)
+    scheduler.get_url_response()
+    total_bytes_downloaded, total_pages_crawled, total_errors = scheduler.crawl_parallel()
 
-        report = CrawlerReport(total_bytes_downloaded, total_pages_crawled, total_errors)
-        report.print_crawler_report(start_time)
-
-    except argparse.ArgumentTypeError:
-        args.print_help()
+    report = CrawlerReport(total_bytes_downloaded, total_pages_crawled, total_errors)
+    report.print_crawler_report(start_time)
 
 
 def validate_url(url):
     if urlparse(url).netloc and urlparse(url).scheme:
         return url
+    raise argparse.ArgumentTypeError('{0} is not a valid url'.format(url))
 
 
-def parse_configuration():
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('-u', '--url', type=validate_url,
-                            help='Enter site url --Input format: https://website.com/ ')
-    arg_parser.add_argument('-d', '--delay', type=int, help='Enter delay time ')
-    arg_parser.add_argument('-t', '--threads', type=int, help='Enter concurrent call requests')
-    arg_parser.add_argument('-l', '--limit', type=int, help='Total urls to visit limit')
+def parse_arguments():
+    try:
+        arg_parser = argparse.ArgumentParser()
+        arg_parser.add_argument('-u', '--url', type=validate_url,
+                                help='Enter site url --Input format: https://website.com/ ')
+        arg_parser.add_argument('-d', '--delay', type=int, help='Enter delay time ')
+        arg_parser.add_argument('-t', '--threads', type=int, help='Enter concurrent call requests')
+        arg_parser.add_argument('-l', '--limit', type=int, help='Total urls to visit limit')
 
-    return arg_parser.parse_args()
+        return arg_parser.parse_args()
+
+    except argparse.ArgumentTypeError:
+        arg_parser.print_help()
 
 
 if __name__ == '__main__':
