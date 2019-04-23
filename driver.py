c428165dc45d8fcb5d0cf6cea97087d105db0c04
Syed.Adnan <adnan@A002-00105.arbisoft.com>
2019-04-23 12:14:09 +0500

    life-of-game

---
+++ driver.py
@@ -0,0 +1,20 @@
+import argparse
+from life_game import LifeGame
+
+def main():
+	""" main function """
+	parser = argparse.ArgumentParser()
+	parser.add_argument("file_name", help="enter file name")
+	parser.add_argument("iterations", help="enter iterations number")
+	args = parser.parse_args()
+	
+	if args.file_name:
+		obj = LifeGame(args.file_name)
+		for counter in range(int(args.iterations)):		
+			obj.display_new_generation()
+			obj.change_transition()
+			print()
+
+
+if __name__ == "__main__":
+	main()
