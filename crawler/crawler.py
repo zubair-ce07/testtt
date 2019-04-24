57eedabad12728e42d18bce8e54a65f2897d63b5
Aiman.Sajid <aiman@unassigned.arbisoft.com>
2019-04-24 12:41:49 +0500

    CrawlStats logic change

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -7,10 +7,8 @@ import logging
 import requests
 from lxml import html
 
-logging.basicConfig(level=logging.DEBUG, format='%(relativeCreated)6d %(threadName)s %(message)s')
 
-
-class FetchResponse:
+class HtmlParser:
     def __init__(self, start_url):
         self.start_url = start_url
 
@@ -25,7 +23,7 @@ class FetchResponse:
     def fetch_absolute_urls(self, relative_urls):
         return [urljoin(self.start_url, url) for url in relative_urls]
 
-    def filter_urls(self, absolute_urls):
+    def filter_domain_urls(self, absolute_urls):
         return [url for url in absolute_urls if self.start_url in url]
 
 
@@ -37,9 +35,7 @@ class Scheduler:
         self.max_urls_limit = max_urls_limit
         self.extracted_urls = Manager().list([start_url])
         self.visited_urls = Manager().list()
-        self.bytes_downloaded = Manager().Value('value', 0)
-        self.pages_crawled = Manager().Value('value', 0)
-        self.errors = Manager().Value('value', 0)
+        self.crawler_stats = CrawlStats()
 
     def crawl_parallel(self):
         while len(self.visited_urls) < self.max_urls_limit:
@@ -48,15 +44,13 @@ class Scheduler:
             pool.terminate()
             pool.join()
 
-        return (self.bytes_downloaded.value,
-                self.pages_crawled.value,
-                self.errors.value)
+        return self.crawler_stats
 
     def get_url_response(self):
         url = self.extracted_urls.pop()
         self.visited_urls.append(url)
 
-        fetch = FetchResponse(self.start_url)
+        fetch = HtmlParser(self.start_url)
         raw_response_status_code, raw_response_text = fetch.parse_url_response(url)
 
         time.sleep(self.delay)
@@ -64,12 +58,12 @@ class Scheduler:
 
     def fetch_next_urls(self, fetch, raw_response_status_code, raw_response_text):
         if raw_response_status_code == 200:
-            self.bytes_downloaded.value += len(raw_response_text)
-            self.pages_crawled.value += 1
+            self.crawler_stats.bytes_downloaded.value += len(raw_response_text)
+            self.crawler_stats.pages_crawled.value += 1
 
             relative_urls = fetch.parse_urls(raw_response_text)
             absolute_urls = fetch.fetch_absolute_urls(relative_urls)
-            domain_urls = fetch.filter_urls(absolute_urls)
+            domain_urls = fetch.filter_domain_urls(absolute_urls)
 
             domain_urls = set(domain_urls)
             domain_urls = domain_urls - set(self.visited_urls)
@@ -77,36 +71,36 @@ class Scheduler:
             self.add_next_urls(domain_urls)
 
         else:
-            self.errors.value += 1
+            self.crawler_stats.errors += 1
 
     def add_next_urls(self, domain_urls):
         [self.extracted_urls.append(url) for url in domain_urls if url not in self.extracted_urls]
 
 
-class CrawlerReport:
-    def __init__(self, bytes_downloaded, pages_crawled, errors):
-        self.bytes_downloaded = bytes_downloaded
-        self.pages_crawled = pages_crawled
-        self.errors = errors
+class CrawlStats:
+    def __init__(self):
+        self.bytes_downloaded = Manager().Value('value', 0)
+        self.pages_crawled = Manager().Value('value', 0)
+        self.errors = Manager().Value('value', 0)
 
     def print_crawler_report(self, start_time):
-        print(f"Total bytes downloaded: {self.bytes_downloaded}")
-        print(f"Total pages crawled: {self.pages_crawled}")
-        print(f"Average page size: {self.bytes_downloaded / self.pages_crawled}")
+        print(f"Total bytes downloaded: {self.bytes_downloaded.value}")
+        print(f"Total pages crawled: {self.pages_crawled.value}")
+        print(f"Average page size: {self.bytes_downloaded.value / self.pages_crawled.value}")
         print(f"Total crawel time: {time.time() - start_time}")
-        print(f"Total Errors: {self.errors}")
+        print(f"Total Errors: {self.errors.value}")
 
 
 def main():
+    logging.basicConfig(level=logging.DEBUG, format='%(relativeCreated)6d %(threadName)s %(message)s')
+
     args = parse_arguments()
     start_time = time.time()
 
     scheduler = Scheduler(args.url, args.delay, args.threads, args.limit)
     scheduler.get_url_response()
-    total_bytes_downloaded, total_pages_crawled, total_errors = scheduler.crawl_parallel()
-
-    report = CrawlerReport(total_bytes_downloaded, total_pages_crawled, total_errors)
-    report.print_crawler_report(start_time)
+    crawl_stats = scheduler.crawl_parallel()
+    crawl_stats.print_crawler_report(start_time)
 
 
 def validate_url(url):
