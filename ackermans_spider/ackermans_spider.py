42a8e7dc1a078fab0265013d7fc7da67977a735a
tanveer mahmood <tanveerpucit111@gmail.com>
2019-04-26 16:19:05 +0500

    create ackerermans_spider.py

---
+++ ackermans_spider/ackermans_spider.py
@@ -0,0 +1,222 @@
+import json
+from urllib.parse import unquote
+
+from scrapy.spiders import Request
+from w3lib.url import add_or_replace_parameter
+
+from .base import BaseParseSpider, BaseCrawlSpider, clean, soupify, Gender
+
+
+class Mixin:
+    retailer = 'ackermans'
+    default_brand = 'Ackermans'
+    allowed_domains = ['ackermans.co.za']
+
+
+class MixinZA(Mixin):
+    retailer = Mixin.retailer + '-za'
+    market = 'ZA'
+    currency = 'ZAR'
+    start_urls = ['https://www.ackermans.co.za/']
+
+    unwanted_items = ['toys', 'walkers']
+    homeware_categories = ['homeware', 'furniture', 'decor', 'home', 'bed', 'bath']
+
+    headers = {
+        'Origin': 'https://www.ackermans.co.za',
+        'Accept': 'application/json, text/plain, */*',
+        'Authorization': 'Bearer x3leg25sl1vvruoa6vr861vgus503cfq',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 '
+                      '(KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
+    }
+
+    product_url_t = 'https://www.ackermans.co.za/product/{}'
+    image_url_t = 'https://cdn.ackermans.co.za/product-images/prod/1100_1100_{}.jpg'
+    categories_url = 'https://magento.ackermans.co.za/rest/default/V1/pepkor/categoryapi/categories'
+
+    category_url_t = 'https://magento.ackermans.co.za/rest/default/V1/pepkor/searchV2?search_criteria%5B' \
+                     'current_page%5D=1&search_criteria%5Bfilter_groups%5D%5B2%5D%5Bfilters%5D%5B0%5D%5B' \
+                     'field%5D=category_ids&search_criteria%5Bfilter_groups%5D%5B2%5D%5Bfilters%5D%5B0%5D' \
+                     '%5Bvalue%5D={}&search_criteria%5Bpage_size%5D=20'
+
+    api_url_t = 'https://magento.ackermans.co.za/rest/default/V1/pepkor/searchV2?' \
+                'search_criteria%5Bfilter_groups%5D%5B0%5D%5Bfilters%5D%5B0%5D%5Bfield%5D=sku&' \
+                'search_criteria%5Bfilter_groups%5D%5B0%5D%5Bfilters%5D%5B0%5D%5Bvalue%5D={}&' \
+                'search_criteria%5Bpage_size%5D=1'
+
+    size_and_colour_url = 'https://magento.ackermans.co.za/rest/default/V1/products/attributes?' \
+                          'search_criteria%5Bfilter_groups%5D%5B0%5D%5Bfilters%5D%5B0%5D%5Bfield%5D' \
+                          '=is_filterable_in_grid&search_criteria%5Bfilter_groups%5D%5B0%5D%5B' \
+                          'filters%5D%5B0%5D%5Bvalue%5D=1'
+
+
+class AckermansParseSpider(BaseParseSpider):
+    sizes_map = []
+    colours_map = []
+
+    def parse(self, response):
+        raw_product = self.raw_product(response)
+        garment = self.new_unique_garment(self.product_id(raw_product))
+
+        if not garment:
+            return
+
+        self.boilerplate_minimal(garment, response, self.product_url(raw_product))
+
+        garment['url_original'] = response.url
+        garment['care'] = self.product_care(raw_product)
+        garment['name'] = self.product_name(raw_product)
+        garment['brand'] = self.product_brand(raw_product)
+        garment["gender"] = self.product_gender(raw_product)
+        garment['category'] = self.product_category(raw_product)
+        garment['image_urls'] = self.product_image_urls(raw_product)
+        garment['description'] = self.product_description(raw_product)
+        garment['skus'] = self.skus(response, raw_product)
+
+        if self.is_homeware(response):
+            garment['industry'] = 'homeware'
+            garment["gender"] = None
+
+        return garment
+
+    def product_id(self, raw_product):
+        return raw_product['sku']
+
+    def product_name(self, raw_product):
+        return raw_product['name']
+
+    def raw_product(self, response):
+        return json.loads(response.text)['product']['docs'][0]
+
+    def brand_soup(self, raw_product):
+        return clean(raw_product['custom_attributes'].get('meta_title', ''))
+
+    def product_description(self, raw_product):
+        return [raw_product['custom_attributes'].get('meta_description', '')]
+
+    def product_url(self, raw_product):
+        return self.product_url_t.format(raw_product['url_key']+'/'+raw_product['sku'])
+
+    def raw_description(self, raw_product):
+        return raw_product['custom_attributes'].get('meta_description', '').split('\.\s')
+
+    def product_category(self, raw_product):
+        return clean(raw_product['custom_attributes'].get('meta_title', '')).split('|')[1:]
+
+    def product_gender(self, raw_product):
+        soup = soupify([self.product_name(raw_product)] + self.product_category(raw_product))
+        return self.gender_lookup(soup) or Gender.ADULTS.value
+
+    def skus(self, response, raw_product):
+        skus = {}
+        money_strs = [raw_product['prices']['minimal_price'], raw_product['prices']['price'], self.currency]
+        common_sku = self.product_pricing_common(response, money_strs=money_strs)
+        sizes, colours = self.sizes_and_colours(raw_product)
+
+        if not (colours and sizes):
+            common_sku['size'] = self.one_size
+            skus[self.one_size] = common_sku
+            return skus
+
+        for colour in colours:
+            for size in sizes:
+                sku = common_sku.copy()
+                sku['colour'] = colour
+                sku['size'] = size
+
+                if not raw_product['extension_attributes']['stock_item'].get('is_in_stock'):
+                    sku['out_of_stock'] = True
+
+                skus[f'{colour}_{size}'] = sku
+
+        return skus
+
+    def sizes_and_colours(self, raw_product):
+        colours = [self.colours_map[str(colour_code)] for colour_code in raw_product['color_ids']]
+        sizes = [self.sizes_map[str(size_code)] for size_code in raw_product['size_ids']]
+        return sizes, colours
+
+    def product_image_urls(self, raw_product):
+        if raw_product['custom_attributes'].get('image_name'):
+            return [self.image_url_t.format(raw_product['custom_attributes']['image_name'])]
+        return []
+
+    def is_homeware(self, response):
+        return any(homeware in response.meta['category_name'] for homeware in self.homeware_categories)
+
+
+class AckermansCrawlSpider(BaseCrawlSpider):
+    def start_requests(self):
+        yield Request(url=self.categories_url, headers=self.headers, callback=self.parse_categories)
+
+    def parse_categories(self, response):
+        response.meta['trail'] = self.add_trail(response)
+        yield Request(url=self.size_and_colour_url, headers=self.headers, callback=self.parse_size_and_colour)
+
+        for url in self.category_ids(response):
+            response.meta['category_name'] = url['category_name'].lower()
+
+            if any(unwanted in response.meta['category_name'] for unwanted in self.unwanted_items):
+                continue
+
+            yield Request(url=self.category_url_t.format(url['category_id']), meta=response.meta.copy(),
+                          headers=self.headers, callback=self.parse_pagination)
+
+    def parse_pagination(self, response):
+        page_size = 20
+        products = json.loads(response.text)['product']['data']['total_count']
+        response.meta['trail'] = self.add_trail(response)
+        pages = int(products)//page_size+2
+
+        for page in range(1, pages):
+            next_url = add_or_replace_parameter(unquote(response.url), 'search_criteria[current_page]', page)
+            yield Request(url=next_url, headers=self.headers, meta=response.meta.copy(), callback=self.parse_listings)
+
+    def parse_listings(self, response):
+        product_ids = [i['sku'] for i in json.loads(response.text)['product']['docs']]
+        response.meta['trail'] = self.add_trail(response)
+
+        for product_id in product_ids:
+            yield Request(url=self.api_url_t.format(product_id), meta=response.meta.copy(),
+                          headers=self.headers, callback=self.parse_item)
+
+    def category_ids(self, response):
+        requests_ids = []
+        raw_categories = json.loads(response.text)
+        self.get_category_ids(raw_categories['children_data'], requests_ids)
+        return requests_ids
+
+    def get_category_ids(self, children_categories, requests_ids, category_trail=None):
+
+        for child_category in children_categories:
+
+            if category_trail:
+                categories = category_trail
+
+            if child_category['level'] == 2:
+                category_trail = child_category['url_key']
+
+            if child_category['level'] >= 3:
+                categories = categories + ' ' + child_category['url_key']
+                requests_ids.append({'category_name': categories, 'category_id': child_category['id']})
+
+            self.get_category_ids(child_category['children_data'], requests_ids, category_trail)
+
+    def parse_size_and_colour(self, response):
+        raw_colours_sizes_maps = json.loads(response.text)['items']
+
+        for map in raw_colours_sizes_maps:
+            if map['attribute_code'] == 'color':
+                self.parse_spider.colours_map = {i['value']: i['label'] for i in map['options']}
+
+            if map['attribute_code'] == 'size':
+                self.parse_spider.sizes_map = {i['value']: i['label'] for i in map['options']}
+
+
+class AckermansZAParseSpider(MixinZA, AckermansParseSpider):
+    name = MixinZA.retailer + '-parse'
+
+
+class AckermansZACrawlSpider(MixinZA, AckermansCrawlSpider):
+    name = MixinZA.retailer + '-crawl'
+    parse_spider = AckermansZAParseSpider()
