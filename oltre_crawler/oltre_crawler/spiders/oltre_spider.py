feae9de1061cafcdb774d3eb7ddb3375026ea719
Aiman.Sajid <aiman@unassigned.arbisoft.com>
2019-05-02 19:07:11 +0500

    Inherited ParserSpider from scrapy.Spider and refactoring of code base

--- oltre_crawler/oltre_crawler/spiders/oltre_spider.py
+++ oltre_crawler/oltre_crawler/spiders/oltre_spider.py
@@ -1,28 +1,23 @@
 import datetime
-from urllib.parse import urljoin
 
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Rule, CrawlSpider
+import scrapy
 
 from oltre_crawler.items import OltreCrawlerItem
 
 
-class Parser:
+class ParseSpider(scrapy.Spider):
     start_url = 'https://www.oltre.com/it/'
+    name = "parse_spider"
 
-    def parse_item(self, response):
-        product = self.parse_info(response)
-        product['requests'] = self.color_requests(response)
-
-        return self.next_request_or_product(product)
-
-    def parse_info(self, response):
+    def parse(self, response):
         product = OltreCrawlerItem()
         product['retailer_sku'] = self.retailer_sku(response)
         product['market'] = 'it'
-        product['retailer'] = 'oltre'
+        product['retailer'] = 'oltre-it'
         product['gender'] = 'Women'
-        product['brand'] = 'oltre-it'
+        product['brand'] = 'oltre'
         product['name'] = self.product_name(response)
         product['category'] = self.product_category(response)
         product['date'] = str(datetime.datetime.now())
@@ -32,12 +27,14 @@ class Parser:
         product['image_urls'] = self.product_image_urls(response)
         product['spider_name'] = 'oltre_spider'
         product['skus'] = self.skus(response)
+        product['requests'] = self.color_requests(response)
 
-        return product
+        return self.next_request_or_product(product)
 
     def color_requests(self, response):
-        color_urls = response.xpath('(//ul[@class="swatches color"])[1]/li/a/@href').getall()
-        return [response.follow(color, callback=self.parse_skus) for color in color_urls[1:]]
+        color_urls = response.xpath(
+            '(//ul[@class="swatches color"])[1]/li[not(contains(@class,"selectable selected"))]/a/@href').getall()
+        return [response.follow(color, callback=self.parse_skus) for color in color_urls]
 
     def parse_skus(self, response):
         product = response.meta['product']
@@ -46,14 +43,13 @@ class Parser:
         return self.next_request_or_product(product)
 
     def next_request_or_product(self, product):
-        if product['requests']:
-            request = product['requests'].pop()
-            request.meta['product'] = product
-            return request
-        else:
+        if not product['requests']:
             del product['requests']
+            return product
 
-        return product
+        request = product['requests'].pop()
+        request.meta['product'] = product
+        return request
 
     def product_name(self, response):
         return response.css('.product-name::text').get()
@@ -62,11 +58,12 @@ class Parser:
         return response.css('#pid::attr(value)').get()
 
     def product_category(self, response):
-        return response.css('.breadcrumb a::text,.breadcrumb span::text').getall()
+        return response.css('.breadcrumb-element ::text').getall()
 
     def product_description(self, response):
-        description = response.css('#description > div::text').getall()
-        return description[1].strip()
+        raw_description = response.xpath('//div[@id="description"]/div/text()').getall()
+        description = [description.strip() for description in raw_description]
+        return list(filter(None, description))
 
     def product_care(self, response):
         raw_care = response.css('#wash > div::text').getall()
@@ -74,17 +71,17 @@ class Parser:
         return list(filter(None, care))
 
     def product_image_urls(self, response):
-        image_urls = response.css('.product-thumbnails a::attr(href)').getall()
-        return [urljoin(self.start_url, image) for image in image_urls]
+        image_urls = response.css('.product-thumbnails ::attr(href)').getall()
+        return [response.urljoin(url) for url in image_urls]
 
     def product_price_detail(self, response):
         product_price = {
             'price': response.css('.price-sales.gtm-sale::text').get().strip().split('€')[1],
             'currency': 'EUR',
         }
-        previous_price = response.css('.price-standard::text').get()
+        previous_price = response.xpath('(//span[@class="price-standard"])[1]/text()').getall()
         if previous_price:
-            product_price['previous_price'] = previous_price.strip().split('€')[1]
+            product_price['previous_price'] = list(map(str.strip, previous_price))
 
         return product_price
 
@@ -94,29 +91,34 @@ class Parser:
 
     def skus(self, response):
         product_detail = []
+        common_sku = {}
         retailer_sku = self.retailer_sku(response)
         color = response.css('.selected-value::text,.title-variation-color::text ').get()
 
         for size in self.size_options(response):
-            product = self.product_price_detail(response)
-            product['size'] = size
-            product['sku_id'] = retailer_sku + size
+            common_sku.update(self.product_price_detail(response))
+            common_sku['size'] = size
+            common_sku['sku_id'] = retailer_sku + size
             if color:
-                product['color'] = color.strip()
+                common_sku['color'] = color.strip()
 
-            product_detail.append(product)
+            product_detail.append(common_sku)
         return product_detail
 
 
 class OltreSpider(CrawlSpider):
     name = 'oltre_spider'
-    start_urls = ['https://www.oltre.com/it/']
-    parser = Parser()
+    start_urls = ['https://www.oltre.com/it']
 
-    category_css = '.main-menu'
-    product_css = '.product-name'
+    allowed_domains = ['oltre.com']
+    parser = ParseSpider()
 
+    category_css = ['.main-menu']
+    product_css = ['.product-name',
+                   '.page-next'
+                   ]
     rules = (
         Rule(LinkExtractor(restrict_css=category_css)),
-        Rule(LinkExtractor(restrict_css=product_css), callback=parser.parse_item)
+        Rule(LinkExtractor(restrict_css=product_css), callback=parser.parse)
     )
+
