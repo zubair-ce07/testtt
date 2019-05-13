e17c614e9cfb9ec8dd7daa191a80d388653d47e3
Aiman.Sajid <aiman@unassigned.arbisoft.com>
2019-05-13 15:15:38 +0500

    Use Link Extractors for pagination, regex for variants and code base refactoring

--- cabourn_crawler/cabourn_spider.py
+++ cabourn_crawler/cabourn_spider.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from scrapy.spiders import Rule
 
@@ -16,7 +17,8 @@ class Mixin:
 
 class CabournParseSpider(Mixin, BaseParseSpider):
     name = Mixin.retailer + '-parse'
-    price_css = '.regular-price .price::text , .special-price .price::text'
+    price_css = '.regular-price ::text , .special-price ::text'
+    raw_description_css = '.pp-description ::text'
 
     def parse(self, response):
         sku_id = self.product_id(response)
@@ -33,36 +35,28 @@ class CabournParseSpider(Mixin, BaseParseSpider):
         return garment
 
     def product_id(self, response):
-        return clean(response.css('.no-display input::attr(value)'))[0]
+        return clean(response.css('.no-display ::attr(value)'))[0]
 
     def product_name(self, response):
-        return clean(response.css('.product-name h1::text'))[0]
+        return clean(response.css('.product-name ::text'))[0]
 
     def image_urls(self, response):
-        css = '.MagicToolboxSelectorsContainer a::attr(href),.MagicToolboxContainer a::attr(href)'
-        return response.css(css).getall()
+        css = '.MagicToolboxSelectorsContainer ::attr(href),.MagicToolboxContainer ::attr(href)'
+        return clean(response.css(css))
 
-    def raw_description(self, response):
-        return clean(response.css('.pp-description p::text'))
-
-    def product_description(self, response):
-        desc = clean(response.css('.description::text'))
-        return desc + [rd for rd in self.raw_description(response) if not self.care_criteria(rd)]
-
-    def product_care(self, response):
-        return [rd for rd in self.raw_description(response) if self.care_criteria(rd)]
+    def product_category(self, response):
+        trail = response.meta['trail'][1][1]
+        return trail.split('/')[3:]
 
     def product_gender(self, response):
         soup = ' '.join(self.product_category(response))
         return self.gender_lookup(soup) or Gender.ADULTS.value
 
-    def product_category(self, response):
-        return response.url.split('/')[4:]
-
     def variants(self, response):
-        script_text = response.xpath('//script[contains(., "Product.ConfigDefaultText")]/text()').getall()
-        raw_text = script_text[0].split('Product.ConfigDefaultText(')[1].split(')')[0]
-        raw_json = json.loads(raw_text)
+        variant_r = '({"attributes.*}})'
+        script_text = clean(response.xpath('//script[contains(., "Product.ConfigDefaultText")]/text()'))
+        raw_text = re.findall(variant_r, script_text[0])
+        raw_json = json.loads(raw_text[0])
 
         return raw_json
 
@@ -80,7 +74,6 @@ class CabournParseSpider(Mixin, BaseParseSpider):
             sku = common_sku.copy()
             size = variant['label']
             sku['size'] = size
-            sku['sku_id'] = retailer_sku + size
 
             skus[f'{retailer_sku}_{size}'] = sku
 
@@ -92,7 +85,8 @@ class CabournCrawlSpider(Mixin, BaseCrawlSpider):
     parse_spider = CabournParseSpider()
 
     listings_css = [
-        '.sub-menu'
+        '.sub-menu',
+        '.next.i-next'
     ]
     products_css = [
         '.product-name'
@@ -102,10 +96,3 @@ class CabournCrawlSpider(Mixin, BaseCrawlSpider):
         Rule(LinkExtractor(restrict_css=products_css), callback='parse_item')
     )
 
-    def parse(self, response):
-        yield from super().parse(response)
-
-        next_page_url = clean(response.css('.next.i-next ::attr(href)'))
-        if next_page_url:
-            response.meta['trail'] = self.add_trail(response)
-            yield response.follow(next_page_url[0], callback=self.parse)
