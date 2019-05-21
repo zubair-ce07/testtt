f56a3a076e486952cd712f5ff1e793cf34b9132b
Aiman.Sajid <aiman@unassigned.arbisoft.com>
2019-05-21 10:31:59 +0500

    Simplified regex for getting variants & removal of indexing.Getting currency from selector now and renaming of image url variable.

--- vionic_crawler/vionic_spider.py
+++ vionic_crawler/vionic_spider.py
@@ -21,6 +21,8 @@ class VionicParseSpider(Mixin, BaseParseSpider):
     care_x = '(//div[@class="product-accordion__content"])[2]//text()'
 
     def parse(self, response):
+        raw_variants = self.variants(response)
+
         sku_id = self.product_id(response)
         garment = self.new_unique_garment(sku_id)
 
@@ -30,9 +32,8 @@ class VionicParseSpider(Mixin, BaseParseSpider):
         self.boilerplate_normal(garment, response)
 
         garment['image_urls'] = self.image_urls(response)
-        raw_variants = self.variants(response)
         garment['gender'] = self.product_gender(raw_variants)
-        garment['skus'] = self.skus(raw_variants)
+        garment['skus'] = self.skus(response, raw_variants)
         return garment
 
     def product_id(self, response):
@@ -45,12 +46,12 @@ class VionicParseSpider(Mixin, BaseParseSpider):
         return clean([t[0] for t in response.meta.get('trail', [])])
 
     def image_urls(self, response):
-        images_s = clean(response.css('.product__slides__item a::attr(href)'))
-        return [response.urljoin(image_s) for image_s in images_s]
+        urls = clean(response.css('.product__slides__item a::attr(href)'))
+        return [response.urljoin(url) for url in urls]
 
     def variants(self, response):
-        xpath = '//script[contains(., "window.products")]/text()'
-        script = response.xpath(xpath)[1].re_first('window.products.*?=(.*)?;')
+        xpath = '//script[contains(., "window.products.")]/text()'
+        script = response.xpath(xpath).re_first('({.*})')
         raw_variants = json.loads(script)
         return raw_variants
 
@@ -58,13 +59,15 @@ class VionicParseSpider(Mixin, BaseParseSpider):
         soup = raw_variants['type']
         return self.gender_lookup(soup) or Gender.ADULTS.value
 
-    def skus(self, raw_variants):
+    def skus(self, response, raw_variants):
         skus = {}
         variants = raw_variants['variants']
 
         price = variants[0]['price']
         previous_price = variants[0]['compare_at_price']
-        common_sku = self.product_pricing_common(None, money_strs=[price, previous_price, 'CAD'])
+        currency = response.css('#in-context-paypal-metadata::attr(data-currency)').get()
+
+        common_sku = self.product_pricing_common(None, money_strs=[price, previous_price, currency])
 
         for variant in variants:
             sku = common_sku.copy()
@@ -96,3 +99,4 @@ class VionicCrawlSpider(Mixin, BaseCrawlSpider):
         Rule(LinkExtractor(restrict_css=listings_css, deny=listings_deny_r), callback='parse'),
         Rule(LinkExtractor(restrict_css=products_css), callback='parse_item')
     )
+
