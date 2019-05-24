beff311c0399e2d92e4df9eb041f83fd4701f879
Aiman.Sajid <aiman@unassigned.arbisoft.com>
2019-05-24 15:21:31 +0500

    Creation of separate methods for attributes.Addition of checks for beauty products & care_criteria in desc & care.Extraction of images for all colours and refactoring of code base.

--- nnnow_crawler/nnnow_spider.py
+++ nnnow_crawler/nnnow_spider.py
@@ -2,8 +2,9 @@ import json
 
 from urllib.parse import urljoin
 from scrapy.spiders import Request
+from w3lib.url import add_or_replace_parameters
 
-from .base import BaseParseSpider, BaseCrawlSpider, clean
+from .base import BaseParseSpider, BaseCrawlSpider, clean, Gender
 
 
 class Mixin:
@@ -28,7 +29,7 @@ class NnnowParseSpider(Mixin, BaseParseSpider):
     def parse(self, response):
         raw_variants = self.variants(response)['PdpData']['mainStyle']
 
-        sku_id = raw_variants['styleId']
+        sku_id = self.product_id(raw_variants)
         garment = self.new_unique_garment(sku_id)
 
         if not garment:
@@ -36,13 +37,13 @@ class NnnowParseSpider(Mixin, BaseParseSpider):
 
         self.boilerplate(garment, response)
 
-        garment['name'] = raw_variants['name']
+        garment['name'] = self.product_name(raw_variants)
         garment['description'] = self.product_description(raw_variants)
         garment['care'] = self.product_care(raw_variants)
-        garment['brand'] = raw_variants['brandName']
+        garment['brand'] = self.product_brand(raw_variants)
         garment['category'] = self.product_category(response)
         garment['image_urls'] = self.image_urls(raw_variants)
-        garment['gender'] = raw_variants['gender']
+        garment['gender'] = self.product_gender(raw_variants)
         garment['skus'] = self.skus(raw_variants, response)
 
         garment['meta'] = {'requests_queue': self.colour_requests(response)}
@@ -52,35 +53,55 @@ class NnnowParseSpider(Mixin, BaseParseSpider):
         garment = response.meta['garment']
         raw_variants = self.variants(response)['PdpData']['mainStyle']
         garment['skus'].update(self.skus(raw_variants, response))
+        garment['image_urls'] += self.image_urls(raw_variants)
 
         return self.next_request_or_garment(garment)
 
     def variants(self, response):
-        xpath = '//script[contains(., "window.DATA=")]/text()'
-        script = response.xpath(xpath).re_first('=(.*)')
+        css = 'script:contains("window.DATA=")::text'
+        script = response.css(css).re_first('=(.*)')
         return json.loads(script)['ProductStore']
+
+    def product_id(self, raw_variant):
+        return clean(raw_variant['styleId'])
+
+    def product_name(self, raw_variant):
+        return clean(raw_variant['name'])
+
+    def product_brand(self, raw_variant):
+        return clean(raw_variant['brandName'])
 
     def product_category(self, response):
         return clean(response.css('.nw-breadcrumb-listitem::text'))
 
     def product_description(self, raw_variant):
-        return [desc for desc in raw_variant['finerDetails']['specs']['list']]
+        description = raw_variant['finerDetails']['specs']['list'] if raw_variant['finerDetails']['specs'] else (
+            raw_variant['finerDetails']['whatItDoes']['list'])
+
+        return [desc for desc in clean(description) if not self.care_criteria(desc)]
 
     def product_care(self, raw_variant):
-        return [care for care in raw_variant['finerDetails']['compositionAndCare']['list']]
+        raw_care = raw_variant['finerDetails']['compositionAndCare']['list'] if raw_variant['finerDetails'][
+            'compositionAndCare'] else []
+
+        return [care for care in clean(raw_care) if self.care_criteria(care)]
+
+    def product_gender(self, raw_variant):
+        gender = clean(raw_variant['gender'])
+        return self.gender_lookup(gender) or Gender.ADULTS.value
 
     def image_urls(self, raw_variant):
         return [raw_image['large'] for raw_image in raw_variant['images']]
 
     def colour_requests(self, response):
-        xpath = '//div[@class="nw-color-chips"]/a[not(contains(@class,"nw-color-item  selected"))]/@href'
-        colour_urls = clean(response.xpath(xpath))
+        css = '.nw-color-chips a:not(.selected)::attr(href)'
+        colour_urls = clean(response.css(css))
 
         return [Request(urljoin(self.site_url, url), callback=self.parse_colour) for url in colour_urls]
 
     def skus(self, raw_variant, response):
         price = raw_variant['skus'][0]['price']
-        previous_price = raw_variant['skus'][0]['mrp']
+        previous_price = raw_variant['skus'][0].get('mrp')
         currency = clean(response.css('[itemprop="priceCurrency"]::text'))[0]
 
         common_sku = self.product_pricing_common(None, money_strs=[price, previous_price, currency])
@@ -93,7 +114,7 @@ class NnnowParseSpider(Mixin, BaseParseSpider):
             sku['size'] = variant['size']
 
             if not variant['inStock']:
-                sku['out-of-stock'] = True
+                sku['out_of_stock'] = True
 
             skus[variant['skuId']] = sku
 
@@ -122,37 +143,34 @@ class NnnowCrawlSpider(Mixin, BaseCrawlSpider):
             'deeplinkurl': f"/{category_name}?p=1&cid={category_id}",
         }
         trail = self.add_trail(response)
-        response.meta.update({'trail': trail, 'category_name': category_name, 'category_id': category_id})
 
         yield Request(self.product_listing_url, method='POST', body=json.dumps(params),
-                      callback=self.parse_product_pagination,
-                      headers=self.headers, meta=response.meta)
+                      callback=self.parse_pagination, headers=self.headers,
+                      meta={'trail': trail, 'params': params})
 
-    def parse_product_pagination(self, response):
+    def parse_pagination(self, response):
         if response.meta.get('pagination'):
             return
 
         trail = self.add_trail(response)
-
-        category_name = response.meta.get('category_name')
-        category_id = response.meta.get('category_id')
 
         raw_listing = json.loads(response.text)['data']
         raw_product = raw_listing['styles']['styleList']
 
         total_page_count = raw_listing['totalPages']
 
-        for page in range(2, total_page_count, 1):
-            params = {
-                'deeplinkurl': f"/{category_name}?p={page}&cid={category_id}",
-            }
+        for page in range(2, total_page_count + 1):
+            params = response.meta.get('params')
+            params['deeplinkurl'] = add_or_replace_parameters(params['deeplinkurl'], {'p': page})
+
             yield Request(self.product_listing_url, method='POST', body=json.dumps(params),
-                          callback=self.parse_product_pagination, headers=self.headers,
+                          callback=self.parse_pagination, headers=self.headers,
                           meta={'trail': trail, 'pagination': True})
 
-        yield from self.parse_product_request(raw_product, trail)
+        yield from self.product_request(raw_product, trail)
 
-    def parse_product_request(self, raw_product, trail):
+    def product_request(self, raw_product, trail):
         for product in raw_product:
             url = urljoin(self.site_url, product['url'])
             yield Request(url, callback=self.parse_item, meta={'trail': trail})
+
