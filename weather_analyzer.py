41fba12d8c000ce7ce7528ad494f247e7a108913
Ikram ullah Khan <ikram@A002-00048.arbisoft.com>
2019-06-12 09:35:03 +0500

    initial commit

---
+++ weather_analyzer.py
@@ -0,0 +1,108 @@
+import os
+import re
+import csv
+
+from day_weather import DayWeather
+
+
+class WeatherAnalyzer:
+    def __init__(self):
+        self.day_weather_list = []
+
+    def collect_files(self, files_path):
+        """collects all datasets files"""
+        list_of_files = []
+        for file_name in os.listdir(files_path):
+            if not file_name.startswith('.'):
+                list_of_files.append(files_path + file_name)
+        return list_of_files
+
+    def read_file_data(self, file_path):
+        """Reads and return file data"""
+        try:
+            file_data = []
+            with open(file_path) as csv_file:
+                csv_reader = csv.reader(csv_file, delimiter=',')
+                for row in csv_reader:
+                    file_data.append(row)
+            return file_data
+        except IOError:
+            print("Error: can\'t find file or read data")
+            exit()
+
+    def read_files(self, files_path):
+        list_of_files = self.collect_files(files_path)
+        for file in list_of_files:
+            file_data = self.read_file_data(file)
+            for day_data in file_data:
+                if len(day_data) > 2:
+                    if day_data[0] != "PKT" and day_data[0] != "PKST" and day_data[1] and day_data[3] \
+                            and day_data[8]:
+                        self.day_weather_list.append(DayWeather(
+                            day_data[0], day_data[1],
+                            day_data[2], day_data[3],
+                            day_data[4], day_data[5],
+                            day_data[6], day_data[7],
+                            day_data[8], day_data[9],
+                            day_data[10], day_data[11],
+                            day_data[12], day_data[13],
+                            day_data[14], day_data[15],
+                            day_data[16], day_data[17],
+                            day_data[18], day_data[19],
+                            day_data[20], day_data[21],
+                            day_data[22]
+                        ))
+
+    def collect_month_data(self, year_month):
+        month_data_list = []
+        for day_data in self.day_weather_list:
+            if self.check_valid_year_month_file(day_data.pkt, year_month):
+                month_data_list.append(day_data)
+        return month_data_list
+
+    def extract_year_data(self, year):
+        year_data_list = []
+        for day_data in self.day_weather_list:
+            if self.check_valid_year_file(day_data.pkt, year):
+                year_data_list.append(day_data)
+        temp_max_obj = max(year_data_list,
+                           key=lambda day_data: int(day_data.max_temperature))
+        temp_min_obj = min(year_data_list,
+                           key=lambda day_data: int(day_data.min_temperature))
+        max_humid_obj = min(year_data_list,
+                            key=lambda day_data: int(day_data.max_humidity))
+        return temp_max_obj, temp_min_obj, max_humid_obj
+
+    def check_valid_year_file(self, day_date, year):
+        match = re.search(r'\d{4}', day_date)
+        return match and (year in day_date)
+
+    def compute_month_data_average(self, month_data_list):
+        max_temp_avg = 0
+        min_temp_avg = 0
+        humidity_avg = 0
+        count_max_temp = 0
+        count_min_temp = 0
+        count_humidty = 0
+        for day_data in month_data_list:
+            if day_data.max_temperature:
+                max_temp_avg += int(day_data.max_temperature)
+                count_max_temp += 1
+            if day_data.min_temperature:
+                min_temp_avg += int(day_data.min_temperature)
+                count_min_temp += 1
+            if day_data.max_humidity:
+                humidity_avg += int(day_data.max_humidity)
+                count_humidty += 1
+        return (max_temp_avg / count_max_temp,
+                min_temp_avg / count_min_temp,
+                humidity_avg / count_humidty)
+
+    def check_valid_year_month_file(self, day_date, year_month):
+        match = re.search(r'\d{4}', day_date)
+        if match:
+            day_date_list = day_date.split("-")
+            year_month_list = year_month.split("/")
+            return day_date_list[0] == year_month_list[0] and day_date_list[
+                1] == year_month_list[1]
+        return False
---
+++ weather_reporter.py
@@ -0,0 +1,121 @@
+import calendar
+
+from color_codes import ColorCode
+from weather_analyzer import WeatherAnalyzer
+
+
+class WeatherReporter:
+
+    def __init__(self):
+        self.weather_analyzer = WeatherAnalyzer()
+
+    def generate_year_report(self, report_year, dir_path):
+        self.weather_analyzer.read_files(dir_path)
+        temp_max_obj, temp_min_obj, \
+            max_humid_obj = self.weather_analyzer.extract_year_data(
+                report_year)
+        self.print_year_temp_report(temp_max_obj, temp_min_obj, max_humid_obj)
+
+    def generate_month_report(self, report_year, dir_path):
+        self.weather_analyzer.read_files(dir_path)
+        month_data_list = self.weather_analyzer.collect_month_data(
+            report_year)
+        max_temp_avg, min_temp_avg, \
+            humidity_avg = self.weather_analyzer.compute_month_data_average(
+                month_data_list)
+        self.print_month_temp_report(max_temp_avg, min_temp_avg, humidity_avg)
+
+    def generate_barchart_report(self, report_year, dir_path):
+        self.weather_analyzer.read_files(dir_path)
+        month_data_list = self.weather_analyzer.collect_month_data(
+            report_year)
+        barchart_data_list = self.calc_month_chart(month_data_list)
+        self.print_month_chart(barchart_data_list)
+        print("\nBonus\n")
+        bonus_barchart_data_list = self.calc_bonus_chart(month_data_list)
+        self.print_bonus_chart(bonus_barchart_data_list)
+
+    def calc_month_chart(self, month_data_list):
+        barchart_data_list = []
+        day_num = 1
+        for day_data in month_data_list:
+            if day_data.max_temperature:
+                barchart_data_list.append([int(day_data.max_temperature),
+                                           ColorCode.RED.value, day_num])
+            if day_data.min_temperature:
+                barchart_data_list.append([int(day_data.min_temperature),
+                                           ColorCode.BLUE.value, day_num])
+                day_num += 1
+        return barchart_data_list
+
+    def calc_bonus_chart(self, month_data_list):
+        barchart_data_list = []
+        day_num = 1
+        for day_data in month_data_list:
+            barchart_min_temp = ""
+            barchart_max_temp = ""
+            temp_max = 0
+            temp_min = 0
+            if day_data.max_temperature:
+                temp_max = int(day_data.max_temperature)
+                barchart_max_temp = ColorCode.RED.value + ('+' * temp_max)
+            if day_data.min_temperature:
+                temp_min = int(day_data.min_temperature)
+                barchart_min_temp = ColorCode.BLUE.value + ('+' * temp_min)
+                barchart_data_list.append([day_num, barchart_min_temp,
+                                           barchart_max_temp,
+                                           temp_min, temp_max])
+            day_num += 1
+        return barchart_data_list
+
+    def print_bonus_chart(self, barchart_data_list):
+        for data_list in barchart_data_list:
+            self.draw_bonus_barchart(data_list[0], data_list[1], data_list[2],
+                                     data_list[3], data_list[4])
+            print("")
+
+    def print_month_chart(self, barchart_data_list):
+        for data_list in barchart_data_list:
+            self.draw_barchart(data_list[0], data_list[1], data_list[2])
+
+    def draw_barchart(self, temp, temp_color_code, day_num):
+        """ draw bar chart """
+        barchart_month = '+' * temp
+        print(ColorCode.GREY.value + (str(day_num)) +
+              " " + (temp_color_code + barchart_month) +
+              " " + (ColorCode.GREY.value + str(temp) + "C"))
+
+    def draw_bonus_barchart(self, day_num, barchart_min_temp,
+                            barchart_max_temp,
+                            temp_min, temp_max):
+        print((ColorCode.GREY.value + str(day_num)) +
+              " " + barchart_min_temp + barchart_max_temp +
+              " " + (ColorCode.GREY.value + str(temp_min) + "C-") +
+              (ColorCode.GREY.value + str(temp_max) + "C"))
+
+    def print_year_temp_report(self, temp_max_obj, temp_min_obj,
+                               max_humid_obj):
+        max_temp_date = temp_max_obj.pkt.split("-")
+        min_temp_date = temp_min_obj.pkt.split("-")
+        max_humidity_date = max_humid_obj.pkt.split("-")
+        print("Highest: " + temp_max_obj.max_temperature +
+              "C on " + calendar.month_name[int(max_temp_date[1])] +
+              " " + str(max_temp_date[2]))
+        print("Lowest: " + temp_min_obj.min_temperature +
+              "C on " + calendar.month_name[int(min_temp_date[1])] +
+              " " + str(min_temp_date[2]))
+        print("Humid: " + max_humid_obj.max_humidity +
+              "% on " + calendar.month_name[int(max_humidity_date[1])] +
+              " " + str(max_humidity_date[2]))
+
+    def print_month_temp_report(self, max_temp_avg, min_temp_avg,
+                                humidity_avg):
+        print("Highest Average: " +
+              str(max_temp_avg) +
+              "C")
+        print("Lowest Average: " +
+              str(min_temp_avg) +
+              "C")
+        print("Average Humidity: " +
+              str(humidity_avg) +
+              "%")
