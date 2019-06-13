eab4c76b60c4b1aa3dec05886807ff2f306dcd9b
Abdullah Zafar <abdullah@A002-00080.arbisoft.com>
2019-06-13 12:20:48 +0500

    sys.args implemented

--- weatherman.py
+++ weatherman.py
@@ -1,1 +1,14 @@
-# initial commit, empty file
+""" This is the driver program for the weatherman task.
+
+
+"""
+import sys
+from weatherHolder import weatherHolder
+
+mypath = sys.argv[1]
+
+# print(mypath)
+
+instance = weatherHolder(mypath)
+
+
