ff667eea7b7b7f7ed24dcb890f33957526e1907f
Abdullah Zafar <abdullah@A002-00080.arbisoft.com>
2019-06-14 12:30:38 +0500

    complete working flow of first task

--- weatherReader.py
+++ weatherReader.py
@@ -3,36 +3,103 @@
 """
 
 import csv
+import sys
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, exists
+from datetime import datetime
 
-allMonths = {1: 'Jan', 2: 'Feb', 3: 'Mar',
-          4: 'Apr', 5: 'May', 6: 'Jun',
-          7: 'Jul', 8: 'Aug', 9: 'Sep',
-          10: 'Oct', 11: 'Nov', 12: 'Dec',}
+monthTranslationDict = {1: 'Jan', 2: 'Feb', 3: 'Mar',
+                        4: 'Apr', 5: 'May', 6: 'Jun',
+                        7: 'Jul', 8: 'Aug', 9: 'Sep',
+                        10: 'Oct', 11: 'Nov', 12: 'Dec',}
+
+
+def makeINT(s, t):
+    s = s.strip()
+
+    if not s and t == 'min':
+        return sys.maxsize
+    elif not s and t == 'max':
+        return (-sys.maxsize - 1)
+    else:
+        return int(s)
+    # return int(s) if s else sys.maxsize if t == 'min' else (-sys.maxsize - 1) if t == "max"
+
+def dateTranslation(date):
+    if date:
+        dateObject = datetime.strptime(date, '%Y-%m-%d')
+    else:
+        return None
+    if dateObject:
+        return datetime.strftime(dateObject,'%B %d')
+    else:
+        return None
 
 
 class results:
     resultList = []
-    def addResult(self, givenDict):
-        self.resultList.append(givenDict)
+    def addResult(self, resultType, value, date):
+        self.resultList.append({'type': resultType, 'value': value, 'date': date})
+
+    def clearResults(self):
+        self.resultList.clear()
 
     def printResult(self):
         for result in self.resultList:
             character = 'C'
             if 'Humidity' in result['type']:
                 character = '%'
-            print(result['type'], ': ', result['value'], character, ' on ', result['day'], sep='')
+            print(result['type'], ': ', result['value'], character, ' on ', dateTranslation(result['date']), sep='')
 
 
-# class calculate:
-#     result = results()
-#     def __init__(self, weatherData, calculationType):
-#         # if calculationType == '-e'
+class calculate:
+    result = results()
 
-#         # elif calculationType == '-a'
+    def calculateHighest(self, weatherData):
+        highestTemp = {'value': (-sys.maxsize - 1), 'date': ''}
+        lowestTemp =  {'value': (sys.maxsize), 'date': ''}
+        highestHumidity =  {'value': (-sys.maxsize - 1), 'date': ''}
+        
+        for singleDay in weatherData:
+            date = singleDay.weatherDataForTheDay.get('PKT')
+            maxTempForTheDay = {'date': date, 'value': makeINT(singleDay.weatherDataForTheDay.get('MaxTemp'), 'max')}
+            minTempForTheDay = {'date': date, 'value': makeINT(singleDay.weatherDataForTheDay.get('MinTemp'), 'min')}
+            maxHumidForTheDay = {'date': date, 'value': makeINT(singleDay.weatherDataForTheDay.get('MaxHumidity'), 'max')}
 
-#         # elif calculationType == '-c'
+            if maxTempForTheDay['value'] > highestTemp['value']:
+                highestTemp['value'] = maxTempForTheDay['value']
+                highestTemp['date'] = date
+
+            if minTempForTheDay['value'] < lowestTemp['value']:
+                lowestTemp['value'] = minTempForTheDay['value']
+                lowestTemp['date'] = date
+
+            if maxHumidForTheDay['value'] > highestHumidity['value']:
+                highestHumidity['value'] = maxHumidForTheDay['value']
+                highestHumidity['date'] = date
+
+        self.result.clearResults()
+        self.result.addResult('Highest', highestTemp['value'], highestTemp['date'])
+        self.result.addResult('Lowest', lowestTemp['value'], lowestTemp['date'])
+        self.result.addResult('Humidity', highestHumidity['value'], highestHumidity['date'])
+
+        return self.result
+
+    def calculateAverage(self, weatherData):
+        print("")
+
+    def calculateBarCharts(self, weatherData):
+        print("")
+
+    def compute(self, weatherData, calculationType):
+        if calculationType == '-e':
+            return self.calculateHighest(weatherData)
+        elif calculationType == '-a':
+            return self.calculateAverage(weatherData)
+        elif calculationType == '-c':
+            self.calculateBarCharts(weatherData)
+
+
 
 
 class singleReading:
@@ -48,21 +115,24 @@ class singleReading:
 class weatherReader:
     data = []
     def __init__(self, directory, request):
-        allFiles = [f for f in listdir(directory)]
+        if exists(directory):
+            allFiles = [f for f in listdir(directory)]
+        else:
+            print("Directory does not exists!")
+            return None
         yearAndMonth = request.split('/')
         year = request.split('/')[0]
         month = 0
         if len(yearAndMonth) > 1:
             if request.split('/')[1]:
                 month = int(request.split('/')[1])
-        # print(year, month)
         
         files = [x for x in allFiles if year in x]
         monthFiles = []
         if month:
-            monthFiles = [x for x in files if allMonths.get(month) in x]
+            monthFiles = [x for x in files if monthTranslationDict.get(month) in x]
             files = monthFiles
-        print(files)
+        # print(files)
 
         for file in files:
             with open(directory + '/' + file) as csvfile:
--- weatherman.py
+++ weatherman.py
@@ -5,29 +5,19 @@
 import sys
 from weatherReader import weatherReader
 from weatherReader import results
-# from weatherReader import calculate
+from weatherReader import calculate
 
 mypath = sys.argv[1]
 option = sys.argv[2]
 req = sys.argv[3]
 
 instance = weatherReader(mypath, req)
-print(instance.data[0].weatherDataForTheDay)
-print(len(instance.data))
 
-# ca = calculate(instance.data, option)
+if not len(instance.data):
+    print("No values are available for the given time period!")
+    exit()
 
-res = results()
-res.addResult({'type': 'Highest', 'value': 321, 'day': '2014-3-2'})
-res.addResult({'type': 'Average Humidity', 'value': 32, 'day': '2014-5-12'})
-print(res.printResult())
-# elif option == '-a'
- 
+calc = calculate()
+resultt = calc.compute(instance.data, option)
 
-# elif option == '-c'
-
-
-# print(instance.data[0].weatherDataForTheDay)
-# print(instance.data[1].weatherDataForTheDay)
-# print(instance.data[2].weatherDataForTheDay)
-
+print(resultt.printResult())
