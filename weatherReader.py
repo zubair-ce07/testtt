876ccb4482d87a5e56f5c70c904a6c2571130a09
Abdullah Zafar <abdullah@A002-00080.arbisoft.com>
2019-06-14 14:59:17 +0500

    first completed version, except bonus

--- weatherReader.py
+++ weatherReader.py
@@ -1,5 +1,8 @@
-""" This is the driver program for the weatherman task.
+""" This file contains the weatherReader, calculator
+    and result classes, which are used to store data,
+    calculate results, and store those results respectively
 
+    Owner: Muhammad Abdullah Zafar -- Arbisoft
 """
 
 import csv
@@ -8,12 +11,16 @@ from os import listdir
 from os.path import isfile, join, exists
 from datetime import datetime
 
+# This dictionary is used to translate numbered months
+# to respective names. Method used to optimize performance
 monthTranslationDict = {1: 'Jan', 2: 'Feb', 3: 'Mar',
                         4: 'Apr', 5: 'May', 6: 'Jun',
                         7: 'Jul', 8: 'Aug', 9: 'Sep',
                         10: 'Oct', 11: 'Nov', 12: 'Dec',}
 
-
+# Customized function created to deal with empty strings.
+# Returns max size or min size depending on the comparison
+# the value is going to be used in.
 def makeINT(s, t):
     s = s.strip()
 
@@ -23,12 +30,14 @@ def makeINT(s, t):
         return (-sys.maxsize - 1)
     else:
         return int(s)
-    # return int(s) if s else sys.maxsize if t == 'min' else (-sys.maxsize - 1) if t == "max"
 
+# Customized function created to deal with empty strings.
+# Returns zero 0 in case of empty string
 def makeINTZero(s):
     s = s.strip()
     return int(s) if s else 0
 
+# This methos translates a date from %Y-%m-%d format to %B %d
 def dateTranslation(date):
     if date:
         dateObject = datetime.strptime(date, '%Y-%m-%d')
@@ -39,26 +48,49 @@ def dateTranslation(date):
     else:
         return None
 
-
-class results:
+# Results container class with methods to add result in the
+# available memeber list, clear the list in case same object
+# is being used multiple times, and print the results
+class resultContainer:
+    # Member list to store multiple results
     resultList = []
-    def addResult(self, resultType, value, date):
-        self.resultList.append({'type': resultType, 'value': value, 'date': date})
 
+    # Appends a result in the list
+    def addResult(self, resultType, value, date):
+        self.resultList.append({'type': resultType, 
+                                'value': value, 
+                                'date': date})
+
+    # Clears the list of all the results
     def clearResults(self):
         self.resultList.clear()
 
+    # Prints the results
     def printResult(self):
         for result in self.resultList:
             character = 'C'
             if 'Humidity' in result['type']:
                 character = '%'
-            print(result['type'], ': ', result['value'], character, ' on ', dateTranslation(result['date']), sep='')
 
+            if result['date']:
+                print(result['type'], ': ', 
+                      result['value'], character, ' on ', 
+                      dateTranslation(result['date']), 
+                      sep='')
+            else:
+                print(result['type'], ': ', 
+                      result['value'], character, 
+                      sep='')
+            
 
-class calculate:
-    result = results()
+# This class generates the calculations using the given data
+# and resturns the resultContainer object with the results in it
+class calculator:
+    # resultContainer object to store results
+    result = resultContainer()
 
+    # Report 1: calculates highest temperature and day, lowest 
+    # temperature and day, most humid day and humidity. 
     def calculateHighest(self, weatherData):
         highestTemp = {'value': (-sys.maxsize - 1), 'date': ''}
         lowestTemp =  {'value': (sys.maxsize), 'date': ''}
@@ -66,9 +98,17 @@ class calculate:
         
         for singleDay in weatherData:
             date = singleDay.weatherDataForTheDay.get('PKT')
-            maxTempForTheDay = {'date': date, 'value': makeINT(singleDay.weatherDataForTheDay.get('MaxTemp'), 'max')}
-            minTempForTheDay = {'date': date, 'value': makeINT(singleDay.weatherDataForTheDay.get('MinTemp'), 'min')}
-            maxHumidForTheDay = {'date': date, 'value': makeINT(singleDay.weatherDataForTheDay.get('MaxHumidity'), 'max')}
+
+
+            maxTempForTheDay = {'date': date, 
+                                'value': makeINT(singleDay.weatherDataForTheDay.get('MaxTemp'), 'max')}
+
+            minTempForTheDay = {'date': date, 
+                                'value': makeINT(singleDay.weatherDataForTheDay.get('MinTemp'), 'min')}
+
+            maxHumidForTheDay = {'date': date, 
+                                 'value': makeINT(singleDay.weatherDataForTheDay.get('MaxHumidity'), 'max')}
+
 
             if maxTempForTheDay['value'] > highestTemp['value']:
                 highestTemp['value'] = maxTempForTheDay['value']
@@ -83,25 +123,55 @@ class calculate:
                 highestHumidity['date'] = date
 
         self.result.clearResults()
-        self.result.addResult('Highest', highestTemp['value'], highestTemp['date'])
-        self.result.addResult('Lowest', lowestTemp['value'], lowestTemp['date'])
-        self.result.addResult('Humidity', highestHumidity['value'], highestHumidity['date'])
+
+        self.result.addResult('Highest', 
+                              highestTemp['value'], 
+                              highestTemp['date'])
+
+        self.result.addResult('Lowest', 
+                              lowestTemp['value'], 
+                              lowestTemp['date'])
+
+        self.result.addResult('Humidity', 
+                              highestHumidity['value'], 
+                              highestHumidity['date'])
 
         return self.result
 
+
+    # Report 2: For a given month display the average highest temperature, 
+    # average lowest temperature, average mean humidity.
     def calculateAverage(self, weatherData):
         avgHighestTemp = 0
         avgLowestTemp =  0
         avgMeanHumidity =  0
+        skipMinDays = 0
+        skipMaxDays = 0
+        skipMeanDays = 0
         
         for singleDay in weatherData:
-            avgHighestTemp += makeINT(singleDay.weatherDataForTheDay.get('MaxTemp'), 'max')
-            avgLowestTemp += makeINT(singleDay.weatherDataForTheDay.get('MinTemp'), 'min')
-            avgMeanHumidity += makeINT(singleDay.weatherDataForTheDay.get('MeanHumidity'), 'max')
+            initialMaxTemp = makeINTZero(singleDay.weatherDataForTheDay.get('MaxTemp'))
+            initialMinTemp = makeINTZero(singleDay.weatherDataForTheDay.get('MinTemp'))
+            initialMeanhumidity = makeINTZero(singleDay.weatherDataForTheDay.get('MeanHumidity'))
 
-        avgHighestTemp /= len(weatherData)
-        avgLowestTemp /= len(weatherData)
-        avgMeanHumidity /= len(weatherData)
+            if not initialMaxTemp:
+                skipMaxDays += 1
+            else:
+                avgHighestTemp += initialMaxTemp
+
+            if not initialMinTemp:
+                skipMinDays += 1
+            else:
+                avgLowestTemp += initialMinTemp
+
+            if not initialMeanhumidity:
+                skipMeanDays += 1
+            else:
+                avgMeanHumidity += initialMeanhumidity
+
+        avgHighestTemp /= len(weatherData) - skipMaxDays
+        avgLowestTemp /= len(weatherData) - skipMinDays
+        avgMeanHumidity /= len(weatherData) - skipMeanDays
 
         self.result.clearResults()
         self.result.addResult('Highest Average', int(avgHighestTemp), '')
@@ -110,6 +180,8 @@ class calculate:
 
         return self.result
 
+    # For a given month draw two horizontal bar charts on the console for the highest 
+    # and lowest temperature on each day. Highest in red and lowest in blue.
     def calculateBarCharts(self, weatherData):
 
         self.result.clearResults()
@@ -118,37 +190,46 @@ class calculate:
             maxTempForTheDay = day.weatherDataForTheDay.get('MaxTemp')
             minTempForTheDay = day.weatherDataForTheDay.get('MinTemp')
             date = day.weatherDataForTheDay.get('PKT')
-            maxString = datetime.strftime(datetime.strptime(date, '%Y-%m-%d'),'%d') + ' '
+
+            maxString = "\033[0;35;40m" + datetime.strftime(datetime.strptime(date, '%Y-%m-%d'),'%d') + ' '
             maxString += ''.join(["\033[0;31;40m+" for x in range(abs(makeINTZero(maxTempForTheDay)))])
             maxString += ' '+ "\033[0;35;40m" + maxTempForTheDay + 'C'
 
             minString = datetime.strftime(datetime.strptime(date, '%Y-%m-%d'),'%d') + ' '
             minString += ''.join(["\033[0;34;40m+" for x in range(abs(makeINTZero(minTempForTheDay)))])
             minString += ' '+ "\033[0;35;40m" + minTempForTheDay + 'C'
+            
             print(maxString)
             print(minString)
+        
+        print("\033[0;0;40m")
 
+    # Method called from the main multiple times, given the request and data
     def compute(self, weatherData, calculationType, req):
         if calculationType == '-e':
             if len(req.split('/')) != 1:
                 print("Wrong format. Exiting!")
                 exit() 
+
             return self.calculateHighest(weatherData)
+
         elif calculationType == '-a':
             if len(req.split('/')) != 2:
                 print("Wrong format. Exiting!")
                 exit()
+
             return self.calculateAverage(weatherData)
+
         elif calculationType == '-c':
             if len(req.split('/')) != 2:
                 print("Wrong format. Exiting!")
                 exit()
+
             print(datetime.strftime(datetime.strptime(req, '%Y/%m'),'%B %Y'))
             return self.calculateBarCharts(weatherData)
 
 
-
-
+# Class for storing a single day weather value in a dict
 class singleReading:
     weatherDataForTheDay = {}
     def __init__(self, oneReading):
@@ -159,27 +240,31 @@ class singleReading:
                                      'MeanHumidity': oneReading.get('Mean Humidity')}
 
 
+# Class that stores required data in a member variable of the
+# object, as specified by the given option
 class weatherReader:
     data = []
+
     def __init__(self, directory, request):
         if exists(directory):
             allFiles = [f for f in listdir(directory)]
         else:
             print("Directory does not exists!")
             return None
+
         yearAndMonth = request.split('/')
         year = request.split('/')[0]
         month = 0
+        monthFiles = []
+        files = [x for x in allFiles if year in x]
+
         if len(yearAndMonth) > 1:
             if request.split('/')[1]:
                 month = int(request.split('/')[1])
-        
-        files = [x for x in allFiles if year in x]
-        monthFiles = []
+
         if month:
             monthFiles = [x for x in files if monthTranslationDict.get(month) in x]
             files = monthFiles
-        # print(files)
 
         for file in files:
             with open(directory + '/' + file) as csvfile:
@@ -187,5 +272,3 @@ class weatherReader:
                 for row in readCSV:
                     dayReading = singleReading(row)
                     self.data.append(dayReading)
-
-        
--- weatherman.py
+++ weatherman.py
@@ -1,24 +1,64 @@
 """ This is the driver program for the weatherman task.
+    Having imported weatherReader, results and calculate
+    classes from the weatherReader.py file, it makes
+    instances of those objects to achieve the tasks as 
+    specified by the command line arguments
 
-
+    Owner: Muhammad Abdullah Zafar -- Arbisoft
 """
+
 import sys
 from weatherReader import weatherReader
-from weatherReader import results
-from weatherReader import calculate
+from weatherReader import calculator
 
-mypath = sys.argv[1]
+myPath = sys.argv[1]
 option = sys.argv[2]
-req = sys.argv[3]
+request = sys.argv[3]
 
-instance = weatherReader(mypath, req)
+# If more than one report is to be generated
+if len(sys.argv) > 4:
+    for index in range(2, len(sys.argv), 2):
+        # Get command line arguments
+        option = sys.argv[index]
+        request = sys.argv[index+1]
 
-if not len(instance.data):
-    print("No values are available for the given time period!")
-    exit()
+        # Create reader and calculator instances
+        # Reader insatance reads the data at creation
+        weatherReaderinstance = weatherReader(myPath, request)
+        calculatorInstance = calculator()
+        
+        # Check if data is available against the given time frame
+        if not len(weatherReaderinstance.data):
+            print("No values are available for the given time period!")
+            print('\n')
+            continue
 
-calc = calculate()
-resultt = calc.compute(instance.data, option, req)
+        # Compute the results by passing request and data 
+        # as well as the required report type to the calculator
+        result = calculatorInstance.compute(weatherReaderinstance.data, option, request)
 
-if resultt:
-    print(resultt.printResult())
+        # Calculator might return empty list as a result.
+        if result:
+            print(result.printResult())
+
+        print('\n')
+
+# If there is only one report to be generated
+else:
+    # Create reader and calculator instances
+    # Reader insatance reads the data at creation
+    weatherReaderinstance = weatherReader(myPath, request)
+    calculatorInstance = calculator()
+
+    # Check if data is available against the given time frame
+    if not len(weatherReaderinstance.data):
+        print("No values are available for the given time period!")
+        exit()
+
+    # Compute the results by passing request and data 
+    # as well as the required report type to the calculator
+    result = calculatorInstance.compute(weatherReaderinstance.data, option, request)
+
+    # Calculator might return empty list as a result.
+    if result:
+        print(result.printResult())
