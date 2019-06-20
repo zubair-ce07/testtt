9ecfd61294d1d5e88ef992efea7b39182c232701
Ikram ullah Khan <ikram@A002-00048.arbisoft.com>
2019-06-20 18:17:10 +0500

    extract change with geall, extract_first change with get, care_terms added, gender terms added, strip blank lines method added, html tag removed, enhanced selector for multiple element select, parse_base_category_links renamed parse_category, parse_sub_category_links rename parse_sub_category, color renamed to colour

--- beyond_limit_spider.py
+++ beyond_limit_spider.py
@@ -20,106 +20,105 @@ class BeyondLimitSpider(Spider):
     name = "beyond_limit"
     allowed_domains = ["beyondlimits.com"]
     start_urls = ["https://www.beyondlimits.com"]
+    gender_terms = [
+        "Men",
+        "Women",
+    ]
+    care_terms = [
+        "Care tips",
+        "Material",
+    ]
 
     def parse(self, response):
-        base_category_links = response.css("div.ft_cloned--inner li.bb_mainmenu--item "
-                                           "a::attr(data-bbfwhref)").extract()
-        yield from [response.follow(base_category_link, callback=self.parse_base_category_links)
+        base_category_links = response.css(".ft_cloned--inner .bb_mainmenu--item "
+                                           "::attr(data-bbfwhref)").getall()
+        yield from [response.follow(base_category_link, callback=self.parse_category)
                     for base_category_link in base_category_links]
 
-    def parse_base_category_links(self, response):
-        sub_category_links = response.css("ul.bb_subcat--list a::attr(href)").extract()
-        yield from [response.follow(sub_category_link, callback=self.parse_sub_category_links)
+    def parse_category(self, response):
+        sub_category_links = response.css(".bb_subcat--list ::attr(href)").getall()
+        yield from [response.follow(sub_category_link, callback=self.parse_sub_category)
                     for sub_category_link in sub_category_links]
 
-    def parse_sub_category_links(self, response):
-        item_links = response.css("li.productData a.bb_product--link::attr(href)").extract()
-        item_links = self.remove_duplicate_links(item_links)
+    def parse_sub_category(self, response):
+        item_links = response.css(".bb_product--link::attr(href)").getall()
         yield from [response.follow(item_link, callback=self.parse_item) for item_link in
                     item_links]
 
     def parse_item(self, response):
         item = Product()
-        selector = response.css(".bb_art--content")
         item["url"] = response.url
-        item["brand"] = "BeyondLimits"
-        item["name"] = self.extract_item_name(selector)
-        item["retailer_sku"] = self.extract_retailer_sku(selector)
+        item["brand"] = self.extract_brand_name()
+        item["name"] = self.extract_item_name(response)
+        item["retailer_sku"] = self.extract_retailer_sku(response)
         item["description"] = self.extract_description(response)
         item["image_urls"] = self.extract_image_urls(response)
         item["care"] = self.extract_care(response)
         item["gender"] = self.extract_gender(response)
         item["category"] = self.extract_category(response)
         item["skus"] = self.extract_skus(response)
+
         return item
 
-    def remove_duplicate_links(self, item_links):
-        return list(set(item_links))
+    def extract_item_name(self, response):
+        return response.css("[itemprop=name]::text").extract_first()
 
-    def extract_item_name(self, selector):
-        return selector.css("[itemprop=name]::text").extract_first()
+    def extract_retailer_sku(self, response):
+        return response.css("[itemprop=productID]::text").get()
 
-    def extract_retailer_sku(self, selector):
-        return selector.css("[itemprop=productID]::text").extract_first()
+    def extract_brand_name(self):
+        return "BeyondLimits"
 
     def extract_gender(self, response):
-        return response.css("div#breadCrumb [itemprop=title]::text").extract()[1].strip()
+        raw_genders = response.css("[itemprop=title]::text").getall()
+        raw_genders = self.remove_unwanted_spaces(raw_genders)
+        return [gender for gender in raw_genders if any(gender_term in gender
+                                                        for gender_term in self.gender_terms)]
 
     def extract_category(self, response):
-        raw_category = response.css("div#breadCrumb [itemprop=title]::text").extract()
-        return [raw_category_item.strip() for raw_category_item in raw_category]
+        raw_category = response.css("[itemprop=title]::text").getall()
+        raw_category = self.remove_unwanted_spaces(raw_category)
+        return [raw_category_item for raw_category_item in raw_category]
 
     def extract_care(self, response):
-        raw_care = response.css("div#description ul > li::text").extract()
-        if not raw_care:
-            raw_care = response.css("div#description ul > li.MsoNormal span::text").extract()
-        if raw_care:
-            raw_care.pop(0)
-
-        return raw_care
+        raw_cares = response.css("#description li::text, #description "
+                                 ".MsoNormal span::text").getall()
+        return [care_item for care_item in raw_cares if any(care_term in care_item
+                                                            for care_term in self.care_terms)]
 
     def extract_image_urls(self, response):
-        return response.css("ul.bb_pic--nav a.bb_pic--navlink::attr(data-bbzoompicurl)").extract()
+        return response.css(".bb_pic--navlink::attr(data-bbzoompicurl)").getall()
 
     def extract_description(self, response):
-        raw_description = response.css("div#description p::text").extract_first()
-        if not raw_description:
-            raw_description = response.css("div#description::text").extract_first()
+        raw_description = response.css("#description p::text, #description ::text").get()
+        return [description for description in raw_description.split(".") if description]
 
-        return [description.rstrip() for description in raw_description.split(".") if description]
-
-    def extract_color(self, response):
-        raw_color = response.css("div#description li::text").extract_first()
-        if not raw_color:
-            raw_color = response.css("div#description li.MsoNormal span::text").extract_first()
-        if raw_color:
-            raw_color = raw_color.split(":")[1].strip().rstrip()
-
-        return raw_color
+    def extract_colour(self, response):
+        return response.css("#description li::text, .MsoNormal span::text").get()
 
     def extract_currency(self, response):
-        return response.css("[itemprop=priceCurrency]::attr(content)").extract_first()
+        return response.css("[itemprop=priceCurrency]::attr(content)").get()
 
     def extract_current_price(self, response):
-        return response.css("[itemprop=price]::text").extract_first()
+        return response.css("[itemprop=price]::text").get()
 
     def extract_previous_prices(self, response):
-        return response.css("span.oldPrice del::text").extract()
-
-    def extract_sizes(self, response):
-        return response.css("select.bb_form--select option[value!=\"\"]::text").extract()
+        return response.css(".oldPrice del::text").getall()
 
     def extract_skus(self, response):
         skus = []
-        for item_size in self.extract_sizes(response):
+        for item_size in response.css(".bb_form--select [value!=\"\"]::text").getall():
             sku = {
-                "colour": self.extract_color(response),
+                "colour": self.extract_colour(response),
                 "price": self.extract_current_price(response),
                 "currency": self.extract_currency(response),
                 "previous_prices": self.extract_previous_prices(response),
                 "size": item_size,
-                "sku_id": f"{self.extract_color(response)}_{item_size}"
+                "sku_id": f"{self.extract_colour(response)}_{item_size}"
             }
             skus.append(sku)
 
         return skus
+
+    def remove_unwanted_spaces(self, raw_list):
+        return [list_item.strip() for list_item in raw_list if list_item]
