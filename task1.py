275738d9d5b202a7493f7af080491c01959fda5c
ahmadarshi <m.ahmad@arbisoft.com>
2019-06-20 13:59:27 +0500

    remove the WeatherFileRecord class and made color schemes for temperature and made funcions pure

--- task1.py
+++ task1.py
@@ -2,8 +2,6 @@ import argparse
 import calendar
 import csv
 import os
-import pprint
-import sys
 
 
 class CleanData:
@@ -12,11 +10,6 @@ class CleanData:
         self.max_temp = max_temp
         self.min_temp = min_temp
         self.max_humidity = max_humidity
-
-
-class WeatherFilesRecord:
-    def __init__(self):
-        self.weather_dic = {}
 
 
 class WeatherReportParse:
@@ -32,30 +25,33 @@ class WeatherReportParse:
     def __init__(self):
         self.month_to_number = {v: k for k, v in enumerate(calendar.month_abbr)}
 
-    def parse_year_data(self, weather_report):
+    def parse_year_data(self):
 
         weather_file_names = [f for f in os.listdir(args.path) if
                               f[0] != '.' and f.split('_')[2] == str(args.year)]
         weather_file_names.sort()
-
+        weather_report = {}
         for weather_file in weather_file_names:
             month = self.month_to_number[weather_file.split('_')[-1].split('.')[0]]
-            weather_report.update({month: csv.DictReader(open(os.path.join
-                                                              (args.path, weather_file)))})
+            file_date = csv.DictReader(open(os.path.join(args.path, weather_file)))
+            weather_report.update({month: file_date})
+            return weather_report
 
-    def parse_month_data(self, weather_report, year_month):
+    def parse_month_data(self):
         a = dict((v, k) for k, v in enumerate(calendar.month_abbr))
-        month_name = calendar.month_name[int(year_month.split('/')[1])][:3]
-        year = str(int(year_month.split('/')[0]))
+        month_name = calendar.month_name[int(args.month.split('/')[1])][:3]
+        year = str(int(args.month.split('/')[0]))
 
-        Weather_file_names = [f for f in os.listdir(args.path) if
-                              f[0] != '.' and f.split('_')[-1].split('.')[0] == month_name and
-                              f.split('_')[2] == year]
-
+        Weather_file_names = [f for f in os.listdir(args.path)
+                              if f[0] != '.' and f.split('_')[-1].split('.')[0] == month_name
+                              and f.split('_')[2] == year]
+        weather_report = {}
         for weather_file in Weather_file_names:
             month = self.month_to_number[weather_file.split('_')[-1].split('.')[0]]
-            weather_report.update({month: csv.DictReader(open(os.path.join
-                                                              (args.path, weather_file)))})
+            file_date = csv.DictReader(open(os.path.join(args.path, weather_file)))
+            weather_report.update({month: file_date})
+
+            return weather_report
 
 
 class DataResults:
@@ -90,7 +86,7 @@ class DataResults:
 
         return temp_sum[0] / temp_count[0], temp_sum[1] / temp_count[1], temp_sum[2] / temp_count[2]
 
-    def show_year_result(self, highest_temp, lowest_temp, max_humidity):
+    def show_extreme_result(self, highest_temp, lowest_temp, max_humidity):
         month_name = calendar.month_name[int(highest_temp.date.split('-')[1])]
         temp = highest_temp.date.split('-')[2]
         print(f'Highest: {highest_temp.max_temp} C on {month_name} {temp}')
@@ -103,7 +99,7 @@ class DataResults:
         temp = max_humidity.date.split('-')[2]
         print(f'Humidity: {max_humidity.max_humidity} C on {month_name} {temp}')
 
-    def show_month_result(self, avg_high_temp, avg_low_temp, avg_hum):
+    def show_average_result(self, avg_high_temp, avg_low_temp, avg_hum):
 
         print(f'Highest Average: {avg_high_temp} C')
         print(f'Lowest Average: {avg_low_temp} C')
@@ -119,55 +115,54 @@ class DataReport:
 
         return self._instance
 
-    def show_seprate_graph(self, clean_data):
-        for weather_date in weather_report:
+    def show_seprate_chart(self, clean_data):
+        for weather_date in clean_data:
             print(weather_date.date.split('-')[-1], end='')
 
             for i in range(0, int(weather_date.max_temp)):
-                print(' + ', end='')
+                print('\033[1;31;40m + ', end='')
 
             print("%s C" % weather_date.max_temp)
             print(weather_date.date.split('-')[-1], end='')
 
             for j in range(0, int(weather_date.min_temp)):
-                print(' - ', end='')
+                print('\033[1;34;40m - ', end='')
             print("%s C" % weather_date.min_temp)
 
         print("\n\n\n")
 
-    def show_merge_graph(self, weather_report):
+    def show_merge_chart(self, weather_report):
         for weather_date in weather_report:
-            low = 0
-            high = 0
-            print(weather_date.date.split('-')[-1], end='')
+            print("\033[1;31;40m %s" % weather_date.date.split('-')[-1], end='\t')
+            print(f'\033[1;31;40m {weather_date.max_temp} C', end='')
 
             for i in range(0, int(weather_date.max_temp)):
-                print(' + ', end='')
-            high = weather_date.max_temp + "C"
+                print('\033[1;31;40m + ', end='')
 
-            for i in range(0, int(weather_date.min_temp)):
-                print(' + ', end='')
-            low = (weather_date.min_temp + "C")
+            for j in range(0, int(weather_date.min_temp)):
+                print('\033[1;34;40m + ', end='')
 
-            print("%s-%s" % (high, low))
+            print(f'\033[1;34;40m {weather_date.min_temp} C')
 
 
-def fetch_clean_data(clean_data, weather_report):
-    for weather_month in weather_report.weather_dic:
-        for weather_date in weather_report.weather_dic[weather_month]:
+def fetch_clean_data(weather_report):
+    clean_data = []
+    for weather_month in weather_report:
+        for weather_date in weather_report[weather_month]:
             if weather_date['Max TemperatureC']:
                 clean_data.append(CleanData(weather_date['PKT'],
                                             weather_date['Max TemperatureC'],
                                             weather_date['Min TemperatureC'],
                                             weather_date['Max Humidity']))
 
+    return clean_data
 
-def check_data_validation(weather_report):
-    if not weather_report:
+
+def check_data_validation(weather_report_dic):
+    if not weather_report_dic:
         print(f'{args.year} is not in the record')
         return 0
-    else:
-        return 1
+    return 1
 
 
 parser = argparse.ArgumentParser(description="Calculating and showing "
@@ -179,44 +174,39 @@ parser.add_argument("-c", "--graphsingle", type=str, help="Show single graph")
 parser.add_argument("-m", "--graphmerged", type=str, help="Show merged graph")
 args = parser.parse_args()
 
-weather_report = WeatherFilesRecord()
 parser = WeatherReportParse()
 results = DataResults()
-clean_data = []
+clean_weather_data = []
 
 if args.year:
-    weather_report = WeatherFilesRecord()
-    parser.parse_year_data(weather_report.weather_dic)
+    weather_report_data = parser.parse_year_data()
 
-    if check_data_validation(weather_report.weather_dic):
-        fetch_clean_data(clean_data, weather_report)
-        highest_temp, lowest_temp, max_humidity = results.year_report(clean_data)
-        results.show_year_result(highest_temp, lowest_temp, max_humidity)
+    if check_data_validation(weather_report_data):
+        clean_weather_data = fetch_clean_data(weather_report_data)
+        highest_temp, lowest_temp, max_humidity = results.year_report(clean_weather_data)
+        results.show_extreme_result(highest_temp, lowest_temp, max_humidity)
 
 if args.month:
-    weather_report = WeatherFilesRecord()
-    parser.parse_month_data(weather_report.weather_dic, args.month)
+    weather_report_data = parser.parse_month_data()
 
-    if check_data_validation(weather_report.weather_dic):
-        fetch_clean_data(clean_data, weather_report)
-        avg_high_temp, avg_low_temp, avg_hum = results.month_report(clean_data)
-        results.show_month_result(avg_high_temp, avg_low_temp, avg_hum)
+    if check_data_validation(weather_report_data):
+        clean_weather_data = fetch_clean_data(weather_report_data)
+        avg_high_temp, avg_low_temp, avg_hum = results.month_report(clean_weather_data)
+        results.show_average_result(avg_high_temp, avg_low_temp, avg_hum)
 
 if args.graphsingle:
-    weather_report = WeatherFilesRecord()
-    parser.parse_month_data(weather_report.weather_dic, args.graphsingle)
+    weather_report_data = parser.parse_month_data()
 
-    if check_data_validation(weather_report.weather_dic):
-        fetch_clean_data(clean_data, weather_report)
+    if check_data_validation(weather_report_data):
+        clean_weather_data = fetch_clean_data(weather_report_data)
         Report = DataReport()
-        Report.show_seprate_graph(clean_data)
+        Report.show_seprate_chart(clean_weather_data)
 
 if args.graphmerged:
-    weather_report = WeatherFilesRecord()
-    parser.parse_month_data(weather_report.weather_dic, args.graphmerged)
+    weather_report_data = parser.parse_month_data()
 
-    if check_data_validation(weather_report.weather_dic):
-        fetch_clean_data(clean_data, weather_report)
+    if check_data_validation(weather_report_data):
+        clean_weather_data = fetch_clean_data(weather_report_data)
         Report = DataReport()
-        Report.show_merge_graph(clean_data)
+        Report.show_merge_chart(clean_weather_data)
 
