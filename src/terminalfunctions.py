ea5e96e0c4c9dff7dda4d208fc8697ad9a80cbb6
Mohammad Owais Basit <owais.basit@arbisoft.com>
2019-06-21 16:33:34 +0500

    feat: check for valid file path
    Used os module to validate the path to file directory given as an argument

--- src/terminalfunctions.py
+++ src/terminalfunctions.py
@@ -1,29 +1,33 @@
 import re
+import os
 
 def validateArguments(args):
     size = len(args)
-    if (size%2 != 0):
+    if size%2 != 0:
         return False
-    if (size < 4):
+    if size < 4:
+        return False
+    if not os.path.exists(args[1]):
+        print("Invalid path!")
         return False
     for i in range(2, size-1, 2):
         args[i]= str(args[i])
-        if (args[i] != '-e' and args[i] != '-a' and args[i] != '-c' and args[i] != '-b'):
+        if args[i] != '-e' and args[i] != '-a' and args[i] != '-c' and args[i] != '-b':
             return False
         value_passed = args[i+1]
-        if (args[i] == "-e"):
+        if args[i] == "-e":
             year = int(value_passed)
-            if (year < 2004 or year > 2016):
+            if year < 2004 or year > 2016:
                 return False
-        if (args[i] == "-a" or args[i] == "-c" or args[i] == "-b"):
+        if args[i] == "-a" or args[i] == "-c" or args[i] == "-b":
             data = str(value_passed)
-            if (re.match("\\d{4}/\\d{2}", data) != None or re.match("\\d{4}/\\d{1}", data) != None):
+            if re.match("\\d{4}/\\d{2}", data) != None or re.match("\\d{4}/\\d{1}", data) != None:
                 combined = data.split('/')
                 split_year = int(combined[0])
                 split_month = int(combined[1])
-                if (split_year < 2004 or split_year > 2016):
+                if split_year < 2004 or split_year > 2016:
                     return False
-                if (split_month < 1 or split_month > 12):
+                if split_month < 1 or split_month > 12:
                     return False
             else:
                 return False
