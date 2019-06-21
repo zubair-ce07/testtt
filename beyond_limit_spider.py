f1bda8f75afb856338441ce7a9311c5ab1eb47b0
Ikram ullah Khan <ikram@A002-00048.arbisoft.com>
2019-06-21 18:22:40 +0500

    geneder_terms changed to case insensitive, css variable used for storing css selector to avoid multiple lines, Request used for absolute urls, response argument added for extract_brand_name method, extract_first removed, clean function addded, description cleaned, double qoutes used inside single qoutes,default_brand created for storing Brand name, common sku values moved outside of the loop, remove_unwanted_spaces changed to clean method

--- beyond_limit_spider.py
+++ beyond_limit_spider.py
@@ -1,6 +1,6 @@
 import scrapy
+from scrapy import Spider, Request
 from scrapy.item import Item
-from scrapy.spiders import Spider
 
 
 class Product(Item):
@@ -20,9 +20,10 @@ class BeyondLimitSpider(Spider):
     name = 'beyond_limit'
     allowed_domains = ['beyondlimits.com']
     start_urls = ['https://www.beyondlimits.com']
+    default_brand = 'BeyondLimits'
     gender_terms = [
-        'Men',
-        'Women',
+        'men',
+        'women',
     ]
     care_terms = [
         'Care tips',
@@ -30,25 +31,22 @@ class BeyondLimitSpider(Spider):
     ]
 
     def parse(self, response):
-        base_category_links = response.css('.ft_cloned--inner .bb_mainmenu--item '
-                                           '::attr(data-bbfwhref)').getall()
-        yield from [response.follow(base_category_link, callback=self.parse_category)
-                    for base_category_link in base_category_links]
+        css = '.ft_cloned--inner .bb_mainmenu--item ::attr(data-bbfwhref)'
+        base_category_links = response.css(css).getall()
+        yield from [Request(url=u, callback=self.parse_category) for u in base_category_links]
 
     def parse_category(self, response):
         sub_category_links = response.css('.bb_subcat--list ::attr(href)').getall()
-        yield from [response.follow(sub_category_link, callback=self.parse_sub_category)
-                    for sub_category_link in sub_category_links]
+        yield from [Request(url=u, callback=self.parse_sub_category) for u in sub_category_links]
 
     def parse_sub_category(self, response):
         item_links = response.css('.bb_product--link::attr(href)').getall()
-        yield from [response.follow(item_link, callback=self.parse_item) for item_link in
-                    item_links]
+        yield from [Request(item_link, callback=self.parse_item) for item_link in item_links]
 
     def parse_item(self, response):
         item = Product()
         item['url'] = response.url
-        item['brand'] = self.extract_brand_name()
+        item['brand'] = self.extract_brand_name(response)
         item['name'] = self.extract_item_name(response)
         item['retailer_sku'] = self.extract_retailer_sku(response)
         item['description'] = self.extract_description(response)
@@ -61,40 +59,36 @@ class BeyondLimitSpider(Spider):
         return item
 
     def extract_item_name(self, response):
-        return response.css('[itemprop=name]::text').extract_first()
+        return response.css('[itemprop=name]::text').get()
 
     def extract_retailer_sku(self, response):
         return response.css('[itemprop=productID]::text').get()
 
-    def extract_brand_name(self):
-        return 'BeyondLimits'
+    def extract_brand_name(self, response):
+        return self.default_brand
 
     def extract_gender(self, response):
         raw_genders = response.css('[itemprop=title]::text').getall()
-        raw_genders = self.remove_unwanted_spaces(raw_genders)
-        return [gender for gender in raw_genders if any(gender_term in gender
-                                                        for gender_term in self.gender_terms)]
+        return next((gn for gn in clean(raw_genders) if gn.lower() in self.gender_terms), "unknown")
 
     def extract_category(self, response):
         raw_category = response.css('[itemprop=title]::text').getall()
-        raw_category = self.remove_unwanted_spaces(raw_category)
-        return [raw_category_item for raw_category_item in raw_category]
+        return [raw_category_item for raw_category_item in clean(raw_category)]
 
     def extract_care(self, response):
-        raw_cares = response.css('#description li::text, #description '
-                                 '.MsoNormal span::text').getall()
-        return [care_item for care_item in raw_cares if any(care_term in care_item
-                                                            for care_term in self.care_terms)]
+        css = '#description li::text, #description .MsoNormal span::text'
+        raw_cares = response.css(css).getall()
+        return [care for care in raw_cares if any(care_t in care for care_t in self.care_terms)]
 
     def extract_image_urls(self, response):
         return response.css('.bb_pic--navlink::attr(data-bbzoompicurl)').getall()
 
     def extract_description(self, response):
-        raw_description = response.css('#description p::text, #description ::text').get()
-        return [description for description in raw_description.split('.') if description]
+        raw_description = response.css('#description p::text, #description:not(li)::text').getall()
+        return [desc for sublist in clean(raw_description) for desc in sublist.split(".") if desc]
 
     def extract_colour(self, response):
-        return response.css('#description li::text, .MsoNormal span::text').get()
+        return response.css('#description li::text, .MsoNormal span::text').re_first(r'\s{1}\w+')
 
     def extract_currency(self, response):
         return response.css('[itemprop=priceCurrency]::attr(content)').get()
@@ -107,18 +101,20 @@ class BeyondLimitSpider(Spider):
 
     def extract_skus(self, response):
         skus = []
-        for item_size in response.css('.bb_form--select [value!=\'\']::text').getall():
-            sku = {
-                'colour': self.extract_colour(response),
-                'price': self.extract_current_price(response),
-                'currency': self.extract_currency(response),
-                'previous_prices': self.extract_previous_prices(response),
-                'size': item_size,
-                'sku_id': f'{self.extract_colour(response)}_{item_size}'
-            }
+        colour = self.extract_colour(response)
+        sku = {
+            'colour': colour,
+            'price': self.extract_current_price(response),
+            'currency': self.extract_currency(response),
+            'previous_prices': self.extract_previous_prices(response)
+        }
+        for item_size in response.css('.bb_form--select [value!=""]::text').getall():
+            sku['size'] = item_size
+            sku['sku_id'] = f'{colour}_{item_size}'
             skus.append(sku)
 
         return skus
 
-    def remove_unwanted_spaces(self, raw_list):
-        return [list_item.strip() for list_item in raw_list if list_item]
+
+def clean(raw_list):
+    return [list_item.strip() for list_item in raw_list if list_item.strip()]
