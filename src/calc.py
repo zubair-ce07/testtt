00e56ef753c1c775ac6965ca09a7d20210bcb320
Mohammad Owais Basit <owais.basit@arbisoft.com>
2019-06-21 16:40:45 +0500

    fix: changes for code efficiency
    Implemented list comprehension, removed extra variables

--- src/calc.py
+++ src/calc.py
@@ -1,90 +1,69 @@
-from ds import YearResults
-from ds import MonthResults
-from ds import ChartResults
+from ds import YearResults, MonthResults, ChartResults
 
-def yearlyCalc(year):
-    max_t = year.months[0].days[0].max_temp
-    max_t_date = year.months[0].days[0].date
+def yearly_calc(year):
+    max_temp = year.months[0].days[0].max_temp
+    max_temp_date = year.months[0].days[0].date
     for month in year.months:
-        for day in month.days:
-            if (day.max_temp == ''):
-                continue
-            if (int(day.max_temp) > int(max_t)):
-                max_t = day.max_temp
-                max_t_date = day.date
+        temp_max_days = [day for day in month.days if day.max_temp]
+        for day in temp_max_days:
+            if int(day.max_temp) > int(max_temp):
+                max_temp = day.max_temp
+                max_temp_date = day.date
 
-    min_t = year.months[0].days[0].min_temp
-    min_t_date = year.months[0].days[0].date
+    min_temp = year.months[0].days[0].min_temp
+    min_temp_date = year.months[0].days[0].date
     for month in year.months:
-        for day in month.days:
-            if (day.min_temp == ''):
-                continue
-            if (int(day.min_temp) < int(min_t)):
-                min_t = day.min_temp
-                min_t_date = day.date
+        temp_min_days = [day for day in month.days if day.min_temp]
+        for day in temp_min_days:
+            if int(day.min_temp) < int(min_temp):
+                min_temp = day.min_temp
+                min_temp_date = day.date
 
-    max_h = year.months[0].days[0].max_humidity
-    max_h_date = year.months[0].days[0].date
+    max_humid = year.months[0].days[0].max_humidity
+    max_humid_date = year.months[0].days[0].date
     for month in year.months:
-        for day in month.days:
-            if (day.max_humidity == ''):
-                continue
-            if (int(day.max_humidity) > int(max_h)):
-                max_h = day.max_humidity
-                max_h_date = day.date
-    year_res = YearResults(max_t, max_t_date, min_t, min_t_date, max_h, max_h_date)
-    return year_res
+        humid_max_days = [day for day in month.days if day.max_humidity]
+        for day in humid_max_days:
+            if int(day.max_humidity) > int(max_humid):
+                max_humid = day.max_humidity
+                max_humid_date = day.date
+    
+    return YearResults(max_temp, max_temp_date, min_temp, min_temp_date, max_humid, max_humid_date)
 
-def monthlyCalc(month):
+def monthly_calc(month):
     sum_high_t = 0
-    count_high_t = 0
-    for day in month.days:
-        if (day.max_temp == ''):
-            continue
+    high_temp_days = [day for day in month.days if day.max_temp]
+    for day in high_temp_days:
         sum_high_t = sum_high_t + int(day.max_temp)
-        count_high_t += 1
-    avg_high_t = sum_high_t/count_high_t
-    avg_high_t = round(avg_high_t)
+    avg_high_t = round(sum_high_t/len(high_temp_days))
 
     sum_low_t = 0
-    count_low_t = 0
-    for day in month.days:
-        if (day.min_temp == ''):
-            continue
+    low_temp_days = [day for day in month.days if day.min_temp]
+    for day in low_temp_days:
         sum_low_t = sum_low_t + int(day.min_temp)
-        count_low_t += 1
-    avg_low_t = sum_low_t/count_low_t
-    avg_low_t = round(avg_low_t)
+    avg_low_t = round(sum_low_t/len(low_temp_days))
 
     sum_mean_h = 0
-    count_mean_h = 0
-    for day in month.days:
-        if (day.mean_humidity == ''):
-            continue
+    mean_humid_days = [day for day in month.days if day.mean_humidity]
+    for day in mean_humid_days:
         sum_mean_h = sum_mean_h + int(day.mean_humidity)
-        count_mean_h += 1
-    avg_mean_h = sum_mean_h/count_mean_h
-    avg_mean_h = round(avg_mean_h)
+    avg_mean_h = round(sum_mean_h/len(mean_humid_days))
 
-    month_res = MonthResults(avg_high_t, avg_low_t, avg_mean_h)
-    return month_res
+    return MonthResults(avg_high_t, avg_low_t, avg_mean_h)
 
-def barChart(month):
+def bar_chart(month):
     high_t = []
     high_d = []
-    for day in month.days:
-        if (day.max_temp == ''):
-            continue
+    high_temp_days = [day for day in month.days if day.max_temp]
+    for day in high_temp_days:
         high_t.append(day.max_temp)
         high_d.append(day.date)
     
     low_t = []
     low_d = []
-    for day in month.days:
-        if (day.min_temp == ''):
-            continue
+    low_temp_days = [day for day in month.days if day.min_temp]
+    for day in low_temp_days:
         low_t.append(day.min_temp)
         low_d.append(day.date)
 
-    chart_res = ChartResults(high_t, low_t, high_d, low_d, month.month_name, month.year)
-    return chart_res
+    return ChartResults(high_t, low_t, high_d, low_d, month.month_name, month.year)
