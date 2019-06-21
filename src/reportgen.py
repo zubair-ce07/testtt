18f490de3f7dc40f65b1d7f31ac4af1d1653c036
Mohammad Owais Basit <owais.basit@arbisoft.com>
2019-06-21 16:44:24 +0500

    fix: changes for code efficiency
    Implemented 'strptime', changed the structure of 'for' loops

--- src/reportgen.py
+++ src/reportgen.py
@@ -1,64 +1,58 @@
 import datetime
 
-from ds import YearResults
-from ds import MonthResults
-from ds import ChartResults
+from ds import YearResults, MonthResults, ChartResults
 
-def yearReport(yearresults):
-    max_t_date_split = str(yearresults.max_temp_date).split('-')
-    max_t_date_formatted = datetime.datetime(int(max_t_date_split[0]), int(max_t_date_split[1]), int(max_t_date_split[2]))
-    max_t_month = max_t_date_formatted.strftime("%B")
-    max_t_day = max_t_date_formatted.strftime("%d")
+def year_report(yearresults):
+    max_temp_date = datetime.datetime.strptime(yearresults.max_temp_date, "%Y-%m-%d")
+    max_temp_month = max_temp_date.strftime("%B")
+    max_temp_day = max_temp_date.strftime("%d")
     print("Highest:", end=" ")
     print(yearresults.max_temp, "C on", sep="", end=" ")
-    print(max_t_month, max_t_day)
+    print(max_temp_month, max_temp_day)
 
-    min_t_date_split = str(yearresults.min_temp_date).split('-')
-    min_t_date_formatted = datetime.datetime(int(min_t_date_split[0]), int(min_t_date_split[1]), int(min_t_date_split[2]))
-    min_t_month = min_t_date_formatted.strftime("%B")
-    min_t_day = min_t_date_formatted.strftime("%d")
+    min_temp_date = datetime.datetime.strptime(yearresults.min_temp_date, "%Y-%m-%d")
+    min_temp_month = min_temp_date.strftime("%B")
+    min_temp_day = min_temp_date.strftime("%d")
     print("Lowest:", end=" ")
     print(yearresults.min_temp, "C on", sep="", end=" ")
-    print(min_t_month, min_t_day)
+    print(min_temp_month, min_temp_day)
 
-    max_h_date_split = str(yearresults.max_humidity_date).split('-')
-    max_h_date_formatted = datetime.datetime(int(max_h_date_split[0]), int(max_h_date_split[1]), int(max_h_date_split[2]))
-    max_h_month = max_h_date_formatted.strftime("%B")
-    max_h_day = max_h_date_formatted.strftime("%d")
+    max_humid_date = datetime.datetime.strptime(yearresults.max_humidity_date, "%Y-%m-%d")
+    max_humid_month = max_humid_date.strftime("%B")
+    max_humid_day = max_humid_date.strftime("%d")
     print("Humidity:", end=" ")
     print(yearresults.max_humidity, "% on", sep="", end=" ")
-    print(max_h_month, max_h_day)
+    print(max_humid_month, max_humid_day)
 
-def monthReport(monthresults):
+def month_report(monthresults):
     print("Highest Average: ", monthresults.avg_high_temp, "C", sep="")
     print("Lowest Average: ", monthresults.avg_low_temp, "C", sep="")
     print("Average Mean Humidity: ", monthresults.avg_mean_humidity, "%", sep="")
     
 
-def chartReport(chartresults):
+def chart_report(chartresults):
     print(chartresults.month, chartresults.year)
-    for i in range(len(chartresults.high_temps)):
+    for i, high_temp in enumerate(chartresults.high_temps):
         split_date = str(chartresults.high_dates[i]).split('-')
         date = split_date[2]
         print('\x1b[3;35m', date, '\x1b[0m', sep="", end = " ")
-        for j in range(int(chartresults.high_temps[i])):
+        for j in range(int(high_temp)):
             print('\x1b[0;31m', "+", '\x1b[0m', sep="", end="")
-        print(" ", '\x1b[3;35m', chartresults.high_temps[i], "C", '\x1b[0m', sep="")
+        print(" ", '\x1b[3;35m', high_temp, "C", '\x1b[0m', sep="")
 
         print('\x1b[3;35m', date, '\x1b[0m', sep="", end = " ")
         for j in range(int(chartresults.low_temps[i])):
            print('\x1b[0;34m', "+", '\x1b[0m', sep="", end="")
         print(" ", '\x1b[3;35m', chartresults.low_temps[i], "C", '\x1b[0m', sep="")
 
-def chartBonusReport(chartresults):
+def chart_bonus_report(chartresults):
     print(chartresults.month, chartresults.year)
-    for i in range(len(chartresults.high_temps)):
+    for i, high_temp in enumerate(chartresults.high_temps):
         split_date = str(chartresults.high_dates[i]).split('-')
         date = split_date[2]
         print('\x1b[3;35m', date, '\x1b[0m', sep="", end = " ")
         for j in range(int(chartresults.low_temps[i])):
             print('\x1b[0;34m', "+", '\x1b[0m', sep="", end="")
-        for j in range(int(chartresults.high_temps[i])):
+        for j in range(int(high_temp)):
            print('\x1b[0;31m', "+", '\x1b[0m', sep="", end="")
-        print(" ", '\x1b[3;35m', chartresults.low_temps[i], "C - ", chartresults.high_temps[i], "C", '\x1b[0m', sep="")
-                
+        print(" ", '\x1b[3;35m', chartresults.low_temps[i], "C - ", high_temp, "C", '\x1b[0m', sep="")
