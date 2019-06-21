1a0435e8d6a5acb17e87a26f493b4b1d9a9f4bf8
Mohammad Owais Basit <owais.basit@arbisoft.com>
2019-06-21 16:28:45 +0500

    style: improved readability of the code
    Removed extra brackets and added blank line at the end of the file.

--- src/weatherman.py
+++ src/weatherman.py
@@ -5,16 +5,12 @@ import calc
 import terminalfunctions as tf
 import reportgen as rg
 
-from ds import YearReading
-from ds import MonthReading
-from ds import YearResults
-from ds import MonthResults
-from ds import ChartResults
+from ds import YearReading, MonthReading, YearResults, MonthResults, ChartResults
 
 arguments_list = list(sys.argv)
 arguments = iter(sys.argv)
 
-if (tf.validateArguments(arguments_list) == False):
+if tf.validateArguments(arguments_list) == False:
     tf.printUsage()
     sys.exit(1)
 
@@ -26,23 +22,23 @@ for i in range(report_count):
     print()
     flag = str(next(arguments))
     value = str(next(arguments))
-    if (flag == "-e"):
+    if flag == "-e":
         year = YearReading(arguments_list[1], value)
-        yresults = calc.yearlyCalc(year)
-        rg.yearReport(yresults)
+        yresults = calc.yearly_calc(year)
+        rg.year_report(yresults)
     else:
         ym_split = value.split('/')
         m = int(ym_split[1])
         month = calendar.month_abbr[m]
-        file_path = str(arguments_list[1]) + "/Murree_weather_" + ym_split[0] + "_" + month + ".txt"
+        file_path = f"{str(arguments_list[1])}/Murree_weather_{ym_split[0]}_{month}.txt"
         month = MonthReading(file_path)
-        if (flag == "-a"):
-            mresults = calc.monthlyCalc(month)
-            rg.monthReport(mresults)
-        if (flag == "-c"):
-            mcresults = calc.barChart(month)
-            rg.chartReport(mcresults)
-        if (flag == "-b"):
-            mcbresults = calc.barChart(month)
-            rg.chartBonusReport(mcbresults)
+        if flag == "-a":
+            mresults = calc.monthly_calc(month)
+            rg.month_report(mresults)
+        if flag == "-c":
+            mcresults = calc.bar_chart(month)
+            rg.chart_report(mcresults)
+        if flag == "-b":
+            mcbresults = calc.bar_chart(month)
+            rg.chart_bonus_report(mcbresults)
 print()
