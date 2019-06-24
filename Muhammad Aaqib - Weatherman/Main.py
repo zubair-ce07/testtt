0d39dd3ce5ef724a6cf0de961f67690e77cf6b0a
Muhammad Aaqib <aaqib.arbisoft.com>
2019-06-24 12:59:02 +0500

    refactor: requested changes made

--- Muhammad Aaqib - Weatherman/Main.py
+++ Muhammad Aaqib - Weatherman/Main.py
@@ -17,32 +17,36 @@ def main():
     date = args.date
 
     if args.yearly:
-        year_data = t.year_reading(data, date)
-        max_temp_data = year_data[0]
-        min_temp_data = year_data[1]
-        max_humidity_data = year_data[2]
-        max_temp = max_temp_data["data"]
-        max_temp_date = max_temp_data["date"]
-        min_temp = min_temp_data["data"]
+        year_data = t.get_year_reading(data, date)
+        max_temp_data = year_data.get("max temp")
+        min_temp_data = year_data.get("min temp")
+        max_humidity_data = year_data.get("max humidity")
+        max_temp = max_temp_data.get("value")
+        max_temp_date = max_temp_data.get("date")
+        min_temp = min_temp_data.get("value")
         min_temp_date = min_temp_data["date"]
-        max_humidity = max_humidity_data["data"]
-        max_humidity_date = max_humidity_data["date"]
-        resultPrint = t.PrintResult()
-        resultPrint.print_year_result(max_temp, max_temp_date, min_temp,
-                                      min_temp_date, max_humidity,
-                                      max_humidity_date)
+        max_humidity = max_humidity_data.get("value")
+        max_humidity_date = max_humidity_data.get("date")
+        result_print = t.ResultPrinter()
+        result_print.print_year_result(max_temp, max_temp_date, min_temp,
+                                       min_temp_date, max_humidity,
+                                       max_humidity_date)
 
     if args.monthly:
-        month_data = t.month_reading(data, date)
-        avg_max_temp = month_data[0]
-        avg_min_temp = month_data[1]
-        avg_mean_humidity = month_data[2]
-        resultPrint = t.PrintResult()
-        resultPrint.print_month_result(avg_max_temp, avg_min_temp,
-                                       avg_mean_humidity)
+        month_data = t.get_month_reading(data, date)
+        avg_max_temp = month_data.get("avg max temp")
+        avg_min_temp = month_data.get("avg min temp")
+        avg_mean_humidity = month_data.get("avg mean humidity")
+        result_print = t.ResultPrinter()
+        result_print.print_month_result(avg_max_temp, avg_min_temp,
+                                        avg_mean_humidity)
 
     if args.graphically:
-        t.month_graph(data, date)
+        chart_data = t.get_month_graph(data, date)
+        year_index = chart_data.get("year index")
+        month_index = chart_data.get("month index")
+        result_print = t.ResultPrinter()
+        result_print.plot_month_barchart(data, year_index, month_index)
 
 
 if __name__ == '__main__':
--- Muhammad Aaqib - Weatherman/Weather.py
+++ Muhammad Aaqib - Weatherman/Weather.py
@@ -12,7 +12,7 @@ class WeatherReading:
         self.mean_humidity = mean_humidity
 
 
-class PrintResult:
+class ResultPrinter:
     def print_year_result(self, max_temp, max_temp_date, min_temp,
                           min_temp_date, max_humidity, max_humidity_date):
         max_temp_date = str_to_date(max_temp_date)
@@ -36,6 +36,142 @@ class PrintResult:
         print(f"Lowest Average: {avg_min_temp}C")
         avg_mean_humidity = round(avg_mean_humidity, 2)
         print(f"Average Mean Humidity: {avg_mean_humidity}%")
+
+    def plot_month_barchart(self, data, year_index, month_index):
+        time = data[year_index][month_index][0].date
+        time = str_to_date(time)
+        month_name = time.strftime("%B")
+        year = time.strftime("%Y")
+        print(f"{month_name} {year}")
+        for i in range(0, len(data[year_index][month_index])):
+            if i + 1 < 10:
+                print(u"\u001b[35m0", end="")
+            print(u"\u001b[35m{}".format(i + 1), end=" ")
+            if data[year_index][month_index][i].max_temp != "":
+                for j in range(0, int(data[year_index][month_index][i]
+                               .max_temp)):
+                    print(u"\u001b[31m+", end="")
+            print(u"\u001b[35m {}C".format(data[year_index][month_index][i]
+                  .max_temp))
+            if i + 1 < 10:
+                print(u"\u001b[35m0", end="")
+            print(u"\u001b[35m{}".format(i + 1), end=" ")
+            if data[year_index][month_index][i].min_temp != "":
+                for j in range(0, int(data[year_index][month_index][i]
+                               .min_temp)):
+                    print(u"\u001b[36m+", end="")
+            print(u"\u001b[35m {}C".format(data[year_index][month_index][i]
+                                           .min_temp))
+            print()
+
+
+class WeatherAnalysis:
+    def find_year_index(self, data, year):
+        year_found = False
+        index = 0
+        for index in range(0, len(data[index])):
+            time = data[index][0][0].date
+            time = str_to_date(time)
+            split_year = time.strftime("%Y")
+            if int(split_year) == int(year):
+                year_found = True
+                break
+        if not year_found:
+            print("Record of this year does not exist in system")
+            return -1
+
+        return index
+
+    def find_month_index(self, data, year_index, month):
+        month_found = False
+        for month_index in range(0, len(data[year_index])):
+            time = data[year_index][month_index][0].date
+            time = str_to_date(time)
+            split_month = time.strftime("%m")
+            if int(split_month) == int(month):
+                month_found = True
+                break
+        if not month_found:
+            print("Record of this month does not exist in system")
+            return - 1
+
+        return month_index
+
+    def find_year_max_temp(self, data, index):
+        max_temp = data[index][0][0].max_temp
+        max_temp_date = data[index][0][0].date
+        for i in range(0, len(data[index])):
+            for j in range(0, len(data[index][i])):
+                if not data[index][i][j].max_temp:
+                    continue
+                if int(data[index][i][j].max_temp) > int(max_temp):
+                    max_temp = data[index][i][j].max_temp
+                    max_temp_date = data[index][i][j].date
+        year_data = {"value": max_temp,
+                     "date": max_temp_date}
+
+        return year_data
+
+    def find_year_min_temp(self, data, index):
+        min_temp = data[index][0][0].min_temp
+        min_temp_date = data[index][0][0].date
+        for i in range(0, len(data[index])):
+            for j in range(0, len(data[index][i])):
+                if not data[index][i][j].min_temp:
+                    continue
+                if int(data[index][i][j].min_temp) < int(min_temp):
+                    min_temp = data[index][i][j].min_temp
+                    min_temp_date = data[index][i][j].date
+        year_data = {"value": min_temp,
+                     "date": min_temp_date}
+
+        return year_data
+
+    def find_year_max_humidity(self, data, index):
+        max_humidity = data[index][0][0].max_humidity
+        max_humidity_date = data[index][0][0].date
+        for i in range(0, len(data[index])):
+            for j in range(0, len(data[index][i])):
+                if not data[index][i][j].max_humidity:
+                    continue
+                if int(data[index][i][j].max_humidity) > int(max_humidity):
+                    max_humidity = data[index][i][j].max_humidity
+                    max_humidity_date = data[index][i][j].date
+        year_data = {"value": max_humidity,
+                     "date": max_humidity_date}
+
+        return year_data
+
+    def find_month_avg_max_temp(self, data, year_index, month_index):
+        avg_max_temp = 0
+        for i in range(0, len(data[year_index][month_index])):
+            if not data[year_index][month_index][i].max_temp:
+                    continue
+            avg_max_temp += int(data[year_index][month_index][i].max_temp)
+        avg_max_temp = avg_max_temp / len(data[year_index][month_index])
+
+        return avg_max_temp
+
+    def find_month_avg_min_temp(self, data, year_index, month_index):
+        avg_min_temp = 0
+        for i in range(0, len(data[year_index][month_index])):
+            if not data[year_index][month_index][i].min_temp:
+                    continue
+            avg_min_temp += int(data[year_index][month_index][i].min_temp)
+        avg_min_temp = avg_min_temp / len(data[year_index][month_index])
+
+        return avg_min_temp
+
+    def find_month_avg_mean_humidity(self, data, year_index, month_index):
+        avg_mean_humidity = 0
+        for i in range(0, len(data[year_index][month_index])):
+            if not data[year_index][month_index][i].mean_humidity:
+                    continue
+            avg_mean_humidity += int(data[year_index][month_index][i]
+                                     .mean_humidity)
+        avg_mean_humidity /= len(data[year_index][month_index])
+
+        return avg_mean_humidity
 
 
 def str_to_date(date):
@@ -62,14 +198,14 @@ def read_file_names(path):
     return file_list
 
 
-def read_row_data(row):
+def read_tuple_data(row):
     max_temp = row['Max TemperatureC']
     min_temp = row['Min TemperatureC']
     max_humidity = row['Max Humidity']
     mean_humidity = row[' Mean Humidity']
-    try:
+    if "PKT" in row:
         date = (row["PKT"])
-    except:
+    else:
         date = (row["PKST"])
     row_data = WeatherReading(date, max_temp, min_temp,
                               max_humidity, mean_humidity)
@@ -94,7 +230,7 @@ def parse_file():
         with open(files, newline='') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                row_data = read_row_data(row)
+                row_data = read_tuple_data(row)
                 day_data.append(row_data)
         month_data.append(day_data)
 
@@ -102,175 +238,54 @@ def parse_file():
     return year_data
 
 
-def find_year_index(data, year):
-    year_found = False
-    index = 0
-    for index in range(0, len(data[index])):
-        time = data[index][0][0].date
-        time = str_to_date(time)
-        split_year = time.strftime("%Y")
-        if int(split_year) == int(year):
-            year_found = True
-            break
-    if not year_found:
-        print("Record of this year does not exist in system")
-        return -1
-
-    return index
-
-
-def find_month_index(data, year_index, month):
-    month_found = False
-    for month_index in range(0, len(data[year_index])):
-        time = data[year_index][month_index][0].date
-        time = str_to_date(time)
-        split_month = time.strftime("%m")
-        if int(split_month) == int(month):
-            month_found = True
-            break
-    if not month_found:
-        print("Record of this month does not exist in system")
-        return - 1
-
-    return month_index
-
-
-def find_year_max_temp(data, index):
-    max_temp = data[index][0][0].max_temp
-    max_temp_date = data[index][0][0].date
-    for i in range(0, len(data[index])):
-        for j in range(0, len(data[index][i])):
-            if not data[index][i][j].max_temp:
-                continue
-            if int(data[index][i][j].max_temp) > int(max_temp):
-                max_temp = data[index][i][j].max_temp
-                max_temp_date = data[index][i][j].date
-    year_data = {"data": max_temp,
-                 "date": max_temp_date}
-    return year_data
-
-
-def find_year_min_temp(data, index):
-    min_temp = data[index][0][0].min_temp
-    min_temp_date = data[index][0][0].date
-    for i in range(0, len(data[index])):
-        for j in range(0, len(data[index][i])):
-            if not data[index][i][j].min_temp:
-                continue
-            if int(data[index][i][j].min_temp) < int(min_temp):
-                min_temp = data[index][i][j].min_temp
-                min_temp_date = data[index][i][j].date
-    year_data = {"data": min_temp,
-                 "date": min_temp_date}
-    return year_data
-
-
-def find_year_max_humidity(data, index):
-    max_humidity = data[index][0][0].max_humidity
-    max_humidity_date = data[index][0][0].date
-    for i in range(0, len(data[index])):
-        for j in range(0, len(data[index][i])):
-            if not data[index][i][j].max_humidity:
-                continue
-            if int(data[index][i][j].max_humidity) > int(max_humidity):
-                max_humidity = data[index][i][j].max_humidity
-                max_humidity_date = data[index][i][j].date
-    year_data = {"data": max_humidity,
-                 "date": max_humidity_date}
-    return year_data
-
-
-def find_month_avg_max_temp(data, year_index, month_index):
-    avg_max_temp = 0
-    for i in range(0, len(data[year_index][month_index])):
-        if not data[year_index][month_index][i].max_temp:
-                continue
-        avg_max_temp += int(data[year_index][month_index][i].max_temp)
-    avg_max_temp = avg_max_temp / len(data[year_index][month_index])
-    return avg_max_temp
-
-
-def find_month_avg_min_temp(data, year_index, month_index):
-    avg_min_temp = 0
-    for i in range(0, len(data[year_index][month_index])):
-        if not data[year_index][month_index][i].min_temp:
-                continue
-        avg_min_temp += int(data[year_index][month_index][i].min_temp)
-    avg_min_temp = avg_min_temp / len(data[year_index][month_index])
-    return avg_min_temp
-
-
-def find_month_avg_mean_humidity(data, year_index, month_index):
-    avg_mean_humidity = 0
-    for i in range(0, len(data[year_index][month_index])):
-        if not data[year_index][month_index][i].mean_humidity:
-                continue
-        avg_mean_humidity += int(data[year_index][month_index][i]
-                                 .mean_humidity)
-    avg_mean_humidity = avg_mean_humidity / len(data[year_index][month_index])
-    return avg_mean_humidity
-
-
-def plot_month_barchart(data, year_index, month_index):
-    time = data[year_index][month_index][0].date
-    time = str_to_date(time)
-    month_name = time.strftime("%B")
-    year = time.strftime("%Y")
-    print(f"{month_name} {year}")
-    for i in range(0, len(data[year_index][month_index])):
-        if i + 1 < 10:
-            print(u"\u001b[35m0", end="")
-        print(u"\u001b[35m" + str(i + 1), end=" ")
-        if data[year_index][month_index][i].max_temp != "":
-            for j in range(0, int(data[year_index][month_index][i]
-                                  .max_temp)):
-                print(u"\u001b[31m+", end="")
-        print(u"\u001b[35m" + " " + data[year_index][month_index][i]
-              .max_temp + "C")
-        if i + 1 < 10:
-            print(u"\u001b[35m0", end="")
-        print(u"\u001b[35m" + str(i + 1), end=" ")
-        if data[year_index][month_index][i].min_temp != "":
-            for j in range(0, int(data[year_index][month_index][i]
-                           .min_temp)):
-                print(u"\u001b[36m+", end="")
-        print(u"\u001b[35m" + " " + f"""{data[year_index][month_index][i]
-              .min_temp}C""")
-        print()
-
-
-def year_reading(data, year):
-    index = find_year_index(data, year)
+def get_year_reading(data, year):
+    year_analysis = WeatherAnalysis()
+    index = year_analysis.find_year_index(data, year)
     if(index == -1):
         return
-    max_temp_data = find_year_max_temp(data, index)
-    min_temp_data = find_year_min_temp(data, index)
-    max_humidity_data = find_year_max_humidity(data, index)
-    year_data = [max_temp_data, min_temp_data, max_humidity_data]
+    max_temp_data = year_analysis.find_year_max_temp(data, index)
+    min_temp_data = year_analysis.find_year_min_temp(data, index)
+    max_humidity_data = year_analysis.find_year_max_humidity(data, index)
+    year_data = {"max temp": max_temp_data,
+                 "min temp": min_temp_data,
+                 "max humidity": max_humidity_data}
+
     return year_data
 
 
-def month_reading(data, month):
+def get_month_reading(data, month):
+    month_analysis = WeatherAnalysis()
     year, month = month.split("/")
-    year_index = find_year_index(data, year)
+    year_index = month_analysis.find_year_index(data, year)
     if(year_index == -1):
         return
-    month_index = find_month_index(data, year_index, month)
+    month_index = month_analysis.find_month_index(data, year_index, month)
     if(month_index == -1):
         return
-    avg_max_temp = find_month_avg_max_temp(data, year_index, month_index)
-    avg_min_temp = find_month_avg_min_temp(data, year_index, month_index)
-    avg_mean_hum = find_month_avg_mean_humidity(data, year_index, month_index)
-    month_data = [avg_max_temp, avg_min_temp, avg_mean_hum]
+
+    avg_max_temp = month_analysis.find_month_avg_max_temp(
+                                  data, year_index, month_index)
+    avg_min_temp = month_analysis.find_month_avg_min_temp(
+                                  data, year_index, month_index)
+    avg_mean_hum = month_analysis.find_month_avg_mean_humidity(
+                                  data, year_index, month_index)
+    month_data = {"avg max temp": avg_max_temp,
+                  "avg min temp": avg_min_temp,
+                  "avg mean humidity": avg_mean_hum}
+
     return month_data
 
 
-def month_graph(data, month):
+def get_month_graph(data, month):
+    month_analysis = WeatherAnalysis()
     year, month = month.split("/")
-    year_index = find_year_index(data, year)
+    year_index = month_analysis.find_year_index(data, year)
     if(year_index == -1):
         return
-    month_index = find_month_index(data, year_index, month)
+    month_index = month_analysis.find_month_index(data, year_index, month)
     if(month_index == -1):
         return
-    plot_month_barchart(data, year_index, month_index)
+    chart_data = {"year index": year_index,
+                  "month index": month_index}
+
+    return chart_data
