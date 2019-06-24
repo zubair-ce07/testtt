68716886e0374c5d1e0cfceff3a1d9668b8911f8
yusra <yusra.khalid@outlook.com>
2019-06-24 15:01:04 +0500

    Restructuring 2

--- ResultStorage.py
+++
@@ -1,9 +0,0 @@
-class ResultStorage:
-
-    def __init__(self,highestTemp,lowestTemp,humidity,highestTempDay,lowestTempDay,humidityDay):
-        self.highestTemp = highestTemp
-        self.lowestTemp = lowestTemp
-        self.humidity = humidity
-        self.highestTempDay = highestTempDay
-        self.lowestTempDay = lowestTempDay
-        self.humidityDay = humidityDay
