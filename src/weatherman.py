00149259bb51a644a027f6a02ccd5df85d681b6e
Mohammad Owais Basit <owais.basit@arbisoft.com>
2019-06-25 16:03:47 +0500

    fix: deleted extra file
    moved function from terminalfunctions.py to weatherman.py along with imports

--- src/weatherman.py
+++ src/weatherman.py
@@ -1,12 +1,33 @@
 import os
+import re
 import argparse
 import calendar
 
 import calc
-import terminalfunctions as tf
 import reportgen as rg
 
 from ds import YearReading, MonthReading, YearResults, MonthResults, ChartResults
+
+def validate_arguments(args):
+    if not os.path.exists(args.dir):
+        print("Invalid path")
+        return False
+    if args.year_month:
+        for year_month in args.year_month:
+            if not re.match("\\d{4}/\\d{2}", year_month) and not re.match("\\d{4}/\\d{1}", year_month):
+                print("Wrong format!")
+                return False
+    if args.chart:
+        for chart in args.chart:
+            if not re.match("\\d{4}/\\d{2}", chart) and not re.match("\\d{4}/\\d{1}", chart):
+                print("Wrong format!")
+                return False
+    if args.bonus:
+        for bonus in args.bonus:
+            if not re.match("\\d{4}/\\d{2}", bonus) and not re.match("\\d{4}/\\d{1}", bonus):
+                print("Wrong format!")
+                return False
+    return True
 
 parser = argparse.ArgumentParser("Display weather reports")
 
@@ -18,7 +39,7 @@ parser.add_argument('-b', nargs = '*', action = 'store', dest = 'bonus', type = 
 
 args = parser.parse_args()
 
-if tf.validate_arguments(args) == True:
+if validate_arguments(args) == True:
     if args.year:
         for year in args.year:
             print()
