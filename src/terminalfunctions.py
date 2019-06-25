f0d9f8f87bbddd492efc0a1432ea55c4d806ed15
Mohammad Owais Basit <owais.basit@arbisoft.com>
2019-06-25 15:45:31 +0500

    fix: rewrote function bodies
    adjusted validation functions in accordance with argparse, removed extra ones

--- src/terminalfunctions.py
+++ src/terminalfunctions.py
@@ -2,27 +2,22 @@ import re
 import os
 
 def validate_arguments(args):
-    size = len(args)
-    if size % 2 != 0:
-        print("Wrong usage!")
-        return False
-    if size < 4:
-        print("Not enough arguments")
-        return False
-    if not os.path.exists(args[1]):
+    if not os.path.exists(args.dir):
         print("Invalid path")
         return False
-    for i in range(2, size - 1, 2):
-        args[i]= str(args[i])
-        if args[i] != '-e' and args[i] != '-a' and args[i] != '-c' and args[i] != '-b':
-            print(f"Invalid flag entered: {args[i]}")
-            return False
-        value_passed = args[i+1]
-        if args[i] == "-a" or args[i] == "-c" or args[i] == "-b":
-            if not re.match("\\d{4}/\\d{2}", str(value_passed)) and not re.match("\\d{4}/\\d{1}", str(value_passed)):
-                print("Wrong usage!")
+    if args.year_month:
+        for year_month in args.year_month:
+            if not re.match("\\d{4}/\\d{2}", year_month) and not re.match("\\d{4}/\\d{1}", year_month):
+                print("Wrong format!")
+                return False
+    if args.chart:
+        for chart in args.chart:
+            if not re.match("\\d{4}/\\d{2}", chart) and not re.match("\\d{4}/\\d{1}", chart):
+                print("Wrong format!")
+                return False
+    if args.bonus:
+        for bonus in args.bonus:
+            if not re.match("\\d{4}/\\d{2}", bonus) and not re.match("\\d{4}/\\d{1}", bonus):
+                print("Wrong format!")
                 return False
     return True
-
-def print_usage():
-    print("Usage: weatherman.py path/to/files-dir [-e YYYY] [-a YYYY/MM] [-c YYYY/MM] [-b YYYY/MM]")
