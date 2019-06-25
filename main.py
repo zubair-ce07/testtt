2bb6150957db58981852297debd7e89f70520815
husnain950 <husnain95@gmail.com>
2019-06-25 15:01:10 +0500

    Refactor: apply proper naming convention docs: pep8 applied to comments main is called directly

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from Handler import *
 
 def main():
     if not validate_arg(sys.argv):
-        print("Error: few Arg", str(sys.argv))
+        print(f"Error: few Arg{str(sys.argv)}\nProper format is /path [-e] year [-a] year/month [-c] year/month")
         exit(0)
     else:
         i = 2  # skipping First 2 arg as they are file name and path
@@ -15,12 +15,12 @@ def main():
             j = 0  # Variable to iterate over ind
             while j < len(ind):
                 token = sys.argv[ind[j] + 1].split('/')
-                pth =\
+                path =\
                     sys.argv[1] + '/weatherfiles/Murree_weather_' + token[0] +\
                     '_' + calendar.month_abbr[int(token[1])] + '.txt'  # append calender.month_abbr to path
                 i = i + 2
                 j = j + 1
-                print(calculate_monthly_report(read_file(pth)))
+                print(calculate_monthly_report(read_file(path)))
                 print()
             # get ind[] of the total indexes encounter for -e flag in the command line argument
             ind = [i for i, x in enumerate(sys.argv) if x == '-e']
@@ -29,10 +29,10 @@ def main():
                 lst = list()
                 month = 1
                 while month <= 12:
-                    pth = \
+                    path = \
                         sys.argv[1] + '/weatherfiles/Murree_weather_' + sys.argv[ind[j] + 1] + \
                         '_' + calendar.month_abbr[month] + '.txt'
-                    lst = lst + read_file(pth)
+                    lst = lst + read_file(path)
                     month = month + 1
                 print(calculate_yearly_report(lst))
                 i = i + 2
@@ -42,14 +42,14 @@ def main():
             j = 0
             while j < len(ind):
                 token = sys.argv[ind[j] + 1].split('/')
-                pth =\
+                path =\
                     sys.argv[1] + '/weatherfiles/Murree_weather_' + token[0] +\
                     '_' + calendar.month_abbr[int(token[1])] + '.txt'
                 i = i + 2
                 j = j + 1
-                print(calculate_chart_report(read_file(pth)))
+                print(calculate_chart_report(read_file(path)))
+
                 print()
 
 
-if __name__ == '__main__':
-    main()
+main()
