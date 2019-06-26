6e35fa00e2c03eca19b6b416f93a766f1a6093a6
husnain950 <husnain95@gmail.com>
2019-06-26 11:59:02 +0500

    fix: clean and optimized __str__

--- ChartResult.py
+++ ChartResult.py
@@ -9,27 +9,23 @@ class ChartResult:
     def __init__(self, high, low):
         self.highest = {"highest": high.highest,
                         "lowest": high.lowest,
-                        "date": high.date}
+                        "date": datetime.strptime(high.date, "%Y-%m-%d").date()}
         self.lowest = {"highest": low.highest,
                        "lowest": low.lowest,
-                       "date": low.date}
+                       "date": datetime.strptime(low.date, "%Y-%m-%d").date()}
+
+    def print_chart(self, count, color_code, string):
+        return '\033[00m' + color_code + string * count + '\033[00m'
 
     def __str__(self):
         return \
-            calendar.month_name[datetime.strptime(self.highest["date"], "%Y-%m-%d").month] + ' ' +\
-            str(datetime.strptime(self.highest["date"], "%Y-%m-%d").year) + '\n' + "\033[95m" +\
-            str(datetime.strptime(self.highest["date"], "%Y-%m-%d").day) +\
-            ' \033[00m' +\
-            '\033[94m+' * self.highest["lowest"] +\
-            '\033[91m+\033[00m' * self.highest["highest"] +\
-            "\033[95m " +\
-            str(self.highest["lowest"]) + 'C - ' + str(self.highest["highest"]) + 'C' +\
-            "\033[00m" +\
-            "\033[95m" + '\n' +\
-            str(datetime.strptime(self.lowest["date"], "%Y-%m-%d").day) +\
-            " \033[00m" +\
-            '\033[94m+' * self.lowest["lowest"] +\
-            '\033[91m+\033[00m' * self.lowest["highest"] +\
-            "\033[95m " +\
-            str(self.lowest["lowest"]) + 'C - ' + str(self.lowest["highest"]) + 'C' +\
-            "\033[00m"
+            self.highest["date"].strftime("%B") + ' ' + \
+            str(self.highest["date"].strftime("%Y")) + '\n' + "\033[95m" +\
+            self.print_chart(1, "\033[95m", str(self.highest["date"].day) + ' ') + \
+            self.print_chart(self.highest["lowest"], '\033[94m', '+') + \
+            self.print_chart(self.highest["highest"], '\033[91m', '+') + \
+            self.print_chart(1, "\033[95m ", str(self.highest["lowest"]) + 'C - ' + str(self.highest["highest"]) + 'C') +\
+            self.print_chart(1,  "\033[95m", '\n' + str(self.lowest["date"].day) + ' ') +\
+            self.print_chart(self.lowest["lowest"], '\033[94m', '+') + \
+            self.print_chart(self.lowest["highest"], '\033[91m', '+') + \
+            self.print_chart(1, "\033[95m ", str(self.lowest["lowest"]) + 'C - ' + str(self.lowest["highest"]) + 'C')
