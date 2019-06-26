560b5aace2faaaec9f60ec4fbae8982ad8ae3920
Hassnain abbas <hassnain_abbas95@hotmail.com>
2019-06-26 22:24:25 +0500

    fix: typo flat

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ def main():
         while index < len(sys.argv):
             # get flag_indexes[] of the total indexes encounter for -a flag in the command line argument
             flag_indexes = [i for i, x in enumerate(sys.argv) if x == '-a']
-            flat_iter = 0  # Variable to iterate over flag_indexes
-            while flat_iter < len(flag_indexes):
-                tokenize_date = sys.argv[flag_indexes[flat_iter] + 1].split('/')
+            flag_iter = 0  # Variable to iterate over flag_indexes
+            while flag_iter < len(flag_indexes):
+                tokenize_date = sys.argv[flag_indexes[flag_iter] + 1].split('/')
                 if int(tokenize_date[0]) not in range(2004, 2017):
                     logging.error('Year must between 2004 - 2016')
                     exit(0)
@@ -32,32 +32,32 @@ def main():
                     sys.argv[1] + '/weatherfiles/Murree_weather_' + tokenize_date[0] +\
                     '_' + calendar.month_abbr[int(tokenize_date[1])] + '.txt'  # append calender.month_abbr to path
                 index += 2
-                flat_iter += 1
+                flag_iter += 1
                 print(ReportGenerator.calculate_monthly_report(Parser.read_file(path)))
                 print()
             # get flag_indexes[] of the total indexes encounter for -e flag in the command line argument
             flag_indexes = [i for i, x in enumerate(sys.argv) if x == '-e']
-            flat_iter = 0
-            while flat_iter < len(flag_indexes):
-                if int(sys.argv[flag_indexes[flat_iter] + 1]) not in range(2004, 2017):
+            flag_iter = 0
+            while flag_iter < len(flag_indexes):
+                if int(sys.argv[flag_indexes[flag_iter] + 1]) not in range(2004, 2017):
                     logging.error('Year must between 2004 - 2016')
                     exit(0)
                 reading_list = list()
                 month = 1
                 while month <= 12:
                     path = \
-                        sys.argv[1] + '/weatherfiles/Murree_weather_' + sys.argv[flag_indexes[flat_iter] + 1] + \
+                        sys.argv[1] + '/weatherfiles/Murree_weather_' + sys.argv[flag_indexes[flag_iter] + 1] + \
                         '_' + calendar.month_abbr[month] + '.txt'
                     reading_list = reading_list + Parser.read_file(path)
                     month += 1
                 print(ReportGenerator.calculate_yearly_report(reading_list))
                 index += 2
-                flat_iter += 1
+                flag_iter += 1
             # get flag_indexes[] of the total indexes encounter for -c flag in the command line argument
             flag_indexes = [i for i, x in enumerate(sys.argv) if x == '-c']
-            flat_iter = 0
-            while flat_iter < len(flag_indexes):
-                tokenize_date = sys.argv[flag_indexes[flat_iter] + 1].split('/')
+            flag_iter = 0
+            while flag_iter < len(flag_indexes):
+                tokenize_date = sys.argv[flag_indexes[flag_iter] + 1].split('/')
                 if int(tokenize_date[0]) not in range(2004, 2017):
                     logging.error('Year must between 2004 - 2016')
                     exit(0)
@@ -68,7 +68,7 @@ def main():
                     sys.argv[1] + '/weatherfiles/Murree_weather_' + tokenize_date[0] +\
                     '_' + calendar.month_abbr[int(tokenize_date[1])] + '.txt'
                 index += 2
-                flat_iter += 1
+                flag_iter += 1
                 print(ReportGenerator.calculate_chart_report(Parser.read_file(path)))
                 print()
 
