197d6d7fae3d1a546996abf94208beb3766e5ab8
husnain950 <husnain95@gmail.com>
2019-06-26 17:05:50 +0500

    fix: None case handled

--- WeatherReading.py
+++ WeatherReading.py
@@ -7,7 +7,7 @@ This class hold weather obj with these fields required for Task1
 class WeatherReading:
     def __init__(self, date, highest, lowest, humidity, mean_humidity):
         self.date = date
-        self.highest = highest
-        self.lowest = lowest
-        self.max_humidity = humidity
-        self.mean_humidity = mean_humidity
+        self.highest = 0 if not highest else int(highest)
+        self.lowest = 0 if not lowest else int(lowest)
+        self.max_humidity = 0 if not humidity else int(humidity)
+        self.mean_humidity = 0 if not mean_humidity else int(mean_humidity)
