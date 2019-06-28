c72cb85bcd4423836e8d6e42c992ff6eda0f030e
husnain950 <husnain95@gmail.com>
2019-06-28 14:30:48 +0500

    Refactor: Conditional statement

--- WeatherReading.py
+++ WeatherReading.py
@@ -7,7 +7,7 @@ This class hold weather obj with these fields required for Task1
 class WeatherReading:
     def __init__(self, date, highest, lowest, humidity, mean_humidity):
         self.date = date
-        self.highest = 0 if not highest else int(highest)
-        self.lowest = 0 if not lowest else int(lowest)
-        self.max_humidity = 0 if not humidity else int(humidity)
-        self.mean_humidity = 0 if not mean_humidity else int(mean_humidity)
+        self.highest = int(highest) if highest else 0
+        self.lowest = int(lowest) if lowest else 0
+        self.max_humidity = int(humidity) if humidity else 0
+        self.mean_humidity = int(mean_humidity) if mean_humidity else 0
