a25fa3cf343f93a4d8cc9d1ea077bb9b42e3dcd8
Ikram ullah Khan <ikram@A002-00048.arbisoft.com>
2019-06-28 18:47:05 +0500

    file contain ullapopken spider and items. spider crawls the ullapopken  and extract items url, name, retailer_sku, brand, category, care gender, image_urls, description and skus

---
+++ ullapopken_spider.py
@@ -0,0 +1,192 @@
+import json
+
+import scrapy
+from scrapy.item import Item
+from scrapy.linkextractors import LinkExtractor
+from scrapy.selector import SelectorList
+from scrapy.spiders import CrawlSpider, Rule, Request
+
+
+class Product(Item):
+    retailer_sku = scrapy.Field()
+    name = scrapy.Field()
+    brand = scrapy.Field()
+    url = scrapy.Field()
+    category = scrapy.Field()
+    care = scrapy.Field()
+    gender = scrapy.Field()
+    image_urls = scrapy.Field()
+    description = scrapy.Field()
+    skus = scrapy.Field()
+    requests_queue = scrapy.Field()
+
+
+class UllaPoPkenSpider(CrawlSpider):
+    name = 'ullapopken'
+    allowed_domains = ['ullapopken.com']
+    start_urls = ['https://www.ullapopken.com/']
+    default_brand = 'Ullapopken'
+
+    default_gender = 'unisex'
+
+    gender_terms = [
+        'women',
+        'men',
+    ]
+
+    category_url_t = 'https://www.ullapopken.com/api/res/category/articles/language/en/' \
+                     'size/60/page/1/category/{}/grouping/{}/filter/_/sort/normal/fs/_'
+    all_items_url_t = 'https://www.ullapopken.com/api/res/category/articles/language/en/' \
+                      'size/{}/page/1/category/{}/grouping/{}/filter/_/sort/normal/fs/_'
+
+    variant_url_t = 'https://www.ullapopken.com/api/res/model/{}/variants'
+    sku_url_t = 'https://www.ullapopken.com/api/res/article/{}'
+
+    category_css = '.first-row'
+    rules = (
+        Rule(LinkExtractor(restrict_css=category_css), callback='parse_category_parameters'),
+    )
+
+    def parse_category_parameters(self, response):
+        category = response.css('#paging::attr(data-category)').get()
+        grouping = response.css('#paging::attr(data-grouping)').get()
+        pagination_url = self.category_url_t.format(category, grouping)
+
+        pagination_request = Request(url=pagination_url, callback=self.parse_pagination)
+        pagination_request.meta['category'] = category
+        pagination_request.meta['grouping'] = grouping
+        pagination_request.meta['item_category'] = self.extract_category(response)
+
+        yield pagination_request
+
+    def parse_pagination(self, response):
+        category = response.meta['category']
+        grouping = response.meta['grouping']
+        response_json = json.loads(response.text)
+
+        total_items = response_json['pagination']['totalNumberOfResults']
+        category_url = self.all_items_url_t.format(total_items, category, grouping)
+
+        pagination_request = Request(url=category_url, callback=self.parse_category)
+        pagination_request.meta['category'] = response.meta['item_category']
+
+        yield pagination_request
+
+    def parse_category(self, response):
+        response_json = json.loads(response.text)
+
+        for data in response_json['results']:
+            item_code = data['masterArticlenumber']
+            item_request = Request(self.variant_url_t.format(item_code), callback=self.parse_item)
+
+            item_request.meta['category'] = response.meta['category']
+            item_request.meta['url'] = data['url']
+            item_request.meta['retailer_sku'] = item_code
+
+            yield item_request
+
+    def parse_item(self, response):
+        item = Product()
+        item['url'] = response.meta['url']
+        item['retailer_sku'] = response.meta['retailer_sku']
+        item['category'] = response.meta['category']
+        item['brand'] = self.extract_brand_name(response)
+        item['gender'] = self.extract_gender(response.meta['category'])
+        item['requests_queue'] = self.construct_item_requests(response)
+        item['skus'] = []
+        item['image_urls'] = []
+
+        return self.get_item_or_parse_request(item)
+
+    def parse_color_dependent_values(self, response):
+        item = response.meta['item']
+        raw_item = json.loads(response.text)
+        item['name'] = self.extract_item_name(raw_item)
+        item['care'] = self.extract_care(raw_item)
+        item['description'] = self.extract_description(raw_item)
+        item['image_urls'] += self.extract_image_urls(raw_item)
+        item['skus'] += self.extract_skus(raw_item)
+
+        return self.get_item_or_parse_request(item)
+
+    def extract_skus(self, raw_item):
+        skus = []
+        common_sku = {
+            'colour': raw_item['colorLocalized']
+        }
+
+        for item in raw_item['skuData']:
+            sku = common_sku.copy()
+            sku['size'] = item['displaySize']
+            sku['sku_id'] = item['skuID']
+            sku.update(self.extract_pricing(item))
+
+            skus.append(sku)
+
+        return skus
+
+    def construct_item_requests(self, response):
+        response_json = json.loads(response.text)
+        color_codes = [data['articleCode'] for data in response_json]
+
+        return [Request(url=self.sku_url_t.format(c), callback=self.parse_color_dependent_values)
+                for c in color_codes]
+
+    def get_item_or_parse_request(self, item):
+        if not item['requests_queue']:
+            del item['requests_queue']
+            return item
+
+        request = item['requests_queue'].pop()
+        request.meta['item'] = item
+
+        return request
+
+    def extract_pricing(self, raw_price):
+        pricing = dict()
+        price = raw_price['reducedPrice'] or raw_price['originalPrice']
+        pricing['currency'] = price['currencyIso']
+        pricing['previous_prices'] = [raw_price['crossedOutPrice']['value']] \
+            if raw_price['crossedOutPrice'] else []
+
+        pricing['price'] = price['value']
+
+        return pricing
+
+    def extract_item_name(self, raw_item):
+        return raw_item['name']
+
+    def extract_brand_name(self, response):
+        return self.default_brand
+
+    def extract_gender(self, raw_genders):
+        for gender in raw_genders:
+            if gender.lower() in self.gender_terms:
+                return gender.lower()
+
+        return self.default_gender
+
+    def extract_category(self, response):
+        return clean(response.css('.active > .nav_content ::text'))
+
+    def extract_image_urls(self, raw_item):
+        return [image_data['url'] for image_data in raw_item['imageDataList']]
+
+    def extract_care(self, raw_item):
+        item_code = raw_item['code']
+        raw_care = raw_item['careInstructions'][item_code]
+
+        return [care['description'] for care in raw_care]
+
+    def extract_description(self, raw_item):
+        item_code = raw_item['code']
+        return clean(raw_item['description'][item_code])
+
+
+def clean(raw_item):
+    if isinstance(raw_item, str):
+        return raw_item.strip()
+    elif isinstance(raw_item, SelectorList):
+        return [r.strip() for r in raw_item.getall() if r.strip()]
+
+    return [r.strip() for r in raw_item if r.strip()]
