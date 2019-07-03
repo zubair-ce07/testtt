1820502e364e7acb46430cb712d944ff3c9aa4a2
ahmadarshi <m.ahmad@arbisoft.com>
2019-07-03 13:30:14 +0500

    used selectors optimally, made a seprate method for sku and removed unnecessary methods

--- task3.py
+++ task3.py
@@ -1,8 +1,9 @@
 import re
+
 import scrapy
 
 
-class ProductDetails(scrapy.Item):
+class AsicsItem(scrapy.Item):
     retailer_sku = scrapy.Field()
     gender = scrapy.Field()
     name = scrapy.Field()
@@ -27,108 +28,97 @@ class AsicsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        home_page = self.get_home_page_data(response)
 
-        for major_category in home_page:
+        for major_category in response.css('.childlink-wrapper > ::attr(href)').getall():
             category_url = response.urljoin(major_category)
+            yield scrapy.Request(url=category_url, callback=self.extract_page_products)
 
-            yield scrapy.Request(url=category_url, callback=self.extract_products)
+    def extract_page_products(self, response):
 
-    def extract_products(self, response):
-        page_products = self.get_page_products(response)
-
-        for product in page_products:
+        for product in response.css('.prod-wrap  > ::attr(href)').getall():
             product_url = response.urljoin(product)
             yield scrapy.Request(url=product_url, callback=self.extract_product_details)
-        next_page = self.get_next_page_url(response)
-        print(next_page)
+
+        next_page = self.next_page_url(response)
         if next_page:
             next_page_url = response.urljoin(next_page)
-            yield scrapy.Request(url=next_page_url, callback=self.extract_products)
+            yield scrapy.Request(url=next_page_url, callback=self.extract_page_products)
 
     def extract_product_details(self, response):
 
-        item = ProductDetails()
-        item['retailer_sku'] = self.get_product_sku(response)
-        item['name'] = self.get_name(response)
-        item['gender'] = self.get_gender(response)
-        item['category'] = self.get_category(response)
-        item['url'] = self.get_product_url(response)
-        item['description'] = self.get_description(response)
-        item['image_urls'] = self.get_image_urls(response)
-        currency = self.get_currency(response)
-        price = self.get_price(response)
+        item = AsicsItem()
+        item['retailer_sku'] = self.product_sku(response)
+        item['name'] = self._name(response)
+        item['gender'] = self.gender(response)
+        item['category'] = self.category(response)
+        item['url'] = self.product_url(response)
+        item['description'] = self.description(response)
+        item['image_urls'] = self.image_urls(response)
+        currency = self.currency(response)
+        price = self.price(response)
         color = self.get_color(response)
 
         item['care'] = []
         item['brand'] = "ASICS"
+        item['skus'] = self.sku_record(response, color, currency, price)
+        yield item
 
+    def next_page_url(self, response):
+        return response.css('rightArrow ::attr(href)').get()
+
+    def size(self, response):
+        return response.css('.SizeOption::text').getall()
+
+    def _name(self, response):
+        return response.css('title::text').get().split('|')[0].strip()
+
+    def gender(self, response):
+        return response.css('title::text').get().split('|')[1].strip()
+
+    def category(self, response):
+        return response.css('.breadcrumb > ::text ').getall()
+
+    def product_url(self, response):
+        return response.url
+
+    def description(self, response):
+        raw_descriptions = response.css('.tabInfoChildContent::text').getall()
+        return self.clean_description(raw_descriptions)
+
+    def clean_description(self, raw_descriptions):
+        for description in raw_descriptions:
+            if re.sub('\s+', '', description):
+                return re.sub('\s+', '', description)
+
+    def image_urls(self, response):
+        return response.css('.owl-carousel > ::attr(data-big)').getall()
+
+    def currency(self, response):
+        return response.css('.inStock > meta[itemprop="priceCurrency"] ::attr(content)').get()
+
+    def price(self, response):
+        return response.css('.inStock > meta[itemprop="price"] ::attr(content)').get()
+
+    def product_sku(self, response):
+        return response.css('.inStock > meta[itemprop="sku"] ::attr(content)').get()
+
+    def get_color(self, response):
+        return response.css('title::text').get().split('|')[2]
+
+    def sku_record(self, response, color, currency, price):
         skus = []
-        sku_records = self.get_sku_record(response)
 
-        for sku in sku_records:
+        for sku in response.css('.size-box-select-container .size-select-list'):
             single_sku = {
-                sku.css('::attr(data-value)').extract_first():
+                sku.css('::attr(data-value)').get():
                     {
                         'color': color,
                         'currency': currency,
                         'price': price,
-                        'size': self.get_size(sku)
-
+                        'size': self.size(sku)
                     }
-
             }
             skus.append(single_sku)
 
-        item['skus'] = skus
-        yield item
-
-    def get_home_page_data(self,response):
-        return response.css('div.childlink-wrapper > a ::attr(href)').extract()
-
-    def get_next_page_url(self, response):
-        return response.css('div.rightArrow ::attr(href)').extract_first()
-
-    def get_page_products(self,response):
-        return response.css('div.gridProduct > div > a ::attr(href)').extract()
-
-    def get_size(self,response):
-        return response.css('a.SizeOption::text').extract()
-
-    def get_name(self,response):
-        return response.css('title::text').extract_first().split('|')[0].strip()
-
-    def get_gender(self,response):
-        return response.css('title::text').extract_first().split('|')[1].strip()
-
-    def get_category(self,response):
-        return response.css('div.breadcrumb > a ::text ').extract()
-
-    def get_product_url(self,response):
-        return response.url
-
-    def get_description(self,response):
-        raw_descriptions = response.css('div.tabInfoChild > div.tabInfoChildContent::text').extract()
-        for descption in raw_descriptions:
-            if re.sub('\s+', '', descption):
-                return re.sub('\s+', '', descption)
-
-    def get_image_urls(self,response):
-        return response.css('div.owl-carousel > img::attr(data-big)').extract()
-
-    def get_currency(self,response):
-        return response.css('div.clearfix > div.inStock > meta ::attr(content)')[2].extract()
-
-    def get_price(self,response):
-        return response.css('div.clearfix > div.inStock > meta ::attr(content)')[3].extract()
-
-    def get_product_sku(self,response):
-        print("AHMAD: ",response)
-        return response.css('div.clearfix > div.inStock > meta ::attr(content)')[0].extract()
-
-    def get_color(self,response):
-        return response.css('title::text').extract_first().split('|')[2]
-
-    def get_sku_record(self,response):
-        return response.css('div.size-select-list')[2].css('div.inStock')
+        return skus
 
