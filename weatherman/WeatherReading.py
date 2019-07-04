dffaffe0a51d87ed81df5711cfced899e666e4f1
yusra <yusra.khalid@outlook.com>
2019-07-04 20:09:02 +0500

    Subscript notation used

--- weatherman/WeatherReading.py
+++ weatherman/WeatherReading.py
@@ -4,10 +4,10 @@ from datetime import datetime
 class WeatherReading:
 
     def __init__(self, weather_reading):
-        self.pkt = datetime.strptime(weather_reading.get('PKT'), '%Y-%m-%d')
-        self.max_temperature = int(weather_reading.get("Max TemperatureC")) if weather_reading.get("Max TemperatureC") else None
-        self.min_temperature = int(weather_reading.get("Min TemperatureC")) if weather_reading.get("Min TemperatureC") else None
-        self.mean_temperature = int(weather_reading.get("Mean TemperatureC")) if weather_reading.get("Mean TemperatureC") else None
-        self.max_humidity = int(weather_reading.get("Max Humidity")) if weather_reading.get("Max Humidity") else None
-        self.min_humidity = int(weather_reading.get(" Min Humidity")) if weather_reading.get(" Min Humidity") else None
-        self.mean_humidity = int(weather_reading.get(" Mean Humidity")) if weather_reading.get(" Mean Humidity") else None
+        self.pkt = datetime.strptime(weather_reading['PKT'], '%Y-%m-%d')
+        self.max_temperature = int(weather_reading["Max TemperatureC"]) if weather_reading["Max TemperatureC"] else None
+        self.min_temperature = int(weather_reading["Min TemperatureC"]) if weather_reading["Min TemperatureC"] else None
+        self.mean_temperature = int(weather_reading["Mean TemperatureC"]) if weather_reading["Mean TemperatureC"] else None
+        self.max_humidity = int(weather_reading["Max Humidity"]) if weather_reading["Max Humidity"] else None
+        self.min_humidity = int(weather_reading[" Min Humidity"]) if weather_reading[" Min Humidity"] else None
+        self.mean_humidity = int(weather_reading[" Mean Humidity"]) if weather_reading[" Mean Humidity"] else None
