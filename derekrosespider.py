bd2c91c398bdb2adf67ba995b7ea2a98752cda9a
Abdullah Faiz <abdullah@A002-00094.arbisoft.com>
2019-07-05 15:25:44 +0500

    scrapy task improved

--- derekrosespider.py
+++ derekrosespider.py
@@ -10,11 +10,11 @@ class MySpider(CrawlSpider):
 
     rules = (
         Rule(LinkExtractor(restrict_css=('.global-nav .global-nav__item[data-nav="mens"]'), \
-                           deny=('/men/clothing/accessories') ),follow=True),
-        Rule(LinkExtractor(restrict_css=('.category-products'), ), callback='main_function'),
+                           deny=('/men/clothing/accessories'))),
+        Rule(LinkExtractor(restrict_css=('.category-products')), callback='parse_item'),
     )
 
-    def main_function(self, response):
+    def parse_item(self, response):
         item = DerekRecord()
 
         item['product_name'] = self.product_name(response)
@@ -23,47 +23,42 @@ class MySpider(CrawlSpider):
         item['details_and_care'] =self.details_and_care(response)
         item['skus'] = self.skus(response)
         item['image_urls'] = self.image_urls(response)
-        item['category'] = self.category(response)
         item['gender'] = self.gender(response)
         item['description'] = self.description(response)
+        item['product_url'] = self.product_url(response)
 
         return item
 
     def price(self, response):
-        converted_price = response.css('span.price::text').get()
-        converted_price = converted_price[1:]
-        converted_price = float(converted_price) * 100
+        price = response.css('span.price::text').get()
+        price_digits = [(elem) for elem in price if elem.isdigit()]
+        converted_price = int(''.join(map(str, price_digits)))
 
-        return converted_price
-
+        return converted_price * 100
 
     def product_name(self, response):
         return response.css('h1.product-details__sub::text').get(),
 
     def skus(self, response):
-          return response.css('.product-details__sku::text').get(),
+        return response.css('.product-details__sku::text').get(),
 
     def description(self, response):
-            return response.css(
-                'div.product-details__main-description p::text').getall(),
+        return response.css('div.product-details__main-description p::text').getall(),
+
     def brand_name(self, response):
-            return response.css('.store-popup__header::text').get().split()[2] + \
-                          response.css('.store-popup__header::text').get().split()[3]
+        return response.css('.brand--is-large .brand__link::text').get().strip()
 
     def image_urls(self, response):
-        Links = []
+        image_links = []
 
         for link in response.css('.media-extra__item'):
             image_urls = link.css('.media-extra__item img::attr(srcset)').get().split()[18]
-            Links.append(image_urls)
+            image_links.append(image_urls)
 
-        return Links
-
-    def category(self, response):
-        return response.css('h1.product-details__sub::text').get().split()[-1]
+        return image_links
 
     def gender(self, response):
-        return response.css('h1.product-details__sub::text').get().split()[0]
+        return response.css('.global-nav .global-nav__item a::attr(data-goto)').get()
 
     def details_and_care(self, response):
         description = {}
@@ -76,6 +71,9 @@ class MySpider(CrawlSpider):
 
         return description,
 
+    def product_url(self, response):
+        return response.css("link[rel='canonical']::attr(href)").get()
+
 
 class DerekRecord(scrapy.Item):
 
@@ -85,8 +83,6 @@ class DerekRecord(scrapy.Item):
     details_and_care = scrapy.Field()
     price = scrapy.Field()
     skus = scrapy.Field()
-    category = scrapy.Field()
     gender = scrapy.Field()
     description = scrapy.Field()
-
-
+    product_url = scrapy.Field()
