2754d49648bc742d341277f790fda1c73a6749cf
Abdullah Faiz <abdullah@A002-00094.arbisoft.com>
2019-07-08 12:24:15 +0500

    Scrapy task final improvements

--- derekrosespider.py
+++ derekrosespider.py
@@ -3,64 +3,63 @@ from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
 
-class MySpider(CrawlSpider):
+class DerekRose(CrawlSpider):
 
     name = 'Derekrose'
     start_urls = ['https://www.derek-rose.com/']
+    listing_css_first = '.global-nav .global-nav__item[data-nav="mens"]'
+    listing_css_second = '.category-products'
 
     rules = (
-        Rule(LinkExtractor(restrict_css=('.global-nav .global-nav__item[data-nav="mens"]'), \
-                           deny=('/men/clothing/accessories'))),
-        Rule(LinkExtractor(restrict_css=('.category-products')), callback='parse_item'),
+        Rule(LinkExtractor(allow=(), deny=(), restrict_css=listing_css_first)),
+        Rule(LinkExtractor(allow=(), deny=(), restrict_css=listing_css_second), callback='parse_item')
     )
 
     def parse_item(self, response):
         item = DerekRecord()
 
-        item['product_name'] = self.product_name(response)
-        item['brand_name'] = self.brand_name(response)
-        item['price'] = self.price(response)
-        item['details_and_care'] =self.details_and_care(response)
-        item['skus'] = self.skus(response)
-        item['image_urls'] = self.image_urls(response)
-        item['gender'] = self.gender(response)
-        item['description'] = self.description(response)
-        item['product_url'] = self.product_url(response)
+        item['product_name'] = self.get_product_name(response)
+        item['brand_name'] = self.get_brand_name(response)
+        item['price'] = self.get_price(response)
+        item['details_and_care'] = self.get_details_and_care(response)
+        item['skus'] = self.get_skus(response)
+        item['image_urls'] = self.get_image_urls(response)
+        item['gender'] = self.get_gender(response)
+        item['description'] = self.get_description(response)
+        item['product_url'] = self.get_product_url(response)
 
         return item
 
-    def price(self, response):
+    def get_price(self, response):
         price = response.css('span.price::text').get()
-        price_digits = [(elem) for elem in price if elem.isdigit()]
-        converted_price = int(''.join(map(str, price_digits)))
+        raw_prices = price_digits = [str(elem) for elem in price if elem.isdigit()]
+        return int(''.join(raw_prices)) * 100
 
-        return converted_price * 100
+    def get_product_name(self, response):
+        return response.css('h1.product-details__sub::text').get()
 
-    def product_name(self, response):
-        return response.css('h1.product-details__sub::text').get(),
+    def get_skus(self, response):
+        return response.css('.product-details__sku::text').get()
 
-    def skus(self, response):
-        return response.css('.product-details__sku::text').get(),
+    def get_description(self, response):
+        return response.css('div.product-details__main-description p::text').getall()
 
-    def description(self, response):
-        return response.css('div.product-details__main-description p::text').getall(),
-
-    def brand_name(self, response):
+    def get_brand_name(self, response):
         return response.css('.brand--is-large .brand__link::text').get().strip()
 
-    def image_urls(self, response):
-        image_links = []
+    def get_image_urls(self, response):
+        image_urls = []
 
         for link in response.css('.media-extra__item'):
-            image_urls = link.css('.media-extra__item img::attr(srcset)').get().split()[18]
-            image_links.append(image_urls)
+            image_url = link.css('.media-extra__item img::attr(srcset)').get().split()[18]
+            image_urls.append(image_url)
 
-        return image_links
+        return image_urls
 
-    def gender(self, response):
+    def get_gender(self, response):
         return response.css('.global-nav .global-nav__item a::attr(data-goto)').get()
 
-    def details_and_care(self, response):
+    def get_details_and_care(self, response):
         description = {}
 
         properties = response.css('.product-details__attrs tr th::text').getall()
@@ -71,7 +70,7 @@ class MySpider(CrawlSpider):
 
         return description,
 
-    def product_url(self, response):
+    def get_product_url(self, response):
         return response.css("link[rel='canonical']::attr(href)").get()
 
 
