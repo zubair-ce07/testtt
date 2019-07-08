3d830c7397772ef6874ceb54623cfb89744f9a52
Abdullah Zafar <abdullah@A002-00080.arbisoft.com>
2019-07-08 17:33:35 +0500

    supports ?listfiles=1 GET argument functionality to provide available files

--- server.js
+++ server.js
@@ -3,22 +3,32 @@ const http = require('http');
 const fs = require('fs');
 
 const app = http.createServer((request, response) => {
-    pathName = url.parse(request.url).pathname;
-    if (pathName == '/') {
-        pathName = '/index.html'
+    parsed = url.parse(request.url);
+    let pathName = parsed.pathname
+    console.log(pathName)
+    if (pathName == '/' && parsed.query != null) {
+        if(parsed.query.split('=')[1] == '1') {
+            let files = fs.readdirSync('files');
+            response.write(files.toString());
+            response.end();
+        }
+    } else {
+        if (pathName == '/') {
+            pathName = '/index.html'
+        }
+    
+        fs.readFile(__dirname + pathName, function(err, data){
+            if(err){
+                fs.readFile(__dirname + '/404.html', function(err, data){
+                    response.write(data);
+                    response.end( );
+                })
+             } else {
+               response.write(data);
+               response.end( );
+             }
+        })
     }
-
-    fs.readFile(__dirname + pathName, function(err, data){
-        if(err){
-            fs.readFile(__dirname + '/404.html', function(err, data){
-                response.write(data);
-                response.end( );
-            })
-         } else {
-           response.write(data);
-           response.end( );
-         }
-    })
 });
 
 app.listen(8080);
