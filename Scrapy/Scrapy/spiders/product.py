1f22ff32abb0580a299287914a6f33e1ad7df9e2
Muhammad Aaqib <aaqib.arbisoft.com>
2019-07-09 18:22:20 +0500

    add: requested changes

--- Scrapy/Scrapy/spiders/product.py
+++ Scrapy/Scrapy/spiders/product.py
@@ -4,95 +4,74 @@ import re
 
 
 class ProductSpider(scrapy.Spider):
-    name = "product"
+    name = 'product'
 
     def start_requests(self):
         url = 'https://www.apc-us.com/'
         yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        for url in response.css(".item .go_page::attr(href)").getall():
+        listing_css = response.css('.item .go_page::attr(href)')
+        product_css = response.css('.item::attr(href)')
+        pagination_css = response.css('.pagination a::attr(href)')
+
+        for url in listing_css.getall():
             yield response.follow(url, callback=self.parse)
 
-        for url in response.css(".item::attr(href)").getall():
+        for url in product_css.getall():
             yield response.follow(url, callback=self.fetch_item_details)
 
-        for url in response.css(".pagination a::attr(href)").getall():
+        for url in pagination_css.getall():
             yield response.follow(url, callback=self.parse)
 
     def fetch_item_details(self, response):
         product = self.get_script_json(response)
-        product_attr = {
-            "name": self.get_name(product),
-            "category": self.get_category(response),
-            "description":  self.get_description(response),
-            "image_urls": self.get_image_urls(response),
-            "brand":  self.get_brand(response),
-            "retailer_sku": self.get_retailer_sku(product),
-            "url": self.get_url(response),
-            "gender": self.get_gender(product),
-            "skus": self.get_skus(response, product)
+        return {
+            'name': product['title'],
+            'category': self.get_category(response),
+            'description':  self.get_description(product),
+            'image_urls': self.get_image_urls(response),
+            'brand':  'A.P.C',
+            'retailer_sku': product['id'],
+            'url': response.url,
+            'gender': self.get_gender(product),
+            'skus': self.get_skus(response, product)
         }
-        yield product_attr
-
-    def get_name(self, product):
-        return product["title"]
 
     def get_category(self, response):
-        return response.css(".breadcrumbs a::text").getall()
+        return response.css('.breadcrumbs a::text').getall()
 
-    def get_description(self, response):
-        return response.css(".description i::text").get() or response.css(".description p::text").get() or \
-               response.css(".description span::text").get() or response.css(".description .ellipsis::text").get() or \
-               response.css(".description::text").get()
-
-    def make_img_url(self, response):
-        for img_url in response.css("::attr(data-zoom-img)").getall():
-            yield response.follow(img_url, None).url
+    def get_description(self, product):
+        return re.sub('<.*?>', '', product['description']).split('. ')
 
     def get_image_urls(self, response):
-        image_urls = []
-        for url in self.make_img_url(response):
-            image_urls.append(url)
-        return image_urls
-
-    def get_brand(self, response):
-        return "A.P.C"
-
-    def get_retailer_sku(self, product):
-        return product["id"]
-
-    def get_url(self, response):
-        return response.url
+        return ['https:{}'.format(re.sub(r"^\s+", "", img.split(",")[-1])) for img in response.css
+                ('[data-zoom-img]::attr(srcset)').getall()]
 
     def get_script_json(self, response):
-        return json.loads(response.css("script[data-product-json]::text").get())
+        return json.loads(response.css('script[data-product-json]::text').get())
 
     def get_gender(self, product):
-        gender_attribute = [attribute for attribute in product["tags"] if "Gender" in attribute]
+        gender_attribute = [attribute for attribute in product['tags'] if 'Gender' in attribute]
         if gender_attribute:
             return re.split('\\bGender:\\b', gender_attribute[0])[-1]
 
     def get_skus(self, response, product):
         skus = []
-        for variant in product["variants"]:
+        for variant in product['variants']:
             skus.append({
-                "sku_id": variant["id"],
-                "color": variant["option1"],
-                "currency": self.get_currency(response),
-                "size": variant["option2"],
-                "out_of_stock": not variant["available"],
-                "price": variant["price"],
-                "previous_price": self.get_prev_price(variant)
+                'sku_id': variant['id'],
+                'color': variant['option1'],
+                'currency': self.get_currency(response),
+                'size': variant['option2'],
+                'out_of_stock': not variant['available'],
+                'price': variant['price'],
+                'previous_price': self.get_prev_price(variant)
             })
         return skus
 
     def get_prev_price(self, variant):
-        if isinstance(variant["compare_at_price"], list):
-            return variant["compare_at_price"]
-        prev_price = []
-        prev_price.append(variant["compare_at_price"])
-        return prev_price
+        return variant['compare_at_price'] if variant['compare_at_price'] is list else [variant['compare_at_price']]
 
     def get_currency(self, response):
-        return response.css("::attr(data-currency)").get()
+        return response.css('[data-currency]::attr(data-currency)').get()
