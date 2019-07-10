7cbd0f80b652271c8383d80c67a39cdf8201dec6
Abdullah Zafar <abdullah@A002-00080.arbisoft.com>
2019-07-10 20:25:03 +0500

    3 cards per row displayed

--- index.js
+++ index.js
@@ -1,3 +1,79 @@
+var country_element = document.getElementById('countrySelector'),
+    language_element = document.getElementById('languageSelector'),
+    criteria_element = document.getElementById('criteriaSelector')
+
+document.getElementById('argumentHandler').addEventListener('change', (event) => {
+    // if(event.target == country_element) {
+    //     console.log(country_element.value)
+    // } else if (event.target == language_element) {
+    //     console.log(language_element.value)
+    // } else if (event.target == criteria_element) {
+    //     console.log(criteria_element.value)
+    // }
+
+    if(language_element.value != 'None' && criteria_element.value != 'None') {
+        fetchUsers(country_element.value, language_element.value, criteria_element.value)
+    }
+    
+})
+
+function fetchUsers(country, language, criteria) {
+    let clientRequest = new XMLHttpRequest()
+    let query = `https://api.github.com/search/users?q=language%3A${language}+location%3A${country}&s=${criteria}&type=users&per_page=20`
+    console.log(query)
+    clientRequest.open('GET', query, true)
+    clientRequest.responseType = 'json'
+    clientRequest.onload = function () {
+        if (clientRequest.status == 200) {
+
+            var displayNode = document.getElementById("disp");
+            while (displayNode.firstChild) {
+                displayNode.removeChild(displayNode.firstChild);
+            }
+
+            let heading = document.createElement('h2');
+            heading.style.textAlign = 'center'
+            heading.style.paddingBottom = '50px'
+            heading.innerText = `Top 20 users in ${country.toUpperCase()}, who work in ${language.toUpperCase()}`
+            displayNode.appendChild(heading)
+
+            cards = []
+
+            for (let singleUser in clientRequest.response['items']){
+                let login = clientRequest.response['items'][singleUser]['login'],
+                    id = clientRequest.response['items'][singleUser]['id'],
+                    avatar_url = clientRequest.response['items'][singleUser]['avatar_url'],
+                    github_url = clientRequest.response['items'][singleUser]['html_url']
+                let userCard = document.createElement('div');
+                userCard.innerHTML = createCard(login, id, avatar_url, github_url);
+                // displayNode.appendChild(userCard)
+                cards.push(userCard)
+            }
+
+            for(let i = 0; i < cards.length - cards.length % 3; i = i + 3) {
+                let cardDeck = document.createElement('div');
+                cardDeck.className = 'card-deck'
+                cardDeck.innerHTML = cards[i].innerHTML + cards[i+1].innerHTML + cards[i+2].innerHTML
+                displayNode.appendChild(cardDeck)
+            }
+            
+            if (cards.length % 3 == 1) {
+                let cardDeck = document.createElement('div');
+                cardDeck.className = 'card-deck'
+                cardDeck.innerHTML = cards[cards.length - cards.length % 3].innerHTML
+                displayNode.appendChild(cardDeck)
+            } else if (cards.length % 3 == 2) {
+                let cardDeck = document.createElement('div');
+                cardDeck.className = 'card-deck'
+                cardDeck.innerHTML = cards[cards.length - cards.length % 3].innerHTML + cards[cards.length - cards.length % 3 + 1].innerHTML
+                displayNode.appendChild(cardDeck)
+            } 
+        }
+    }
+
+    clientRequest.send()
+}
+
 function createCard(login, id, avatar_url, github_url) {
     return `<div class="card" style="width: 18rem;">
             <img class="card-img-top" src="${avatar_url}" alt="Card image cap">
@@ -8,34 +84,3 @@ function createCard(login, id, avatar_url, github_url) {
             </div>
             </div>`
 }
-
-let users = []
-
-function sendRequest(since) {
-    if(since < 100000){
-        let xhr = new XMLHttpRequest()
-        xhr.open('GET', 'https://api.github.com/users?per_page=100&since=' + since, true)
-        xhr.responseType = 'json'
-        xhr.onload = function () {
-            if (xhr.status == 200) {
-                for (let singleUser in xhr.response){
-                    let login = xhr.response[singleUser]['login'],
-                        id = xhr.response[singleUser]['id'],
-                        avatar_url = xhr.response[singleUser]['avatar_url'],
-                        github_url = xhr.response[singleUser]['html_url']
-
-                    let userCard = document.createElement('div');
-                    userCard.innerHTML = createCard(login, id, avatar_url, github_url);
-                    document.getElementById('disp').appendChild(userCard)
-                }
-                since += xhr.response.length
-                console.log(since)
-                // sendRequest(since)
-            }
-        }
-        xhr.send()
-    }
-}
-
-sendRequest(0);
-
