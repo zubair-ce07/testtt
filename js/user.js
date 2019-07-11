38dbe7e060ad8b4b0b13c2540eac145b1a9019ac
Abdullah Zafar <abdullah@A002-00080.arbisoft.com>
2019-07-11 16:02:07 +0500

    basic functional flow

--- js/user.js
+++ js/user.js
@@ -1,111 +1,180 @@
 var username = new URL(window.location).searchParams.get("login")
 document.getElementById("username").innerText = username
-let clientRequest = new XMLHttpRequest()
-let query = `https://api.github.com/users/${username}`
+var per_page = 8
 
-clientRequest.open('GET', query, true)
-clientRequest.responseType = 'json'
-clientRequest.onload = function () {
-    if (clientRequest.status == 200) {
-
-        document.getElementById("avatar").src = clientRequest.response['avatar_url']
-        document.getElementById("github-url").href = clientRequest.response['html_url']
-        document.getElementById("github-url").target = '__blank'
-        document.getElementById("real-name").innerText = clientRequest.response['name']
-        document.getElementById("user-location").innerText = clientRequest.response['location'] ? clientRequest.response['location'] : "-"
-        document.getElementById("user-company").innerText = clientRequest.response['company'] ? clientRequest.response['company'] : "-"
-        document.getElementById("user-bio").innerText = clientRequest.response['bio'] ? clientRequest.response['bio'] : ""
-        var date = new Date(clientRequest.response['created_at']);
-        document.getElementById("user-joined").innerText = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`
-        document.getElementById("user-blog").innerHTML = clientRequest.response['blog'] ? `<a target="__blank" href=${clientRequest.response['blog']}> ${clientRequest.response['blog']} </a>` : "-"
-        document.getElementById("user-followers-badge").innerText = clientRequest.response['followers']
-        document.getElementById("user-following-badge").innerText = clientRequest.response['following']
-        document.getElementById("user-repos-badge").innerText = clientRequest.response['public_repos']
-    }
+if(username != null){
+    fetchUser(username)
 }
 
-clientRequest.send()
+
+document.getElementById('user-profile-page').addEventListener('click', (event) => {
+    if(event.target.getAttribute('href') == '#home') {
+        fetchUser(username)
+    } else if(event.target.getAttribute('href') == '#followers') {
+        fetchFollowers(username)
+    } else if(event.target.getAttribute('href') == '#following') {
+        fetchFollowing(username)
+    } else if(event.target.getAttribute('href') == '#repositories') {
+        fetchRepos(username)
+    }
+})
 
 
-
-
-// document.getElementById('argumentHandler').addEventListener('change', (event) => {
-//     if(language_element.value != 'None' && criteria_element.value != 'None') {
-//         fetchUsers(country_element.value, language_element.value, criteria_element.value)
-//     }
-// })
-
-function fetchUsers(country, language, criteria) {
+function fetchUser(username){
+    let query = `https://api.github.com/users/${username}`
     let clientRequest = new XMLHttpRequest()
-    let query = `https://api.github.com/search/users?q=language%3A${language}+location%3A${country}&s=${criteria}&type=users&per_page=${per_page}`
-    // console.log(query)
     clientRequest.open('GET', query, true)
     clientRequest.responseType = 'json'
     clientRequest.onload = function () {
         if (clientRequest.status == 200) {
 
-            var displayNode = document.getElementById("disp");
-            displayNode.style.visibility = 'visible'
-            while (displayNode.firstChild) {
-                displayNode.removeChild(displayNode.firstChild);
-            }
+            document.getElementById("avatar").src = clientRequest.response['avatar_url']
+            document.getElementById("github-url").href = clientRequest.response['html_url']
+            document.getElementById("github-url").target = '__blank'
+            document.getElementById("real-name").innerText = clientRequest.response['name']
+            document.getElementById("user-location").innerText = clientRequest.response['location'] ? clientRequest.response['location'] : "-"
+            document.getElementById("user-company").innerText = clientRequest.response['company'] ? clientRequest.response['company'] : "-"
+            document.getElementById("user-bio").innerText = clientRequest.response['bio'] ? clientRequest.response['bio'] : ""
+            var date = new Date(clientRequest.response['created_at']);
+            document.getElementById("user-joined").innerText = `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`
+            document.getElementById("user-blog").innerHTML = clientRequest.response['blog'] ? `<a target="__blank" href=${clientRequest.response['blog']}> ${clientRequest.response['blog']} </a>` : "-"
+            document.getElementById("user-followers-badge").innerText = clientRequest.response['followers']
+            document.getElementById("user-following-badge").innerText = clientRequest.response['following']
+            document.getElementById("user-repos-badge").innerText = clientRequest.response['public_repos']
+        }
+    }
 
-            let heading = document.createElement('h2');
-            heading.style.textAlign = 'center'
-            heading.style.paddingBottom = '50px'
-            heading.innerText = `Top ${per_page} users in ${country.toUpperCase()}, who work in ${language.toUpperCase()}`
-            displayNode.appendChild(heading)
+    clientRequest.send()
+}
 
+function fetchFollowers(username) {
+    let query = `https://api.github.com/users/${username}/followers?per_page=${per_page}`
+    let clientRequest = new XMLHttpRequest()
+    clientRequest.open('GET', query, true)
+    clientRequest.responseType = 'json'
+    clientRequest.onload = function () {
+        if (clientRequest.status == 200) {
+            let displayNode = document.getElementById("display-followers");
+            displayUsers(displayNode, username, clientRequest.response)
+        }
+
+        document.getElementById('display-followers-button').href = `https://github.com/${username}/followers`
+        document.getElementById('display-followers-button').target = '__blank'
+    }
+    clientRequest.send()
+}
+
+function fetchFollowing(username) {
+    let query = `https://api.github.com/users/${username}/following?per_page=${per_page}`
+    let clientRequest = new XMLHttpRequest()
+    clientRequest.open('GET', query, true)
+    clientRequest.responseType = 'json'
+    clientRequest.onload = function () {
+        if (clientRequest.status == 200) {
+
+            let displayNode = document.getElementById("display-following");
+            displayUsers(displayNode, username, clientRequest.response)
+        }
+
+        document.getElementById('display-following-button').href = `https://github.com/${username}/following`
+        document.getElementById('display-following-button').target = '__blank'
+    }
+    
+    clientRequest.send()
+}
+
+
+function fetchRepos(username) {
+    let query = `https://api.github.com/users/${username}/repos?per_page=${per_page}`
+    let clientRequest = new XMLHttpRequest()
+    clientRequest.open('GET', query, true)
+    clientRequest.responseType = 'json'
+    clientRequest.onload = function () {
+        if (clientRequest.status == 200) {
+            let displayNode = document.getElementById("display-repositories");
             let cards = []
 
-            for (let singleUser in clientRequest.response['items']){
-                let login = clientRequest.response['items'][singleUser]['login'],
-                    id = clientRequest.response['items'][singleUser]['id'],
-                    avatar_url = clientRequest.response['items'][singleUser]['avatar_url'],
-                    github_url = clientRequest.response['items'][singleUser]['html_url'],
-                    api_url = clientRequest.response['items'][singleUser]['url']
+            for (let oneRepo in clientRequest.response) {
+                let repoName = clientRequest.response[oneRepo]['name'],
+                    repoFullName = clientRequest.response[oneRepo]['full_name'],
+                    repoURL = clientRequest.response[oneRepo]['html_url'],
+                    repoDescription = clientRequest.response[oneRepo]['description'] ? clientRequest.response[oneRepo]['description'] : '<span class="text-warning text-center"> No Description Available </span>',
+                    repoCreated = new Date(clientRequest.response[oneRepo]['created_at']).toDateString(),
+                    repoUpdated = new Date(clientRequest.response[oneRepo]['updated_at']).toDateString(),
+                    repoWatchers = clientRequest.response[oneRepo]['watchers_count'],
+                    repoLanguage = clientRequest.response[oneRepo]['language'],
+                    repoForks = clientRequest.response[oneRepo]['forks_count']
+                    repoIssuesCount = clientRequest.response[oneRepo]['open_issues_count']
+                    repoLicense = clientRequest.response[oneRepo]["license"] ? clientRequest.response[oneRepo]["license"]['name'] : "unknown"
                 
                 let userCard = document.createElement('div');
-                userCard.innerHTML = createCard(+singleUser+1, login, id, avatar_url, github_url, api_url);
+                userCard.innerHTML = createRepoCard(repoName, repoDescription, repoCreated, repoUpdated, repoWatchers, repoLanguage, repoForks, repoIssuesCount, repoLicense, repoURL);
                 cards.push(userCard)
             }
 
-            for(let i = 0; i < cards.length - cards.length % 4; i = i + 4) {
+            for(let i = 0; i < cards.length - cards.length % 2; i = i + 2) {
                 let cardDeck = document.createElement('div');
                 cardDeck.className = 'card-deck'
-                cardDeck.innerHTML = cards[i].innerHTML + cards[i+1].innerHTML + cards[i+2].innerHTML + cards[i+3].innerHTML
+                cardDeck.innerHTML = cards[i].innerHTML + cards[i+1].innerHTML
                 displayNode.appendChild(cardDeck)
             }
             
             var lastDeck = document.createElement('div');
             lastDeck.className = 'card-deck'
 
-            switch(cards.length % 4) {
+            switch(cards.length % 2) {
                 case 1:
-                    lastDeck.innerHTML = cards[cards.length - cards.length % 4].innerHTML
+                    lastDeck.innerHTML = cards[cards.length - cards.length % 2].innerHTML
                     break;
-                case 2:
-                    lastDeck.innerHTML = cards[cards.length - cards.length % 4].innerHTML + cards[cards.length - cards.length % 4 + 1].innerHTML
-                    break;
-                case 3:
-                    lastDeck.innerHTML = cards[cards.length - cards.length % 4].innerHTML + cards[cards.length - cards.length % 4 + 1].innerHTML + cards[cards.length - cards.length % 4 + 2].innerHTML
-                    break;
-                default:
-                    console.log(cards.length % 4)
-            }
+            }    
 
             displayNode.appendChild(lastDeck)
-
-            let endText = document.createElement('h4');
-            endText.style.textAlign = 'center'
-            // endText.style.paddingBottom = '20px'
-            endText.innerText = `... End of Results ...`
-            endText.style.marginTop = '50px'
-            displayNode.appendChild(endText)
         }
+
+        document.getElementById('display-repositories-button').href = `https://github.com/${username}/`
+        document.getElementById('display-repositories-button').target = '__blank'
+    }
+    clientRequest.send()
+}
+
+function displayUsers(displayNode, username, api_call_result) {
+    let cards = []
+
+    for (let singleUser in api_call_result) {
+        let login = api_call_result[singleUser]['login'],
+            id = api_call_result[singleUser]['id'],
+            avatar_url = api_call_result[singleUser]['avatar_url'],
+            github_url = api_call_result[singleUser]['html_url'],
+            api_url = api_call_result[singleUser]['url']
+        
+        let userCard = document.createElement('div');
+        userCard.innerHTML = createCard(+singleUser+1, login, id, avatar_url, github_url, api_url);
+        cards.push(userCard)
     }
 
-    clientRequest.send()
+    for(let i = 0; i < cards.length - cards.length % 4; i = i + 4) {
+        let cardDeck = document.createElement('div');
+        cardDeck.className = 'card-deck'
+        cardDeck.innerHTML = cards[i].innerHTML + cards[i+1].innerHTML + cards[i+2].innerHTML + cards[i+3].innerHTML
+        displayNode.appendChild(cardDeck)
+    }
+    
+    var lastDeck = document.createElement('div');
+    lastDeck.className = 'card-deck'
+
+    switch(cards.length % 4) {
+        case 1:
+            lastDeck.innerHTML = cards[cards.length - cards.length % 4].innerHTML
+            break;
+        case 2:
+            lastDeck.innerHTML = cards[cards.length - cards.length % 4].innerHTML + cards[cards.length - cards.length % 4 + 1].innerHTML
+            break;
+        case 3:
+            lastDeck.innerHTML = cards[cards.length - cards.length % 4].innerHTML + cards[cards.length - cards.length % 4 + 1].innerHTML + cards[cards.length - cards.length % 4 + 2].innerHTML
+            break;
+    }    
+
+    displayNode.appendChild(lastDeck)
 }
 
 function createCard(number, login, id, avatar_url, github_url) {
@@ -117,6 +186,47 @@ function createCard(number, login, id, avatar_url, github_url) {
             </div>
             <div class="card-footer bg-secondary text-center">
                 <a href="user.html?login=${login}" class="btn btn-success">View Profile</a>
+            </div>
+            </div>`
+}
+
+function createRepoCard(repoName, repoDescription, repoCreated, repoUpdated, repoWatchers, repoLanguage, repoForks, repoIssuesCount, repoLicense, repoURL){
+    return `<div class="card text-white bg-secondary mb-3 border-success" style="width: 18rem;">
+
+            <div class="card-header text-center">
+                <div class="row">
+                    <div class="col-sm">
+                        <p class="text-center font-weight-bold"> ${repoName} </p> 
+                    </div>
+                    <div class="col-sm text-right my-auto">
+                        <a href="${repoURL}" target="__blank" class="btn btn-primary">View on Github</a>
+                    </div>
+                </div>
+                <p>Language: ${repoLanguage} </p> 
+                <p> License: ${repoLicense} </p>
+            </div>
+
+            <div class="card-body">
+                <p> ${repoDescription} </p>
+            </div>
+
+            <div class="card-footer bg-secondary text-center">
+                <p class="bg-danger p-1 medium"> Created at: ${repoCreated} </p>
+                <p class="bg-info p-1"> Last Updated: <br> ${repoUpdated} </p>
+            </div>
+            
+            <div class="card-footer bg-secondary medium">
+                <div class="row text-center">
+                    <div class="col-sm bg-primary">
+                    Watchers: ${repoWatchers}
+                    </div>
+                    <div class="col-sm bg-warning">
+                    Forks: ${repoForks}
+                    </div>
+                    <div class="col-sm bg-danger">
+                    Issues: ${repoIssuesCount}
+                    </div>
+                </div>
             </div>
             </div>`
 }
