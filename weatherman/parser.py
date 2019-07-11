203cba109184a03849fdb21d7c9a866b71a98cb6
asadali145 <asad.ali@arbisoft.com>
2019-07-11 13:19:12 +0500

    Created the weatherman package

---
+++ weatherman/parser.py
@@ -0,0 +1,65 @@
+import os
+from collections import defaultdict
+
+
+def read_files(years, months, data_path):
+    """This function will read all the weather data."""
+
+    # Dictionary to store all the data
+    data_for_all_months = {}
+
+    # Read all files
+    for year in years:
+        for month in months:
+
+            file = data_path + "Murree_weather_" + year + "_" + month + ".txt"
+
+            if os.path.exists(file):
+
+                monthly_readings = file_parser(file)
+                data_for_all_months[year + "_" + month] = monthly_readings
+    
+    return data_for_all_months
+
+# 
+
+
+def file_parser(month):
+    """This function will read a File in a dictionary object."""
+
+    
+    monthly_readings = defaultdict(lambda: [])
+    f = open(month)
+    data_header = f.readline().replace('\n', '').replace(" ",'').split(',')
+
+    for reading in f:
+
+        reading = reading.replace("\n", '').split(',')
+
+        for header_val, value in zip(data_header, reading):
+
+            if value:
+                if header_val in ['PKST', 'PKT', 'Events']:
+
+                    monthly_readings[header_val].append(value)
+
+                elif header_val in [
+                    'MaxVisibilityKm', 'MeanVisibilityKm',
+                    'MinVisibilitykM', 'Precipitationmm',
+                    'MeanSeaLevelPressurehPa'
+                    ]:
+
+                    monthly_readings[header_val].append(float(value))
+
+                else:
+
+                    try:
+                        monthly_readings[header_val].append(int(value))
+                    except:
+                        print (header_val, "    ", value)
+            else:
+
+                monthly_readings[header_val].append(None)
+    f.close()
+    
+    return monthly_readings
