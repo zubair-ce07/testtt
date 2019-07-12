b9e1877d91316e9f1765a6ea18a68f30e0134c14
Uzair Amer <uzair.amer@arbisoft.com>
2019-07-12 10:52:33 +0500

    Improved minor bugs

---
+++ recursivecrawler/crawlrecord.py
@@ -0,0 +1,4 @@
+class CrawlerRecord:
+    def __init__(self, total_bytes_read, total_requests):
+        self.total_bytes_read = total_bytes_read
+        self.total_requests = total_requests
--- recursivecrawler/main.py
+++ recursivecrawler/main.py
@@ -2,69 +2,67 @@ import argparse
 import asyncio
 import parsel
 import requests
+import time
 
 from concurrent.futures import ProcessPoolExecutor
-
-
-class CrawlerRecord:
-    def __init__(self, total_bytes_read, total_requests):
-        self.total_bytes_read = total_bytes_read
-        self.total_requests = total_requests
+from crawlrecord import CrawlerRecord
 
 
 final_record = CrawlerRecord(total_bytes_read=0, total_requests=0)
 
 
-def request_and_increase_count(url):
-    response = requests.get(url)
-    final_record.total_requests += 1
-    return response
-
-
-def fetch_urls(url: str, max_count: int, total_urls: list = [], recursive_call=False, return_list=False):
-    """
-    Recursively calls itself until the max_count requirement is met
-    """
-    response = request_and_increase_count(url)
-    selector = parsel.Selector(text=response.text)
-
-    a_tags = selector.css('a')
-    urls = [f'http:{a_tag.attrib["href"]}' for a_tag in a_tags if a_tag.attrib.get('href')]
-    # total_urls.extend(urls)
-    if len(total_urls) < max_count:
-        fetch_urls(urls[0],
-                   max_count=max_count - len(total_urls),
-                   total_urls=total_urls, recursive_call=True)
-    elif recursive_call:
+def request_and_increase_count(url: str):
+    try:
+        response = requests.get(url)
+        final_record.total_requests += 1
+        return response
+    except Exception as e:
+        print(f'Unable to process URL: {url}')
         return None
 
-    if return_list:
-        return total_urls[:max_count]
 
-    queue = asyncio.Queue(maxsize=max_count)
-    total_urls = total_urls[:max_count]
-    for url in total_urls:
-        queue.put_nowait(url)
+def url_validator(url: str):
+    if not(url.startswith('http:') or url.startswith('https:')):
+        return f'https:{url}'
+    return url
+
+
+def fetch_urls(url: str, max_count: int) -> list:
+    response = request_and_increase_count(url)
+    selector = parsel.Selector(text=response.text)
+    a_tags = selector.css('a')
+    urls = [url_validator(a_tag.attrib['href']) for a_tag in a_tags if a_tag.attrib.get('href')]
+    if len(urls) < max_count:
+        urls.extend(fetch_urls(urls[0], max_count=max_count - len(urls)))
+    return urls[:max_count]
+
+
+def list_to_async_queue(inputs: list) -> asyncio.Queue:
+    queue = asyncio.Queue(maxsize=len(inputs))
+    for input in inputs:
+        queue.put_nowait(input)
     return queue
 
 
-async def async_worker(index:int, queue: asyncio.Queue, download_delay: int) -> None:
+async def async_url_worker(index:int, queue: asyncio.Queue, download_delay: int) -> None:
     while True:
         url = await queue.get()
         print(f'Worker {index} got url {url}')
         loop = asyncio.get_running_loop()
         response = await loop.run_in_executor(None, request_and_increase_count, url)
-        print(f'Worker {index} read {len(response.content)} bytes')
-        final_record.total_bytes_read += len(response.content)
+        if response:
+            print(f'Worker {index} read {len(response.content)} bytes')
+            final_record.total_bytes_read += len(response.content)
         queue.task_done()
         print(f'Urls left: {queue.qsize()}')
         await asyncio.sleep(download_delay)
 
 
-def parallel_worker(url:str):
-    print(f'Working on URL {url}')
-    response = requests.get(url)
-    return len(response.content)
+def parallel_url_worker(args: tuple) -> int:
+    time.sleep(args[0])
+    print(f'Working on URL {args[1]}')
+    response = request_and_increase_count(args[1])
+    return len(response.content) if response else 0
 
 
 async def main():
@@ -76,19 +74,25 @@ async def main():
     parser.add_argument('-download_delay', '-d', type=int, required=False, default=0)
 
     args = parser.parse_args()
+    urls = fetch_urls(url=args.url, max_count=args.max_urls_count)
+
     if args.parallel:
-        urls = fetch_urls(url=args.url, max_count=args.max_urls_count, return_list=True)
         executor = ProcessPoolExecutor(max_workers=args.concurrent_requests)
-        results = executor.map(parallel_worker, urls)
+        process_args = ((args.download_delay, url) for url in urls)
+        results = executor.map(parallel_url_worker, process_args)
+
         for result in results:
-            result.total_bytes_read += result
-            result.total_requests += 1
+            final_record.total_bytes_read += result
+            final_record.total_requests += 1
+
     else:
-        urls_queue = fetch_urls(url=args.url, max_count=args.max_urls_count)
+        urls_queue = list_to_async_queue(urls)
         tasks = []
+
         for task_index in range(args.concurrent_requests):
-            tasks.append(asyncio.create_task(async_worker(task_index, queue=urls_queue,
-                                                          download_delay=args.download_delay)))
+            tasks.append(asyncio.create_task(async_url_worker(task_index, queue=urls_queue,
+                                                              download_delay=args.download_delay)))
+
         await urls_queue.join()
 
     print(f'Total Requests: {final_record.total_requests}')
@@ -97,4 +101,6 @@ async def main():
 
 
 if __name__ == '__main__':
+    start_time = time.monotonic()
     asyncio.run(main())
+    print(f'Finished in {time.monotonic() - start_time} seconds')
