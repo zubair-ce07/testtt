63822546dbd1ed564e1be456ed7d7cbd878a61ac
Faizan Baig <52661937+faizanbaig01@users.noreply.github.com>
2019-07-12 16:06:28 +0500

    Add files via upload

--- Arguments.py
+++ Arguments.py
@@ -4,8 +4,12 @@ import argparse
 def argument_parse():
     p = argparse.ArgumentParser()
     p.add_argument('file_path', help='Local File Path on Your Machine')
-    p.add_argument('-e', '-yearly', type=lambda arg: datetime.strptime(arg, '%Y'), nargs="*")
-    p.add_argument('-a', '-monthly', type=lambda arg: datetime.strptime(arg, '%Y/%m'), nargs="*")
-    p.add_argument('-b', '-bonus', type=lambda arg: datetime.strptime(arg, '%Y/%m'), nargs="*")
-    p.add_argument('-c', '-chart', type=lambda arg: datetime.strptime(arg, '%Y/%m'), nargs="*")
+    p.add_argument('-e', '-yearly', type=lambda argument:
+    datetime.strptime(argument, '%Y'), nargs="*")
+    p.add_argument('-a', '-monthly', type=lambda argument:
+    datetime.strptime(argument, '%Y/%m'), nargs="*")
+    p.add_argument('-b', '-bonus', type=lambda argument:
+    datetime.strptime(argument, '%Y/%m'), nargs="*")
+    p.add_argument('-c', '-chart', type=lambda argument:
+    datetime.strptime(argument, '%Y/%m'), nargs="*")
     return p.parse_args()
