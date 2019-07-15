5c9473c9c83082e92a9873a86abdd8e597993f57
Mohammad Owais Basit <owais.basit@arbisoft.com>
2019-07-15 12:47:51 +0500

    fix: removed extra model and added choices
    Removed Category model and added choices to the category field in Article model

--- content-aggregator/aggregator/articles/models.py
+++ content-aggregator/aggregator/articles/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
 class Website(models.Model):
-    website_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     url = models.URLField(max_length=200, unique=True)
 
     def __str__(self):
-        return self.website_name
+        return self.name
 
 class Author(models.Model):
     full_name = models.CharField(max_length=200, unique=True)
@@ -13,15 +13,33 @@ class Author(models.Model):
     def __str__(self):
         return self.full_name
 
-class Category(modesl.Model):
-    category_name = models.CharField(max_length=100, unique=True)
-
-    def __str__(self):
-        return self.category_name
-
 class Article(models.Model):
+    TECH = 'Technology'
+    BUSINESS = 'Business'
+    SPORTS = 'Sports'
+    EDUCATION = 'Education'
+    SCIENCE = 'Science'
+    INTERNATIONAL = 'International'
+    ENTERTAINMENT = 'Entertainment'
+    LIFESTYLE = 'Lifestyle'
+    MISC = 'Miscellaneous'
+
     title = models.CharField(max_length=300)
-    category = models.ForeignKey(Category, related_name="category", on_delete=models.CASCADE)
+    category = models.CharField(
+        max_length=13,
+        choices=[
+            (TECH, TECH),
+            (BUSINESS, BUSINESS),
+            (SPORTS, SPORTS),
+            (EDUCATION, EDUCATION),
+            (SCIENCE, SCIENCE),
+            (INTERNATIONAL, INTERNATIONAL),
+            (ENTERTAINMENT, ENTERTAINMENT),
+            (LIFESTYLE, LIFESTYLE),
+            (MISC, MISC)
+        ]
+        default=MISC
+    )
     authors = models.ManyToManyField(Author, related_name='authors', on_delete=models.CASCADE)
     website = models.ForeignKey(Website, related_name='website', on_delete=models.CASCADE)
     content = models.TextField()
