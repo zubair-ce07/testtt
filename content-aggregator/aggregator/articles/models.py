7c37d7e2d1e93ee20c2fc99b642e6bc990ed7cb4
Mohammad Owais Basit <owais.basit@arbisoft.com>
2019-07-15 12:24:29 +0500

    fix: made minor changes and added new model for Category

--- content-aggregator/aggregator/articles/models.py
+++ content-aggregator/aggregator/articles/models.py
@@ -1,29 +1,32 @@
 from django.db import models
 
 class Website(models.Model):
-    name = models.CharField(max_length=200)
-    address = models.CharField(max_length=200, unique=True)
+    website_name = models.CharField(max_length=200)
+    url = models.URLField(max_length=200, unique=True)
 
     def __str__(self):
-        return self.address
+        return self.website_name
 
 class Author(models.Model):
-    name = models.CharField(max_length=200, unique=True)
-    num_of_articles = models.IntegerField()
+    full_name = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
-        return self.name
+        return self.full_name
+
+class Category(modesl.Model):
+    category_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.category_name
 
 class Article(models.Model):
     title = models.CharField(max_length=300)
-    category = models.CharField(max_length=50)
-    authors = models.ArrayField(
-        models.ForeignKey(Author, related_name='authors', on_delete=models.CASCADE)
-    )
+    category = models.ForeignKey(Category, related_name="category", on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author, related_name='authors', on_delete=models.CASCADE)
     website = models.ForeignKey(Website, related_name='website', on_delete=models.CASCADE)
-    body = models.TextField(max_length=4000)
-    publish_time = models.DateTimeField('date published')
-    url = models.URLField(max_length=400)
+    content = models.TextField()
+    publish_time = models.DateTimeField()
+    url = models.URLField(max_length=400, unique=True)
 
     def __str__(self):
         return self.title
