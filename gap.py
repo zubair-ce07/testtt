5a03c6309d844cc90b4242a7aadb404c6209af55
ahmadarshi <m.ahmad@arbisoft.com>
2019-07-17 19:01:14 +0500

    generalized category url and extracted ids from json

--- gap.py
+++ gap.py
@@ -13,7 +13,7 @@ def clean(raw_data):
         return re.sub('\s+', ' ', raw_data).strip()
 
 
-class GapItem(scrapy.Item):
+class WeItem(scrapy.Item):
     retailer_sku = scrapy.Field()
     gender = scrapy.Field()
     name = scrapy.Field()
@@ -26,88 +26,95 @@ class GapItem(scrapy.Item):
     skus = scrapy.Field()
 
 
-class GapSpider(CrawlSpider):
+class Gap(CrawlSpider):
     name = 'gap'
     allowed_domains = ['gap.cn']
+    api_url = 'https://www.gap.cn/gap/rest/category?'
+    product_url = 'https://www.gap.cn/gap/rest/productnew?id='
     start_urls = [
-        'https://www.gap.cn/gap/rest/category?cid=2300',
-        'https://www.gap.cn/gap/rest/category?cid=4',
-        'https://www.gap.cn/gap/rest/category?cid=3',
-        'https://www.gap.cn/gap/rest/category?cid=6',
-        'https://www.gap.cn/gap/rest/category?cid=5',
-        'https://www.gap.cn/gap/rest/category?cid=33748',
-        'https://www.gap.cn/gap/rest/category?cid=33758',
-        'https://www.gap.cn/gap/rest/category?cid=7564',
-        'https://www.gap.cn/gap/rest/category?cid=44030',
+        'https://www.gap.cn/gap/rest/category?action=getTopNavs&store_id=1'
     ]
 
     def parse(self, response):
-        category_url = 'https://www.gap.cn/gap/rest/category?id='
-        yield from [response.follow(f'{category_url}{category_code}',
+        listings_json = json.loads(response.text)
+
+        yield from [response.follow(f'{self.api_url}cid={listing_id.get("id")}',
+                                    callback=self.parse_categories)
+                    for listing_id in listings_json.get('data').get('marketTopNavs')]
+
+    def parse_categories(self, response):
+        categories_json = json.loads(response.text)
+
+        yield from [response.follow(f'{self.api_url}id={category_code}',
                                     callback=self.parse_subcategory)
-                    for category_code in re.findall('entity_id\":\"(.+?)\"', response.text)]
+                    for category_code in
+                    categories_json.get('data').get('currentProduct'
+                                                    'List').get('currentChildCategoryIdAll').split(',')]
 
     def parse_subcategory(self, response):
-        subcategory_url = 'https://www.gap.cn/gap/rest/category?cid='
-        yield from [response.follow(f'{subcategory_url}{subcategory_code}&store_id=1&from=side'
+        subcategories_json = json.loads(response.text)
+
+        yield from [response.follow(f'{self.api_url}cid={subcategory_code}&store_id=1&from=side'
                                     f'&customer_group_id=0', callback=self.parse_products)
-                    for subcategory_code in re.findall('entity_id\":\"(.+?)\"', response.text)]
+                    for subcategory_code in subcategories_json.get('data').get('child_categories')]
 
     def parse_products(self, response):
-        product_url = 'https://www.gap.cn/gap/rest/productnew?id='
-        yield from [response.follow(f'{product_url}{product_code}&store_id=1&customer_group_id=0',
+        products_json = json.loads(response.text)
+
+        yield from [response.follow(f'{self.product_url}{product_code}&store_id=1&customer_group_id=0',
                                     callback=self.parse_item)
-                    for product_code in re.findall('productId\":(.+?),', response.text)]
+                    for product_code in products_json.get('data').get('currentProduct'
+                                                                      'List').get('productIds')]
 
     def parse_item(self, response):
-        raw_json = json.loads(response.text)
+        raw_product = json.loads(response.text)
 
-        item = GapItem()
-        item['retailer_sku'] = self.retailer_sku(raw_json)
-        item['gender'] = self.product_gender(raw_json)
-        item['name'] = self.product_name(raw_json)
-        item['category'] = self.product_category(raw_json)
-        item['url'] = self.product_url(raw_json)
-        item['brand'] = self.product_brand(raw_json)
-        item['description'] = self.product_description(raw_json)
-        item['care'] = self.product_care(raw_json)
-        item['image_urls'] = self.images_url(raw_json)
-        item['skus'] = self.parse_sku(raw_json)
+        item = WeItem()
+        item['retailer_sku'] = self.retailer_sku(raw_product)
+        item['gender'] = self.product_gender(raw_product)
+        item['name'] = self.product_name(raw_product)
+        item['category'] = self.product_category(raw_product)
+        item['url'] = self.product_url(raw_product)
+        item['brand'] = self.product_brand(raw_product)
+        item['description'] = self.product_description(raw_product)
+        item['care'] = self.product_care(raw_product)
+        item['image_urls'] = self.images_url(raw_product)
+        item['skus'] = self.parse_sku(raw_product)
 
         yield item
 
-    def retailer_sku(self, raw_json):
-        return raw_json.get('data').get('trackingcode').get('productId')
+    def retailer_sku(self, raw_product):
+        return raw_product.get('data').get('trackingcode').get('productId')
 
-    def product_gender(self, raw_json):
-        return raw_json.get('data').get('prefixCategoryName')
+    def product_gender(self, raw_product):
+        return raw_product.get('data').get('prefixCategoryName')
 
-    def product_name(self, raw_json):
-        return raw_json.get('data').get('productName')
+    def product_name(self, raw_product):
+        return raw_product.get('data').get('productName')
 
-    def product_category(self, raw_json):
-        return raw_json.get('data').get('categoriesName')
+    def product_category(self, raw_product):
+        return raw_product.get('data').get('categoriesName')
 
-    def product_url(self, raw_json):
-        return raw_json.get('data').get('shareUrl')
+    def product_url(self, raw_product):
+        return raw_product.get('data').get('shareUrl')
 
-    def product_brand(self, raw_json):
-        return raw_json.get('data').get('trackingcode').get('dps360_brand')
+    def product_brand(self, raw_product):
+        return raw_product.get('data').get('trackingcode').get('dps360_brand')
 
-    def product_description(self, raw_json):
-        return raw_json.get('data').get('productDetail').get('productFiber')
+    def product_description(self, raw_product):
+        return raw_product.get('data').get('productDetail').get('productFiber')
 
-    def product_care(self, raw_json):
-        return raw_json.get('data').get('productDetail').get('productCare')
+    def product_care(self, raw_product):
+        return raw_product.get('data').get('productDetail').get('productCare')
 
-    def images_url(self, raw_json):
+    def images_url(self, raw_product):
         return [image_list.get('productImageUrl')
-                for image_list in raw_json.get('data').get('imageList')]
+                for image_list in raw_product.get('data').get('imageList')]
 
-    def parse_sku(self, raw_json):
+    def parse_sku(self, raw_product):
         sku = {}
 
-        for color_json in raw_json.get('data').get('colors'):
+        for color_json in raw_product.get('data').get('colors'):
             color = color_json.get("colorName")
 
             for size in color_json.get('size'):
