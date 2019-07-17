52f5803a659cbdb80b13fed1136e8def360383c8
yusra <yusra.khalid@outlook.com>
2019-07-17 13:05:09 +0500

    Boilerplate added

--- task3/newYorker/newYorker/spiders/new_yorker.py
+++ task3/newYorker/newYorker/spiders/new_yorker.py
@@ -14,33 +14,32 @@ class NewYorkerSpider(scrapy.Spider):
                   "https://api.newyorker.de/csp/products/public/query?"
                   "filters[country]=pt&filters[gender]=FEMALE&limit=24&offset=0",
                   ]
+    industry = None
 
     def parse(self, response):
         for item in json.loads(response.text)['items']:
             yield scrapy.Request(
-                url='https://api.newyorker.de/csp/products/public/product'
-                    '/matchingProducts?country=de&id=' +
-                    str(item['id'])+'&variantId=001&limit=3',
-                meta={'trail': response.request.url},
+                url=f'https://api.newyorker.de/csp/products/public/product'
+                f'/matchingProducts?country=de&id={str(item["id"])}&variantId=001&limit=3',
+                meta={'trail': response.url},
                 callback=self.parse_product
             )
         for i in range(0, json.loads(response.text)['totalCount'], 24):
             yield scrapy.Request(
-                url=w3lib.url.add_or_replace_parameter(response.request.url, "offset", str(i)),
-                meta={'trail': response.request.url},
+                url=w3lib.url.add_or_replace_parameter(response.url, "offset", str(i)),
+                meta={'trail': response.url},
                 callback=self.parse_pagination)
             yield scrapy.Request(
-                url=w3lib.url.add_or_replace_parameter(response.request.url, "offset", str(i)),
-                meta={'trail': response.request.url},
+                url=w3lib.url.add_or_replace_parameter(response.url, "offset", str(i)),
+                meta={'trail': response.url},
                 callback=self.parse_pagination)
 
     def parse_pagination(self, response):
         for item in json.loads(response.text)['items']:
             yield scrapy.Request(
-                url='https://api.newyorker.de/csp/products/public/product'
-                    '/matchingProducts?country=de&id=' +
-                    str(item['id'])+'&variantId=001&limit=3',
-                meta={'trail': response.request.url},
+                url=f'https://api.newyorker.de/csp/products/public/product'
+                    f'/matchingProducts?country=de&id={str(item["id"])}&variantId=001&limit=3',
+                meta={'trail': response.url},
                 callback=self.parse_product
             )
 
@@ -52,29 +51,32 @@ class NewYorkerSpider(scrapy.Spider):
             for sku in variant.get('variants'):
                 if sku:
                     for image in sku.get('images'):
-                        images.append('https://nyimages.blob.core.windows.net/ny-public/'+image.get('key'))
+                        images.append(f"https://nyimages.blob.core.windows.net/ny-public/{image.get('key')}")
                     skus.append(self.extract_skus(sku))
                     price = sku.get('original_price')
                     currency = sku.get('currency')
-            product_types.append(NewyorkerItem(
-                retailer_sku=variant['id'],
-                market=variant['country'],
-                url_original=response.url,
-                url='https://www.newyorker.de/pt/products/?gender=MALE#/products/detail/'+variant["id"]+'/001',
-                category=variant['maintenance_group'],
-                description=variant['descriptions'],
-                brand=variant['brand'],
-                gender=variant['customer_group'],
-                price=price,
-                currency=currency,
-                image_urls=images,
-                spider_name='new_yorker',
-                crawl_start_time=self.crawler.stats.get_stats()['start_time'].strftime("%Y-%m-%dT%H:%M:%s"),
-                skus=skus,
-                industry=None,
-                trail=response.meta.get('trail')
-            ))
+            product = self.boilerplate(NewyorkerItem(), response)
+            product['retailer_sku'] = variant['id']
+            product['market'] = variant['country']
+            product['url'] = f'https://www.newyorker.de/pt/products/?gender=MALE#/products/detail/{variant["id"]}/001'
+            product['category'] = variant['maintenance_group']
+            product['description'] = variant['descriptions']
+            product['brand'] = variant['brand']
+            product['gender'] = variant['customer_group']
+            product['price'] = price
+            product['currency'] = currency
+            product['image_urls'] = images
+            product['skus'] = skus
+            product_types.append(product)
         return product_types
+
+    def boilerplate(self, product, response):
+        product['spider_name'] = self.name
+        product['industry'] = self.industry
+        product['trail'] = response.meta.get('trail')
+        product['url_original'] = response.url
+        product['crawl_start_time'] = self.crawler.stats.get_stats()['start_time'].strftime("%Y-%m-%dT%H:%M:%s")
+        return product
 
     def extract_skus(self, product_variant):
         skus = []
