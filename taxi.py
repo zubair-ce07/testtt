6de6791693fd4ea451d12f3fbda4384b5301a51a
faizanbaig01 <mirza.faizan@arbisoft.com>
2019-07-18 17:32:28 +0500

    from command line

--- taxi.py
+++ taxi.py
@@ -5,50 +5,56 @@ import psutil
 
 
 class TaxiMeterApp:
-    total_fare = total_speed = wait_time = ride_time = total_distance = 0
-    moving_fare = 0.00001
+    total_speed = wait_time = ride_time = total_distance = 0
     base_fare = 80
-
+    total_fare = base_fare
+    kilometer = 0
     def ride_fare_calculation(self):
-        self.ride_time += 0.01
+        self.ride_time += 0.002
 
-        if self.total_speed <= 2:
+        if keyboard.is_pressed('up'):
+
+            self.total_speed = self.total_speed + 0.2
+
+        elif keyboard.is_pressed('down'):
+            self.total_speed -= 0.1
+
+            if self.total_speed < 0:
+                self.total_speed = 0
+
+        if self.total_speed <= 15:
             self.wait_time = self.wait_time + 0.01
 
         if self.wait_time > 15:
             self.wait_time = 0.0
             self.total_fare += 16
 
-        if keyboard.is_pressed('up'):
-
-            self.total_speed = self.total_speed + 0.05
-
-        elif keyboard.is_pressed('down') and self.total_speed != 0:
-            if self.total_speed <= 0.05:
-                self.total_speed = 0
-
-            else:
-                self.total_speed -= 0.05
-
         self.total_distance = self.total_distance + (self.total_speed * self.ride_time) / 5000
-        self.total_fare = self.total_fare + (self.moving_fare * self.total_distance)
+        self.total_fare = self.total_fare + (self.total_distance/15000)
+
+
+        if self.total_distance > 1000:
+            self.total_distance = 0;
+            self.kilometer += 1
 
         if keyboard.is_pressed('p'):
-             time.sleep(5)
+            time.sleep(5)
         if keyboard.is_pressed('e'):
             p = psutil.Process()
             p.suspend()
 
     def output(self):
-        print("Ride Time: %.2f" % self.ride_time)
-        print("Distance : %.2f Meters" % self.total_distance)
-        print("Speed: %.2f MPS" % self.total_speed)
-        print("Fare: %.2f" % self.total_fare)
+        print("Ride Time: %d Seconds" % self.ride_time)
+        print("Distance : %d KM %d Meters" % (self.kilometer, self.total_distance))
+        print("Speed: %.2f KPH" % self.total_speed)
+        print("Fare: %d Rs" % self.total_fare)
         print("Wait Time: %.2f" % self.wait_time)
 
 
-t1 = TaxiMeterApp()
+taxi_meter_app = TaxiMeterApp()
+
 while True:
-    t1.ride_fare_calculation()
-    t1.output()
+
+    taxi_meter_app.ride_fare_calculation()
+    taxi_meter_app.output()
     system('clear')
