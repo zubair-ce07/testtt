d4656434aa6ea3c1a59e11565611f7738a107a78
Faizan Baig <52661937+faizanbaig01@users.noreply.github.com>
2019-07-18 15:32:37 +0500

    datatype consistent

--- record.py
+++ record.py
@@ -6,8 +6,8 @@ class WeatherData:
     def __init__(self, data):
 
         self.date = datetime.strptime(data.get('PKT'), '%Y-%m-%d')
-        self.highest_temp = int(data["Max TemperatureC"]) if data["Max TemperatureC"] else None
-        self.lowest_temp = int(data["Min TemperatureC"]) if data["Min TemperatureC"] else None
-        self.highest_humidity = int(data["Max Humidity"]) if data["Max Humidity"] else None
-        self.avg_humidity = int(data[" Mean Humidity"]) if data[" Mean Humidity"] else None
+        self.highest_temp = int(data["Max TemperatureC"])
+        self.lowest_temp = int(data["Min TemperatureC"])
+        self.highest_humidity = int(data["Max Humidity"])
+        self.avg_humidity = int(data[" Mean Humidity"])
 
