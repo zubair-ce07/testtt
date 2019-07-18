542d8e2a91bfbb71aa0a9f0dcf2dd198b47bad80
Faizan Baig <52661937+faizanbaig01@users.noreply.github.com>
2019-07-18 16:38:00 +0500

    glob

--- weather_data.py
+++ weather_data.py
@@ -1,31 +1,25 @@
-import os
-from record import *
 import csv
-import calendar
-from datetime import datetime
+import glob
+from record import WeatherData
 
-class FileReader:
-    def __init__(self, file_path, year, month=None):
 
-        file_names = os.listdir(file_path)
-        years = []
-        self.records = []
-        type(int(year))
-        if month:
-            month = int(month)
-            month = calendar.month_abbr[month]
+class FileParser:
 
-        for file_name in file_names:
-            if year in file_name:
-                if month:
-                    if month in file_name:
-                        years.append(file_name)
-                else:
-                    years.append(file_name)
-        for file_name in years:
-            with open(file_path + file_name) as records:
-                file_reader = csv.DictReader(records)
-                for row in file_reader:
-                    new_record = WeatherData(row)
-                    self.records.append(new_record)
+    years = []
 
+    @staticmethod
+    def record_values(self, record):
+        valid_record = [record["Max TemperatureC"], record["Min TemperatureC"],
+                        record[" Mean Humidity"], record.get("PKT", record.get("PKST"))]
+        if all(valid_record):
+            return True
+
+    def file_reader(self, files_path):
+        records = []
+        files_path += f"Murree_weather_*"
+        for files in glob.glob(files_path):
+
+            with open(files, "r") as single_file:
+                file_reader = csv.DictReader(single_file)
+                records += [WeatherData(row) for row in file_reader if self.record_values(self, row)]
+        return records
