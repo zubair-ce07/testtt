9f71d114df3cbfceb9f82c052f29dc077439a27d
Uzair Amer <uzair.amer@arbisoft.com>
2019-07-18 18:28:20 +0500

    Used sep in print and minor improvement in average calculations 2

--- weatherman/reportgenerator/reportprinter.py
+++ weatherman/reportgenerator/reportprinter.py
@@ -24,8 +24,8 @@ class ReportPrinter:
         print(f'{weather_records[0].pkt.strftime("%B")} {weather_records[0].pkt.year}')
 
         for record in weather_records:
-            red_plus_sign = '+' * abs(record.max_temp) if record.max_temp else ' '
-            blue_plus_sign = '+' * abs(record.min_temp) if record.min_temp else ' '
+            red_plus_sign = '+' * abs(record.max_temp) if record.max_temp else ''
+            blue_plus_sign = '+' * abs(record.min_temp) if record.min_temp else ''
             purple_min_max_temp = self._purple_t.format(f'{record.min_temp}C - {record.max_temp}C')
             print(f'{self._purple_t.format(record.pkt.day)}', f'{self._blue_t.format(blue_plus_sign)}'
                   f'{self._red_t.format(red_plus_sign)}', f'{self._purple_t.format(purple_min_max_temp)}',
