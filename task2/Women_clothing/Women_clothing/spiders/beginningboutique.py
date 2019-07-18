883388a253230bdfa46851c5a7701071be3416ae
yusra <yusra.khalid@outlook.com>
2019-07-18 15:36:23 +0500

    Xpath changed to css

--- task2/Women_clothing/Women_clothing/spiders/beginningboutique.py
+++ task2/Women_clothing/Women_clothing/spiders/beginningboutique.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-import scrapy
 import re
 import json
+
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+
 from Women_clothing.items import BeginningboutiqueItem
 
 
@@ -17,21 +18,20 @@ class Beginningboutique(CrawlSpider):
 
     rules = (
         Rule(LinkExtractor(
-            deny='/products',
-            restrict_xpaths=["//div[@class='header-nav-wrapper']",
-                             "//ul[@class='pagination']"]),
+            restrict_css=[".header-nav-wrapper",
+                          ".pagination"]),
              callback='parse'),
 
         Rule(LinkExtractor(
-            allow='/products',
-            restrict_xpaths="//div[@id='shopify-section-collection']"),
+            restrict_css="#shopify-section-collection"),
             callback='product_parse')
     )
 
     def parse(self, response):
         for request in super(Beginningboutique, self).parse(response):
-            trail = response.meta.get('trail') if response.meta.get('trail') else []
-            request.meta['trail'] = trail + [response.request.url]
+            trail = response.meta.get('trail', [])
+            trail_name = response.css('.section-heading__heading::text').get()
+            request.meta['trail'] = trail + [trail_name, [response.url]]
             yield request
 
     def product_parse(self, response):
@@ -40,9 +40,7 @@ class Beginningboutique(CrawlSpider):
         product['retailer_sku'] = self.retailer_sku(response)
         product['category'] = self.category(response)
         product['brand'] = self.brand(response)
-        product['url'] = self.url(response)
-        product['url_original'] = self.url(response)
-        product['product_name'] = self.product_name(response)
+        product['name'] = self.product_name(response)
         product['description'] = self.description(response)
         product['care'] = self.care(response)
         product['image_urls'] = self.image_urls(response)
@@ -58,59 +56,50 @@ class Beginningboutique(CrawlSpider):
         product['gender'] = self.gender
         product['crawl_start_time'] = self.crawler.stats.get_stats()['start_time'].strftime("%Y-%m-%dT%H:%M:%s")
         product['trail'] = response.meta.get('trail')
+        product['url'] = response.url
+        product['url_original'] = response.url
+        product['industry'] = None
         return product
 
     def product_name(self, response):
         return response.css('.product-heading__title::text').get()
 
     def retailer_sku(self, response):
-        return response.xpath("//div[@class='product__heart']/div/@data-product-id").get()
+        return response.css('.product-heading *::attr(data-product-id)').get()
 
     def category(self, response):
-        return response.xpath("//div[@id='shopify-section-related-collections']//a[@href]/text()").getall()
+        return response.css('#shopify-section-related-collections * a::text').getall()
 
     def brand(self, response):
-        return response.xpath('//div[@class="product-heading"]//a/text()').get()
-
-    def url(self, response):
-        return response.url
+        return response.css('.product-heading * a::text').get()
 
     def description(self, response):
-        product_details = response.xpath("//div[@class='product__specs']//li[1]//p/text()").getall()
-        if not product_details:
-            product_details = response.xpath("//div[@class='product__specs']//li[1]/div/text()").getall()
-            detail_reference = response.xpath("//div[@class='product__specs-detail']/a[@href]/text()").getall()
-            for details in range(len(product_details), -1, -1):
-                if len(detail_reference) > details:
-                    product_details.insert(details + 1, detail_reference[details])
-        return product_details+response.xpath('//div[@class="product__specs-detail"]/ul/li/text()').getall()
+        return response.css('span:contains(DESCRIPTION) + .product__specs-detail ::text').getall()
 
     def care(self, response):
-        care = response.xpath("//ul[@class='product__specs-list']/li[2]//li/text()").getall()
-        return care or response.xpath("//div[@class='product__specs']//div/text()").getall()
+        return response.css('span:contains(FABRICATION) + .product__specs-detail ::text').getall()
 
     def image_urls(self, response):
-        return response.xpath("//div[@class='product-images-wrapper']//img/@src").getall()
+        return response.css('.product-images-wrapper * img::attr(src)').getall()
 
     def skus(self, response):
         path = "//head/script[contains(., 'window.ShopifyAnalytics.meta.currency')]/text()"
         meta = response.xpath(path).get()
-        meta_json = json.loads(re.findall(r'"variants.*"}]', meta)[0][11:])
-        for variants in meta_json:
-            variants.pop('name')
-            variants.pop('id')
-            sku_in_meta = variants.pop('sku')
-            variants['color'] = re.findall(r'-[A-Z|a-z]+', sku_in_meta)
-            variants['color'] = variants['color'][0][1:] if variants['color'] else None
-            variants['size'] = variants.pop('public_title')
-            variants['currency'] = self.currency(response)
-        return meta_json
+        variants = json.loads(re.findall(r'"variants.*"}]', meta)[0][11:])
+        sku_in_meta = variants[0].get('sku')
+        color = re.findall(r'-[A-Z|a-z]+', sku_in_meta)
+        color = color[0][1:] if color else None
+        skus = {}
+        for size in response.css('#SingleOptionSelector-0 > option::text').getall():
+            skus[f'{self.product_name(response)} - {size}'] = {'colour': color,
+                                                               'price': self.price(response),
+                                                               'currency': self.currency(response),
+                                                               'size': size,
+                                                               }
+        return skus
 
     def price(self, response):
-        try:
-            return float(response.css('.money::text').get()[1:])
-        except:
-            return response.css('.money::text').get()[1:]
+        return float(response.css('.product__price * span::text').get()[1:])
 
     def currency(self, response):
-        return response.css('.money::text').get()[0]
+        return response.css('.product__price * span::text').get()[0]
