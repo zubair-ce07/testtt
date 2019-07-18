8f7725cff4f32d42af0da5684d1b4987d335fb49
Faizan Baig <52661937+faizanbaig01@users.noreply.github.com>
2019-07-18 16:40:52 +0500

    minor changes

--- calculations.py
+++ calculations.py
@@ -4,22 +4,20 @@ from statistics import mean
 class DataCalculator:
 
     def monthly_records(self, weather_records, date):
-        return [record for record in weather_records if
-                record.date.year == date.year and
-                record.date.month == date.month]
+        return [row for row in weather_records if row.date.year == date.year and row.date.month == date.month]
 
-    def monthly_analysis(self, weather_records, date):
+    def get_max_min(self, weather_records, date):
         records = self.monthly_records(weather_records, date)
-        high_temp = mean(records.max_temp for records in records)
-        min_temp = mean(records.min_temp for records in records)
-        mean_humidity = mean(records.avg_humidity for records in records)
+        highest_temp = mean(records.highest_temp for records in records)
+        lowest_temp = mean(records.lowest_temp for records in records)
+        avg_humidity = mean(records.avg_humidity for records in records)
 
-        return [high_temp, min_temp, mean_humidity]
+        return [highest_temp, lowest_temp, avg_humidity]
 
-    def yearly_analysis(self, weather_records, year):
+    def month_average(self, weather_records, year):
         records = [record for record in weather_records if record.date.year == year]
-        max_temp_record = max(records, key=lambda record: record.max_temp)
-        min_temp_record = max(records, key=lambda record: record.min_temp)
-        max_humidity_record = max(records, key=lambda record: record.max_humidity)
+        max_temp_record = max(records, key=lambda record: record.highest_temp)
+        min_temp_record = max(records, key=lambda record: record.lowest_temp)
+        max_humidity_record = max(records, key=lambda record: record.highest_humidity)
 
         return [max_temp_record, min_temp_record, max_humidity_record]
