93f3260e164c81f739df1adc6ae24ff6d1d52cb7
yusra <yusra.khalid@outlook.com>
2019-07-18 19:30:18 +0500

    Scrapped jobs added to the database by API

--- task4/cv_maker/cv_maker_app/serializers.py
+++ task4/cv_maker/cv_maker_app/serializers.py
@@ -3,6 +3,9 @@ from cv_maker_app.models import Job
 
 
 class JobSerializer(serializers.HyperlinkedModelSerializer):
+    skill1 = serializers.CharField(allow_blank=True)
+    skill2 = serializers.CharField(allow_blank=True)
+    skill3 = serializers.CharField(allow_blank=True)
     class Meta:
         model = Job
         fields = ['title', 'city', 'experience', 'description', 'skill1', 'skill2', 'skill3']
