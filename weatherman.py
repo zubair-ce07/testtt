55bbbd43817d38abce4da2825c769a7f7efb7b80
Faizan Baig <52661937+faizanbaig01@users.noreply.github.com>
2019-07-18 16:41:58 +0500

    argument validations in parser

--- weatherman.py
+++ weatherman.py
@@ -1,34 +1,43 @@
 import argparse
-from weather_data import FileReader
-from reporter import *
+
+from datetime import datetime
+
+from calculations import DataCalculator
+from weather_data import FileParser
+from reporter import ReportGenerator
 
 
 def check_args():
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('-e', nargs='*')
-    parser.add_argument('-a', nargs='*')
-    parser.add_argument('-c', nargs='*')
-    parser.add_argument('-b', nargs='*')
-    parser.add_argument('file_path')
-    args = parser.parse_args()
+    parser.add_argument('file_path', help='The local file path of weather files')
+    parser.add_argument('-e', nargs="*", type=lambda arg: datetime.strptime(arg, '%Y'), )
+    parser.add_argument('-a', nargs="*", type=lambda arg: datetime.strptime(arg, '%Y/%m'))
+    parser.add_argument('-c', nargs="*", type=lambda arg: datetime.strptime(arg, '%Y/%m'))
+    parser.add_argument('-b', nargs="*", type=lambda arg: datetime.strptime(arg, '%Y/%m'))
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = check_args()
     if args.e:
-        for year in args.e:
-            get_max_min(FileReader(args.file_path, year))
+        for arguments in args.e:
+            outcome = DataCalculator().month_average(FileParser.file_reader(FileParser, args.file_path),arguments.date().year)
+            ReportGenerator().print_max_min(outcome)
+
     if args.a:
-        for year_month in args.a:
-            month_average(FileReader(args.file_path, year_month.split('/')[0], year_month.split('/')[1]))
+        for arguments in args.a:
+            outcome = DataCalculator().get_max_min(FileParser.file_reader(FileParser, args.file_path),arguments.date())
+            ReportGenerator().monthly_average(outcome)
+
     if args.c:
-        for year_month in args.c:
-            print_chart(FileReader(args.file_path, year_month.split('/')[0], year_month.split('/')[1]))
+        for arguments in args.c:
+            outcome = DataCalculator().monthly_records(FileParser.file_reader(FileParser, args.file_path),arguments.date())
+            ReportGenerator().print_chart(outcome)
+
     if args.b:
-        for year_month in args.b:
-            bonus_task(FileReader(args.file_path, year_month.split('/')[0], year_month.split('/')[1]))
+        for arguments in args.b:
+            outcome = DataCalculator().monthly_records(FileParser.file_reader(FileParser, args.file_path),arguments.date())
+            ReportGenerator().bonus_chart(outcome)
 
 
-def main():
-    check_args()
-
-
-if __name__ == '__main__':
-    main()
