daeefef318cadd509b61f9e4bb76157c9b578324
Ali.Azhar <ali@arbisoft.arbisoft.com>
2019-07-19 16:02:56 +0500

    using csv reader to parse weather files

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-import json
+import csv
 from weather import WeatherReading
 from column_attribute_association import associations
 
@@ -14,33 +14,25 @@ class WeatherParser():
             parameters[key] = raw_weather_readings[index]
         return parameters
 
-    def __parse_column_names(self, raw_weather_readings):
-        """Returns column names from the CSV file"""
-        column_names = raw_weather_readings[0].split(',')
-        column_names = [c.strip() for c in column_names]
-        return column_names
-
-    def __parse_rows(self, raw_weather_readings):
-        """Returns array of data rows from the CSV file"""
-        data_rows = raw_weather_readings[1:]
-        data_rows = [row.split(',') for row in data_rows]
-        return data_rows
-
     def parse_weather_file(self, file):
         """Returns WeatherReading objects after parsing the weather reading CSV file"""
         with open(file, 'r') as weather_file:
-            raw_weather_readings = weather_file.read().strip().split('\n')
+            raw_weather_readings = csv.reader(weather_file)
 
-            if raw_weather_readings[-1][0:4] == "<!--":
-                raw_weather_readings = raw_weather_readings[0:-1]
-
-            data_rows = self.__parse_rows(raw_weather_readings)
+            # skip the headers
+            next(raw_weather_readings)  
+            next(raw_weather_readings)  
 
             objects = []
 
-            for data_row in data_rows:
-                parameters = self.__get_parameters(data_row)
-                raw_weather_readings_object = WeatherReading(**parameters)
-                objects.append(raw_weather_readings_object)
+            for row in raw_weather_readings:
+                if(row):
+                    if row[0][0:4] == "<!--":
+                        continue
+
+                    parameters = self.__get_parameters(row)
+                    raw_weather_readings_object = WeatherReading(**parameters)
+                    objects.append(raw_weather_readings_object)
+
 
         return objects
