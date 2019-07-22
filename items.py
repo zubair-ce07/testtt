78d975fef1f67ba23542e0654d135f78ff5804b1
Ibraheem Muhammad Nadeem <ibraheem@A002-00138.arbisoft.com>
2019-07-22 17:56:37 +0500

    Improved structure of the code. Reduced redundant code and imroved variable names. Changed structure of some Fields in BeyondLimitsItem.

--- items.py
+++ items.py
@@ -1,19 +1,20 @@
 from scrapy import Field, Item
 
 
-class BeyondLimitItem(Field):
-    name = Field(),
-    gender = Field(),
-    description = Field(),
-    retailer_sku = Field(),
-    image_urls = Field(),
-    care = Field(),
-    url = Field(),
-    lang = Field(),
-    brand = Field(),
-    category = Field(),
-    color = Field(),
-    skus = Field(),
-    crawl_start_time = Field(),
-    time = Field(),
+class BeyondLimitItem(Item):
+    name = Field()
+    gender = Field()
+    description = Field()
+    retailer_sku = Field()
+    image_urls = Field()
+    care = Field()
+    url = Field()
+    lang = Field()
+    brand = Field()
+    category = Field()
+    sku = Field()
+    crawl_start_time = Field()
+    date = Field()
     crawl_id = Field()
+    market = Field()
+    retailer = Field()
