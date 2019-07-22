5ec6472b36c70ca5da431ea81315cf372609645b
Uzair Amer <uzair.amer@arbisoft.com>
2019-07-22 18:46:14 +0500

    Extracting color, care and genders

--- snkrs/items.py
+++ snkrs/items.py
@@ -1,24 +1,16 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class SnkrsItem(scrapy.Item):
     name = scrapy.Field()
     retailer_sku = scrapy.Field()
-    lang = scrapy.Field()
     trail = scrapy.Field()
     gender = scrapy.Field()
     category = scrapy.Field()
     brand = scrapy.Field()
     url = scrapy.Field()
     retailer = scrapy.Field()
-    url_original = scrapy.Field()
+    lang = scrapy.Field()
     description = scrapy.Field()
     care = scrapy.Field()
     image_urls = scrapy.Field()
@@ -28,9 +20,9 @@ class SnkrsItem(scrapy.Item):
 
 
 class SnkrsSkuItem(scrapy.Item):
-    colour = scrapy.Field()
+    color = scrapy.Field()
     price = scrapy.Field()
     currency = scrapy.Field()
     size = scrapy.Field()
-    size_type = scrapy.Field()
+    previous_prices = scrapy.Field()
     sku_id = scrapy.Field()
--- snkrs/snkrs_spider.py
+++ snkrs/snkrs_spider.py
@@ -1,109 +1,133 @@
+from copy import copy
+
 from scrapy import Request
 from scrapy.spiders import Spider
-
 from snkrs.items import SnkrsItem, SnkrsSkuItem
 
 
 class SnkrsSpider(Spider):
+    lang = 'en'
     name = 'snkrsspider'
-    start_urls = ['https://www.snkrs.com/en/64-adidas',
-                  'https://www.snkrs.com/en/185-adidas-performance',
-                  'https://www.snkrs.com/en/192-adidas-skateboarding',
-                  'https://www.snkrs.com/en/77-asics',
-                  'https://www.snkrs.com/en/73-carhartt-wip',
-                  'https://www.snkrs.com/en/176-chinatown-market',
-                  'https://www.snkrs.com/en/175-crep-protect',
-                  'https://www.snkrs.com/en/240-drake-cereal',
-                  'https://www.snkrs.com/en/154-converse',
-                  'https://www.snkrs.com/en/186-g-shock',
-                  'https://www.snkrs.com/en/187-hypebeast',
-                  'https://www.snkrs.com/en/65-jordan',
-                  'https://www.snkrs.com/en/177-medicom-toy',
-                  'https://www.snkrs.com/en/67-new-balance',
-                  'https://www.snkrs.com/en/66-nike',
-                  'https://www.snkrs.com/en/129-nike-sb',
-                  'https://www.snkrs.com/en/150-polar',
-                  'https://www.snkrs.com/en/68-puma',
-                  'https://www.snkrs.com/en/69-reebok',
-                  'https://www.snkrs.com/en/79-stussy',
-                  'https://www.snkrs.com/en/178-the-new-order',
-                  'https://www.snkrs.com/en/229-the-north-face',
-                  'https://www.snkrs.com/en/120-tired',
-                  'https://www.snkrs.com/en/70-vans',
-                  'https://www.snkrs.com/en/182-snkrs',
-                  'https://www.snkrs.com/en/226-91-denim']
+    possible_genders = ['Men', 'Women', 'Girl', 'Boy', 'Kids', 'Adults']
+    start_urls = ['https://www.snkrs.com/en/']
 
     def parse(self, response):
+        for url in response.css('.sf-menu a::attr(href)').extract():
+            yield Request(url=url, meta={'trail': [self._parse_page_title(response)]},
+                          callback=self.parse_product_url)
+
+    def parse_product_url(self, response):
+        trail = response.meta['trail']
+        trail.append(self._parse_page_title(response))
+
         for url in response.css('.ajax_block_product .right-block a::attr(href)').extract():
-            yield Request(url=url, meta={'trail_url': [response.request.url]}, callback=self.parse_product)
+            yield Request(url=url, meta={'trail': trail},
+                          callback=self.parse_product)
+
+        next_page = response.css('#pagination_next_bottom a::attr(href)').extract_first()
+        if next_page:
+            yield Request(url=response.urljoin(next_page), meta={'trail': trail},
+                          callback=self.parse_product)
 
     def parse_product(self, response):
         snkrs_item = SnkrsItem()
         snkrs_item['name'] = self._parse_name(response)
-        snkrs_item['retailer_sku'] = self._parse_retailer_sku(response)
-        snkrs_item['lang'] = self._parse_language(response)
+        snkrs_item['retailer_sku'] = self._sku_id_generator(response)
         snkrs_item['retailer'] = self._parse_retailer(response)
         snkrs_item['category'] = self._parse_category(response)
         snkrs_item['brand'] = self._parse_brand(response)
-        snkrs_item['url'] = response.url
-        snkrs_item['url_original'] = self._parse_url_orginal(response)
+        snkrs_item['care'] = self._parse_care(response)
         snkrs_item['description'] = self._parse_description(response)
         snkrs_item['image_urls'] = self._parse_image_urls(response)
-        snkrs_item['price'] = self._parse_price(response)
-        snkrs_item['currency'] = self._parse_currency(response)
-        snkrs_item['trail'] = [response.meta['trail_url'], [response.request.url]]
+        snkrs_item['gender'] = self._parse_gender(response).lower()
         snkrs_item['skus'] = self._parse_skus(response)
+        trail = copy(response.meta['trail'])
+        trail.append(self._parse_page_title(response))
+        snkrs_item['trail'] = trail
+        snkrs_item['lang'] = self.lang
+        snkrs_item['url'] = response.url
         yield snkrs_item
 
+    def _parse_page_title(self, response):
+        return response.css('head title::text').extract()[0], response.url
+
     def _parse_name(self, response):
-        return response.css('.pb-center-column h1::text').extract_first()
+        return response.css('.pb-center-column h1::text').extract()[0]
 
     def _parse_retailer_sku(self, response):
-        return response.css('#product_reference span::text').extract_first()
-
-    def _parse_language(self, response):
-        return response.css('html::attr(lang)').extract_first().strip()[:2]
+        return response.css('#product_reference span::text').extract()[0]
 
     def _parse_retailer(self, response):
-        return response.css('#header_logo a::attr(title)').extract_first()
+        return response.css('#header_logo a::attr(title)').extract()[0]
 
     def _parse_category(self, response):
-        return response.css('.breadcrumb li a::text').extract()[1:-1]
+        return response.css('ol.breadcrumb li a::text').extract()[1:]
+
+    def _parse_care(self, response):
+        return [c.replace('-', '').strip() for c in
+                response.css('#short_description_content p:contains("%")::text').extract()] or []
+
+    def _parse_gender(self, response):
+        name = self._parse_name(response)
+        categories = self._parse_category(response)
+        descriptions = self._parse_description(response)
+
+        for gender in self.possible_genders:
+            if gender in name:
+                return gender
+
+            for category in categories:
+                if gender in category.split(' '):
+                    return gender
+
+            for description in descriptions:
+                if gender in description.split(' '):
+                    return gender
+
+        return ''
+
+    def _parse_color(self, response):
+        return response.css('#short_description_content p:contains("Color")::text').extract()[0].split(':')[1].strip()
 
     def _parse_brand(self, response):
-        return response.css('#product_marques img::attr(alt)').extract_first()
-
-    def _parse_url_orginal(self, response):
-        return response.css('#center_column meta::attr(content)').extract_first()
+        return response.css('#product_marques img::attr(alt)').extract()[0]
 
     def _parse_description(self, response):
         return response.css('#short_description_content p::text').extract()
 
     def _parse_image_urls(self, response):
-        urls = response.css('.pb-left-column img#bigpic::attr(src)').extract()
-        urls.extend(response.css('#views_block #carrousel_frame li a::attr(href)').extract())
-        return urls
+        return response.css('#views_block #carrousel_frame li a::attr(href)').extract()
+
+    def _parse_previous_price(self, response):
+        return [p.split(' ')[0] for p in response.css('#old_price_display span::text').extract()]
 
     def _parse_price(self, response):
-        return response.css('#our_price_display::attr(content)').extract_first()
+        return response.css('#our_price_display::attr(content)').extract()[0]
 
     def _parse_currency(self, response):
-        return response.css('p.our_price_display meta::attr(content)').extract_first()
+        return response.css('p.our_price_display meta::attr(content)').extract()[0]
+
+    def _populate_sku_item(self, size, response):
+        sku_item = SnkrsSkuItem()
+        sku_item['size'] = size
+        sku_item['color'] = self._parse_color(response)
+        sku_item['price'] = self._parse_price(response)
+        sku_item['currency'] = self._parse_currency(response)
+        sku_item['sku_id'] = self._sku_id_generator(response)
+        sku_item['previous_prices'] = self._parse_previous_price(response)
+        return sku_item
 
     def _parse_skus(self, response):
-        size_types = [size_type.strip() for size_type in response.css('#view_sizes span:not(.hidden)::text').extract()]
-        sizes_response = response.css('.attribute_list li:not(.hidden)')
+        sizes_s = response.css('.attribute_list ul li span.units_container span:first-child::text').extract() or \
+                  response.css('.attribute_list li span.units_container::text').extract()
 
         skus = []
-        if sizes_response:
-            for size_response in sizes_response:
-                sku_item = SnkrsSkuItem()
-                sku_item['size_type'] = size_types
-                sku_item['size'] = [size_unit.strip()
-                                    for size_unit in size_response.css('span span::text').extract() or
-                                    size_response.css('span::text').extract()]
-                sku_item['price'] = self._parse_price(response)
-                sku_item['currency'] = self._parse_currency(response)
-                sku_item['sku_id'] = self._parse_retailer_sku(response)
-                skus.append(sku_item)
+        if sizes_s:
+            skus = [self._populate_sku_item(size.strip(), response) for size in sizes_s]
+        else:
+            skus.append(self._populate_sku_item('One Size', response))
+
         return skus
+
+    def _sku_id_generator(self, response):
+        return ''.join([n[0] for n in self._parse_name(response).split(' ')])
