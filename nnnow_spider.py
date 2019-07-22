1cb32e143e65bf332d68ce7a5cbb8d99979ef0b7
Abdullah Faiz <abdullah@A002-00094.arbisoft.com>
2019-07-22 14:40:13 +0500

    3rd task remaining changes

--- nnnow_spider.py
+++ nnnow_spider.py
@@ -1,8 +1,8 @@
 import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
-import re
 import json
+import requests
 
 
 class Nnnow(CrawlSpider):
@@ -19,20 +19,13 @@ class Nnnow(CrawlSpider):
 
     def parse_item(self, response):
         item = NnnowRecord()
-
         item['name'] = self.get_name(response)
         item['brand'] = self.get_brand(response)
-        item['price'] = self.get_price(response)
-        item['previous_prices'] = self.get_previous_prices(response)
-        item['size'] = self.get_size(response)
-        item['color'] = self.get_color(response)
         item['category'] = self.get_category(response)
         item['image_urls'] = self.get_image_urls(response)
-        item['currency'] = self.get_currency(response)
-        item['sku_id'] = self.get_sku_id(response)
         item['url'] = self.get_url(response)
-        item['care'] = self.get_care(response)
-        item['description'] = self.get_description(response)
+        item['description'] = {'description' : self.get_description(response), 'care' : self.get_care(response)}
+        item['skus'] = self.get_skus(response)
 
         return item
 
@@ -41,18 +34,6 @@ class Nnnow(CrawlSpider):
 
     def get_brand(self, response):
         return response.css('.nw-product-name .nw-product-brandtxt::text').get()
-
-    def get_price(self, response):
-        return response.css('.nw-priceblock span.nw-priceblock-amt::text').get()
-
-    def get_previous_prices(self, response):
-        return response.css('.nw-priceblock del.nw-priceblock-amt::text').get()
-
-    def get_size(self, response):
-        return response.css('.nw-sizeblock-container   button.nw-size-chip::text').getall()
-
-    def get_color(self, response):
-        return response.css('.nw-color-title span.nw-color-name::text').get()
 
     def get_category(self, response):
         return response.css('.nw-breadcrumblist-list .nw-breadcrumb-listitem::text').getall()
@@ -63,8 +44,41 @@ class Nnnow(CrawlSpider):
     def get_currency(self, response):
         return response.css('.nw-sizeblock-container  span::attr(content)').get()
 
-    def get_sku_id(self, response):
-        return response.css('.nw-sizeblock-container  .nwc-btn span::text').get()
+    def get_skus(self, response):
+        all_data = response.xpath('//script[contains(text(), "specs")]/text()').re_first("DATA= (.+)")
+        convert_data = json.loads(all_data)
+        urls = convert_data['ProductStore']['PdpData']['colors']['colors']['']
+        all_color_urls = []
+        for item in urls:
+            one_url = item['url']
+            all_color_urls.append(one_url)
+        color_ids = []
+        for item in all_color_urls:
+            url_split = item.split("-")[-1]
+            color_ids.append(url_split)
+        headers = {
+            'Origin': 'https://www.nnnow.com',
+            'Content-Type': 'application/json',
+            'module': 'odin',
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+        }
+        for item in color_ids:
+            data = {"module":"odin","styleId":""}
+            data['styleId'] = item
+            response = requests.post('https://api.nnnow.com/d/api/product/details', headers=headers, data=json.dumps(data))
+            skus_result = response.json()['data']['mainStyle']['skus']
+            color_result = response.json()['data']['colors']['selectedColor']['primaryColor']
+            sku_list = []
+            for item in skus_result:
+                if item['inStock'] == 'False':
+                    out_of_stock = 'True'
+                else:
+                    out_of_stock = 'False'
+            for item in skus_result:
+                sku = (dict({'Size': item['size'], 'skuId': item['skuId'], 'price': item['price'], 'out_of_stock': out_of_stock }))
+                sku_list.append(sku)
+            all_skus = dict({'color': color_result, 'skus': sku_list})
+            return all_skus
 
     def get_url(self, response):
         return response.css("meta[name='og_url']::attr(content)").get()
@@ -72,30 +86,22 @@ class Nnnow(CrawlSpider):
     def get_care(self, response):
         care = response.xpath('//script[contains(text(), "specs")]/text()').re_first("DATA= (.+)")
         converted_care = json.loads(care)
-
         return converted_care['ProductStore']['PdpData']['mainStyle']['finerDetails']['compositionAndCare']['list']
 
     def get_description(self, response):
         description = response.xpath('//script[contains(text(), "specs")]/text()').re_first("DATA= (.+)")
         converted_description = json.loads(description)
-
         return converted_description['ProductStore']['PdpData']['mainStyle']['finerDetails']['specs']['list']
-
 
 
 class NnnowRecord(scrapy.Item):
 
     name = scrapy.Field()
     brand = scrapy.Field()
-    price = scrapy.Field()
-    previous_prices = scrapy.Field()
-    size = scrapy.Field()
-    color = scrapy.Field()
     category = scrapy.Field()
     image_urls = scrapy.Field()
     currency = scrapy.Field()
-    sku_id = scrapy.Field()
+    skus= scrapy.Field()
     url = scrapy.Field()
     care = scrapy.Field()
     description = scrapy.Field()
-
