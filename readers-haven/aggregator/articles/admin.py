6c6ca1c5776b1a2516c0227876ee6749fed99707
Mohammad Owais Basit <owais.basit@arbisoft.com>
2019-07-23 18:39:52 +0500

    feat: display comments
    comments are displayed under articles now on the detailed article page

--- readers-haven/aggregator/articles/admin.py
+++ readers-haven/aggregator/articles/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Website, Article, Author
+from .models import Website, Article, Author, Comment
 
 admin.site.register(Website)
 admin.site.register(Author)
 admin.site.register(Article)
+admin.site.register(Comment)
