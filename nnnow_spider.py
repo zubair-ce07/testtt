b0183291269d69f34b47231019e629350bebf366
Abdullah Faiz <abdullah@A002-00094.arbisoft.com>
2019-07-23 15:14:00 +0500

    Scrapy 3rd tasks changes final

--- nnnow_spider.py
+++ nnnow_spider.py
@@ -1,32 +1,34 @@
 import scrapy
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
 import json
 import requests
+
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 
 class Nnnow(CrawlSpider):
 
     name = 'Nnnowspider'
     start_urls = ['https://www.nnnow.com/']
-    all_gender_paths = '/men', '/women', '/kids', '/footwear'
+    all_products_paths = '/men', '/women', '/kids', '/footwear'
     product_css = '.nwc-grid-col .nw-productlist '
 
     rules = (
-        Rule(LinkExtractor(allow=all_gender_paths)),
+        Rule(LinkExtractor(allow=all_products_paths)),
         Rule(LinkExtractor(restrict_css=product_css), callback='parse_item')
     )
 
     def parse_item(self, response):
+        converted_data = self.get_all_data(response)
         item = NnnowRecord()
         item['name'] = self.get_name(response)
         item['brand'] = self.get_brand(response)
         item['category'] = self.get_category(response)
         item['image_urls'] = self.get_image_urls(response)
         item['url'] = self.get_url(response)
-        item['description'] = {'description' : self.get_description(response), 'care' : self.get_care(response)}
-        item['skus'] = self.get_skus(response)
-
+        item['description'] =self.get_description(converted_data)
+        item['care'] = self.get_care(converted_data)
+        item['skus'] = self.get_skus(converted_data)
         return item
 
     def get_name(self, response):
@@ -44,18 +46,21 @@ class Nnnow(CrawlSpider):
     def get_currency(self, response):
         return response.css('.nw-sizeblock-container  span::attr(content)').get()
 
-    def get_skus(self, response):
+    def get_all_data(self, response):
         all_data = response.xpath('//script[contains(text(), "specs")]/text()').re_first("DATA= (.+)")
-        convert_data = json.loads(all_data)
-        urls = convert_data['ProductStore']['PdpData']['colors']['colors']['']
-        all_color_urls = []
+        converted_data = json.loads(all_data)
+        return converted_data
+
+    def get_skus(self, converted_data):
+        urls = converted_data['ProductStore']['PdpData']['colors']['colors']['']
+        color_ids = []
         for item in urls:
             one_url = item['url']
-            all_color_urls.append(one_url)
-        color_ids = []
-        for item in all_color_urls:
-            url_split = item.split("-")[-1]
-            color_ids.append(url_split)
+            split_url = one_url.split("-")[-1]
+            color_ids.append(split_url)
+        return self.color_requests(color_ids)
+
+    def color_requests(self, color_ids):
         headers = {
             'Origin': 'https://www.nnnow.com',
             'Content-Type': 'application/json',
@@ -66,32 +71,31 @@ class Nnnow(CrawlSpider):
             data = {"module":"odin","styleId":""}
             data['styleId'] = item
             response = requests.post('https://api.nnnow.com/d/api/product/details', headers=headers, data=json.dumps(data))
-            skus_result = response.json()['data']['mainStyle']['skus']
-            color_result = response.json()['data']['colors']['selectedColor']['primaryColor']
-            sku_list = []
-            for item in skus_result:
-                if item['inStock'] == 'False':
-                    out_of_stock = 'True'
-                else:
-                    out_of_stock = 'False'
-            for item in skus_result:
-                sku = (dict({'Size': item['size'], 'skuId': item['skuId'], 'price': item['price'], 'out_of_stock': out_of_stock }))
-                sku_list.append(sku)
+            return self.generate_skus(response)
+
+    def generate_skus(self, response):
+        skus_result = response.json()['data']['mainStyle']['skus']
+        color_result = response.json()['data']['colors']['selectedColor']['primaryColor']
+        sku_list = []
+        for item in skus_result:
+            if item['inStock'] == 'False':
+                out_of_stock = 'True'
+            else:
+                out_of_stock = 'False'
+            sku = (dict(
+                {'Size': item['size'], 'skuId': item['skuId'], 'price': item['price'], 'out_of_stock': out_of_stock}))
+            sku_list.append(sku)
             all_skus = dict({'color': color_result, 'skus': sku_list})
-            return all_skus
+        return all_skus
 
     def get_url(self, response):
         return response.css("meta[name='og_url']::attr(content)").get()
 
-    def get_care(self, response):
-        care = response.xpath('//script[contains(text(), "specs")]/text()').re_first("DATA= (.+)")
-        converted_care = json.loads(care)
-        return converted_care['ProductStore']['PdpData']['mainStyle']['finerDetails']['compositionAndCare']['list']
+    def get_care(self, converted_data):
+        return converted_data['ProductStore']['PdpData']['mainStyle']['finerDetails']['compositionAndCare']['list']
 
-    def get_description(self, response):
-        description = response.xpath('//script[contains(text(), "specs")]/text()').re_first("DATA= (.+)")
-        converted_description = json.loads(description)
-        return converted_description['ProductStore']['PdpData']['mainStyle']['finerDetails']['specs']['list']
+    def get_description(self, converted_data):
+        return converted_data['ProductStore']['PdpData']['mainStyle']['finerDetails']['specs']['list']
 
 
 class NnnowRecord(scrapy.Item):
