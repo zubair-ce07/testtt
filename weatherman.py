e3d1cdb86ae69259896d66cd35342afce86dcccf
Muhammad.Asim <asim@A002-00110.arbisoft.com>
2019-07-23 12:09:16 +0500

    Completed Yearly Report Generation

--- weatherman.py
+++ weatherman.py
@@ -1,12 +1,14 @@
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, isdir
 import pandas as pd
 from pprint import pprint
+import click
+from datetime import datetime
 
 MIN_TEMPC_COL = "Min TemperatureC"
 MAX_TEMPC_COL = "Max TemperatureC"
 MAX_HUMIDITY_COL = "Max Humidity"
-DATE_COL = "PKST"  # TODO: DATE_COL can have multiple names like PKST or PKT
+DATE_COL = "PKST"
 
 
 class WeatherMan:
@@ -35,7 +37,6 @@ class WeatherMan:
         """
         dfs = []
         for file_path in file_paths:
-            print("Reading from file", file_path)
             df = pd.read_csv(file_path, index_col=None, engine="python")
             # Renaming PKT to PKST to sync the columns over multiple files
             if "PKT" in df.columns:
@@ -60,7 +61,8 @@ class WeatherMan:
 
             return a required data in json format
         """
-        if not self.weather_files_dict[year]:
+
+        if not self.weather_files_dict.get(year):
             return
 
         file_paths = self.weather_files_dict[year]
@@ -68,12 +70,15 @@ class WeatherMan:
         self.preprocess_dataframe()
 
         # fetching required rows out of dataframe
-        max_temp_row = self.dataframe.loc[self.dataframe[MAX_TEMPC_COL].idxmax(
-        )]
-        min_temp_row = self.dataframe.loc[self.dataframe[MIN_TEMPC_COL].idxmin(
-        )]
-        max_humidity_row = self.dataframe.loc[self.dataframe[MAX_HUMIDITY_COL].idxmax(
-        )]
+        max_temp_row = self.dataframe.  \
+            loc[self.dataframe[MAX_TEMPC_COL].idxmax(
+            )]
+        min_temp_row = self.dataframe.  \
+            loc[self.dataframe[MIN_TEMPC_COL].idxmin(
+            )]
+        max_humidity_row = self.dataframe.  \
+            loc[self.dataframe[MAX_HUMIDITY_COL].idxmax(
+            )]
 
         data = {}
         # Temparature stats for given Year
@@ -102,6 +107,16 @@ class Report:
         yearlyData = self.wm.get_year_stats(year)
         self.printYearlyReport(yearlyData)
 
+    def format_date(self, date):
+        """
+            gets date in a format yyyy-mm-dd
+        """
+        year = int(date.split("-")[0])
+        month = int(date.split("-")[1])
+        day = int(date.split("-")[2])
+
+        return datetime(year, month, day).strftime("%b %d")
+
     def printYearlyReport(self, data):
         """
             data type : json
@@ -118,15 +133,41 @@ class Report:
                 }
             }
         """
+        if data is None:
+            print("Data not found for year")
+            return
 
-        print(f'Highest: {data["temp"]["max"]}C on {data["temp"]["maxDay"]}')
-        print(f'Lowest: {data["temp"]["min"]}C on {data["temp"]["minDay"]}')
-        print(f'Humid: {data["humidity"]["max"]}% on {data["temp"]["maxDay"]}')
+        print("Highest: %dC on %s" %
+              (data["temp"]["max"], self.format_date(data["temp"]["maxDay"])))
+
+        print("Lowest: %dC on %s" %
+              (data["temp"]["min"], self.format_date(data["temp"]["minDay"])))
+
+        print("Humid: %dC on %s" %
+              (
+                  data["humidity"]["max"],
+                  self.format_date(data["humidity"]["maxDay"]))
+              )
+
+
+@click.command()
+@click.option(
+    '-e',
+    nargs=1,
+    help="prints the stats for most humdity, "
+    "lowest temparature, and"
+    "highest temparture for the given year.")
+@click.argument('path_to_files')
+def argumentParser(e, path_to_files):
+    if not isdir(path_to_files):
+        print("File path of data directory is invalid.")
+        return
+
+    if(e):
+        report = Report(path_to_files)
+        report.yearlyReport(e)
+        return
 
 
 if __name__ == "__main__":
-    # getting directory
-    data_dir = 'weatherfiles/weatherfiles/'
-    report = Report(data_dir)
-    report.yearlyReport('2004')
-    # FIXME: for data where date column is PKT rather than PKST
+    argumentParser()
