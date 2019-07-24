976ea88513237aa271bb4ecf8d72ea365f58ffb8
Faizan Baig <52661937+faizanbaig01@users.noreply.github.com>
2019-07-24 18:23:40 +0500

    date broken into 2 lines

--- weather_record.py
+++ weather_record.py
@@ -3,7 +3,8 @@ from datetime import datetime
 
 class WeatherData:
     def __init__(self, data):
-        self.date = datetime.strptime(data.get("PKT", data.get("PKST")), '%Y-%m-%d')
+        date = data.get("PKT", data.get('PKST'))
+        self.date = datetime.strptime(date, '%Y-%m-%d')
         self.max_temp = int(data["Max TemperatureC"])
         self.min_temp = int(data["Min TemperatureC"])
         self.mean_temperature = float(data["Mean TemperatureC"])
