d2621d4a6c8771675c9300aa9f463f61cdfc89a5
ahmadarshi <m.ahmad@arbisoft.com>
2019-07-24 17:24:21 +0500

    refactored variables and casting

--- task1.py
+++ task1.py
@@ -13,12 +13,11 @@ class WeatherRecord:
         self.max_humidity = max_humidity
 
 
-class WeatherReportParse:
+class WeatherRecordsParser:
     required_fields = ['Max TemperatureC', 'Min TemperatureC',
                        'Max Humidity']
 
     def read_files_records(self, file_names_template):
-
         files = glob.glob(file_names_template, )
         return sum([self.get_records(weather_file)
                     for weather_file in files], [])
@@ -42,16 +41,16 @@ class WeatherReportParse:
 class CalculateWeatherResults:
 
     def calculate_extreme_report(self, records):
-        highest_temp = max(records, key=lambda x: int(x.max_temp))
-        lowest_temp = min(records, key=lambda x: int(x.min_temp))
-        max_humidity = max(records, key=lambda x: int(x.max_humidity))
+        highest_temp = max(records, key=lambda x: x.max_temp)
+        lowest_temp = min(records, key=lambda x: x.min_temp)
+        max_humidity = max(records, key=lambda x: x.max_humidity)
 
         return highest_temp, lowest_temp, max_humidity
 
     def calculate_month_report(self, records):
-        max_temps_avg = sum([int(x.max_temp) for x in records]) / len(records)
-        min_temps_avg = sum([int(x.min_temp) for x in records]) / len(records)
-        max_humidities_avg = sum([int(x.max_humidity) for x in records]) / len(records)
+        max_temps_avg = sum([x.max_temp for x in records]) / len(records)
+        min_temps_avg = sum([x.min_temp for x in records]) / len(records)
+        max_humidities_avg = sum([x.max_humidity for x in records]) / len(records)
 
         return max_temps_avg, min_temps_avg, max_humidities_avg
 
@@ -96,13 +95,13 @@ class WeatherReportsProcess():
             date_object = datetime.strptime(weather_record.date, '%Y-%m-%d')
             print(date_object.strftime('%d'), end='')
 
-            for plus_counter in range(0, int(weather_record.max_temp)):
+            for plus_counter in range(0, weather_record.max_temp):
                 print(f'\033[1;31;40m + ', end='')
 
             print(f'{weather_record.max_temp} C')
             print(date_object.strftime('%d'), end='')
 
-            for plus_counter in range(0, int(weather_record.min_temp)):
+            for plus_counter in range(0, weather_record.min_temp):
                 print(f'\033[1;34;40m - ', end='')
             print(f'{weather_record.min_temp} C')
 
@@ -116,10 +115,10 @@ class WeatherReportsProcess():
             print(f'\033[1;31;40m {date_object.strftime("%d")}', end='\t')
             print(f'\033[1;31;40m {weather_record.max_temp} C', end='')
 
-            for plus_counter in range(0, int(weather_record.max_temp)):
+            for plus_counter in range(0, weather_record.max_temp):
                 print('\033[1;31;40m + ', end='')
 
-            for plus_counter in range(0, int(weather_record.min_temp)):
+            for plus_counter in range(0, weather_record.min_temp):
                 print('\033[1;34;40m + ', end='')
 
             print(f'\033[1;34;40m {weather_record.min_temp} C')
@@ -135,41 +134,37 @@ def check_dir_path(string):
 
 def seprate_year_month(year_month):
     date = datetime.strptime(year_month, '%Y/%m')
-    return f'{date.strftime("%Y")}_{date.strftime("%b")}*'
-
-
-def year_pattren(year):
-    return f'{year}*'
+    return f'{date.strftime("%Y")}_{date.strftime("%b")}'
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Calculating and showing '
-                                                 'weather reports fetched from the data files')
-    parser.add_argument('path', type=check_dir_path, help='Directory Destinantion')
-    parser.add_argument('-e', '--year', type=year_pattren, help='Year Parameter')
-    parser.add_argument('-a', '--month', type=seprate_year_month, help='Month Parameter')
-    parser.add_argument('-c', '--graphsingle', type=seprate_year_month, help='Show single graph')
-    parser.add_argument('-m', '--graphmerged', type=seprate_year_month, help='Show merged graph')
-    args = parser.parse_args()
+    record_parser = argparse.ArgumentParser(description='Calculating and showing '
+                                                        'weather reports fetched from the data files')
+    record_parser.add_argument('path', type=check_dir_path, help='Directory Destinantion')
+    record_parser.add_argument('-e', '--year', type=str, help='Year Parameter')
+    record_parser.add_argument('-a', '--month', type=seprate_year_month, help='Month Parameter')
+    record_parser.add_argument('-c', '--graphsingle', type=seprate_year_month, help='Show single graph')
+    record_parser.add_argument('-m', '--graphmerged', type=seprate_year_month, help='Show merged graph')
+    args = record_parser.parse_args()
 
-    parser = WeatherReportParse()
+    record_parser = WeatherRecordsParser()
     process_results = WeatherReportsProcess()
-    file_names_template = args.path + '/Murree_weather_{}*'
+    file_names_template = f'{args.path}/Murree_weather_' + '{}*'
 
     if args.year:
-        records = parser.read_files_records(file_names_template.format(args.year))
+        records = record_parser.read_files_records(file_names_template.format(args.year))
         process_results.process_extreme_result(records)
 
     if args.month:
-        records = parser.read_files_records(file_names_template.format(args.month))
+        records = record_parser.read_files_records(file_names_template.format(args.month))
         process_results.process_average_result(records)
 
     if args.graphsingle:
-        records = parser.read_files_records(file_names_template.format(args.graphsingle))
+        records = record_parser.read_files_records(file_names_template.format(args.graphsingle))
         process_results.show_seprate_chart(records)
 
     if args.graphmerged:
-        records = parser.read_files_records(file_names_template.format(args.graphmerged))
+        records = record_parser.read_files_records(file_names_template.format(args.graphmerged))
         process_results.show_merge_chart(records)
 
 
