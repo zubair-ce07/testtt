90ee0c5a47e2684371250087e69e9b62a6f43e13
Faizan Baig <52661937+faizanbaig01@users.noreply.github.com>
2019-07-24 15:26:02 +0500

    consistent datatype

--- weather_record.py
+++ weather_record.py
@@ -4,8 +4,8 @@ from datetime import datetime
 class WeatherData:
     def __init__(self, data):
         self.date = datetime.strptime(data.get("PKT", data.get("PKST")), '%Y-%m-%d')
-        self.highest_temp = int(data["Max TemperatureC"])
-        self.lowest_temp = int(data["Min TemperatureC"])
-        self.avg_humidity = int(data[" Mean Humidity"])
-        self.highest_humidity = int(data["Max Humidity"])
-
+        self.max_temp = int(data["Max TemperatureC"])
+        self.min_temp = int(data["Min TemperatureC"])
+        self.mean_temperature = float(data["Mean TemperatureC"])
+        self.max_humidity = float(data["Max Humidity"])
+        self.mean_humidity = float(data[" Min Humidity"])
