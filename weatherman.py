079c99ad0a08059270003ec7c3f754403391dffb
Faizan Baig <52661937+faizanbaig01@users.noreply.github.com>
2019-07-25 14:29:57 +0500

    is_validate_path renamed to is_valid_path

--- weatherman.py
+++ weatherman.py
@@ -3,7 +3,7 @@ import os
 
 from datetime import datetime
 
-from weather_record_parser import FileParser
+from weather_parser import FileParser
 from weather_calculations import WeatherCalculations
 from weather_reporter import ReportGenerator
 
@@ -20,10 +20,10 @@ def is_valid_year_month(date):
     except Exception:
         raise argparse.ArgumentTypeError(f"Invalid Date")
 
-    return is_valid_year(date.year), date.month
+    return is_valid_year(date.year), int(date.month)
 
 
-def is_validate_path(file_path):
+def is_valid_path(file_path):
     if os.path.isdir(file_path):
         return file_path
     raise argparse.ArgumentTypeError(f"Invalid Directory")
@@ -31,7 +31,7 @@ def is_validate_path(file_path):
 
 def check_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("file_path", help="The local file path of weather files", type=is_validate_path)
+    parser.add_argument("file_path", help="The local file path of weather files", type=is_valid_path)
     parser.add_argument("-e", help="highest, lowest, highest humidity for a given year", type=is_valid_year)
     parser.add_argument("-a", help="average highest, lowest, mean humidity for a given month", type=is_valid_year_month)
     parser.add_argument("-c", help="separate bar charts for highest and lowest temp", type=is_valid_year_month)
