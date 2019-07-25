e69346d9c0c55120a949fbfb611d3b44eda564b6
Faizan Baig <52661937+faizanbaig01@users.noreply.github.com>
2019-07-25 18:06:08 +0500

    speed->TaxiRide

--- taxi_ride.py
+++ taxi_ride.py
@@ -1,14 +1,14 @@
 import keyboard
 
 
-class Speed:
+class TaxiRide:
     def __init__(self):
         self.total_speed = 0
 
     def speed_state(self):
         if keyboard.is_pressed('up'):
 
-            self.total_speed = self.total_speed + 0.2
+            self.total_speed = self.total_speed + 0.1
 
         elif keyboard.is_pressed('down'):
             self.total_speed -= 0.1
