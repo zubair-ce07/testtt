fed39619e8f1a76c3de1233ecaf8a792aa52e048
Faizan Baig <52661937+faizanbaig01@users.noreply.github.com>
2019-07-25 16:00:51 +0500

    None changed to " "

--- weather_result.py
+++ weather_result.py
@@ -1,7 +1,7 @@
 
 class WeatherResult:
-    def __init__(self, max_temp="None", max_avg_temp="None", min_temp="None", min_avg_temp="None",
-                 max_humidity="None", mean_humidity="None"):
+    def __init__(self, max_temp="", max_avg_temp="", min_temp="", min_avg_temp="",
+                 max_humidity="", mean_humidity=""):
         self.all_temp = []
         self.max_temp = max_temp
         self.min_temp = min_temp
