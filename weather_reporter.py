6b6409d9fc828e9c4bad9c96689167470fa057c7
Faizan Baig <52661937+faizanbaig01@users.noreply.github.com>
2019-07-25 14:56:57 +0500

    correct bonus outcome and color changes

--- weather_reporter.py
+++ weather_reporter.py
@@ -2,11 +2,12 @@ class ReportGenerator:
 
     low = "\033[1;34m+"
     high = "\033[1;31m+"
-    red = "\033[1;31m"
-    blue = "\033[1;34m"
+    red_color_code = "\033[1;31m"
+    purple_color_code = "\033[1;35m"
+    blue_color_code = "\033[1;34m"
 
     @staticmethod
-    def print_yearly(data):
+    def print_yearly_report(data):
         print(f"Highest: {data.max_temp.max_temp}C" + " on " 
               f"{data.max_temp.date:%B %d}")
         print(f"Lowest: {data.min_temp.min_temp}C" + " on "
@@ -15,7 +16,7 @@ class ReportGenerator:
               f"{data.max_humidity.date:%B %d}")
 
     @staticmethod
-    def print_monthly(data):
+    def print_monthly_report(data):
         print(f"Highest Average: {data.max_avg_temp.mean_temp}C")
         print(f"Lowest Average: {data.min_avg_temp.mean_temp}C")
         print(f"Average Mean Humidity: {data.mean_humidity:.0f}%")
@@ -25,15 +26,22 @@ class ReportGenerator:
         for weather_reading in data.monthly_temp:
             max_temp = weather_reading.max_temp
             min_temp = weather_reading.min_temp
-            red = ReportGenerator.red
-            blue = ReportGenerator.blue
-            print(f"{red}{weather_reading.date.day} {ReportGenerator.high * max_temp} {max_temp}C")
-            print(f"{blue}{weather_reading.date.day} {ReportGenerator.low * min_temp} {min_temp}C")
+            red_color = ReportGenerator.red_color_code
+            blue_color = ReportGenerator.blue_color_code
+            purple_color = ReportGenerator.purple_color_code
+            print(f"{purple_color}{weather_reading.date:%d}"
+                  f" {red_color}{ReportGenerator.high * max_temp} {max_temp}C")
+            print(f"{purple_color}{weather_reading.date:%d}"
+                  f" {blue_color}{ReportGenerator.low * min_temp} {min_temp}C")
 
     @staticmethod
     def print_bonus_chart(data):
         for weather_reading in data.monthly_temp:
             max_temp = weather_reading.max_temp
             min_temp = weather_reading.min_temp
-            print(f"{ReportGenerator.low * min_temp}{ReportGenerator.high * max_temp}"
-                  f" {ReportGenerator.blue}{min_temp}C - {ReportGenerator.red}{max_temp}C")
+            red_color = ReportGenerator.red_color_code
+            blue_color = ReportGenerator.blue_color_code
+            purple_color = ReportGenerator.purple_color_code
+            print(f"{purple_color}{weather_reading.date:%d} "
+                  f"{ReportGenerator.low * min_temp}{ReportGenerator.high * max_temp}"
+                  f" {purple_color}{min_temp}C - {max_temp}C")
