af8c9cef7092716e59f6ab1e6605291e33f36598
Faizan Baig <52661937+faizanbaig01@users.noreply.github.com>
2019-07-25 18:05:12 +0500

    separate methods to get distance and fare

--- calculations.py
+++ calculations.py
@@ -1,17 +1,27 @@
-class DataCalculator:
+from taxi_ride import TaxiRide
+
+
+class RideFareCalculations:
     def __init__(self):
         self.total_distance = 0
         self.total_fare = 80
-        self.ride_time = 0
+        self.kilometer = 0
         self.wait_time = 0
+        self.speed = TaxiRide()
 
-    def ride_fare_calculation(self, speed):
-        self.ride_time += 0.002
+    def get_distance(self, speed, ride_time):
+        self.total_distance += (speed * ride_time) / 5000
+        if self.total_distance > 1000:
+            self.total_distance = 0
+            self.kilometer += 1
+        return self.total_distance, self.kilometer
 
-        if speed <= 15:
-            self.wait_time = self.wait_time + 0.01
+    def get_fare(self, total_distance):
+        self.total_fare += (total_distance/15000)
+        if self.speed.speed_state() <= 15:
+            self.wait_time = self.wait_time + 0.002
+        if self.wait_time > 5:
+            self.total_fare += 16
+            self.wait_time = 0.0
 
-        self.total_distance = self.total_distance + (speed * self.ride_time) / 5000
-        self.total_fare = self.total_fare + (self.total_distance / 15000)
-
-        return self.total_distance, self.total_fare, self.ride_time, self.wait_time
+        return self.total_fare, self.wait_time
