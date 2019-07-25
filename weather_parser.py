7971b19d865e236b18c3f220789408a760c1b705
Faizan Baig <52661937+faizanbaig01@users.noreply.github.com>
2019-07-25 14:57:32 +0500

    file_reader -> parse

--- weather_parser.py
+++ weather_parser.py
@@ -1,7 +1,7 @@
 import csv
 import glob
 
-from weather_record import WeatherData
+from weather_record import WeatherRecords
 
 
 class FileParser:
@@ -11,11 +11,11 @@ class FileParser:
     def __init__(self):
         self.weather_records = []
 
-    def file_reader(self, files_path):
+    def parse(self, files_path):
         files_path += f"Murree_weather_*"
         for files in glob.glob(files_path):
             with open(files, "r") as weather_file:
                 for weather_record in csv.DictReader(weather_file):
                     if all(weather_record.get(row) for row in FileParser.required_fields):
-                        self.weather_records.append(WeatherData(weather_record))
+                        self.weather_records.append(WeatherRecords(weather_record))
         return self.weather_records
