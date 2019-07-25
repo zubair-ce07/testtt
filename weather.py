8cd068450e87820f2cb99a5491bf827897353552
imaliazhar <aliazharthegreat@live.com>
2019-07-25 16:45:38 +0500

    added check for empty values

--- weather.py
+++ weather.py
@@ -4,31 +4,17 @@ from datetime import datetime
 class WeatherReading:
     """Structure to store weather readings data"""
 
-    def __init__(self, readings_row):
-        if "PKT" in readings_row:
-            self.date = datetime.strptime(readings_row["PKT"], '%Y-%m-%d')
-        elif "PSKT" in readings_row:
-            self.date = datetime.strptime(readings_row["PSKT"], '%Y-%m-%d')
+    def _parse_numeric_value(self, row, key):
+        value = row.get(key, "")
+        return int(value) if value.isdigit() else None
 
-        self.max_temperature = int(readings_row["Max TemperatureC"])
-        self.mean_temperature = int(readings_row["Mean TemperatureC"])
-        self.min_temperature = int(readings_row["Min TemperatureC"])
-        # self.dew_point = int(readings_row["Dew PointC"])
-        # self.mean_dew_point = readings_row["MeanDew PointC"]
-        # self.min_dew_point = readings_row["Min DewpointC"]
-        self.max_humidity = int(readings_row["Max Humidity"])
-        self.mean_humidity = int(readings_row[" Mean Humidity"])
-        self.min_humidity = int(readings_row[" Min Humidity"])
-        # self.max_sea_level_pressure = readings_row[" Max Sea Level PressurehPa"]
-        # self.mean_sea_level_pressure = readings_row[" Mean Sea Level PressurehPa"]
-        # self.min_sea_level_pressure = readings_row[" Min Sea Level PressurehPa"]
-        # self.max_visibility = readings_row[" Max VisibilityKm"]
-        # self.mean_visibility = readings_row[" Mean VisibilityKm"]
-        # self.min_visibility = readings_row[" Min VisibilitykM"]
-        # self.max_wind_speed = readings_row[" Max Wind SpeedKm/h"]
-        # self.mean_wind_speed = readings_row[" Mean Wind SpeedKm/h"]
-        # self.max_gust_speed = readings_row[" Max Gust SpeedKm/h"]
-        # self.precipitation = readings_row["Precipitationmm"]
-        # self.cloud_cover = readings_row[" CloudCover"]
-        # self.event = readings_row[" Events"]
-        # self.wind_dir_degrees = readings_row["WindDirDegrees"]
+    def __init__(self, readings_row):
+        date = readings_row.get("PKT", readings_row.get("PKST"))
+        self.date = datetime.strptime(date, '%Y-%m-%d')
+
+        self.max_temperature = self._parse_numeric_value(readings_row, "Max TemperatureC")
+        self.mean_temperature= self._parse_numeric_value(readings_row, "Mean TemperatureC")
+        self.min_temperature= self._parse_numeric_value(readings_row, "Min TemperatureC")
+        self.max_humidity= self._parse_numeric_value(readings_row, "Max Humidity")
+        self.mean_humidity= self._parse_numeric_value(readings_row, " Mean Humidity")
+        self.min_humidity= self._parse_numeric_value(readings_row, " Min Humidity")
