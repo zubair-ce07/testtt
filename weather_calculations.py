492451316ed6e076c97cf745d108116695a85f71
Faizan Baig <52661937+faizanbaig01@users.noreply.github.com>
2019-07-25 15:24:58 +0500

    initialized variables on separate lines

--- weather_calculations.py
+++ weather_calculations.py
@@ -5,22 +5,24 @@ class WeatherCalculations:
     @staticmethod
     def get_weather_results(data, year, month=''):
         results = WeatherResult()
-        required_records = WeatherCalculations.get_required_records(data, year, month)
-        maximum_temperature, minimum_temperature, maximum_humidity =\
-            WeatherCalculations.get_max_min(required_records)
-        max_avg_temp, min_avg_temp, mean_humidity =\
-            WeatherCalculations.get_max_min_avg(required_records)
+        filter_records = WeatherCalculations.filter_records(data, year, month)
+        maximum_temperature = WeatherCalculations.get_max_temp(filter_records)
+        minimum_temperature = WeatherCalculations.get_min_temp(filter_records)
+        maximum_humidity = WeatherCalculations.get_max_humidity(filter_records)
+        max_avg_temp = WeatherCalculations.get_max_avg(filter_records)
+        min_avg_temp = WeatherCalculations.get_min_avg(filter_records)
+        mean_humidity = WeatherCalculations.get_avg_humidity(filter_records)
         results.max_temp = maximum_temperature
         results.min_temp = minimum_temperature
         results.max_humidity = maximum_humidity
         results.max_avg_temp = max_avg_temp
         results.min_avg_temp = min_avg_temp
         results.mean_humidity = mean_humidity
-        results.monthly_temp = required_records
+        results.monthly_temp = filter_records
         return results
 
     @staticmethod
-    def get_required_records(weather_records, year, month):
+    def filter_records(weather_records, year, month):
         required_records = []
         for row in weather_records:
             if (row.date.month == month if month else True) and row.date.year == year:
@@ -28,15 +30,26 @@ class WeatherCalculations:
         return required_records
 
     @staticmethod
-    def get_max_min(required_records):
-        max_temp_record = max(required_records, key=lambda weatherdata: weatherdata.max_temp)
-        min_temp_record = min(required_records, key=lambda weatherdata: weatherdata.min_temp)
-        max_humidity_record = max(required_records, key=lambda weatherdata: weatherdata.max_humidity)
-        return [max_temp_record, min_temp_record, max_humidity_record]
+    def get_max_temp(filter_records):
+        return max(filter_records, key=lambda weather_records: weather_records.max_temp)
 
     @staticmethod
-    def get_max_min_avg(required_records):
-        max_avg_temp = max(required_records, key=lambda weatherdata: weatherdata.mean_temp)
-        min_avg_temp = min(required_records, key=lambda weatherdata: weatherdata.mean_temp)
-        mean_humidity = sum(record.mean_humidity for record in required_records) / len(required_records)
-        return[max_avg_temp, min_avg_temp, mean_humidity]
+    def get_min_temp(filter_records):
+        return min(filter_records, key=lambda weather_records: weather_records.min_temp)
+
+    @staticmethod
+    def get_max_humidity(filter_records):
+        return max(filter_records, key=lambda weather_records: weather_records.max_humidity)
+
+    @staticmethod
+    def get_max_avg(filter_records):
+        return max(filter_records, key=lambda weather_records: weather_records.mean_temp)
+
+    @staticmethod
+    def get_min_avg(filter_records):
+        return min(filter_records, key=lambda weather_records: weather_records.mean_temp)
+
+    @staticmethod
+    def get_avg_humidity(filter_records):
+        return sum(record.mean_humidity for record in filter_records) / len(filter_records)
+
