7b2346264b154f7bf01afec6328ce79117997ce7
ahmadarshi <m.ahmad@arbisoft.com>
2019-07-25 17:49:09 +0500

    cleaned code

--- task2.py
+++ task2.py
@@ -9,18 +9,19 @@ class TaxiMeter:
     cumulative_ideal_time = 0.0
 
     def calculate_fare(self):
-        if keyboard.is_pressed('1'):  # if key 'q' is pressed
+
+        if keyboard.is_pressed('1'):
             self.speed += 0.01
             self.total_fare += (self.moving_car_fare * self.speed)
-
         elif keyboard.is_pressed('0') and self.speed != 0:
+
             if self.speed <= 0.01:
                 self.speed = 0
             else:
                 self.speed -= 0.01
                 self.total_fare += (self.moving_car_fare * self.speed)
-
         else:
+
             if self.speed == 0:
                 self.cumulative_ideal_time += 0.01
 
@@ -31,15 +32,20 @@ class TaxiMeter:
                 self.total_fare += (self.moving_car_fare * self.speed)
 
     def display_fare(self):
-        print("Speed: %.2f" % self.speed)
-        print("Total fare: %.2f" % self.total_fare)
-        print("Idle Time: %.2f" % self.cumulative_ideal_time)
+        print(f'Speed: {self.speed}')
+        print(f'Total fare: {self.total_fare}')
+        print(f'Idle Time: {self.cumulative_ideal_time}')
 
 
-taxi_meter = TaxiMeter()
+def main():
+    taxi_meter = TaxiMeter()
 
-while True:
-    taxi_meter.calculate_fare()
-    taxi_meter.display_fare()
-    system('clear')
+    while True:
+        taxi_meter.calculate_fare()
+        taxi_meter.display_fare()
+        system('clear')
+
+
+if __name__ == '__main__':
+    main()
 
