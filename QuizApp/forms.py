e32c4eb6be2a3dd13d738ff546b2c9f6ee2e6101
husnain950 <husnain95@gmail.com>
2019-07-26 11:53:42 +0500

    Deleted user change form

--- QuizApp/forms.py
+++ QuizApp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 
 from QuizApp.models import CustomUser, Question, Quiz
 
@@ -11,23 +11,14 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ('username', 'email', 'is_teacher', 'is_student')
 
 
-class CustomUserChangeForm(UserChangeForm):
-
-    class Meta(UserChangeForm):
-        model = CustomUser
-        fields = ('email',)
-
 class QuizForm(forms.ModelForm):
     class Meta:
         model = Quiz
         fields = ['name']
+
 
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
         fields = ['text']
 
-
-# class AnswerForm(forms.ModelForm):
-#     class Meta:
-#         model = Answer
