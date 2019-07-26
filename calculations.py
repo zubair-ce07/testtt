32c29da24099d48efe398f0c0ba981fc131a3a2a
Faizan Baig <52661937+faizanbaig01@users.noreply.github.com>
2019-07-26 18:22:44 +0500

    enum used, kilometer removed

--- calculations.py
+++ calculations.py
@@ -1,26 +1,33 @@
+from aenum import Enum
+
 from taxi_ride import TaxiRide
+
+
+class Numbers(Enum):
+    KILOMETER = 1000
+    TIME_PASSED = 0.002
+    FARE_INCREMENT = 16
+    SPEED_INCREMENT_DECREMENT = 1
+    REAL_FARE = 50000
 
 
 class RideFareCalculations:
     def __init__(self):
         self.total_distance = 0
         self.total_fare = 80
-        self.kilometer = 0
         self.wait_time = 0
         self.speed = TaxiRide()
 
     def get_distance(self, speed, ride_time):
-        self.total_distance += (speed * ride_time) / 5000
-        if self.total_distance > 1000:
-            self.total_distance = 0
-            self.kilometer += 1
-        return self.total_distance, self.kilometer
+        self.total_distance += (speed * ride_time)/Numbers.KILOMETER.value
+        return self.total_distance
 
-    def get_fare(self, total_distance):
-        self.total_fare += (total_distance/15000)
-        if self.speed.speed_state() <= 15:
-            self.wait_time = self.wait_time + 0.002
-        if self.wait_time > 5:
-            self.total_fare += 16
-            self.wait_time = 0.0
+    def get_fare(self, speed, total_distance):
+        if speed <= 0.5:
+            self.wait_time += Numbers.TIME_PASSED.value
+            if self.wait_time > 10:
+                self.wait_time = 0
+                self.total_fare += Numbers.FARE_INCREMENT.value
+        else:
+            self.total_fare += total_distance/Numbers.REAL_FARE.value
         return self.total_fare, self.wait_time
