ce66df47bfd0fea921e8fc912b3670d0c3c635a8
husnain950 <husnain95@gmail.com>
2019-07-26 11:55:05 +0500

    Removed commented code

--- QuizApp/admin.py
+++ QuizApp/admin.py
@@ -2,12 +2,6 @@ from django.contrib import admin
 
 from QuizApp.models import *
 
-# class CustomUserAdmin(UserAdmin):
-#     add_form = CustomUserCreationForm
-#     form = CustomUserChangeForm
-#     model = CustomUser
-#     list_display = ['email',]
-
 admin.site.register(CustomUser)
 admin.site.register(Quiz)
 admin.site.register(Question)
