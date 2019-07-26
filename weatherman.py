dc12f9654145f6b1f1719824b98299fb3f9b5518
rabiapannu <rabia.pannu@arbisoft.com>
2019-07-26 15:24:30 +0500

    weatherman.py modified

--- weatherman.py
+++ weatherman.py
@@ -3,154 +3,162 @@ import glob
 from datetime import datetime
 import argparse
 
+RED_TEXT = "\033[1;31m"
+BLUE_TEXT = "\033[1;34m"
+WHITE_TEXT = "\033[0;37m"
+
 
 class ReportGenerator:
-    def __init__(self, weather_record):
-        self.weather_record = weather_record
+    def __init__(self, weather_record, year, month='*'):
+        self.year = year
+        self.month = month
+        self.weather_record = []
+        if self.month == '*':
+            for r in weather_record.records:
+                if r.PKT.year == int(self.year):
+                    self.weather_record.append(r)
+        else:
+            for r in weather_record.records:
+                if r.PKT.year == int(self.year) and r.PKT.month == int(self.month):
+                    self.weather_record.append(r)
         self.cal = Calculations()
 
     def report_for_year(self):
         self.cal.calculations_for_year(self.weather_record)
-        print("Highest: %dC on %s" % (self.cal.maxTemp.MaxTemperature, self.cal.maxTemp.PKT.strftime('%B %-d')))
-        print("Lowest: %dC on %s" % (self.cal.minTemp.MinTemperature, self.cal.minTemp.PKT.strftime('%B %-d')))
-        print("Humidity: {}% on {}".format(self.cal.maxHumidity.MaxHumidity, self.cal.maxHumidity.PKT.strftime('%B %-d')))
+        print(f"Highest: {self.cal.maxTemp.MaxTemperature}C on {self.cal.maxTemp.PKT.strftime('%B %-d')}")
+        print(f"Lowest: {self.cal.minTemp.MinTemperature}C on {self.cal.minTemp.PKT.strftime('%B %-d')}")
+        print(f"Humidity: {self.cal.maxHumidity.MaxHumidity}% on {self.cal.maxHumidity.PKT.strftime('%B %-d')}")
 
     def report_for_month(self):
         self.cal.calculations_for_month(self.weather_record)
-        print("Highest Average: {}C".format(self.cal.avgHighestTemp))
-        print("Lowest Average: {}C".format(self.cal.avgLowestTemp))
-        print("Average Mean Humidity: {}%".format(self.cal.avgMeanHumidity))
+        print(f"Highest Average: {self.cal.avgHighestTemp}C")
+        print(f"Lowest Average: {self.cal.avgLowestTemp}C")
+        print(f"Average Mean Humidity: {self.cal.avgMeanHumidity}%")
 
     def bar_charts(self):
-        for r in self.weather_record.records:
+        for r in self.weather_record:
             max_t = r.MaxTemperature
             min_t = r.MinTemperature
             if max_t:
-                bar = ''
-                for i in range(0, self.cal.take_mod(max_t)):
-                    bar += '+'
-                print(r.PKT.strftime('%d') + "\033[1;31m " + bar + "\033[0;37m " + str(r.MaxTemperature)+"C")
+                bar = "+" * abs(max_t)
+                print(r.PKT.strftime('%d') + RED_TEXT + bar + WHITE_TEXT + str(r.MaxTemperature)+"C")
             if min_t:
-                bar = ''
-                for i in range(0, self.cal.take_mod(min_t)):
-                    bar += '+'
-                print(r.PKT.strftime('%d') + "\033[1;34m " + bar + "\033[0;37m " + str(r.MinTemperature)+"C")
+                bar = "+" * abs(min_t)
+                print(r.PKT.strftime('%d') + BLUE_TEXT + bar + WHITE_TEXT + str(r.MinTemperature)+"C")
 
 
 class Calculations:
 
     def __init__(self):
-        # for yearly reports
+        # FOR yearly reports
         self.maxTemp = None
         self.minTemp = None
         self.maxHumidity = None
-        # for monthly reports
+        # FOR monthly reports
         self.avgHighestTemp = None
         self.avgLowestTemp = None
         self.avgMeanHumidity = None
 
     def calculations_for_year(self, weather_record):
-        self.maxTemp = max([r for r in weather_record.records if r.MaxTemperature is not None],
+        self.maxTemp = max([r for r in weather_record if r.MaxTemperature],
                            key=lambda x: x.MaxTemperature)
-        self.minTemp = min([r for r in weather_record.records if r.MinTemperature is not None],
+        self.minTemp = min([r for r in weather_record if r.MinTemperature],
                            key=lambda x: x.MinTemperature)
-        self.maxHumidity = max([r for r in weather_record.records if r.MaxHumidity is not None],
+        self.maxHumidity = max([r for r in weather_record if r.MaxHumidity],
                                key=lambda x: x.MaxHumidity)
 
     def calculations_for_month(self, weather_record):
-        readings1 = [r.MaxTemperature for r in weather_record.records if r.MaxTemperature is not None]
-        self.avgHighestTemp = sum(readings1)/float(len(readings1))
-        readings2 = [r.MinTemperature for r in weather_record.records if r.MinTemperature is not None]
-        self.avgLowestTemp = sum(readings2)/float(len(readings2))
-        readings3 = [r.MeanHumidity for r in weather_record.records if r.MeanHumidity is not None]
-        self.avgMeanHumidity = sum(readings3)/float(len(readings3))
-
-    def take_mod(self, x):
-        if x < 0:
-            return x * -1
-        else:
-            return x
+        readings1 = [r.MaxTemperature for r in weather_record if r.MaxTemperature]
+        self.avgHighestTemp = sum(readings1)//len(readings1)
+        readings2 = [r.MinTemperature for r in weather_record if r.MinTemperature]
+        self.avgLowestTemp = sum(readings2)//len(readings2)
+        readings3 = [r.MeanHumidity for r in weather_record if r.MeanHumidity]
+        self.avgMeanHumidity = sum(readings3)//len(readings3)
 
 
 class WeatherData:
 
     def __init__(self, row):
-        if row.get("PKT"):
+        if row.get('PKT'):
             self.PKT = datetime.strptime(row.get('PKT'), '%Y-%m-%d')
-        elif row.get("PKST"):
+        elif row.get('PKST'):
             self.PKT = datetime.strptime(row.get('PKST'), '%Y-%m-%d')
-        if row['Max TemperatureC']:
-            self.MaxTemperature = int(row['Max TemperatureC'])
-        else:
-            self.MaxTemperature = None
-        if row['Mean TemperatureC']:
-            self.MeanTemperature = int(row['Mean TemperatureC'])
-        else:
-            self.MeanTemperature = None
-        if row['Min TemperatureC']:
-            self.MinTemperature = int(row['Min TemperatureC'])
-        else:
-            self.MinTemperature = None
-        if row['Max Humidity']:
-            self.MaxHumidity = int(row['Max Humidity'])
-        else:
-            self.MaxHumidity = None
-        if row[' Mean Humidity']:
-            self.MeanHumidity = int(row[' Mean Humidity'])
-        else:
-            self.MeanHumidity = None
-        if row[' Min Humidity']:
-            self.MinHumidity = int(row[' Min Humidity'])
-        else:
-            self.MinHumidity = None
+        self.MaxTemperature = int(row['Max TemperatureC'])
+        self.MeanTemperature = int(row['Mean TemperatureC'])
+        self.MinTemperature = int(row['Min TemperatureC'])
+        self.MaxHumidity = int(row['Max Humidity'])
+        self.MeanHumidity = int(row[' Mean Humidity'])
+        self.MinHumidity = int(row[' Min Humidity'])
 
 
 class WeatherRecord:
 
-    def __init__(self, year, month='*'):
-        self.year = year
-        self.month = month
+    def __init__(self, path):
         self.records = []
 
-        for file in glob.glob("weatherfiles/Murree_weather_"+self.year+"_"+self.month+".txt"):
-            for row in csv.DictReader(open(file)):
-                self.records.append(WeatherData(row))
+        for file_name in glob.glob(f"{path}/Murree_weather_{'*'}_{'*'}.txt"):
+            for row in csv.DictReader(open(file_name)):
+                if self.is_valid_row(row):
+                    self.records.append(WeatherData(row))
+
+    @staticmethod
+    def is_valid_row(row):
+        if not row.get('PKT') and not row.get('PKST'):
+            return False
+        if not row.get('Max TemperatureC'):
+            return False
+        if not row['Mean TemperatureC']:
+            return False
+        if not row.get('Min TemperatureC'):
+            return False
+        if not row.get('Max Humidity'):
+            return False
+        if not row.get(' Mean Humidity'):
+            return False
+        if not row.get(' Min Humidity'):
+            return False
+        return True
 
 
-if __name__ == '__main__':
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-p')
+    arg_parser.add_argument('-e', nargs='*', type=lambda x: datetime.strptime(x, '%Y'),
+                            help="Enter a year to get the max temp, min temp and max humidity")
+    arg_parser.add_argument('-a', nargs='*', type=lambda x: datetime.strptime(x, '%Y/%m'),
+                            help="Enter a year/month to get the average highest temp, "
+                                 "average lowest temp and average mean humidity")
+    arg_parser.add_argument('-c', nargs='*', type=lambda x: datetime.strptime(x, '%Y/%m'),
+                            help="Enter a year/month to generate bar charts for each day's highest and lowest temp")
+    args = arg_parser.parse_args()
 
-    argParser = argparse.ArgumentParser()
-    argParser.add_argument('-e', nargs='*')
-    argParser.add_argument('-a', nargs='*')
-    argParser.add_argument('-c', nargs='*')
-    args = argParser.parse_args()
+    weather_record = WeatherRecord(args.p)
 
-    if(args.e):
+    if args.e:
         for arg in args.e:
-            print("For {}:".format(arg))
-            weather_record = WeatherRecord(year=arg)
-            report = ReportGenerator(weather_record)
+            print("For {}:".format(arg.year))
+            report = ReportGenerator(weather_record, year=str(arg.year))
             report.report_for_year()
             print("\n")
 
-    if(args.a):
+    if args.a:
         for arg in args.a:
-            print("For {}:".format(arg))
-            year, month = arg.split("/")
-            date = datetime(int(year), int(month), 20)
-            month = date.strftime('%b')
-            weather_record = WeatherRecord(year, month)
-            report = ReportGenerator(weather_record)
+            my_date = str(arg.year)+"/"+str(arg.month)
+            print(f"For {my_date}")
+            year, month = my_date.split("/")
+            report = ReportGenerator(weather_record, year, month)
             report.report_for_month()
             print("\n")
 
-    if(args.c):
+    if args.c:
         for arg in args.c:
-            print("For {}:".format(arg))
-            year, month = arg.split("/")
-            date = datetime(int(year), int(month), 20)
-            month = date.strftime('%b')
-            weather_record = WeatherRecord(year, month)
-            report = ReportGenerator(weather_record)
+            my_date = str(arg.year)+"/"+str(arg.month)
+            print(f"For {my_date}")
+            year, month = my_date.split("/")
+            report = ReportGenerator(weather_record, year, month)
             report.bar_charts()
             print("\n")
+
+
+if __name__ == '__main__':
+    main()
