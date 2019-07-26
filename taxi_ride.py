ddc978da7a310ccaa9ad4916d9a5f8e1315fcdc8
Faizan Baig <52661937+faizanbaig01@users.noreply.github.com>
2019-07-26 18:22:59 +0500

    psutil added

--- taxi_ride.py
+++ taxi_ride.py
@@ -1,15 +1,29 @@
+import time
 import keyboard
+import psutil
 
 
 class TaxiRide:
     def __init__(self):
         self.total_speed = 0
+        self.speed_increment_decrement = 1
+        self.delay = 0.1
 
-    def speed_state(self):
+    def get_speed(self):
         if keyboard.is_pressed('up'):
-            self.total_speed = self.total_speed + 0.1
+            self.total_speed = self.total_speed + self.speed_increment_decrement
+            time.sleep(self.delay)
         elif keyboard.is_pressed('down'):
-            self.total_speed -= 0.1
+            self.total_speed -= self.speed_increment_decrement
+            time.sleep(self.delay)
             if self.total_speed < 0:
                 self.total_speed = 0
         return self.total_speed
+
+    @staticmethod
+    def get_taxi_state():
+        if keyboard.is_pressed('p'):
+            time.sleep(5)
+        if keyboard.is_pressed('e'):
+            stop_ride = psutil.Process()
+            stop_ride.suspend()
