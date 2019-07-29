975d159bd9deb8ee46c91f13899947ae14f2fe5b
Abdullah Zafar <abdullah@A002-00080.arbisoft.com>
2019-07-29 17:27:53 +0500

    Use `DOTENV` instead of keeping a credentials file for DB

--- nodejs-practice/admin_panel/database/configuration.js
+++ nodejs-practice/admin_panel/database/configuration.js
@@ -1,7 +1,5 @@
-const credentials = require("./credentials.js");
-
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${credentials.username}:${credentials.password}@cluster0-ttgom.mongodb.net/test?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}`;
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 module.exports = client;
