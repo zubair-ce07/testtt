04fe6e8882f28e6542cfa4c78a1b1ca124745ce1
Arslan Shafiq <arslan.shafiq@arbisoft.com>
2019-07-29 16:03:34 +0500

    added argument details

--- Task_2/spider.py
+++ Task_2/spider.py
@@ -1,15 +1,35 @@
 from time import time
 from threading import Thread
 from urllib.parse import urlparse
+import argparse
 
 from crawl_worker import CrawlWorker
 
+
+def setup_arguments():
+    parser = argparse.ArgumentParser(description='URL data monitor.')
+
+    parser.add_argument('-download_delay', type=int, default=100, \
+        help='Time delay in ms for each worker to hit URL.')
+
+    parser.add_argument('-page_count', type=int, default=15, \
+        help='Total number of URLs to hit.')
+
+    parser.add_argument('-c_requests', type=int, default=5, \
+        help='Total number of cuncurrent requests')
+
+    return parser.parse_args()
 
 def create_worker():
     worker = CrawlWorker()
     worker.load_url()
 
 def main():
+    args = setup_arguments()
+    CrawlWorker.cuncurrent_request_allowed = args.c_requests
+    CrawlWorker.download_delay = args.download_delay
+    CrawlWorker.total_pages_to_load = args.page_count
+
     start_time = time()
     CrawlWorker()
     url = urlparse('https://arbisoft.com/')
