eaecb48b003f9090b1bd252190b71711c8099395
Arslan Shafiq <arslan.shafiq@arbisoft.com>
2019-07-29 15:41:48 +0500

    initial code commit

---
+++ Task_2/crawl_worker.py
@@ -0,0 +1,65 @@
+import queue
+from urllib.parse import urlparse
+import requests
+from parsel import Selector
+
+
+SUCCESS_RESPONSE_CODE = 200
+
+class CrawlWorker:
+
+    total_bytes_downloaded = 0
+    total_pages_to_load = 15
+    loading_request_made = 0
+    already_visisted_urls = []
+    cuncurrent_request_allowed = 5
+    cuncurrent_request_made = 0
+    page_loaded_successfully = 0
+    url_queue = None
+    download_delay = 1
+
+    def __init__(self):
+        CrawlWorker.cuncurrent_request_made += 1
+        if not CrawlWorker.url_queue:
+            CrawlWorker.url_queue = queue.Queue(maxsize=CrawlWorker.total_pages_to_load)
+
+
+
+    def __del__(self):
+        CrawlWorker.cuncurrent_request_made -= 1
+
+    def load_url(self):
+        if CrawlWorker.url_queue.qsize() == 0:
+            return
+        CrawlWorker.loading_request_made += 1
+        url = CrawlWorker.url_queue.get()
+        CrawlWorker.url_queue.task_done()
+        CrawlWorker.already_visisted_urls.append(url.geturl())
+        print("Loading URL: " + url.geturl())
+
+        response = requests.get(url.geturl())
+        if response.status_code == SUCCESS_RESPONSE_CODE:
+            CrawlWorker.total_bytes_downloaded += len(response.content)
+            CrawlWorker.page_loaded_successfully += 1
+            if CrawlWorker.page_loaded_successfully >= CrawlWorker.total_pages_to_load:
+                return
+
+            selector = Selector(response.text)
+            href_links = selector.xpath('//a/@href').getall()
+            try:
+                for link in href_links:
+                    new_url = urlparse(link)
+                    if (not new_url.geturl() in CrawlWorker.already_visisted_urls) \
+                        and (new_url.netloc == url.netloc) \
+                        and new_url:
+                        CrawlWorker.url_queue.put(new_url, False)
+
+            except queue.Full:
+                pass
+            finally:
+                if CrawlWorker.total_pages_to_load < CrawlWorker.loading_request_made:
+                    self.load_url()
+        else:
+            if CrawlWorker.total_pages_to_load <= CrawlWorker.loading_request_made:
+                CrawlWorker.loading_request_made -= 1
+                self.load_url()
---
+++ Task_2/spider.py
@@ -0,0 +1,34 @@
+from time import time
+from threading import Thread
+from urllib.parse import urlparse
+
+from crawl_worker import CrawlWorker
+
+
+def create_worker():
+    worker = CrawlWorker()
+    worker.load_url()
+
+def main():
+    '''doc string'''
+
+    start_time = time()
+    CrawlWorker()
+    url = urlparse('https://arbisoft.com/')
+    CrawlWorker.url_queue.put(url, False)
+    while True:
+        if CrawlWorker.cuncurrent_request_made < CrawlWorker.cuncurrent_request_allowed \
+            and CrawlWorker.loading_request_made < CrawlWorker.total_pages_to_load:
+            worker_thread = Thread(target=create_worker)
+            worker_thread.start()
+
+        if CrawlWorker.page_loaded_successfully >= CrawlWorker.total_pages_to_load:
+            print("page loaded: " + str(CrawlWorker.page_loaded_successfully))
+            print("Total downloaded size: " + str(CrawlWorker.total_bytes_downloaded))
+            print("Total time taken: %s" % (time() - start_time))
+            break
+
+
+if __name__ == "__main__":
+
+    main()
