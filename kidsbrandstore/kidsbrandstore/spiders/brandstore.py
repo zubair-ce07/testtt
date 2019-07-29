cc49a3792b549308f9abda8a99d4d0b699674852
asadali145 <asad.ali@arbisoft.com>
2019-07-29 19:07:57 +0500

    pycodestyle changes

--- kidsbrandstore/kidsbrandstore/spiders/brandstore.py
+++ kidsbrandstore/kidsbrandstore/spiders/brandstore.py
@@ -18,22 +18,22 @@ class BrandStoreSpider(CrawlSpider):
     def parse(self, response):
         product_urls = response.xpath("//div[@class='bottom-product-grid']/a/@href").getall()
         for url in product_urls:
-            yield scrapy.Request(response.urljoin(url).replace(':443',''), callback=self.parse_item)
+            yield scrapy.Request(response.urljoin(url).replace(':443', ''), callback=self.parse_item)
 
         for href in response.xpath('//a/@href').getall():
-            yield scrapy.Request(response.urljoin(href).replace(':443',''), self.parse)
+            yield scrapy.Request(response.urljoin(href).replace(':443', ''), self.parse)
 
     def parse_item(self, response):
         product = KidsbrandstoreItem()
         product['retailer_sku'] = response.css('span.product_id::text').extract_first()
         product['category'] = response.css('span.category::text').extract_first()
         product['brand'] = response.css('span.brand::text').extract_first()
-        product['url'] = response.url   #response.css('span.url::text').extract_first()
+        product['url'] = response.url
         product['name'] = response.css('span.name::text').extract_first()
         product['description'] = response.css('span.description::text').extract_first()
         product['care'] = (response.xpath('//div[@class="product-information-wrapper"]/p/text()').extract()[-1]
-                        if response.xpath('//div[@class="product-information-wrapper"]/p/text()').extract()
-                        else None)
+                           if response.xpath('//div[@class="product-information-wrapper"]/p/text()').extract()
+                           else None)
         product['image_urls'] = response.xpath('//figure//a//img/@src').extract()
         product['skus'] = []
         product['price'] = response.css('span.price::text').extract_first()
