88e45c75facd7a9e1e7a1f87485e7c35533d3d34
ahmadarshi <m.ahmad@arbisoft.com>
2019-07-30 18:49:49 +0500

    refactored request and removed duplicate requests

--- task4_Cea.py
+++ task4_Cea.py
@@ -48,20 +48,19 @@ class CeaSpider(CrawlSpider):
                   '{}&sc=1'
     image_t = 'https://cea.vteximg.com.br/arquivos/ids/{}.jpg'
     listing_css = '.header_submenu_item-large'
-    parse_product_css = '.product-details_name'
+    product_css = '.product-details_name'
 
     rules = (
         Rule(LinkExtractor(restrict_css=listing_css), callback='parse_paginantion'),
-        Rule(LinkExtractor(restrict_css=parse_product_css), callback='parse_item'),
+        Rule(LinkExtractor(restrict_css=product_css), callback='parse_item'),
     )
 
     def parse_paginantion(self, response):
-        script = response.css('script:contains("pagecount")')
-        page_count = int(script.re_first('pagecount_\d+ = (.+?);'))
-        page_url_parameters = '{}{{}}'.format(script.re_first(".load\(\'(.+?)\' +"))
+        pagination_sel = response.css('script:contains("pagecount")')
+        page_count = int(pagination_sel.re_first('pagecount_\d+ = (.+?);'))
+        pagination_url = pagination_sel.re_first(".load\(\'(.+?)\' +")
 
-        return [response.follow(url=page_url_parameters.format(page_number),
-                                callback=self.parse)
+        return [response.follow(url=f'{pagination_url}{page_number}', callback=self.parse)
                 for page_number in range(1, page_count)]
 
     def parse_item(self, response):
@@ -73,21 +72,29 @@ class CeaSpider(CrawlSpider):
         item['url'] = self.product_url(response)
         item['brand'] = self.product_brand()
         item['description'] = self.product_description(response)
-        item['image_urls'] = []
         item['care'] = []
-        item['skus'] = {}
-        item['requests'] = self.colour_requests(response)
+
+        item['skus'] = self.product_skus(response)
+        item['image_urls'] = []
+        item['requests'] = self.colour_requests(response, item)
 
         return self.next_request_or_item(item)
 
-    def parse_color(self, response):
-        return response.follow(url=self.get_image_url(response), callback=self.get_imagesid,
-                               meta={'response': response})
+    def colour_requests(self, response, item):
+        requests = [response.follow(self.image_url_t.format(item['retailer_sku']),
+                                    callback=self.parse_images, meta={'item': item})]
+        color_queue = response.css('.img-wrapper ::attr(href)').getall()
+        for color_url in color_queue:
+            requests.append(response.follow(color_url, callback=self.parse_product))
 
-    def parse_sku(self, response):
+        return requests
+
+    def parse_product(self, response):
         item = response.meta['item']
         item['skus'].update(self.product_skus(response))
-        return self.next_request_or_item(item)
+        product_id = self.retailer_sku(response)
+        return response.follow(self.image_url_t.format(product_id),
+                               callback=self.parse_images, meta={'item': item})
 
     def product_skus(self, response):
         skus = {}
@@ -96,25 +103,34 @@ class CeaSpider(CrawlSpider):
         for sku in raw_json.get('skus'):
             color = sku.get('dimensions').get('Cor')
             size = sku.get('dimensions').get('Tamanho')
-            skus.update({f'{color}_{size}': {
+            skus[f'{color}_{size}'] = {
                 'out_of_stock': not sku.get('available'),
                 'price': sku.get('bestPrice'),
                 'previous_price': sku.get('listPrice'),
                 'size': size,
                 'color': color,
-            }})
+            }
 
         return skus
 
+    def parse_images(self, response):
+
+        item = response.meta['item']
+        raw_images_json = json.loads(response.text)[0]['items'][0]
+
+        item['image_urls'].extend([self.image_t.format(i["imageId"])
+                                   for i in raw_images_json['images']])
+
+        return self.next_request_or_item(item)
+
     def raw_product(self, response):
-        return json.loads(response.css('script:contains("var skuJson_0")').re("var skuJson_0 "
-                                                                              "= (.+?);")[0])
+        raw_css = 'script:contains("var skuJson_0")'
+        raw_product = response.css(raw_css).re_first('var skuJson_0 = (.+?);')
+        return json.loads(raw_product)
 
     def retailer_sku(self, response):
         raw_json = self.raw_product(response)
-        if raw_json.get('productId'):
-            return raw_json.get('productId')
-        raise Exception('Product not available')
+        return raw_json['productId']
 
     def product_name(self, response):
         return response.css('title::text').get()
@@ -140,32 +156,10 @@ class CeaSpider(CrawlSpider):
     def product_description(self, response):
         return clean(response.css('.productDescription::text').get())
 
-    def colour_requests(self, response):
-        requests = [response.follow(response.url, callback=self.parse_color, dont_filter=True)]
-        color_queue = response.css('.img-wrapper ::attr(href)').getall()
-
-        for color_url in color_queue:
-            requests.append(response.follow(color_url, callback=self.parse_color))
-
-        return requests
-
-    def get_image_url(self, response):
-        product_id = self.retailer_sku(response)
-        return self.image_url_t.format(product_id)
-
-    def get_imagesid(self, response):
-        item = response.meta['response'].meta['item']
-        raw_images_json = json.loads(response.text)
-
-        item['image_urls'].extend([self.image_t.format(i["imageId"])
-                                   for i in raw_images_json[0]['items'][0]['images']])
-
-        return self.parse_sku(response.meta['response'])
-
     def next_request_or_item(self, item):
         if item['requests']:
             request = item['requests'].pop()
-            request.meta.update({'item': item})
+            request.meta['item'] = item
             return request
         item.pop('requests', None)
         return item
