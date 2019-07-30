9f4b4f07717cc10b752ee2ba086f73f2789f3f48
faizanbaig01 <mirza.faizan@arbisoft.com>
2019-07-30 18:32:38 +0500

    calculation functions

--- taxi.py
+++ taxi.py
@@ -1,34 +1,41 @@
-import os
-import time
-from taxi_ride import TaxiRide
-from calculations import FareCalculator, Numbers
+from enum import Enum
 
 
-class TaxiMeterApp:
-    ride_time = 0
+class Numbers(Enum):
+    FARE_WAIT_TIME = 5
+    FARE_DISTANCE = 15
+    TIME_PASSED = 0.05
+    DISTANCE_KM = 1000
+    WAIT_MIN = 60
 
+
+class TaxiMeter:
     def __init__(self):
-        self.speed = TaxiRide()
-        self.fare_calculator = FareCalculator()
+        self.total_distance = 0
+        self.total_fare = 80
+        self.wait_time = 0
+        self.ride_time = 0
+        self.taxi_speed = 0
 
-    def get_results(self):
+    def calculate_ride_time(self):
         self.ride_time += Numbers.TIME_PASSED.value
-        total_distance = self.fare_calculator.get_distance(self.speed.get_speed(), self.ride_time)
-        total_fare= self.fare_calculator.get_fare(total_distance, self.ride_time)
-        wait_time = self.fare_calculator.get_wait_time(self.speed.get_speed())
-        taxi_meter_app.print_results(total_distance, total_fare, wait_time)
 
-    def print_results(self, total_distance, total_fare, wait_time):
-        print(f'Ride Time: {round(self.ride_time)} Seconds')
-        print(f'Distance : {round(total_distance/Numbers.KILOMETER.value)} KM {round(total_distance)} M')
-        print(f'Speed: {round(self.speed.get_speed())} KPH')
-        print(f'Fare: {round(total_fare)} Rs')
-        print(f'Wait Time: {round(wait_time)} Seconds')
+    def calculate_wait_time(self):
+        if self.taxi_speed <= 10:
+            self.wait_time += Numbers.TIME_PASSED.value
 
+    def calculate_distance(self):
+        self.total_distance += self.taxi_speed * Numbers.TIME_PASSED.value
 
-taxi_meter_app = TaxiMeterApp()
-while True:
-    taxi_meter_app.get_results()
-    TaxiRide.get_taxi_state()
-    os.system('clear')
+    def calculate_fair(self):
+        distance_fair = (self.total_distance * Numbers.FARE_DISTANCE.value)/Numbers.DISTANCE_KM.value
+        wait_time_fair = (self.wait_time * Numbers.FARE_WAIT_TIME.value)/Numbers.WAIT_MIN.value
+        self.total_fare = distance_fair + wait_time_fair
 
+    def print_results(self):
+        print(f'Ride_Time: {round(self.ride_time)} Seconds')
+        print(f'Distance: {round(self.total_distance//1000)} KM '
+              f'{round(self.total_distance % 1000)} M')
+        print(f'Speed: {round(self.taxi_speed)} MPS')
+        print(f'Fare: {round(self.total_fare)} Rs')
+        print(f'Wait_Time: {round(self.wait_time)} Seconds')
