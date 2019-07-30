6a0813135b78aed011d4dda63f25bb00ef2c2069
asadali145 <asad.ali@arbisoft.com>
2019-07-30 16:45:16 +0500

    Used css selectors for extracting URLs and structured the code

--- kidsbrandstore/kidsbrandstore/items.py
+++ kidsbrandstore/kidsbrandstore/items.py
@@ -1,15 +1,7 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
 class KidsbrandstoreItem(scrapy.Item):
-    # define the fields for your item here like:
     retailer_sku = scrapy.Field()
     gender = scrapy.Field()
     category = scrapy.Field()
--- kidsbrandstore/kidsbrandstore/spiders/brandstore.py
+++ kidsbrandstore/kidsbrandstore/spiders/brandstore.py
@@ -11,54 +11,93 @@ class BrandStoreSpider(CrawlSpider):
     start_urls = ['http://kidsbrandstore.de/']
 
     rules = (
-        Rule(LinkExtractor(allow="https://kidsbrandstore\.de/[\w-]*/", deny='\.html'), callback='parse'),
-        Rule(LinkExtractor(allow="https://kidsbrandstore\.de/[\w-]*", deny='\.html'), callback='parse_item'),
+        Rule(LinkExtractor(deny_extensions=['html'], restrict_css=(".paginationControl",
+                           "div[id=bottom-types]", "div[id=bottom-brands]"))),
+        Rule(LinkExtractor(restrict_css=".bottom-product-grid"), callback='parse_item'),
         )
-
-    def parse(self, response):
-        product_urls = response.xpath("//div[@class='bottom-product-grid']/a/@href").getall()
-        for url in product_urls:
-            yield scrapy.Request(response.urljoin(url).replace(':443', ''), callback=self.parse_item)
-
-        for href in response.xpath('//a/@href').getall():
-            yield scrapy.Request(response.urljoin(href).replace(':443', ''), self.parse)
 
     def parse_item(self, response):
         product = KidsbrandstoreItem()
-        product['retailer_sku'] = response.css('span.product_id::text').extract_first()
-        product['category'] = response.css('span.category::text').extract_first()
-        product['brand'] = response.css('span.brand::text').extract_first()
-        product['url'] = response.url
-        product['name'] = response.css('span.name::text').extract_first()
-        product['description'] = response.css('span.description::text').extract_first()
-        product['care'] = (response.xpath('//div[@class="product-information-wrapper"]/p/text()').extract()[-1]
-                           if response.xpath('//div[@class="product-information-wrapper"]/p/text()').extract()
-                           else None)
-        product['image_urls'] = response.xpath('//figure//a//img/@src').extract()
-        product['skus'] = []
-        product['price'] = response.css('span.price::text').extract_first()
-        product['currency'] = response.css('span.price_currency_code::text').extract_first()
+        product['retailer_sku'] = self.parse_retailer_sku(response)
+        product['category'] = self.parse_category(response)
+        product['brand'] = self.parse_brand(response)
+        product['url'] = self.parse_url(response)
+        product['name'] = self.parse_name(response)
+        product['description'] = self.parse_description(response)
+        product['care'] = self.parse_care(response)
+        product['image_urls'] = self.parse_image_urls(response)
+        product['skus'] = self.parse_skus(response)
+        product['price'] = self.parse_price(response)
+        product['currency'] = self.parse_currency(response)
+        product['gender'] = self.parse_gender(response)
+        yield product
 
-        product_list = "".join(response.xpath('//div[@id="product-list-also-check"]/p/a/text()').extract())
-        if response.xpath('//svg').extract():
-            product['gender'] = "boys/girls"
+    def parse_retailer_sku(self, response):
+        return response.css('.product_id::text').extract_first()
+
+    def parse_category(self, response):
+        return response.css('.category::text').extract_first()
+
+    def parse_brand(self, response):
+        return response.css('.brand::text').extract_first()
+
+    def parse_url(self, response):
+        return response.url
+
+    def parse_name(self, response):
+        return response.css('.name::text').extract_first()
+
+    def parse_description(self, response):
+        return response.css('.description::text').extract_first()
+
+    def parse_care(self, response):
+        return (response.css('div[class=product-information-wrapper] p::text').extract()[-3]
+                if response.css('div[class=product-information-wrapper] p::text').extract()
+                else None)
+
+    def parse_image_urls(self, response):
+        return response.css('figure a img::attr(src)').extract()
+
+    def parse_price(self, response):
+        return response.css('.price::text').extract_first()
+
+    def parse_currency(self, response):
+        return response.css('.price_currency_code::text').extract_first()
+
+    def parse_gender(self, response):
+        product_list = "".join(response.css('div[id=product-list-also-check] p a::text').extract())
+        if response.css('svg').extract():
+            return "boys/girls"
         elif "jungen" in product_list:
-            product['gender'] = "boys"
+            return "boys"
         elif "mädchen" in product_list:
-            product['gender'] = "girls"
+            return "girls"
         else:
-            product['gender'] = "unisex-kids"
+            return "unisex-kids"
 
-        color = (response.css('span.desktop-header::text').extract_first().split('-')[-1].replace('.', '').replace(' ', '')
-                 if response.css('span.desktop-header::text').extract() else None)
-        sizes = response.xpath('//label[@class="attribute-title"]/text()').extract()
+    def parse_color(self, response):
+        if response.css('.desktop-header::text').extract():
+            title = response.css('.desktop-header::text').extract_first()
+            return title.split('-')[-1].replace('.', '').replace(' ', '')
+        else:
+            return None
+
+    def parse_sizes(self, response):
+        return response.css('.attribute-title::text').extract()
+
+    def parse_skus(self, response):
+        skus = []
+        color = self.parse_color(response)
+        sizes = self.parse_sizes(response)
+        price = self.parse_price(response)
+        currency = self.parse_currency(response)
 
         for size in sizes:
-            product['skus'].append({
+            skus.append({
                 "colour": color,
-                "price": product['price'],
-                "currency": product['currency'],
+                "price": price,
+                "currency": currency,
                 "size": size,
                 "sku_id": f'{color}_{size}'
-                })
-        yield product
+            })
+        return skus
