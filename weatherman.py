bbb7461cabaf4825b5b1a638ad63dcb37103076b
Mareena.Nazir <mareena@A002-00008.arbisoft.com>
2019-07-31 15:45:47 +0500

    Updated weatherman

--- weatherman.py
+++ weatherman.py
@@ -1,13 +1,9 @@
 import csv
 import glob
-import calendar
 import argparse
 
 from datetime import datetime
 from colorama import Fore, Style
-
-
-FILE_NAME_T = "{path}Murree_weather_{date}.txt"
 
 
 class Validator:
@@ -41,137 +37,141 @@ class ReportPrinter:
 
     @staticmethod
     def print_year_report(max_temp, min_temp, max_humidity):
-        max_temp = "Highest: {}C on {}".format(max_temp["max_temp"],
-                                               max_temp["date"].strftime("%B %d")
-                                               )
-        min_temp = "Lowest: {}C on {}".format(min_temp["min_temp"],
-                                              min_temp["date"].strftime("%B %d")
-                                              )
-        max_humidity = "Humidity: {}% on {}".format(max_humidity["max_humidity"],
-                                                    max_humidity["date"].strftime("%B %d")
-                                                    )
+        max_temp = "Highest: {}C on {}".format(
+            max_temp["max_temp"],
+            max_temp["date"].strftime("%B %d")
+        )
+        min_temp = "Lowest: {}C on {}".format(
+            min_temp["min_temp"],
+            min_temp["date"].strftime("%B %d")
+        )
+        max_humidity = "Humidity: {}% on {}".format(
+            max_humidity["max_humidity"],
+            max_humidity["date"].strftime("%B %d")
+        )
         print(max_temp, min_temp, max_humidity, sep="\n")
 
     @staticmethod
-    def print_single_bar_chart_report(daily_data):
-        print(daily_data["date"].strftime("%d")
+    def print_single_bar_chart_report(record):
+        print(record["date"].strftime("%d")
               + Fore.RED
               + " "
-              + "+" * daily_data["min_temp"]
+              + "+" * record["min_temp"]
               + Fore.BLUE
-              + "+" * (daily_data["max_temp"] - daily_data["min_temp"])
+              + "+" * (record["max_temp"] - record["min_temp"])
               + Style.RESET_ALL
-              + str(daily_data["min_temp"])
+              + str(record["min_temp"])
               + " - "
-              + str(daily_data["max_temp"]))
+              + str(record["max_temp"]))
 
     @staticmethod
-    def print_double_bar_chart_report(daily_data):
-        print(Fore.RED + "{} {} {}C".format(daily_data["date"].strftime("%d"),
-                                            "+" * daily_data["max_temp"],
-                                            daily_data["max_temp"]
-                                            )
+    def print_double_bar_chart_report(record):
+        print(Fore.RED + "{} {} {}C".format(
+            record["date"].strftime("%d"),
+            "+" * record["max_temp"],
+            record["max_temp"]
+        )
               )
-        print(Fore.BLUE + "{} {} {}C".format(daily_data["date"].strftime("%d"),
-                                             "+" * daily_data["min_temp"],
-                                             daily_data["min_temp"]
-                                             )
+        print(Fore.BLUE + "{} {} {}C".format(
+            record["date"].strftime("%d"),
+            "+" * record["min_temp"],
+            record["min_temp"]
+        )
               )
         print(Style.RESET_ALL)
 
 
 class ReportGenerator:
 
-    @staticmethod
-    def generate_month_report(file_name, path):
-        records = ReportReader.read_month_records(file_name, path)
+    def __init__(self, path):
+        self.report_reader = ReportReader(path)
+
+    def generate_month_report(self, date):
+        records = self.report_reader.read_records(date)
         if not records:
             return
+
         length = len(records)
-        avg_max_temp = round(sum(daily_data["max_temp"] for daily_data in records) / length)
-        avg_min_temp = round(sum(daily_data["min_temp"] for daily_data in records) / length)
-        avg_mean_humidity = round(sum(daily_data["mean_humidity"] for daily_data in records) / length)
+        avg_max_temp = round(sum(record["max_temp"] for record in records) / length)
+        avg_min_temp = round(sum(record["min_temp"] for record in records) / length)
+        avg_mean_humidity = round(sum(record["mean_humidity"] for record in records) / length)
         ReportPrinter.print_month_report(avg_max_temp, avg_min_temp, avg_mean_humidity)
 
-    @staticmethod
-    def generate_year_report(given_date, path):
-        records = ReportReader.read_year_records(given_date, path)
+    def generate_year_report(self, year):
+        records = self.report_reader.read_records(year)
         if not records:
             return
+
         max_temp = max(records, key=lambda x: x['max_temp'])
         min_temp = min(records, key=lambda x: x['min_temp'])
         max_humidity = max(records, key=lambda x: x['max_humidity'])
         ReportPrinter.print_year_report(max_temp, min_temp, max_humidity)
 
-    @staticmethod
-    def generate_single_bar_chart_report(file_name, path):
-        records = ReportReader.read_month_records(file_name, path)
+    def generate_single_bar_chart_report(self, date):
+        records = self.report_reader.read_records(date)
         if not records:
             return
+
         for record in records:
             ReportPrinter.print_single_bar_chart_report(record)
 
-    @staticmethod
-    def generate_double_bar_chart_report(file_name, path):
-        records = ReportReader.read_month_records(file_name, path)
+    def generate_double_bar_chart_report(self, date):
+        records = self.report_reader.read_records(date)
         if not records:
             return
+
         for record in records:
             ReportPrinter.print_double_bar_chart_report(record)
 
-    @staticmethod
-    def generate_reports(arguments):
-        path = arguments.path
+    def generate_reports(self, arguments):
         if arguments.a:
-            ReportGenerator.generate_month_report(arguments.a, path)
+            self.generate_month_report(arguments.a)
+
         if arguments.b:
-            ReportGenerator.generate_single_bar_chart_report(arguments.b, path)
+            self.generate_single_bar_chart_report(arguments.b)
+
         if arguments.c:
-            ReportGenerator.generate_double_bar_chart_report(arguments.c, path)
+            self.generate_double_bar_chart_report(arguments.c)
+
         if arguments.e:
-            ReportGenerator.generate_year_report(arguments.e, path)
+            self.generate_year_report(arguments.e)
 
 
 class ReportReader:
 
-    @staticmethod
-    def get_files(path):
-        return [file for file in glob.glob(path + "**/*.txt", recursive=True)]
+    def __init__(self, path):
+        self.path = path
+        self.files = [file_ for file_ in glob.glob(path + "**/*.txt", recursive=True)]
 
     @staticmethod
-    def read_daily_records(daily_record):
-        if daily_record["Max TemperatureC"] and daily_record["Min TemperatureC"] \
-                and daily_record["Max Humidity"] and daily_record[" Mean Humidity"]:
+    def read_daily_records(record):
+        if record["Max TemperatureC"] and record["Min TemperatureC"]\
+                and record["Max Humidity"] and record[" Mean Humidity"]:
             record = {
-                "date": datetime.strptime(daily_record["PKT"], "%Y-%m-%d"),
-                "max_temp": int(daily_record["Max TemperatureC"]),
-                "min_temp": int(daily_record["Min TemperatureC"]),
-                "max_humidity": int(daily_record["Max Humidity"]),
-                "mean_humidity": int(daily_record[" Mean Humidity"])
+                "date": datetime.strptime(record["PKT"], "%Y-%m-%d"),
+                "max_temp": int(record["Max TemperatureC"]),
+                "min_temp": int(record["Min TemperatureC"]),
+                "max_humidity": int(record["Max Humidity"]),
+                "mean_humidity": int(record[" Mean Humidity"])
             }
+
             return record
 
-    @staticmethod
-    def read_month_records(date, path):
+    def read_records(self, date):
         records = []
-        file_name = FILE_NAME_T.format(path=path, date=date)
-        if file_name in ReportReader.get_files(path):
-            with open(file_name) as weather_file:
+        
+        file_names = [file_ for file_ in self.files if date in file_]
+        for file_ in file_names:
+            with open(file_) as weather_file:
+
                 reader = csv.DictReader(weather_file)
                 for row in reader:
-                    daily_data = ReportReader.read_daily_records(row)
-                    if daily_data:
-                        records.append(daily_data)
-            return records
+                    record = ReportReader.read_daily_records(row)
 
-    @staticmethod
-    def read_year_records(year, path):
-        records = []
-        for i in range(1, 13):
-            date = "{}_{}".format(year, calendar.month_abbr[i])
-            records = ReportReader.read_month_records(date, path)
-            if records:
-                records.extend(records)
+                    if record:
+                        records.append(record)
+
+            records.extend(records)
         return records
 
 
@@ -183,14 +183,16 @@ def get_arguments_list():
     parser.add_argument("-c", type=Validator.valid_date)
     parser.add_argument("-e", type=Validator.valid_year)
     args = parser.parse_args()
+
     if not (args.a or args.b or args.c or args.e):
         parser.error('No arguments provided.')
+
     return args
 
 
 def main():
     arguments = get_arguments_list()
-    report_generator = ReportGenerator()
+    report_generator = ReportGenerator(arguments.path)
     report_generator.generate_reports(arguments)
 
 
