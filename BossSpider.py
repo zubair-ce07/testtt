d354859a8bfdc281facb0d70f2871d00e10883fd
ahmadarshi <m.ahmad@arbisoft.com>
2019-07-31 16:18:21 +0500

    improved requesting and parsing

--- BossSpider.py
+++ BossSpider.py
@@ -1,35 +1,34 @@
 import re
 
-import scrapy
+from scrapy import Item, Field
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider
 from scrapy.spiders import Rule
 
 
-def clean(raw_data):
-    if isinstance(raw_data, list):
-        return [re.sub('\s+', ' ', data).strip() for data in raw_data
-                if re.sub('\s+', ' ', data).strip()]
-    elif isinstance(raw_data, str):
-        return re.sub('\s+', ' ', raw_data).strip()
-    return ''
+def clean(raw_strs):
+    if isinstance(raw_strs, list):
+        cleaned_strs = [re.sub('\s+', ' ', st).strip() for st in raw_strs]
+        return [st for st in cleaned_strs if st]
+    elif isinstance(raw_strs, str):
+        return re.sub('\s+', ' ', raw_strs).strip()
 
 
-class BossItem(scrapy.Item):
-    retailer_sku = scrapy.Field()
-    gender = scrapy.Field()
-    name = scrapy.Field()
-    category = scrapy.Field()
-    url = scrapy.Field()
-    brand = scrapy.Field()
-    description = scrapy.Field()
-    care = scrapy.Field()
-    image_urls = scrapy.Field()
-    stock = scrapy.Field()
-    color = scrapy.Field()
-    price = scrapy.Field()
-    skus = scrapy.Field()
-    requests = scrapy.Field()
+class BossItem(Item):
+    retailer_sku = Field()
+    gender = Field()
+    name = Field()
+    category = Field()
+    url = Field()
+    brand = Field()
+    description = Field()
+    care = Field()
+    image_urls = Field()
+    stock = Field()
+    color = Field()
+    price = Field()
+    skus = Field()
+    requests = Field()
 
 
 class BossSpider(CrawlSpider):
@@ -37,7 +36,7 @@ class BossSpider(CrawlSpider):
     allowed_domains = ['hugoboss.com']
     start_urls = [
         'https://www.hugoboss.com/uk/home'
-    ]
+      ]
 
     listing_css = '.main-header'
     product_css = '.product-tile__link'
@@ -51,7 +50,6 @@ class BossSpider(CrawlSpider):
 
     def parse_item(self, response):
         item = BossItem()
-
         item['retailer_sku'] = self.retailer_sku(response)
         item['gender'] = self.product_gender(response)
         item['name'] = self.product_name(response)
@@ -60,17 +58,72 @@ class BossSpider(CrawlSpider):
         item['brand'] = self.product_brand(response)
         item['description'] = self.product_description(response)
         item['care'] = self.product_care(response)
-        item['image_urls'] = self.images_url(response)
-        item['skus'] = {}
-        item['requests'] = self.colors_queues(response)
 
+        item['image_urls'] = self.images_url(response)
+        item['skus'] = self.product_skus(response)
+
+        item['requests'] = self.colour_requests(response)
+        
         return self.next_request_or_item(item)
 
+    def colour_requests(self, response):
+        color_urls = response.css('.swatch-list__image--is-large ::attr(href)').getall()
+        return [response.follow(url, callback=self.parse_color)
+                for url in color_urls if url not in response.url]
+
+    def parse_color(self, response):
+        item = response.meta['item']
+        item['image_urls'] += self.images_url(response)
+        item['skus'].update(self.product_skus(response))
+        return self.next_request_or_item(item)
+
+    def product_skus(self, response):
+        sizes_sel = response.css('.product-stage__choose-size--container a')
+        color = clean(response.css('.product-stage__control-item__label'
+                                   '--variations ::text').getall()[2])
+        common_sku = {
+            'color': color,
+            'price': clean(response.css('.product-price--price-sales ::text').get()),
+            'previous_price': clean(response.css('.product-price--price-'
+                                                 'standard s ::text').getall())
+        }
+        skus = {}
+        if not sizes_sel:
+            size = 'One_Size'
+            key = f'{color}_{size}'
+            skus[key] = {
+                'out_of_stock': False,
+                'size': size
+            }
+            skus[key].update(common_sku)
+
+            return skus
+
+        for size_sel in sizes_sel:
+            size = clean(size_sel.css('span ::text').getall())[0]
+            key = f'{color}_{size}'
+            skus[key] = {
+                'out_of_stock': not bool(size_sel.css('::attr(href)').get()),
+                'size': size
+            }
+            skus[key].update(common_sku)
+
+        return skus
+
+    def next_request_or_item(self, item):
+        if item['requests']:
+            request = item['requests'].pop()
+            request.meta['item'] = item
+            return request
+
+        item.pop('requests', None)
+        return item
+
     def retailer_sku(self, response):
-        return response.css('script[type="text/javascript"]').re("productSku\":\"(.+?)\"")[0]
+        return response.css('script:contains("productSku")').re_first("productSku\":\"(.+?)\"")
 
     def product_gender(self, response):
-        return response.css('script[type="text/javascript"]').re("productGender\":\"(.+?)\"")[0]
+        return response.css('script:contains("productGender")').re_first("productGender\":\"(.+?)\"")
 
     def product_name(self, response):
         return response.css('.font__h2 ::text').get()
@@ -82,7 +135,7 @@ class BossSpider(CrawlSpider):
         return response.url
 
     def product_brand(self, response):
-        return response.css('meta[itemprop= "brand"] ::attr(content)').get() or "BOSS"
+        return response.css('[itemprop= "brand"] ::attr(content)').get() or "BOSS"
 
     def product_description(self, response):
         return clean(response.css('.description div ::text').get())
@@ -92,63 +145,4 @@ class BossSpider(CrawlSpider):
 
     def images_url(self, response):
         return response.css('.slider-item--thumbnail-image ::attr(src)').getall()
-
-    def colors_queues(self, response):
-        color_queue = response.css('.swatch-list__image--is-large ::attr(href)').getall()
-        return [response.follow(color_url, callback=self.parse_size)
-                for color_url in color_queue]
-
-    def parse_size(self, response):
-
-        item = response.meta['item']
-        sizes = response.css('.product-stage__choose-size--container a')
-
-        for a in sizes:
-            if a.css('::attr(href)'):
-                item['requests'].append(response.follow(a.css('::attr(href)').get(), callback=self.parse_sku))
-            else:
-                item['requests'].append(
-                    response.follow(response.url, callback=self.parse_sku, dont_filter=True,
-                                    meta={'size': clean(a.css('.swatch-list__size ::text').get())}))
-
-        return self.next_request_or_item(item)
-
-    def parse_sku(self, response):
-        size = None
-        out_of_stock = False
-        if 'size' in response.meta:
-            size = response.meta['size']
-            out_of_stock = True
-
-        item = response.meta['item']
-        color = clean(response.css('.product-stage__control-item__label--variations '
-                                   '::text').getall()[2])
-        price = clean(response.css('.product-price--price-sales ::text').get())
-        previous_price = clean(response.css('.product-price--price-standard s ::text').getall())
-        if not size:
-            size = self.product_size(response)
-
-        item['skus'].update({f'{color}_{size}': {
-            'color': color,
-            'price': price,
-            'out_of_stock': out_of_stock,
-            'previous_price': previous_price,
-            'size': size
-        }})
-
-        return self.next_request_or_item(item)
-
-    def product_size(self, response):
-        size = clean(response.css('.product-stage__control-item__selcted-size ::text').get())
-        if size:
-            return size
-        return 'One Size'
-
-    def next_request_or_item(self, item):
-        if item['requests']:
-            request = item['requests'].pop()
-            request.meta.update({'item': item})
-            return request
-        item.pop('requests', None)
-        return item
 
