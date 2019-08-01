4ceec2003d80b931d9362830543bf4e635d2138b
Rehan.Ahmad <rehan@A002-00007.arbisoft.com>
2019-08-01 18:08:05 +0500

    fixed db file

--- django_practice/django_practice/settings.py
+++ django_practice/django_practice/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = 'django_practice.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/home/rehan/PycharmProjects/db.sqlite3',
     }
 }
 
