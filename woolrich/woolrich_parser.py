5cbc396ce69d1042505de8c8e48b6400d9e7ecba
Uzair Amer <uzair.amer@arbisoft.com>
2019-08-01 14:13:29 +0500

    Separated crawling and parsing spiders

--- woolrich/woolrich_parser.py
+++ woolrich/woolrich_parser.py
@@ -1,15 +1,21 @@
 from re import sub
 
-
+from scrapy import Request
+from scrapy.spiders import Spider
 from woolrich.items import WoolrichItem
 
 
-class WoolrichParser:
+class WoolrichParser(Spider):
     market = 'UK'
     currency = 'GBP'
     brand = 'woolrich'
-    name = 'woolrichspider'
+    name = 'woolrichparser'
     retailer = 'woolrich-uk'
+
+    SIZE_BACKLOG = 'size_backlog'
+    COLOUR_BACKLOG = 'colour_backlog'
+    ITEM = 'item'
+
     genders = [
         ('women', 'women'),
         ('female', 'women'),
@@ -26,101 +32,124 @@ class WoolrichParser:
         ('adults', 'unisex-adults'),
     ]
 
-    @classmethod
-    def init_item(cls, response):
+    def parse(self, response):
         item = WoolrichItem()
         item['url'] = response.url
-        item['market'] = cls.market
-        item['retailer'] = cls.retailer
-        item['trail'] = response.meta['trail']
-        item['name'] = cls.get_name(response)
-        item['care'] = cls.get_care(response)
-        item['image_urls'] = cls.get_image_urls(response)
-        item['retailer_sku'] = cls.get_product_id(response)
-        item['gender'] = cls.get_gender(response)
-        item['description'] = cls.get_description(response)
-        item['category'] = cls.get_categories(response)
-        return item
+        item['market'] = self.market
+        item['retailer'] = self.retailer
+        item['trail'] = response.meta.get('trail', [])
+        item['name'] = self.get_name(response)
+        item['care'] = self.get_care(response)
+        item['image_urls'] = self.get_image_urls(response)
+        item['retailer_sku'] = self.get_product_id(response)
+        item['gender'] = self.get_gender(response)
+        item['description'] = self.get_description(response)
+        item['category'] = self.get_categories(response)
+        colour_backlog = self.get_colour_backlog(response)
 
-    @staticmethod
-    def add_trail(response):
+        params = {}
+        params[self.ITEM] = item
+        params[self.COLOUR_BACKLOG] = colour_backlog
+        params[self.SIZE_BACKLOG] = []
+
+        return Request(url=colour_backlog[0], meta=params, callback=self.parse_colour_sizes)
+
+    def parse_colour_sizes(self, response):
+        meta = response.meta
+        colour_backlog, item, size_backlog = meta[self.COLOUR_BACKLOG], meta[self.ITEM], meta[self.SIZE_BACKLOG]
+        size_backlog.extend(self.get_size_backlog(response))
+        colour_backlog = colour_backlog[1:]
+
+        params = {}
+        params[self.ITEM] = item
+        params[self.SIZE_BACKLOG] = size_backlog
+
+        if colour_backlog:
+            params[self.COLOUR_BACKLOG] = colour_backlog
+            yield Request(url=colour_backlog[0], meta=params, callback=self.parse_colour_sizes)
+        else:
+            yield Request(url=size_backlog[0][0], meta=params, callback=self.parse_skus)
+
+    def parse_skus(self, response):
+        size_backlog, item = response.meta[self.SIZE_BACKLOG], response.meta[self.ITEM]
+
+        skus = item.get('skus', [])
+        skus_per_size = self.get_sku(response, {'size': size_backlog[0][1]})
+        skus.append(skus_per_size)
+
+        item['skus'] = skus
+        item['image_urls'].extend(self.get_image_urls(response))
+
+        size_backlog = size_backlog[1:]
+        if size_backlog:
+            params = {}
+            params[self.ITEM] = item
+            params[self.SIZE_BACKLOG] = size_backlog
+            yield Request(url=size_backlog[0][0], meta=params, callback=self.parse_skus)
+        else:
+            item['image_urls'] = set(item['image_urls'])
+            yield item
+
+    def add_trail(self, response):
         trail = (response.css('head title::text').get(), response.url)
         return [*response.meta['trail'], trail] if response.meta.get('trail') else [trail]
 
-    @staticmethod
-    def get_page_meta(response):
-        css = '.search-result-content::attr({})'
-        page_result = response.css(f'{css.format("data-pagesize")}, {css.format("data-searchcount")}').getall()
-        return list(map(int, page_result))
-
-    @staticmethod
-    def get_in_stock(response):
+    def get_in_stock(self, response):
         in_stock = response.css('.in-stock-msg::text').get()
         return int(sub(r'\sitem\sleft', '', in_stock)) if in_stock else 0
 
-    @staticmethod
-    def get_name(response):
+    def get_name(self, response):
         return response.css('#product-content .product-name::text').get().strip()
 
-    @staticmethod
-    def get_size_backlog(response):
+    def get_size_backlog(self, response):
         css = '.swatches.size .swatchanchor::{}'
         sizes = [s.strip() for s in response.css(css.format('text')).getall()]
         urls = response.css(css.format('attr(href)')).getall()
         return list(zip(urls, sizes))
 
-    @staticmethod
-    def get_colour_backlog(response):
+    def get_colour_backlog(self, response):
         return response.css('.swatches.color .swatchanchor::attr(href)').getall()
 
-    @staticmethod
-    def get_product_id(response):
-        return response.css('.sku::attr(skuid)').get().strip()
+    def get_product_id(self, response):
+        product_id = sub(r'.*dwvar_', '', response.url)
+        return sub(r'_color.*', '', product_id)
 
-    @staticmethod
-    def get_categories(response):
+    def get_categories(self, response):
         return response.css('.breadcrumb-element::text').getall()[:-1]
 
-    @staticmethod
-    def get_description(response):
+    def get_description(self, response):
         descriptions = response.css('.description::text').getall()
         return [description.strip() for description in descriptions]
 
-    @staticmethod
-    def get_care(response):
+    def get_care(self, response):
         return [care.strip() for care in response.css('.fit-content::text').getall()]
 
-    @staticmethod
-    def get_image_urls(response):
+    def get_image_urls(self, response):
         css = '#thumbnails .carousel-container-inner a::attr(href)'
         return response.css(css).getall()
 
-    @classmethod
-    def get_gender(cls, response):
+    def get_gender(self, response):
         genders_candidate = ' '.join([response.url]
                                      + [url for title, url in response.meta['trail']]
                                      ).lower()
-        for tag, gender in cls.genders:
+        for tag, gender in self.genders:
             if tag in genders_candidate:
                 return gender
         return 'unisex-adults'
 
-    @classmethod
-    def get_sku(cls, response, additional_attrs):
+    def get_sku(self, response, additional_attrs):
         colour_css = '.swatches.color .selected .swatch::text'
         price_css = '#product-content .price-sales::text'
-        sku_item = {
-            'currency': cls.currency,
-            'colour': response.css(colour_css).get().strip(),
-            'price': int(response.css(price_css).get()[1:].replace('.', '')),
-            **additional_attrs
-        }
+        sku_item = additional_attrs
+        sku_item['currency'] = self.currency,
+        sku_item['colour'] = response.css(colour_css).get().strip()
+        sku_item['price'] = int(response.css(price_css).get()[1:].replace('.', ''))
 
         previous_prices = response.css('#product-content .price-sales::text').getall()
         if previous_prices:
             previous_prices = [int(p[1:].replace('.', '')) for p in previous_prices]
             sku_item['previous_prices'] = previous_prices
 
-        sku_item['sku_id'] = cls.get_product_id(response)
-        sku_item['in_stock'] = cls.get_in_stock(response)
+        sku_item['sku_id'] = f'{sku_item["colour"].replace(" ", "-")}_{sku_item["size"]}'.lower()
+        sku_item['in_stock'] = self.get_in_stock(response)
         return sku_item
--- woolrich/woolrich_spider.py
+++ woolrich/woolrich_spider.py
@@ -1,78 +1,44 @@
-from math import ceil
-from urllib.parse import urlencode
+from w3lib.url import add_or_replace_parameters
 
 from scrapy import Request
-from scrapy.spiders import Spider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
 from woolrich.spiders.woolrich_parser import WoolrichParser
 
 
-class WoolrichSpider(Spider):
+class WoolrichSpider(CrawlSpider):
     name = 'woolrichspider'
-    SIZE_BACKLOG = 'size_backlog'
-    COLOUR_BACKLOG = 'colour_backlog'
-    TRAIL = 'trail'
-    ITEM = 'item'
-
+    woolrich_parser = WoolrichParser()
     start_urls = [
         'https://www.woolrich.eu/en/gb/home'
     ]
 
+    rules = [
+        Rule(link_extractor=LinkExtractor(restrict_css='a.thumb-link'), callback='parse_item'),
+        Rule(link_extractor=LinkExtractor(restrict_css='.marked a.has-sub-menu'), callback='parse_listing')
+    ]
+
     def parse(self, response):
-        for url in response.css('.marked a::attr(href)')[:-1].getall():
-            yield Request(url=url, callback=self.parse_listing)
+        requests = super(WoolrichSpider, self).parse(response)
+        for request in requests:
+            request.meta['trail'] = self.woolrich_parser.add_trail(response)
+            yield request
 
     def parse_listing(self, response):
-        products_on_page, remaining_products = WoolrichParser.get_page_meta(response)
-        requests_count = ceil(remaining_products / products_on_page)
+        products_on_page, remaining_products = self.get_page_metadata(response)
+        requests_count = remaining_products // products_on_page + 1
         for index in range(0, requests_count):
             params = {
                 'sz': products_on_page,
                 'start': products_on_page * index
             }
-            yield Request(url=f'{response.url}?{urlencode(params)}',
-                          meta={self.TRAIL: WoolrichParser.add_trail(response)},
-                          callback=self.parse_product_listing)
+            yield Request(url=add_or_replace_parameters(response.url, params),
+                          meta={'trail': self.woolrich_parser.add_trail(response)})
 
-    def parse_product_listing(self, response):
-        for url in response.css('a.thumb-link::attr(href)').getall():
-            yield Request(url=url, callback=self.parse_product,
-                          meta={self.TRAIL: WoolrichParser.add_trail(response)})
+    def parse_item(self, response):
+        yield self.woolrich_parser.parse(response)
 
-    def parse_product(self, response):
-        item = WoolrichParser.init_item(response)
-        colour_backlog = WoolrichParser.get_colour_backlog(response)
-        yield Request(url=colour_backlog[0], meta={self.ITEM: item,
-                                                   self.COLOUR_BACKLOG: colour_backlog,
-                                                   self.SIZE_BACKLOG: []},
-                      callback=self.parse_colour_sizes)
-
-    def parse_colour_sizes(self, response):
-        meta = response.meta
-        colour_backlog, item, size_backlog = meta[self.COLOUR_BACKLOG], meta[self.ITEM], meta[self.SIZE_BACKLOG]
-        size_backlog.extend(WoolrichParser.get_size_backlog(response))
-        colour_backlog = colour_backlog[1:]
-        if colour_backlog:
-            yield Request(url=colour_backlog[0], meta={self.ITEM: item,
-                                                       self.COLOUR_BACKLOG: colour_backlog,
-                                                       self.SIZE_BACKLOG: size_backlog},
-                          callback=self.parse_colour_sizes)
-        else:
-            yield Request(url=size_backlog[0][0], meta={self.ITEM: item,
-                                                        self.SIZE_BACKLOG: size_backlog},
-                          callback=self.parse_skus)
-
-    def parse_skus(self, response):
-        size_backlog, item = response.meta[self.SIZE_BACKLOG], response.meta[self.ITEM]
-        skus = item.get('skus', [])
-        sku_item = WoolrichParser.get_sku(response, {'size': size_backlog[0][1]})
-        skus.append(sku_item)
-        item['skus'] = skus
-        item['image_urls'].extend(WoolrichParser.get_image_urls(response))
-        size_backlog = size_backlog[1:]
-        if size_backlog:
-            yield Request(url=size_backlog[0][0], meta={self.ITEM: item,
-                                                        self.SIZE_BACKLOG: size_backlog},
-                          callback=self.parse_skus)
-        else:
-            item['image_urls'] = set(item['image_urls'])
-            yield item
+    def get_page_metadata(self, response):
+        css = '.search-result-content::attr({})'
+        page_result = response.css(f'{css.format("data-pagesize")}, {css.format("data-searchcount")}').getall()
+        return list(map(int, page_result))
