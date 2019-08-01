f7041dd9fb7718b8c5a7f28ec54df6c16860df13
Faizan Baig <52661937+faizanbaig01@users.noreply.github.com>
2019-08-01 15:30:45 +0500

    restructured code

--- concurrent_spider.py
+++ concurrent_spider.py
@@ -1,66 +1,71 @@
 import asyncio
-import aiohttp
-import re
-import parsel
+from urllib.parse import urljoin, urlparse
+
+import requests
+from parsel import Selector
+from concurrent.futures import ThreadPoolExecutor
 
 
-class RecursiveCrawler:
+class ConcurrentSpider:
 
-    def __init__(self, url_list, concurrent_requests, url_limit, delay):
+    def __init__(self, start_url, max_urls, download_delay, concurrent_requests):
+        self.allowed_domain = urlparse(start_url).netloc
+        self.urls_limit = max_urls
+        self.download_delay = download_delay
+        self.bounded_semaphore = asyncio.BoundedSemaphore(concurrent_requests)
+        self.executor = ThreadPoolExecutor(max_workers=concurrent_requests)
+        self.visited_urls = []
+        self.queued_urls = {start_url}
+        self.downloaded_bytes = 0
+        self.event_loop = asyncio.get_event_loop()
 
-        self.urls = url_list
-        self.concurrent_requests = concurrent_requests
-        self.max_urls = url_limit
-        self.download_delay = delay
-        self.length = 0
-        self.request_count = 0
+    @staticmethod
+    def extract_links(response):
+        return Selector(response.text).css('a::attr(href)').extract()
 
-    async def get_url_body(self, url):
+    @staticmethod
+    def make_link_absolute(base_url, url):
+        return urljoin(base_url, url)
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                html = await response.text()
-                length = len(html)
-                self.length += length
-                self.request_count += 1
+    def filter_links(self, links):
+        return {link for link in links if self.validate_link(link)}
 
-        return html
+    def validate_link(self, link):
+        return link not in self.visited_urls and self.allowed_domain == urlparse(link).netloc
 
-    async def visited_urls(self, workers, work_queue):
-        while not work_queue.empty():
-            current_url = await work_queue.get()
+    def parse_response(self, response):
+        links = self.extract_links(response)
+        absolute_links = [self.make_link_absolute(response.url, link) for link in links]
+        self.queued_urls.update(self.filter_links(absolute_links))
 
-            try:
-                print(current_url)
-                await self.get_results(current_url, workers)
-                await asyncio.sleep(self.download_delay)
+    async def make_request(self, url):
+        self.parse_response(requests.get(url))
 
-            except Exception:
-                await asyncio.sleep(self.download_delay)
+    async def schedule_requests(self):
+        while self.queued_urls:
+            print(f'QUEUED-> {self.queued_urls}')
+            print(f'VISITED-> {self.visited_urls}')
+            if len(self.visited_urls) == self.urls_limit:
+                break
+            if not self.queued_urls:
+                continue
+            url = self.queued_urls.pop()
+            if url in self.visited_urls:
+                continue
+            print(f'URL-> {url}')
+            self.visited_urls.append(url)
+            await self.bounded_semaphore
+            await self.make_request(url)
 
-    async def get_results(self, url, workers):
+            await asyncio.sleep(self.download_delay)
 
-        if self.request_count <= self.max_urls:
-            html = await self.get_url_body(url)
-            self.__get_urls_html(html)
-            return 'Completed'
+    def crawl(self):
+        try:
+            self.event_loop.run_until_complete(self.schedule_requests())
+        finally:
+            self.event_loop.close()
 
-    def __get_urls_html(self, html):
-
-        select = parsel.Selector(text=html)
-        divs = select.xpath('//div')
-        divs = divs.xpath('.//p')
-        page_url_list = divs.css('a::attr(href)').extract()
-        for urls in page_url_list:
-            if re.match('http', str(urls)):
-                self.urls.append(urls)
-
-    def event_loop(self):
-
-        while self.max_urls > self.request_count:
-            queue = asyncio.Queue()
-            [queue.put_nowait(url) for url in self.urls[self.request_count:self.max_urls]]
-            loop = asyncio.get_event_loop()
-            tasks = [self.visited_urls(workers, queue) for workers in range(self.concurrent_requests)]
-            loop.run_until_complete(asyncio.wait(tasks))
-
+    def print_results(self):
+        print(f'Number of Requests Made: {len(self.visited_urls)}')
+        print(f'Total Downloaded Bytes: {self.downloaded_bytes}')
+        print(f'Average Page Size: {self.downloaded_bytes//len(self.visited_urls)}')
