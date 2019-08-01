c9708fc12178929a7b8c1ce3715625aa6eef4dfd
Shahrukh Ijaz <shahrukh@A002-00097.arbisoft.com>
2019-08-01 16:45:31 +0500

    optimization done

--- cpuc/cpuc/spiders/epuc.py
+++ cpuc/cpuc/spiders/epuc.py
@@ -1,3 +1,5 @@
+""" This is the scraper for vermont website dockets """
+
 import re
 from datetime import datetime, timedelta
 
@@ -9,6 +11,8 @@ from cpuc.items import ProceedingDetail, Filing, Document
 
 
 class VermontSpider(scrapy.Spider):
+    """ This is the class of web spider of vermont website dockets """
+
     name = "vermont"
     allowed_domains = ['epuc.vermont.gov']
     start_urls = ['https://epuc.vermont.gov/?q=User']
@@ -46,8 +50,8 @@ class VermontSpider(scrapy.Spider):
         search_start_date = self.get_start_date()
         search_end_date = self.get_end_date()
         formdata = {
-            'data(181445)': search_end_date,
-            'data(181445_right)': search_start_date,
+            'data(181445)': search_start_date,
+            'data(181445_right)': search_end_date,
         }
 
         return FormRequest.from_response(
@@ -58,7 +62,8 @@ class VermontSpider(scrapy.Spider):
         )
 
     def parse_proceeding_numbers(self, response):
-        """In this method, all docket numbers are turned into a list and send for getting infromation. """
+        """In this method, all docket numbers are turned into
+            a list and send for getting infromation. """
 
         proceeding_urls = response.meta.get('proceeding_urls', [])
         proceeding_type = response.meta.get('proceeding_type', [])
@@ -79,13 +84,11 @@ class VermontSpider(scrapy.Spider):
                 meta={
                     'proceeding_urls': proceeding_urls,
                     'proceeding_type': proceeding_type
-                }
-             )
+                })
 
         else:
 
             for index, url in enumerate(proceeding_urls):
-
                 yield response.follow(
                     url,
                     callback=self.parse_proceeding_details,
@@ -98,7 +101,6 @@ class VermontSpider(scrapy.Spider):
         """In this method, all proceeding docket detail will scrap"""
 
         if "q=node/104/" in response.url:
-
             return self.handle_legacy_request(response)
 
         docket_loader = ItemLoader(item=ProceedingDetail(), response=response, selector=response)
@@ -114,9 +116,11 @@ class VermontSpider(scrapy.Spider):
         docket_loader.add_value("source_url", source_url)
         docket_loader.add_value('state_id', response.url.rsplit('/', 1)[-1])
 
-        docket_loader.add_value('title', self.removed_garbage_from_data(
+        title = self.removed_garbage_from_data(
             response.xpath("//div[@id='formBody_1065']/table//tr/td//"
-                           "span[last()]/text()").getall()))
+                           "span[last()]/text()").getall())
+
+        docket_loader.add_value('title', title)
 
         docket_loader.add_value('proceeding_type', response.meta['proceeding_type'])
 
@@ -129,7 +133,15 @@ class VermontSpider(scrapy.Spider):
         docket_loader.add_value('spider_name', self.name)
         docket_loader.add_value('state', "VT")
 
-        yield response.follow(
+        # yield response.follow(
+        #     url=response.url + '/FV-People-Portal',
+        #     callback=self.parse_filing_parties,
+        #     meta={
+        #         'docket_loader': docket_loader
+        #     }
+        # )
+
+        return response.follow(
             url=response.url + '/FV-People-Portal',
             callback=self.parse_filing_parties,
             meta={
@@ -141,15 +153,14 @@ class VermontSpider(scrapy.Spider):
         """In this method, all people who are involving in each docket are parse"""
 
         docket_loader = response.meta['docket_loader']
-        filing_parties = response.xpath("//tr[starts-with(@id, 'tree.branch.')][not(starts-with(@class, 'tt'))]")
+        filing_parties = response.xpath("//tr[starts-with(@id, 'tree.branch.')]"
+                                        "[not(starts-with(@class, 'tt'))]")
         parties = []
 
         for party in filing_parties:
-
             party = party.xpath("td[2]/span[last()]/text()").get()
 
             if party:
-
                 parties.append(party.strip())
 
         docket_loader.add_value('filing_parties', parties)
@@ -191,8 +202,6 @@ class VermontSpider(scrapy.Spider):
 
         self.parse_filings(filings, response)
 
-        docket_loader.add_value('filings', filings)
-
         url = response.url.replace('/FV-ALLOTDOX-PTL', '/FV-PFEXAFF-PTL')
 
         yield response.follow(
@@ -205,6 +214,8 @@ class VermontSpider(scrapy.Spider):
         )
 
     def parse_prefiled_testimony_exhibits_documents(self, response):
+        """ Parse prefiled testimony exhibts documents """
+
         docket_loader = response.meta['docket_loader']
         filings = response.meta['filing']
 
@@ -227,7 +238,7 @@ class VermontSpider(scrapy.Spider):
 
                 doc_type = filing.xpath("td[2]/text()").getall()
                 doc_type = self.removed_garbage_from_data(doc_type)
-                match = re.search(r'(\d+/\d+/\d+)', doc_type[0])
+                match = self.get_date(doc_type[0])
 
                 if match:
                     filing_loader.add_value('filled_on', match.group(1))
@@ -236,32 +247,29 @@ class VermontSpider(scrapy.Spider):
 
             elif self.get_column_count(filing) == 7:
 
-                if len(filing_loader.get_output_value('types')) == 0:
-
+                if not filing_loader.get_output_value('types'):
                     filing_loader.add_value('types', doc_type)
 
-                if filing_loader.get_output_value('filled_on') is None:
-
+                if not filing_loader.get_output_value('filled_on'):
                     filled_on = filing.xpath("td[2]/text()").getall()
                     filled_on = self.removed_garbage_from_data(filled_on)
                     filing_loader.add_value('filled_on', filled_on)
 
-                filing_loader.replace_value('description', filing.xpath("td[3]/text()").get().strip())
+                filing_loader.replace_value('description',
+                                            filing.xpath("td[3]/text()").get().strip())
 
                 document = self.parse_document_details(self.remove_garbage_from_string(
                     filing.xpath("td[7]/a/@href").get()), response.meta['docket_loader'])
 
                 filing_loader.replace_value('documents', document)
-                del document
 
-                filing_parties = self.get_filing_if_exist(response, filing)
+                filing_parties = self.get_filing(response, filing)
 
                 if filing_parties:
 
                     filing_loader.replace_value('filing_parties', filing_parties)
 
                 filings.append(filing_loader.load_item())
-                del filing_loader
                 filing_loader = ItemLoader(item=Filing(), response=response)
 
             elif self.get_column_count(filing) == 8:
@@ -270,7 +278,8 @@ class VermontSpider(scrapy.Spider):
                 filing_loader.add_value('filled_on', filled_on)
                 description = filing.xpath("td[3]/text()").get().strip()
                 filing_loader.add_value('description', description)
-                filing_parties = self.removed_garbage_from_data(filing.xpath("td[5]/text()").getall())
+                filing_parties = self.removed_garbage_from_data(
+                    filing.xpath("td[5]/text()").getall())
                 filing_loader.add_value('filing_parties', filing_parties)
 
                 document = self.parse_document_details(self.remove_garbage_from_string(
@@ -278,30 +287,38 @@ class VermontSpider(scrapy.Spider):
                 filing_loader.add_value('documents', document)
 
                 filings.append(filing_loader.load_item())
-                del filing_loader
                 filing_loader = ItemLoader(item=Filing(), response=response)
 
     @staticmethod
+    def get_date(data):
+        """ if date in string then return """
+
+        return re.search(r'(\d+/\d+/\d+)', data)
+
+    @staticmethod
     def parse_document_details(document_url, docket_loader):
+        """ make data of a document and return """
+
         document_loader = ItemLoader(item=Document())
         title = document_url.rsplit('/', 1)[-1]
         document_loader.add_value('title', title)
         document_loader.add_value('extension', ".PDF")
         document_loader.add_value('source_url', "https://epuc.vermont.gov/{}".format(document_url))
         document_loader.add_value('blob_name', "{}_{}_{}".format(
-                                  docket_loader.get_output_value('state'),
-                                  docket_loader.get_output_value('state_id'),
-                                  document_loader.get_output_value('title')))
+            docket_loader.get_output_value('state'),
+            docket_loader.get_output_value('state_id'),
+            document_loader.get_output_value('title')))
 
         return document_loader.load_item()
 
     @staticmethod
-    def get_filing_if_exist(response, filing):
-        filings_data = response.xpath("//table[starts-with(@id, 'form.treeTable')]/thead/tr[2]/th[5]/text()")\
-            .get().strip()
+    def get_filing(response, filing):
+        """ return filing data """
+
+        filings_data = response.xpath("//table[starts-with(@id, 'form.treeTable')]"
+                                      "/thead/tr[2]/th[5]/text()").get().strip()
 
         if filings_data == 'Filed By':
-
             filing_parties = filing.xpath("td[4]/text()").get().strip()
 
             return filing_parties
@@ -310,16 +327,22 @@ class VermontSpider(scrapy.Spider):
 
     @staticmethod
     def get_column_count(filing):
+        """ return how much column in each row """
+
         return len(filing.xpath("td").getall())
 
     @staticmethod
     def get_end_date():
+        """ return current date """
+
         search_start_date = datetime.now().date()
         search_start_date = search_start_date.strftime("%m/%d/%Y")
 
         return search_start_date
 
     def get_start_date(self):
+        """ return date till where you want data """
+
         search_end_date = (datetime.now() - timedelta(days=int(self.days))).date()
         search_end_date = search_end_date.strftime("%m/%d/%Y")
 
@@ -327,37 +350,41 @@ class VermontSpider(scrapy.Spider):
 
     @staticmethod
     def removed_garbage_from_data(data_list):
-        return [data.strip() for data in data_list if data.strip() and len(data.strip()) > 1]
+        """ remove garabage of data and return string """
+
+        return [data.strip() for data in data_list if data.strip() and data.strip() != ':']
 
     def handle_legacy_request(self, response):
+        """  handle legacy case parse filed on and replace url for further data and return """
+
         filed_on = response.xpath("//table//tr[1]/td[2]/text()").get()
+        url = response.url.replace('q=node/104/', 'q=node/64/')
 
         if 'Date Filed' in filed_on:
 
-            match = re.search(r'(\d+/\d+/\d+)', filed_on)
+            match = self.get_date(filed_on)
             filed_on = match.group(1)
-            url = response.url.replace('q=node/104/', 'q=node/64/')
 
-            return response.follow(
+            yield response.follow(
                 url=url,
                 callback=self.parse_proceeding_details,
                 meta={
                     'filed_on': filed_on,
-                    'source_url': response.urls
+                    'source_url': response.url
                 }
             )
 
     @staticmethod
     def parse_filed_on_date(content):
+        """ return filed on date """
+
         filed_on = content.xpath("//tr[3]/td[2]/text()").get()
         if filed_on:
             return filed_on.split(':')[1].strip()
-
+        return None
 
     @staticmethod
     def remove_garbage_from_string(data):
-        if data.strip():
+        """ remove garabage of string and return """
 
-            return data.strip()
-
-        return None
+        return data.strip() if data else None
