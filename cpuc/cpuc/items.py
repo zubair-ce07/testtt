b8e70aa3505c7f301d55be78252e1dc087dd8e30
Abbas.Nazar <abbas@A002-00083.arbisoft.com>
2019-08-05 17:43:48 +0500

    scraping done wihtout pages

--- cpuc/cpuc/items.py
+++ cpuc/cpuc/items.py
@@ -8,7 +8,33 @@
 import scrapy
 
 
-class CpucItem(scrapy.Item):
+class Proceeding(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+
+    proceeding_no = scrapy.Field()
+    filled_by = scrapy.Field()
+    service_list = scrapy.Field()
+    industry = scrapy.Field()
+    filling_date = scrapy.Field()
+    category = scrapy.Field()
+    current_status = scrapy.Field()
+    description = scrapy.Field()
+    staff = scrapy.Field()
+    documents = scrapy.Field()
+
+
+class ProceedingDocument(scrapy.Item):
+    # filling_date = scrapy.Field()
+    # document_type = scrapy.Field()
+    # filled_by = scrapy.Field()
+    # description = scrapy.Field()
+    results = scrapy.Field()
+    link = scrapy.Field()
+
+
+class Document(scrapy.Item):
+    title = scrapy.Field()
+    doc_type = scrapy.Field()
+    pdf_link = scrapy.Field()
+    published_date = scrapy.Field()
--- cpuc/cpuc/spiders/cpuc_scrap.py
+++ cpuc/cpuc/spiders/cpuc_scrap.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
+import re
+from urllib.parse import urljoin
+
 import scrapy
 from scrapy.linkextractors import LinkExtractor
+
+from cpuc.items import Proceeding, ProceedingDocument, Document
 
 
 class CpucScrapSpider(scrapy.Spider):
     name = 'cpuc_scrap'
     start_urls = ['https://apps.cpuc.ca.gov/apex/f?p=401:1:0::NO:RP/']
-
-    data = {}
 
     def parse(self, response):
         yield scrapy.FormRequest.from_response(
@@ -19,50 +22,89 @@ class CpucScrapSpider(scrapy.Spider):
 
     def proceeding_parse(self, response):
         proceeding_links = LinkExtractor(
-            allow=r"f?p=401:56:0::NO:RP,57,RIR:P5_PROCEEDING_SELECT:"
+            allow=r"f\?p=401:56:0::NO:RP,57,RIR:P5_PROCEEDING_SELECT:"
         ).extract_links(response)
         for link in proceeding_links:
             yield scrapy.Request(link.url, callback=self.scrap_proceeding)
 
     def scrap_proceeding(self, response):
-        proceeding_code = response.css(
-            '.rc-content-main > h1::text').get().split('-')[0].strip()
-        # print(proceeding_code)
-        filled_by = response.css(
-            '#P56_FILED_BY::text').get()
-        service_list_url = response.css(
-            '#P56_SERVICE_LISTS > span > a::attr(href)').get()
-        industry = response.css(
-            '#P56_INDUSTR::text').get()
-        filling_date = response.css(
-            '#P56_FILING_DATE::text').get()
-        category = response.css(
-            '#P56_CATEGORY::text').get()
-        status = response.css(
-            '#P56_STATUS::text').get()
-        description = response.css(
-            '#P56_DESCRIPTION::text').get()
-        staff = response.css(
-            '#P56_STAFF::text').get()
+        proceeding = Proceeding(
+            proceeding_no=response.css(
+                '.rc-content-main > h1::text').get().split('-')[0].strip(),
+            filled_by=response.css(
+                '#P56_FILED_BY::text').get(),
+            service_list=response.css(
+                '#P56_SERVICE_LISTS > span > a::attr(href)').get(),
+            industry=response.css(
+                '#P56_INDUSTRY::text').get(),
+            filling_date=response.css(
+                '#P56_FILING_DATE::text').get(),
+            category=response.css(
+                '#P56_CATEGORY::text').get(),
+            current_status=response.css(
+                '#P56_STATUS::text').get(),
+            description=response.css(
+                '#P56_DESCRIPTION::text').get(),
+            staff=response.css(
+                '#P56_STAFF::text').get(),
+            documents=[]
+        )
 
-        self.data[proceeding_code] = {
-            "filled_by": filled_by,
-            "service_list_url": service_list_url,
-            "industry": industry,
-            "filling_date": filling_date,
-            "category": category,
-            "status": status,
-            "description": description,
-            "staff": staff
-        }
         # print(self.data[proceeding_code])
         link = response.url.replace('56', '57')
-        yield scrapy.Request(link, callback=self.scrap_documents)
+        request = scrapy.Request(
+            link, callback=self.scrap_documents)
+        request.meta['proceeding'] = proceeding
+        yield request
 
     def scrap_documents(self, response):
+        proceeding = response.meta['proceeding']
+
         documents_links = LinkExtractor(
-            allow=r"http: // docs.cpuc.ca.gov/SearchRes.aspx?DocFormat \
-             = ALL & DocID =.*"
+            allow=r"http://docs.cpuc.ca.gov/SearchRes.aspx\?DocFormat=ALL&DocID="
         ).extract_links(response)
-        for link in documents_links:
-            print(link.url)
+        if documents_links:
+            request = scrapy.Request(
+                documents_links[0].url, callback=self.scrap_document, dont_filter=True)
+            request.meta['proceeding'] = proceeding
+            # proceeding_document = ProceedingDocument()
+            # proceeding_document['link'] = link.url
+            # proceeding_document['results'] = []
+            # request.meta['proceeding_document'] = proceeding_document
+            request.meta['documents_links'] = documents_links
+            yield request
+        else:
+            yield proceeding
+
+    def scrap_document(self, response):
+        proceeding = response.meta['proceeding']
+        documents_links = response.meta['documents_links']
+        proceeding_document = ProceedingDocument()
+        proceeding_document['link'] = documents_links[0].url
+        results = response.selector.xpath(
+            '//table[@id="ResultTable"]/tbody/tr')  # css was not working
+        proceeding_document['results'] = []
+        for result in results:
+            if(result.css('.ResultTitleTD').get()):
+                document = Document(
+                    title=re.sub(re.compile(r'<[^>]+>'), '',
+                                 result.css('.ResultTitleTD').get()),
+                    doc_type=result.css('.ResultTypeTD::text').get(),
+                    pdf_link=urljoin(
+                        'http://docs.cpuc.ca.gov', result.css(
+                            '.ResultLinkTD > a::attr(href)').get()
+                    ),
+                    published_date=result.css('.ResultDateTD::text').get()
+                )
+                proceeding_document['results'].append(document)
+        documents_links.remove(documents_links[0])
+        proceeding['documents'].append(proceeding_document)
+
+        if documents_links:
+            request = scrapy.Request(
+                documents_links[0].url, callback=self.scrap_document, dont_filter=True)
+            request.meta['proceeding'] = proceeding
+            request.meta['documents_links'] = documents_links
+            yield request
+        else:
+            yield proceeding
