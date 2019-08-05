eaea3571ada488619b4c6c226161aeca314ee7f6
imaliazhar <aliazharthegreat@live.com>
2019-08-05 11:54:12 +0500

    refactoring: removing unnecessary variables; using list comprehensions

--- parser.py
+++ parser.py
@@ -8,22 +8,27 @@ from weather import WeatherReading
 class WeatherParser:
     """Parses weather readings from CSV file to WeatherReading objects"""
 
+    def _is_row_valid(self, row):
+        date = row.get('PKT', row.get('PKST'))
+        if "<!--" in date:  # check if row is not a comment
+            return False
+        return True
+
     def parse_weather_files(self, readings_dir, year, month=0):
         """Returns WeatherReading objects after parsing the weather reading CSV file"""
         file_paths = f"{readings_dir}/*{year}*{calendar.month_abbr[month]}*"
         file_paths = glob.glob(file_paths)
 
-        objects = []
+        weather_readings = []
 
         for file_path in file_paths:
             with open(file_path, 'r') as weather_file:
                 next(weather_file)  # discard empty line at top
-                weather_reading_rows = csv.DictReader(weather_file)
+                rows = csv.DictReader(weather_file)
 
-                for row in weather_reading_rows:
-                    date = row.get('PKT', row.get('PKST'))
-                    if date[0:4] == "<!--":  # check if row is not a comment
-                        continue
-                    raw_weather_readings_object = WeatherReading(row)
-                    objects.append(raw_weather_readings_object)
-        return objects
+                readings = [WeatherReading(row)
+                            for row in rows if self._is_row_valid(row)]
+
+                weather_readings.extend(readings)
+
+        return weather_readings
