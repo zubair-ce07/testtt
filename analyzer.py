38c3fcfc98317e0757f5d26c05936d58be385fdb
imaliazhar <aliazharthegreat@live.com>
2019-08-05 11:16:14 +0500

    shifted filtering to a separate method

--- analyzer.py
+++ analyzer.py
@@ -1,21 +1,25 @@
 class WeatherAnalyzer:
     """Extracts aggregate information from WeatherReading objects"""
 
+    def _filter(self, readings, key):
+        "Filter readings by given attribute"
+        return [r for r in readings if getattr(r, key) is not None]
+
     def get_average_of_attributes(self, readings, key):
         """Returns average of values for given key"""
-        readings = [r for r in readings if getattr(r, key) is not None]
+        readings = self._filter(readings, key)
         total = sum((getattr(r, key) for r in readings))
         count = len(readings)
         return round(total/count) if count != 0 else 0
 
     def get_maximum_reading(self, readings, key):
         """Returns reading with maximum value for key"""
-        readings = [r for r in readings if getattr(r, key) is not None]
+        readings = self._filter(readings, key)
         return max(readings, key=lambda r: getattr(r, key))
 
     def get_minimum_reading(self, readings, key):
         """Returns reading with minimum value for key"""
-        readings = [r for r in readings if getattr(r, key) is not None]
+        readings = self._filter(readings, key)
         return min(readings, key=lambda r: getattr(r, key))
 
     def get_attribute_list(self, readings, key):
