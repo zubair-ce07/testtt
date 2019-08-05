d7c0dce954f9b59ac498ca50c11b8ee57e4f3987
imaliazhar <aliazharthegreat@live.com>
2019-08-05 12:26:14 +0500

    refactoring: corrected indentation, shortened names

--- reporter.py
+++ reporter.py
@@ -7,55 +7,62 @@ class WeatherReporter:
     _RED = '\033[91m'
     _WHITE = '\033[00m'
 
-    def report_year_extremes(self, highest_temperature_day, lowest_temperature_day,
-                             most_humid_day):
+    def report_year_extremes(self, max_temp_day, min_temp_day, most_humid_day):
         """Print year extremes to the console"""
-        hottest_day_report = f"Highest: {highest_temperature_day.max_temperature}C" \
-            f" on {calendar.month_name[highest_temperature_day.date.month]} " \
-            f" {highest_temperature_day.date.day}"
+        hottest_day_report = (
+            f"Highest: {max_temp_day.max_temperature}C"
+            f" on {calendar.month_name[max_temp_day.date.month]} "
+            f" {max_temp_day.date.day}"
+        )
 
-        coldest_day_report = f"Lowest: {lowest_temperature_day.min_temperature}C" \
-            f" on {calendar.month_name[lowest_temperature_day.date.month]}" \
-            f" {lowest_temperature_day.date.day}"
+        coldest_day_report = (
+            f"Lowest: {min_temp_day.min_temperature}C"
+            f" on {calendar.month_name[min_temp_day.date.month]}"
+            f" {min_temp_day.date.day}"
+        )
 
-        humid_day_report = f"Humidity: {most_humid_day.max_humidity}%" \
-            f" on {calendar.month_name[most_humid_day.date.month]}" \
+        humid_day_report = (
+            f"Humidity: {most_humid_day.max_humidity}%"
+            f" on {calendar.month_name[most_humid_day.date.month]}"
             f" {most_humid_day.date.day}"
+        )
 
         print(hottest_day_report, coldest_day_report,
               humid_day_report, sep="\n", end="\n\n")
 
-    def report_month_averages(self, avg_highest_temperature, avg_lowest_temperature,
-                              avg_mean_humidity):
+    def report_month_avgs(self, avg_max_temp, avg_min_temp, avg_mean_humidity):
         """Print month averages to the console"""
-        avg_highest_temp_report = f"Highest Average: {avg_highest_temperature}C"
+        avg_max_temp_report = f"Highest Average: {avg_max_temp}C"
 
-        avg_lowest_temp_report = f"Lowest Average: {avg_lowest_temperature}C"
+        avg_min_temp_report = f"Lowest Average: {avg_min_temp}C"
 
         avg_mean_humidity_report = f"Average Mean Humidity: {avg_mean_humidity}%"
 
-        print(avg_highest_temp_report, avg_lowest_temp_report,
+        print(avg_max_temp_report, avg_min_temp_report,
               avg_mean_humidity_report, sep="\n", end="\n\n")
 
-    def report_month_temperatures(self, max_temperatures, min_temperatures,
-                                  single_line=False):
+    def report_month_temps(self, max_temps, min_temps, single_line=False):
         """
             Print bar chart for highest and lowest temperatures for a month
             Optionally on a single line
         """
 
-        for i, (max_temp, min_temp) in enumerate(zip(max_temperatures, min_temperatures)):
-            single_line_report = f"{i+1:02d}" \
-                f" {self._get_colored_plus(max_temp, self._RED)}" \
-                f"{self._get_colored_plus(min_temp, self._BLUE)}" \
+        for day, (max_temp, min_temp) in enumerate(zip(max_temps, min_temps)):
+            single_line_report = (
+                f"{day+1:02d}"
+                f" {self._get_colored_bar(max_temp, self._RED)}"
+                f"{self._get_colored_bar(min_temp, self._BLUE)}"
                 f" {max_temp}C - {min_temp}C"
+            )
 
-            dual_line_report = f"{i+1:02d}" \
-                f" {self._get_colored_plus(max_temp, self._RED)}" \
-                f" {max_temp}C" \
-                f"{i+1:02d}" \
-                f" {self._get_colored_plus(min_temp, self._BLUE)}" \
+            dual_line_report = (
+                f"{day+1:02d}"
+                f" {self._get_colored_bar(max_temp, self._RED)}"
+                f" {max_temp}C"
+                f"\n{day+1:02d}"
+                f" {self._get_colored_bar(min_temp, self._BLUE)}"
                 f" {min_temp}C"
+            )
 
             if single_line:
                 print(single_line_report)
@@ -63,7 +70,6 @@ class WeatherReporter:
                 print(dual_line_report)
         print("")
 
-    def _get_colored_plus(self, times, color):
-        """Print colored +++ to the console"""
-        selected_color = f"{color}{'+' * times}{self._WHITE}"
-        return selected_color
+    def _get_colored_bar(self, bar_length, color):
+        """Get colored bar (+++) of specified length"""
+        return f"{color}{'+' * bar_length}{self._WHITE}"
