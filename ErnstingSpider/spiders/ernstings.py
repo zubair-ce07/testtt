e48f91672e4c8a38295ea2a3415c4b298ef411c3
Mareena.Nazir <mareena@A002-00008.arbisoft.com>
2019-08-07 16:15:10 +0500

    Updated Spider

--- ErnstingSpider/spiders/ernstings.py
+++ ErnstingSpider/spiders/ernstings.py
@@ -19,29 +19,42 @@ GENDER_MAP = {
 }
 
 
+def clean(raw_text):
+    if not raw_text:
+        return None
+
+    if type(raw_text) is list:
+        return [re.sub('\s+', '', raw_text) for raw_text in raw_text if raw_text.strip()]
+
+    return re.sub('\s+', '', raw_text)
+
+
 class ErnstingsSpider(CrawlSpider):
     name = "ernstings"
     allowed_domains = ["ernstings-family.de"]
     start_urls = ["https://ernstings-family.de/"]
     rules = [
         Rule(LinkExtractor(restrict_css=".main-navigation-holder")),
-        Rule(LinkExtractor(restrict_css="#subnavi-"), callback="parse_sub_category", )
+        Rule(LinkExtractor(restrict_css="#subnavi-"), callback="parse_sub_category")
     ]
 
     def parse_sub_category(self, response):
-        profile_name = "EF_findProductsBySearchTerm_Details"
         category_id = response.css("meta[name='pageId']::attr(content)").get()
-        total_products = self.get_products_count(response)
-        total_pages = ceil(total_products / 24) + 1
+        total_pages = self.get_pages_count(response)
+        url_t = "https://www.ernstings-family.de/search/resources/store/10151/" \
+                "productview/bySearchTerm/*?pageNumber={page_no}&pageSize=24&" \
+                "categoryId={category_id}&profileName=EF_findProductsBySearchTerm_Details"
+        
         for page_no in range(1, total_pages):
-            url = f"https://www.ernstings-family.de/search/resources/store/10151/productview/bySearchTerm/*?" \
-                  f"pageNumber={page_no}&pageSize=24&categoryId={category_id}&profileName={profile_name}"
-            yield Request(url, callback=self.parse_products_api, meta={"category_id": category_id})
+            url = url_t.format(page_no=page_no, category_id=category_id)
+            yield Request(url, callback=self.parse_products_api)
 
     def parse_products_api(self, response):
-        category_id = response.meta["category_id"]
         results = json.loads(response.text)
-
+        url = results["resourceId"]
+        pattern = re.compile("categoryId=(\d+)")
+        category_id = pattern.search(url).group(1)
+        
         for result in results["catalogEntryView"]:
             product_id = result["uniqueID"]
             product_url = f"https://www.ernstings-family.de/ProductDisplay?categoryId" \
@@ -51,78 +64,70 @@ class ErnstingsSpider(CrawlSpider):
     def parse_product(self, response):
         item = ErnstingSpiderItem()
         item["product_id"] = response.css("meta[name='pageId']::attr(content)").get()
-        item["name"] = self.clean(response.css(".product-name::text").get())
+        item["name"] = clean(response.css(".product-name::text").get())
         item["category"] = response.css(".breadcrumb-list li span[property='name']::text").getall()
         item["url"] = response.url
         item["date"] = datetime.now().timestamp()
-        gender = self.map_gender(response.css(".breadcrumb-list li span[property='name']::text").get())
-        item["gender"] = gender
+        item["gender"] = self.extract_gender(response)
         item["description"] = self.get_product_description(response)
         item["care"] = self.get_product_care(response)
-        item["img_urls"] = response.css("img.product-view-slide-thumb::attr(src)").getall()
+        item["img_urls"] = response.css(".product-view-slide-holder a::attr(href)").getall()
         item["brand"] = response.css(".teaser-brand img::attr(src)").re("(\w+).png")
         item["lang"] = "de"
         item["market"] = "De"
         item["skus"] = self.extract_skus(response)
         yield item
 
-    def extract_skus(self, response):
-        skus = {}
-        color = self.clean(response.css(".product-detail-product-color::text").get())
-        currency = response.css("script::text").re('"commandContextCurrency": "(.+)",')
+    @staticmethod
+    def get_pages_count(response):
+        total_products = int(response.css(".product-count-holder::text").re_first("(\d+)"))
+        return ceil(total_products / 24) + 1
 
-        size_options = response.css("#select-size-product-detail option")
-        for option in size_options:
-            out_of_stock = False
-            if option.css("[disabled]").get():
-                out_of_stock = True
+    @staticmethod
+    def extract_gender(response):
+        gender = response.css(".breadcrumb-list li span[property='name']::text").get()
+        return GENDER_MAP.get(gender, "unisex-adults")
 
-            size = option.css("::text").get()
-            sku_key = f"{color}_{size}"
-            offer_price = self.clean(response.css(".offer-price::text").get())
-            previous_price = self.clean(response.css(".product-price::text").get())
-            skus[sku_key] = {
-                "size": size,
-                "color": color,
-                "previous_price": previous_price,
-                "offer_price": offer_price,
-                "currency": currency,
-                "out_of_stock": out_of_stock
-            }
-
-        return skus
+    @staticmethod
+    def get_product_description(response):
+        description_xpath = "//div[@class='product-detail-information-contents']" \
+                            "//span[contains(.,'Details')]/../.."
+        description = clean(response.xpath(description_xpath).css("p::text").getall())
+        description_bullets = clean(response.xpath(description_xpath).css("li::text").getall())
+        return [description, description_bullets]
 
     @staticmethod
     def get_product_care(response):
         material_xpath = "//div[@class='product-detail-information-contents']" \
                          "//span[contains(.,'Material')]/../.."
-        material = response.xpath(material_xpath).css("p::text").extract()
-        care_css = ".product-detail-information-icon-list .care-icon-text-wrapper img::attr(alt)"
-        care = response.css(care_css).extract()
-        return [care, material]
-
-    def get_product_description(self, response):
-        description = self.clean(response.css(".product-detail-information-contents p::text").get())
-        description_bullets = response.css(".product-detail-information-contents li::text").getall()
-        return [description, description_bullets]
+        material = clean(response.xpath(material_xpath).css("p::text").getall())
+        material_bullets = clean(response.xpath(material_xpath).css("li::text").getall())
+        care = clean(response.css(".care-icon-text-wrapper img::attr(alt)").getall())
+        return [care, material, material_bullets]
 
     @staticmethod
-    def get_products_count(response):
-        total_products = response.css("#product-list-load-more-products .btn-label::text").re("\d")
-        total_products = "".join(total_products)
-        if total_products:
-            total_products = int(total_products) + 24
-            return total_products
-        return 1
+    def extract_skus(response):
+        skus = {}
+        color = clean(response.css(".product-detail-product-color::text").get())
+        currency = response.css("script::text").re('"commandContextCurrency": "(.+)",')
+        offer_price = clean(response.css(".offer-price::text").getall())
+        previous_price_css ="product-price" if not offer_price else "strike-price"
+        previous_price = clean(response.css(f".{previous_price_css}::text").getall())
+        sku = {
+            "color": color,
+            "currency": currency,
+            "previous_price": previous_price,
+            "offer_price": offer_price,
+        }
 
-    @staticmethod
-    def clean(raw_text):
-        if raw_text:
-            return re.sub('\s+', '', raw_text)
+        size_options = response.css("#select-size-product-detail option")
+        for option in size_options:
+            out_of_stock = True if option.css("[disabled]") else False
+            size = option.css("::text").get()
+            sku_key = f"{color}_{size}"
+            sku["size"] = size
+            sku["out_of_stock"] = out_of_stock
+            skus[sku_key] = sku
 
-    @staticmethod
-    def map_gender(gender):
-        if gender in GENDER_MAP:
-            return GENDER_MAP[gender]
-        return "unisex-adults"
-
+        return skus
+s
