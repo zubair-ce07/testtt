e01d6d5b6f2664744b4b089af46a30a182baee8f
Ali Azhar <ali.azhar@arbisoft.com>
2019-08-08 14:43:07 +0500

    Ali Azhar - Weatherman (#1551)
    Merged

---
+++ parser.py
@@ -0,0 +1,32 @@
+import csv
+import glob
+import calendar
+
+from weather import WeatherReading
+
+
+class WeatherParser:
+    """Parses weather readings from CSV file to WeatherReading objects"""
+
+    def _is_row_valid(self, row):
+        date = row.get("PKT", row.get("PKST"))
+        if "<!--" in date:  # check if row is not a comment
+            return False
+        return True
+
+    def parse_weather_files(self, readings_dir, year, month=0):
+        """Returns WeatherReading objects after parsing the weather reading CSV file"""
+        file_paths = f"{readings_dir}/*{year}*{calendar.month_abbr[month]}*"
+        file_paths = glob.glob(file_paths)
+
+        weather_readings = []
+
+        for file_path in file_paths:
+            with open(file_path, "r") as weather_file:
+                next(weather_file)  # discard empty line at top
+                rows = csv.DictReader(weather_file)
+
+                weather_readings += [WeatherReading(row)
+                                     for row in rows if self._is_row_valid(row)]
+
+        return weather_readings
