6cc1bbbb0d2b42bed36a167f99bcac26cb9b78e5
Muhammad.Asim <asim@A002-00110.arbisoft.com>
2019-08-09 11:32:33 +0500

    Added User Profile Model

--- freelancing/accounts/forms.py
+++ freelancing/accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class SignUpForm(UserCreationForm):
@@ -16,3 +17,9 @@ class SignUpForm(UserCreationForm):
             'password1',
             'password2'
         )
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('description', 'country')
