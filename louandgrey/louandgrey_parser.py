fa5ac9b86679684ca5ba5284f38e70477aaf6c87
Uzair Amer <uzair.amer@arbisoft.com>
2019-08-09 16:55:52 +0500

    Even better way of finding out_of_stock

--- louandgrey/louandgrey_parser.py
+++ louandgrey/louandgrey_parser.py
@@ -73,7 +73,7 @@ class LouandgreyParser(Spider):
                 sku['colour'] = raw_skus['colorName']
                 sku['size'] = raw_sku['sizeAbbr']
                 sku['sku_id'] = raw_sku['skuId']
-                sku['out_of_stock'] = not raw_sku['available'] == 'true'
+                sku['out_of_stock'] = raw_sku['available'] != 'true'
                 skus.append(sku)
 
         return skus
