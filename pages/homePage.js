41586cbf07c46e663ff1754a3f22b8b50bcbe53f
junaid1840 <junaid_nazir15@yahoo.com>
2019-08-17 18:44:59 +0500

    removed all comments

--- pages/homePage.js
+++ pages/homePage.js
@@ -1,6 +1,5 @@
 const moment = require('moment');
 const homePage = function () {
-    // Ignore the Angular app check because the application we are testing is developed in ReactJS
     browser.ignoreSynchronization = true;
     const EC = protractor.ExpectedConditions;
     this.loadHomePage = async function() {
@@ -33,10 +32,8 @@ const homePage = function () {
         element(by.css("button[aria-label='Edit search']")).click();
         let graphElement = element.all(by.css('.graph-col'));
         if(operationType === 'first_half') {
-            // Waiting for the second graph to be invisible
             browser.wait(EC.invisibilityOf(graphElement.get(1)),5000);
         } else {
-            // Waiting for the second graph to be visible
             browser.wait(EC.visibilityOf(graphElement.get(1)),5000);
         }
         graphElement = element.all(by.css('.graph-col'));
@@ -45,27 +42,21 @@ const homePage = function () {
     this.selectDateFromCalendar = function () {
         element(by.css("div[id*='-dateRangeInput-display-end-inner']")).click();
         const newCalendarDateElement = element(by.css("div[aria-label='September 10']"));
-        // Wait for the calendar to be visible
         browser.wait(EC.elementToBeClickable(newCalendarDateElement), 5000);
         newCalendarDateElement.click();
-        // Wait for the Calendar to hide
         browser.wait(EC.invisibilityOf(newCalendarDateElement), 5000);
     };
     this.getGraphBarTooltip = function () {
         const graphBar = element(by.css("button[data-date='2019-08-29']"));
         browser.actions().mouseMove(graphBar).perform();
         const tooltip = graphBar.element(by.css('.bar')).element(by.css('.price-info')).element(by.css('.price-price'));
-        // Wait for the tooltip to be visible
         browser.wait(EC.visibilityOf(tooltip),5000);
         return tooltip.getText();
     };
     this.getNewDate = async function () {
         const selectedBar = element(by.css('.Button-No-Standard-Style.js-bar.item.selected'));
-        // Wait for the selected bar to be visible
         browser.wait(EC.elementToBeClickable(selectedBar), 8000);
-        // Getting the pre-selected date
         const pre_selectedDate = selectedBar.getAttribute('data-date');
-        // Adding two days in the current selected date
         return moment(pre_selectedDate).add(2, 'days').format('YYYY-MM-DD');
     };
     this.getNewSelectedGraphBar = async function (barDate) {
@@ -105,18 +96,15 @@ const homePage = function () {
         browser.wait(EC.invisibilityOf(searchTheseDaysButton),5000);
     };
     this.showDetails = function () {
-        // Wait for the Graph to be populated so new data could be loaded
         const graphElement = element.all(by.css('.graph-col'));
         browser.wait(EC.visibilityOf(graphElement.get(1)),5000);
         const showDetailsBtn = element(by.css("a[id*='-extra-info-details-link-toggleMore']"));
-        // Wait for the show details Btn to be clickable
         browser.wait(EC.elementToBeClickable(showDetailsBtn),5000);
         showDetailsBtn.click();
     };
     this.getDepartureDateFromDetailsPanel = function () {
         let detailsPanel = element.all(by.css("div[class='resultInner']"));
         detailsPanel = detailsPanel.first();
-        // Wait for the deatils drop down to be visible
         browser.wait(EC.visibilityOf(detailsPanel),5000);
         const detailsPanelDepartureDate = element.all(by.css('.leg-dates-set')).first();
         browser.wait(EC.visibilityOf(detailsPanelDepartureDate),5000);
--- spec.js
+++ spec.js
@@ -1,106 +1,70 @@
 const moment = require('moment');
 const momondoHomepage = require('./pages/homePage');
 describe('Protractor Demo App', function() {
-    // To ignore checking the angular app
-    browser.ignoreSynchronization = true;
     let SELECTED_DATE = '';
     momondoHomepage.loadHomePage();
-    // Step 1
-    // 1)
     it('Should display ‘London (LON)’ in the origin field', function() {
         expect(momondoHomepage.getOrigin()).toEqual('London (LON)');
     });
-    // 2)
     it('Should display ‘New York (NYC)’ in the destination field.', function() {
         expect(momondoHomepage.getDestination()).toEqual('New York (NYC)');
     });
-    // 3)
     it('Should display ‘Sat 8/24’ in the departure date field', function() {
         expect(momondoHomepage.getDepartureDate()).toBe('Sat 8/24');
     });
-    // 4)
     it('Should display ‘Sun 9/22’ in the return date field.', function() {
         expect(momondoHomepage.getReturnDate()).toBe('Sun 9/22');
     });
-    // 5)
     it('Should display ‘1 Adult, Economy’ in travelers field', function() {
         expect(momondoHomepage.getTravelers()).toBe('1 Adult, Economy');
     });
-    // 6)
     it('Should display ‘Estimated Price Graph’ below the search form', function() {
        expect(momondoHomepage.isGraphVisible()).toEqual(true);
     });
-
-
-    // Step 2
     it('Should display first half of estimated price graph',async function () {
         momondoHomepage.selectTripType('One-way');
         expect(momondoHomepage.getMainVisibleGraphCount('first_half')).toEqual(1);
     });
-
-
-    // Step 3
     it('Should display both graphs',async function () {
         momondoHomepage.selectTripType('Round-trip');
         momondoHomepage.selectDateFromCalendar();
         expect(momondoHomepage.getMainVisibleGraphCount('both')).toEqual(2);
     });
-
-    // Step 4
     it('Should display tooltip with price',function () {
         expect(momondoHomepage.getGraphBarTooltip()).toContain('USD');
     });
-
-    // Step 5
-    // 1)
     it('Should highlight selected bar',async function () {
-        // Getting date of two days after than currently selected one
         SELECTED_DATE = await momondoHomepage.getNewDate();
-        // Getting new Selected bar
         const newSelectGraphBar = await momondoHomepage.getNewSelectedGraphBar(SELECTED_DATE);
-        // Getting status of the selected bar
         expect(momondoHomepage.getSelectedBarStatus(newSelectGraphBar)).toContain('selected');
     });
-    // 2)
     it('Should display Price of selected bar',async function () {
         const selectedBarStatus = await momondoHomepage.getSelectedBarPrice();
         expect(selectedBarStatus).toEqual(true);
     });
-    // 3)
     it('Should display ‘Price shown are estimates per person”',function () {
         const selectedPriceTextShownStatus = momondoHomepage.getSelectedPriceTextShown();
         expect(selectedPriceTextShownStatus).toEqual(true);
     });
-    // 4)
     it('Should display ‘Search these days’ button',function () {
         const searchBtnShown = momondoHomepage.isSearchBtnShown();
         expect(searchBtnShown).toEqual(true);
     });
-
-
-
-    // Step 6
-    // 1)
     it('Should display updated date in first result’s details section',async function () {
         momondoHomepage.searchTheseDays();
         momondoHomepage.showDetails();
         const FirstResultDepartureDate = momondoHomepage.getDepartureDateFromDetailsPanel();
-        // Should match the departure date in first result card i.e. Tue, Aug 10 with selected date in the bar graph i.e YYYY-MM-DD
         const expectedDate = moment(SELECTED_DATE).format('ddd, MMM D');
         expect(FirstResultDepartureDate).toEqual(expectedDate);
     });
-    // 2)
     it('Should display updated departure date',function () {
         const departureField = momondoHomepage.getDepartureDate();
-        // Should match the departure date in search form i.e. Tue M/D with selected date in the bar graph i.e YYYY-MM-DD
         const expectedDate = moment(SELECTED_DATE).format('ddd M/D');
         expect(departureField.getText()).toBe(expectedDate);
     });
-    // 3)
     it('Should not display ‘Price shown are estimates per person’ label',function () {
         expect(momondoHomepage.isPricesShownTextExist());
     });
-    // 4)
     it('Should not display ‘Search these days’ button',function () {
         expect(momondoHomepage.isSearchTheseDaysButtonExist())
     })
