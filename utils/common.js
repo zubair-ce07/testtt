7132e47df3a62d8d21b639fb40a42c69619f7231
sehrishnaveed <sehrish.naveed@arbisoft.com>
2019-08-17 11:22:57 +0500

    named exports added in common.js file.

--- utils/common.js
+++ utils/common.js
@@ -1,21 +1,25 @@
-export function waitForElementPresence(elementSelector, timeout, error) {
+function waitForElementPresence(elementSelector, timeout, error) {
     let EC = protractor.ExpectedConditions;
     browser.wait(EC.presenceOf(elementSelector), timeout, error);
 }
 
-export function waitForElementVisibility(elementSelector, timeout, error) {
+function waitForElementVisibility(elementSelector, timeout, error) {
     let EC = protractor.ExpectedConditions;
     browser.wait(EC.visibilityOf(elementSelector), timeout, error);
 }
 
-export function handleException(functionName, errMsg) {
-    console.log(`function ${functionName} > Exception message`, errMsg);
+function getElementByCSS(selector) {
+    return element(by.css(selector));
 }
 
-export function getAllElementByCSS(selector) {
+function getAllElementByCSS(selector) {
     return element.all(by.css(selector));
 }
 
-export function getSingleElementByCSS(selector) {
-    return element(by.css(selector));
+export
+{
+    waitForElementPresence,
+    waitForElementVisibility,
+    getElementByCSS,
+    getAllElementByCSS
 }
