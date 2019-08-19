c793d3e224de37d8e769b64e94d5ec24ab362947
hanzla-bit <53176670+hanzla-bit@users.noreply.github.com>
2019-08-19 11:28:26 +0500

    Database(MySQL) working in separate files.
    Modified 'setting.py' & 'pipelines.py' to work the database separately. Clone/download these files and replace them within your scrapy project and then crawl the website using command 'scrapy crawl wheels' and it will crawl used cars data within pakwheels.com website.
    Also, to have all the records, which are more than 35000, in database/json/csv or any other output file then un-comment line#25 & line#26.

--- pakwheels_spider.py
+++ pakwheels_spider.py
@@ -1,5 +1,4 @@
 import scrapy
-import mysql.connector as mc
 from ..items import PakwheelsItem
 
 
@@ -53,50 +52,4 @@ class scrapySpider(scrapy.Spider):
         var = var.replace("\r", "")
         items['description'] = var.strip()
 
-        self.total += 1
-        print('^'*50, self.total)
-        self.dataToMysql(items['make'], items['model'], items['year'], items['millage'], items['transmission'],
-                         items['engine_type'], items['reg_city'], items['assembly'], items['engine_capacity'],
-                         items['body_type'], items['features'], items['description'])
-
-        # print('^'*40, items['reg_city'])
         yield items
-
-    def dataToMysql(self, make, model, year, millage, transmission, eng_type, reg_city, asmb, eng_cap
-                    , body_type, features, desc):
-        conn = mc.connect(user='root', password='1234', host='127.0.0.1')
-        mycursor = conn.cursor()
-        mycursor.execute('CREATE DATABASE IF NOT EXISTS wheelsDB')
-        mycursor.execute('USE wheelsDB')
-
-        tbl_des = (
-            'CREATE TABLE IF NOT EXISTS usedCars('
-            'used_cars_id int AUTO_INCREMENT,'
-            'make varchar(15),'
-            'model varchar(15),'
-            'year varchar(5),'
-            'millage varchar(10),'
-            'transmission varchar(10),'
-            'engine_type varchar(10),'
-            'reg_city varchar(10),'
-            'assembly varchar(10),'
-            'engine_capacity varchar(10),'
-            'body_type varchar(10),'
-            'features varchar(500),'
-            'description varchar(500),'
-            'PRIMARY KEY(used_cars_id) )'
-        )
-        tbl_in_sql = """INSERT INTO usedCars(make, model, year, millage, transmission, engine_type, reg_city,
-        assembly, engine_capacity, body_type, features, description)
-        VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-        features = ''.join(i for i in features)
-        features = features.strip()
-        tbl_in_val = (make, model, year, millage, transmission, eng_type, reg_city, asmb,
-                      eng_cap, body_type, features, desc)
-
-        mycursor.execute(tbl_des)
-        mycursor.execute(tbl_in_sql, tbl_in_val)
-
-        conn.commit()
-        mycursor.close()
-        conn.close()
