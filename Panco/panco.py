1f7e027292fedffd61fcc1cbc60fded8e45b20f8
Arslan Shafiq <arslan.shafiq@arbisoft.com>
2019-08-19 14:54:25 +0500

    implemented using crawl spider as suggested

--- Panco/panco.py
+++ Panco/panco.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
-import json
-
-import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 import w3lib.url
 
-from ..items import Product
+from .panco_parser import PancoParserSpider
 
-class PancoSpider(scrapy.Spider):
-    name = 'panco'
-    possible_genders = ['Yenidoğan', 'Kiz Bebek', 'Kiz Çocuk', 'Erkek Bebek', 'Erkek Çocuk']
+class PancoCrawlerSpider(CrawlSpider):
+    name = 'panco_crawler'
     allowed_domains = ['panco.com.tr']
     start_urls = ['https://panco.com.tr/']
+    product_parser = PancoParserSpider()
 
-    def parse(self, response):
-        links = response.css(".js-categories-carousel a::attr(href)")
-        requests = [response.follow(link, self.parse_category) for link in links]
-        return requests
+    rules = (
+        Rule(LinkExtractor(restrict_css=('.js-categories-carousel')), callback='parse_category'),
+    )
 
     def parse_category(self, response):
         next_page = response.css(".paginate-bottom a.js-pagination-next::attr(href)").get()
@@ -26,53 +24,8 @@ class PancoSpider(scrapy.Spider):
 
         products = response.css(".product-item-info a::attr(href)").getall()
         for product in products:
-            yield response.follow(product, self.parse_product)
-
-    def parse_product(self, response):
-        product_item = Product()
-        product_data = json.loads(response.css(".js-main-wrapper .analytics-data::text").get())
-        product_item['name'] = response.css(".product-name::text").get()
-        product_item['retailer_sku'] = response.css(".product-number::text").get()
-        product_item['brand'] = product_data['productDetail']['data']['brand']
-        product_item['lang'] = response.css("html::attr(lang)").get()
-        product_item['description'] = response.css(".js-product-content__tab--delivery div.content::text").get().strip()
-        product_item['image_urls'] = response.css(".js-product-slider__main img::attr(src)").getall()
-        product_item['category'] = self.get_category(response)
-        product_item['gender'] = self.get_gender(response)
-        product_item['url'] = response.request.url
-        product_item['skus'] = self.get_skus(response)
-        return product_item
+            yield self.product_parser.start_product_request(product, response)
 
     def get_next_page(self, next_page):
         if next_page != "#":
             return next_page.split("?page=")[1]
-
-    def get_category(self, response):
-        return response.css(".breadcrumb a::text, .breadcrumb span::text").getall()[1:]
-
-    def get_gender(self, response):
-        category = ' '.join(self.get_category(response))
-        for gender in self.possible_genders:
-            if gender.lower() in category.lower():
-                return gender
-
-    def get_skus(self, response):
-        product_data = json.loads(response.css(".js-main-wrapper .analytics-data::text").get())
-        product_price = product_data['productDetail']['data']['price']
-        old_price = product_data['productDetail']['data']['dimension16']
-        currency = response.css("head meta[property='og:price:currency']::attr(content)").get()
-        product_colors = response.css(".product-variant__item .variants-wrapper a::attr(data-value)").getall()
-        product_sizes = response.css(".product-variant__item .product-size-item::attr(data-value)").getall()
-
-        skus = {}
-        for color in product_colors:
-            for size in product_sizes:
-                sku =	{
-                    "colour": color,
-                    "price": product_price,
-                    "currency": currency,
-                    "size": size,
-                    "privious_prices": old_price
-                }
-                skus[f"{color}_{size}"] = sku
-        return skus
---
+++ Panco/panco_parser.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import json
+
+import scrapy
+
+from ..items import Product
+
+class PancoParserSpider(scrapy.Spider):
+    name = 'panco_parser'
+    allowed_domains = ['panco.com']
+    start_urls = ['https://www.panco.com.tr']
+    possible_genders = ['Yenidoğan', 'Kiz Bebek', 'Kiz Çocuk', 'Erkek Bebek', 'Erkek Çocuk']
+
+    def start_product_request(self, product, response):
+        return response.follow(product, self.parse_product)
+
+    def parse_product(self, response):
+        product_item = Product()
+        product_data = json.loads(response.css(".js-main-wrapper .analytics-data::text").get())
+        product_item['name'] = response.css(".product-name::text").get()
+        product_item['retailer_sku'] = response.css(".product-number::text").get()
+        product_item['brand'] = product_data['productDetail']['data']['brand']
+        product_item['lang'] = response.css("html::attr(lang)").get()
+        product_item['description'] = response.css(".js-product-content__tab--delivery div.content::text").get().strip()
+        product_item['image_urls'] = response.css(".js-product-slider__main img::attr(src)").getall()
+        product_item['category'] = self.get_category(response)
+        product_item['gender'] = self.get_gender(response)
+        product_item['url'] = response.request.url
+        product_item['skus'] = self.get_skus(response)
+        return product_item
+
+    def get_category(self, response):
+        return response.css(".breadcrumb a::text, .breadcrumb span::text").getall()[1:]
+
+    def get_gender(self, response):
+        category = ' '.join(self.get_category(response))
+        for gender in self.possible_genders:
+            if gender.lower() in category.lower():
+                return gender
+
+    def get_skus(self, response):
+        product_data = json.loads(response.css(".js-main-wrapper .analytics-data::text").get())
+        product_price = product_data['productDetail']['data']['price']
+        old_price = product_data['productDetail']['data']['dimension16']
+        currency = response.css("head meta[property='og:price:currency']::attr(content)").get()
+        product_colors = response.css(".product-variant__item .variants-wrapper a::attr(data-value)").getall()
+        product_sizes = response.css(".product-variant__item .product-size-item::attr(data-value)").getall()
+
+        skus = {}
+        for color in product_colors:
+            for size in product_sizes:
+                sku =	{
+                    "colour": color,
+                    "price": product_price,
+                    "currency": currency,
+                    "size": size,
+                    "privious_prices": old_price
+                }
+                skus[f"{color}_{size}"] = sku
+        return skus
