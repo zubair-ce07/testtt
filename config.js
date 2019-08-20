5511d5c2d34babad91a66be82f6aecf6e6f25ab5
junaid1840 <junaid_nazir15@yahoo.com>
2019-08-20 18:18:23 +0500

    Revert "Updated assignment into TypeScript & improved names of variables and some function names."
    This reverts commit 3c583a82f7c9b6ae262e7f61a5cbfbb70869fbc9.

--- config.js
+++ config.js
@@ -1,10 +1,5 @@
 exports.config = {
-    framework: 'jasmine',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['spec.ts'],
-    onPrepare() {
-        require('ts-node').register({
-            project: require('path').join(__dirname, './tsconfig.json') // Relative path of tsconfig.json file
-        });
-    }
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['spec.js'],
 };
