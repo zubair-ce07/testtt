9d26476b0e15940dff2dc4519168abe4ec7641ac
Muhammad.Asim <asim@A002-00110.arbisoft.com>
2019-08-20 12:09:32 +0500

    added seller gig models

--- freelancing/seller/models.py
+++ freelancing/seller/models.py
@@ -1,3 +1,56 @@
 from django.db import models
-
+from accounts.models import User
 # Create your models here.
+
+
+class Category(models.Model):
+    category_name = models.TextField(max_length=100)
+
+
+class SearchTag(models.Model):
+    search_tag = models.TextField(max_length=100)
+
+
+class Gig(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    gig_title = models.TextField(max_length=300)
+    category = models.ManyToManyField(Category)
+    search_tag = models.ManyToManyField(SearchTag)
+
+
+class Package(models.Model):
+    gig = models.ForeignKey(Gig, on_delete=models.CASCADE)
+    name = models.TextField(max_length=100)
+    details_offering = models.TextField(max_length=300)
+    delivery_time = models.IntegerField()  # Num of Days
+    revisions = models.IntegerField()
+    price = models.IntegerField()  # only integer dollars
+
+
+class Description(models.Model):
+    gig = models.ForeignKey(Gig, on_delete=models.CASCADE)
+    description = models.TextField(max_length=1000)
+
+
+class Faq(models.Model):
+    gig = models.ForeignKey(Gig, on_delete=models.CASCADE)
+    question = models.TextField(max_length=400)
+    answer = models.TextField(max_length=400)
+
+
+class Requirements(models.Model):
+    gig = models.ForeignKey(Gig, on_delete=models.CASCADE)
+    requirement_text = models.TextField(max_length=500)
+
+
+def gig_gallery_images_path(instance, filename):
+    """
+        returns the path where the
+        gig gallery images should be stored
+    """
+    return "images/gigs/gallery/{0}_{1}_{2}".format(instance.gig.id, instance.gig.gig_title, filename)
+
+
+class Gallery(models.Model):
+    gig = models.ForeignKey(Gig, on_delete=models.CASCADE)
+    gig_image = models.ImageField(upload_to=gig_gallery_images_path)
