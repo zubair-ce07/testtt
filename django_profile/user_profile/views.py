072485195096d125fbcc54cf718fe1fe81d783c9
Abbas.Nazar <abbas@A002-00083.arbisoft.com>
2019-08-20 19:42:42 +0500

    refactor:cleaned login view

--- django_profile/user_profile/views.py
+++ django_profile/user_profile/views.py
@@ -94,52 +94,16 @@ class LoginView(auth_views.SuccessURLAllowedHostsMixin, FormView):
     @method_decorator(never_cache)
     def dispatch(self, request, *args, **kwargs):
         if self.redirect_authenticated_user and self.request.user.is_authenticated:
-            redirect_to = self.get_success_url()
+            redirect_to = resolve_url('profile')
             if redirect_to == self.request.path:
                 raise ValueError(
-                    "Redirection loop for authenticated user detected. Check that "
+                    "Redirection loop for authenticated user detected. Check that"
                     "your LOGIN_REDIRECT_URL doesn't point to a login page."
                 )
             return HttpResponseRedirect(redirect_to)
         return super().dispatch(request, *args, **kwargs)
 
-    def get_success_url(self):
-        url = self.get_redirect_url()
-        return url or resolve_url('profile')
-
-    def get_redirect_url(self):
-        """Return the user-originating redirect URL if it's safe."""
-        redirect_to = self.request.POST.get(
-            self.redirect_field_name,
-            self.request.GET.get(self.redirect_field_name, '')
-        )
-        url_is_safe = is_safe_url(
-            url=redirect_to,
-            allowed_hosts=self.get_success_url_allowed_hosts(),
-            require_https=self.request.is_secure(),
-        )
-        return redirect_to if url_is_safe else ''
-
-    def get_form_class(self):
-        return self.authentication_form or self.form_class
-
-    def get_form_kwargs(self):
-        kwargs = super().get_form_kwargs()
-        kwargs['request'] = self.request
-        return kwargs
-
     def form_valid(self, form):
         """Security check complete. Log the user in."""
         auth_login(self.request, form.get_user())
-        return HttpResponseRedirect(self.get_success_url())
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        current_site = get_current_site(self.request)
-        context.update({
-            self.redirect_field_name: self.get_redirect_url(),
-            'site': current_site,
-            'site_name': current_site.name,
-            **(self.extra_context or {})
-        })
-        return context
+        return HttpResponseRedirect('user')
