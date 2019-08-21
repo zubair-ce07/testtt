fd2f705740c51944715bf38356cac47f1e633b2e
Arslan Shafiq <arslan.shafiq@arbisoft.com>
2019-08-21 14:31:05 +0500

    updated request chaining as suggested

--- Panco/items.py
+++ Panco/items.py
@@ -15,3 +15,4 @@ class Product(scrapy.Item):
     skus = scrapy.Field()
     price = scrapy.Field()
     currency = scrapy.Field()
+    meta = scrapy.Field()
--- Panco/panco_parser.py
+++ Panco/panco_parser.py
@@ -2,12 +2,12 @@
 import json
 from urllib import parse
 
-import scrapy
+from scrapy import Spider, Request
 import w3lib.url
 
 from ..items import Product
 
-class PancoParserSpider(scrapy.Spider):
+class PancoParserSpider(Spider):
     name = 'panco_parser'
     allowed_domains = ['panco.com']
     start_urls = ['https://www.panco.com.tr']
@@ -26,8 +26,9 @@ class PancoParserSpider(scrapy.Spider):
         product_item['gender'] = self.get_gender(response)
         product_item['url'] = response.request.url
         product_item['skus'] = {}
-        response.meta['product_item'] = product_item
-        return self.parse_skus(response)
+        product_item['meta'] = {'request_queue': self.get_skus_requests(response)}
+
+        return self.check_sku_requests(product_item)
 
     def get_category(self, response):
         return response.css(".breadcrumb a::text, .breadcrumb span::text").getall()[1:]
@@ -38,27 +39,29 @@ class PancoParserSpider(scrapy.Spider):
             if gender.lower() in category.lower():
                 return gender
 
-    def parse_skus(self, response):
-        if 'colours' in response.meta:
-            remaining_colours = response.meta['colours']
-        else:
-            remaining_colours = response.css(".product-variant__item .variants-wrapper a::attr(data-value)").getall()
-        product_item = response.meta['product_item']
-        product_item['skus'].update(self.get_colour_skus(response))
-        remaining_colours.remove(self.get_selected_colour(response))
-        if remaining_colours:
-            url = w3lib.url.add_or_replace_parameter(response.request.url, 'integration_color', remaining_colours[0])
-            request = scrapy.Request(url, callback=self.parse_skus)
+    def get_skus_requests(self, response):
+        colors = response.css(".product-variant__item .variants-wrapper a::attr(data-value)").getall()
+        requests = []
+        for color in colors:
+            url = w3lib.url.add_or_replace_parameter(response.request.url, 'integration_color', color)
+            requests.append(Request(url, callback=self.parse_colour_skus))
+        return requests
+
+    def check_sku_requests(self, product_item):
+        if product_item['meta'] and product_item['meta']['request_queue']:
+            request = product_item['meta']['request_queue'].pop(0)
             request.meta['product_item'] = product_item
-            request.meta['colours'] = remaining_colours
             return request
         else:
+            del product_item['meta']
             return product_item
 
     def get_selected_colour(self, response):
         return response.css(".product-variant__item .variants-wrapper a.is-select::attr(data-value)").get()
 
-    def get_colour_skus(self, response):
+    def parse_colour_skus(self, response):
+        product_item = response.meta['product_item']
+
         raw_product = json.loads(response.css(".js-main-wrapper .analytics-data::text").get())
         product_price = raw_product['productDetail']['data']['price']
         old_prices = [raw_product['productDetail']['data']['dimension16']]
@@ -79,7 +82,8 @@ class PancoParserSpider(scrapy.Spider):
             if not availble:
                 new_sku['out_of_stock'] = True
             colour_skus[f"{selected_colour}_{size}"] = new_sku
-        return colour_skus
+        product_item['skus'].update(colour_skus)
+        return self.check_sku_requests(product_item)
 
     def get_size_availability(self, response):
         size_classes = response.css(".product-variant__item .product-size-item::attr(class)").getall()
