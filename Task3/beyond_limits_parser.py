d67fc33f02828e49b9ae7da1afc1c5ebbb7d82e6
Arslan Shafiq <arslan.shafiq@arbisoft.com>
2019-08-21 18:03:28 +0500

    - pythonic code

--- Task3/beyond_limits_parser.py
+++ Task3/beyond_limits_parser.py
@@ -17,7 +17,7 @@ class BeyondParserSpider(scrapy.Spider):
         product_item['brand'] = "BeyondLimits"
         product_item['lang'] = "en"
         product_item['description'] = self.get_product_description(response)
-        product_item['image_urls'] = response.css(".bb_pic--navlink ::attr(href)").getall()
+        product_item['image_urls'] = self.parse_image_urls(response)
         product_item['gender'] = self.get_gender(response)
         product_item['category'] = self.get_category(response)
         product_item['url'] = response.request.url
@@ -27,8 +27,10 @@ class BeyondParserSpider(scrapy.Spider):
         return product_item
 
     def get_product_description(self, response):
-        description = response.css("#description p::text, #description::text").getall()
-        return description
+        return response.css("#description p::text, #description::text").getall()
+
+    def parse_image_urls(self, response):
+        return response.css(".bb_pic--navlink ::attr(href)").getall()
 
     def get_gender(self, response):
         category = ' '.join(self.get_category(response))
@@ -39,8 +41,7 @@ class BeyondParserSpider(scrapy.Spider):
     def get_category(self, response):
         categories_css = ".bb_breadcrumb--item span::text, .bb_breadcrumb--item strong::text"
         categories = response.css(categories_css).getall()
-        if categories:
-            return categories[1:]
+        return categories[1:] if categories else []
 
     def get_care(self, response):
         description_list = response.css("#description li::text").getall()
@@ -52,17 +53,13 @@ class BeyondParserSpider(scrapy.Spider):
 
     def get_colours(self, response):
         description = " ".join(response.css("#description li::text").getall())
-        colours = re.findall ( 'Colour: (.*?) Care', description, re.DOTALL)
-        if colours:
-            return colours[0].split(",")
+        return re.findall('Colour: (.*?) Care', description, re.DOTALL)
 
     def get_skus(self, response):
         product_price = response.css(".price span::attr(content)").get()
         old_price = response.css(".oldPrice del::text").getall()
         currency = response.css(".price meta::attr(content)").get()
-        product_sizes = response.css(".bb_form--select option::text").getall()
-        if product_sizes:
-            del product_sizes[0]
+        product_sizes = response.css(".bb_form--select option::text").getall()[1:]
         product_colors = self.get_colours(response)
         skus = {}
         for color in product_colors:
