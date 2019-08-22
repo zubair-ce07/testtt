b307e86a352272375e580be8474fbd23220681af
Uzair Amer <uzair.amer@arbisoft.com>
2019-08-22 15:38:22 +0500

    Refactored whole spider

--- speedo/speedo_spider.py
+++ speedo/speedo_spider.py
@@ -17,18 +17,42 @@ class SpeedoParser(Spider):
         'speedo.com.au'
     ]
 
+    genders = [
+        ('women', 'women'),
+        ('boy', 'boys'),
+        ('boys', 'boys'),
+        ('girls', 'girls'),
+        ('men', 'men'),
+        ('adults', 'unisex-adults'),
+        ('unisex', 'unisex-adults'),
+        ('kids', 'unisex-kids'),
+        ('babies', 'unisex-kids'),
+        ('toddlers', 'unisex-kids'),
+    ]
+
+    # TODO: Delete till start requests and add WIP if necessary
+    custom_settings = {
+        'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+    }
+
+    cookie = {'KP_UID': '4ed37e67-487e-f2fc-eef0-9a1e4e3b3da6'}
+
+    def start_requests(self):
+        yield Request(url='https://speedo.com.au/product/mariner-supreme/8_11317B972CB.html',
+                      cookies=self.cookie)
+
     def parse(self, response):
         item = SpeedoItem()
-        item['brand'] = self.brand
         item['skus'] = []
         item['image_urls'] = []
         item['url'] = response.url
+        item['brand'] = self.brand
         item['market'] = self.market
         item['retailer'] = self.retailer
+        item['trail'] = response.meta.get('trail', [])
         item['name'] = self.get_name(response)
         item['care'] = self.get_care(response)
         item['gender'] = self.get_gender(response)
-        item['trail'] = response.meta.get('trail', [])
         item['category'] = self.get_categories(response)
         item['retailer_sku'] = self.get_product_id(response)
         item['description'] = self.get_description(response)
@@ -37,16 +61,20 @@ class SpeedoParser(Spider):
         return self.next_request_or_item(item)
 
     def parse_colour_requests(self, response):
+        raw_product = json.loads(response.text)
         item = response.meta['item']
-        raw_sizes = json.loads(response.text)['product']['variationAttributes'][1]['values']
-        item['requests'].extend([Request(size['url'], callback=self.parse_skus_and_images) for size in raw_sizes])
+        item['image_urls'].extend(self.get_image_urls(raw_product))
+
+        attrs = raw_product['product']['variationAttributes']
+        raw_sizes = next((a['values'] for a in attrs if a['attributeId'].lower() == 'size'), [])
+        requests = [Request(s['url'], callback=self.parse_skus) for s in raw_sizes]
+        item['requests'].extend(requests)
 
         return self.next_request_or_item(item)
 
-    def parse_skus_and_images(self, response):
+    def parse_skus(self, response):
         item = response.meta['item']
         item['skus'].append(self.get_sku(response))
-        item['image_urls'].extend(self.get_image_urls(response))
 
         return self.next_request_or_item(item)
 
@@ -64,36 +92,62 @@ class SpeedoParser(Spider):
         urls = response.css('a.color-attribute::attr(href)').getall()
         return [Request(url=url, callback=self.parse_colour_requests) for url in urls]
 
-    def get_image_urls(self, response):
-        raw_urls = json.loads(response.text)['product']['images']['large']
-        return [url['url'] for url in raw_urls]
-
     def get_name(self, response):
         return response.css('.product-name::text').get()
+
+    def get_care(self, response):
+        care = response.css('.product-spec-list dt:contains("material") + dd::text').get(default='')
+        return self.sanitize_list(care.split(','))
+
+    def get_gender(self, response):
+        gender_candidate = response.css('.product-spec-list dd::text').get().lower()
+
+        for tag, gender in self.genders:
+            if tag in gender_candidate:
+                return gender
+
+        return 'unisex-adults'
+
+    def get_categories(self, response):
+        return self.sanitize_list(response.css('.breadcrumb a::text').getall()[1:-1])
 
     def get_product_id(self, response):
         return response.css('.product-id::text').get()
 
     def get_description(self, response):
-        css = '.render-product-details::attr(data-json)'
-        description_map = json.loads(response.css(css).get())
-        features = re.findall(r'<li>(.*?)</li>', description_map[1]['content'])
+        css = '.product-detail-card'
+        css = f'{css}.features li::text, {css}.details .card-text::text'
+        return self.sanitize_list(response.css(css).getall())
 
-        return self.sanitize_list([description_map[0]['content']] + features)
+    def get_image_urls(self, raw_product):
+        return [url['url'] for url in raw_product['product']['images']['large']]
 
     def get_sku(self, response):
-        product_map = json.loads(response.text)['product']['variationAttributes']
+        raw_product = json.loads(response.text)
+        raw_colour, raw_size = self.get_raw_colour_and_size(raw_product)
 
-        sku = self.get_pricing_details(response)
-        sku['colour'] = product_map[0]['displayValue']
-        sku['size'] = product_map[1]['displayValue']
-        sku['sku_id'] = f'{sku["colour"]}-{sku["size"]}'
-        sku['out_of_stock'] = self.get_out_of_stock(response)
+        sku = self.get_pricing_details(raw_product)
+        if raw_colour:
+            sku['colour'] = raw_colour['displayValue']
+        sku['size'] = raw_size['displayValue'] if raw_size else 'One Size'
+        sku['sku_id'] = f'{sku.get("colour", "")}-{sku["size"]}'
+        sku['out_of_stock'] = self.get_out_of_stock(raw_product)
 
         return sku
 
-    def get_pricing_details(self, response):
-        price_map = json.loads(response.text)['product']['price']
+    def get_raw_colour_and_size(self, raw_product):
+        raw_colour = raw_size = ''
+
+        for attrs in raw_product['product']['variationAttributes']:
+            if attrs['attributeId'].lower() in ('color', 'colour'):
+                raw_colour = attrs
+            if attrs['attributeId'].lower() == 'size':
+                raw_size = attrs
+
+        return raw_colour, raw_size
+
+    def get_pricing_details(self, raw_product):
+        price_map = raw_product['product']['price']
         pricing = {'price': self.sanitize_price(price_map['sales']['decimalPrice'])}
         pricing['currency'] = price_map['sales']['currency']
 
@@ -102,26 +156,15 @@ class SpeedoParser(Spider):
 
         return pricing
 
-    def get_categories(self, response):
-        return self.sanitize_list(response.css('.breadcrumb a::text').getall()[1:-1])
-
-    def get_gender(self, response):
-        gender = response.css('.product-spec-list dd::text').get()
-        return 'unisex-kids' if gender in ['Babies', 'Toddlers'] else gender
-
-    def get_care(self, response):
-        care = response.css('.product-spec-list dd:nth-last-child(1)::text').get()
-        return self.sanitize_list(care.split(','))
+    def get_out_of_stock(self, raw_product):
+        messages = raw_product['product']['availability']['messages']
+        return not bool(re.findall(r'>(.*?)</div>', messages[0]))
 
     def sanitize_list(self, inputs):
         return [i.strip() for i in inputs if i and i.strip()]
 
     def sanitize_price(self, price):
         return float(''.join(re.findall(r'\d+', price)))
-
-    def get_out_of_stock(self, response):
-        messages = json.loads(response.text)['product']['availability']['messages']
-        return not bool(re.findall(r'>(.*?)</div>', messages[0]))
 
 
 class SpeedoCrawler(CrawlSpider):
@@ -140,14 +183,14 @@ class SpeedoCrawler(CrawlSpider):
         'USER_AGENT': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
     }
 
-    cookie = {'KP_UID': '28cff93f-9c85-e95e-ddb5-449687693fc5'}
+    cookie = {'KP_UID': '4ed37e67-487e-f2fc-eef0-9a1e4e3b3da6'}
 
     listing_css = ['.nav-item.dropdown', '.product-list-cards', '.show-more']
     product_css = '.product-grid'
     rules = [
-        Rule(link_extractor=LinkExtractor(restrict_css=product_css), callback='parse_product'),
-        Rule(link_extractor=LinkExtractor(restrict_css=listing_css, tags=['a', 'button::attr(data-url)']),
-             callback='parse'),
+        Rule(link_extractor=LinkExtractor(restrict_css=product_css), callback=speedo_parser.parse),
+        Rule(link_extractor=LinkExtractor(restrict_css=listing_css, tags=['a', 'button::attr(data-url)'],
+                                          attrs=['href', 'data-url']), callback='parse'),
     ]
 
     def start_requests(self):
@@ -158,9 +201,6 @@ class SpeedoCrawler(CrawlSpider):
         trail = self.add_trail(response)
 
         return [r.replace(meta={**r.meta, 'trail': trail.copy()}) for r in requests]
-
-    def parse_product(self, response):
-        yield self.speedo_parser.parse(response)
 
     def add_trail(self, response):
         new_trail = [(response.css('head title::text').get(), response.url)]
