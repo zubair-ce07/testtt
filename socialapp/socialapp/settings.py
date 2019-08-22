e23bdfe635762413104eec241ad6f0f32eda8e8a
Rehan Ahmad <rehan99000@gmail.com>
2019-08-22 16:35:56 +0500

    basic structure

--- socialapp/socialapp/settings.py
+++ socialapp/socialapp/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user_management',
+    'posts_management',
+    'messages_management',
 ]
 
 MIDDLEWARE = [
