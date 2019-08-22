25c6af197d8bb92998897a6810b80629dc5e5995
FarhanAS1 <farhan.shoukat@arbisoft.com>
2019-08-22 14:20:12 +0500

    all changes performed

--- fila_crawler.py
+++ fila_crawler.py
@@ -13,62 +13,53 @@ class FilaCrawler(CrawlSpider):
     allowed_domains = ['fila.com.br']
     start_urls = ['https://www.fila.com.br/']
 
+    listings_css = ('ol.nav-primary > li > a', 'a.next')
+
+    currency = 'BRL'
+    brand = 'Fila'
+    retailer = 'fila-br'
+
     rules = (
-        Rule(LinkExtractor(deny='outlet', restrict_css=('ol.nav-primary > li > a', 'a.next')), callback='parse'),
+        Rule(LinkExtractor(deny='outlet', restrict_css=listings_css), callback='parse'),
         Rule(LinkExtractor(restrict_css='ul.products-grid.products-grid-comum > li div.container-info a'),
              callback='parse_product'),
     )
 
     def parse(self, response):
-        requests = list(super().parse(response))
         trail = response.meta.get('trail', [["", response.url]])
-        for request in requests:
+        for request in super().parse(response):
             request.meta['trail'] = trail + [[request.meta['link_text'].strip(), request.url]]
             yield request
 
     def parse_product(self, response):
-        retailer_sku = FilaCrawler.get_retailer_sku(response)
+        product = dict()
 
-        trail = response.meta['trail'][:-1]
+        product['retailer_sku'] = self.extract_retailer_sku(response)
+        product['trail'] = response.meta['trail'][:-1]
+        product['gender'] = self.extract_gender(response.url)
+        product['category'] = self.extract_category(product['trail'])
+        product['brand'] = FilaCrawler.brand
+        product['url'] = self.extract_url(response)
+        product['date'] = self.extract_date(response)
+        product['currency'] = FilaCrawler.currency
+        product['market'] = self.extract_market(product['currency'])
+        product['retailer'] = FilaCrawler.retailer
+        product['url_original'] = response.url
+        product['name'] = self.extract_name(response)
+        product['description'] = self.extract_description(response)
+        product['care'] = self.extract_care(product['description'])
+        product['image_urls'] = self.extract_image_urls(response)
+        product['price'] = self.extract_price(response)
+        product['skus'] = self.extract_skus(response, product['retailer_sku'], product['price'], product['currency'])
+        product['spider_name'] = FilaCrawler.name
+        product['crawl_start_time'] = self.extract_crawl_start_time()
 
-        name = FilaCrawler.get_name(response)
+        yield product
 
-        description = FilaCrawler.get_description(response)
-
-        price = FilaCrawler.get_price(response)
-
-        currency = FilaCrawler.get_currency()
-
-        market = FilaCrawler.get_market(currency)
-
-        yield {
-            'retailer_sku': retailer_sku,
-            'trail': trail,
-            'gender': FilaCrawler.get_gender(response.url),
-            'category': FilaCrawler.get_category(trail),
-            'brand': 'Fila',
-            'url': FilaCrawler.get_url(response),
-            'date': FilaCrawler.get_date(response),
-            'market': market,
-            'retailer': FilaCrawler.get_retailer(response, market),
-            'url_original': response.url,
-            'name': name,
-            'description': description,
-            'care': FilaCrawler.get_care(description),
-            'image_urls': FilaCrawler.get_image_urls(response),
-            'skus': FilaCrawler.get_skus(response, retailer_sku, price, currency),
-            'price': price,
-            'currency': currency,
-            'spider_name': FilaCrawler.name,
-            'crawl_start_time': self.get_crawl_start_time()
-        }
-
-    @staticmethod
-    def get_retailer_sku(response):
+    def extract_retailer_sku(self, response):
         return response.css('.wrap-sku > small::text').get().strip()
 
-    @staticmethod
-    def get_gender(url):
+    def extract_gender(self, url):
         genders = {
             'masculina': {'masculina', 'masculino'},
             'feminina': {'feminina', 'feminino'},
@@ -76,66 +67,55 @@ class FilaCrawler(CrawlSpider):
             'unisex': {'unisex'}
         }
 
-        name_lowered = url.lower()
+        url_lowercase = url.lower()
         for gender in genders:
-            if check_existence(genders[gender], name_lowered):
+            if check_existence(genders[gender], url_lowercase):
                 return gender
         return ''
 
-    @staticmethod
-    def get_description(response):
+    def extract_description(self, response):
         description = response.css('div.wrap-long-description > p::text').get()
         return [description] if description is not None else []
 
-    @staticmethod
-    def get_care(description):
+    def extract_care(self, description):
         care_words = {'sintética', 'composição'}
         return description if len(description) != 0 and check_existence(care_words, description[0].lower()) else []
 
-    @staticmethod
-    def get_category(trail):
+    def extract_category(self, trail):
         return [t[0] for t in itertools.islice(trail, 1, None)]
 
-    @staticmethod
-    def get_url(response):
+    def extract_url(self, response):
         return response.css('head link[rel="canonical"]::attr("href")').get()
 
-    @staticmethod
-    def get_date(response):
+    def extract_date(self, response):
         date = response.headers["Date"].decode('utf-8')
         return datetime.strptime(date, '%a, %d %b %Y %H:%M:%S GMT').strftime('%Y-%m-%dT%H:%M:%S.%f')
 
-    @staticmethod
-    def get_market(currency):
+    def extract_market(self, currency):
         return ccy.currency(currency).default_country
 
-    @staticmethod
-    def get_retailer(response, market):
+    def extract_retailer(self, response, market):
         return f'{urlparse(response.url).netloc.split(".")[1]}-{market.lower()}'
 
-    @staticmethod
-    def get_name(response):
+    def extract_name(self, response):
         return response.css('div.product-name h1::text').get().strip()
 
-    @staticmethod
-    def get_image_urls(response):
+    def extract_image_urls(self, response):
         return list(set(response.css('.product-image-gallery > img::attr("src")').getall()))
 
-    @staticmethod
-    def get_skus(response, retailer_sku, price, currency):
+    def extract_skus(self, response, retailer_sku, price, currency):
+        price_currency_dict = {'price': price, 'currency': currency}
+
         product_sizes = response.css('#configurable_swatch_size .swatch-label::text').getall()
-        return [{'price': price, 'currency': currency, 'size': product_size, 'sku_id': f'{retailer_sku}-{product_size}'}
+        return [{**price_currency_dict,
+                 'size': product_size,
+                 'sku_id': f'{retailer_sku}-{product_size}'}
                 for product_size in product_sizes]
 
-    @staticmethod
-    def get_price(response):
+    def extract_price(self, response):
         return response.css('.normal_price_span::text').get()
 
-    @staticmethod
-    def get_currency():
-        return 'BRL'
-
-    def get_crawl_start_time(self):
+    def extract_crawl_start_time(self):
         return self.crawler.stats._stats['start_time'].strftime('%Y-%m-%dT%H:%M:%S.%f')
 
 
