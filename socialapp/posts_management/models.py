c1d1c6050d1fa7de54261cf340003d0c860bb1b9
Rehan Ahmad <rehan99000@gmail.com>
2019-08-23 14:57:27 +0500

    defined schema

--- socialapp/posts_management/models.py
+++ socialapp/posts_management/models.py
@@ -1,3 +1,43 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from socialapp.user_management.models import UserProfile, Group
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, default=None, null=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+
+class TextPost(Post):
+    text = models.TextField()
+
+
+def get_image_filename(instance):
+    return "post_images/{}_{}".format(instance.post.id, instance.post.created_at)
+
+
+class Picture(models.Model):
+    image = models.ImageField(upload_to=get_image_filename)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+
+class EventPost(Post):
+    date = models.DateField(default='')
+    time = models.TimeField(default='')
+    description = models.TextField()
+
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    vote = models.BooleanField(default=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    text = models.TextField()
--- socialapp/user_management/models.py
+++ socialapp/user_management/models.py
@@ -8,12 +8,20 @@ class Group(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    group = models.ForeignKey(Group, on_delete=models.DO_NOTHING, blank=True, default=SET_NULL)
+    group = models.ManyToManyField(Group, on_delete=models.DO_NOTHING, blank=True, default=SET_NULL)
     phone = PhoneField(blank=True, )
     address = models.TextField()
+
+    friends = models.ManyToManyField('self', through='Friend')
+
+    def __str__(self):
+        return self.user.username
 
 
 class WorkInformation(models.Model):
@@ -40,7 +48,13 @@ class AcademicInformation(models.Model):
     end_date = models.DateField(default='')
 
 
-class Friends(models.Model):
-    user1 = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='user1')
-    user2 = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='user2')
+class UserGroup(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    is_admin = models.BooleanField(default=False)
+
+
+class Friend(models.Model):
+    user_to = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='user_to')
+    user_from = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='user_from')
     close_friend = models.BooleanField(default=False)
