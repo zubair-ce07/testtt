7b79a5e2eeef4f5d8087fd7b96e488b8b76f5efd
Abdullah Zafar <mabdullahzafar08@gmail.com>
2019-08-23 17:09:50 +0500

    Move routes code to main (library.js) file

--- nodebb-plugin-philu-badging/src/routes.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var routes = module.exports;
-
-routes.init = function (params, controllers, callback) {
-    params.router.post('/api/saveConfig', [], controllers.saveConfiguration);
-};
