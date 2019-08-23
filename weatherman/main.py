cf47c08afa8fb213d5a8d8fa3da06553ddbb282c
Saeed Raza <saeed.raza@arbisoft.com>
2019-08-23 20:16:51 +0500

    improved code quality

--- weatherman/main.py
+++ weatherman/main.py
@@ -6,143 +6,131 @@ import glob
 
 
 class FileManager:
-    def __init__(self, filename, mode):
-        self.filename = filename
-        self.mode = mode
-        self.file = None
-
-    def __enter__(self):
-        try:
-            self.file = open(self.filename, self.mode)
-            self.file.__next__()
-            csv_reader = csv.DictReader(self.file)
-            return csv_reader
-        except EnvironmentError:
-            print("%s File not found" % self.filename)
-            return []
-
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        if not self.file.closed:
-            self.file.close()
+    @staticmethod
+    def read(filename, mode):
+        csv_file = open(filename, mode)
+        csv_file.__next__()
+        dict_reader = csv.DictReader(csv_file)
+        for row in dict_reader:
+            yield row
 
 
 class Formatter:
     @staticmethod
-    def display_extremes(results):
+    def display_extremes(result):
         print("\n\nHighest: {}C on {} {}".format(
-            results["highest"],
-            datetime.datetime.strptime(results["highest_day"], "%Y-%m-%d").strftime("%b"),
-            datetime.datetime.strptime(results["highest_day"], "%Y-%m-%d").day))
+            result["highest"],
+            datetime.datetime.strptime(result["highest_day"], "%Y-%m-%d").strftime("%b"),
+            datetime.datetime.strptime(result["highest_day"], "%Y-%m-%d").day))
         print("Lowest: {}C on {} {}".format(
-            results["lowest"],
-            datetime.datetime.strptime(results["lowest_day"], "%Y-%m-%d").strftime("%b"),
-            datetime.datetime.strptime(results["lowest_day"], "%Y-%m-%d").day))
+            result["lowest"],
+            datetime.datetime.strptime(result["lowest_day"], "%Y-%m-%d").strftime("%b"),
+            datetime.datetime.strptime(result["lowest_day"], "%Y-%m-%d").day))
         print("Humid: {}C on {} {}".format(
-            results["humid"],
-            datetime.datetime.strptime(results["humid_day"], "%Y-%m-%d").strftime("%b"),
-            datetime.datetime.strptime(results["humid_day"], "%Y-%m-%d").day))
+            result["humid"],
+            datetime.datetime.strptime(result["humid_day"], "%Y-%m-%d").strftime("%b"),
+            datetime.datetime.strptime(result["humid_day"], "%Y-%m-%d").day))
 
     @staticmethod
-    def display_averages(results):
-        print("\n\nHighest Average: {}C".format(int(results["sum_high_temp"] / results["days_high_temp"])))
-        print("Lowest Average: {}C".format(int(results["sum_low_temp"] / results["days_low_temp"])))
-        print("Average Humidity: {}".format(int(results["sum_humidity"] / results["days_humidity"])))
+    def display_averages(result):
+        print("\n\nHighest Average: {}C".format(result["avg_high_temp"]))
+        print("Lowest Average: {}C".format(result["avg_low_temp"]))
+        print("Average Humidity: {}".format(result["avg_humidity"]))
 
     @staticmethod
-    def display_bars(results):
-        for day, bars in results.items():
+    def display_bars(result):
+        for day, bars in result.items():
             print("{}\033[0;32;40m {}\033[0;31;40m{} \033[0;37;40m{}C - {}C \n".format(
                 day, bars[0], bars[1], len(bars[0]), len(bars[1])))
 
 
 class Operations:
     def find_extremes(self, files):
-        results = {}
+        result = {}
+        file_manager = FileManager()
         for file in files:
-            with FileManager(file, 'r') as rows:
-                rows.__next__()
-                for row in rows:
-                    flag, high, max_day = self.get_max_temperature(row, results)
-                    if flag:
-                        results["highest"], results["highest_day"] = high, max_day
+            for row in file_manager.read(file, 'r'):
+                flag, high, max_day = self.get_max_temperature(row, result)
+                if flag:
+                    result["highest"], result["highest_day"] = high, max_day
 
-                    flag, low, low_day = self.get_lowest_temperature(row, results)
-                    if flag:
-                        results["lowest"], results["lowest_day"] = low, low_day
+                flag, low, low_day = self.get_lowest_temperature(row, result)
+                if flag:
+                    result["lowest"], result["lowest_day"] = low, low_day
 
-                    flag, humid, humid_day = self.get_max_humidity(row, results)
-                    if flag:
-                        results["humid"], results["humid_day"] = humid, humid_day
-        return results
+                flag, humid, humid_day = self.get_max_humidity(row, result)
+                if flag:
+                    result["humid"], result["humid_day"] = humid, humid_day
+
+        return result
 
     @staticmethod
-    def get_max_temperature(row, results):
-        if ("highest" not in results and row["Max TemperatureC"]) \
+    def get_max_temperature(row, result):
+        if ("highest" not in result and row["Max TemperatureC"]) \
                 or (row["Max TemperatureC"]
-                    and int(row["Max TemperatureC"]) > results["highest"]):
+                    and int(row["Max TemperatureC"]) > result["highest"]):
             keys = list(row.keys())
             return True, int(row["Max TemperatureC"]), row[keys[0]]
         return False, None, None
 
     @staticmethod
-    def get_lowest_temperature(row, results):
-        if ("lowest" not in results and row["Min TemperatureC"]) \
+    def get_lowest_temperature(row, result):
+        if ("lowest" not in result and row["Min TemperatureC"]) \
                 or (row["Min TemperatureC"]
-                    and int(row["Min TemperatureC"]) < results["lowest"]):
+                    and int(row["Min TemperatureC"]) < result["lowest"]):
             keys = list(row.keys())
             return True, int(row["Min TemperatureC"]), row[keys[0]]
         return False, None, None
 
     @staticmethod
-    def get_max_humidity(row, results):
-        if ("humid" not in results and row["Max Humidity"]) \
+    def get_max_humidity(row, result):
+        if ("humid" not in result and row["Max Humidity"]) \
                 or (row["Max Humidity"]
-                    and int(row["Max Humidity"]) > results["humid"]):
+                    and int(row["Max Humidity"]) > result["humid"]):
             keys = list(row.keys())
             return True, int(row["Max Humidity"]), row[keys[0]]
         return False, None, None
 
     @staticmethod
     def calculate_averages(file):
-        results = {
-            "sum_high_temp": 0,
-            "days_high_temp": 0,
-            "sum_low_temp": 0,
-            "days_low_temp": 0,
-            "sum_humidity": 0,
-            "days_humidity": 0,
+        result = {
+            "avg_high_temp": 0,
+            "avg_low_temp": 0,
+            "avg_humidity": 0,
         }
-        with FileManager(file, 'r') as rows:
-            rows.__next__()
-            for row in rows:
-                if row["Max TemperatureC"]:
-                    results["sum_high_temp"] += int(row["Max TemperatureC"])
-                    results["days_high_temp"] += 1
-                if row["Min TemperatureC"]:
-                    results["sum_low_temp"] += int(row["Min TemperatureC"])
-                    results["days_low_temp"] += 1
-                if row[" Mean Humidity"]:
-                    results["sum_humidity"] += int(row[" Mean Humidity"])
-                    results["days_humidity"] += 1
-        return results
+        days_high_temp = 0
+        days_low_temp = 0
+        days_humidity = 0
+        file_manager = FileManager()
+        for row in file_manager.read(file, 'r'):
+            if row["Max TemperatureC"]:
+                result["avg_high_temp"] += int(row["Max TemperatureC"])
+                days_high_temp += 1
+            if row["Min TemperatureC"]:
+                result["avg_low_temp"] += int(row["Min TemperatureC"])
+                days_low_temp += 1
+            if row[" Mean Humidity"]:
+                result["avg_humidity"] += int(row[" Mean Humidity"])
+                days_humidity += 1
+        result["avg_high_temp"] = int(result["avg_high_temp"] / days_high_temp)
+        result["avg_low_temp"] = int(result["avg_low_temp"] / days_low_temp)
+        result["avg_humidity"] = int(result["avg_humidity"] / days_humidity)
+        return result
 
     @staticmethod
     def generate_bars(file):
-        results = {}
-        with FileManager(file, 'r') as rows:
-            for row in rows:
-                low_temp_str = ''
-                high_temp_str = ''
-                keys = list(row.keys())
-
-                if row["Min TemperatureC"]:
-                    for x in range(int(row["Min TemperatureC"])):
-                        high_temp_str += '+'
-                if row["Max TemperatureC"]:
-                    for x in range(int(row["Max TemperatureC"])):
-                        low_temp_str += '+'
-                results[row[keys[0]]] = [low_temp_str, high_temp_str]
-        return results
+        result = {}
+        file_manager = FileManager()
+        for row in file_manager.read(file, 'r'):
+            low_temp_str = ''
+            high_temp_str = ''
+            keys = list(row.keys())
+            if row["Min TemperatureC"]:
+                high_temp_str = "+" * int(row["Min TemperatureC"])
+            if row["Max TemperatureC"]:
+                low_temp_str = "+" * int(row["Min TemperatureC"])
+            result[row[keys[0]]] = [low_temp_str, high_temp_str]
+        return result
 
 
 def parse_arguments():
@@ -157,11 +145,12 @@ def parse_arguments():
 
 
 def get_file_to_read(args, all_files):
-    year, mon = args.time_span.split("/")[0], args.time_span.split("/")[1]
+    date_values = args.time_span.split("/")
+    year, mon = date_values[0], date_values[1]
     file_to_read = ''
-    for file in all_files:
-        if file.__contains__(year) and file.__contains__(calendar.month_abbr[int(mon)]):
-            file_to_read = file
+    for file_path in all_files:
+        if year in file_path and calendar.month_abbr[int(mon)] in file_path:
+            file_to_read = file_path
     return file_to_read
 
 
@@ -173,7 +162,7 @@ def main():
     if args.extremes:
         files_to_read = []
         for file in all_files:
-            if file.__contains__(args.time_span):
+            if args.time_span in file:
                 files_to_read.append(file)
         fmt.display_extremes(ops.find_extremes(files_to_read))
     elif args.averages:
