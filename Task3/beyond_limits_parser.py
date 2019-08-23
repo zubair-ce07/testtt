86fde0d6778642ca11e1e19029e99eb5c8b531df
Arslan Shafiq <arslan.shafiq@arbisoft.com>
2019-08-23 15:45:26 +0500

    - organized code - updated function names

--- Task3/beyond_limits_parser.py
+++ Task3/beyond_limits_parser.py
@@ -12,38 +12,44 @@ class BeyondParserSpider(scrapy.Spider):
 
     def parse_product(self, response):
         product_item = Product()
-        product_item['name'] = response.css(".bb_art--title::text").get()
-        product_item['retailer_sku'] = response.css(".bb_art--artnum span::text").get()
+        product_item['name'] = self.parse_name(response)
+        product_item['retailer_sku'] = self.parse_retailer_sku(response)
         product_item['brand'] = "BeyondLimits"
         product_item['lang'] = "en"
-        product_item['description'] = self.get_product_description(response)
+        product_item['description'] = self.parse_product_description(response)
         product_item['image_urls'] = self.parse_image_urls(response)
-        product_item['gender'] = self.get_gender(response)
-        product_item['category'] = self.get_category(response)
-        product_item['url'] = response.request.url
-        product_item['care'] = self.get_care(response)
-        product_item['skus'] = self.get_skus(response)
+        product_item['gender'] = self.parse_gender(response)
+        product_item['category'] = self.parse_category(response)
+        product_item['url'] = response.url
+        product_item['care'] = self.parse_care(response)
+        product_item['skus'] = self.parse_skus(response)
 
         return product_item
 
-    def get_product_description(self, response):
+    def parse_name(self, response):
+        return response.css(".bb_art--title::text").get()
+
+    def parse_retailer_sku(self, response):
+        return response.css(".bb_art--artnum span::text").get()
+
+    def parse_product_description(self, response):
         return response.css("#description p::text, #description::text").getall()
 
     def parse_image_urls(self, response):
         return response.css(".bb_pic--navlink ::attr(href)").getall()
 
-    def get_gender(self, response):
-        category = ' '.join(self.get_category(response))
+    def parse_gender(self, response):
+        soup = ' '.join(self.parse_category(response))
         for gender in self.possible_genders:
-            if gender.lower() in category.lower():
+            if gender.lower() in soup.lower():
                 return gender
 
-    def get_category(self, response):
+    def parse_category(self, response):
         categories_css = ".bb_breadcrumb--item span::text, .bb_breadcrumb--item strong::text"
         categories = response.css(categories_css).getall()
-        return categories[1:] if categories else []
+        return categories[1:]
 
-    def get_care(self, response):
+    def parse_care(self, response):
         description_list = response.css("#description li::text").getall()
         product_care = []
         for product_feature in description_list:
@@ -51,16 +57,16 @@ class BeyondParserSpider(scrapy.Spider):
                 product_care.append(product_feature)
         return product_care
 
-    def get_colours(self, response):
+    def parse_colours(self, response):
         description = " ".join(response.css("#description li::text").getall())
         return re.findall('Colour: (.*?) Care', description, re.DOTALL)
 
-    def get_skus(self, response):
+    def parse_skus(self, response):
         product_price = response.css(".price span::attr(content)").get()
         old_price = response.css(".oldPrice del::text").getall()
         currency = response.css(".price meta::attr(content)").get()
         product_sizes = response.css(".bb_form--select option::text").getall()[1:]
-        product_colors = self.get_colours(response)
+        product_colors = self.parse_colours(response)
         skus = {}
         for color in product_colors:
             for size in product_sizes:
