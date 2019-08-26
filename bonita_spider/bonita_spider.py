c065c555347e99344be45e8dd84841542dbc0f96
Ibraheem MuhammadNadeem <ibraheem.m@arbisoft.com>
2019-08-26 14:29:29 +0500

    Improved usage of base and css selectors

--- bonita_spider/bonita_spider.py
+++ bonita_spider/bonita_spider.py
@@ -9,7 +9,7 @@ class Mixin:
     default_brand = "Bonita"
 
 
-class MixinDe(Mixin):
+class MixinDE(Mixin):
     retailer = Mixin.retailer + "-de"
     market = "DE"
 
@@ -18,7 +18,7 @@ class MixinDe(Mixin):
 
 
 class ParseSpider(BaseParseSpider):
-    description_css = "#productDetails .m-product-details__text li::text"
+    raw_description_css = ".m-product-details__text ::text"
     care_css = "#material .m-product-details__text ul li::text"
     price_css = ".o-product-information__product-price"
 
@@ -33,13 +33,13 @@ class ParseSpider(BaseParseSpider):
 
         garment["gender"] = self.product_gender(garment)
         garment["image_urls"] = self.image_urls(response)
-        garment["skus"] = self.skus(response, garment)
+        garment["skus"] = self.skus(response)
 
         return garment
 
     def product_id(self, response):
         css = ".m-product-details__text p::text"
-        return response.css(css).getall()[-1].split(": ")[1]
+        return clean(response.css(css))[-1].split(": ")[1]
 
     def product_name(self, response):
         css = ".o-product-information__product-name::text"
@@ -55,28 +55,36 @@ class ParseSpider(BaseParseSpider):
 
     def image_urls(self, response):
         css = ".m-zoom__image::attr(srcset)"
-        images = response.css(css).getall()
+        images = clean(response.css(css))
         return [image.split("1x, ")[1].split("2x")[0] for image in images]
 
-    def skus(self, response, garment):
+    def skus(self, response):
         skus = {}
         common_sku = self.product_pricing_common(response)
-        colour = self.detect_colour(garment["name"])
+        colour = self.detect_colour_from_name(response)
 
         if colour:
             common_sku["colour"] = colour
+        else:
+            common_sku["colour"] = ""
 
-        for size in clean(response.css(".a-size__label::text")):
+        for size_sel in response.css(".a-size__label"):
             sku = common_sku.copy()
-            sku["size"] = size
+            sku["size"] = size_sel.css("::text").get()
 
-            skus[f"{colour}_{size}"] = sku
+            if size_sel.css(".a-size__label--disabled"):
+                sku["out_of_stock"] = True
+
+            skus[f"{sku['colour']}_{sku['size']}"] = sku
+
+        if not skus:
+            common_sku["size"] = "One Size"
+            skus[f"{common_sku['colour']}_{common_sku['size']}"] = common_sku
 
         return skus
 
 
 class CrawlSpider(BaseCrawlSpider):
-
     products_css = [".o-product-list"]
     listings_css = [
         ".o-header",
@@ -89,10 +97,10 @@ class CrawlSpider(BaseCrawlSpider):
     )
 
 
-class ParseSpiderDe(MixinDe, ParseSpider):
-    name = MixinDe.retailer + "-parse"
+class ParseSpiderDE(MixinDE, ParseSpider):
+    name = MixinDE.retailer + "-parse"
 
 
-class CrawlSpiderDe(MixinDe, CrawlSpider):
-    name = MixinDe.retailer + "-crawl"
-    parse_spider = ParseSpiderDe()
+class CrawlSpiderDE(MixinDE, CrawlSpider):
+    name = MixinDE.retailer + "-crawl"
+    parse_spider = ParseSpiderDE()
