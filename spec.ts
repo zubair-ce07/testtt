3cb415b7be6dc6c0168101f50644159d06591e5e
junaid1840 <junaid_nazir15@yahoo.com>
2019-08-28 14:44:00 +0500

    Used chai as promised instead of async await in assertions where possible.

--- spec.ts
+++ spec.ts
@@ -1,4 +1,9 @@
-import {expect,assert} from 'chai';
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised'
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+const assert = chai.assert;
+
 import HotelsFrontPage from "./pages/HotelsFrontPage";
 import {HotelSearchResultsPage} from "./pages/HotelSearchResultsPage";
 import {MapPage} from "./pages/MapPage";
@@ -8,58 +13,58 @@ describe('Hotels page', () => {
     const hotelSearchResultsPageObj = new HotelSearchResultsPage();
     const mapPageObj = new MapPage();
     hotelsFrontPageObj.goToHotelsFrontPage();
-    it('Should display the origin field', async () => {
-        expect(await hotelsFrontPageObj.isHotelsOriginVisible()).to.be.true;
+    it('Should display the origin field', () => {
+        expect(hotelsFrontPageObj.isHotelsOriginVisible()).to.eventually.be.true;
     });
-    it('Should display the start date field', async () => {
-        expect(await hotelsFrontPageObj.isHotelsStartDateVisible()).to.be.true;
+    it('Should display the start date field', () => {
+        expect(hotelsFrontPageObj.isHotelsStartDateVisible()).to.eventually.be.true;
     });
-    it('Should display the start date field', async () => {
-        expect(await hotelsFrontPageObj.isHotelsEndDateVisible()).to.be.true;
+    it('Should display the start date field', () => {
+        expect(hotelsFrontPageObj.isHotelsEndDateVisible()).to.eventually.be.true;
     });
-    it('Should display ‘1 room, 2 guests’ in guests field', async () => {
-        expect(await hotelsFrontPageObj.getGuestFieldText()).to.equal('1 room, 2 guests')
+    it('Should display ‘1 room, 2 guests’ in guests field',() => {
+        expect(hotelsFrontPageObj.getGuestFieldText()).to.eventually.equal('1 room, 2 guests')
     });
     it('Should load hotels results page', async () => {
         await hotelsFrontPageObj.searchByNewHotelsOrigin();
-        expect(await hotelSearchResultsPageObj.isHotelsResultPageLoaded()).to.be.true;
+        expect(await hotelSearchResultsPageObj.isHotelsResultPageLoaded()).to.eventually.be.true;
     });
     it('Should display at least 5 hotel results',async () => {
         assert.isAtLeast(await hotelSearchResultsPageObj.getTotalNumOfHotelsResults(),5);
     });
     it('Should display hotel details section', async () => {
         await hotelSearchResultsPageObj.openHotelDetailsDropdown();
-        expect(await hotelSearchResultsPageObj.isHotelDetailsDropdownVisible()).to.be.true;
+        expect(await hotelSearchResultsPageObj.isHotelDetailsDropdownVisible()).to.eventually.be.true;
     });
-    it('Should display hotel images in ‘Details’ section.', async () => {
-        expect(await hotelSearchResultsPageObj.isDetailsTabPhotosVisible()).to.be.true;
+    it('Should display hotel images in ‘Details’ section.',() => {
+        expect(hotelSearchResultsPageObj.isDetailsTabPhotosVisible()).to.eventually.be.true;
     });
-    it('Should display map in ‘Map’ section.', async () => {
-        expect(await hotelSearchResultsPageObj.isMapInMapTabVisible()).to.be.true;
+    it('Should display map in ‘Map’ section.', () => {
+        expect(hotelSearchResultsPageObj.isMapInMapTabVisible()).to.eventually.be.true;
     });
-    it('Should display reviews in ‘Reviews’ section', async () => {
-        expect(await hotelSearchResultsPageObj.isReviewsInReviewsTabVisible()).to.be.true;
+    it('Should display reviews in ‘Reviews’ section', () => {
+        expect(hotelSearchResultsPageObj.isReviewsInReviewsTabVisible()).to.eventually.be.true;
     });
-    it('Should display rates in ‘Rates’ section', async () => {
-        expect(await hotelSearchResultsPageObj.isRatesInRatesTabVisible()).to.be.true;
+    it('Should display rates in ‘Rates’ section', () => {
+        expect(hotelSearchResultsPageObj.isRatesInRatesTabVisible()).to.eventually.be.true;
     });
     it('Should display map view',async () => {
         await mapPageObj.clickShowMap();
-       expect(await mapPageObj.isMainMapViewVisible()).to.be.true;
+        expect(mapPageObj.isMainMapViewVisible()).to.eventually.be.true;
     });
-    it('Should display left rail filters', async () => {
-        expect(await mapPageObj.isHorizontalFiltersVisible()).to.be.true;
+    it('Should display left rail filters', () => {
+        expect(mapPageObj.isHorizontalFiltersVisible()).to.eventually.be.true;
     });
-    it('Should display hotel info', async () => {
-        expect(await mapPageObj.isHotelInfoVisible()).to.be.true;
+    it('Should display hotel info', () => {
+        expect(mapPageObj.isHotelInfoVisible()).to.eventually.be.true;
     });
-    it('Should display hotel card on the left side', async () => {
-       expect(await mapPageObj.isHotelCardVisible()).to.be.true;
+    it('Should display hotel card on the left side', () => {
+        expect(mapPageObj.isHotelCardVisible()).to.eventually.be.true;
     });
     it('Should open provider page in new tab', async () => {
         let providerName = await mapPageObj.getBookingProviderName();
         providerName = providerName.toLowerCase();
         await mapPageObj.clickViewDealBtn();
-        expect(await mapPageObj.isBookingProviderPageOpened(providerName)).to.include(providerName);
+        expect(mapPageObj.isBookingProviderPageOpened(providerName)).to.eventually.include(providerName);
     });
 });
