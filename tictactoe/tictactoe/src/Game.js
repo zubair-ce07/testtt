fa15dd16f743b3a534df7b0c8a9f00019009b59b
Rehan Ahmad <rehan99000@gmail.com>
2019-08-28 13:02:20 +0500

    implemented tictaoe in react and modularized game components

---
+++ tictactoe/tictactoe/src/Game.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "./App.css";
+import { Board, calculateWinner, Stats } from "./GameComponents"
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      console.log(squares);
+      return;
+    }
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      history: history.concat([{ squares: squares }]),
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
+  render() {
+    const current = this.state.history[this.state.stepNumber];
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board squares={current.squares} onClick={i => this.handleClick(i)} />
+        </div>
+        <div className="game-info">
+          <Stats
+            history={this.state.history}
+            stepNumber={this.state.stepNumber}
+            xIsNext={this.state.xIsNext}
+            setState={(state) => { this.setState(state) }}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+export default Game;
