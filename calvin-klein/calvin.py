8d406e9b1b4f473683c1bb6a569a6581be14e10c
ahmadarshi <m.ahmad@arbisoft.com>
2019-08-29 17:35:26 +0500

    refactored parsing

--- calvin-klein/calvin.py
+++ calvin-klein/calvin.py
@@ -18,7 +18,7 @@ def clean(raw_strs):
 
 
 class CalvinParseSpider(Spider):
-    name = 'calvinparse'
+    name = 'calvinparsespider'
 
     def parse_item(self, response):
         item = CalvinItem()
@@ -33,13 +33,12 @@ class CalvinParseSpider(Spider):
         item['trail'] = self.product_trail(response)
         item['image_urls'] = self.product_image_urls(response)
         item['skus'] = self.product_skus(response)
-
         return [item] + self.colour_requests(response)
 
     def colour_requests(self, response):
         color_urls = response.css('.swatch-option ::attr(href)').getall()
-        return [Request(color_url, callback=self.parse)
-                for color_url in color_urls]
+        return [Request(color_url, callback=self.parse_item,
+                        meta={'trail': response.meta['trail']}) for color_url in color_urls]
 
     def retailer_sku(self, response):
         return response.css('::attr(data-product-sku)').get()
@@ -52,8 +51,7 @@ class CalvinParseSpider(Spider):
         return response.css(css).re_first('\'category\': \"(.+?)\"')
 
     def product_category(self, response):
-        return [category[0] for category in response.meta['trail']
-                if category[0]]
+        return [category[0] for category in response.meta['trail']]
 
     def product_url(self, response):
         return response.url
@@ -83,17 +81,20 @@ class CalvinParseSpider(Spider):
             'price': response.css('[itemprop="price"]::text').get(),
             'prev_price': response.css(prev_price_css).get()
         }
+
+        skus = {}
         size_css = 'script:contains(\"options\")'
         raw_sizes = response.css(size_css).re_first('\"options\"\:(.+?),\"position')
 
-        skus = {}
         for raw_size in json.loads(raw_sizes):
-            common_sku['size'] = raw_size['label']
-            sku_id = f'{common_sku["color"]}_{common_sku["size"]}'
-            skus[sku_id] = common_sku.copy()
+            sku = common_sku.copy()
+            sku['size'] = raw_size['label']
 
             if not int(raw_size['stock_status']):
-                skus[sku_id]['out_of_stock'] = True
+                sku['out_of_stock'] = True
+
+            sku_id = f'{sku["color"]}_{sku["size"]}'
+            skus[sku_id] = sku
 
         return skus
 
@@ -107,7 +108,7 @@ class CalvinCrawlSpider(CrawlSpider):
     parse_spider = CalvinParseSpider()
     PAGE_SIZE = 30
 
-    category_css = ['.sub-nav-wrapper']
+    category_css = ['.sub-nav-wrapper ']
     product_css = ['.product-item-photo']
 
     rules = (
@@ -115,21 +116,20 @@ class CalvinCrawlSpider(CrawlSpider):
         Rule(LinkExtractor(restrict_css=product_css), callback='parse_item')
     )
 
-    def parse(self, response):
-        trail = response.meta.get('trail', [])
-        categories_css = '[data-container="body"]::attr(class)'
-        categories = response.css(categories_css).re_first('categorypath-(.+?) ', '')
-        trail.append((categories.replace('-', ' '), response.url))
-
-        return Request(response.url, super().parse, meta={'trail': trail})
-
     def parse_categories(self, response):
-        total_pages = int(response.css('.toolbar-number::text').get()) // self.PAGE_SIZE + 2
+        response.meta['trail'] = self.get_product_trail(response)
+        self.parse(response)
 
-        for page_number in range(1, total_pages):
+        total_pages = int(response.css('.toolbar-number::text').get()) // self.PAGE_SIZE + 2
+        for page_number in range(2, total_pages):
+            trail = self.get_product_trail(response)
             pagination_url = add_or_replace_parameter(response.url, 'p', page_number)
-            yield Request(pagination_url, callback=self.parse,
-                          meta={'trail': response.meta['trail'].copy()})
+            yield Request(pagination_url, callback=self.parse, meta={'trail': trail})
 
     def parse_item(self, response):
-        return Request(response)
+        return self.parse_spider.parse_item(response)
+
+    def get_product_trail(self, response):
+        categories_css = '[data-container="body"]::attr(class)'
+        categories = response.css(categories_css).re_first('categorypath-(.+?) ', '')
+        return categories.replace('-', ' '), response.url
