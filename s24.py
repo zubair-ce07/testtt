d774125ea0f20e12485a1e48e179056e0570f17f
ahmadarshi <m.ahmad@arbisoft.com>
2019-08-29 19:23:34 +0500

    seprated method for parsing and crawling

--- s24.py
+++ s24.py
@@ -6,7 +6,8 @@ import urllib.parse as urlparse
 
 from scrapy import Item, Field, Request
 from scrapy.linkextractors import LinkExtractor
-from scrapy.spiders import CrawlSpider, Rule
+from scrapy.spiders import CrawlSpider, Spider, Rule
+from w3lib.url import add_or_replace_parameter, add_or_replace_parameters
 
 
 def clean(raw_strs):
@@ -32,89 +33,10 @@ class _24sItem(Item):
     requests = Field()
 
 
-class _24sSpider(CrawlSpider):
-    name = '24s'
-    allowed_domains = ['24s.com', 'dkpvob44gb-dsn.algolia.net']
-    start_urls = [
-        'https://www.24s.com/en-us/'
-    ]
+class _24sParseSpider(Spider):
+    name = '24sparsespider'
 
-    pagination_url = 'https://dkpvob44gb-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia+for+vanilla' \
-                     '+JavaScript+3.32.1%3BJS+Helper+2.26.1&x-algolia-application-id=DKPVOB44GB&x-algolia-api-' \
-                     'key=9b23af4f250bb432947c20b7c82890a2'
-
-    headers = {
-        'accept': 'application/json',
-        'Origin': 'https://www.24s.com',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 '
-                      'Safari/537.36',
-        'content-type': 'application/x-www-form-urlencoded',
-    }
-
-    body_json = json.loads('{"requests":[{"indexName":"prod_products","params":"query=&tagFilters={}&page=0&'
-                           'getRankingInfo=true&ruleContexts={}"}]}')
-
-    listings_css = ['.nav-link', '[class="device-drop-btn"]']
-    rules = (
-        Rule(LinkExtractor(restrict_css=listings_css), callback='parse_categories'),
-    )
-
-    def parse_categories(self, response):
-        body = self.request_payload(response)
-        return Request(self.pagination_url, method='POST', body=body, headers=self.headers,
-                       callback=self.parse_pagination)
-
-    def request_payload(self, response):
-        tag_filter = self.get_tag_filters(response)
-        rule_contexts = self.get_rule_contexts(response)
-        params = self.body_json['requests'][0]['params'].format(tag_filter, rule_contexts)
-        self.body_json['requests'][0]['params'] = params
-
-        return json.dumps(self.body_json)
-
-    def get_tag_filters(self, response):
-        raw_tag_filter = ["US"] + response.css('::attr(data-filter)').get().split(',')
-        tag_filter = str(raw_tag_filter).replace('\'', '"').replace(' ', '')
-        return urllib.parse.quote(tag_filter)
-
-    def get_rule_contexts(self, response):
-        raw_rule_context = response.css('::attr(data-filter)').get()
-
-        if ',' not in raw_rule_context:
-            raw_rule_context = f'{raw_rule_context},'
-
-        rule_context = raw_rule_context.replace('\'', '"').replace(' ', '').replace(',', '_MERCH_US')
-        return urllib.parse.quote(rule_context)
-
-    def parse_pagination(self, response):
-        self.parse_products(response)
-        total_pages = json.loads(response.text)['results'][0]['nbPages'] + 1
-
-        for page_number in range(1, total_pages):
-            body = self.request_body(response, page_number)
-            yield Request(self.pagination_url, method='POST', body=body, headers=self.headers,
-                          callback=self.parse_products, dont_filter=True)
-
-    def request_body(self, response, page_number):
-        body = json.loads(response.request.body)
-        body['requests'][0]['params'] = re.sub('page=(.+?)&', f'page={page_number}&',
-                                               body['requests'][0]['params'])
-        return json.dump(body)
-
-    def parse_products(self, response):
-        raw_products = json.loads(response.text)['results'][0]
-
-        for product in raw_products['hits']:
-            product_url = self.clean_url(f'{product["title_en"]}-{product["brand"]}_{product["item_group_id"]}?'
-                                         f'defaultSku={product["sku"]}&color={product["color_brand"]}')
-            yield response.follow(product_url, callback=self.parse_item)
-
-    def clean_url(self, raw_str):
-        raw_str = (c for c in unicodedata.normalize('NFD', raw_str) if unicodedata.category(c) != 'Mn')
-        raw_str = ''.join(raw_str).lower()
-        return re.sub(' |\/', '-', raw_str.replace('"', ''))
-
-    def parse_item(self, response):
+    def parse(self, response):
         item = _24sItem()
         item['retailer_sku'] = self.retailer_sku(response)
         item['gender'] = self.product_gender(response)
@@ -126,23 +48,14 @@ class _24sSpider(CrawlSpider):
         item['care'] = self.product_care(response)
         item['skus'] = self.product_skus(response)
         item['image_urls'] = self.product_images(response)
-
         item['requests'] = self.color_requests(response)
+
         return self.next_request_or_item(item)
 
     def color_requests(self, response):
         product_json = json.loads(response.css('::attr(data-variants-tree)').get())
-        return [Request(self.new_color_url(color, response), callback=self.parse_color)
+        return [Request(add_or_replace_parameter(response.url, 'color', color), callback=self.parse_color)
                 for color in product_json['_children'] if not color['selected']]
-
-    def new_color_url(self, color, response):
-        params = {'color': color}
-        url_parts = list(urllib.parse.urlparse(response.url))
-        query = dict(urllib.parse.parse_qsl(url_parts[4]))
-        query.update(params)
-        url_parts[4] = urllib.parse.urlencode(query)
-
-        return urllib.parse.urlunparse(url_parts)
 
     def parse_color(self, response):
         item = response.meta['item']
@@ -172,6 +85,7 @@ class _24sSpider(CrawlSpider):
             if 'Out of stock' in size:
                 common_sku['out_of_stock'] = True
                 size = clean(size.split('-')[0])
+
             common_sku['size'] = size
             skus[f'{color}_{size}'] = common_sku.copy()
 
@@ -210,4 +124,98 @@ class _24sSpider(CrawlSpider):
 
     def product_care(self, response):
         return clean(response.css('.grey ::text').getall()[-1].split('/'))
+
+
+class _24sCrawlSpider(CrawlSpider):
+    name = '24s'
+    allowed_domains = ['24s.com', 'dkpvob44gb-dsn.algolia.net']
+    start_urls = [
+        'https://www.24s.com/en-us/'
+    ]
+    parse_spider = _24sParseSpider()
+
+    pagination_url = 'https://dkpvob44gb-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia+for+vanilla' \
+                     '+JavaScript+3.32.1%3BJS+Helper+2.26.1&x-algolia-application-id=DKPVOB44GB&x-algolia-api-' \
+                     'key=9b23af4f250bb432947c20b7c82890a2'
+
+    headers = {
+        'accept': 'application/json',
+        'Origin': 'https://www.24s.com',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 '
+                      'Safari/537.36',
+        'content-type': 'application/x-www-form-urlencoded',
+    }
+
+    request_body_json = json.loads('{"requests":[{"indexName":"prod_products","params":"query=&tagFilters={}&page=0&'
+                                   'getRankingInfo=true&ruleContexts={}"}]}')
+
+    listings_css = ['.nav-link', '[class="device-drop-btn"]']
+    deny = ['brands', 'le-bon-marche', 'your-24s']
+
+    rules = (
+        Rule(LinkExtractor(restrict_css=listings_css, deny=deny), callback='parse_categories'),
+    )
+
+    def parse_categories(self, response):
+        payload = self.request_payload(response)
+        return Request(self.pagination_url, method='POST', body=payload,
+                       headers=self.headers, callback=self.parse_pagination)
+
+    def request_payload(self, response):
+        tag_filter = self.get_tag_filters(response)
+        rule_contexts = self.get_rule_contexts(response)
+        raw_params = self.request_body_json['requests'][0]['params']
+        params = raw_params.format(tag_filter, rule_contexts)
+        self.request_body_json['requests'][0]['params'] = params
+
+        return json.dumps(self.request_body_json)
+
+    def get_tag_filters(self, response):
+        raw_tag_filter = ["US"] + response.css('::attr(data-filter)').split(',')
+        tag_filter = str(raw_tag_filter).replace('\'', '"').replace(' ', '')
+        return urllib.parse.quote(tag_filter)
+
+    def get_rule_contexts(self, response):
+        raw_rule_context = response.css('::attr(data-filter)').get()
+
+        if ',' not in raw_rule_context:
+            raw_rule_context = f'{raw_rule_context},'
+
+        rule_context = raw_rule_context.replace('\'', '"').replace(' ', '').replace(',', '_MERCH_US')
+        return urllib.parse.quote(rule_context)
+
+    def parse_pagination(self, response):
+        self.parse_products(response)
+        total_pages = json.loads(response.text)['results'][0]['nbPages'] + 1
+
+        for page_number in range(1, total_pages):
+            body = self.request_body(response, page_number)
+            yield Request(self.pagination_url, method='POST', body=body, headers=self.headers,
+                          callback=self.parse_products, dont_filter=True)
+
+    def request_body(self, response, page_number):
+        body = json.loads(response.request.body)
+        raw_params = body['requests'][0]['params']
+        params = add_or_replace_parameter(raw_params, 'page', page_number)
+        body['requests'][0]['params'] = params
+        return json.dump(body)
+
+    def parse_products(self, response):
+        raw_products = json.loads(response.text)['results'][0]
+
+        for product in raw_products['hits']:
+            product_url = f'{product["title_en"]}-{product["brand"]}_{product["item_group_id"]}'
+            args = {
+                'defaultSku': product['sku'],
+                'color': product['color_brand']
+            }
+            product_url = add_or_replace_parameters(product_url, args)
+
+            yield response.follow(self.clean_url(product_url), callback=self.parse_spider.parse)
+
+    def clean_url(self, raw_str):
+        raw_str = (c for c in unicodedata.normalize('NFD', raw_str) if unicodedata.category(c) != 'Mn')
+        raw_str = ''.join(raw_str).lower()
+        return re.sub(' |\/', '-', raw_str.replace('"', ''))
+
 
