330ba148e4893c59ac3c686afc68905ef751ec5f
Uzair Amer <uzair.amer@arbisoft.com>
2019-09-02 15:11:34 +0500

    Completed first iteration

--- forumsport/forumsport_spider.py
+++ forumsport/forumsport_spider.py
@@ -4,8 +4,7 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Request, Rule
 from w3lib.url import add_or_replace_parameters
 
-from .base import BaseCrawlSpider, BaseParseSpider, clean, extract_text,\
-    Gender, soupify
+from .base import BaseCrawlSpider, BaseParseSpider, clean, Gender, soupify
 
 
 class Mixin:
@@ -20,46 +19,11 @@ class MixinES(Mixin):
     start_urls = ['https://www.forumsport.com/ropa-calzado/']
 
 
-class MixinIT(Mixin):
-    retailer = Mixin.retailer + '-it'
-    lang = 'it'
-    allowed_domains = ['eu.forumsport.com/it-it/']
-    start_urls = ['https://eu.forumsport.com/it-it/']
-
-
-class MixinFR(Mixin):
-    retailer = Mixin.retailer + '-fr'
-    lang = 'fr'
-    allowed_domains = ['eu.forumsport.com/fr-fr/']
-    start_urls = ['https://eu.forumsport.com/fr-fr/']
-
-
-class MixinDE(Mixin):
-    retailer = Mixin.retailer + '-de'
-    lang = 'de'
-    allowed_domains = ['eu.forumsport.com/de-de/']
-    start_urls = ['https://eu.forumsport.com/de-de/']
-
-
-class MixinPT(Mixin):
-    retailer = Mixin.retailer + '-pt'
-    lang = 'pt'
-    allowed_domains = ['eu.forumsport.com/pt-pt/']
-    start_urls = ['https://eu.forumsport.com/pt-pt/']
-
-
-class MixinUK(Mixin):
-    retailer = Mixin.retailer + '-uk'
-    lang = 'en'
-    allowed_domains = ['eu.forumsport.com']
-    start_urls = ['https://eu.forumsport.com/en-uk/menu']
-
-
 class ParseSpider(BaseParseSpider):
-    care_css = '.adv-feature-list h4::text, #description .features li::text'
-    description_css = '.short-description-container p::text'
-    brand_css = '.model-features-card::attr(data-brand)'
-    price_css = '.previous-price, .usizy-external::attr(data-price)'
+    description_css = '[itemprop="disambiguatingDescription"]::text'
+    care_css = '.adv-feature-list strong::text'
+    brand_css = '.model-brand-card meta[itemprop="brand"]::attr(content)'
+    price_css = '.previous-price, .price::attr(content)'
 
     def parse(self, response):
         garment = self.new_unique_garment(self.product_id(response))
@@ -69,17 +33,10 @@ class ParseSpider(BaseParseSpider):
 
         self.boilerplate_normal(garment, response)
         garment['gender'] = self.product_gender(response)
-        garment['image_urls'] = []
-        garment['skus'] = {}
-        garment['meta'] = {
-            'requests_queue': self.colour_requests(response)
-        }
+        garment['image_urls'] = self.image_urls(response)
+        garment['skus'] = self.skus(response)
 
-        return self.next_request_or_garment(garment)
-
-    def parse_colour_requests(self, response):
-        garment = response.meta['garment']
-        garment['skus'].update(self.skus(response))
+        return garment, self.colour_request(response)
 
     def product_id(self, response):
         return clean(response.css('.ref span::text'))[0]
@@ -101,21 +58,45 @@ class ParseSpider(BaseParseSpider):
         css = '.review.show-for-large-up::text, .product-name .description::text'
         return self.gender_lookup(soupify(clean(response.css(css)))) or Gender.ADULTS.value
 
-    def colour_requests(self, response):
-        css = '.color-section a::attr(href)'
-        urls = extract_text(response.css(css), post_process=response.urljoin)
+    def colour_request(self, response):
+        css = 'a[style="enlaces_hermanos"]::attr(href)'
+        return response.follow(url=clean(response.css(css))[0])
 
-        # TODO: Extract images
-        return [Request(url=url, callback=self.parse_colour_requests) for url in urls]
+    def image_urls(self, response):
+        urls = response.css('.gallery-image img::attr(src)').getall()
+        return [response.urljoin(url=url) for url in urls]
 
     def skus(self, response):
+        skus = {}
         common_sku = self.product_pricing_common(response)
-        common_sku['colour'] = clean(response.css('.color-section .active'))[0]
+        raw_colour = clean(response.css('.gallery-image img::attr(title)'))[0]
+        colour = self.detect_colour(raw_colour)
+
+        if colour:
+            common_sku['colour'] = colour
+
+        oos_css = '.size-selected span:contains("uds disponibles")'
+        size_css = '.model-features-card .conver-sizes-items:not(.active) .size-item'
+
+        for size_s in response.css(size_css) or ['One Size']:
+            sku = common_sku.copy()
+
+            if size_s == 'One Size':
+                sku['size'] = size_s
+                sku['out_of_stock'] = not bool(response.css(oos_css))
+            else:
+                sku['size'] = clean(size_s.css('::text'))[0]
+                sku['out_of_stock'] = bool(size_s.css('.off'))
+
+            skus[f'{self.product_id(response)}_{sku["size"]}'] = sku
+
+        return skus
 
 
 class CrawlSpider(BaseCrawlSpider):
-    listings_css = ['.no-children']
+    listings_css = ['.rowmenu li.sub-title~li']
     products_css = ['.product-preview']
+    pagination_url = 'https://www.forumsport.com/listado.php'
 
     rules = (
         Rule(LinkExtractor(restrict_css=products_css), callback='parse_item'),
@@ -126,48 +107,24 @@ class CrawlSpider(BaseCrawlSpider):
         meta = response.meta.copy()
         meta['trail'] = self.add_trail(response)
 
-        css_t = '.product-search-page::attr({})'
-        url = response.urljoin(clean(response.css(css_t.format('data-scrolling-url')))[0])
-        total_pages = int(clean(response.css(css_t.format('data-total-pages')))[0])
+        css_t = 'input[name="{}"]::attr(value)'
+        total_products = int(clean(response.css(css_t.format('total')))[0])
+        session_id = int(clean(response.css(css_t.format('seccion')))[0])
+        category = clean(response.css(css_t.format('nombre_seccion')))[0]
+        params = {
+            'seccion': session_id,
+            'nombre_seccion': category
+        }
 
-        for page_num in range(1, total_pages):
-            new_url = f'{url}/{page_num}-{page_num}'
-            new_url = add_or_replace_parameters(new_url, {'page': page_num})
-            yield Request(url=new_url, meta=meta)
+        for page_num in range(2, total_products // 24 + 2):
+            params['paginacion[pagina]'] = page_num
+            yield Request(url=add_or_replace_parameters(self.pagination_url, params), meta=meta)
 
 
 class ForumSportESParseSpider(MixinES, ParseSpider):
     name = MixinES.retailer + '-parse'
-    description_css = '[itemprop="disambiguatingDescription"]::text'
-    care_css = '.adv-feature-list strong::text'
-    brand_css = '.model-brand-card meta[itemprop="brand"]::attr(content)'
-    price_css = '.previous-price, .price::attr(content)'
-
-    def skus(self, response):
-        common_sku = self.product_pricing_common(response)
-        common_sku['colour'] = clean(response.css('.color-section .active'))[0]
-        skus = {}
-
-
-class ForumSportUKParseSpider(MixinUK, ParseSpider):
-    name = MixinUK.retailer + '-parse'
-
-
-class ForumSportFRParseSpider(MixinFR, ParseSpider):
-    name = MixinFR.retailer + '-parse'
-    listing_css = ['.rowmenu li.sub-title~li']
 
 
 class ForumSportESCrawlSpider(MixinES, CrawlSpider):
     name = MixinES.retailer + '-crawl'
     parse_spider = ForumSportESParseSpider()
-
-
-class ForumSportUKCrawlSpider(MixinUK, CrawlSpider):
-    name = MixinUK.retailer + '-crawl'
-    parse_spider = ForumSportUKParseSpider()
-
-
-class ForumSportFRCrawlSpider(MixinFR, CrawlSpider):
-    name = MixinFR.retailer + '-crawl'
-    parse_spider = ForumSportUKParseSpider()
