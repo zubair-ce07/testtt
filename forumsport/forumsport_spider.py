3ec34d5b0aa91f38e3b1080ab0a28a2378b59c5b
Uzair Amer <uzair.amer@arbisoft.com>
2019-09-03 14:40:03 +0500

    Added reviews support

--- forumsport/forumsport_spider.py
+++ forumsport/forumsport_spider.py
@@ -1,5 +1,3 @@
-import json
-
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Request, Rule
 from w3lib.url import add_or_replace_parameters
@@ -9,21 +7,24 @@ from .base import BaseCrawlSpider, BaseParseSpider, clean, Gender, soupify
 
 class Mixin:
     retailer = 'forumsport'
-    market = 'ES'
+    one_sizes = ['unica']
 
 
 class MixinES(Mixin):
     retailer = Mixin.retailer + '-es'
+    market = 'ES'
     lang = 'es'
     allowed_domains = ['forumsport.com']
     start_urls = ['https://www.forumsport.com/ropa-calzado/']
 
 
 class ParseSpider(BaseParseSpider):
+    one_size = 'unica'
+
     description_css = '[itemprop="disambiguatingDescription"]::text'
     care_css = '.adv-feature-list strong::text'
     brand_css = '.model-brand-card meta[itemprop="brand"]::attr(content)'
-    price_css = '.previous-price, .price::attr(content)'
+    price_css = '.price-tag-big .previous-price,.price-tag-big .price::attr(content)'
 
     def parse(self, response):
         garment = self.new_unique_garment(self.product_id(response))
@@ -32,11 +33,13 @@ class ParseSpider(BaseParseSpider):
             return
 
         self.boilerplate_normal(garment, response)
+
+        garment.update(self.product_reviews(response))
         garment['gender'] = self.product_gender(response)
         garment['image_urls'] = self.image_urls(response)
         garment['skus'] = self.skus(response)
 
-        return garment, self.colour_request(response)
+        return [garment] + self.colour_requests(response)
 
     def product_id(self, response):
         return clean(response.css('.ref span::text'))[0]
@@ -46,25 +49,33 @@ class ParseSpider(BaseParseSpider):
         return self.remove_brand_from_text(self.product_brand(response), raw_product_name)
 
     def product_category(self, response):
-        raw_categories = clean(response.css('script:contains("BreadcrumbList")::text'))
-
-        if not raw_categories:
-            return []
-
-        raw_categories = json.loads(raw_categories[0])['itemListElement']
-        return clean([c['item']['name'] for c in raw_categories[1:-1]])
+        trail = response.meta['trail']
+        categories = [t for t, _ in trail] + [clean(response.css('title::text'))[0].split('|')[1]]
+        return clean(categories)
 
     def product_gender(self, response):
         css = '.review.show-for-large-up::text, .product-name .description::text'
         return self.gender_lookup(soupify(clean(response.css(css)))) or Gender.ADULTS.value
 
-    def colour_request(self, response):
+    def product_reviews(self, response):
+        css_t = '.ratings-overview .rating{}'
+        comments = response.css('comment')
+
+        if not comments:
+            return {}
+
+        return {
+            'total_score': float(clean(response.css(css_t.format('::text')))[0]),
+            'review_score': float(clean(response.css(css_t.format(' span::text')), pattern_re=r'/')[0]),
+            'number_of_reviews': len(comments)
+        }
+
+    def colour_requests(self, response):
         css = 'a[style="enlaces_hermanos"]::attr(href)'
-        return response.follow(url=clean(response.css(css))[0])
+        return [response.follow(url=url) for url in clean(response.css(css))]
 
     def image_urls(self, response):
-        urls = response.css('.gallery-image img::attr(src)').getall()
-        return [response.urljoin(url=url) for url in urls]
+        return clean(response.css('.gallery-image img::attr(src)'))
 
     def skus(self, response):
         skus = {}
@@ -75,17 +86,18 @@ class ParseSpider(BaseParseSpider):
         if colour:
             common_sku['colour'] = colour
 
-        oos_css = '.size-selected span:contains("uds disponibles")'
-        size_css = '.model-features-card .conver-sizes-items:not(.active) .size-item'
+        oos_css = '.size-selected span:contains("disponible")'
+        size_css = '.conver-sizes-items.active .size-item, script:contains("UNICA")'
 
-        for size_s in response.css(size_css) or ['One Size']:
+        for size_s in response.css(size_css):
             sku = common_sku.copy()
+            size = size_s.re_first(r'"(.*?)"')
 
-            if size_s == 'One Size':
-                sku['size'] = size_s
+            if size and size.lower() in self.one_sizes:
+                sku['size'] = self.one_size
                 sku['out_of_stock'] = not bool(response.css(oos_css))
             else:
-                sku['size'] = clean(size_s.css('::text'))[0]
+                sku['size'] = clean(size_s.css('::text'))[0].lower()
                 sku['out_of_stock'] = bool(size_s.css('.off'))
 
             skus[f'{self.product_id(response)}_{sku["size"]}'] = sku
@@ -94,6 +106,8 @@ class ParseSpider(BaseParseSpider):
 
 
 class CrawlSpider(BaseCrawlSpider):
+    page_size = 24
+
     listings_css = ['.rowmenu li.sub-title~li']
     products_css = ['.product-preview']
     pagination_url = 'https://www.forumsport.com/listado.php'
@@ -109,16 +123,14 @@ class CrawlSpider(BaseCrawlSpider):
 
         css_t = 'input[name="{}"]::attr(value)'
         total_products = int(clean(response.css(css_t.format('total')))[0])
-        session_id = int(clean(response.css(css_t.format('seccion')))[0])
-        category = clean(response.css(css_t.format('nombre_seccion')))[0]
         params = {
-            'seccion': session_id,
-            'nombre_seccion': category
+            'seccion': int(clean(response.css(css_t.format('seccion')))[0]),
+            'nombre_seccion': clean(response.css(css_t.format('nombre_seccion')))[0]
         }
 
-        for page_num in range(2, total_products // 24 + 2):
+        for page_num in range(2, total_products // self.page_size + 2):
             params['paginacion[pagina]'] = page_num
-            yield Request(url=add_or_replace_parameters(self.pagination_url, params), meta=meta)
+            yield Request(url=add_or_replace_parameters(self.pagination_url, params), meta=meta.copy())
 
 
 class ForumSportESParseSpider(MixinES, ParseSpider):
