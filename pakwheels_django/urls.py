cb095703124828457f9c8e619913d030a0e996ee
hanzla-bit <53176670+hanzla-bit@users.noreply.github.com>
2019-09-04 11:43:46 +0500

    Made localhost as main page

--- pakwheels_django/urls.py
+++ pakwheels_django/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('usedcars/', include('usedcars.urls')),
+    path('', include('usedcars.urls')),
 ]
