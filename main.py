f12dc934553733872e62939beb680a434a61e3e5
Hassnain abbas <hassnain_abbas95@hotmail.com>
2019-09-07 16:44:46 +0500

    Refactor: variable x-->f

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
         index = 2  # skipping First 2 arg as they are file name and path
         while index < len(sys.argv):
             # get flag_indexes[] of the total indexes encounter for -a flag in the command line argument
-            flag_indexes = [i for i, x in enumerate(sys.argv) if x == '-a']
+            flag_indexes = [i for i, f in enumerate(sys.argv) if f == '-a']
             flag_iter = 0  # Variable to iterate over flag_indexes
             while flag_iter < len(flag_indexes):
                 year, month = Parser.date_tokenizer(sys.argv[flag_indexes[flag_iter] + 1])
@@ -25,7 +25,7 @@ def main():
                 print(ReportGenerator.calculate_monthly_report(Parser.read_file(path)))
                 print()
             # get flag_indexes[] of the total indexes encounter for -e flag in the command line argument
-            flag_indexes = [i for i, x in enumerate(sys.argv) if x == '-e']
+            flag_indexes = [i for i, f in enumerate(sys.argv) if f == '-e']
             flag_iter = 0
             while flag_iter < len(flag_indexes):
                 year = sys.argv[flag_indexes[flag_iter] + 1]
@@ -41,7 +41,7 @@ def main():
                 index += 2
                 flag_iter += 1
             # get flag_indexes[] of the total indexes encounter for -c flag in the command line argument
-            flag_indexes = [i for i, x in enumerate(sys.argv) if x == '-c']
+            flag_indexes = [i for i, f in enumerate(sys.argv) if f == '-c']
             flag_iter = 0
             while flag_iter < len(flag_indexes):
                 year, month = Parser.date_tokenizer(sys.argv[flag_indexes[flag_iter] + 1])
