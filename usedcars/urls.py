87ce121d630e724ecb3887055bcf708a0043b3ba
hanzla-bit <hanzla.khalid@arbisoft.com>
2019-09-11 21:39:46 +0500

    More pages with more options

--- usedcars/urls.py
+++ usedcars/urls.py
@@ -4,7 +4,16 @@ from . import views
 
 app_name = 'usedcars'
 urlpatterns = [
-    # /usedcars/
-    path('', views.IndexView.as_view(), name='index')
+    # 127.0.0.1:8000/
+    path('', views.index_view, name='index'),
+
+    # /table/
+    path('table', views.table_page, name='table'),
+
+    # /[Make of company here]/
+    path('<str:company_name>', views.detail_test, name='temp_view'),
+
+    # /singleCar/23/
+    path('singleCar/<int:car_id>', views.single_car, name='single_car_view')
 
 ]
--- usedcars/views.py
+++ usedcars/views.py
@@ -1,10 +1,32 @@
 from .models import UsedCars
 from django.views import generic
+from django.http import Http404
+from django.shortcuts import render
 
 
-class IndexView(generic.ListView):
-    template_name = 'usedcars/index.html'
-    context_object_name = 'wheels'
+def index_view(request):
+    return render(request, 'usedcars/index.html')
 
-    def get_queryset(self):
-        return UsedCars.objects.all()
+
+def table_page(request):
+    try:
+        objs = UsedCars.objects.all()
+    except UsedCars.DoesNotExist:
+        raise Http404("Table Page does not EXIST!")
+    return render(request, 'usedcars/tables.html', {'wheels': objs})
+
+
+def detail_test(request, company_name):
+    try:
+        objs = UsedCars.objects.all()
+    except UsedCars.DoesNotExist:
+        raise Http404("Show Car Page does not exist")
+    return render(request, 'usedcars/showCars.html', {'usedCars': objs, 'company_name': company_name})
+
+
+def single_car(request, car_id):
+    try:
+        obj = UsedCars.objects.get(id=car_id)
+    except UsedCars.DoesNotExist:
+        raise Http404("Single Car Page does not exist")
+    return render(request, 'usedcars/singleCar.html', {'usedCar': obj})
