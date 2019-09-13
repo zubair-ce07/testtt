6e2c8f231094d1fea103627d01fe917cc5ca3ca9
Ali Mahmood <ali.mahmood@arbisoft.com>
2019-09-13 21:09:08 +0500

    added command line argument

--- weatherman/weatherman.py
+++ weatherman/weatherman.py
@@ -5,6 +5,27 @@ import sys
 from datetime import datetime
 
 
+class Validator:
+    @staticmethod
+    def valid_date(date):
+        try:
+            date = date.split('/')
+            date = '{}-{}'.format(date[0], date[1])
+            return date
+        except ValueError:
+            msg = "Not a valid date: '{0}'.".format(date)
+            raise argparse.ArgumentTypeError(msg)
+
+    @staticmethod
+    def valid_year(year):
+        try:
+            datetime.strptime(year, "%Y")
+            return year
+        except ValueError:
+            msg = "Not a valid year: '{0}'.".format(year)
+            raise argparse.ArgumentTypeError(msg)
+
+
 class ReportReader:
     def __init__(self):
         os.chdir('/home/ali/PycharmProjects/hello-world/weatherdata')
@@ -67,16 +88,22 @@ class ReportGenerator:
     def __init__(self):
         self.report_reader = ReportReader()
 
-    def generate_monthly_report(self):
-        records = self.report_reader.read_monthly_report('2005-6')
+    def generate_reports(self, arguments):
+        if arguments.e:
+            self.generate_yearly_report(arguments.e)
+        if arguments.a:
+            self.generate_monthly_report(arguments.a)
+
+    def generate_monthly_report(self, month):
+        records = self.report_reader.read_monthly_report(month)
         length = len(records)
         avg_max_temp = round(sum(daily_record['Max TemperatureC'] for daily_record in records) / length)
         avg_min_temp = round(sum(daily_record['Min TemperatureC'] for daily_record in records) / length)
         avg_mean_humidity = round(sum(daily_record[' Mean Humidity'] for daily_record in records) / length)
         self.display_montly_report(avg_max_temp, avg_min_temp, avg_mean_humidity)
 
-    def generate_yearly_report(self):
-        records = self.report_reader.read_yearly_report('2005')
+    def generate_yearly_report(self, year):
+        records = self.report_reader.read_yearly_report(year)
         max_temp = max(records, key=lambda x: x['Max TemperatureC'])
         min_temp = min(records, key=lambda x: x['Min TemperatureC'])
         max_humidity = max(records, key=lambda x: x['Max Humidity'])
@@ -104,9 +131,22 @@ class ReportGenerator:
         print(max_temp, min_temp, max_humidity, sep="\n")
 
 
+def get_arguments_list():
+    parser = argparse.ArgumentParser()
+    if len(sys.argv) > 1:
+        parser.add_argument('-e', type=Validator.valid_year)
+        parser.add_argument('-a', type=Validator.valid_date)
+        args = parser.parse_args()
+    else:
+        parser.error('No arguments provided.')
+
+    return args
+
+
 def main():
-    report2 = ReportGenerator()
-    report2.generate_monthly_report()
+    arguments = get_arguments_list()
+    report = ReportGenerator()
+    report.generate_reports(arguments)
 
 
 if __name__ == "__main__":
