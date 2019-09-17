c7a3f2096784183a28437e5e1422869689ac5ee1
arslan-shafiq <me.arslanshafiq@gmail.com>
2019-09-17 12:04:16 +0500

    - used base methods - removed unnecessary code - more pythonic code

--- DVF/dvf_spider.py
+++ DVF/dvf_spider.py
@@ -5,7 +5,7 @@ from scrapy.link import Link
 from scrapy.spiders import Rule, Request
 
 from skuscraper.parsers.genders import Gender
-from .base import BaseParseSpider, BaseCrawlSpider, clean
+from .base import BaseParseSpider, BaseCrawlSpider, clean, soupify
 
 class Mixin:
     retailer = 'dvf'
@@ -37,29 +37,20 @@ class DvfParseSpider(BaseParseSpider):
         garment['skus'] = {}
 
         garment['meta'] = {'requests_queue': self.colors_requests(response)}
-        return self.next_request_or_garment(garment)
+        response.meta['garment'] = garment
+        return self.parse_color(response)
 
     def parse_color(self, response):
         garment = response.meta['garment']
         garment['image_urls'] += self.image_urls(response)
-        garment['meta']['requests_queue'] += self.sizes_requests(response)
-
-        return self.next_request_or_garment(garment)
-
-    def parse_size(self, response):
-        garment = response.meta['garment']
         garment['skus'].update(self.skus(response))
 
         return self.next_request_or_garment(garment)
 
     def colors_requests(self, response):
-        links = [response.url if 'color=&' in link else link
-                 for link in clean(response.css('.selectableSwatch::attr(data-swatch-href)'))]
-        return [Request(link, callback=self.parse_color, dont_filter=True) for link in links]
-
-    def sizes_requests(self, response):
-        links = clean(response.css('.pdp-size-qty-container a::attr(href)'))
-        return [Request(link, callback=self.parse_size, dont_filter=True) for link in links]
+        links = [link for link in clean(response.css('.selectableSwatch::attr(data-swatch-href)'))
+                 if not 'color=&' in link]
+        return [Request(link, callback=self.parse_color) for link in links]
 
     def raw_product(self, response):
         return json.loads(clean(response.css('[id="product-gtm-data"]::attr(data-gtmdata)'))[0])
@@ -80,37 +71,35 @@ class DvfParseSpider(BaseParseSpider):
         return [self.raw_product(response)['category']]
 
     def product_gender(self, response):
-        raw_data = clean(response.css('[id="secret_sauce_product_json"]::text'))
-        soup = raw_data[0] if raw_data else ''
+        soup = soupify(clean(response.css('[id="secret_sauce_product_json"]::text')))
         return self.gender_lookup(soup) or Gender.ADULTS.value
-
-    def product_previous_prices(self, response):
-        return clean(response.css('.pdp-original-price::text'))
-
-    def is_available(self, response):
-        return not clean(response.css('.soldout-title'))
 
     def skus(self, response):
         raw_product = self.raw_product(response)
+        skus = {}
 
-        sku = self.product_pricing_common(response)
-        sku['size'] = raw_product['dimension13'] or self.one_size
-        sku['colour'] = raw_product['dimension19']
-        if not self.is_available(response):
-            sku['out_of_stock'] = True
+        sizes = clean(response.css('.pdp-size-qty-container .sizeText::text'))
+        available_sizes = clean(response.css('.pdp-size-qty-container .available .sizeText::text'))
+        for size in sizes:
+            sku = self.product_pricing_common(response)
+            sku['size'] = size
+            sku['colour'] = raw_product['dimension19']
+            if not size in available_sizes:
+                sku['out_of_stock'] = True
+            skus[f"{sku['colour']}_{sku['size']}"] = sku
 
-        return {f"{sku['colour']}_{sku['size']}": sku}
+        return skus
 
 
 class DvfUSParseSpider(MixinUS, DvfParseSpider):
     name = MixinUS.retailer + '-parse'
 
 
-class DvfLinkExtractor():
+class PaginationLE():
 
     def extract_links(self, response):
         next_page = clean(response.css('.infinite-scroll-placeholder::attr(data-grid-url)'))
-        return [Link(next_page[0])] if next_page else []
+        return next_page and [Link(next_page[0])]
 
 
 class DvfCrawlSpider(BaseCrawlSpider):
@@ -119,7 +108,7 @@ class DvfCrawlSpider(BaseCrawlSpider):
 
     rules = (
         Rule(LinkExtractor(restrict_css=listings_css), callback='parse'),
-        Rule(DvfLinkExtractor(), callback='parse'),
+        Rule(PaginationLE(), callback='parse'),
         Rule(LinkExtractor(restrict_css=products_css), callback='parse_item')
     )
 
