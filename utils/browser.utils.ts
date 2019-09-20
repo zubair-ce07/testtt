0d0d84c47786973e01f9327f420ae6a81a726c12
Awais Ali <awaisraad@outlook.com>
2019-09-20 14:45:43 +0500

    Added function to wait for element to be interactive

--- utils/browser.utils.ts
+++ utils/browser.utils.ts
@@ -1,5 +1,17 @@
-import { browser, ElementFinder } from "protractor";
+import { browser, ElementFinder, ExpectedConditions as EC } from "protractor";
 
-export async function scrollElementIntoView(elm: ElementFinder): Promise<void> {
-  return browser.executeScript((elm: HTMLElement) => elm.scrollIntoView(false), elm);
+export async function scrollElementIntoView(element: ElementFinder): Promise<void> {
+  return browser.executeScript((elm: HTMLElement) => elm.scrollIntoView(false), element);
+}
+
+export async function waitForElementToBeInteractive(element: ElementFinder, timeout?: number, message?: string) {
+  await browser.wait(
+    EC.and(
+      EC.presenceOf(element),
+      EC.visibilityOf(element),
+      EC.elementToBeClickable(element)
+    ),
+    timeout,
+    message
+  );
 }
