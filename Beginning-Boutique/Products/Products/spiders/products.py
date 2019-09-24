8758ae942798d302fb3fcfc8da0969248eac72e4
Ali Mahmood <ali.mahmood@arbisoft.com>
2019-09-24 20:44:41 +0500

    implemented crawler scraping all the pages includes pagination

--- Beginning-Boutique/Products/Products/spiders/products.py
+++ Beginning-Boutique/Products/Products/spiders/products.py
@@ -2,15 +2,12 @@ import scrapy
 from ..items import ProductsItem
 
 
-class QuotesSpider(scrapy.Spider):
+class BeginningBoutique(scrapy.Spider):
     name = "product_store"
 
     def start_requests(self):
-        urls = [
-            'https://www.beginningboutique.com.au/products/grande-long-sleeve-party-dress-white'
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+        start_url = 'https://www.beginningboutique.com.au/'
+        yield scrapy.Request(url=start_url, callback=self.parse)
 
     def store_product(self, brand, name, price, care, description, image_urls, gender,
                       currency, market, url_original, retailer):
@@ -33,7 +30,7 @@ class QuotesSpider(scrapy.Spider):
         currency = 'AUD'
         url_original = response.url
         domain = url_original.split('/')[2].split('.')
-        market = domain[len(domain)-1]
+        market = domain[len(domain) - 1]
         retailer = '{}-{}'.format(domain[1], market)
         brand = response.css('.product-heading__vendor a::text')[0].extract()
         name = response.css('.product-heading__title::text')[0].extract()
@@ -46,8 +43,25 @@ class QuotesSpider(scrapy.Spider):
         return self.store_product(brand, name, price, care, description,
                                   image_urls, gender, currency, market, url_original, retailer)
 
-    def parse(self, response):
+    def parse_product(self, response):
         product_items = self.extract_product(response)
         yield {
             'items': product_items
         }
+
+    def parse_category(self, response):
+        category_urls = response.css('a.product-card-image-wrapper::attr(href)').getall()
+        for url in category_urls:
+            yield response.follow(url=url, callback=self.parse_product, dont_filter=True)
+
+    def parse_category_page(self, response):
+        category_pagination_urls = response.css('li.pagination__item a::attr(href)').getall()
+        if len(category_pagination_urls) > 0:
+            for pagination_url in category_pagination_urls:
+                yield response.follow(url=pagination_url, callback=self.parse_category, dont_filter=True)
+        yield response.follow(url=response.url, callback=self.parse_category, dont_filter=True)
+
+    def parse(self, response):
+        products_urls = response.css('.site-nav--has-dropdown a.site-nav__link::attr(href)').getall()
+        for url in products_urls:
+            yield response.follow(url, callback=self.parse_category_page, dont_filter=True)
