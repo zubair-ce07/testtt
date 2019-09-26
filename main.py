f9299d93b0d866d35a07bd8ab617f65962bbce42
Aun Raza <ali@A002-00065.arbisoft.com>
2019-09-26 17:02:35 +0500

    Iteration-26/9

--- main.py
+++ main.py
@@ -2,62 +2,57 @@ import weatherman as weatherman
 import argparse
 import datetime
 
-class ArgumentsValidation:
-    def get_arguments(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument("path", type=str)
-        parser.add_argument("-a",required=False, help="Format should be like yyyy/m")
-        parser.add_argument("-c",required=False, help="Format should be like yyyy/m")
-        parser.add_argument("-e",required=False, help="Format should be like yyyy")
-        args = parser.parse_args()
-        if not (args.a or args.c or args.e):
-            parser.error('No arguments provided.')
-        return args
+def validate_year(argument_string): 
+    if len(argument_string) == 4 and argument_string.isdigit(): 
+        return argument_string
+    raise ValueError('Not a valid year format')
 
-    def generate_reports(self,args):
-        report = weatherman.WeatherReport()
-        file_info = weatherman.FileData()
-        if args.a:
-        
-            if datetime.datetime.strptime(args.a, '%Y/%m'):
-                file_names = file_info.get_file_name("a", args.a, args.path)
-                if file_names:
-                    file_data = file_info.reading_file(file_names)
-                    report.display_monthly_report(file_data)
-                else:
-                    print("File may not be available against -a argument!")
-            else:
-                print('Not a valid date fomat')
+def validate_month(argument_string): 
+    date_arg = argument_string.split('/')
+    year_arg = date_arg[0] 
+    month_arg = date_arg[1]
+    if len(year_arg) == 4 and len(month_arg) > 0 and year_arg.isdigit() and month_arg.isdigit(): 
+        return argument_string
+    raise ValueError('Not a valid year or month format')
 
-        if args.c:
-            if datetime.datetime.strptime(args.c, '%Y/%m'):
-                file_names = file_info.get_file_name("c", args.c, args.path)
-                if file_names:
-                    file_data = file_info.reading_file(file_names)
-                    report.display_month_chart_report(file_data)
-                    report.display_month_bar_chart(file_data)
-                else:
-                    print("File may not be available against -c argument!")
-            else:
-                print('Not a valid date format')
+def get_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=str)
+    parser.add_argument("-a", help="Format should be like yyyy/m", type=validate_month)
+    parser.add_argument("-c", help="Format should be like yyyy/m", type=validate_month)
+    parser.add_argument("-e", help="Format should be like yyyy", type=validate_year)
+    args = parser.parse_args()
+    if not (args.a or args.c or args.e):
+        parser.error('No arguments provided!')
+    return args
 
-        if args.e:
-            if datetime.datetime.strptime(args.e, '%Y'):
-                file_names = file_info.get_file_name("e", args.e, args.path)
-                if file_names:
-                    file_data = file_info.reading_file(file_names)
-                    report.display_yearly_report(file_data)
-                else:
-                    print("File may not be available against -e argument!")
-            else:
-                print('Not a valid year format')
-
-
-def main():
-    arguments = ArgumentsValidation()
-    args = arguments.get_arguments()
-    arguments.generate_reports(args)
+def generate_reports(args):
+    report = weatherman.WeatherAnalyze()
+    if args.a:
+        file_names = report.get_file_name("a", args.a, args.path)
+        if file_names:
+            file_data = report.reading_file(file_names)
+            report.display_monthly_report(file_data)
+        else:
+            print("File may not be available against -a argument!")
+    if args.c:
+        file_names = report.get_file_name("c", args.c, args.path)
+        if file_names:
+            file_data = report.reading_file(file_names)
+            report.display_month_chart_report(file_data)
+            report.display_month_bar_chart(file_data)
+        else:
+            print("File may not be available against -c argument!")
+    if args.e:
+        file_names = report.get_file_name("e", args.e, args.path)
+        if file_names:
+            file_data = report.reading_file(file_names)
+            report.display_yearly_report(file_data)
+        else:
+            print("File may not be available against -e argument!")
     
-
+def main():
+    arguments = get_arguments()
+    generate_reports(arguments)
 if __name__ == "__main__":
     main()
--- weatherman.py
+++ weatherman.py
@@ -1,20 +1,15 @@
-import os
 import csv
 import glob
 from datetime import datetime
 
-
-class WeatherAnalysis:
+class WeatherData:
     
     def __init__(self, weather_reading):
         if weather_reading:
-            if weather_reading['PKT']:
-                self.full_date = weather_reading['PKT'] 
-                self.date_split = weather_reading['PKT'].split('-')
-                self.day = int(self.date_split[2])
-            if weather_reading['Max TemperatureC']:
+            if 'PKT' in weather_reading:
+                self.full_date = datetime.strptime(weather_reading['PKT'], '%Y-%m-%d').date() 
+            if 'Max TemperatureC' in weather_reading:
                 self.max_temp = int(weather_reading['Max TemperatureC'])
-            
             if weather_reading['Min TemperatureC']:
                 self.min_temp = int(weather_reading['Min TemperatureC'])
             if weather_reading[' Mean Humidity']:
@@ -22,32 +17,26 @@ class WeatherAnalysis:
             if weather_reading['Max Humidity']:
                 self.max_humidity = int(weather_reading['Max Humidity'])
 
-        
-class WeatherReport:
-    def __init__(self):
-        print('')
-
+class WeatherAnalyze:
+    
     def display_month_bar_chart(self, file_data):
-         
         for data_row in file_data:
             max_value = data_row.max_temp
             min_value = data_row.min_temp
-            date = data_row.day
-            print(date, end="")
-            for value in range(max_value):
-                print("\033[1;31m+\033[1;m", end="")
-            
+            date = data_row.full_date
+            print(date.day, end="")
             if min_value < 0:
-                for value in range(abs(min_value)):
-                    print("\033[1;34m-\033[1;m", end="")
-                    
-            for value in range(min_value):
-                print("\033[1;34m+\033[1;m", end="")
-            print(max_value, end="")
-            print("C- ", end="")
+                for lowest_value in range(abs(min_value)):
+                    print("\033[1;34m-\033[1;m", end = "")
+            for lowest_value in range(min_value):
+                print("\033[1;34m+\033[1;m", end = "")   
+            for highest_value in range(max_value):
+                print("\033[1;31m+\033[1;m", end = "")
             print(min_value, end="")
-            print("C")
-
+            print("C- ", end="")
+            print(max_value, end="")
+            print("C")         
+    
     def display_monthly_report(self, file_data):
         high_temp = self.get_max_temp(file_data)
         print("Highest Average : {}C".format(high_temp))
@@ -55,113 +44,80 @@ class WeatherReport:
         low_temp = self.get_min_temp(file_data)
         print("Lowest Average : {}C".format(low_temp))
 
-        mean_humidity = int(self.get_mean_humidity(file_data))
+        mean_humidity = int(self.get_mean_average_humidity(file_data))
         print("Average Mean Humidity : {}% ".format(mean_humidity))
 
     def display_month_chart_report(self, file_data):
         self.file_data = file_data
         for file_row in file_data:
-            
-                max_value = file_row.max_temp
-                min_value = file_row.min_temp
-                get_day = file_row.day
-                print(get_day, end="")
+                max_value = int(file_row.max_temp)
+                min_value = int(file_row.min_temp)
+                get_day = file_row.full_date
+                print(get_day.day, end="")
                 for max_temp_count in range(max_value):
-                    print("\033[1;31m+\033[1;m", end="")
+                    print("\033[1;31m+\033[1;m", end = "")
                 print(" ", max_value, "C")
-                get_day = file_row.day
-                print(get_day, end="")
+                get_day = file_row.full_date
+                print(get_day.day, end="")
                 if min_value < 0:
                     for min_temp_count in range(abs(min_value)):
-                        print("\033[1;34m-\033[1;m", end="")
+                        print("\033[1;34m-\033[1;m", end = "")
                     print(" {}C".format(min_value))
                 else:
                     for min_temp_count in range(min_value):
-                        print("\033[1;34m+\033[1;m", end="")
+                        print("\033[1;34m+\033[1;m", end = "")
                     print(" {}C".format(min_value))
-        
+    
     def display_yearly_report(self, file_data):
-        high_temp = self.get_max_average(file_data)
-        date = self.get_required_date(file_data,file_data.high_temp,reverse_flag=True)
+        high_temp = self.get_highest_average_temp(file_data)
+        date = self.get_required_date(file_data,high_temp,reverse_flag=True)
         print("Highest: {}C on {} {}".format(high_temp,date.strftime("%B"),date.day))
         
-        date = self.get_required_date(file_data,file_data.min_temp,reverse_flag=False)
-        low_temp = self.get_min_average(file_data)
+        low_temp = self.get_lowest_average_temp(file_data)
+        date = self.get_required_date(file_data,low_temp,reverse_flag=False)
         print("Lowest: {}C on {} {}".format(low_temp,date.strftime("%B"),date.day))
 
-        date = self.get_required_date(file_data,file_data.average_humidity,reverse_flag=True)
-        mean_humidity = self.get_mean_humidity(file_data)
-        print("Humidity: {} on {} {}%".format(mean_humidity,date.strftime("%B"),date.day))
+        mean_humidity = self.get_mean_average_humidity(file_data)
+        date = self.get_required_date(file_data,mean_humidity,reverse_flag=True)
+        print("Humidity: {}% on {} {}".format(mean_humidity,date.strftime("%B"),date.day))
 
     def get_max_temp(self, file_data):
-        max_list = []
-        for file_rows in file_data:
-            if file_rows.max_temp !='' or not file_rows:
-                max_list.append(file_rows.max_temp)
-            
+        max_list = [file_rows.max_temp for file_rows in file_data if file_rows]
         return max(max_list)
 
     def get_min_temp(self, file_data):
-        min_list = []
-        for file_rows in file_data:
-            min_list.append(file_rows.min_temp)
-            
+        min_list = [file_rows.min_temp for file_rows in file_data if file_rows]
         return min(min_list)
 
-    def get_mean_humidity(self, file_data):
-        mean_list = []
-        for file_rows in file_data:
-            mean_list.append(file_rows.max_temp)
-            
-        return (sum(mean_list)/len(mean_list))
+    def get_mean_average_humidity(self, file_data):
+        humidity_mean_list = [file_rows.mean_humidity for file_rows in file_data if file_rows]
+        return len(humidity_mean_list)
     
-    def get_max_average(self, file_data):
-        mean_list_max = []
-        
-        for file_rows in file_data:
-            mean_list_max.append(file_rows.max_temp)
-            
-        return (sum(mean_list_max)/len(mean_list_max))
+    def get_highest_average_temp(self, file_data):
+        mean_list_max = [file_rows.max_temp for file_rows in file_data if file_rows]
+        return round(sum(mean_list_max)/len(mean_list_max))
 
-    def get_min_average(self, file_data):
-        mean_list_min = []
-        for file_rows in file_data:
-            mean_list_min.append(file_rows.min_temp)
-            
-        return (sum(mean_list_min)/len(mean_list_min))    
+    def get_lowest_average_temp(self, file_data):
+        mean_list_min = [file_rows.min_temp for file_rows in file_data if file_rows]    
+        return round(sum(mean_list_min)/len(mean_list_min))    
 
-    def get_required_date(self, file_data,required_value,reverse_flag):
-        file_data = [file_row.full_date for file_row in file_data if required_value ]
+    def get_required_date(self, file_data, required_value, reverse_flag):
+        file_data = [file_row.full_date for file_row in file_data if required_value]
         file_data.sort(key=lambda x: int(required_value), reverse=reverse_flag)
         return file_data[0]
 
-class FileData:
-    def reading_file(self,file_names):
-        weather_readings = []
+    def reading_file(self, file_names):
         for file_name in file_names:
             with open(file_name, 'r') as csvfile:
                 weather_file_readings = csv.DictReader(csvfile)
-                
-                for row in weather_file_readings:
-                    weather_readings.append(WeatherAnalysis(row)) 
-                   
-        return weather_readings            
-        
+                weather_readings = [WeatherData(file_row) for file_row in weather_file_readings]
+        return weather_readings           
 
-
-    def get_file_name(self,arguments, file_name, file_path):
+    def get_file_name(self, arguments, file_name, file_path):
         pattern = "*{}_{}*.txt"
-
         if arguments == "a" or arguments == "c":
             file_month = datetime.strptime(file_name, "%Y/%m").strftime("%b")
             pattern = pattern.format(file_name.split("/")[0], file_month)
         elif arguments == "e":
             pattern = pattern.format(file_name, "")
-
         return glob.glob(file_path + pattern)
-
-
-        
-
-
-
