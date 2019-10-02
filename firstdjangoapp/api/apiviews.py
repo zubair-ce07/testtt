c16951ada87e26518a0e01a170da794348ac2fd5
Asad Ali <asad.ali@arbisoft.com>
2019-10-02 10:08:02 +0500

    prefetch related, added customized key in cache for filtered products

--- firstdjangoapp/api/apiviews.py
+++ firstdjangoapp/api/apiviews.py
@@ -1,9 +1,8 @@
-from django.db.models import Q
 from rest_framework import generics
 
 from .backends import SimpleFilterBackend
 from .permissions import AllowAnyOrAdmin, IsLoggedInUserOrAdmin, IsAdmin, ReadOnly
-from .redis_cache import cached_product, cached_filter_products, cached_user, cached_users_queryset
+from .redis_cache import cached_product, cached_filtered_products, cached_user, cached_users_queryset
 from .serializers import ProductSerializer, UserSerializer
 
 
@@ -13,33 +12,7 @@ class ProductList(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
 
     def get_queryset(self):
-        query_params = self.request.GET
-        brand = query_params.get('Brand', None)
-        size = query_params.get('Size', None)
-        colour = query_params.get('Colour', None)
-        category = query_params.get('Category', None)
-        name = query_params.get('Name', None)
-        minimum_price = query_params.get('Minimum Price', None)
-        maximum_price = query_params.get('Maximum Price', None)
-        out_of_stock = query_params.get('Out of Stock', None)
-        q = Q()
-        if out_of_stock == 'false' or out_of_stock == 'true':
-            out_of_stock = False if out_of_stock == 'false' else True
-        if out_of_stock is not None:
-            q = q & Q(out_of_stock=out_of_stock)
-        if brand:
-            q = q & Q(brand__iexact=brand)
-        if size:
-            q = q & Q(skus__size__iexact=size)
-        if colour:
-            q = q & Q(skus__colour__iexact=colour)
-        if category:
-            q = q & Q(categories__category__iexact=category)
-        if name:
-            q = q & Q(name__contains=name)
-        if maximum_price and minimum_price:
-            q = q & Q(skus__price__range=(int(minimum_price), int(maximum_price)))
-        return cached_filter_products(q)
+        return cached_filtered_products(self.request)
 
 
 class ProductDetail(generics.RetrieveAPIView):
--- firstdjangoapp/api/redis_cache.py
+++ firstdjangoapp/api/redis_cache.py
@@ -1,16 +1,76 @@
 from django.contrib.auth.models import User
 from django.core.cache import cache
+from django.db.models import Q
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 
 from shopcity.models import Product
 
 
-def cached_filter_products(q):
-    query_set = cache.get(f'products_{q}')
+def _params(request):
+    query_params = request.GET
+    filtered_params = {
+        'brand': query_params.get('Brand', None),
+        'size': query_params.get('Size', None),
+        'colour': query_params.get('Colour', None),
+        'category': query_params.get('Category', None),
+        'name': query_params.get('Name', None),
+        'minimum_price': query_params.get('Minimum Price', None),
+        'maximum_price': query_params.get('Maximum Price', None),
+    }
+    out_of_stock = query_params.get('Out of Stock', None)
+    if out_of_stock == 'false' or out_of_stock == 'true':
+        out_of_stock = False if out_of_stock == 'false' else True
+    filtered_params['out_of_stock'] = out_of_stock
+    return filtered_params
+
+
+def _q_query(params):
+    q = Q()
+    if params['out_of_stock'] is not None:
+        q = q & Q(out_of_stock=params['out_of_stock'])
+    if params["brand"]:
+        q = q & Q(brand__iexact=params["brand"])
+    if params['size']:
+        q = q & Q(skus__size__iexact=params['size'])
+    if params['colour']:
+        q = q & Q(skus__colour__iexact=params['colour'])
+    if params['category']:
+        q = q & Q(categories__category__iexact=params['category'])
+    if params['name']:
+        q = q & Q(name__contains=params['name'])
+    if params['maximum_price'] and params['minimum_price']:
+        q = q & Q(skus__price__range=(int(params['minimum_price']), int(params['maximum_price'])))
+    return q
+
+
+def _cache_key(params):
+    key = ''
+    if params['out_of_stock'] is not None:
+        key += f"_out_of_stock_{str(params['out_of_stock'])}"
+    if params["brand"]:
+        key += f"_brand_{params['brand'].lower()}"
+    if params['size']:
+        key += f"_size_{params['size'].lower()}"
+    if params['colour']:
+        key += f"_colour_{params['colour'].lower()}"
+    if params['category']:
+        key += f"_category_{params['category'].lower()}"
+    if params['name']:
+        key += f"_name_{params['name'].lower()}"
+    if params['maximum_price'] and params['minimum_price']:
+        key += f"_min_price_{str(params['minimum_price'])}_max_price_{str(params['maximum_price'])}"
+    return key
+
+
+def cached_filtered_products(request):
+    params = _params(request)
+    cache_key = _cache_key(params)
+    query_set = cache.get(f'products_{cache_key}')
     if not query_set:
-        query_set = Product.objects.filter(q).distinct()
-        cache.set(f'products_{q}', query_set)
+        q = _q_query(params)
+        query_set = Product.objects.prefetch_related('skus').filter(q).distinct()
+        cache.set(f'products_{cache_key}', query_set)
     return query_set
 
 
@@ -25,7 +85,7 @@ def cached_users_queryset():
 def cached_product(product_id):
     product = cache.get(f'product_{product_id}')
     if not product:
-        product = Product.objects.filter(retailer_sku=product_id)
+        product = Product.objects.prefetch_related('skus').filter(retailer_sku=product_id)
         cache.set(f'product_{product[0].retailer_sku}', product)
     return product
 
@@ -39,7 +99,7 @@ def cached_user(user_id):
 
 
 def clear_products_cache():
-    cache.delete('products')
+    cache.delete('products_*')
 
 
 @receiver(post_delete, sender=Product)
@@ -54,7 +114,7 @@ def product_post_save_handler(sender, **kwargs):
 
 
 def clear_users_cache():
-    cache.delete('users')
+    cache.delete('users*')
 
 
 @receiver(post_delete, sender=User)
