ea3d6dc60f0e151fe91972d2affa15a4a2a784f9
roshaanrasool <roshaan.rasool@arbisoft.com>
2019-10-04 10:40:25 +0500

    Refactor:Alignment errrors in code pylint

--- tausendkind/tausendkind/spiders/product_details.py
+++ tausendkind/tausendkind/spiders/product_details.py
@@ -34,11 +34,8 @@ class ProductDetailsSpider(scrapy.Spider):
         This function returns the header bar
         urls and yields a scrapy request for each.
         """
-        for header_url in response.css(
-                "ul.flex-items-lg-between > li > a::attr(href)"
-        ).extract():
-            yield scrapy.Request(url=header_url,
-                                 callback=self.parse_urls)
+        for header_url in response.css("ul.flex-items-lg-between > li > a::attr(href)").extract():
+            yield scrapy.Request(url=header_url, callback=self.parse_urls)
 
     def parse_urls(self, response):
         """
@@ -53,8 +50,7 @@ class ProductDetailsSpider(scrapy.Spider):
             if "concept-store" not in response.url:
                 sub_urls.append(response.url)
             for sub_url in sub_urls:
-                yield scrapy.Request(url=sub_url,
-                                     callback=self.parse_product_urls)
+                yield scrapy.Request(url=sub_url, callback=self.parse_product_urls)
 
     def parse_product_urls(self, response):
         """
@@ -66,13 +62,10 @@ class ProductDetailsSpider(scrapy.Spider):
         """
         for url_key in re.findall(
                 r'"url_key":\"(.*?)\"',
-                response.css(
-                    "#main > div.row.rel.old-row > script:nth-child(6)::text"
-                    ).get()):
+                response.css("#main > div.row.rel.old-row > script:nth-child(6)::text").get()):
             yield scrapy.Request(
                 url="https://www.tausendkind.de/{}".format(url_key),
-                callback=self.parse_details,
-            )
+                callback=self.parse_details)
 
     @staticmethod
     def parse_details(response):
@@ -97,24 +90,17 @@ class ProductDetailsSpider(scrapy.Spider):
         )
         price = response.css("strong.pdp-price::text").re_first(r"\d+,\d+")
         original_price = response.css(
-            "del.pdp-reduction strong::text").re_first(
-                r"\d+,\d+")
+            "del.pdp-reduction strong::text").re_first(r"\d+,\d+")
         yield {
-            "Sku": response.css(
-                "#catalog-product-view > script:nth-child(2)").re_first(
-                    r'"sku":\"(.*?)\"'),
+            "Sku": response.css("#catalog-product-view > script:nth-child(2)").re_first(
+                r'"sku":\"(.*?)\"'),
             "product_name": response.css("#product-name::text").get(),
             "description": description,
             "price": price,
-            "original_price": original_price
-                              if not price == original_price else "",
-            "currency": response.css("strong.pdp-price::text")
-                        .re_first(r"[A-Z]{3}"),
+            "original_price": original_price if not price == original_price else "",
+            "currency": response.css("strong.pdp-price::text").re_first(r"[A-Z]{3}"),
             "variant_sizes": response.css(
-                "li.select__option::attr(data-sizeval)"
-            ).extract(),
+                "li.select__option::attr(data-sizeval)").extract(),
             "variant_sizes_out_of_stock": response.css(
-                "li.select__option.disabled::attr(data-sizeval)"
-            ).extract(),
+                "li.select__option.disabled::attr(data-sizeval)").extract(),
         }
-
