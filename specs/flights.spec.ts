e79ebb1ea8eaa01961e7770682a7761962e77d5c
Awais Ali <awaisraad@outlook.com>
2019-10-04 13:26:16 +0500

    Outlined test cases

---
+++ specs/flights.spec.ts
@@ -0,0 +1,127 @@
+const BRAND_NAME = process.env.BRAND_NAME;
+
+describe(`${BRAND_NAME} SEM Flights Search`, () => {
+  it('should show "Find hotel deals" in h1 header', async () => {
+    
+  });
+  
+  it('should show destination pre-filled by geo IP', async () => {
+  
+  });
+  
+  it('should show pre-selected dates, check-in date tomorrow with 1 night duration', async () => {
+    
+  });
+  
+  it('should show photo banner behind main search form', async () => {
+  
+  });
+  
+  it('should show Extend your search section with compare to when destination is filled', async () => {
+  
+  });
+  
+  it('should show no compare to while destination value is empty', async () => {
+  
+  });
+  
+  it('should show email subscription field', async () => {
+  
+  });
+  
+  it('should show 3 ad slots', async () => {
+  
+  });
+  
+  it('should show tiles with links to flights', async () => {
+  
+  });
+  
+  it('should show worldwide, continent and country links', async () => {
+  
+  });
+  
+  it('should show Find the perfect hotel section with search button', async () => {
+  
+  });
+  
+  it('should show mobile app section', async () => {
+  
+  });
+  
+  it('should set destination until compare to appear', async () => {
+  
+  });
+  
+  it('should select at least one compare to option if not selected already', async () => {
+  
+  });
+  
+  it('should be able to perform search, and load relevant compare to page and results page', async () => {
+  
+  });
+  
+  it('should show correct data on results page', async () => {
+  
+  });
+  
+  it('should return to SEM landing page', async () => {
+  
+  });
+  
+  it('should open hotels search overlay after clicking on hotels link in a tile', async () => {
+  
+  });
+  
+  it('should select at least one compare to option if not selected already', async () => {
+  
+  });
+  
+  it('should be able to perform search, and load relevant compare to page and results page', async () => {
+  
+  });
+  
+  it('should show correct data on results page', async () => {
+  
+  });
+  
+  it('should return to SEM landing page', async () => {
+  
+  });
+  
+  it('should open flights search overlay after clicking on flights link in a tile', async () => {
+  
+  });
+  
+  it('should select at least one compare to option if not selected already', async () => {
+  
+  });
+  
+  it('should be able to perform search, and load relevant compare to page and results page', async () => {
+  
+  });
+  
+  it('should show correct data on results page', async () => {
+  
+  });
+  
+  it('should return to SEM landing page', async () => {
+  
+  });
+  
+  it('should open hotels search overlay after clicking on Search Now in Find the perfect hotel section', async () => {
+  
+  });
+  
+  it('should select at least one compare to option if not selected already', async () => {
+  
+  });
+  
+  it('should be able to perform search, and load relevant compare to page and results page', async () => {
+  
+  });
+  
+  it('should show correct data on results page', async () => {
+  
+  });
+});
