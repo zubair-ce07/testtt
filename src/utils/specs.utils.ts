d36ee3bae353113aed53edd3fee3683688f7cba2
Awais Ali <awaisraad@outlook.com>
2019-10-07 11:50:00 +0500

    FIX BUG

--- src/utils/specs.utils.ts
+++ src/utils/specs.utils.ts
@@ -8,7 +8,7 @@ export async function findCurrentLocation(): Promise<string> {
 
 export function diffInDays(d1: string, d2: string): number {
   const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
-  return (toDate(d1).getTime() - toDate(d2).getTime()) / ONE_DAY_IN_MS;
+  return (toDate(d2).getTime() - toDate(d1).getTime()) / ONE_DAY_IN_MS;
 }
 
 export function toDate(text: string): Date {
