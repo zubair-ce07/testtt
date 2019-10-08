678a2a9705caa90bb6d91bdcc477bc5d80e0a7ff
Muazam.Husnain <muazam@A002-00130.arbisoft.com>
2019-10-08 14:56:02 +0500

    used loop instead of list comprehension and seperated crawler and parser

--- task2-beyondlimits/beyondlimits_spider.py
+++ task2-beyondlimits/beyondlimits_spider.py
@@ -1,10 +1,28 @@
 import re
-import scrapy
+import unicodedata
 
-from ..items import DetailsItems
+from scrapy.spiders import CrawlSpider
+
+from ..items import BeyondLimitItem
 
 
 class BeyondLimitsExtractor:
+    def parse_details(self, response):
+        product_details = BeyondLimitItem()
+
+        product_details['retailer_sku'] = self.extract_retailor_sku(response)
+        product_details['url'] = self.extract_url(response)
+        product_details['gender'] = self.extract_gender(response)
+        product_details['category'] = self.extract_category(response)
+        product_details['brand'] = self.extract_brand(response)
+        product_details['name'] = self.extract_name(response)
+        product_details['description'] = self.extract_description(response)
+        product_details['care'] = self.extract_care(response)
+        product_details['img_urls'] = self.extract_img_urls(response)
+        product_details['skus'] = self.extract_skus(response)
+
+        yield product_details
+
     def extract_retailor_sku(self, response):
         return response.css('[itemprop="productID"]::text').get()
 
@@ -15,7 +33,8 @@ class BeyondLimitsExtractor:
         return re.search(r'(?<=/)(Wo|wo)?(M|m)en?(?=/)', response.url).group()
 
     def extract_category(self, response):
-        return re.search(r'(?<=.com/).+(?=/)', response.url).group()
+        categories = response.css('.bb_breadcrumb--inner span a ::text').getall()
+        return [category.strip() for category in categories if category.strip() != 'Home' and category.strip()]
 
     def extract_brand(self, response):
         return response.css('[property="og:site_name"]::attr(content)').get()
@@ -24,10 +43,10 @@ class BeyondLimitsExtractor:
         return response.css(".bb_art--title::text").get()
 
     def extract_description(self, response):
-        return response.css('#description p::text').get()
+        return response.css('#description p::text').getall()
 
     def extract_care(self, response):
-        return response.css('#description ul li::text')[1].get()
+        return response.css('#description ul li::text')[1].getall()
 
     def extract_img_urls(self, response):
         return response.css(".bb_pic--nav li a::attr(href)").getall()
@@ -37,13 +56,19 @@ class BeyondLimitsExtractor:
         price = response.css('[itemprop="price"]::attr(content)').get()
         currency = response.css('[itemprop="priceCurrency"]::attr(content)').get()
         previous_prices = response.css('.oldPrice del::text').get()
+        if previous_prices:
+            previous_prices = unicodedata.normalize('NFKD', previous_prices).encode('ascii', 'ignore').split()
         sizes = response.css('#bb-variants--0 option')
-        return [{'colour': colour, 'price': price, 'currency': currency, 'previous_prices': previous_prices,
-                 'size': size.css("option::text").get(), 'sku_id': colour + "_" + size.css("option::text").get()} 
-                for size in sizes if size.css("option::attr(value)").get()]
+        skus = []
+        for size in sizes:
+            if size.css("option::attr(value)").get():
+                skus.append({'colour': colour, 'price': price, 'currency': currency, 'previous_prices': previous_prices,
+                             'size': size.css("option::text").get(),
+                             'sku_id': colour + "_" + size.css("option::text").get()})
+        return skus
 
 
-class CrawlSpider(scrapy.Spider):
+class BeyondLimitsSpider(CrawlSpider):
     name = "beyondlimits"
     allowed_domains = ['beyondlimits.com']
     start_urls = [
@@ -55,24 +80,10 @@ class CrawlSpider(scrapy.Spider):
             yield response.follow(category.get(), callback=self.parse_category)
 
     def parse_category(self, response):
-        for detail_url in response.css('.bb_product--link.bb_product--imgsizer::attr(href)'):
-            yield response.follow(detail_url.get(), callback=self.parse_details)
-
-    def parse_details(self, response):
-        product_details = DetailsItems()
         details_extractor = BeyondLimitsExtractor()
-        product_details['retailer_sku'] = details_extractor.extract_retailor_sku(response)
-        product_details['url'] = details_extractor.extract_url(response)
-        product_details['gender'] = details_extractor.extract_gender(response)
-        product_details['category'] = details_extractor.extract_category(response)
-        product_details['brand'] = details_extractor.extract_brand(response)
-        product_details['name'] = details_extractor.extract_name(response)
-        product_details['description'] = details_extractor.extract_description(response)
-        product_details['care'] = details_extractor.extract_care(response)
-        product_details['img_urls'] = details_extractor.extract_img_urls(response)
-        product_details['skus'] = details_extractor.extract_skus(response)
+        for detail_url in response.css('.bb_product--link.bb_product--imgsizer::attr(href)'):
+            yield response.follow(detail_url.get(), callback=details_extractor.parse_details)
 
-        yield product_details
 
 
 class DetailsItems(scrapy.Item):
