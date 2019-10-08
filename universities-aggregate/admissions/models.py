d9dcdd54c9092c7a812971734a7e29856c47bbd7
Ali Mahmood <ali.mahmood@arbisoft.com>
2019-10-08 17:26:25 +0500

    Fixed database migration problem

--- universities-aggregate/admissions/models.py
+++ universities-aggregate/admissions/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Institute(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
 
 
 class Program(models.Model):
