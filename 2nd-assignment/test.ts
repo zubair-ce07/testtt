4db93e91df017bb7e2aa2ca805b58b0bc281503d
Muhammad.Bilal <bilal@A002-00040.arbisoft.com>
2019-10-10 17:29:42 +0500

    Updated functionality to call eventually.be.true in all assertions

--- 2nd-assignment/test.ts
+++ 2nd-assignment/test.ts
@@ -18,57 +18,57 @@ describe("kayak Automation", async function() {
   });
 
   it("should display the 'Travel Inspiration' section", function() {
-    homePageObject.travelSection().isDisplayed().should.eventually.be.equal(true);
+    homePageObject.travelSection().isDisplayed().should.eventually.be.true;
   });
 
   it("should display the 'Trending Cities' section", function() {
-    homePageObject.citiesSection().isDisplayed().should.eventually.be.equal(true);
+    homePageObject.citiesSection().isDisplayed().should.eventually.be.true;
   });
 
   it("should display the 'Trending Countries' section", function() {
-    homePageObject.countriesSection().isDisplayed().should.eventually.equal(true);
+    homePageObject.countriesSection().isDisplayed().should.eventually.be.true;
   });
 
   it("Should display at least 2 tiles under 'Travel Inspiration' ", function() {
-    homePageObject.getTilesInTravelInspiration().should.eventually.be.equal(true);
+    homePageObject.getTilesInTravelInspiration().should.eventually.be.true;
   });
 
   it("Should display at least 20 tiles under 'Trending Cities' ", function() {
-    homePageObject.getTrendingCityTiles().should.eventually.be.equal(true);
+    homePageObject.getTrendingCityTiles().should.eventually.be.true;
   });
 
   it("Should display at least 20 tiles under 'Trending Countries' ", function() {
-    homePageObject.getTrendingCountriesTiles().should.eventually.be.equal(true);
+    homePageObject.getTrendingCountriesTiles().should.eventually.be.true;
   });
 
   it("Should display image in first tile under 'Travel Inspiration'", function() {
-    homePageObject.getTravelInspirationFirstImage().should.eventually.be.equal(true);
+    homePageObject.getTravelInspirationFirstImage().should.eventually.be.true;
   });
 
   it("Should display image in first tile under 'Trending Cities'", function() {
-    homePageObject.citiesFirstImage.isDisplayed().should.eventually.be.equal(true);
+    homePageObject.citiesFirstImage.isDisplayed().should.eventually.be.true;
   });
 
   it("Should display image in first tile under 'Trending Countries'", function() {
-    homePageObject.countriesFirstImage.isDisplayed().should.eventually.be.equal(true);
+    homePageObject.countriesFirstImage.isDisplayed().should.eventually.be.true;
   });
 
   it("Should be able to click the first tile under 'Travel Inspiration' section", async function() {
-    await homePageObject.clickFirstTravelInspirationTile().should.eventually.equal(true);
+    await homePageObject.clickFirstTravelInspirationTile().should.eventually.true;
   });
 
   it("Should be able to click 'More Inspiration' button  under 'Travel Inspiration' section", async function() {
     homePageObject.navigateToHomePage();
-    await homePageObject.clickMoreInspirationButton().should.eventually.equal(true);
+    await homePageObject.clickMoreInspirationButton().should.eventually.be.true;
   });
 
   it("Should be able to click the 1st tile under 'Trending Cities' section", async function() {
     homePageObject.navigateToHomePage();
-    await homePageObject.clickFirstCityTile().should.eventually.equal(true);
+    await homePageObject.clickFirstCityTile().should.eventually.be.true;
   });
 
   it("Should be able to click the 1st tile under 'Trending Countries' section", function() {
     homePageObject.navigateToHomePage();
-    homePageObject.clickFirstCountryTile().should.eventually.equal(true);
+    homePageObject.clickFirstCountryTile().should.eventually.be.true;
   });
 });
