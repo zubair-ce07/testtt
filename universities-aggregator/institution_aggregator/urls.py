05cc675f72ce01873c31135c478ee6d0164e497d
Ali Mahmood <ali.mahmood@arbisoft.com>
2019-10-16 17:39:28 +0500

    update versioning in api

--- universities-aggregator/institution_aggregator/urls.py
+++ universities-aggregator/institution_aggregator/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/', include('institutions.urls')),
+    path('api/v1/', include('institutions.urls')),
     path('admin/', admin.site.urls),
 ]
