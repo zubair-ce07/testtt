33b8d450230168ba356987558abf6c99d84c7f66
Mehar Hasnain <mehar.hasnain@arbisoft.com>
2019-10-17 18:23:55 +0500

    Added Pagination Request

--- Sixthstreet Spider/sixthstreet_spider.py
+++ Sixthstreet Spider/sixthstreet_spider.py
@@ -1,9 +1,10 @@
+import json
 from scrapy.spiders import Rule, CrawlSpider
 from scrapy.linkextractors import LinkExtractor
 from scrapy import Request
-from json import loads, dumps
 
-from sixthstreet.items import SixthstreetItem 
+from sixthstreet.items import SixthstreetItem
+from sixthstreet.utils import parse_gender
 
 
 class ParseSpider():
@@ -37,9 +38,8 @@ class ParseSpider():
          #'//[@class="breadcrumb"]//a/text()'
         return response.css('.breadcrumb a::text').getall()
 
-    def get_gender(self, response):
-        xpath = '//span[contains(text(), "Gender")]/following-sibling::span/text()'
-        return response.xpath(xpath).get()   
+    def get_gender(self, response):        
+        return parse_gender(response)
 
     def get_url(self, response):
         return response.request.url
@@ -53,11 +53,11 @@ class ParseSpider():
         return response.css('.product_name::text').get()
 
     def get_image_urls(self, response):
-        image_text = response.css('script').re_first(r'"images":(.*),"index"')
-        image_json = loads(image_text)
-        product_id = list(image_json.keys())[0]
+        image_raw = response.css('script').re_first(r'"images":(.*),"index"')
+        image_parsed = json.loads(image_raw)
+        product_id = list(image_parsed.keys())[0]
         
-        return [product['img'] for product in image_json[product_id]]
+        return [product['img'] for product in image_parsed[product_id]]
 
     def get_availability(self, response):
         availability = response.css('[itemprop="availability"]::attr(value)').get()
@@ -65,15 +65,14 @@ class ParseSpider():
         return True if availability != 'In Stock' else False
 
     def get_sizes(self, response):
-        sizes_text = response.css('script').re_first(r"sizeOptionArr = JSON.parse\('(.*)'\);")
-        sizes_json = loads(sizes_text)
-        sizes = [size['UK'] for size in sizes_json]
+        sizes_raw = response.css('script').re_first(r"sizeOptionArr = JSON.parse\('(.*)'\);")
+        sizes_parsed = json.loads(sizes_raw)
+        sizes = [size['UK'] for size in sizes_parsed]
         
         return sizes if sizes else [self.ONE_SIZE]       
 
     def get_skus(self, response):
         skus = {}
-        sku_attributes = {}
 
         colour_xpath = '//span[contains(text(),"Color")]/following-sibling::span/text()'
         currency_css = 'meta[itemprop="priceCurrency"]::attr(content)'
@@ -81,7 +80,8 @@ class ParseSpider():
         
         for colour in response.xpath(colour_xpath).getall():
             for size in self.get_sizes(response):
-                sku_attributes['previous_price'] = int(response.css(price_css).get())
+                sku_attributes = {}
+                sku_attributes['previous_price'] = int(float(response.css(price_css).get())*100)
                 sku_attributes['currency'] = response.css(currency_css).get()
                 sku_attributes['colour'] = colour
                 sku_attributes['size'] = size
@@ -107,19 +107,19 @@ class CrawlSpider(CrawlSpider):
     )   
     
     def parse_listings(self, response):
-        formdata_text = response.css('script').re_first('window\.algoliaConfig = (.*);</script>')
-        category_text = response.css('body::attr(class)').re_first('categorypath-(.*) category')
+        formdata_raw = response.css('script').re_first('window\.algoliaConfig = (.*);</script>')
+        category_raw = response.css('body::attr(class)').re_first('categorypath-(.*) category')
 
-        query_text = list(map(lambda category: category.capitalize(), category_text))
-        query = '%20'.join(query_text)
+        query_raw = list(map(lambda category: category.capitalize(), category_raw))
+        query = '%20'.join(query_raw)
         
-        formdata_json = loads(formdata_text)
-        application_id = formdata_json['applicationId']        
-
+        formdata_parsed = json.loads(formdata_raw)
+        application_id = formdata_parsed['applicationId']    
+              
         url = f'https://{application_id.lower()}-3.algolianet.com/1/indexes/*/' \
             'queries?x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%20(lite)%203.27.0%3Binstantsearch.js' \
             '%202.10.2%3BMagento2%20integration%20(1.10.0)%3BJS%20Helper%202.26.0&' \
-            f'x-algolia-application-id={application_id}&x-algolia-api-key={formdata_json["apiKey"]}'
+            f'x-algolia-application-id={application_id}&x-algolia-api-key={formdata_parsed["apiKey"]}'
         
         formdata = {
             "requests":[
@@ -129,13 +129,47 @@ class CrawlSpider(CrawlSpider):
                 }
             ]
         }
+            
+        yield Request(url, method="POST", body=json.dumps(formdata), meta={'listings':response}, callback=self.parse_pagination)
+
+    def parse_pagination(self, response):
+        listings_response = response.meta['listings']
+
+        formdata_raw = listings_response.css('script').re_first('window\.algoliaConfig = (.*);</script>')
+        category_raw = listings_response.css('body::attr(class)').re_first('categorypath-(.*) category')
+
+        query_raw = list(map(lambda category: category.capitalize(), category_raw))
+        query = '%20'.join(query_raw)
+        
+        formdata_parsed = json.loads(formdata_raw)
+        application_id = formdata_parsed['applicationId']
+        products = json.loads(response.body_as_unicode()) 
+        page_number = 0   
               
-        yield Request(url, method="POST", body=dumps(formdata), callback=self.parse_urls)
+        url = f'https://{application_id.lower()}-3.algolianet.com/1/indexes/*/' \
+            'queries?x-algolia-agent=Algolia%20for%20vanilla%20JavaScript%20(lite)%203.27.0%3Binstantsearch.js' \
+            '%202.10.2%3BMagento2%20integration%20(1.10.0)%3BJS%20Helper%202.26.0&' \
+            f'x-algolia-application-id={application_id}&x-algolia-api-key={formdata_parsed["apiKey"]}'
+
+        while page_number <= int(products['results'][0]['nbPages']):
+            formdata = {
+                "requests":[
+                    {
+                    "indexName": "enterprise_magento_english_products",
+                    "params": f"query={query}&hitsPerPage=60&maxValuesPerFacet=60&page={page_number}"
+                    }
+                ]
+            }
+
+            page_number += 1    
+
+            yield Request(url, method="POST", body=json.dumps(formdata), callback=self.parse_urls)
         
     def parse_urls(self, response):
-        products = loads(response.body_as_unicode())
-        urls = [product['url'] for product in products['results'][0]['hits']]
+        products = json.loads(response.body_as_unicode())
 
+        urls = [product['url'] for product in products['results'][0]['hits']]
+       
         for url in urls:
             yield Request(url, callback=self.product_parser.parse_product)
 
