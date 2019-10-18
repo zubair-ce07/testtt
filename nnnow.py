47c16d9be74f812c5f2958c64d9066e3e086455d
Aun Raza <aunraza021@gmail.com>
2019-10-18 10:40:18 +0500

    rafactor: Issues resolve

--- nnnow.py
+++ nnnow.py
@@ -61,31 +61,30 @@ class NnnowSpider(CrawlSpider):
 
     rules = (
         Rule(LinkExtractor(restrict_css='.nw-leftnavmobile-list a',
-             deny=('/offers')), callback='parse_product_listing'),
+             deny='/offers'), callback='parse_product_listing'),
     )
 
     def get_pages_count(self, response):
         raw_product = json.loads(response.css
                                  ('script').re_first('DATA=(.+)</script>'))
-        parse_raw_product = raw_product['ProductStore']['ProductData']
-        return 1 if not parse_raw_product else parse_raw_product['totalPages']
+        raw_product = raw_product['ProductStore']['ProductData']
+        return raw_product['totalPages'] if raw_product else 1
 
     def parse_product_listing(self, response):
-        get_page_numbers = self.get_pages_count(response)
-        if get_page_numbers:
-            request_url = 'https://api.nnnow.com/d/apiV2/listing/products'
-            category_name = response.url.split('/')[3]
-            category_id = 'tn_{}'.format(category_name.replace('-', '_'))
-            for page in range(get_page_numbers):
-                params = {
-                    "deeplinkurl": f"/{category_name}?p={page}&cid={category_id}"
-                }
-                yield Request(request_url, method='POST',
-                              body=json.dumps(params),
-                              callback=self.parse_listing,
-                              headers=self.headers)
+        pages_count = self.get_pages_count(response)
+        request_url = 'https://api.nnnow.com/d/apiV2/listing/products'
+        category_name = response.url.split('/')[3]
+        category_id = 'tn_{}'.format(category_name.replace('-', '_'))
+        for page in range(pages_count):
+            params = {
+                "deeplinkurl": f"/{category_name}?p={page}&cid={category_id}"
+            }
+            yield Request(request_url, method='POST',
+                          body=json.dumps(params),
+                          callback=self.product_listing,
+                          headers=self.headers)
 
-    def parse_listing(self, response):
+    def product_listing(self, response):
         raw_product = json.loads(response.text)
         for raw_product in raw_product['data']['styles']['styleList']:
             url = urljoin(self.base_url, raw_product['url'])
@@ -108,17 +107,17 @@ class NnnowSpider(CrawlSpider):
     def parse_product(self, response):
         raw_product = json.loads(response.css('script')
                                  .re_first('DATA=(.+)</script>'))
-        parse_raw_product = raw_product['ProductStore']['PdpData']['mainStyle']
+        raw_product = raw_product['ProductStore']['PdpData']['mainStyle']
         loader = ProductItemLoader(item=Product(), response=response)
         loader.add_css('name', '.nw-product-name .nw-product-title::text')
         loader.add_css('category',
                        '.nw-breadcrumblist-list .nw-breadcrumb-listitem::text')
         loader.add_value('description',
-                         parse_raw_product['finerDetails']['specs']['list'])
+                         raw_product['finerDetails']['specs']['list'])
         loader.add_css('image_urls', '.nw-maincarousel-wrapper img::attr(src)')
         loader.add_css('brand', '.nw-product-name .nw-product-brandtxt::text')
-        loader.add_value('retailer_sku', parse_raw_product['styleId'])
+        loader.add_value('retailer_sku', raw_product['styleId'])
         loader.add_value('url', response.url),
-        loader.add_value('skus', self.get_skus(parse_raw_product, response)),
-        loader.add_value('gender', parse_raw_product['gender'])
+        loader.add_value('skus', self.get_skus(raw_product, response)),
+        loader.add_value('gender', raw_product['gender'])
         return loader.load_item()
