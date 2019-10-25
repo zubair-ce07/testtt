04e2e5100a7853379715348a89d34fbb668ec24f
Muazam.Husnain <muazam@A002-00130.arbisoft.com>
2019-10-25 17:21:35 +0500

    implemented re_first and changed some names

--- task5-nnnow.py
+++ task5-nnnow.py
@@ -1,5 +1,4 @@
 import json
-import re
 
 from scrapy import Request
 from scrapy.spiders import CrawlSpider
@@ -12,7 +11,7 @@ class NnnowParser:
 
     def parse_details(self, response):
         item = NnnowItem()
-        product_detail = self.load_json_data(response)
+        product_detail = self.get_raw_product(response)
 
         item['retailer_sku'] = self.extract_retailor_sku(product_detail)
         item['gender'] = self.extract_gender(product_detail)
@@ -22,7 +21,7 @@ class NnnowParser:
         item['name'] = self.extract_name(product_detail)
         item['description'] = self.extract_description(product_detail)
         item['care'] = self.extract_care(product_detail)
-        item['img_urls'] = self.extract_img_urls(product_detail)
+        item['image_urls'] = self.extract_img_urls(product_detail)
         item['skus'] = self.extract_skus(product_detail, response)
 
         item['request_queue'] = self.extract_color_requests(response)
@@ -30,15 +29,15 @@ class NnnowParser:
 
     def parse_color(self, response):
         item = response.meta['item']
-        product_detail = self.load_json_data(response)
+        product_detail = self.get_raw_product(response)
 
-        item['img_urls'] += self.extract_img_urls(product_detail)
+        item['image_urls'] += self.extract_img_urls(product_detail)
         item['skus'] += self.extract_skus(product_detail, response)
 
         yield self.get_item_or_req_to_yield(item)
 
-    def load_json_data(self, response):
-        page_json_data = re.search('(?<=window.DATA= )(.*)', response.css('script::text').get()).group()
+    def get_raw_product(self, response):
+        page_json_data = response.css('script::text').re_first('(?<=window.DATA= )(.*)')
         return json.loads(page_json_data)['ProductStore']['PdpData']['mainStyle']
 
     def get_item_or_req_to_yield(self, item):
@@ -86,7 +85,7 @@ class NnnowParser:
 
     def extract_img_urls(self, product_detail):
         return [imgs['medium'] for imgs in product_detail['images']]
-    
+
     def extract_common_skus(self, product_detail, response):
         color = product_detail['colorDetails']['primaryColor']
         currency = response.css('[itemProp="priceCurrency"]::attr(content)').get()
@@ -94,14 +93,14 @@ class NnnowParser:
 
     def extract_skus(self, product_detail, response):
         common_sku = self.extract_common_skus(product_detail, response)
-        
+
         skus = []
         for product_sku in product_detail['skus']:
             sku = common_sku.copy()
             sku['price'] = product_sku['price']
             sku['previous_price'] = [product_sku['mrp']]
             sku['size'] = product_sku['size']
-            sku['outofstock'] = not product_sku['inStock']
+            sku['out_of_stock'] = not product_sku['inStock']
             sku['sku_id'] = product_sku['skuId']
             skus.append(sku)
         return skus
@@ -124,7 +123,7 @@ class NnnowSpider(CrawlSpider):
         for menu_item in nav_menu_items:
             yield response.follow(menu_item['url'], callback=self.parse_category)
 
-    def parse_category(self, response):
+    def parse_pagination(self, response):
         page_data_json = self.load_json_data(response)
         total_pages = page_data_json['ProductStore']['ProductData']['totalPages']
 
@@ -136,10 +135,10 @@ class NnnowSpider(CrawlSpider):
 
         for pg_no in range(1, total_pages):
             payload = {'deeplinkurl': f'/{category}?p={str(pg_no)}&cid=tn_{category.replace("-", "_")}'}
-            yield response.follow(request_url, callback=self.parse_pages, method='POST', body=json.dumps(payload),
-                                  headers=request_headers)
+            yield response.follow(request_url, callback=self.parse_category, method='POST',
+                                  body=json.dumps(payload), headers=request_headers)
 
-    def parse_pages(self, response):
+    def parse_category(self, response):
         products_urls = json.loads(response.body)['data']['styles']['styleList']
 
         for product_url in products_urls:
@@ -147,7 +146,7 @@ class NnnowSpider(CrawlSpider):
             yield response.follow(urljoin(self.url_domain, url_product), callback=self.nnnow_details.parse_details)
 
     def load_json_data(self, response):
-        return json.loads(re.search('(?<=window.DATA= )(.*)', response.css('script::text').get()).group())
+        return json.loads(response.css('script::text').re_first('(?<=window.DATA= )(.*)'))
 
 
 class NnnowItem(scrapy.Item):
