a5cdbb7fb48237b1c4bf66e88aa01ff119ee9075
Muhammad Ahmad Faraz <ahmadfaraz629@gmail.com>
2019-10-29 12:42:28 +0500

    Record read by File Class instead of FileReader

--- file_reader.py
+++ file_reader.py
@@ -13,11 +13,13 @@ class File:
         _file = open(name)
         next(_file)
         header = [format_header(h) for h in next(_file).split(',')]
-        self.__file = csv.DictReader(_file, fieldnames=header)
+        self.__file_pointer = csv.DictReader(_file, fieldnames=header)
 
-    @property
-    def file(self):
-        return self.__file
+    def records(self):
+        """Read file record one by one"""
+        for record in self.__file_pointer:
+            if record['max_temperaturec'] is not None:
+                yield record
 
 
 class FileReader:
@@ -27,6 +29,7 @@ class FileReader:
         date_pattern = get_date_pattern(date)
         self.__files = glob.glob(f"{path}/*_weather_{date_pattern}.txt")
         self.__current_file_index = 0
+        self._file = None
 
     def has_next_file(self):
         """Check is next file is available"""
@@ -40,13 +43,15 @@ class FileReader:
         """Move index to next file"""
         self.__current_file_index += 1
 
+    def open_next_file(self):
+        self._file = File(self.get_next_filename())
+
     def records(self):
         """Yield data one by one for all the files"""
         while self.has_next_file():
-            _file = File(self.get_next_filename()).file
-            for record in _file:
-                if record['max_temperaturec'] is not None:
-                    yield record
+            self.open_next_file()
+            for record in self._file.records():
+                yield record
             self.move_to_next_file()
 
         self.__current_file_index = 0
