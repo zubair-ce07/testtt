ab9be6450278dede328b9d3f7e5ab74d474c0117
Mehar Hasnain <mehar.hasnain@arbisoft.com>
2019-10-29 12:51:52 +0500

    Add utility function for pricing, refactor code

--- My  Spiders/softsurroundings_spider.py
+++ My  Spiders/softsurroundings_spider.py
@@ -2,7 +2,7 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 from ..items import Product 
-from ..utils import parse_gender
+from ..utils import parse_gender, parse_price
 
 
 class ParseSpider():
@@ -50,7 +50,7 @@ class ParseSpider():
         return response.css('#item::text').get()
                
     def get_brand(self, response):
-        css = 'meta[property="og:site_name"]::attr(content)'
+        css = '[property="og:site_name"]::attr(content)'
         return response.css(css).get()
 
     def get_gender(self, response):
@@ -58,10 +58,9 @@ class ParseSpider():
         size_categories = response.css('#sizecat a::text').getall()
         description = self.get_description(response)
 
-        gender_text = f"{title_text} {' '.join(size_categories)} {' '.join(description)}"
+        raw_gender = f"{title_text} {' '.join(size_categories + description)}"
 
-        return parse_gender(gender_text)
-
+        return parse_gender(raw_gender)
 
     def get_care(self, response):                
         return response.css('#careAndContentInfo::text').getall()    
@@ -70,60 +69,50 @@ class ParseSpider():
         return response.css('.pagingBreadCrumb a::text').getall()
 
     def get_description(self, response):
-        css = 'span[itemprop="description"] p::text, span[itemprop="description"]::text'        
+        css = '[itemprop="description"] p::text, [itemprop="description"]::text'        
         return response.css(css).getall()
 
     def get_url(self, response):
         return response.url
 
     def get_name(self, response):        
-        return response.css('span[itemprop="name"]::text').get()
+        return response.css('[itemprop="name"]::text').get()
 
     def get_image_urls(self, response):        
         return response.css('#detailAltImgs > li a::attr(href)').getall()        
 
     def get_skus(self, response):
         skus = {}
-
-        price_css = 'span[itemprop="price"]::text'
-        currency_css = 'span[itemprop="priceCurrency"]::attr(content)'
+       
+        currency_css = '[itemprop="priceCurrency"]::attr(content)'
         currency = response.css(currency_css).get()
 
         color_css = '.swatchlink .color::attr(data-value)'        
-        color_ids = response.css(color_css).getall() or response.css('input[name^="specOne"]::attr(value)').get()
+        color_ids = response.css(color_css).getall() or response.css('[name^="specOne"]::attr(value)').get()
 
         sizes = response.css('a.box.size::attr(id)').getall()                                
         size_ids = [size.split('_')[1] for size in sizes]
 
         for color_id in color_ids:
             for size_id in size_ids:
+                color_css = f'img[id="color_{color_id}"] + div > span::text, #color .basesize::text'
                 sku_attributes = {}
 
-                sku_attributes.update(self.get_previous_price(response))
-                sku_attributes['current_price'] = self.convert_price(response.css(price_css).get())
+                sku_attributes.update(self.get_price(response))                
                 sku_attributes['currency'] = currency
-                sku_attributes['colour'] = self.get_color(response, color_id)
-                sku_attributes['size'] = self.get_size(response, size_id)               
+                sku_attributes['colour'] = response.css(color_css).get()
+                sku_attributes['size'] = response.css(f'a[id$="{size_id}"]::text, #size .basesize::text').get()               
 
                 skus[f'{color_id}{size_id}'] = sku_attributes
                                
         return skus
+   
+    def get_price(self, response):
+        previous_price = response.css('.ctntPrice::text').re_first(r'Was \$(.*);')
+        current_price = response.css('[itemprop="price"]::text').get()       
 
-    def get_size(self, response, size_id):
-        css = f'a[id$="{size_id}"]::text, #size .basesize::text'
-        return response.css(css).get()
-    
-    def get_color(self, response, color_id):
-        css = f'img[id="color_{color_id}"] + div > span::text, #color .basesize::text'
-        return response.css(css).get()
-
-    def get_previous_price(self, response):       
-        previous_price = response.css('.ctntPrice::text').re_first(r'Was \$(.*);')               
-        return {'previous_price': self.convert_price(previous_price)} if previous_price else {}
-
-    def convert_price(self, price):
-        return int(float(price)*100)    
-
+        return parse_price(previous_price, current_price)
+        
     def skus_requests(self, response):
         size_cat = response.css('#sizecat > a::attr(id)').getall()
         sizes = [size.split('_')[1] for size in size_cat]        
@@ -136,16 +125,14 @@ class ParseSpider():
         product_id = self.get_retailer_sku(response)
         color_css = '.swatchlink .color::attr(data-value)'
 
-        color_ids = response.css(color_css).getall() or response.css('input[name^="specOne"]::attr(value)').get()
-        sizes = response.css('a.box.size::attr(id)').getall()
-
-        size_ids = [size.split('_')[1] for size in sizes]
+        color_ids = response.css(color_css).getall() or response.css('[name^="specOne"]::attr(value)').get()        
+        size_ids = [size.split('_')[1] for size in response.css('a.box.size::attr(id)').getall()]
         
         for color_id in color_ids:
             for size_id in size_ids:
 
                 availability_requests.append(
-                    response.follow(f'/p/{product_id.lower()}/{color_id}{size_id}', method='POST', \
+                    response.follow(f'/p/{product_id.lower()}/{color_id}{size_id}',
                     callback=self.parse_availability))
 
         return availability_requests        
@@ -177,7 +164,4 @@ class CrawlSpider(CrawlSpider):
     )
     
     def parse_item(self, response):        
-        return self.softsurroundings_parser.parse_product(response)
-
-    def parse_start_url(self, response):
-        return self.parse_item(response)    
+        return self.softsurroundings_parser.parse_product(response) 
--- My  Spiders/utils.py
+++ My  Spiders/utils.py
@@ -1,10 +1,23 @@
-def parse_gender(gender_text):
-    FEMALE_KEYWORDS = ['women', 'woman', 'misses', 'female', 'feminine']
-    MALE_KEYWORDS = [' men', 'man', 'male', 'masculine']    
+FEMALE_KEYWORDS = ['women', 'woman', 'misses', 'female', 'feminine']
+MALE_KEYWORDS = [' men', 'man', 'male', 'masculine']
 
-    if any(gender in gender_text.lower() for gender in FEMALE_KEYWORDS):
+
+def parse_gender(raw_gender):
+    raw_gender = raw_gender.lower()
+
+    if any(gender in raw_gender for gender in FEMALE_KEYWORDS):
         return 'Women'
-    elif any(gender in gender_text.lower() for gender in MALE_KEYWORDS):
+    elif any(gender in raw_gender for gender in MALE_KEYWORDS):
         return 'Men'
 
     return 'Unisex adult'
+
+
+def parse_price(previous_price, current_price):
+    if previous_price:
+        return {
+            'previous_price': int(float(previous_price)*100),
+            'current_price': int(float(current_price)*100) 
+        }
+
+    return {'current_price': int(float(current_price)*100)}    
