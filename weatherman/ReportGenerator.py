3cfe3a1df56a064ee01e30d0f7fb5ae3d20bfdf2
Ishfaq Ahmed <ishfaq.ahmed@arbisoft.com>
2019-10-31 13:00:47 +0500

    modified weatherman

--- weatherman/ReportGenerator.py
+++ weatherman/ReportGenerator.py
@@ -1,5 +1,4 @@
 import os.path
-from reportlab.pdfgen import canvas
 
 
 month_lst = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
@@ -18,24 +17,22 @@ class YearlyReport:
         self.max_humid = 0
         self.max_humid_month = 0
 
-        self.ReportGenerator()
+        self._report_generator()
 
-    def ReportGenerator(self):
+    def _report_generator(self):
         print("\n    Report 1")
         
         
-        for month in range(12):
-            
-            my_file = self.file_path + "_" + month_lst[month] + ".txt"
+        for current_month in month_lst:
+            my_file = self.file_path + "_" + current_month + ".txt"
             temprature_list = []
             humidity_list = []
-
-            if os.path.isfile(my_file): 
+            
+            if os.path.isfile(my_file):
                 with open(my_file) as file:
                     next(file)             #skip first line of the file
-                    # Values of temprature and humidity inserted into perticular list
-                    for line in file:
-                        file = line.split(',')
+                    for each_line in file:
+                        file = each_line.split(',')
                         temprature_list.append(file[2])
                         humidity_list.append(file[8])
         
@@ -49,36 +46,29 @@ class YearlyReport:
 
                 if len(temprature_list) != 0 and self.max_temp < max(temprature_list):
                     self.max_temp = max(temprature_list)
-                    self.max_temp_month = month
+                    self.max_temp_month = current_month
                     
                 if len(temprature_list) != 0 and self.min_temp > min(temprature_list):    
                     self.min_temp = min(temprature_list)
-                    self.min_temp_month = month
+                    self.min_temp_month = current_month
 
                 if len(humidity_list) != 0 and self.max_temp < max(humidity_list):
                     self.max_humid = max(humidity_list)
-                    self.max_humid_month = month    
+                    self.max_humid_month = current_month    
 
             
-        print("Highest: " + str(self.max_temp) + "C on " + month_lst[self.max_temp_month])   
-        print("Lowest: " + str(self.min_temp) + "C on " + month_lst[self.min_temp_month])
-        print("Humidity: " + str(self.max_humid) + "% on " + month_lst[self.max_humid_month])
+        print(f"Highest: {self.max_temp} C on  {self.max_temp_month}")
+        print(f"Lowest: {self.min_temp} C on  {self.min_temp_month}")
+        print(f"Humidity: {self.max_humid} C on  {self.max_humid_month}")
 
-
-        # Genarates report
-        c = canvas.Canvas("Report1.pdf")
-        c.drawString(150, 700, "Highest: " + str(self.max_temp) + "C on " + month_lst[self.max_temp_month])
-        c.drawString(150, 685, "Lowest: " + str(self.min_temp) + "C on " + month_lst[self.min_temp_month])
-        c.drawString(150, 670, "Humidity: " + str(self.max_humid) + "% on " + month_lst[self.max_humid_month])
-        c.save()
-    
+     
 class MonthlyReport:
     def __init__(self, file_path, month):
         self.file_path = file_path
         self.month = month
-        self.ReportGenerator()
+        self._report_generator()
 
-    def ReportGenerator(self):
+    def _report_generator(self):
         print("\n    Report 2")
         month = int(self.month)
         my_file = self.file_path + "_" + month_lst[month] + ".txt"
@@ -87,7 +77,6 @@ class MonthlyReport:
         if os.path.isfile(my_file): 
             with open(my_file) as file:
                 next(file)          #skip first line of the file
-                # Values of temprature and humidity inserted into perticular list
                 for line in file:
                     file = line.split(',')
                     temprature_list.append(file[3])
@@ -97,29 +86,23 @@ class MonthlyReport:
                 temprature_list.remove('')
             while '' in humidity_list:
                 humidity_list.remove('')    
-                
+            #converts string elementes of the list into int for calculations    
             temprature_list = list(map(int, temprature_list)) 
             humidity_list = list(map(int, humidity_list))
 
             if len(temprature_list) != 0:
-                print("Highest Average: " + str(max(temprature_list)) + "C")
-                print("Lowest Average: " + str(min(temprature_list))+ "C")
-                print("Average Mean Humidity: " + str(sum(humidity_list)/len(humidity_list)) + "%")    
+                print(f"Highest Average: {max(temprature_list)}C")
+                print(f"Lowest Average: {min(temprature_list)}C")
+                print(f"Average Mean Humidity: {sum(humidity_list)/len(humidity_list)}%C")  
 
-                # Genarates report
-                c = canvas.Canvas("Report2.pdf")
-                c.drawString(150, 700, "Highest Average: " + str(max(temprature_list)) + "C")
-                c.drawString(150, 685, "Lowest Average: " + str(min(temprature_list))+ "C")
-                c.drawString(150, 670, "Average Mean Humidity: " + str(sum(humidity_list)/len(humidity_list)) + "%")
-                c.save()        
-
+                
 class HorizontalBarReport:
     def __init__(self, file_path, month):
         self.file_path = file_path
         self.month = month
-        self.ReportGenerator()
+        self._report_generator()
 
-    def ReportGenerator(self):
+    def _report_generator(self):
         print("\n    Report 3")
         month = int(self.month)
         my_file = self.file_path + "_" + month_lst[self.month] + ".txt"
@@ -144,17 +127,17 @@ class HorizontalBarReport:
             max_temprature_list = list(map(int, max_temprature_list)) 
             min_temprature_list = list(map(int, min_temprature_list))
 
-            c = canvas.Canvas("Report3.pdf")
             for i in range(len(max_temprature_list)):
                 if i < 9:
-                    print("\n0" + str(i+1) + " "),
+                    print("\n0" + str(i+1), end=" ")
                 else:
-                    print("\n" + str(i+1) + " "),
+                    print("\n" + str(i+1), end=" ")
 
 
                 for j in range(min_temprature_list[i]):
-                    print('\033[1;34m' + '+' + '\033[1;m'),              
+                    print(f'\033[1;34m' '+' '\033[1;m', end=" ")              
                 
                 for j in range(max_temprature_list[i]):
-                    print('\033[31m' + '+' + '\033[0m'),
-                print(str(min_temprature_list[i]) + "C" + " - " + str(max_temprature_list[i]) + "C")    
+                    print(f'\033[31m' '+' '\033[0m', end=" ")
+                print(f'{min_temprature_list[i]}C - {max_temprature_list[i]}C')    
+                
--- weatherman/readfromfiles.py
+++ weatherman/readfromfiles.py
@@ -8,9 +8,9 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("path", type=str, help="to get the path of weather file")
-    parser.add_argument("-e", "--report1", help="to Show report1",type=str)
-    parser.add_argument("-a", "--report2", help="to Show report2",type=str)
-    parser.add_argument("-c", "--report3", help="to Show report3",type=str)
+    parser.add_argument("-e", "--e", help="to Show report1",type=str)
+    parser.add_argument("-a", "--a", help="to Show report2",type=str)
+    parser.add_argument("-c", "--c", help="to Show report3",type=str)
     
 
     argForFilepath = parser.parse_args()
@@ -18,24 +18,26 @@ def main():
     filepath = filepath[1:]     #Eliminate '/'
     args = parser.parse_args()
     
-    if args.report1:
-        Date = args.report1
+    if args.e:
+        Date = args.e
         fpath = filepath + "/Murree_weather_" + Date
         yearly_report = ReportGenerator.YearlyReport(fpath)
 
-    if args.report2:
-        Date = args.report2
+    if args.a:
+        Date = args.a
         Date = Date.split('/')
         fpath = filepath + "/Murree_weather_" + Date[0]     #Spearate year and month
         monthly_report = ReportGenerator.MonthlyReport(fpath, int(Date[1])-1)
         
-    if args.report3:
-        Date = args.report3
+    if args.c:
+        Date = args.c
         Date = Date.split('/')
         fpath = filepath + "/Murree_weather_" + Date[0]     #Spearate year and month
+        print(filepath)
         horizontal_bar_report = ReportGenerator.HorizontalBarReport(fpath, int(Date[1])-1)
         
     print("***PDF Report is Also generated***")
 
-main()
+if(__name__ == "__main__"):
+    main()
 
