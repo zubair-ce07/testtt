452f5b4a128fd47435673f9816eaf5006854b5d1
Mehar Hasnain <mehar.hasnain@arbisoft.com>
2019-11-01 17:48:45 +0500

    Move format_date method inside the spider file, refactor code

--- My Spiders/Expatriates Spider/expatriates_spider.py
+++ My Spiders/Expatriates Spider/expatriates_spider.py
@@ -1,8 +1,8 @@
 from scrapy.spiders import Spider
+from datetime import datetime
 from string import Template
 
 from ..items import Classified
-from ..utils import format_date
 
 
 class ExpatriatesParseSpider():
@@ -13,14 +13,14 @@ class ExpatriatesParseSpider():
         classified['name'] = self.get_name(response)
         classified['date'] = self.get_date(response)
         classified['category'] = self.get_category(response)
-        classified['region'] = self.get_region(response) or response.css('new_region::text').get().strip()
+        classified['region'] = self.get_region(response)
         classified['classified_id'] = self.get_classified_id(response)
         classified['phone'] = self.get_phone(response)
         classified['description'] = self.get_description(response)
         classified['image_urls'] = self.get_image_urls(response)
         classified['url'] = self.get_url(response)
 
-        yield classified
+        return classified
 
     def get_name(self, response):
         return response.css('.page-title h1::text').get().strip()
@@ -28,30 +28,35 @@ class ExpatriatesParseSpider():
     def get_date(self, response):
         date = response.css('.post-info li').re_first(r'<strong>Date:</strong> (.*)</li>')
                 
-        return format_date(date, '%A, %B %d, %Y')
+        return self.format_date(date, '%A, %B %d, %Y')
 
     def get_category(self, response):
         return response.css('.post-info li').re_first(r'<strong>Category:</strong> (.*)</li>')
 
     def get_region(self, response):
-        return response.css('.post-info li').re_first(r'<strong>Region:</strong> (.*)</li>')
+        region = response.css('.post-info li').re_first(r'<strong>Region:</strong> (.*)</li>')
+
+        return region if region else response.css('new_region::text').get().strip()
 
     def get_classified_id(self, response):
         return response.css('.post-info li').re_first(r'<strong>Posting ID:</strong> (.*)</li>')
     
     def get_phone(self, response):
-        return int(response.css('.posting-phone a::text').get())
+        phone = response.css('.posting-phone a::text').get()
+        
+        return int(phone) if phone else ''
 
-    def get_description(self, response):
-        raw_description = [description.strip() for description in response.css('.post-body::text').getall()]
-
-        return [description for description in raw_description if description]
+    def get_description(self, response):        
+        return [description.strip() for description in response.css('.post-body::text').getall()][:-2]
 
     def get_image_urls(self, response):
         return response.css('.posting-images img::attr(src)').getall()
 
     def get_url(self, response):
         return response.url
+
+    def format_date(self, date, date_format):
+        return datetime.strptime(date, date_format).date()
 
 
 class ExpatriatesCrawlSpider(Spider):
@@ -70,7 +75,7 @@ class ExpatriatesCrawlSpider(Spider):
             yield response.follow(self.url_template.substitute(page=count), callback=self.parse_listings)
 
     def parse_listings(self, response):        
-        classified_urls = response.css('.listing-content a::attr(href)').getall()
+        classified_urls = [url for url in response.css('.listing-content a::attr(href)').getall() if '/cls' in url]
 
         for url in classified_urls:
             yield response.follow(url, callback=self.parse_item)      
--- My Spiders/Expatriates Spider/utils.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime
-
-def format_date(date, date_format):
-    return datetime.strptime(date, date_format).date()
