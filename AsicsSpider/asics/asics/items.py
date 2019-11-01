ab06f0bfd382d889e18c93741a8878a85b1de25a
Muhammad Ahmad Faraz <ahmadfaraz629@gmail.com>
2019-11-01 15:37:04 +0500

    Asics Spider Complete

--- AsicsSpider/asics/asics/items.py
+++ AsicsSpider/asics/asics/items.py
@@ -10,5 +10,22 @@ import scrapy
 
 class AsicsItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    product_name = scrapy.Field()
+    description = scrapy.Field()
+    category = scrapy.Field()
+    url = scrapy.Field()
+    original_url = scrapy.Field()
+    image_urls = scrapy.Field()
+    brand = scrapy.Field()
+    currency = scrapy.Field()
+    lang = scrapy.Field()
+    gender = scrapy.Field()
+    price = scrapy.Field()
+    skus = scrapy.Field()
+    care = scrapy.Field()
+    date = scrapy.Field()
+    merch_info = scrapy.Field(default=[])
+    trail: scrapy.Field(default=None)
+    uuid: scrapy.Field(default=None)
+    retailer: scrapy.Field(default='asics-us')
+    market: scrapy.Field(default='US')
--- AsicsSpider/asics/asics/spiders/asics_spider.py
+++ AsicsSpider/asics/asics/spiders/asics_spider.py
@@ -1,55 +1,133 @@
 import scrapy
 from scrapy.http.request import Request
+import re
+from asics.items import AsicsItem
+from datetime import datetime
 
 
 class AsicsSpider(scrapy.Spider):
     name = 'asics'
-
-    start_urls = [
-        "http://www.asics.com/us/en-us/"
-    ]
+    allowed_domains = ["asics.com"]
+    product = AsicsItem()
+    product_variant_links = []
 
     def start_requests(self):
         yield Request(url="http://www.asics.com/us/en-us/", callback=self.parse_categories)
 
     def parse_categories(self, response):
         for link in response.css(".show-menu-item::attr(href)").getall():
-            page = response.urljoin(f'{link}?start=0')
-            yield response.follow(page, callback=self.parse_products)  # going to one layer deep from landing page
+            yield response.follow(link, callback=self.parse_products)  # going to one layer deep from landing page
 
     def parse_products(self, response):
-        counter = 0
         products = response.css(".product-image a::attr(href)").getall()
-        current_product = int(response.request.url.split("start=")[1])
-        print(f'Products Count {current_product}')
+        next_page = response.css(".page-next::attr(href)").get()
         for link in products:
-            # print(link)
-            page = response.urljoin(link)
-            yield response.follow(page, callback=self.parse_product)
-            counter += 1
+            yield response.follow(link, callback=self.parse_single_product)
+        if next_page:
+            yield response.follow(next_page, callback=self.parse_products)
 
-        if len(products) > 0:
-            page = response.urljoin(f"{response.request.url.split('start=')[0]}start={current_product + counter}")
-            yield response.follow(page, callback=self.parse_products)
+    def parse_single_product(self, response):
+        self.product['description'] = description(response)
+        self.product['product_name'] = product_name(response)
+        self.product['category'] = product_category(response)
+        self.product['image_urls'] = []
+        self.product['skus'] = {}
+        self.product['date'] = datetime.now().timestamp()
+        self.product['price'] = price(response)
+        self.product['url'] = response.url
+        self.product['original_url'] = response.url
 
-    def parse_product(self, response):
-        description = response.css(".product-info-section-inner::text").get()
-        product_name = response.css(".pdp-top__product-name::text").get()
-        category = response.css(".product-classification span::text").get()
-        image_url = response.css(".thumbnail-link::attr(href)").getall()
         script = response.xpath('//script[@type="text/javascript"]')
-        brand = script.re(r'"brand": "(\w+)"')
-        currency = script.re(r'"currency": "(\w+)"')
-        lang = script.re(r'"language": "(\w+)"')
-        gender = script.re(r'"product_gender": \[\n +"(\w+)"')
-        price = script.re(r'"product_unit_price": \[\n +"(\w+)"')
-        print({'product_name': product_name, 'description': description, 'image_url': image_url, 'brand':brand})
-        yield {}
+        self.product['brand'] = brand(script)
+        self.product['currency'] = currency(script)
+        self.product['lang'] = lang(script)
+        self.product['gender'] = gender(script)
 
-    # def parse_target_links(self, response):
-    #     for link in response.css('.art-indexbutton-wrapper .art-indexbutton::attr(href)').extract():
-    #         yield response.follow(link, callback=self.parse_docs)  # tracking links leading to the target page
-    #
-    # def parse(self, response):
-    #     items = [item for item in response.css('.titre_12_red::text').extract()]
-    #     yield {"categories": items}  # this is where the scraper parses the info
+        self.product_variants_links = response.css(".js-color::attr(href)").getall()[1:]
+        for product in self.parse(response):
+            if not isinstance(product, Request):
+                yield product
+
+    def parse(self, response):
+        self.product['image_urls'] += image_urls(response)
+        self.product['skus'].update(parse_sku(response))
+
+        if len(self.product_variant_links) > 0:
+            page = self.product_variant_links[0]
+            del self.product_variant_links[0]
+            yield response.follow(page, callback=self.parse)
+        else:
+            yield self.product
+
+
+def product_name(response):
+    return response.css(".pdp-top__product-name::text").get().strip()
+
+
+def product_category(response):
+    return [response.css(".product-classification span::text").get()]
+
+
+def description(response):
+    return response.css(".product-info-section-inner::text").get().strip()
+
+
+def image_urls(response):
+    images = response.css(".thumbnail-link::attr(href)").getall()
+    if '#' in images:
+        images.remove('#')
+    return images
+
+
+def product_category(response):
+    return [response.css(".product-classification span::text").get()]
+
+
+def price(response):
+    return response.css(".price-sales::text").extract()[1].strip()[1:]
+
+
+def brand(script):
+    return script.re(r'"brand": "(\w+)"')
+
+
+def currency(script):
+    return script.re(r'"currency": "(\w+)"')
+
+
+def lang(script):
+    return script.re(r'"language": "(\w+)"')
+
+
+def gender(script):
+    return script.re(r'"product_gender": \[\n +"(\w+)"')
+
+
+def sku(response):
+    return response.css(".product-number span+ span::text").get().strip()
+
+
+def colour(response):
+    return response.css(".variants__header--light::text").get().strip()
+
+
+def sku_sizes(response):
+    return response.css(".variation-group-value .js-ajax::text").getall()
+
+
+def parse_sku(response):
+    skus = {}
+    product_sku = sku(response)
+    product_colour = colour(response)
+    product_sku_sizes = sku_sizes(response)
+
+    for sku_size in product_sku_sizes:
+        sku_size = sku_size.strip()
+        skus[f"{product_sku.replace('.', '|')}|{product_colour}|{sku_size}"] = {
+            "colour": product_colour,
+            "size": sku_size,
+            "price": price(response),
+            'currency': 'USD'
+        }
+
+    return skus
