377af2a1d07c8c42f714050669b08dcc414ad0c9
Mehar Hasnain <mehar.hasnain@arbisoft.com>
2019-11-11 15:10:41 +0500

    Refactor code

--- My Spiders/Expatriates Spider/expatriates_spider.py
+++ My Spiders/Expatriates Spider/expatriates_spider.py
@@ -1,11 +1,12 @@
 from scrapy.spiders import Spider
 from datetime import datetime
-from string import Template
+from urllib.parse import urljoin
 
 from ..items import Classified
 
 
 class ExpatriatesParseSpider():
+    BASE_URL = 'https://www.expatriates.com'
 
     def parse_classified(self, response):
         classified = Classified()
@@ -26,37 +27,31 @@ class ExpatriatesParseSpider():
         return response.css('.page-title h1::text').get().strip()
 
     def get_date(self, response):
-        date = response.css('.post-info li').re_first(r'<strong>Date:</strong> (.*)</li>')
-                
-        return self.format_date(date, '%A, %B %d, %Y')
+        date = response.css('.post-info li:contains(Date)::text').get().strip()                        
+        return datetime.strptime(date, '%A, %B %d, %Y').date()
 
     def get_category(self, response):
-        return response.css('.post-info li').re_first(r'<strong>Category:</strong> (.*)</li>')
+        return response.css('.post-info li:contains(Category)::text').get().strip()
 
     def get_region(self, response):
-        region = response.css('.post-info li').re_first(r'<strong>Region:</strong> (.*)</li>')
-
+        region = response.css('.post-info li:contains(Region)::text').get().replace('(', '').strip()
         return region if region else response.css('new_region::text').get().strip()
 
     def get_classified_id(self, response):
-        return response.css('.post-info li').re_first(r'<strong>Posting ID:</strong> (.*)</li>')
+        return response.css('.post-info li:contains("Posting ID")::text').get().strip()
     
     def get_phone(self, response):
-        phone = response.css('.posting-phone a::text').get()
-        
+        phone = response.css('.posting-phone a::text').get()        
         return int(phone) if phone else ''
 
     def get_description(self, response):        
         return [description.strip() for description in response.css('.post-body::text').getall()][:-2]
 
     def get_image_urls(self, response):
-        return response.css('.posting-images img::attr(src)').getall()
+        return [urljoin(self.BASE_URL, url) for url in response.css('.posting-images img::attr(src)').getall()]
 
     def get_url(self, response):
         return response.url
-
-    def format_date(self, date, date_format):
-        return datetime.strptime(date, date_format).date()
 
 
 class ExpatriatesCrawlSpider(Spider):
@@ -66,19 +61,20 @@ class ExpatriatesCrawlSpider(Spider):
 
     classified_parser = ExpatriatesParseSpider()
 
-    url_template = Template('https://www.expatriates.com/classifieds/saudi-arabia/for-sale/index$page.html')
+    listings_url = 'https://www.expatriates.com/classifieds/saudi-arabia/for-sale/index{page}.html'
 
     def parse(self, response):
         pages_count = int(response.css('.pagination a::text').getall()[-2])
 
         for count in range(100, pages_count*100, 100):                        
-            yield response.follow(self.url_template.substitute(page=count), callback=self.parse_listings)
+            yield response.follow(self.listings_url.format(page=count), callback=self.parse_listings)
 
     def parse_listings(self, response):        
-        classified_urls = [url for url in response.css('.listing-content a::attr(href)').getall() if '/cls' in url]
+        classified_urls = response.css('.listing-content a::attr(href)').getall()
 
         for url in classified_urls:
-            yield response.follow(url, callback=self.parse_item)      
+            if '/cls' in url:
+                yield response.follow(url, callback=self.parse_item)      
 
     def parse_item(self, response):        
         return self.classified_parser.parse_classified(response)        
