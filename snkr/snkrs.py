b6922244b4404be646e2fa725b973e9d237059cd
AhmadRaza2446 <ahmadraza2446@gmail.com>
2019-11-15 14:52:22 +0500

    Required Changes In a File

--- snkr/snkrs.py
+++ snkr/snkrs.py
@@ -1,5 +1,6 @@
-import scrapy
 import re
+
+import scrapy
 
 from ..items import SnkrsItem
 
@@ -13,53 +14,15 @@ class SnkrsSpider(scrapy.Spider):
         urls = response.css('a.a-niveau1::attr(href)').getall()
 
         for url in urls:
-            yield response.follow(url, callback=self.listing_page)
+            yield response.follow(url, callback=self.parse_listing_page)
 
-    def listing_page(self, response):
-        urls = response.css('ul.product_list.grid.row' +
-            ' > .ajax_block_product.block_home.col-xs-6.col-sm-4.col-md-3' +
-            ' > .product-container > .left-block > .product-image-container' +
-            ' > .product_img_link::attr(href)').getall()
+    def parse_listing_page(self, response):
+        urls = response.css('.product_img_link::attr(href)').getall()
 
         for url in urls:
-            yield response.follow(url, callback=self.product_page)
+            yield response.follow(url, callback=self.parse_product_page)
 
-    def get_retailer_sku(self, response):
-        return response.css('div.nosto_product > span.product_id::text').get()
-
-    def get_brand(self, response):
-        return response.css('div.nosto_product > span.brand::text').get()
-
-    def get_category(self, response):
-        return response.css('div.nosto_category::text').get()
-
-    def get_desciption(self, response):
-        return response.css('div.rte > p::text').getall()
-
-    def get_gender(self, response):
-        gender = response.css('div.nosto_category::text').re_first(r'(Men|Women|Skate|Lifestyle)')
-
-        if gender == 'Men':
-            return gender
-
-        elif gender == 'Women':
-            return gender
-
-        elif (gender != 'Men') or (gender != 'Women'):
-            gender = 'unisex-adults'
-            return gender
-
-    def get_url(self, response):
-        return response.css('div.nosto_product > span.url::text').get()
-
-    def get_name(self, response):
-        return response.css('div.nosto_product > span.name::text').get()
-
-    def get_image_urls(self, response):
-        return  response.css('div.nosto_product > .image_url::text').getall() + \
-                response.css('div.nosto_product > .alternate_image_url::text').getall()
-
-    def product_page(self, response):
+    def parse_product_page(self, response):
         items = SnkrsItem()
 
         items['retailer_sku'] = self.get_retailer_sku(response)
@@ -70,18 +33,56 @@ class SnkrsSpider(scrapy.Spider):
         items['url'] = response.url
         items['name'] = self.get_name(response)
         items['image_urls'] = self.get_image_urls(response)
-        shoes_name = response.css('div.nosto_product > span.name::text').get()
+        shoes_name = items['name']
         get_colour = re.sub(r'.*- ', '', shoes_name)
 
-        for sizes in response.css('span.units_container > .size_EU::text').getall():
-            items['skus'] = {get_colour + "_" + sizes:
-                {
+        for sizes in response.css(
+                'span.units_container .size_EU::text').getall():
+            items['skus'] = {
+                get_colour +
+                "_" +
+                sizes: {
                     "colour": get_colour,
-                    "currency": response.css('div.nosto_product > span.price_currency_code::text').get(),
-                    "price": response.css('div.nosto_product > span.price::text').get(),
+                    "currency": response.css('div.nosto_product span.price_currency_code::text').get(),
+                    "price": response.css('div.nosto_product span.price::text').get(),
                     "old_price": response.xpath('//span[@id="old_price_display"]/span/text()').get(),
-                    "size": sizes
-                },
+                    "size": sizes},
             }
-            
+            yield items['skus']
+
         yield items
+
+    def get_retailer_sku(self, response):
+        return response.css('div.nosto_product span.product_id::text').get()
+
+    def get_brand(self, response):
+        return response.css('div.nosto_product span.brand::text').get()
+
+    def get_category(self, response):
+        return response.css('div.nosto_category::text').get()
+
+    def get_desciption(self, response):
+        return response.css('div.rte p::text').getall()
+
+    def get_gender(self, response):
+        gender = response.css('div.nosto_category::text').re_first(
+            r'(Men|Women|Skate|Lifestyle)')
+
+        if gender == 'Men':
+            gender = 'men'
+            return gender
+
+        elif gender == 'Women':
+            gender = 'women'
+            return gender
+
+        else:
+            gender = 'unisex-adults'
+            return gender
+
+    def get_name(self, response):
+        return response.css('div.nosto_product span.name::text').get()
+
+    def get_image_urls(self, response):
+        return response.css('div.nosto_product .image_url::text').getall(), response.css(
+            'div.nosto_product' + ' .alternate_image_url::text').getall()
