22ec6196e5c9f459982f7058a96a989503f758e9
fahad-shawal <fahad.shawal@arbisoft.com>
2019-12-04 14:48:55 +0500

    Code refactored. Change of logic and value extraction

--- skuscraper/spiders/6thstreet.py
+++ skuscraper/spiders/6thstreet.py
@@ -5,7 +5,7 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Rule
 from w3lib.url import add_or_replace_parameter
 
-from .base import BaseCrawlSpider, BaseParseSpider, clean, soupify
+from .base import BaseCrawlSpider, BaseParseSpider, clean, soupify, Gender
 
 
 class Mixin:
@@ -36,15 +36,15 @@ class SixthStreetSpider(BaseParseSpider):
         garment['name'] = self.product_name(response)
         garment['description'] = self.product_description(raw_product)
         garment['care'] = self.product_care(response)
-        garment['category'] = self.product_category(raw_product['categories']['level2'][0])
+        garment['category'] = self.product_category(raw_product['categories'])
         garment['brand'] = self.product_brand(raw_product)
         garment['image_urls'] = self.product_images(raw_product)
-        garment['gender'] = self.product_gender(response, raw_product)
-        garment['skus'] = self.product_skus(response, raw_product)
+        garment['gender'] = self.product_gender(raw_product)
+        garment['skus'] = self.product_skus(raw_product)
         return garment
 
     def product_retailer_sku(self, response):
-        raw_retailer_sku = response.css('.pdp_middle_right li span')[1]
+        raw_retailer_sku = response.css('.pdp_middle_right li::text').get()
         return clean(raw_retailer_sku)
 
     def product_name(self, response):
@@ -55,7 +55,10 @@ class SixthStreetSpider(BaseParseSpider):
         return [raw_data['description']]
 
     def product_category(self, raw_data):
-        return raw_data.split(' /// ')
+        raw_category = raw_data.get('level2')
+        if not raw_category:
+            raw_category = raw_data['level1']
+        return raw_category[0].split(' /// ')
 
     def product_brand(self, raw_data):
         return clean(raw_data['brand_name'])
@@ -63,12 +66,16 @@ class SixthStreetSpider(BaseParseSpider):
     def product_images(self, raw_data):
         return raw_data['gallery_images']
 
-    def product_gender(self, response, raw_data):
-        soup = soupify(raw_data['gender'])
+    def product_gender(self, raw_data):
+        raw_gender = raw_data.get('gender')
+        if not raw_gender:
+            return Gender.ADULTS.value
+
+        soup = soupify(raw_gender)
         gender = self.gender_lookup(soup, merge_genders=True)
         return gender
 
-    def product_skus(self, response, raw_product):
+    def product_skus(self, raw_product):
         sizes_us = raw_product['size_us']
         sizes_uk = raw_product['size_uk']
         sizes_eu = raw_product['size_eu']
@@ -113,12 +120,10 @@ class SixthStreetCrawler(BaseCrawlSpider):
         raw_data = response.css('[crossorigin="anonymous"] + script::text').re_first('=\s*({.*})')
         raw_data = json.loads(raw_data)
         query = raw_data.get('request')['path'].replace('///', '')
-        body = self.make_request_body(query)
-        url = add_or_replace_parameter(self.url, 'x-algolia-application-id', raw_data.get('applicationId'))
-        url = add_or_replace_parameter(url, 'x-algolia-api-key', raw_data.get('apiKey'))
+        url = self.add_parameters(self.url, raw_data)
 
-        yield FormRequest(url=url, body=json.dumps(body), method='POST', callback=self.parse_products,
-                          meta={'trail': self.add_trail(response)})
+        yield FormRequest(url=url, body=self.make_request_body(query), method='POST',
+                          callback=self.parse_products, meta={'trail': self.add_trail(response)})
 
     def parse_products(self, response):
         raw_data = json.loads(response.text)
@@ -130,16 +135,21 @@ class SixthStreetCrawler(BaseCrawlSpider):
 
         current_page = raw_data['results'][0]['page']
         total_pages = raw_data['results'][0]['nbPages']
-
         if current_page < total_pages:
             query = raw_data['results'][0]['query']
             body = self.make_request_body(query, current_page+1)
             yield FormRequest(url=response.url, body=json.dumps(body), method='POST', callback=self.parse_products)
 
     def make_request_body(self, query, page_num=0):
-        return {"requests": [{"indexName": "enterprise_magento_english_products",
+        body = {"requests": [{"indexName": "enterprise_magento_english_products",
                               "params": f"query={query}&hitsPerPage=60&maxValuesPerFacet=60&page={page_num}"}
                              ]}
+        return json.dumps(body)
+
+    def add_parameters(self, url, raw_data):
+        url = add_or_replace_parameter(url, 'x-algolia-application-id', raw_data['applicationId'])
+        url = add_or_replace_parameter(url, 'x-algolia-api-key', raw_data['apiKey'])
+        return url
 
 
 class SixthStreetAESpider(MixinAE, SixthStreetSpider):
