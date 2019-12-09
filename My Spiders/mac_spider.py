f4157d0b06f38ec216e2745949cb193dc129f608
Mehar Hasnain <mehar.hasnain@arbisoft.com>
2019-12-09 12:46:20 +0500

    Revert "Separate parse method for colour requests"
    This reverts commit 89c86ea00a8afa22295bd19b6d8f2f4189a9a69f.

--- My Spiders/mac_spider.py
+++ My Spiders/mac_spider.py
@@ -8,6 +8,7 @@ from ..utils import map_gender, format_price
 
 class Mixin:
     retailer = 'mac'
+    csrf_token = ''
 
 
 class MixinAT(Mixin):
@@ -23,7 +24,6 @@ class MixinAT(Mixin):
 
 
 class MacParseSpider(MixinAT):
-    csrf_token = None
     
     def parse_product(self, response):
         product = Product()
@@ -43,13 +43,12 @@ class MacParseSpider(MixinAT):
 
         return self.request_or_product(product)
 
-    def parse_colours(self, response):
-        product = response.meta['product']
-        product['meta']['requests'] += self.sku_requests(response)
-        return self.request_or_product(product)
-
     def parse_skus(self, response):
         product = response.meta['product']
+
+        if not response.meta.get('size_request'):
+            product['meta']['requests'] += self.size_requests(response)
+
         product['skus'].update(self.get_skus(response))
         return self.request_or_product(product)
 
@@ -88,16 +87,21 @@ class MacParseSpider(MixinAT):
         return response.css('.image-slider--container [itemprop="image"]::attr(srcset)').getall()
 
     def get_skus(self, response):
+        skus = {}
+
         colour = response.css('.color-images--article-switcher .selected--option a::attr(title)').get()
         size = response.css('[checked="checked"] + [for="group[1]"]::text').get().strip()
         len = response.css('[checked="checked"] + [for="group[2]"]::text').get().strip()
 
         sku = {'colour': colour} if colour else {}
         sku.update(self.get_price(response))
-        sku['size'] = self.one_size if size in self.one_sizes else f"{size}_{len}"
+        sku['size'] = self.one_size if size.strip() in self.one_sizes else size.strip()
+        sku['length'] = len
         sku['out_of_stock'] = bool(response.css('#notifyHideBasket'))
 
-        return {f"{sku['colour']}_{sku['size']}" if colour else sku['size']: sku}
+        skus[f"{sku['colour']}_{sku['size']}_{len}" if colour else f"{sku['size']}_{len}"] = sku
+
+        return skus
 
     def get_price(self, response):
         currency = response.css('[itemprop="priceCurrency"]::attr(content)').get()
@@ -109,10 +113,10 @@ class MacParseSpider(MixinAT):
 
     def color_requests(self, response):
         color_urls = response.css('.color-images--article-switcher a::attr(href)').getall()
-        return [response.follow(url, callback=self.parse_colours, dont_filter=True) for url in color_urls]
+        return [response.follow(url, callback=self.parse_skus, dont_filter=True) for url in color_urls]
 
-    def sku_requests(self, response):
-        sku_requests = []
+    def size_requests(self, response):
+        size_requests = []
         size_ids = response.css('div:not(.is--disabled)>[name="group[1]"]::attr(value)').getall()
         length_ids = response.css('div:not(.is--disabled)>[name="group[2]"]::attr(value)').getall()
 
@@ -124,10 +128,10 @@ class MacParseSpider(MixinAT):
                     "__csrf_token": self.csrf_token
                 }
 
-                sku_requests.append(FormRequest(url=response.url, formdata=formdata,
-                                                callback=self.parse_skus, dont_filter=True))
+                size_requests.append(FormRequest(url=response.url, formdata=formdata, callback=self.parse_skus,
+                                                 meta={'size_request': True}, dont_filter=True))
 
-        return sku_requests
+        return size_requests
 
     def request_or_product(self, product):
         if product['meta']['requests']:
@@ -151,7 +155,7 @@ class MacCrawlSpider(Spider):
     product_parser = MacParseSpider()
 
     def parse(self, response):
-        self.product_parser.csrf_token = response.headers['X-Csrf-Token'].decode('utf-8')
+        Mixin.csrf_token = response.headers['X-Csrf-Token'].decode('utf-8')
         return response.follow(self.retailer_url, callback=self.parse_category,
                                meta=add_trail(response))
 
